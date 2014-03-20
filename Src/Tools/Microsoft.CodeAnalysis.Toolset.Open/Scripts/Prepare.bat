3c4e3acf13adc43f516bd93413ed1fdc21ddd9ad
KevinRansom <kevinr@microsoft.com>
2014-03-19 19:38:05 -0700

    Enable OpenSource Builders to debug their creations
     Change Description:
    
         This is the toolset changes to enable Open source developers to debug the results of their compiler hacking efforts.
    
         How it works:
         Unfortunately we can't do VSIX magic or binding redirect magic.  However, that leaves a pretty easy to understand solution.
         The Opensource developers will work in the defaultmain hive, their compiled product will be installed in the roslyndev hive.
         During the build we will overwrite the binaries in their deployed locations with the result of the build.
         They select CompilerPackage as their start package and press F5 this will build their product and deploy it to the start a new instance of Visual Studio using the Roslyndev hive and their new code.
    
         How to use it:
         There is a one time initialization prepare.bat, this must be run as an administrator, it creates the RoslynDev hive, Initializes and does sn - Vr on each Roslyn Assembly that the open source project builds
    
         to undo this, run prepare.bat with the u parameter.
    
         Once you have run prepare.bat set a global environment variable "OpenSourceDebug" to true just load up the roslyn solution and press F5, the build will put the right bits at the right place in the hive.  If the variable is not set then the normal build occurs and the RoslynHive will not be modified.
    
         There really isn't that much to it.
    
         Please Note I have hard coded the version # to 0.6.0.0 when building for OpenSourceDebug, builds otherwise the standard build applies.
    
         When the vbcscompiler has an app config based timeout switch, the build will replace the app.configs i n the package directories with equivalents from the open source build.
     (changeset 1209382)

---
+++ Src/Tools/Microsoft.CodeAnalysis.Toolset.Open/Scripts/Prepare.bat
@@ -0,0 +1,81 @@
+@echo off
+
+setlocal
+rem -----------------------------------------------
+rem is batch executing with adiMistrator privileges
+rem
+rem -----------------------------------------------
+set filename=%windir%\$%random%$
+(@echo Hello, this is the administrator >%filename% ) 2> nul && (del %filename% & set elevated=1) || (set elevated=0)
+if %elevated% == 0 (
+    echo This script requires an elevated command shell to run.
+    echo finished
+    goto :eof
+    )
+
+if "%targetVS%"=="" set targetVS=12.0
+if "%targetHive%"=="" set targetHive=Roslyn
+
+set PreviewVersion=0.6.40308.1
+set unconfigure=false
+set verification=-Vr 
+set label=Disable
+
+echo Prepare for Roslyn Open Source development
+echo.
+
+echo Create or clean the RoslynDev addin hive
+echo.
+
+if "%VSSDK120Install%" == "" ( 
+    echo ERROR!   To build and debug the .Net compilers Open source projects requires the latest Visual Studio SDK.
+    echo.
+    set errorlevel=1
+    goto :eof
+    )
+
+if /I "%1"=="u" (
+    set unconfigure=true
+    set verification=-Vu
+    set label=Enable
+    )
+
+if not "%unconfigure%" == "true" (
+    set unconfigure=false
+    set verification=-Vr 
+    set label=Disable
+    )
+
+if not "%unconfigure%" == "true" (
+    "%VSSDK120Install%\VisualStudioIntegration\Tools\Bin\CreateExpInstance.exe" /Reset /VSInstance=%targetVS% /RootSuffix=%TargetHive%  2> nul
+    )
+
+call :DisableVerificationFor Microsoft.CodeAnalysis.dll
+call :DisableVerificationFor Microsoft.CodeAnalysis.CSharp.dll
+call :DisableVerificationFor Microsoft.CodeAnalysis.VisualBasic.dll
+call :DisableVerificationFor Microsoft.CodeAnalysis.Workspaces.dll
+call :DisableVerificationFor Microsoft.CodeAnalysis.CSharp.Workspaces.dll
+call :DisableVerificationFor Microsoft.CodeAnalysis.VisualBasic.Workspaces.dll
+call :DisableVerificationFor CompilerPackage.dll
+call :DisableVerificationFor Roslyn.Compilers.BuildTasks.dll
+call :DisableVerificationFor rcsc.exe
+call :DisableVerificationFor rvbc.exe
+call :DisableVerificationFor VBCSCompiler.exe
+
+rem List skip verification entries
+echo.
+sn -q -Vl
+
+if "%unconfigure%" == "true" (
+    rem Reset the RoslynDev hive from the main hive, 
+    "%VSSDK120Install%\VisualStudioIntegration\Tools\Bin\CreateExpInstance.exe" /Clean /VSInstance=%targetVS% /RootSuffix=%TargetHive%  2> nul
+    reg delete HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\%targetVS%%TargetHive% /F > nul 2>&1
+    reg delete HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\%targetVS%%TargetHive%_Config /F > nul 2>&1
+    )
+
+goto :eof
+
+:DisableVerificationFor %1
+@echo %label% verification for %1
+for /F %%f in ('dir /s /b "%LOCALAPPDATA%\Microsoft\VisualStudio\%targetVS%%TargetHive%\%1"') do sn -q %verification% "%%f" && goto :eof
+goto :eof
