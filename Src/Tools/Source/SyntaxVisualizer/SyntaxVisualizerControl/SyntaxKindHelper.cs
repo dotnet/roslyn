372c0643b78afe384879723bdf77f8d928380646
shyamn <gnamboo@microsoft.com>
2014-03-24 16:43:00 -0700

    Port following change to preview branch
    1211735	Add syntax viuslizer
    
     (changeset 1213681)

---
+++ Src/Tools/Source/SyntaxVisualizer/SyntaxVisualizerControl/SyntaxKindHelper.cs
@@ -0,0 +1,76 @@
+// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+
+namespace Roslyn.SyntaxVisualizer.Control
+{
+    public static class SyntaxKindHelper
+    {
+        // Helpers that return the language-sepcific (C# / VB) SyntaxKind of a language-agnostic
+        // SyntaxNode / SyntaxToken / SyntaxTrivia.
+
+        public static string GetKind(this SyntaxNodeOrToken nodeOrToken)
+        {
+            var kind = string.Empty;
+
+            if (nodeOrToken.IsNode)
+            {
+                kind = nodeOrToken.AsNode().GetKind();
+            }
+            else
+            {
+                kind = nodeOrToken.AsToken().GetKind();
+            }
+
+            return kind;
+        }
+
+        public static string GetKind(this SyntaxNode node)
+        {
+            var kind = string.Empty;
+
+            if (node.Language == LanguageNames.CSharp)
+            {
+                kind = node.CSharpKind().ToString();
+            }
+            else 
+            {
+                kind = node.VisualBasicKind().ToString();
+            }
+
+            return kind;
+        }
+
+        public static string GetKind(this SyntaxToken token)
+        {
+            var kind = string.Empty;
+
+            if (token.Language == LanguageNames.CSharp)
+            {
+                kind = token.CSharpKind().ToString();
+            }
+            else 
+            {
+                kind = token.VisualBasicKind().ToString();
+            }
+
+            return kind;
+        }
+
+        public static string GetKind(this SyntaxTrivia trivia)
+        {
+            var kind = string.Empty;
+
+            if (trivia.Language == LanguageNames.CSharp)
+            {
+                kind = trivia.CSharpKind().ToString();
+            }
+            else
+            {
+                kind = trivia.VisualBasicKind().ToString();
+            }
+
+            return kind;
+        }
+    }
+}
---
+++ Src/Tools/Source/SyntaxVisualizer/SyntaxVisualizerDgmlHelper/SyntaxKindHelper.vb
@@ -0,0 +1,61 @@
+' Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports System.Runtime.CompilerServices
+Imports Microsoft.CodeAnalysis
+
+Friend Module SyntaxKindHelper
+    'Helpers that return the language-sepcific (C# / VB) SyntaxKind of a language-agnostic
+    'SyntaxNode / SyntaxToken / SyntaxTrivia.
+
+    <Extension()>
+    Friend Function GetKind(nodeOrToken As SyntaxNodeOrToken) As String
+        Dim kind = String.Empty
+
+        If nodeOrToken.IsNode Then
+            kind = nodeOrToken.AsNode().GetKind()
+        Else
+            kind = nodeOrToken.AsToken().GetKind()
+        End If
+
+        Return kind
+    End Function
+
+    <Extension()>
+    Friend Function GetKind(node As SyntaxNode) As String
+        Dim kind = String.Empty
+
+        If node.Language = LanguageNames.CSharp Then
+            kind = node.CSharpKind().ToString()
+        Else
+            kind = node.VisualBasicKind().ToString()
+        End If
+
+        Return kind
+    End Function
+
+    <Extension()>
+    Friend Function GetKind(token As SyntaxToken) As String
+        Dim kind = String.Empty
+
+        If token.Language = LanguageNames.CSharp Then
+            kind = token.CSharpKind().ToString()
+        Else
+            kind = token.VisualBasicKind().ToString()
+        End If
+
+        Return kind
+    End Function
+
+    <Extension()>
+    Friend Function GetKind(trivia As SyntaxTrivia) As String
+        Dim kind = String.Empty
+
+        If trivia.Language = LanguageNames.CSharp Then
+            kind = trivia.CSharpKind().ToString()
+        Else
+            kind = trivia.VisualBasicKind().ToString()
+        End If
+
+        Return kind
+    End Function
+End Module
