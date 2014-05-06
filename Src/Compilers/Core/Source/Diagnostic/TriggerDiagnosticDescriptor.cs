feeccd3201eb1729c53fc336337c24eb519b51c4
manishv <mavasani@microsoft.com>
2014-05-06 15:24:48 -0700

    Address code review feeedback for changeset 1251270 (Add "CustomTags" property to DiagnosticDescriptor and Diagnostic types) (changeset 1251448)

---
+++ Src/Compilers/Core/Source/Diagnostic/TriggerDiagnosticDescriptor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.CodeAnalysis
+{
+    /// <summary>
+    /// Provides a description about a <see cref="Diagnostic"/> that is a trigger for some custom behavior for code analyis clients.
+    /// </summary>
+    public class TriggerDiagnosticDescriptor : DiagnosticDescriptor
+    {
+        public static string TriggerCategory = "TriggerDiagnostic";
+
+        /// <summary>
+        /// Create a TriggerDiagnosticDescriptor, which provides description about a <see cref="Diagnostic"/> that is a trigger for some custom behavior for code analyis clients.
+        /// </summary>
+        /// <param name="id">A unique identifier for the diagnostic. For example, code analysis diagnostic ID "CA1001".</param>
+        /// <param name="customTags">Optional custom tags for the diagnostic. See <see cref="WellKnownDiagnosticTags"/> for some well known tags.</param>
+        public TriggerDiagnosticDescriptor(string id, params string[] customTags)
+            : base (id, description: "", messageFormat: "", category: TriggerCategory, defaultSeverity: DiagnosticSeverity.Hidden, isEnabledByDefault: true, customTags: customTags)
+        {
+        }
+    }
+}
--- Src/Compilers/Core/Source/Diagnostic/WellKnownDiagnosticTags.cs
+++ Src/Compilers/Core/Source/Diagnostic/WellKnownDiagnosticTags.cs
@@ -3,14 +3,6 @@
     public static class WellKnownDiagnosticTags
     {
         /// <summary>
-        /// Indicates that the diagnostic cannot be suppressed and its default severity cannot be escalated.
-        /// </summary>
-        /// <remarks>
-        /// Visual Studio Ruleset Editor doesn't display diagnostics with this tag as such diagnostics cannot be configured in the Ruleset Editor.
-        /// </remarks>
-        public const string CannotBeSuppressedOrEscalated = "CannotBeSuppressedOrEscalated";
-
-        /// <summary>
         /// Indicates that the diagnostic is related to some unnecessary source code.
         /// </summary>
         public const string Unnecessary = "Unnecessary";
