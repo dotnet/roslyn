e4449221f2e94429a3f342a0795cf8b939c30786
manishv <mavasani@microsoft.com>
2014-05-30 11:22:30 -0700

    Add support for a custom PreviewOperation CodeActionOperation: This enables clients to display custom WPF controls in the preview dialog. This preview control might have custom description, hyperlinks and other custom controls appropriate for the code action preview. (changeset 1267319)

---
+++ Src/Workspaces/Core/CodeActions/Operations/PreviewOperation.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+using System.Threading;
+
+namespace Microsoft.CodeAnalysis.CodeActions
+{
+    /// <summary>
+    /// Represents a preview operation for generating a custom user preview for the operation.
+    /// </summary>
+    public abstract class PreviewOperation : CodeActionOperation
+    {
+        /// <summary>
+        /// Gets a custom preview control for the operation.
+        /// If preview is null and <see cref="CodeActionOperation.Description"/> is non-null, then <see cref="CodeActionOperation.Description"/> is used to generate the preview.
+        /// </summary>
+        public abstract object GetPreview();
+    }
+}
