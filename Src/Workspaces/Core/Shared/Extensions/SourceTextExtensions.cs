4380d0ec4ccf3c96e72325157410c8767a087421
Pilchie <kevinpi@microsoft.com>
2014-06-19 11:30:55 -0700

    Support Uncomment Selection command when there is no selection, or it is a subset of the block comment. (changeset 1282151)

--- Src/Workspaces/Core/Shared/Extensions/SourceTextExtensions.cs
+++ Src/Workspaces/Core/Shared/Extensions/SourceTextExtensions.cs
@@ -148,6 +148,39 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
             return -1;
         }
 
+        public static int LastIndexOf(this SourceText text, string value, int startIndex, bool caseSensitive)
+        {
+            var normalized = caseSensitive ? value : value.ToLowerInvariant();
+            startIndex = startIndex + normalized.Length > text.Length
+                ? text.Length - normalized.Length
+                : startIndex;
+
+            for (var i = startIndex; i >= 0; i--)
+            {
+                var match = true;
+                for (var j = 0; j < normalized.Length; j++)
+                {
+                    // just use indexer of source text. perf of indexer depends on actual implementation of SourceText.
+                    // * all of our implementation at editor layer should provide either O(1) or O(logn).
+                    //
+                    // only one implementation we have that could have bad indexer perf is CompositeText with heavily modified text
+                    // at compiler layer but I believe that being used in find all reference will be very rare if not none.
+                    if (!Match(normalized[j], text[i + j], caseSensitive))
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
         private static readonly TextInfo invariantTextInfo = CultureInfo.InvariantCulture.TextInfo;
 
         private static bool Match(char nomalizedLeft, char right, bool caseSensitive)
