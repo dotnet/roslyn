3fc879748b9b80fc7936ff594ca8c839313fd1f7
chandera <rchande@microsoft.com>
2014-07-09 11:23:37 -0700

    When preselecting types, suggest an alias, if applicable. (changeset 1294282)

--- Src/Workspaces/CSharp/Extensions/ITypeSymbolExtensions.cs
+++ Src/Workspaces/CSharp/Extensions/ITypeSymbolExtensions.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
@@ -8,6 +11,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Simplification;
 using Microsoft.CodeAnalysis.Text;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp.Extensions
 {
@@ -41,6 +45,32 @@ namespace Microsoft.CodeAnalysis.CSharp.Extensions
             }
             while (containingType != null);
             return false;
+        }
+
+        public static async Task<ISymbol> FindApplicableAlias(this ITypeSymbol type, int position, SemanticModel semanticModel, CancellationToken cancellationToken)
+        {
+            var root = await semanticModel.GetOriginalSemanticModel().SyntaxTree.GetRootAsync(cancellationToken).ConfigureAwait(false);
+
+            IEnumerable<UsingDirectiveSyntax> applicableUsings = GetApplicableUsings(position, root as CompilationUnitSyntax);
+            foreach (var applicableUsing in applicableUsings)
+            {
+                var alias = semanticModel.GetOriginalSemanticModel().GetDeclaredSymbol(applicableUsing, cancellationToken);
+                if (alias != null && alias.Target == type)
+                {
+                    return alias;
+                }
+            }
+
+            return null;
+        }
+
+        private static IEnumerable<UsingDirectiveSyntax> GetApplicableUsings(int position, SyntaxNode root)
+        {
+            var namespaceUsings = root.FindToken(position).Parent.GetAncestors<NamespaceDeclarationSyntax>().SelectMany(n => n.Usings);
+            var allUsings = root is CompilationUnitSyntax
+                ? ((CompilationUnitSyntax)root).Usings.Concat(namespaceUsings)
+                : namespaceUsings;
+            return allUsings.Where(u => u.Alias != null);
         }
     }
 }
