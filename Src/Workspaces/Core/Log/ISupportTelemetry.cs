c1b008d2e8e2608d21d1ff6baa738e73e9346cf9
heejaechang <hechang@microsoft.com>
2014-07-18 12:44:01 -0700

    added Microsoft Internal only ISupportTelemetry interface
    this will be used by microsoft internal teams to mark whether their plugin's information such as diagnostic id is okay to be used for telemetry data or not. (changeset 1301447)

---
+++ Src/Workspaces/Core/Log/ISupportTelemetry.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.CodeAnalysis.Internal
+{
+    /// <summary>
+    /// Empty interface to mark supporting telemetry.
+    /// 
+    /// this is an interface for microsoft internal usage only.
+    /// </summary>
+    public interface ISupportTelemetry
+    {
+    }
+}
