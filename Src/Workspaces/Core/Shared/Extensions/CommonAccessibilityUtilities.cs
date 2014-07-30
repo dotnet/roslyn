b75d16882b8e7ce5cb528809ac92c6c226869539
srivatsn <srivatsn@microsoft.com>
2014-07-30 14:51:42 -0700

    Fixing 530858: Generate property: leads to CS0053 Inconsistent Accessibility
    When a property is generated inside an internal type but it's return type is protected, we were generating a public property which leads to inconsistent accessibility. While computing minimum accessibility, we were treating internal as more restrictive than protected which is not how the compiler sees it. I've changed it to this order of most restrictive to least restricitve : private > protectedandinternal > protected > internal > protectedorinternal > public. I've tests the matrix of combinartions and they all seem to produce compilable code now.  (changeset 1308601)

--- Src/Workspaces/Core/Shared/Extensions/CommonAccessibilityUtilities.cs
+++ Src/Workspaces/Core/Shared/Extensions/CommonAccessibilityUtilities.cs
@@ -15,7 +15,12 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
 
             if (accessibility1 == Accessibility.ProtectedAndInternal || accessibility2 == Accessibility.ProtectedAndInternal)
             {
-                return Accessibility.Internal;
+                return Accessibility.ProtectedAndInternal;
+            }
+
+            if (accessibility1 == Accessibility.Protected || accessibility2 == Accessibility.Protected)
+            {
+                return Accessibility.Protected;
             }
 
             if (accessibility1 == Accessibility.Internal || accessibility2 == Accessibility.Internal)
@@ -25,12 +30,7 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
 
             if (accessibility1 == Accessibility.ProtectedOrInternal || accessibility2 == Accessibility.ProtectedOrInternal)
             {
-                return Accessibility.Internal;
-            }
-
-            if (accessibility1 == Accessibility.Protected || accessibility2 == Accessibility.Protected)
-            {
-                return Accessibility.Protected;
+                return Accessibility.ProtectedOrInternal;
             }
 
             return Accessibility.Public;
