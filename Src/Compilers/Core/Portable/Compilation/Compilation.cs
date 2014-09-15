7f177c76775f27731fe02b859070e6c7266b622a
Pharring <pharring@microsoft.com>
2014-09-15 10:19:04 -0700

    PERF: String allocations in feature check. The compiler's feature check uses a dictionary keyed on string. The keys are case insensitive so, rather than using ".ToLowerInvariant" on every lookup, I added an equality comparer. Since this is portable, we don't have an InvariantCulture comparer, so I switched it to Ordinal. Ordinal comparison is sufficient for compiler feature flags. This was responsible for 0.1% of allocations in the C# typing test:      Name                                                                   Inc %          Inc      |+ mscorlib.ni!String.ToLower                                            0.1    7,276,920      | + Microsoft.CodeAnalysis.ni!Compilation.Feature                        0.1    7,062,104      | |+ Microsoft.CodeAnalysis.CSharp.ni!DisposeCheckerPass.Analyze         0.1    4,175,664
    because "DisposeCheckerPass" checks for the presence of the "checkdispose" feature. (changeset 1335644)

--- Src/Compilers/Core/Portable/Compilation/Compilation.cs
+++ Src/Compilers/Core/Portable/Compilation/Compilation.cs
@@ -1676,13 +1676,11 @@ namespace Microsoft.CodeAnalysis
         {
             if (this.lazyFeatures == null)
             {
-                var set = new Dictionary<string, string>();
+                var set = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
                 if (Options.Features != null)
                 {
-                    foreach (var s in Options.Features)
+                    foreach (var feature in Options.Features)
                     {
-                        string feature = s.ToLowerInvariant();
-
                         int colon = feature.IndexOf(':');
                         if (colon > 0)
                         {
@@ -1701,7 +1699,7 @@ namespace Microsoft.CodeAnalysis
             }
 
             string v;
-            return this.lazyFeatures.TryGetValue(p.ToLowerInvariant(), out v) ? v : null;
+            return this.lazyFeatures.TryGetValue(p, out v) ? v : null;
         }
 
         #endregion
