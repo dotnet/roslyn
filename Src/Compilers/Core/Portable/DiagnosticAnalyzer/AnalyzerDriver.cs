7f9df282f350df27e4e45be5b544e1db98ea93ec
manishv <mavasani@microsoft.com>
2014-11-04 17:30:18 -0800

    Fix build break from my previous checkin: Remove usings for deleted namespace Microsoft.CodeAnalysis.Diagnostics.Internal
    Pass me the virtual squirrel... (changeset 1366544)

--- Src/Compilers/Core/Portable/DiagnosticAnalyzer/AnalyzerDriver.cs
+++ Src/Compilers/Core/Portable/DiagnosticAnalyzer/AnalyzerDriver.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using Microsoft.CodeAnalysis.Collections;
-using Microsoft.CodeAnalysis.Diagnostics.Internal;
 using Roslyn.Utilities;
 using System;
 using System.Collections.Generic;
