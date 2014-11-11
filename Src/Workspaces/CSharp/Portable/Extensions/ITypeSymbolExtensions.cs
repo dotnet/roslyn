e8675f215369be28ef6a9e34d2ca1f3c740b1085
chandera <rchande@microsoft.com>
2014-11-11 13:00:42 -0800

    Fix a handful of completion bugs (changeset 1370118)

--- Src/Workspaces/CSharp/Portable/Extensions/ITypeSymbolExtensions.cs
+++ Src/Workspaces/CSharp/Portable/Extensions/ITypeSymbolExtensions.cs
@@ -48,15 +48,14 @@ namespace Microsoft.CodeAnalysis.CSharp.Extensions
         }
 
         public static async Task<ISymbol> FindApplicableAlias(this ITypeSymbol type, int position, SemanticModel semanticModel, CancellationToken cancellationToken)
-       { 
-            var token = semanticModel.SyntaxTree.FindTokenOnLeftOfPosition(position, cancellationToken);
-            var declaration = token.GetAncestor<MemberDeclarationSyntax>();
-            if (declaration != null)
+        { 
+            if (semanticModel.IsSpeculativeSemanticModel)
             {
-                position = declaration.SpanStart;
+                position = semanticModel.OriginalPositionForSpeculation;
+                semanticModel = semanticModel.ParentModel;
             }
 
-            var root = await semanticModel.GetOriginalSemanticModel().SyntaxTree.GetRootAsync(cancellationToken).ConfigureAwait(false);
+            var root = await semanticModel.SyntaxTree.GetRootAsync(cancellationToken).ConfigureAwait(false);
 
             IEnumerable<UsingDirectiveSyntax> applicableUsings = GetApplicableUsings(position, root as CompilationUnitSyntax);
             foreach (var applicableUsing in applicableUsings)
