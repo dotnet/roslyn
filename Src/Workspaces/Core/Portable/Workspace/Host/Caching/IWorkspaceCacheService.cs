e50106fc8fcf109cc6f4e1bfead59332c933f32d
Pharring <pharring@microsoft.com>
2014-12-02 11:50:58 -0800

    Introduce IWorkspaceCacheService which will be used to flush caches when the host determines that it needs to free memory. (changeset 1379254)

---
+++ Src/Workspaces/Core/Portable/Workspace/Host/Caching/IWorkspaceCacheService.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.CodeAnalysis.Host
+{
+    /// <summary>
+    /// Workspace service for cache implementations.
+    /// </summary>
+    internal interface IWorkspaceCacheService : IWorkspaceService
+    {
+        /// <summary>
+        /// May be raised by a Workspace host when available memory is getting low in order to request
+        /// that caches be flushed.
+        /// </summary>
+        event EventHandler CacheFlushRequested;
+    }
+}
