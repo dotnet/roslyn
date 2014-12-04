90070e40c6b0137c9776a96d382dfc82ee8710f2
heejaechang <hechang@microsoft.com>
2014-12-03 21:08:58 -0800

    removed unused usings. (changeset 1380676)

--- Src/Compilers/Core/Portable/DiagnosticAnalyzer/CompilerDiagnosticAnalyzer.CompilationAnalyzer.cs
+++ Src/Compilers/Core/Portable/DiagnosticAnalyzer/CompilerDiagnosticAnalyzer.CompilationAnalyzer.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 
 namespace Microsoft.CodeAnalysis.Diagnostics
 {
@@ -15,6 +13,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
         private class CompilationAnalyzer
         {
             private readonly Compilation compilation;
+
             public CompilationAnalyzer(Compilation compilation)
             {
                 this.compilation = compilation;
