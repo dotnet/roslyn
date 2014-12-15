9ee65cac92372961905c5d1068e2401928cf718e
angocke <angocke@microsoft.com>
2014-12-15 14:01:46 -0800

    Simplify updating LKG compiler & analyzers by introducing "global" NuGet references
    This changeset introduces the concept of a "global" nuget package. The references to the NuGet packages are inserted into the Open VSL.Settings.targets file, which is imported into every project in Roslyn. This ensures that every project automatically gets these "global" packages. (changeset 1386548)

--- Src/.nuget/NuGetRestore.ps1
+++ Src/.nuget/NuGetRestore.ps1
@@ -1,4 +1,5 @@
 $NuGetExe = "$PSScriptRoot\NuGet.exe"
 
+& $NuGetExe restore "$PSScriptRoot\packages.config" -PackagesDirectory "$PSScriptRoot\..\..\packages"
 & $NuGetExe restore "$PSScriptRoot\..\Roslyn.sln"
 & $NuGetExe restore "$PSScriptRoot\..\Samples\Samples.sln"
