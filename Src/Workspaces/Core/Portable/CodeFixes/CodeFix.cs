d3b3ea99c4a6722917f017f195c02e84913d34e4
shyamn <gnamboo@microsoft.com>
2014-12-15 18:09:43 -0800

    - Group code fixes offered in the light bulb by the issue being fixed (as opposed to by the provider that supplied the fix). The order of the offered fixes should mostly remain the same as before - however, they will now be grouped differently - in most cases this will result in fewer groups in the overall list + this should make it easier to understand where fixes for one issue end and next one begins (in cases where we have fixes for more than one diagnostic on a given line)... (This addresses bug 1077181)
    - Stop prioritizing fixes based on whether they are fixing a warning / error / hidden diagnostic. Fixes are already being ordered (based on ExtensionOrdering as well as based on proximity to cursor) - we shouldn't allow the priority setting to interfere with this ordering. We now use priority only for ordering fixes above refactorings and for ordering everything above the suppression fixes. This should also fix the issue where the rename tracking code fix would always show up at the bottom of the list below GFU fixes. (This addresses bugs 1090799 and 1074182) (changeset 1386745)

--- Src/Workspaces/Core/Portable/CodeFixes/CodeFix.cs
+++ Src/Workspaces/Core/Portable/CodeFixes/CodeFix.cs
@@ -17,6 +17,30 @@ namespace Microsoft.CodeAnalysis.CodeFixes
         internal readonly CodeAction Action;
         internal readonly ImmutableArray<Diagnostic> Diagnostics;
 
+        /// <summary>
+        /// This is the diagnostic that will show up in the preview pane header when a particular fix
+        /// is selected in the light bulb menu. We also group all fixes with the same <see cref="PrimaryDiagnostic"/>
+        /// together (into a single SuggestedActionSet) in the light bulb menu.
+        /// </summary>
+        /// <remarks>
+        /// A given fix can fix one or more diagnostics. However, our light bulb UI (preview pane, grouping
+        /// of fixes in the light bulb menu etc.) currently keeps things simple and pretends that
+        /// each fix fixes a single <see cref="PrimaryDiagnostic"/>.
+        /// 
+        /// Implementation-wise the <see cref="PrimaryDiagnostic"/> is always the first diagnostic that
+        /// the <see cref="CodeFixProvider"/> supplied when registering the fix (<see 
+        /// cref="CodeFixContext.RegisterFix(CodeAction, IEnumerable{Diagnostic})"/>). This could change
+        /// in the future, if we decide to change the UI to depict the true mapping between fixes and diagnostics
+        /// or if we decide to use some other heuristic to determine the <see cref="PrimaryDiagnostic"/>.
+        /// </remarks>
+        internal Diagnostic PrimaryDiagnostic
+        {
+            get
+            {
+                return Diagnostics[0];
+            }
+        }
+
         internal CodeFix(CodeAction action, Diagnostic diagnostic)
         {
             this.Action = action;
