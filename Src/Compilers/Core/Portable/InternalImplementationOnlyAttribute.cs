721c748e7755ad114768ba8cd575f9ced8c49a21
srivatsn <srivatsn@microsoft.com>
2015-01-09 12:15:10 -0800

    CR feedback: Adding AttributeUsage to the InternalImplementationOnlyAttribute. (changeset 1395250)

--- Src/Compilers/Core/Portable/InternalImplementationOnlyAttribute.cs
+++ Src/Compilers/Core/Portable/InternalImplementationOnlyAttribute.cs
@@ -6,6 +6,7 @@ namespace System.Runtime.CompilerServices
     /// This is a marker attribute that can be put on an interface to denote that only internal implementations
     /// of that interface should exist.
     /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
     internal sealed class InternalImplementationOnlyAttribute : Attribute
     {
     }
