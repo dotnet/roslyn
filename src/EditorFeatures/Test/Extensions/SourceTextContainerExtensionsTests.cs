d15260d974c336724d5c5170316f4afe66489ba8
Giftednewt <giftednewt@hotmail.com>
2015-02-02 21:05:27 +0000

    resolves #38 by fixing typo in TextEditorResources and adding unit tests for SourceContainerExtensions

---
+++ src/EditorFeatures/Test/Extensions/SourceTextContainerExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis.Text;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.UnitTests.Extensions
+{
+    public class SourceTextContainerExtensionsTests
+    {
+        [Fact]
+        public void GetBufferTextFromNonTextContainerThrows()
+        {
+            var containerMock = new Mock<SourceTextContainer>();
+            Assert.Throws<ArgumentException>(containerMock.Object.GetTextBuffer);
+        }
+
+        [Fact]
+        public void GetBufferTextFromTextContainerReturnsTextBuffer()
+        {
+            var textSnapshotMock = new Mock<VisualStudio.Text.ITextSnapshot>();
+            var bufferMock = new Mock<VisualStudio.Text.ITextBuffer>();
+            bufferMock.SetupGet(x => x.CurrentSnapshot).Returns(textSnapshotMock.Object);
+            bufferMock.SetupGet(x => x.Properties).Returns(new VisualStudio.Utilities.PropertyCollection());
+
+            var textContainer = Microsoft.CodeAnalysis.Text.Extensions.TextBufferContainer.From(bufferMock.Object);
+
+
+            var textBuffer = textContainer.GetTextBuffer();
+
+            Assert.Equal(bufferMock.Object, textBuffer);
+        }
+    }
+}
