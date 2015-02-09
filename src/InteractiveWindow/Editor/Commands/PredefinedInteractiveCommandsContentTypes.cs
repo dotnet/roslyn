6857b458ad6f6f1a0e47699ca3fa59c8d6853986
RoslynTeam <roslynteam@microsoft.com>
2015-02-08 18:18:53 -0800

    Fix Roslyn.sln build
    This makes the necessary changes to allow for Roslyn.sln to build from
    the command line.

--- src/InteractiveWindow/Editor/Commands/PredefinedInteractiveCommandsContentTypes.cs
+++ src/InteractiveWindow/Editor/Commands/PredefinedInteractiveCommandsContentTypes.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Utilities;
 
-namespace Roslyn.Editor.InteractiveWindow.Commands
+namespace Microsoft.VisualStudio.InteractiveWindow.Commands
 {
     public static class PredefinedInteractiveCommandsContentTypes
     {
