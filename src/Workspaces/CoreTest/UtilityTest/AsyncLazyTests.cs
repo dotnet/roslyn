8b8889d5e5fc8dd105a2265b14327394a54ce669
Jason Malinowski <jason.malinowski@microsoft.com>
2015-02-09 16:35:16 -0800

    Disable tests related to GitHub issue #204

--- src/Workspaces/CoreTest/UtilityTest/AsyncLazyTests.cs
+++ src/Workspaces/CoreTest/UtilityTest/AsyncLazyTests.cs
@@ -125,7 +125,7 @@ namespace Microsoft.CodeAnalysis.UnitTests
             Assert.Equal(expectedTaskStatus, observedAntecedentTaskStatus.Value);
         }
 
-        [Fact]
+        [Fact(Skip = "GitHub Issue 204")]
         [Trait(Traits.Feature, Traits.Features.AsyncLazy)]
         public void CancellationDuringInlinedComputationFromGetValueAsyncStillCachesResult()
         {
@@ -159,7 +159,7 @@ namespace Microsoft.CodeAnalysis.UnitTests
             }
         }
 
-        [Fact]
+        [Fact(Skip = "GitHub Issue 204")]
         [Trait(Traits.Feature, Traits.Features.AsyncLazy)]
         public void CancellationDuringInlinedComputationFromGetValueWithoutSynchronousComputationStillCachesResult()
         {
