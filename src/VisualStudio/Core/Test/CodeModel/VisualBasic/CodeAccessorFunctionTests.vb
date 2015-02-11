4b9ad62b11e778a32bc4c9a13a814f1605abf56c
Dustin Campbell <dustin@teamcampbell.org>
2015-02-11 08:32:37 -0800

    Move unit tests to correct file

--- src/VisualStudio/Core/Test/CodeModel/VisualBasic/CodeAccessorFunctionTests.vb
+++ src/VisualStudio/Core/Test/CodeModel/VisualBasic/CodeAccessorFunctionTests.vb
@@ -1,8 +1,6 @@
 ' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-Imports System.Runtime.InteropServices
 Imports Microsoft.CodeAnalysis
-Imports Microsoft.CodeAnalysis.Text
 Imports Roslyn.Test.Utilities
 
 Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.CodeModel.VisualBasic
@@ -47,6 +45,91 @@ End Class
 </Code>
 
             TestAccess(code, EnvDTE.vsCMAccess.vsCMAccessPrivate)
+        End Sub
+
+#End Region
+
+#Region "FunctionKind tests"
+
+        <ConditionalFact(GetType(x86)), Trait(Traits.Feature, Traits.Features.CodeModel)>
+        Public Sub FunctionKind_AddHandler()
+            Dim code =
+<Code>
+Imports System
+
+Public Class C1
+
+   Public Custom Event E1 As EventHandler
+
+      $$AddHandler(ByVal value As EventHandler)
+      End AddHandler
+
+      RemoveHandler(ByVal value As EventHandler)
+      End RemoveHandler
+
+      RaiseEvent(ByVal sender As Object, ByVal e As EventArgs)
+      End RaiseEvent
+
+   End Event
+
+End Clas
+</Code>
+
+            TestFunctionKind(code, EnvDTE80.vsCMFunction2.vsCMFunctionAddHandler)
+        End Sub
+
+        <ConditionalFact(GetType(x86)), Trait(Traits.Feature, Traits.Features.CodeModel)>
+        Public Sub FunctionKind_RemoveHandler()
+            Dim code =
+<Code>
+Imports System
+
+Public Class C1
+
+   Public Custom Event E1 As EventHandler
+
+      AddHandler(ByVal value As EventHandler)
+      End AddHandler
+
+      $$RemoveHandler(ByVal value As EventHandler)
+      End RemoveHandler
+
+      RaiseEvent(ByVal sender As Object, ByVal e As EventArgs)
+      End RaiseEvent
+
+   End Event
+
+End Clas
+</Code>
+
+            TestFunctionKind(code, EnvDTE80.vsCMFunction2.vsCMFunctionRemoveHandler)
+        End Sub
+
+        <ConditionalFact(GetType(x86)), Trait(Traits.Feature, Traits.Features.CodeModel)>
+        Public Sub FunctionKind_RaiseEvent()
+            Dim code =
+<Code>
+Imports System
+
+Public Class C1
+
+   Public Custom Event E1 As EventHandler
+
+      AddHandler(ByVal value As EventHandler)
+      End AddHandler
+
+      RemoveHandler(ByVal value As EventHandler)
+      End RemoveHandler
+
+      $$RaiseEvent(ByVal sender As Object, ByVal e As EventArgs)
+      End RaiseEvent
+
+   End Event
+
+End Clas
+</Code>
+
+            TestFunctionKind(code, EnvDTE80.vsCMFunction2.vsCMFunctionRaiseEvent)
         End Sub
 
 #End Region
