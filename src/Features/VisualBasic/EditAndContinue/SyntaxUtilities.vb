9326ce9bd0c43ea5b9958b153060a545b16fc1fd
Tomas Matousek <tomat@microsoft.com>
2015-02-19 09:15:23 -0800

    EnC: Calculate reverse map for each updated method containing lambdas

--- src/Features/VisualBasic/EditAndContinue/SyntaxUtilities.vb
+++ src/Features/VisualBasic/EditAndContinue/SyntaxUtilities.vb
@@ -1,14 +1,7 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 Imports System.Collections.Immutable
-Imports System.Composition
 Imports System.Runtime.InteropServices
-Imports System.Threading
-Imports Microsoft.CodeAnalysis.Differencing
-Imports Microsoft.CodeAnalysis.EditAndContinue
-Imports Microsoft.CodeAnalysis.Host
-Imports Microsoft.CodeAnalysis.Host.Mef
-Imports Microsoft.CodeAnalysis.Text
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.EditAndContinue
@@ -16,8 +9,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.EditAndContinue
         <Conditional("DEBUG")>
         Public Shared Sub AssertIsBody(syntax As SyntaxNode, allowLambda As Boolean)
             ' lambda/query
-            Dim body As SyntaxNode = Nothing
-            If IsLambdaBodyStatement(syntax, body) AndAlso syntax Is body Then
+            If IsLambdaBody(syntax) Then
                 Debug.Assert(allowLambda)
                 Debug.Assert(TypeOf syntax Is ExpressionSyntax OrElse TypeOf syntax Is LambdaHeaderSyntax)
                 Return
@@ -60,58 +52,84 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.EditAndContinue
             Debug.Assert(False)
         End Sub
 
-        Public Shared Function IsLambdaBodyStatement(node As SyntaxNode, <Out> ByRef body As SyntaxNode) As Boolean
-            Dim parent = node.Parent
+        Public Shared Function IsLambdaBody(node As SyntaxNode) As Boolean
+            Dim body As SyntaxNode = Nothing
+            Return IsLambdaBodyStatementOrExpression(node, body) AndAlso node Is body
+        End Function
+
+        ''' <summary>
+        ''' Returns true if the specified <paramref name="node"/> is a statement whose parent is a <see cref="LambdaExpressionSyntax"/>,
+        ''' or and expression of a query lambda. Returns the node that represents the body of the lambda in <paramref name="body"/>.
+        ''' </summary>
+        Public Shared Function IsLambdaBodyStatementOrExpression(node As SyntaxNode, <Out> Optional ByRef body As SyntaxNode = Nothing) As Boolean
+            Dim parent = node?.Parent
             If parent Is Nothing Then
                 body = Nothing
                 Return False
             End If
 
-            body = node
-            Select Case parent.Kind
+            Dim body1 As SyntaxNode = Nothing
+            Dim body2 As SyntaxNode = Nothing
+
+            Return TryGetLambdaBodies(parent, body1, body2) AndAlso
+                   (node Is body1 OrElse node Is body2)
+        End Function
+
+        Public Shared Function TryGetLambdaBodies(node As SyntaxNode, <Out> ByRef body1 As SyntaxNode, <Out> ByRef body2 As SyntaxNode) As Boolean
+            body1 = Nothing
+            body2 = Nothing
+
+            Select Case node.Kind
                 Case SyntaxKind.MultiLineFunctionLambdaExpression,
                      SyntaxKind.SingleLineFunctionLambdaExpression,
                      SyntaxKind.MultiLineSubLambdaExpression,
                      SyntaxKind.SingleLineSubLambdaExpression
                     ' The header of the lambda represents its body.
-                    body = DirectCast(parent, LambdaExpressionSyntax).SubOrFunctionHeader
+                    body1 = DirectCast(node, LambdaExpressionSyntax).SubOrFunctionHeader
                     Return True
 
                 Case SyntaxKind.WhereClause
-                    Dim whereClause = DirectCast(parent, WhereClauseSyntax)
-                    Return whereClause.Condition Is node
+                    body1 = DirectCast(node, WhereClauseSyntax).Condition
+                    Return True
 
                 ' source sequence in From and Aggregate (other than the first in the query)
                 Case SyntaxKind.CollectionRangeVariable
-                    Dim collectionRange = DirectCast(parent, CollectionRangeVariableSyntax)
-                    Return collectionRange.Expression Is node AndAlso Not IsFirstInQuery(collectionRange)
+                    Dim collectionRange = DirectCast(node, CollectionRangeVariableSyntax)
+                    If IsFirstInQuery(collectionRange) Then
+                        Return False
+                    End If
+
+                    body1 = collectionRange.Expression
+                    Return True
 
                 ' function call in Group By, Group Join, Aggregate: the argument 
                 Case SyntaxKind.FunctionAggregation
-                    Dim functionAggregation = DirectCast(parent, FunctionAggregationSyntax)
-                    Return functionAggregation.Argument Is node
+                    body1 = DirectCast(node, FunctionAggregationSyntax).Argument
+                    Return True
 
                 ' variable in Let, Select, Group By: the RHS
                 Case SyntaxKind.ExpressionRangeVariable
-                    Dim expressionRange = DirectCast(parent, ExpressionRangeVariableSyntax)
-                    Return expressionRange.Expression Is node
+                    body1 = DirectCast(node, ExpressionRangeVariableSyntax)
+                    Return True
 
                 Case SyntaxKind.TakeWhileClause,
                      SyntaxKind.SkipWhileClause
-                    Dim partitionWhileClause = DirectCast(parent, PartitionWhileClauseSyntax)
-                    Return partitionWhileClause.Condition Is node
+                    body1 = DirectCast(node, PartitionWhileClauseSyntax).Condition
+                    Return True
 
                 Case SyntaxKind.AscendingOrdering,
                      SyntaxKind.DescendingOrdering
-                    Dim ordering = DirectCast(parent, OrderingSyntax)
-                    Return ordering.Expression Is node
+                    body1 = DirectCast(node, OrderingSyntax).Expression
+                    Return True
 
                 Case SyntaxKind.JoinCondition
-                    Dim joinCondition = DirectCast(parent, JoinConditionSyntax)
-                    Return joinCondition.Left Is node OrElse joinCondition.Right Is node
+                    Dim joinCondition = DirectCast(node, JoinConditionSyntax)
+                    body1 = joinCondition.Left
+                    body2 = joinCondition.Right
+                    Return True
             End Select
 
-            Debug.Assert(Not SyntaxUtilities.IsLambda(parent.Kind))
+            Debug.Assert(Not IsLambda(node))
             Return False
         End Function
 
@@ -190,6 +208,10 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.EditAndContinue
 
         Public Shared Function IsNotLambda(node As SyntaxNode) As Boolean
             Return Not IsLambda(node.Kind())
+        End Function
+
+        Public Shared Function IsLambda(node As SyntaxNode) As Boolean
+            Return IsLambda(node.Kind())
         End Function
 
         Public Shared Function IsLambda(kind As SyntaxKind) As Boolean
