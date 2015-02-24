469133b31c8d0087117c953f28e0db2697192203
Brett Forsgren <brettfo@microsoft.com>
2015-02-23 17:02:18 -0800

    update unit tests to use localized resources

--- src/EditorFeatures/VisualBasicTest/Completion/CompletionProviders/SymbolCompletionProviderTests.vb
+++ src/EditorFeatures/VisualBasicTest/Completion/CompletionProviders/SymbolCompletionProviderTests.vb
@@ -2795,7 +2795,7 @@ Class SomeClass
     End Sub
 End Class</Code>.Value
 
-            VerifyItemExists(code, "Foo", "(Deprecated) Sub SomeClass.Foo()")
+            VerifyItemExists(code, "Foo", $"({VBFeaturesResources.Deprecated}) Sub SomeClass.Foo()")
         End Sub
 
         <Fact, Trait(Traits.Feature, Traits.Features.Completion)>
