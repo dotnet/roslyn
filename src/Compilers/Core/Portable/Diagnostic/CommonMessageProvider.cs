6b92b1820c6dcf1198366e56db600e768bccc227
mavasani <mavasani@microsoft.com>
2015-03-12 03:59:45 -0700

    Add a new switch '/errorlog:' to VB and C# command line compilers to log all the compiler diagnostics into a log file. Add command line parsing tests and error log content validation tests.

--- src/Compilers/Core/Portable/Diagnostic/CommonMessageProvider.cs
+++ src/Compilers/Core/Portable/Diagnostic/CommonMessageProvider.cs
@@ -151,6 +151,7 @@ namespace Microsoft.CodeAnalysis
         public abstract int WRN_AnalyzerCannotBeCreated { get; }
         public abstract int WRN_NoAnalyzerInAssembly { get; }
         public abstract int ERR_CantReadRulesetFile { get; }
+        public abstract int ERR_CompileCancelled { get; }
 
         // compilation options:
         public abstract int ERR_BadCompilationOptionValue { get; }
