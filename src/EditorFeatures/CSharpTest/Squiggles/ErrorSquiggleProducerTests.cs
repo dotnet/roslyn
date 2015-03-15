5b9ccdeabda820ff2cc443ff2022cc396720877b
Heejae Chang <heejaechang@outlook.com>
2015-03-15 03:39:03 -0700

    added unit test for build zero length squiggle change

--- src/EditorFeatures/CSharpTest/Squiggles/ErrorSquiggleProducerTests.cs
+++ src/EditorFeatures/CSharpTest/Squiggles/ErrorSquiggleProducerTests.cs
@@ -10,9 +10,11 @@ using Microsoft.CodeAnalysis.CSharp.Diagnostics.SimplifyTypeNames;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Editor.UnitTests.Squiggles;
 using Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.Text.Adornments;
 using Microsoft.VisualStudio.Text.Tagging;
 using Roslyn.Test.Utilities;
+using Roslyn.Utilities;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Squiggles
@@ -123,6 +125,82 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Squiggles
             var firstSpan = spans.First();
             Assert.Equal(PredefinedErrorTypeNames.SyntaxError, firstSpan.Tag.ErrorType);
             Assert.Contains("Bar", (string)firstSpan.Tag.ToolTipContent, StringComparison.Ordinal);
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.ErrorSquiggles)]
+        public void BuildErrorZeroLengthSpan()
+        {
+            var workspaceXml =
+@"<Workspace>
+    <Project Language=""C#"" CommonReferences=""true"">
+        <Document FilePath = ""Test.cs"" >
+            class Test
+{
+}
+        </Document>
+    </Project>
+</Workspace>";
+
+            using (var workspace = TestWorkspaceFactory.CreateWorkspace(workspaceXml))
+            {
+                var document = workspace.Documents.First();
+
+                var updateArgs = new DiagnosticsUpdatedArgs(
+                        new object(), workspace, workspace.CurrentSolution, document.Project.Id, document.Id,
+                        ImmutableArray.Create(
+                            CreateDiagnosticData(workspace, document, new TextSpan(0, 0)),
+                            CreateDiagnosticData(workspace, document, new TextSpan(0, 1))));
+
+                var spans = GetErrorsFromUpdateSource(workspace, document, updateArgs);
+
+                Assert.Equal(1, spans.Count());
+                var first = spans.First();
+
+                Assert.Equal(1, first.Span.Span.Length);
+            }
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.ErrorSquiggles)]
+        public void LiveErrorZeroLengthSpan()
+        {
+            var workspaceXml =
+@"<Workspace>
+    <Project Language=""C#"" CommonReferences=""true"">
+        <Document FilePath = ""Test.cs"" >
+            class Test
+{
+}
+        </Document>
+    </Project>
+</Workspace>";
+
+            using (var workspace = TestWorkspaceFactory.CreateWorkspace(workspaceXml))
+            {
+                var document = workspace.Documents.First();
+
+                var updateArgs = new DiagnosticsUpdatedArgs(
+                        new LiveId(), workspace, workspace.CurrentSolution, document.Project.Id, document.Id,
+                        ImmutableArray.Create(
+                            CreateDiagnosticData(workspace, document, new TextSpan(0, 0)),
+                            CreateDiagnosticData(workspace, document, new TextSpan(0, 1))));
+
+                var spans = GetErrorsFromUpdateSource(workspace, document, updateArgs);
+
+                Assert.Equal(2, spans.Count());
+                var first = spans.First();
+                var second = spans.Last();
+
+                Assert.Equal(1, first.Span.Span.Length);
+                Assert.Equal(1, second.Span.Span.Length);
+            }
+        }
+
+        private class LiveId : UpdateArgsId
+        {
+            // use just a random analyzer
+            public LiveId() : base(new CSharpSimplifyTypeNamesDiagnosticAnalyzer())
+            {
+            }
         }
 
         private static IEnumerable<ITagSpan<IErrorTag>> GetErrorSpans(params string[] content)
