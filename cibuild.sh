56c2c8bf63a015f240fe8fbfd5d0e9f98ac76c86
Jared Parsons <jaredpparsons@gmail.com>
2015-03-17 17:37:57 -0700

    Made the script more robust

--- cibuild.sh
+++ cibuild.sh
@@ -2,9 +2,17 @@
 
 echo Changing mono snapshot
 . mono-snapshot mono/20150316155603
+if [ $? -ne 0 ]; then
+    echo Could not set mono snapshot 
+    exit 1
+fi
 
 echo Restoring NuGet packages
 mono src/.nuget/NuGet.exe restore src/Roslyn.sln
+if [ $? -ne 0 ]; then
+    echo Failed restoring NuGet packages
+    exit 1
+fi
 
 cd src/Compilers/CSharp/csc
 
