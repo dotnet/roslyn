7890a0531ea3d0a8ddfea59ef456491e39887c31
jaredpar <jaredpar@microsoft.com>
2015-03-17 17:09:35 -0700

    Fix the snapshot command

--- cibuild.sh
+++ cibuild.sh
@@ -1,4 +1,4 @@
-. mono-snapshot 20150316155603
+. mono-snapshot mono/20150316155603
 mono src/.nuget/NuGet.exe restore src/Roslyn.sln
 cd src/Compilers/CSharp/csc
 xbuild /p:SignAssembly=false /p:DebugSymbols=false /p:DefineConstants=COMPILERCORE,DEBUG csc.csproj
