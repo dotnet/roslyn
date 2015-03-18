65460d07d8d52a02cc0a86564e4e2c40f753950d
jaredpar <jaredpar@microsoft.com>
2015-03-17 17:24:33 -0700

    One more update to the build script

--- cibuild.sh
+++ cibuild.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
+echo Changing mono snapshot
 . mono-snapshot mono/20150316155603
+
+echo Restoring NuGet packages
 mono src/.nuget/NuGet.exe restore src/Roslyn.sln
+
 cd src/Compilers/CSharp/csc
+
+echo Compiling
 xbuild /p:SignAssembly=false /p:DebugSymbols=false /p:DefineConstants=COMPILERCORE,DEBUG csc.csproj
