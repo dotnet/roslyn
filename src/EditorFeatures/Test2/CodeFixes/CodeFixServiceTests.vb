849ba7831216261a8a842c2fee3a17ab4e8517af
Jonathon Marolf <jmarolf@microsoft.com>
2015-03-24 11:06:01 -0700

    fixing VB Tests
    fixing VB Tests

--- src/EditorFeatures/Test2/CodeFixes/CodeFixServiceTests.vb
+++ src/EditorFeatures/Test2/CodeFixes/CodeFixServiceTests.vb
@@ -36,6 +36,7 @@ Namespace Microsoft.CodeAnalysis.Editor.Implementation.CodeFixes.UnitTests
                 Dim analyzer = diagnosticService.CreateIncrementalAnalyzer(workspace)
                 Dim codefixService = New CodeFixService(
                                         diagnosticService,
+                                        workspace,
                                         {New Lazy(Of CodeFixProvider, Mef.CodeChangeProviderMetadata)(
                                             Function() workspaceCodeFixProvider,
                                             New Mef.CodeChangeProviderMetadata(New Dictionary(Of String, Object)() From {{"Name", "C#"}, {"Languages", {LanguageNames.CSharp}}}))},
@@ -99,6 +100,7 @@ Namespace Microsoft.CodeAnalysis.Editor.Implementation.CodeFixes.UnitTests
 
                 Dim codefixService = New CodeFixService(
                                         diagnosticService,
+                                        workspace,
                                         {New Lazy(Of CodeFixProvider, Mef.CodeChangeProviderMetadata)(
                                             Function() workspaceCodeFixProvider,
                                             New Mef.CodeChangeProviderMetadata(New Dictionary(Of String, Object)() From {{"Name", "C#"}, {"Languages", {LanguageNames.CSharp}}}))},
