9049eef61c2b86ed78f86184425e78c9f913a2c1
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-03-25 16:03:14 -0700

    Allow subclasses of AbstractCompletionService to customize more behavior (esp. around MRU).
    Right now TypeScript doesn't want the precise MRU algorithm you get by default with Roslyn's
    completion list.  For example, we are working toward a concept of 'strong' completion
    suggestions and 'weak' completion suggestions.  We'd prefer a 'strong' completion suggestion
    be preselected over a 'weak' one if both are viable.
    
    However, the MRU logic is baked into the existing completion service code.  I've broken this
    out in a couple of ways.  First, instead of referring to MRU anywhere in the API, there are
    just simple methods used for asking 'should i preselect this?' and 'is this item better than
    this other item'.  By default these are implemented by using the MRU list.  However, a subclass
    can use another mechanism.

--- src/Features/Core/Completion/ICompletionService.cs
+++ src/Features/Core/Completion/ICompletionService.cs
@@ -24,11 +24,6 @@ namespace Microsoft.CodeAnalysis.Completion
         ICompletionRules GetDefaultCompletionRules();
 
         /// <summary>
-        /// Clears the most-recently-used cache used by completion.
-        /// </summary>
-        void ClearMRUCache();
-
-        /// <summary>
         /// Returns the CompletionItemGroups for the specified position in the document.
         /// </summary>
         Task<IEnumerable<CompletionItemGroup>> GetGroupsAsync(Document document, int position, CompletionTriggerInfo triggerInfo, IEnumerable<ICompletionProvider> completionProviders, CancellationToken cancellationToken);
@@ -49,6 +44,25 @@ namespace Microsoft.CodeAnalysis.Completion
         /// if a snippet exists with a shortcut that matches the completion item's insertion text.
         /// </summary>
         Task<string> GetSnippetExpansionNoteForCompletionItemAsync(CompletionItem completionItem, Workspace workspace);
+
+        /// <summary>
+        /// Called when a completion item is committed.  The completion service can then use this to
+        /// help inform future completion requests.  For example, it can use this information to 
+        /// help decide if this item is 'better' than another when queried.
+        /// </summary>
+        void CompletionItemComitted(CompletionItem item);
+
+        /// <summary>
+        /// Returns true if 'item1' is a better match for the filter text typed so far versus 
+        /// 'item2'.  The better match will be selected after text is typed.
+        /// </summary>
+        bool IsBetterFilterMatch(CompletionItem item1, CompletionItem item2);
+
+        /// <summary>
+        /// Used to allow the completion service to indicate if this item should be preselected.
+        /// Only called when no filter text has been provided.
+        /// </summary>
+        bool ShouldPreselect(CompletionItem item);
 
         /// <summary>
         /// True if the completion list should be dismissed if the user's typing causes it to filter
---
+++ src/Features/Core/Completion/MostRecentlyUsedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.CodeAnalysis.Completion
+{
+    internal class MostRecentlyUsedList<T>
+    {
+        private readonly object gate = new object();
+        private readonly List<T> items = new List<T>();
+        private readonly int maxSize;
+
+        public MostRecentlyUsedList(int maxSize = 10)
+        {
+            this.maxSize = maxSize;
+        }
+
+        public void Add(T value)
+        {
+            lock (gate)
+            {
+                var removed = items.Remove(value);
+                if (!removed && items.Count == maxSize)
+                {
+                    items.RemoveAt(0);
+                }
+
+                items.Add(value);
+            }
+        }
+
+        public int IndexOf(T value)
+        {
+            lock (gate)
+            {
+                return items.IndexOf(value);
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            lock (gate)
+            {
+                return items.Contains(value);
+            }
+        }
+
+        public void Clear()
+        {
+            lock (gate)
+            {
+                items.Clear();
+            }
+        }
+    }
+}
