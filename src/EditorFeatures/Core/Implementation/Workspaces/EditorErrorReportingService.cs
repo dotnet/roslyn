dotnet/roslyn

commit 11a827c4f7ea5cb72cfe559b7cc4664d59afa095

Responding to PR feedback
    - Adding code refactoring tests
    - Using the error logger for cases where there is not UI to show the user
    - Made sure the cookie registered with the IDE gets unregistered on close
    - Responding to general fixup comments

src/EditorFeatures/Core/Implementation/Workspaces/EditorErrorReportingService.cs
@@ -1,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Extensions;
+using Microsoft.CodeAnalysis.Internal.Log;
+
+namespace Microsoft.CodeAnalysis.Editor.Implementation.Workspaces
+{
+    internal class EditorErrorReportingService : IErrorReportingService
+    {
+        public void ShowErrorInfoForCodeFix(string codefixName, Action OnEnableClicked, Action OnEnableAndIgnoreClicked)
+        {
+            var message = LogMessage.Create($"{codefixName} crashed");
+            Logger.Log(FunctionId.CodeFixe_Crash, message);
+        }
+    }
+}

src/Features/Core/CodeFixes/CodeFixService.cs
@@ -19,6 +19,7 @@ using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CodeFixes
 {
+    using Microsoft.CodeAnalysis.ErrorLogger;
     using DiagnosticId = String;
     using LanguageKind = String;
 
@@ -39,24 +40,23 @@ namespace Microsoft.CodeAnalysis.CodeFixes
         private readonly ConditionalWeakTable<AnalyzerReference, ProjectCodeFixProvider>.CreateValueCallback _createProjectCodeFixProvider;
 
         private readonly ImmutableDictionary<LanguageKind, Lazy<ISuppressionFixProvider>> _suppressionProvidersMap;
+        private readonly IErrorLoggerService _errorLogger;
 
         private ImmutableDictionary<CodeFixProvider, FixAllProviderInfo> _fixAllProviderMap;
-
-        private readonly IExtensionManager _extensionManager;
 
         [ImportingConstructor]
         public CodeFixService(
             IDiagnosticAnalyzerService service,
-            Workspace workspace,
+            IErrorLoggerService logger,
             [ImportMany]IEnumerable<Lazy<CodeFixProvider, CodeChangeProviderMetadata>> fixers,
             [ImportMany]IEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>> suppressionProviders)
         {
+            _errorLogger = logger;
             _diagnosticService = service;
-            _extensionManager = workspace.Services.GetService<IExtensionManager>();
             var fixersPerLanguageMap = fixers.ToPerLanguageMapWithMultipleLanguages();
             var suppressionProvidersPerLanguageMap = suppressionProviders.ToPerLanguageMapWithMultipleLanguages();
 
-            _workspaceFixersMap = GetFixerPerLanguageMap(fixersPerLanguageMap);
+            _workspaceFixersMap = GetFixerPerLanguageMap(fixersPerLanguageMap, null);
             _suppressionProvidersMap = GetSuppressionProvidersPerLanguageMap(suppressionProvidersPerLanguageMap);
 
             // REVIEW: currently, fixer's priority is statically defined by the fixer itself. might considering making it more dynamic or configurable.
@@ -189,12 +189,13 @@ namespace Microsoft.CodeAnalysis.CodeFixes
 
             var tree = await document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
             var diagnostics = diagnosticDataCollection.Select(data => data.ToDiagnostic(tree)).ToImmutableArray();
+            var extensionManager = document.Project.Solution.Workspace.Services.GetService<IExtensionManager>();
 
             foreach (var fixer in allFixers.Distinct())
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
-                Func<Diagnostic, bool> hasFix = (d) => this.GetFixableDiagnosticIds(fixer).Contains(d.Id);
+                Func<Diagnostic, bool> hasFix = (d) => this.GetFixableDiagnosticIds(fixer, extensionManager).Contains(d.Id);
                 Func<ImmutableArray<Diagnostic>, Task<IEnumerable<CodeFix>>> getFixes =
                     async (dxs) =>
                     {
@@ -258,8 +259,8 @@ namespace Microsoft.CodeAnalysis.CodeFixes
                 // this can happen for suppression case where all diagnostics can't be suppressed
                 return result;
             }
-
-            var fixes = await _extensionManager.PerformFunctionAsync(fixer, () => getFixes(diagnostics)).ConfigureAwait(false);
+            var extensionManager = document.Project.Solution.Workspace.Services.GetService<IExtensionManager>();
+            var fixes = await extensionManager.PerformFunctionAsync(fixer, () => getFixes(diagnostics)).ConfigureAwait(false);
             if (fixes != null && fixes.Any())
             {
                 FixAllCodeActionContext fixAllContext = null;
@@ -267,7 +268,7 @@ namespace Microsoft.CodeAnalysis.CodeFixes
                 if (codeFixProvider != null)
                 {
                     // If the codeFixProvider supports fix all occurrences, then get the corresponding FixAllProviderInfo and fix all context.
-                    var fixAllProviderInfo = _extensionManager.PerformFunction(codeFixProvider, () => ImmutableInterlocked.GetOrAdd(ref _fixAllProviderMap, codeFixProvider, FixAllProviderInfo.Create));
+                    var fixAllProviderInfo = extensionManager.PerformFunction(codeFixProvider, () => ImmutableInterlocked.GetOrAdd(ref _fixAllProviderMap, codeFixProvider, FixAllProviderInfo.Create));
                     if (fixAllProviderInfo != null)
                     {
                         fixAllContext = new FixAllCodeActionContext(document, fixAllProviderInfo, codeFixProvider, diagnostics, this.GetDocumentDiagnosticsAsync, this.GetProjectDiagnosticsAsync, cancellationToken);
@@ -369,10 +370,12 @@ namespace Microsoft.CodeAnalysis.CodeFixes
                 verifyArguments: false,
                 cancellationToken: cancellationToken);
 
+            var extensionManager = document.Project.Solution.Workspace.Services.GetService<IExtensionManager>();
+
             // we do have fixer. now let's see whether it actually can fix it
             foreach (var fixer in allFixers)
             {
-                await _extensionManager.PerformActionAsync(fixer, () => fixer.RegisterCodeFixesAsync(context) ?? SpecializedTasks.EmptyTask).ConfigureAwait(false);
+                await extensionManager.PerformActionAsync(fixer, () => fixer.RegisterCodeFixesAsync(context) ?? SpecializedTasks.EmptyTask).ConfigureAwait(false);
                 if (!fixes.Any())
                 {
                     continue;
@@ -386,13 +389,36 @@ namespace Microsoft.CodeAnalysis.CodeFixes
 
         private static readonly Func<DiagnosticId, List<CodeFixProvider>> s_createList = _ => new List<CodeFixProvider>();
 
-        private ImmutableArray<DiagnosticId> GetFixableDiagnosticIds(CodeFixProvider fixer)
+        private ImmutableArray<DiagnosticId> GetFixableDiagnosticIds(CodeFixProvider fixer, IExtensionManager extensionManager)
         {
-            return _extensionManager.PerformFunction(fixer, () => ImmutableInterlocked.GetOrAdd(ref _fixerToFixableIdsMap, fixer, f => f.FixableDiagnosticIds), ImmutableArray<DiagnosticId>.Empty);
+            // If we are passed a null extension manager it means we do not have access to a document so there is nothing to 
+            // show the user.  In this case we will log any exceptions that occur, but the user will not see them.
+            if (extensionManager != null)
+            {
+                return extensionManager.PerformFunction(
+                    fixer, 
+                    () => ImmutableInterlocked.GetOrAdd(ref _fixerToFixableIdsMap, fixer, f => f.FixableDiagnosticIds), 
+                    ImmutableArray<DiagnosticId>.Empty);
+            }
+
+            try
+            {
+                return ImmutableInterlocked.GetOrAdd(ref _fixerToFixableIdsMap, fixer, f => f.FixableDiagnosticIds);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                _errorLogger?.LogError(fixer.GetType().Name, e.Message + Environment.NewLine + e.StackTrace);
+                return ImmutableArray<DiagnosticId>.Empty;
+            }
         }
 
         private ImmutableDictionary<LanguageKind, Lazy<ImmutableDictionary<DiagnosticId, ImmutableArray<CodeFixProvider>>>> GetFixerPerLanguageMap(
-            Dictionary<LanguageKind, List<Lazy<CodeFixProvider, CodeChangeProviderMetadata>>> fixersPerLanguage)
+            Dictionary<LanguageKind, List<Lazy<CodeFixProvider, CodeChangeProviderMetadata>>> fixersPerLanguage,
+            IExtensionManager extensionManager)
         {
             var fixerMap = ImmutableDictionary.Create<LanguageKind, Lazy<ImmutableDictionary<DiagnosticId, ImmutableArray<CodeFixProvider>>>>();
             foreach (var languageKindAndFixers in fixersPerLanguage)
@@ -403,7 +429,7 @@ namespace Microsoft.CodeAnalysis.CodeFixes
 
                     foreach (var fixer in languageKindAndFixers.Value)
                     {
-                        foreach (var id in this.GetFixableDiagnosticIds(fixer.Value))
+                        foreach (var id in this.GetFixableDiagnosticIds(fixer.Value, extensionManager))
                         {
                             if (string.IsNullOrWhiteSpace(id))
                             {
@@ -475,13 +501,14 @@ namespace Microsoft.CodeAnalysis.CodeFixes
 
         private ImmutableDictionary<DiagnosticId, List<CodeFixProvider>> ComputeProjectFixers(Project project)
         {
+            var extensionManager = project.Solution.Workspace.Services.GetService<IExtensionManager>();
             ImmutableDictionary<DiagnosticId, List<CodeFixProvider>>.Builder builder = null;
             foreach (var reference in project.AnalyzerReferences)
             {
                 var projectCodeFixerProvider = _analyzerReferenceToFixersMap.GetValue(reference, _createProjectCodeFixProvider);
                 foreach (var fixer in projectCodeFixerProvider.GetFixers(project.Language))
                 {
-                    var fixableIds = this.GetFixableDiagnosticIds(fixer);
+                    var fixableIds = this.GetFixableDiagnosticIds(fixer, extensionManager);
                     foreach (var id in fixableIds)
                     {
                         if (string.IsNullOrWhiteSpace(id))

src/VisualStudio/Core/Def/Implementation/Log/VisualStudioErrorLogger.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.ErrorLogger;
+using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.VisualStudio.Shell;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.Log
 {
-    internal class VisualStudioErrorLogger : IErrorLogger
+    [ExportWorkspaceService(typeof(IErrorLoggerService), ServiceLayer.Host),  Export(typeof(IErrorLoggerService)), Shared]
+    internal class VisualStudioErrorLogger : IErrorLoggerService
     {
         public void LogError(string source, string message)
         {
             ActivityLog.LogError(source, message);
         }
 
-               public bool TryLogError(string source, string message)
+        public bool TryLogError(string source, string message)
         {
             return ActivityLog.TryLogError(source, message);
         }

src/Workspaces/Core/Portable/Log/FunctionId.cs
@@ -298,6 +298,7 @@ namespace Microsoft.CodeAnalysis.Internal.Log
         SuggestedActions_HasSuggestedActionsAsync,
         SuggestedActions_GetSuggestedActions,
         AnalyzerDependencyCheckingService_CheckForConflictsAsync,
-        VirtualMemory_MemoryLow
+        VirtualMemory_MemoryLow,
+        CodeFixe_Crash,
     }
 }

src/EditorFeatures/Test/CodeFixes/CodeFixServiceTests.cs
@@ -4,12 +4,14 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CodeFixes.Suppression;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces;
+using Microsoft.CodeAnalysis.ErrorLogger;
 using Microsoft.CodeAnalysis.Extensions;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.SolutionCrawler;
@@ -33,7 +35,7 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
             using (var workspace = CSharpWorkspaceFactory.CreateWorkspaceFromFile(code))
             {
                 var fixService = new CodeFixService(
-                    diagnosticService, workspace, fixers, SpecializedCollections.EmptyEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>>());
+                    diagnosticService, workspace.Services.GetService<IErrorLoggerService>(), fixers, SpecializedCollections.EmptyEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>>());
 
                 var incrementalAnalzyer = (IIncrementalAnalyzerProvider)diagnosticService;
 
@@ -58,36 +60,63 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
         public void TestGetCodeFixWithExceptionInRegisterMethod()
         {
             GetFirstDiagnosticWithFix(new ErrorCases.ExceptionInRegisterMethod());
+            GetAddedFixes(new ErrorCases.ExceptionInRegisterMethod());
         }
 
         [Fact]
         public void TestGetCodeFixWithExceptionInRegisterMethodAsync()
         {
             GetFirstDiagnosticWithFix(new ErrorCases.ExceptionInRegisterMethodAsync());
+            GetAddedFixes(new ErrorCases.ExceptionInRegisterMethodAsync());
         }
 
         [Fact]
         public void TestGetCodeFixWithExceptionInFixableDiagnosticIds()
         {
-            GetFixes(new ErrorCases.ExceptionInFixableDiagnosticIds());
+            GetDefaultFixes(new ErrorCases.ExceptionInFixableDiagnosticIds());
+            GetAddedFixes(new ErrorCases.ExceptionInFixableDiagnosticIds());
         }
 
         [Fact]
         public void TestGetCodeFixWithExceptionInGetFixAllProvider()
         {
-            GetFixes(new ErrorCases.ExceptionInGetFixAllProvider());
+            GetAddedFixes(new ErrorCases.ExceptionInGetFixAllProvider());
         }
 
-        public void GetFixes(CodeFixProvider codefix)
+        public void GetDefaultFixes(CodeFixProvider codefix)
         {
             TestDiagnosticAnalyzerService diagnosticService;
             CodeFixService fixService;
-            using (var workspace = ServiceSetup(codefix, out diagnosticService, out fixService))
+            IErrorLoggerService errorLogger;
+            using (var workspace = ServiceSetup(codefix, out diagnosticService, out fixService, out errorLogger))
             {
                 Document document;
                 EditorLayerExtensionManager.ExtensionManager extensionManager;
                 GetDocumentAndExtensionManager(diagnosticService, workspace, out document, out extensionManager);
                 var fixes = fixService.GetFixesAsync(document, TextSpan.FromBounds(0, 0), CancellationToken.None).Result;
+                Assert.True(((TestErrorLogger)errorLogger).Messages.Count == 1);
+                string message;
+                Assert.True(((TestErrorLogger)errorLogger).Messages.TryGetValue(codefix.GetType().Name, out message));
+            }
+        }
+
+        public void GetAddedFixes(CodeFixProvider codefix)
+        {
+            TestDiagnosticAnalyzerService diagnosticService;
+            CodeFixService fixService;
+            IErrorLoggerService errorLogger;
+            using (var workspace = ServiceSetup(codefix, out diagnosticService, out fixService, out errorLogger))
+            {
+                Document document;
+                EditorLayerExtensionManager.ExtensionManager extensionManager;
+                GetDocumentAndExtensionManager(diagnosticService, workspace, out document, out extensionManager);
+                var incrementalAnalzyer = (IIncrementalAnalyzerProvider)diagnosticService;
+                var analyzer = incrementalAnalzyer.CreateIncrementalAnalyzer(workspace);
+                var reference = new MockAnalyzerReference(codefix);
+                var project = workspace.CurrentSolution.Projects.Single().AddAnalyzerReference(reference);
+                document = project.Documents.Single();
+                var fixes = fixService.GetFixesAsync(document, TextSpan.FromBounds(0, 0), CancellationToken.None).Result;
+
                 Assert.True(extensionManager.IsDisabled(codefix));
                 Assert.False(extensionManager.IsIgnored(codefix));
             }
@@ -97,7 +126,8 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
         {
             TestDiagnosticAnalyzerService diagnosticService;
             CodeFixService fixService;
-            using (var workspace = ServiceSetup(codefix, out diagnosticService, out fixService))
+            IErrorLoggerService errorLogger;
+            using (var workspace = ServiceSetup(codefix, out diagnosticService, out fixService, out errorLogger))
             {
                 Document document;
                 EditorLayerExtensionManager.ExtensionManager extensionManager;
@@ -108,7 +138,7 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
             }
         }
 
-        private static TestWorkspace ServiceSetup(CodeFixProvider codefix, out TestDiagnosticAnalyzerService diagnosticService, out CodeFixService fixService)
+        private static TestWorkspace ServiceSetup(CodeFixProvider codefix, out TestDiagnosticAnalyzerService diagnosticService, out CodeFixService fixService, out IErrorLoggerService errorLogger)
         {
             diagnosticService = new TestDiagnosticAnalyzerService(DiagnosticExtensions.GetCompilerDiagnosticAnalyzersMap());
             var fixers = SpecializedCollections.SingletonEnumerable(
@@ -117,8 +147,9 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
                 new CodeChangeProviderMetadata("Test", languages: LanguageNames.CSharp)));
             var code = @"class Program { }";
             var workspace = CSharpWorkspaceFactory.CreateWorkspaceFromFile(code);
+            errorLogger = new TestErrorLogger();
             fixService = new CodeFixService(
-                    diagnosticService, workspace, fixers, SpecializedCollections.EmptyEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>>());
+                    diagnosticService, errorLogger, fixers, SpecializedCollections.EmptyEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>>());
             return workspace;
         }
 
@@ -160,8 +191,18 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
 
         private class MockAnalyzerReference : AnalyzerReference, ICodeFixProviderFactory
         {
-            public readonly MockFixer Fixer = new MockFixer();
+            public readonly CodeFixProvider Fixer;
             public readonly MockDiagnosticAnalyzer Analyzer = new MockDiagnosticAnalyzer();
+
+            public MockAnalyzerReference()
+            {
+               Fixer = new MockFixer();
+            }
+
+            public MockAnalyzerReference(CodeFixProvider codeFix)
+            {
+                Fixer = codeFix;
+            }
 
             public override string Display
             {
@@ -212,6 +253,29 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
                     {
                         c.ReportDiagnostic(Diagnostic.Create(_descriptor, c.Tree.GetLocation(TextSpan.FromBounds(0, 0))));
                     });
+                }
+            }
+        }
+
+        internal class TestErrorLogger : IErrorLoggerService
+        {
+            public Dictionary<string, string> Messages = new Dictionary<string, string>();
+
+            public void LogError(string source, string message)
+            {
+                Messages.Add(source, message);
+            }
+
+            public bool TryLogError(string source, string message)
+            {
+                try
+                {
+                    Messages.Add(source, message);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
                 }
             }
         }

src/EditorFeatures/Core/EditorFeatures.csproj
@@ -633,6 +633,8 @@
     <Compile Include="Implementation\TodoComment\TodoCommentState.cs" />
     <Compile Include="Implementation\TodoComment\TodoCommentTokens.cs" />
     <Compile Include="Implementation\TodoComment\TodoTaskItem.cs" />
+    <Compile Include="Implementation\Workspaces\EditorErrorReportingService.cs" />
+    <Compile Include="Implementation\Workspaces\EditorErrorReportingServiceFactory.cs" />
     <Compile Include="Implementation\Workspaces\ProjectCacheServiceFactory.cs" />
     <Compile Include="Implementation\Workspaces\EditorTextFactoryService.cs" />
     <Compile Include="Implementation\Workspaces\TextUndoHistoryWorkspaceServiceFactoryService.cs" />

commit 9e5240d10e62ee1342df480656049d099791d1d0

Fixing Mef composition
Fixing Mef composition error caused by previous commit
(11a827c4f7ea5cb72cfe559b7cc4664d59afa095)

src/EditorFeatures/Core/Implementation/Workspaces/EditorErrorReportingService.cs
@@ -13,7 +13,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Workspaces
         public void ShowErrorInfoForCodeFix(string codefixName, Action OnEnableClicked, Action OnEnableAndIgnoreClicked)
         {
             var message = LogMessage.Create($"{codefixName} crashed");
-            Logger.Log(FunctionId.CodeFixe_Crash, message);
+            Logger.Log(FunctionId.Extension_Excpetion, message);
         }
     }
 }

src/Features/Core/CodeFixes/CodeFixService.cs
@@ -40,18 +40,18 @@ namespace Microsoft.CodeAnalysis.CodeFixes
         private readonly ConditionalWeakTable<AnalyzerReference, ProjectCodeFixProvider>.CreateValueCallback _createProjectCodeFixProvider;
 
         private readonly ImmutableDictionary<LanguageKind, Lazy<ISuppressionFixProvider>> _suppressionProvidersMap;
-        private readonly IErrorLoggerService _errorLogger;
+        private readonly IEnumerable<Lazy<IErrorLoggerService>> _errorLoggers;
 
         private ImmutableDictionary<CodeFixProvider, FixAllProviderInfo> _fixAllProviderMap;
 
         [ImportingConstructor]
         public CodeFixService(
             IDiagnosticAnalyzerService service,
-            IErrorLoggerService logger,
+            [ImportMany]IEnumerable<Lazy<IErrorLoggerService>> loggers,
             [ImportMany]IEnumerable<Lazy<CodeFixProvider, CodeChangeProviderMetadata>> fixers,
             [ImportMany]IEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>> suppressionProviders)
         {
-            _errorLogger = logger;
+            _errorLoggers = loggers;
             _diagnosticService = service;
             var fixersPerLanguageMap = fixers.ToPerLanguageMapWithMultipleLanguages();
             var suppressionProvidersPerLanguageMap = suppressionProviders.ToPerLanguageMapWithMultipleLanguages();
@@ -416,7 +416,10 @@ namespace Microsoft.CodeAnalysis.CodeFixes
             }
             catch (Exception e)
             {
-                _errorLogger?.LogError(fixer.GetType().Name, e.Message + Environment.NewLine + e.StackTrace);
+                foreach (var logger in _errorLoggers)
+                {
+                    logger.Value.LogError(fixer.GetType().Name, e.Message + Environment.NewLine + e.StackTrace);
+                }
                 return ImmutableArray<DiagnosticId>.Empty;
             }
         }

src/VisualStudio/Core/Def/Implementation/Log/VisualStudioErrorLogger.cs
@@ -22,6 +22,5 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.Log
         {
             return ActivityLog.TryLogError(source, message);
         }
-
     }
 }

src/Workspaces/Core/Portable/Log/FunctionId.cs
@@ -299,6 +299,6 @@ namespace Microsoft.CodeAnalysis.Internal.Log
         SuggestedActions_GetSuggestedActions,
         AnalyzerDependencyCheckingService_CheckForConflictsAsync,
         VirtualMemory_MemoryLow,
-        CodeFixe_Crash,
+        Extension_Excpetion,
     }
 }

src/Workspaces/Core/Portable/Log/WorkspaceErrorLogger.cs
@@ -1,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.ErrorLogger;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Internal.Log;
+
+namespace Microsoft.CodeAnalysis.ErrorLogger
+{
+    [ExportWorkspaceService(typeof(IErrorLoggerService)), Export(typeof(IErrorLoggerService)), Shared]
+    class WorkspaceErrorLogger : IErrorLoggerService
+    {
+        public void LogError(string source, string message)
+        {
+            Logger.GetLogger()?.Log(FunctionId.Extension_Excpetion, LogMessage.Create(source + " : " + message));
+        }
+
+        public bool TryLogError(string source, string message)
+        {
+            var logger = Logger.GetLogger();
+            if (logger != null)
+            {
+                logger.Log(FunctionId.Extension_Excpetion, LogMessage.Create(source + " : " + message));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+

src/Workspaces/Core/Portable/Workspaces.csproj
@@ -385,6 +385,7 @@
     <Compile Include="Log\LogAggregator.cs" />
     <Compile Include="Log\Logger.LogBlock.cs" />
     <Compile Include="Log\LogMessage.cs" />
+    <Compile Include="Log\WorkspaceErrorLogger.cs" />
     <Compile Include="Rename\TokenRenameInfo.cs" />
     <Compile Include="Serialization\AssemblySerializationInfoService.cs" />
     <Compile Include="Serialization\IAssemblySerializationInfoService.cs" />

src/EditorFeatures/Test/CodeFixes/CodeFixServiceTests.cs
@@ -34,8 +34,9 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
 ";
             using (var workspace = CSharpWorkspaceFactory.CreateWorkspaceFromFile(code))
             {
+                var logger = SpecializedCollections.SingletonEnumerable(new Lazy<IErrorLoggerService>(() => workspace.Services.GetService<IErrorLoggerService>()));
                 var fixService = new CodeFixService(
-                    diagnosticService, workspace.Services.GetService<IErrorLoggerService>(), fixers, SpecializedCollections.EmptyEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>>());
+                    diagnosticService, logger, fixers, SpecializedCollections.EmptyEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>>());
 
                 var incrementalAnalzyer = (IIncrementalAnalyzerProvider)diagnosticService;
 
@@ -147,9 +148,10 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeFixes
                 new CodeChangeProviderMetadata("Test", languages: LanguageNames.CSharp)));
             var code = @"class Program { }";
             var workspace = CSharpWorkspaceFactory.CreateWorkspaceFromFile(code);
-            errorLogger = new TestErrorLogger();
+            var logger = SpecializedCollections.SingletonEnumerable(new Lazy<IErrorLoggerService>(() => new TestErrorLogger()));
+            errorLogger = logger.First().Value;
             fixService = new CodeFixService(
-                    diagnosticService, errorLogger, fixers, SpecializedCollections.EmptyEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>>());
+                    diagnosticService, logger, fixers, SpecializedCollections.EmptyEnumerable<Lazy<ISuppressionFixProvider, CodeChangeProviderMetadata>>());
             return workspace;
         }
 