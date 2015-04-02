2dc19d818df85ae031b23a73227a7f383cb1d02c
Heejae Chang <heejaechang@outlook.com>
2015-04-02 13:46:47 -0700

    ported #1730 from master to preview
    https://github.com/dotnet/roslyn/commit/db3fdb639abbf1e5656d7c9e9e0646c1dcf73cbd
    
    ..
    
    report project k/shared project IVsHierarchy to error list

--- src/VisualStudio/Core/Def/Implementation/TableDataSource/VisualStudioBaseTodoListTable.cs
+++ src/VisualStudio/Core/Def/Implementation/TableDataSource/VisualStudioBaseTodoListTable.cs
@@ -187,7 +187,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TableDataSource
                                 content = GetProjectName(_factory._workspace, _factory._documentId.ProjectId);
                                 return content != null;
                             case StandardTableKeyNames.Project:
-                                content = GetHierarchy(_factory._workspace, _factory._documentId.ProjectId);
+                                content = GetHierarchy(_factory._workspace, _factory._documentId.ProjectId, _factory._documentId);
                                 return content != null;
                             case StandardTableKeyNames.TaskCategory:
                                 content = VSTASKCATEGORY.CAT_COMMENTS;
