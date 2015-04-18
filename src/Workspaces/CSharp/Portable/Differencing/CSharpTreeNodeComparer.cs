6240ee891a585eb67b6b306047bf057ed7c55df7
Heejae Chang <heejaechang@outlook.com>
2015-04-18 13:25:37 -0700

    add new whole tree TreeComparer
    plan is using this for change preview window to show better textual diff.
    
    this is still work in progress. currently it can diff changes down to trivia level including strucutred trivia,
    but current TreeComparer doesnt give enough information to change diff to actual textual edits to transform old tree to new tree.
    
    for example, insert or re-order doesnt give old location to apply changes.

---
+++ src/Workspaces/CSharp/Portable/Differencing/CSharpTreeNodeComparer.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.Differencing;
+
+namespace Microsoft.CodeAnalysis.CSharp.Differencing
+{
+    internal class CSharpTreeNodeComparer : TreeNodeComparer
+    {
+        public readonly static CSharpTreeNodeComparer Instance = new CSharpTreeNodeComparer();
+
+        private static readonly int Count = Enum.GetValues(typeof(SyntaxKind)).Cast<SyntaxKind>().Select(k => (int)k).Max() - (int)SyntaxKind.TildeToken + 2;
+
+        private CSharpTreeNodeComparer() : base(new CSharpTreeNodeEquivalentChecker())
+        {
+        }
+
+        protected internal override int LabelCount
+        {
+            get
+            {
+                return Count;
+            }
+        }
+
+        protected internal override int GetLabel(TreeNode node)
+        {
+            if (node.Kind == (int)SyntaxKind.None)
+            {
+                return 0;
+            }
+
+            if (node.Kind == (int)SyntaxKind.List)
+            {
+                return 1;
+            }
+
+            return node.Kind - (int)SyntaxKind.TildeToken + 2;
+        }
+
+        private class CSharpTreeNodeEquivalentChecker : ITreeNodeComparer
+        {
+            private static readonly Func<TreeNode, TreeNode, bool> checker = (left, right) =>
+            {
+                if (left.IsNode && right.IsNode)
+                {
+                    return SyntaxFactory.AreEquivalent(left.AsNode(), right.AsNode());
+                }
+
+                if (left.IsToken && right.IsToken)
+                {
+                    return SyntaxFactory.AreEquivalent(left.AsToken(), right.AsToken());
+                }
+
+                if (left.IsTrivia && right.IsTrivia)
+                {
+                    var leftTrivia = left.AsTrivia();
+                    var rightTrivia = right.AsTrivia();
+
+                    if (leftTrivia.HasStructure && rightTrivia.HasStructure)
+                    {
+                        return SyntaxFactory.AreEquivalent(leftTrivia.GetStructure(), rightTrivia.GetStructure());
+                    }
+
+                    return leftTrivia.ToString() == rightTrivia.ToString();
+                }
+
+                return false;
+            };
+
+            public LcsTreeNodes LcsTreeNodes { get; } = new LcsTreeNodes(checker);
+
+            public bool AreEquivalent(TreeNode left, TreeNode right)
+            {
+                return checker(left, right);
+            }
+        }
+    }
+}
