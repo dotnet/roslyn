dc5daf08e2500a6fc91d07be47b1f33d9056921a
jaredpar <jaredpar@microsoft.com>
2015-04-20 09:12:51 -0700

    Script updates
    Couple of changes to the scripts:
    
    - Factored out installing the PCL libraries to setup-pcl.sh.  Makes it
    much easier to install PCL against custom builds of mono
    - cibuild.sh accepts an optional `—os` option now. Makes it easier to
    customize Jenkins based on the OS target.

--- build/linux/setup-snapshot.sh
+++ build/linux/setup-snapshot.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PCL_NAME=PortableReferenceAssemblies-2014-04-14
-
 if [ "$EUID" -ne 0 ]; then
     echo "Error: This script must be run as root"
     exit 1
@@ -15,21 +13,5 @@ if [ ! -d "$MONO_PREFIX" ]; then
     exit 1
 fi
 
-PCL_TARGET=$MONO_PREFIX/lib/mono/xbuild-frameworks/.NETPortable
-if [ -d "$PCL_TARGET" ]; then
-    echo "Error: PCL already installed at $PCL_TARGET"
-    exit 1
-fi
-
-# Now to install the PCL on the snapshot 
-cd /tmp 
-wget http://storage.bos.xamarin.com/bot-provisioning/$PCL_NAME.zip -O /tmp/pcl.zip
-unzip pcl.zip 
-if [ ! -d "$PCL_NAME" ]; then
-    echo "Error: Did not unzip the PCL correctly"
-    exit 1
-fi
-
-echo "Installing to $PCL_TARGET"
-mkdir $PCL_TARGET
-mv $PCL_NAME/* $PCL_TARGET
+# Now install the PCL assemblies on the snapshot
+source setup-pcl $MONO_PREFIX
