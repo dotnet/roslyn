58ca057bee08baddb2f4f19e8836c1bd70ee905f
Tom Meschter <tom.meschter@microsoft.com>
2015-04-21 14:37:31 -0700

    Push loaders out to the hosts
    Move the analyzer assembly loaders out of `CodeAnalysis.Desktop` and
    into the individual host projects (csc. csi, vbc, vbi, etc.). This
    solves two issues:
    
    1. The loader types can now be made internal.
    2. `CodeAnalysis.Desktop` no longer has a dependency on the loader
    implementation, and the loaders can be swapped or modified on a per-host
    basis as needed.

---
+++ src/Compilers/Helpers/SimpleAnalyzerAssemblyLoader.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Reflection;
+
+namespace Microsoft.CodeAnalysis
+{
+    /// <summary>
+    /// Loads analyzer assemblies from their original locations in the file system.
+    /// Assemblies will only be loaded from the locations specified when the loader
+    /// is instantiated.
+    /// </summary>
+    /// <remarks>
+    /// This type is meant to be used in scenarios where it is OK for the analyzer
+    /// assemblies to be locked on disk for the lifetime of the host; for example,
+    /// csc.exe and vbc.exe. In scenarios where support for updating or deleting
+    /// the analyzer on disk is required a different loader should be used.
+    /// </remarks>
+    internal sealed class SimpleAnalyzerAssemblyLoader : AbstractAnalyzerAssemblyLoader
+    {
+        protected override Assembly LoadCore(string fullPath)
+        {
+            return Assembly.LoadFrom(fullPath);
+        }
+    }
+}
