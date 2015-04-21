bc007f446f30af23b752d98bcf8f38185e924ff4
John Hamby <jhamby@microsoft.com>
2015-04-20 17:01:57 -0700

    Delete commented code

--- src/Features/Core/Diagnostics/DiagnosticAnalyzerService_IncrementalAnalyzer.cs
+++ src/Features/Core/Diagnostics/DiagnosticAnalyzerService_IncrementalAnalyzer.cs
@@ -207,7 +207,6 @@ namespace Microsoft.CodeAnalysis.Diagnostics
 
             private BaseDiagnosticIncrementalAnalyzer GetAnalyzer(bool useV2)
             {
-                // useV2 = true;
                 return useV2 ? (BaseDiagnosticIncrementalAnalyzer)_engineV2 : _engineV1;
             }
         }
