8dc21d31c026342d01324cdd2283b52ea4c56d82
Daniel Plaisted <daplaist@microsoft.com>
2015-04-23 17:31:49 -0700

    When Array.Length has a setter defined, fix issue compiling code that sets the Array.Length property
    Also compile aacorlib dynamically instead of using checked in binary embedded as resource.

---
+++ src/Compilers/CSharp/Test/Semantic/Semantics/PropertyAccessTests.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.CSharp.UnitTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Roslyn.Test.Utilities;
+
+namespace Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests.Semantics
+{
+    public class PropertyAccessTests : CompilingTestBase
+    {
+        [Fact]
+        [WorkItem(1140706, "DevDiv")]
+        public void SetArrayLength()
+        {
+            var text =
+@"class Test
+{
+    void M()
+    {
+        int[] arr = { 1 };
+        arr.Length = 0;
+    }
+}";
+            var comp = CreateCompilation(
+                trees: new[] { Parse(text) },
+                references: new[] { AacorlibRef });
+
+            comp.VerifyEmitDiagnostics();
+        }
+    }
+}
