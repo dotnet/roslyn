99157dd64bdf5198770069865548396f9e4515ce
Heejae Chang <heejaechang@outlook.com>
2015-04-24 17:37:12 -0700

    refactored esent code to support project k and save project name along with project path. this is slightly different than saving context but should fulfill the need.

--- src/VisualStudio/Core/Def/Implementation/Workspace/Esent/EsentStorage.DocumentTable.cs
+++ src/VisualStudio/Core/Def/Implementation/Workspace/Esent/EsentStorage.DocumentTable.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using Microsoft.Isam.Esent.Interop;
 using Roslyn.Utilities;
 
@@ -7,93 +8,38 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.Esent
 {
     internal partial class EsentStorage
     {
-        public class DocumentTable : AbstractTable
+        public class DocumentTable : ProjectDocumentTable
         {
             private const string TableName = "DocumentTable";
-
-            private const string ProjectColumnName = "Project";
-            private const string DocumentColumnName = "Document";
             private const string NameColumnName = "Name";
             private const string ValueColumnName = "Value";
 
-            private const string DocumentIndexName = "DocumentIndex";
-            private const string ProjectAndDocumentAndNameIndexName = "ProjectAndDocumentAndNameIndex";
-
             private JET_COLUMNID _projectColumnId;
+            private JET_COLUMNID _projectNameColumnId;
             private JET_COLUMNID _documentColumnId;
             private JET_COLUMNID _nameColumnId;
             private JET_COLUMNID _valueColumnId;
 
             public override void Create(JET_SESID sessionId, JET_DBID databaseId)
             {
-                var projectColumnCreate = new JET_COLUMNCREATE()
-                {
-                    szColumnName = ProjectColumnName,
-                    coltyp = JET_coltyp.Long,
-                    grbit = ColumndefGrbit.ColumnNotNULL
-                };
+                var nameColumnCreate = CreateIdColumn(NameColumnName);
+                var valueColumnCreate = CreateBinaryColumn(ValueColumnName);
 
-                var documentColumnCreate = new JET_COLUMNCREATE()
-                {
-                    szColumnName = DocumentColumnName,
-                    coltyp = JET_coltyp.Long,
-                    grbit = ColumndefGrbit.ColumnNotNULL
-                };
+                var columns = CreateProjectDocumentColumns(nameColumnCreate, valueColumnCreate);
 
-                var nameColumnCreate = new JET_COLUMNCREATE()
-                {
-                    szColumnName = NameColumnName,
-                    coltyp = JET_coltyp.Long,
-                    grbit = ColumndefGrbit.ColumnNotNULL
-                };
-
-                var valueColumnCreate = new JET_COLUMNCREATE()
-                {
-                    szColumnName = ValueColumnName,
-                    coltyp = JET_coltyp.LongBinary,
-                    grbit = ColumndefGrbit.None
-                };
-
-                var columns = new JET_COLUMNCREATE[] { projectColumnCreate, documentColumnCreate, nameColumnCreate, valueColumnCreate };
-
-                var projectAndDocumentAndNameIndexKey = "+" + ProjectColumnName + "\0+" + DocumentColumnName + "\0+" + NameColumnName + "\0\0";
-                var documentIndexKey = "+" + DocumentColumnName + "\0\0";
+                var primaryIndexKey = CreateProjectDocumentIndexKey(NameColumnName);
 
                 var indexes = new JET_INDEXCREATE[]
                 {
-                    new JET_INDEXCREATE
-                    {
-                        szIndexName = ProjectAndDocumentAndNameIndexName,
-                        szKey = projectAndDocumentAndNameIndexKey,
-                        cbKey = projectAndDocumentAndNameIndexKey.Length,
-                        grbit = CreateIndexGrbit.IndexPrimary | CreateIndexGrbit.IndexUnique | CreateIndexGrbit.IndexDisallowNull,
-                        ulDensity = 80
-                    },
-                    new JET_INDEXCREATE
-                    {
-                        szIndexName = DocumentIndexName,
-                        szKey = documentIndexKey,
-                        cbKey = documentIndexKey.Length,
-                        grbit = CreateIndexGrbit.IndexDisallowNull,
-                        ulDensity = 80
-                    }
+                    CreatePrimaryIndex(primaryIndexKey)
                 };
 
-                var tableCreate = new JET_TABLECREATE()
-                {
-                    szTableName = TableName,
-                    ulPages = 16,
-                    ulDensity = 80,
-                    rgcolumncreate = columns,
-                    cColumns = columns.Length,
-                    rgindexcreate = indexes,
-                    cIndexes = indexes.Length
-                };
+                var tableCreate = CreateTable(TableName, columns, indexes);
 
                 Api.JetCreateTableColumnIndex3(sessionId, databaseId, tableCreate);
 
-                _projectColumnId = projectColumnCreate.columnid;
-                _documentColumnId = documentColumnCreate.columnid;
+                GetColumnIds(columns, out _projectColumnId, out _projectNameColumnId, out _documentColumnId);
+
                 _nameColumnId = nameColumnCreate.columnid;
                 _valueColumnId = valueColumnCreate.columnid;
 
@@ -104,8 +50,8 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.Esent
             {
                 using (var table = new Table(sessionId, databaseId, TableName, OpenTableGrbit.ReadOnly))
                 {
-                    _projectColumnId = Api.GetTableColumnid(sessionId, table, ProjectColumnName);
-                    _documentColumnId = Api.GetTableColumnid(sessionId, table, DocumentColumnName);
+                    GetColumnIds(sessionId, table, out _projectColumnId, out _projectNameColumnId, out _documentColumnId);
+
                     _nameColumnId = Api.GetTableColumnid(sessionId, table, NameColumnName);
                     _valueColumnId = Api.GetTableColumnid(sessionId, table, ValueColumnName);
                 }
@@ -113,7 +59,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.Esent
 
             public override AbstractTableAccessor GetTableAccessor(OpenSession openSession)
             {
-                return new DocumentTableAccessor(openSession, TableName, ProjectAndDocumentAndNameIndexName, _projectColumnId, _documentColumnId, _nameColumnId, _valueColumnId);
+                return new DocumentTableAccessor(
+                    openSession, TableName, PrimaryIndexName,
+                    _projectColumnId, _projectNameColumnId, _documentColumnId, _nameColumnId, _valueColumnId);
             }
         }
     }
