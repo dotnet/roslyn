4f7cfc3cce3f1a5501aa0aacb68a8cb096877ae0
Pilchie <kevinpi@microsoft.com>
2015-04-25 20:14:42 -0700

    Expose a public API for building RQNames
    * Rename RQNameService to RQName and make public.
    * Rename TryBuildForPublicApi to From and have it return null.
    * Remove TryBuild, since we *only* build RQNames for public Api's now.
    * Clean up implementation to not pass flag around for buildForPublicApi
    * Remove RQDynamicType
    * Create a singleton for System.Object.
    * Add a comment explaining why we erase dynamic.
    
    Fixes #55.

--- src/VisualStudio/Core/Def/Implementation/RQName/RQNameService.cs
+++ src/VisualStudio/Core/Def/Implementation/RQName/RQNameService.cs
@@ -1,25 +1,23 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.LanguageServices.Implementation.RQName;
 
-namespace Microsoft.VisualStudio.LanguageServices.Implementation.RQName
+namespace Microsoft.VisualStudio.LanguageServices
 {
-    internal static class RQNameService
+    /// <summary>
+    /// Helpers related to <see cref="RQName"/>s.
+    /// </summary>
+    public static class RQName
     {
-        public static bool TryBuild(ISymbol symbol, out string rqname)
+        /// <summary>
+        /// Returns an RQName for the given symbol, or <code>null</code>if the symbol cannot be represented by an RQName.
+        /// </summary>
+        /// <param name="symbol">The symbol to build an RQName for.</param>
+        public static string From(ISymbol symbol)
         {
             var node = RQNodeBuilder.Build(symbol);
-            rqname = (node != null) ? ParenthesesTreeWriter.ToParenthesesFormat(node.ToSimpleTree()) : null;
-
-            return node != null;
-        }
-
-        public static bool TryBuildForPublicAPIs(ISymbol symbol, out string rqname)
-        {
-            var node = RQNodeBuilder.Build(symbol, buildForPublicAPIs: true);
-            rqname = (node != null) ? ParenthesesTreeWriter.ToParenthesesFormat(node.ToSimpleTree()) : null;
-
-            return node != null;
+            return (node != null) ? ParenthesesTreeWriter.ToParenthesesFormat(node.ToSimpleTree()) : null;
         }
     }
 }
