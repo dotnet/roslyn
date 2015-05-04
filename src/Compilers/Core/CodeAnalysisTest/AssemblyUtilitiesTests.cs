526b85d29ff27cfb3ee0ca8fd7b776cc3f0dd580
Tom Meschter <tom.meschter@microsoft.com>
2015-05-04 09:38:31 -0700

    Add a utility method to find assembly dependencies
    Add a utility method to find assembly dependencies.
    
    Given a path to an assembly, `AssemblyUtilities.FindAssemblySet`
    identifies file in the same directory that could satisfy the assembly's
    dependencies.
    
    This commit contains just the code itself and some unit tests. A later
    commit will wire it into the compilers/VS.

---
+++ src/Compilers/Core/CodeAnalysisTest/AssemblyUtilitiesTests.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Roslyn.Test.Utilities;
+using Roslyn.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.UnitTests
+{
+    public class AssemblyUtilitiesTests : TestBase
+    {
+        [Fact]
+        public void FindAssemblySet_SingleAssembly()
+        {
+            var directory = Temp.CreateDirectory();
+
+            var alphaDll = directory.CreateFile("Alpha.dll").WriteAllBytes(TestResources.AssemblyLoadTests.AssemblyLoadTests.Alpha);
+
+            var results = AssemblyUtilities.FindAssemblySet(alphaDll.Path);
+
+            Assert.Equal(expected: 1, actual: results.Length);
+            Assert.Equal(expected: alphaDll.Path, actual: results[0]);
+        }
+
+        [Fact]
+        public void FindAssemblySet_TwoUnrelatedAssemblies()
+        {
+            var directory = Temp.CreateDirectory();
+
+            var alphaDll = directory.CreateFile("Alpha.dll").WriteAllBytes(TestResources.AssemblyLoadTests.AssemblyLoadTests.Alpha);
+            var betaDll = directory.CreateFile("Beta.dll").WriteAllBytes(TestResources.AssemblyLoadTests.AssemblyLoadTests.Beta);
+
+            var results = AssemblyUtilities.FindAssemblySet(alphaDll.Path);
+
+            Assert.Equal(expected: 1, actual: results.Length);
+            Assert.Equal(expected: alphaDll.Path, actual: results[0]);
+        }
+
+        [Fact]
+        public void FindAssemblySet_SimpleDependency()
+        {
+            var directory = Temp.CreateDirectory();
+
+            var alphaDll = directory.CreateFile("Alpha.dll").WriteAllBytes(TestResources.AssemblyLoadTests.AssemblyLoadTests.Alpha);
+            var gammaDll = directory.CreateFile("Gamma.dll").WriteAllBytes(TestResources.AssemblyLoadTests.AssemblyLoadTests.Gamma);
+
+            var results = AssemblyUtilities.FindAssemblySet(alphaDll.Path);
+
+            Assert.Equal(expected: 2, actual: results.Length);
+            Assert.Contains(alphaDll.Path, results, StringComparer.OrdinalIgnoreCase);
+            Assert.Contains(gammaDll.Path, results, StringComparer.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public void FindAssemblySet_TransitiveDependencies()
+        {
+            var directory = Temp.CreateDirectory();
+
+            var alphaDll = directory.CreateFile("Alpha.dll").WriteAllBytes(TestResources.AssemblyLoadTests.AssemblyLoadTests.Alpha);
+            var gammaDll = directory.CreateFile("Gamma.dll").WriteAllBytes(TestResources.AssemblyLoadTests.AssemblyLoadTests.Gamma);
+            var deltaDll = directory.CreateFile("Delta.dll").WriteAllBytes(TestResources.AssemblyLoadTests.AssemblyLoadTests.Delta);
+
+            var results = AssemblyUtilities.FindAssemblySet(alphaDll.Path);
+
+            Assert.Equal(expected: 3, actual: results.Length);
+            Assert.Contains(alphaDll.Path, results, StringComparer.OrdinalIgnoreCase);
+            Assert.Contains(gammaDll.Path, results, StringComparer.OrdinalIgnoreCase);
+            Assert.Contains(deltaDll.Path, results, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ src/Compilers/Core/Portable/AssemblyUtilities.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+
+namespace Roslyn.Utilities
+{
+    internal static class AssemblyUtilities
+    {
+        /// <summary>
+        /// Given a path to an assembly, identifies files in the same directory
+        /// that could satisfy the assembly's dependencies.
+        /// </summary>
+        /// <remarks>
+        /// Dependencies are identified by simply checking the name of an assembly
+        /// reference against a file name; if they match the file is considered a
+        /// dependency. Other factors, such as version, culture, public key, etc., 
+        /// are not considered, and so the returned collection may include items that
+        /// cannot in fact satisfy the original assembly's dependencies.
+        /// </remarks>
+        public static ImmutableArray<string> FindAssemblySet(string filePath)
+        {
+            Queue<string> workList = new Queue<string>();
+            HashSet<string> assemblySet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            workList.Enqueue(filePath);
+
+            while (workList.Count > 0)
+            {
+                string assemblyPath = workList.Dequeue();
+
+                assemblySet.Add(assemblyPath);
+
+                var directory = Path.GetDirectoryName(assemblyPath);
+
+                using (var reader = new PEReader(FileUtilities.OpenRead(assemblyPath)))
+                {
+                    var metadataReader = reader.GetMetadataReader();
+                    var assemblyReferenceHandles = metadataReader.AssemblyReferences;
+
+                    foreach (var handle in assemblyReferenceHandles)
+                    {
+                        var reference = metadataReader.GetAssemblyReference(handle);
+                        var referenceName = metadataReader.GetString(reference.Name);
+
+                        string referencePath = Path.Combine(directory, referenceName + ".dll");
+
+                        if (!assemblySet.Contains(referencePath) &&
+                            File.Exists(referencePath))
+                        {
+                            workList.Enqueue(referencePath);
+                        }
+                    }
+                }
+            }
+
+            return ImmutableArray.CreateRange(assemblySet);
+        }
+    }
+}
