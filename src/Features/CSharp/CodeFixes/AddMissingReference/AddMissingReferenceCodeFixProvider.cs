24253a3aff24316ba2135e37826cef4ddf5cf536
Jonathon Marolf <jmarolf@microsoft.com>
2015-05-07 16:34:54 -0700

    Make add missing reference more reliable.
    We now look at the identifier names in an effort to find the IErrorType symbol that triggered the diagnostic. Once we find the error symbol we return the assembly identity assuming that this is the correct error symbol since we can't filter by MissingMetadataTypeSymbol.

--- src/Features/CSharp/CodeFixes/AddMissingReference/AddMissingReferenceCodeFixProvider.cs
+++ src/Features/CSharp/CodeFixes/AddMissingReference/AddMissingReferenceCodeFixProvider.cs
@@ -3,42 +3,22 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.AddMissingReference;
 using Microsoft.CodeAnalysis.CodeFixes;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.AddMissingReference
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = PredefinedCodeFixProviderNames.AddMissingReference), Shared]
     [ExtensionOrder(After = PredefinedCodeFixProviderNames.SimplifyNames)]
-    internal class AddMissingReferenceCodeFixProvider : CodeFixProvider
+    internal class AddMissingReferenceCodeFixProvider : AbstractAddMissingReferenceCodeFixProvider<IdentifierNameSyntax>
     {
         private const string CS0012 = "CS0012"; // The type 'A' is defined in an assembly that is not referenced. You must add a reference to assembly 'ProjectA, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'.
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
             get { return ImmutableArray.Create(CS0012); }
-        }
-
-        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
-        {
-            var uniqueIdentities = new HashSet<AssemblyIdentity>();
-            foreach (var diagnostic in context.Diagnostics)
-            {
-                AssemblyIdentity identity;
-                if (HACK_TryGetMissingAssemblyIdentity(diagnostic.GetMessage(), out identity) && !uniqueIdentities.Contains(identity))
-                {
-                    uniqueIdentities.Add(identity);
-                    context.RegisterCodeFix(
-                        await AddMissingReferenceCodeAction.CreateAsync(context.Document.Project, identity, context.CancellationToken).ConfigureAwait(false),
-                        diagnostic);
-                }
-            }
-        }
-
-        private bool HACK_TryGetMissingAssemblyIdentity(string message, out AssemblyIdentity assemblyIdentity)
-        {
-            return AssemblyIdentity.TryParseDisplayName(message.Split('\'')[3], out assemblyIdentity);
         }
     }
 }
---
+++ src/Features/Core/AddMissingReference/AbstractAddMissingReferenceCodeFixProvider.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.CodeAnalysis.AddMissingReference
+{
+    internal abstract class AbstractAddMissingReferenceCodeFixProvider<TIdentifierNameSyntax> : CodeFixProvider
+        where TIdentifierNameSyntax  : SyntaxNode
+    {
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var cancellationToken = context.CancellationToken;
+            var root = await context.Document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var model = await context.Document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+            var uniqueIdentities = new HashSet<AssemblyIdentity>();
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                var nodes = FindNodes(root, diagnostic);
+                var types = GetTypesForNodes(model, nodes, cancellationToken).Distinct();
+                AssemblyIdentity identity = GetAssemblyIdentity(types);
+                if (identity != null && !uniqueIdentities.Contains(identity))
+                {
+                    uniqueIdentities.Add(identity);
+                    context.RegisterCodeFix(
+                        await AddMissingReferenceCodeAction.CreateAsync(context.Document.Project, identity, context.CancellationToken).ConfigureAwait(false),
+                        diagnostic);
+                }
+            }
+        }
+
+        private static IEnumerable<SyntaxNode> FindNodes(SyntaxNode root, Diagnostic diagnostic)
+        {
+            var node = root.FindNode(diagnostic.Location.SourceSpan);
+            return node.DescendantNodesAndSelf().OfType<TIdentifierNameSyntax>();
+        }
+
+        private static IEnumerable<ITypeSymbol> GetTypesForNodes(SemanticModel model, IEnumerable<SyntaxNode> nodes, CancellationToken cancellationToken)
+        {
+            foreach (var node in nodes)
+            {
+                yield return model.GetTypeInfo(node, cancellationToken).Type;
+                var symbol = model.GetSymbolInfo(node, cancellationToken).GetAnySymbol();
+                if (symbol != null)
+                {
+                    if (symbol is IMethodSymbol)
+                    {
+                        foreach (var param in (symbol as IMethodSymbol).Parameters)
+                        {
+                            yield return param.Type;
+                        }
+                    }
+                    if (symbol is IPropertySymbol)
+                    {
+                        foreach (var param in (symbol as IPropertySymbol).Parameters)
+                        {
+                            yield return param.Type;
+                        }
+                    }
+                    yield return symbol?.GetContainingTypeOrThis();
+                }
+                var originalSymbol = symbol?.OriginalDefinition;
+                if (originalSymbol != null)
+                {
+                    if (originalSymbol is IMethodSymbol)
+                    {
+                        foreach (var param in (originalSymbol as IMethodSymbol).Parameters)
+                        {
+                            yield return param.Type;
+                        }
+                    }
+                    if (originalSymbol is IPropertySymbol)
+                    {
+                        foreach (var param in (originalSymbol as IPropertySymbol).Parameters)
+                        {
+                            yield return param.Type;
+                        }
+                    }
+                    yield return originalSymbol?.GetContainingTypeOrThis();
+                }
+            }
+        }
+
+        private static AssemblyIdentity GetAssemblyIdentity(IEnumerable<ITypeSymbol> types)
+        {
+            foreach (var type in types)
+            {
+                var identity = type?.GetBaseTypesAndThis().OfType<IErrorTypeSymbol>().FirstOrDefault()?.ContainingAssembly?.Identity;
+                if (identity != null)
+                {
+                    return identity;
+                }
+
+                identity = type?.AllInterfaces.OfType<IErrorTypeSymbol>().FirstOrDefault()?.ContainingAssembly?.Identity;
+                if (identity != null)
+                {
+                    return identity;
+                }
+            }
+
+            return null;
+        }
+    }
+}
