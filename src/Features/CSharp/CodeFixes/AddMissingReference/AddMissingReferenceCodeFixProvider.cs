d84c4b141a0ec827efd89083124f0a995d9ea37e
Jonathon Marolf <jmarolf@microsoft.com>
2015-05-08 16:41:23 -0700

    Sorting usings

--- src/Features/CSharp/CodeFixes/AddMissingReference/AddMissingReferenceCodeFixProvider.cs
+++ src/Features/CSharp/CodeFixes/AddMissingReference/AddMissingReferenceCodeFixProvider.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using Microsoft.CodeAnalysis.AddMissingReference;
 using Microsoft.CodeAnalysis.CodeFixes;
-using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.AddMissingReference
