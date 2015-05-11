81075e9b393c2614c2a2c6e342615dc7eaf65f5d
Jonathon Marolf <jmarolf@microsoft.com>
2015-05-11 13:13:01 -0700

    Responding to PR feedback
    Assert if we are not on the UI thread and give test authors the choice
    to explicitly pass in the default namespace.

--- src/VisualStudio/Core/Def/Implementation/ProjectSystem/VisualStudioProjectManagementService.cs
+++ src/VisualStudio/Core/Def/Implementation/ProjectSystem/VisualStudioProjectManagementService.cs
@@ -6,6 +6,7 @@ using System.Composition;
 using System.Linq;
 using EnvDTE;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.ProjectManagement;
 using Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem;
@@ -16,10 +17,12 @@ using Roslyn.Utilities;
 namespace Roslyn.VisualStudio.Services.Implementation.ProjectSystem
 {
     [ExportWorkspaceService(typeof(IProjectManagementService), ServiceLayer.Host), Shared]
-    internal class VisualStudioProjectManagementService : IProjectManagementService
+    internal class VisualStudioProjectManagementService : ForegroundThreadAffinitizedObject, IProjectManagementService
     {
         public string GetDefaultNamespace(Microsoft.CodeAnalysis.Project project, Workspace workspace)
         {
+            this.AssertIsForeground();
+
             if (project.Language == LanguageNames.VisualBasic)
             {
                 return "";
