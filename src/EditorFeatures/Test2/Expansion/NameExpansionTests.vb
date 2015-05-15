bded15c7b196ccf07f089afb6af23e51a1e971e7
Shyam N <gnamboo@microsoft.com>
2015-05-15 16:03:14 -0700

    Fix expander to not crash when it encounters alias that binds to a missing namespace.
    Fixes #1913
    
    When expander encounters an alias identifier that binds to a unbound namespace, it would fall through a code path that would throw NotImplementedException. This change fixes the expander to 'handle' this case without crashing (by simply retruning the IdentifierNameSyntax for the namespace that the alias binds to).
    
    I discovered a difference in behavior between VB and C# SemanticModel which means that although I added a corresponding change to VB's expander, my fix is never invoked. I have logged #2805 for the difference and added a skipped test.

--- src/EditorFeatures/Test2/Expansion/NameExpansionTests.vb
+++ src/EditorFeatures/Test2/Expansion/NameExpansionTests.vb
@@ -50,6 +50,193 @@ class C
             Test(input, expected)
         End Sub
 
+        <WorkItem(1913, "https://github.com/dotnet/roslyn/issues/1913")>
+        <Fact, Trait(Traits.Feature, Traits.Features.Expansion)>
+        Public Sub CSharp_SimpleIdentifierAliasExpansion_AliasBinds()
+            Dim input =
+<Workspace>
+    <Project Language="C#" CommonReferences="true">
+        <Document>
+using System;
+namespace NS
+{
+    using Short = LongNamespace;
+    class Test
+    {
+        public object Method1()
+        {
+            return (new {|Expand:Short|}.MyClass()).Prop;
+        }
+    }
+}
+namespace LongNamespace
+{
+    public class MyClass
+    {
+        public object Prop { get; }
+    }
+}
+        </Document>
+    </Project>
+</Workspace>
+
+            Dim expected =
+<code>
+using System;
+namespace NS
+{
+    using Short = LongNamespace;
+    class Test
+    {
+        public object Method1()
+        {
+            return (new global::LongNamespace.MyClass()).Prop;
+        }
+    }
+}
+namespace LongNamespace
+{
+    public class MyClass
+    {
+        public object Prop { get; }
+    }
+}
+</code>
+
+            Test(input, expected)
+        End Sub
+
+        <WorkItem(1913, "https://github.com/dotnet/roslyn/issues/1913")>
+        <Fact, Trait(Traits.Feature, Traits.Features.Expansion)>
+        Public Sub CSharp_SimpleIdentifierAliasExpansion_AliasDoesNotBind()
+            Dim input =
+<Workspace>
+    <Project Language="C#" CommonReferences="true">
+        <Document>
+using System;
+namespace NS
+{
+    using Short = LongNamespace;
+    class Test
+    {
+        public object Method1()
+        {
+            return (new {|Expand:Short|}.MyClass()).Prop;
+        }
+    }
+}
+        </Document>
+    </Project>
+</Workspace>
+
+            Dim expected =
+<code>
+using System;
+namespace NS
+{
+    using Short = LongNamespace;
+    class Test
+    {
+        public object Method1()
+        {
+            return (new LongNamespace.MyClass()).Prop;
+        }
+    }
+}
+</code>
+
+            Test(input, expected)
+        End Sub
+
+        <WorkItem(1913, "https://github.com/dotnet/roslyn/issues/1913")>
+        <Fact, Trait(Traits.Feature, Traits.Features.Expansion)>
+        Public Sub VisualBasic_SimpleIdentifierAliasExpansion_AliasBinds()
+            Dim input =
+<Workspace>
+    <Project Language="Visual Basic" CommonReferences="true">
+        <Document>
+Imports System
+Imports ShortName = LongNamespace
+Namespace NS
+    Class Test
+        Public Function Method1() As Object
+            Return (New {|Expand:ShortName|}.Class1()).Prop
+        End Function
+    End Class
+End Namespace
+Namespace LongNamespace
+    Public Class Class1
+        Public Readonly Property Prop As Object
+            Get
+                Return Nothing
+            End Get
+        End Property
+    End Class
+End Namespace
+        </Document>
+    </Project>
+</Workspace>
+
+            Dim expected =
+<code>
+Imports System
+Imports ShortName = LongNamespace
+Namespace NS
+    Class Test
+        Public Function Method1() As Object
+            Return (New Global.LongNamespace.Class1()).Prop
+        End Function
+    End Class
+End Namespace
+Namespace LongNamespace
+    Public Class Class1
+        Public Readonly Property Prop As Object
+            Get
+                Return Nothing
+            End Get
+        End Property
+    End Class
+End Namespace
+</code>
+
+            Test(input, expected)
+        End Sub
+
+        <WorkItem(1913, "https://github.com/dotnet/roslyn/issues/1913")>
+        <Fact(Skip:="https://github.com/dotnet/roslyn/issues/2805"), Trait(Traits.Feature, Traits.Features.Expansion)>
+        Public Sub VisualBasic_SimpleIdentifierAliasExpansion_AliasDoesNotBind()
+            Dim input =
+<Workspace>
+    <Project Language="Visual Basic" CommonReferences="true">
+        <Document>
+Imports System
+Imports ShortName = LongNamespace
+Namespace NS
+    Class Test
+        Public Function Method1() As Object
+            Return (New {|Expand:ShortName|}.Class1()).Prop
+        End Function
+    End Class
+End Namespace
+        </Document>
+    </Project>
+</Workspace>
+
+            Dim expected =
+<code>
+Imports System
+Imports ShortName = LongNamespace
+Namespace NS
+    Class Test
+        Public Function Method1() As Object
+            Return (New LongNamespace.Class1()).Prop
+        End Function
+    End Class
+End Namespace
+</code>
+
+            Test(input, expected)
+        End Sub
 #End Region
 
     End Class
