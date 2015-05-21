666c9522f468c04d208a3ca579983a327580392c
Andrew Casey <andrew.casey@microsoft.com>
2015-05-21 13:14:49 -0700

    Emit My.InternalXmlHelper under the root namespace
    ...as in Dev12.

--- src/ExpressionEvaluator/VisualBasic/Test/ExpressionCompiler/ExpressionCompilerTests.vb
+++ src/ExpressionEvaluator/VisualBasic/Test/ExpressionCompiler/ExpressionCompilerTests.vb
@@ -1784,7 +1784,7 @@ End Class"
                 ExpressionCompilerUtilities.GenerateUniqueName(),
                 allReferences,
                 exeBytes,
-                Nothing)
+                New SymReader(pdbBytes, exeBytes)) ' Need SymReader to find root namespace.
             Dim context = CreateMethodContext(runtime, "Root.C.M")
             Dim errorMessage As String = Nothing
             Dim testData = New CompilationTestData()
@@ -1799,7 +1799,7 @@ End Class"
   IL_0001:  ldstr      ""a""
   IL_0006:  ldstr      """"
   IL_000b:  call       ""Function System.Xml.Linq.XName.Get(String, String) As System.Xml.Linq.XName""
-  IL_0010:  call       ""Function My.InternalXmlHelper.get_AttributeValue(System.Xml.Linq.XElement, System.Xml.Linq.XName) As String""
+  IL_0010:  call       ""Function Root.My.InternalXmlHelper.get_AttributeValue(System.Xml.Linq.XElement, System.Xml.Linq.XName) As String""
   IL_0015:  ret
 }")
         End Sub
