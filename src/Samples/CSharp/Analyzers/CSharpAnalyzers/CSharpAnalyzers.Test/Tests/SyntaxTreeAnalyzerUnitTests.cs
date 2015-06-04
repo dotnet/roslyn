a058e1a5c9e65e350665000832edfa825d43844f
Manish Vasani <mavasani@microsoft.com>
2015-06-04 15:33:45 -0700

    Add sample C# and VB analyzers to demonstrate different kind of analyzers based on kind of actions registered and analysis scenarios. Also added simple unit tests for the analyzers.

---
+++ src/Samples/CSharp/Analyzers/CSharpAnalyzers/CSharpAnalyzers.Test/Tests/SyntaxTreeAnalyzerUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace CSharpAnalyzers.Test
+{
+    [TestClass]
+    public class SyntaxTreeAnalyzerUnitTests
+        : DiagnosticVerifier
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var test = @"
+class C
+{
+    public void M()
+    {
+    }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticIds.SyntaxTreeAnalyzerRuleId,
+                Message = string.Format(Resources.SyntaxTreeAnalyzerMessageFormat, "Test0.cs"),
+                Severity = DiagnosticSeverity.Warning
+            };
+
+            var parseOptions = CSharpParseOptions.Default.WithDocumentationMode(DocumentationMode.Diagnose);
+            VerifyCSharpDiagnostic(test, parseOptions, compilationOptions: null);
+
+            parseOptions = CSharpParseOptions.Default.WithDocumentationMode(DocumentationMode.None);
+            VerifyCSharpDiagnostic(test, parseOptions, compilationOptions: null, expected: expected);
+
+            parseOptions = CSharpParseOptions.Default.WithDocumentationMode(DocumentationMode.Parse);
+            VerifyCSharpDiagnostic(test, parseOptions, compilationOptions: null, expected: expected);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SyntaxTreeAnalyzer();
+        }
+    }
+}
