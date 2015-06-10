0a875c0ac1c57a0bbda3b87ba552d31684b7c323
Dustin Campbell <dustin@teamcampbell.org>
2015-06-10 15:41:00 -0700

    Rename old extension method container types

--- src/Workspaces/Core/Portable/Shared/Extensions/SyntaxNodeOrTokenExtensions.cs
+++ src/Workspaces/Core/Portable/Shared/Extensions/SyntaxNodeOrTokenExtensions.cs
@@ -1,16 +1,11 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Shared.Extensions
 {
-    internal static class CommonSyntaxNodeOrTokenExtensions
+    internal static class SyntaxNodeOrTokenExtensions
     {
         public static IEnumerable<SyntaxNodeOrToken> DepthFirstTraversal(this SyntaxNodeOrToken node)
         {
--- src/Workspaces/Core/Portable/Shared/Extensions/SyntaxTriviaExtensions.cs
+++ src/Workspaces/Core/Portable/Shared/Extensions/SyntaxTriviaExtensions.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Shared.Extensions
 {
-    internal static class CommonSyntaxTriviaExtensions
+    internal static class SyntaxTriviaExtensions
     {
         public static int Width(this SyntaxTrivia trivia)
         {
