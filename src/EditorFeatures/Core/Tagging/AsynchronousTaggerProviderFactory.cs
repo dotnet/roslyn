54b0706bdc5c34809982358f2b0400ac4844b701
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-07-15 17:37:38 -0700

    Clean up comments.

--- src/EditorFeatures/Core/Tagging/AsynchronousTaggerProviderFactory.cs
+++ src/EditorFeatures/Core/Tagging/AsynchronousTaggerProviderFactory.cs
@@ -1,30 +1,26 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using Microsoft.CodeAnalysis.Editor.Shared.Tagging;
-using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
-using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Shared.TestHooks;
-using Microsoft.VisualStudio.Text;
-using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
 
 namespace Microsoft.CodeAnalysis.Editor.Tagging
 {
     /// <summary>
-    /// Creates <see cref="ITaggerProvider"/>s and <see cref="IViewTaggerProvider"/> that can create
-    ///<see cref="ITagger{T}"/>s that compute <see cref="ITag"/>s for a text buffer in an asynchronous
+    /// Creates <see cref="ITaggerProvider"/>s and <see cref="IViewTaggerProvider"/>s that can create
+    /// <see cref="ITagger{T}"/>s that compute <see cref="ITag"/>s for a text buffer in an asynchronous
     /// manner.  Computation of the tags is handled by a provided <see cref="IAsynchronousTaggerDataSource{TTag}"/>.
     /// The <see cref="ITagger{T}"/> produced by the provider handles the jobs of scheduling when to 
     /// compute tags, managing the collection of tags, as well as notifying and keeping the user 
     /// interface up to date with the latest tags produced.
+    /// 
+    /// For <see cref="ITaggerProvider"/>s and <see cref="IViewTaggerProvider"/>s that would like to 
+    /// pick up a lot of default behavior, there are also <see cref="AsynchronousTaggerProvider{T}"/> and
+    /// <see cref="AsynchronousViewTaggerProvider{T}"/> that can be subclassed.  However, for consumers
+    /// that cannot subclass (for example, because they're already subclassing something else), this 
+    /// factory serves as a convenient way to create asynchronous <see cref="ITaggerProvider"/>s that
+    /// they can then defer to.
     /// </summary>
-    // Note: it might seem desirable to avoid having this class and just expose
-    // AsynchronousBufferTaggerProvider and AsynchronousViewTaggerProvider.  However, those types
-    // expose a lot of complexity through their protected surface area.  For example, all the code
-    // to handle and manipulate complex tag sources.  For taggers that have no need to deal with 
-    // that (including 3rd party taggers), this class serves as a much nicer tagging entrypoint.
     internal static class AsynchronousTaggerProviderFactory
     {
         /// <summary>
