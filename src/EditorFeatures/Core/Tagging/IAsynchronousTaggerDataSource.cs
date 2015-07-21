32fa03dcb1d6364ac5afa6722cac63c22c6e0f94
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-07-21 14:06:21 -0700

    Move enum into its own file.

--- src/EditorFeatures/Core/Tagging/IAsynchronousTaggerDataSource.cs
+++ src/EditorFeatures/Core/Tagging/IAsynchronousTaggerDataSource.cs
@@ -13,35 +13,6 @@ using Microsoft.VisualStudio.Text.Tagging;
 namespace Microsoft.CodeAnalysis.Editor.Tagging
 {
     /// <summary>
-    /// What the async tagger infrastructure should do in the presence of text edits.
-    /// </summary>
-    [Flags]
-    internal enum TaggerTextChangeBehavior
-    {
-        /// <summary>
-        /// The async tagger infrastructure will not track any text changes and will not do 
-        /// anything special in the presence of them.
-        /// </summary>
-        None = 0,
-
-        /// <summary>
-        /// The async tagger infrastructure will track text changes to the subject buffer it is 
-        /// attached to.  The text changes will be provided to the <see cref="AsynchronousTaggerContext{TTag, TState}"/>
-        /// that is passed to <see cref="IAsynchronousTaggerDataSource{TTag, TState}.ProduceTagsAsync"/>.
-        /// </summary>
-        TrackTextChanges = 1 << 0,
-
-        /// <summary>
-        /// The async tagger infrastructure will not track text changes to the subject buffer it is 
-        /// attached to.  The text changes will be provided to the <see cref="AsynchronousTaggerContext{TTag, TState}"/>
-        /// that is passed to <see cref="IAsynchronousTaggerDataSource{TTag, TState}.ProduceTagsAsync"/>.
-        /// 
-        /// Tags that intersect the text change range will immediately removed.
-        /// </summary>
-        RemoveTagsThatIntersectEdits = TrackTextChanges | (1 << 1)
-    }
-
-    /// <summary>
     /// Data source for the <see cref="AsynchronousTaggerProvider{TTag, TState}"/>.  This type tells the
     /// <see cref="AsynchronousTaggerProvider{TTag, TState}"/> when tags need to be recomputed, as well
     /// as producing the tags when requested.
---
+++ src/EditorFeatures/Core/Tagging/TaggerTextChangeBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Microsoft.CodeAnalysis.Editor.Tagging
+{
+    /// <summary>
+    /// What the async tagger infrastructure should do in the presence of text edits.
+    /// </summary>
+    [Flags]
+    internal enum TaggerTextChangeBehavior
+    {
+        /// <summary>
+        /// The async tagger infrastructure will not track any text changes and will not do 
+        /// anything special in the presence of them.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The async tagger infrastructure will track text changes to the subject buffer it is 
+        /// attached to.  The text changes will be provided to the <see cref="AsynchronousTaggerContext{TTag, TState}"/>
+        /// that is passed to <see cref="IAsynchronousTaggerDataSource{TTag, TState}.ProduceTagsAsync"/>.
+        /// </summary>
+        TrackTextChanges = 1 << 0,
+
+        /// <summary>
+        /// The async tagger infrastructure will not track text changes to the subject buffer it is 
+        /// attached to.  The text changes will be provided to the <see cref="AsynchronousTaggerContext{TTag, TState}"/>
+        /// that is passed to <see cref="IAsynchronousTaggerDataSource{TTag, TState}.ProduceTagsAsync"/>.
+        /// 
+        /// Tags that intersect the text change range will immediately removed.
+        /// </summary>
+        RemoveTagsThatIntersectEdits = TrackTextChanges | (1 << 1)
+    }
+}
