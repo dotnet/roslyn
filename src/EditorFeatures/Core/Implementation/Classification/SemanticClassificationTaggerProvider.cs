02804c6ca114dd515bc7877d9fca50361172a849
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-07-21 03:01:59 -0700

    Move semantic classification over to the new tagger infrastructure.
    Remove its tag-source based implementation as that is no longer necessary.

--- src/EditorFeatures/Core/Implementation/Classification/SemanticClassificationTaggerProvider.cs
+++ src/EditorFeatures/Core/Implementation/Classification/SemanticClassificationTaggerProvider.cs
@@ -21,36 +21,26 @@ using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Utilities;
 using Roslyn.Utilities;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Microsoft.CodeAnalysis.Editor.Implementation.Classification
 {
-    using Context = AsynchronousTaggerContext<IClassificationTag, object>;
+    using LanguageServices;
+    using Text.Shared.Extensions;
+    using Text;
+    using Context = AsynchronousTaggerContext<IClassificationTag, VersionStamp?>;
 
     [Export(typeof(ITaggerProvider))]
     [TagType(typeof(IClassificationTag))]
     [ContentType(ContentTypeNames.CSharpContentType)]
     [ContentType(ContentTypeNames.VisualBasicContentType)]
-    internal partial class SemanticClassificationTaggerProvider :
-        ForegroundThreadAffinitizedObject,
-        ITaggerProvider,
-        IAsynchronousTaggerDataSource<IClassificationTag, object>
+    internal partial class SemanticClassificationTaggerProvider : AsynchronousTaggerProvider<IClassificationTag, VersionStamp?>
     {
         private readonly ISemanticChangeNotificationService _semanticChangeNotificationService;
         private readonly ClassificationTypeMap _typeMap;
-        private readonly Lazy<ITaggerProvider> _asynchronousTaggerProvider;
 
-        // We don't want to remove a tag just because it intersected an edit.  This can 
-        // cause flashing when a edit touches the edge of a classified symbol without
-        // changing it.  For example, if you have "Console." and you remove the <dot>,
-        // then you don't want to remove the classification for 'Console'.
-        public bool IgnoreCaretMovementToExistingTag => false;
-        public bool RemoveTagsThatIntersectEdits => false;
-        public IEqualityComparer<IClassificationTag> TagComparer => null;
-        public SpanTrackingMode SpanTrackingMode => SpanTrackingMode.EdgeExclusive;
-        public bool ComputeTagsSynchronouslyIfNoAsynchronousComputationHasCompleted => false;
-
-        public IEnumerable<Option<bool>> Options => SpecializedCollections.SingletonEnumerable(InternalFeatureOnOffOptions.SemanticColorizer);
-        public IEnumerable<PerLanguageOption<bool>> PerLanguageOptions => null;
+        public override IEnumerable<Option<bool>> Options => SpecializedCollections.SingletonEnumerable(InternalFeatureOnOffOptions.SemanticColorizer);
 
         private IEditorClassificationService _classificationService;
 
@@ -60,30 +50,13 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Classification
             ISemanticChangeNotificationService semanticChangeNotificationService,
             ClassificationTypeMap typeMap,
             [ImportMany] IEnumerable<Lazy<IAsynchronousOperationListener, FeatureMetadata>> asyncListeners)
+            : base(new AggregateAsynchronousOperationListener(asyncListeners, FeatureAttribute.Classification), notificationService)
         {
             _semanticChangeNotificationService = semanticChangeNotificationService;
             _typeMap = typeMap;
-            _asynchronousTaggerProvider = new Lazy<ITaggerProvider>(() =>
-                new AsynchronousBufferTaggerProviderWithTagSource<IClassificationTag, object>(
-                    this,
-                    new AggregateAsynchronousOperationListener(asyncListeners, FeatureAttribute.Classification),
-                    notificationService,
-                    CreateTagSource));
         }
 
-        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
-        {
-            return _asynchronousTaggerProvider.Value.CreateTagger<T>(buffer);
-        }
-
-        private ProducerPopulatedTagSource<IClassificationTag, object> CreateTagSource(
-            ITextView textViewOpt, ITextBuffer subjectBuffer,
-            IAsynchronousOperationListener asyncListener, IForegroundNotificationService notificationService)
-        {
-            return new SemanticClassificationTagSource(subjectBuffer, this, asyncListener, notificationService);
-        }
-
-        public ITaggerEventSource CreateEventSource(ITextView textViewOpt, ITextBuffer subjectBuffer)
+        public override ITaggerEventSource CreateEventSource(ITextView textViewOpt, ITextBuffer subjectBuffer)
         {
             return TaggerEventSources.Compose(
                 TaggerEventSources.OnSemanticChanged(subjectBuffer, TaggerDelay.Short, _semanticChangeNotificationService),
@@ -91,41 +64,138 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Classification
                 TaggerEventSources.OnTextChanged(subjectBuffer, TaggerDelay.Short, reportChangedSpans: true));
         }
 
-        public IEnumerable<SnapshotSpan> GetSpansToTag(ITextView textViewOpt, ITextBuffer subjectBuffer)
+        public override async Task ProduceTagsAsync(Context context)
         {
-            return null;
+            Debug.Assert(context.SpansToTag.IsSingle());
+            Debug.Assert(context.CaretPosition == null);
+
+            var spanToTag = context.SpansToTag.Single();
+            var document = spanToTag.Document;
+
+            if (document == null)
+            {
+                return;
+            }
+
+            if (_classificationService == null)
+            {
+                _classificationService = document.Project.LanguageServices.GetService<IEditorClassificationService>();
+            }
+
+            if (_classificationService == null)
+            {
+                return;
+            }
+
+            var cancellationToken = context.CancellationToken;
+            var newVersion = await document.Project.GetDependentSemanticVersionAsync(cancellationToken).ConfigureAwait(false);
+
+            await ProduceTagsAsync(context, spanToTag, newVersion).ConfigureAwait(false);
+
+            context.State = newVersion;
         }
 
-        public Task ProduceTagsAsync(Context context)
+        private async Task ProduceTagsAsync(Context context, DocumentSnapshotSpan spanToTag, VersionStamp newVersion)
         {
-            return TaggerUtilities.Delegate(context, ProduceTagsAsync);
+            if (await TryProduceTagsSpecializedAsync(context, spanToTag, newVersion).ConfigureAwait(false))
+            {
+                return;
+            }
+
+            // We weren't able to use our specialized codepaths for semantic classifying. 
+            // Fall back to classifying the full span that was asked for.
+            await ClassifySpansAsync(context, spanToTag).ConfigureAwait(false);
         }
 
-        private async Task ProduceTagsAsync(Context context, DocumentSnapshotSpan spanToTag, int? caretPosition)
+        private Task<bool> TryProduceTagsSpecializedAsync(Context context, DocumentSnapshotSpan spanToTag, VersionStamp newVersion)
+        {
+            var range = context.TextChangeRange;
+
+            if (range == null)
+            {
+                return Task.FromResult(TryProduceTagsWithNoTextChange(context, newVersion));
+            }
+            else
+            {
+                return TryProduceTagsWithTextChange(context, spanToTag, newVersion, range);
+            }
+        }
+
+        private async Task<bool> TryProduceTagsWithTextChange(Context context, DocumentSnapshotSpan spanToTag, VersionStamp newVersion, TextChangeRange? range)
+        {
+            // there was top level edit, check whether that edit updated top level element
+            var document = spanToTag.Document;
+            var service = document.Project.LanguageServices.GetService<ISyntaxFactsService>();
+            var oldVersion = context.State;
+            if (service == null || newVersion != oldVersion)
+            {
+                // There's been a version change since the last time we were called.  Reclassify 
+                // everything.
+                return false;
+            }
+
+            // perf optimization. Check whether all edits since the last update has happened within
+            // a member. If it did, it will find the member that contains the changes and only refresh
+            // that member.  If possible, try to get a speculative binder to make things even cheaper.
+
+            var cancellationToken = context.CancellationToken;
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            var changedSpan = new TextSpan(range.Value.Span.Start, range.Value.NewLength);
+            var member = service.GetContainingMemberDeclaration(root, changedSpan.Start);
+            if (member == null || !member.FullSpan.Contains(changedSpan))
+            {
+                // The edit was not fully contained in a member.  Reclassify everything.
+                return false;
+            }
+
+            var subTextSpan = service.GetMemberBodySpanForSpeculativeBinding(member);
+            var subSpan = subTextSpan.Contains(changedSpan) ? subTextSpan.ToSpan() : member.FullSpan.ToSpan();
+
+            var subSpanToTag = new DocumentSnapshotSpan(spanToTag.Document,
+                new SnapshotSpan(spanToTag.SnapshotSpan.Snapshot, subSpan));
+
+            // re-classify only the member we're inside.
+            await ClassifySpansAsync(context, subSpanToTag).ConfigureAwait(false);
+            return true;
+        }
+
+        private static bool TryProduceTagsWithNoTextChange(Context context, VersionStamp newVersion)
+        {
+            var oldVersion = context.State;
+
+            // active file can be called twice for the same top level edit (the very last top
+            // level edit).  One from text edit event source and one from semantic change event
+            // source.  This make sure that when we are called to recompute due to semantic 
+            // change event source, we haven't already recompute it by text edits event source.
+            // for opened files that are not active, it should be called by semantic change 
+            // event source and recompute tags for whole file.
+            if (newVersion != oldVersion)
+            {
+                // There was no text change range specified.  And there was a version change,
+                // we have to reclassify everything.
+                return false;
+            }
+
+            // Mark that we didn't tag any spans.  That way we preserve all the old tags
+            // from the last time we were called.
+            context.SetSpansTagged(SpecializedCollections.EmptyEnumerable<DocumentSnapshotSpan>());
+
+            // Return true so we do not do any further classification.
+            return true;
+        }
+
+        private async Task ClassifySpansAsync(Context context, DocumentSnapshotSpan spanToTag)
         {
             try
             {
-                var cancellationToken = context.CancellationToken;
                 var document = spanToTag.Document;
                 var snapshotSpan = spanToTag.SnapshotSpan;
                 var snapshot = snapshotSpan.Snapshot;
-                if (document == null)
-                {
-                    return;
-                }
-
-                if (_classificationService == null)
-                {
-                    _classificationService = document.Project.LanguageServices.GetService<IEditorClassificationService>();
-                }
-
-                if (_classificationService == null)
-                {
-                    return;
-                }
 
                 // we don't directly reference the semantic model here, we just keep it alive so 
                 // the classification service does not need to block to produce it.
+                var cancellationToken = context.CancellationToken;
                 using (Logger.LogBlock(FunctionId.Tagger_SemanticClassification_TagProducer_ProduceTags, cancellationToken))
                 {
                     var textSpan = snapshotSpan.Span.ToTextSpan();
@@ -138,6 +208,9 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Classification
 
                     ClassificationUtilities.Convert(_typeMap, snapshotSpan.Snapshot, classifiedSpans, context.AddTag);
                     ClassificationUtilities.ReturnClassifiedSpanList(classifiedSpans);
+
+                    // Let the context know that this was the span we actually tried to tag.
+                    context.SetSpansTagged(SpecializedCollections.SingletonEnumerable(spanToTag));
                 }
             }
             catch (Exception e) when (FatalError.ReportUnlessCanceled(e))
