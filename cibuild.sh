4ca8634bc5bd3838743eeae2d67927d92c0bb592
Andy Gocke <angocke@microsoft.com>
2015-07-30 13:17:12 -0700

    Update the XUnit runners to support CoreCLR

--- cibuild.sh
+++ cibuild.sh
@@ -10,7 +10,7 @@ usage()
     echo "  --os <os>           OS to run (Linux / Darwin)"
 }
 
-XUNIT_VERSION=2.0.0-alpha-build2576
+XUNIT_VERSION=2.1.0-beta3-build3029
 BUILD_CONFIGURATION=Debug
 OS_NAME=$(uname -s)
 
@@ -175,7 +175,7 @@ set_mono_path()
 
 test_roslyn()
 {
-    local xunit_runner=packages/xunit.runners.$XUNIT_VERSION/tools/xunit.console.x86.exe
+    local xunit_runner=packages/xunit.runner.console.$XUNIT_VERSION/tools/xunit.console.x86.exe
     local test_binaries=(
         Roslyn.Compilers.CSharp.CommandLine.UnitTests
         Roslyn.Compilers.CSharp.Syntax.UnitTests
@@ -203,7 +203,6 @@ test_roslyn()
 # temporary work around.  
 echo Restoring NuGet packages
 run_nuget restore Roslyn.sln
-run_nuget install xunit.runners -PreRelease -Version $XUNIT_VERSION -OutputDirectory packages
 
 set_mono_path
 which mono
