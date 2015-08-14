60e642d3665683a78f8a075e63fc9bf98036d661
Tomas Matousek <tomas.matousek@microsoft.com>
2015-08-14 10:33:20 -0700

    Portable scripting tests

---
+++ src/Scripting/VisualBasicTest.Desktop/InteractiveSessionTests.vb
@@ -0,0 +1,15 @@
+' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports Microsoft.CodeAnalysis.Scripting.Test
+Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Roslyn.Test.Utilities
+Imports Xunit
+
+Namespace Microsoft.CodeAnalysis.Scripting.VisualBasic.UnitTests
+
+    Public Class InteractiveSessionTests
+        Inherits TestBase
+
+        ' TODO: port tests from C#
+    End Class
+End Namespace
