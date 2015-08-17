d3dd374399eef951366830beb533e701e877e165
David Kean <davkean@microsoft.com>
2015-08-17 15:25:54 -0700

    Added a line break between copyright and usings to match Roslyn standards

--- src/VisualStudio/ProjectSystem/Microsoft.VisualStudio.ProjectSystem.Managed/IItemTypeGuidProvider.cs
+++ src/VisualStudio/ProjectSystem/Microsoft.VisualStudio.ProjectSystem.Managed/IItemTypeGuidProvider.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+
 // WORKAROUND: Temporary until we can pick up the latest CPS SDK
 using System;
 
