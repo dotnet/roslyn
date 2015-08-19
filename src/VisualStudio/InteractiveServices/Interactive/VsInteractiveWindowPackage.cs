585975ee847377c598b106ad9f8257c37df5a9bc
Andrew Casey <andrew.casey@microsoft.com>
2015-08-19 11:12:20 -0700

    Force loading of the Roslyn package to hook up its FatalError handlers.

--- src/VisualStudio/InteractiveServices/Interactive/VsInteractiveWindowPackage.cs
+++ src/VisualStudio/InteractiveServices/Interactive/VsInteractiveWindowPackage.cs
@@ -33,6 +33,11 @@ namespace Microsoft.VisualStudio.LanguageServices.Interactive
         {
             base.Initialize();
 
+            // Load the Roslyn package so that its FatalError handlers are hooked up.
+            IVsPackage setupPackage;
+            var shell = (IVsShell)this.GetService(typeof(SVsShell));
+            shell.LoadPackage(Guids.RoslynPackageId, out setupPackage);
+
             _componentModel = (IComponentModel)GetService(typeof(SComponentModel));
             _interactiveWindowProvider = _componentModel.DefaultExportProvider.GetExportedValue<TVsInteractiveWindowProvider>();
 
