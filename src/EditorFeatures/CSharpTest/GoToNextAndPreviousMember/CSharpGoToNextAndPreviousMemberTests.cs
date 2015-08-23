ab9cd4cd31f57b4f820d1648096e87c2c3380ab4
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2015-08-23 16:08:17 -0700

    First test.

--- src/EditorFeatures/CSharpTest/GoToNextAndPreviousMember/CSharpGoToNextAndPreviousMemberTests.cs
+++ src/EditorFeatures/CSharpTest/GoToNextAndPreviousMember/CSharpGoToNextAndPreviousMemberTests.cs
@@ -2,11 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests
 {
-    class CSharpGoToNextAndPreviousMemberTests
+    public class CSharpGoToNextAndPreviousMemberTests
     {
+        [Fact, Trait(Traits.Feature, Traits.Features.GoToNextAndPreviousMember)]
+        public void EmptyFile()
+        {
+            var code = @"$$";
+            Assert.Null(GetTargetPosition(code, next: true));
+        }
+
+        private static int? GetTargetPosition(string code, bool next)
+        {
+            int? targetPosition;
+            using (var workspace = TestWorkspaceFactory.CreateWorkspaceFromLines(
+                LanguageNames.CSharp,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary),
+                CSharpParseOptions.Default,
+                code))
+            {
+                var hostDocument = workspace.DocumentWithCursor;
+
+                targetPosition = GoToNextAndPreviousMethodCommandHandler.GetTargetPosition(
+                    workspace.CurrentSolution.GetDocument(hostDocument.Id),
+                    hostDocument.CursorPosition.Value,
+                    next,
+                    CancellationToken.None);
+            }
+
+            return targetPosition;
+        }
     }
 }
