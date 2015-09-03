e86c57a3ce07e24c1cd037c85f49dc17af13f178
Andrew Casey <andrew.casey@microsoft.com>
2015-09-03 14:20:41 -0700

    Fix layer.

--- src/Interactive/EditorFeatures/Core/Implementation/Interactive/InteractiveDocumentSupportsSuggestionService.cs
+++ src/Interactive/EditorFeatures/Core/Implementation/Interactive/InteractiveDocumentSupportsSuggestionService.cs
@@ -1,13 +1,12 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Composition;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Editor.Shared.SuggestionSupport;
 using Microsoft.CodeAnalysis.Host.Mef;
 
-namespace Microsoft.VisualStudio.LanguageServices.Implementation.SuggestionService
+namespace Microsoft.CodeAnalysis.Editor.Implementation.Interactive
 {
-    [ExportWorkspaceService(typeof(IDocumentSupportsSuggestionService), ServiceLayer.Host), Shared]
+    [ExportWorkspaceService(typeof(IDocumentSupportsSuggestionService), WorkspaceKind.Interactive), Shared]
     internal sealed class VisualStudioDocumentSupportsCodeFixService : IDocumentSupportsSuggestionService
     {
         public bool SupportsCodeFixes(Document document)
