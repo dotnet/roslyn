e5430878a8c8aa904e73be38cf34aa7336246bb4
Manish Vasani <mavasani@microsoft.com>
2015-09-11 15:29:52 -0700

    Fix a deadlock in the analyzer driver
    Primary issue is that we currently try to force complete and decode SuppressMessageAttributes in the analyzer callbacks to report a diagnostic. We need to decode these attributes to apply source suppressions to reported diagnostics. However, force completing attributes can lead to us binding more symbols, and subsequently generating more symbol declared events and attempting to callback into the analyzer again.
    
    Fix is to decode the suppress message attributes as a post pass in the analyzer driver and avoid any symbol force completion during analyzer callbacks to report diagnostics.
    
    Fixes #4858

--- src/Test/Utilities/Shared/Diagnostics/DiagnosticExtensions.cs
+++ src/Test/Utilities/Shared/Diagnostics/DiagnosticExtensions.cs
@@ -170,7 +170,7 @@ namespace Microsoft.CodeAnalysis
             Compilation newCompilation;
             var driver = AnalyzerDriver.CreateAndAttachToCompilation(c, analyzersArray, options, AnalyzerManager.Instance, onAnalyzerException, false, out newCompilation, CancellationToken.None);
             var discarded = newCompilation.GetDiagnostics();
-            diagnostics = driver.GetDiagnosticsAsync().Result.AddRange(exceptionDiagnostics);
+            diagnostics = driver.GetDiagnosticsAsync(newCompilation).Result.AddRange(exceptionDiagnostics);
 
             return (TCompilation)newCompilation; // note this is a new compilation
         }
