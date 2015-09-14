91c86ba88d46daf73b9509d440752681c6004e43
Brett V. Forsgren <brettfo@microsoft.com>
2015-09-14 13:43:03 -0700

    add missing copyright notices

--- src/EditorFeatures/VisualBasic/UseAutoProperty/UseAutoPropertyAnalyzer.vb
+++ src/EditorFeatures/VisualBasic/UseAutoProperty/UseAutoPropertyAnalyzer.vb
@@ -1,4 +1,6 @@
-﻿Imports System.Collections.Concurrent
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports System.Collections.Concurrent
 Imports System.ComponentModel.Composition
 Imports System.Threading
 Imports Microsoft.CodeAnalysis.Diagnostics
