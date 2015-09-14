d1ea89b8088d56402709e861d968b8747038fa13
David Kean <davkean@microsoft.com>
2015-09-14 13:36:57 -0700

    Added XML comments to HierarchyId

--- src/VisualStudio/ProjectSystem/Microsoft.VisualStudio.ProjectSystem.Managed.VS/Shell/HierarchyId.cs
+++ src/VisualStudio/ProjectSystem/Microsoft.VisualStudio.ProjectSystem.Managed.VS/Shell/HierarchyId.cs
@@ -3,15 +3,31 @@
 namespace Microsoft.VisualStudio.Shell
 {
     /// <summary>
-    ///     Represents an ITEMID in an IVsHierarchy
+    ///     Represents an ITEMID in an IVsHierarchy.
     /// </summary>
     internal struct HierarchyId
     {
         private readonly uint _id;
 
-        public static readonly HierarchyId Selection = new HierarchyId(VSConstants.VSITEMID_SELECTION);
+        /// <summary>
+        ///     Represents the root of a project hierarchy and is used to identify the entire hierarchy, as opposed 
+        ///     to a single item.
+        /// </summary>
         public static readonly HierarchyId Root = new HierarchyId(VSConstants.VSITEMID_ROOT);
+
+        /// <summary>
+        ///     Represents the currently selected items, which can include the root of the hierarchy.
+        /// </summary>
+        public static readonly HierarchyId Selection = new HierarchyId(VSConstants.VSITEMID_SELECTION);
+
+        /// <summary>
+        ///     Represents the absence of a project item. This value is used when there is no current selection.
+        /// </summary>
         public static readonly HierarchyId Nil = new HierarchyId(VSConstants.VSITEMID_NIL);
+
+        /// <summary>
+        ///     Represent an empty item.
+        /// </summary>
         public static readonly HierarchyId Empty = new HierarchyId(0);
 
         public HierarchyId(uint id)
@@ -19,31 +35,53 @@ namespace Microsoft.VisualStudio.Shell
             _id = id;
         }
 
+        /// <summary>
+        ///     Returns the underlying ITEMID.
+        /// </summary>
         public uint Id
         {
             get { return _id; }
         }
 
+        /// <summary>
+        ///     Returns a value indicating if the <see cref="HierarchyId"/> represents the root of a project hierarchy
+        ///     and is used to identify the entire hierarchy, as opposed to a single item.
+        /// </summary>
         public bool IsRoot
         {
             get { return _id == Root.Id; }
         }
 
+        /// <summary>
+        ///     Returns a value indicating if the <see cref="HierarchyId"/> represents the currently selected items, 
+        ///     which can include the root of the hierarchy.
+        /// </summary>
         public bool IsSelection
         {
             get { return _id == Selection.Id; }
         }
 
+        /// <summary>
+        ///    Returns a value indicating if the <see cref="HierarchyId"/> is empty.
+        /// </summary>
         public bool IsEmpty
         {
             get { return _id == Empty.Id; }
         }
 
+        /// <summary>
+        ///    Returns a value indicating if the <see cref="HierarchyId"/> is <see cref="IsNil"/> or 
+        ///    <see cref="IsEmpty"/>.
+        /// </summary>
         public bool IsNilOrEmpty
         {
             get { return IsNil || IsEmpty; }
         }
 
+        /// <summary>
+        ///    Returns a value indicating if the <see cref="HierarchyId"/> represents the absence of a project item. 
+        ///    This value is used when there is no current selection.
+        /// </summary>
         public bool IsNil
         {
             get { return _id == Nil.Id; }
