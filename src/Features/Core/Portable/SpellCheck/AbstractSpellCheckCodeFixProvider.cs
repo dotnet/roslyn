2136f84a1ce47c74a068bfdc584b42c856177ff9
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-09-15 00:48:19 -0700

    PR feedback.

--- src/Features/Core/Portable/SpellCheck/AbstractSpellCheckCodeFixProvider.cs
+++ src/Features/Core/Portable/SpellCheck/AbstractSpellCheckCodeFixProvider.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeActions;
