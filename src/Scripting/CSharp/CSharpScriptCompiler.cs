3c8990258aca3d43087a2e0de647dd3bfa637a1d
Tomas Matousek <tomas.matousek@microsoft.com>
2015-09-16 18:34:31 -0700

    Implements C# CLI REPL and script runner

--- src/Scripting/CSharp/CSharpScriptCompiler.cs
+++ src/Scripting/CSharp/CSharpScriptCompiler.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Threading;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Scripting.Hosting;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Scripting.CSharp
