acf0c59cb4d90aa289453ba7b797dc543c8b38e6
Darren Blaby <giftednewt@hotmail.com>
2015-09-17 01:42:21 +0100

    Add copyright to files

--- src/EditorFeatures/VisualBasicTest/Diagnostics/OverloadBase/OverloadBaseTests.vb
+++ src/EditorFeatures/VisualBasicTest/Diagnostics/OverloadBase/OverloadBaseTests.vb
@@ -1,4 +1,6 @@
-﻿Imports Microsoft.CodeAnalysis
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports Microsoft.CodeAnalysis
 Imports Microsoft.CodeAnalysis.CodeFixes
 Imports Microsoft.CodeAnalysis.Diagnostics
 Imports Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.Diagnostics
