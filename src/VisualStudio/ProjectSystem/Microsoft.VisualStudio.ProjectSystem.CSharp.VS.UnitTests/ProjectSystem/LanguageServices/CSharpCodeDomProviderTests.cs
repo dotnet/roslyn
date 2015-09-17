08da09f5101c44df9efec9284a47f6ca0b016451
David Kean <davkean@microsoft.com>
2015-09-17 11:22:12 -0700

    Changed CSharp/VisualBasic CodeDomProvider's to derive from fx code providers

---
+++ src/VisualStudio/ProjectSystem/Microsoft.VisualStudio.ProjectSystem.CSharp.VS.UnitTests/ProjectSystem/LanguageServices/CSharpCodeDomProviderTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.VisualStudio.ProjectSystem.LanguageServices
+{
+    [UnitTest]
+    public class CSharpCodeDomProviderTests
+    {
+        [Fact]
+        public void Constructor_DoesNotThrow()
+        {
+            new CSharpCodeDomProvider();
+        }
+    }
+}
