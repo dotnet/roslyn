7d128cfe7e34e9249f3dbeb93a75ea83fdc5ebe0
David Kean <davkean@microsoft.com>
2015-09-18 12:36:00 -0700

    Added tests for VsProjectFeatures

---
+++ src/VisualStudio/ProjectSystem/Microsoft.VisualStudio.ProjectSystem.Managed.VS.UnitTests/Mocks/IUnconfiguredProjectVsServicesFactory.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.VisualStudio.Shell.Interop;
+using Moq;
+
+namespace Microsoft.VisualStudio.ProjectSystem
+{
+    internal static class IUnconfiguredProjectVsServicesFactory
+    {
+        public static IUnconfiguredProjectVsServices ImplementHierarchy(Func<IVsHierarchy> action)
+        {
+            var mock = new Mock<IUnconfiguredProjectVsServices>();
+            mock.SetupGet(h => h.Hierarchy)
+                .Returns(action);
+
+            return mock.Object;
+        }
+    }
+}
