1970bcba20740efd97f2c12c1a2ddcceb28c4261
Hosch250 <hosch250@live.com>
2015-09-21 21:03:00 -0500

    Should fix issue #5219

---
+++ src/Compilers/VisualBasic/Test/Syntax/Syntax/SyntaxNodeTests.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Xunit
+
+Namespace Syntax
+
+    Public Class SyntaxNodeTests
+        <Fact>
+        Public Sub SyntaxTree_WithOperatorTokenUpdatesOperatorToken()
+
+            Dim expression = SyntaxFactory.AddExpression(SyntaxFactory.ParseExpression("5"),
+                                                         SyntaxFactory.ParseToken("+"),
+                                                         SyntaxFactory.ParseExpression("3"))
+
+            Dim newOperatorToken = SyntaxFactory.Token(SyntaxKind.MinusToken)
+            
+            Dim newExpression = expression.WithOperatorToken(newOperatorToken)
+
+            Assert.Equal(newExpression.Kind, SyntaxKind.SubtractExpression)
+            Assert.Equal(newExpression.OperatorToken.Kind, SyntaxKind.MinusToken)
+        End Sub
+    End Class
+End NameSpace
