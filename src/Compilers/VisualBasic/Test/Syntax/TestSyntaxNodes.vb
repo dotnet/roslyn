a4b0396261de88126fbd943ed0fb2ac950bbd3a4
Hosch250 <hosch250@live.com>
2015-09-22 12:56:13 -0500

    Move unit test.

--- src/Compilers/VisualBasic/Test/Syntax/TestSyntaxNodes.vb
+++ src/Compilers/VisualBasic/Test/Syntax/TestSyntaxNodes.vb
@@ -49,6 +49,21 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.UnitTests
         End Function
 
         <Fact>
+        Public Sub SyntaxTree_WithOperatorTokenUpdatesOperatorToken()
+
+            Dim expression = SyntaxFactory.AddExpression(SyntaxFactory.ParseExpression("5"),
+                                                         SyntaxFactory.ParseToken("+"),
+                                                         SyntaxFactory.ParseExpression("3"))
+
+            Dim newOperatorToken = SyntaxFactory.Token(SyntaxKind.MinusToken)
+            
+            Dim newExpression = expression.WithOperatorToken(newOperatorToken)
+
+            Assert.Equal(newExpression.Kind, SyntaxKind.SubtractExpression)
+            Assert.Equal(newExpression.OperatorToken.Kind, SyntaxKind.MinusToken)
+        End Sub
+
+        <Fact>
         Public Sub TestParents()
             Dim simpleTree = CreateSimpleTree()
 
