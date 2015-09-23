51919aab369feec39abfcd43fc656a3c7e6470f6
Andy Gocke <angocke@microsoft.com>
2015-09-23 16:30:21 -0700

    Update the *nix NuGet packages

--- cibuild.sh
+++ cibuild.sh
@@ -79,7 +79,7 @@ restore_nuget()
 {
     acquire_sem_or_wait "restore_nuget"
 
-    local package_name="nuget.12.zip"
+    local package_name="nuget.13.zip"
     local target="/tmp/$package_name"
     echo "Installing NuGet Packages $target"
     if [ -f $target ]; then
