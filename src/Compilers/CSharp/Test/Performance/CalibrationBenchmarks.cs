02f4bd30a2c064e68ff3954445e84f585ac15256
Paul Harrington <pharring@microsoft.com>
2015-09-23 21:49:57 -0700

    Reduce permutaions of calibration sleep test

--- src/Compilers/CSharp/Test/Performance/CalibrationBenchmarks.cs
+++ src/Compilers/CSharp/Test/Performance/CalibrationBenchmarks.cs
@@ -15,14 +15,7 @@ namespace Microsoft.CodeAnalysis.CSharp.PerformanceTests
         }
 
         [Benchmark]
-        [InlineData(10)]
-        [InlineData(20)]
-        [InlineData(50)]
         [InlineData(100)]
-        [InlineData(200)]
-        [InlineData(500)]
-        [InlineData(1000)]
-        [InlineData(2000)]
         public void Sleep(int durationInMilliseconds)
         {
             Benchmark.Iterate(() => Thread.Sleep(durationInMilliseconds));
