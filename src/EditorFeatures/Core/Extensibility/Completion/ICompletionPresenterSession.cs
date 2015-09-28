05dbd248b369f2a6189492e5e6f20f83d34a2829
Ravi Chande <rchande@microsoft.com>
2015-09-28 15:39:09 -0700

    Update ICompletionPresenter with the concept of presenting multiple models * Add a CompletionListSelected event based on the list selected in the editor * Encapsulate the old parameters of PresentModels into a single object

--- src/EditorFeatures/Core/Extensibility/Completion/ICompletionPresenterSession.cs
+++ src/EditorFeatures/Core/Extensibility/Completion/ICompletionPresenterSession.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Completion;
 using Microsoft.VisualStudio.Text;
 
@@ -9,8 +10,7 @@ namespace Microsoft.CodeAnalysis.Editor
 {
     internal interface ICompletionPresenterSession : IIntelliSensePresenterSession
     {
-        void PresentItems(ITrackingSpan triggerSpan, IList<CompletionItem> items, CompletionItem selectedItem,
-            CompletionItem presetBuilder, bool suggestionMode, bool isSoftSelected);
+        void PresentModels(ITrackingSpan triggerSpan, ImmutableArray<CompletionPresentationData> models, bool suggestionMode);
 
         void SelectPreviousItem();
         void SelectNextItem();
@@ -19,5 +19,35 @@ namespace Microsoft.CodeAnalysis.Editor
 
         event EventHandler<CompletionItemEventArgs> ItemSelected;
         event EventHandler<CompletionItemEventArgs> ItemCommitted;
+        event EventHandler<CompletionListSelectedEventArgs> CompletionListSelected;
+    }
+
+    internal class CompletionPresentationData
+    {
+        public int ModelId { get; }
+
+        public IList<CompletionItem> Items { get; }
+        public CompletionItem SelectedItem { get; }
+        public CompletionItem PresetBuilder { get; }
+        public bool IsSoftSelected { get; }
+        public string Title { get; }
+        public bool IsSelectedList { get; }
+
+        public CompletionPresentationData(IList<CompletionItem> items, 
+            CompletionItem selectedItem, 
+            CompletionItem presetBuilder, 
+            bool isSoftSelected, 
+            int modelId, 
+            string title, 
+            bool isSelectedModel)
+        {
+            this.Items = items;
+            this.SelectedItem = selectedItem;
+            this.PresetBuilder = presetBuilder;
+            this.IsSoftSelected = isSoftSelected;
+            this.ModelId = modelId;
+            this.Title = title;
+            this.IsSelectedList = isSelectedModel;
+        }
     }
 }
