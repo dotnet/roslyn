599a835bf9b2026896363cf4d28b130df702a74e
Ravi Chande <rchande@microsoft.com>
2015-09-28 15:42:53 -0700

    Update IController and ISession to produce multiple models * Instead of a single TModel, the result is now an ImmutableArray<TModel>

--- src/EditorFeatures/Core/Implementation/IntelliSense/IController.cs
+++ src/EditorFeatures/Core/Implementation/IntelliSense/IController.cs
@@ -1,12 +1,13 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Shared.TestHooks;
 
 namespace Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense
 {
     internal interface IController<TModel>
     {
-        void OnModelUpdated(TModel result);
+        void OnModelsUpdated(ImmutableArray<TModel> result);
         IAsyncToken BeginAsyncOperation();
         void StopModelComputation();
     }
--- src/EditorFeatures/Core/Implementation/IntelliSense/ISession.cs
+++ src/EditorFeatures/Core/Implementation/IntelliSense/ISession.cs
@@ -1,13 +1,15 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Immutable;
+
 namespace Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense
 {
     internal interface ISession<TModel>
     {
-        TModel InitialUnfilteredModel { get; }
+        ImmutableArray<TModel> InitialUnfilteredModels { get; }
 
         void Stop();
 
-        TModel WaitForController();
+        ImmutableArray<TModel> WaitForController();
     }
 }
