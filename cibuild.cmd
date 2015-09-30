e537b18e4f32364516291efc57e693ccaacb8432
Jared Parsons <jaredpar@microsoft.com>
2015-09-30 12:01:19 -0700

    Fix the check

--- cibuild.cmd
+++ cibuild.cmd
@@ -19,16 +19,6 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd
 REM Build the compiler so we can self host it for the full build
 nuget.exe restore -verbosity quiet %RoslynRoot%build/ToolsetPackages/project.json
 nuget.exe restore -verbosity quiet %RoslynRoot%build/Toolset.sln
-
-REM Verify that our project.lock.json files didn't change as a result of 
-REM restore.  If they do then the commit changed the dependencies without 
-REM updating the lock files.
-git diff --exit-code --quiet
-if ERRORLEVEL 1 (
-    echo Commit changed dependencies without updating project.lock.json
-    exit /b 1
-)
-
 msbuild /nologo /v:m /m %RoslynRoot%build/Toolset.sln /p:Configuration=%BuildConfiguration%
 
 mkdir %RoslynRoot%Binaries\Bootstrap
@@ -46,6 +36,15 @@ if ERRORLEVEL 1 (
 REM Kill any instances of VBCSCompiler.exe to release locked files;
 REM otherwise future CI runs may fail while trying to delete those files.
 taskkill /F /IM vbcscompiler.exe
+
+REM Verify that our project.lock.json files didn't change as a result of 
+REM restore.  If they do then the commit changed the dependencies without 
+REM updating the lock files.
+git diff --exit-code --quiet
+if ERRORLEVEL 1 (
+    echo Commit changed dependencies without updating project.lock.json
+    exit /b 1
+)
 
 REM It is okay and expected for taskkill to fail (it's a cleanup routine).  Ensure
 REM caller sees successful exit.
