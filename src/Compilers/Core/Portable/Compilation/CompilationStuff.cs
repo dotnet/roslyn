123ee3174948b8cbd1a0b70fe72662d2d3ec3337
Neal Gafter <neal@gafter.com>
2015-10-05 16:57:47 -0700

    I know it isn't the best name, but I'm putting it out there for feedback and suggestions.

--- src/Compilers/Core/Portable/Compilation/CompilationStuff.cs
+++ src/Compilers/Core/Portable/Compilation/CompilationStuff.cs
@@ -5,8 +5,9 @@ namespace Microsoft.CodeAnalysis.Collections
 {
     /// <summary>
     /// A mutable typesafe dictionary from a pair of compilation and key (of type <see cref="CompilationStuff.Key{T}"/>)
-    /// to a value (of type T). All keys and values associated with a compilation are reachable for GC purposes for
-    /// only as long as the compilation is reachable.
+    /// to a value (of type T). All keys and values associated with a compilation are reachable for GC purposes as a
+    /// consequence of this API for only as long as the compilation is reachable. These extension methods make a
+    /// compilation act as a property bag but without the <see cref="Compilation"/> itself being mutable.
     /// </summary>
     public static class CompilationStuff
     {
