061f21b9f02935ec1ec4e274d4cc5bab6de3be53
Neal Gafter <neal@gafter.com>
2015-10-05 16:50:49 -0700

    Add a general-purpose property bag to Compilation.

--- src/Compilers/Core/Portable/Compilation/Compilation.cs
+++ src/Compilers/Core/Portable/Compilation/Compilation.cs
@@ -33,6 +33,11 @@ namespace Microsoft.CodeAnalysis
     public abstract partial class Compilation
     {
         /// <summary>
+        /// For use only in <see cref="Microsoft.CodeAnalysis.Collections.CompilationStuff"/>.
+        /// </summary>
+        internal ConcurrentDictionary<object, object> _compilationStuff = new ConcurrentDictionary<object, object>();
+
+        /// <summary>
         /// Returns true if this is a case sensitive compilation, false otherwise.  Case sensitivity
         /// affects compilation features such as name lookup as well as choosing what names to emit
         /// when there are multiple different choices (for example between a virtual method and an
