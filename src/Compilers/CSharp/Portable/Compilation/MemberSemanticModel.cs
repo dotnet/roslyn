da756b6a0803890047d49e919d01140ac096bde2
Ty Overby <ty@pre-alpha.com>
2015-10-06 13:09:28 -0700

    Fixes binder-choosing around typeof expressions
    Previously, it was possible for the semantic model to think
    that you were *inside* of a typeof when you were merely near it.
    
    This commit adds an additional check that the cursor position is
    actually inside of the parenthesis that make up the typeof
    invocation.

--- src/Compilers/CSharp/Portable/Compilation/MemberSemanticModel.cs
+++ src/Compilers/CSharp/Portable/Compilation/MemberSemanticModel.cs
@@ -153,8 +153,8 @@ namespace Microsoft.CodeAnalysis.CSharp
             for (var current = node; binder == null; current = current.ParentOrStructuredTriviaParent)
             {
                 Debug.Assert(current != null); // Why were we asked for an enclosing binder for a node outside our root?
-
                 StatementSyntax stmt = current as StatementSyntax;
+                TypeOfExpressionSyntax typeOfExpression;
                 if (stmt != null)
                 {
                     if (LookupPosition.IsInStatementScope(position, stmt))
@@ -197,9 +197,14 @@ namespace Microsoft.CodeAnalysis.CSharp
                         }
                     }
                 }
-                else if (current.Kind() == SyntaxKind.TypeOfExpression && typeOfArgument == null)
+                else if (current.Kind() == SyntaxKind.TypeOfExpression &&
+                    typeOfArgument == null &&
+                    LookupPosition.IsBetweenTokens(
+                        position,
+                        (typeOfExpression = (TypeOfExpressionSyntax)current).OpenParenToken,
+                        typeOfExpression.CloseParenToken))
                 {
-                    typeOfArgument = ((TypeOfExpressionSyntax)current).Type;
+                    typeOfArgument = typeOfExpression.Type;
                     typeOfEncounteredBeforeUnexpectedAnonymousFunction = unexpectedAnonymousFunction == null;
                 }
                 else
