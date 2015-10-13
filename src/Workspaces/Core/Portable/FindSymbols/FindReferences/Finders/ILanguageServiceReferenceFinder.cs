cd5481da2dcb607902bc16027f43dc2b59181bd8
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-10-13 13:36:33 -0700

    When renaming a VB form, cascade to the synthesized compiler "My" property for the instance of the form.

---
+++ src/Workspaces/Core/Portable/FindSymbols/FindReferences/Finders/ILanguageServiceReferenceFinder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.CodeAnalysis.FindSymbols.Finders
+{
+    internal interface ILanguageServiceReferenceFinder : ILanguageService
+    {
+        Task<IEnumerable<ISymbol>> DetermineCascadedSymbolsAsync(INamedTypeSymbol symbol, Project project, CancellationToken cancellationToken);
+    }
+}
