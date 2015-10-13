406929c2c75902886eed66b3f6474a43b58946d5
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-10-13 13:56:30 -0700

    Change check to assert.

--- src/Workspaces/VisualBasic/Portable/FindSymbols/VisualBasicReferenceFinder.vb
+++ src/Workspaces/VisualBasic/Portable/FindSymbols/VisualBasicReferenceFinder.vb
@@ -10,31 +10,22 @@ Namespace Microsoft.CodeAnalysis.FindSymbols
         Implements ILanguageServiceReferenceFinder
 
         Public Async Function DetermineCascadedSymbolsAsync(namedType As INamedTypeSymbol, project As Project, cancellationToken As CancellationToken) As Task(Of IEnumerable(Of ISymbol)) Implements ILanguageServiceReferenceFinder.DetermineCascadedSymbolsAsync
-            If namedType.Language = LanguageNames.VisualBasic AndAlso project.Language = LanguageNames.VisualBasic Then
-                Dim compilation = Await project.GetCompilationAsync(cancellationToken).ConfigureAwait(False)
+            Debug.Assert(project.Language = LanguageNames.VisualBasic)
+            Dim compilation = Await project.GetCompilationAsync(cancellationToken).ConfigureAwait(False)
 
-                ' If this is a WinForms project, then the VB 'my' feature may have synthesized 
-                ' a property that would return an instance of the main Form type for the project.
-                ' Search for such properties and cascade to them as well.
-                Dim myPropertySymbols = GetMatchingMyPropertySymbols(namedType, DirectCast(compilation, VisualBasicCompilation), cancellationToken).Distinct().ToList()
-                If myPropertySymbols.Count > 0 Then
-                    Return myPropertySymbols
-                End If
+            ' If this is a WinForms project, then the VB 'my' feature may have synthesized 
+            ' a property that would return an instance of the main Form type for the project.
+            ' Search for such properties and cascade to them as well.
+            Dim myPropertySymbols = GetMatchingMyPropertySymbols(namedType, DirectCast(compilation, VisualBasicCompilation), cancellationToken).Distinct().ToList()
+            If myPropertySymbols.Count > 0 Then
+                Return myPropertySymbols
             End If
 
             Return Nothing
         End Function
 
-        Private Function GetMatchingMyPropertySymbols(symbol As INamedTypeSymbol, compilation As VisualBasicCompilation, cancellationToken As CancellationToken) As IEnumerable(Of IPropertySymbol)
-            Return GetMatchingMyPropertySymbols(symbol, compilation, compilation.GlobalNamespace, cancellationToken).Concat(
-                   GetMatchingMyPropertySymbols(symbol, compilation, compilation.RootNamespace, cancellationToken))
-        End Function
-
-        Private Function GetMatchingMyPropertySymbols(namedType As INamedTypeSymbol,
-                                                      compilation As VisualBasicCompilation,
-                                                      [namespace] As INamespaceSymbol,
-                                                      cancellationToken As CancellationToken) As IEnumerable(Of IPropertySymbol)
-            Return From childNamespace In [namespace].GetNamespaceMembers()
+        Private Function GetMatchingMyPropertySymbols(namedType As INamedTypeSymbol, compilation As VisualBasicCompilation, cancellationToken As CancellationToken) As IEnumerable(Of IPropertySymbol)
+            Return From childNamespace In compilation.RootNamespace.GetNamespaceMembers()
                    Where childNamespace.IsMyNamespace(compilation)
                    From type In childNamespace.GetAllTypes(cancellationToken)
                    Where type.Name = "MyForms"
