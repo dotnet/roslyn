0f029d06064cfc2ffa333a8408bf9570ff933b1b
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-10-13 18:47:10 -0700

    Add the abstract corrector stub.

---
+++ src/EditorFeatures/VisualBasic/AutomaticEndConstructCorrection/AbstractCorrector.vb
@@ -0,0 +1,14 @@
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports System.Threading
+Imports Microsoft.CodeAnalysis.Editor.Host
+Imports Microsoft.CodeAnalysis.Text
+Imports Microsoft.CodeAnalysis.Text.Shared.Extensions
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+Imports Microsoft.VisualStudio.Text
+
+Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.AutomaticEndConstructCorrection
+    Friend Class AbstractCorrector
+
+    End Class
+End Namespace
