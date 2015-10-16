8255ae76d884d4334b27a7c6ccd8a7fbb165a1fa
Kevin Halverson <kevinh@microsoft.com>
2015-10-15 17:07:29 -0700

    Change parsing of #! from Comment -> Directive...
    This change adds a ShebangDirectiveTriviaSyntax type to the public API.

--- src/Compilers/CSharp/Portable/Syntax/SyntaxNormalizer.cs
+++ src/Compilers/CSharp/Portable/Syntax/SyntaxNormalizer.cs
@@ -666,7 +666,6 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax
             switch (kind)
             {
                 case SyntaxKind.SingleLineCommentTrivia:
-                case SyntaxKind.ShebangDirectiveTrivia:
                     return true;
                 case SyntaxKind.MultiLineCommentTrivia:
                     return !isTrailingTrivia;
