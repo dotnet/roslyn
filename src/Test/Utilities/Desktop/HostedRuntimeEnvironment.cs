d34becfa2446ebc5fb5049e2d792ebf4702a3d65
Jared Parsons <jaredpar@microsoft.com>
2015-10-19 10:08:59 -0700

    Limit dump count

--- src/Test/Utilities/Desktop/HostedRuntimeEnvironment.cs
+++ src/Test/Utilities/Desktop/HostedRuntimeEnvironment.cs
@@ -450,6 +450,7 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
         // The key is the manifest module MVID, which is unique for each distinct assembly. 
         private static readonly ConcurrentDictionary<Guid, Assembly> s_domainAssemblyCache;
         private static readonly ConcurrentDictionary<Guid, Assembly> s_domainReflectionOnlyAssemblyCache;
+        private static int s_dumpCount;
 
         // Modules managed by this manager. All such modules must have unique simple name.
         private readonly Dictionary<string, ModuleData> _modules;
@@ -723,8 +724,17 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
             StringBuilder sb = new StringBuilder();
             foreach (var module in modules)
             {
+                // Limit the number of dumps to 10.  After 10 we're likely in a bad state and are 
+                // dumping lots of unnecessary data.
+                if (s_dumpCount > 10)
+                {
+                    break; 
+                }
+
                 if (module.InMemoryModule)
                 {
+                    Interlocked.Increment(ref s_dumpCount);
+
                     if (dumpDirectory == null)
                     {
                         var assemblyLocation = typeof(HostedRuntimeEnvironment).Assembly.Location;
