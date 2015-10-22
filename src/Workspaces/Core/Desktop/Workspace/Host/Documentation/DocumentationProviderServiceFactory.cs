c6e0c063bbf10e7b3a3f76d80c274e6b282e13cc
Balaji Krishnan <balaji.krishnan@microsoft.com>
2015-10-22 16:12:35 -0700

    Look for documentation files based on culture info
    Add logic to resolve documentation file path, given an assembly path,
    based on current culture in Workspace's DocumentationProviderService.

--- src/Workspaces/Core/Desktop/Workspace/Host/Documentation/DocumentationProviderServiceFactory.cs
+++ src/Workspaces/Core/Desktop/Workspace/Host/Documentation/DocumentationProviderServiceFactory.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Composition;
+using System.Globalization;
 using System.IO;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Host.Mef;
@@ -29,15 +30,46 @@ namespace Microsoft.CodeAnalysis.Host
                     throw new ArgumentNullException(nameof(assemblyPath));
                 }
 
-                assemblyPath = Path.ChangeExtension(assemblyPath, "xml");
-
-                DocumentationProvider provider;
-                if (!_assemblyPathToDocumentationProviderMap.TryGetValue(assemblyPath, out provider))
+                string xmlDocumentationFilePath;
+                if (!TryFindDocumentationFile(assemblyPath, out xmlDocumentationFilePath))
                 {
-                    provider = _assemblyPathToDocumentationProviderMap.GetOrAdd(assemblyPath, _path => new FileBasedXmlDocumentationProvider(_path));
+                    return _assemblyPathToDocumentationProviderMap.GetOrAdd(nameof(DocumentationProvider.Default), DocumentationProvider.Default);
                 }
 
-                return provider;
+                return _assemblyPathToDocumentationProviderMap.GetOrAdd(xmlDocumentationFilePath, _path => new FileBasedXmlDocumentationProvider(_path));
+            }
+
+            private bool TryFindDocumentationFile(string assemblyPath, out string xmlDocumentationFilePath)
+            {
+                var xmlFileName = Path.ChangeExtension(Path.GetFileName(assemblyPath), "xml");
+                var originalDirectory = Path.GetDirectoryName(assemblyPath);
+
+                // 1. Look in subdirectories based on current culture
+                var culture = CultureInfo.CurrentUICulture;
+                var xmlFilePath = string.Empty;
+
+                while (culture != CultureInfo.InvariantCulture)
+                {
+                    xmlFilePath = Path.Combine(originalDirectory, culture.Name, xmlFileName);
+                    if (File.Exists(xmlFilePath))
+                    {
+                        xmlDocumentationFilePath = xmlFilePath;
+                        return true;
+                    }
+
+                    culture = culture.Parent;
+                }
+
+                // 2. Look in the same directory as the assembly itself
+                xmlFilePath = Path.Combine(originalDirectory, xmlFileName);
+                if (File.Exists(xmlFilePath))
+                {
+                    xmlDocumentationFilePath = xmlFilePath;
+                    return true;
+                }
+
+                xmlDocumentationFilePath = null;
+                return false;
             }
         }
     }
