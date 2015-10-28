4c3fcb7c72ce66e0fc82dfef90a74ab8e3b4dfa1
Andy Gocke <angocke@microsoft.com>
2015-10-28 13:36:00 -0700

    Update the nuget zip drop with new packages

--- cibuild.sh
+++ cibuild.sh
@@ -58,7 +58,7 @@ done
 
 restore_nuget()
 {
-    local package_name="nuget.26.zip"
+    local package_name="nuget.27.zip"
     local target="/tmp/$package_name"
     echo "Installing NuGet Packages $target"
     if [ -f $target ]; then
