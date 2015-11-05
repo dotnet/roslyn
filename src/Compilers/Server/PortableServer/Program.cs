1b8edeb676ac54f11f87a840fce2226c40214d26
Jared Parsons <jaredpar@microsoft.com>
2015-11-05 13:47:22 -0800

    Got the project setup aign
    Turns out we do need a shared DLL project.  A lot of types that need to
    be shared between the servers.

--- src/Compilers/Server/PortableServer/Program.cs
+++ src/Compilers/Server/PortableServer/Program.cs
@@ -2,17 +2,41 @@
 
 using System;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.BuildTasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Microsoft.CodeAnalysis.CompilerServer
 {
     public class Program
     {
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            return 0;
+            Go().GetAwaiter().GetResult();
+        }
+
+        private static async Task Go()
+        {
+            var ipAddress = (await Dns.GetHostAddressesAsync("localhost").ConfigureAwait(true))[0];
+            var listener = new TcpListener(ipAddress, port: 12000);
+            while (true)
+            {
+                var client = await listener.AcceptTcpClientAsync().ConfigureAwait(true);
+                await RunCompilation(client, CancellationToken.None).ConfigureAwait(true);
+            }
+        }
+
+        private static Task RunCompilation(TcpClient client, CancellationToken cancellationToken)
+        {
+            throw new Exception();
+            /*
+            var stream = client.GetStream();
+            var buildRequest = await BuildRequest.ReadAsync(stream, cancellationToken).ConfigureAwait(true);
+            */
         }
     }
 }
