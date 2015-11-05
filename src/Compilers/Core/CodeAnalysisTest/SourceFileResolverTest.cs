b4de55045b9af2c6f1071756d8307c2a851f8722
Ty Overby <ty@pre-alpha.com>
2015-11-05 13:53:48 -0800

    add tests for SourceFileResolver

---
+++ src/Compilers/Core/CodeAnalysisTest/SourceFileResolverTest.cs
@@ -0,0 +1,39 @@
+﻿using Roslyn.Utilities;
+using System;
+using System.Collections.Immutable;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.UnitTests
+{
+    public class SourceFileResolverTest
+    {
+        [Fact]
+        public void IncorrectPathmaps()
+        {
+            string isABaseDirectory = "";
+            if (PortableShim.Path.DirectorySeparatorChar == '/') {
+                isABaseDirectory = "/";
+            }
+            else
+            {
+                isABaseDirectory = "C://";
+            }
+
+            Assert.Throws<ArgumentNullException>(() =>
+                new SourceFileResolver(
+                    ImmutableArray.Create(""), 
+                    isABaseDirectory, 
+                    ImmutableArray.Create(KeyValuePair.Create<string, string>("key", null))));
+        }
+
+        [Fact]
+        public void badBaseDirectory()
+        {
+            Assert.Throws<ArgumentException>(() => 
+                new SourceFileResolver(
+                    ImmutableArray.Create(""), 
+                    "not_a_root directory", 
+                    ImmutableArray.Create(KeyValuePair.Create<string, string>("key", "value"))));
+        }
+    }
+}
--- src/Compilers/Core/Portable/SourceFileResolver.cs
+++ src/Compilers/Core/Portable/SourceFileResolver.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Roslyn.Utilities;
