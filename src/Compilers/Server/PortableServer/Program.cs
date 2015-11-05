95f69364a5bb089b1a46e482689ef8a2ca7c9fd0
Jared Parsons <jaredpar@microsoft.com>
2015-11-05 11:52:02 -0800

    Create the portable server project

---
+++ src/Compilers/Server/PortableServer/Program.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.BuildTasks;
+
+namespace Microsoft.CodeAnalysis.CompilerServer
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
