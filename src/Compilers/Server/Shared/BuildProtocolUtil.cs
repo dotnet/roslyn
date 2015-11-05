e6427fa4216ec145733cf38f84adc8610b3d54f9
Jared Parsons <jaredpar@microsoft.com>
2015-11-05 15:35:22 -0800

    Moving to a shared source project

---
+++ src/Compilers/Server/Shared/BuildProtocolUtil.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Microsoft.CodeAnalysis.CompilerServer
+{
+    internal static class BuildProtocolUtil
+    {
+        internal static RunRequest GetRunRequest(BuildRequest req)
+        {
+            string currentDirectory;
+            string libDirectory;
+            string[] arguments = GetCommandLineArguments(req, out currentDirectory, out libDirectory);
+            string language = "";
+            switch (req.Language)
+            {
+                case BuildProtocolConstants.RequestLanguage.CSharpCompile:
+                    language = LanguageNames.CSharp;
+                    break;
+                case BuildProtocolConstants.RequestLanguage.VisualBasicCompile:
+                    language = LanguageNames.VisualBasic;
+                    break;
+            }
+
+            return new RunRequest(language, currentDirectory, libDirectory, arguments);
+        }
+
+        internal static string[] GetCommandLineArguments(BuildRequest req, out string currentDirectory, out string libDirectory)
+        {
+            currentDirectory = null;
+            libDirectory = null;
+            List<string> commandLineArguments = new List<string>();
+
+            foreach (BuildRequest.Argument arg in req.Arguments)
+            {
+                if (arg.ArgumentId == BuildProtocolConstants.ArgumentId.CurrentDirectory)
+                {
+                    currentDirectory = arg.Value;
+                }
+                else if (arg.ArgumentId == BuildProtocolConstants.ArgumentId.LibEnvVariable)
+                {
+                    libDirectory = arg.Value;
+                }
+                else if (arg.ArgumentId == BuildProtocolConstants.ArgumentId.CommandLineArgument)
+                {
+                    int argIndex = arg.ArgumentIndex;
+                    while (argIndex >= commandLineArguments.Count)
+                        commandLineArguments.Add("");
+                    commandLineArguments[argIndex] = arg.Value;
+                }
+            }
+
+            return commandLineArguments.ToArray();
+        }
+
+        internal static BuildResponse GetBuildResponse(RunResult result)
+        {
+            switch (result.Kind)
+            {
+                case RunResultKind.BadAnalyzer:
+                    return new AnalyzerInconsistencyBuildResponse();
+                case RunResultKind.BadLanguage:
+                    return new CompletedBuildResponse(-1, utf8output: false, output: "", errorOutput: "");
+                case RunResultKind.Run:
+                    return new CompletedBuildResponse(result.ReturnCode, result.Utf8Output, result.Output, errorOutput: "");
+                default:
+                    Debug.Assert(false);
+                    throw new Exception($"Bad enum value {result.Kind}");
+            }
+        }
+    }
+}
--- src/Compilers/Server/VBCSCompiler/CompilerRequestHandler.cs
+++ src/Compilers/Server/VBCSCompiler/CompilerRequestHandler.cs
@@ -31,67 +31,9 @@ namespace Microsoft.CodeAnalysis.CompilerServer
         /// </summary>
         public BuildResponse HandleRequest(BuildRequest req, CancellationToken cancellationToken)
         {
-            var request = GetRunRequest(req);
+            var request = BuildProtocolUtil.GetRunRequest(req);
             var result = _compilerRunHandler.HandleRequest(request, cancellationToken);
-            switch (result.Kind)
-            {
-                case RunResultKind.BadAnalyzer:
-                    return new AnalyzerInconsistencyBuildResponse();
-                case RunResultKind.BadLanguage:
-                    return new CompletedBuildResponse(-1, utf8output: false, output: "", errorOutput: "");
-                case RunResultKind.Run:
-                    return new CompletedBuildResponse(result.ReturnCode, result.Utf8Output, result.Output, errorOutput: "");
-                default:
-                    Debug.Assert(false);
-                    throw new Exception($"Bad enum value {result.Kind}");
-            }
-        }
-
-        private static RunRequest GetRunRequest(BuildRequest req)
-        {
-            string currentDirectory;
-            string libDirectory;
-            string[] arguments = GetCommandLineArguments(req, out currentDirectory, out libDirectory);
-            string language = "";
-            switch (req.Language)
-            {
-                case BuildProtocolConstants.RequestLanguage.CSharpCompile:
-                    language = LanguageNames.CSharp;
-                    break;
-                case BuildProtocolConstants.RequestLanguage.VisualBasicCompile:
-                    language = LanguageNames.VisualBasic;
-                    break;
-            }
-
-            return new RunRequest(language, currentDirectory, libDirectory, arguments);
-        }
-
-        private static string[] GetCommandLineArguments(BuildRequest req, out string currentDirectory, out string libDirectory)
-        {
-            currentDirectory = null;
-            libDirectory = null;
-            List<string> commandLineArguments = new List<string>();
-
-            foreach (BuildRequest.Argument arg in req.Arguments)
-            {
-                if (arg.ArgumentId == BuildProtocolConstants.ArgumentId.CurrentDirectory)
-                {
-                    currentDirectory = arg.Value;
-                }
-                else if (arg.ArgumentId == BuildProtocolConstants.ArgumentId.LibEnvVariable)
-                {
-                    libDirectory = arg.Value;
-                }
-                else if (arg.ArgumentId == BuildProtocolConstants.ArgumentId.CommandLineArgument)
-                {
-                    int argIndex = arg.ArgumentIndex;
-                    while (argIndex >= commandLineArguments.Count)
-                        commandLineArguments.Add("");
-                    commandLineArguments[argIndex] = arg.Value;
-                }
-            }
-
-            return commandLineArguments.ToArray();
+            return BuildProtocolUtil.GetBuildResponse(result);
         }
     }
 }
