2762d3c6eac728bca33a974e9e278e18b255e772
Jason Malinowski <jason.malinowski@microsoft.com>
2015-11-11 14:46:56 -0800

    Update the MSBuild files written by the extension to match NuGet
    This more or less matches what our NuGet files write out, although
    we split it into two separate files to ensure we override what we want
    right away, but allow the project file to control whether we use the
    compiler server.

--- src/Compilers/Extension/CompilerPackage.cs
+++ src/Compilers/Extension/CompilerPackage.cs
@@ -11,12 +11,8 @@ namespace Roslyn.Compilers.Extension
     [ProvideAutoLoad(UIContextGuids.SolutionExists)]
     public sealed class CompilerPackage : Package
     {
-        private const string MSBuildDirectory = @"Microsoft\MSBuild\14.0";
-
         private const string WriteFileExceptionMessage =
-@"Unable to write {0}
-
-{1}
+@"{1}
 
 To reload the Roslyn compiler package, close Visual Studio and any MSBuild processes, then restart Visual Studio.";
 
@@ -39,7 +35,7 @@ To reload the Roslyn compiler package, close Visual Studio and any MSBuild proce
                 var hiveName = registryParts[3];
                 var roslynHive = string.Format(@"{0}.{1}", registryParts[2], registryParts[3]);
 
-                WriteTargetsFile(packagePath, roslynHive);
+                WriteMSBuildFiles(packagePath, roslynHive);
 
                 try
                 {
@@ -53,61 +49,79 @@ To reload the Roslyn compiler package, close Visual Studio and any MSBuild proce
             }
         }
 
-        private void WriteTargetsFile(string packagePath, string hiveName)
+        private void WriteMSBuildFiles(string packagePath, string hiveName)
         {
-            var targetsFileContent =
-                $@"<?xml version=""1.0"" encoding=""utf-8""?>
+            // First we want to ensure any existing Roslyn files are deleted so we don't have old stuff floating
+            // aroud and causing troubles
+            foreach (var file in new DirectoryInfo(GetMSBuildPath()).EnumerateFiles($"*Roslyn*{hiveName}*", SearchOption.AllDirectories))
+            {
+                file.Delete();
+            }
+
+            try
+            {
+                // The props we want to be included as early as possible since we want our tasks to be used and
+                // to ensure our setting of targets path happens early enough
+                var propsContent =
+                    $@"<?xml version=""1.0"" encoding=""utf-8""?>
 <Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
-  <PropertyGroup Condition = ""'$(RoslynHive)' == '{hiveName}'"">
+  <PropertyGroup Condition=""'$(RoslynHive)' == '{hiveName}'"">
+    <RoslynCompilerExtensionActive>true</RoslynCompilerExtensionActive>
+    <CSharpCoreTargetsPath>{packagePath}\Microsoft.CSharp.Core.targets</CSharpCoreTargetsPath>
+    <VisualBasicCoreTargetsPath>{packagePath}\Microsoft.VisualBasic.Core.targets</VisualBasicCoreTargetsPath>
+  </PropertyGroup> 
+
+  <UsingTask TaskName=""Microsoft.CodeAnalysis.BuildTasks.Csc"" AssemblyFile=""{packagePath}\Microsoft.Build.Tasks.CodeAnalysis.dll"" Condition=""'$(RoslynCompilerExtensionActive)' == 'true'"" />
+  <UsingTask TaskName=""Microsoft.CodeAnalysis.BuildTasks.Vbc"" AssemblyFile=""{packagePath}\Microsoft.Build.Tasks.CodeAnalysis.dll"" Condition=""'$(RoslynCompilerExtensionActive)' == 'true'"" />
+</Project>";
+
+                WriteMSBuildFile(propsContent, $@"Imports\Microsoft.Common.props\ImportBefore\Roslyn.Compilers.Extension.{hiveName}.props");
+
+                // This targets content we want to be included later since the project flie might touch UseSharedCompilation
+                var targetsContent =
+                    $@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <!-- If we're not using the compiler server, set ToolPath/Exe to direct to the exes in this package -->
+  <PropertyGroup Condition=""'$(RoslynCompilerExtensionActive)' == 'true' and '$(UseSharedCompilation)' != 'true'"">
     <CscToolPath>{packagePath}</CscToolPath>
     <CscToolExe>csc.exe</CscToolExe>
     <VbcToolPath>{packagePath}</VbcToolPath>
     <VbcToolExe>vbc.exe</VbcToolExe>
+    <UseSharedCompilation>false</UseSharedCompilation>
   </PropertyGroup>
 </Project>";
 
-            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var msbuildExtensionsPath = Path.Combine(localAppData, MSBuildDirectory);
-
-            foreach (var msbuildLanguage in new[] { "CSharp", "VisualBasic" })
+                WriteMSBuildFile(targetsContent, $@"Microsoft.CSharp.targets\ImportBefore\Roslyn.Compilers.Extension.{hiveName}.targets");
+                WriteMSBuildFile(targetsContent, $@"Microsoft.VisualBasic.targets\ImportBefore\Roslyn.Compilers.Extension.{hiveName}.targets");
+            }
+            catch (Exception e)
             {
-                var targetsFile = Path.Combine(msbuildExtensionsPath, $@"Microsoft.{msbuildLanguage}.targets\ImportAfter\Roslyn.Compilers.Extension.{msbuildLanguage}.{hiveName}.targets");
-
-                try
-                {
-                    var directory = new DirectoryInfo(Path.GetDirectoryName(targetsFile));
-                    if (!directory.Exists)
-                    {
-                        directory.Create();
-                    }
-
-                    // Delete any other targets we might have left around
-                    foreach (var file in directory.GetFileSystemInfos("*.targets"))
-                    {
-                        if (file.Name.Contains("Roslyn"))
-                        {
-                            file.Delete();
-                        }
-                    }
-
-                    File.WriteAllText(targetsFile, targetsFileContent);
-                }
-                catch (Exception e)
-                {
-                    ReportWriteFileException(targetsFile, e);
-                }
+                VsShellUtilities.ShowMessageBox(
+                    this,
+                    string.Format(WriteFileExceptionMessage, e.Message),
+                    null,
+                    OLEMSGICON.OLEMSGICON_WARNING,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
             }
         }
 
-        private void ReportWriteFileException(string path, Exception e)
+        private static void WriteMSBuildFile(string content, string relativeFilePath)
         {
-            VsShellUtilities.ShowMessageBox(
-                this,
-                string.Format(WriteFileExceptionMessage, path, e.Message),
-                null,
-                OLEMSGICON.OLEMSGICON_WARNING,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            var fileFullPath = Path.Combine(GetMSBuildPath(), relativeFilePath);
+            var directory = new DirectoryInfo(Path.GetDirectoryName(fileFullPath));
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+
+            File.WriteAllText(fileFullPath, content);
+        }
+
+        private static string GetMSBuildPath()
+        {
+            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return Path.Combine(localAppData, @"Microsoft\MSBuild\14.0");
         }
     }
 }
