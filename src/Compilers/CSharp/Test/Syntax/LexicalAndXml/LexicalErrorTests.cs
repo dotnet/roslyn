078df7bc230e23c2369c1eb6f8e125379bbabf3d
leppie <xacc.ide@gmail.com>
2015-11-18 08:31:23 +0200

    Add and fix tests

--- src/Compilers/CSharp/Test/Syntax/LexicalAndXml/LexicalErrorTests.cs
+++ src/Compilers/CSharp/Test/Syntax/LexicalAndXml/LexicalErrorTests.cs
@@ -44,6 +44,38 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
                 Diagnostic(ErrorCode.ERR_FloatOverflow, "3e100m").WithArguments("decimal"));
         }
 
+        [Fact]
+        public void CS0595ERR_InvalidReal()
+        {
+            var test =
+@"public class C
+{
+    double d1 = 0e;
+    double d2 = .0e;
+    double d3 = 0.0e;
+    double d4 = 0e+;
+    double d5 = 0e-;
+}";
+
+            ParserErrorMessageTests.ParseAndValidate(test,
+                  // (3,17): error CS0595: Invalid real literal
+                  //     double d1 = 0e;
+                  Diagnostic(ErrorCode.ERR_InvalidReal, "").WithLocation(3, 17),
+                  // (4,17): error CS0595: Invalid real literal
+                  //     double d2 = .0e;
+                  Diagnostic(ErrorCode.ERR_InvalidReal, "").WithLocation(4, 17),
+                  // (5,17): error CS0595: Invalid real literal
+                  //     double d3 = 0.0e;
+                  Diagnostic(ErrorCode.ERR_InvalidReal, "").WithLocation(5, 17),
+                  // (6,17): error CS0595: Invalid real literal
+                  //     double d4 = 0e+;
+                  Diagnostic(ErrorCode.ERR_InvalidReal, "").WithLocation(6, 17),
+                  // (7,17): error CS0595: Invalid real literal
+                  //     double d5 = 0e-;
+                  Diagnostic(ErrorCode.ERR_InvalidReal, "").WithLocation(7, 17)
+                );
+        }
+
         [WorkItem(6079, "https://github.com/dotnet/roslyn/issues/6079")]
         [Fact]
         public void FloatLexicalError()
@@ -55,18 +87,18 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
 }";
             // The precise errors don't matter so much as the fact that the compiler should not crash.
             ParserErrorMessageTests.ParseAndValidate(test,
-                // (3,23): error CS0594: Floating-point constant is outside the range of type 'double'
-                //     const double d1 = 0endOfDirective.Span;
-                Diagnostic(ErrorCode.ERR_FloatOverflow, "").WithArguments("double").WithLocation(3, 23),
-                // (3,25): error CS1002: ; expected
-                //     const double d1 = 0endOfDirective.Span;
-                Diagnostic(ErrorCode.ERR_SemicolonExpected, "ndOfDirective").WithLocation(3, 25),
-                // (3,43): error CS1519: Invalid token ';' in class, struct, or interface member declaration
-                //     const double d1 = 0endOfDirective.Span;
-                Diagnostic(ErrorCode.ERR_InvalidMemberDecl, ";").WithArguments(";").WithLocation(3, 43),
-                // (3,43): error CS1519: Invalid token ';' in class, struct, or interface member declaration
-                //     const double d1 = 0endOfDirective.Span;
-                Diagnostic(ErrorCode.ERR_InvalidMemberDecl, ";").WithArguments(";").WithLocation(3, 43)
+                  // (3,23): error CS0595: Invalid real literal
+                  //     const double d1 = 0endOfDirective.Span;
+                  Diagnostic(ErrorCode.ERR_InvalidReal, "").WithLocation(3, 23),
+                  // (3,25): error CS1002: ; expected
+                  //     const double d1 = 0endOfDirective.Span;
+                  Diagnostic(ErrorCode.ERR_SemicolonExpected, "ndOfDirective").WithLocation(3, 25),
+                  // (3,43): error CS1519: Invalid token ';' in class, struct, or interface member declaration
+                  //     const double d1 = 0endOfDirective.Span;
+                  Diagnostic(ErrorCode.ERR_InvalidMemberDecl, ";").WithArguments(";").WithLocation(3, 43),
+                  // (3,43): error CS1519: Invalid token ';' in class, struct, or interface member declaration
+                  //     const double d1 = 0endOfDirective.Span;
+                  Diagnostic(ErrorCode.ERR_InvalidMemberDecl, ";").WithArguments(";").WithLocation(3, 43)
                 );
         }
 
