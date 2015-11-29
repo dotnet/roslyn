e83f21a3738d7408fdc9b38ff0438694e6cb5291
Jared Parsons <jaredpar@microsoft.com>
2015-11-28 16:39:15 -0800

    Basic local caching

--- src/Tools/Source/RunTests/LocalDataStorage.cs
+++ src/Tools/Source/RunTests/LocalDataStorage.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,62 @@ namespace RunTests
     /// </summary>
     internal sealed class LocalDataStorage : IDataStorage
     {
+        internal const string DirectoryName = "RunTestsStorage";
+
+        private readonly string _storagePath;
+
+        internal LocalDataStorage(string storagePath = null)
+        {
+            _storagePath = storagePath ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DirectoryName);
+        }
+
         public bool TryGetTestResult(string cacheKey, out TestResult testResult)
         {
             testResult = default(TestResult);
+
+            var filePath = Path.Combine(_storagePath, cacheKey);
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    return false;
+                }
+
+                var text = File.ReadAllText(filePath);
+                if (text.Length > 0)
+                {
+                    testResult = new TestResult(succeeded: true, assemblyName: text, elapsed: TimeSpan.FromSeconds(0), errorOutput: string.Empty);
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                // Okay for exception to occur here on I/O
+            }
+
             return false;
         }
 
         public void AddTestResult(string cacheKey, TestResult testResult)
         {
-            
+            // TODO: Cache more than just success 
+            if (testResult.Succeeded)
+            {
+                try
+                {
+                    if (!Directory.Exists(_storagePath))
+                    {
+                        Directory.CreateDirectory(_storagePath);
+                    }
+
+                    var filePath = Path.Combine(_storagePath, cacheKey);
+                    File.WriteAllText(filePath, testResult.AssemblyName);
+                }
+                catch (Exception)
+                {
+                    // I/O errors are expected and okay here.
+                }
+            }
         }
     }
 }
