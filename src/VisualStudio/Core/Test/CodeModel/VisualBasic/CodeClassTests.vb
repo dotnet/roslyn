1bf1bb702429d4d0b3fe07e868490fc0dff7fa97
Dustin Campbell <dustin@teamcampbell.org>
2015-12-03 14:47:37 -0800

    Explicitly remove code model elements from the CleanableWeakComHandleTable when they are deleted
    There are WinForms scenarios where code model elements can be deleted and re-added, for example,
    when the modifiers of a control on the designer surface are changed the control will be deleted
    and immediately re-added. We must be sure to delete the element's node key from the
    CleanableWeakComHandleTable or else an exception will be thrown when the element is re-added with
    the same node key.

--- src/VisualStudio/Core/Test/CodeModel/VisualBasic/CodeClassTests.vb
+++ src/VisualStudio/Core/Test/CodeModel/VisualBasic/CodeClassTests.vb
@@ -3123,6 +3123,24 @@ End Class
             TestGetBaseName(code, "N.M.Generic(Of String)")
         End Sub
 
+        ' Note: This unit test has diverged and is not asynchronous in stabilization. If merged into master,
+        ' take the master version and remove this comment.
+        Public Sub TestAddDeleteManyTimes()
+            Dim code =
+<Code>
+Class C$$
+End Class
+</Code>
+
+            TestElement(code,
+                Sub(codeClass)
+                    For i = 1 To 100
+                        Dim variable = codeClass.AddVariable("x", "System.Int32")
+                        codeClass.RemoveMember(variable)
+                    Next
+                End Sub)
+        End Sub
+
         <ConditionalWpfFact(GetType(x86)), Trait(Traits.Feature, Traits.Features.CodeModel)>
         Public Sub ExternalClass_ImplementedInterfaces()
             Dim code =
