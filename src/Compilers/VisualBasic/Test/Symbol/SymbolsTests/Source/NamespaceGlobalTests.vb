6e56728a2c5496b063844015d7822ae7a04b0efe
Neal Gafter <neal@gafter.com>
2015-12-03 12:40:43 -0800

    Revert a test due to deterministic decl table.

--- src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/Source/NamespaceGlobalTests.vb
+++ src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/Source/NamespaceGlobalTests.vb
@@ -73,8 +73,8 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.UnitTests
             Dim symbolA = CompilationUtils.VerifyGlobalNamespace(compilation1, "a.vb", "A", {"A", "A"}, False)
             CompilationUtils.AssertTheseDeclarationDiagnostics(compilation1, <errors>
 BC30179: class 'A' and class 'A' conflict in namespace '&lt;Default&gt;'.
-                            Class A 'invalid
-                                  ~
+Class A
+      ~
      </errors>)
         End Sub
 
