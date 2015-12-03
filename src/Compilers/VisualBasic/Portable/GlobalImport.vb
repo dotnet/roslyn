05b52c97c5c6890032e9c6366a5c741893521139
Artur Spychaj <drognanar@gmail.com>
2015-12-03 10:04:47 -0800

    Use IsKind instead of TypeOf to check syntax type

--- src/Compilers/VisualBasic/Portable/GlobalImport.vb
+++ src/Compilers/VisualBasic/Portable/GlobalImport.vb
@@ -8,6 +8,7 @@ Imports System.Threading
 Imports Microsoft.CodeAnalysis
 Imports Microsoft.CodeAnalysis.Text
 Imports Microsoft.CodeAnalysis.VisualBasic.Symbols
+Imports Microsoft.CodeAnalysis.VisualBasic
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 Imports TypeKind = Microsoft.CodeAnalysis.TypeKind
 
@@ -40,7 +41,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
 
         Friend ReadOnly Property IsXmlClause As Boolean
             Get
-                Return TypeOf Clause Is XmlNamespaceImportsClauseSyntax
+                Return Clause.IsKind(SyntaxKind.XmlNamespaceImportsClause)
             End Get
         End Property
 
