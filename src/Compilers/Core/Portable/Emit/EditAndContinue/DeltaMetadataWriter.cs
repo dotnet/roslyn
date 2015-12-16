b39a99020a521f76e30e54b6bb69dac1e6d925e3
Tomas Matousek <tomas.matousek@microsoft.com>
2015-12-16 08:24:34 -0800

    Eliminate EncFuncCode in favor of EditAndContinueOperation

--- src/Compilers/Core/Portable/Emit/EditAndContinue/DeltaMetadataWriter.cs
+++ src/Compilers/Core/Portable/Emit/EditAndContinue/DeltaMetadataWriter.cs
@@ -696,10 +696,10 @@ namespace Microsoft.CodeAnalysis.Emit
             PopulateEncLogTableRows(TableIndex.EventMap, previousSizes, deltaSizes);
             PopulateEncLogTableRows(TableIndex.PropertyMap, previousSizes, deltaSizes);
 
-            PopulateEncLogTableEventsOrProperties(_eventDefs, TokenTypeIds.Event, EncFuncCode.AddEvent, _eventMap, TokenTypeIds.EventMap);
-            PopulateEncLogTableFieldsOrMethods(_fieldDefs, TokenTypeIds.FieldDef, EncFuncCode.AddField);
-            PopulateEncLogTableFieldsOrMethods(_methodDefs, TokenTypeIds.MethodDef, EncFuncCode.AddMethod);
-            PopulateEncLogTableEventsOrProperties(_propertyDefs, TokenTypeIds.Property, EncFuncCode.AddProperty, _propertyMap, TokenTypeIds.PropertyMap);
+            PopulateEncLogTableEventsOrProperties(_eventDefs, TokenTypeIds.Event, EditAndContinueOperation.AddEvent, _eventMap, TokenTypeIds.EventMap);
+            PopulateEncLogTableFieldsOrMethods(_fieldDefs, TokenTypeIds.FieldDef, EditAndContinueOperation.AddField);
+            PopulateEncLogTableFieldsOrMethods(_methodDefs, TokenTypeIds.MethodDef, EditAndContinueOperation.AddMethod);
+            PopulateEncLogTableEventsOrProperties(_propertyDefs, TokenTypeIds.Property, EditAndContinueOperation.AddProperty, _propertyMap, TokenTypeIds.PropertyMap);
 
             PopulateEncLogTableParameters();
 
@@ -721,7 +721,7 @@ namespace Microsoft.CodeAnalysis.Emit
         private void PopulateEncLogTableEventsOrProperties<T>(
             DefinitionIndex<T> index,
             int tokenType,
-            EncFuncCode addCode,
+            EditAndContinueOperation addCode,
             EventOrPropertyMapIndex map,
             int mapTokenType)
             where T : ITypeDefinitionMember
@@ -744,14 +744,14 @@ namespace Microsoft.CodeAnalysis.Emit
 
                 builder.AddEncLogEntry(
                     token: tokenType | index[member],
-                    code: EncFuncCode.Default);
+                    code: EditAndContinueOperation.Default);
             }
         }
 
         private void PopulateEncLogTableFieldsOrMethods<T>(
             DefinitionIndex<T> index,
             int tokenType,
-            EncFuncCode addCode)
+            EditAndContinueOperation addCode)
             where T : ITypeDefinitionMember
         {
             foreach (var member in index.GetRows())
@@ -765,7 +765,7 @@ namespace Microsoft.CodeAnalysis.Emit
 
                 builder.AddEncLogEntry(
                     token: tokenType | index[member],
-                    code: EncFuncCode.Default);
+                    code: EditAndContinueOperation.Default);
             }
         }
 
@@ -778,11 +778,11 @@ namespace Microsoft.CodeAnalysis.Emit
 
                 builder.AddEncLogEntry(
                     token: TokenTypeIds.MethodDef | _methodDefs[methodDef],
-                    code: EncFuncCode.AddParameter);
+                    code: EditAndContinueOperation.AddParameter);
 
                 builder.AddEncLogEntry(
                     token: TokenTypeIds.ParamDef | (parameterFirstId + i),
-                    code: EncFuncCode.Default);
+                    code: EditAndContinueOperation.Default);
             }
         }
 
@@ -793,7 +793,7 @@ namespace Microsoft.CodeAnalysis.Emit
             {
                 builder.AddEncLogEntry(
                     token: tokenType | index[member],
-                    code: EncFuncCode.Default);
+                    code: EditAndContinueOperation.Default);
             }
         }
 
@@ -808,7 +808,7 @@ namespace Microsoft.CodeAnalysis.Emit
             {
                 builder.AddEncLogEntry(
                     token: tokenType | (firstRowId + i),
-                    code: EncFuncCode.Default);
+                    code: EditAndContinueOperation.Default);
             }
         }
 
--- src/Compilers/Core/Portable/System.Reflection.Metadata/Ecma335/MetadataBuilder.Tables.cs
+++ src/Compilers/Core/Portable/System.Reflection.Metadata/Ecma335/MetadataBuilder.Tables.cs
@@ -661,7 +661,7 @@ namespace System.Reflection.Metadata.Ecma335
             });
         }
 
-        public void AddEncLogEntry(int token, EncFuncCode code)
+        public void AddEncLogEntry(int token, EditAndContinueOperation code)
         {
             _encLogTable.Add(new EncLogRow
             {
