b04dcaf2076c247cb975d9468081759cc2316184
Dennis Fischer <fischer_dennis@live.de>
2015-12-20 05:24:06 +0100

    Fixes that unverifiable IL is generated for switch over Nullable<Int64> and Nullable<UInt64>
    The compiler tries to create a conversion of the expression on the case switch label to the type that is switched on. if the parameter is a Nullable this doesn't work and it assumed that the type is compatible with the underlying type of the Nullable expression. This is not the case if we have a Nullable<long> and an integer on the case switch.

--- src/Compilers/CSharp/Test/Emit/CodeGen/SwitchTests.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/SwitchTests.cs
@@ -1947,6 +1947,160 @@ class Program
 ");
         }
 
+        [Fact]
+        public void SwitchOnNullableInt64WithInt32Label()
+        {
+            var text = @"public static class C
+{
+    public static bool F(long? x)
+    {
+        switch (x)
+        {
+            case 1:
+                return true;
+            default:
+                return false;
+        }
+    }
+    static void Main()
+    {
+        System.Console.WriteLine(F(1));
+    }
+}";
+            var compVerifier = CompileAndVerify(text, expectedOutput: "True");
+            compVerifier.VerifyIL("C.F(long?)",
+@"
+{
+        // Code size       28 (0x1c)
+        .maxstack  2
+        .locals init (long? V_0,
+                      long V_1)
+        IL_0000:  ldarg.0
+        IL_0001:  stloc.0
+        IL_0002:  ldloca.s   V_0
+        IL_0004:  call       ""bool long?.HasValue.get""
+        IL_0009:  brfalse.s  IL_001a
+        IL_000b:  ldloca.s   V_0
+        IL_000d:  call       ""long long?.GetValueOrDefault()""
+        IL_0012:  stloc.1
+        IL_0013:  ldloc.1
+        IL_0014:  ldc.i4.1
+        IL_0015:  conv.i8
+        IL_0016:  bne.un.s   IL_001a
+        IL_0018:  ldc.i4.1
+        IL_0019:  ret
+        IL_001a:  ldc.i4.0
+        IL_001b:  ret
+}"
+            );
+        }
+
+        [Fact]
+        public void SwitchOnNullableWithNonConstant()
+        {
+            var text = @"public static class C
+{
+    public static bool F(int? x)
+    {
+        int i = 4;
+        switch (x)
+        {
+            case i:
+                return true;
+            default:
+                return false;
+        }
+    }
+    static void Main()
+    {
+        System.Console.WriteLine(F(1));
+    }
+}";
+            var compilation = base.CreateCSharpCompilation(text);
+
+            var expected = Diagnostic(ErrorCode.ERR_ConstantExpected, "case i:");
+
+            compilation.VerifyDiagnostics(expected);
+        }
+
+        [Fact]
+        public void SwitchOnNullableWithNonCompatibleType()
+        {
+            var text = @"public static class C
+{
+    public static bool F(int? x)
+    {
+        switch (x)
+        {
+            case default(System.DateTime):
+                return true;
+            default:
+                return false;
+        }
+    }
+    static void Main()
+    {
+        System.Console.WriteLine(F(1));
+    }
+}";
+            var compilation = base.CreateCSharpCompilation(text);
+
+            var expected = Diagnostic(ErrorCode.ERR_NoImplicitConv, "default(System.DateTime)").WithArguments("System.DateTime", "int?");
+
+            compilation.VerifyDiagnostics(expected);
+        }
+
+        [Fact]
+        public void SwitchOnNullableInt64WithInt32LabelWithEnum()
+        {
+            var text = @"public static class C
+{
+    public static bool F(long? x)
+    {
+        switch (x)
+        {
+            case (int)Foo.X:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public enum Foo : int { X  = 1 }
+
+    static void Main()
+    {
+        System.Console.WriteLine(F(1));
+    }
+}";
+            var compVerifier = CompileAndVerify(text, expectedOutput: "True");
+            compVerifier.VerifyIL("C.F(long?)",
+@"
+{
+        // Code size       28 (0x1c)
+        .maxstack  2
+        .locals init (long? V_0,
+                      long V_1)
+        IL_0000:  ldarg.0
+        IL_0001:  stloc.0
+        IL_0002:  ldloca.s   V_0
+        IL_0004:  call       ""bool long?.HasValue.get""
+        IL_0009:  brfalse.s  IL_001a
+        IL_000b:  ldloca.s   V_0
+        IL_000d:  call       ""long long?.GetValueOrDefault()""
+        IL_0012:  stloc.1
+        IL_0013:  ldloc.1
+        IL_0014:  ldc.i4.1
+        IL_0015:  conv.i8
+        IL_0016:  bne.un.s   IL_001a
+        IL_0018:  ldc.i4.1
+        IL_0019:  ret
+        IL_001a:  ldc.i4.0
+        IL_001b:  ret
+}"
+            );
+        }
+
         // TODO: Add more targeted tests for verifying switch bucketing
 
         #region "String tests"
