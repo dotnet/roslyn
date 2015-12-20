15a725aa4bf5d031950436e22adccd53fb06fe02
Kevin Pilch-Bisson <kevin@pilch-bisson.net>
2015-12-19 16:10:45 -0800

    Fix formatting and add some constructor overloads

--- src/EditorFeatures/Core/CodeDefinitionWindowLocation.cs
+++ src/EditorFeatures/Core/CodeDefinitionWindowLocation.cs
@@ -1,20 +1,32 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using Microsoft.CodeAnalysis.Text;
+
 namespace Microsoft.CodeAnalysis.Editor
 {
     internal struct CodeDefinitionWindowLocation
-        {
-            public string DisplayName { get; }
-            public string FilePath { get; }
-            public int Line { get; }
-            public int Character { get; }
+    {
+        public string DisplayName { get; }
+        public string FilePath { get; }
+        public int Line { get; }
+        public int Character { get; }
 
-            public CodeDefinitionWindowLocation(string displayName, string filePath, int line, int character)
-            {
-                DisplayName = displayName;
-                FilePath = filePath;
-                Line = line;
-                Character = character;
-            }
+        public CodeDefinitionWindowLocation(string displayName, string filePath, int line, int character)
+        {
+            DisplayName = displayName;
+            FilePath = filePath;
+            Line = line;
+            Character = character;
         }
+
+        public CodeDefinitionWindowLocation(string displayName, string filePath, LinePositionSpan position)
+            : this (displayName, filePath, position.Start.Line, position.Start.Character)
+        {
+        }
+
+        public CodeDefinitionWindowLocation(string displayName, FileLinePositionSpan position)
+            : this (displayName, position.Path, position.Span)
+        {
+        }
+    }
 }
