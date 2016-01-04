d7ec35dd8bb1bbb8a5bd1e2f4be9c2fd8db096e9
Cyrus Najmabadi <cyrusn@microsoft.com>
2016-01-04 11:55:57 -0800

    Remove unnecessary usings.

--- src/Compilers/CSharp/Portable/Syntax/CompilationUnitSyntax.cs
+++ src/Compilers/CSharp/Portable/Syntax/CompilationUnitSyntax.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using Microsoft.CodeAnalysis.Syntax;
 
 namespace Microsoft.CodeAnalysis.CSharp.Syntax
 {
--- src/Compilers/VisualBasic/Portable/Syntax/CompilationUnitSyntax.vb
+++ src/Compilers/VisualBasic/Portable/Syntax/CompilationUnitSyntax.vb
@@ -1,7 +1,5 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-Imports Microsoft.CodeAnalysis.Syntax
-
 Namespace Microsoft.CodeAnalysis.VisualBasic.Syntax
 
     Partial Public NotInheritable Class CompilationUnitSyntax
