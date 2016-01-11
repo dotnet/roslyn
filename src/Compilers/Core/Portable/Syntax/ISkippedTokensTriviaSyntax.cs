51417fbcdb2b1335dffb4a7f4bb4af79383d5369
Dustin Campbell <dustin@teamcampbell.org>
2016-01-11 14:05:37 -0800

    Add ISkippedTokensTriviaSyntax to Syntax API
    This change adds an ISkippedTokensTriviaSyntax interface that is
    implementation by SkippedTokensTriviaSyntax in both C# and VB. It simply
    exposes the Tokens property so that code that walks over skipped tokens
    does not need to be duplicated for each language.

---
+++ src/Compilers/Core/Portable/Syntax/ISkippedTokensTriviaSyntax.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.CodeAnalysis
+{
+#pragma warning disable RS0010
+    /// <summary>
+    /// Represents structured trivia that contains skipped tokens. This is implemented by
+    /// <see cref="T:Microsoft.CodeAnalysis.CSharp.Syntax.SkippedTokensTriviaSyntax"/> and
+    /// <see cref="T:Microsoft.CodeAnalysis.VisualBasic.Syntax.SkippedTokensTriviaSyntax"/>.
+    /// </summary>
+#pragma warning restore RS0010
+    public interface ISkippedTokensTriviaSyntax
+    {
+        SyntaxTokenList Tokens { get; }
+    }
+}
