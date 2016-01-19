836317c757b808c24764cfe2ecdf36dea87261be
Cyrus Najmabadi <cyrusn@microsoft.com>
2016-01-19 11:31:46 -0800

    Adding copyright.

--- src/EditorFeatures/VisualBasicTest/Diagnostics/MakeMethodSynchronous/MakeMethodSynchronousTests.vb
+++ src/EditorFeatures/VisualBasicTest/Diagnostics/MakeMethodSynchronous/MakeMethodSynchronousTests.vb
@@ -1,4 +1,6 @@
-﻿Imports Microsoft.CodeAnalysis.CodeFixes
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports Microsoft.CodeAnalysis.CodeFixes
 Imports Microsoft.CodeAnalysis.Diagnostics
 Imports Microsoft.CodeAnalysis.VisualBasic.MakeMethodSynchronous
 
