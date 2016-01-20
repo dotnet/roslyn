c53653531ddf2e9b16797cb59c202602ad11f8ad
Andrew Casey <andrew.casey@microsoft.com>
2016-01-19 18:27:46 -0800

    Unswitch files

--- src/Scripting/VisualBasic/Hosting/ObjectFormatter/VisualBasicMemberFilter.vb
+++ src/Scripting/VisualBasic/Hosting/ObjectFormatter/VisualBasicMemberFilter.vb
@@ -1,27 +1,16 @@
 ' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-Imports System.Reflection
 Imports Microsoft.CodeAnalysis.Scripting.Hosting
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.Scripting.Hosting
 
-    Public Class VisualBasicObjectFormatter
-        Inherits CommonObjectFormatter
+    Public Class VisualBasicMemberFilter
+        Inherits CommonMemberFilter
 
-        Protected Overrides ReadOnly Property TypeNameFormatter As CommonTypeNameFormatter
-        Protected Overrides ReadOnly Property PrimitiveFormatter As CommonPrimitiveFormatter
-        Protected Overrides ReadOnly Property Filter As MemberFilter
-
-        Public Sub New()
-            PrimitiveFormatter = New VisualBasicPrimitiveFormatter()
-            TypeNameFormatter = New VisualBasicTypeNameFormatter(PrimitiveFormatter)
-            Filter = New VisualBasicMemberFilter()
-        End Sub
-
-        Protected Overrides Function FormatRefKind(parameter As ParameterInfo) As String
-            Return If(parameter.IsOut, "ByRef", "")
+        Protected Overrides Function IsGeneratedMemberName(name As String) As Boolean
+            ' TODO (tomat)
+            Return MyBase.IsGeneratedMemberName(name)
         End Function
     End Class
 
 End Namespace
-
