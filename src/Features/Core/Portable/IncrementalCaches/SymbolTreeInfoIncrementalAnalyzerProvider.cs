3cb96fcc1cd9d819b30ae775ca0c5a2beb9b7223
Cyrus Najmabadi <cyrusn@microsoft.com>
2016-01-20 08:37:20 -0800

    Simplify code.

--- src/Features/Core/Portable/IncrementalCaches/SymbolTreeInfoIncrementalAnalyzerProvider.cs
+++ src/Features/Core/Portable/IncrementalCaches/SymbolTreeInfoIncrementalAnalyzerProvider.cs
@@ -221,11 +221,10 @@ namespace Microsoft.CodeAnalysis.IncrementalCaches
                     var projectTask = SymbolTreeInfo.GetInfoForSourceAssemblyAsync(project, cancellationToken);
 
                     await Task.WhenAll(referencesTask, projectTask).ConfigureAwait(false);
-                    var info = await projectTask.ConfigureAwait(false);
 
                     // Mark that we're up to date with this project.  Future calls with the same 
                     // semantic version can bail out immediately.
-                    projectInfo = new ProjectInfo(version, info);
+                    projectInfo = new ProjectInfo(version, await projectTask.ConfigureAwait(false));
                     _projectToInfo.AddOrUpdate(project.Id, projectInfo, (_1, _2) => projectInfo);
                 }
             }
