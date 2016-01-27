bf403adbab5131581b57467b6028abdda5e18ef4
Heejae Chang <hechang@microsoft.com>
2016-01-27 02:07:30 -0800

    updated comments

--- src/VisualStudio/Core/Def/Implementation/TableDataSource/Suppression/SuppressionStateColumnFilterDefinition.cs
+++ src/VisualStudio/Core/Def/Implementation/TableDataSource/Suppression/SuppressionStateColumnFilterDefinition.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using Microsoft.VisualStudio.Shell.TableControl;
 using Microsoft.VisualStudio.Utilities;
 using Microsoft.Internal.VisualStudio.Shell.TableControl;
 using System;
@@ -10,7 +8,7 @@ using System;
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.TableDataSource
 {
     /// <summary>
-    /// Error list column for Suppression state of a diagnostic.
+    /// Supporession column definition filter
     /// </summary>
     /// <remarks>
     /// TODO: Move this column down to the shell as it is shared by multiple issue sources (Roslyn and FxCop).
