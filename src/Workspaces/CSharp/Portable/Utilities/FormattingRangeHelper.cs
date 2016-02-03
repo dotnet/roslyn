cdde8a1362d16834d4eb40bd0cd18f6ff05abc38
Balaji Soundrarajan <basoundr@microsoft.com>
2016-02-03 13:04:37 -0800

    Format lock statement with single statement appropriately
    Fixes #7900
    
    Format lock statement with embedded statement as a special node

--- src/Workspaces/CSharp/Portable/Utilities/FormattingRangeHelper.cs
+++ src/Workspaces/CSharp/Portable/Utilities/FormattingRangeHelper.cs
@@ -358,7 +358,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Utilities
                 node.Kind() == SyntaxKind.TryStatement ||
                 node.Kind() == SyntaxKind.CatchClause ||
                 node.Kind() == SyntaxKind.FinallyClause ||
-                node.Kind() == SyntaxKind.LabeledStatement;
+                node.Kind() == SyntaxKind.LabeledStatement ||
+                node.Kind() == SyntaxKind.LockStatement;
         }
 
         private static SyntaxNode GetTopContainingNode(SyntaxNode node)
