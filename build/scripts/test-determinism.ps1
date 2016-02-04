136b6309bbec23f08950ef8b941871ed58fd5c69
Jared Parsons <jaredpar@microsoft.com>
2016-02-03 22:11:50 -0800

    Hook determinism testing into cibuild.cmd

--- build/scripts/test-determinism.ps1
+++ build/scripts/test-determinism.ps1
@@ -12,27 +12,45 @@ $map = @{}
 $i = 0;
 while ($i -lt 3 -and $allGood) {
 
-    rm -re -fo $debugDir
-    msbuild /v:m /m $sln
+    # Clean out the previous run
+    write-host "Cleaning the Binaries"
+    rm -re -fo "Binaries\Debug"
+    rm -re -fo "Binaries\Obj"
+    msbuild /nologo /v:m /t:clean $sln
+
+    write-host "Building the Solution"
+    msbuild /nologo /v:m /m $sln
 
     pushd $debugDir
 
-    foreach ($dll in gci -re -in Microsoft.CodeAnalysis.*dll,Roslyn.*dll) {
+    write-host "Testing the binaries"
+    foreach ($dll in gci -re -in Microsoft.CodeAnalysis.*dll,Roslyn.*dll,cs*exe,vb*exe) {
         $dllName = split-path -leaf $dll
         $dllHash = get-md5 $dll
         if ($i -eq 0) {
+            write-host "`tRecording $dllName = $dllHash"
             $map[$dllName] = $dllHash
         }
         else {
             $oldHash = $map[$dllName]
-            if ($oldHash -ne $dllHash) {
-                write-host "$dllName changed"
+            if ($oldHash -eq $dllHash) {
+                write-host "`tVerified $dllName"
+            }
+            else {
+                write-host "`tERROR! $dllName changed"
                 $allGood = $false
             }
         }
     }
 
     popd
+
+    # Sanity check to ensure we didn't return a false positive because we failed
+    # to examine any binaries.
+    if ($map.Count -lt 10) {
+        write-host "Found no binaries to process"
+        $allGood = $false
+    }
 
     $i++
 }
