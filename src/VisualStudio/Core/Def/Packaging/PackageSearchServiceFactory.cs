934630edd95c4232a79304693336538010881940
CyrusNajmabadi <cyrusn@microsoft.com>
2016-02-06 20:36:53 -0800

    Break factory into its own file.

--- src/VisualStudio/Core/Def/Packaging/PackageSearchServiceFactory.cs
+++ src/VisualStudio/Core/Def/Packaging/PackageSearchServiceFactory.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Composition;
 using System.IO;
 using System.IO.Compression;
 using System.Reflection;
@@ -10,49 +9,17 @@ using System.Xml.Linq;
 using Elfie.Model;
 using Elfie.Model.Structures;
 using Elfie.Model.Tree;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
-using Microsoft.CodeAnalysis.Host;
-using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.Packaging;
 using Microsoft.CodeAnalysis.Shared.Utilities;
 using Microsoft.Internal.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Settings;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Shell.Settings;
-using Roslyn.Utilities;
 using VSShell = Microsoft.VisualStudio.Shell;
 
 namespace Microsoft.VisualStudio.LanguageServices.Packaging
 {
-    [ExportWorkspaceServiceFactory(typeof(IPackageSearchService), WorkspaceKind.Host), Shared]
-    internal class PackageSearchServiceFactory : IWorkspaceServiceFactory
-    {
-        private readonly VSShell.SVsServiceProvider _serviceProvider;
-
-        [ImportingConstructor]
-        public PackageSearchServiceFactory(VSShell.SVsServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
-
-        public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
-        {
-            // Only support package search in vs workspace.
-            return workspaceServices.Workspace is VisualStudioWorkspace
-                ? new PackageSearchService(_serviceProvider)
-                : (IPackageSearchService)new NullPackageSearchService();
-        }
-
-        private class NullPackageSearchService : IPackageSearchService
-        {
-            public IEnumerable<PackageSearchResult> Search(string name, int arity, CancellationToken cancellationToken)
-            {
-                return SpecializedCollections.EmptyEnumerable<PackageSearchResult>();
-            }
-        }
-    }
-
     internal class PackageSearchService : ForegroundThreadAffinitizedObject, IPackageSearchService, IDisposable
     {
         private const string HostId = "RoslynNuGetSearch";
---
+++ src/VisualStudio/Core/Def/Packaging/PackageSearchServiceFactory1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Packaging;
+using Roslyn.Utilities;
+using VSShell = Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.LanguageServices.Packaging
+{
+    [ExportWorkspaceServiceFactory(typeof(IPackageSearchService), WorkspaceKind.Host), Shared]
+    internal class PackageSearchServiceFactory : IWorkspaceServiceFactory
+    {
+        private readonly VSShell.SVsServiceProvider _serviceProvider;
+
+        [ImportingConstructor]
+        public PackageSearchServiceFactory(VSShell.SVsServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
+        {
+            // Only support package search in vs workspace.
+            return workspaceServices.Workspace is VisualStudioWorkspace
+                ? new PackageSearchService(_serviceProvider)
+                : (IPackageSearchService)new NullPackageSearchService();
+        }
+
+        private class NullPackageSearchService : IPackageSearchService
+        {
+            public IEnumerable<PackageSearchResult> Search(string name, int arity, CancellationToken cancellationToken)
+            {
+                return SpecializedCollections.EmptyEnumerable<PackageSearchResult>();
+            }
+        }
+    }
+}
