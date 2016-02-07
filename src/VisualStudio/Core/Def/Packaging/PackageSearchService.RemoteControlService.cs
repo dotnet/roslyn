30510dbdaa7b844820b27460848b7df6b7ae43fa
CyrusNajmabadi <cyrusn@microsoft.com>
2016-02-06 20:45:29 -0800

    Break out nested type.

--- src/VisualStudio/Core/Def/Packaging/PackageSearchService.RemoteControlService.cs
+++ src/VisualStudio/Core/Def/Packaging/PackageSearchService.RemoteControlService.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Internal.VisualStudio.Shell.Interop;
 
 namespace Microsoft.VisualStudio.LanguageServices.Packaging
 {
     internal partial class PackageSearchService
     {
+        private class RemoteControlService : IPackageSearchRemoteControlService
+        {
+            private readonly object _remoteControlService;
+
+            public RemoteControlService(object remoteControlService)
+            {
+                _remoteControlService = remoteControlService;
+            }
+
+            public IPackageSearchRemoteControlClient CreateClient(string hostId, string serverPath, int pollingMinutes)
+            {
+                var serviceType = _remoteControlService.GetType();
+                var serviceAssembly = serviceType.Assembly;
+                var clientType = serviceAssembly.GetType("Microsoft.VisualStudio.Services.RemoteControl.VSRemoteControlClient");
+
+                var vsClient = Activator.CreateInstance(clientType, args: new object[]
+                {
+                    HostId,
+                    serverPath,
+                    pollingMinutes,
+                    null
+                });
+
+                var clientField = vsClient.GetType().GetField("client", BindingFlags.Instance | BindingFlags.NonPublic);
+                var client = clientField.GetValue(vsClient) as IDisposable;
+                return new RemoteControlClient(client);
+            }
+        }
+
+        private class RemoteControlClient : IPackageSearchRemoteControlClient
+        {
+            private readonly IDisposable client;
+
+            public RemoteControlClient(IDisposable client)
+            {
+                this.client = client;
+            }
+
+            public void Dispose()
+            {
+                client.Dispose();
+            }
+
+            public Task<Stream> ReadFileAsync(__VsRemoteControlBehaviorOnStale behavior)
+            {
+                var clientType = client.GetType();
+                var readFileAsyncMethod = clientType.GetMethod("ReadFileAsync");
+                var streamTask = (Task<Stream>)readFileAsyncMethod.Invoke(client, new object[] { (int)behavior });
+
+                return streamTask;
+            }
+        }
     }
 }
--- src/VisualStudio/Core/Def/Packaging/PackageSearchService.cs
+++ src/VisualStudio/Core/Def/Packaging/PackageSearchService.cs
@@ -68,7 +68,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Packaging
                 return null;
             }
 
-            return new DefaultPackageRemoteControlService(vsService);
+            return new RemoteControlService(vsService);
         }
 
         public PackageSearchService(
@@ -579,59 +579,6 @@ namespace Microsoft.VisualStudio.LanguageServices.Packaging
             {
                 GetFullName(nameParts, path.Parent);
                 nameParts.Add(path.Name.ToString());
-            }
-        }
-
-        private class DefaultPackageRemoteControlService : IPackageSearchRemoteControlService
-        {
-            private readonly object _remoteControlService;
-
-            public DefaultPackageRemoteControlService(object remoteControlService)
-            {
-                _remoteControlService = remoteControlService;
-            }
-
-            public IPackageSearchRemoteControlClient CreateClient(string hostId, string serverPath, int pollingMinutes)
-            {
-                var serviceType = _remoteControlService.GetType();
-                var serviceAssembly = serviceType.Assembly;
-                var clientType = serviceAssembly.GetType("Microsoft.VisualStudio.Services.RemoteControl.VSRemoteControlClient");
-
-                var vsClient = Activator.CreateInstance(clientType, args: new object[]
-                {
-                    HostId,
-                    serverPath,
-                    pollingMinutes,
-                    null
-                });
-
-                var clientField = vsClient.GetType().GetField("client", BindingFlags.Instance | BindingFlags.NonPublic);
-                var client = clientField.GetValue(vsClient) as IDisposable;
-                return new DefaultRemoteControlClient(client);
-            }
-        }
-
-        private class DefaultRemoteControlClient : IPackageSearchRemoteControlClient
-        {
-            private readonly IDisposable client;
-
-            public DefaultRemoteControlClient(IDisposable client)
-            {
-                this.client = client;
-            }
-
-            public void Dispose()
-            {
-                client.Dispose();
-            }
-
-            public Task<Stream> ReadFileAsync(__VsRemoteControlBehaviorOnStale behavior)
-            {
-                var clientType = client.GetType();
-                var readFileAsyncMethod = clientType.GetMethod("ReadFileAsync");
-                var streamTask = (Task<Stream>)readFileAsyncMethod.Invoke(client, new object[] { (int)behavior});
-
-                return streamTask;
             }
         }
 
