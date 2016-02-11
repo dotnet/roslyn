ec2622b35769a563a00a8e39a3e50e111928abd4
Andy Gocke <angocke@microsoft.com>
2016-02-10 17:02:12 -0800

    Update version of Microsft.Build.Mono.Debug

--- build/scripts/tests.sh
+++ build/scripts/tests.sh
@@ -11,10 +11,10 @@ chmod +x ./corerun
 
 mkdir -p ../xUnitResults/
 
-./corerun ./xunit.console.netcore.exe *.UnitTests.dll -xml ../xUnitResults/CoreClrUnitTestResults.xml
+./corerun ./xunit.console.netcore.exe *.UnitTests.dll -parallel all -xml ../xUnitResults/CoreClrUnitTestResults.xml
 
 if [ $? -ne 0 ]; then
-	echo Unit test failed
-	exit 1
+    echo Unit test failed
+    exit 1
 fi
 
