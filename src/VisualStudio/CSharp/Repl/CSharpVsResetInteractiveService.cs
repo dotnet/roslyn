8bc7bcd6338cc00151c92d434551eb443b83d144
Artur Spychaj <drognanar@gmail.com>
2016-02-17 10:11:06 -0800

    Move ResetInteractive out of InteractiveFeatures

---
+++ src/VisualStudio/CSharp/Repl/CSharpVsResetInteractiveService.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using EnvDTE;
+using Microsoft.CodeAnalysis.Editor;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.LanguageServices.Interactive;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Roslyn.VisualStudio.Services.Interactive;
+using System;
+using System.ComponentModel.Composition;
+
+namespace Microsoft.VisualStudio.LanguageServices.CSharp.Interactive
+{
+    [ExportInteractive(typeof(AbstractResetInteractiveCommand), ContentTypeNames.CSharpContentType)]
+    internal sealed class CSharpVsResetInteractiveFactory
+        : AbstractResetInteractiveCommand
+    {
+        private readonly IComponentModel _componentModel;
+        private readonly CSharpVsInteractiveWindowProvider _interactiveWindowProvider;
+        private readonly IServiceProvider _serviceProvider;
+
+        [ImportingConstructor]
+        public CSharpVsResetInteractiveFactory(
+            [Import]CSharpVsInteractiveWindowProvider interactiveWindowProvider,
+            [Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+        {
+            _interactiveWindowProvider = interactiveWindowProvider;
+            _serviceProvider = serviceProvider;
+            _componentModel = (IComponentModel)GetService(typeof(SComponentModel));
+        }
+
+        private object GetService(Type type)
+        {
+            return _serviceProvider.GetService(type);
+        }
+
+        internal override void ExecuteResetInteractive()
+        {
+            var resetInteractive = new VsResetInteractive(
+                        (DTE)this.GetService(typeof(SDTE)),
+                        _componentModel,
+                        (IVsMonitorSelection)this.GetService(typeof(SVsShellMonitorSelection)),
+                        (IVsSolutionBuildManager)this.GetService(typeof(SVsSolutionBuildManager)),
+                        CreateReference,
+                        CreateImport);
+
+            var vsInteractiveWindow = _interactiveWindowProvider.Open(instanceId: 0, focus: true);
+
+            EventHandler focusWindow = null;
+            focusWindow = (s, e) =>
+            {
+                // We have to set focus to the Interactive Window *after* the wait indicator is dismissed.
+                vsInteractiveWindow.Show(focus: true);
+                resetInteractive.ExecutionCompleted -= focusWindow;
+            };
+
+            resetInteractive.Execute(vsInteractiveWindow.InteractiveWindow, LanguageName + " Interactive");
+            resetInteractive.ExecutionCompleted += focusWindow;
+        }
+
+        protected override string LanguageName
+        {
+            get { return "C#"; }
+        }
+
+        protected override string ProjectKind
+        {
+            get { return VSLangProj.PrjKind.prjKindCSharpProject; }
+        }
+
+        protected override string CreateReference(string referenceName)
+        {
+            return string.Format("#r \"{0}\"", referenceName);
+        }
+
+        protected override string CreateImport(string namespaceName)
+        {
+            return string.Format("using {0};", namespaceName);
+        }
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/Interactive/AbstractResetInteractiveMenuCommand.cs
@@ -0,0 +1,139 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Roslyn.VisualStudio.Services.Interactive;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.CodeAnalysis.Editor;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.Interactive
+{
+    internal abstract class AbstractResetInteractiveMenuCommand
+    {
+        protected abstract string ProjectKind { get; }
+
+        protected abstract CommandID GetResetInteractiveFromProjectCommandID();
+
+        private readonly OleMenuCommandService _menuCommandService;
+        private readonly IVsMonitorSelection _monitorSelection;
+        private readonly IComponentModel _componentModel;
+        private readonly string _contentType;
+
+        private AbstractResetInteractiveCommand ResetInteractiveService
+        {
+            get
+            {
+                return _componentModel.DefaultExportProvider
+                    .GetExports<AbstractResetInteractiveCommand, ContentTypeMetadata>()
+                    .Where(resetInteractiveService => resetInteractiveService.Metadata.ContentTypes.Contains(_contentType))
+                    .Single().Value;
+            }
+        }
+
+        public AbstractResetInteractiveMenuCommand(
+            string contentType,
+            OleMenuCommandService menuCommandService,
+            IVsMonitorSelection monitorSelection,
+            IComponentModel componentModel)
+        {
+            _contentType = contentType;
+            _menuCommandService = menuCommandService;
+            _monitorSelection = monitorSelection;
+            _componentModel = componentModel;
+        }
+
+        internal void InitializeResetInteractiveFromProjectCommand()
+        {
+            var resetInteractiveFromProjectCommand = new OleMenuCommand(
+                (sender, args) =>
+                {
+                    ResetInteractiveService.ExecuteResetInteractive();
+                },
+                GetResetInteractiveFromProjectCommandID());
+
+            resetInteractiveFromProjectCommand.Supported = true;
+
+            resetInteractiveFromProjectCommand.BeforeQueryStatus += (_, __) =>
+            {
+                EnvDTE.Project project;
+                FrameworkName frameworkName;
+                GetActiveProject(out project, out frameworkName);
+                var targetFramework = project.Properties.Item("TargetFramework");
+                var available = project != null && project.Kind == ProjectKind
+                    && frameworkName != null && frameworkName.Identifier == ".NETFramework";
+
+                resetInteractiveFromProjectCommand.Enabled = available;
+                resetInteractiveFromProjectCommand.Supported = available;
+                resetInteractiveFromProjectCommand.Visible = available;
+            };
+
+            _menuCommandService.AddCommand(resetInteractiveFromProjectCommand);
+        }
+
+        private bool GetActiveProject(out EnvDTE.Project project, out FrameworkName frameworkName)
+        {
+            project = null;
+            frameworkName = null;
+
+            IntPtr hierarchyPointer = IntPtr.Zero;
+            IntPtr selectionContainerPointer = IntPtr.Zero;
+
+            try
+            {
+                uint itemid;
+                IVsMultiItemSelect multiItemSelect;
+
+                Marshal.ThrowExceptionForHR(
+                    _monitorSelection.GetCurrentSelection(
+                        out hierarchyPointer,
+                        out itemid,
+                        out multiItemSelect,
+                        out selectionContainerPointer));
+
+                if (itemid != (uint)VSConstants.VSITEMID.Root)
+                {
+                    return false;
+                }
+
+                var hierarchy = Marshal.GetObjectForIUnknown(hierarchyPointer) as IVsHierarchy;
+                if (hierarchy == null)
+                {
+                    return false;
+                }
+
+                object extensibilityObject;
+                object targetFrameworkVersion;
+                object targetFrameworkMonikerObject;
+                Marshal.ThrowExceptionForHR(
+                    hierarchy.GetProperty((uint)VSConstants.VSITEMID.Root, (int)__VSHPROPID.VSHPROPID_ExtObject, out extensibilityObject));
+                Marshal.ThrowExceptionForHR(
+                    hierarchy.GetProperty((uint)VSConstants.VSITEMID.Root, (int)__VSHPROPID3.VSHPROPID_TargetFrameworkVersion, out targetFrameworkVersion));
+                Marshal.ThrowExceptionForHR(
+                    hierarchy.GetProperty((uint)VSConstants.VSITEMID.Root, (int)__VSHPROPID4.VSHPROPID_TargetFrameworkMoniker, out targetFrameworkMonikerObject));
+
+                string targetFrameworkMoniker = targetFrameworkMonikerObject as string;
+                frameworkName = new System.Runtime.Versioning.FrameworkName(targetFrameworkMoniker);
+
+                project = extensibilityObject as EnvDTE.Project;
+                return true;
+            }
+            finally
+            {
+                if (hierarchyPointer != IntPtr.Zero)
+                {
+                    Marshal.Release(hierarchyPointer);
+                }
+
+                if (selectionContainerPointer != IntPtr.Zero)
+                {
+                    Marshal.Release(selectionContainerPointer);
+                }
+            }
+        }
+    }
+}
