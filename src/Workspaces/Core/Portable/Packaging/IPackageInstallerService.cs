eecb79209b652559c5906b02f9df4e7291a8d846
CyrusNajmabadi <cyrusn@microsoft.com>
2016-02-20 20:56:19 -0800

    Add the IPackageInstallerService.
    This interface is used to provide facilities for querying Nuget information
    on any thread.  The normal nuget apis are thread affinitized and that's
    problematic for the code actions we want running on the background.

---
+++ src/Workspaces/Core/Portable/Packaging/IPackageInstallerService.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.Packaging
+{
+    internal interface IPackageInstallerService : IWorkspaceService
+    {
+        bool IsInstalled(Workspace workspace, ProjectId projectId, string packageName);
+
+        bool TryInstallPackage(Workspace workspace, DocumentId documentId, string packageName, string versionOpt, CancellationToken cancellationToken);
+
+        IEnumerable<string> GetInstalledVersions(string packageName);
+
+        IEnumerable<Project> GetProjectsWithInstalledPackage(Solution solution, string packageName, string version);
+
+        void ShowManagePackagesDialog(string packageName);
+
+        ImmutableArray<string> PackageSources { get; }
+    }
+}
