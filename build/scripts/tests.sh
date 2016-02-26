bbf5729053cd1d1183013860dc3885c8782266f2
Andy Gocke <angocke@microsoft.com>
2016-02-25 16:11:34 -0800

    Put the CoreCLR test results in a xUnitResults directory

--- build/scripts/tests.sh
+++ build/scripts/tests.sh
@@ -9,9 +9,9 @@ cd Binaries/$BUILD_CONFIGURATION/CoreClrTest
 
 chmod +x ./corerun
 
-mkdir -p ../xUnitResults/
+mkdir -p xUnitResults
 
-./corerun ./xunit.console.netcore.exe *.UnitTests.dll -parallel all -xml ../xUnitResults/CoreClrUnitTestResults.xml
+./corerun ./xunit.console.netcore.exe *.UnitTests.dll -parallel all -xml xUnitResults/TestResults.xml
 
 if [ $? -ne 0 ]; then
     echo Unit test failed
