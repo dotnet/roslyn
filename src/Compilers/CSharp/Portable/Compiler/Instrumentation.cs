90cafcf37a11081cdbe1c220548e59771aee9d59
John Hamby <jhamby@microsoft.com>
2016-02-26 12:21:16 -0800

    Baby steps.

--- src/Compilers/CSharp/Portable/Compiler/Instrumentation.cs
+++ src/Compilers/CSharp/Portable/Compiler/Instrumentation.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.Semantics;
 
 namespace Microsoft.CodeAnalysis.CSharp
 {
@@ -14,7 +15,25 @@ namespace Microsoft.CodeAnalysis.CSharp
         {
             if (methodBody != null)
             {
+                OperationWalker collector = new InstrumentationCollectionWalker();
+                collector.Visit(methodBody);
+            }
+        }
+    }
 
+    internal sealed class InstrumentationCollectionWalker : OperationWalker
+    {
+        public override void Visit(IOperation operation)
+        {
+            switch (operation.Kind)
+            {
+                case OperationKind.BlockStatement:
+                    base.Visit(operation);
+                    break;
+                case OperationKind.ExpressionStatement:
+                    break;
+                case OperationKind.LoopStatement:
+                    break;
             }
         }
     }
