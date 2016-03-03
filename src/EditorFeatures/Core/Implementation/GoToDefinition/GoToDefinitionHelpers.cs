dfcfbaa6f443236b29e912b9a4e9f16045952c04
Manish Vasani <mavasani@microsoft.com>
2016-03-03 11:47:51 -0800

    Initial work to add support for external language service result providers for Find All References, GotoDefinition and NavigateTo.
    All the newly added result provider interfaces are internal, and would eventually go through API design review to enable third party plugin implementations.

--- src/EditorFeatures/Core/Implementation/GoToDefinition/GoToDefinitionHelpers.cs
+++ src/EditorFeatures/Core/Implementation/GoToDefinition/GoToDefinitionHelpers.cs
@@ -2,9 +2,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Editor.Host;
 using Microsoft.CodeAnalysis.Editor.Navigation;
 using Microsoft.CodeAnalysis.FindSymbols;
@@ -18,6 +20,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.GoToDefinition
         public static bool TryGoToDefinition(
             ISymbol symbol,
             Project project,
+            IEnumerable<Lazy<INavigableDefinitionProvider>> externalDefinitionProviders,
             IEnumerable<Lazy<INavigableItemsPresenter>> presenters,
             CancellationToken cancellationToken,
             bool thirdPartyNavigationAllowed = true,
@@ -63,10 +66,21 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.GoToDefinition
             var options = project.Solution.Workspace.Options;
 
             var preferredSourceLocations = NavigableItemFactory.GetPreferredSourceLocations(solution, symbol).ToArray();
+            var title = NavigableItemFactory.GetSymbolDisplayString(project, symbol);
             if (!preferredSourceLocations.Any())
             {
+                // Attempt to find source locations from external definition providers.
+                if (thirdPartyNavigationAllowed && externalDefinitionProviders.Any())
+                {
+                    var externalSourceDefinitions = FindExternalDefinitionsAsync(symbol, project, externalDefinitionProviders, cancellationToken).WaitAndGetResult(cancellationToken).ToImmutableArray();
+                    if (externalSourceDefinitions.Length > 0)
+                    {
+                        return TryGoToDefinition(externalSourceDefinitions, title, project.Solution.Workspace, presenters, throwOnHiddenDefinition);
+                    }
+                }
+
                 // If there are no visible source locations, then tell the host about the symbol and 
-                // allow it to navigate to it.  THis will either navigate to any non-visible source
+                // allow it to navigate to it.  This will either navigate to any non-visible source
                 // locations, or it can appropriately deal with metadata symbols for hosts that can go 
                 // to a metadata-as-source view.
 
@@ -77,20 +91,30 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.GoToDefinition
                     cancellationToken: cancellationToken);
             }
 
+            var navigableItems = preferredSourceLocations.Select(location => NavigableItemFactory.GetItemFromSymbolLocation(solution, symbol, location)).ToImmutableArray();
+            return TryGoToDefinition(navigableItems, title, project.Solution.Workspace, presenters, throwOnHiddenDefinition);
+        }
+
+        public static bool TryGoToDefinition(
+            ImmutableArray<INavigableItem> navigableItems,
+            string title,
+            Workspace workspace,
+            IEnumerable<Lazy<INavigableItemsPresenter>> presenters,
+            bool throwOnHiddenDefinition)
+        {
             // If we have a single location, then just navigate to it.
-            if (preferredSourceLocations.Length == 1)
+            if (navigableItems.Length == 1 && navigableItems[0].Document != null)
             {
-                var firstItem = preferredSourceLocations[0];
-                var workspace = project.Solution.Workspace;
+                var firstItem = navigableItems[0];
                 var navigationService = workspace.Services.GetService<IDocumentNavigationService>();
 
-                if (navigationService.CanNavigateToSpan(workspace, solution.GetDocument(firstItem.SourceTree).Id, firstItem.SourceSpan))
+                if (navigationService.CanNavigateToSpan(workspace, firstItem.Document.Id, firstItem.SourceSpan))
                 {
                     return navigationService.TryNavigateToSpan(
                         workspace,
-                        documentId: solution.GetDocument(firstItem.SourceTree).Id,
+                        documentId: firstItem.Document.Id,
                         textSpan: firstItem.SourceSpan,
-                        options: options.WithChangedOption(NavigationOptions.PreferProvisionalTab, true));
+                        options: workspace.Options.WithChangedOption(NavigationOptions.PreferProvisionalTab, true));
                 }
                 else
                 {
@@ -113,14 +137,61 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.GoToDefinition
 
                 if (presenters.Any())
                 {
-                    presenters.First().Value.DisplayResult(NavigableItemFactory.GetSymbolDisplayString(project, symbol),
-                        preferredSourceLocations.Select(location => NavigableItemFactory.GetItemFromSymbolLocation(solution, symbol, location)).ToList());
-
+                    presenters.First().Value.DisplayResult(title, navigableItems);
                     return true;
                 }
 
                 return false;
             }
+        }
+
+        public static async Task<IEnumerable<INavigableItem>> FindExternalDefinitionsAsync(Document document, int position, IEnumerable<Lazy<INavigableDefinitionProvider>> externalDefinitionProviders, CancellationToken cancellationToken)
+        {
+            foreach (var definitionProvider in externalDefinitionProviders)
+            {
+                var definitions = await definitionProvider.Value.FindDefinitionsAsync(document, position, cancellationToken).ConfigureAwait(false);
+                if (definitions != null && definitions.Any())
+                {
+                    var preferredDefinitions = NavigableItemFactory.GetPreferredNavigableItems(document.Project.Solution, definitions);
+                    if (preferredDefinitions.Any())
+                    {
+                        return preferredDefinitions;
+                    }
+                }
+            }
+
+            return SpecializedCollections.EmptyEnumerable<INavigableItem>();
+        }
+
+        public static async Task<IEnumerable<INavigableItem>> FindExternalDefinitionsAsync(ISymbol symbol, Project project, IEnumerable<Lazy<INavigableDefinitionProvider>> externalDefinitionProviders, CancellationToken cancellationToken)
+        {
+            foreach (var definitionProvider in externalDefinitionProviders)
+            {
+                var definitions = await definitionProvider.Value.FindDefinitionsAsync(project, symbol, cancellationToken).ConfigureAwait(false);
+                if (definitions != null && definitions.Any())
+                {
+                    var preferredDefinitions = NavigableItemFactory.GetPreferredNavigableItems(project.Solution, definitions);
+                    if (preferredDefinitions.Any())
+                    {
+                        return preferredDefinitions;
+                    }
+                }
+            }
+
+            return SpecializedCollections.EmptyEnumerable<INavigableItem>();
+        }
+
+        public static bool TryExternalGoToDefinition(Document document, int position, IEnumerable<Lazy<INavigableDefinitionProvider>> externalDefinitionProviders, IEnumerable<Lazy<INavigableItemsPresenter>> presenters, CancellationToken cancellationToken)
+        {
+            var externalDefinitions = FindExternalDefinitionsAsync(document, position, externalDefinitionProviders, cancellationToken).WaitAndGetResult(cancellationToken);
+            if (externalDefinitions != null && externalDefinitions.Any())
+            {
+                var itemsArray = externalDefinitions.ToImmutableArrayOrEmpty();
+                var title = itemsArray[0].DisplayString;
+                return TryGoToDefinition(externalDefinitions.ToImmutableArrayOrEmpty(), title, document.Project.Solution.Workspace, presenters, throwOnHiddenDefinition: true);
+            }
+
+            return false;
         }
 
         private static bool TryThirdPartyNavigation(ISymbol symbol, Solution solution)
--- src/VisualStudio/Core/Def/Implementation/ProjectSystem/MiscellaneousFilesWorkspace.cs
+++ src/VisualStudio/Core/Def/Implementation/ProjectSystem/MiscellaneousFilesWorkspace.cs
@@ -237,7 +237,15 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
         {
             var workspaceRegistration = (WorkspaceRegistration)sender;
             uint docCookie;
-            Contract.ThrowIfFalse(_docCookieToWorkspaceRegistration.TryGetKey(workspaceRegistration, out docCookie));
+
+            // external workspace that listens to events from IVSRunningDocumentTable4 (just like MiscellaneousFilesWorkspace does) and registers the text buffer for opened document prior to Miscellaneous workspace getting notified of the docCookie - the prior contract assumes that MiscellaneousFilesWorkspace is always the first one to get notified
+            if (!_docCookieToWorkspaceRegistration.TryGetKey(workspaceRegistration, out docCookie))
+            {
+                // We haven't even started tracking the document corresponding to this registration - likely because some external workspace registered the document's buffer prior to MiscellaneousWorkspace getting notified of it.
+                // Just bail out for now, we will eventually receive the opened document notification and track its docCookie registration.
+                return;
+            }
+
             var moniker = _runningDocumentTable.GetDocumentMoniker(docCookie);
 
             if (workspaceRegistration.Workspace == null)
@@ -255,7 +263,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
                     }
                 }
                 else
-                { 
+                {
                     // We should now claim this
                     AttachToDocument(docCookie, moniker);
                 }
