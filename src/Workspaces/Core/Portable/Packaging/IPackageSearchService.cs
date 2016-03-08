1dbcb4c246b9e2b794723e6e99bd11cb32b8a58d
CyrusNajmabadi <cyrusn@microsoft.com>
2016-03-08 12:01:49 -0800

    Break out package searching from reference assembly searching.

--- src/Workspaces/Core/Portable/Packaging/IPackageSearchService.cs
+++ src/Workspaces/Core/Portable/Packaging/IPackageSearchService.cs
@@ -25,26 +25,52 @@ namespace Microsoft.CodeAnalysis.Packaging
             string source, string name, int arity, CancellationToken cancellationToken);
     }
 
+    internal interface IReferenceAssemblySearchService : IWorkspaceService
+    {
+        /// <summary>
+        /// Searches for reference assemblies that contain a type with the provided name and arity.
+        /// Note: Implementations are free to return the results they feel best for the
+        /// given data.  Specifically, they can do exact or fuzzy matching on the name.
+        /// They can use or ignore the arity depending on their capabilities. 
+        /// 
+        /// Implementations should return results in order from best to worst (from their
+        /// perspective).
+        /// </summary>
+        IEnumerable<ReferenceAssemblyWithTypeResult> FindReferenceAssembliesWithType(
+            string name, int arity, CancellationToken cancellationToken);
+    }
+
+    internal class ReferenceAssemblyWithTypeResult
+    {
+        public readonly IReadOnlyList<string> ContainingNamespaceNames;
+        public readonly string AssemblyName;
+        public readonly string TypeName;
+
+        public ReferenceAssemblyWithTypeResult(
+            string assemblyName,
+            string typeName,
+            IReadOnlyList<string> containingNamespaceNames)
+        {
+            AssemblyName = assemblyName;
+            TypeName = typeName;
+            ContainingNamespaceNames = containingNamespaceNames;
+        }
+    }
+
     internal class PackageWithTypeResult
     {
-        public readonly bool IsDesktopFramework;
         public readonly IReadOnlyList<string> ContainingNamespaceNames;
         public readonly string PackageName;
-        public readonly string AssemblyName;
         public readonly string TypeName;
         public readonly string Version;
 
         public PackageWithTypeResult(
-            bool isDesktopFramework,
             string packageName,
-            string assemblyName,
             string typeName, 
             string version,
             IReadOnlyList<string> containingNamespaceNames)
         {
-            IsDesktopFramework = isDesktopFramework;
             PackageName = packageName;
-            AssemblyName = assemblyName;
             TypeName = typeName;
             Version = string.IsNullOrWhiteSpace(version) ? null : version;
             ContainingNamespaceNames = containingNamespaceNames;
