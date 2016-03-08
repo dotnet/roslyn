f11f6efa9c59f537d15961414fd3f26d0f631c66
Heejae Chang <heejaechang@outlook.com>
2016-03-07 17:53:26 -0800

    Revert "Revert "Disable diagnostic service for projects that we know we don't have all information to provide good results.""

--- src/Workspaces/Core/Portable/Workspace/Solution/Solution.CompilationTracker.State.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/Solution.CompilationTracker.State.cs
@@ -39,10 +39,10 @@ namespace Microsoft.CodeAnalysis
                 public ValueSource<Compilation> Compilation { get; }
 
                 /// <summary>
-                /// Specifies if there are references that got dropped in the production of <see cref="FinalCompilation"/>. This can return
+                /// Specifies whether <see cref="FinalCompilation"/> and all compilations it depends on contain full information or not. This can return
                 /// null if the state isn't at the point where it would know, and it's necessary to transition to <see cref="FinalState"/> to figure that out.
                 /// </summary>
-                public virtual bool? HasCompleteReferences => null;
+                public virtual bool? HasSuccessfullyLoadedTransitively => null;
 
                 /// <summary>
                 /// The final compilation if available, otherwise an empty <see cref="ValueSource{Compilation}"/>.
@@ -135,19 +135,15 @@ namespace Microsoft.CodeAnalysis
             /// </summary>
             private sealed class FinalState : State
             {
-                private readonly bool _hasCompleteReferences;
+                private readonly bool _hasSuccessfullyLoadedTransitively;
 
-                public override ValueSource<Compilation> FinalCompilation
-                {
-                    get { return this.Compilation; }
-                }
+                public override bool? HasSuccessfullyLoadedTransitively => _hasSuccessfullyLoadedTransitively;
+                public override ValueSource<Compilation> FinalCompilation => this.Compilation;
 
-                public override bool? HasCompleteReferences => _hasCompleteReferences;
-
-                public FinalState(ValueSource<Compilation> finalCompilationSource, bool hasCompleteReferences)
+                public FinalState(ValueSource<Compilation> finalCompilationSource, bool hasSuccessfullyLoadedTransitively)
                     : base(finalCompilationSource, finalCompilationSource.GetValue().Clone().RemoveAllReferences())
                 {
-                    _hasCompleteReferences = hasCompleteReferences;
+                    _hasSuccessfullyLoadedTransitively = hasSuccessfullyLoadedTransitively;
                 }
             }
         }
