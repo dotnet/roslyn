ec5a2bf4696bd7de00c6f0624e9ff96cfc7aae22
Tomas Matousek <tomas.matousek@microsoft.com>
2016-03-07 19:50:20 -0800

    Merge fixup

--- src/Compilers/Core/Portable/PEWriter/MetadataWriter.PortablePdb.cs
+++ src/Compilers/Core/Portable/PEWriter/MetadataWriter.PortablePdb.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Cci
         }
 
         private readonly Dictionary<DebugSourceDocument, DocumentHandle> _documentIndex = new Dictionary<DebugSourceDocument, DocumentHandle>();
-        private readonly Dictionary<IImportScope, ImportScopeHandle> _scopeIndex = new Dictionary<IImportScope, ImportScopeHandle>();
+        private readonly Dictionary<IImportScope, ImportScopeHandle> _scopeIndex = new Dictionary<IImportScope, ImportScopeHandle>(ImportScopeEqualityComparer.Instance);
 
         private void SerializeMethodDebugInfo(IMethodBody bodyOpt, int methodRid, StandaloneSignatureHandle localSignatureHandleOpt, ref LocalVariableHandle lastLocalVariableHandle, ref LocalConstantHandle lastLocalConstantHandle)
         {
