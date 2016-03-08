15a513c8acebd01f1d807debc04f308bdb16e073
Ty Overby <ty@pre-alpha.com>
2016-03-08 15:31:58 -0800

    Only run nuget upload script from roslyn-internal

--- src/Tools/UploadNugetZip/CreateAndUpload.ps1
+++ src/Tools/UploadNugetZip/CreateAndUpload.ps1
@@ -1,10 +1,18 @@
 $ScriptDir = $PSScriptRoot
 $AzCopyLoc = Join-Path ${env:ProgramFiles(x86)} "Microsoft SDKs\Azure\AzCopy\AzCopy.exe"
+$RestoreScriptLoc = Join-Path $ScriptDir \..\..\..\..\Restore.cmd
+$NugetExeLoc = Join-Path $ScriptDir \..\..\..\nuget.exe
 
 If (-Not (Test-Path $AzCopyLoc)) {
     echo "Azure Copy could not be found.  Download and install here:"
     echo "http://aka.ms/downloadazcopy"
     exit 1
+}
+
+if (-Not (test-path $RestoreScriptLoc)) {
+    echo "You must run this script from inside of roslyn-internal"
+    echo "looking for $RestoreScriptLoc"
+    exit 2
 }
 
 $Branch = Read-Host 'which branch is this for? [Master|Future]'
@@ -23,7 +31,10 @@ echo "=           Clearing nuget caches"
 echo "==============================================="
 echo ""
 
-& $ScriptDir\..\..\..\nuget.exe locals all -clear
+& $NugetExeLoc locals all -clear
+if ($error.count -eq 0) {
+    exit 3
+}
 
 echo ""
 echo "==============================================="
@@ -31,7 +42,10 @@ echo "=       Restoring nuget to fill cache"
 echo "==============================================="
 echo ""
 
-& $ScriptDir\..\..\..\Restore.cmd
+& $RestoreScriptLoc 
+if ($error.count -eq 0) {
+    exit 3
+}
 
 echo ""
 echo "==============================================="
@@ -41,6 +55,9 @@ echo ""
 
 Add-Type -Assembly "System.IO.Compression.FileSystem";
 [System.IO.Compression.ZipFile]::CreateFromDirectory("$HOME/.nuget", "$ScriptDir/$NugetZipName", "Fastest", $true);
+if ($error.count -eq 0) {
+    exit 4
+}
 
 echo "Done"
 
@@ -51,4 +68,3 @@ echo "==============================================="
 echo ""
 
 & $AzCopyLoc /Source:$ScriptDir /Dest:https://dotnetci.blob.core.windows.net/roslyn /DestKey:$AzureKey /Pattern:$NugetZipName
-
