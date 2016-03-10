8e7d219a53d259705a21502bdfb1e6a99219c1f5
CyrusNajmabadi <cyrusn@microsoft.com>
2016-03-10 15:38:03 -0800

    Add initial startup class.

---
+++ src/Workspaces/HubServices/SymbolSearch/SymbolSearchStartup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.VsHub.Server.ServiceModulesCommon;
+using Owin;
+
+namespace Microsoft.CodeAnalysis.HubServices.SymbolSearch
+{
+    internal class SymbolSearchStartup : ServiceStartupBase
+    {
+        protected override void BuildApplication(IAppBuilder app)
+        {
+            // Code provided by VsHub team.
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+
+            // TODO(cyrusn): Spin up thread to keep databases up to date.
+        }
+    }
+}
