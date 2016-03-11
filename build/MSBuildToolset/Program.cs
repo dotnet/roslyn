ad02d65a9ed16993c1792a848cb4865e41a3b6c4
Andy Gocke <angocke@microsoft.com>
2016-03-10 16:21:28 -0800

    Respond to PR comments

--- build/MSBuildToolset/Program.cs
+++ build/MSBuildToolset/Program.cs
@@ -1,10 +1,8 @@
-﻿
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 namespace DoNotUse
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-        }
+        public static int Main(string[] args) => 1;
     }
 }
