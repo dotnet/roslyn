32a2811a1382763d0ba55b50a17f328b1861be36
John Hamby <jhamby@microsoft.com>
2016-03-14 15:57:31 -0700

    Several interesting tests now pass.

--- src/Compilers/CSharp/Test/Emit/Emit/DynamicAnalysis/DynamicCodeCoverageTests.cs
+++ src/Compilers/CSharp/Test/Emit/Emit/DynamicAnalysis/DynamicCodeCoverageTests.cs
@@ -74,9 +74,16 @@ namespace Microsoft.CodeAnalysis.Runtime
     public class Instrumentation
     {
         private static bool[][] _payloads;
-        
+        private static System.Guid _mvid;
+
         public static void CreatePayload(System.Guid mvid, int methodToken, ref bool[] payload, int payloadLength)
         {
+            if (_mvid != mvid)
+            {
+                _payloads = null;
+                _mvid = mvid;
+            }
+
             if (_payloads == null)
             {
                 _payloads = new bool[100][];
@@ -155,7 +162,476 @@ True
 True
 True
 ";
+            string expectedTestMainIL = @"{
+  // Code size       16 (0x10)
+  .maxstack  1
+  .locals init (int? V_0)
+  IL_0000:  call       ""int? Program.N()""
+  IL_0005:  pop
+  IL_0006:  ldloca.s   V_0
+  IL_0008:  initobj    ""int?""
+  IL_000e:  ldloc.0
+  IL_000f:  ret
+}";
+            CompilationVerifier verifier = CompileAndVerify(source, emitOptions: EmitOptions.Default.WithEmitDynamicAnalysisData(true), expectedOutput: expectedOutput);
+            verifier.VerifyIL("Program.TestMain", expectedTestMainIL);
+        }
+
+        [Fact]
+        public void ManyStatementsCoverage()
+        {
+            string source = @"
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TestMain();
+    }
+
+    static void TestMain()
+    {
+        VariousStatements(2);
+    }
+
+    static void VariousStatements(int z)
+    {
+        int x = z + 10;
+        switch (z)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+
+        if (x > 10)
+        {
+            x++;
+        }
+        else
+        {
+            x--;
+        }
+
+        for (int y = 0; y < 50; y++)
+        {
+            if (y < 30)
+            {
+                x++;
+                continue;
+            }
+            else
+                break;
+        }
+
+        int[] a = new int[] { 1, 2, 3, 4 };
+        foreach (int i in a)
+        {
+            x++;
+        }
+
+        while (x < 100)
+        {
+            x++;
+        }
+
+        try
+        {
+            x++;
+            if (x > 10)
+            {
+                throw new System.Exception();
+            }
+            x++;
+        }
+        catch (System.Exception e)
+        {
+            x++;
+        }
+        finally
+        {
+            x++;
+        }
+
+        lock (new object())
+        {
+            ;
+        }
+
+        Console.WriteLine(x);
+        return;
+    }
+}
+
+namespace Microsoft.CodeAnalysis.Runtime
+{
+    public class Instrumentation
+    {
+        private static bool[][] _payloads;
+        private static System.Guid _mvid;
+
+        public static void CreatePayload(System.Guid mvid, int methodToken, ref bool[] payload, int payloadLength)
+        {
+            if (_mvid != mvid)
+            {
+                _payloads = null;
+                _mvid = mvid;
+            }
+
+            if (_payloads == null)
+            {
+                _payloads = new bool[100][];
+            }
+
+            if (System.Threading.Interlocked.CompareExchange(ref payload, new bool[payloadLength], null) == null)
+            {
+                int methodIndex = methodToken & 0xffffff;
+                _payloads[methodIndex] = payload;
+            }
+        }
+
+        public static void FlushPayload()
+        {
+            Console.WriteLine(""Flushing"");
+            if (_payloads == null)
+            {
+                return;
+            }
+            for (int i = 0; i < _payloads.Length; i++)
+            {
+                bool[] payload = _payloads[i];
+                if (payload != null)
+                {
+                    Console.WriteLine(i);
+                    for (int j = 0; j < payload.Length; j++)
+                    {
+                        Console.WriteLine(payload[j]);
+                        payload[j] = false;
+                    }
+                }
+            }
+        }
+    }
+}
+";
+            string expectedOutput = @"Flushing
+1
+True
+True
+103
+Flushing
+1
+False
+False
+2
+True
+True
+3
+True
+False
+True
+False
+False
+True
+True
+True
+False
+False
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+False
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+True
+";
+
             CompileAndVerify(source, emitOptions: EmitOptions.Default.WithEmitDynamicAnalysisData(true), expectedOutput: expectedOutput);
         }
+
+        [Fact]
+        public void LambdaCoverage()
+        {
+            string source = @"
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TestMain();
+    }
+
+    static void TestMain()
+    {
+        int y = 5;
+        Func<int, int> tester = (x) =>
+        {
+            while (x > 10)
+            {
+                return y;
+            }
+
+            return x;
+        };
+
+        y = 75;
+        if (tester(20) > 50)
+            Console.WriteLine(""OK"");
+        else
+            Console.WriteLine(""Bad"");        
+    }
+}
+
+namespace Microsoft.CodeAnalysis.Runtime
+{
+    public class Instrumentation
+    {
+        private static bool[][] _payloads;
+        private static System.Guid _mvid;
+
+        public static void CreatePayload(System.Guid mvid, int methodToken, ref bool[] payload, int payloadLength)
+        {
+            if (_mvid != mvid)
+            {
+                _payloads = null;
+                _mvid = mvid;
+            }
+
+            if (_payloads == null)
+            {
+                _payloads = new bool[100][];
+            }
+
+            if (System.Threading.Interlocked.CompareExchange(ref payload, new bool[payloadLength], null) == null)
+            {
+                int methodIndex = methodToken & 0xffffff;
+                _payloads[methodIndex] = payload;
+            }
+        }
+
+        public static void FlushPayload()
+        {
+            Console.WriteLine(""Flushing"");
+            if (_payloads == null)
+            {
+                return;
+            }
+            for (int i = 0; i < _payloads.Length; i++)
+            {
+                bool[] payload = _payloads[i];
+                if (payload != null)
+                {
+                    Console.WriteLine(i);
+                    for (int j = 0; j < payload.Length; j++)
+                    {
+                        Console.WriteLine(payload[j]);
+                        payload[j] = false;
+                    }
+                }
+            }
+        }
+    }
+}
+";
+            string expectedOutput = @"Flushing
+1
+True
+True
+OK
+Flushing
+1
+False
+False
+2
+True
+True
+True
+True
+False
+True
+True
+True
+True
+False
+True
+True
+";
+
+            CompileAndVerify(source, emitOptions: EmitOptions.Default.WithEmitDynamicAnalysisData(true), expectedOutput: expectedOutput);
+        }
+
+        [Fact]
+        public void AsyncCoverage()
+        {
+            string source = @"
+using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TestMain();
+    }
+
+    static void TestMain()
+    {
+        Console.WriteLine(Outer(""Goo"").Result);
+    }
+
+    async static Task<string> Outer(string s)
+    {
+        string s1 = await First(s);
+        string s2 = await Second(s);
+
+        return s1 + s2;
+    }
+
+    async static Task<string> First(string s)
+    {
+        string result = await Second(s) + ""XXX"";
+        if (result.Length > 2)
+            return result;
+        else
+            return ""Too short"";
+    }
+
+    async static Task<string> Second(string s)
+    {
+        string doubled;
+        if (s.Length > 2)
+            doubled = s + s;
+        else
+            doubled = ""HuhHuh"";
+        return await Task.Factory.StartNew(() => doubled);
+    }
+}
+
+namespace Microsoft.CodeAnalysis.Runtime
+{
+    public class Instrumentation
+    {
+        private static bool[][] _payloads;
+        private static System.Guid _mvid;
+
+        public static void CreatePayload(System.Guid mvid, int methodToken, ref bool[] payload, int payloadLength)
+        {
+            if (_mvid != mvid)
+            {
+                _payloads = null;
+                _mvid = mvid;
+            }
+
+            if (_payloads == null)
+            {
+                _payloads = new bool[100][];
+            }
+
+            if (System.Threading.Interlocked.CompareExchange(ref payload, new bool[payloadLength], null) == null)
+            {
+                int methodIndex = methodToken & 0xffffff;
+                _payloads[methodIndex] = payload;
+            }
+        }
+
+        public static void FlushPayload()
+        {
+            Console.WriteLine(""Flushing"");
+            if (_payloads == null)
+            {
+                return;
+            }
+            for (int i = 0; i < _payloads.Length; i++)
+            {
+                bool[] payload = _payloads[i];
+                if (payload != null)
+                {
+                    Console.WriteLine(i);
+                    for (int j = 0; j < payload.Length; j++)
+                    {
+                        Console.WriteLine(payload[j]);
+                        payload[j] = false;
+                    }
+                }
+            }
+        }
+    }
+}
+";
+            string expectedOutput = @"Flushing
+1
+True
+True
+GooGooXXXGooGoo
+Flushing
+1
+False
+False
+2
+True
+True
+3
+True
+True
+True
+True
+4
+True
+True
+False
+True
+True
+5
+True
+True
+False
+True
+True
+True
+";
+
+            CompileAndVerify(source, emitOptions: EmitOptions.Default.WithEmitDynamicAnalysisData(true), expectedOutput: expectedOutput);
+        }
+
+        private CompilationVerifier CompileAndVerify(string source, EmitOptions emitOptions, string expectedOutput = null)
+        {
+            return base.CompileAndVerify(source, expectedOutput: expectedOutput, additionalRefs: s_asyncRefs, emitOptions: emitOptions);
+        }
+
+        private static readonly MetadataReference[] s_asyncRefs = new[] { MscorlibRef_v4_0_30316_17626, SystemRef_v4_0_30319_17929, SystemCoreRef_v4_0_30319_17929 };
     }
 }
