8c29cdfc2a60564466f85bdc189f6c108a673392
Jonathon Marolf <jmarolf@microsoft.com>
2016-03-14 15:59:48 -0700

    adding mock completion service and verifying that we are adding things to the list

--- src/EditorFeatures/Test2/IntelliSense/CompletionServiceTests.vb
+++ src/EditorFeatures/Test2/IntelliSense/CompletionServiceTests.vb
@@ -1,6 +1,7 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 Imports System.Globalization
+Imports System.Threading
 Imports System.Threading.Tasks
 Imports Microsoft.CodeAnalysis.Completion
 Imports Microsoft.CodeAnalysis.CSharp.Completion
@@ -8,6 +9,7 @@ Imports Microsoft.CodeAnalysis.CSharp.Completion.Providers
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces
 Imports Microsoft.CodeAnalysis.Options
 Imports Microsoft.CodeAnalysis.Text
+Imports Moq
 
 Namespace Microsoft.CodeAnalysis.Editor.UnitTests.IntelliSense
     Public Class CompletionServiceTests
@@ -23,28 +25,32 @@ Namespace Microsoft.CodeAnalysis.Editor.UnitTests.IntelliSense
             </Workspace>
             Using workspace = Await TestWorkspace.CreateAsync(workspaceDefinition)
                 Dim document = workspace.CurrentSolution.Projects.First.Documents.First
-                Dim completionService As AbstractCompletionService = New CSharpCompletionService()
-                Dim list = Await completionService.GetCompletionListAsync(
+                Dim mockCompletionService = New Mock(Of AbstractCompletionService)
+                mockCompletionService.Setup(Function(service) service.GetDefaultCompletionProviders()).Returns({New TestCompletionProvider()})
+
+                Dim list = Await mockCompletionService.Object.GetCompletionListAsync(
                     document:=document,
                     position:=0,
                     triggerInfo:=CompletionTriggerInfo.CreateInvokeCompletionTriggerInfo(),
                     options:=Nothing,
-                    providers:={New TestCompletionProvider()},
+                    providers:=Nothing,
                     cancellationToken:=Nothing)
-                Assert.Null(list)
+                Assert.NotNull(list)
+                Assert.NotEmpty(list.Items)
+                Assert.True(list.Items.Length = 1, "Completion list contained more than one item")
+                Assert.Equal("Completion Item From Test Completion Provider", list.Items.First.DisplayText)
             End Using
         End Function
 
         Private Class TestCompletionProvider
             Inherits CompletionListProvider
-            Public Sub New()
-            End Sub
 
             Public Overrides Function IsTriggerCharacter(text As SourceText, characterPosition As Int32, options As OptionSet) As [Boolean]
                 Return True
             End Function
 
             Public Overrides Function ProduceCompletionListAsync(context As CompletionListContext) As Task
+                context.AddItem(New CompletionItem(Me, "Completion Item From Test Completion Provider", filterSpan:=New TextSpan(), descriptionFactory:=Nothing, glyph:=Nothing))
                 Return Task.CompletedTask
             End Function
         End Class
