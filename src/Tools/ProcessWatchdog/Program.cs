792d70953d0713ed53707a88c78f3faf06c66efe
Larry Golding <lgolding@microsoft.com>
2016-03-14 13:59:02 -0700

    ProcessWatchdog: Add CommandLineParser NuGet package

--- src/Tools/ProcessWatchdog/Program.cs
+++ src/Tools/ProcessWatchdog/Program.cs
@@ -5,14 +5,39 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
+using CommandLine;
+
 
 namespace ProcessWatchdog
 {
     internal sealed class Program
     {
+        private Options _options;
+
+        public Program(Options options)
+        {
+            _options = options;
+        }
+
+        private int Run()
+        {
+            return 0;
+        }
+
         private static void Main(string[] args)
         {
             Banner();
+
+            Parser.Default.ParseArguments<Options>(args)
+                .MapResult(
+                    options => Run(options),
+                    err => 1);
+        }
+
+        private  static int Run(Options options)
+        {
+            var program = new Program(options);
+            return program.Run();
         }
 
         private static void Banner()
