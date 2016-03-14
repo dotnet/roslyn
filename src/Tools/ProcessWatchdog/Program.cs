3941fade6ce0edffe1460b677e3be7c0f76aefd4
Larry Golding <lgolding@microsoft.com>
2016-03-14 13:36:54 -0700

    ProcessWatchdog: Banner

--- src/Tools/ProcessWatchdog/Program.cs
+++ src/Tools/ProcessWatchdog/Program.cs
@@ -1,11 +1,33 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
 namespace ProcessWatchdog
 {
     internal sealed class Program
     {
         private static void Main(string[] args)
         {
+            Banner();
+        }
+
+        private static void Banner()
+        {
+            Assembly entryAssembly = Assembly.GetEntryAssembly();
+            IEnumerable<Attribute> attributes = entryAssembly.GetCustomAttributes();
+
+            string version = entryAssembly.GetName().Version.ToString();
+            Console.WriteLine(string.Format(CultureInfo.CurrentCulture, Resources.Banner, Resources.ApplicationName, version));
+
+            var copyrightAttribute = attributes.Single(a => a is AssemblyCopyrightAttribute) as AssemblyCopyrightAttribute;
+            string copyright = copyrightAttribute.Copyright;
+
+            Console.WriteLine(copyright);
+            Console.WriteLine();
         }
     }
 }
