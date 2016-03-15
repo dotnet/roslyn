8af69fd598e029177f1947a9aea1b800f736243c
Jared Parsons <jaredpar@microsoft.com>
2016-03-15 16:11:22 -0700

    Refactor the script

--- build/scripts/test-determinism.ps1
+++ build/scripts/test-determinism.ps1
@@ -1,18 +1,32 @@
-param ([string]$buildDir = $(throw "Need a directory containing a compiler build to test with"))
+param ([string]$script:buildDir = $(throw "Need a directory containing a compiler build to test with"))
+set-strictmode -version 2.0
 $ErrorActionPreference="Stop"
 
-try
-{
-    if (-not ([IO.Path]::IsPathRooted($buildDir))) {
-        write-error "The build path must be absolute"
-        exit 1
-    }
+### Variables available to the entire script.
 
-    $rootDir = resolve-path (split-path -parent (split-path -parent $PSScriptRoot))
+# List of binary names that should be skipped because they have a known issue that
+# makes them non-deterministic.  
+$script:skipList = @(
+
+    # https://github.com/dotnet/roslyn/issues/8739
+    "Microsoft.VisualStudio.ProjectSystem.Managed.dll"
+)
+
+# Holds the determinism data checked on every build.
+$script:dataMap = @{}
+
+# The set of errors that we encountered during 
+
+function Run-Build()
+{
+    param ( [string]$rootDir = $(throw "Need a root directory to build"),
+            [bool]$buildMap = $(throw "Are we building the map"))
+            
     $sln = join-path $rootDir "Roslyn.sln"
     $debugDir = join-path $rootDir "Binaries\Debug"
     $errorDir = join-path $rootDir "Binaries\Determinism"
     $errorList = @()
+    $allGood = $true
 
     # Create directories that may or may not exist to make the script execution below 
     # clean in either case.
@@ -22,89 +36,73 @@ try
 
     pushd $rootDir
 
-    # List of binary names that should be skipped because they have a known issue that
-    # makes them non-deterministic.  
-    $skipList = @(
+    # Clean out the previous run
+    write-host "Cleaning the Binaries"
+    rm -re -fo "Binaries\Debug" 
+    rm -re -fo "Binaries\Obj"
+    & msbuild /nologo /v:m /nodeReuse:false /t:clean $sln
 
-        # https://github.com/dotnet/roslyn/issues/8739
-        "Microsoft.VisualStudio.ProjectSystem.Managed.dll"
-    )
+    write-host "Building the Solution"
+    & msbuild /nologo /v:m /nodeReuse:false /m /p:DebugDeterminism=true /p:BootstrapBuildPath=$script:buildDir /p:Features=debug-determinism /p:UseRoslynAnalyzers=false $sln
 
-    $allGood = $true
-    $map = @{}
-    $i = 0;
-    while ($i -lt 3 -and $allGood) {
+    popd
 
-        # Clean out the previous run
-        write-host "Cleaning the Binaries"
-        rm -re -fo "Binaries\Debug" 
-        rm -re -fo "Binaries\Obj"
-        & msbuild /nologo /v:m /nodeReuse:false /t:clean $sln
+    pushd $debugDir
 
-        write-host "Building the Solution"
-        & msbuild /nologo /v:m /nodeReuse:false /m /p:DebugDeterminism=true /p:BootstrapBuildPath=$buildDir /p:Features=debug-determinism /p:UseRoslynAnalyzers=false $sln
+    write-host "Testing the binaries"
+    foreach ($dll in gci -re -in *.dll,*.exe) {
+        $dllFullName = $dll.FullName
+        $dllName = split-path -leaf $dllFullName
+        $dllHash = (get-filehash $dll -algorithm MD5).Hash
+        $keyFullName = $dllFullName + ".key"
+        $keyName = split-path -leaf $keyFullName
 
-        pushd $debugDir
+        # Do not process binaries that have been explicitly skipped or do not have a key
+        # file.  The lack of a key file means it's a binary that wasn't specifically 
+        # built for that directory (dependency).  Only need to check the binaries we are
+        # building. 
+        if ($script:skipList.Contains($dllName) -or -not (test-path $keyFullName)) {
+            continue;
+        }
 
-        write-host "Testing the binaries"
-        foreach ($dll in gci -re -in *.dll,*.exe) {
-            $dllFullName = $dll.FullName
-            $dllName = split-path -leaf $dllFullName
-            $dllHash = (get-filehash $dll -algorithm MD5).Hash
-            $keyFullName = $dllFullName + ".key"
-            $keyName = split-path -leaf $keyFullName
-
-            # Do not process binaries that have been explicitly skipped or do not have a key
-            # file.  The lack of a key file means it's a binary that wasn't specifically 
-            # built for that directory (dependency).  Only need to check the binaries we are
-            # building. 
-            if ($skipList.Contains($dllName) -or -not (test-path $keyFullName)) {
-                continue;
-            }
-
-            if ($i -eq 0) {
-                write-host "`tRecording $dllName = $dllHash"
-                $data = @{}
-                $data["Hash"] = $dllHash
-                $data["Content"] = [IO.File]::ReadAllBytes($dllFullName)
-                $data["Key"] = [IO.File]::ReadAllBytes($dllFullName + ".key")
-                $map[$dllFullName] = $data
+        if ($buildMap) {
+            write-host "`tRecording $dllName = $dllHash"
+            $data = @{}
+            $data["Hash"] = $dllHash
+            $data["Content"] = [IO.File]::ReadAllBytes($dllFullName)
+            $data["Key"] = [IO.File]::ReadAllBytes($dllFullName + ".key")
+            $script:dataMap[$dllFullName] = $data
+        }
+        else {
+            $data = $script:dataMap[$dllFullName]
+            $oldHash = $data.Hash
+            if ($oldHash -eq $dllHash) {
+                write-host "`tVerified $dllName"
             }
             else {
-                $data = $map[$dllFullName]
-                $oldHash = $data.Hash
-                if ($oldHash -eq $dllHash) {
-                    write-host "`tVerified $dllName"
-                }
-                else {
-                    write-host "`tERROR! $dllName"
-                    $allGood = $false
-                    $errorList += $dllName
+                write-host "`tERROR! $dllName"
+                $allGood = $false
+                $errorList += $dllName
 
-                    # Save out the original and baseline so Jenkins will archive them for investigation
-                    pushd $errorDir
-                    [IO.File]::WriteAllBytes((join-path $errorDir ($dllName + ".original")), $data.Content)
-                    [IO.File]::WriteAllBytes((join-path $errorDir ($keyName + ".original")), $data.Key)
-                    cp $dllFullName ($dllName + ".baseline")
-                    cp $keyFullName ($keyName + ".baseline")
-                    popd
-                }
+                # Save out the original and baseline so Jenkins will archive them for investigation
+                pushd $errorDir
+                [IO.File]::WriteAllBytes((join-path $errorDir ($dllName + ".original")), $data.Content)
+                [IO.File]::WriteAllBytes((join-path $errorDir ($keyName + ".original")), $data.Key)
+                cp $dllFullName ($dllName + ".baseline")
+                cp $keyFullName ($keyName + ".baseline")
+                popd
             }
         }
-
-        popd
-
-        # Sanity check to ensure we didn't return a false positive because we failed
-        # to examine any binaries.
-        if ($map.Count -lt 10) {
-            write-host "Found no binaries to process"
-            $allGood = $false
-        }
-
-        $i++
     }
 
     popd
+
+    # Sanity check to ensure we didn't return a false positive because we failed
+    # to examine any binaries.
+    if ($script:dataMap.Count -lt 10) {
+        write-host "Found no binaries to process"
+        $allGood = $false
+    }
 
     if (-not $allGood) {
         write-host "Determinism failed for the following binaries:"
@@ -120,6 +118,19 @@ try
         write-host "Please send $zipFile to compiler team for analysis"
         exit 1
     }
+}
+
+try
+{
+    if (-not ([IO.Path]::IsPathRooted($script:buildDir))) {
+        write-error "The build path must be absolute"
+        exit 1
+    }
+
+    $rootDir = resolve-path (split-path -parent (split-path -parent $PSScriptRoot))
+    Run-Build -rootDir $rootDir -buildMap $true
+    Run-Build -rootDir $rootDir -buildMap $false
+    Run-Build -rootDir $rootDir -buildMap $false
 
     exit 0
 }
