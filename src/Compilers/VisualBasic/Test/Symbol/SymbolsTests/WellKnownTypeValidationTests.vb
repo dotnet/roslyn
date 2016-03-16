0aef22873f51c74a8a667aca93d3515a349fd500
John Hamby <jhamby@microsoft.com>
2016-03-16 11:52:53 -0700

    Another test fix.

--- src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/WellKnownTypeValidationTests.vb
+++ src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/WellKnownTypeValidationTests.vb
@@ -532,6 +532,9 @@ End Namespace
                     Case WellKnownType.System_FormattableString, WellKnownType.System_Runtime_CompilerServices_FormattableStringFactory
                         ' Not available on all platforms.
                         Continue For
+                    Case WellKnownType.Microsoft_CodeAnalysis_Runtime_Instrumentation
+                        ' Not always available.
+                        Continue For
                 End Select
 
                 Dim symbol = comp.GetWellKnownType(wkt)
@@ -645,6 +648,9 @@ End Namespace
                         Continue For
                     Case WellKnownMember.System_Array__Empty
                         ' Not available yet, but will be in upcoming release.
+                        Continue For
+                    Case WellKnownMember.Microsoft_CodeAnalysis_Runtime_Instrumentation__CreatePayload, WellKnownMember.Microsoft_CodeAnalysis_Runtime_Instrumentation__FlushPayload
+                        ' Not always available.
                         Continue For
                 End Select
 
