83a15415931738a630b0ae4de8b0cde362b6649c
Tanner Gooding <tannergooding@users.noreply.github.com>
2016-03-16 10:34:55 -0700

    Adding basic integration support for getting/setting the editor window contents.

--- src/VisualStudio/TestUtilities/Remoting/RemotingHelper.cs
+++ src/VisualStudio/TestUtilities/Remoting/RemotingHelper.cs
@@ -1,16 +1,23 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Threading;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.LanguageServices.CSharp.Interactive;
-using Microsoft.VisualStudio.LanguageServices.Interactive;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
 
 namespace Roslyn.VisualStudio.Test.Utilities.Remoting
 {
     internal static class RemotingHelper
     {
+        private static Guid IWpfTextViewId = new Guid("8C40265E-9FDB-4F54-A0FD-EBB72B7D0476");
+
         public static InteractiveWindowWrapper CreateCSharpInteractiveWindowWrapper()
         {
             var componentModel = (IComponentModel)(ServiceProvider.GlobalProvider.GetService(typeof(SComponentModel)));
@@ -19,7 +26,57 @@ namespace Roslyn.VisualStudio.Test.Utilities.Remoting
             return new InteractiveWindowWrapper(vsInteractiveWindow.InteractiveWindow);
         }
 
+        public static string GetActiveTextViewContents()
+        {
+            var textViewHost = GetActiveTextViewHost();
+            return ExecuteOnUIThread(textViewHost.TextView.TextSnapshot.GetText);
+        }
+
+        public static void SetActiveTextViewContents(string text)
+        {
+            var textViewHost = GetActiveTextViewHost();
+            ExecuteOnUIThread(() => {
+                var textView = textViewHost.TextView;
+                var textSnapshot = textView.TextSnapshot;
+                var replacementSpan = new SnapshotSpan(textSnapshot, 0, textSnapshot.Length);
+                textView.TextBuffer.Replace(replacementSpan, text);
+            });
+        }
+
+        private static void ExecuteOnUIThread(Action action)
+            => Application.Current.Dispatcher.Invoke(action);
+
         private static T ExecuteOnUIThread<T>(Func<T> action)
             => Application.Current.Dispatcher.Invoke(action);
+
+        private static IWpfTextViewHost GetActiveTextViewHost()
+        {
+            WaitForApplicationIdle();
+
+            var vsTextManager = (IVsTextManager)(ServiceProvider.GlobalProvider.GetService(typeof(SVsTextManager)));
+
+            IVsTextView vsTextView = null;
+            var hresult = vsTextManager.GetActiveView(fMustHaveFocus: 1, pBuffer: null, ppView: out vsTextView);
+
+            if (hresult != VSConstants.S_OK)
+            {
+                throw Marshal.GetExceptionForHR(hresult);
+            }
+
+            var vsUserData = (IVsUserData)(vsTextView);
+
+            object wpfTextViewHost = null;
+            vsUserData.GetData(ref IWpfTextViewId, out wpfTextViewHost);
+
+            if (hresult != VSConstants.S_OK)
+            {
+                throw Marshal.GetExceptionForHR(hresult);
+            }
+
+            return (IWpfTextViewHost)(wpfTextViewHost);
+        }
+
+        private static void WaitForApplicationIdle()
+            => Application.Current.Dispatcher.Invoke(() => { }, DispatcherPriority.ApplicationIdle);
     }
 }
---
+++ src/VisualStudio/TestUtilities/Window/EditorWindow.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Roslyn.VisualStudio.Test.Utilities.Remoting;
+
+namespace Roslyn.VisualStudio.Test.Utilities
+{
+    public class EditorWindow
+    {
+        private IntegrationHost _host;
+
+        internal EditorWindow(IntegrationHost host)
+        {
+            _host = host;
+        }
+
+        public string Text
+        {
+            get
+            {
+                return RemotingHelper.GetActiveTextViewContents();
+            }
+
+            set
+            {
+                RemotingHelper.SetActiveTextViewContents(value);
+            }
+        }
+    }
+}
