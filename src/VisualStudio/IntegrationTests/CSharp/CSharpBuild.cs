fdef0a7d93a52bc7eb75adac3436897c6a1c1d8f
Tanner Gooding <tannergooding@users.noreply.github.com>
2016-03-16 10:48:13 -0700

    Adding the start of the CSharp and VisualBasic Build integration tests.

---
+++ src/VisualStudio/IntegrationTests/CSharp/CSharpBuild.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Roslyn.VisualStudio.Test.Utilities;
+using Xunit;
+
+namespace Roslyn.VisualStudio.Integration.UnitTests
+{
+    [Collection(nameof(SharedIntegrationHost))]
+    public class CSharpBuild
+    {
+        private IntegrationHost _host;
+
+        public CSharpBuild(IntegrationHost host)
+        {
+            _host = host;
+            _host.Initialize();
+
+            var solution = _host.SolutionExplorer.CreateSolution(nameof(CSharpBuild));
+            var project = solution.AddProject("TestProj", ProjectTemplate.ConsoleApplication, ProjectLanguage.CSharp);
+        }
+
+        [Fact]
+        public void BuildProject()
+        {
+            var editorText = @"using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(""Hello, World!"");
+    }
+}";
+
+            _host.EditorWindow.Text = editorText;
+            Assert.Equal(editorText, _host.EditorWindow.Text);
+
+            // TODO: Validate build works as expected
+        }
+    }
+}
