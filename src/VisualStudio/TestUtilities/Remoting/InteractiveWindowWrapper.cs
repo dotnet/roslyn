abf4cb6e3cb3dbb68e0202fd28f8f8d3fd7971c1
Tanner Gooding <tannergooding@users.noreply.github.com>
2016-03-16 10:03:39 -0700

    Adding integration support for testing the Interactive Window.

---
+++ src/VisualStudio/TestUtilities/Remoting/InteractiveWindowWrapper.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.VisualStudio.InteractiveWindow;
+
+namespace Roslyn.VisualStudio.Test.Utilities.Remoting
+{
+    internal class InteractiveWindowWrapper : MarshalByRefObject
+    {
+        private IInteractiveWindow _interactiveWindow;
+
+        public InteractiveWindowWrapper(IInteractiveWindow interactiveWindow)
+        {
+            _interactiveWindow = interactiveWindow;
+        }
+
+        public string CurrentSnapshotText
+            => _interactiveWindow.TextView.TextBuffer.CurrentSnapshot.GetText();
+
+        public bool IsInitializing
+            => _interactiveWindow.IsInitializing;
+
+        public void Submit(string text)
+            => _interactiveWindow.SubmitAsync(new[] { text }).ConfigureAwait(continueOnCapturedContext: false).GetAwaiter().GetResult();
+    }
+}
---
+++ src/VisualStudio/TestUtilities/Remoting/RemotingHelper.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Windows;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.LanguageServices.CSharp.Interactive;
+using Microsoft.VisualStudio.LanguageServices.Interactive;
+using Microsoft.VisualStudio.Shell;
+
+namespace Roslyn.VisualStudio.Test.Utilities.Remoting
+{
+    internal static class RemotingHelper
+    {
+        public static InteractiveWindowWrapper CreateCSharpInteractiveWindowWrapper()
+        {
+            var componentModel = (IComponentModel)(ServiceProvider.GlobalProvider.GetService(typeof(SComponentModel)));
+            var vsInteractiveWindowProvider = componentModel.GetService<CSharpVsInteractiveWindowProvider>();
+            var vsInteractiveWindow = ExecuteOnUIThread(() => vsInteractiveWindowProvider.Open(0, true));
+            return new InteractiveWindowWrapper(vsInteractiveWindow.InteractiveWindow);
+        }
+
+        private static T ExecuteOnUIThread<T>(Func<T> action)
+            => Application.Current.Dispatcher.Invoke(action);
+    }
+}
