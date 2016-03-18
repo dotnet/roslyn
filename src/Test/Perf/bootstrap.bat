6cb5d8aa9a8ff96fc5c45d347b3a0cd77ffca2f4
Kevin Halverson <kevinh@microsoft.com>
2016-03-18 09:35:42 -0700

    Fix relative paths...

--- src/Test/Perf/bootstrap.bat
+++ src/Test/Perf/bootstrap.bat
@@ -1,9 +1,9 @@
 @echo off
 
-call ..\..\..\Restore.cmd
+call %~dp0..\..\..\Restore.cmd
 
 set MSBuild=%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe
 if not exist "%MSBuild%" set MSBuild=%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe
-"%MSBuild%" ..\..\Interactive\csi\csi.csproj /p:Configuration=Release /p:OutDir="%~dp0infra\bin\\"
+"%MSBuild%" %~dp0..\..\Interactive\csi\csi.csproj /p:Configuration=Release /p:OutDir="%~dp0infra\bin\\"
 
 
