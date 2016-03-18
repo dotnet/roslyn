7fabbe9b0d7c469738a423825791527433cc4529
Kevin Halverson <kevinh@microsoft.com>
2016-03-18 09:39:27 -0700

    Add missing quotes...

--- src/Test/Perf/bootstrap.bat
+++ src/Test/Perf/bootstrap.bat
@@ -1,9 +1,9 @@
 @echo off
 
-call %~dp0..\..\..\Restore.cmd
+call "%~dp0..\..\..\Restore.cmd"
 
 set MSBuild=%ProgramFiles%\MSBuild\14.0\bin\msbuild.exe
 if not exist "%MSBuild%" set MSBuild=%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe
-"%MSBuild%" %~dp0..\..\Interactive\csi\csi.csproj /p:Configuration=Release /p:OutDir="%~dp0infra\bin\\"
+"%MSBuild%" "%~dp0..\..\Interactive\csi\csi.csproj" /p:Configuration=Release /p:OutDir="%~dp0infra\bin\\"
 
 
