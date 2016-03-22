e8c096482cf11123a9a72c624661505a467266a6
CyrusNajmabadi <cyrusn@microsoft.com>
2016-03-22 13:29:21 -0700

    Add support for highlighting matching portions of a completion item when filtering.
    Also add support for completion item filtering.

---
+++ src/EditorFeatures/Core/Implementation/IntelliSense/Completion/Temporary/Completion2.cs
@@ -0,0 +1,51 @@
+﻿#if false
+// Copyright (c) Microsoft Corporation
+// All rights reserved
+// REMOVE ONCE WE ACTUALLY REFERENCE THE REAL EDITOR DLLS.
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Text;
+
+namespace Microsoft.VisualStudio.Language.Intellisense
+{
+    /// <summary>
+    /// Represents a set of completions that supports a row of filter buttons displayed at the bottom on the intellisense popup.
+    /// </summary>
+    internal class CompletionSet2 : CompletionSet
+    {
+        private readonly IReadOnlyList<IIntellisenseFilter> _filters = null;
+        /// <summary>
+        /// Initializes a new instance of <see cref="CompletionSet2"/>.
+        /// </summary>
+        public CompletionSet2()
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of <see cref="CompletionSet2"/> with the specified name, text and filters.
+        /// </summary>
+        /// <param name="moniker">The unique, non-localized identifier for the completion set.</param>
+        /// <param name="displayName">The localized name of the completion set.</param>
+        /// <param name="applicableTo">The tracking span to which the completions apply.</param>
+        /// <param name="completions">The list of completions.</param>
+        /// <param name="completionBuilders">The list of completion builders.</param>
+        /// <param name="filters">The list of <see cref="IIntellisenseFilter"/>s that will be displayed at the bottom of the completion dialog.</param>
+        public CompletionSet2(string moniker,
+                              string displayName,
+                              ITrackingSpan applicableTo,
+                              IEnumerable<Completion> completions,
+                              IEnumerable<Completion> completionBuilders,
+                              IReadOnlyList<IIntellisenseFilter> filters)
+            : base(moniker, displayName, applicableTo, completions, completionBuilders)
+        {
+            _filters = filters;
+        }
+        public virtual IReadOnlyList<IIntellisenseFilter> Filters
+        {
+            get
+            {
+                return _filters;
+            }
+        }
+    }
+}
+#endif
--- src/Features/Core/Portable/FeaturesResources.Designer.cs
+++ src/Features/Core/Portable/FeaturesResources.Designer.cs
@@ -503,6 +503,15 @@ namespace Microsoft.CodeAnalysis {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Classes.
+        /// </summary>
+        internal static string Classes {
+            get {
+                return ResourceManager.GetString("Classes", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Computing fix all occurrences code fix....
         /// </summary>
         internal static string ComputingFixAllOccurrences {
@@ -517,6 +526,15 @@ namespace Microsoft.CodeAnalysis {
         internal static string Constant {
             get {
                 return ResourceManager.GetString("Constant", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Constants.
+        /// </summary>
+        internal static string Constants {
+            get {
+                return ResourceManager.GetString("Constants", resourceCulture);
             }
         }
         
@@ -571,6 +589,15 @@ namespace Microsoft.CodeAnalysis {
         internal static string Delegate {
             get {
                 return ResourceManager.GetString("Delegate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Delegates.
+        /// </summary>
+        internal static string Delegates {
+            get {
+                return ResourceManager.GetString("Delegates", resourceCulture);
             }
         }
         
@@ -683,6 +710,15 @@ namespace Microsoft.CodeAnalysis {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Enums.
+        /// </summary>
+        internal static string Enums {
+            get {
+                return ResourceManager.GetString("Enums", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to enum value.
         /// </summary>
         internal static string EnumValue {
@@ -728,11 +764,29 @@ namespace Microsoft.CodeAnalysis {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Events.
+        /// </summary>
+        internal static string Events {
+            get {
+                return ResourceManager.GetString("Events", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Exceptions:.
         /// </summary>
         internal static string Exceptions {
             get {
                 return ResourceManager.GetString("Exceptions", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Extension methods.
+        /// </summary>
+        internal static string Extension_methods {
+            get {
+                return ResourceManager.GetString("Extension_methods", resourceCulture);
             }
         }
         
@@ -809,6 +863,15 @@ namespace Microsoft.CodeAnalysis {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Fields.
+        /// </summary>
+        internal static string Fields {
+            get {
+                return ResourceManager.GetString("Fields", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Find and install latest version.
         /// </summary>
         internal static string Find_and_install_latest_version {
@@ -859,6 +922,15 @@ namespace Microsoft.CodeAnalysis {
         internal static string FixAllTitle_Solution {
             get {
                 return ResourceManager.GetString("FixAllTitle_Solution", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Fix Name Violation: {0}.
+        /// </summary>
+        internal static string FixNamingViolation {
+            get {
+                return ResourceManager.GetString("FixNamingViolation", resourceCulture);
             }
         }
         
@@ -1268,6 +1340,15 @@ namespace Microsoft.CodeAnalysis {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Interfaces.
+        /// </summary>
+        internal static string Interfaces {
+            get {
+                return ResourceManager.GetString("Interfaces", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Introduce constant for &apos;{0}&apos;.
         /// </summary>
         internal static string IntroduceConstantFor {
@@ -1403,6 +1484,24 @@ namespace Microsoft.CodeAnalysis {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Locals.
+        /// </summary>
+        internal static string Locals {
+            get {
+                return ResourceManager.GetString("Locals", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Locals and parameters.
+        /// </summary>
+        internal static string Locals_and_parameters {
+            get {
+                return ResourceManager.GetString("Locals_and_parameters", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to local variable.
         /// </summary>
         internal static string LocalVariable {
@@ -1444,6 +1543,15 @@ namespace Microsoft.CodeAnalysis {
         internal static string MethodReferencedImplicitly {
             get {
                 return ResourceManager.GetString("MethodReferencedImplicitly", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Methods.
+        /// </summary>
+        internal static string Methods {
+            get {
+                return ResourceManager.GetString("Methods", resourceCulture);
             }
         }
         
@@ -1574,6 +1682,15 @@ namespace Microsoft.CodeAnalysis {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Modules.
+        /// </summary>
+        internal static string Modules {
+            get {
+                return ResourceManager.GetString("Modules", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Moving &apos;{0}&apos; will prevent the debug session from continuing..
         /// </summary>
         internal static string MovingAWillPreventTheDebug {
@@ -1588,6 +1705,114 @@ namespace Microsoft.CodeAnalysis {
         internal static string Namespace {
             get {
                 return ResourceManager.GetString("Namespace", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Namespaces.
+        /// </summary>
+        internal static string Namespaces {
+            get {
+                return ResourceManager.GetString("Namespaces", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Naming Styles.
+        /// </summary>
+        internal static string NamingStylesDiagnosticTitle {
+            get {
+                return ResourceManager.GetString("NamingStylesDiagnosticTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The first word, &apos;{0}&apos;, must begin with a lower case character.
+        /// </summary>
+        internal static string NamingViolation_FirstWordMustBeginWithLowercase {
+            get {
+                return ResourceManager.GetString("NamingViolation_FirstWordMustBeginWithLowercase", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The first word, &apos;{0}&apos;, must begin with an upper case character.
+        /// </summary>
+        internal static string NamingViolation_FirstWordMustBeginWithUppercase {
+            get {
+                return ResourceManager.GetString("NamingViolation_FirstWordMustBeginWithUppercase", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Missing prefix: &apos;{0}&apos;.
+        /// </summary>
+        internal static string NamingViolation_MissingPrefix {
+            get {
+                return ResourceManager.GetString("NamingViolation_MissingPrefix", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Missing suffix: &apos;{0}&apos;.
+        /// </summary>
+        internal static string NamingViolation_MissingSuffix {
+            get {
+                return ResourceManager.GetString("NamingViolation_MissingSuffix", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to These non-leading words must begin with a lowercase letter: {0}.
+        /// </summary>
+        internal static string NamingViolation_NonLeadingWordsMustBeginWithLowercase {
+            get {
+                return ResourceManager.GetString("NamingViolation_NonLeadingWordsMustBeginWithLowercase", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to These non-leading words must begin with an upper case letter: {0}.
+        /// </summary>
+        internal static string NamingViolation_NonLeadingWordsMustBeginWithUppercase {
+            get {
+                return ResourceManager.GetString("NamingViolation_NonLeadingWordsMustBeginWithUppercase", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to These words cannot contain lower case characters: {0}.
+        /// </summary>
+        internal static string NamingViolation_WordsCannotContainLowercaseLetters {
+            get {
+                return ResourceManager.GetString("NamingViolation_WordsCannotContainLowercaseLetters", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to These words cannot contain upper case characters: {0}.
+        /// </summary>
+        internal static string NamingViolation_WordsCannotContainUppercaseLetters {
+            get {
+                return ResourceManager.GetString("NamingViolation_WordsCannotContainUppercaseLetters", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to These words must begin with upper case characters: {0}.
+        /// </summary>
+        internal static string NamingViolation_WordsMustBeginWithUppercase {
+            get {
+                return ResourceManager.GetString("NamingViolation_WordsMustBeginWithUppercase", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &apos;{0}&apos; naming violation - {1}.
+        /// </summary>
+        internal static string NamingViolationDescription {
+            get {
+                return ResourceManager.GetString("NamingViolationDescription", resourceCulture);
             }
         }
         
@@ -1723,6 +1948,15 @@ namespace Microsoft.CodeAnalysis {
         internal static string ProjectAvailability {
             get {
                 return ResourceManager.GetString("ProjectAvailability", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Properties.
+        /// </summary>
+        internal static string Properties {
+            get {
+                return ResourceManager.GetString("Properties", resourceCulture);
             }
         }
         
@@ -1921,6 +2155,15 @@ namespace Microsoft.CodeAnalysis {
         internal static string SpecifiedFileNotFoundFormat {
             get {
                 return ResourceManager.GetString("SpecifiedFileNotFoundFormat", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Structures.
+        /// </summary>
+        internal static string Structures {
+            get {
+                return ResourceManager.GetString("Structures", resourceCulture);
             }
         }
         
