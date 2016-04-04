4f8cb81034680696b331b5ead6431f2597504de7
Ravi Chande <rchande@microsoft.com>
2016-04-04 14:56:09 -0700

    Author a simple IDE perf test
    This test opens a C# solution, opens a file in the solution, and closes VS
    when the CPU is idle.

---
+++ src/Test/Perf/projects/vsopencsharp/TestSolution/test/ConsoleApplication24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
