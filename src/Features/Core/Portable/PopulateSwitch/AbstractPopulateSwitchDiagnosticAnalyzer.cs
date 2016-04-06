722875d86c05c3de35bdb890b480f700bf61d862
CyrusNajmabadi <cyrusn@microsoft.com>
2016-04-06 16:48:05 -0700

    Offer to add switch labels even if switching on an enum type.

--- src/Features/Core/Portable/PopulateSwitch/AbstractPopulateSwitchDiagnosticAnalyzer.cs
+++ src/Features/Core/Portable/PopulateSwitch/AbstractPopulateSwitchDiagnosticAnalyzer.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Threading;
+using System;
 
 namespace Microsoft.CodeAnalysis.Diagnostics.PopulateSwitch
 {
-    internal abstract class AbstractPopulateSwitchDiagnosticAnalyzerBase<TLanguageKindEnum, TSwitchBlockSyntax, TExpressionSyntax> : DiagnosticAnalyzer, IBuiltInAnalyzer 
+    internal abstract class AbstractPopulateSwitchDiagnosticAnalyzerBase<TLanguageKindEnum, TSwitchBlockSyntax, TExpressionSyntax> : DiagnosticAnalyzer, IBuiltInAnalyzer
         where TLanguageKindEnum : struct
         where TSwitchBlockSyntax : SyntaxNode
         where TExpressionSyntax : SyntaxNode
@@ -23,6 +24,8 @@ namespace Microsoft.CodeAnalysis.Diagnostics.PopulateSwitch
         #region Interface methods
 
         protected abstract ImmutableArray<TLanguageKindEnum> SyntaxKindsOfInterest { get; }
+        protected abstract TExpressionSyntax GetExpression(TSwitchBlockSyntax node);
+        protected abstract List<TExpressionSyntax> GetCaseLabels(TSwitchBlockSyntax switchBlock, out bool hasDefaultCase);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_descriptor);
 
@@ -34,64 +37,53 @@ namespace Microsoft.CodeAnalysis.Diagnostics.PopulateSwitch
         private void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
             var model = context.SemanticModel;
+            var tree = model.SyntaxTree;
             var switchBlock = (TSwitchBlockSyntax)context.Node;
 
-            if (SwitchIsFullyPopulated(model, switchBlock))
+            if (SwitchIsIncomplete(model, switchBlock) &&
+                !tree.OverlapsHiddenPosition(switchBlock.Span, context.CancellationToken))
             {
-                return;
+                var diagnostic = Diagnostic.Create(s_descriptor, tree.GetLocation((Text.TextSpan)switchBlock.Span));
+                context.ReportDiagnostic(diagnostic);
             }
-
-            var tree = model.SyntaxTree;
-            var span = switchBlock.Span;
-            if (tree.OverlapsHiddenPosition(span, context.CancellationToken))
-            {
-                return;
-            }
-
-            var diagnostic = Diagnostic.Create(s_descriptor, tree.GetLocation(span));
-            context.ReportDiagnostic(diagnostic);
         }
 
         #endregion
 
-        protected abstract TExpressionSyntax GetExpression(TSwitchBlockSyntax node);
-        protected abstract List<TExpressionSyntax> GetCaseLabels(TSwitchBlockSyntax switchBlock, out bool hasDefaultCase);
-
-        private bool SwitchIsFullyPopulated(SemanticModel model, TSwitchBlockSyntax node)
+        private bool SwitchIsIncomplete(SemanticModel model, TSwitchBlockSyntax node)
         {
-            var enumType = model.GetTypeInfo(GetExpression(node)).Type as INamedTypeSymbol;
-            if (enumType == null || enumType.TypeKind != TypeKind.Enum)
-            {
-                return true;
-            }
-
-            // ignore enums marked with Flags per spec: https://github.com/dotnet/roslyn/issues/6766#issuecomment-156878851
-            foreach (var attribute in enumType.GetAttributes())
-            {
-                var containingClass = attribute.AttributeClass.ToDisplayString();
-                if (containingClass == typeof(System.FlagsAttribute).FullName)
-                {
-                    return true;
-                }
-            }
-
             bool hasDefaultCase;
             var caseLabels = GetCaseLabels(node, out hasDefaultCase);
 
             if (!hasDefaultCase)
             {
+                // If the switch doesn't have a 'default' label, always provide the option to the
+                // user to add one.
+                return true;
+            }
+
+            // We know the switch has a 'default' label.  Now we need to determine if there are 
+            // any missing labels so that we can offer to generate them for the user.
+
+            var enumType = model.GetTypeInfo(GetExpression(node)).Type as INamedTypeSymbol;
+            if (enumType == null || enumType.TypeKind != TypeKind.Enum)
+            {
+                // If we can't determine the type of this switch, or we're switching over someting
+                // that sin't an enum, just consider this switch complete.  We can't add any cases
+                // here.
                 return false;
             }
 
-            var labelSymbols = new List<ISymbol>();
+            var labelSymbols = new HashSet<ISymbol>();
             foreach (var label in caseLabels)
             {
                 var symbol = model.GetSymbolInfo(label).Symbol;
                 if (symbol == null)
                 {
-                    // something is wrong with the label and the SemanticModel was unable to determine its symbol
-                    // abort analyzer
-                    return true;
+                    // something is wrong with the label and the SemanticModel was unable to 
+                    // determine its symbol.  Abort the analyzer by considering this switch
+                    // statement as complete.
+                    return false;
                 }
 
                 labelSymbols.Add(symbol);
@@ -108,11 +100,12 @@ namespace Microsoft.CodeAnalysis.Diagnostics.PopulateSwitch
                 
                 if (!labelSymbols.Contains(member))
                 {
-                    return false;
+                    // We're missing a label for this member.  The switch is not complete.
+                    return true;
                 }
             }
 
-            return true;
+            return false;
         }
 
         public DiagnosticAnalyzerCategory GetAnalyzerCategory() => DiagnosticAnalyzerCategory.SemanticSpanAnalysis;
