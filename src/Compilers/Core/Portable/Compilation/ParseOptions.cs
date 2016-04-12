13e76021a3b5f0048da3c93205598f35c7848e0d
Eriawan Kusumawardhono <eriawan@live.com>
2016-04-12 22:36:09 +0700

    add comment based on comment on PR #10304 by Jason Malinowski

--- src/Compilers/Core/Portable/Compilation/ParseOptions.cs
+++ src/Compilers/Core/Portable/Compilation/ParseOptions.cs
@@ -38,6 +38,9 @@ namespace Microsoft.CodeAnalysis
             return CommonWithKind(kind);
         }
 
+        // It was supposed to be a protected implementation detail. 
+        // The "pattern" we have for these is the public With* method is the only public callable one, 
+        // and that forwards to the protected Common* like all the other methods in the class (see PR #10304 comments by Jason Malinowski) 
         [EditorBrowsable(EditorBrowsableState.Never)]
         public abstract ParseOptions CommonWithKind(SourceCodeKind kind);
 
