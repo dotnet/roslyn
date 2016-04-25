8d4fbb6a21ce8edc6c0ce46c7a963c17a98b6fa3
Manish Vasani <mavasani@microsoft.com>
2016-04-25 05:03:34 -0700

    Fix VisualBasicSettingsManagerOptionSerializer.GetStorageKeyForOption so that we handle ServiceFeatureOnOffOptions.OptionName separately.

--- src/VisualStudio/VisualBasic/Impl/Options/VisualBasicSettingsManagerOptionSerializer.vb
+++ src/VisualStudio/VisualBasic/Impl/Options/VisualBasicSettingsManagerOptionSerializer.vb
@@ -117,10 +117,10 @@ Namespace Microsoft.VisualStudio.LanguageServices.VisualBasic.Options
                         Return SettingStorageRoot + "AutoRequiredMemberInsert"
                     Case FeatureOnOffOptions.FormatOnPaste.Name
                         Return Nothing
-                    Case ServiceFeatureOnOffOptions.ClosedFileDiagnostic.Name
-                        ' ClosedFileDiagnostics has been deprecated in favor of BasicClosedFileDiagnostics
-                        Return SettingStorageRoot + NameOf(AutomationObject.BasicClosedFileDiagnostics)
                 End Select
+            ElseIf key.Feature = ServiceFeatureOnOffOptions.OptionName Then
+                ' ClosedFileDiagnostics has been deprecated in favor of BasicClosedFileDiagnostics
+                Return SettingStorageRoot + NameOf(AutomationObject.BasicClosedFileDiagnostics)
             End If
 
             Return MyBase.GetStorageKeyForOption(key)
