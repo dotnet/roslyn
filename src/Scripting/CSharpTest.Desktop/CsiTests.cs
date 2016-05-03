a07e4afe838417da947c767aefb71b4148783d2f
Tomas Matousek <tomas.matousek@microsoft.com>
2016-05-03 16:00:45 -0700

    Temporarily disable test CurrentWorkingDirectory_Change

--- src/Scripting/CSharpTest.Desktop/CsiTests.cs
+++ src/Scripting/CSharpTest.Desktop/CsiTests.cs
@@ -31,7 +31,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Scripting.Hosting.UnitTests
             Assert.False(result.ContainsErrors);
         }
 
-        [Fact]
+        [Fact(Skip = "https://github.com/dotnet/roslyn/issues/7826")]
         public void CurrentWorkingDirectory_Change()
         {
             var dir = Temp.CreateDirectory();
