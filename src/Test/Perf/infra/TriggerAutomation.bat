1faaa0c32df04efd8cb20ebf5475cc38f14bcd1c
Balaji Soundrarajan <basoundr@microsoft.com>
2016-05-04 09:33:29 -0700

    Fix path and make TraceManager verbose

--- src/Test/Perf/infra/TriggerAutomation.bat
+++ src/Test/Perf/infra/TriggerAutomation.bat
@@ -6,9 +6,9 @@ call "%VS140COMNTOOLS%VsDevCmd.bat"
 pushd %~dp0
 
 :: Fetch the signed master release binaries and copy it to open binaries release folder
-csi fetch_build.csx Master %~dp0../../../../Binaries/Release
+csi fetch_build.csx Master %~dp0..\..\..\..\Binaries\Release
 
 :: Start Test Automation from the binaries directory
-csi %~dp0../../../../Binaries/Release/Perf/infra/automation.csx
+csi %~dp0..\..\..\..\Binaries\Release\Perf\infra\automation.csx
 
 popd
