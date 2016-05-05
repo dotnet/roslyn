102e39f159b339d44a02253029bad1d01c865a3f
AlekseyTs <alekseyt@microsoft.com>
2016-05-05 16:26:02 -0700

    Follow-up on merge from Tuples to Future.

--- src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenTupleTest.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenTupleTest.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
