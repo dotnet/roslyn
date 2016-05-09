4a23f1db2c4547a3862125b9a891d1d10058178c
Balaji Soundrarajan <basoundr@microsoft.com>
2016-05-09 16:28:48 -0700

    One TempDirectory For all tests
    There are 2 reasons.
    1. We are running into errors trying to unzip test solutions in the temp folder which is rooted deep inside the binaries folder
    2. When trying to run the tests again, we need to delete the temp directories of each directory separately because we could pick up csx files from the test files.
    
    We could solve the 2nd issue by cleaning up after the run. But the 1st issue still stands

--- src/Test/Perf/util/RelativeDirectory.cs
+++ src/Test/Perf/util/RelativeDirectory.cs
@@ -23,27 +23,13 @@ namespace Roslyn.Test.Performance.Utilities
 
         public string MyWorkingDirectory => _workingDir;
 
-
-        /// Returns the directory that you can put artifacts like
-        /// etl traces or compiled binaries
-        public string MyArtifactsDirectory
+        public string TempDirectory
         {
             get
             {
-                var path = Path.Combine(MyWorkingDirectory, "artifacts");
-                Directory.CreateDirectory(path);
-                return path;
-            }
-        }
-
-        public string MyTempDirectory
-        {
-            get
-            {
-                var workingDir = MyWorkingDirectory;
-                var path = Path.Combine(workingDir, "temp");
-                Directory.CreateDirectory(path);
-                return path;
+                var tempDirectory = Path.Combine(Environment.ExpandEnvironmentVariables("%SYSTEMDRIVE%")+@"\", "PerfTemp");
+                Directory.CreateDirectory(tempDirectory);
+                return tempDirectory;
             }
         }
 
@@ -111,7 +97,7 @@ namespace Roslyn.Test.Performance.Utilities
         public void DownloadProject(string name, int version, ILogger logger)
         {
             var zipFileName = $"{name}.{version}.zip";
-            var zipPath = Path.Combine(MyTempDirectory, zipFileName);
+            var zipPath = Path.Combine(TempDirectory, zipFileName);
             // If we've already downloaded the zip, assume that it
             // has been downloaded *and* extracted.
             if (File.Exists(zipPath))
@@ -121,7 +107,7 @@ namespace Roslyn.Test.Performance.Utilities
             }
 
             // Remove all .zip files that were downloaded before.
-            foreach (var path in Directory.EnumerateFiles(MyTempDirectory, $"{name}.*.zip"))
+            foreach (var path in Directory.EnumerateFiles(TempDirectory, $"{name}.*.zip"))
             {
                 logger.Log($"Removing old zip {path}");
                 File.Delete(path);
@@ -135,8 +121,8 @@ namespace Roslyn.Test.Performance.Utilities
             logger.Log($"Done Downloading");
 
             // Extract to temp directory
-            logger.Log($"Extracting {zipPath} to {MyTempDirectory}");
-            ZipFile.ExtractToDirectory(zipPath, MyTempDirectory);
+            logger.Log($"Extracting {zipPath} to {TempDirectory}");
+            ZipFile.ExtractToDirectory(zipPath, TempDirectory);
             logger.Log($"Done Extracting");
         }
     }
