4b0d3c4949170f6213c1d0ecce80931dc6981f1d
Jason Malinowski <jason.malinowski@microsoft.com>
2016-05-11 14:39:39 -0700

    Use the correct options when formatting new files
    We must now use document options rather than the system options.

--- src/VisualStudio/CSharp/Impl/LanguageService/CSharpEditorFactory.cs
+++ src/VisualStudio/CSharp/Impl/LanguageService/CSharpEditorFactory.cs
@@ -1,12 +1,7 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using System.Threading;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Formatting;
-using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.LanguageServices.Implementation;
 
 namespace Microsoft.VisualStudio.LanguageServices.CSharp.LanguageService
@@ -23,12 +18,6 @@ namespace Microsoft.VisualStudio.LanguageServices.CSharp.LanguageService
         protected override string ContentTypeName
         {
             get { return "CSharp"; }
-        }
-
-        protected override IList<TextChange> GetFormattedTextChanges(VisualStudioWorkspace workspace, string filePath, SourceText text, CancellationToken cancellationToken)
-        {
-            var root = SyntaxFactory.ParseSyntaxTree(text, path: filePath, cancellationToken: cancellationToken).GetRoot(cancellationToken);
-            return Formatter.GetFormattedTextChanges(root, workspace, cancellationToken: cancellationToken);
         }
     }
 }
--- src/VisualStudio/VisualBasic/Impl/LanguageService/VisualBasicEditorFactory.vb
+++ src/VisualStudio/VisualBasic/Impl/LanguageService/VisualBasicEditorFactory.vb
@@ -1,10 +1,6 @@
 ' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 Imports System.Runtime.InteropServices
-Imports System.Threading
-Imports Microsoft.CodeAnalysis.Formatting
-Imports Microsoft.CodeAnalysis.Text
-Imports Microsoft.CodeAnalysis.VisualBasic
 Imports Microsoft.VisualStudio.LanguageServices.Implementation
 
 Namespace Microsoft.VisualStudio.LanguageServices.VisualBasic
@@ -21,10 +17,5 @@ Namespace Microsoft.VisualStudio.LanguageServices.VisualBasic
                 Return "Basic"
             End Get
         End Property
-
-        Protected Overrides Function GetFormattedTextChanges(workspace As VisualStudioWorkspace, filePath As String, text As SourceText, cancellationToken As CancellationToken) As IList(Of TextChange)
-            Dim root = SyntaxFactory.ParseSyntaxTree(text, path:=filePath, cancellationToken:=cancellationToken).GetRoot(cancellationToken)
-            Return Formatter.GetFormattedTextChanges(root, workspace, cancellationToken:=cancellationToken)
-        End Function
     End Class
 End Namespace
