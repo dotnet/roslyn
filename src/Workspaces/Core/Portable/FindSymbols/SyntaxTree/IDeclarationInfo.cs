77c293e067a8b2263a51a541c7744a584746d60e
CyrusNajmabadi <cyrusn@microsoft.com>
2016-05-16 14:34:36 -0700

    Comment code.

--- src/Workspaces/Core/Portable/FindSymbols/SyntaxTree/IDeclarationInfo.cs
+++ src/Workspaces/Core/Portable/FindSymbols/SyntaxTree/IDeclarationInfo.cs
@@ -2,6 +2,10 @@
 
 namespace Microsoft.CodeAnalysis.FindSymbols
 {
+    /// <summary>
+    /// Information about all the declarations defined within a document.  Each declaration in the
+    /// document get a single item in <see cref="IDeclarationInfo.DeclaredSymbolInfos"/>.
+    /// </summary>
     internal interface IDeclarationInfo
     {
         IReadOnlyList<DeclaredSymbolInfo> DeclaredSymbolInfos { get; }
