a6e2bea1d9714f0defe26b34f365255384b78c00
CyrusNajmabadi <cyrusn@microsoft.com>
2016-05-16 14:30:58 -0700

    Remove unnecessary code.

--- src/Workspaces/Core/Portable/Workspace/Host/ILanguageService.cs
+++ src/Workspaces/Core/Portable/Workspace/Host/ILanguageService.cs
@@ -1,13 +1,11 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis.Text;
-
 namespace Microsoft.CodeAnalysis.Host
 {
     /// <summary>
     /// Empty interface just to mark language services.
     /// </summary>
-    public interface ILanguageService 
+    public interface ILanguageService
     {
     }
 }
