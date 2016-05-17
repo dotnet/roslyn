ad6a554573fe210a86566ce2ef34cc8eee8ced8f
Julien Couvreur <julien.couvreur@gmail.com>
2016-05-17 13:32:14 -0700

    More work on parsing

--- src/Compilers/CSharp/Test/Syntax/Parsing/DeconstructionTest.cs
+++ src/Compilers/CSharp/Test/Syntax/Parsing/DeconstructionTest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Xunit;
 
@@ -497,6 +498,98 @@ class C
             EOF();
         }
 
+        [Fact]
+        public void NestedDeconstructionAssignment()
+        {
+            // (E, ...) = is a deconstruction-assignment
+            var tree = UsingTree(@"
+class C
+{
+    void Foo()
+    {
+        (x, (y, z)) = foo;
+    }
+}", options: TestOptions.Regular.WithTuplesFeature());
+            N(SyntaxKind.CompilationUnit);
+            {
+                N(SyntaxKind.ClassDeclaration);
+                {
+                    N(SyntaxKind.ClassKeyword);
+                    N(SyntaxKind.IdentifierToken);
+                    N(SyntaxKind.OpenBraceToken);
+                    N(SyntaxKind.MethodDeclaration);
+                    {
+                        N(SyntaxKind.PredefinedType);
+                        {
+                            N(SyntaxKind.VoidKeyword);
+                        }
+                        N(SyntaxKind.IdentifierToken);
+                        N(SyntaxKind.ParameterList);
+                        {
+                            N(SyntaxKind.OpenParenToken);
+                            N(SyntaxKind.CloseParenToken);
+                        }
+                        N(SyntaxKind.Block);
+                        {
+                            N(SyntaxKind.OpenBraceToken);
+                            N(SyntaxKind.ExpressionStatement);
+                            {
+                                N(SyntaxKind.SimpleAssignmentExpression);
+                                {
+                                    N(SyntaxKind.TupleExpression);
+                                    {
+                                        N(SyntaxKind.OpenParenToken);
+                                        N(SyntaxKind.Argument);
+                                        {
+                                            N(SyntaxKind.IdentifierName);
+                                            {
+                                                N(SyntaxKind.IdentifierToken);
+                                            }
+                                        }
+                                        N(SyntaxKind.CommaToken);
+                                        N(SyntaxKind.Argument);
+                                        {
+                                            N(SyntaxKind.TupleExpression);
+                                            {
+                                                N(SyntaxKind.OpenParenToken);
+                                                N(SyntaxKind.Argument);
+                                                {
+                                                    N(SyntaxKind.IdentifierName);
+                                                    {
+                                                        N(SyntaxKind.IdentifierToken);
+                                                    }
+                                                }
+                                                N(SyntaxKind.CommaToken);
+                                                N(SyntaxKind.Argument);
+                                                {
+                                                    N(SyntaxKind.IdentifierName);
+                                                    {
+                                                        N(SyntaxKind.IdentifierToken);
+                                                    }
+                                                }
+                                                N(SyntaxKind.CloseParenToken);
+                                            }
+                                        }
+                                        N(SyntaxKind.CloseParenToken);
+                                    }
+                                    N(SyntaxKind.EqualsToken);
+                                    N(SyntaxKind.IdentifierName);
+                                    {
+                                        N(SyntaxKind.IdentifierToken);
+                                    }
+                                }
+                                N(SyntaxKind.SemicolonToken);
+                            }
+                            N(SyntaxKind.CloseBraceToken);
+                        }
+                    }
+                    N(SyntaxKind.CloseBraceToken);
+                }
+                N(SyntaxKind.EndOfFileToken);
+            }
+            EOF();
+        }
+
         [Fact(Skip = "PROTOTYPE(tuples)")]
         public void DeconstructionDeclaration()
         {
@@ -510,6 +603,89 @@ class C
     }
 }", options: TestOptions.Regular.WithTuplesFeature());
             EOF();
+        }
+
+        [Fact]
+        public void Statement1()
+        {
+            var text = "(T, T)[] id;";
+            var statement = SyntaxFactory.ParseStatement(text, offset: 0, options: TestOptions.Regular.WithTuplesFeature());
+            Assert.False(statement.HasErrors);
+
+            Assert.Equal(SyntaxKind.LocalDeclarationStatement, statement.Kind());
+            var declaration = ((LocalDeclarationStatementSyntax)statement).Declaration;
+            Assert.Equal("(T, T)[]", declaration.Type.ToString());
+            Assert.Equal("id", declaration.Variables.ToString());
+        }
+
+        [Fact]
+        public void Statement2()
+        {
+            var text = "(x).ToString();";
+            var statement = SyntaxFactory.ParseStatement(text, offset: 0, options: TestOptions.Regular.WithTuplesFeature());
+            Assert.False(statement.HasErrors);
+
+            Assert.Equal(SyntaxKind.ExpressionStatement, statement.Kind());
+            var expression = ((ExpressionStatementSyntax)statement).Expression;
+            Assert.Equal(SyntaxKind.InvocationExpression, expression.Kind());
+        }
+
+        [Fact]
+        public void Statement3()
+        {
+            var text = "(x, x).ToString();";
+            var statement = SyntaxFactory.ParseStatement(text, offset: 0, options: TestOptions.Regular.WithTuplesFeature());
+            Assert.False(statement.HasErrors);
+
+            Assert.Equal(SyntaxKind.ExpressionStatement, statement.Kind());
+            var expression = ((ExpressionStatementSyntax)statement).Expression;
+            Assert.Equal(SyntaxKind.InvocationExpression, expression.Kind());
+        }
+
+        [Fact]
+        public void Statement4()
+        {
+            var text = "((x)).ToString();";
+            var statement = SyntaxFactory.ParseStatement(text, offset: 0, options: TestOptions.Regular.WithTuplesFeature());
+            Assert.False(statement.HasErrors);
+
+            Assert.Equal(SyntaxKind.ExpressionStatement, statement.Kind());
+            var expression = ((ExpressionStatementSyntax)statement).Expression;
+            Assert.Equal(SyntaxKind.InvocationExpression, expression.Kind());
+        }
+
+        [Fact]
+        public void Statement5()
+        {
+            var text = "((x, y) = M()).ToString();";
+            var statement = SyntaxFactory.ParseStatement(text, offset: 0, options: TestOptions.Regular.WithTuplesFeature());
+            Assert.False(statement.HasErrors);
+
+            Assert.Equal(SyntaxKind.ExpressionStatement, statement.Kind());
+            var expression = ((ExpressionStatementSyntax)statement).Expression;
+            Assert.Equal(SyntaxKind.InvocationExpression, expression.Kind());
+
+            var invocation = (InvocationExpressionSyntax)expression;
+            var lhs = (MemberAccessExpressionSyntax)invocation.Expression;
+            var lhsContent = (ParenthesizedExpressionSyntax)lhs.Expression;
+            Assert.Equal(SyntaxKind.SimpleAssignmentExpression, lhsContent.Expression.Kind());
+        }
+
+        [Fact]
+        public void Statement6()
+        {
+            var text = "(((x, y))z).Foo();";
+            var statement = SyntaxFactory.ParseStatement(text, offset: 0, options: TestOptions.Regular.WithTuplesFeature());
+            Assert.False(statement.HasErrors);
+
+            Assert.Equal(SyntaxKind.ExpressionStatement, statement.Kind());
+            var expression = ((ExpressionStatementSyntax)statement).Expression;
+            Assert.Equal(SyntaxKind.InvocationExpression, expression.Kind());
+
+            var invocation = (InvocationExpressionSyntax)expression;
+            var lhs = (MemberAccessExpressionSyntax)invocation.Expression;
+            var lhsContent = (ParenthesizedExpressionSyntax)lhs.Expression;
+            Assert.Equal(SyntaxKind.CastExpression, lhsContent.Expression.Kind());
         }
     }
 }
