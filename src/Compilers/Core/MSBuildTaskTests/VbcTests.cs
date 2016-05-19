a7ed576823746066a22ea661e5af255db2941cdd
Andy Gocke <angocke@microsoft.com>
2016-05-18 23:14:51 -0700

    Wire up /runtimemetadataversion to the build task (#11404)
    Fixes #5608

--- src/Compilers/Core/MSBuildTaskTests/VbcTests.cs
+++ src/Compilers/Core/MSBuildTaskTests/VbcTests.cs
@@ -74,6 +74,20 @@ namespace Microsoft.CodeAnalysis.BuildTasks.UnitTests
         }
 
         [Fact]
+        public void RuntimeMetadataVersionFlag()
+        {
+            var vbc = new Vbc();
+            vbc.Sources = MSBuildUtil.CreateTaskItems("test.vb");
+            vbc.RuntimeMetadataVersion = "v1234";
+            Assert.Equal("/optionstrict:custom /out:test.exe /runtimemetadataversion:v1234 test.vb", vbc.GenerateResponseFileContents());
+
+            vbc = new Vbc();
+            vbc.Sources = MSBuildUtil.CreateTaskItems("test.vb");
+            vbc.RuntimeMetadataVersion = null;
+            Assert.Equal("/optionstrict:custom /out:test.exe test.vb", vbc.GenerateResponseFileContents());
+        }
+
+        [Fact]
         public void TargetTypeDll()
         {
             var vbc = new Vbc();
