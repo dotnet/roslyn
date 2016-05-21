28776fdce6b118017e1f0728648d9f18d4f71d68
Tomáš Matoušek <tmat@users.noreply.github.com>
2016-05-20 18:27:58 -0700

    Fix reporting of EnC (and other) diagnostics (#11447)
    * Fix reporting of EnC diagnostics
    
    * Keep original overloads on DocumentDiagnosticAnalyzer for compat with TS and F#

--- src/Features/Core/Portable/Diagnostics/Analyzers/IDocumentDiagnosticAnalyzer.cs
+++ src/Features/Core/Portable/Diagnostics/Analyzers/IDocumentDiagnosticAnalyzer.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Diagnostics
 {
@@ -12,8 +14,33 @@ namespace Microsoft.CodeAnalysis.Diagnostics
     internal abstract class DocumentDiagnosticAnalyzer : DiagnosticAnalyzer
     {
         // REVIEW: why DocumentDiagnosticAnalyzer doesn't have span based analysis?
-        public abstract Task AnalyzeSyntaxAsync(Document document, Action<Diagnostic> addDiagnostic, CancellationToken cancellationToken);
-        public abstract Task AnalyzeSemanticsAsync(Document document, Action<Diagnostic> addDiagnostic, CancellationToken cancellationToken);
+        // TODO: Make abstract once TypeScript and F# move over to the overloads above
+        public async virtual Task<ImmutableArray<Diagnostic>> AnalyzeSyntaxAsync(Document document, CancellationToken cancellationToken)
+        {
+            var builder = ArrayBuilder<Diagnostic>.GetInstance();
+            await AnalyzeSyntaxAsync(document, builder.Add, cancellationToken).ConfigureAwait(false);
+            return builder.ToImmutableAndFree();
+        }
+
+        // TODO: Make abstract once TypeScript and F# move over to the overloads above
+        public async virtual Task<ImmutableArray<Diagnostic>> AnalyzeSemanticsAsync(Document document, CancellationToken cancellationToken)
+        {
+            var builder = ArrayBuilder<Diagnostic>.GetInstance();
+            await AnalyzeSemanticsAsync(document, builder.Add, cancellationToken).ConfigureAwait(false);
+            return builder.ToImmutableAndFree();
+        }
+
+        // TODO: Remove once TypeScript and F# move over to the overloads above
+        public virtual Task AnalyzeSyntaxAsync(Document document, Action<Diagnostic> addDiagnostic, CancellationToken cancellationToken)
+        {
+            throw ExceptionUtilities.Unreachable;
+        }
+
+        // TODO: Remove once TypeScript and F# move over to the overloads above
+        public virtual Task AnalyzeSemanticsAsync(Document document, Action<Diagnostic> addDiagnostic, CancellationToken cancellationToken)
+        {
+            throw ExceptionUtilities.Unreachable;
+        }
 
         /// <summary>
         /// it is not allowed one to implement both DocumentDiagnosticAnalzyer and DiagnosticAnalyzer
