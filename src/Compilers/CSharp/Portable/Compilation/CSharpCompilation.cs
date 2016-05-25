b8798c445e019f9a39c7be501998552fb58d7209
Charles Stoner <chucks@microsoft.com>
2016-05-25 12:42:52 -0700

    Order GlobalNamespace declarations to match Compilation.SyntaxTrees

--- src/Compilers/CSharp/Portable/Compilation/CSharpCompilation.cs
+++ src/Compilers/CSharp/Portable/Compilation/CSharpCompilation.cs
@@ -1867,6 +1867,14 @@ namespace Microsoft.CodeAnalysis.CSharp
             }
         }
 
+        internal MergedNamespaceDeclaration MergedRootDeclaration
+        {
+            get
+            {
+                return Declarations.GetMergedRoot(this);
+            }
+        }
+
         /// <summary>
         /// Gets the diagnostics produced during the parsing stage of a compilation. There are no diagnostics for declarations or accessor or
         /// method bodies, for example.
@@ -2836,7 +2844,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 throw new ArgumentException(CSharpResources.NoNoneSearchCriteria, nameof(filter));
             }
 
-            return this.Declarations.ContainsName(predicate, filter, cancellationToken);
+            return DeclarationTable.ContainsName(this.MergedRootDeclaration, predicate, filter, cancellationToken);
         }
 
         /// <summary>
@@ -2934,7 +2942,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 var result = new HashSet<ISymbol>();
                 var spine = new List<MergedNamespaceOrTypeDeclaration>();
 
-                AppendSymbolsWithName(spine, _compilation.Declarations.MergedRoot, predicate, filter, result, cancellationToken);
+                AppendSymbolsWithName(spine, _compilation.MergedRootDeclaration, predicate, filter, result, cancellationToken);
 
                 return result;
             }
