f4f5b1ee90474b97a7dff08df7d7c453660b7d20
Tomas Matousek <tomas.matousek@microsoft.com>
2016-05-26 18:45:52 -0700

    Handle IOException when reading method body during EnC

--- src/Compilers/Core/Portable/MetadataReader/PEModule.cs
+++ src/Compilers/Core/Portable/MetadataReader/PEModule.cs
@@ -5,6 +5,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Metadata;
@@ -147,6 +148,17 @@ namespace Microsoft.CodeAnalysis
             }
         }
 
+        /// <exception cref="IOException">
+        /// Error reading from the stream (only when prefetching data).
+        /// Only thrown first time the reader is accessed.
+        /// </exception>
+        /// <exception cref="BadImageFormatException">
+        /// The data read from stream have invalid format.
+        /// Only thrown first time the reader is accessed.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">
+        /// The underlying memory has been disposed.
+        /// </exception>
         internal MetadataReader MetadataReader
         {
             get
@@ -168,6 +180,8 @@ namespace Microsoft.CodeAnalysis
             }
         }
 
+        /// <exception cref="IOException">Error reading from the stream (only when prefetching data).</exception>
+        /// <exception cref="BadImageFormatException">The data read from stream have invalid format.</exception>
         private unsafe void InitializeMetadataReader()
         {
             MetadataReader newReader;
@@ -2949,6 +2963,7 @@ namespace Microsoft.CodeAnalysis
             get { return _peReaderOpt != null && _peReaderOpt.IsEntireImageAvailable; }
         }
 
+        /// <exception cref="IOException">Error reading from the stream (only when prefetching data).</exception>
         /// <exception cref="BadImageFormatException">Invalid metadata.</exception>
         internal MethodBodyBlock GetMethodBodyOrThrow(MethodDefinitionHandle methodHandle)
         {
