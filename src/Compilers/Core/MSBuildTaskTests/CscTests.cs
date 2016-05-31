01ea468266f2b82e8bddd4e617f4b35ac20d879d
Andy Gocke <angocke@microsoft.com>
2016-05-31 11:20:23 -0700

    Add ChecksumAlgorithm to csc/vbc build task
    Fixes #10451

--- src/Compilers/Core/MSBuildTaskTests/CscTests.cs
+++ src/Compilers/Core/MSBuildTaskTests/CscTests.cs
@@ -91,6 +91,20 @@ namespace Microsoft.CodeAnalysis.BuildTasks.UnitTests
         }
 
         [Fact]
+        public void ChecksumAlgorithmFlag()
+        {
+            var csc = new Csc();
+            csc.Sources = MSBuildUtil.CreateTaskItems("test.cs");
+            csc.ChecksumAlgorithm = "sha256";
+            Assert.Equal("/out:test.exe /checksumalgorithm:sha256 test.cs", csc.GenerateResponseFileContents());
+
+            csc = new Csc();
+            csc.Sources = MSBuildUtil.CreateTaskItems("test.cs");
+            csc.ChecksumAlgorithm = null;
+            Assert.Equal("/out:test.exe test.cs", csc.GenerateResponseFileContents());
+        }
+
+        [Fact]
         public void TargetTypeDll()
         {
             var csc = new Csc();
