7089898833bf580430b4a8a08cbed79132e40897
Ty Overby <ty@pre-alpha.com>
2016-06-02 14:16:52 -0700

    Fix case on LanguageVersion

--- src/Compilers/VisualBasic/Portable/CommandLine/VisualBasicCommandLineParser.vb
+++ src/Compilers/VisualBasic/Portable/CommandLine/VisualBasicCommandLineParser.vb
@@ -100,7 +100,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
             Dim parseDocumentationComments As Boolean = False ' Don't just null check documentationFileName because we want to do this even if the file name is invalid.
             Dim outputKind As OutputKind = OutputKind.ConsoleApplication
             Dim ssVersion As SubsystemVersion = SubsystemVersion.None
-            Dim languageVersion As LanguageVersion = languageVersion.VisualBasic15
+            Dim languageVersion As LanguageVersion = LanguageVersion.VisualBasic15
             Dim mainTypeName As String = Nothing
             Dim win32ManifestFile As String = Nothing
             Dim win32ResourceFile As String = Nothing
