0a86188ef16c911cf70c63e41847a97cf5f0fe3b
VSadov <vsadov@microsoft.com>
2016-06-17 16:07:52 -0700

    removed unnecessary usings.

--- src/Compilers/CSharp/Portable/Binder/Semantics/Conversions/Conversion.cs
+++ src/Compilers/CSharp/Portable/Binder/Semantics/Conversions/Conversion.cs
@@ -1,13 +1,10 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Roslyn.Utilities;
 using System;
 using System.Collections.Immutable;
 using System.Diagnostics;
-using Microsoft.CodeAnalysis.CSharp.Symbols;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
-using Roslyn.Utilities;
-using System.Linq;
 
 namespace Microsoft.CodeAnalysis.CSharp
 {
