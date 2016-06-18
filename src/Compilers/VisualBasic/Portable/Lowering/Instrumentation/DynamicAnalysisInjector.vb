a76a419214d094affd7cad6615af7cbdc7f63000
John Hamby <jhamby@microsoft.com>
2016-06-18 15:58:28 -0700

    More progress.

--- src/Compilers/VisualBasic/Portable/Lowering/Instrumentation/DynamicAnalysisInjector.vb
+++ src/Compilers/VisualBasic/Portable/Lowering/Instrumentation/DynamicAnalysisInjector.vb
@@ -59,64 +59,56 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
             _methodEntryInstrumentation = AddAnalysisPoint(MethodDeclarationIfAvailable(methodBody.Syntax), factory)
         End Sub
 
-        Private Shared Function MarkStatementWithSequencePoint(original As BoundStatement, rewritten As BoundStatement) As BoundStatement
-            Return New BoundSequencePoint(original.Syntax, rewritten)
-        End Function
-
         Public Overrides Function InstrumentExpressionStatement(original As BoundExpressionStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentExpressionStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentExpressionStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentStopStatement(original As BoundStopStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentStopStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentStopStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentEndStatement(original As BoundEndStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentEndStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentEndStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentContinueStatement(original As BoundContinueStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentContinueStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentContinueStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentExitStatement(original As BoundExitStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentExitStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentExitStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentGotoStatement(original As BoundGotoStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentGotoStatement(original, rewritten))
-        End Function
-
-        Public Overrides Function InstrumentLabelStatement(original As BoundLabelStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentLabelStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentGotoStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentRaiseEventStatement(original As BoundRaiseEventStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentRaiseEventStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentRaiseEventStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentReturnStatement(original As BoundReturnStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentReturnStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentReturnStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentThrowStatement(original As BoundThrowStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentThrowStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentThrowStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentOnErrorStatement(original As BoundOnErrorStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentOnErrorStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentOnErrorStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentResumeStatement(original As BoundResumeStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentResumeStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentResumeStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentAddHandlerStatement(original As BoundAddHandlerStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentAddHandlerStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentAddHandlerStatement(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentRemoveHandlerStatement(original As BoundRemoveHandlerStatement, rewritten As BoundStatement) As BoundStatement
-            Return MarkStatementWithSequencePoint(original, MyBase.InstrumentRemoveHandlerStatement(original, rewritten))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentRemoveHandlerStatement(original, rewritten))
         End Function
 
         Public Overrides Function CreateBlockPrologue(original As BoundBlock) As BoundStatement
@@ -175,125 +167,24 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
             Return rewritten
         End Function
 
-        Public Overrides Function InstrumentDoLoopEpilogue(original As BoundDoLoopStatement, epilogueOpt As BoundStatement) As BoundStatement
-            ' adds EndXXX sequence point to a statement
-            ' NOTE: if target statement happens to be a block, then 
-            ' sequence point goes inside the block
-            ' This ensures that when we stopped on EndXXX, we are still in the block's scope
-            ' and can examine locals declared in the block.
-            Return New BoundSequencePoint(DirectCast(original.Syntax, DoLoopBlockSyntax).LoopStatement, MyBase.InstrumentDoLoopEpilogue(original, epilogueOpt))
-        End Function
-
-        Public Overrides Function CreateSyncLockStatementPrologue(original As BoundSyncLockStatement) As BoundStatement
-            ' create a sequence point that contains the whole SyncLock statement as the first reachable sequence point
-            ' of the SyncLock statement. 
-            Return New BoundSequencePoint(DirectCast(original.Syntax, SyncLockBlockSyntax).SyncLockStatement, MyBase.CreateSyncLockStatementPrologue(original))
-        End Function
-
         Public Overrides Function InstrumentSyncLockObjectCapture(original As BoundSyncLockStatement, rewritten As BoundStatement) As BoundStatement
-            Return New BoundSequencePoint(original.LockExpression.Syntax, MyBase.InstrumentSyncLockObjectCapture(original, rewritten))
-        End Function
-
-        Public Overrides Function CreateSyncLockExitDueToExceptionEpilogue(original As BoundSyncLockStatement) As BoundStatement
-            ' Add a sequence point to highlight the "End SyncLock" syntax in case the body has thrown an exception
-            Return New BoundSequencePoint(DirectCast(original.Syntax, SyncLockBlockSyntax).EndSyncLockStatement, MyBase.CreateSyncLockExitDueToExceptionEpilogue(original))
-        End Function
-
-        Public Overrides Function CreateSyncLockExitNormallyEpilogue(original As BoundSyncLockStatement) As BoundStatement
-            ' Add a sequence point to highlight the "End SyncLock" syntax in case the body has been complete executed and
-            ' exited normally
-            Return New BoundSequencePoint(DirectCast(original.Syntax, SyncLockBlockSyntax).EndSyncLockStatement, MyBase.CreateSyncLockExitNormallyEpilogue(original))
-        End Function
-
-        Public Overrides Function InstrumentWhileEpilogue(original As BoundWhileStatement, epilogueOpt As BoundStatement) As BoundStatement
-            Return New BoundSequencePoint(DirectCast(original.Syntax, WhileBlockSyntax).EndWhileStatement, MyBase.InstrumentWhileEpilogue(original, epilogueOpt))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentSyncLockObjectCapture(original, rewritten))
         End Function
 
         Public Overrides Function InstrumentWhileStatementConditionalGotoStart(original As BoundWhileStatement, ifConditionGotoStart As BoundStatement) As BoundStatement
-            Return New BoundSequencePoint(DirectCast(original.Syntax, WhileBlockSyntax).WhileStatement,
-                                          MyBase.InstrumentWhileStatementConditionalGotoStart(original, ifConditionGotoStart))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentWhileStatementConditionalGotoStart(original, ifConditionGotoStart))
         End Function
 
         Public Overrides Function InstrumentDoLoopStatementEntryOrConditionalGotoStart(original As BoundDoLoopStatement, ifConditionGotoStartOpt As BoundStatement) As BoundStatement
-            Return New BoundSequencePoint(DirectCast(original.Syntax, DoLoopBlockSyntax).DoStatement,
-                                          MyBase.InstrumentDoLoopStatementEntryOrConditionalGotoStart(original, ifConditionGotoStartOpt))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentDoLoopStatementEntryOrConditionalGotoStart(original, ifConditionGotoStartOpt))
         End Function
 
         Public Overrides Function InstrumentForEachStatementConditionalGotoStart(original As BoundForEachStatement, ifConditionGotoStart As BoundStatement) As BoundStatement
-            ' Add hidden sequence point
-            Return New BoundSequencePoint(Nothing, MyBase.InstrumentForEachStatementConditionalGotoStart(original, ifConditionGotoStart))
+            Return AddDynamicAnalysis(original, MyBase.InstrumentForEachStatementConditionalGotoStart(original, ifConditionGotoStart))
         End Function
 
         Public Overrides Function InstrumentIfStatementConditionalGoto(original As BoundIfStatement, condGoto As BoundStatement) As BoundStatement
-            condGoto = MyBase.InstrumentIfStatementConditionalGoto(original, condGoto)
-
-            Select Case original.Syntax.Kind
-                Case SyntaxKind.MultiLineIfBlock
-                    Dim asMultiline = DirectCast(original.Syntax, MultiLineIfBlockSyntax)
-                    condGoto = New BoundSequencePoint(asMultiline.IfStatement, condGoto)
-                Case SyntaxKind.ElseIfBlock
-                    Dim asElseIfBlock = DirectCast(original.Syntax, ElseIfBlockSyntax)
-                    condGoto = New BoundSequencePoint(asElseIfBlock.ElseIfStatement, condGoto)
-                Case SyntaxKind.SingleLineIfStatement
-                    Dim asSingleLine = DirectCast(original.Syntax, SingleLineIfStatementSyntax)
-                    condGoto = New BoundSequencePointWithSpan(asSingleLine, condGoto, TextSpan.FromBounds(asSingleLine.IfKeyword.SpanStart, asSingleLine.ThenKeyword.EndPosition - 1))
-            End Select
-
-            Return condGoto
-        End Function
-
-        Public Overrides Function InstrumentIfStatementAfterIfStatement(original As BoundIfStatement, afterIfStatement As BoundStatement) As BoundStatement
-            ' Associate afterIf with EndIf
-            Return New BoundSequencePoint(DirectCast(original.Syntax, MultiLineIfBlockSyntax).EndIfStatement,
-                                          MyBase.InstrumentIfStatementAfterIfStatement(original, afterIfStatement))
-        End Function
-
-        Public Overrides Function InstrumentIfStatementConsequenceEpilogue(original As BoundIfStatement, epilogueOpt As BoundStatement) As BoundStatement
-            epilogueOpt = MyBase.InstrumentIfStatementConsequenceEpilogue(original, epilogueOpt)
-
-            Dim syntax As VisualBasicSyntaxNode = Nothing
-            Select Case original.Syntax.Kind
-                Case SyntaxKind.MultiLineIfBlock
-                    syntax = DirectCast(original.Syntax, MultiLineIfBlockSyntax).EndIfStatement
-                Case SyntaxKind.ElseIfBlock
-                    syntax = DirectCast(original.Syntax.Parent, MultiLineIfBlockSyntax).EndIfStatement
-                Case SyntaxKind.SingleLineIfStatement
-                    ' single line if has no EndIf
-                    Return epilogueOpt
-            End Select
-
-            Return New BoundSequencePoint(syntax, epilogueOpt)
-        End Function
-
-        Public Overrides Function InstrumentIfStatementAlternativeEpilogue(original As BoundIfStatement, epilogueOpt As BoundStatement) As BoundStatement
-            Return New BoundSequencePoint(DirectCast(original.AlternativeOpt.Syntax.Parent, MultiLineIfBlockSyntax).EndIfStatement,
-                                          MyBase.InstrumentIfStatementAlternativeEpilogue(original, epilogueOpt))
-        End Function
-
-        Public Overrides Function CreateIfStatementAlternativePrologue(original As BoundIfStatement) As BoundStatement
-            Dim prologue = MyBase.CreateIfStatementAlternativePrologue(original)
-
-            Select Case original.AlternativeOpt.Syntax.Kind
-                Case SyntaxKind.ElseBlock
-                    prologue = New BoundSequencePoint(DirectCast(original.AlternativeOpt.Syntax, ElseBlockSyntax).ElseStatement, prologue)
-                Case SyntaxKind.SingleLineElseClause
-                    prologue = New BoundSequencePointWithSpan(original.AlternativeOpt.Syntax, prologue,
-                                                              DirectCast(original.AlternativeOpt.Syntax, SingleLineElseClauseSyntax).ElseKeyword.Span)
-            End Select
-
-            Return prologue
-        End Function
-
-        Public Overrides Function InstrumentDoLoopStatementCondition(original As BoundDoLoopStatement, rewrittenCondition As BoundExpression, currentMethodOrLambda As MethodSymbol) As BoundExpression
-            Return AddConditionSequencePoint(MyBase.InstrumentDoLoopStatementCondition(original, rewrittenCondition, currentMethodOrLambda), original, currentMethodOrLambda)
-        End Function
-
-        Public Overrides Function InstrumentWhileStatementCondition(original As BoundWhileStatement, rewrittenCondition As BoundExpression, currentMethodOrLambda As MethodSymbol) As BoundExpression
-            Return AddConditionSequencePoint(MyBase.InstrumentWhileStatementCondition(original, rewrittenCondition, currentMethodOrLambda), original, currentMethodOrLambda)
-        End Function
-
-        Public Overrides Function InstrumentForEachStatementCondition(original As BoundForEachStatement, rewrittenCondition As BoundExpression, currentMethodOrLambda As MethodSymbol) As BoundExpression
-            Return AddConditionSequencePoint(MyBase.InstrumentForEachStatementCondition(original, rewrittenCondition, currentMethodOrLambda), original, currentMethodOrLambda)
+            Return AddDynamicAnalysis(original, MyBase.InstrumentIfStatementConditionalGoto(original, condGoto))
         End Function
 
         Public Overrides Function InstrumentObjectForLoopInitCondition(original As BoundForToStatement, rewrittenInitCondition As BoundExpression, currentMethodOrLambda As MethodSymbol) As BoundExpression
@@ -615,27 +506,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
             return AddDynamicAnalysis(original, base.InstrumentUsingTargetCapture(original, usingTargetCapture));
         }
         
-        private BoundStatement AddDynamicAnalysis(BoundStatement original, BoundStatement rewritten)
-        {
-            if (!original.WasCompilerGenerated)
-            {
-                // Do not instrument implicit constructor initializers
-                if (!original.IsConstructorInitializer() || original.Syntax.Kind() != SyntaxKind.ConstructorDeclaration)
-                {
-                    return CollectDynamicAnalysis(original, rewritten);
-                }
-            }
-
-            return rewritten;
-        }
-
-        private BoundStatement CollectDynamicAnalysis(BoundStatement original, BoundStatement rewritten)
-        {
-            SyntheticBoundNodeFactory statementFactory = new SyntheticBoundNodeFactory(_method, original.Syntax, _factory.CompilationState, _diagnostics);
-            return statementFactory.Block(ImmutableArray.Create(AddAnalysisPoint(SyntaxForSpan(original), statementFactory), rewritten));
-        }
-
-        
+       
 
         
         
@@ -646,6 +517,19 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
     }
 }
 #End If
+        Private Function AddDynamicAnalysis(original As BoundStatement, rewritten As BoundStatement) As BoundStatement
+            If Not original.WasCompilerGenerated Then
+                Return CollectDynamicAnalysis(original, rewritten)
+            End If
+
+            Return rewritten
+        End Function
+
+        Private Function CollectDynamicAnalysis(original As BoundStatement, rewritten As BoundStatement) As BoundStatement
+            Dim statementFactory As New SyntheticBoundNodeFactory(_factory.TopLevelMethod, _method, original.Syntax, _factory.CompilationState, _diagnostics)
+            Return statementFactory.Block(ImmutableArray.Create(AddAnalysisPoint(SyntaxForSpan(original), statementFactory), rewritten))
+        End Function
+
         Private Function AddAnalysisPoint(syntaxForSpan As VisualBasicSyntaxNode, statementFactory As SyntheticBoundNodeFactory) As BoundStatement
             ' Add an entry in the spans array.
 
