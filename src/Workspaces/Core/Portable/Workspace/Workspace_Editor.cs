70b34cf6e76eb676b5fba425572f2764129310e5
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2016-06-29 11:00:31 -0700

    Support "OnProjectRemoved" calls when the project has open documents
    We already had code to deal with this case, we just hit checks before
    it which threw.  The existing code needed to be modified to not modify
    the collection it was enumerating though.
    
    Fixes internal issue 233612.  Unloading a project with an open file that is
    in a shared project or is linked would hit hit the exception in
    CheckProjectCanBeRemoved because it still had Open documents.  Normal
    projects didn't hit this because the documents were already closed by
    RunningDocumentTableEvents.

--- src/Workspaces/Core/Portable/Workspace/Workspace_Editor.cs
+++ src/Workspaces/Core/Portable/Workspace/Workspace_Editor.cs
@@ -75,7 +75,9 @@ namespace Microsoft.CodeAnalysis
 
             if (openDocs != null)
             {
-                foreach (var docId in openDocs)
+                // ClearOpenDocument will remove the document from the original set.
+                var copyOfOpenDocs = openDocs.ToList();
+                foreach (var docId in copyOfOpenDocs)
                 {
                     this.ClearOpenDocument(docId);
                 }
@@ -175,6 +177,7 @@ namespace Microsoft.CodeAnalysis
             }
         }
 
+        [Obsolete("This method will be removed in a future release.")]
         protected void CheckProjectDoesNotContainOpenDocuments(ProjectId projectId)
         {
             if (ProjectHasOpenDocuments(projectId))
