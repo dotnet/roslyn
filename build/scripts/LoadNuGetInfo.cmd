ab8b1c8a1a3da1c0756d8af7d888a1539d50ff79
Jared Parsons <jaredpar@microsoft.com>
2016-06-29 23:03:01 -0700

    Add LoadNuGetInfo.ps1 for powershell as well

--- build/scripts/LoadNuGetInfo.cmd
+++ build/scripts/LoadNuGetInfo.cmd
@@ -3,8 +3,8 @@
 REM This is a script that will set environment information about where to find
 REM NuGet.exe, it's version and ensure that it's present on the enlistment.
 set NuGetExeVersion=3.5.0-beta2
-set NuGetExeFolder="%~dp0..\..\Binaries\Download\%NuGetExeVersion%"
-set NuGetExe="%NuGetExeFolder%\NuGet.exe"
+set NuGetExeFolder=%~dp0..\..\Binaries\Download\%NuGetExeVersion%
+set NuGetExe=%NuGetExeFolder%\NuGet.exe
 
 REM Download NuGet.exe if we haven't already
 if not exist "%NuGetExe%" (
