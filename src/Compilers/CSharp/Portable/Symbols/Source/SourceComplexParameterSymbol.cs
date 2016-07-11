7fa168ca6faf316ac93cda04e5820bf37237457b
Andy Gocke <angocke@microsoft.com>
2016-07-11 16:12:42 -0700

    Disallow TupleElementNamesAttribute in source

--- src/Compilers/CSharp/Portable/Symbols/Source/SourceComplexParameterSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourceComplexParameterSymbol.cs
@@ -623,6 +623,10 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 // DynamicAttribute should not be set explicitly.
                 arguments.Diagnostics.Add(ErrorCode.ERR_ExplicitDynamicAttr, arguments.AttributeSyntaxOpt.Location);
             }
+            else if (attribute.IsTargetAttribute(this, AttributeDescription.TupleElementNamesAttribute))
+            {
+                arguments.Diagnostics.Add(ErrorCode.ERR_ExplicitTupleElementNames, arguments.AttributeSyntaxOpt.Location);
+            }
         }
 
 
