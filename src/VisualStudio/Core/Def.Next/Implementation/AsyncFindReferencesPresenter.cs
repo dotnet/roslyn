bb1e9ff14b25d338070cfa94dd1855c557c3cc6c
CyrusNajmabadi <cyrusn@microsoft.com>
2016-07-20 17:49:17 -0700

    Add initial implementation of the async presenter.

---
+++ src/VisualStudio/Core/Def.Next/Implementation/AsyncFindReferencesPresenter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using Microsoft.CodeAnalysis.Editor.Host;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Projection;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Shell.TableManager;
+using Microsoft.CodeAnalysis.Shared.TestHooks;
+using Microsoft.VisualStudio.Shell.FindAllReferences;
+using Microsoft.CodeAnalysis.Editor;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation
+{
+    [Export(typeof(IAsyncFindReferencesPresenter)), Shared]
+    internal partial class AsyncFindReferencesPresenter :
+        ForegroundThreadAffinitizedObject, IAsyncFindReferencesPresenter
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IAsynchronousOperationListener _asyncListener;
+
+        private readonly ITextBufferFactoryService _textBufferFactoryService;
+        private readonly IProjectionBufferFactoryService _projectionBufferFactoryService;
+        private readonly IEditorOptionsFactoryService _editorOptionsFactoryService;
+        private readonly ITextEditorFactoryService _textEditorFactoryService;
+
+        private readonly ClassificationTypeMap _typeMap;
+        private readonly IFindAllReferencesService _vsFindAllReferencesService;
+
+        [ImportingConstructor]
+        public AsyncFindReferencesPresenter(
+            Shell.SVsServiceProvider serviceProvider,
+            ITextBufferFactoryService textBufferFactoryService,
+            IProjectionBufferFactoryService projectionBufferFactoryService,
+            IEditorOptionsFactoryService editorOptionsFactoryService,
+            ITextEditorFactoryService textEditorFactoryService,
+            ClassificationTypeMap typeMap,
+            [ImportMany] IEnumerable<Lazy<IAsynchronousOperationListener, FeatureMetadata>> asyncListeners)
+        {
+            _serviceProvider = serviceProvider;
+            _textBufferFactoryService = textBufferFactoryService;
+            _projectionBufferFactoryService = projectionBufferFactoryService;
+            _editorOptionsFactoryService = editorOptionsFactoryService;
+            _textEditorFactoryService = textEditorFactoryService;
+            _typeMap = typeMap;
+            _asyncListener = new AggregateAsynchronousOperationListener(
+                asyncListeners, FeatureAttribute.ReferenceHighlighting);
+
+            _vsFindAllReferencesService = (IFindAllReferencesService)_serviceProvider.GetService(typeof(SVsFindAllReferences));
+        }
+
+        public FindReferencesContext StartSearch()
+        {
+            this.AssertIsForeground();
+
+            var window = _vsFindAllReferencesService.StartSearch("");
+
+            foreach (var source in window.Manager.Sources.ToArray())
+            {
+                window.Manager.RemoveSource(source);
+            }
+
+            var dataSource = new DataSource(this);
+            window.Manager.AddSource(dataSource);
+
+            return dataSource;
+        }
+    }
+}
