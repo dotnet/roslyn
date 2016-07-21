2aa645bbb4b5935be29a88f1fadf0bc4830d141c
CyrusNajmabadi <cyrusn@microsoft.com>
2016-07-20 17:58:00 -0700

    Add initial implementation of the subscription.

---
+++ src/VisualStudio/Core/Def.Next/Implementation/AsyncFindReferencesPresenter.Subscription.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell.TableManager;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation
+{
+    internal partial class AsyncFindReferencesPresenter
+    {
+        private class Subscription : IDisposable
+        {
+            private readonly CancellationTokenSource _tokenSource;
+            public readonly ITableDataSink TableDataSink;
+
+            public Subscription(CancellationTokenSource tokenSource, ITableDataSink sink)
+            {
+                _tokenSource = tokenSource;
+                TableDataSink = sink;
+            }
+
+            public void Dispose()
+            {
+                _tokenSource.Cancel();
+            }
+        }
+    }
+}
