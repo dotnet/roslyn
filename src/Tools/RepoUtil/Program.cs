d5f860ebb20ff2e72937f2d336a671ac800a3c47
Jared Parsons <jaredpar@microsoft.com>
2016-07-24 16:59:01 -0700

    Added the RepoUtil project

---
+++ src/Tools/RepoUtil/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoUtil
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+        }
+    }
+}
