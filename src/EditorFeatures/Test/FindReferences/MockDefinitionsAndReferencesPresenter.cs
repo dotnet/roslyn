e081f2d7c34a07a086bc8ba741506e077fe63262
CyrusNajmabadi <cyrusn@microsoft.com>
2016-07-24 20:55:26 -0700

    Rename files to match types.

--- src/EditorFeatures/Test/FindReferences/MockDefinitionsAndReferencesPresenter.cs
+++ src/EditorFeatures/Test/FindReferences/MockDefinitionsAndReferencesPresenter.cs
@@ -1,14 +1,11 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using Microsoft.CodeAnalysis.Editor.Host;
 using Microsoft.CodeAnalysis.Editor.Implementation.FindReferences;
-using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace Microsoft.CodeAnalysis.Editor.UnitTests.FindReferences
 {
-    internal class DefinitionsAndReferencesPresenter : IDefinitionsAndReferencesPresenter
+    internal class MockDefinitionsAndReferencesPresenter : IDefinitionsAndReferencesPresenter
     {
         public DefinitionsAndReferences DefinitionsAndReferences;
 
