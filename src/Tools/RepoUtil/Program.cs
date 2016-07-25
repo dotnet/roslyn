5674ad2384ec8d79d24a28f4eb67107eaf40d08d
Jared Parsons <jaredpar@microsoft.com>
2016-07-24 18:01:11 -0700

    Added a verify command to peek at the status of the repo

--- src/Tools/RepoUtil/Program.cs
+++ src/Tools/RepoUtil/Program.cs
@@ -10,8 +10,98 @@ namespace RepoUtil
 {
     internal static class Program
     {
-        internal static void Main(string[] args)
+        private enum Mode
         {
+            Usage,
+            Verify
+        }
+
+        internal static readonly string[] ProjectJsonFileRelativeNames = Array.Empty<string>();
+
+        internal static int Main(string[] args)
+        {
+            return Run(args) ? 0 : 1;
+        }
+
+        private static bool Run(string[] args)
+        { 
+            string sourcesPath;
+            Mode mode;
+            if (!TryParseCommandLine(args, out mode, out sourcesPath))
+            {
+                return false;
+            }
+
+            switch (mode)
+            {
+                case Mode.Usage:
+                    Usage();
+                    return true;
+                case Mode.Verify:
+                    return Verify(sourcesPath);
+                default:
+                    throw new Exception("Unrecognized mode");
+            }
+        }
+
+        private static bool Verify(string sourcesPath)
+        {
+            var fileNames = Data
+                .StaticList
+                .Concat(Data.FloatingList)
+                .Select(x => new FileName(sourcesPath, x));
+            var allGood = false;
+            foreach (var fileName in fileNames)
+            {
+                if (!File.Exists(fileName.FullPath))
+                {
+                    Console.WriteLine($"Project file {fileName} does not exist");
+                    allGood = false;
+                }
+            }
+
+            if (!allGood)
+            {
+                return false;
+            }
+
+            return ProjectJsonUtil.VerifyTracked(sourcesPath, fileNames);
+        }
+
+        private static void Usage()
+        {
+            var text = @"
+  -verify: check the state of the repo
+";
+            Console.Write(text);
+        }
+
+        private static bool TryParseCommandLine(string[] args, out Mode mode, out string sourcesPath)
+        {
+            var allGood = true;
+            var binariesPath = Path.GetDirectoryName(Path.GetDirectoryName(AppContext.BaseDirectory));
+            sourcesPath = Path.GetDirectoryName(binariesPath);
+            mode = Mode.Usage;
+
+            var index = 0;
+            while (index < args.Length)
+            {
+                var arg = args[index];
+                switch (arg.ToLower())
+                {
+                    case "-verify":
+                        mode = Mode.Verify;
+                        index++;
+                        break;
+                    default:
+                        Console.Write($"Option {arg} is unrecognized");
+                        allGood = false;
+                        index++;
+                        break;
+                }
+            }
+
+            return allGood;
         }
     }
 }
---
+++ src/Tools/RepoUtil/ProjectJsonUtil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace RepoUtil
+{
+    internal static class ProjectJsonUtil
+    {
+        /// <summary>
+        /// Does the specified project.json file need to be tracked by our repo util? 
+        /// </summary>
+        internal static bool NeedsTracking(string filePath)
+        {
+            // Need to track any file that has dependencies
+            var obj = JObject.Parse(File.ReadAllText(filePath));
+            var dependencies = (JObject)obj["dependencies"];
+            return dependencies != null && dependencies.Properties().Any();
+        }
+
+        internal static bool VerifyTracked(string sourcesPath, IEnumerable<FileName> fileNames)
+        {
+            var set = new HashSet<FileName>(fileNames);
+            var allGood = true;
+
+            foreach (var file in Directory.EnumerateFiles(sourcesPath, "project.json", SearchOption.AllDirectories))
+            {
+                var relativeName = file.Substring(sourcesPath.Length + 1);
+                var fileName = new FileName(sourcesPath, relativeName);
+                if (set.Contains(fileName) || !NeedsTracking(file))
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Need to track {fileName}");
+                allGood = false;
+            }
+
+            return allGood;
+        }
+    }
+}
