258e595c2212afa9d0508022d0ff1eb1282f5705
CyrusNajmabadi <cyrusn@microsoft.com>
2016-07-26 13:53:49 -0700

    Easier implementation by just subclassing Editor base type.

--- src/VisualStudio/Core/Def.Next/Implementation/StreamingFindReferencesPresenter.TableEntriesSnapshot.cs
+++ src/VisualStudio/Core/Def.Next/Implementation/StreamingFindReferencesPresenter.TableEntriesSnapshot.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Immutable;
-using System.Windows;
-using Microsoft.VisualStudio.Imaging.Interop;
+﻿using System.Collections.Immutable;
 using Microsoft.VisualStudio.Shell.TableControl;
 using Microsoft.VisualStudio.Shell.TableManager;
 
@@ -9,79 +6,35 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation
 {
     internal partial class StreamingFindReferencesPresenter
     {
-        private class TableEntriesSnapshot : ITableEntriesSnapshot, IWpfTableEntriesSnapshot
+        private class TableEntriesSnapshot : WpfTableEntriesSnapshotBase
         {
-            public int VersionNumber { get; }
+            private readonly int _versionNumber;
+
             private readonly ImmutableList<ReferenceEntry> _referenceEntries;
 
             public TableEntriesSnapshot(ImmutableList<ReferenceEntry> referenceEntries, int versionNumber)
             {
                 _referenceEntries = referenceEntries;
-                VersionNumber = versionNumber;
+                _versionNumber = versionNumber;
             }
 
-            int ITableEntriesSnapshot.Count => _referenceEntries.Count;
+            public override int VersionNumber => _versionNumber;
 
-            void IDisposable.Dispose()
-            {
-            }
+            public override int Count => _referenceEntries.Count;
 
-            int ITableEntriesSnapshot.IndexOf(int currentIndex, ITableEntriesSnapshot newSnapshot)
+            public override int IndexOf(int currentIndex, ITableEntriesSnapshot newSnapshot)
             {
                 // We only add items to the end of our list, and we never reorder.
                 // As such, any index in us will map to the same index in any newer snapshot.
                 return currentIndex;
             }
 
-            void ITableEntriesSnapshot.StartCaching()
-            {
-            }
-
-            void ITableEntriesSnapshot.StopCaching()
-            {
-            }
-
-            bool ITableEntriesSnapshot.TryGetValue(int index, string keyName, out object content)
+            public override bool TryGetValue(int index, string keyName, out object content)
             {
                 return _referenceEntries[index].TryGetValue(keyName, out content);
             }
 
-            public bool TryCreateImageContent(int index, string columnName, bool singleColumnView, out ImageMoniker content)
-            {
-                content = default(ImageMoniker);
-                return false;
-            }
-
-            public bool TryCreateStringContent(int index, string columnName, bool truncatedText, bool singleColumnView, out string content)
-            {
-                content = null;
-                return false;
-            }
-
-            public bool TryCreateColumnContent(int index, string columnName, bool singleColumnView, out FrameworkElement content)
-            {
-                content = null;
-                return false;
-            }
-
-            public bool CanCreateDetailsContent(int index)
-            {
-                return false;
-            }
-
-            public bool TryCreateDetailsContent(int index, out FrameworkElement expandedContent)
-            {
-                expandedContent = null;
-                return false;
-            }
-
-            public bool TryCreateDetailsStringContent(int index, out string content)
-            {
-                content = null;
-                return false;
-            }
-
-            public bool TryCreateToolTip(int index, string columnName, out object toolTip)
+            public override bool TryCreateToolTip(int index, string columnName, out object toolTip)
             {
                 return this._referenceEntries[index].TryCreateToolTip(columnName, out toolTip);
             }
