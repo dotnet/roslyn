008c705d0ec98998a4d6d2a028afb0eef7cf0bc7
Heejae Chang <hechang@microsoft.com>
2016-07-26 18:33:35 -0700

    rename and moving files between feature/workspace layers
    only real change is having ICompilerDiagnosticAnalyzer interface which can either have inproc implementation or out of proc implementation.
    
    inproc is needed since diagnostics are in feature layer and one who uses feature layer out side of VS host need an implementation.

---
+++ src/Features/Core/Portable/Diagnostics/EngineV2/ICompilerDiagnosticAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Workspaces.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.Diagnostics.EngineV2
+{
+    internal interface ICompilerDiagnosticAnalyzer : IWorkspaceService
+    {
+        Task<DiagnosticAnalysisResultMap<DiagnosticAnalyzer, DiagnosticAnalysisResult>> AnalyzeAsync(CompilationWithAnalyzers analyzerDriver, Project project, CancellationToken cancellationToken);
+    }
+}
