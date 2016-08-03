61a78ba0bfc632d47ce82872f5b4895170c8849f
Julien Couvreur <julien.couvreur@gmail.com>
2016-08-02 18:48:14 -0700

    Compacting C# 7 error codes

--- src/Compilers/CSharp/Test/Symbol/Symbols/SymbolErrorTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/SymbolErrorTests.cs
@@ -1053,7 +1053,7 @@ class C
                 // (5,15): error CS1003: Syntax error, ',' expected
                 //     int F<int>() { }  // CS0081
                 Diagnostic(ErrorCode.ERR_SyntaxError, "(").WithArguments(",", "(").WithLocation(5, 15),
-                // (5,15): error CS8200: Tuple must contain at least two elements.
+                // (5,15): error CS8124: Tuple must contain at least two elements.
                 //     int F<int>() { }  // CS0081
                 Diagnostic(ErrorCode.ERR_TupleTooFewElements, "()").WithLocation(5, 15),
                 // (5,18): error CS1001: Identifier expected
@@ -1062,7 +1062,7 @@ class C
                 // (5,18): error CS1026: ) expected
                 //     int F<int>() { }  // CS0081
                 Diagnostic(ErrorCode.ERR_CloseParenExpected, "{").WithLocation(5, 18),
-                // (5,15): error CS8200: Tuple must contain at least two elements.
+                // (5,15): error CS8124: Tuple must contain at least two elements.
                 //     int F<int>() { }  // CS0081
                 Diagnostic(ErrorCode.ERR_TupleTooFewElements, "()").WithLocation(5, 15),
                 // (5,9): error CS0161: 'C.F<>(int, ?)': not all code paths return a value
@@ -1104,7 +1104,7 @@ class C
                 // (5,15): error CS1003: Syntax error, ',' expected
                 //     int F<int>() { }  // CS0081
                 Diagnostic(ErrorCode.ERR_SyntaxError, "(").WithArguments(",", "(").WithLocation(5, 15),
-                // (5,15): error CS8200: Tuple must contain at least two elements.
+                // (5,15): error CS8124: Tuple must contain at least two elements.
                 //     int F<int>() { }  // CS0081
                 Diagnostic(ErrorCode.ERR_TupleTooFewElements, "()").WithLocation(5, 15),
                 // (5,15): error CS8059: Feature 'tuples' is not available in C# 6.  Please use language version 7 or greater.
@@ -1116,7 +1116,7 @@ class C
                 // (5,18): error CS1026: ) expected
                 //     int F<int>() { }  // CS0081
                 Diagnostic(ErrorCode.ERR_CloseParenExpected, "{").WithLocation(5, 18),
-                // (5,15): error CS8200: Tuple must contain at least two elements.
+                // (5,15): error CS8124: Tuple must contain at least two elements.
                 //     int F<int>() { }  // CS0081
                 Diagnostic(ErrorCode.ERR_TupleTooFewElements, "()").WithLocation(5, 15),
                 // (5,9): error CS0161: 'C.F<>(int, ?)': not all code paths return a value
