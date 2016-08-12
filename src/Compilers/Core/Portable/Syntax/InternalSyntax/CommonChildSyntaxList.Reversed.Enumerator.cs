c4ed419816537b2624bb0b5b9c155fa8c49dcb01
CyrusNajmabadi <cyrusn@microsoft.com>
2016-08-11 23:05:03 -0400

    Share more syntax code between C# and VB.

--- src/Compilers/Core/Portable/Syntax/InternalSyntax/CommonChildSyntaxList.Reversed.Enumerator.cs
+++ src/Compilers/Core/Portable/Syntax/InternalSyntax/CommonChildSyntaxList.Reversed.Enumerator.cs
@@ -1,15 +1,8 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.CodeAnalysis.CSharp.Symbols;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
-
-namespace Microsoft.CodeAnalysis.CSharp.Syntax.InternalSyntax
+namespace Microsoft.CodeAnalysis.Syntax.InternalSyntax
 {
-    internal partial struct ChildSyntaxList
+    internal partial struct CommonChildSyntaxList
     {
         internal partial struct Reversed
         {
