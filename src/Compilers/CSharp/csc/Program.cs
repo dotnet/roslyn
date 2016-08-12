7ebbc90a6faf48e0b38aec9bcbc0cd080429827d
Jared Parsons <jaredpar@microsoft.com>
2016-08-12 08:32:43 -0700

    Track it down

--- src/Compilers/CSharp/csc/Program.cs
+++ src/Compilers/CSharp/csc/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using System.IO;
 using Microsoft.CodeAnalysis.CommandLine;
 using Roslyn.Utilities;
@@ -9,7 +10,18 @@ namespace Microsoft.CodeAnalysis.CSharp.CommandLine
     public class Program
     {
         public static int Main(string[] args)
-            => Main(args, SpecializedCollections.EmptyArray<string>());
+        {
+            try
+            {
+                return Main(args, SpecializedCollections.EmptyArray<string>());
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                System.Console.WriteLine(ex.StackTrace);
+                return 1;
+            }
+        }
 
         public static int Main(string[] args, string[] extraArgs)
             => DesktopBuildClient.Run(args, extraArgs, RequestLanguage.CSharpCompile, Csc.Run, new DesktopAnalyzerAssemblyLoader());
