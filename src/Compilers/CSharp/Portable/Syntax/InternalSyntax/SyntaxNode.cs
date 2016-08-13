c07f8323cbb04e7351000e70f80684037a4addf2
CyrusNajmabadi <cyrusn@microsoft.com>
2016-08-12 21:55:10 -0400

    Fix imports

--- src/Compilers/CSharp/Portable/Syntax/InternalSyntax/SyntaxNode.cs
+++ src/Compilers/CSharp/Portable/Syntax/InternalSyntax/SyntaxNode.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using Microsoft.CodeAnalysis.Syntax.InternalSyntax;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp.Syntax.InternalSyntax
