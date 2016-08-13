16e806b87cc9ae015dd475889d3960b3bdb5295e
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2016-08-12 17:08:08 -0700

    Add P2P references for P2P references
    Fixes #12707.

--- src/VisualStudio/CSharp/Test/ProjectSystemShim/CPS/CSharpReferencesTests.cs
+++ src/VisualStudio/CSharp/Test/ProjectSystemShim/CPS/CSharpReferencesTests.cs
@@ -46,9 +46,7 @@ namespace Roslyn.VisualStudio.CSharp.UnitTests.ProjectSystemShim.CPS
                 ((IWorkspaceProjectContext)project4).BinOutputPath = @"C:\project4.dll";
                 Assert.Equal(@"C:\project4.dll", project4.BinOutputPath);
 
-                // This is currently broken by https://github.com/dotnet/roslyn/issues/12707
-                // Reverse the below assert to Assert.True once the above bug is fixed.
-                Assert.False(project3.GetCurrentProjectReferences().Any(pr => pr.ProjectId == project4.Id));
+                Assert.True(project3.GetCurrentProjectReferences().Any(pr => pr.ProjectId == project4.Id));
 
                 // Remove project reference
                 project3.RemoveProjectReference(project1);
