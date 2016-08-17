ad38d5db0fe2932228757c46d9ea443c33f4b122
CyrusNajmabadi <cyrusn@microsoft.com>
2016-08-17 17:43:17 -0400

    Simplify code.

--- src/Compilers/CSharp/Portable/Syntax/BaseSyntaxNodeExtensions.cs
+++ src/Compilers/CSharp/Portable/Syntax/BaseSyntaxNodeExtensions.cs
@@ -1,28 +1,28 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis.Syntax.InternalSyntax;
-
 namespace Microsoft.CodeAnalysis.CSharp.Syntax
 {
+    using Microsoft.CodeAnalysis.Syntax.InternalSyntax;
+
     internal static class GreenNodeExtensions
     {
-        internal static Microsoft.CodeAnalysis.Syntax.InternalSyntax.SyntaxList<T> ToGreenList<T>(this SyntaxNode node) where T : InternalSyntax.CSharpSyntaxNode
+        internal static SyntaxList<T> ToGreenList<T>(this SyntaxNode node) where T : InternalSyntax.CSharpSyntaxNode
         {
             return node != null ?
                 ToGreenList<T>(node.Green) :
-                default(CodeAnalysis.Syntax.InternalSyntax.SyntaxList<T>);
+                default(SyntaxList<T>);
         }
 
-        internal static Microsoft.CodeAnalysis.Syntax.InternalSyntax.SeparatedSyntaxList<T> ToGreenSeparatedList<T>(this SyntaxNode node) where T : InternalSyntax.CSharpSyntaxNode
+        internal static SeparatedSyntaxList<T> ToGreenSeparatedList<T>(this SyntaxNode node) where T : InternalSyntax.CSharpSyntaxNode
         {
             return node != null ?
-                new CodeAnalysis.Syntax.InternalSyntax.SeparatedSyntaxList<T>(ToGreenList<T>(node.Green)) :
-                default(CodeAnalysis.Syntax.InternalSyntax.SeparatedSyntaxList<T>);
+                new SeparatedSyntaxList<T>(ToGreenList<T>(node.Green)) :
+                default(SeparatedSyntaxList<T>);
         }
 
-        internal static Microsoft.CodeAnalysis.Syntax.InternalSyntax.SyntaxList<T> ToGreenList<T>(this GreenNode node) where T : InternalSyntax.CSharpSyntaxNode
+        internal static SyntaxList<T> ToGreenList<T>(this GreenNode node) where T : InternalSyntax.CSharpSyntaxNode
         {
-            return new CodeAnalysis.Syntax.InternalSyntax.SyntaxList<T>(node);
+            return new SyntaxList<T>(node);
         }
     }
 }
