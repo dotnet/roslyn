b4a8a60c3bf281f7f16a64594f283d436877fdbf
Ty Overby <ty@pre-alpha.com>
2016-08-17 13:51:11 -0700

    Remove diagnostic

--- SetDevCommandPrompt.cmd
+++ SetDevCommandPrompt.cmd
@@ -2,8 +2,6 @@
 
 :: prefer building with Dev15
 
-SET
-
 set CommonToolsDir=%VS150COMNTOOLS%
 if not exist "%CommonToolsDir%" set CommonToolsDir=%VS140COMNTOOLS%
 if not exist "%CommonToolsDir%" exit /b 1
