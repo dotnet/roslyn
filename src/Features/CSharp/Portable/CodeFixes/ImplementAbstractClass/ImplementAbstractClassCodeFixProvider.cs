0e878b145690645c91eabf033d3838dbdcbe17f7
CyrusNajmabadi <cyrusn@microsoft.com>
2016-08-23 12:06:17 -0400

    Share implementation of ImplementAbstractClass.

--- src/Features/CSharp/Portable/CodeFixes/ImplementAbstractClass/ImplementAbstractClassCodeFixProvider.cs
+++ src/Features/CSharp/Portable/CodeFixes/ImplementAbstractClass/ImplementAbstractClassCodeFixProvider.cs
@@ -1,90 +1,21 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Immutable;
 using System.Composition;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeFixes.ImplementAbstractClass;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.ImplementAbstractClass;
-using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.ImplementAbstractClass
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = PredefinedCodeFixProviderNames.ImplementAbstractClass), Shared]
     [ExtensionOrder(After = PredefinedCodeFixProviderNames.GenerateType)]
-    internal class ImplementAbstractClassCodeFixProvider : CodeFixProvider
+    internal class ImplementAbstractClassCodeFixProvider : 
+        AbstractImplementAbstractClassCodeFixProvider<ClassDeclarationSyntax>
     {
         private const string CS0534 = nameof(CS0534); // 'Program' does not implement inherited abstract member 'Foo.bar()'
 
-        public sealed override ImmutableArray<string> FixableDiagnosticIds =>
-            ImmutableArray.Create(CS0534);
-
-        public sealed override FixAllProvider GetFixAllProvider() =>
-            WellKnownFixAllProviders.BatchFixer;
-
-        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        public ImplementAbstractClassCodeFixProvider() : base(CS0534)
         {
-            var cancellationToken = context.CancellationToken;
-            var document = context.Document;
-
-            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-
-            var token = root.FindToken(context.Span.Start);
-            if (!token.Span.IntersectsWith(context.Span))
-            {
-                return;
-            }
-
-            var classNode = token.Parent as ClassDeclarationSyntax;
-            if (classNode == null)
-            {
-                return;
-            }
-
-            var service = document.GetLanguageService<IImplementAbstractClassService>();
-
-            if (await service.CanImplementAbstractClassAsync(
-                document,
-                classNode,
-                cancellationToken).ConfigureAwait(false))
-            {
-                var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-
-                var classSymbol = semanticModel.GetDeclaredSymbol(classNode);
-                var abstractType = classSymbol.BaseType;
-                var id = GetCodeActionId(abstractType.ContainingAssembly.Name, abstractType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat));
-                context.RegisterCodeFix(
-                    new MyCodeAction(
-                        c => ImplementAbstractClassAsync(document, classNode, c),
-                        id),
-                    context.Diagnostics);
-            }
-        }
-
-        // internal for testing purposes.
-        internal static string GetCodeActionId(string assemblyName, string abstractTypeFullyQualifiedName)
-        {
-            return CSharpFeaturesResources.Implement_Abstract_Class + ";" +
-                assemblyName + ";" +
-                abstractTypeFullyQualifiedName;
-        }
-
-        private Task<Document> ImplementAbstractClassAsync(
-            Document document, ClassDeclarationSyntax classNode, CancellationToken cancellationToken)
-        {
-            var service = document.GetLanguageService<IImplementAbstractClassService>();
-            return service.ImplementAbstractClassAsync(document, classNode, cancellationToken);
-        }
-
-        private class MyCodeAction : CodeAction.DocumentChangeAction
-        {
-            public MyCodeAction(Func<CancellationToken, Task<Document>> createChangedDocument, string id) :
-                base(CSharpFeaturesResources.Implement_Abstract_Class, createChangedDocument, id)
-            {
-            }
         }
     }
 }
--- src/Features/VisualBasic/Portable/CodeFixes/ImplementAbstractClass/ImplementAbstractClassCodeFixProvider.vb
+++ src/Features/VisualBasic/Portable/CodeFixes/ImplementAbstractClass/ImplementAbstractClassCodeFixProvider.vb
@@ -1,87 +1,21 @@
 ' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-Imports System.Collections.Immutable
-Imports System.Threading
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
-Imports Microsoft.CodeAnalysis.CodeActions
 Imports Microsoft.CodeAnalysis.CodeFixes
-Imports Microsoft.CodeAnalysis.ImplementAbstractClass
 Imports System.Composition
+Imports Microsoft.CodeAnalysis.CodeFixes.ImplementAbstractClass
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.CodeFixes.ImplementAbstractClass
     <ExportCodeFixProvider(LanguageNames.VisualBasic,
         Name:=PredefinedCodeFixProviderNames.ImplementAbstractClass), [Shared]>
     <ExtensionOrder(After:=PredefinedCodeFixProviderNames.GenerateType)>
     Friend Class ImplementAbstractClassCodeFixProvider
-        Inherits CodeFixProvider
+        Inherits AbstractImplementAbstractClassCodeFixProvider(Of ClassBlockSyntax)
 
         Friend Const BC30610 As String = "BC30610" ' Class 'foo' must either be declared 'MustInherit' or override the following inherited 'MustOverride' member(s): 
 
-        Public NotOverridable Overrides ReadOnly Property FixableDiagnosticIds As ImmutableArray(Of String)
-            Get
-                Return ImmutableArray.Create(BC30610)
-            End Get
-        End Property
-
-        Public NotOverridable Overrides Function GetFixAllProvider() As FixAllProvider
-            Return WellKnownFixAllProviders.BatchFixer
-        End Function
-
-        Public NotOverridable Overrides Async Function RegisterCodeFixesAsync(context As CodeFixContext) As Task
-            Dim cancellationToken = context.CancellationToken
-            Dim document = context.Document
-
-            Dim root = Await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(False)
-
-            Dim token = root.FindToken(context.Span.Start)
-            If Not token.Span.IntersectsWith(context.Span) Then
-                Return
-            End If
-
-            Dim classNode = token.GetAncestors(Of ClassBlockSyntax)() _
-                                 .FirstOrDefault(Function(c) c.Span.IntersectsWith(context.Span))
-
-            If classNode Is Nothing Then
-                Return
-            End If
-
-            Dim service = document.GetLanguageService(Of IImplementAbstractClassService)()
-
-            If Await service.CanImplementAbstractClassAsync(
-                document, classNode, cancellationToken).ConfigureAwait(False) Then
-
-                Dim model = Await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(False)
-                Dim classSymbol = model.GetDeclaredSymbol(classNode)
-
-                Dim typeName = classSymbol.BaseType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)
-                Dim id = GetCodeActionId(classSymbol.BaseType.ContainingAssembly.Name, typeName)
-                context.RegisterCodeFix(
-                    New MyCodeAction(
-                        Function(c) ImplementAbstractClassAsync(document, classNode, c),
-                        id),
-                    context.Diagnostics)
-                Return
-            End If
-        End Function
-
-        Friend Shared Function GetCodeActionId(assemblyName As String, abstractTypeFullyQualifiedName As String) As String
-            Return VBFeaturesResources.Implement_Abstract_Class + ";" +
-                assemblyName + ";" +
-                abstractTypeFullyQualifiedName
-        End Function
-
-        Private Function ImplementAbstractClassAsync(
-                document As Document, classBlock As ClassBlockSyntax, cancellationToken As CancellationToken) As Task(Of Document)
-            Dim service = document.GetLanguageService(Of IImplementAbstractClassService)()
-            Return service.ImplementAbstractClassAsync(document, classBlock, cancellationToken)
-        End Function
-
-        Private Class MyCodeAction
-            Inherits CodeAction.DocumentChangeAction
-
-            Public Sub New(createChangedDocument As Func(Of CancellationToken, Task(Of Document)), id As String)
-                MyBase.New(VBFeaturesResources.Implement_Abstract_Class, createChangedDocument, id)
-            End Sub
-        End Class
+        Public Sub New()
+            MyBase.New(BC30610)
+        End Sub
     End Class
 End Namespace
