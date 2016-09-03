7410a9b036ab9eafae624c2e5901aaac5a67ccd9
Adam Speight <AdamSpeight2008@users.noreply.github.com>
2016-09-03 23:57:42 +0100

    Remove now redundant code.
    It uses the enum from typekind.cx

--- src/Compilers/VisualBasic/Portable/Symbols/TypeKind.vb
+++ src/Compilers/VisualBasic/Portable/Symbols/TypeKind.vb
@@ -1,34 +1,8 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 Imports System.Runtime.CompilerServices
-Imports Microsoft.CodeAnalysis.Text
-Imports Microsoft.CodeAnalysis.VisualBasic.Symbols
-Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.Symbols
-
-#If False Then
-    ''' <summary>
-    ''' Represents the different kinds of types.
-    ''' </summary>
-    Public Enum TypeKind
-        Unknown = 0
-
-        ArrayType = 1
-        [Class] = 2
-        [Delegate] = 3
-        ' DynamicType = 4
-        [Enum] = 5
-        [Error] = 6
-        [Interface] = 7
-        [Module] = 8
-        ' PointerType = 9
-        [Structure] = 10
-        TypeParameter = 11
-
-        Submission = 12
-    End Enum
-#End If
 
     Friend Module EnumConversions
         <Extension()>
