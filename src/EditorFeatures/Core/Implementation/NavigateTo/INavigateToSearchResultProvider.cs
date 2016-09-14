4c0f1b179e4b1e5de1a4be767648046c2234e551
CyrusNajmabadi <cyrusn@microsoft.com>
2016-09-14 12:56:04 -0700

    Reference latest VS binaries so we can consume them in our features.

--- src/EditorFeatures/Core/Implementation/NavigateTo/INavigateToSearchResultProvider.cs
+++ src/EditorFeatures/Core/Implementation/NavigateTo/INavigateToSearchResultProvider.cs
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,6 +11,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.NavigateTo
         /// <summary>
         /// Compute navigate to search results for the given search pattern in the given project.
         /// </summary>
-        Task<IEnumerable<INavigateToSearchResult>> SearchProjectAsync(Project project, string searchPattern, CancellationToken cancellationToken);
+        Task<ImmutableArray<INavigateToSearchResult>> SearchProjectAsync(Project project, string searchPattern, CancellationToken cancellationToken);
+        Task<ImmutableArray<INavigateToSearchResult>> SearchDocumentAsync(Document document, string searchPattern, CancellationToken cancellationToken);
     }
 }
