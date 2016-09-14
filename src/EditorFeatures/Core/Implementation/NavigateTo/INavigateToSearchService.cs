71357b384b2ca5cd64dbef59839070d9cb64e8d6
CyrusNajmabadi <cyrusn@microsoft.com>
2016-09-14 12:56:04 -0700

    Reference latest VS binaries so we can consume them in our features.

--- src/EditorFeatures/Core/Implementation/NavigateTo/INavigateToSearchService.cs
+++ src/EditorFeatures/Core/Implementation/NavigateTo/INavigateToSearchService.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
@@ -9,7 +10,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.NavigateTo
 {
     internal interface INavigateToSearchService : ILanguageService
     {
-        // TODO(cyrusn): Should this be an IAsyncEnumerable instead?
-        Task<IEnumerable<INavigateToSearchResult>> SearchProjectAsync(Project project, string searchPattern, CancellationToken cancellationToken);
+        Task<ImmutableArray<INavigateToSearchResult>> SearchProjectAsync(Project project, string searchPattern, CancellationToken cancellationToken);
+        Task<ImmutableArray<INavigateToSearchResult>> SearchDocumentAsync(Document document, string searchPattern, CancellationToken cancellationToken);
     }
 }
