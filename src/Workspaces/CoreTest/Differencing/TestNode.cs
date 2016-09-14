c38dae7eba01351ff35c949f466e210c5ef30efd
CyrusNajmabadi <cyrusn@microsoft.com>
2016-09-14 12:00:46 -0700

    Add tests back in.

---
+++ src/Workspaces/CoreTest/Differencing/TestNode.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.Differencing.UnitTests
+{
+    public class TestNode
+    {
+        public const int MaxValue = 10;
+        public const int MaxLabel = 1;
+
+        public readonly int Label;
+        public readonly int Value;
+        public readonly TestNode[] Children;
+        public TestNode Parent;
+
+        private TestNode _lazyRoot;
+
+        public TestNode(int label, int value, params TestNode[] children)
+        {
+            Debug.Assert(value >= 0 && value <= MaxValue);
+            Debug.Assert(label >= 0 && label <= MaxLabel);
+
+            this.Label = label;
+            this.Value = value;
+            this.Children = children;
+
+            foreach (var child in children)
+            {
+                child.Parent = this;
+            }
+        }
+
+        public TestNode Root
+        {
+            get
+            {
+                if (_lazyRoot == null)
+                {
+                    _lazyRoot = this.Parent == null ? this : this.Parent.Root;
+                }
+
+                return _lazyRoot;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", Label, Value);
+        }
+    }
+}
---
+++ src/Workspaces/CoreTest/Differencing/TestTreeComparer.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.Differencing.UnitTests
+{
+    public class TestTreeComparer : TreeComparer<TestNode>
+    {
+        public static readonly TestTreeComparer Instance = new TestTreeComparer();
+
+        private TestTreeComparer()
+        {
+        }
+
+        protected internal override int LabelCount
+        {
+            get
+            {
+                return TestNode.MaxLabel + 1;
+            }
+        }
+
+        public override double GetDistance(TestNode left, TestNode right)
+        {
+            return Math.Abs((double)left.Value - right.Value) / TestNode.MaxValue;
+        }
+
+        public override bool ValuesEqual(TestNode oldNode, TestNode newNode)
+        {
+            return oldNode.Value == newNode.Value;
+        }
+
+        protected internal override IEnumerable<TestNode> GetChildren(TestNode node)
+        {
+            return node.Children;
+        }
+
+        protected internal override IEnumerable<TestNode> GetDescendants(TestNode node)
+        {
+            yield return node;
+
+            foreach (var child in node.Children)
+            {
+                foreach (var descendant in GetDescendants(child))
+                {
+                    yield return descendant;
+                }
+            }
+        }
+
+        protected internal override int GetLabel(TestNode node)
+        {
+            return node.Label;
+        }
+
+        protected internal override TextSpan GetSpan(TestNode node)
+        {
+            return new TextSpan(0, 10);
+        }
+
+        protected internal override int TiedToAncestor(int label)
+        {
+            return 0;
+        }
+
+        protected internal override bool TreesEqual(TestNode left, TestNode right)
+        {
+            return left.Root == right.Root;
+        }
+
+        protected internal override bool TryGetParent(TestNode node, out TestNode parent)
+        {
+            parent = node.Parent;
+            return parent != null;
+        }
+    }
+}
