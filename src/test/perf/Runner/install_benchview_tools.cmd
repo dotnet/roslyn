5596699e4d3ba50484257d393e3c8f340bd02935
John Soklaski <jsokla@microsoft.com>
2016-09-14 17:06:37 -0700

    Acquire BenchView Tools using Perf Runner project

---
+++ src/test/perf/Runner/install_benchview_tools.cmd
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+set "OUTPUT_DIR=%~1"
+if "%OUTPUT_DIR%" == "" (echo Usage: build_benchview_tools.cmd ^<output directory^> & exit /b 1)
+
+call "%~dp0clean_benchview_tools.cmd" "%OUTPUT_DIR%" || exit /b 1
+
+:: Installing BenchView Tools
+call "%OUTPUT_DIR%\..\..\build\scripts\LoadNuGetInfo.cmd" || (echo Failed to load nuget. & exit /b 1)
+
+nuget install Microsoft.BenchView.JSONFormat -Source http://benchviewtestfeed.azurewebsites.net/nuget -OutputDirectory "%OUTPUT_DIR%." -Prerelease -ExcludeVersion 1>NUL 2>&1 || echo Failed to install nuget package
+
+exit /b 0
