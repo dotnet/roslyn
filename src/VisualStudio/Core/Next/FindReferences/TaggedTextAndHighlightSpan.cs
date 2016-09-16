30a9593e20ec1d37fd5149ba28f3e1007e78d1ec
CyrusNajmabadi <cyrusn@microsoft.com>
2016-09-16 14:09:41 -0700

    Sort usings.

--- src/VisualStudio/Core/Next/FindReferences/TaggedTextAndHighlightSpan.cs
+++ src/VisualStudio/Core/Next/FindReferences/TaggedTextAndHighlightSpan.cs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.VisualStudio.LanguageServices.FindReferences
 {
