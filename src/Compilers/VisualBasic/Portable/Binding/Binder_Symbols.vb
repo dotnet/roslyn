127ca09546c877829efa807a40572fce35d1f064
AlekseyTs <alekseyt@microsoft.com>
2016-09-17 15:58:45 -0700

    Fix name lookup through an unbound generic type.
    Fixes #9850.

--- src/Compilers/VisualBasic/Portable/Binding/Binder_Symbols.vb
+++ src/Compilers/VisualBasic/Portable/Binding/Binder_Symbols.vb
@@ -1047,6 +1047,15 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
                                 LookupOptions.AttributeTypeOnly,
                                 useSiteDiagnostics)
                 Else
+                    Dim isLeftUnboundGenericType As Boolean = leftSymbol.Kind = SymbolKind.NamedType AndAlso DirectCast(leftSymbol, NamedTypeSymbol).IsUnboundGenericType
+
+                    If isLeftUnboundGenericType Then
+                        ' If left name bound to an unbound generic type,
+                        ' we want to perform right name lookup within
+                        ' left's original named type definition.
+                        leftSymbol = DirectCast(leftSymbol, NamedTypeSymbol).OriginalDefinition
+                    End If
+
                     binder.LookupMember(lookupResult,
                                         leftSymbol,
                                         rightIdentToken.ValueText,
@@ -1057,7 +1066,13 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
                     If lookupResult.HasSingleSymbol AndAlso lookupResult.SingleSymbol.Kind = SymbolKind.NamedType Then
                         Dim namedType = DirectCast(lookupResult.SingleSymbol, NamedTypeSymbol)
 
-                        If namedType.Arity > 0 AndAlso Not namedType.IsDefinition AndAlso namedType Is namedType.ConstructedFrom Then
+                        ' If left name bound to an unbound generic type
+                        ' and right name bound to a generic type, we must
+                        ' convert right to an unbound generic type.
+                        If isLeftUnboundGenericType AndAlso namedType.IsGenericType Then
+                            lookupResult.ReplaceSymbol(namedType.AsUnboundGenericType())
+
+                        ElseIf namedType.Arity > 0 AndAlso Not namedType.IsDefinition AndAlso namedType Is namedType.ConstructedFrom Then
                             Debug.Assert(lookupResult.HasDiagnostic)
 
                             ' Note: this preserves any error associated with the generic type, which is what we want.
@@ -1109,6 +1124,14 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
                 End If
 
                 Dim leftSymbol As NamespaceOrTypeSymbol = DirectCast(lookupResult.SingleSymbol, NamespaceOrTypeSymbol)
+                Dim isLeftUnboundGenericType As Boolean = leftSymbol.Kind = SymbolKind.NamedType AndAlso DirectCast(leftSymbol, NamedTypeSymbol).IsUnboundGenericType
+
+                If isLeftUnboundGenericType Then
+                    ' If left name bound to an unbound generic type,
+                    ' we want to perform right name lookup within
+                    ' left's original named type definition.
+                    leftSymbol = DirectCast(leftSymbol, NamedTypeSymbol).OriginalDefinition
+                End If
 
                 binder.ReportDiagnosticsIfObsolete(diagBag, leftSymbol, leftNameSyntax)
 
@@ -1135,6 +1158,13 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
                 If genericType Is Nothing Then
                     ' no symbol or symbol is a namespace
                     Return
+
+                ElseIf isLeftUnboundGenericType AndAlso genericType.IsGenericType Then
+                    ' If left name bound to an unbound generic type
+                    ' and right name bound to a generic type, we must
+                    ' convert right to an unbound generic type.
+                    lookupResult.ReplaceSymbol(genericType.AsUnboundGenericType())
+
                 Else
                     ' Construct the type and validate constraints.
                     Dim constructedType = binder.ConstructAndValidateConstraints(
