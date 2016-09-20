227a8ceacd090447f0cc98c84c90e1033d92600b
Remy Willems <remy.willems@ortec-finance.com>
2016-09-21 00:19:57 +0200

    Removed unused imports.

--- src/Compilers/Core/CodeAnalysisTest/CommonCompilationOptionsTests.cs
+++ src/Compilers/Core/CodeAnalysisTest/CommonCompilationOptionsTests.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis.CSharp;
 using Roslyn.Test.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.UnitTests
