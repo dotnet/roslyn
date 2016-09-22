ea25e66882536f75e26b11a517c6e2ecd26907d0
CyrusNajmabadi <cyrusn@microsoft.com>
2016-09-22 14:05:25 -0700

    Fix tests.

--- src/EditorFeatures/CSharpTest/Completion/CompletionProviders/CrefCompletionProviderTests.cs
+++ src/EditorFeatures/CSharpTest/Completion/CompletionProviders/CrefCompletionProviderTests.cs
@@ -440,72 +440,7 @@ class C
                     ImmutableArray.Create<CompletionProvider>(provider));
                 var completionList = await GetCompletionListAsync(service, document, hostDocument.CursorPosition.Value, CompletionTrigger.Default);
 
-               Assert.True(called);
-            }
-
-            public SyntaxNode GetObjectCreationInitializer(SyntaxNode objectCreationExpression)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool IsSimpleAssignmentStatement(SyntaxNode statement)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void GetPartsOfAssignmentStatement(SyntaxNode statement, out SyntaxNode left, out SyntaxNode right)
-            {
-                throw new NotImplementedException();
-            }
-
-            public SyntaxNode GetNameOfMemberAccessExpression(SyntaxNode memberAccessExpression)
-            {
-                throw new NotImplementedException();
-            }
-
-            public SyntaxToken GetOperatorTokenOfMemberAccessExpression(SyntaxNode memberAccessExpression)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool IsSimpleMemberAccessExpression(SyntaxNode node)
-            {
-                throw new NotImplementedException();
-            }
-
-            public SyntaxToken GetIdentifierOfSimpleName(SyntaxNode node)
-            {
-                throw new NotImplementedException();
-            }
-
-            public SyntaxToken GetIdentifierOfVariableDeclarator(SyntaxNode node)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool IsIdentifierName(SyntaxNode node)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool IsLocalDeclarationStatement(SyntaxNode node)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool IsDeclaratorOfLocalDeclarationStatement(SyntaxNode declator, SyntaxNode localDeclarationStatement)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool AreEquivalent(SyntaxToken token1, SyntaxToken token2)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool AreEquivalent(SyntaxNode node1, SyntaxNode node2)
-            {
-                throw new NotImplementedException();
+                Assert.True(called);
             }
         }
     }
