af5b6d8e6f8bc238fa301e2cf9a6147e2e871db6
CyrusNajmabadi <cyrusn@microsoft.com>
2016-09-24 22:47:38 -0700

    move type.

--- src/Workspaces/Core/Desktop/SymbolSearch/ISymbolSearchUpdateEngineFactory.cs
+++ src/Workspaces/Core/Desktop/SymbolSearch/ISymbolSearchUpdateEngineFactory.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Composition;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
 
 namespace Microsoft.CodeAnalysis.SymbolSearch
 {
@@ -9,5 +11,16 @@ namespace Microsoft.CodeAnalysis.SymbolSearch
     {
         Task<ISymbolSearchUpdateEngine> CreateEngineAsync(
             Workspace workspace, ISymbolSearchLogService logService);
+    }
+
+    [ExportWorkspaceService(typeof(ISymbolSearchUpdateEngineFactory)), Shared]
+    internal class DefaultSymbolSearchUpdateEngineFactory : ISymbolSearchUpdateEngineFactory
+    {
+        public Task<ISymbolSearchUpdateEngine> CreateEngineAsync(
+            Workspace workspace, ISymbolSearchLogService logService)
+        {
+            return Task.FromResult<ISymbolSearchUpdateEngine>(
+                new SymbolSearchUpdateEngine(logService));
+        }
     }
 }
