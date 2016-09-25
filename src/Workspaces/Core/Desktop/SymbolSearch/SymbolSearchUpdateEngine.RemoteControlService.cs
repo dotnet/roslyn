b81e3004f28daac0909a520916d612997d5fc1bf
CyrusNajmabadi <cyrusn@microsoft.com>
2016-09-25 01:50:03 -0700

    Add comment.

--- src/Workspaces/Core/Desktop/SymbolSearch/SymbolSearchUpdateEngine.RemoteControlService.cs
+++ src/Workspaces/Core/Desktop/SymbolSearch/SymbolSearchUpdateEngine.RemoteControlService.cs
@@ -10,6 +10,8 @@ namespace Microsoft.CodeAnalysis.SymbolSearch
         {
             public IRemoteControlClient CreateClient(string hostId, string serverPath, int pollingMinutes)
             {
+                // BaseUrl provided by the VS RemoteControl client team.  This is URL we are supposed
+                // to use to publish and access data from.
                 const string BaseUrl = "https://az700632.vo.msecnd.net/pub";
 
                 return new RemoteControlClient(hostId, BaseUrl, serverPath, pollingMinutes);
