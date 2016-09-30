68f8d7f5126dab934437236671caa0e718836780
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2016-09-30 14:35:56 -0300

    Embed a local definition of IVsSolution7 to avoid loading .Next dll early

---
+++ src/VisualStudio/Core/Def/Implementation/ProjectSystem/Interop/IVsSolution7.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem.Interop
+{
+    [ComImport]
+    [Guid("D32B0C42-8AEE-4772-B5C3-04565CDA5A47")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IVsSolution7
+    {
+        void OpenFolder([MarshalAs(UnmanagedType.LPWStr)]string folderPath);
+        void CloseFolder([MarshalAs(UnmanagedType.LPWStr)]string folderPath);
+        bool IsSolutionLoadDeferred();
+    }
+}
