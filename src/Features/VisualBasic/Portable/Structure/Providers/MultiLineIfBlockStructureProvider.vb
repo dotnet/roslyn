53d22da15fe17573251aa28125b8f089ba3cbef4
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-04 10:58:39 -0700

    Fix block type.

--- src/Features/VisualBasic/Portable/Structure/Providers/MultiLineIfBlockStructureProvider.vb
+++ src/Features/VisualBasic/Portable/Structure/Providers/MultiLineIfBlockStructureProvider.vb
@@ -13,7 +13,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Structure
                                                   cancellationToken As CancellationToken)
             spans.AddIfNotNull(CreateRegionFromBlock(
                                node, node.IfStatement, autoCollapse:=False,
-                               type:=BlockTypes.Using, isCollapsible:=True))
+                               type:=BlockTypes.Conditional, isCollapsible:=True))
         End Sub
     End Class
 End Namespace
