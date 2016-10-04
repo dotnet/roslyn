f0129d1fe68c58f0bd1250773251fa9ec5771b07
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-03 18:47:05 -0700

    Move code.

--- src/EditorFeatures/Next/Structure/VisualStudio15StructureTaggerProvider.cs
+++ src/EditorFeatures/Next/Structure/VisualStudio15StructureTaggerProvider.cs
@@ -45,55 +45,6 @@ namespace Microsoft.CodeAnalysis.Editor.Structure
                 parentTag, snapshot, region);
         }
 
-        private static string ConvertType(string type)
-        {
-            switch (type)
-            {
-                case BlockTypes.Nonstructural: return PredefinedStructureTypes.Nonstructural;
-
-                // Top level declarations.  Note that Enum is not currently supported
-                // and that we map Module down to Class.
-                case BlockTypes.Namespace: return PredefinedStructureTypes.Namespace;
-                case BlockTypes.Structure: return PredefinedStructureTypes.Struct;
-                case BlockTypes.Interface: return PredefinedStructureTypes.Interface;
-                case BlockTypes.Module:
-                case BlockTypes.Class: return PredefinedStructureTypes.Class;
-
-                // Member declarations
-                case BlockTypes.Accessor: return PredefinedStructureTypes.AccessorBlock;
-                case BlockTypes.Constructor: return PredefinedStructureTypes.Constructor;
-                case BlockTypes.Destructor: return PredefinedStructureTypes.Destructor;
-                case BlockTypes.Method: return PredefinedStructureTypes.Method;
-                case BlockTypes.Operator: return PredefinedStructureTypes.Operator;
-
-                // Map events/indexers/properties all to the 'property' type.
-                case BlockTypes.Event:
-                case BlockTypes.Indexer:
-                case BlockTypes.Property: return PredefinedStructureTypes.PropertyBlock;
-
-                // Statements
-                case BlockTypes.Case: return PredefinedStructureTypes.Case;
-                case BlockTypes.Conditional: return PredefinedStructureTypes.Conditional;
-                case BlockTypes.Lock: return PredefinedStructureTypes.Lock;
-                case BlockTypes.Loop: return PredefinedStructureTypes.Loop;
-                case BlockTypes.TryCatchFinally: return PredefinedStructureTypes.TryCatchFinally;
-                case BlockTypes.Standalone: return PredefinedStructureTypes.Standalone;
-
-                // Expressions
-                case BlockTypes.AnonymousMethod: return PredefinedStructureTypes.AnonymousMethodBlock;
-
-                // These types don't currently map to any editor types.  Just make them
-                // the 'Unknown' type for now.
-                case BlockTypes.Enum:
-                case BlockTypes.Xml:
-                case BlockTypes.LocalFunction:
-                case BlockTypes.Using:
-                case BlockTypes.Switch:
-                default:
-                    return PredefinedStructureTypes.Unknown;
-            }
-        }
-
         private class RoslynBlockTag : RoslynOutliningRegionTag, IBlockTag
         {
             public IBlockTag Parent { get; }
@@ -153,6 +104,55 @@ namespace Microsoft.CodeAnalysis.Editor.Structure
                        Hash.Combine(this.Type, 
                        Hash.Combine(this.CollapsedForm,
                        Hash.Combine(this.StatementSpan.GetHashCode(), this.Span.GetHashCode())))));
+            }
+
+            private static string ConvertType(string type)
+            {
+                switch (type)
+                {
+                    case BlockTypes.Nonstructural: return PredefinedStructureTypes.Nonstructural;
+
+                    // Top level declarations.  Note that Enum is not currently supported
+                    // and that we map Module down to Class.
+                    case BlockTypes.Namespace: return PredefinedStructureTypes.Namespace;
+                    case BlockTypes.Structure: return PredefinedStructureTypes.Struct;
+                    case BlockTypes.Interface: return PredefinedStructureTypes.Interface;
+                    case BlockTypes.Module:
+                    case BlockTypes.Class: return PredefinedStructureTypes.Class;
+
+                    // Member declarations
+                    case BlockTypes.Accessor: return PredefinedStructureTypes.AccessorBlock;
+                    case BlockTypes.Constructor: return PredefinedStructureTypes.Constructor;
+                    case BlockTypes.Destructor: return PredefinedStructureTypes.Destructor;
+                    case BlockTypes.Method: return PredefinedStructureTypes.Method;
+                    case BlockTypes.Operator: return PredefinedStructureTypes.Operator;
+
+                    // Map events/indexers/properties all to the 'property' type.
+                    case BlockTypes.Event:
+                    case BlockTypes.Indexer:
+                    case BlockTypes.Property: return PredefinedStructureTypes.PropertyBlock;
+
+                    // Statements
+                    case BlockTypes.Case: return PredefinedStructureTypes.Case;
+                    case BlockTypes.Conditional: return PredefinedStructureTypes.Conditional;
+                    case BlockTypes.Lock: return PredefinedStructureTypes.Lock;
+                    case BlockTypes.Loop: return PredefinedStructureTypes.Loop;
+                    case BlockTypes.TryCatchFinally: return PredefinedStructureTypes.TryCatchFinally;
+                    case BlockTypes.Standalone: return PredefinedStructureTypes.Standalone;
+
+                    // Expressions
+                    case BlockTypes.AnonymousMethod: return PredefinedStructureTypes.AnonymousMethodBlock;
+
+                    // These types don't currently map to any editor types.  Just make them
+                    // the 'Unknown' type for now.
+                    case BlockTypes.Enum:
+                    case BlockTypes.Xml:
+                    case BlockTypes.LocalFunction:
+                    case BlockTypes.Using:
+                    case BlockTypes.Switch:
+                    default:
+                        return PredefinedStructureTypes.Unknown;
+                }
             }
         }
     }
