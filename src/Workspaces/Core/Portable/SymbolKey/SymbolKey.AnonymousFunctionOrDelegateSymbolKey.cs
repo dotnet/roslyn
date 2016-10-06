99c79290bd677de585d05e6a07affc40a2608f1d
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-06 15:25:40 -0700

    Add comments.

--- src/Workspaces/Core/Portable/SymbolKey/SymbolKey.AnonymousFunctionOrDelegateSymbolKey.cs
+++ src/Workspaces/Core/Portable/SymbolKey/SymbolKey.AnonymousFunctionOrDelegateSymbolKey.cs
@@ -20,6 +20,12 @@ namespace Microsoft.CodeAnalysis
             {
                 Debug.Assert(symbol.IsAnonymousDelegateType() || symbol.IsAnonymousFunction());
 
+                // Write out if this was an anonymous delegate or anonymous function.
+                // In both cases they'll have the same location (the location of 
+                // the lambda that forced them into existence).  When we resolve the
+                // symbol later, if it's an anonymous delegate, we'll first resolve to
+                // the anonymous-function, then use that anonymous-functoin to get at
+                // the synthesized anonymous delegate.
                 visitor.WriteBoolean(symbol.IsAnonymousDelegateType());
                 visitor.WriteLocation(symbol.Locations.FirstOrDefault());
             }
@@ -30,25 +36,28 @@ namespace Microsoft.CodeAnalysis
                 var location = reader.ReadLocation();
 
                 var syntaxTree = location.SourceTree;
-                if (syntaxTree != null)
+                if (syntaxTree == null)
                 {
-                    var semanticModel = reader.Compilation.GetSemanticModel(syntaxTree);
-                    var root = syntaxTree.GetRoot(reader.CancellationToken);
-                    var node = root.FindNode(location.SourceSpan, getInnermostNodeForTie: true);
-
-                    var symbol = semanticModel.GetSymbolInfo(node, reader.CancellationToken)
-                                              .GetAnySymbol();
-
-                    if (isAnonymousDelegateType)
-                    {
-                        var anonymousDelegate = (symbol as IMethodSymbol).AssociatedAnonymousDelegate;
-                        symbol = anonymousDelegate;
-                    }
-
-                    return new SymbolKeyResolution(symbol);
+                    return default(SymbolKeyResolution);
                 }
 
-                return default(SymbolKeyResolution);
+                var semanticModel = reader.Compilation.GetSemanticModel(syntaxTree);
+                var root = syntaxTree.GetRoot(reader.CancellationToken);
+                var node = root.FindNode(location.SourceSpan, getInnermostNodeForTie: true);
+
+                var symbol = semanticModel.GetSymbolInfo(node, reader.CancellationToken)
+                                          .GetAnySymbol();
+
+                // If this was a key for an anonymous delegate type, then go find the
+                // associated delegate for this lambda and return that instead of the 
+                // lambda function symbol itself.
+                if (isAnonymousDelegateType)
+                {
+                    var anonymousDelegate = (symbol as IMethodSymbol).AssociatedAnonymousDelegate;
+                    symbol = anonymousDelegate;
+                }
+
+                return new SymbolKeyResolution(symbol);
             }
         }
     }
