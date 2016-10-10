e5fbfbc85aabda85e8bfbf36469306ac44696156
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-09 23:45:47 -0700

    Add tests.

---
+++ src/EditorFeatures/CSharpTest/UseExpressionBody/UseExpressionBodyForMethodsTests.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.UseExpressionBody;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics;
+using Microsoft.CodeAnalysis.Options;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.UseExpressionBody
+{
+    public class UseExpressionBodyForMethodsTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
+    {
+        internal override Tuple<DiagnosticAnalyzer, CodeFixProvider> CreateDiagnosticProviderAndFixer(Workspace workspace)
+            => new Tuple<DiagnosticAnalyzer, CodeFixProvider>(
+                new UseExpressionBodyForMethodsDiagnosticAnalyzer(),
+                new UseExpressionBodyForMethodsCodeFixProvider());
+
+        private static readonly Dictionary<OptionKey, object> UseExpressionBody =
+            new Dictionary<OptionKey, object>
+            {
+                { CSharpCodeStyleOptions.PreferExpressionBodiedMethods, CodeStyleOptions.trueWithNoneEnforcement }
+            };
+
+        private static readonly Dictionary<OptionKey, object> UseBlockBody =
+            new Dictionary<OptionKey, object>
+            {
+                { CSharpCodeStyleOptions.PreferExpressionBodiedMethods, CodeStyleOptions.falseWithNoneEnforcement }
+            };
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseExpressionBody)]
+        public async Task TestUseExpressionBody1()
+        {
+            await TestAsync(
+@"class C
+{
+    void Foo()
+    {
+        [|Bar|]();
+    }
+}",
+@"class C
+{
+    void Foo() => Bar();
+}", options: UseExpressionBody);
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseExpressionBody)]
+        public async Task TestUseExpressionBody2()
+        {
+            await TestAsync(
+@"class C
+{
+    int Foo()
+    {
+        return [|Bar|]();
+    }
+}",
+@"class C
+{
+    int Foo() => Bar();
+}", options: UseExpressionBody);
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseExpressionBody)]
+        public async Task TestUseExpressionBody3()
+        {
+            await TestAsync(
+@"class C
+{
+    int Foo()
+    {
+        [|throw|] new NotImplementedException();
+    }
+}",
+@"class C
+{
+    int Foo() => throw new NotImplementedException();
+}", options: UseExpressionBody);
+        }
+    }
+}
