b29c22548f67d91938581877f671fc81842796b7
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-11 15:05:38 -0700

    Use reflection to call into the right helper.

--- src/VisualStudio/Core/Def/Experimentation/VisualStudioExperimentationService.cs
+++ src/VisualStudio/Core/Def/Experimentation/VisualStudioExperimentationService.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Composition;
+using System.Reflection;
 using Microsoft.CodeAnalysis.Experiments;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.Internal.VisualStudio.Shell.Interop;
@@ -11,7 +12,8 @@ namespace Microsoft.VisualStudio.LanguageServices.Experimentation
     [ExportWorkspaceService(typeof(IExperimentationService), ServiceLayer.Host), Shared]
     internal class VisualStudioExperimentationService : IExperimentationService
     {
-        private readonly IVsExperimentationService _experimentationServiceOpt;
+        private readonly object _experimentationServiceOpt;
+        private readonly MethodInfo _isCachedFlightEnabledInfo;
 
         [ImportingConstructor]
         public VisualStudioExperimentationService(
@@ -19,7 +21,12 @@ namespace Microsoft.VisualStudio.LanguageServices.Experimentation
         {
             try
             {
-                _experimentationServiceOpt = (IVsExperimentationService)serviceProvider.GetService(typeof(SVsServiceProvider));
+                _experimentationServiceOpt = serviceProvider.GetService(typeof(SVsExperimentationService));
+                if (_experimentationServiceOpt != null)
+                {
+                    _isCachedFlightEnabledInfo = _experimentationServiceOpt.GetType().GetMethod(
+                        "IsCachedFlightEnabled", BindingFlags.Public | BindingFlags.Instance);
+                }
             }
             catch
             {
@@ -27,6 +34,20 @@ namespace Microsoft.VisualStudio.LanguageServices.Experimentation
         }
 
         public bool IsExperimentEnabled(string experimentName)
-            => _experimentationServiceOpt?.IsCachedFlightEnabled(experimentName) ?? false;
+        {
+            if (_isCachedFlightEnabledInfo != null)
+            {
+                try
+                {
+                    return (bool)_isCachedFlightEnabledInfo.Invoke(_experimentationServiceOpt, new object[] { experimentName });
+                }
+                catch
+                {
+
+                }
+            }
+
+            return false;
+        }
     }
 }
