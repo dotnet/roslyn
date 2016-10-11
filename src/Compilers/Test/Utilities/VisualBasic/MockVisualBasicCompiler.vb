0342d70a9caa013377c8e05792f0a1c8e31a7b8a
Jared Parsons <jaredpar@microsoft.com>
2016-10-11 15:58:45 -0700

    Fix the VB command line unit tests

--- src/Compilers/Test/Utilities/VisualBasic/MockVisualBasicCompiler.vb
+++ src/Compilers/Test/Utilities/VisualBasic/MockVisualBasicCompiler.vb
@@ -11,16 +11,16 @@ Friend Class MockVisualBasicCompiler
     Private ReadOnly _analyzers As ImmutableArray(Of DiagnosticAnalyzer)
     Public Compilation As Compilation
 
-    Public Sub New(baseDirectory As String, tempDirectory As String, args As String(), Optional analyzer As DiagnosticAnalyzer = Nothing)
-        MyClass.New(Nothing, baseDirectory, tempDirectory, args, analyzer)
+    Public Sub New(baseDirectory As String, args As String(), Optional analyzer As DiagnosticAnalyzer = Nothing)
+        MyClass.New(Nothing, baseDirectory, args, analyzer)
     End Sub
 
-    Public Sub New(responseFile As String, baseDirectory As String, tempDirectory As String, args As String(), Optional analyzer As DiagnosticAnalyzer = Nothing)
-        MyClass.New(responseFile, baseDirectory, tempDirectory, args, If(analyzer Is Nothing, ImmutableArray(Of DiagnosticAnalyzer).Empty, ImmutableArray.Create(analyzer)))
+    Public Sub New(responseFile As String, baseDirectory As String, args As String(), Optional analyzer As DiagnosticAnalyzer = Nothing)
+        MyClass.New(responseFile, baseDirectory, args, If(analyzer Is Nothing, ImmutableArray(Of DiagnosticAnalyzer).Empty, ImmutableArray.Create(analyzer)))
     End Sub
 
-    Public Sub New(responseFile As String, workingDirectory As String, tempDirectory As String, args As String(), analyzers As ImmutableArray(Of DiagnosticAnalyzer))
-        MyBase.New(VisualBasicCommandLineParser.Default, responseFile, args, CreateBuildPaths(workingDirectory, tempDirectory), Environment.GetEnvironmentVariable("LIB"), New DesktopAnalyzerAssemblyLoader())
+    Public Sub New(responseFile As String, workingDirectory As String, args As String(), analyzers As ImmutableArray(Of DiagnosticAnalyzer))
+        MyBase.New(VisualBasicCommandLineParser.Default, responseFile, args, CreateBuildPaths(workingDirectory, Path.GetTempPath()), Environment.GetEnvironmentVariable("LIB"), New DesktopAnalyzerAssemblyLoader())
 
         _analyzers = analyzers
     End Sub
