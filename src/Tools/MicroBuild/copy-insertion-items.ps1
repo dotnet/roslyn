d779e4a2afb4829f6bc0328658d6a47e6886e487
Jared Parsons <jaredpar@microsoft.com>
2016-10-12 09:13:59 -0700

    Build some projects into sub directories
    Build VisualStudio.Setup.Next and XAML projects in separate sub directories.
    
    This fixes the problem of conflicting assembly references during build.
    
    Since the default for Roslyn is to have all projects build and dump their targets and dependencies into the same build directory, if the projects have conflicting dependency versions then build order and parallel builds can stomp on each other causing build failures.
    
    This change causes the two projects that dependencies on Dev15 versions of VS assemblies to build into sub directories, so their new dependencies don't pollute the primary build directory.

--- src/Tools/MicroBuild/copy-insertion-items.ps1
+++ src/Tools/MicroBuild/copy-insertion-items.ps1
@@ -9,9 +9,9 @@ try
 {
     $items = @(
         "ExpressionEvaluatorPackage.vsix",
-        "Roslyn.VisualStudio.InteractiveComponents.vsix",
-        "Roslyn.VisualStudio.Setup.Next.vsix",
-        "Roslyn.VisualStudio.Setup.vsix",
+        "VisualStudioInteractiveComponents\Roslyn.VisualStudio.InteractiveComponents.vsix",
+        "VisualStudioSetup.Next\Roslyn.VisualStudio.Setup.Next.vsix",
+        "VisualStudioSetup\Roslyn.VisualStudio.Setup.vsix",
         "Microsoft.CodeAnalysis.ExpressionEvaluator.json",
         "Microsoft.CodeAnalysis.VisualStudio.Setup.json",
         "Microsoft.CodeAnalysis.VisualStudio.Setup.Next.json",
