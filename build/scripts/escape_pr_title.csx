2f90d815bb338dd1fe0d400eccc3516df4ab0345
Ty Overby <ty@pre-alpha.com>
2016-10-17 14:28:38 -0700

    Switch escape mechanism

--- build/scripts/escape_pr_title.csx
+++
@@ -1,3 +0,0 @@
-var prTitle = System.Environment.GetEnvironmentVariable("ghprbPullTitle");
-var escaped = String.Join("", prTitle.Where(c => char.IsLetterOrDigit(c) || c == ' '));
-Console.Write(escaped);
--- cibuild.cmd
+++ cibuild.cmd
@@ -6,14 +6,9 @@ set RoslynRoot=%~dp0
 set BuildConfiguration=Debug
 
 REM Because override the C#/VB toolset to build against our LKG package, it is important
-REM that we do not reuse MSBuild nodes from other jobs/builds on the machine. Otherwise, 
+REM that we do not reuse MSBuild nodes from other jobs/builds on the machine. Otherwise,
 REM we'll run into issues such as https://github.com/dotnet/roslyn/issues/6211.
 set MSBuildAdditionalCommandLineArgs=/nologo /m /nodeReuse:false /consoleloggerparameters:Verbosity=minimal /filelogger /fileloggerparameters:Verbosity=normal
-
-REM Github pull request titles are unescaped, so we need to run another program to
-REM do the escaping and replace it.
-REM for /f "tokens=*" %%i in ('csi .\build\scripts\escape_pr_title.csx') do set "ghprbPullTitle=%%i"
-REM echo "PULL REQUEST TITLE: %ghprbPullTitle%"
 
 :ParseArguments
 if "%1" == "" goto :DoneParsing
@@ -58,7 +53,7 @@ REM Output the commit that we're building, for reference in Jenkins logs
 echo Building this commit:
 git show --no-patch --pretty=raw HEAD
 
-REM Restore the NuGet packages 
+REM Restore the NuGet packages
 call "%RoslynRoot%\Restore.cmd" || goto :BuildFailed
 
 REM Ensure the binaries directory exists because msbuild can fail when part of the path to LogFile isn't present.
