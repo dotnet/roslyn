3ae31b361080775abbb03a5f7e0ba1e1d24e6f03
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-17 17:10:11 -0700

    Remove strange inheritance hierarchies.

--- src/EditorFeatures/Core/Implementation/Suggestions/CodeFixSuggestedAction.cs
+++ src/EditorFeatures/Core/Implementation/Suggestions/CodeFixSuggestedAction.cs
@@ -4,23 +4,29 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Globalization;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Editor.Host;
+using Microsoft.CodeAnalysis.Extensions;
 using Microsoft.CodeAnalysis.Shared.TestHooks;
 using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Editor.Implementation.Suggestions
 {
     /// <summary>
     /// Represents light bulb menu item for code fixes.
     /// </summary>
-    internal class CodeFixSuggestedAction : SuggestedActionWithFlavors, ITelemetryDiagnosticID<string>
+    internal sealed class CodeFixSuggestedAction : SuggestedAction, ISuggestedActionWithFlavors, ITelemetryDiagnosticID<string>
     {
         private readonly CodeFix _fix;
-        private readonly SuggestedActionSet _fixAllSuggestedActionSet;
+        public SuggestedActionSet FixAllSuggestedActionSet { get; }
+
+        private ImmutableArray<SuggestedActionSet> _actionSets;
 
         public CodeFixSuggestedAction(
             Workspace workspace,
@@ -35,7 +41,83 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Suggestions
             : base(workspace, subjectBuffer, editHandler, waitIndicator, action, provider, operationListener)
         {
             _fix = fix;
-            _fixAllSuggestedActionSet = fixAllSuggestedActionSet;
+            FixAllSuggestedActionSet = fixAllSuggestedActionSet;
+        }
+
+        public override bool HasActionSets
+        {
+            get
+            {
+                // HasActionSets is called synchronously on the UI thread. In order to avoid blocking the UI thread,
+                // we need to provide a 'quick' answer here as opposed to the 'right' answer. Providing the 'right'
+                // answer is expensive (because we will need to call CodeAction.GetPreviewOperationsAsync() (to
+                // compute whether or not we should display the flavored action for 'Preview Changes') which in turn
+                // will involve computing the changed solution for the ApplyChangesOperation for the fix / refactoring
+                // So we always return 'true' here (so that platform will call GetActionSetsAsync() below). Platform
+                // guarantees that nothing bad will happen if we return 'true' here and later return 'null' / empty
+                // collection from within GetPreviewAsync().
+
+                return true;
+            }
+        }
+
+        public async override Task<IEnumerable<SuggestedActionSet>> GetActionSetsAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            // Light bulb will always invoke this property on the UI thread.
+            AssertIsForeground();
+
+            if (_actionSets.IsDefault)
+            {
+                var extensionManager = this.Workspace.Services.GetService<IExtensionManager>();
+
+                _actionSets = await extensionManager.PerformFunctionAsync(Provider, async () =>
+                {
+                    var builder = ArrayBuilder<SuggestedActionSet>.GetInstance();
+
+                    // We use ConfigureAwait(true) to stay on the UI thread.
+                    var previewChangesSuggestedActionSet = await GetPreviewChangesSuggestedActionSetAsync(cancellationToken).ConfigureAwait(true);
+                    if (previewChangesSuggestedActionSet != null)
+                    {
+                        builder.Add(previewChangesSuggestedActionSet);
+                    }
+
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    var fixAllSuggestedActionSet = this.FixAllSuggestedActionSet;
+                    if (fixAllSuggestedActionSet != null)
+                    {
+                        builder.Add(fixAllSuggestedActionSet);
+                    }
+
+                    return builder.ToImmutableAndFree();
+                    // We use ConfigureAwait(true) to stay on the UI thread.
+                }, defaultValue: ImmutableArray<SuggestedActionSet>.Empty).ConfigureAwait(true);
+            }
+
+            Contract.ThrowIfTrue(_actionSets.IsDefault);
+            return _actionSets;
+        }
+
+        private async Task<SuggestedActionSet> GetPreviewChangesSuggestedActionSetAsync(CancellationToken cancellationToken)
+        {
+            var previewResult = await GetPreviewResultAsync(cancellationToken).ConfigureAwait(true);
+            if (previewResult == null)
+            {
+                return null;
+            }
+
+            var changeSummary = previewResult.ChangeSummary;
+            if (changeSummary == null)
+            {
+                return null;
+            }
+
+            var previewAction = new PreviewChangesCodeAction(Workspace, CodeAction, changeSummary);
+            var previewSuggestedAction = new PreviewChangesSuggestedAction(
+                Workspace, SubjectBuffer, EditHandler, WaitIndicator, previewAction, Provider, OperationListener);
+            return new SuggestedActionSet(ImmutableArray.Create(previewSuggestedAction));
         }
 
         /// <summary>
@@ -97,11 +179,6 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Suggestions
         protected override DiagnosticData GetDiagnostic()
         {
             return _fix.GetPrimaryDiagnosticData();
-        }
-
-        protected override SuggestedActionSet GetFixAllSuggestedActionSet()
-        {
-            return _fixAllSuggestedActionSet;
         }
     }
 }
