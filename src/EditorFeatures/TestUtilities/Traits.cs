b525f82ce8094881668856080c4410ad45578acd
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-18 17:55:22 -0700

    Add a feature to convert existing code to use 'x is Type t' pattern.

--- src/EditorFeatures/TestUtilities/Traits.cs
+++ src/EditorFeatures/TestUtilities/Traits.cs
@@ -60,6 +60,7 @@ namespace Roslyn.Test.Utilities
             public const string CodeActionsImplementInterface = "CodeActions.ImplementInterface";
             public const string CodeActionsInlineDeclaration = "CodeActions.InlineDeclaration";
             public const string CodeActionsInlineTemporary = "CodeActions.InlineTemporary";
+            public const string CodeActionsInlineTypeCheck = "CodeActions.InlineTypeCheck";
             public const string CodeActionsInsertBraces = "CodeActions.InsertBraces";
             public const string CodeActionsInsertMissingCast = "CodeActions.InsertMissingCast";
             public const string CodeActionsInsertMissingTokens = "CodeActions.InsertMissingTokens";
