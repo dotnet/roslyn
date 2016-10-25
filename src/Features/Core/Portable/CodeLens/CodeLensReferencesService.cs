d1b50e85a54974f5c7d2f61b5017e3579be34ea3
Paul Vick <paulv@microsoft.com>
2016-10-25 16:44:01 -0700

    Fix test indicators

--- src/Features/Core/Portable/CodeLens/CodeLensReferencesService.cs
+++ src/Features/Core/Portable/CodeLens/CodeLensReferencesService.cs
@@ -263,18 +263,12 @@ namespace Microsoft.CodeAnalysis.CodeLens
         public async Task<string> GetFullyQualifiedName(Solution solution, DocumentId documentId, SyntaxNode syntaxNode,
             CancellationToken cancellationToken)
         {
-            var commonLocation = syntaxNode.GetLocation();
-            var document = solution.GetDocument(commonLocation.SourceTree);
-
-            if (document == null)
-            {
-                return null;
-            }
-
             using (solution.Services.CacheService?.EnableCaching(document.Project.Id))
             {
+                var document = solution.GetDocument(syntaxNode.GetLocation().SourceTree);
                 var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-                return GetEnclosingMethod(semanticModel, commonLocation, cancellationToken)?.ToDisplayString(MethodDisplayFormat);
+
+                return semanticModel.GetDeclaredSymbol(syntaxNode, cancellationToken)?.ToDisplayString(MethodDisplayFormat);
             }
         }
     }
