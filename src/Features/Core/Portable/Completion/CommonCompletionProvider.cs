19b9c2a153b119d180a21afc3474eb2a9a0c4b5a
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-24 18:14:58 -0700

    Sort usings.

--- src/Features/Core/Portable/Completion/CommonCompletionProvider.cs
+++ src/Features/Core/Portable/Completion/CommonCompletionProvider.cs
@@ -1,13 +1,13 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Options;
-using Microsoft.CodeAnalysis.Text;
-using System.Threading;
 using Microsoft.CodeAnalysis.Snippets;
-using System;
-using System.Linq;
-using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Completion
 {
