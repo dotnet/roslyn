bcc3993b51de868c6cad439a4140c99c6e907db9
Manish Vasani <mavasani@microsoft.com>
2016-10-26 02:26:52 -0700

    Handle IWorkspaceProjectContext creation requests with broken BinTargetPath
    CPS might provide such invalid paths during initialization or when project is in unrestored state. Now we handle these cases to ensure that BinOutputPath is either null or a rooted path.
    Fixes #14520

--- src/VisualStudio/Core/Impl/ProjectSystem/CPS/CPSProject.cs
+++ src/VisualStudio/Core/Impl/ProjectSystem/CPS/CPSProject.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.IO;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.VisualStudio.LanguageServices.Implementation.TaskList;
@@ -27,6 +28,21 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem.C
             : base(projectTracker, reportExternalErrorCreatorOpt, projectDisplayName, projectFilePath,
                    hierarchy, language, projectGuid, serviceProvider, visualStudioWorkspaceOpt, hostDiagnosticUpdateSourceOpt, commandLineParserServiceOpt)
         {
+            if (binOutputPath != null)
+            {
+                // Ensure that binOutputPath is either null or a rooted path.
+                // CPS might provide such invalid paths during initialization or when project is in unrestored state.
+                if (binOutputPath == String.Empty)
+                {
+                    binOutputPath = null;
+                }
+                else if (!Path.IsPathRooted(binOutputPath))
+                {
+                    // Make it a rooted path.
+                    binOutputPath = Path.Combine(Path.GetTempPath(), binOutputPath);
+                }
+            }
+
             // We need to ensure that the bin output path for the project has been initialized before we hookup the project with the project tracker.
             SetBinOutputPathAndRelatedData(binOutputPath);
 
