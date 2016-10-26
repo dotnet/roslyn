5b47dcb04f6fc4a2a26fe9505ba4743afd2a449e
Omar Tawfik <otawfik@microsoft.com>
2016-10-26 16:31:33 -0700

    Using PathUtilities.IsSameDirectoryOrChildOf() to check relativity of paths

--- src/Compilers/CSharp/Test/CommandLine/CommandLineDiagnosticFormatterTests.cs
+++ src/Compilers/CSharp/Test/CommandLine/CommandLineDiagnosticFormatterTests.cs
@@ -31,7 +31,7 @@ namespace Microsoft.CodeAnalysis.CSharp.CommandLine.UnitTests
 
         [ConditionalFact(typeof(WindowsOnly))]
         [WorkItem(14725, "https://github.com/dotnet/roslyn/issues/14725")]
-        public void RelativizeNormalizedPathShouldHandleRootPaths()
+        public void RelativizeNormalizedPathShouldHandleRootPaths_1()
         {
             var formatter = new CommandLineDiagnosticFormatter(
                 baseDirectory: @"C:\",
@@ -39,6 +39,42 @@ namespace Microsoft.CodeAnalysis.CSharp.CommandLine.UnitTests
                 displayEndLocations: false);
 
             Assert.Equal(@"temp.cs", formatter.RelativizeNormalizedPath(@"c:\temp.cs"));
+        }
+
+        [ConditionalFact(typeof(WindowsOnly))]
+        [WorkItem(14725, "https://github.com/dotnet/roslyn/issues/14725")]
+        public void RelativizeNormalizedPathShouldHandleRootPaths_2()
+        {
+            var formatter = new CommandLineDiagnosticFormatter(
+                baseDirectory: @"C:\A\B",
+                displayFullPaths: false,
+                displayEndLocations: false);
+
+            Assert.Equal(@"C\D\temp.cs", formatter.RelativizeNormalizedPath(@"c:\A\B\C\D\temp.cs"));
+        }
+
+        [ConditionalFact(typeof(WindowsOnly))]
+        [WorkItem(14725, "https://github.com/dotnet/roslyn/issues/14725")]
+        public void RelativizeNormalizedPathShouldHandleDirectoriesWithSamePrefix_1()
+        {
+            var formatter = new CommandLineDiagnosticFormatter(
+                baseDirectory: @"C:\AB",
+                displayFullPaths: false,
+                displayEndLocations: false);
+
+            Assert.Equal(@"c:\ABCD\file.cs", formatter.RelativizeNormalizedPath(@"c:\ABCD\file.cs"));
+        }
+
+        [ConditionalFact(typeof(WindowsOnly))]
+        [WorkItem(14725, "https://github.com/dotnet/roslyn/issues/14725")]
+        public void RelativizeNormalizedPathShouldHandleDirectoriesWithSamePrefix_2()
+        {
+            var formatter = new CommandLineDiagnosticFormatter(
+                baseDirectory: @"C:\ABCD",
+                displayFullPaths: false,
+                displayEndLocations: false);
+
+            Assert.Equal(@"c:\AB\file.cs", formatter.RelativizeNormalizedPath(@"c:\AB\file.cs"));
         }
     }
 }
