1c5545e550dbc77e350d1c9fd9887209f9739653
CyrusNajmabadi <cyrusn@microsoft.com>
2016-10-29 20:10:50 -0700

    Add C# tests.

---
+++ src/EditorFeatures/CSharpTest/UseCoalesceExpression/UseCoalesceExpressionTests.cs
@@ -0,0 +1,152 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.UseCoalesceExpression;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics;
+using Microsoft.CodeAnalysis.UseCoalesceExpression;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.UseCoalesceExpression
+{
+    public class UseCoalesceExpressionTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
+    {
+        internal override Tuple<DiagnosticAnalyzer, CodeFixProvider> CreateDiagnosticProviderAndFixer(Workspace workspace)
+        {
+            return new Tuple<DiagnosticAnalyzer, CodeFixProvider>(
+                new CSharpUseCoalesceExpressionDiagnosticAnalyzer(),
+                new UseCoalesceExpressionCodeFixProvider());
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCoalesceExpression)]
+        public async Task TestOnLeft_Equals()
+        {
+            await TestAsync(
+@"
+using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = [||]x == null ? y : x;
+    }
+}",
+@"using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = x ?? y;
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCoalesceExpression)]
+        public async Task TestOnLeft_NotEquals()
+        {
+            await TestAsync(
+@"
+using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = [||]x != null ? x : y;
+    }
+}",
+@"using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = x ?? y;
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCoalesceExpression)]
+        public async Task TestOnRight_Equals()
+        {
+            await TestAsync(
+@"
+using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = [||]null == x ? y : x;
+    }
+}",
+@"using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = x ?? y;
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCoalesceExpression)]
+        public async Task TestOnRight_NotEquals()
+        {
+            await TestAsync(
+@"
+using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = [||]null != x ? x : y;
+    }
+}",
+@"using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = x ?? y;
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCoalesceExpression)]
+        public async Task TestComplexExpression()
+        {
+            await TestAsync(
+@"
+using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = [||]x.ToString() == null ? y : x.ToString();
+    }
+}",
+@"using System;
+
+class C
+{
+    void M(string x, string y)
+    {
+        var z = x.ToString() ?? y;
+    }
+}");
+        }
+    }
+}
