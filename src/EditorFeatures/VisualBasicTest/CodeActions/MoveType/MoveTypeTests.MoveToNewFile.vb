3f504f9284d448b8383a0d424453d6e8209fc6c9
CyrusNajmabadi <cyrusn@microsoft.com>
2016-11-02 16:43:46 -0700

    Add VB tests.

--- src/EditorFeatures/VisualBasicTest/CodeActions/MoveType/MoveTypeTests.MoveToNewFile.vb
+++ src/EditorFeatures/VisualBasicTest/CodeActions/MoveType/MoveTypeTests.MoveToNewFile.vb
@@ -7,10 +7,10 @@ Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.CodeRefactorings.M
         <WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)>
         Public Async Function TestMissing_OnMatchingFileName() As Task
             Dim code =
-<File>
+"
 [||]Class test1
 End Class
-</File>.ConvertTestSourceTag()
+"
 
             Await TestMissingAsync(code)
         End Function
@@ -18,92 +18,91 @@ End Class
         <WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)>
         Public Async Function TestMissing_Nested_OnMatchingFileName_Simple() As Task
             Dim code =
-<File>
+"
 Class Outer
     [||]Class test1
     End Class
-End Class
-</File>.ConvertTestSourceTag()
+    End Class
+"
 
             Await TestMissingAsync(code)
-        End Function
+    End Function
 
-        <WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)>
-        Public Async Function MultipleTypesInFileWithNoContainerNamespace() As Task
-            Dim code =
-<File>
+    <WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)>
+    Public Async Function MultipleTypesInFileWithNoContainerNamespace() As Task
+        Dim code =
+"
 [||]Class Class1
 End Class
 
 Class Class2
 End Class
-</File>
+"
             Dim codeAfterMove =
-<File>
+"
 Class Class2
-End Class
-</File>
+    End Class
+"
             Dim expectedDocumentName = "Class1.vb"
 
             Dim destinationDocumentText =
-<File>
+        "
 Class Class1
 End Class
-</File>
+"
             Await TestMoveTypeToNewFileAsync(code, codeAfterMove, expectedDocumentName, destinationDocumentText)
         End Function
 
         <WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)>
         Public Async Function MoveNestedTypeToNewFile_Simple() As Task
             Dim code =
-<File>
+"
 Public Class Class1
     Class Class2[||]
     End Class
 End Class
-</File>
+"
             Dim codeAfterMove =
-<File>
+"
 Public Partial Class Class1
 End Class
-</File>
+"
             Dim expectedDocumentName = "Class2.vb"
 
             Dim destinationDocumentText =
-<File>
+"
 Public Partial Class Class1
     Class Class2
-
     End Class
 End Class
-</File>
+"
             Await TestMoveTypeToNewFileAsync(code, codeAfterMove, expectedDocumentName, destinationDocumentText)
         End Function
 
         <WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)>
         Public Async Function MoveNestedTypeToNewFile_Simple_DottedName() As Task
             Dim code =
-<File>
+    "
 Public Class Class1
     Class Class2[||]
     End Class
 End Class
-</File>
+"
             Dim codeAfterMove =
-<File>
+"
 Public Partial Class Class1
 End Class
-</File>
+"
             Dim expectedDocumentName = "Class1.Class2.vb"
 
             Dim destinationDocumentText =
-<File>
+        "
 Public Partial Class Class1
     Class Class2
 
     End Class
 End Class
-</File>
+"
             Await TestMoveTypeToNewFileAsync(code, codeAfterMove, expectedDocumentName, destinationDocumentText, index:=1)
         End Function
 
@@ -111,33 +110,75 @@ End Class
         <WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)>
         Public Async Function MoveNestedTypeToNewFile_RemoveComments() As Task
             Dim code =
-<File>
+"
 ''' Outer comment
 Public Class Class1
-    ''' Inner comment
-    Class Class2[||]
+        ''' Inner comment
+        Class Class2[||]
     End Class
-End Class
-</File>
+    End Class
+"
             Dim codeAfterMove =
-<File>
+"
 ''' Outer comment
 Public Partial Class Class1
 End Class
-</File>
+"
             Dim expectedDocumentName = "Class1.Class2.vb"
 
             Dim destinationDocumentText =
-<File>
+"
 Public Partial Class Class1
     ''' Inner comment
     Class Class2
     End Class
 End Class
-</File>
+"
             Await TestMoveTypeToNewFileAsync(
                 code, codeAfterMove, expectedDocumentName, destinationDocumentText,
                 index:=1, compareTokens:=False)
+        End Function
+
+        <WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)>
+        Public Async Function TestImports() As Task
+            Dim code =
+"
+' Used only by inner
+Imports System
+
+' Not used
+Imports System.Collections
+
+Class Outer
+    [||]Class Inner
+        Sub M(d as DateTime)
+        End Sub
+    End Class
+End Class
+"
+            Dim codeAfterMove =
+"
+' Not used
+Imports System.Collections
+
+Partial Class Outer
+End Class
+"
+            Dim expectedDocumentName = "Inner.vb"
+
+            Dim destinationDocumentText =
+"
+' Used only by inner
+Imports System
+
+Partial Class Outer
+    Class Inner
+        Sub M(d as DateTime)
+        End Sub
+    End Class
+End Class
+"
+            Await TestMoveTypeToNewFileAsync(code, codeAfterMove, expectedDocumentName, destinationDocumentText)
         End Function
     End Class
 End Namespace
