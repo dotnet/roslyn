675584c561d9cdd81f206e54bfe90af49532371c
Kevin Pilch <kevinpi@microsoft.com>
2016-11-02 13:49:59 -0700

    Use NuGet 3.6.0-beta1 which has support for -msbuildpath to find MSBuild

--- build/scripts/LoadNuGetInfo.cmd
+++ build/scripts/LoadNuGetInfo.cmd
@@ -2,9 +2,15 @@
 
 REM This is a script that will set environment information about where to find
 REM NuGet.exe, it's version and ensure that it's present on the enlistment.
-set NuGetExeVersion=3.5.0-beta2
+set NuGetExeVersion=3.6.0-beta1
 set NuGetExeFolder=%~dp0..\..
 set NuGetExe=%NuGetExeFolder%\NuGet.exe
+
+if "%VisualStudioVersion%" == "15.0" (
+    set NuGetAdditionalCommandLineArgs=-verbosity quiet -configfile "%NuGetExeFolder%\nuget.config" -Project2ProjectTimeOut 1200 -msbuildpath "%DevenvDir%\..\..\MSBuild\15.0\Bin"
+) else (
+    set NuGetAdditionalCommandLineArgs=-verbosity quiet -configfile "%NuGetExeFolder%\nuget.config" -Project2ProjectTimeOut 1200
+)
 
 REM Download NuGet.exe if we haven't already
 if not exist "%NuGetExe%" (
