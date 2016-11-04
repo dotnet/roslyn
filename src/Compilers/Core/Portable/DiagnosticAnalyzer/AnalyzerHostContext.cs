3869b21ee1691def9a69a0cfae351c44b31aa099
Heejae Chang <hechang@microsoft.com>
2016-11-04 15:53:08 -0700

    added AnalyzerHostContext that expose a way to control lifetime of analyzer host context
    context manages things like analyzer states, supported diagnostics, exception handlers and etc.

---
+++ src/Compilers/Core/Portable/DiagnosticAnalyzer/AnalyzerHostContext.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.CodeAnalysis.Diagnostics
+{
+    /// <summary>
+    /// Manages properties of analyzers (such as registered actions, supported diagnostics) for analyzer host
+    /// 
+    /// It ensures the following for analyzer host:
+    /// 1) <see cref="DiagnosticAnalyzer.Initialize(AnalysisContext)"/> is invoked only once per-analyzer.
+    /// 2) <see cref="DiagnosticAnalyzer.SupportedDiagnostics"/> is invoked only once per-analyzer.
+    /// 3) <see cref="CompilationStartAnalyzerAction"/> registered during Initialize are invoked only once per-compilation
+    /// </summary>
+    public abstract class AnalyzerHostContext : IDisposable
+    {
+        private bool _disposed;
+
+        /// <summary>
+        /// Create new default <see cref="AnalyzerHostContext"/>
+        /// </summary>
+        public static AnalyzerHostContext Create()
+        {
+            return new AnalyzerManager();
+        }
+
+        internal AnalyzerHostContext()
+        {
+            // we don't allow user to define its own context yet
+            _disposed = false;
+        }
+
+        protected abstract void Dispose(bool disposing);
+
+        public void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            _disposed = true;
+
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~AnalyzerHostContext()
+        {
+            // make sure we dispose context to prevent
+            // internal states from leaking
+            Dispose(false);
+        }
+    }
+}
--- src/Compilers/Core/Portable/DiagnosticAnalyzer/DiagnosticAnalyzerExtensions.cs
+++ src/Compilers/Core/Portable/DiagnosticAnalyzer/DiagnosticAnalyzerExtensions.cs
@@ -14,9 +14,22 @@ namespace Microsoft.CodeAnalysis.Diagnostics
         /// <param name="analyzers">The set of analyzers to include in future analyses.</param>
         /// <param name="options">Options that are passed to analyzers.</param>
         /// <param name="cancellationToken">A cancellation token that can be used to abort analysis.</param>
-        public static CompilationWithAnalyzers WithAnalyzers(this Compilation compilation, ImmutableArray<DiagnosticAnalyzer> analyzers, AnalyzerOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        public static CompilationWithAnalyzers WithAnalyzers(this Compilation compilation, ImmutableArray<DiagnosticAnalyzer> analyzers, AnalyzerOptions options, CancellationToken cancellationToken)
         {
             return new CompilationWithAnalyzers(compilation, analyzers, options, cancellationToken);
+        }
+
+        /// <summary>
+        /// Returns a new compilation with attached diagnostic analyzers.
+        /// </summary>
+        /// <param name="compilation">Compilation to which analyzers are to be added.</param>
+        /// <param name="analyzers">The set of analyzers to include in future analyses.</param>
+        /// <param name="context"><see cref="AnalyzerHostContext"/> analyzers will run under</param>
+        /// <param name="options">Options that are passed to analyzers.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used to abort analysis.</param>
+        public static CompilationWithAnalyzers WithAnalyzers(this Compilation compilation, ImmutableArray<DiagnosticAnalyzer> analyzers, AnalyzerHostContext context = null, AnalyzerOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return new CompilationWithAnalyzers(compilation, analyzers, context, options, cancellationToken);
         }
 
         /// <summary>
@@ -28,6 +41,18 @@ namespace Microsoft.CodeAnalysis.Diagnostics
         public static CompilationWithAnalyzers WithAnalyzers(this Compilation compilation, ImmutableArray<DiagnosticAnalyzer> analyzers, CompilationWithAnalyzersOptions analysisOptions)
         {
             return new CompilationWithAnalyzers(compilation, analyzers, analysisOptions);
+        }
+
+        /// <summary>
+        /// Returns a new compilation with attached diagnostic analyzers.
+        /// </summary>
+        /// <param name="compilation">Compilation to which analyzers are to be added.</param>
+        /// <param name="analyzers">The set of analyzers to include in future analyses.</param>
+        /// <param name="context"><see cref="AnalyzerHostContext"/> analyzers will run under</param>
+        /// <param name="analysisOptions">Options to configure analyzer execution within <see cref="CompilationWithAnalyzers"/>.</param>
+        public static CompilationWithAnalyzers WithAnalyzers(this Compilation compilation, ImmutableArray<DiagnosticAnalyzer> analyzers, AnalyzerHostContext context, CompilationWithAnalyzersOptions analysisOptions)
+        {
+            return new CompilationWithAnalyzers(compilation, analyzers, context, analysisOptions);
         }
     }
 }
