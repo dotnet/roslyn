3fba7904a90d3b89bb4928aed709e04b3a4707e7
Jonathon Marolf <jmarolf@microsoft.com>
2016-11-09 15:07:28 -0800

    adding tests

--- src/Workspaces/Core/Portable/CodeStyle/CodeStyleHelpers.cs
+++ src/Workspaces/Core/Portable/CodeStyle/CodeStyleHelpers.cs
@@ -1,17 +1,30 @@
-﻿namespace Microsoft.CodeAnalysis.CodeStyle
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.CodeAnalysis.CodeStyle
 {
     internal static class CodeStyleHelpers
     {
         public static CodeStyleOption<bool> ParseEditorConfigCodeStyleOption(string arg)
         {
             var args = arg.Split(':');
+            bool isEnabled = false;
             if (args.Length != 2)
             {
+                if (arg.Length == 1)
+                {
+                    if (bool.TryParse(args[0].Trim(), out isEnabled) && !isEnabled)
+                    {
+                        return new CodeStyleOption<bool>(value: isEnabled, notification: NotificationOption.None);
+                    }
+                    else
+                    {
+                        return CodeStyleOption<bool>.Default;
+                    }
+                }
                 return CodeStyleOption<bool>.Default;
             }
 
-            bool isEnabled = false;
-            if (!bool.TryParse(args[0], out isEnabled))
+            if (!bool.TryParse(args[0].Trim(), out isEnabled))
             {
                 return CodeStyleOption<bool>.Default;
             }
@@ -22,7 +35,7 @@
                 case "suggestion": return new CodeStyleOption<bool>(value: isEnabled, notification: NotificationOption.Suggestion);
                 case "warning": return new CodeStyleOption<bool>(value: isEnabled, notification: NotificationOption.Warning);
                 case "error": return new CodeStyleOption<bool>(value: isEnabled, notification: NotificationOption.Error);
-                default: return CodeStyleOption<bool>.Default;
+                default: return new CodeStyleOption<bool>(value: isEnabled, notification: NotificationOption.None);
             }
         }
     }
