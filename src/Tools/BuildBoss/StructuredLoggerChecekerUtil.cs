8d3bf0f5c22f55b642a75893472c26e7afb0680f
Jared Parsons <jaredpar@microsoft.com>
2016-11-10 22:14:16 -0800

    Code to process a structured log file

--- src/Tools/BuildBoss/StructuredLoggerChecekerUtil.cs
+++ src/Tools/BuildBoss/StructuredLoggerChecekerUtil.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace BuildBoss
 {
@@ -22,7 +23,15 @@ namespace BuildBoss
     /// </summary>
     internal sealed class StructuredLoggerChecekerUtil : ICheckerUtil
     {
+        internal static readonly StringComparer FilePathComparer = StringComparer.OrdinalIgnoreCase;
+        private static readonly string s_copyingFileFrom = "Copying file from \"";
+        private static readonly string s_creatingHardLink = "Creating hard link to copy \"";
+        private static readonly string s_didNotCopy = "Did not copy from file \"";
+        private static readonly string s_to = "\" to \"";
+        private static readonly string s_toFile = "\" to file \"";
+
         private readonly XDocument _document;
+        private readonly Dictionary<string, List<string>> _copyMap = new Dictionary<string, List<string>>(FilePathComparer);
 
         internal StructuredLoggerChecekerUtil(XDocument document)
         {
@@ -31,7 +40,67 @@ namespace BuildBoss
 
         public bool Check(TextWriter textWriter)
         {
-            return true;
+            foreach (var element in _document.XPathSelectElements("//CopyTask"))
+            {
+                foreach (var message in element.Elements(XName.Get("Message")))
+                {
+                    ProcessMessage(message);
+                }
+            }
+
+            var allGood = true;
+            foreach (var pair in _copyMap.OrderBy(x => x.Key))
+            {
+                var list = pair.Value;
+                if (list.Count > 1)
+                {
+                    textWriter.WriteLine($"Multiple writes to {pair.Key}");
+                    foreach (var item in list)
+                    {
+                        textWriter.WriteLine($"\t{item}");
+
+                    }
+
+                    allGood = false;
+                }
+            }
+
+            return allGood;
+        }
+
+        private void ProcessMessage(XElement element)
+        {
+            var text = element.Value.Trim();
+            if (text.StartsWith(s_copyingFileFrom))
+            {
+                ProcessCopyingFileFrom(text, s_copyingFileFrom, s_to);
+            }
+            else if (text.StartsWith(s_creatingHardLink))
+            {
+                ProcessCopyingFileFrom(text, s_creatingHardLink, s_to);
+            }
+            else if (text.StartsWith(s_didNotCopy))
+            {
+                // TODO: decide to handle skipped copied files 
+            }
+        }
+
+        private void ProcessCopyingFileFrom(string text, string prefix, string infix)
+        {
+            var split = text.IndexOf(infix);
+            var prefixLength = prefix.Length;
+            int toLength = infix.Length;
+            var source = text.Substring(prefixLength, split - prefixLength);
+            var destination = text.Substring(split + toLength, text.Length - 2 - split - toLength);
+
+            List<string> list;
+            if (!_copyMap.TryGetValue(destination, out list))
+            {
+                list = new List<string>();
+                _copyMap[destination] = list;
+            }
+
+            list.Add(source);
         }
     }
 }
