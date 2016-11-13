aa5e20de279b6c66707581aedf3468d40526c6e1
CyrusNajmabadi <cyrusn@microsoft.com>
2016-11-12 21:43:07 -0800

    Remove unused TypeSwitch function.

--- src/Workspaces/Core/Portable/Shared/Extensions/ObjectExtensions.TypeSwitch.cs
+++ src/Workspaces/Core/Portable/Shared/Extensions/ObjectExtensions.TypeSwitch.cs
@@ -8,28 +8,6 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
     {
         #region TypeSwitch on Func<T>
 
-        public static TResult TypeSwitch<TBaseType, TDerivedType1, TDerivedType2, TResult>(this TBaseType obj, Func<TDerivedType1, TResult> matchFunc1, Func<TDerivedType2, TResult> matchFunc2, Func<TBaseType, TResult> defaultFunc = null)
-            where TDerivedType1 : TBaseType
-            where TDerivedType2 : TBaseType
-        {
-            if (obj is TDerivedType1)
-            {
-                return matchFunc1((TDerivedType1)obj);
-            }
-            else if (obj is TDerivedType2)
-            {
-                return matchFunc2((TDerivedType2)obj);
-            }
-            else if (defaultFunc != null)
-            {
-                return defaultFunc(obj);
-            }
-            else
-            {
-                return default(TResult);
-            }
-        }
-
         public static TResult TypeSwitch<TBaseType, TDerivedType1, TDerivedType2, TDerivedType3, TResult>(this TBaseType obj, Func<TDerivedType1, TResult> matchFunc1, Func<TDerivedType2, TResult> matchFunc2, Func<TDerivedType3, TResult> matchFunc3, Func<TBaseType, TResult> defaultFunc = null)
             where TDerivedType1 : TBaseType
             where TDerivedType2 : TBaseType
