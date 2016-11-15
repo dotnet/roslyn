def0b88ab4a550ccc57d9a3769fadf53c37a892a
Jared Parsons <jaredpar@microsoft.com>
2016-11-15 10:57:39 -0800

    Respond to PR feedback

--- src/Tools/MicroBuild/run-pdbgit.ps1
+++ src/Tools/MicroBuild/run-pdbgit.ps1
@@ -27,15 +27,29 @@ try
     $packagesPath = Get-PackagesPath
     $pdbGit = join-path $packagesPath "pdbgit\$pdbGitVersion\tools\PdbGit.exe"
 
+    # Linking our PDBs to Git is really only interesting for PE / PDB that are 
+    # being shipped to customers.  For local developement the original PDB 
+    # information is sufficient. 
+    #
+    # The set of binaries which are signed is equivalent to the set of binaries
+    # that are shipped to customers.  Grab this list from our signing config
+    # file.
     $signToolDataPath = join-path $sourcePath "build\config\SignToolData.json"
     $signToolData = convertfrom-json (get-content $signToolDataPath -raw)
     $allGood = $true
     $count = 0
 
     foreach ($relativePath in $signToolData.sign | %{ $_.values }) {
+        $ext = [IO.Path]::GetExtension($relativePath)
+        if (($ext -ne ".exe") -and ($ext -ne ".dll")) {
+            continue
+        }
+
         $binaryPath = join-Path $binariesPath $relativePath
         $pdbPath = [IO.Path]::ChangeExtension($binaryPath, ".pdb")
         if (-not (test-path $pdbPath)) {
+            write-host "Could not find PDB $pdbPath"
+            $allGood = $false
             continue;
         }
 
