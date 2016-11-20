75a1a4dd9beab780e401c6a234dfaeea1926a788
CyrusNajmabadi <cyrusn@microsoft.com>
2016-11-20 12:44:04 -0800

    Clean u pcode.

--- src/Features/CSharp/Portable/CodeFixes/ImplementInterface/ImplementInterfaceCodeFixProvider.cs
+++ src/Features/CSharp/Portable/CodeFixes/ImplementInterface/ImplementInterfaceCodeFixProvider.cs
@@ -25,10 +25,8 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.ImplementInterface
         private const string CS0737 = nameof(CS0737); // 'Class' does not implement interface member 'IInterface.M()'. 'Class.M()' cannot implement an interface member because it is not public.
         private const string CS0738 = nameof(CS0738); // 'C' does not implement interface member 'I.Method1()'. 'B.Method1()' cannot implement 'I.Method1()' because it does not have the matching return type of 'void'.
 
-        public sealed override ImmutableArray<string> FixableDiagnosticIds
-        {
-            get { return ImmutableArray.Create(CS0535, CS0737, CS0738); }
-        }
+        public sealed override ImmutableArray<string> FixableDiagnosticIds { get; }
+            = ImmutableArray.Create(CS0535, CS0737, CS0738);
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
