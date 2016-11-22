2296985a0e28702e33ef66cb3602668d28838058
Julien Couvreur <julien.couvreur@gmail.com>
2016-11-22 10:35:48 -0800

    IDE type inference for deconstructions should not crash on discards

--- src/Workspaces/CSharp/Portable/LanguageServices/CSharpTypeInferenceService.TypeInferrer.cs
+++ src/Workspaces/CSharp/Portable/LanguageServices/CSharpTypeInferenceService.TypeInferrer.cs
@@ -2020,7 +2020,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                         }
                     }
 
-                    if (elementTypesBuilder.Contains(null))
+                    if (elementTypesBuilder.Contains(null) || elementTypesBuilder.Count != arguments.Count)
                     {
                         return false;
                     }
