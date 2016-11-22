0188f5696e5684143b2dda09bb2c5173fa82d735
Tanner Gooding <tagoo@microsoft.com>
2016-11-22 12:58:55 -0800

    Updating things so the open-vsi tests run by default.

--- netci.groovy
+++ netci.groovy
@@ -195,7 +195,7 @@ set TMP=%TEMP%
     }
   }
 
-  def triggerPhraseOnly = true
+  def triggerPhraseOnly = false
   def triggerPhraseExtra = "open-vsi"
   Utilities.setMachineAffinity(myJob, 'Windows_NT', 'latest-or-auto-dev15-rc')
   addRoslynJob(myJob, jobName, branchName, isPr, triggerPhraseExtra, triggerPhraseOnly)
