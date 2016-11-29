e3c38d7ec8142af7de9725c5d1117596b204bb92
Ravi Chande <rchande@microsoft.com>
2016-11-29 08:37:22 -0800

    CR feedback

--- src/Features/CSharp/Portable/Completion/SuggestionMode/CSharpSuggestionModeCompletionProvider.cs
+++ src/Features/CSharp/Portable/Completion/SuggestionMode/CSharpSuggestionModeCompletionProvider.cs
@@ -152,25 +152,20 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.SuggestionMode
                 position = token.Parent.SpanStart;
             }
 
+            // In the following situation, the type inferrer will infer Task to support target type preselection
+            // Action a = Task.$$
+            // We need to explicitly exclude invocation/member access from suggestion mode
+            var previousToken = token.GetPreviousTokenIfTouchingWord(position);
+            if (previousToken.IsKind(SyntaxKind.DotToken) &&
+                previousToken.Parent.IsKind(SyntaxKind.InvocationExpression, SyntaxKind.SimpleMemberAccessExpression))
+            {
+                return false;
+            }
+
             // If we're an argument to a function with multiple overloads, 
             // open the builder if any overload takes a delegate at our argument position
             var inferredTypeInfo = typeInferrer.GetTypeInferenceInfo(semanticModel, position, cancellationToken: cancellationToken);
-
-            if (inferredTypeInfo.Any(type => GetDelegateType(type, semanticModel.Compilation).IsDelegateType()))
-            {
-                // In the following situation, the type inferrer will infer Task to support target type preselection
-                // Action a = Task.$$
-                // We need to explicitly exclude invocation/member access from suggestion mode
-                var previousToken = token.GetPreviousTokenIfTouchingWord(position);
-                if (previousToken.IsKind(SyntaxKind.DotToken))
-                {
-                    return !previousToken.Parent.IsKind(SyntaxKind.InvocationExpression, SyntaxKind.SimpleMemberAccessExpression);
-                }
-
-                return true;
-            }
-
-            return false;
+            return inferredTypeInfo.Any(type => GetDelegateType(type, semanticModel.Compilation).IsDelegateType());
         }
 
         private ITypeSymbol GetDelegateType(TypeInferenceInfo typeInferenceInfo, Compilation compilation)
