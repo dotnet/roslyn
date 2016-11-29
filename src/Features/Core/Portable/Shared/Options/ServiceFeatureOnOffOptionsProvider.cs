56e8533cb68b06210754f5158276833f1926d7d4
CyrusNajmabadi <cyrusn@microsoft.com>
2016-11-29 13:25:10 -0800

    Simplify the api and implementation for IOptionProviders

--- src/Features/Core/Portable/Shared/Options/ServiceFeatureOnOffOptionsProvider.cs
+++ src/Features/Core/Portable/Shared/Options/ServiceFeatureOnOffOptionsProvider.cs
@@ -1,17 +1,16 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Composition;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Options.Providers;
-using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Shared.Options
 {
     [ExportOptionProvider, Shared]
     internal class ServiceFeatureOnOffOptionsProvider : IOptionProvider
     {
-        private readonly IEnumerable<IOption> _options = SpecializedCollections.SingletonEnumerable(ServiceFeatureOnOffOptions.ClosedFileDiagnostic);
-        public IEnumerable<IOption> GetOptions() => _options;
+        public ImmutableArray<IOption> Options { get; } = ImmutableArray.Create<IOption>(
+            ServiceFeatureOnOffOptions.ClosedFileDiagnostic);
     }
 }
