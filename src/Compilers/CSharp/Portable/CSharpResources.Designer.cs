ebf0ee2bdfe3bbdc67dd4ad870096147fcb09245
jcouv <jcouv@JULIENCOUVRDEE1>
2016-12-14 22:48:02 -0800

    Deconstruct should return void and disallow 1-deconstruction

--- src/Compilers/CSharp/Portable/CSharpResources.Designer.cs
+++ src/Compilers/CSharp/Portable/CSharpResources.Designer.cs
@@ -3113,6 +3113,15 @@ namespace Microsoft.CodeAnalysis.CSharp {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Deconstructions are only allowed into two or more parts..
+        /// </summary>
+        internal static string ERR_DeconstructionNeedsTwoParts {
+            get {
+                return ResourceManager.GetString("ERR_DeconstructionNeedsTwoParts", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Deconstruction &apos;var (...)&apos; form disallows a specific type for &apos;var&apos;..
         /// </summary>
         internal static string ERR_DeconstructionVarFormDisallowsSpecificType {
@@ -6047,7 +6056,7 @@ namespace Microsoft.CodeAnalysis.CSharp {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to No Deconstruct instance or extension method was found for type &apos;{0}&apos;, with {1} out parameters..
+        ///   Looks up a localized string similar to No Deconstruct instance or extension method was found for type &apos;{0}&apos;, with {1} out parameters and a void return type..
         /// </summary>
         internal static string ERR_MissingDeconstruct {
             get {
