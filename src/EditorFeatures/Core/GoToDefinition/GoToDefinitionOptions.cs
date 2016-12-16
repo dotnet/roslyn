25ba09f180db37d3de6830eb7cffa5fe014aa7a5
CyrusNajmabadi <cyrusn@microsoft.com>
2016-12-16 13:30:51 -0800

    Add per language options to disable GoToDef and GoToImpl.

---
+++ src/EditorFeatures/Core/GoToDefinition/GoToDefinitionOptions.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Options;
+
+namespace Microsoft.CodeAnalysis.Editor
+{
+    internal static class GoToDefinitionOptions
+    {
+        public static readonly PerLanguageOption<bool> Enabled = new PerLanguageOption<bool>(
+            nameof(GoToDefinitionOptions), nameof(Enabled), defaultValue: true);
+    }
+}
