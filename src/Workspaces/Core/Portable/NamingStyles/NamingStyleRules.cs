127a8138d98bcfcc7f4e4d6dd7fc8a59dc9f5f58
CyrusNajmabadi <cyrusn@microsoft.com>
2016-12-21 15:55:07 -0800

    Make a class again.

--- src/Workspaces/Core/Portable/NamingStyles/NamingStyleRules.cs
+++ src/Workspaces/Core/Portable/NamingStyles/NamingStyleRules.cs
@@ -4,7 +4,7 @@ using System.Collections.Immutable;
 
 namespace Microsoft.CodeAnalysis.Diagnostics.Analyzers.NamingStyles
 {
-    internal struct NamingStyleRules
+    internal class NamingStyleRules
     {
         public ImmutableArray<NamingRule> NamingRules { get; }
 
