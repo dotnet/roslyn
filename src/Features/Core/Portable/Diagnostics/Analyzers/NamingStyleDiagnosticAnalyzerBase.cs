1eb22a1143caa6231d5c58450ee42ec72fe25dc7
CyrusNajmabadi <cyrusn@microsoft.com>
2016-12-22 13:44:23 -0800

    Fix build failure.

--- src/Features/Core/Portable/Diagnostics/Analyzers/NamingStyleDiagnosticAnalyzerBase.cs
+++ src/Features/Core/Portable/Diagnostics/Analyzers/NamingStyleDiagnosticAnalyzerBase.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis.CodeStyle;
+using Microsoft.CodeAnalysis.NamingStyles;
 using Microsoft.CodeAnalysis.Simplification;
 using Roslyn.Utilities;
 
