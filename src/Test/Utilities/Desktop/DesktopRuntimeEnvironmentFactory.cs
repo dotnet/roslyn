bc1564d55de15a3eecb2c6ae5256dbc9a1289430
Jared Parsons <jaredpar@microsoft.com>
2016-12-22 10:55:31 -0800

    Re-arranging portable TestUtilities part 2
    The Compilers.sln solution is compiling again and tests which don't
    require AppDomain support are running again.  There is a bit of work
    that is needed for AppDomains that I want to separate out.  In
    particular the separation to portable means we have to redo some of the
    serialization code.

---
+++ src/Test/Utilities/Desktop/DesktopRuntimeEnvironmentFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Test.Utilities;
+
+namespace Roslyn.Test.Utilities
+{
+    public sealed class DesktopRuntimeEnvironmentFactory : IRuntimeEnvironmentFactory
+    {
+        public IRuntimeEnvironment Create(IEnumerable<ModuleData> additionalDependencies)
+        {
+            return new HostedRuntimeEnvironment(additionalDependencies);
+        }
+    }
+}
