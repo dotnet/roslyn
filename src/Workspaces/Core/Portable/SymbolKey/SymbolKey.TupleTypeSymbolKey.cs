96df2b626591ba8cec723c127ee0071b6cbc40e6
CyrusNajmabadi <cyrusn@microsoft.com>
2017-01-04 16:29:00 -0800

    Support roundtripping errors/non-errors.

--- src/Workspaces/Core/Portable/SymbolKey/SymbolKey.TupleTypeSymbolKey.cs
+++ src/Workspaces/Core/Portable/SymbolKey/SymbolKey.TupleTypeSymbolKey.cs
@@ -14,35 +14,71 @@ namespace Microsoft.CodeAnalysis
             {
                 Debug.Assert(symbol.IsTupleType);
 
-                var elementTypes = ArrayBuilder<ISymbol>.GetInstance();
+
                 var friendlyNames = ArrayBuilder<string>.GetInstance();
                 var locations = ArrayBuilder<Location>.GetInstance();
 
+                var isError = symbol.TupleUnderlyingType.TypeKind == TypeKind.Error;
+                visitor.WriteBoolean(isError);
+
+                if (isError)
+                {
+                    var elementTypes = ArrayBuilder<ISymbol>.GetInstance();
+
+                    foreach (var element in symbol.TupleElements)
+                    {
+                        elementTypes.Add(element.Type);
+                    }
+
+                    visitor.WriteSymbolKeyArray(elementTypes.ToImmutableAndFree());
+                }
+                else
+                {
+                    visitor.WriteSymbolKey(symbol.TupleUnderlyingType);
+                }
+
                 foreach (var element in symbol.TupleElements)
                 {
-                    elementTypes.Add(element.Type);
                     friendlyNames.Add(element.IsImplicitlyDeclared ? null : element.Name);
                     locations.Add(element.Locations.FirstOrDefault());
                 }
 
-                visitor.WriteSymbolKeyArray(elementTypes.ToImmutableAndFree());
                 visitor.WriteStringArray(friendlyNames.ToImmutableAndFree());
                 visitor.WriteLocationArray(locations.ToImmutableAndFree());
             }
 
             public static SymbolKeyResolution Resolve(SymbolKeyReader reader)
             {
-                var elementTypes = reader.ReadSymbolKeyArray().SelectAsArray(r => r.GetAnySymbol() as ITypeSymbol);
-                var elementNames = reader.ReadStringArray();
-                var elementLocations = reader.ReadLocationArray();
-
-                if (!elementTypes.Any(t => t == null))
+                var isError = reader.ReadBoolean();
+                if (isError)
                 {
+                    var elementTypes = reader.ReadSymbolKeyArray().SelectAsArray(r => r.GetAnySymbol() as ITypeSymbol);
+                    var elementNames = reader.ReadStringArray();
+                    var elementLocations = reader.ReadLocationArray();
+
+                    if (!elementTypes.Any(t => t == null))
+                    {
+                        try
+                        {
+                            var result = reader.Compilation.CreateTupleTypeSymbol(
+                                elementTypes, elementNames, elementLocations);
+                            return new SymbolKeyResolution(result);
+                        }
+                        catch (ArgumentException)
+                        {
+                        }
+                    }
+                }
+                else
+                {
+                    var underlyingTypeResolution = reader.ReadSymbolKey();
+                    var elementNames = reader.ReadStringArray();
+                    var elementLocations = reader.ReadLocationArray();
                     try
                     {
-                        var result = reader.Compilation.CreateTupleTypeSymbol(
-                            elementTypes, elementNames, elementLocations);
-                        return new SymbolKeyResolution(result);
+                        var result = GetAllSymbols<INamedTypeSymbol>(underlyingTypeResolution).Select(
+                            t => reader.Compilation.CreateTupleTypeSymbol(t, elementNames, elementLocations));
+                        return CreateSymbolInfo(result);
                     }
                     catch (ArgumentException)
                     {
