50a1b3e40817b0da0f86b4f8c65206db1c2c837e
Stefan Nikolei <stefan@nikolei.eu>
2017-01-07 23:02:03 +0100

    Added tests

--- src/EditorFeatures/CSharpTest/Diagnostics/RemoveUnusedVar/RemoveUnusedVarTest.cs
+++ src/EditorFeatures/CSharpTest/Diagnostics/RemoveUnusedVar/RemoveUnusedVarTest.cs
@@ -1,21 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.CodeFixes.RemoveUnusedVar;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Roslyn.Test.Utilities;
+using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics.RemoveUnusedVar
 {
-    public partial class RemoveUnusedVarTest: AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
+    public partial class RemoveUnusedVarTest : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
     {
         internal override Tuple<DiagnosticAnalyzer, CodeFixProvider> CreateDiagnosticProviderAndFixer(Workspace workspace)
         {
             return new Tuple<DiagnosticAnalyzer, CodeFixProvider>(
-                new DataFlowAnalysis(), new RemoveUnusedVarCodeFixProvider());
+                null, new RemoveUnusedVarCodeFixProvider());
         }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveUnusedVar)]
+        public async Task RemoveUnusedVar()
+        {
+            await TestAsync(
+@"class Class
+{
+    void Method()
+    {
+        [|int a = 3;|]
+    }
+}",
+@"class Class
+{
+    void Method()
+    {
+    }
+}");
+        }
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveUnusedVar)]
+        public async Task RemoveUnusedVar1()
+        {
+            await TestAsync(
+@"class Class
+{
+    void Method()
+    {
+        [|string a;|]
+        string b = "";
+        var c = b;
+    }
+}",
+@"class Class
+{
+    void Method()
+    {
+        string b = "";
+        var c = b;
+    }
+}");
+        }
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveUnusedVar)]
+        public async Task RemoveUnusedVar3()
+        {
+            await TestAsync(
+@"class Class
+{
+    void Method()
+    {
+        [|string a;|]
+    }
+}",
+@"class Class
+{
+    void Method()
+    {
+    }
+}");
+        }
+
     }
 }
