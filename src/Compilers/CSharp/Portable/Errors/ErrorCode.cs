a925e56012e14ac295cda47ad9ee226b63cd59cd
AlekseyTs <alekseyt@microsoft.com>
2017-01-20 19:08:04 -0800

    Properly detect and handle delegate relaxations when ByRef returns are involved.
    Fixes #13206.

--- src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
@@ -1436,9 +1436,10 @@ namespace Microsoft.CodeAnalysis.CSharp
         ERR_MustDeclareForeachIteration = 8186,
         ERR_TupleElementNamesInDeconstruction = 8187,
         ERR_ExpressionTreeContainsThrowExpression = 8188,
+        ERR_DelegateRefMismatch = 8189,
         #endregion stragglers for C# 7
 
-        // Available  = 8189-8195
+        // Available  = 8190-8195
 
         #region diagnostics for out var
         ERR_ImplicitlyTypedOutVariableUsedInTheSameArgumentList = 8196,
