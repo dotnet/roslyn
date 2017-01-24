8f173be74170338ad89923370841d5d2c25c4492
Ty Overby <ty@pre-alpha.com>
2017-01-24 15:06:06 -0800

    add end to end stack depth test

---
+++ src/Compilers/CSharp/Test/Emit/Emit/EndToEndTests.cs
@@ -0,0 +1,53 @@
+﻿using Roslyn.Test.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.CodeAnalysis.Test.Utilities;
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Emit
+{
+    public class EndToEndTests: EmitMetadataTestBase
+    {
+        [WorkItem(16669, "https://github.com/dotnet/roslyn/issues/16669")]
+        [Fact]
+        public void OverflowOnFluentCall()
+        {
+#if DEBUG
+            int numberFluentCalls = 290;
+#else
+            int numberFluentCalls = 590;
+#endif
+            string MakeCode()
+            {
+                var builder = new StringBuilder();
+                builder.AppendLine(
+    @"class C {
+    C M(string x) { return this; }
+    void M2() {
+        new C()
+");
+                for (int i = 0; i < numberFluentCalls; i++)
+                {
+                    builder.AppendLine(@"            .M(""test"")");
+                }
+                builder.AppendLine(
+               @"            .M(""test"");
+    }
+}");
+                return builder.ToString();
+            }
+            var source = MakeCode();
+
+            var thread = new System.Threading.Thread(() =>
+            {
+                var compilation = CreateCompilationWithMscorlib(source);
+                compilation.GetDiagnostics();
+                compilation.EmitToArray();
+            }, 0);
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
