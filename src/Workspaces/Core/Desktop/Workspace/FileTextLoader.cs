e83cd71746e66b42b33755674304d514fcbc6395
Heejae Chang <hechang@microsoft.com>
2017-01-26 12:30:56 -0800

    added max file limit when reading text file from disk.
    this is added so that we can reduce chance of OOM when user added massive size text file to solution.
    
    current default max is 100MB. but user can override it through hidden reg key if they want to.
    
    if such file is added, FileTextLoader will throw InvalidDataException and Workspace will raise Workspace Failed event.
    
    in VS host case, we will show the failed event in output windows.
    
    made it to use test option service since option is mutable workspace state which affects all other tests.

--- src/Workspaces/Core/Desktop/Workspace/FileTextLoader.cs
+++ src/Workspaces/Core/Desktop/Workspace/FileTextLoader.cs
@@ -7,11 +7,28 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Internal.Log;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis
 {
+    internal static class FileTextLoaderOptions
+    {
+        /// <summary>
+        /// Hidden registry key to control maximum size of a text file we will read into memory. 
+        /// we have this option to reduce a chance of OOM when user adds massive size files to the solution.
+        /// Default threshold is 100MB which came from some internal data on big files and some discussion.
+        /// 
+        /// User can override default value by setting DWORD value on FileLengthThreshold in 
+        /// "[VS HIVE]\Roslyn\Internal\Performance\Text"
+        /// </summary>
+        [ExportOption]
+        internal static readonly Option<long> FileLengthThreshold = new Option<long>(nameof(FileTextLoaderOptions), nameof(FileLengthThreshold), defaultValue: 100 * 1024 * 1024,
+            storageLocations: new LocalUserProfileStorageLocation(@"Roslyn\Internal\Performance\Text\FileLengthThreshold"));
+    }
+
     [DebuggerDisplay("{GetDebuggerDisplay(), nq}")]
     public class FileTextLoader : TextLoader
     {
@@ -69,6 +86,8 @@ namespace Microsoft.CodeAnalysis
         /// <exception cref="IOException"></exception>
         public override async Task<TextAndVersion> LoadTextAndVersionAsync(Workspace workspace, DocumentId documentId, CancellationToken cancellationToken)
         {
+            ValidateFileLength(workspace, _path);
+
             DateTime prevLastWriteTime = FileUtilities.GetFileTimeStamp(_path);
 
             TextAndVersion textAndVersion;
@@ -168,6 +187,8 @@ namespace Microsoft.CodeAnalysis
 
         internal override TextAndVersion LoadTextAndVersionSynchronously(Workspace workspace, DocumentId documentId, CancellationToken cancellationToken)
         {
+            ValidateFileLength(workspace, _path);
+
             DateTime prevLastWriteTime = FileUtilities.GetFileTimeStamp(_path);
 
             TextAndVersion textAndVersion;
@@ -197,6 +218,31 @@ namespace Microsoft.CodeAnalysis
         private string GetDebuggerDisplay()
         {
             return nameof(Path) + " = " + Path;
+        }
+
+        private static void ValidateFileLength(Workspace workspace, string path)
+        {
+            // Validate file length is under our threshold. 
+            // Otherwise, rather than reading the content into the memory, we will throw
+            // InvalidDataException to caller of FileTextLoader.LoadText to deal with 
+            // the situation.
+            // 
+            // check this (http://source.roslyn.io/#Microsoft.CodeAnalysis.Workspaces/Workspace/Solution/TextDocumentState.cs,132)
+            // to see how workspace deal with exception from FileTextLoader. other consumer can handle the exception differently
+            var fileLength = FileUtilities.GetFileLength(path);
+            var threshold = workspace.Options.GetOption(FileTextLoaderOptions.FileLengthThreshold);
+            if (fileLength > threshold)
+            {
+                // log max file length which will log to VS telemetry in VS host
+                Logger.Log(FunctionId.FileTextLoader_FileLengthThresholdExceeded, KeyValueLogMessage.Create(m =>
+                {
+                    m["FileLength"] = fileLength;
+                    m["Ext"] = PathUtilities.GetExtension(path);
+                }));
+
+                var message = string.Format(WorkspacesResources.File_0_size_of_1_exceeds_maximum_allowed_size_of_2, path, fileLength, threshold);
+                throw new InvalidDataException(message);
+            }
         }
     }
 }
