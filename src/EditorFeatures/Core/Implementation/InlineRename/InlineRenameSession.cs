556ad4e43819ea2f0bdca3af53c70a39916429c8
Saul Rennison <saul.rennison@gmail.com>
2017-01-27 23:56:20 +0000

    Fixed inline renames not using GetFinalSymbolName for the actual rename
    Fixes #16460

--- src/EditorFeatures/Core/Implementation/InlineRename/InlineRenameSession.cs
+++ src/EditorFeatures/Core/Implementation/InlineRename/InlineRenameSession.cs
@@ -380,7 +380,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.InlineRename
         {
             AssertIsForeground();
             VerifyNotDismissed();
-            this.ReplacementText = replacementText;
+            this.ReplacementText = _renameInfo.GetFinalSymbolName(replacementText);
 
             Action propagateEditAction = delegate
             {
@@ -587,7 +587,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.InlineRename
                     newSolution = previewService.PreviewChanges(
                         string.Format(EditorFeaturesResources.Preview_Changes_0, EditorFeaturesResources.Rename),
                         "vs.csharp.refactoring.rename",
-                        string.Format(EditorFeaturesResources.Rename_0_to_1_colon, this.OriginalSymbolName, _renameInfo.GetFinalSymbolName(this.ReplacementText)),
+                        string.Format(EditorFeaturesResources.Rename_0_to_1_colon, this.OriginalSymbolName, this.ReplacementText),
                         _renameInfo.FullDisplayName,
                         _renameInfo.Glyph,
                         _conflictResolutionTask.Result.NewSolution,
