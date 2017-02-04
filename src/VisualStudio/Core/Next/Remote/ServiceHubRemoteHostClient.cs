524b609511613424cc16501757360fd7faebcc96
Heejae Chang <hechang@microsoft.com>
2017-02-03 18:37:33 -0800

    made all services to explicitly call JsonRpc.StartListening. otherwise, we can get into a race.

--- src/VisualStudio/Core/Next/Remote/ServiceHubRemoteHostClient.cs
+++ src/VisualStudio/Core/Next/Remote/ServiceHubRemoteHostClient.cs
@@ -18,7 +18,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Remote
     using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
     using Workspace = Microsoft.CodeAnalysis.Workspace;
 
-    internal partial class ServiceHubRemoteHostClient : RemoteHostClient
+    internal sealed partial class ServiceHubRemoteHostClient : RemoteHostClient
     {
         private readonly HubClient _hubClient;
         private readonly JsonRpc _rpc;
@@ -81,10 +81,12 @@ namespace Microsoft.VisualStudio.LanguageServices.Remote
             _hubClient = hubClient;
             _hostGroup = hostGroup;
 
-            _rpc = JsonRpc.Attach(stream, target: this);
+            _rpc = new JsonRpc(stream, stream, target: this);
 
             // handle disconnected situation
             _rpc.Disconnected += OnRpcDisconnected;
+
+            _rpc.StartListening();
         }
 
         protected override async Task<Session> CreateServiceSessionAsync(string serviceName, PinnedRemotableDataScope snapshot, object callbackTarget, CancellationToken cancellationToken)
