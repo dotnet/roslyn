014acbe5dc8e66315b6d9c5ed6934b8b4b8c6000
CyrusNajmabadi <cyrusn@microsoft.com>
2017-02-09 14:01:18 -0800

    AddParameterwork

---
+++ src/Features/Core/Portable/AddParameter/AbstractAddParameterCodeFixProvider.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace Microsoft.CodeAnalysis.AddParameter
+{
+    internal abstract class AbstractAddParameterCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => throw new NotImplementedException();
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
