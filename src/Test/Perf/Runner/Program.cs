58d8b77ea31d3d980839c1f0568c21bf3ade26b9
Ravi Chande <rchande@microsoft.com>
2017-02-14 13:41:50 -0800

    Remove line

--- src/Test/Perf/Runner/Program.cs
+++ src/Test/Perf/Runner/Program.cs
@@ -147,7 +147,6 @@ namespace Runner
 
             // Print message at startup
             Log("Starting Performance Test Run");
-            Log($"Base directory {testDirectory}");
             Log("hash: " + FirstLine(StdoutFromOrDefault("git", "show --format=\"%h\" HEAD --", "git missing")));
             Log("time: " + DateTime.Now.ToString());
 
