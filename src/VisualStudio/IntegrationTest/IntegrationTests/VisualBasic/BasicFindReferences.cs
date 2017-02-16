d8040e296a897765de685789fe29f11beaf18f52
Tom Meschter <tom.meschter@microsoft.com>
2017-02-15 16:01:07 -0800

    Add a simple integration test for VB Find References
    This test sets up a very simple project where a local is defined in one location and referenced in another. It then verifies that:
    
    1. Shift+F12 invoked Find All References.
    2. The "'local' references" window with the results is active.
    3. The results window includes the expected references.

---
+++ src/VisualStudio/IntegrationTest/IntegrationTests/VisualBasic/BasicFindReferences.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Shared.TestHooks;
+using Microsoft.VisualStudio.IntegrationTest.Utilities;
+using Microsoft.VisualStudio.IntegrationTest.Utilities.Input;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Roslyn.VisualStudio.IntegrationTests.VisualBasic
+{
+    [Collection(nameof(SharedIntegrationHostFixture))]
+    public class BasicFindReferences : AbstractEditorTest
+    {
+        protected override string LanguageName => LanguageNames.VisualBasic;
+
+        public BasicFindReferences(VisualStudioInstanceFactory instanceFactory)
+            : base(instanceFactory, nameof(BasicFindReferences))
+        {
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.FindReferences)]
+        public void Locals()
+        {
+            SetUpEditor(@"
+Class Program
+  Sub Main()
+      Dim local = 1
+      Console.WriteLine(loca$$l)
+  End Sub
+End Class
+");
+
+            SendKeys(Shift(VirtualKey.F12));
+
+            VisualStudioWorkspaceOutOfProc.WaitForAsyncOperations(FeatureAttribute.FindReferences);
+
+            var activeWindowCaption = VisualStudio.Instance.Shell.GetActiveWindowCaption();
+
+            const string localReferencesCaption = "'local' references";
+            Assert.Equal(expected: localReferencesCaption, actual: activeWindowCaption);
+
+            var findReferencesResults = VisualStudio.Instance.FindUsagesWindow.GetContents(localReferencesCaption);
+
+            Assert.Equal(expected: 1, actual: findReferencesResults.Length);
+            Assert.Equal(expected: "Console.WriteLine(local)", actual: findReferencesResults[0]);
+        }
+    }
+}
