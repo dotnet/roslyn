6420430ecf3b7431bf7ab5c0d9792e4e1461736b
CyrusNajmabadi <cyrusn@microsoft.com>
2017-02-23 02:42:41 -0800

    Fix issue generating GetHashCode for zero members.

--- src/Workspaces/Core/Portable/Shared/Extensions/ICodeDefinitionFactoryExtensions_CreateGetHashCodeMethod.cs
+++ src/Workspaces/Core/Portable/Shared/Extensions/ICodeDefinitionFactoryExtensions_CreateGetHashCodeMethod.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Microsoft.CodeAnalysis.CodeGeneration;
 using Microsoft.CodeAnalysis.Editing;
@@ -9,7 +10,7 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
 {
     internal static partial class ICodeDefinitionFactoryExtensions
     {
-        private const string GetHashCodeName = "GetHashCode";
+        private const string GetHashCodeName = nameof(object.GetHashCode);
 
         public static IMethodSymbol CreateGetHashCodeMethod(
             this SyntaxGenerator factory,
@@ -40,50 +41,91 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
             IList<ISymbol> members,
             CancellationToken cancellationToken)
         {
-            const string HashCodeName = "hashCode";
-
-            // -1521134295
-            var permuteValue = factory.NegateExpression(
-                factory.LiteralExpression(1521134295));
-
             var statements = new List<SyntaxNode>();
 
-            var hashCodeNameExpression = factory.IdentifierName(HashCodeName);
+            var hasBaseGetHashCode = HasExistingBaseGetHashCodeMethod(containingType, cancellationToken);
+            var baseHashCode = factory.InvocationExpression(
+                factory.MemberAccessExpression(factory.BaseExpression(), GetHashCodeName));
 
-            var firstHashValue = ComputeHashValue(factory, compilation, members[0]);
-            if (members.Count == 1)
+            if (members.Count == 0)
             {
 #if false
-                return this.S1.GetHashCode();
+                return 0; // or
+                return base.GetHashCode();
 #endif
-                statements.Add(factory.ReturnStatement(firstHashValue));
+                var expression = hasBaseGetHashCode
+                    ? baseHashCode
+                    : factory.LiteralExpression(0);
+
+                statements.Add(factory.ReturnStatement(expression));
             }
             else
             {
-#if false
-                var hashCode = this.S1.GetHashCode();
-#endif
-                statements.Add(factory.LocalDeclarationStatement(HashCodeName, firstHashValue));
+                const string HashCodeName = "hashCode";
 
-                for (var i = 1; i < members.Count; i++)
+                // -1521134295
+                var permuteValue = factory.NegateExpression(
+                    factory.LiteralExpression(1521134295));
+
+                var hashCodeNameExpression = factory.IdentifierName(HashCodeName);
+
+                var firstMemberHashValue = ComputeHashValue(factory, compilation, members[0]);
+                if (members.Count == 1 && !hasBaseGetHashCode)
                 {
 #if false
-                    hashCode = hashCode * 0xA5555529 + value
+                    return this.S1.GetHashCode();
 #endif
-                    statements.Add(factory.ExpressionStatement(
-                        factory.AssignmentStatement(hashCodeNameExpression,
-                            factory.AddExpression(
-                                factory.MultiplyExpression(hashCodeNameExpression, permuteValue),
-                                ComputeHashValue(factory, compilation, members[i])))));
+                    statements.Add(factory.ReturnStatement(firstMemberHashValue));
                 }
+                else
+                {
+#if false
+                    var hashCode = this.S1.GetHashCode(); // or
+                    var hashCode = base.GetHashCode();
+#endif
+
+                    var firstInit = hasBaseGetHashCode
+                        ? baseHashCode
+                        : firstMemberHashValue;
+                    statements.Add(factory.LocalDeclarationStatement(HashCodeName, firstInit));
+
+                    var startingIndex = hasBaseGetHashCode ? 0 : 1;
+                    for (var i = startingIndex; i < members.Count; i++)
+                    {
+#if false
+                        hashCode = hashCode * 0xA5555529 + value
+#endif
+                        statements.Add(factory.ExpressionStatement(
+                            factory.AssignmentStatement(hashCodeNameExpression,
+                                factory.AddExpression(
+                                    factory.MultiplyExpression(hashCodeNameExpression, permuteValue),
+                                    ComputeHashValue(factory, compilation, members[i])))));
+                    }
 
 #if false
                 return hashCode;
 #endif
-                statements.Add(factory.ReturnStatement(hashCodeNameExpression));
+                    statements.Add(factory.ReturnStatement(hashCodeNameExpression));
+                }
             }
 
             return statements;
+        }
+
+        private static bool HasExistingBaseGetHashCodeMethod(INamedTypeSymbol containingType, CancellationToken cancellationToken)
+        {
+            // Check if any of our base types override GetHashCode.  If so, first check with them.
+            var existingMethods =
+                from baseType in containingType.GetBaseTypes()
+                from method in baseType.GetMembers(GetHashCodeName).OfType<IMethodSymbol>()
+                where method.IsOverride &&
+                      method.DeclaredAccessibility == Accessibility.Public &&
+                      !method.IsStatic &&
+                      method.Parameters.Length == 0 &&
+                      method.ReturnType.SpecialType == SpecialType.System_Int32
+                select method;
+
+            return existingMethods.Any();
         }
 
         private static SyntaxNode ComputeHashValue(
