d2fbc0140191f5da8770a29af4affa04788c5751
AdamSpeight2008 <Adam.Speight@Virgin.net>
2017-02-24 16:14:34 +0000

    Add Optional parameter to PooledStringBuilder.CreatePool
    This simplifies the code needed for non-default pool sizes.
    The values of `lower_bound`(32) and `upper_bound` (256) are arbitrary.

--- src/Dependencies/PooledObjects/PooledStringBuilder.cs
+++ src/Dependencies/PooledObjects/PooledStringBuilder.cs
@@ -72,10 +72,18 @@ namespace Microsoft.CodeAnalysis.Collections
         private static readonly ObjectPool<PooledStringBuilder> s_poolInstance = CreatePool();
 
         // if someone needs to create a private pool;
-        public static ObjectPool<PooledStringBuilder> CreatePool()
+        /// <summary>
+        /// If someone need to create a private pool
+        /// </summary>
+        /// <param name="size">Initial size of the pool. Bounded to 32 &lt;= Size &lt;= 256)</param>
+        /// <returns></returns>
+        public static ObjectPool<PooledStringBuilder> CreatePool(int size = 32)
         {
+            const int lower_bound = 32;
+            const int upper_bound = 256;
             ObjectPool<PooledStringBuilder> pool = null;
-            pool = new ObjectPool<PooledStringBuilder>(() => new PooledStringBuilder(pool), 32);
+            size = (size < lower_bound ? lower_bound: (size > upper_bound ? upper_bound : size));
+            pool = new ObjectPool<PooledStringBuilder>(() => new PooledStringBuilder(pool), size);
             return pool;
         }
 
