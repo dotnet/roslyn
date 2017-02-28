0d6ef38edd274cc32cae2d9f7b40f783d4896a4f
AdamSpeight2008 <Adam.Speight@Virgin.net>
2017-02-28 15:56:46 +0000

    Further Feedback changes.

--- src/Compilers/Test/Utilities/VisualBasic/CompilationTestUtils.vb
+++ src/Compilers/Test/Utilities/VisualBasic/CompilationTestUtils.vb
@@ -888,7 +888,7 @@ Friend Module CompilationUtils
                     .AppendLine("UNEXPECTED ERROR MESSAGES:")
                     .AppendLine(actualText.Substring(expectedText.Length))
 
-                    Assert.True(False, messages.ToStringAndFree())
+                    Assert.True(False, .ToString())
                 Else
                     Dim expectedLines = expectedText.Split({vbCrLf}, StringSplitOptions.RemoveEmptyEntries)
                     Dim actualLines = actualText.Split({vbCrLf}, StringSplitOptions.RemoveEmptyEntries)
@@ -912,7 +912,7 @@ Friend Module CompilationUtils
                     Next
 
                     If appendedLines > 0 Then
-                        Assert.True(False, messages.Builder.ToString())
+                        Assert.True(False, .ToString())
                     Else
                         CompareLineByLine(expectedText, actualText)
                     End If
