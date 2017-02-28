d6dbe5bed158c4a094f2d4bca577c50396b3ef94
AdamSpeight2008 <Adam.Speight@Virgin.net>
2017-02-28 17:15:25 +0000

    Test doubled pool sizes

--- src/Compilers/Test/Utilities/VisualBasic/ParserTestUtilities.vb
+++ src/Compilers/Test/Utilities/VisualBasic/ParserTestUtilities.vb
@@ -13,7 +13,7 @@ Imports Xunit
 Imports Microsoft.CodeAnalysis.Collections
 
 Friend Module ParserTestUtilities
-    Friend ReadOnly Property PooledStringBuilderPool As ObjectPool(Of PooledStringBuilder) = PooledStringBuilder.CreatePool(If(Environment.Is64BitProcess, 64, 32))
+    Friend ReadOnly Property PooledStringBuilderPool As ObjectPool(Of PooledStringBuilder) = PooledStringBuilder.CreatePool(If(Environment.Is64BitProcess, 128, 64))
 
     ' TODO (tomat): only checks error codes; we should also check error span and arguments
     Public Function ParseAndVerify(code As XCData, Optional expectedErrors As XElement = Nothing) As SyntaxTree
