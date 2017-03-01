b34755ec1ede310738d2add1b5a956236c1290a8
CyrusNajmabadi <cyrusn@microsoft.com>
2017-02-28 17:47:46 -0800

    Make methods synchronous

--- src/EditorFeatures/TestUtilities/Workspaces/TestWorkspace_Create.cs
+++ src/EditorFeatures/TestUtilities/Workspaces/TestWorkspace_Create.cs
@@ -60,13 +60,13 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces
         /// Creates a single buffer in a workspace.
         /// </summary>
         /// <param name="content">Lines of text, the buffer contents</param>
-        internal static Task<TestWorkspace> CreateAsync(
+        internal static async Task<TestWorkspace> CreateAsync(
             string language,
             CompilationOptions compilationOptions,
             ParseOptions parseOptions,
             string content)
         {
-            return CreateAsync(language, compilationOptions, parseOptions, new[] { content });
+            return Create(language, compilationOptions, parseOptions, new[] { content });
         }
 
         /// <summary>
@@ -99,7 +99,7 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces
         }
 
         /// <param name="files">Can pass in multiple file contents: files will be named test1.cs, test2.cs, etc.</param>
-        internal static async Task<TestWorkspace> CreateAsync(
+        internal static TestWorkspace Create(
             string language,
             CompilationOptions compilationOptions,
             ParseOptions parseOptions,
