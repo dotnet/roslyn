6f0b9c6eaf84297f1b32862b86bd1a56e0b409b4
Tom Meschter <tom.meschter@microsoft.com>
2017-03-03 13:48:10 -0800

    Add doc comments for ExtractInterfaceDialog_OutOfProc

--- src/VisualStudio/IntegrationTest/TestUtilities/OutOfProcess/ExtractInterfaceDialog_OutOfProc.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/OutOfProcess/ExtractInterfaceDialog_OutOfProc.cs
@@ -7,6 +7,9 @@ using Microsoft.CodeAnalysis.Shared.TestHooks;
 
 namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
 {
+    /// <summary>
+    /// Handles interaction with the Extract Interface Dialog.
+    /// </summary>
     public class ExtractInterfaceDialog_OutOfProc : OutOfProcComponent
     {
         private const string ExtractInterfaceDialogID = "ExtractInterfaceDialog";
@@ -16,6 +19,9 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
         {
         }
 
+        /// <summary>
+        /// Verifies that the Extract Interface dialog is currently open.
+        /// </summary>
         public void VerifyOpen()
         {
             var dialog = DialogHelpers.FindDialog(GetMainWindowHWnd(), ExtractInterfaceDialogID, isOpen: true);
@@ -26,6 +32,9 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
             }
         }
 
+        /// <summary>
+        /// Verifies that the Extract Interface dialog is currently closed.
+        /// </summary>
         public void VerifyClosed()
         {
             var dialog = DialogHelpers.FindDialog(GetMainWindowHWnd(), ExtractInterfaceDialogID, isOpen: false);
@@ -36,18 +45,27 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
             }
         }
 
+        /// <summary>
+        /// Clicks the "OK" button and waits for the Extract Interface operation to complete.
+        /// </summary>
         public void ClickOK()
         {
             DialogHelpers.PressButton(GetMainWindowHWnd(), ExtractInterfaceDialogID, "OkButton");
             VisualStudioInstance.VisualStudioWorkspace.WaitForAsyncOperations(FeatureAttribute.LightBulb);
         }
 
+        /// <summary>
+        /// Clicks the "Cancel" button and waits for the Extract Interface operation to complete.
+        /// </summary>
         public void ClickCancel()
         {
             DialogHelpers.PressButton(GetMainWindowHWnd(), ExtractInterfaceDialogID, "CancelButton");
             VisualStudioInstance.VisualStudioWorkspace.WaitForAsyncOperations(FeatureAttribute.LightBulb);
         }
 
+        /// <summary>
+        /// Returns the name of the generated file that will contain the interface.
+        /// </summary>
         public string GetTargetFileName()
         {
             var dialog = DialogHelpers.GetOpenDialog(GetMainWindowHWnd(), ExtractInterfaceDialogID);
@@ -57,6 +75,9 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
             return fileNameTextBox.GetValue();
         }
 
+        /// <summary>
+        /// Gets the set of members that are currently checked.
+        /// </summary>
         public string[] GetSelectedItems()
         {
             var dialog = DialogHelpers.GetOpenDialog(GetMainWindowHWnd(), ExtractInterfaceDialogID);
@@ -71,16 +92,26 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
                 .ToArray();
         }
 
+        /// <summary>
+        /// Clicks the "Deselect All" button.
+        /// </summary>
         public void ClickDeselectAll()
         {
             DialogHelpers.PressButton(GetMainWindowHWnd(), ExtractInterfaceDialogID, "DeselectAllButton");
         }
 
+        /// <summary>
+        ///  Clicks the "Select All" button.
+        /// </summary>
         public void ClickSelectAll()
         {
             DialogHelpers.PressButton(GetMainWindowHWnd(), ExtractInterfaceDialogID, "SelectAllButton");
         }
 
+        /// <summary>
+        /// Clicks the checkbox on the given item, cycling it from on to off or from off to on.
+        /// </summary>
+        /// <param name="item"></param>
         public void ToggleItem(string item)
         {
             var dialog = DialogHelpers.GetOpenDialog(GetMainWindowHWnd(), ExtractInterfaceDialogID);
