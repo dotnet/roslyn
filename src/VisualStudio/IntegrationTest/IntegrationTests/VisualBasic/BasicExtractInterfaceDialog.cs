beff18da3f464335a6f100016b3fa136c48fafd3
Tom Meschter <tom.meschter@microsoft.com>
2017-03-03 10:25:01 -0800

    Add the first Extract Interface Dialog test
    Includes the basic test that runs Extract Interface and clicks the "OK" button, and just enough infrastructure to make it work.

---
+++ src/VisualStudio/IntegrationTest/IntegrationTests/VisualBasic/BasicExtractInterfaceDialog.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.IntegrationTest.Utilities;
+using Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Roslyn.VisualStudio.IntegrationTests.VisualBasic
+{
+    [Collection(nameof(SharedIntegrationHostFixture))]
+    public class BasicExtractInterfaceDialog : AbstractEditorTest
+    {
+        protected override string LanguageName => LanguageNames.VisualBasic;
+
+        private ExtractInterfaceDialog_OutOfProc ExtractInterfaceDialog => VisualStudio.Instance.ExtractInterfaceDialog;
+
+        public BasicExtractInterfaceDialog(VisualStudioInstanceFactory instanceFactory)
+            : base(instanceFactory, nameof(BasicExtractInterfaceDialog))
+        {
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsExtractInterface)]
+        public void CoreScenario()
+        {
+            SetUpEditor(@"Class C$$
+    Public Sub M()
+    End Sub
+End Class");
+
+            InvokeCodeActionList();
+            VerifyCodeAction("Extract Interface...",
+                applyFix: true,
+                blockUntilComplete: false);
+
+            ExtractInterfaceDialog.VerifyOpen();
+            ExtractInterfaceDialog.ClickOK();
+            ExtractInterfaceDialog.VerifyClosed();
+
+            VisualStudio.Instance.SolutionExplorer.OpenFile(ProjectName, "Class1.vb");
+
+            VerifyTextContains(@"Class C
+    Implements IC
+    Public Sub M() Implements IC.M
+    End Sub
+End Class");
+
+            VisualStudio.Instance.SolutionExplorer.OpenFile(ProjectName, "IC.vb");
+
+            VerifyTextContains(@"Interface IC
+    Sub M()
+End Interface");
+        }
+    }
+}
---
+++ src/VisualStudio/IntegrationTest/TestUtilities/OutOfProcess/ExtractInterfaceDialog_OutOfProc.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis.Shared.TestHooks;
+
+namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
+{
+    public class ExtractInterfaceDialog_OutOfProc : OutOfProcComponent
+    {
+        private const string ExtractInterfaceDialogID = "ExtractInterfaceDialog";
+
+        public ExtractInterfaceDialog_OutOfProc(VisualStudioInstance visualStudioInstance)
+            : base(visualStudioInstance)
+        {
+        }
+
+        public void VerifyOpen()
+        {
+            var dialog = DialogHelpers.FindDialog(GetMainWindowHWnd(), ExtractInterfaceDialogID, isOpen: true);
+
+            if (dialog == null)
+            {
+                throw new InvalidOperationException($"Expected the '{ExtractInterfaceDialogID}' dialog to be open but it is not.");
+            }
+        }
+
+        public void VerifyClosed()
+        {
+            var dialog = DialogHelpers.FindDialog(GetMainWindowHWnd(), ExtractInterfaceDialogID, isOpen: false);
+
+            if (dialog != null)
+            {
+                throw new InvalidOperationException($"Expected the '{ExtractInterfaceDialogID}' dialog to be closed but it is not.");
+            }
+        }
+
+        public void ClickOK()
+        {
+            DialogHelpers.PressButton(GetMainWindowHWnd(), ExtractInterfaceDialogID, "OkButton");
+            VisualStudioInstance.VisualStudioWorkspace.WaitForAsyncOperations(FeatureAttribute.LightBulb);
+        }
+
+        private int GetMainWindowHWnd()
+        {
+            return VisualStudioInstance.Shell.GetHWnd();
+        }
+    }
+}
