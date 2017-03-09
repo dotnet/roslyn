754ef0659530a41bc7d5f74518bc718910026a5e
AdamSpeight2008 <Adam.Speight@Virgin.net>
2017-03-09 08:43:38 +0000

    Use ImmutableArray rather then IList

--- src/Compilers/Test/Utilities/VisualBasic/CompilationTestUtils.vb
+++ src/Compilers/Test/Utilities/VisualBasic/CompilationTestUtils.vb
@@ -841,7 +841,7 @@ Friend Module CompilationUtils
                                            ) As (tree As SyntaxTree, spans As IList(Of TextSpan))
         Dim codeWithMarker As String = programElement.Source.Text
         Dim codeWithoutMarker As String = Nothing
-        Dim spans As IList(Of TextSpan) = Nothing
+        Dim spans As ImmutableArray(Of TextSpan) = Nothing
         MarkupTestFile.GetSpans(codeWithMarker, codeWithoutMarker, spans)
 
         Dim text = SourceText.From(codeWithoutMarker, Encoding.UTF8)
