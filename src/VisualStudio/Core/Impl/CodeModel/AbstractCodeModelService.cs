b1c4b0219754097a37f916294022e247e61979da
CyrusNajmabadi <cyrusn@microsoft.com>
2017-03-10 16:40:22 -0800

    Use patterns in more places.

--- src/VisualStudio/Core/Impl/CodeModel/AbstractCodeModelService.cs
+++ src/VisualStudio/Core/Impl/CodeModel/AbstractCodeModelService.cs
@@ -992,9 +992,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.CodeModel
         {
             int result;
 
-            if (position is int)
+            if (position is int i)
             {
-                result = (int)position;
+                result = i;
             }
             else if (position is EnvDTE.CodeElement)
             {
@@ -1012,9 +1012,8 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.CodeModel
 
                 result = getIndexInContainer(containerNode, child => child == positionNode);
             }
-            else if (position is string)
+            else if (position is string name)
             {
-                var name = (string)position;
                 result = getIndexInContainer(containerNode, child => GetName(child) == name);
             }
             else if (position == null || position == Type.Missing)
