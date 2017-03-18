4af7a76da897c21ae4e8b9340adb20bf3dec1993
CyrusNajmabadi <cyrusn@microsoft.com>
2017-03-17 18:05:49 -0700

    Add tests.

--- src/EditorFeatures/CSharpTest/InitializeParameter/InitializeMemberFromParameterTests.cs
+++ src/EditorFeatures/CSharpTest/InitializeParameter/InitializeMemberFromParameterTests.cs
@@ -38,5 +38,140 @@ class C
     }
 }");
         }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInitializeParameter)]
+        public async Task TestInitializeFieldWithUnderscoreName()
+        {
+            await TestInRegularAndScript1Async(
+@"
+class C
+{
+    private string _s;
+
+    public C([||]string s)
+    {
+    }
+}",
+@"
+class C
+{
+    private string _s;
+
+    public C(string s)
+    {
+        this._s = s;
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInitializeParameter)]
+        public async Task TestInitializeWritableProperty()
+        {
+            await TestInRegularAndScript1Async(
+@"
+class C
+{
+    private string S { get; }
+
+    public C([||]string s)
+    {
+    }
+}",
+@"
+class C
+{
+    private string S { get; }
+
+    public C(string s)
+    {
+        this.S = s;
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInitializeParameter)]
+        public async Task TestInitializeFieldWithDifferentName()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"
+class C
+{
+    private string t;
+
+    public C([||]string s)
+    {
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInitializeParameter)]
+        public async Task TestInitializeNonWritableProperty()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"
+class C
+{
+    private string S => null;
+
+    public C([||]string s)
+    {
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInitializeParameter)]
+        public async Task TestInitializePropertyWithBadName()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"
+class C
+{
+    private string T { get; }
+
+    public C([||]string s)
+    {
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInitializeParameter)]
+        public async Task TestInitializeFieldWithWrongType()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"
+class C
+{
+    private int s;
+
+    public C([||]string s)
+    {
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInitializeParameter)]
+        public async Task TestInitializeFieldWithConvertibleType()
+        {
+            await TestInRegularAndScriptAsync(
+@"
+class C
+{
+    private object s;
+
+    public C([||]string s)
+    {
+    }
+}",
+@"
+class C
+{
+    private object s;
+
+    public C(string s)
+    {
+        this.s = s;
+    }
+}");
+        }
     }
 }
