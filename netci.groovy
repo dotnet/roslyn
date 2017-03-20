998941327f20a1d19ae331505d9c2926edc01a71
Jared Parsons <jaredpar@microsoft.com>
2017-03-20 09:32:33 -0700

    Move Microbuild back to a VS image
    The build depends on a global tool corflags to complete.  Unblocking build for now.

--- netci.groovy
+++ netci.groovy
@@ -186,7 +186,7 @@ commitPullList.each { isPr ->
 
   def triggerPhraseOnly = false
   def triggerPhraseExtra = "microbuild"
-  Utilities.setMachineAffinity(myJob, 'Windows_NT', 'win2016-base')
+  Utilities.setMachineAffinity(myJob, 'Windows_NT', 'latest-or-auto-dev15-0')
   addRoslynJob(myJob, jobName, branchName, isPr, triggerPhraseExtra, triggerPhraseOnly)
 }
 
