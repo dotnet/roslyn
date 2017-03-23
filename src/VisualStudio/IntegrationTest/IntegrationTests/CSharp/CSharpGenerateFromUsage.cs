d43f02053de0b8b8ba15248781ffd632cd92e644
David Poeschl <davip@microsoft.com>
2017-03-23 14:33:19 -0700

    Port Generate From Usage tests

---
+++ src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpGenerateFromUsage.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Shared.TestHooks;
+using Microsoft.VisualStudio.IntegrationTest.Utilities;
+using Roslyn.Test.Utilities;
+using Roslyn.VisualStudio.IntegrationTests.Extensions;
+using Roslyn.VisualStudio.IntegrationTests.Extensions.Editor;
+using Xunit;
+
+namespace Roslyn.VisualStudio.IntegrationTests.CSharp
+{
+    [Collection(nameof(SharedIntegrationHostFixture))]
+    public class CSharpGenerateFromUsage : AbstractEditorTest
+    {
+        protected override string LanguageName => LanguageNames.CSharp;
+
+        public CSharpGenerateFromUsage(VisualStudioInstanceFactory instanceFactory)
+            : base(instanceFactory, nameof(CSharpGenerateFromUsage))
+        {
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsGenerateLocal)]
+        public void GenerateLocal()
+        {
+            SetUpEditor(
+@"class Program
+{
+    static void Main(string[] args)
+    {
+        string s = $$xyz;
+    }
+}");
+            this.VerifyCodeAction("Generate local 'xyz'", applyFix: true);
+            this.VerifyTextContains(
+@"class Program
+{
+    static void Main(string[] args)
+    {
+        string xyz = null;
+        string s = xyz;
+    }
+}");
+        }
+    }
+}
---
+++ src/VisualStudio/IntegrationTest/IntegrationTests/VisualBasic/BasicGenerateFromUsage.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.IntegrationTest.Utilities;
+using Roslyn.Test.Utilities;
+using Roslyn.VisualStudio.IntegrationTests.Extensions;
+using Roslyn.VisualStudio.IntegrationTests.Extensions.Editor;
+using Xunit;
+
+namespace Roslyn.VisualStudio.IntegrationTests.VisualBasic
+{
+    [Collection(nameof(SharedIntegrationHostFixture))]
+    public class BasicGenerateFromUsage : AbstractEditorTest
+    {
+        protected override string LanguageName => LanguageNames.VisualBasic;
+
+        public BasicGenerateFromUsage(VisualStudioInstanceFactory instanceFactory)
+            : base(instanceFactory, nameof(BasicGenerateFromUsage))
+        {
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsGenerateLocal)]
+        public void GenerateLocal()
+        {
+            SetUpEditor(
+@"Module Program
+    Sub Main(args As String())
+        Dim x As String = $$xyz
+    End Sub
+End Module");
+            this.VerifyCodeAction("Generate local 'xyz'", applyFix: true);
+            this.VerifyTextContains(
+@"Module Program
+    Sub Main(args As String())
+        Dim xyz As String = Nothing
+        Dim x As String = xyz
+    End Sub
+End Module");
+        }
+    }
+}
