cf12cd2a4be6a7c626f0380876556350d2b7f8b8
Ty Overby <ty@pre-alpha.com>
2017-03-23 14:42:28 -0700

    add perf test

---
+++ src/Compilers/Perf/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Xunit.Performance;
+using System.Threading;
+
+namespace Runner
+{
+    public static class Program
+    {
+        [Benchmark]
+        public static void BenchFoo() {
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    Thread.Sleep(1);
+                }
+            }
+        }
+    }
+}
