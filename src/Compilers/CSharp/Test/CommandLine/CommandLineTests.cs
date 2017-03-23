c9b6181f0a0c6692e36820cd689f6066218a40d6
Julien Couvreur <julien.couvreur@gmail.com>
2017-03-23 10:28:55 -0700

    Make refout tests more resilient to environment differences

--- src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
+++ src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
@@ -8915,11 +8915,11 @@ class C {
         public void RefOut()
         {
             var dir = Temp.CreateDirectory();
-            dir.CreateDirectory("ref");
+            var refDir = dir.CreateDirectory("ref");
 
             var src = dir.CreateFile("a.cs");
             src.WriteAllText(@"
-class C
+public class C
 {
     /// <summary>Main method</summary>
     public static void Main()
@@ -8977,10 +8977,43 @@ class C
                 new[] { "CompilationRelaxationsAttribute", "RuntimeCompatibilityAttribute", "DebuggableAttribute" }
                 );
 
-            output = ProcessUtilities.RunAndGetOutput(refDll, startFolder: dir.ToString(), expectedRetCode: -532462766);
+            VerifyNullReferenceException(refDir, "a.dll");
 
             // Clean up temp files
             CleanupAllGeneratedFiles(dir.Path);
+        }
+
+        /// <summary>
+        /// Calls C.Main() on the program and verifies that a null reference exception is thrown.
+        /// </summary>
+        internal static void VerifyNullReferenceException(TempDirectory dir, string programName)
+        {
+            var src = dir.CreateFile("verifier.cs");
+            src.WriteAllText(@"
+class Verifier
+{
+    public static void Main()
+    {
+        try
+        {
+            C.Main();
+        }
+        catch(System.NullReferenceException)
+        {
+            System.Console.Write(""Got expected exception"");
+        }
+    }
+}");
+
+            var outWriter = new StringWriter(CultureInfo.InvariantCulture);
+            var csc = new MockCSharpCompiler(null, dir.Path,
+                new[] { "/nologo", "/out:verifier.exe", $"/reference:{programName}", "verifier.cs" });
+
+            int exitCode = csc.Run(outWriter);
+            Assert.Equal(0, exitCode);
+            string dirPath = dir.ToString();
+            string output = ProcessUtilities.RunAndGetOutput(Path.Combine(dirPath, "verifier.exe"), startFolder: dirPath, expectedRetCode: 0);
+            Assert.Equal("Got expected exception", output);
         }
 
         [Fact]
--- src/Compilers/VisualBasic/Test/CommandLine/CommandLineTests.vb
+++ src/Compilers/VisualBasic/Test/CommandLine/CommandLineTests.vb
@@ -8019,11 +8019,11 @@ End Class
         <Fact>
         Public Sub RefOut()
             Dim dir = Temp.CreateDirectory()
-            dir.CreateDirectory("ref")
+            Dim refDir = dir.CreateDirectory("ref")
 
             Dim src = dir.CreateFile("a.vb")
             src.WriteAllText("
-Class C
+Public Class C
     ''' <summary>Main method</summary>
     Public Shared Sub Main()
         System.Console.Write(""Hello"")
@@ -8081,10 +8081,38 @@ a
                 {"CompilationRelaxationsAttribute", "RuntimeCompatibilityAttribute", "DebuggableAttribute", "STAThreadAttribute"}
                 )
 
-            output = ProcessUtilities.RunAndGetOutput(refDll, startFolder:=dir.ToString(), expectedRetCode:=-532462766)
+            VerifyNullReferenceException(refDir, "a.dll")
 
             ' Clean up temp files
             CleanupAllGeneratedFiles(dir.Path)
+        End Sub
+
+        ''' <summary>
+        ''' Calls C.Main() on the program and verifies that a null reference exception is thrown.
+        ''' </summary>
+        Private Shared Sub VerifyNullReferenceException(dir As TempDirectory, referenceName As String)
+
+            Dim src = dir.CreateFile("verifier.vb")
+            src.WriteAllText("
+Class Verifier
+    Public Shared Sub Main()
+        Try
+            C.Main()
+        Catch ex As System.NullReferenceException
+            System.Console.Write(""Got expected exception"")
+        End Try
+    End Sub
+End Class")
+
+            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
+            Dim vbc = New MockVisualBasicCompiler(Nothing, dir.Path,
+                {"/define:_MYTYPE=""Empty"" ", "/nologo", "/out:verifier.exe", $"/reference:{referenceName}", "verifier.vb"})
+
+            Dim exitCode = vbc.Run(outWriter)
+            Assert.Equal(0, exitCode)
+            Dim dirPath As String = dir.ToString()
+            Dim output = ProcessUtilities.RunAndGetOutput(Path.Combine(dirPath, "verifier.exe"), startFolder:=dirPath, expectedRetCode:=0)
+            Assert.Equal("Got expected exception", output)
         End Sub
 
         <Fact>
