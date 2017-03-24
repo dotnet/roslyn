966b8f8945f5f4ae3ce9c3cc94d54dbd1f8f1fc1
Ty Overby <ty@pre-alpha.com>
2017-03-24 12:56:45 -0700

    attempt to build a runner

---
+++ src/Compilers/XunitPerfRunner/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Microsoft.Xunit.Performance.Api;
+
+namespace Runner
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (XunitPerformanceHarness p = new XunitPerformanceHarness(args))
+            {
+                string entryAssemblyPath = Assembly.GetEntryAssembly().Location;
+                p.RunBenchmarks(entryAssemblyPath);
+            }
+        }
+    }
+}
