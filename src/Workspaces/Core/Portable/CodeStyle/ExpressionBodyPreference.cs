eaa9031f3bec723a6a4510f0cf14f7193b22dfde
CyrusNajmabadi <cyrusn@microsoft.com>
2017-03-26 13:15:07 -0700

    Changed doc comment.

--- src/Workspaces/Core/Portable/CodeStyle/ExpressionBodyPreference.cs
+++ src/Workspaces/Core/Portable/CodeStyle/ExpressionBodyPreference.cs
@@ -2,12 +2,12 @@
 
 namespace Microsoft.CodeAnalysis.CodeStyle
 {
-    /// <summary>
+    /// <remarks>
     /// Note: the order of this enum is important.  We originally only supported two values,
     /// and we encoded this as a bool with 'true = WhenPossible' and 'false = never'.  To
     /// preserve compatibility we map the false value to 0 and the true value to 1.  All new
     /// values go after these. 
-    /// </summary>
+    /// </remarks>
     internal enum ExpressionBodyPreference
     {
         // Value can not be changed. 'false' was the "never" value back when we used CodeStyleOption<bool>
