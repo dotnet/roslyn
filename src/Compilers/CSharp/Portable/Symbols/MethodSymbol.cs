c113421c9cc94626293056eb99888b8b1d53afb1
Jared Parsons <jaredpar@microsoft.com>
2017-03-29 17:32:59 -0700

    Disallow ref returning Main method
    The Main method cannot have a ref return.
    
    closes #17923

--- src/Compilers/CSharp/Portable/Symbols/MethodSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/MethodSymbol.cs
@@ -646,6 +646,11 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 return false;
             }
 
+            if (RefKind != RefKind.None)
+            {
+                return false;
+            }
+
             if (Parameters.Length == 0)
             {
                 return true;
