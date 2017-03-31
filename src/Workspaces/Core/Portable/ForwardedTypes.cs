e6faebe7b25483272ff7e4d3b25e1ba154cb84ab
Sam Harwell <sam.harwell@microsoft.com>
2017-03-31 07:39:07 -0500

    Move PerLanguageOption<T> to the compiler layer

---
+++ src/Workspaces/Core/Portable/ForwardedTypes.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Runtime.CompilerServices;
+using Microsoft.CodeAnalysis.Options;
+
+[assembly: TypeForwardedTo(typeof(IOption))]
+[assembly: TypeForwardedTo(typeof(OptionStorageLocation))]
+[assembly: TypeForwardedTo(typeof(PerLanguageOption<>))]
