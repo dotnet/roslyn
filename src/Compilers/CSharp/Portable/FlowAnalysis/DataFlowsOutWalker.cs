4e9c84777cae4e8c71c3c1816457efed2f7f3189
Sam Harwell <sam.harwell@microsoft.com>
2017-04-01 05:56:52 -0500

    Consolidate fields at top of source file

--- src/Compilers/CSharp/Portable/FlowAnalysis/DataFlowsOutWalker.cs
+++ src/Compilers/CSharp/Portable/FlowAnalysis/DataFlowsOutWalker.cs
@@ -20,6 +20,13 @@ namespace Microsoft.CodeAnalysis.CSharp
     {
         private readonly ImmutableArray<ISymbol> _dataFlowsIn;
 
+        private readonly HashSet<Symbol> _dataFlowsOut = new HashSet<Symbol>();
+
+#if DEBUG
+        // we'd like to ensure that only variables get returned in DataFlowsOut that were assigned to inside the region.
+        private readonly HashSet<Symbol> _assignedInside = new HashSet<Symbol>();
+#endif
+
         private DataFlowsOutWalker(CSharpCompilation compilation, Symbol member, BoundNode node, BoundNode firstInRegion, BoundNode lastInRegion, HashSet<Symbol> unassignedVariables, ImmutableArray<ISymbol> dataFlowsIn)
             : base(compilation, member, node, firstInRegion, lastInRegion, unassignedVariables, trackUnassignments: true)
         {
@@ -44,13 +51,6 @@ namespace Microsoft.CodeAnalysis.CSharp
                 walker.Free();
             }
         }
-
-        private readonly HashSet<Symbol> _dataFlowsOut = new HashSet<Symbol>();
-
-#if DEBUG
-        // we'd like to ensure that only variables get returned in DataFlowsOut that were assigned to inside the region.
-        private readonly HashSet<Symbol> _assignedInside = new HashSet<Symbol>();
-#endif
 
         private new HashSet<Symbol> Analyze(ref bool badRegion)
         {
