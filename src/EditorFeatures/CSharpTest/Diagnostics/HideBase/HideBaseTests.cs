11c052fb66e7f3e3ee2b3902559c0cc0693efdcd
Maxime Rouiller <angelzerosoft@gmail.com>
2017-04-03 14:50:07 -0400

    Fixed WorkItemAttribute

--- src/EditorFeatures/CSharpTest/Diagnostics/HideBase/HideBaseTests.cs
+++ src/EditorFeatures/CSharpTest/Diagnostics/HideBase/HideBaseTests.cs
@@ -94,7 +94,7 @@ class App : Application
 }");
         }
 
-        [WorkItem(18391, "Issue where 'new' keyword would be in the wrong order on 'const' fields.")]
+        [WorkItem(18391, "https://github.com/dotnet/roslyn/issues/18391")]
         [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddNew)]
         public async Task TestAddNewToConstant()
         {
