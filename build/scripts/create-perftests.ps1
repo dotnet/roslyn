116175910fe5afe2b05fc825d7fa314cb0a30556
Jared Parsons <jaredpar@microsoft.com>
2017-04-06 14:54:11 -0700

    Remove unneeded scripts

--- build/scripts/create-perftests.ps1
+++ build/scripts/create-perftests.ps1
@@ -14,7 +14,7 @@ try {
         Create-Directory $target
     }
 
-    Push-Location $bindir
+    Push-Location $binariesDir
     foreach ($subDir in @("Dlls", "UnitTests")) {
         Push-Location $subDir
         foreach ($path in Get-ChildItem -re -in "PerfTests") {
