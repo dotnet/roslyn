3e6a87046fc015b8f51cfb965f8976fef1bb18a9
CyrusNajmabadi <cyrusn@microsoft.com>
2017-04-15 15:28:06 -0700

    Revert change.

--- src/EditorFeatures/Next/NavigateTo/Dev15NavigateToHostVersionService.cs
+++ src/EditorFeatures/Next/NavigateTo/Dev15NavigateToHostVersionService.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.ComponentModel.Composition;
 using Microsoft.CodeAnalysis.Editor.Implementation.NavigateTo;
 using Microsoft.VisualStudio.Language.NavigateTo.Interfaces;
 
@@ -9,11 +8,6 @@ namespace Microsoft.CodeAnalysis.Editor.NavigateTo
     [ExportVersionSpecific(typeof(INavigateToHostVersionService), VisualStudioVersion.Dev15)]
     internal partial class Dev15NavigateToHostVersionService : INavigateToHostVersionService
     {
-        [ImportingConstructor]
-        public Dev15NavigateToHostVersionService()
-        {
-        }
-
         public bool GetSearchCurrentDocument(INavigateToOptions options)
         {
             var options2 = options as INavigateToOptions2;
