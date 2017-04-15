653f56dd9c2aafb46090a19852a733d62ef15af9
jcouv <jcouv@JULIENCOUVRDEE1>
2017-04-15 00:18:39 -0700

    Missing files

---
+++ src/Workspaces/CSharp/Portable/Simplification/CSharpInferredMemberNameReducer.Rewriter.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Options;
+
+namespace Microsoft.CodeAnalysis.CSharp.Simplification
+{
+    internal partial class CSharpInferredMemberNameReducer
+    {
+        private class Rewriter : AbstractExpressionRewriter
+        {
+            public Rewriter(OptionSet optionSet, CancellationToken cancellationToken)
+                : base(optionSet, cancellationToken)
+            {
+            }
+
+            public override SyntaxNode VisitArgument(ArgumentSyntax node)
+            {
+                return SimplifyExpression(
+                    node,
+                    newNode: base.VisitArgument(node),
+                    simplifier: SimplifyTupleName);
+            }
+
+            public override SyntaxNode VisitAnonymousObjectMemberDeclarator(AnonymousObjectMemberDeclaratorSyntax node)
+            {
+                return SimplifyExpression(
+                    node,
+                    newNode: base.VisitAnonymousObjectMemberDeclarator(node),
+                    simplifier: SimplifyAnonymousTypeMemberName);
+            }
+        }
+    }
+}
---
+++ src/Workspaces/CSharp/Portable/Simplification/CSharpInferredMemberNameReducer.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Extensions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Options;
+
+namespace Microsoft.CodeAnalysis.CSharp.Simplification
+{
+    internal partial class CSharpInferredMemberNameReducer : AbstractCSharpReducer
+    {
+        public override IExpressionRewriter CreateExpressionRewriter(OptionSet optionSet, CancellationToken cancellationToken)
+        {
+            return new Rewriter(optionSet, cancellationToken);
+        }
+
+        private static ArgumentSyntax SimplifyTupleName(ArgumentSyntax node, SemanticModel semanticModel, OptionSet optionSet, CancellationToken cancellationToken)
+        {
+            string inferredName = node.IsParentKind(SyntaxKind.TupleExpression) ?
+                CSharpSimplificationService.ExtractAnonymousTypeMemberName(node.Expression).ValueText :
+                null;
+
+            if (inferredName == null || inferredName != node?.NameColon?.Name.Identifier.ValueText)
+            {
+                return node;
+            }
+
+            return node.WithNameColon(null);
+        }
+
+
+        private static SyntaxNode SimplifyAnonymousTypeMemberName(AnonymousObjectMemberDeclaratorSyntax node, SemanticModel semanticModel, OptionSet optionSet, CancellationToken canellationToken)
+        {
+            string inferredName = CSharpSimplificationService.ExtractAnonymousTypeMemberName(node.Expression).ValueText;
+
+            if (inferredName == null || inferredName != node?.NameEquals?.Name.Identifier.ValueText)
+            {
+                return node;
+            }
+
+            return node.WithNameEquals(null);
+        }
+    }
+}
