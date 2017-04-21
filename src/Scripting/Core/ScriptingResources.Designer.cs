1971df81f2d6b71f793bf03ec42699489f4eb6e5
BattleRush <thecombobreaker95@gmail.com>
2017-04-21 21:57:45 +0200

    Added localization for InteractiveAssemblyLoader
    Resource file schema update from 1.3 to 2.0

--- src/Scripting/Core/ScriptingResources.Designer.cs
+++ src/Scripting/Core/ScriptingResources.Designer.cs
@@ -71,6 +71,24 @@ namespace Microsoft.CodeAnalysis.Scripting {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Assembly &apos;{0}, Version={1}&apos; has already been loaded from &apos;{2}&apos;. A different assembly with the same name and version can&apos;t be loaded: &apos;{3}&apos;..
+        /// </summary>
+        internal static string AsemblyAlreadyLoaded {
+            get {
+                return ResourceManager.GetString("AsemblyAlreadyLoaded", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Assembly &apos;{0}&apos; has already been loaded from &apos;{1}&apos;. A different assembly with the same name can&apos;t be loaded unless it&apos;s signed: &apos;{2}&apos;..
+        /// </summary>
+        internal static string AsemblyAlreadyLoadedNotSigned {
+            get {
+                return ResourceManager.GetString("AsemblyAlreadyLoadedNotSigned", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to  at {0} : {1}.
         /// </summary>
         internal static string AtFileLine {
