3ca627245e710f41b7077071f5df163c54ed41aa
vsadov <vsadov@microsoft.com>
2017-04-21 06:32:56 -0700

    Disable more tests (for now with wrong bug IDs)

--- src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpEncapsulateField.cs
+++ src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpEncapsulateField.cs
@@ -30,7 +30,7 @@ namespace myNamespace
     }
 }";
 
-        [Fact, Trait(Traits.Feature, Traits.Features.EncapsulateField)]
+        [Fact(Skip = "https://github.com/dotnet/roslyn/issues/18870"), Trait(Traits.Feature, Traits.Features.EncapsulateField)]
         public void EncapsulateThroughCommand()
         {
             SetUpEditor(TestSource);
