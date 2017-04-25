50feea5e98424c9c69abb43a6ad186e75fb63a65
gafter <neal@gafter.com>
2017-04-25 14:27:59 -0700

    Additional test for #18811

--- src/Compilers/CSharp/Test/Emit/CodeGen/PatternTests.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/PatternTests.cs
@@ -14,7 +14,7 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.CodeGen
     public class PatternTests : EmitMetadataTestBase
     {
         [Fact, WorkItem(18811, "https://github.com/dotnet/roslyn/issues/18811")]
-        public void MissingNullable()
+        public void MissingNullable_01()
         {
             var source = @"namespace System {
     public class Object { }
@@ -41,6 +41,35 @@ static class C {
                 // (9,48): error CS0656: Missing compiler required member 'System.Nullable`1.get_HasValue'
                 //     public static bool M() => ((object)123) is int i;
                 Diagnostic(ErrorCode.ERR_MissingPredefinedMember, "int i").WithArguments("System.Nullable`1", "get_HasValue").WithLocation(9, 48)
+                );
+        }
+
+        [Fact, WorkItem(18811, "https://github.com/dotnet/roslyn/issues/18811")]
+        public void MissingNullable_02()
+        {
+            var source = @"namespace System {
+    public class Object { }
+    public abstract class ValueType { }
+    public struct Void { }
+    public struct Boolean { }
+    public struct Int32 { }
+    public struct Nullable<T> where T : struct { }
+}
+static class C {
+    public static bool M() => ((object)123) is int i;
+}
+";
+            var compilation = CreateCompilation(source, options: TestOptions.ReleaseDll);
+            compilation.GetDiagnostics().Verify();
+            compilation.GetEmitDiagnostics().Verify(
+                // warning CS8021: No value for RuntimeMetadataVersion found. No assembly containing System.Object was found nor was a value for RuntimeMetadataVersion specified through options.
+                Diagnostic(ErrorCode.WRN_NoRuntimeMetadataVersion),
+                // (10,48): error CS0656: Missing compiler required member 'System.Nullable`1.GetValueOrDefault'
+                //     public static bool M() => ((object)123) is int i;
+                Diagnostic(ErrorCode.ERR_MissingPredefinedMember, "int i").WithArguments("System.Nullable`1", "GetValueOrDefault").WithLocation(10, 48),
+                // (10,48): error CS0656: Missing compiler required member 'System.Nullable`1.get_HasValue'
+                //     public static bool M() => ((object)123) is int i;
+                Diagnostic(ErrorCode.ERR_MissingPredefinedMember, "int i").WithArguments("System.Nullable`1", "get_HasValue").WithLocation(10, 48)
                 );
         }
     }
