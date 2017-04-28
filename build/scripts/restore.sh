39e4ca7a7db032288fbcc15cd750415d173ac30d
Andy Gocke <angocke@microsoft.com>
2017-04-28 16:23:06 -0700

    Re-add HOME workarounds as this doesn't seem to be fixed on Mac

--- build/scripts/restore.sh
+++ build/scripts/restore.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Workaround, see https://github.com/dotnet/roslyn/issues/10210
+export HOME=$(cd ~ && pwd)
+
 echo "Restoring toolset packages"
 
 dotnet restore -v Minimal --disable-parallel $(pwd)/build/ToolsetPackages/BaseToolset.csproj
