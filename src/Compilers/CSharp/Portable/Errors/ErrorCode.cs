6585a9b6a24bebe80d32f60c92586e6f08a6f553
BattleRush <thecombobreaker95@gmail.com>
2017-04-28 21:47:32 +0200

    Changed ERR_LocalFunctionMissingBody error code
    Changed ERR_LocalFunctionMissingBody error code from 8111 to 8112

--- src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
@@ -1344,7 +1344,7 @@ namespace Microsoft.CodeAnalysis.CSharp
         #region diagnostics for local functions introduced in C# 7
         ERR_DynamicLocalFunctionParamsParameter = 8108,
         ERR_ExpressionTreeContainsLocalFunction = 8110,
-        ERR_LocalFunctionMissingBody = 8111,
+        ERR_LocalFunctionMissingBody = 8112,
         #endregion diagnostics for local functions introduced in C# 7
 
         #region diagnostics for instrumentation
@@ -1353,7 +1353,7 @@ namespace Microsoft.CodeAnalysis.CSharp
 
         #endregion
 
-        // Available = 8112, 8113, 8114, 8115
+        // Available = 8113, 8114, 8115
 
         #region diagnostics for pattern-matching introduced in C# 7
         ERR_ThrowMisplaced = 8115,
