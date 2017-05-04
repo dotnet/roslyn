217e47774cefd944779aa0357342d56bf731499f
Manish Vasani <mavasani@microsoft.com>
2017-05-03 17:19:02 -0700

    Verify IOperation API version in analyzer packages
    We now do couple of things:
    1. All the RegisterOperationXXXAction invocations in our analyzer packages verify that the loaded version of Microsoft.CodeAnalysis is in the range against which the analyzer package was built. If not, then they silently bail out, preventing crashes downstream.
    2. Each of the analyzer packages get a new VersionCheckAnalyzer that detects this version mismatch and reports a version mismatch warning between analyzer package and Microsoft.CodeAnalysis package.

---
+++ src/Roslyn.Diagnostics.Analyzers/Core/VersionCheckAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Roslyn.Diagnostics.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class VersionCheckAnalyzer : AbstractVersionCheckAnalyzer
+    {
+        protected override string AnalyzerPackageName => "Roslyn.Diagnostics.Analyzers";
+    }
+}
