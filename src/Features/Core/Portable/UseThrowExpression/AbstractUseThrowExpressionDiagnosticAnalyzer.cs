9b9f11b24274a8a5ba472a100d93dbd7ff3b76b9
CyrusNajmabadi <cyrusn@microsoft.com>
2017-05-09 16:38:18 -0700

    Use-throw-expression should not be offered when the 'if-block' has additional statements in it.

--- src/Features/Core/Portable/UseThrowExpression/AbstractUseThrowExpressionDiagnosticAnalyzer.cs
+++ src/Features/Core/Portable/UseThrowExpression/AbstractUseThrowExpressionDiagnosticAnalyzer.cs
@@ -116,6 +116,12 @@ namespace Microsoft.CodeAnalysis.UseThrowExpression
                 return;
             }
 
+            if (!IsOnlyStatementOfIf(ifOperation, throwOperation))
+            {
+                // The if-statement can only have a single throw-statement in it.
+                return;
+            }
+
             var containingBlock = GetOperation(
                 semanticModel, ifOperation.Syntax.Parent, cancellationToken) as IBlockStatement;
             if (containingBlock == null)
@@ -187,6 +193,18 @@ namespace Microsoft.CodeAnalysis.UseThrowExpression
                             ifOperation.Syntax.Span.End)),
                         additionalLocations: allLocations));
             }
+        }
+
+        private bool IsOnlyStatementOfIf(IIfStatement ifOperation, IThrowStatement throwStatement)
+        {
+            if (ifOperation.IfTrueStatement.Syntax == throwStatement.Syntax)
+            {
+                return true;
+            }
+
+            return ifOperation.IfTrueStatement is IBlockStatement block &&
+                   block.Statements.Length == 1 &&
+                   block.Statements[0].Syntax == throwStatement.Syntax;
         }
 
         protected abstract ISyntaxFactsService GetSyntaxFactsService();
