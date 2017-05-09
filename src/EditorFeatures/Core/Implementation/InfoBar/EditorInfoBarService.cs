e9053e6b088fe4327ac68006e2763efcef39e70a
Fredric Silberberg <frsilb@microsoft.com>
2017-05-09 13:09:18 -0700

    Moved EditorInfoBarService to its own folder, export as workspace service

--- src/EditorFeatures/Core/Implementation/InfoBar/EditorInfoBarService.cs
+++ src/EditorFeatures/Core/Implementation/InfoBar/EditorInfoBarService.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
 using Microsoft.CodeAnalysis.Extensions;
+using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.Internal.Log;
 
 namespace Microsoft.CodeAnalysis.Editor.Implementation.Workspaces
 {
+    [ExportWorkspaceService(typeof(IInfoBarService))]
     internal class EditorInfoBarService : IInfoBarService
     {
         public void ShowInfoBarInActiveView(string message, params InfoBarUI[] items)
