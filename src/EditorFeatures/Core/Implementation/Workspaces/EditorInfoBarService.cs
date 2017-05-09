435ee7dc08cbefb1c1b450ec0ba0901cd95b26a8
Fredric Silberberg <frsilb@microsoft.com>
2017-05-08 18:05:28 -0700

    Refactored IErrorReportingService to extract the info bar display logic into IInfoBarService

---
+++ src/EditorFeatures/Core/Implementation/Workspaces/EditorInfoBarService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Extensions;
+using Microsoft.CodeAnalysis.Internal.Log;
+
+namespace Microsoft.CodeAnalysis.Editor.Implementation.Workspaces
+{
+    internal class EditorInfoBarService : IInfoBarService
+    {
+        public void ShowInfoBarInActiveView(string message, params InfoBarUI[] items)
+        {
+            ShowInfoBarInGlobalView(message, items);
+        }
+
+        public void ShowInfoBarInGlobalView(string message, params InfoBarUI[] items)
+        {
+            Logger.Log(FunctionId.Extension_InfoBar, message);
+        }
+    }
+}
--- src/VisualStudio/Core/Def/Implementation/Workspace/VisualStudioErrorReportingServiceFactory.cs
+++ src/VisualStudio/Core/Def/Implementation/Workspace/VisualStudioErrorReportingServiceFactory.cs
@@ -1,15 +1,9 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Composition;
-using Microsoft.CodeAnalysis.Editor;
 using Microsoft.CodeAnalysis.Extensions;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.Shared.TestHooks;
-using Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem;
-using Microsoft.VisualStudio.Shell;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation
 {
@@ -19,12 +13,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation
         private readonly IErrorReportingService _singleton;
 
         [ImportingConstructor]
-        public VisualStudioErrorReportingServiceFactory(
-            SVsServiceProvider serviceProvider,
-            IForegroundNotificationService foregroundNotificationService,
-            [ImportMany] IEnumerable<Lazy<IAsynchronousOperationListener, FeatureMetadata>> asyncListeners)
+        public VisualStudioErrorReportingServiceFactory(IInfoBarService infoBarService)
         {
-            _singleton = new VisualStudioErrorReportingService(serviceProvider, foregroundNotificationService, new AggregateAsynchronousOperationListener(asyncListeners, FeatureAttribute.InfoBar));
+            _singleton = new VisualStudioErrorReportingService(infoBarService);
         }
 
         public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
