dc1ac8ad8990151f3c6551654de5b2be66de0bd7
gafter <neal@gafter.com>
2017-05-15 16:03:38 -0700

    Add test confirming that #19122 is fixed. Fixes #19122

--- src/Compilers/CSharp/Test/Emit/CodeGen/PatternTests.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/PatternTests.cs
@@ -109,5 +109,88 @@ public class C
   IL_0023:  ret
 }");
         }
+
+        [Fact, WorkItem(19122, "https://github.com/dotnet/roslyn/issues/19122")]
+        public void PatternCrash_01()
+        {
+            var source = @"using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Class2 : IDisposable
+{
+    public Class2(bool parameter = false)
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+}
+
+class X<T>
+{
+    IdentityAccessor<T> idAccessor = new IdentityAccessor<T>();
+    void Y<U>() where U : T
+    {
+        // BUG: The following line is the problem
+        if (GetT().FirstOrDefault(p => idAccessor.GetId(p) == Guid.Empty) is U u)
+        {
+        }
+    }
+
+    IEnumerable<T> GetT()
+    {
+        yield return default(T);
+    }
+}
+class IdentityAccessor<T>
+{
+    public Guid GetId(T t)
+    {
+        return Guid.Empty;
+    }
+}";
+            var compilation = CreateStandardCompilation(source, options: TestOptions.DebugDll, references: new[] { LinqAssemblyRef });
+            compilation.VerifyDiagnostics();
+            var compVerifier = CompileAndVerify(compilation);
+            compVerifier.VerifyIL("X<T>.Y<U>",
+@"{
+  // Code size       67 (0x43)
+  .maxstack  3
+  .locals init (U V_0, //u
+                bool V_1,
+                object V_2,
+                U V_3)
+  IL_0000:  nop
+  IL_0001:  ldarg.0
+  IL_0002:  call       ""System.Collections.Generic.IEnumerable<T> X<T>.GetT()""
+  IL_0007:  ldarg.0
+  IL_0008:  ldftn      ""bool X<T>.<Y>b__1_0<U>(T)""
+  IL_000e:  newobj     ""System.Func<T, bool>..ctor(object, System.IntPtr)""
+  IL_0013:  call       ""T System.Linq.Enumerable.FirstOrDefault<T>(System.Collections.Generic.IEnumerable<T>, System.Func<T, bool>)""
+  IL_0018:  box        ""T""
+  IL_001d:  stloc.2
+  IL_001e:  ldloc.2
+  IL_001f:  isinst     ""U""
+  IL_0024:  ldnull
+  IL_0025:  cgt.un
+  IL_0027:  dup
+  IL_0028:  brtrue.s   IL_0035
+  IL_002a:  ldloca.s   V_3
+  IL_002c:  initobj    ""U""
+  IL_0032:  ldloc.3
+  IL_0033:  br.s       IL_003b
+  IL_0035:  ldloc.2
+  IL_0036:  unbox.any  ""U""
+  IL_003b:  stloc.0
+  IL_003c:  stloc.1
+  IL_003d:  ldloc.1
+  IL_003e:  brfalse.s  IL_0042
+  IL_0040:  nop
+  IL_0041:  nop
+  IL_0042:  ret
+}");
+        }
     }
 }
