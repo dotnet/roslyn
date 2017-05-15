50a08ac23248831b3e6fa90c3b04977b069f9745
vsadov <vsadov@microsoft.com>
2017-05-15 16:51:26 -0700

    Fix tests after merge

--- src/Compilers/CSharp/Test/Semantic/Semantics/TargetTypedDefaultTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/TargetTypedDefaultTests.cs
@@ -23,7 +23,7 @@ class C
     }
 }
 ";
-            var comp = CreateStandardCompilation(source);
+            var comp = CreateStandardCompilation(source, parseOptions: TestOptions.Regular7);
             comp.VerifyDiagnostics(
                 // (6,17): error CS8107: Feature 'default literal' is not available in C# 7. Please use language version 7.1 or greater.
                 //         int x = default;
@@ -297,7 +297,10 @@ class C
             comp.VerifyDiagnostics(
                 // (6,9): error CS8150: By-value returns may only be used in methods that return by value
                 //         return default;
-                Diagnostic(ErrorCode.ERR_MustHaveRefReturn, "return").WithLocation(6, 9)
+                Diagnostic(ErrorCode.ERR_MustHaveRefReturn, "return").WithLocation(6, 9),
+                // (6,16): error CS8151: The return expression must be of type 'int' because this method returns by reference
+                //         return default;
+                Diagnostic(ErrorCode.ERR_RefReturnMustHaveIdentityConversion, "default").WithArguments("int").WithLocation(6, 16)
                 );
         }
 
