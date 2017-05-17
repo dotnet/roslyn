bd5373c3c80e32b0ea834a8b6c6e43b7fc8bfb77
CyrusNajmabadi <cyrusn@microsoft.com>
2017-05-17 15:03:34 -0700

    Move to new test harness.

--- src/Compilers/CSharp/Test/Syntax/Parsing/DeconstructionTests.cs
+++ src/Compilers/CSharp/Test/Syntax/Parsing/DeconstructionTests.cs
@@ -2161,7 +2161,7 @@ class C
         var(x, y) += e;            // error 1
     }
 }";
-            CreateCompilationWithMscorlib(source).VerifyDiagnostics(
+            CreateStandardCompilation(source).VerifyDiagnostics(
                 // (6,9): error CS8199: The syntax 'var (...)' as an lvalue is reserved.
                 //         var(x, y) += e;            // error 1
                 Diagnostic(ErrorCode.ERR_VarInvocationLvalueReserved, "var(x, y)").WithLocation(6, 9),
@@ -2188,7 +2188,7 @@ class C
         var(x, y)++;               // error 2
     }
 }";
-            CreateCompilationWithMscorlib(source).VerifyDiagnostics(
+            CreateStandardCompilation(source).VerifyDiagnostics(
                 // (6,9): error CS8199: The syntax 'var (...)' as an lvalue is reserved.
                 //         var(x, y)++;               // error 2
                 Diagnostic(ErrorCode.ERR_VarInvocationLvalueReserved, "var(x, y)").WithLocation(6, 9),
@@ -2215,7 +2215,7 @@ class C
         ++var(x, y);               // error 3
     }
 }";
-            CreateCompilationWithMscorlib(source).VerifyDiagnostics(
+            CreateStandardCompilation(source).VerifyDiagnostics(
                 // (6,11): error CS8199: The syntax 'var (...)' as an lvalue is reserved.
                 //         ++var(x, y);               // error 3
                 Diagnostic(ErrorCode.ERR_VarInvocationLvalueReserved, "var(x, y)").WithLocation(6, 11),
@@ -2244,7 +2244,7 @@ class C
 
     void X(out object x) { x = null; }
 }";
-            CreateCompilationWithMscorlib(source).VerifyDiagnostics(
+            CreateStandardCompilation(source).VerifyDiagnostics(
                 // (6,15): error CS0103: The name 'var' does not exist in the current context
                 //         X(out var(x, y));          // error 4
                 Diagnostic(ErrorCode.ERR_NameNotInContext, "var").WithArguments("var").WithLocation(6, 15),
@@ -2273,7 +2273,7 @@ class C
 
     void X(ref object x) { x = null; }
 }";
-            CreateCompilationWithMscorlib(source).VerifyDiagnostics(
+            CreateStandardCompilation(source).VerifyDiagnostics(
                 // (6,15): error CS0103: The name 'var' does not exist in the current context
                 //         X(ref var(x, y));          // error 5
                 Diagnostic(ErrorCode.ERR_NameNotInContext, "var").WithArguments("var").WithLocation(6, 15),
@@ -2300,7 +2300,7 @@ class C
         return ref var(x, y);      // error 6
     }
 }";
-            CreateCompilationWithMscorlib(source).VerifyDiagnostics(
+            CreateStandardCompilation(source).VerifyDiagnostics(
                 // (6,20): error CS8199: The syntax 'var (...)' as an lvalue is reserved.
                 //         return ref var(x, y);      // error 6
                 Diagnostic(ErrorCode.ERR_VarInvocationLvalueReserved, "var(x, y)").WithLocation(6, 20),
@@ -2327,7 +2327,7 @@ class C
         ref int x = ref var(x, y); // error 7
     }
 }";
-            CreateCompilationWithMscorlib(source).VerifyDiagnostics(
+            CreateStandardCompilation(source).VerifyDiagnostics(
                 // (6,25): error CS8199: The syntax 'var (...)' as an lvalue is reserved.
                 //         ref int x = ref var(x, y); // error 7
                 Diagnostic(ErrorCode.ERR_VarInvocationLvalueReserved, "var(x, y)").WithLocation(6, 25),
@@ -2354,7 +2354,7 @@ class C
         var (x, 1) = e;            // error 8
     }
 }";
-            CreateCompilationWithMscorlib(source).VerifyDiagnostics(
+            CreateStandardCompilation(source).VerifyDiagnostics(
                 // (4,10): error CS1519: Invalid token 'object' in class, struct, or interface member declaration
                 //     void object M(string e)
                 Diagnostic(ErrorCode.ERR_InvalidMemberDecl, "object").WithArguments("object").WithLocation(4, 10),
