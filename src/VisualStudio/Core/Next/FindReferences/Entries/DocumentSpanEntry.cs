a97af6e1f0e7cd9b99f37706eabe3a49ea98775b
CyrusNajmabadi <cyrusn@microsoft.com>
2017-05-17 02:43:45 -0700

    Fix build break.

--- src/VisualStudio/Core/Next/FindReferences/Entries/DocumentSpanEntry.cs
+++ src/VisualStudio/Core/Next/FindReferences/Entries/DocumentSpanEntry.cs
@@ -21,6 +21,7 @@ using Microsoft.VisualStudio.LanguageServices.Implementation.Extensions;
 using Microsoft.VisualStudio.PlatformUI;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
+using DocumentHighlighting = Microsoft.CodeAnalysis.DocumentHighlighting;
 
 namespace Microsoft.VisualStudio.LanguageServices.FindUsages
 {
@@ -33,14 +34,14 @@ namespace Microsoft.VisualStudio.LanguageServices.FindUsages
         /// </summary>
         private class DocumentSpanEntry : AbstractDocumentSpanEntry
         {
-            private readonly HighlightSpanKind _spanKind;
+            private readonly DocumentHighlighting.HighlightSpanKind _spanKind;
             private readonly ClassifiedSpansAndHighlightSpan _classifiedSpansAndHighlights;
 
             public DocumentSpanEntry(
                 AbstractTableDataSourceFindUsagesContext context,
                 RoslynDefinitionBucket definitionBucket,
                 DocumentSpan documentSpan,
-                HighlightSpanKind spanKind,
+                DocumentHighlighting.HighlightSpanKind spanKind,
                 string documentName,
                 Guid projectGuid,
                 SourceText sourceText,
@@ -53,9 +54,9 @@ namespace Microsoft.VisualStudio.LanguageServices.FindUsages
 
             protected override IList<System.Windows.Documents.Inline> CreateLineTextInlines()
             {
-                var propertyId = _spanKind == HighlightSpanKind.Definition
+                var propertyId = _spanKind == DocumentHighlighting.HighlightSpanKind.Definition
                     ? DefinitionHighlightTag.TagId
-                    : _spanKind == HighlightSpanKind.WrittenReference
+                    : _spanKind == DocumentHighlighting.HighlightSpanKind.WrittenReference
                         ? WrittenReferenceHighlightTag.TagId
                         : ReferenceHighlightTag.TagId;
 
@@ -163,9 +164,9 @@ namespace Microsoft.VisualStudio.LanguageServices.FindUsages
                     _sourceText.ToString(), contentType);
 
                 // Create an appropriate highlight span on that buffer for the reference.
-                var key = _spanKind == HighlightSpanKind.Definition
+                var key = _spanKind == DocumentHighlighting.HighlightSpanKind.Definition
                     ? PredefinedPreviewTaggerKeys.DefinitionHighlightingSpansKey
-                    : _spanKind == HighlightSpanKind.WrittenReference
+                    : _spanKind == DocumentHighlighting.HighlightSpanKind.WrittenReference
                         ? PredefinedPreviewTaggerKeys.WrittenReferenceHighlightingSpansKey
                         : PredefinedPreviewTaggerKeys.ReferenceHighlightingSpansKey;
                 textBuffer.Properties.RemoveProperty(key);
