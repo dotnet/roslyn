750842936f09983823aac013f8e0899821526c29
CyrusNajmabadi <cyrusn@microsoft.com>
2017-05-22 15:18:47 -0700

    Remove unused usings.

--- src/VisualStudio/Core/Test/LanguageBlockTests.vb
+++ src/VisualStudio/Core/Test/LanguageBlockTests.vb
@@ -1,10 +1,8 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 Imports System.Threading
-Imports System.Threading.Tasks
 Imports Microsoft.CodeAnalysis
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces
-Imports Microsoft.CodeAnalysis.Text
 Imports Microsoft.VisualStudio.LanguageServices.Implementation.LanguageService
 Imports Roslyn.Test.Utilities
 
