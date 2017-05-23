db9b378339d07a65ec5073ac1193940ec9754251
Ravi Chande <rchande@microsoft.com>
2017-05-23 14:40:20 -0700

    Use FTAO CurrentForegroundThread when constructing JTC

--- src/EditorFeatures/TestUtilities/TestExportJoinableTaskContext.cs
+++ src/EditorFeatures/TestUtilities/TestExportJoinableTaskContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.VisualStudio.Threading;
 
 namespace Microsoft.CodeAnalysis.Editor.UnitTests
@@ -14,9 +15,10 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests
     // contain an ExportAttribute. The Editor's own unit tests
     // export it from a field and we need to do the same in order
     // to be able to compose in any part of the Editor.
-    internal class TestExportJoinableTaskContext
+    internal class TestExportJoinableTaskContext : ForegroundThreadAffinitizedObject
     {
         [Export]
-        private JoinableTaskContext _joinableTaskContext = new JoinableTaskContext();
+        private JoinableTaskContext _joinableTaskContext = new JoinableTaskContext(
+            mainThread: ForegroundThreadAffinitizedObject.CurrentForegroundThreadData.Thread);
     }
 }
