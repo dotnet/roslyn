32a0147599433f3b2be8e1b6b05ae93cbf7147dc
Artur Spychaj <drognanar@gmail.com>
2017-05-24 14:26:53 -0700

    Extract AbstractTypescriptIntegrationTest class

---
+++ src/VisualStudio/IntegrationTest/IntegrationTests/Typescript/AbstractTypescriptIntegrationTest.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Shared.TestHooks;
+using Microsoft.VisualStudio.IntegrationTest.Utilities;
+using ProjectUtils = Microsoft.VisualStudio.IntegrationTest.Utilities.Common.ProjectUtils;
+
+namespace Roslyn.VisualStudio.IntegrationTests.Typescript
+{
+    public abstract class AbstractTypescriptIntegrationTest : AbstractEditorTest
+    {
+        public AbstractTypescriptIntegrationTest(VisualStudioInstanceFactory instanceFactory, string solutionName)
+            : base(instanceFactory, solutionName)
+        {
+            var project = new ProjectUtils.Project(ProjectName);
+            VisualStudio.SolutionExplorer.AddFile(project, "Lib.ts", contents:
+@"export function foo(): string {
+    return ""Hello World""
+}");
+            VisualStudio.SolutionExplorer.AddFile(project, "File1.ts");
+            VisualStudio.SolutionExplorer.OpenFile(project, "File1.ts");
+
+            // Wait for the workspace to finish loading the typescript project.
+            // Otherwise the file is part of miscellaneous files and Roslyn features will not work.
+            VisualStudio.Workspace.WaitForAsyncOperations(FeatureAttribute.Workspace);
+        }
+
+        /// <remarks>
+        /// For <see cref="TypescriptCommonTest"/> a typescript file is created
+        /// inside of a CSharp project. Thus the project's language name is CSharp.
+        /// </remarks>
+        protected override string LanguageName => LanguageNames.CSharp;
+    }
+}
--- src/VisualStudio/IntegrationTest/IntegrationTests/Typescript/TypescriptCompletionTest.cs
+++ src/VisualStudio/IntegrationTest/IntegrationTests/Typescript/TypescriptCompletionTest.cs
@@ -1,38 +1,18 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Shared.TestHooks;
 using Microsoft.VisualStudio.IntegrationTest.Utilities;
 using Microsoft.VisualStudio.IntegrationTest.Utilities.Input;
-using ProjectUtils = Microsoft.VisualStudio.IntegrationTest.Utilities.Common.ProjectUtils;
 using Xunit;
 
 namespace Roslyn.VisualStudio.IntegrationTests.Typescript
 {
     [Collection(nameof(SharedIntegrationHostFixture))]
-    public class TypescriptCompletionTest : AbstractEditorTest
+    public class TypescriptCompletionTest : AbstractTypescriptIntegrationTest
     {
         public TypescriptCompletionTest(VisualStudioInstanceFactory instanceFactory)
             : base(instanceFactory, nameof(TypescriptCompletionTest))
         {
-            var project = new ProjectUtils.Project(ProjectName);
-            VisualStudio.SolutionExplorer.AddFile(project, "Lib.ts", contents:
-@"export function foo(): string {
-    return ""Hello World""
-}");
-            VisualStudio.SolutionExplorer.AddFile(project, "File1.ts");
-            VisualStudio.SolutionExplorer.OpenFile(project, "File1.ts");
-
-            // Wait for current file to become part of the typescript project
-            // and not miscellaneous files. Otherwise intellisense will not work.
-            VisualStudio.Workspace.WaitForAsyncOperations(FeatureAttribute.Workspace);
         }
-
-        /// <remarks>
-        /// For <see cref="TypescriptCommonTest"/> a typescript file is created
-        /// inside of a CSharp project. Thus the project's language name is CSharp.
-        /// </remarks>
-        protected override string LanguageName => LanguageNames.CSharp;
 
         [Fact]
         public void CompleteNumberMethod()
