a7dece3aefa8ad2a036c21bc9824c29b30d1e6d4
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2017-05-26 18:18:43 +0200

    Initial attempt to create an InternalsVisibleToCompletionProvider

---
+++ src/EditorFeatures/CSharpTest/Completion/CompletionProviders/InternalsVisibleToCompletionProviderTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.CSharp.Completion.Providers;
+using Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionProviders
+{
+    public class InternalsVisibleToCompletionProviderTests : AbstractCSharpCompletionProviderTests
+    {
+        public InternalsVisibleToCompletionProviderTests(CSharpTestWorkspaceFixture workspaceFixture) : base(workspaceFixture)
+        {
+            // I needed to configure the workspace here, because CreateWorkspace was never called.
+            var ws = workspaceFixture.GetWorkspace();
+            var solution = ws.CurrentSolution;
+            solution = solution.AddProject(ProjectId.CreateNewId(), "ClassLibrary1", "ClassLibrary1.dll", LanguageNames.CSharp);
+            ws.ChangeSolution(solution);
+        }
+
+        protected override TestWorkspace CreateWorkspace(string fileContents)
+        {
+            // This would be the place to configure the workspace, but it is never called.
+            return base.CreateWorkspace(fileContents);
+        }
+
+        internal override CompletionProvider CreateCompletionProvider() => new InternalsVisibleToCompletionProvider();
+
+        [Fact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task NameCref()
+        {
+            var text = @"
+[assembly: InternalsVisibleTo(""$$"")]
+";
+            await VerifyItemExistsAsync(text, "ClassLibrary1.dll");
+        }
+    }
+}
---
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/InternalsVisibleToCompletionProvider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.CSharp.Extensions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
+{
+    internal sealed class InternalsVisibleToCompletionProvider : CommonCompletionProvider
+    {
+        internal override bool IsInsertionTrigger(SourceText text, int insertedCharacterPosition, OptionSet options)
+        {
+            var ch = text[insertedCharacterPosition];
+            if (ch == '\"')
+            {
+                return true;
+            }
+            return base.IsInsertionTrigger(text, insertedCharacterPosition, options);
+        }
+
+        public override async Task ProvideCompletionsAsync(CompletionContext context)
+        {
+            var cancellationToken = context.CancellationToken;
+            var syntaxTree = await context.Document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
+            var token = syntaxTree.FindTokenOnLeftOfPosition(context.Position, cancellationToken);
+            var attr = token.GetAncestor<AttributeSyntax>();
+            if (attr == null)
+            {
+                return;
+            }
+            if (attr.Name.TryGetNameParts(out IList<string> nameParts) && nameParts.Count > 0)
+            {
+                var lastName = nameParts[nameParts.Count - 1];
+                if (lastName == "InternalsVisibleTo" || lastName == "InternalsVisibleToAttribute")
+                {
+                    foreach (var p in context.Document.Project.Solution.Projects)
+                    {
+                        var item = CompletionItem.Create(displayText: p.AssemblyName);
+                        context.AddItem(item);
+                    }
+                }
+            }
+        }
+    }
+}
