a211a808716d8794c4296610df18540e901d33e1
Martin Strecker <Martin.Strecker@ifpconsulting.de>
2017-05-29 17:34:54 +0200

    Improved detection of AttributeSyntax. Added support for strong naming.

--- src/EditorFeatures/CSharpTest/Completion/CompletionProviders/InternalsVisibleToCompletionProviderTests.cs
+++ src/EditorFeatures/CSharpTest/Completion/CompletionProviders/InternalsVisibleToCompletionProviderTests.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.Completion;
+﻿using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Completion.Providers;
 using Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces;
 using Roslyn.Test.Utilities;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionProviders
 {
     public class InternalsVisibleToCompletionProviderTests : AbstractCSharpCompletionProviderTests
     {
+        private static readonly string s_keyPairFile = SigningTestHelpers.KeyPairFile;
+        private static readonly DesktopStrongNameProvider s_defaultProvider = new SigningTestHelpers.VirtualizedStrongNameProvider(ImmutableArray.Create<string>());
         public InternalsVisibleToCompletionProviderTests(CSharpTestWorkspaceFixture workspaceFixture) : base(workspaceFixture)
         {
             // I needed to configure the workspace here, because CreateWorkspace was never called.
             var ws = workspaceFixture.GetWorkspace();
             var solution = ws.CurrentSolution;
-            solution = solution.AddProject(ProjectId.CreateNewId(), "ClassLibrary1", "ClassLibrary1.dll", LanguageNames.CSharp);
+            var pi1 = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "ClassLibrary1", "ClassLibrary1", LanguageNames.CSharp,
+                compilationOptions: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, cryptoKeyFile: s_keyPairFile, strongNameProvider: s_defaultProvider));
+            var pi2 = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "ClassLibrary2", "ClassLibrary2", LanguageNames.CSharp);
+            solution = solution.AddProject(pi1).AddProject(pi2);
             ws.ChangeSolution(solution);
         }
 
@@ -37,7 +39,49 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionPr
             var text = @"
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo(""$$"")]
 ";
-            await VerifyItemExistsAsync(text, "ClassLibrary1.dll");
+            await VerifyItemExistsAsync(text, "ClassLibrary1");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task CodeCompletionIsEmptyAtClosingDoubleQuote()
+        {
+            var text = @"
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(""""$$)]
+";
+            await VerifyNoItemsExistAsync(text);
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task CodeCompletionDoesNotContainsCurrentAssembly()
+        {
+            var text = @"
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(""$$"")]
+";
+            await VerifyItemIsAbsentAsync(text, "Test");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task CodeCompletionInsertsAssemblyNameOnCommit()
+        {
+            var before = @"
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(""$$"")]
+";
+            var after = @"
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(""ClassLibrary2"")]
+";
+            await VerifyProviderCommitAsync(before, "ClassLibrary2", after, null, "");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task CodeCompletionInsertsPublicKeyOnCommit()
+        {
+            var before = @"
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(""$$"")]
+";
+            var after = @"
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(""ClassLibrary1, PublicKey=00240000048000009400000006020000002400005253413100040000010001002b986f6b5ea5717d35c72d38561f413e267029efa9b5f107b9331d83df657381325b3a67b75812f63a9436ceccb49494de8f574f8e639d4d26c0fcf8b0e9a1a196b80b6f6ed053628d10d027e032df2ed1d60835e5f47d32c9ef6da10d0366a319573362c821b5f8fa5abc5bb22241de6f666a85d82d6ba8c3090d01636bd2bb"")]
+";
+            await VerifyProviderCommitAsync(before, "ClassLibrary1", after, null, "");
         }
     }
 }
--- src/Features/CSharp/Portable/Completion/CompletionProviders/InternalsVisibleToCompletionProvider.cs
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/InternalsVisibleToCompletionProvider.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.Completion;
+﻿using Microsoft.CodeAnalysis.Completion;
 using Microsoft.CodeAnalysis.CSharp.Extensions;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
 {
     internal sealed class InternalsVisibleToCompletionProvider : CommonCompletionProvider
     {
+        private const string PublicKeyPropertyKey = "PublicKey";
+
         internal override bool IsInsertionTrigger(SourceText text, int insertedCharacterPosition, OptionSet options)
         {
             var ch = text[insertedCharacterPosition];
@@ -25,24 +30,88 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
         {
             var cancellationToken = context.CancellationToken;
             var syntaxTree = await context.Document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
-            var token = syntaxTree.FindTokenOnLeftOfPosition(context.Position, cancellationToken);
-            var attr = token.GetAncestor<AttributeSyntax>();
-            if (attr == null)
+            if (syntaxTree.IsEntirelyWithinStringLiteral(context.Position, cancellationToken))
             {
-                return;
-            }
-            if (attr.Name.TryGetNameParts(out IList<string> nameParts) && nameParts.Count > 0)
-            {
-                var lastName = nameParts[nameParts.Count - 1];
-                if (lastName == "InternalsVisibleTo" || lastName == "InternalsVisibleToAttribute")
+                var token = syntaxTree.FindTokenOnLeftOfPosition(context.Position, cancellationToken);
+                var attr = GetAttributeSyntaxOfToken(token);
+                if (attr == null)
                 {
-                    foreach (var p in context.Document.Project.Solution.Projects)
+                    return;
+                }
+                if (attr.Name.TryGetNameParts(out IList<string> nameParts) && nameParts.Count > 0)
+                {
+                    var lastName = nameParts[nameParts.Count - 1];
+                    if (lastName == "InternalsVisibleTo" || lastName == "InternalsVisibleToAttribute")
                     {
-                        var item = CompletionItem.Create(displayText: p.AssemblyName);
-                        context.AddItem(item);
+                        await AddAssemblyCompletionItems(context, cancellationToken).ConfigureAwait(false);
                     }
                 }
             }
+        }
+
+        private static async Task AddAssemblyCompletionItems(CompletionContext context, CancellationToken cancellationToken)
+        {
+            var currentProject = context.Document.Project;
+            foreach (var p in context.Document.Project.Solution.Projects)
+            {
+                if (p == currentProject)
+                {
+                    continue;
+                }
+                var publicKey = await GetPublicKeyOfProject(p, cancellationToken).ConfigureAwait(false);
+                var item = string.IsNullOrEmpty(publicKey)
+                    ? CompletionItem.Create(displayText: p.AssemblyName)
+                    : CompletionItem.Create(displayText: p.AssemblyName, properties: ImmutableDictionary.Create<string, string>().Add(PublicKeyPropertyKey, publicKey));
+                context.AddItem(item);
+            }
+        }
+
+        private static async Task<string> GetPublicKeyOfProject(Project p, CancellationToken cancellationToken)
+        {
+            var compilationOptions = p.CompilationOptions;
+            if (string.IsNullOrEmpty(compilationOptions.CryptoKeyFile) && string.IsNullOrEmpty(compilationOptions.CryptoKeyContainer))
+            {
+                return string.Empty;
+            }
+            var c = await p.GetCompilationAsync(cancellationToken).ConfigureAwait(false);
+            if (c.Assembly?.Identity?.IsStrongName ?? false)
+            {
+                var sb = new StringBuilder(capacity: c.Assembly.Identity.PublicKey.Length * 2);
+                foreach (var b in c.Assembly.Identity.PublicKey)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            else
+                return string.Empty;
+        }
+
+        private static AttributeSyntax GetAttributeSyntaxOfToken(SyntaxToken token)
+        {
+            var node = token.Parent;
+            if (node is LiteralExpressionSyntax && node.Kind() == SyntaxKind.StringLiteralExpression)
+            {
+                node = node.Parent;
+                if (node is AttributeArgumentSyntax)
+                {
+                    node = node.Parent;
+                    if (node is AttributeArgumentListSyntax)
+                    {
+                        return node.Parent as AttributeSyntax;
+                    }
+                }
+            }
+            return default(AttributeSyntax);
+        }
+
+        protected override Task<TextChange?> GetTextChangeAsync(CompletionItem selectedItem, char? ch, CancellationToken cancellationToken)
+        {
+            var assemblyName = selectedItem.DisplayText;
+            if (selectedItem.Properties.TryGetValue(PublicKeyPropertyKey, out string publicKey))
+                assemblyName += ", PublicKey=" + publicKey;
+            TextChange? tc = new TextChange(selectedItem.Span, assemblyName);
+            return Task.FromResult(tc);
         }
     }
 }
