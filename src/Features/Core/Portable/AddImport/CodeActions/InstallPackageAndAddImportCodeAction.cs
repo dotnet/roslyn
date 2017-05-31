ac4e335a28fb81885588427ba1f17e3e3c361892
CyrusNajmabadi <cyrusn@microsoft.com>
2017-05-31 14:36:14 -0700

    indent to make PR easier ot understand.

--- src/Features/Core/Portable/AddImport/CodeActions/InstallPackageAndAddImportCodeAction.cs
+++ src/Features/Core/Portable/AddImport/CodeActions/InstallPackageAndAddImportCodeAction.cs
@@ -14,138 +14,138 @@ namespace Microsoft.CodeAnalysis.CodeFixes.AddImport
 {
     internal abstract partial class AbstractAddImportCodeFixProvider<TSimpleNameSyntax>
     {
-        private class InstallPackageAndAddImportCodeAction : CodeAction
-        {
-            /// <summary>
-            /// The document before we added the import. Used so we can roll back if installing
-            /// the package failed.
-            /// </summary>
-            private readonly Document _oldDocument;
-
-            /// <summary>
-            /// The changes to make to <see cref="_oldDocument"/> to add the import.
-            /// </summary>
-            private readonly ImmutableArray<TextChange> _textChanges;
-
-            /// <summary>
-            /// The operation that will actually install the nuget package.
-            /// </summary>
-            private readonly InstallPackageDirectlyCodeActionOperation _installOperation;
-
-            public override string Title { get; }
-            public override string EquivalenceKey => Title;
-            internal override CodeActionPriority Priority { get; }
-
-            public InstallPackageAndAddImportCodeAction(
-                string title,
-                CodeActionPriority priority,
-                Document oldDocument,
-                ImmutableArray<TextChange> textChanges,
-                InstallPackageDirectlyCodeActionOperation installOperation)
+            private class InstallPackageAndAddImportCodeAction : CodeAction
             {
-                Title = title;
-                Priority = priority;
-                _oldDocument = oldDocument;
-                _textChanges = textChanges;
-                _installOperation = installOperation;
-            }
+                /// <summary>
+                /// The document before we added the import. Used so we can roll back if installing
+                /// the package failed.
+                /// </summary>
+                private readonly Document _oldDocument;
 
-            /// <summary>
-            /// For preview purposes we return all the operations in a list.  This way the 
-            /// preview system stiches things together in the UI to make a suitable display.
-            /// i.e. if we have a SolutionChangedOperation and some other operation with a 
-            /// Title, then the UI will show that nicely to the user.
-            /// </summary>
-            protected override async Task<IEnumerable<CodeActionOperation>> ComputePreviewOperationsAsync(CancellationToken cancellationToken)
-            {
-                // Make a SolutionChangeAction.  This way we can let it generate the diff
-                // preview appropriately.
-                var solutionChangeAction = new SolutionChangeAction(
-                    "", c => GetUpdatedSolutionAsync(c));
+                /// <summary>
+                /// The changes to make to <see cref="_oldDocument"/> to add the import.
+                /// </summary>
+                private readonly ImmutableArray<TextChange> _textChanges;
 
-                var result = ArrayBuilder<CodeActionOperation>.GetInstance();
-                result.AddRange(await solutionChangeAction.GetPreviewOperationsAsync(cancellationToken).ConfigureAwait(false));
-                result.Add(_installOperation);
-                return result.ToImmutableAndFree();
-            }
+                /// <summary>
+                /// The operation that will actually install the nuget package.
+                /// </summary>
+                private readonly InstallPackageDirectlyCodeActionOperation _installOperation;
 
-            private async Task<Solution> GetUpdatedSolutionAsync(CancellationToken cancellationToken)
-            {
-                var oldText = await _oldDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
-                var newText = oldText.WithChanges(_textChanges);
+                public override string Title { get; }
+                public override string EquivalenceKey => Title;
+                internal override CodeActionPriority Priority { get; }
 
-                var newDocument = _oldDocument.WithText(newText);
-                var newRoot = await newDocument.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-
-                // Suppress diagnostics on the import we create.  Because we only get here when we are 
-                // adding a nuget package, it is certainly the case that in the preview this will not
-                // bind properly.  It will look silly to show such an error, so we just suppress things.
-                var updatedRoot = newRoot.WithAdditionalAnnotations(SuppressDiagnosticsAnnotation.Create());
-                var updatedDocument = newDocument.WithSyntaxRoot(updatedRoot);
-
-                return updatedDocument.Project.Solution;
-            }
-
-            /// <summary>
-            /// However, for application purposes, we end up returning a single operation
-            /// that will then apply all our sub actions in order, stopping the moment
-            /// one of them fails.
-            /// </summary>
-            protected override async Task<IEnumerable<CodeActionOperation>> ComputeOperationsAsync(
-                CancellationToken cancellationToken)
-            {
-                var oldText = await _oldDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
-                var newText = oldText.WithChanges(_textChanges);
-
-                return ImmutableArray.Create<CodeActionOperation>(
-                    new InstallPackageAndAddImportOperation(
-                        _oldDocument.Id, oldText, newText, _installOperation));
-            }
-        }
-
-        private class InstallPackageAndAddImportOperation : CodeActionOperation
-        {
-            private readonly DocumentId _changedDocumentId;
-            private readonly SourceText _oldText;
-            private readonly SourceText _newText;
-            private readonly InstallPackageDirectlyCodeActionOperation _installPackageOperation;
-
-            public InstallPackageAndAddImportOperation(
-                DocumentId changedDocumentId,
-                SourceText oldText,
-                SourceText newText,
-                InstallPackageDirectlyCodeActionOperation item2)
-            {
-                _changedDocumentId = changedDocumentId;
-                _oldText = oldText;
-                _newText = newText;
-                _installPackageOperation = item2;
-            }
-
-            internal override bool ApplyDuringTests => _installPackageOperation.ApplyDuringTests;
-            public override string Title => _installPackageOperation.Title;
-
-            internal override bool TryApply(Workspace workspace, IProgressTracker progressTracker, CancellationToken cancellationToken)
-            {
-                var newSolution = workspace.CurrentSolution.WithDocumentText(
-                    _changedDocumentId, _newText);
-
-                // First make the changes to add the import to the document.
-                if (workspace.TryApplyChanges(newSolution, progressTracker))
+                public InstallPackageAndAddImportCodeAction(
+                    string title,
+                    CodeActionPriority priority,
+                    Document oldDocument,
+                    ImmutableArray<TextChange> textChanges,
+                    InstallPackageDirectlyCodeActionOperation installOperation)
                 {
-                    if (_installPackageOperation.TryApply(workspace, progressTracker, cancellationToken))
-                    {
-                        return true;
-                    }
-
-                    // Installing the nuget package failed.  Roll back the workspace.
-                    var rolledBackSolution = workspace.CurrentSolution.WithDocumentText(
-                        _changedDocumentId, _oldText);
-                    workspace.TryApplyChanges(rolledBackSolution, progressTracker);
+                    Title = title;
+                    Priority = priority;
+                    _oldDocument = oldDocument;
+                    _textChanges = textChanges;
+                    _installOperation = installOperation;
                 }
 
-                return false;
+                /// <summary>
+                /// For preview purposes we return all the operations in a list.  This way the 
+                /// preview system stiches things together in the UI to make a suitable display.
+                /// i.e. if we have a SolutionChangedOperation and some other operation with a 
+                /// Title, then the UI will show that nicely to the user.
+                /// </summary>
+                protected override async Task<IEnumerable<CodeActionOperation>> ComputePreviewOperationsAsync(CancellationToken cancellationToken)
+                {
+                    // Make a SolutionChangeAction.  This way we can let it generate the diff
+                    // preview appropriately.
+                    var solutionChangeAction = new SolutionChangeAction(
+                        "", c => GetUpdatedSolutionAsync(c));
+
+                    var result = ArrayBuilder<CodeActionOperation>.GetInstance();
+                    result.AddRange(await solutionChangeAction.GetPreviewOperationsAsync(cancellationToken).ConfigureAwait(false));
+                    result.Add(_installOperation);
+                    return result.ToImmutableAndFree();
+                }
+
+                private async Task<Solution> GetUpdatedSolutionAsync(CancellationToken cancellationToken)
+                {
+                    var oldText = await _oldDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
+                    var newText = oldText.WithChanges(_textChanges);
+
+                    var newDocument = _oldDocument.WithText(newText);
+                    var newRoot = await newDocument.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+                    // Suppress diagnostics on the import we create.  Because we only get here when we are 
+                    // adding a nuget package, it is certainly the case that in the preview this will not
+                    // bind properly.  It will look silly to show such an error, so we just suppress things.
+                    var updatedRoot = newRoot.WithAdditionalAnnotations(SuppressDiagnosticsAnnotation.Create());
+                    var updatedDocument = newDocument.WithSyntaxRoot(updatedRoot);
+
+                    return updatedDocument.Project.Solution;
+                }
+
+                /// <summary>
+                /// However, for application purposes, we end up returning a single operation
+                /// that will then apply all our sub actions in order, stopping the moment
+                /// one of them fails.
+                /// </summary>
+                protected override async Task<IEnumerable<CodeActionOperation>> ComputeOperationsAsync(
+                    CancellationToken cancellationToken)
+                {
+                    var oldText = await _oldDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
+                    var newText = oldText.WithChanges(_textChanges);
+
+                    return ImmutableArray.Create<CodeActionOperation>(
+                        new InstallPackageAndAddImportOperation(
+                            _oldDocument.Id, oldText, newText, _installOperation));
+                }
             }
-        }
+
+            private class InstallPackageAndAddImportOperation : CodeActionOperation
+            {
+                private readonly DocumentId _changedDocumentId;
+                private readonly SourceText _oldText;
+                private readonly SourceText _newText;
+                private readonly InstallPackageDirectlyCodeActionOperation _installPackageOperation;
+
+                public InstallPackageAndAddImportOperation(
+                    DocumentId changedDocumentId,
+                    SourceText oldText,
+                    SourceText newText,
+                    InstallPackageDirectlyCodeActionOperation item2)
+                {
+                    _changedDocumentId = changedDocumentId;
+                    _oldText = oldText;
+                    _newText = newText;
+                    _installPackageOperation = item2;
+                }
+
+                internal override bool ApplyDuringTests => _installPackageOperation.ApplyDuringTests;
+                public override string Title => _installPackageOperation.Title;
+
+                internal override bool TryApply(Workspace workspace, IProgressTracker progressTracker, CancellationToken cancellationToken)
+                {
+                    var newSolution = workspace.CurrentSolution.WithDocumentText(
+                        _changedDocumentId, _newText);
+
+                    // First make the changes to add the import to the document.
+                    if (workspace.TryApplyChanges(newSolution, progressTracker))
+                    {
+                        if (_installPackageOperation.TryApply(workspace, progressTracker, cancellationToken))
+                        {
+                            return true;
+                        }
+
+                        // Installing the nuget package failed.  Roll back the workspace.
+                        var rolledBackSolution = workspace.CurrentSolution.WithDocumentText(
+                            _changedDocumentId, _oldText);
+                        workspace.TryApplyChanges(rolledBackSolution, progressTracker);
+                    }
+
+                    return false;
+                }
+            }
     }
 }
--- src/Features/Core/Portable/AddImport/CodeActions/SymbolReferenceCodeAction.cs
+++ src/Features/Core/Portable/AddImport/CodeActions/SymbolReferenceCodeAction.cs
@@ -12,121 +12,121 @@ namespace Microsoft.CodeAnalysis.CodeFixes.AddImport
 {
     internal abstract partial class AbstractAddImportCodeFixProvider<TSimpleNameSyntax>
     {
-        /// <summary>
-        /// Code action we use when just adding a using, possibly with a project or
-        /// metadata reference.  We don't use the standard code action types because
-        /// we want to do things like show a glyph if this will do more than just add
-        /// an import.
-        /// </summary>
-        private class SymbolReferenceCodeAction : CodeAction
-        {
-            public override string Title { get; }
-            public override ImmutableArray<string> Tags { get; }
-            internal override CodeActionPriority Priority { get; }
-
-            public override string EquivalenceKey => this.Title;
-
             /// <summary>
-            /// The <see cref="Document"/> we started the add-import analysis in.
+            /// Code action we use when just adding a using, possibly with a project or
+            /// metadata reference.  We don't use the standard code action types because
+            /// we want to do things like show a glyph if this will do more than just add
+            /// an import.
             /// </summary>
-            private readonly Document _contextDocument;
-
-            /// <summary>
-            /// The changes to make to <see cref="_contextDocument"/> to add the import.
-            /// </summary>
-            private readonly ImmutableArray<TextChange> _textChanges;
-
-            /// <summary>
-            /// The optional id for a <see cref="Project"/> we'd like to add a reference to.
-            /// </summary>
-            private readonly ProjectId _projectReferenceToAdd;
-
-            /// <summary>
-            /// If we're adding <see cref="_portableExecutableReferenceFilePathToAdd"/> then this
-            /// is the id for the <see cref="Project"/> we can find that <see cref="PortableExecutableReference"/>
-            /// referenced from.
-            /// </summary>
-            private readonly ProjectId _portableExecutableReferenceProjectId;
-
-            /// <summary>
-            /// If we want to add a <see cref="PortableExecutableReference"/> metadata reference, this 
-            /// is the <see cref="PortableExecutableReference.FilePath"/> for it.
-            /// </summary>
-            private readonly string _portableExecutableReferenceFilePathToAdd;
-
-            private SymbolReferenceCodeAction(
-                Document contextDocument,
-                ImmutableArray<TextChange> textChanges,
-                string title, ImmutableArray<string> tags,
-                CodeActionPriority priority)
+            private class SymbolReferenceCodeAction : CodeAction
             {
-                _contextDocument = contextDocument;
-                _textChanges = textChanges;
-                Title = title;
-                Tags = tags;
-                Priority = priority;
-            }
+                public override string Title { get; }
+                public override ImmutableArray<string> Tags { get; }
+                internal override CodeActionPriority Priority { get; }
 
-            public SymbolReferenceCodeAction(
-                Document contextDocument,
-                ImmutableArray<TextChange> textChanges,
-                string title, ImmutableArray<string> tags,
-                CodeActionPriority priority,
-                ProjectId projectReferenceToAdd)
-                : this(contextDocument, textChanges, title, tags, priority)
-            {
-                // We only want to add a project reference if the project the import references
-                // is different from the project we started from.
-                if (projectReferenceToAdd != contextDocument.Project.Id)
+                public override string EquivalenceKey => this.Title;
+
+                /// <summary>
+                /// The <see cref="Document"/> we started the add-import analysis in.
+                /// </summary>
+                private readonly Document _contextDocument;
+
+                /// <summary>
+                /// The changes to make to <see cref="_contextDocument"/> to add the import.
+                /// </summary>
+                private readonly ImmutableArray<TextChange> _textChanges;
+
+                /// <summary>
+                /// The optional id for a <see cref="Project"/> we'd like to add a reference to.
+                /// </summary>
+                private readonly ProjectId _projectReferenceToAdd;
+
+                /// <summary>
+                /// If we're adding <see cref="_portableExecutableReferenceFilePathToAdd"/> then this
+                /// is the id for the <see cref="Project"/> we can find that <see cref="PortableExecutableReference"/>
+                /// referenced from.
+                /// </summary>
+                private readonly ProjectId _portableExecutableReferenceProjectId;
+
+                /// <summary>
+                /// If we want to add a <see cref="PortableExecutableReference"/> metadata reference, this 
+                /// is the <see cref="PortableExecutableReference.FilePath"/> for it.
+                /// </summary>
+                private readonly string _portableExecutableReferenceFilePathToAdd;
+
+                private SymbolReferenceCodeAction(
+                    Document contextDocument,
+                    ImmutableArray<TextChange> textChanges,
+                    string title, ImmutableArray<string> tags,
+                    CodeActionPriority priority)
                 {
-                    _projectReferenceToAdd = projectReferenceToAdd;
-                }
-            }
-
-            public SymbolReferenceCodeAction(
-                Document contextDocument,
-                ImmutableArray<TextChange> textChanges,
-                string title, ImmutableArray<string> tags,
-                CodeActionPriority priority,
-
-                ProjectId portableExecutableReferenceProjectId,
-                string portableExecutableReferenceFilePathToAdd)
-                : this(contextDocument, textChanges, title, tags, priority)
-            {
-                _portableExecutableReferenceProjectId = portableExecutableReferenceProjectId;
-                _portableExecutableReferenceFilePathToAdd = portableExecutableReferenceFilePathToAdd;
-            }
-
-            protected override async Task<Solution> GetChangedSolutionAsync(CancellationToken cancellationToken)
-            {
-                var oldText = await _contextDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
-                var newText = oldText.WithChanges(_textChanges);
-
-                var updatedProject = _contextDocument.WithText(newText).Project;
-
-                if (_projectReferenceToAdd != null)
-                {
-                    updatedProject = updatedProject.AddProjectReference(new ProjectReference(_projectReferenceToAdd));
-                }
-                else if (_portableExecutableReferenceFilePathToAdd != null)
-                {
-                    var projectWithReference = updatedProject.Solution.GetProject(_portableExecutableReferenceProjectId);
-                    var reference = projectWithReference.MetadataReferences
-                                                        .OfType<PortableExecutableReference>()
-                                                        .First(pe => pe.FilePath == _portableExecutableReferenceFilePathToAdd);
-
-                    updatedProject = updatedProject.AddMetadataReference(reference);
+                    _contextDocument = contextDocument;
+                    _textChanges = textChanges;
+                    Title = title;
+                    Tags = tags;
+                    Priority = priority;
                 }
 
-                var updatedSolution = updatedProject.Solution;
-                return updatedSolution;
+                public SymbolReferenceCodeAction(
+                    Document contextDocument,
+                    ImmutableArray<TextChange> textChanges,
+                    string title, ImmutableArray<string> tags,
+                    CodeActionPriority priority,
+                    ProjectId projectReferenceToAdd)
+                    : this(contextDocument, textChanges, title, tags, priority)
+                {
+                    // We only want to add a project reference if the project the import references
+                    // is different from the project we started from.
+                    if (projectReferenceToAdd != contextDocument.Project.Id)
+                    {
+                        _projectReferenceToAdd = projectReferenceToAdd;
+                    }
+                }
+
+                public SymbolReferenceCodeAction(
+                    Document contextDocument,
+                    ImmutableArray<TextChange> textChanges,
+                    string title, ImmutableArray<string> tags,
+                    CodeActionPriority priority,
+
+                    ProjectId portableExecutableReferenceProjectId,
+                    string portableExecutableReferenceFilePathToAdd)
+                    : this(contextDocument, textChanges, title, tags, priority)
+                {
+                    _portableExecutableReferenceProjectId = portableExecutableReferenceProjectId;
+                    _portableExecutableReferenceFilePathToAdd = portableExecutableReferenceFilePathToAdd;
+                }
+
+                protected override async Task<Solution> GetChangedSolutionAsync(CancellationToken cancellationToken)
+                {
+                    var oldText = await _contextDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
+                    var newText = oldText.WithChanges(_textChanges);
+
+                    var updatedProject = _contextDocument.WithText(newText).Project;
+
+                    if (_projectReferenceToAdd != null)
+                    {
+                        updatedProject = updatedProject.AddProjectReference(new ProjectReference(_projectReferenceToAdd));
+                    }
+                    else if (_portableExecutableReferenceFilePathToAdd != null)
+                    {
+                        var projectWithReference = updatedProject.Solution.GetProject(_portableExecutableReferenceProjectId);
+                        var reference = projectWithReference.MetadataReferences
+                                                            .OfType<PortableExecutableReference>()
+                                                            .First(pe => pe.FilePath == _portableExecutableReferenceFilePathToAdd);
+
+                        updatedProject = updatedProject.AddMetadataReference(reference);
+                    }
+
+                    var updatedSolution = updatedProject.Solution;
+                    return updatedSolution;
+                }
+
+                internal override bool PerformFinalApplicabilityCheck
+                    => _projectReferenceToAdd != null;
+
+                internal override bool IsApplicable(Workspace workspace)
+                    => _projectReferenceToAdd != null && workspace.CanAddProjectReference(_contextDocument.Project.Id, _projectReferenceToAdd);
             }
-
-            internal override bool PerformFinalApplicabilityCheck
-                => _projectReferenceToAdd != null;
-
-            internal override bool IsApplicable(Workspace workspace)
-                => _projectReferenceToAdd != null && workspace.CanAddProjectReference(_contextDocument.Project.Id, _projectReferenceToAdd);
-        }
     }
 }
