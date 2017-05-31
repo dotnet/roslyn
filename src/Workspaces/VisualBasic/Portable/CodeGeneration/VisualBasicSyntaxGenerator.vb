bfcee95d794c36c2e20e3f34ddb8ec64bda09a30
Tom Deseyn <tom.deseyn@gmail.com>
2017-05-31 11:43:14 +0200

    Fix CreateTupleType access modifier

--- src/Workspaces/VisualBasic/Portable/CodeGeneration/VisualBasicSyntaxGenerator.vb
+++ src/Workspaces/VisualBasic/Portable/CodeGeneration/VisualBasicSyntaxGenerator.vb
@@ -531,7 +531,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.CodeGeneration
             End If
         End Function
 
-        Public Overrides Function CreateTupleType(elements As IEnumerable(Of SyntaxNode)) As SyntaxNode
+        Protected Overrides Function CreateTupleType(elements As IEnumerable(Of SyntaxNode)) As SyntaxNode
             Return SyntaxFactory.TupleType(SyntaxFactory.SeparatedList(elements.Cast(Of TupleElementSyntax)()))
         End Function
 
