8936dcf75441b1f5e80e49421a13c46bc6eb9a41
CyrusNajmabadi <cyrusn@microsoft.com>
2017-06-01 23:35:09 -0700

    Fallback when there is no installer service.

--- src/Features/Core/Portable/AddImport/AbstractAddImportCodeFixProvider.cs
+++ src/Features/Core/Portable/AddImport/AbstractAddImportCodeFixProvider.cs
@@ -58,7 +58,7 @@ namespace Microsoft.CodeAnalysis.AddImport
                 : null;
 
             var packageSources = symbolSearchService != null && searchNuGetPackages
-                ? GetPackageInstallerService(document).PackageSources
+                ? GetPackageSources(document)
                 : ImmutableArray<PackageSource>.Empty;
 
             // We might have multiple different diagnostics covering the same span.  Have to
@@ -91,6 +91,9 @@ namespace Microsoft.CodeAnalysis.AddImport
 
         private IPackageInstallerService GetPackageInstallerService(Document document)
             => _packageInstallerService ?? document.Project.Solution.Workspace.Services.GetService<IPackageInstallerService>();
+
+        private ImmutableArray<PackageSource> GetPackageSources(Document document)
+            => GetPackageInstallerService(document)?.PackageSources ?? ImmutableArray<PackageSource>.Empty;
 
         private CodeAction TryCreateCodeAction(Document document, AddImportFixData fixData)
         {
