f629039dbebbc4d119ed572646c0ab3da5af3922
Sam Harwell <sam.harwell@microsoft.com>
2017-06-16 09:37:07 -0500

    Move nested struct to the end of the containing class

--- src/Workspaces/Core/Portable/Workspace/Solution/Checksum.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/Checksum.cs
@@ -16,61 +16,6 @@ namespace Microsoft.CodeAnalysis
     {
         public static readonly Checksum Null = new Checksum(Array.Empty<byte>());
 
-        /// <summary>
-        /// This structure stores the 20-byte SHA 1 hash as an inline value rather than requiring the use of
-        /// <c>byte[]</c>.
-        /// </summary>
-        [StructLayout(LayoutKind.Explicit, Size = 20)]
-        private struct Sha1Hash : IEquatable<Sha1Hash>
-        {
-            [FieldOffset(0)]
-            private long Data1;
-
-            [FieldOffset(8)]
-            private long Data2;
-
-            [FieldOffset(16)]
-            private int Data3;
-
-            public static bool operator ==(Sha1Hash x, Sha1Hash y)
-                => x.Equals(y);
-
-            public static bool operator !=(Sha1Hash x, Sha1Hash y)
-                => !x.Equals(y);
-
-            public void WriteTo(ObjectWriter writer)
-            {
-                writer.WriteInt64(Data1);
-                writer.WriteInt64(Data2);
-                writer.WriteInt32(Data3);
-            }
-
-            public static Sha1Hash ReadFrom(ObjectReader reader)
-            {
-                Sha1Hash result = default;
-                result.Data1 = reader.ReadInt64();
-                result.Data2 = reader.ReadInt64();
-                result.Data3 = reader.ReadInt32();
-                return result;
-            }
-
-            public override int GetHashCode()
-            {
-                // The checksum is already a hash. Just read a 4-byte value to get a well-distributed hash code.
-                return (int)Data1;
-            }
-
-            public override bool Equals(object obj)
-                => obj is Sha1Hash other && Equals(other);
-
-            public bool Equals(Sha1Hash other)
-            {
-                return Data1 == other.Data1
-                    && Data2 == other.Data2
-                    && Data3 == other.Data3;
-            }
-        }
-
         private Sha1Hash _checkSum;
 
         public unsafe Checksum(byte[] checksum)
@@ -147,6 +92,61 @@ namespace Microsoft.CodeAnalysis
         public static string GetChecksumsLogInfo(IEnumerable<Checksum> checksums)
         {
             return string.Join("|", checksums.Select(c => c.ToString()));
+        }
+
+        /// <summary>
+        /// This structure stores the 20-byte SHA 1 hash as an inline value rather than requiring the use of
+        /// <c>byte[]</c>.
+        /// </summary>
+        [StructLayout(LayoutKind.Explicit, Size = 20)]
+        private struct Sha1Hash : IEquatable<Sha1Hash>
+        {
+            [FieldOffset(0)]
+            private long Data1;
+
+            [FieldOffset(8)]
+            private long Data2;
+
+            [FieldOffset(16)]
+            private int Data3;
+
+            public static bool operator ==(Sha1Hash x, Sha1Hash y)
+                => x.Equals(y);
+
+            public static bool operator !=(Sha1Hash x, Sha1Hash y)
+                => !x.Equals(y);
+
+            public void WriteTo(ObjectWriter writer)
+            {
+                writer.WriteInt64(Data1);
+                writer.WriteInt64(Data2);
+                writer.WriteInt32(Data3);
+            }
+
+            public static Sha1Hash ReadFrom(ObjectReader reader)
+            {
+                Sha1Hash result = default;
+                result.Data1 = reader.ReadInt64();
+                result.Data2 = reader.ReadInt64();
+                result.Data3 = reader.ReadInt32();
+                return result;
+            }
+
+            public override int GetHashCode()
+            {
+                // The checksum is already a hash. Just read a 4-byte value to get a well-distributed hash code.
+                return (int)Data1;
+            }
+
+            public override bool Equals(object obj)
+                => obj is Sha1Hash other && Equals(other);
+
+            public bool Equals(Sha1Hash other)
+            {
+                return Data1 == other.Data1
+                    && Data2 == other.Data2
+                    && Data3 == other.Data3;
+            }
         }
     }
 }
