2626dd87ae7e4647cda61a068e2c78804310a534
Tomas Matousek <tomat@microsoft.com>
2017-06-16 19:02:43 -0700

    Remove unused namespaces

--- src/Dependencies/CodeAnalysis.Debugging/CustomDebugInfoReader.cs
+++ src/Dependencies/CodeAnalysis.Debugging/CustomDebugInfoReader.cs
@@ -6,7 +6,6 @@ using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Globalization;
 using System.Text;
-using Microsoft.CodeAnalysis.Collections;
 using Microsoft.CodeAnalysis.PooledObjects;
 
 #pragma warning disable RS0010 // Avoid using cref tags with a prefix
