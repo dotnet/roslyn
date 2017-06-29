cf0930587188749e648f38ae1b15932b42c7f15a
CyrusNajmabadi <cyrusn@microsoft.com>
2017-06-29 14:14:41 -0700

    Use shared helper

--- src/Features/CSharp/Portable/Organizing/Organizers/ModifiersOrganizer.cs
+++ src/Features/CSharp/Portable/Organizing/Organizers/ModifiersOrganizer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Extensions;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp.Organizing.Organizers
