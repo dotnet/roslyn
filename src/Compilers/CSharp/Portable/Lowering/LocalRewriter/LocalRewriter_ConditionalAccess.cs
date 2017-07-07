a6f56640b183de30ac1f8b0b3cc3af68731e6b0a
Julien Couvreur <julien.couvreur@gmail.com>
2017-07-07 15:35:59 -0700

    Add assertion

--- src/Compilers/CSharp/Portable/Lowering/LocalRewriter/LocalRewriter_ConditionalAccess.cs
+++ src/Compilers/CSharp/Portable/Lowering/LocalRewriter/LocalRewriter_ConditionalAccess.cs
@@ -15,6 +15,7 @@ namespace Microsoft.CodeAnalysis.CSharp
 
         public override BoundNode VisitLoweredConditionalAccess(BoundLoweredConditionalAccess node)
         {
+            Debug.Assert(false);
             return node;
         }
 
