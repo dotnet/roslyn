4f7d0f1dd936cdd03c248fa5f433ea54aac3f6e6
CyrusNajmabadi <cyrusn@microsoft.com>
2017-07-16 13:03:25 -0700

    Add VB tests.

--- src/Features/VisualBasic/Portable/AddAccessibilityModifiers/VisualBasicAddAccessibilityModifiersDiagnosticAnalyzer.vb
+++ src/Features/VisualBasic/Portable/AddAccessibilityModifiers/VisualBasicAddAccessibilityModifiersDiagnosticAnalyzer.vb
@@ -21,60 +21,55 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.AddAccessibilityModifiers
 
         Private Sub ProcessMembers(context As SyntaxTreeAnalysisContext, generator As SyntaxGenerator,
                                    [option] As CodeStyleOption(Of AccessibilityModifiersRequired), members As SyntaxList(Of StatementSyntax))
-            For Each member In members
-                ProcessMember(context, generator, [option], member)
-            Next
-        End Sub
+                For Each member In members
+                    ProcessMember(context, generator, [option], member)
+                Next
+            End Sub
 
-        Private Sub ProcessMember(context As SyntaxTreeAnalysisContext, generator As SyntaxGenerator,
+            Private Sub ProcessMember(context As SyntaxTreeAnalysisContext, generator As SyntaxGenerator,
                                   [option] As CodeStyleOption(Of AccessibilityModifiersRequired), member As StatementSyntax)
 
 
-            If member.Kind() = SyntaxKind.NamespaceBlock Then
-                Dim namespaceBlock = DirectCast(member, NamespaceBlockSyntax)
-                ProcessMembers(context, generator, [option], namespaceBlock.Members)
-            End If
+                If member.Kind() = SyntaxKind.NamespaceBlock Then
+                    Dim namespaceBlock = DirectCast(member, NamespaceBlockSyntax)
+                    ProcessMembers(context, generator, [option], namespaceBlock.Members)
+                End If
 
-            ' If we have a class or struct or module, recurse inwards.
-            If member.IsKind(SyntaxKind.ClassBlock) OrElse
+                ' If we have a class or struct or module, recurse inwards.
+                If member.IsKind(SyntaxKind.ClassBlock) OrElse
                member.IsKind(SyntaxKind.StructureBlock) OrElse
                member.IsKind(SyntaxKind.ModuleBlock) Then
 
-                Dim typeBlock = DirectCast(member, TypeBlockSyntax)
-                ProcessMembers(context, generator, [option], typeBlock.Members)
-            End If
+                    Dim typeBlock = DirectCast(member, TypeBlockSyntax)
+                    ProcessMembers(context, generator, [option], typeBlock.Members)
+                End If
 
-            Dim declarationStatement = TryCast(member, DeclarationStatementSyntax)
-            If declarationStatement IsNot Nothing Then
-                Return
-            End If
+                ' Have to have a name to report the issue on.
+                Dim name = member.GetNameToken()
+                If name.Kind() = SyntaxKind.None Then
+                    Return
+                End If
 
-            ' Have to have a name to report the issue on.
-            Dim name = declarationStatement.GetNameToken()
-            If name.Kind() = SyntaxKind.None Then
-                Return
-            End If
+                ' If they already have accessibility, no need to report anything.
+                Dim Accessibility = generator.GetAccessibility(member)
+                If Accessibility <> Accessibility.NotApplicable Then
+                    Return
+                End If
 
-            ' If they already have accessibility, no need to report anything.
-            Dim Accessibility = generator.GetAccessibility(member)
-            If Accessibility <> Accessibility.NotApplicable Then
-                Return
-            End If
+                ' Certain members never have accessibility. Don't bother reporting on them.
 
-            ' Certain members never have accessibility. Don't bother reporting on them.
-
-            If member.IsKind(SyntaxKind.ConstructorBlock) AndAlso
+                If member.IsKind(SyntaxKind.ConstructorBlock) AndAlso
                member.GetModifiers().Any(SyntaxKind.SharedKeyword) Then
 
-                Return
-            End If
+                    Return
+                End If
 
-            ' Missing accessibility.  Report issue to user.
-            Dim additionalLocations = ImmutableArray.Create(member.GetLocation())
-            context.ReportDiagnostic(Diagnostic.Create(
+                ' Missing accessibility.  Report issue to user.
+                Dim additionalLocations = ImmutableArray.Create(member.GetLocation())
+                context.ReportDiagnostic(Diagnostic.Create(
                 CreateDescriptorWithSeverity([option].Notification.Value),
                 name.GetLocation(),
                 additionalLocations:=additionalLocations))
-        End Sub
-    End Class
+            End Sub
+        End Class
 End Namespace
