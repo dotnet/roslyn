6d2d9106204edad04c97dc85aa63fc4f085bea0d
CyrusNajmabadi <cyrusn@microsoft.com>
2017-07-17 14:08:52 -0700

    Restore formatting.

--- src/Compilers/CSharp/Portable/Symbols/Symbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Symbol.cs
@@ -8,13 +8,11 @@ using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
-using Microsoft.CodeAnalysis.Collections;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
-using LanguageParser = Microsoft.CodeAnalysis.CSharp.Syntax.InternalSyntax.LanguageParser;
 
 namespace Microsoft.CodeAnalysis.CSharp
 {
