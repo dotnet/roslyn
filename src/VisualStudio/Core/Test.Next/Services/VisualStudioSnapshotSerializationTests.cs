4a162e6f788d393ac827c824fd3517efbaa41591
Kevin Pilch <me@pilchie.com>
2017-07-18 16:04:57 -0700

    Test full serialization, not just building checksums

--- src/VisualStudio/Core/Test.Next/Services/VisualStudioSnapshotSerializationTests.cs
+++ src/VisualStudio/Core/Test.Next/Services/VisualStudioSnapshotSerializationTests.cs
@@ -1,12 +1,15 @@
-﻿using System.Threading.Tasks;
-using System.Threading;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Execution;
+using Microsoft.CodeAnalysis.Serialization;
 using Microsoft.CodeAnalysis.UnitTests;
 using Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem;
 using Microsoft.VisualStudio.Shell.Interop;
 using Moq;
 using Roslyn.Test.Utilities;
+using Roslyn.Utilities;
 using Xunit;
 
 namespace Roslyn.VisualStudio.Next.UnitTests.Services
@@ -27,14 +30,37 @@ namespace Roslyn.VisualStudio.Next.UnitTests.Services
                 workspace: workspace,
                 loader: null,
                 language: project.Language);
+            var analyzerReference = analyzer.GetReference();
             project = project.WithAnalyzerReferences(new AnalyzerReference[]
             {
-                analyzer.GetReference(),
+                analyzerReference,
             });
 
             var checksum = await project.State.GetChecksumAsync(CancellationToken.None).ConfigureAwait(false);
-
             Assert.NotNull(checksum);
+
+            var assetBuilder = new CustomAssetBuilder(workspace);
+            var serializer = new Serializer(workspace);
+
+            var asset = assetBuilder.Build(analyzerReference, CancellationToken.None);
+
+            using (var stream = SerializableBytes.CreateWritableStream())
+            using (var writer = new ObjectWriter(stream))
+            {
+                await asset.WriteObjectToAsync(writer, CancellationToken.None).ConfigureAwait(false);
+
+                stream.Position = 0;
+                using (var reader = ObjectReader.TryGetReader(stream))
+                {
+                    var recovered = serializer.Deserialize<AnalyzerReference>(asset.Kind, reader, CancellationToken.None);
+                    var assetFromStorage = assetBuilder.Build(recovered, CancellationToken.None);
+
+                    Assert.Equal(asset.Checksum, assetFromStorage.Checksum);
+
+                    // This won't round trip, but we should get an UnresolvedAnalyzerReference, with the same path
+                    Assert.Equal(analyzerReference.FullPath, recovered.FullPath);
+                }
+            }
         }
     }
 }
