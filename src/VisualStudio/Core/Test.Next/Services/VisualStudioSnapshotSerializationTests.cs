c65ce85ea66cd0ccc68323d22010478b3d2ae4f8
Kevin Pilch <me@pilchie.com>
2017-07-18 17:30:27 -0700

    Dispose of VisualStudioAnalyzer

--- src/VisualStudio/Core/Test.Next/Services/VisualStudioSnapshotSerializationTests.cs
+++ src/VisualStudio/Core/Test.Next/Services/VisualStudioSnapshotSerializationTests.cs
@@ -22,43 +22,45 @@ namespace Roslyn.VisualStudio.Next.UnitTests.Services
             var workspace = new AdhocWorkspace();
             var project = workspace.CurrentSolution.AddProject("empty", "empty", LanguageNames.CSharp);
             var mockFileChangeService = new Mock<IVsFileChangeEx>();
-            var analyzer = new VisualStudioAnalyzer(
+            using (var analyzer = new VisualStudioAnalyzer(
                 @"PathToAnalyzer",
                 fileChangeService: mockFileChangeService.Object,
                 hostDiagnosticUpdateSource: null,
                 projectId: project.Id,
                 workspace: workspace,
                 loader: null,
-                language: project.Language);
-            var analyzerReference = analyzer.GetReference();
-            project = project.WithAnalyzerReferences(new AnalyzerReference[]
+                language: project.Language))
             {
-                analyzerReference,
-            });
-
-            var checksum = await project.State.GetChecksumAsync(CancellationToken.None).ConfigureAwait(false);
-            Assert.NotNull(checksum);
-
-            var assetBuilder = new CustomAssetBuilder(workspace);
-            var serializer = new Serializer(workspace);
-
-            var asset = assetBuilder.Build(analyzerReference, CancellationToken.None);
-
-            using (var stream = SerializableBytes.CreateWritableStream())
-            using (var writer = new ObjectWriter(stream))
-            {
-                await asset.WriteObjectToAsync(writer, CancellationToken.None).ConfigureAwait(false);
-
-                stream.Position = 0;
-                using (var reader = ObjectReader.TryGetReader(stream))
+                var analyzerReference = analyzer.GetReference();
+                project = project.WithAnalyzerReferences(new AnalyzerReference[]
                 {
-                    var recovered = serializer.Deserialize<AnalyzerReference>(asset.Kind, reader, CancellationToken.None);
-                    var assetFromStorage = assetBuilder.Build(recovered, CancellationToken.None);
+                analyzerReference,
+                });
 
-                    Assert.Equal(asset.Checksum, assetFromStorage.Checksum);
+                var checksum = await project.State.GetChecksumAsync(CancellationToken.None).ConfigureAwait(false);
+                Assert.NotNull(checksum);
 
-                    // This won't round trip, but we should get an UnresolvedAnalyzerReference, with the same path
-                    Assert.Equal(analyzerReference.FullPath, recovered.FullPath);
+                var assetBuilder = new CustomAssetBuilder(workspace);
+                var serializer = new Serializer(workspace);
+
+                var asset = assetBuilder.Build(analyzerReference, CancellationToken.None);
+
+                using (var stream = SerializableBytes.CreateWritableStream())
+                using (var writer = new ObjectWriter(stream))
+                {
+                    await asset.WriteObjectToAsync(writer, CancellationToken.None).ConfigureAwait(false);
+
+                    stream.Position = 0;
+                    using (var reader = ObjectReader.TryGetReader(stream))
+                    {
+                        var recovered = serializer.Deserialize<AnalyzerReference>(asset.Kind, reader, CancellationToken.None);
+                        var assetFromStorage = assetBuilder.Build(recovered, CancellationToken.None);
+
+                        Assert.Equal(asset.Checksum, assetFromStorage.Checksum);
+
+                        // This won't round trip, but we should get an UnresolvedAnalyzerReference, with the same path
+                        Assert.Equal(analyzerReference.FullPath, recovered.FullPath);
+                    }
                 }
             }
         }
