6d27d2bc0875277d78b4f71f8f4807a8315d682b
Fredric Silberberg <frsilb@microsoft.com>
2017-07-18 17:07:53 -0700

    Added missing copyrights to test files

--- src/Compilers/VisualBasic/Test/Semantic/IOperation/IOperationTests_IDynamicMemberReferenceExpression.vb
+++ src/Compilers/VisualBasic/Test/Semantic/IOperation/IOperationTests_IDynamicMemberReferenceExpression.vb
@@ -1,4 +1,6 @@
-﻿Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.UnitTests.Semantics
 
