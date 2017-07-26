cfb19ce4616c5b573ab82ded92b3a8add3f0b81a
Jared Parsons <jaredpparsons@gmail.com>
2017-07-26 16:29:13 -0700

    Enforce minimum MSBuild version
    The Roslyn build depends on having a minimum of MSBuild 15.1 in order to
    function. Anything less than that will fail with very crpytic errors,
    usually around NuGet runtime identifiers.
    
    This change causes our build to declaratively fail with a very specific
    message if the wrong version of MSBuild is used. It should help point
    customers in the right direction to fixing up the problem.
    
    Note: This was made to be parameterized in Imports.targets because very
    soon it will be changing to depend on 15.3.

---
+++ src/Tools/BuildUtil/BuildUtil/ValidateBuildEnvironment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Roslyn.MSBuild.Util
+{
+    public sealed class ValidateBuildEnvironment : Task
+    {
+        [Required]
+        public string MSBuildBinPath { get; set; }
+
+        /// <summary>
+        /// The minimum file version for MSBuild.exe. The task will fail if the MSBuild version exceeds 
+        /// this value.
+        /// 
+        /// Use the following page to map releases to file versions 
+        ///
+        /// https://github.com/Microsoft/msbuild/releases
+        /// </summary>
+        [Required]
+        public string MSBuildMinimimFileVersion { get; set; }
+
+        /// <summary>
+        /// Friendly developer facing version of the MSBuild message. 
+        /// </summary>
+        [Required]
+        public string MSBuildMinimimDisplayVersion { get; set; }
+
+        public override bool Execute()
+        {
+            var allGood = true;
+
+            if (MSBuildMinimimFileVersion != null)
+            {
+                var msbuildFilePath = Path.Combine(MSBuildBinPath, "msbuild.exe");
+                var fileVersionInfo = FileVersionInfo.GetVersionInfo(msbuildFilePath);
+                var fileVersion = new Version(fileVersionInfo.FileVersion);
+                var minimumVersion = new Version(MSBuildMinimimFileVersion);
+                if (fileVersion < minimumVersion)
+                {
+                    Log.LogError($"MSBuild version {fileVersion} is less than the required minimum version {minimumVersion} ({MSBuildMinimimDisplayVersion})");
+                    allGood = false;
+                }
+            }
+
+            return allGood;
+        }
+    }
+}
