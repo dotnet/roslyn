40528e4bd2cea1103848056ccf2de9b0db7bc5e7
Heejae Chang <hechang@microsoft.com>
2017-07-27 16:04:43 -0700

    given parent can be null if it is root of the operation tree

--- src/Compilers/Core/Portable/Operations/Operation.cs
+++ src/Compilers/Core/Portable/Operations/Operation.cs
@@ -80,8 +80,8 @@ namespace Microsoft.CodeAnalysis
         {
             var result = Interlocked.CompareExchange(ref _parentDoNotAccessDirectly, parent, null);
 
-            // tree must belong to same semantic model
-            Debug.Assert(((Operation)parent).SemanticModel == SemanticModel);
+            // tree must belong to same semantic model if parent is given
+            Debug.Assert(parent == null || ((Operation)parent).SemanticModel == SemanticModel);
 
             // make sure given parent and one we already have is same if we have one already
             Debug.Assert(result == null || result == parent);
