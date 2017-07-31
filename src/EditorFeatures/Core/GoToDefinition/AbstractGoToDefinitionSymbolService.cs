24074bdc59d85bb72c380c137181c2d9e8c47556
Ravi Chande <rchande@microsoft.com>
2017-07-31 14:32:13 -0700

    Clean up unused usings

--- src/EditorFeatures/Core/GoToDefinition/AbstractGoToDefinitionSymbolService.cs
+++ src/EditorFeatures/Core/GoToDefinition/AbstractGoToDefinitionSymbolService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.FindSymbols;
-using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Editor.GoToDefinition
