c9831d7de38fa589788b08d188abd19637c21ba6
Ravi Chande <rchande@microsoft.com>
2017-08-01 08:28:12 -0700

    Test both spans

--- src/EditorFeatures/Test2/NavigableSymbols/NavigableSymbolsTest.vb
+++ src/EditorFeatures/Test2/NavigableSymbols/NavigableSymbolsTest.vb
@@ -2,9 +2,11 @@
 
 Imports System
 Imports System.Linq
+Imports System.Collections.Immutable
 Imports System.Threading
 Imports System.Threading.Tasks
 Imports Microsoft.CodeAnalysis.Text
+Imports Microsoft.CodeAnalysis.Text.Shared.Extensions
 Imports Microsoft.CodeAnalysis.Editor.Host
 Imports Microsoft.CodeAnalysis.Editor.NavigableSymbols
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.BraceMatching
@@ -29,37 +31,37 @@ Namespace Microsoft.CodeAnalysis.Editor.UnitTests.NavigableSymbols
         <WpfFact>
         Public Async Function TestCharp() As Task
             Dim markup = "
-class [|C|]
+class {|target:C|}
 {
-    C$$ c
+    {|highlighted:C|}$$ c
 }"
             Dim text As String = Nothing
-            Dim position As Integer
-            Dim span As TextSpan
-            MarkupTestFile.GetPositionAndSpan(markup, text, position, span)
+            Dim position As Integer? = Nothing
+            Dim spans As IDictionary(Of String, ImmutableArray(Of TextSpan)) = Nothing
+            MarkupTestFile.GetPositionAndSpans(markup, text, position, spans)
 
             Using workspace = TestWorkspace.CreateCSharp(text, exportProvider:=s_exportProvider)
-                Await TestNavigated(workspace, position, span)
+                Await TestNavigated(workspace, position.Value, spans)
             End Using
         End Function
 
         <WpfFact>
         Public Async Function TestVB() As Task
             Dim markup = "
-Class [|C|]
-    Dim c as C$$
+Class {|target:C|}
+    Dim c as {|highlighted:C|}$$
 End Class"
             Dim text As String = Nothing
-            Dim position As Integer
-            Dim span As TextSpan
-            MarkupTestFile.GetPositionAndSpan(markup, text, position, span)
+            Dim position As Integer? = Nothing
+            Dim spans As IDictionary(Of String, ImmutableArray(Of TextSpan)) = Nothing
+            MarkupTestFile.GetPositionAndSpans(markup, text, position, spans)
 
             Using workspace = TestWorkspace.CreateVisualBasic(text, exportProvider:=s_exportProvider)
-                Await TestNavigated(workspace, position, span)
+                Await TestNavigated(workspace, position.Value, spans)
             End Using
         End Function
 
-        Private Async Function TestNavigated(workspace As TestWorkspace, position As Integer, span As TextSpan) As Task
+        Private Async Function TestNavigated(workspace As TestWorkspace, position As Integer, spans As IDictionary(Of String, ImmutableArray(Of TextSpan))) As Task
             Dim presenter = {New Lazy(Of IStreamingFindUsagesPresenter)(Function() New MockStreamingFindUsagesPresenter(Sub() Return))}
             Dim service = New NavigableSymbolService(TestWaitIndicator.Default, presenter)
             Dim view = workspace.Documents.First().GetTextView()
@@ -68,14 +70,19 @@ End Class"
             Dim source = service.TryCreateNavigableSymbolSource(view, buffer)
             Dim symbol = Await source.GetNavigableSymbolAsync(triggerSpan, CancellationToken.None)
 
+            Dim highlightedSpan = spans("highlighted").First()
+            Dim navigationTarget = spans("target").First()
+
             Assert.NotNull(symbol)
+            Assert.Equal(highlightedSpan.ToSpan(), symbol.SymbolSpan.Span)
+
             symbol.Navigate(symbol.Relationships.First())
 
             Dim navigationService = DirectCast(workspace.Services.GetService(Of IDocumentNavigationService)(), MockDocumentNavigationServiceProvider.MockDocumentNavigationService)
             Assert.Equal(True, navigationService.TryNavigateToLineAndOffsetReturnValue)
             Assert.Equal(True, navigationService.TryNavigateToPositionReturnValue)
             Assert.Equal(True, navigationService.TryNavigateToSpanReturnValue)
-            Assert.Equal(span, navigationService.ProvidedTextSpan)
+            Assert.Equal(navigationTarget, navigationService.ProvidedTextSpan)
         End Function
     End Class
 End Namespace
