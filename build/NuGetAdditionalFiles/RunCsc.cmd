d72bcf54aa369ba78f39481e6f077ee812f46a65
Ashley Hauck <ashauck@microsoft.com>
2017-08-02 16:05:29 +0000

    Clean up RunCsc/Vbc.cmd a tiny bit

--- build/NuGetAdditionalFiles/RunCsc.cmd
+++ build/NuGetAdditionalFiles/RunCsc.cmd
@@ -1,4 +1,7 @@
-echo off
-if defined DOTNET_HOST_PATH set HOST_PATH=%DOTNET_HOST_PATH%\
-if not defined HOST_PATH set HOST_PATH=
+@echo off
+if defined DOTNET_HOST_PATH (
+    set HOST_PATH=%DOTNET_HOST_PATH%\
+) else (
+    set HOST_PATH=
+)
 %HOST_PATH%dotnet %~dp0\csc.dll %*
