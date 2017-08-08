0bfd1e285995eb5ae0d52e41ac488d23632857a8
Victor Zaytsev <allisq3@gmail.com>
2017-08-08 20:10:26 +0300

    Fixed #5258

--- src/Features/Core/Portable/IntroduceVariable/AbstractIntroduceVariableService.AbstractIntroduceVariableCodeAction.cs
+++ src/Features/Core/Portable/IntroduceVariable/AbstractIntroduceVariableService.AbstractIntroduceVariableCodeAction.cs
@@ -75,13 +75,6 @@ namespace Microsoft.CodeAnalysis.IntroduceVariable
                 var singleLineExpression = _document.Project.LanguageServices.GetService<ISyntaxFactsService>().ConvertToSingleLine(expression);
                 var nodeString = singleLineExpression.ToString();
 
-                // prevent the display string from being too long
-                const int MaxLength = 40;
-                if (nodeString.Length > MaxLength)
-                {
-                    nodeString = nodeString.Substring(0, MaxLength) + "...";
-                }
-
                 return CreateDisplayText(nodeString);
             }
 
