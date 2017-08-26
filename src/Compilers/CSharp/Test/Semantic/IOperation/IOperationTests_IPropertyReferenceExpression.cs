03c741ef7bd302b8d549d9b526146aa1521277b8
Fredric Silberberg <frsilb@microsoft.com>
2017-08-25 17:01:37 -0700

    Added test for derived property type initializers.

---
+++ src/Compilers/CSharp/Test/Semantic/IOperation/IOperationTests_IPropertyReferenceExpression.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests
+{
+    public partial class IOperationTests : SemanticModelTestBase
+    {
+        [CompilerTrait(CompilerFeature.IOperation)]
+        [Fact]
+        public void IPropertyReferenceExpression_PropertyReferenceInDerivedTypeUsesDerivedTypeAsInstanceType()
+        {
+            string source = @"
+class C
+{
+    void M1()
+    {
+        C2 c2 = new C2() { /*<bind>*/P1 = 1/*</bind>*/ };
+    }
+}
+
+class C1
+{
+    public virtual int P1 { get; set; }
+}
+
+class C2 : C1
+{
+}
+";
+            string expectedOperationTree = @"
+IPropertyReferenceExpression: System.Int32 C1.P1 { get; } (OperationKind.PropertyReferenceExpression, Type: System.Int32) (Syntax: 'c2.P1')
+  Instance Receiver: ILocalReferenceExpression: c2 (OperationKind.LocalReferenceExpression, Type: C2) (Syntax: 'c2')
+";
+            var expectedDiagnostics = DiagnosticDescription.None;
+
+            VerifyOperationTreeAndDiagnosticsForTest<AssignmentExpressionSyntax>(source, expectedOperationTree, expectedDiagnostics);
+        }
+
+    }
+}
