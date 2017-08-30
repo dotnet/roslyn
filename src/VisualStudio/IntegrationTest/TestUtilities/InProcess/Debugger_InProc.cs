4f1bec1be6a7a8e182e11284162225255c59e599
Ivan Basov <basov@hotmail.com>
2017-08-30 14:34:20 -0700

    vb integration tests: workaroung for operation not supported exception

--- src/VisualStudio/IntegrationTest/TestUtilities/InProcess/Debugger_InProc.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/InProcess/Debugger_InProc.cs
@@ -1,11 +1,34 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
 using EnvDTE;
 
 namespace Microsoft.VisualStudio.IntegrationTest.Utilities.InProcess
 {
     internal class Debugger_InProc : InProcComponent
     {
+        /// <summary>
+        /// HResult for "Operation Not Supported" when raising commands. 
+        /// </summary>
+        private const uint OperationNotSupportedHResult = 0x8971003c;
+
+        /// <summary>
+        /// Time to wait between retries if "Operation Not Supported" is thrown when raising a debugger stepping command.
+        /// </summary>
+        private static readonly TimeSpan DebuggerCommandRetryTimeout = TimeSpan.FromSeconds(10);
+
+        /// <summary>
+        /// Time to wait before re-polling a delegate.
+        /// </summary>
+        private static readonly TimeSpan DefaultPollingInterCallSleep = TimeSpan.FromMilliseconds(250);
+
+        /// <summary>
+        /// The value which is used to increase all time-outs by a constant factor. By default, this value is 1.0.
+        /// </summary>
+        private const float TimeoutMultiplier = 1.0f;
+
         private readonly Debugger _debugger;
 
         private Debugger_InProc()
@@ -24,7 +47,7 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.InProcess
 
         public void Go(bool waitForBreakMode) => _debugger.Go(waitForBreakMode);
 
-        public void StepOver(bool waitForBreakOrEnd) => _debugger.StepOver(waitForBreakOrEnd);
+        public void StepOver(bool waitForBreakOrEnd) => this.WaitForRaiseDebuggerDteCommand(() => _debugger.StepOver(waitForBreakOrEnd));
 
         public void Stop(bool waitForDesignMode) => _debugger.Stop(WaitForDesignMode: waitForDesignMode);
 
@@ -33,5 +56,105 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.InProcess
         public void ExecuteStatement(string statement) => _debugger.ExecuteStatement(statement);
 
         public Common.Expression GetExpression(string expressionText) => new Common.Expression(_debugger.GetExpression(expressionText));
+
+        /// <summary>
+        /// Executes the specified action delegate and retries if Operation Not Supported is thrown.
+        /// </summary>
+        /// <param name="action">Action delegate to exectute.</param>
+        private void WaitForRaiseDebuggerDteCommand(Action action)
+        {
+            bool actionSucceeded = false;
+
+            Func<bool> predict = delegate
+            {
+                try
+                {
+                    action();
+                    actionSucceeded = true;
+                }
+                catch (COMException ex)
+                {
+                    if ((uint)ex.ErrorCode != OperationNotSupportedHResult)
+                    {
+                        string message = string.Format(
+                            CultureInfo.InvariantCulture,
+                            "Failed to raise debugger command, an unexpected '{0}' was thrown with the HResult of '{1}'.",
+                            typeof(COMException),
+                            ex.ErrorCode);
+
+                        throw new Exception(message, ex);
+                    }
+
+                    actionSucceeded = false;
+                }
+
+                return actionSucceeded;
+            };
+
+            // Repeat the the command if "Operation Not Supported" is thrown.
+            if (!this.TryWaitFor(DebuggerCommandRetryTimeout, predict))
+            {
+                string message = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "Failed to raise debugger command within '{0}' seconds.",
+                    DebuggerCommandRetryTimeout.TotalSeconds);
+
+                throw new Exception(message);
+            }
+        }
+
+        /// <summary>
+        /// Polls for the specified delegate to return true for the given timeout.
+        /// </summary>
+        /// <param name="timeout">Timeout to keep polling.</param>
+        /// <param name="predicate">Delegate to invoke.</param>
+        /// <returns>True if the delegate returned true when polled, otherwise false.</returns>
+        public bool TryWaitFor(TimeSpan timeout, Func<bool> predicate)
+        {
+            return this.TryWaitFor(timeout, DefaultPollingInterCallSleep, predicate);
+        }
+
+        /// <summary>
+        /// Polls for the specified delegate to return true for the given timeout.
+        /// </summary>
+        /// <param name="timeout">Timeout to keep polling.</param>
+        /// <param name="interval">Time to wait between polling.</param>
+        /// <param name="predicate">Delegate to invoke.</param>
+        /// <returns>
+        /// True if the delegate returned true when polled, otherwise false.
+        /// </returns>
+        private bool TryWaitFor(TimeSpan timeout, TimeSpan interval, Func<bool> predicate)
+        {
+            DateTime endTime = DateTime.UtcNow + this.ApplyTimeoutMultiplier(timeout);
+            bool validationDelegateSuccess = false;
+
+            while (DateTime.UtcNow < endTime)
+            {
+                // Note: we don't do this inline in the while() condition and return the result of 
+                // (DateTime.Now < startTime + timeout) as this could lead to cases where the validation
+                // delegate returned true, at the boundary of the valid time, and would return false
+                // when hitting the return statement. 
+                if (predicate.Invoke())
+                {
+                    validationDelegateSuccess = true;
+                    break;
+                }
+
+                System.Threading.Thread.Sleep(interval);
+            }
+
+            return validationDelegateSuccess;
+        }
+
+
+        /// <summary>
+        /// Multiplies the given TimeSpan by the current global timeout multiplier.
+        /// </summary>
+        /// <param name="timeSpan">A timeout value to multiply</param>
+        /// <returns>The product of the given timespan and the global timeout multiplier</returns>
+        private TimeSpan ApplyTimeoutMultiplier(TimeSpan timeSpan)
+        {
+            return TimeSpan.FromMilliseconds(TimeoutMultiplier * timeSpan.TotalMilliseconds);
+        }
     }
 }
