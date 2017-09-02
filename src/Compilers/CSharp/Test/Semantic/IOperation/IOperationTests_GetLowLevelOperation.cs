c5430aa8c3ba94289da9f16dd41aa9e22ddf4586
Edgardo Zoppi <edgarzpp@gmail.com>
2017-09-01 19:39:27 -0700

    Adding some unit tests for GetLowLevelOperation method.

---
+++ src/Compilers/CSharp/Test/Semantic/IOperation/IOperationTests_GetLowLevelOperation.cs
@@ -0,0 +1,168 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Semantics;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests
+{
+    public partial class IOperationTests : SemanticModelTestBase
+    {
+        [CompilerTrait(CompilerFeature.IOperation)]
+        [Fact]
+        public void GetLowLevelOperation_FromMethod()
+        {
+            string source = @"
+class C
+{
+    /*<bind>*/
+    static int Method(int p)
+    {
+        return p;
+    }
+    /*</bind>*/
+}
+";
+            string expectedOperationTree = @"
+IBlockStatement (1 statements) (OperationKind.BlockStatement) (Syntax: '{ ... }')
+  IReturnStatement (OperationKind.ReturnStatement) (Syntax: 'return p;')
+    ReturnedValue: IParameterReferenceExpression: p (OperationKind.ParameterReferenceExpression, Type: System.Int32) (Syntax: 'p')
+";
+            var expectedDiagnostics = DiagnosticDescription.None;
+            VerifyOperationTreeAndDiagnosticsForTest<BaseMethodDeclarationSyntax>(source, expectedOperationTree, expectedDiagnostics, useLoweredTree: true);
+        }
+
+        [CompilerTrait(CompilerFeature.IOperation)]
+        [Fact]
+        public void GetLowLevelOperation_FromConstructor()
+        {
+            string source = @"
+class C
+{
+    private int _field = 0;
+
+    /*<bind>*/
+    public C(int p)
+    {
+        _field = p;
+    }
+    /*</bind>*/
+}
+";
+            string expectedOperationTree = @"
+IBlockStatement (2 statements) (OperationKind.BlockStatement) (Syntax: 'public C(in ... }')
+  IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: 'public C(in ... }')
+    Expression: IInvocationExpression ( System.Object..ctor()) (OperationKind.InvocationExpression, Type: System.Object) (Syntax: 'public C(in ... }')
+        Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: 'public C(in ... }')
+        Arguments(0)
+  IBlockStatement (1 statements) (OperationKind.BlockStatement) (Syntax: '{ ... }')
+    IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: '_field = p;')
+      Expression: ISimpleAssignmentExpression (OperationKind.SimpleAssignmentExpression, Type: System.Int32) (Syntax: '_field = p')
+          Left: IFieldReferenceExpression: System.Int32 C._field (OperationKind.FieldReferenceExpression, Type: System.Int32) (Syntax: '_field')
+              Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '_field')
+          Right: IParameterReferenceExpression: p (OperationKind.ParameterReferenceExpression, Type: System.Int32) (Syntax: 'p')
+";
+            var expectedDiagnostics = DiagnosticDescription.None;
+            VerifyOperationTreeAndDiagnosticsForTest<BaseMethodDeclarationSyntax>(source, expectedOperationTree, expectedDiagnostics, useLoweredTree: true);
+        }
+
+        [CompilerTrait(CompilerFeature.IOperation)]
+        [Fact]
+        public void GetLowLevelOperation_FromFinalizer()
+        {
+            string source = @"
+class C
+{
+    private int _field = 0;
+
+    /*<bind>*/
+    ~C()
+    {
+        _field += 0;
+    }
+    /*</bind>*/
+}
+";
+            string expectedOperationTree = @"
+IBlockStatement (1 statements) (OperationKind.BlockStatement) (Syntax: '{ ... }')
+  ITryStatement (OperationKind.TryStatement) (Syntax: '{ ... }')
+    Body: IBlockStatement (1 statements) (OperationKind.BlockStatement) (Syntax: '{ ... }')
+        IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: '_field += 0;')
+          Expression: ISimpleAssignmentExpression (OperationKind.SimpleAssignmentExpression, Type: System.Int32) (Syntax: '_field += 0')
+              Left: IFieldReferenceExpression: System.Int32 C._field (OperationKind.FieldReferenceExpression, Type: System.Int32) (Syntax: '_field')
+                  Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '_field')
+              Right: IFieldReferenceExpression: System.Int32 C._field (OperationKind.FieldReferenceExpression, Type: System.Int32) (Syntax: '_field')
+                  Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '_field')
+    Catch clauses(0)
+    Finally: IBlockStatement (1 statements) (OperationKind.BlockStatement) (Syntax: '{ ... }')
+        IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: '{ ... }')
+          Expression: IInvocationExpression ( void System.Object.Finalize()) (OperationKind.InvocationExpression, Type: System.Void) (Syntax: '{ ... }')
+              Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.BaseClass) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '{ ... }')
+              Arguments(0)
+";
+            var expectedDiagnostics = DiagnosticDescription.None;
+            VerifyOperationTreeAndDiagnosticsForTest<BaseMethodDeclarationSyntax>(source, expectedOperationTree, expectedDiagnostics, useLoweredTree: true);
+        }
+
+        [CompilerTrait(CompilerFeature.IOperation)]
+        [Fact]
+        public void GetLowLevelOperation_FromPropertyAccessorGet()
+        {
+            string source = @"
+class C
+{
+    private int _property = 0;
+
+    int Property
+    {
+        /*<bind>*/
+        get { return _property; }
+        /*</bind>*/
+    }
+}
+";
+            string expectedOperationTree = @"
+IBlockStatement (1 statements) (OperationKind.BlockStatement) (Syntax: '{ return _property; }')
+  IReturnStatement (OperationKind.ReturnStatement) (Syntax: 'return _property;')
+    ReturnedValue: IFieldReferenceExpression: System.Int32 C._property (OperationKind.FieldReferenceExpression, Type: System.Int32) (Syntax: '_property')
+        Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '_property')
+";
+            var expectedDiagnostics = DiagnosticDescription.None;
+            VerifyOperationTreeAndDiagnosticsForTest<AccessorDeclarationSyntax>(source, expectedOperationTree, expectedDiagnostics, useLoweredTree: true);
+        }
+
+        [CompilerTrait(CompilerFeature.IOperation)]
+        [Fact]
+        public void GetLowLevelOperation_FromPropertyAccessorSet()
+        {
+            string source = @"
+class C
+{
+    private int _property = 0;
+
+    int Property
+    {
+        get { return _property; }
+        /*<bind>*/
+        set { _property = value; }
+        /*</bind>*/
+    }
+}
+";
+            string expectedOperationTree = @"
+IBlockStatement (1 statements) (OperationKind.BlockStatement) (Syntax: '{ _property = value; }')
+  IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: '_property = value;')
+    Expression: ISimpleAssignmentExpression (OperationKind.SimpleAssignmentExpression, Type: System.Int32) (Syntax: '_property = value')
+        Left: IFieldReferenceExpression: System.Int32 C._property (OperationKind.FieldReferenceExpression, Type: System.Int32) (Syntax: '_property')
+            Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '_property')
+        Right: IParameterReferenceExpression: value (OperationKind.ParameterReferenceExpression, Type: System.Int32) (Syntax: 'value')
+
+";
+            var expectedDiagnostics = DiagnosticDescription.None;
+            VerifyOperationTreeAndDiagnosticsForTest<AccessorDeclarationSyntax>(source, expectedOperationTree, expectedDiagnostics, useLoweredTree: true);
+        }
+    }
+}
---
+++ src/Compilers/VisualBasic/Test/Semantic/IOperation/IOperationTests_GetLowLevelOperation.vb
@@ -0,0 +1,146 @@
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports Microsoft.CodeAnalysis.Test.Utilities
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+Imports Roslyn.Test.Utilities
+
+Namespace Microsoft.CodeAnalysis.VisualBasic.UnitTests.Semantics
+
+    Partial Public Class IOperationTests
+        Inherits SemanticModelTestBase
+
+        <CompilerTrait(CompilerFeature.IOperation)>
+        <Fact>
+        Public Sub GetLowLevelOperation_FromMethod()
+            Dim source = <![CDATA[
+Class C
+    Function Method(p As Integer) As Integer'BIND:"Function Method(p As Integer) As Integer"
+        Return p
+    End Function
+End Class
+]]>.Value
+
+            Dim expectedOperationTree = <![CDATA[
+IBlockStatement (3 statements, 1 locals) (OperationKind.BlockStatement) (Syntax: 'Function Me ... nd Function')
+  Locals: Local_1: Method As System.Int32
+  IBlockStatement (2 statements) (OperationKind.BlockStatement) (Syntax: 'Return p')
+    IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: 'Return p')
+      Expression: ISimpleAssignmentExpression (OperationKind.SimpleAssignmentExpression, Type: System.Int32) (Syntax: 'Return p')
+          Left: ILocalReferenceExpression: Method (OperationKind.LocalReferenceExpression, Type: System.Int32) (Syntax: 'Return p')
+          Right: IParameterReferenceExpression: p (OperationKind.ParameterReferenceExpression, Type: System.Int32) (Syntax: 'p')
+    IBranchStatement (BranchKind.GoTo, Label: exit) (OperationKind.BranchStatement) (Syntax: 'Return p')
+  ILabeledStatement (Label: exit) (OperationKind.LabeledStatement) (Syntax: 'End Function')
+    Statement: null
+  IReturnStatement (OperationKind.ReturnStatement) (Syntax: 'End Function')
+    ReturnedValue: ILocalReferenceExpression: Method (OperationKind.LocalReferenceExpression, Type: System.Int32) (Syntax: 'End Function')
+]]>.Value
+
+            Dim expectedDiagnostics = String.Empty
+
+            VerifyOperationTreeAndDiagnosticsForTest(Of MethodStatementSyntax)(source, expectedOperationTree, expectedDiagnostics, useLoweredTree:=True)
+        End Sub
+
+        <CompilerTrait(CompilerFeature.IOperation)>
+        <Fact>
+        Public Sub GetLowLevelOperation_FromConstructor()
+            Dim source = <![CDATA[
+Class C
+    Private _field As Integer = 0
+
+    Public Sub New(p As Integer)'BIND:"Public Sub New(p As Integer)"
+        _field = p
+    End Sub
+End Class
+]]>.Value
+
+            Dim expectedOperationTree = <![CDATA[
+IBlockStatement (3 statements) (OperationKind.BlockStatement) (Syntax: 'Public Sub  ... End Sub')
+  IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: '_field = p')
+    Expression: ISimpleAssignmentExpression (OperationKind.SimpleAssignmentExpression, Type: System.Int32) (Syntax: '_field = p')
+        Left: IFieldReferenceExpression: C._field As System.Int32 (OperationKind.FieldReferenceExpression, Type: System.Int32) (Syntax: '_field')
+            Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '_field')
+        Right: IParameterReferenceExpression: p (OperationKind.ParameterReferenceExpression, Type: System.Int32) (Syntax: 'p')
+  ILabeledStatement (Label: exit) (OperationKind.LabeledStatement) (Syntax: 'End Sub')
+    Statement: null
+  IReturnStatement (OperationKind.ReturnStatement) (Syntax: 'End Sub')
+    ReturnedValue: null
+]]>.Value
+
+            Dim expectedDiagnostics = String.Empty
+
+            VerifyOperationTreeAndDiagnosticsForTest(Of SubNewStatementSyntax)(source, expectedOperationTree, expectedDiagnostics, useLoweredTree:=True)
+        End Sub
+
+        <CompilerTrait(CompilerFeature.IOperation)>
+        <Fact>
+        Public Sub GetLowLevelOperation_FromPropertyAccessorGet()
+            Dim source = <![CDATA[
+Class C
+    Private _property As Integer = 0
+
+    Private ReadOnly Property MyProperty() As Integer
+        Get'BIND:"Get"
+            Return _property
+        End Get
+    End Property
+End Class
+]]>.Value
+
+            Dim expectedOperationTree = <![CDATA[
+IBlockStatement (3 statements, 1 locals) (OperationKind.BlockStatement) (Syntax: 'Get'BIND:"G ... End Get')
+  Locals: Local_1: MyProperty As System.Int32
+  IBlockStatement (2 statements) (OperationKind.BlockStatement) (Syntax: 'Return _property')
+    IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: 'Return _property')
+      Expression: ISimpleAssignmentExpression (OperationKind.SimpleAssignmentExpression, Type: System.Int32) (Syntax: 'Return _property')
+          Left: ILocalReferenceExpression: MyProperty (OperationKind.LocalReferenceExpression, Type: System.Int32) (Syntax: 'Return _property')
+          Right: IFieldReferenceExpression: C._property As System.Int32 (OperationKind.FieldReferenceExpression, Type: System.Int32) (Syntax: '_property')
+              Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '_property')
+    IBranchStatement (BranchKind.GoTo, Label: exit) (OperationKind.BranchStatement) (Syntax: 'Return _property')
+  ILabeledStatement (Label: exit) (OperationKind.LabeledStatement) (Syntax: 'End Get')
+    Statement: null
+  IReturnStatement (OperationKind.ReturnStatement) (Syntax: 'End Get')
+    ReturnedValue: ILocalReferenceExpression: MyProperty (OperationKind.LocalReferenceExpression, Type: System.Int32) (Syntax: 'End Get')
+]]>.Value
+
+            Dim expectedDiagnostics = String.Empty
+
+            VerifyOperationTreeAndDiagnosticsForTest(Of AccessorStatementSyntax)(source, expectedOperationTree, expectedDiagnostics, useLoweredTree:=True)
+        End Sub
+
+        <CompilerTrait(CompilerFeature.IOperation)>
+        <Fact>
+        Public Sub GetLowLevelOperation_FromPropertyAccessorSet()
+            Dim source = <![CDATA[
+Class C
+    Private _property As Integer = 0
+
+    Private Property MyProperty() As Integer
+        Get
+            Return _property
+        End Get
+        Set'BIND:"Set"
+            _property = value
+        End Set
+    End Property
+End Class
+]]>.Value
+
+            Dim expectedOperationTree = <![CDATA[
+IBlockStatement (3 statements) (OperationKind.BlockStatement) (Syntax: 'Set'BIND:"S ... End Set')
+  IExpressionStatement (OperationKind.ExpressionStatement) (Syntax: '_property = value')
+    Expression: ISimpleAssignmentExpression (OperationKind.SimpleAssignmentExpression, Type: System.Int32) (Syntax: '_property = value')
+        Left: IFieldReferenceExpression: C._property As System.Int32 (OperationKind.FieldReferenceExpression, Type: System.Int32) (Syntax: '_property')
+            Instance Receiver: IInstanceReferenceExpression (InstanceReferenceKind.Implicit) (OperationKind.InstanceReferenceExpression, Type: C) (Syntax: '_property')
+        Right: IParameterReferenceExpression: Value (OperationKind.ParameterReferenceExpression, Type: System.Int32) (Syntax: 'value')
+  ILabeledStatement (Label: exit) (OperationKind.LabeledStatement) (Syntax: 'End Set')
+    Statement: null
+  IReturnStatement (OperationKind.ReturnStatement) (Syntax: 'End Set')
+    ReturnedValue: null
+]]>.Value
+
+            Dim expectedDiagnostics = String.Empty
+
+            VerifyOperationTreeAndDiagnosticsForTest(Of AccessorStatementSyntax)(source, expectedOperationTree, expectedDiagnostics, useLoweredTree:=True)
+        End Sub
+    End Class
+End Namespace
