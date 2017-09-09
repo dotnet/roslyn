7018bb44ce50548fad286bea8d9955f0f13ae3bf
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-09-09 16:25:01 -0700

    Continued work.

--- src/Features/Core/Portable/UseNamedArguments/AbstractUseNamedArgumentsCodeRefactoringProvider.cs
+++ src/Features/Core/Portable/UseNamedArguments/AbstractUseNamedArgumentsCodeRefactoringProvider.cs
@@ -7,17 +7,28 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CodeStyle;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Text;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.UseNamedArguments
 {
-    internal abstract class AbstractUseNamedArgumentsCodeRefactoringProvider : CodeRefactoringProvider
+    internal abstract class AbstractUseNamedArgumentsDiagnosticAnalyzer<TSyntaxKind>
+        : AbstractCodeStyleDiagnosticAnalyzer
+        where TSyntaxKind : struct
     {
+        public const string IncludeTrailingArgumentsKey = nameof(IncludeTrailingArgumentsKey);
+        public const string ArgumentNameKey = nameof(ArgumentNameKey);
+
         protected interface IAnalyzer
         {
-            Task ComputeRefactoringsAsync(
-                CodeRefactoringContext context, SyntaxNode root, CancellationToken cancellationToken);
+            void AnalyzeArgument(AbstractUseNamedArgumentsDiagnosticAnalyzer<TSyntaxKind> analyzer, SyntaxNodeAnalysisContext context);
         }
 
         protected abstract class Analyzer<TBaseArgumentSyntax, TSimpleArgumentSyntax, TArgumentListSyntax> : IAnalyzer
@@ -25,47 +36,16 @@ namespace Microsoft.CodeAnalysis.UseNamedArguments
             where TSimpleArgumentSyntax : TBaseArgumentSyntax
             where TArgumentListSyntax : SyntaxNode
         {
-            public async Task ComputeRefactoringsAsync(
-                CodeRefactoringContext context, SyntaxNode root, CancellationToken cancellationToken)
+            public void AnalyzeArgument(AbstractUseNamedArgumentsDiagnosticAnalyzer<TSyntaxKind> analyzer, SyntaxNodeAnalysisContext context)
             {
-                if (context.Span.Length > 0)
-                {
-                    return;
-                }
-
-                var position = context.Span.Start;
-                var token = root.FindToken(position);
-                if (token.Span.Start == position &&
-                    IsCloseParenOrComma(token))
-                {
-                    token = token.GetPreviousToken();
-                    if (token.Span.End != position)
-                    {
-                        return;
-                    }
-                }
-
-                var argument = root.FindNode(token.Span).FirstAncestorOrSelfUntil<TBaseArgumentSyntax>(node => node is TArgumentListSyntax) as TSimpleArgumentSyntax;
+                var cancellationToken = context.CancellationToken;
+                var argument = context.Node as TSimpleArgumentSyntax;
                 if (argument == null)
                 {
                     return;
                 }
 
                 if (!IsPositionalArgument(argument))
-                {
-                    return;
-                }
-
-                // Arguments can be arbitrarily large.  Only offer this feature if the caret is on hte
-                // line that the argument starts on.
-
-                var document = context.Document;
-                var sourceText = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
-
-                var argumentStartLine = sourceText.Lines.GetLineFromPosition(argument.Span.Start).LineNumber;
-                var caretLine = sourceText.Lines.GetLineFromPosition(position).LineNumber;
-
-                if (argumentStartLine != caretLine)
                 {
                     return;
                 }
@@ -81,7 +61,7 @@ namespace Microsoft.CodeAnalysis.UseNamedArguments
                     return;
                 }
 
-                var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+                var semanticModel = context.SemanticModel;
 
                 var symbol = semanticModel.GetSymbolInfo(receiver, cancellationToken).Symbol;
                 if (symbol == null)
@@ -122,27 +102,83 @@ namespace Microsoft.CodeAnalysis.UseNamedArguments
                     }
                 }
 
-                var argumentName = parameters[argumentIndex].Name;
+                var syntaxTree = semanticModel.SyntaxTree;
+                var optionSet = context.Options.GetDocumentOptionSetAsync(syntaxTree, cancellationToken).GetAwaiter().GetResult();
+                if (optionSet == null)
+                {
+                    return;
+                }
 
-                if (this.SupportsNonTrailingNamedArguments(root.SyntaxTree.Options) &&
+                var option = optionSet.GetOption(CodeStyleOptions.RequireNamedArguments, semanticModel.Language);
+
+                var severity = GetSeverity();
+
+                var argumentName = parameters[argumentIndex].Name;
+                var diagnosticLocation = GetDiagnosticLocation();
+                var additionalLocations = ImmutableArray.Create(argument.GetLocation());
+
+                var properties = ImmutableDictionary<string, string>.Empty.Add(ArgumentNameKey, argumentName);
+
+                if (this.SupportsNonTrailingNamedArguments(syntaxTree.Options) &&
                     argumentIndex < argumentCount - 1)
                 {
-                    context.RegisterRefactoring(
-                        new MyCodeAction(
-                            string.Format(FeaturesResources.Add_argument_name_0, argumentName),
-                            c => AddNamedArgumentsAsync(root, document, argument, parameters, argumentIndex, includingTrailingArguments: false)));
-
-                    context.RegisterRefactoring(
-                        new MyCodeAction(
-                            string.Format(FeaturesResources.Add_argument_name_0_including_trailing_arguments, argumentName),
-                            c => AddNamedArgumentsAsync(root, document, argument, parameters, argumentIndex, includingTrailingArguments: true)));
+                    properties = properties.Add(IncludeTrailingArgumentsKey, "");
                 }
-                else
+
+                context.ReportDiagnostic(
+                    Diagnostic.Create(analyzer.CreateDescriptorWithSeverity(severity), diagnosticLocation, additionalLocations, properties));
+
+                return;
+
+                // Local functions
+
+                DiagnosticSeverity GetSeverity()
                 {
-                    context.RegisterRefactoring(
-                        new MyCodeAction(
-                            string.Format(FeaturesResources.Add_argument_name_0, argumentName),
-                            c => AddNamedArgumentsAsync(root, document, argument, parameters, argumentIndex, includingTrailingArguments: true)));
+                    switch (option.Value)
+                    {
+                        default:
+                        case NamedArgumentsRequired.Never: return DiagnosticSeverity.Hidden;
+                        case NamedArgumentsRequired.Always: return option.Notification.Value;
+                        case NamedArgumentsRequired.ForLiterals:
+                            var syntaxFacts = GetSyntaxFactsService();
+                            var isLiteral = syntaxFacts.IsLiteralExpression(syntaxFacts.GetExpressionOfArgument(argument));
+                            return isLiteral ? option.Notification.Value : DiagnosticSeverity.Hidden;
+                    }
+                }
+
+                Location GetDiagnosticLocation()
+                {
+                    if (severity != DiagnosticSeverity.Hidden)
+                    {
+                        // We're showing some sort of UI affordance (i.e a squiggle or tickler),
+                        // just place it on the first token of hte argument so that the span is
+                        // not too large
+                        return argument.GetFirstToken().GetLocation();
+                    }
+
+                    // We're showing a hidden diagnostic.  This is trickier.  We want to make 
+                    // the fix available anywhere on the argument as long as:
+                    // 
+                    // a) The location is on the same line as the argument.
+                    // b) The location is before any other argument.  i.e. we don't want to have
+                    //      Goo(Bar(1)) and offer to add named args for both the inner and outer
+                    //      arguments when on the '1'.
+                    var descendentArgumentList = argument.DescendantNodes().OfType<TArgumentListSyntax>().FirstOrDefault();
+                    var span = descendentArgumentList == null
+                        ? argument.Span
+                        : TextSpan.FromBounds(argument.SpanStart, descendentArgumentList.SpanStart);
+
+                    var sourceText = syntaxTree.GetText(cancellationToken);
+                    var startLine = sourceText.Lines.GetLineFromPosition(span.Start);
+                    var endLine = sourceText.Lines.GetLineFromPosition(span.End);
+
+                    if (startLine.LineNumber == endLine.LineNumber)
+                    {
+                        return syntaxTree.GetLocation(span);
+                    }
+
+                    // Span cross multiple lines.  Cut it off at the end of the first line.
+                    return syntaxTree.GetLocation(TextSpan.FromBounds(span.Start, startLine.End));
                 }
             }
 
@@ -188,7 +224,7 @@ namespace Microsoft.CodeAnalysis.UseNamedArguments
             protected abstract TArgumentListSyntax WithArguments(
                 TArgumentListSyntax argumentList, IEnumerable<TBaseArgumentSyntax> namedArguments, IEnumerable<SyntaxToken> separators);
 
-            protected abstract bool IsCloseParenOrComma(SyntaxToken token);
+            protected abstract ISyntaxFactsService GetSyntaxFactsService();
             protected abstract bool IsLegalToAddNamedArguments(ImmutableArray<IParameterSymbol> parameters, int argumentCount);
             protected abstract TSimpleArgumentSyntax WithName(TSimpleArgumentSyntax argument, string name);
             protected abstract bool IsPositionalArgument(TSimpleArgumentSyntax argument);
@@ -200,39 +236,105 @@ namespace Microsoft.CodeAnalysis.UseNamedArguments
         private readonly IAnalyzer _argumentAnalyzer;
         private readonly IAnalyzer _attributeArgumentAnalyzer;
 
-        protected AbstractUseNamedArgumentsCodeRefactoringProvider(
+        protected AbstractUseNamedArgumentsDiagnosticAnalyzer(
             IAnalyzer argumentAnalyzer,
             IAnalyzer attributeArgumentAnalyzer)
+            : base(IDEDiagnosticIds.UseNamedArgumentId,
+                   new LocalizableResourceString(nameof(FeaturesResources.Prefer_explicitly_provided_argument_name), FeaturesResources.ResourceManager, typeof(FeaturesResources)))
         {
             _argumentAnalyzer = argumentAnalyzer;
             _attributeArgumentAnalyzer = attributeArgumentAnalyzer;
         }
 
-        public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+
+        public override DiagnosticAnalyzerCategory GetAnalyzerCategory()
+            => DiagnosticAnalyzerCategory.SemanticSpanAnalysis;
+
+        public override bool OpenFileOnly(Workspace workspace)
+            => false;
+
+        protected override void InitializeWorker(AnalysisContext context)
+            => context.RegisterSyntaxNodeAction(AnalyzeArgument, GetArgumentSyntaxKinds());
+
+        protected abstract ImmutableArray<TSyntaxKind> GetArgumentSyntaxKinds();
+
+        private void AnalyzeArgument(SyntaxNodeAnalysisContext context)
         {
-            var document = context.Document;
-            if (document.Project.Solution.Workspace.Kind == WorkspaceKind.MiscellaneousFiles)
+            _argumentAnalyzer.AnalyzeArgument(this, context);
+            _attributeArgumentAnalyzer?.AnalyzeArgument(this, context);
+        }
+    }
+
+    internal class AbstractUseNamedArgumentCodeFixProvider : SyntaxEditorBasedCodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds
+            => ImmutableArray.Create(IDEDiagnosticIds.UseNamedArgumentId);
+
+        protected override bool IncludeDiagnosticDuringFixAll(Diagnostic diagnostic)
+            => diagnostic.Severity != DiagnosticSeverity.Hidden;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+            var supportsNonTrailingArguments = diagnostic.Properties.ContainsKey(AbstractUseNamedArgumentsDiagnosticAnalyzer<int>.IncludeTrailingArgumentsKey);
+            var argumentName = diagnostic.Properties[AbstractUseNamedArgumentsDiagnosticAnalyzer<int>.ArgumentNameKey];
+
+            if (supportsNonTrailingArguments)
             {
-                return;
+                context.RegisterCodeFix(new MyCodeAction(
+                    string.Format(FeaturesResources.Add_argument_name_0, argumentName),
+                    c => FixAsync(context.Document, diagnostic, includeTrailingArguments: false, cancellationToken: c)), context.Diagnostics);
+
+                context.RegisterCodeFix(new MyCodeAction(
+                    string.Format(FeaturesResources.Add_argument_name_0_including_trailing_arguments, argumentName),
+                    c => FixAsync(context.Document, diagnostic, includeTrailingArguments: true, cancellationToken: c)), context.Diagnostics);
+            }
+            else
+            {
+                context.RegisterCodeFix(new MyCodeAction(
+                    string.Format(FeaturesResources.Add_argument_name_0, argumentName),
+                    c => FixAsync(context.Document, diagnostic, includeTrailingArguments: true, cancellationToken: c)), context.Diagnostics);
             }
 
-            var cancellationToken = context.CancellationToken;
-            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            return SpecializedTasks.EmptyTask;
+            //    context.RegisterRefactoring(
+            //        new MyCodeAction(
+            //            string.Format(FeaturesResources.Add_argument_name_0_including_trailing_arguments, argumentName),
+            //            c => AddNamedArgumentsAsync(root, document, argument, parameters, argumentIndex, includingTrailingArguments: true)));
+            //}
+            //else
+            //{
+            //    context.RegisterRefactoring(
+            //        new MyCodeAction(
+            //            string.Format(FeaturesResources.Add_argument_name_0, argumentName),
+            //            c => AddNamedArgumentsAsync(root, document, argument, parameters, argumentIndex, includingTrailingArguments: true)));
+            //}
+        }
 
-            await _argumentAnalyzer.ComputeRefactoringsAsync(
-                context, root, cancellationToken).ConfigureAwait(false);
+        protected override Task FixAllAsync(Document document, ImmutableArray<Diagnostic> diagnostics, SyntaxEditor editor, CancellationToken cancellationToken)
+        {
+            return FixAllAsync(document, )
+        }
 
-            if (_attributeArgumentAnalyzer != null)
-            {
-                await _attributeArgumentAnalyzer.ComputeRefactoringsAsync(
-                    context, root, cancellationToken).ConfigureAwait(false);
-            }
+        private Task<Document> FixAsync(
+            Document document, Diagnostic diagnostic, 
+            bool includeTrailingArguments, CancellationToken cancellationToken)
+        {
+            return FixAllWithEditorAsync(document,
+                editor => FixAllAsync(document, ImmutableArray.Create(diagnostic), editor, includeTrailingArguments, cancellationToken),
+                cancellationToken)
+        }
+
+        private Task FixAllAsync(Document document, object diagnostics, SyntaxEditor editor, bool includeTrailingArguments, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
 
         private class MyCodeAction : CodeAction.DocumentChangeAction
         {
             public MyCodeAction(string title, Func<CancellationToken, Task<Document>> createChangedDocument)
-                : base(title, createChangedDocument)
+                : base(title, createChangedDocument, title)
             {
             }
         }
