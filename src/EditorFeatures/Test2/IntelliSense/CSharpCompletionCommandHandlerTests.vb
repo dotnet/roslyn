0273fdb609348ec16402a7b765f7589ad549dd28
Ravi Chande <rchande@microsoft.com>
2017-09-15 13:01:30 -0700

    Add a test

--- src/EditorFeatures/Test2/IntelliSense/CSharpCompletionCommandHandlerTests.vb
+++ src/EditorFeatures/Test2/IntelliSense/CSharpCompletionCommandHandlerTests.vb
@@ -10,11 +10,15 @@ Imports Microsoft.CodeAnalysis.Editor.CSharp.Formatting
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Extensions
 Imports Microsoft.CodeAnalysis.Options
 Imports Microsoft.CodeAnalysis.Text
+Imports Microsoft.VisualStudio.Editor
+Imports Microsoft.VisualStudio.LanguageServices.Implementation.Snippets
 Imports Microsoft.VisualStudio.Text
 Imports Microsoft.VisualStudio.Text.Editor
 Imports Microsoft.VisualStudio.Text.Operations
 Imports Microsoft.VisualStudio.Text.Projection
+Imports Microsoft.VisualStudio.TextManager.Interop
 Imports Microsoft.VisualStudio.Utilities
+Imports MSXML
 Imports Roslyn.Utilities
 
 Namespace Microsoft.CodeAnalysis.Editor.UnitTests.IntelliSense
@@ -3523,6 +3527,119 @@ class C
             End Using
         End Function
 
+        <WorkItem(15348, "https://github.com/dotnet/roslyn/issues/15348")>
+        <WpfFact, Trait(Traits.Feature, Traits.Features.Completion)>
+        Public Async Function NoNamesWhileExpansionSession() As Task
+            Dim client = New TestSnippetClient(New Guid(), Nothing, Nothing, Nothing)
+            client.ExpansionSession = New MockExpansionSession()
+            Using state = TestState.CreateCSharpTestState(
+                          <Document>
+class Goo
+{
+    Goo G$$()
+    {
+
+    }
+}
+                              </Document>)
+                state.TextView.Properties.AddProperty(GetType(AbstractSnippetExpansionClient), client)
+                state.SendInvokeCompletionList()
+                Await state.AssertNoCompletionSession()
+            End Using
+        End Function
+
+        Private Class TestSnippetClient
+            Inherits AbstractSnippetExpansionClient
+
+            Public Sub New(languageServiceGuid As Guid, textView As ITextView, subjectBuffer As ITextBuffer, editorAdaptersFactoryService As IVsEditorAdaptersFactoryService)
+                MyBase.New(languageServiceGuid, textView, subjectBuffer, editorAdaptersFactoryService)
+            End Sub
+
+            Public Overrides Function GetExpansionFunction(xmlFunctionNode As IXMLDOMNode, bstrFieldName As String, ByRef pFunc As IVsExpansionFunction) As Integer
+                Throw New NotImplementedException()
+            End Function
+
+            Public Overrides Function TryHandleTab() As Boolean
+                Return MyBase.TryHandleTab()
+            End Function
+
+            Public Overrides Function TryHandleBackTab() As Boolean
+                Return MyBase.TryHandleBackTab()
+            End Function
+
+            Public Overrides Function TryHandleEscape() As Boolean
+                Return MyBase.TryHandleEscape()
+            End Function
+
+            Public Overrides Function TryHandleReturn() As Boolean
+                Return MyBase.TryHandleReturn()
+            End Function
+
+            Public Overrides Function TryInsertExpansion(startPositionInSubjectBuffer As Integer, endPositionInSubjectBuffer As Integer) As Boolean
+                Return MyBase.TryInsertExpansion(startPositionInSubjectBuffer, endPositionInSubjectBuffer)
+            End Function
+
+            Protected Overrides Function InsertEmptyCommentAndGetEndPositionTrackingSpan() As ITrackingSpan
+                Throw New NotImplementedException()
+            End Function
+
+            Friend Overrides Function AddImports(document As Document, position As Integer, snippetNode As XElement, placeSystemNamespaceFirst As Boolean, cancellationToken As CancellationToken) As Document
+                Throw New NotImplementedException()
+            End Function
+        End Class
+
+        Private Class MockExpansionSession
+            Implements IVsExpansionSession
+
+            Public Function EndCurrentExpansion(fLeaveCaret As Integer) As Integer Implements IVsExpansionSession.EndCurrentExpansion
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GoToNextExpansionField(fCommitIfLast As Integer) As Integer Implements IVsExpansionSession.GoToNextExpansionField
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GoToPreviousExpansionField() As Integer Implements IVsExpansionSession.GoToPreviousExpansionField
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GetFieldValue(bstrFieldName As String, ByRef pbstrValue As String) As Integer Implements IVsExpansionSession.GetFieldValue
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function SetFieldDefault(bstrFieldName As String, bstrNewValue As String) As Integer Implements IVsExpansionSession.SetFieldDefault
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GetFieldSpan(bstrField As String, ptsSpan() As VisualStudio.TextManager.Interop.TextSpan) As Integer Implements IVsExpansionSession.GetFieldSpan
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GetHeaderNode(bstrNode As String, ByRef pNode As IXMLDOMNode) As Integer Implements IVsExpansionSession.GetHeaderNode
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GetDeclarationNode(bstrNode As String, ByRef pNode As IXMLDOMNode) As Integer Implements IVsExpansionSession.GetDeclarationNode
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GetSnippetNode(bstrNode As String, ByRef pNode As IXMLDOMNode) As Integer Implements IVsExpansionSession.GetSnippetNode
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GetSnippetSpan(pts() As VisualStudio.TextManager.Interop.TextSpan) As Integer Implements IVsExpansionSession.GetSnippetSpan
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function SetEndSpan(ts As VisualStudio.TextManager.Interop.TextSpan) As Integer Implements IVsExpansionSession.SetEndSpan
+                Throw New NotImplementedException()
+            End Function
+
+            Public Function GetEndSpan(pts() As VisualStudio.TextManager.Interop.TextSpan) As Integer Implements IVsExpansionSession.GetEndSpan
+                Throw New NotImplementedException()
+            End Function
+        End Class
+
         Private Class MultipleChangeCompletionProvider
             Inherits CompletionProvider
 
@@ -3547,7 +3664,7 @@ class C
 
             Public Overrides Function GetChangeAsync(document As Document, item As CompletionItem, commitKey As Char?, cancellationToken As CancellationToken) As Task(Of CompletionChange)
                 Dim newText =
-"using NewUsing;
+    "using NewUsing;
 using System;
 class C
 {
@@ -3555,7 +3672,7 @@ class C
         return InsertedItem"
 
                 Dim change = CompletionChange.Create(
-                    New TextChange(New TextSpan(0, _caretPosition), newText))
+                    New TextChange(New Text.TextSpan(0, _caretPosition), newText))
                 Return Task.FromResult(change)
             End Function
 
