e5014224073c56a98b6bb32784569ddf67caaa42
Ivan Basov <basov@hotmail.com>
2017-09-20 18:05:42 -0700

    some more tests migrated

--- src/VisualStudio/IntegrationTest/TestUtilities/OutOfProcess/LocalsWindow_OutOfProc.Verifier.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/OutOfProcess/LocalsWindow_OutOfProc.Verifier.cs
@@ -1,5 +1,8 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
@@ -17,21 +20,30 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
 
             public void CheckEntry(string entryName, string expectedType, string expectedValue)
             {
-                var entry =_localsWindow._localsWindowInProc.GetEntry(entryName);
+                var entry = _localsWindow._debuggerService.GetLocals().FirstOrDefault(expession => expession.Name == entryName);
+                Assert.NotNull(entry);
                 Assert.Equal(expectedType, entry.Type);
                 Assert.Equal(expectedValue, entry.Value);
             }
 
             public void CheckEntry(string[] entryNames, string expectedType, string expectedValue)
             {
-                var entry = _localsWindow._localsWindowInProc.GetEntry(entryNames);
+                var entry =  GetEntry(entryNames);
                 Assert.Equal(expectedType, entry.Type);
                 Assert.Equal(expectedValue, entry.Value);
             }
 
             public void CheckCount(int expectedCount)
             {
-                var actualCount = _localsWindow._localsWindowInProc.GetCount();
+                int actualCount;
+                    if (_localsWindow._debuggerService.CurrentStackFrame != null)
+                {
+                    actualCount = _localsWindow._debuggerService.GetLocals().Length;
+                }
+                    else
+                {
+                    actualCount = 0;
+                }
                 Assert.Equal(expectedCount, actualCount);
             }
         }
