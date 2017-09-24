5af2c4f9bc766e7d3cd82f56a2e9124845381a60
Adam Speight <AdamSpeight2008@users.noreply.github.com>
2017-09-24 09:18:41 +0100

    Update CharacterInfo.vb
    Correcting a possible copy and paste error, should be `FULLWIDTH_LATIN_SMALL_LETTER_B`.
    
    Extended integer prefix tests to cover full range of permitted characters.

--- src/Compilers/VisualBasic/Portable/Scanner/CharacterInfo.vb
+++ src/Compilers/VisualBasic/Portable/Scanner/CharacterInfo.vb
@@ -257,7 +257,19 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
 
         Friend Shared Function BeginsBaseLiteral(c As Char) As Boolean
             Return (c = "H"c Or c = "O"c Or c = "B"c Or c = "h"c Or c = "o"c Or c = "b"c) OrElse
-                    (IsFullWidth(c) AndAlso (c = FULLWIDTH_LATIN_CAPITAL_LETTER_H Or c = FULLWIDTH_LATIN_CAPITAL_LETTER_O Or c = FULLWIDTH_LATIN_CAPITAL_LETTER_B Or c = FULLWIDTH_LATIN_SMALL_LETTER_H Or c = FULLWIDTH_LATIN_SMALL_LETTER_O Or c = FULLWIDTH_LATIN_CAPITAL_LETTER_B))
+                    (IsFullWidth(c) AndAlso (BeginsBaseLiteral_Hex(c) OrElse BeginsBaseLiteral_Oct(c) OrElse BeginsBaseLiteral_Bin(c)))
+        End Function
+
+        Private Shared Function BeginsBaseLiteral_Hex(c As Char) As Boolean
+            Return c = FULLWIDTH_LATIN_CAPITAL_LETTER_H Or c = FULLWIDTH_LATIN_SMALL_LETTER_H
+        End Function
+
+        Private Shared Function BeginsBaseLiteral_Oct(c As Char) As Boolean
+            Return c = FULLWIDTH_LATIN_CAPITAL_LETTER_O Or c = FULLWIDTH_LATIN_SMALL_LETTER_O
+        End Function
+
+        Private Shared Function BeginsBaseLiteral_Bin(c As Char) As Boolean
+            Return c = FULLWIDTH_LATIN_CAPITAL_LETTER_B Or c = FULLWIDTH_LATIN_SMALL_LETTER_B
         End Function
 
         Private Shared ReadOnly s_isIDChar As Boolean() =
--- src/Compilers/VisualBasic/Test/Syntax/Parser/ParseExpression.vb
+++ src/Compilers/VisualBasic/Test/Syntax/Parser/ParseExpression.vb
@@ -94,11 +94,28 @@ Public Class ParseExpressionTest
 
 #Region "Literal Test"
 
-
-    <Fact>
-    Public Sub ParseIntegerLiteralTest()
+    <Fact, Trait("IntegerLiteral_Prefix", "Hex")>
+    Private Sub ParseIntegerLiteralTest_HexPrefix()
         ParseExpression("&H1")
+        ParseExpression("&h1")
+        ParseExpression("&Ｈ1")
+        ParseExpression("&ｈ1")
+    End Sub
+
+    <Fact, Trait("IntegerLiteral_Prefix", "Oct")>
+    Private Sub ParseIntegerLiteralTest_OctPrefix()
         ParseExpression("&O1")
+        ParseExpression("&o1")
+        ParseExpression("&Ｏ1")
+        ParseExpression("&ｏ1")
+    End Sub
+
+    <Fact, Trait("IntegerLiteral_Prefix", "Bin")>
+    Private Sub ParseIntegerLiteralTest_BinPrefix()
+        ParseExpression("&B1")
+        ParseExpression("&b1")
+        ParseExpression("&Ｂ1")
+        ParseExpression("&ｂ1")
     End Sub
 
     <Fact>
