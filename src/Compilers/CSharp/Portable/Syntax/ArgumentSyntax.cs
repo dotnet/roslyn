877962324d388cc0e3d195467275a22fd4db174c
Neal Gafter <neal@gafter.com>
2017-10-02 15:02:15 -0700

    Change ArgumentSyntax.RefOrOutKeyword to RefKindKeyword And add overloads for compatibility. Fixes #22441

---
+++ src/Compilers/CSharp/Portable/Syntax/ArgumentSyntax.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.CodeAnalysis.CSharp.Syntax
+{
+    public sealed partial class ArgumentSyntax
+    {
+        public SyntaxToken RefOrOutKeyword
+        {
+            get => this.RefKindKeyword;
+        }
+        public ArgumentSyntax WithRefOrOutKeyword(SyntaxToken refOrOutKeyword)
+        {
+            return this.Update(this.NameColon, refOrOutKeyword, this.Expression);
+        }
+    }
+}
