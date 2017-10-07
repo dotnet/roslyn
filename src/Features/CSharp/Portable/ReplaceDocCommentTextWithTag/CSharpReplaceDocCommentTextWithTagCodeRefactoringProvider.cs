c74fc14f5804ba09d9d0dbe1e07f6f6eb3e740a3
Cheryl Borley <chborl@users.noreply.github.com>
2017-10-06 17:24:34 -0700

    Restrict trigger keywords and don't trigger inside XMLAttribute (22278)
    Fixes #22278 by restricting the keywords that will trigger the see langword refactoring.  Also, prevent see lang refactoring from being offered when already in an XML attribute

--- src/Features/CSharp/Portable/ReplaceDocCommentTextWithTag/CSharpReplaceDocCommentTextWithTagCodeRefactoringProvider.cs
+++ src/Features/CSharp/Portable/ReplaceDocCommentTextWithTag/CSharpReplaceDocCommentTextWithTagCodeRefactoringProvider.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Composition;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -11,15 +12,37 @@ namespace Microsoft.CodeAnalysis.CSharp.ReplaceDocCommentTextWithTag
     internal class CSharpReplaceDocCommentTextWithTagCodeRefactoringProvider :
         AbstractReplaceDocCommentTextWithTagCodeRefactoringProvider
     {
+        private static HashSet<string> s_keywords = new HashSet<string>
+        {
+            SyntaxFacts.GetText(SyntaxKind.NullKeyword),
+            SyntaxFacts.GetText(SyntaxKind.StaticKeyword),
+            SyntaxFacts.GetText(SyntaxKind.VirtualKeyword),
+            SyntaxFacts.GetText(SyntaxKind.TrueKeyword),
+            SyntaxFacts.GetText(SyntaxKind.FalseKeyword),
+            SyntaxFacts.GetText(SyntaxKind.AbstractKeyword),
+            SyntaxFacts.GetText(SyntaxKind.SealedKeyword),
+            SyntaxFacts.GetText(SyntaxKind.AsyncKeyword),
+            SyntaxFacts.GetText(SyntaxKind.AwaitKeyword)
+        };
+
         protected override bool IsXmlTextToken(SyntaxToken token)
             => token.Kind() == SyntaxKind.XmlTextLiteralToken ||
                token.Kind() == SyntaxKind.XmlTextLiteralNewLineToken;
 
-        protected override bool IsAnyKeyword(string text)
-            => SyntaxFacts.GetKeywordKind(text) != SyntaxKind.None ||
-               SyntaxFacts.GetContextualKeywordKind(text) != SyntaxKind.None;
+        protected override bool IsInXMLAttribute(SyntaxToken token)
+        {
+            return (token.Parent.Kind() == SyntaxKind.XmlCrefAttribute
+                || token.Parent.Kind() == SyntaxKind.XmlNameAttribute
+                || token.Parent.Kind() == SyntaxKind.XmlTextAttribute);
+        }
+
+        protected override bool IsKeyword(string text)
+        {
+            return s_keywords.Contains(text);
+        }
 
         protected override SyntaxNode ParseExpression(string text)
             => SyntaxFactory.ParseExpression(text);
+
     }
 }
