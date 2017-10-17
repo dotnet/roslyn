4c5ab29648a9cab17dc164d32e1d934150f431c4
Manish Vasani <mavasani@microsoft.com>
2017-10-17 11:33:48 -0700

    Revert "Address PR feedback and remove unused resource and comment out unused error code"
    This reverts commit 75687b52623f5873991ea5a161cbafb22c81fa82.

--- src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
@@ -1294,7 +1294,7 @@ namespace Microsoft.CodeAnalysis.CSharp
         ERR_EncodinglessSyntaxTree = 8055,
         // ERR_AccessorListAndExpressionBody = 8056, Deprecated in favor of ERR_BlockBodyAndExpressionBody
         ERR_BlockBodyAndExpressionBody = 8057,
-        //ERR_FeatureIsExperimental = 8058, No experimental feature
+        ERR_FeatureIsExperimental = 8058,
         ERR_FeatureNotAvailableInVersion6 = 8059,
         // available 8062-8069
         ERR_SwitchFallOut = 8070,
