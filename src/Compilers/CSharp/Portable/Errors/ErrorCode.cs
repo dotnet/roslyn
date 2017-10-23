9c6a003471df02abe933253904d71442a9177406
Julien Couvreur <julien.couvreur@gmail.com>
2017-10-23 18:46:12 +0100

    Fixing indent

--- src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
@@ -1538,6 +1538,6 @@ namespace Microsoft.CodeAnalysis.CSharp
         ERR_RefReturnThis = 8354,
         #endregion diagnostics introduced for `ref readonly`, `ref ternary` and `ref-like` features in C# 7.2
 
-       ERR_PredefinedValueTupleTypeAmbiguous3 = 8356,
+        ERR_PredefinedValueTupleTypeAmbiguous3 = 8356,
     }
 }
