c4037920d3674f2e477065a5c1b19e500219c482
Tomas Matousek <tomas.matousek@microsoft.com>
2017-11-07 12:01:04 -0800

    Merge fixups

--- src/Compilers/CSharp/Test/Semantic/IOperation/IOperationTests.cs
+++ src/Compilers/CSharp/Test/Semantic/IOperation/IOperationTests.cs
@@ -141,7 +141,7 @@ System.Console.WriteLine();
             var tree = compilation.SyntaxTrees.Single();
             var statement = tree.GetRoot().DescendantNodes().OfType<StatementSyntax>().Single();
             var model = compilation.GetSemanticModel(tree);
-            var operation = model.GetOperationInternal(statement);
+            var operation = model.GetOperation(statement);
 
             Assert.Equal(OperationKind.ExpressionStatement, operation.Kind);
             Assert.Null(operation.Parent);
