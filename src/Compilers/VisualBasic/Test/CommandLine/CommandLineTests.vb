7dea5aaea6c16216fbc393d39cfebccfae99cb30
Andy Gocke <angocke@microsoft.com>
2017-11-13 11:51:02 -0800

    Refactor CommonCompiler.RunCore (#23059)
    RunCore previously had a mix of diagnostic reporting methods including
    lists of Diagnostics, lists of DiagnosticInfos, ConcurrentQueues of
    Diagnostics, and just printing things directly to the console. This
    commit tries to simplify things by moving to using Diagnostics and
    DiagnosticBags wherever possible in the compile+emit phase of RunCore.

--- src/Compilers/VisualBasic/Test/CommandLine/CommandLineTests.vb
+++ src/Compilers/VisualBasic/Test/CommandLine/CommandLineTests.vb
@@ -8675,7 +8675,7 @@ End Module
 
             Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
             Assert.Equal(1, csc.Run(outWriter))
-            Assert.Equal($"error BC2012: can't open '{xmlPath}' for writing: Fake IOException{Environment.NewLine}", outWriter.ToString())
+            Assert.Equal($"vbc : error BC2012: can't open '{xmlPath}' for writing: Fake IOException{Environment.NewLine}", outWriter.ToString())
         End Sub
 
         <Theory>
@@ -8703,7 +8703,7 @@ End Module
 
             Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
             Assert.Equal(1, csc.Run(outWriter))
-            Assert.Equal($"error BC2012: can't open '{sourceLinkPath}' for writing: Fake IOException{Environment.NewLine}", outWriter.ToString())
+            Assert.Equal($"vbc : error BC2012: can't open '{sourceLinkPath}' for writing: Fake IOException{Environment.NewLine}", outWriter.ToString())
         End Sub
 
         <Fact>
