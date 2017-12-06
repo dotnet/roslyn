dfe65aaabbc080fec354afffe2465e7edc0b782c
Jared Parsons <jaredpar@microsoft.com>
2017-12-06 14:24:47 -0800

    Test theory new props caused int failure

--- build/scripts/build.ps1
+++ build/scripts/build.ps1
@@ -558,6 +558,8 @@ function Ensure-ProcDump() {
 function Redirect-Temp() {
     $temp = Join-Path $binariesDir "Temp"
     Create-Directory $temp
+    Copy-Item (Join-Path $repoDir "src\Workspaces\CoreTestUtilities\TestFiles\Directory.Build.props") $temp
+    Copy-Item (Join-Path $repoDir "src\Workspaces\CoreTestUtilities\TestFiles\Directory.Build.targets") $temp
     ${env:TEMP} = $temp
     ${env:TMP} = $temp
 }
