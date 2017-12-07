05330cbaee65745a67081957b0f869f52e01666b
Sam Harwell <sam.harwell@microsoft.com>
2017-12-07 17:39:59 -0600

    Updated formatting of decompiler license notice

--- src/VisualStudio/Core/Def/ServicesVSResources.Designer.cs
+++ src/VisualStudio/Core/Def/ServicesVSResources.Designer.cs
@@ -598,10 +598,9 @@ namespace Microsoft.VisualStudio.LanguageServices {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to IMPORTANT: Visual Studio includes decompiling functionality (“Decompiler”) that enables reproducing source code from binary code. By accessing and using the Decompiler, you agree to the Visual Studio license terms and the terms for the Decompiler below.
-        ///If you do not agree with these combined terms, do not access or use the Decompiler.
+        ///   Looks up a localized string similar to IMPORTANT: Visual Studio includes decompiling functionality (“Decompiler”) that enables reproducing source code from binary code. By accessing and using the Decompiler, you agree to the Visual Studio license terms and the terms for the Decompiler below. If you do not agree with these combined terms, do not access or use the Decompiler.
         /// 
-        ///You acknowledge that binary code and source code might be protected by copyright and trademark laws.  Before using the Decompiler on any binary code, you need to first:  [rest of string was truncated]&quot;;.
+        ///You acknowledge that binary code and source code might be protected by copyright and trademark laws.  Before using the Decompiler on any binary code, you need to first:   [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Decompiler_Legal_Notice_Message {
             get {
