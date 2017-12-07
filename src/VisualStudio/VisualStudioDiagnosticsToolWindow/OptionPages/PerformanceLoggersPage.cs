98ab9f4e8ba6082c4f57dfc0e112bf8ce2ac68b9
Heejae Chang <heejaechang@outlook.com>
2017-12-07 12:31:39 -0800

    added a way to set logging information synced to remote host and more logging for error list. (#23614)

--- src/VisualStudio/VisualStudioDiagnosticsToolWindow/OptionPages/PerformanceLoggersPage.cs
+++ src/VisualStudio/VisualStudioDiagnosticsToolWindow/OptionPages/PerformanceLoggersPage.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Microsoft.CodeAnalysis.Internal.Log;
 using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.Remote;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.LanguageServices;
 using Microsoft.VisualStudio.LanguageServices.Implementation;
@@ -15,14 +19,17 @@ namespace Roslyn.VisualStudio.DiagnosticsWindow.OptionsPages
     internal class PerformanceLoggersPage : AbstractOptionPage
     {
         private IGlobalOptionService _optionService;
+        private IRemoteHostClientService _remoteHostClientService;
 
         protected override AbstractOptionPageControl CreateOptionPage(IServiceProvider serviceProvider)
         {
             if (_optionService == null)
             {
                 var componentModel = (IComponentModel)serviceProvider.GetService(typeof(SComponentModel));
-
                 _optionService = componentModel.GetService<IGlobalOptionService>();
+
+                var workspace = componentModel.GetService<VisualStudioWorkspace>();
+                _remoteHostClientService = workspace.Services.GetService<IRemoteHostClientService>();
             }
 
             return new InternalOptionsControl(nameof(LoggerOptions), serviceProvider);
@@ -32,14 +39,62 @@ namespace Roslyn.VisualStudio.DiagnosticsWindow.OptionsPages
         {
             base.OnApply(e);
 
+            var loggerTypes = GetLoggerTypes().ToList();
+
+            // first set VS options
+            var options = Logger.GetLoggingChecker(_optionService);
+            SetRoslynLogger(loggerTypes, () => new EtwLogger(options));
+            SetRoslynLogger(loggerTypes, () => new TraceLogger(options));
+
+            // second set RemoteHost options
+            var client = _remoteHostClientService.TryGetRemoteHostClientAsync(CancellationToken.None).Result;
+            if (client == null)
+            {
+                // Remote host is disabled
+                return;
+            }
+
+            var functionIds = GetFunctionIds(options).ToList();
+            var unused = client.TryRunRemoteAsync(
+                WellKnownRemoteHostServices.RemoteHostService,
+                nameof(IRemoteHostService.SetLoggingFunctionIds),
+                new object[] { loggerTypes, functionIds },
+                CancellationToken.None).Result;
+        }
+
+        private static IEnumerable<string> GetFunctionIds(Func<FunctionId, bool> options)
+        {
+            foreach (var functionId in Enum.GetValues(typeof(FunctionId)).Cast<FunctionId>())
+            {
+                if (options(functionId))
+                {
+                    yield return functionId.ToString();
+                }
+            }
+        }
+
+        private IEnumerable<string> GetLoggerTypes()
+        {
             if (_optionService.GetOption(LoggerOptions.EtwLoggerKey))
             {
-                Logger.SetLogger(AggregateLogger.AddOrReplace(new EtwLogger(_optionService), Logger.GetLogger(), l => l is EtwLogger));
+                yield return nameof(EtwLogger);
             }
 
             if (_optionService.GetOption(LoggerOptions.TraceLoggerKey))
             {
-                Logger.SetLogger(AggregateLogger.AddOrReplace(new TraceLogger(_optionService), Logger.GetLogger(), l => l is TraceLogger));
+                yield return nameof(TraceLogger);
+            }
+        }
+
+        private static void SetRoslynLogger<T>(List<string> loggerTypes, Func<T> creator) where T : ILogger
+        {
+            if (loggerTypes.Contains(typeof(T).Name))
+            {
+                Logger.SetLogger(AggregateLogger.AddOrReplace(creator(), Logger.GetLogger(), l => l is T));
+            }
+            else
+            {
+                Logger.SetLogger(AggregateLogger.Remove(Logger.GetLogger(), l => l is T));
             }
         }
     }
