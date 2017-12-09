38e5a6607feb5f3b97c76ae737d145d91aacc75a
jcouv <jcouv@JULIENCOUVRDEE1>
2017-12-09 11:00:12 -0800

    NormalizeWhitespace: no space between keyword and open paren, add newline before constraints

--- src/Compilers/CSharp/Portable/Syntax/SyntaxNormalizer.cs
+++ src/Compilers/CSharp/Portable/Syntax/SyntaxNormalizer.cs
@@ -206,7 +206,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax
 
                 case SyntaxKind.CloseParenToken:
                     return (((currentToken.Parent is StatementSyntax) && nextToken.Parent != currentToken.Parent)
-                        || nextToken.Kind() == SyntaxKind.OpenBraceToken) ? 1 : 0;
+                        || nextToken.Kind() == SyntaxKind.OpenBraceToken
+                        || nextToken.Kind() == SyntaxKind.WhereKeyword) ? 1 : 0;
 
                 case SyntaxKind.CloseBracketToken:
                     if (currentToken.Parent is AttributeListSyntax && !(currentToken.Parent.Parent is ParameterSyntax))
@@ -428,6 +429,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax
                     !next.IsKind(SyntaxKind.QuestionToken) &&
                     !next.IsKind(SyntaxKind.SemicolonToken) &&
                     !next.IsKind(SyntaxKind.OpenBracketToken) &&
+                    (!next.IsKind(SyntaxKind.OpenParenToken) || KeywordNeedsSeparatorBeforeOpenParen(token.Kind())) &&
                     !next.IsKind(SyntaxKind.CloseParenToken) &&
                     !next.IsKind(SyntaxKind.CloseBraceToken) &&
                     !next.IsKind(SyntaxKind.ColonColonToken) &&
@@ -453,6 +455,25 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax
             }
 
             return false;
+        }
+
+        private static bool KeywordNeedsSeparatorBeforeOpenParen(SyntaxKind kind)
+        {
+            switch (kind)
+            {
+                case SyntaxKind.TypeOfKeyword:
+                case SyntaxKind.DefaultKeyword:
+                case SyntaxKind.NewKeyword:
+                case SyntaxKind.BaseKeyword:
+                case SyntaxKind.ThisKeyword:
+                case SyntaxKind.CheckedKeyword:
+                case SyntaxKind.UncheckedKeyword:
+                case SyntaxKind.SizeOfKeyword:
+                case SyntaxKind.ArgListKeyword:
+                    return false;
+                default:
+                    return true;
+            }
         }
 
         private static bool IsXmlTextToken(SyntaxKind kind)
