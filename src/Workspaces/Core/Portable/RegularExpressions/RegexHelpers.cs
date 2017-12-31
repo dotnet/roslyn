74ca79886e6947eacf83373125fc61c53e7fdfd3
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2017-12-30 23:19:04 -0800

    Move type to file.

---
+++ src/Workspaces/Core/Portable/RegularExpressions/RegexHelpers.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.RegularExpressions
+{
+    internal static class RegexHelpers
+    {
+        public static TextSpan GetSpan(RegexToken token)
+            => GetSpan(token.VirtualChars);
+
+        public static TextSpan GetSpan(RegexToken token1, RegexToken token2)
+            => GetSpan(token1.VirtualChars[0], token2.VirtualChars.Last());
+
+        public static TextSpan GetSpan(ImmutableArray<VirtualChar> virtualChars)
+            => GetSpan(virtualChars[0], virtualChars.Last());
+
+        public static TextSpan GetSpan(VirtualChar firstChar, VirtualChar lastChar)
+            => TextSpan.FromBounds(firstChar.Span.Start, lastChar.Span.End);
+
+        public static bool HasOption(RegexOptions options, RegexOptions val)
+            => (options & val) != 0;
+
+        public static RegexOptions OptionFromCode(VirtualChar ch)
+        {
+            switch (ch)
+            {
+                case 'i': case 'I':
+                    return RegexOptions.IgnoreCase;
+                case 'm': case 'M':
+                    return RegexOptions.Multiline;
+                case 'n': case 'N':
+                    return RegexOptions.ExplicitCapture;
+                case 's': case 'S':
+                    return RegexOptions.Singleline;
+                case 'x': case 'X':
+                    return RegexOptions.IgnorePatternWhitespace;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
--- src/Workspaces/Core/Portable/RegularExpressions/RegexParser.cs
+++ src/Workspaces/Core/Portable/RegularExpressions/RegexParser.cs
@@ -13,43 +13,6 @@ namespace Microsoft.CodeAnalysis.RegularExpressions
 {
     using static RegexHelpers;
 
-    internal static class RegexHelpers
-    {
-        public static TextSpan GetSpan(RegexToken token)
-            => GetSpan(token.VirtualChars);
-
-        public static TextSpan GetSpan(RegexToken token1, RegexToken token2)
-            => GetSpan(token1.VirtualChars[0], token2.VirtualChars.Last());
-
-        public static TextSpan GetSpan(ImmutableArray<VirtualChar> virtualChars)
-            => GetSpan(virtualChars[0], virtualChars.Last());
-
-        public static TextSpan GetSpan(VirtualChar firstChar, VirtualChar lastChar)
-            => TextSpan.FromBounds(firstChar.Span.Start, lastChar.Span.End);
-
-        public static bool HasOption(RegexOptions options, RegexOptions val)
-            => (options & val) != 0;
-
-        public static RegexOptions OptionFromCode(VirtualChar ch)
-        {
-            switch (ch)
-            {
-                case 'i': case 'I':
-                    return RegexOptions.IgnoreCase;
-                case 'm': case 'M':
-                    return RegexOptions.Multiline;
-                case 'n': case 'N':
-                    return RegexOptions.ExplicitCapture;
-                case 's': case 'S':
-                    return RegexOptions.Singleline;
-                case 'x': case 'X':
-                    return RegexOptions.IgnorePatternWhitespace;
-                default:
-                    throw new InvalidOperationException();
-            }
-        }
-    }
-
     internal struct RegexParser
     {
         private RegexLexer _lexer;
@@ -58,7 +21,6 @@ namespace Microsoft.CodeAnalysis.RegularExpressions
         private RegexToken _currentToken;
         private readonly List<string> _captureNames;
         private readonly List<int> _captureNumbers;
-        //private int currentAutoCapture;
 
         private RegexParser(
             ImmutableArray<VirtualChar> text, RegexOptions options,
