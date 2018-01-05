49f86cc4967eff9847ed5f718d7254094db6086c
Oleg Tkachenko <olegtk@microsoft.com>
2018-01-05 08:49:46 -0800

    turn test JTC export into a property to avoid specifying part creation policy

--- src/EditorFeatures/TestUtilities/TestExportJoinableTaskContext.cs
+++ src/EditorFeatures/TestUtilities/TestExportJoinableTaskContext.cs
@@ -6,13 +6,19 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests
 {
     // Starting with 15.3 the editor took a dependency on JoinableTaskContext
     // in Text.Logic and Intellisense layers as an editor host provided service.
-    [PartCreationPolicy(CreationPolicy.NonShared)] // JTC is "main thread" affinitized so should not be shared
     internal class TestExportJoinableTaskContext
     {
         [ThreadStatic]
         private static JoinableTaskContext s_jtcThreadStatic;
 
         [Export]
-        private JoinableTaskContext _joinableTaskContext = s_jtcThreadStatic ?? (s_jtcThreadStatic = new JoinableTaskContext());
+        private JoinableTaskContext JoinableTaskContext
+        {
+            get
+            {
+                // Make sure each import gets JTC set up with the calling thread as the "main" thread
+                return s_jtcThreadStatic ?? (s_jtcThreadStatic = new JoinableTaskContext());
+            }
+        }
     }
 }
