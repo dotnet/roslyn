71cf5ac52d5bd6213c42b9f0d84bece470d26f30
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-01-08 17:51:16 -0800

    Use unchecked math.

--- src/Workspaces/Core/Portable/VirtualChars/VirtualChar.cs
+++ src/Workspaces/Core/Portable/VirtualChars/VirtualChar.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.VirtualChars
@@ -42,15 +40,17 @@ namespace Microsoft.CodeAnalysis.VirtualChars
 
         public bool Equals(VirtualChar other)
             => Char == other.Char &&
-               Span.Equals(other.Span);
+               Span == other.Span;
 
         public override int GetHashCode()
         {
-            var hashCode = 244102310;
-            hashCode = hashCode * -1521134295 + base.GetHashCode();
-            hashCode = hashCode * -1521134295 + Char.GetHashCode();
-            hashCode = hashCode * -1521134295 + EqualityComparer<TextSpan>.Default.GetHashCode(Span);
-            return hashCode;
+            unchecked
+            {
+                var hashCode = 244102310;
+                hashCode = hashCode * -1521134295 + Char.GetHashCode();
+                hashCode = hashCode * -1521134295 + Span.GetHashCode();
+                return hashCode;
+            }
         }
 
         public static bool operator ==(VirtualChar char1, VirtualChar char2)
