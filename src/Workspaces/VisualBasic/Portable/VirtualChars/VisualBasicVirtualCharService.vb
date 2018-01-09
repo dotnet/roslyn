7005196c22e552cd5ee0675704bbfab0c219e4ee
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-01-08 18:06:57 -0800

    Share source.

--- src/Workspaces/VisualBasic/Portable/VirtualChars/VisualBasicVirtualCharService.vb
+++ src/Workspaces/VisualBasic/Portable/VirtualChars/VisualBasicVirtualCharService.vb
@@ -1,8 +1,8 @@
-﻿Imports System.Collections.Immutable
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports System.Collections.Immutable
 Imports System.Composition
 Imports Microsoft.CodeAnalysis.Host.Mef
-Imports Microsoft.CodeAnalysis.PooledObjects
-Imports Microsoft.CodeAnalysis.Text
 Imports Microsoft.CodeAnalysis.VirtualChars
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.VirtualChars
@@ -13,42 +13,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.VirtualChars
         Public Shared ReadOnly Instance As IVirtualCharService = New VisualBasicVirtualCharService()
 
         Protected Overrides Function TryConvertToVirtualCharsWorker(token As SyntaxToken) As ImmutableArray(Of VirtualChar)
-            If token.Kind() <> SyntaxKind.StringLiteralToken Then
-                Return Nothing
-            End If
-
-            Const StartDelimeter = """"
-            Const EndDelimeter = """"
-
-            Dim tokenText = token.Text
-            If Not tokenText.StartsWith(StartDelimeter) OrElse
-                Not tokenText.EndsWith(EndDelimeter) Then
-
-                Debug.Assert(False, "This should not be reachable as long as the compiler added no diagnostics.")
-                Return Nothing
-            End If
-
-            Dim startIndexInclusive = StartDelimeter.Length
-            Dim endIndexExclusive = tokenText.Length - EndDelimeter.Length
-
-            Dim result = ArrayBuilder(Of VirtualChar).GetInstance()
-
-            Dim offset = token.SpanStart
-            Dim index = startIndexInclusive
-            While index < endIndexExclusive
-                If tokenText(index) = """"c AndAlso
-                    tokenText(index + 1) = """"c Then
-
-                    result.Add(New VirtualChar(""""c, New TextSpan(offset + index, 2)))
-                    index = index + 2
-
-                Else
-                    result.Add(New VirtualChar(tokenText(index), New TextSpan(offset + index, 1)))
-                    index = index + 1
-                End If
-            End While
-
-            Return result.ToImmutableAndFree()
+            Return TryConvertSimpleDelimitedString(token, """", """")
         End Function
     End Class
 End Namespace
