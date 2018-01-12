523be29ea955e7a4a060403d929d1c14a6ed1d2a
Tomas Matousek <tomat@microsoft.com>
2018-01-11 17:41:52 -0800

    Add VisualStudioActiveStatementProviderTests

---
+++ src/VisualStudio/Core/Test.Next/EditAndContiue/Mocks/DkmActiveStatement.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.VisualStudio.Debugger.Clr;
+using Microsoft.VisualStudio.Debugger.Symbols;
+
+namespace Microsoft.VisualStudio.LanguageServices.UnitTests.EditAndContinue
+{
+    internal sealed class DkmActiveStatement
+    {
+        public DkmActiveStatementFlags Flags { get; set; }
+        public DkmInstructionSymbol InstructionSymbol { get; set; }
+        public DkmClrInstructionAddress InstructionAddress { get; set; }
+        public uint ExecutingMethodVersion { get; set; }
+        public DkmThread Thread { get; set; }
+
+        public DkmActiveStatement(
+            Guid threadId,
+            Guid moduleId,
+            int methodToken,
+            uint methodVersion,
+            uint ilOffset,
+            DkmActiveStatementFlags flags)
+        {
+            Flags = flags;
+            InstructionSymbol = new DkmInstructionSymbol() { Module = new DkmModule() { Id = new DkmModuleId(moduleId, default) } };
+            InstructionAddress = new DkmClrInstructionAddress()
+            {
+                MethodId = new DkmClrMethodId(methodToken, methodVersion),
+                ILOffset = ilOffset,
+            };
+            ExecutingMethodVersion = methodVersion;
+            Thread = new DkmThread() { UniqueId = threadId };
+        }
+    }
+}
