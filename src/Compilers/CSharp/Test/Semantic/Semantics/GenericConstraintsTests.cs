21c139338884fd95471563e957e318f8c1bc7f94
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2018-01-16 13:01:34 -0800

    More feedback

--- src/Compilers/CSharp/Test/Semantic/Semantics/GenericConstraintsTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/GenericConstraintsTests.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
 using Microsoft.CodeAnalysis.CSharp.UnitTests;
 using Microsoft.CodeAnalysis.Test.Utilities;
 using Xunit;
@@ -339,7 +336,7 @@ public class Test2
         }
 
         [Fact]
-        public void EnumConstraint_InheritenceChain()
+        public void EnumConstraint_InheritanceChain()
         {
             CreateStandardCompilation(@"
 public enum E
