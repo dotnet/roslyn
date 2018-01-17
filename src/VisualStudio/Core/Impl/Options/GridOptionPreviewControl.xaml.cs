619213b55badce1f494f9489e33119e0e2148505
JieCarolHu <JieCarolHu@gmail.com>
2018-01-16 20:03:09 -0800

    add text and hyperlink to C# code style page

--- src/VisualStudio/Core/Impl/Options/GridOptionPreviewControl.xaml.cs
+++ src/VisualStudio/Core/Impl/Options/GridOptionPreviewControl.xaml.cs
@@ -1,10 +1,16 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Navigation;
+using Microsoft.CodeAnalysis.Diagnostics.Log;
 using Microsoft.CodeAnalysis.Options;
+using Microsoft.VisualStudio.LanguageServices.Implementation.Utilities;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.Options
 {
@@ -14,19 +20,33 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.Options
         private readonly IServiceProvider _serviceProvider;
         private readonly Func<OptionSet, IServiceProvider, AbstractOptionPreviewViewModel> _createViewModel;
 
+        public static string CodeStylePageHeader = "The settings configured here only apply to your machine. To configure these settings to travel with your solution, use EditorConfig. ";
+        public static string CodeStylePageHeaderLearnMoreText = "Learn more";
+        public static Uri CodeStylePageHeaderLearnMoreUri = new Uri("https://docs.microsoft.com/en-us/visualstudio/ide/editorconfig-code-style-settings-reference");
         public static string DescriptionHeader => ServicesVSResources.Description;
         public static string PreferenceHeader => ServicesVSResources.Preference;
         public static string SeverityHeader => ServicesVSResources.Severity;
 
-        internal GridOptionPreviewControl(IServiceProvider serviceProvider, 
-            Func<OptionSet, IServiceProvider, 
-            AbstractOptionPreviewViewModel> createViewModel) 
+        internal GridOptionPreviewControl(IServiceProvider serviceProvider,
+            Func<OptionSet, IServiceProvider,
+            AbstractOptionPreviewViewModel> createViewModel)
             : base(serviceProvider)
         {
             InitializeComponent();
 
             _serviceProvider = serviceProvider;
             _createViewModel = createViewModel;
+        }
+
+        private void LearnMoreHyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            if (e.Uri == null)
+            {
+                return;
+            }
+
+            BrowserHelper.StartBrowser(e.Uri);
+            e.Handled = true;
         }
 
         private void Options_SelectionChanged(object sender, SelectionChangedEventArgs e)
