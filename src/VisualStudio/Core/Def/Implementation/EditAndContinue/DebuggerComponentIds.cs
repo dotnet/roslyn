359d8be049a7facdad337c24b9428aac1141d0a5
Tomas Matousek <tomat@microsoft.com>
2018-01-17 18:49:01 -0800

    Ensure XAPI is initialized before calling Concord APIs.

---
+++ src/VisualStudio/Core/Def/Implementation/EditAndContinue/DebuggerComponentIds.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.VisualStudio.LanguageServices.EditAndContinue
+{
+    internal static class DebuggerComponentIds
+    {
+        /// <summary>
+        /// Component id as specified in ManagedEditAndContinueService.vsdconfigxml.
+        /// </summary>
+        public static readonly Guid ManagedEditAndContinueService = new Guid("A96BBE03-0408-41E3-8613-6086FD494B43");
+    }
+}
