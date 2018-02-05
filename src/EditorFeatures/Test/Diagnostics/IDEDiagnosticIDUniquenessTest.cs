f71668c1cf0777aff92a51ce3587aec6c3b108f2
Cheryl Borley <chborl@users.noreply.github.com>
2018-02-05 11:19:47 -0800

    Added test for unique IDEDiagnosticIDs

---
+++ src/EditorFeatures/Test/Diagnostics/IDEDiagnosticIDUniquenessTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.UnitTests.Diagnostics
+{
+    public class IDEDiagnosticIDUniquenessTest
+    {
+        [Fact, Trait(Traits.Feature, Traits.Features.Diagnostics)]
+        public void UniqueIDEDiagnosticIds()
+        {
+            Type type = typeof(IDEDiagnosticIds);
+            List<string> ListOfIDEDiagnosticIds = type.GetFields().Select(x => x.GetValue(null).ToString()).ToList();
+            Assert.Equal(ListOfIDEDiagnosticIds.Count, ListOfIDEDiagnosticIds.Distinct().Count());
+        }
+    }
+}
