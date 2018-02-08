57a0329edcd44874458c62d4138452ca08a8eab9
Andy Gocke <angocke@microsoft.com>
2018-02-08 14:10:47 -0800

    Use 'continue' statement

--- build.sh
+++ build.sh
@@ -60,47 +60,38 @@ do
             ;;
         --docker)
             build_in_docker=true
+            continue
             ;;
         --debug)
             build_configuration=Debug
-            args="$args $1"
             ;;
         --release)
             build_configuration=Release
-            args="$args $1"
             ;;
         --restore|-r)
             restore=true
-            args="$args $1"
             ;;
         --build|-b)
             build=true
-            args="$args $1"
             ;;
         --test|-t)
             test_=true
-            args="$args $1"
             ;;
         --mono)
             use_mono=true
-            args="$args $1"
             ;;
         --build-bootstrap)
             build_bootstrap=true
-            args="$args $1"
             ;;
         --use-bootstrap)
             use_bootstrap=true
-            args="$args $1"
             ;;
         --bootstrap)
             build_bootstrap=true
             use_bootstrap=true
-            args="$args $1"
             ;;
         --stop-vbcscompiler)
             stop_vbcscompiler=true
-            args="$args $1"
             ;;
         *)
             echo "$1"
@@ -108,6 +99,7 @@ do
             exit 1
         ;;
     esac
+    args="$args $1"
     shift
 done
 
