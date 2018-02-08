facb834dcc6fad7bcfbcd515ec35e4746a435f49
Alireza Habibi <habibi.alireza@outlook.com>
2018-02-08 09:11:32 +0330

    Add parsing test for bad implicit stackalloc

--- src/Compilers/CSharp/Test/Syntax/Parsing/StackAllocInitializerTests.cs
+++ src/Compilers/CSharp/Test/Syntax/Parsing/StackAllocInitializerTests.cs
@@ -108,5 +108,55 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Parsing
             }
             EOF();
         }
+
+        [Fact]
+        public void StackAllocInitializer_04()
+        {
+            UsingExpression("stackalloc[1] { 42 }", options: TestOptions.Regular7,
+                // (1,1): error CS8107: Feature 'stackalloc initializer' is not available in C# 7.0. Please use language version 7.3 or greater.
+                // stackalloc[1] { 42 }
+                Diagnostic(ErrorCode.ERR_FeatureNotAvailableInVersion7, "stackalloc").WithArguments("stackalloc initializer", "7.3").WithLocation(1, 1),
+                // (1,12): error CS1003: Syntax error, ']' expected
+                // stackalloc[1] { 42 }
+                Diagnostic(ErrorCode.ERR_SyntaxError, "1").WithArguments("]", "").WithLocation(1, 12),
+                // (1,12): error CS1514: { expected
+                // stackalloc[1] { 42 }
+                Diagnostic(ErrorCode.ERR_LbraceExpected, "1").WithLocation(1, 12),
+                // (1,13): error CS1003: Syntax error, ',' expected
+                // stackalloc[1] { 42 }
+                Diagnostic(ErrorCode.ERR_SyntaxError, "]").WithArguments(",", "]").WithLocation(1, 13),
+                // (1,15): error CS1003: Syntax error, ',' expected
+                // stackalloc[1] { 42 }
+                Diagnostic(ErrorCode.ERR_SyntaxError, "{").WithArguments(",", "{").WithLocation(1, 15),
+                // (1,21): error CS1513: } expected
+                // stackalloc[1] { 42 }
+                Diagnostic(ErrorCode.ERR_RbraceExpected, "").WithLocation(1, 21));
+            N(SyntaxKind.ImplicitStackAllocArrayCreationExpression);
+            {
+                N(SyntaxKind.StackAllocKeyword);
+                N(SyntaxKind.OpenBracketToken);
+                M(SyntaxKind.CloseBracketToken);
+                N(SyntaxKind.ArrayInitializerExpression);
+                {
+                    M(SyntaxKind.OpenBraceToken);
+                    N(SyntaxKind.NumericLiteralExpression);
+                    {
+                        N(SyntaxKind.NumericLiteralToken, "1");
+                    }
+                    M(SyntaxKind.CommaToken);
+                    N(SyntaxKind.ArrayInitializerExpression);
+                    {
+                        N(SyntaxKind.OpenBraceToken);
+                        N(SyntaxKind.NumericLiteralExpression);
+                        {
+                            N(SyntaxKind.NumericLiteralToken, "42");
+                        }
+                        N(SyntaxKind.CloseBraceToken);
+                    }
+                    M(SyntaxKind.CloseBraceToken);
+                }
+            }
+            EOF();
+        }
     }
 }
