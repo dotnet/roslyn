0d7da1be807c0b0dd5b391a5ea9a69500266ac31
Martin Strecker <MaStr11@users.noreply.github.com>
2018-02-21 16:53:53 +0100

    Localization resources added

--- src/Features/Core/Portable/FeaturesResources.Designer.cs
+++ src/Features/Core/Portable/FeaturesResources.Designer.cs
@@ -232,7 +232,18 @@ namespace Microsoft.CodeAnalysis {
                 return ResourceManager.GetString("Add_parameter_to_0", resourceCulture);
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized string similar to Add parameter to &apos;{0}&apos; (including overrides/implementations).
+        /// </summary>
+        internal static string Add_parameter_to_0_including_overrides_implementations
+        {
+            get
+            {
+                return ResourceManager.GetString("Add_parameter_to_0_including_overrides_implementations", resourceCulture);
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized string similar to Add parameters to &apos;{0}&apos;.
         /// </summary>
@@ -2534,7 +2545,18 @@ namespace Microsoft.CodeAnalysis {
                 return ResourceManager.GetString("Re_triage_0_currently_1", resourceCulture);
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized string similar to Related method signatures found in metadata will not be updated..
+        /// </summary>
+        internal static string Related_method_signatures_found_in_metadata_will_not_be_updated
+        {
+            get
+            {
+                return ResourceManager.GetString("Related_method_signatures_found_in_metadata_will_not_be_updated", resourceCulture);
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized string similar to Remarks:.
         /// </summary>
