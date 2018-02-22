2e8509fc85985de2a5266d12d5012d6d02461814
Jason Malinowski <jason.malinowski@microsoft.com>
2018-02-21 16:03:13 -0800

    Add an "implementation" of ILoggingServiceInternal so we can MEF compose
    This is working around
    https://devdiv.visualstudio.com/DevDiv/_workitems/edit/570290

--- src/VisualStudio/CSharp/Test/Interactive/InteractiveWindowTestHost.cs
+++ src/VisualStudio/CSharp/Test/Interactive/InteractiveWindowTestHost.cs
@@ -1,12 +1,17 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
 using System.Linq;
-using Microsoft.VisualStudio.Utilities;
-using Microsoft.VisualStudio.InteractiveWindow;
-using Microsoft.CodeAnalysis.Editor.UnitTests.Utilities;
 using Microsoft.CodeAnalysis.Editor.UnitTests;
+using Microsoft.CodeAnalysis.Editor.UnitTests.Utilities;
+using Microsoft.VisualStudio.InteractiveWindow;
+using Microsoft.VisualStudio.Telemetry;
+using Microsoft.VisualStudio.Text.Utilities;
+using Microsoft.VisualStudio.Utilities;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Interactive
 {
@@ -27,6 +32,39 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Interactive
             }.Concat(MinimalTestExportProvider.GetEditorAssemblies());
             return new AggregateCatalog(assemblies.Select(a => new AssemblyCatalog(a)));
         });
+
+        // Provide an export of ILoggingServiceInternal to work around https://devdiv.visualstudio.com/DevDiv/_workitems/edit/570290
+        [Export(typeof(ILoggingServiceInternal))]
+        private sealed class HACK_LoggingProvider : ILoggingServiceInternal
+        {
+            public void AdjustCounter(string key, string name, int delta = 1)
+            {
+            }
+
+            public void PostCounters()
+            {
+            }
+
+            public void PostEvent(string key, params object[] namesAndProperties)
+            {
+            }
+
+            public void PostEvent(string key, IReadOnlyList<object> namesAndProperties)
+            {
+            }
+
+            public void PostEvent(DataModelEventType eventType, string eventName, TelemetryResult result = TelemetryResult.Success, params (string name, object property)[] namesAndProperties)
+            {
+            }
+
+            public void PostEvent(DataModelEventType eventType, string eventName, TelemetryResult result, IReadOnlyList<(string name, object property)> namesAndProperties)
+            {
+            }
+
+            public void PostFault(string eventName, string description, Exception exceptionObject, string additionalErrorInfo, bool? isIncludedInWatsonSample)
+            {
+            }
+        }
 
         internal InteractiveWindowTestHost()
         {
