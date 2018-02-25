cc16f43ffabea7ba9fd58542750ed5a01b3da0cc
Sam Harwell <sam.harwell@microsoft.com>
2018-02-25 11:05:21 -0600

    Fix violations of CA1801 (Review unused parameters)
    Closes #1355

--- src/Test.Utilities/CodeFixTestBase.cs
+++ src/Test.Utilities/CodeFixTestBase.cs
@@ -123,7 +123,7 @@ namespace Test.Utilities
             var additionalFileText = newAdditionalFileToVerify.GetText().ToString();
 
             Solution newSolution;
-            var runner = new CodeFixRunner(analyzerOpt, codeFixProvider, DefaultTestValidationMode);
+            var runner = new CodeFixRunner(analyzerOpt, codeFixProvider, validationMode);
             if (onlyFixFirstFixableDiagnostic || codeFixIndex.HasValue)
             {
                 newSolution = runner.ApplySingleFix(document.Project, additionalFiles, codeFixIndex.HasValue ? codeFixIndex.Value : 0, fixableDiagnosticIndex: 0);
