98e4ec1d51b6fd0236d6574ed8e46c1d34b93848
Jared Parsons <jaredpparsons@gmail.com>
2018-03-06 14:34:12 -0800

    Skipping emit on Unix

--- build/scripts/tests.sh
+++ build/scripts/tests.sh
@@ -66,6 +66,11 @@ do
     echo Running "${runtime} ${file_name[@]}"
     if [[ "${runtime}" == "dotnet" ]]; then
         runner="dotnet exec --depsfile ${deps_json} --runtimeconfig ${runtimeconfig_json}"
+        if [[ "${file_name[@]}" == *'Roslyn.Compilers.CSharp.Emit.UnitTests.dll' ]]
+        then
+            echo "Skipping ${file_name[@]}"
+            continue
+        fi
     elif [[ "${runtime}" == "mono" ]]; then
         runner=mono
         if [[ "${file_name[@]}" == *'Microsoft.CodeAnalysis.CSharp.Scripting.UnitTests.dll' || "${file_name[@]}" == *'Roslyn.Compilers.CompilerServer.UnitTests.dll' || "${file_name[@]}" == *'Roslyn.Compilers.CSharp.Emit.UnitTests.dll' ]]
