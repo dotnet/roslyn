76a27aade462043b78fe2de83601d44e1715d4b1
Tomas Matousek <tomat@microsoft.com>
2018-03-06 17:24:59 -0800

    Select specific overload in DebugDirectoryExtensions reflection

--- src/Compilers/CSharp/Test/Emit/Emit/CompilationEmitTests.cs
+++ src/Compilers/CSharp/Test/Emit/Emit/CompilationEmitTests.cs
@@ -15,6 +15,7 @@ using Microsoft.CodeAnalysis.CSharp.Symbols.Metadata.PE;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Microsoft.CodeAnalysis.Emit;
 using Microsoft.CodeAnalysis.Test.Utilities;
+using Roslyn.Reflection.PortableExecutable;
 using Roslyn.Test.Utilities;
 using Roslyn.Utilities;
 using Xunit;
