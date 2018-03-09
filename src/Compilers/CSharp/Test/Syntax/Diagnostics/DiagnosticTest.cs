b9255be38726f00775305096fba586afc7a331cc
Charles Stoner <chucks@microsoft.com>
2018-03-08 20:44:59 -0800

    Misc.

--- src/Compilers/CSharp/Test/Syntax/Diagnostics/DiagnosticTest.cs
+++ src/Compilers/CSharp/Test/Syntax/Diagnostics/DiagnosticTest.cs
@@ -283,6 +283,7 @@ class X
                         case ErrorCode.WRN_NullabilityMismatchInArgument:
                         case ErrorCode.WRN_NullabilityMismatchInReturnTypeOfTargetDelegate:
                         case ErrorCode.WRN_NullabilityMismatchInParameterTypeOfTargetDelegate:
+                        case ErrorCode.WRN_NoBestNullabilityConditionalExpression:
                             Assert.Equal(1, ErrorFacts.GetWarningLevel(errorCode));
                             break;
                         case ErrorCode.WRN_InvalidVersionFormat:
