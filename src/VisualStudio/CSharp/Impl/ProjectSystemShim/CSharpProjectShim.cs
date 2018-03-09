7c3d3c80aa59cbdf000f3a4ff50138936432dcde
Jason Malinowski <jason.malinowski@microsoft.com>
2018-03-09 15:18:50 -0800

    Move CSharpProjectShimWithServices.CanUseTextBuffer down
    It's the only remaining stuff in CSharpProjectShimWithServices, so
    just move it down a layer.

--- src/VisualStudio/CSharp/Impl/ProjectSystemShim/CSharpProjectShim.cs
+++ src/VisualStudio/CSharp/Impl/ProjectSystemShim/CSharpProjectShim.cs
@@ -8,6 +8,7 @@ using EnvDTE;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Utilities;
+using Microsoft.CodeAnalysis.Editor;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.VisualStudio.LanguageServices.CSharp.ProjectSystemShim.Interop;
 using Microsoft.VisualStudio.LanguageServices.Implementation.CodeModel;
@@ -15,6 +16,7 @@ using Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem;
 using Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem.Legacy;
 using Microsoft.VisualStudio.LanguageServices.Implementation.TaskList;
 using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.TextManager.Interop;
 using Roslyn.Utilities;
 
@@ -83,6 +85,16 @@ namespace Microsoft.VisualStudio.LanguageServices.CSharp.ProjectSystemShim
         private string GetIdForErrorCode(int errorCode)
         {
             return "CS" + errorCode.ToString("0000");
+        }
+
+        protected override bool CanUseTextBuffer(ITextBuffer textBuffer)
+        {
+            // In Web scenarios, the project system tells us about all files in the project, including ".aspx" and ".cshtml" files.
+            // The impact of this is that we try to add a StandardTextDocument for the file, and parse it on disk, etc, which won't
+            // end well.  We prevent that from happening by not allowing buffers that aren't of our content type to be used for
+            // StandardTextDocuments.  In the web scenarios, we will instead end up creating a ContainedDocument that actually 
+            // knows about the secondary buffer that contains valid code in our content type.
+            return textBuffer.ContentType.IsOfType(ContentTypeNames.CSharpContentType);
         }
 
         protected override CompilationOptions CreateCompilationOptions(CommandLineArguments commandLineArguments, ParseOptions newParseOptions)
