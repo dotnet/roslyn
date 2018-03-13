6b566f7ee5072e5474998494645e296153af9c55
Heejae Chang <heejaechang@outlook.com>
2018-03-13 12:52:45 -0700

    fix codeFix info bar behavior and fix AutoProperty to only offer refactoring when it is on right context (#25363)
    * it looks like codefix info bar has been broken for a while, (since 2016 - https://github.com/dotnet/roslyn/commit/eb5d3f83c07e2494dd5901c07fe8950e23e95b7e#diff-69343eea131fc14c4f6cff1408eb016a)
    
    this fixes it to behave correctly.
    
    * fix AutoProperty to offer the fix only when it is on right context.
    
    * added test
    
    * wrong cancellation token is used. it cause refactoring to not cancellable

--- src/EditorFeatures/Core/Implementation/EditorLayerExtensionManager.cs
+++ src/EditorFeatures/Core/Implementation/EditorLayerExtensionManager.cs
@@ -69,10 +69,10 @@ namespace Microsoft.CodeAnalysis.Editor
                     {
                         base.HandleException(provider, exception);
 
-                        _errorReportingService?.ShowErrorInfoInActiveView(String.Format(WorkspacesResources._0_encountered_an_error_and_has_been_disabled,  provider.GetType().Name),
+                        _errorReportingService?.ShowErrorInfoInActiveView(String.Format(WorkspacesResources._0_encountered_an_error_and_has_been_disabled, provider.GetType().Name),
                             new InfoBarUI(WorkspacesResources.Show_Stack_Trace, InfoBarUI.UIKind.HyperLink, () => ShowDetailedErrorInfo(exception), closeAfterAction: false),
                             new InfoBarUI(WorkspacesResources.Enable, InfoBarUI.UIKind.Button, () => { EnableProvider(provider); LogEnableProvider(provider); }),
-                            new InfoBarUI(WorkspacesResources.Enable_and_ignore_future_errors, InfoBarUI.UIKind.Button, () => { EnableProvider(provider); LogEnableProvider(provider); }),
+                            new InfoBarUI(WorkspacesResources.Enable_and_ignore_future_errors, InfoBarUI.UIKind.Button, () => { EnableProvider(provider); IgnoreProvider(provider); LogEnableAndIgnoreProvider(provider); }),
                             new InfoBarUI(String.Empty, InfoBarUI.UIKind.Close, () => LogLeaveDisabled(provider)));
                     }
                     else
