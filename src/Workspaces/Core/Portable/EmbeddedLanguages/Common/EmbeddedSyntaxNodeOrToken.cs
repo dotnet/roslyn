94736b856d3348bc88af59355f762e9c69d23c7c
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-03-16 11:28:00 -0700

    Add common code.

---
+++ src/Workspaces/Core/Portable/EmbeddedLanguages/Common/EmbeddedSyntaxNodeOrToken.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.EmbeddedLanguages.Common
+{
+    internal struct EmbeddedSyntaxNodeOrToken<TNode> where TNode : EmbeddedSyntaxNode<TNode>
+    {
+        public readonly TNode Node;
+        public readonly EmbeddedSyntaxToken Token;
+
+        private EmbeddedSyntaxNodeOrToken(TNode node) : this()
+        {
+            Debug.Assert(node != null);
+            Node = node;
+        }
+
+        private EmbeddedSyntaxNodeOrToken(EmbeddedSyntaxToken token) : this()
+        {
+            Debug.Assert(token.RawKind != 0);
+            Token = token;
+        }
+
+        public bool IsNode => Node != null;
+
+        public static implicit operator EmbeddedSyntaxNodeOrToken<TNode>(TNode node)
+            => new EmbeddedSyntaxNodeOrToken<TNode>(node);
+
+        public static implicit operator EmbeddedSyntaxNodeOrToken<TNode>(EmbeddedSyntaxToken token)
+            => new EmbeddedSyntaxNodeOrToken<TNode>(token);
+    }
+}
