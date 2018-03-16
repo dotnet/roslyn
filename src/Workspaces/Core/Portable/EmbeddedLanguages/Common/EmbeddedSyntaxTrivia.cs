4a2a34d408803f2ecc2d77be21ae48fef1b84d78
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-03-16 11:20:37 -0700

    Add common code.

---
+++ src/Workspaces/Core/Portable/EmbeddedLanguages/Common/EmbeddedSyntaxTrivia.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis.EmbeddedLanguages.Common;
+using Microsoft.CodeAnalysis.EmbeddedLanguages.VirtualChars;
+
+namespace Microsoft.CodeAnalysis.EmbeddedLanguages.Common
+{
+    /// <summary>
+    /// Trivia on a <see cref="EmbeddedSyntaxToken"/>.
+    /// </summary>
+    internal struct EmbeddedSyntaxTrivia
+    {
+        public readonly int RawKind;
+        public readonly ImmutableArray<VirtualChar> VirtualChars;
+
+        /// <summary>
+        /// A place for diagnostics to be stored during parsing.  Not intended to be accessed 
+        /// directly.  These will be collected and aggregated into <see cref="EmbeddedSyntaxTree.Diagnostics"/>
+        /// </summary> 
+        internal readonly ImmutableArray<EmbeddedDiagnostic> Diagnostics;
+
+        public EmbeddedSyntaxTrivia(int rawKind, ImmutableArray<VirtualChar> virtualChars)
+            : this(rawKind, virtualChars, ImmutableArray<EmbeddedDiagnostic>.Empty)
+        {
+        }
+
+
+        public EmbeddedSyntaxTrivia(int rawKind, ImmutableArray<VirtualChar> virtualChars, ImmutableArray<EmbeddedDiagnostic> diagnostics)
+        {
+            Debug.Assert(virtualChars.Length > 0);
+            RawKind = rawKind;
+            VirtualChars = virtualChars;
+            Diagnostics = diagnostics;
+        }
+    }
+}
