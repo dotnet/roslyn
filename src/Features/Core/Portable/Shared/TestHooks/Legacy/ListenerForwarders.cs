ce44aa778d811e90e3728a523a86c01c75d32a58
Sam Harwell <sam.harwell@microsoft.com>
2018-03-17 02:17:15 -0500

    Move the listener/waiter test hooks to the workspaces layer

---
+++ src/Features/Core/Portable/Shared/TestHooks/Legacy/ListenerForwarders.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.AsynchronousOperationListener))]
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.AsynchronousOperationListenerProvider))]
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.EmptyAsyncToken))]
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.IAsynchronousOperationListenerProvider))]
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.IAsynchronousOperationWaiter))]
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.IAsynchronousOperationListener))]
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.IAsyncToken))]
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.TaskExtensions))]
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Shared.TestHooks.FeatureMetadata))]
+#pragma warning restore CS0618 // Type or member is obsolete
