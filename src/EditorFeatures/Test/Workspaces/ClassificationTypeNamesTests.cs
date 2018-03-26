d1770cc9c2214b0db6a3e68b6742223849e20db4
Sam Harwell <sam.harwell@microsoft.com>
2018-03-26 14:27:12 -0500

    Use IClassificationTypeRegistryService instead of faking the behavior
    This change suggested by @jasonmalinowski makes the ClassificationTypeExported test
    simpler and more accurate.

--- src/EditorFeatures/Test/Workspaces/ClassificationTypeNamesTests.cs
+++ src/EditorFeatures/Test/Workspaces/ClassificationTypeNamesTests.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Reflection;
 using Microsoft.CodeAnalysis.Classification;
 using Microsoft.VisualStudio.Text.Classification;
@@ -29,26 +27,11 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces
         [WorkItem(25716, "https://github.com/dotnet/roslyn/issues/25716")]
         public void ClassificationTypeExported(string fieldName, object constantValue)
         {
-            Assert.IsType<string>(constantValue);
-            string classificationTypeName = (string)constantValue;
+            var classificationTypeName = Assert.IsType<string>(constantValue);
             var exportProvider = TestExportProvider.ExportProviderWithCSharpAndVisualBasic;
-            var exports = exportProvider.GetExports<ClassificationTypeDefinition, IClassificationTypeDefinitionMetadata>();
-            var export = exports.FirstOrDefault(x => x.Metadata.Name == classificationTypeName);
-            Assert.True(export != null, $"{nameof(ClassificationTypeNames)}.{fieldName} has value \"{classificationTypeName}\", but no matching {nameof(ClassificationTypeDefinition)} was exported.");
-        }
-
-        public interface IClassificationTypeDefinitionMetadata
-        {
-            string Name
-            {
-                get;
-            }
-
-            [DefaultValue(null)]
-            IEnumerable<string> BaseDefinition
-            {
-                get;
-            }
+            var classificationTypeRegistryService = exportProvider.GetExport<IClassificationTypeRegistryService>().Value;
+            var classificationType = classificationTypeRegistryService.GetClassificationType(classificationTypeName);
+            Assert.True(classificationType != null, $"{nameof(ClassificationTypeNames)}.{fieldName} has value \"{classificationTypeName}\", but no matching {nameof(ClassificationTypeDefinition)} was exported.");
         }
     }
 }
