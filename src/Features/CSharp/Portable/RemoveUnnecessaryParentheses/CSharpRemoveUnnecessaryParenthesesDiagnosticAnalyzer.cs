7d90d3f17821d70e5282f4cea52498bb42b5ab6b
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-03-26 17:30:54 -0700

    Indent switches.

--- src/Features/CSharp/Portable/RemoveUnnecessaryParentheses/CSharpRemoveUnnecessaryParenthesesDiagnosticAnalyzer.cs
+++ src/Features/CSharp/Portable/RemoveUnnecessaryParentheses/CSharpRemoveUnnecessaryParenthesesDiagnosticAnalyzer.cs
@@ -44,50 +44,50 @@ namespace Microsoft.CodeAnalysis.CSharp.RemoveUnnecessaryParentheses
 
             switch (parenthesizedExpression.Parent)
             {
-            case ConditionalExpressionSyntax _:
-                // If our parent is a conditional, then only remove parens if the inner
-                // expression is a primary. i.e. it's ok to remove any of the following:
-                //
-                //      (a()) ? (b.length) : (c[0])
-                //
-                // But we shouldn't remove parens for anything more complex like:
-                //
-                //      ++a ? b + c : d << e
-                //
-                precedenceKind = PrecedenceKind.Other;
-                clarifiesPrecedence = false;
-                return innerIsSimple;
+                case ConditionalExpressionSyntax _:
+                    // If our parent is a conditional, then only remove parens if the inner
+                    // expression is a primary. i.e. it's ok to remove any of the following:
+                    //
+                    //      (a()) ? (b.length) : (c[0])
+                    //
+                    // But we shouldn't remove parens for anything more complex like:
+                    //
+                    //      ++a ? b + c : d << e
+                    //
+                    precedenceKind = PrecedenceKind.Other;
+                    clarifiesPrecedence = false;
+                    return innerIsSimple;
 
-            case CastExpressionSyntax _:
-                // if our parent is a cast, then we may have something like: (int)(-x)
-                // While the latter parens aren't necessary, they can help clarify precedence
-                // as '(int)-x' looks somewhat like a binary expression.
-                precedenceKind = PrecedenceKind.Cast;
-                clarifiesPrecedence = IsAmbiguousInnerExpressionKindForCast(innerKind);
-                return true;
+                case CastExpressionSyntax _:
+                    // if our parent is a cast, then we may have something like: (int)(-x)
+                    // While the latter parens aren't necessary, they can help clarify precedence
+                    // as '(int)-x' looks somewhat like a binary expression.
+                    precedenceKind = PrecedenceKind.Cast;
+                    clarifiesPrecedence = IsAmbiguousInnerExpressionKindForCast(innerKind);
+                    return true;
 
-            case AssignmentExpressionSyntax assignmentExpression:
-                parentExpression = assignmentExpression;
-                break;
+                case AssignmentExpressionSyntax assignmentExpression:
+                    parentExpression = assignmentExpression;
+                    break;
 
-            case BinaryExpressionSyntax binaryExpression:
-                parentExpression = binaryExpression;
-                break;
+                case BinaryExpressionSyntax binaryExpression:
+                    parentExpression = binaryExpression;
+                    break;
 
-            case IsPatternExpressionSyntax isPatternExpression:
-                // on the left side of an 'x is pat' expression
-                parentExpression = isPatternExpression;
-                break;
+                case IsPatternExpressionSyntax isPatternExpression:
+                    // on the left side of an 'x is pat' expression
+                    parentExpression = isPatternExpression;
+                    break;
 
-            case ConstantPatternSyntax constantPattern when constantPattern.Parent is IsPatternExpressionSyntax isPatternExpression:
-                // on the right side of an 'x is const_pattern' expression
-                parentExpression = isPatternExpression;
-                break;
+                case ConstantPatternSyntax constantPattern when constantPattern.Parent is IsPatternExpressionSyntax isPatternExpression:
+                    // on the right side of an 'x is const_pattern' expression
+                    parentExpression = isPatternExpression;
+                    break;
 
-            default:
-                precedenceKind = PrecedenceKind.Other;
-                clarifiesPrecedence = false;
-                return true;
+                default:
+                    precedenceKind = PrecedenceKind.Other;
+                    clarifiesPrecedence = false;
+                    return true;
             }
 
             // We're parented by something binary-like. 
@@ -118,18 +118,18 @@ namespace Microsoft.CodeAnalysis.CSharp.RemoveUnnecessaryParentheses
             var precedence = parentExpression.GetOperatorPrecedence();
             switch (precedence)
             {
-            case OperatorPrecedence.NullCoalescing: return PrecedenceKind.Coalesce;
-            case OperatorPrecedence.ConditionalOr:
-            case OperatorPrecedence.ConditionalAnd: return PrecedenceKind.Logical;
-            case OperatorPrecedence.LogicalOr:
-            case OperatorPrecedence.LogicalXor:
-            case OperatorPrecedence.LogicalAnd: return PrecedenceKind.Bitwise;
-            case OperatorPrecedence.Equality: return PrecedenceKind.Equality;
-            case OperatorPrecedence.RelationalAndTypeTesting: return PrecedenceKind.Relational;
-            case OperatorPrecedence.Shift: return PrecedenceKind.Shift;
-            case OperatorPrecedence.Additive:
-            case OperatorPrecedence.Multiplicative: return PrecedenceKind.Arithmetic;
-            case OperatorPrecedence.AssignmentAndLambdaExpression: return PrecedenceKind.Assignment;
+                case OperatorPrecedence.NullCoalescing: return PrecedenceKind.Coalesce;
+                case OperatorPrecedence.ConditionalOr:
+                case OperatorPrecedence.ConditionalAnd: return PrecedenceKind.Logical;
+                case OperatorPrecedence.LogicalOr:
+                case OperatorPrecedence.LogicalXor:
+                case OperatorPrecedence.LogicalAnd: return PrecedenceKind.Bitwise;
+                case OperatorPrecedence.Equality: return PrecedenceKind.Equality;
+                case OperatorPrecedence.RelationalAndTypeTesting: return PrecedenceKind.Relational;
+                case OperatorPrecedence.Shift: return PrecedenceKind.Shift;
+                case OperatorPrecedence.Additive:
+                case OperatorPrecedence.Multiplicative: return PrecedenceKind.Arithmetic;
+                case OperatorPrecedence.AssignmentAndLambdaExpression: return PrecedenceKind.Assignment;
             }
 
             throw ExceptionUtilities.UnexpectedValue(precedence);
