34a4ae4fba2507ad3d07d2bc698c136168bb61e1
Caio Saldanha <t-camaia@microsoft.com>
2018-03-29 14:56:49 -0700

    Remove redundant calls to DecodeWellKnownAttribute

--- src/Compilers/VisualBasic/Portable/Symbols/Source/SourceMethodSymbol.vb
+++ src/Compilers/VisualBasic/Portable/Symbols/Source/SourceMethodSymbol.vb
@@ -1732,8 +1732,6 @@ lReportErrorOnTwoTokens:
                         Return
                     End If
                 End If
-
-                MyBase.DecodeWellKnownAttribute(arguments)
             End If
         End Sub
 
@@ -1760,8 +1758,6 @@ lReportErrorOnTwoTokens:
 
             If attrData.IsTargetAttribute(Me, AttributeDescription.MarshalAsAttribute) Then
                 MarshalAsAttributeDecoder(Of CommonReturnTypeWellKnownAttributeData, AttributeSyntax, VisualBasicAttributeData, AttributeLocation).Decode(arguments, AttributeTargets.ReturnValue, MessageProvider.Instance)
-            Else
-                MyBase.DecodeWellKnownAttribute(arguments)
             End If
         End Sub
 
