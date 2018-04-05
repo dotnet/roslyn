4068e59679cb325b1a4903dac421222cc9e11488
Manish Vasani <mavasani@microsoft.com>
2018-04-04 17:10:55 -0700

    Performance and reliability fixes for flow analysis
    1. Combine Null and PointsToAnalysis into a single analysis. Null state for entities is now tracked as part of PointsTo analysis.
    2. Fix the hang for the new DFA rules by being more conservating in tracking PointsTo values during merge.
    3. Remove lot of unncessary null checks in Merge and Compare functions and add asserts to validate inputs and output of these functions.

--- src/Utilities/Extensions/ITypeSymbolExtensions.cs
+++ src/Utilities/Extensions/ITypeSymbolExtensions.cs
@@ -165,6 +165,12 @@ namespace Analyzer.Utilities.Extensions
         public static bool IsNonNullableValueType(this ITypeSymbol typeSymbol)
             => typeSymbol != null && typeSymbol.IsValueType && typeSymbol.OriginalDefinition.SpecialType != SpecialType.System_Nullable_T;
 
+        public static bool IsNullableValueType(this ITypeSymbol typeSymbol)
+            => typeSymbol != null && typeSymbol.IsValueType && typeSymbol.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T;
+
+        public static bool IsReferenceTypeOrNullableValueType(this ITypeSymbol typeSymbol)
+            => typeSymbol != null && (typeSymbol.IsReferenceType || typeSymbol.IsNullableValueType());
+
         public static Accessibility DetermineMinimalAccessibility(this ITypeSymbol typeSymbol)
         {
             return typeSymbol.Accept(MinimalAccessibilityVisitor.Instance);
