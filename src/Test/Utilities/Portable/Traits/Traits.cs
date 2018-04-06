bfb9edaabf5e03ec42c42e55e64f6d5fed254360
Alireza Habibi <habibi.alireza@outlook.com>
2018-04-06 17:58:50 +0430

    Revert code.

--- src/Test/Utilities/Portable/Traits/Traits.cs
+++ src/Test/Utilities/Portable/Traits/Traits.cs
@@ -55,7 +55,6 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
             public const string CodeActionsConvertForEachToFor = "CodeActions.ConvertForEachToFor";
             public const string CodeActionsConvertIfToSwitch = "CodeActions.ConvertIfToSwitch";
             public const string CodeActionsConvertQueryToForEach = "CodeActions.ConvertQueryToForEach";
-            public const string CodeActionsConvertLocalFunctionToMethod = "CodeActions.ConvertLocalFunctionToMethod";
             public const string CodeActionsCorrectExitContinue = "CodeActions.CorrectExitContinue";
             public const string CodeActionsCorrectFunctionReturnType = "CodeActions.CorrectFunctionReturnType";
             public const string CodeActionsCorrectNextControlVariable = "CodeActions.CorrectNextControlVariable";
