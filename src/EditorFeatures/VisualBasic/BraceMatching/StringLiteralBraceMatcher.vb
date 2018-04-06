a16e0b5f2b6ddb6c807069cd46036b1e6b55d52b
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-04-05 20:28:17 -0700

    Indent.

--- src/EditorFeatures/VisualBasic/BraceMatching/StringLiteralBraceMatcher.vb
+++ src/EditorFeatures/VisualBasic/BraceMatching/StringLiteralBraceMatcher.vb
@@ -18,8 +18,8 @@ Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.BraceMatching
             If position = token.SpanStart OrElse position = token.Span.End - 1 Then
                 If token.Kind = SyntaxKind.StringLiteralToken AndAlso Not token.ContainsDiagnostics Then
                     Return New BraceMatchingResult(
-                    New TextSpan(token.SpanStart, 1),
-                    New TextSpan(token.Span.End - 1, 1))
+                        New TextSpan(token.SpanStart, 1),
+                        New TextSpan(token.Span.End - 1, 1))
                 End If
             End If
 
