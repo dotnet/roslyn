ddbbeb639e8531f07d277efe847b90cc29dab0ec
AdamSpeight2008 <adam.speight@virgin.net>
2018-04-10 03:16:03 +0100

    Add StringComparers for FileSystemNameComparer and IdentifierComparer.

---
+++ src/Compilers/Core/Portable/StringComparers.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis
+{
+    public static class StringComparers
+    {
+       public static StringComparer FileSystemNameComparer =  StringComparer.OrdinalIgnoreCase;
+       public static StringComparer IdentifierComparer = CaseInsensitiveComparison.Comparer; 
+    }
+}
