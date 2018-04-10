d81a0072a89b543b2b0f036322a5fbd90998eb76
Andy Gocke <angocke@microsoft.com>
2018-04-09 23:32:23 -0700

    Remove extraneous SpecializedCollections.Empty code

--- src/Compilers/Core/Portable/InternalUtilities/SpecializedCollections.Empty.Set.cs
+++ src/Compilers/Core/Portable/InternalUtilities/SpecializedCollections.Empty.Set.cs
@@ -1,76 +1,21 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-
 namespace Roslyn.Utilities
 {
     internal partial class SpecializedCollections
     {
         private partial class Empty
         {
-            internal class Set<T> : Collection<T>, ISet<T>, IReadOnlySet<T>
+            internal class ReadOnlySet<T> : IReadOnlySet<T>
             {
-                public static readonly new Set<T> Instance = new Set<T>();
+                public static readonly ReadOnlySet<T> Instance = new ReadOnlySet<T>();
 
-                protected Set()
-                {
-                }
+                private ReadOnlySet()
+                { }
 
-                public new bool Add(T item)
-                {
-                    throw new NotSupportedException();
-                }
+                public int Count => 0;
 
-                public void ExceptWith(IEnumerable<T> other)
-                {
-                    throw new NotSupportedException();
-                }
-
-                public void IntersectWith(IEnumerable<T> other)
-                {
-                    throw new NotSupportedException();
-                }
-
-                public bool IsProperSubsetOf(IEnumerable<T> other)
-                {
-                    return !other.IsEmpty();
-                }
-
-                public bool IsProperSupersetOf(IEnumerable<T> other)
-                {
-                    return false;
-                }
-
-                public bool IsSubsetOf(IEnumerable<T> other)
-                {
-                    return true;
-                }
-
-                public bool IsSupersetOf(IEnumerable<T> other)
-                {
-                    return other.IsEmpty();
-                }
-
-                public bool Overlaps(IEnumerable<T> other)
-                {
-                    return false;
-                }
-
-                public bool SetEquals(IEnumerable<T> other)
-                {
-                    return other.IsEmpty();
-                }
-
-                public void SymmetricExceptWith(IEnumerable<T> other)
-                {
-                    throw new NotSupportedException();
-                }
-
-                public void UnionWith(IEnumerable<T> other)
-                {
-                    throw new NotSupportedException();
-                }
+                public bool Contains(T item) => false;
             }
         }
     }
