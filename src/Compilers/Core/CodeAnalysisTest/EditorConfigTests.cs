70c9bebcee6844f8b4598ecb57a9ad69d0df7bc0
Andy Gocke <angocke@microsoft.com>
2018-04-16 12:05:48 -0500

    Add editorconfig parser and unit tests
    This is the same parser used by Visual Studio for editorconfig with a
    minor modification to use case-insensitive comparison for property keys.

---
+++ src/Compilers/Core/CodeAnalysisTest/EditorConfigTests.cs
@@ -0,0 +1,217 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Linq;
+using Roslyn.Test.Utilities;
+using Roslyn.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.UnitTests
+{
+    public class EditorConfigTests
+    {
+        private static EditorConfig ParseConfigFile(string text) => EditorConfig.Parse(text, "");
+
+        [Fact]
+        public void SimpleCase()
+        {
+            var config = EditorConfig.Parse(@"
+root = true
+
+# Comment1
+# Comment2
+##################################
+
+my_global_prop = my_global_val
+
+[*.cs]
+my_prop = my_val
+
+", "Z:\\bogus");
+
+            Assert.Equal("", config.GlobalSection.Name);
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(2, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_global_prop", "my_global_val"),
+                        KeyValuePair.Create("root", "true") },
+                properties);
+
+            var namedSections = config.NamedSections;
+            Assert.Equal(1, namedSections.Count);
+            Assert.Equal("*.cs", namedSections[0].Name);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop", "my_val")},
+                namedSections[0].Properties);
+            
+            Assert.True(config.IsRoot);
+            Assert.Equal("Z:\\bogus", config.Directory);
+        }
+
+        [Fact]
+        public void MissingClosingBracket()
+        {
+            var config = ParseConfigFile(@"
+[*.cs
+my_prop = my_val");
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop", "my_val")},
+                properties);
+
+            Assert.Equal(0, config.NamedSections.Count);
+        }
+
+        [Fact]
+        public void CaseInsensitivePropKey()
+        {
+            var config = ParseConfigFile(@"
+my_PROP = my_VAL");
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+
+            Assert.True(properties.TryGetValue("my_prop", out var val));
+            Assert.Equal(val, "my_VAL");
+            Assert.Equal("my_PROP", properties.Keys.Single());
+        }
+
+        [Fact]
+        public void DuplicateKeys()
+        {
+            var config = ParseConfigFile(@"
+my_prop = my_val
+my_prop = my_other_val");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+            Assert.Equal(new[] { KeyValuePair.Create("my_prop", "my_other_val") }, properties);
+        }
+
+        [Fact]
+        public void DuplicateKeysCasing()
+        {
+            var config = ParseConfigFile(@"
+my_prop = my_val
+my_PROP = my_other_val");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+            Assert.Equal(new[] { KeyValuePair.Create("my_prop", "my_other_val") }, properties);
+        }
+
+        [Fact]
+        public void MissingKey()
+        {
+            var config = ParseConfigFile(@"
+= my_val1
+my_prop = my_val2");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop", "my_val2") },
+                properties);
+        }
+
+        [Fact]
+        public void MissingVal()
+        {
+            var config = ParseConfigFile(@"
+my_prop1 =
+my_prop2 = my_val");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(2, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop1", ""),
+                        KeyValuePair.Create("my_prop2", "my_val") },
+                properties);
+        }
+
+        [Fact]
+        public void SpacesInProperties()
+        {
+            var config = ParseConfigFile(@"
+my prop1 = my_val1
+my_prop2 = my val2");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop2", "my val2") },
+                properties);
+        }
+
+        [Fact]
+        public void EndOfLineComments()
+        {
+            var config = ParseConfigFile(@"
+my_prop2 = my val2 # Comment");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop2", "my val2") },
+                properties);
+        }
+
+        [Fact]
+        public void SymbolsStartKeys()
+        {
+            var config = ParseConfigFile(@"
+@!$abc = my_val1
+@!$\# = my_val2");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(0, properties.Count);
+        }
+
+        [Fact]
+        public void EqualsAndColon()
+        {
+            var config = ParseConfigFile(@"
+my:key1 = my_val
+my_key2 = my:val");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(2, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my", "key1 = my_val"),
+                        KeyValuePair.Create("my_key2", "my:val")},
+                properties);
+        }
+
+        [Fact]
+        public void SymbolsInProperties()
+        {
+            var config = ParseConfigFile(@"
+my@key1 = my_val
+my_key2 = my@val");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_key2", "my@val")},
+                properties);
+        }
+
+        [Fact]
+        public void LongLines()
+        {
+            // PROTOTYPE(editorconfig): This example is described in the Python ConfigParser as
+            // allowing line continuation via the RFC 822 specification, section 3.1.1 LONG
+            // HEADER FIELDS. It's unclear whether it's intended for editorconfig to permit
+            // this interpretation. The VS parser does not. We should probably check other
+            // parsers for completeness.
+            var config = ParseConfigFile(@"
+long: this value continues
+   in the next line");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(1, properties.Count);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("long", "this value continues")},
+                properties);
+        }
+    }
+}
---
+++ src/Compilers/Core/Portable/CommandLine/EditorConfig.cs
@@ -0,0 +1,127 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.CodeAnalysis
+{
+    /// <summary>
+    /// Represents a single EditorConfig file, see http://editorconfig.org for details about the format.
+    /// </summary>
+    internal class EditorConfig
+    {
+        // Matches EditorConfig section header such as "[*.{js,py}]", see http://editorconfig.org for details
+        private static Regex _sectionMatcher = new Regex(@"^\s*\[(([^#;]|\\#|\\;)+)\]\s*([#;].*)?$", RegexOptions.Compiled);
+        // Matches EditorConfig property such as "indent_style = space", see http://editorconfig.org for details
+        private static Regex _propertyMatcher = new Regex(@"^\s*([\w\.\-_]+)\s*[=:]\s*(.*?)\s*([#;].*)?$", RegexOptions.Compiled);
+
+        public Section GlobalSection = new Section(string.Empty);
+
+        public string Directory { get; private set; }
+
+        public IList<Section> NamedSections = new List<Section>();
+
+        private EditorConfig()
+        {
+        }
+
+        /// <summary>
+        /// Gets whether this editorconfig is a the topmost editorconfig.
+        /// </summary>
+        public bool IsRoot
+        {
+            get
+            {
+                return this.GlobalSection.Properties.ContainsKey("root") && this.GlobalSection.Properties["root"] == "true";
+            }
+        }
+
+        public static EditorConfig Parse(string text, string editorConfigParentDirectory)
+        {
+            var editorConfig = new EditorConfig();
+            editorConfig.Directory = editorConfigParentDirectory;
+            var activeSection = editorConfig.GlobalSection;
+            using(var reader = new StringReader(text))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+
+                    if (IsComment(line))
+                    {
+                        continue;
+                    }
+
+                    var sectionMatches = _sectionMatcher.Matches(line);
+                    if (sectionMatches.Count > 0 && sectionMatches[0].Groups.Count > 0)
+                    {
+                        var sectionName = sectionMatches[0].Groups[1].Value;
+                        if (!string.IsNullOrEmpty(sectionName))
+                        {
+                            activeSection = new Section(sectionName);
+                            editorConfig.NamedSections.Add(activeSection);
+                        }
+                        continue;
+                    }
+
+                    var propMatches = _propertyMatcher.Matches(line);
+                    if (propMatches.Count > 0 && propMatches[0].Groups.Count > 1)
+                    {
+                        var key = propMatches[0].Groups[1].Value;
+                        var value = propMatches[0].Groups[2].Value;
+                        if (!string.IsNullOrEmpty(key))
+                        {
+                            activeSection.Properties[key.Trim()] = value?.Trim();
+                        }
+                        continue;
+                    }
+                }
+            }
+
+            return editorConfig;
+        }
+
+        private static bool IsComment(string line)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                char c = line[i];
+                if (!Char.IsWhiteSpace(c))
+                {
+                    return c == '#' || c == ';';
+                }
+            }
+
+            return false;
+        }
+
+        internal class Section
+        {
+            public Section(string name)
+            {
+                this.Name = name;
+                // N.B. The editorconfig documentation is quite loose on property interpretation.
+                // Specifically, it says:
+                //      Currently all properties and values are case-insensitive.
+                //      They are lowercased when parsed.
+                // This is not a mandate for case-insensitive parsing, but a strong suggestion.
+                // As for *which* case-insensitive comparison, we use CaseInsensitiveComparison.Comparer,
+                // which is the recommended Unicode comparison for case-insensitive identifiers. The
+                // implementation performs the equivalent of a lower-case comparsion. The
+                // editorconfig file is defined as using a UTF-8 encoding, making Unicode comparison
+                // relevant.
+                this.Properties = new Dictionary<string, string>(CaseInsensitiveComparison.Comparer);
+            }
+
+            public string Name { get; }
+
+            public IDictionary<string, string> Properties { get; }
+        }
+    }
+}
