6a911b3aed05b8519e995c3a02c6e7d8f8d86f1f
Johan Larsson <johan_gavle1@hotmail.com>
2018-04-16 23:15:23 +0200

    Merge tests.

--- src/EditorFeatures/CSharpTest/UseNameOf/UseNameOfTests.cs
+++ src/EditorFeatures/CSharpTest/UseNameOf/UseNameOfTests.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.UseNameOf
 {
-    public partial class UseNameOfTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
+    public class UseNameOfTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
     {
         internal override (DiagnosticAnalyzer, CodeFixProvider) CreateDiagnosticProviderAndFixer(Workspace workspace)
             => (new CSharpUseNameOfDiagnosticAnalyzer(), new CSharpUseNameofCodeFixProvider());
@@ -165,7 +165,6 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.UseNameOf
 }");
         }
 
-
         [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseNameOf)]
         public async Task InstanceFieldInitializer()
         {
@@ -200,6 +199,77 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.UseNameOf
     public Foo()
     {
         var text = nameof(this.bar);
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseNameOf)]
+        public async Task IgnoreDebuggerDisplay()
+        {
+            await TestMissingAsync(
+                @"
+[System.Diagnostics.DebuggerDisplay(""{Name}"")]
+public class Foo
+{
+    public string Name { get; }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseNameOf)]
+        public async Task IgnoreTypeName()
+        {
+            await TestMissingAsync(
+                @"
+public class Foo
+{
+    public void Bar()
+    {
+        var text = Id(""Foo"");
+    }
+
+    private static string Id(string value) => value;
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseNameOf)]
+        public async Task IgnoreLocalWhenNotInScope()
+        {
+            await TestMissingAsync(
+                @"
+public class Foo
+{
+    public Foo()
+    {
+        {
+            var text = Id(""text"");
+        }
+        {
+            var text = Id(""text"");
+        }
+        {
+            var text = Id(""text"");
+        }
+    }
+
+    private static string Id(string value) => value;
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseNameOf)]
+        public async Task IgnoreNamespaceName()
+        {
+            await TestMissingAsync(
+                @"
+namespace Namespace
+{
+    public class Foo
+    {
+        public Foo()
+        {
+            var text = Id(""Namespace"");
+        }
+
+        private static string Id(string value) => value;
     }
 }");
         }
