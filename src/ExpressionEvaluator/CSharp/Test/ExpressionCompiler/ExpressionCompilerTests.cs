a31a456b87bbecbdeae42439bcf125e450579a95
Ivan Basov <basov@hotmail.com>
2018-04-20 16:05:25 -0700

    Compiler doesnt recognise property in generic if declaration is an interface

--- src/ExpressionEvaluator/CSharp/Test/ExpressionCompiler/ExpressionCompilerTests.cs
+++ src/ExpressionEvaluator/CSharp/Test/ExpressionCompiler/ExpressionCompilerTests.cs
@@ -2108,6 +2108,56 @@ class C<T>
         }
 
         [Fact]
+        public void GenericWithInterfaceConstraint()
+        {
+            var source =
+@"public interface IFoo
+{
+    string Key { get; set; }
+}
+
+public class Foo : IFoo
+{
+    public string Key { get; set; }
+}
+
+class C
+{
+    public static void M<T>(T foo) where T : IFoo
+    {
+    }
+}";
+            var compilation0 = CreateCompilation(source, options: TestOptions.DebugDll);
+            WithRuntimeInstance(compilation0, runtime =>
+            {
+                var context = CreateMethodContext(runtime, "C.M");
+                string error;
+                var testData = new CompilationTestData();
+                context.CompileExpression(
+                    expr: "foo.Key",
+                    error: out error,
+                    testData: testData);
+
+                var methodData = testData.GetMethodData("<>x.<>m0<T>(T)");
+                var method = methodData.Method;
+                Assert.Equal(1, method.Parameters.Length);
+                var eeTypeParameterSymbol = (EETypeParameterSymbol)method.Parameters[0].Type;
+                Assert.Equal(1, eeTypeParameterSymbol.AllEffectiveInterfacesNoUseSiteDiagnostics.Length);
+                Assert.Equal("IFoo", eeTypeParameterSymbol.AllEffectiveInterfacesNoUseSiteDiagnostics[0].Name);
+
+                methodData.VerifyIL(
+@"{
+  // Code size       14 (0xe)
+  .maxstack  1
+  IL_0000:  ldarga.s   V_0
+  IL_0002:  constrained. ""T""
+  IL_0008:  callvirt   ""string IFoo.Key.get""
+  IL_000d:  ret
+}");
+            });
+        }
+
+        [Fact]
         public void AssignEmitError()
         {
             var longName = new string('P', 1100);
