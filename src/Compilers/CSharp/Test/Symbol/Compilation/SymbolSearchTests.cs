a6628b50e912987d29b81fa55039cbf841e533ae
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-04-22 13:01:19 -0700

    C# tests.

--- src/Compilers/CSharp/Test/Symbol/Compilation/SymbolSearchTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Compilation/SymbolSearchTests.cs
@@ -24,6 +24,12 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
                 var compilation = GetTestCompilation();
                 compilation.GetSymbolsWithName(n => true, SymbolFilter.None);
             });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var compilation = GetTestCompilation();
+                compilation.GetSymbolsWithName("", SymbolFilter.None);
+            });
         }
 
         [Fact]
@@ -38,7 +44,17 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
             Assert.Throws<ArgumentNullException>(() =>
             {
                 var compilation = GetTestCompilation();
-                compilation.GetSymbolsWithName(null);
+                compilation.GetSymbolsWithName(predicate: null);
+            });
+        }
+
+        [Fact]
+        public void TestNameNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var compilation = GetTestCompilation();
+                compilation.GetSymbolsWithName(name: null);
             });
         }
 
@@ -47,14 +63,14 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
         {
             var compilation = GetTestCompilation();
 
-            Test(compilation, n => n == "System", includeNamespace: true, includeType: false, includeMember: false, count: 1);
-            Test(compilation, n => n == "System", includeNamespace: true, includeType: true, includeMember: false, count: 1);
-            Test(compilation, n => n == "System", includeNamespace: true, includeType: false, includeMember: true, count: 1);
-            Test(compilation, n => n == "System", includeNamespace: true, includeType: true, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "System", includeNamespace: true, includeType: false, includeMember: false, count: 1);
+            TestNameAndPredicate(compilation, "System", includeNamespace: true, includeType: true, includeMember: false, count: 1);
+            TestNameAndPredicate(compilation, "System", includeNamespace: true, includeType: false, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "System", includeNamespace: true, includeType: true, includeMember: true, count: 1);
 
-            Test(compilation, n => n == "System", includeNamespace: false, includeType: false, includeMember: true, count: 0);
-            Test(compilation, n => n == "System", includeNamespace: false, includeType: true, includeMember: false, count: 0);
-            Test(compilation, n => n == "System", includeNamespace: false, includeType: true, includeMember: true, count: 0);
+            TestNameAndPredicate(compilation, "System", includeNamespace: false, includeType: false, includeMember: true, count: 0);
+            TestNameAndPredicate(compilation, "System", includeNamespace: false, includeType: true, includeMember: false, count: 0);
+            TestNameAndPredicate(compilation, "System", includeNamespace: false, includeType: true, includeMember: true, count: 0);
         }
 
         [Fact]
@@ -62,14 +78,14 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
         {
             var compilation = GetTestCompilation();
 
-            Test(compilation, n => n == "MyNamespace", includeNamespace: true, includeType: false, includeMember: false, count: 1);
-            Test(compilation, n => n == "MyNamespace", includeNamespace: true, includeType: true, includeMember: false, count: 1);
-            Test(compilation, n => n == "MyNamespace", includeNamespace: true, includeType: false, includeMember: true, count: 1);
-            Test(compilation, n => n == "MyNamespace", includeNamespace: true, includeType: true, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "MyNamespace", includeNamespace: true, includeType: false, includeMember: false, count: 1);
+            TestNameAndPredicate(compilation, "MyNamespace", includeNamespace: true, includeType: true, includeMember: false, count: 1);
+            TestNameAndPredicate(compilation, "MyNamespace", includeNamespace: true, includeType: false, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "MyNamespace", includeNamespace: true, includeType: true, includeMember: true, count: 1);
 
-            Test(compilation, n => n == "MyNamespace", includeNamespace: false, includeType: false, includeMember: true, count: 0);
-            Test(compilation, n => n == "MyNamespace", includeNamespace: false, includeType: true, includeMember: false, count: 0);
-            Test(compilation, n => n == "MyNamespace", includeNamespace: false, includeType: true, includeMember: true, count: 0);
+            TestNameAndPredicate(compilation, "MyNamespace", includeNamespace: false, includeType: false, includeMember: true, count: 0);
+            TestNameAndPredicate(compilation, "MyNamespace", includeNamespace: false, includeType: true, includeMember: false, count: 0);
+            TestNameAndPredicate(compilation, "MyNamespace", includeNamespace: false, includeType: true, includeMember: true, count: 0);
         }
 
         [Fact]
@@ -77,14 +93,14 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
         {
             var compilation = GetTestCompilation();
 
-            Test(compilation, n => n == "Test", includeNamespace: false, includeType: true, includeMember: false, count: 1);
-            Test(compilation, n => n == "Test", includeNamespace: false, includeType: true, includeMember: true, count: 1);
-            Test(compilation, n => n == "Test", includeNamespace: true, includeType: true, includeMember: false, count: 1);
-            Test(compilation, n => n == "Test", includeNamespace: true, includeType: true, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "Test", includeNamespace: false, includeType: true, includeMember: false, count: 1);
+            TestNameAndPredicate(compilation, "Test", includeNamespace: false, includeType: true, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "Test", includeNamespace: true, includeType: true, includeMember: false, count: 1);
+            TestNameAndPredicate(compilation, "Test", includeNamespace: true, includeType: true, includeMember: true, count: 1);
 
-            Test(compilation, n => n == "Test", includeNamespace: false, includeType: false, includeMember: true, count: 0);
-            Test(compilation, n => n == "Test", includeNamespace: true, includeType: false, includeMember: false, count: 0);
-            Test(compilation, n => n == "Test", includeNamespace: true, includeType: false, includeMember: true, count: 0);
+            TestNameAndPredicate(compilation, "Test", includeNamespace: false, includeType: false, includeMember: true, count: 0);
+            TestNameAndPredicate(compilation, "Test", includeNamespace: true, includeType: false, includeMember: false, count: 0);
+            TestNameAndPredicate(compilation, "Test", includeNamespace: true, includeType: false, includeMember: true, count: 0);
         }
 
         [Fact]
@@ -92,14 +108,14 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
         {
             var compilation = GetTestCompilation();
 
-            Test(compilation, n => n == "Test1", includeNamespace: false, includeType: true, includeMember: false, count: 1);
-            Test(compilation, n => n == "Test1", includeNamespace: false, includeType: true, includeMember: true, count: 1);
-            Test(compilation, n => n == "Test1", includeNamespace: true, includeType: true, includeMember: false, count: 1);
-            Test(compilation, n => n == "Test1", includeNamespace: true, includeType: true, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "Test1", includeNamespace: false, includeType: true, includeMember: false, count: 1);
+            TestNameAndPredicate(compilation, "Test1", includeNamespace: false, includeType: true, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "Test1", includeNamespace: true, includeType: true, includeMember: false, count: 1);
+            TestNameAndPredicate(compilation, "Test1", includeNamespace: true, includeType: true, includeMember: true, count: 1);
 
-            Test(compilation, n => n == "Test1", includeNamespace: false, includeType: false, includeMember: true, count: 0);
-            Test(compilation, n => n == "Test1", includeNamespace: true, includeType: false, includeMember: false, count: 0);
-            Test(compilation, n => n == "Test1", includeNamespace: true, includeType: false, includeMember: true, count: 0);
+            TestNameAndPredicate(compilation, "Test1", includeNamespace: false, includeType: false, includeMember: true, count: 0);
+            TestNameAndPredicate(compilation, "Test1", includeNamespace: true, includeType: false, includeMember: false, count: 0);
+            TestNameAndPredicate(compilation, "Test1", includeNamespace: true, includeType: false, includeMember: true, count: 0);
         }
 
         [Fact]
@@ -107,14 +123,14 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
         {
             var compilation = GetTestCompilation();
 
-            Test(compilation, n => n == "myField", includeNamespace: false, includeType: false, includeMember: true, count: 1);
-            Test(compilation, n => n == "myField", includeNamespace: false, includeType: true, includeMember: true, count: 1);
-            Test(compilation, n => n == "myField", includeNamespace: true, includeType: false, includeMember: true, count: 1);
-            Test(compilation, n => n == "myField", includeNamespace: true, includeType: true, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "myField", includeNamespace: false, includeType: false, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "myField", includeNamespace: false, includeType: true, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "myField", includeNamespace: true, includeType: false, includeMember: true, count: 1);
+            TestNameAndPredicate(compilation, "myField", includeNamespace: true, includeType: true, includeMember: true, count: 1);
 
-            Test(compilation, n => n == "myField", includeNamespace: false, includeType: true, includeMember: false, count: 0);
-            Test(compilation, n => n == "myField", includeNamespace: true, includeType: false, includeMember: false, count: 0);
-            Test(compilation, n => n == "myField", includeNamespace: true, includeType: true, includeMember: false, count: 0);
+            TestNameAndPredicate(compilation, "myField", includeNamespace: false, includeType: true, includeMember: false, count: 0);
+            TestNameAndPredicate(compilation, "myField", includeNamespace: true, includeType: false, includeMember: false, count: 0);
+            TestNameAndPredicate(compilation, "myField", includeNamespace: true, includeType: true, includeMember: false, count: 0);
         }
 
         [Fact]
@@ -195,6 +211,22 @@ enum Enum
 }
 ";
             return CreateCompilation(source: new string[] { source });
+        }
+
+        private static void TestNameAndPredicate(CSharpCompilation compilation, string name, bool includeNamespace, bool includeType, bool includeMember, int count)
+        {
+            Test(compilation, name, includeNamespace, includeType, includeMember, count);
+            Test(compilation, n => n == name, includeNamespace, includeType, includeMember, count);
+        }
+
+        private static void Test(CSharpCompilation compilation, string name, bool includeNamespace, bool includeType, bool includeMember, int count)
+        {
+            var filter = SymbolFilter.None;
+            filter = includeNamespace ? filter | SymbolFilter.Namespace : filter;
+            filter = includeType ? filter | SymbolFilter.Type : filter;
+            filter = includeMember ? filter | SymbolFilter.Member : filter;
+
+            Assert.Equal(count, compilation.GetSymbolsWithName(name, filter).Count());
         }
 
         private static void Test(CSharpCompilation compilation, Func<string, bool> predicate, bool includeNamespace, bool includeType, bool includeMember, int count)
