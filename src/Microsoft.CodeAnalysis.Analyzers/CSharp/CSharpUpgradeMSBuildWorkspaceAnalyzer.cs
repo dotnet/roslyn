9bdc05ab760a743b324539d923fa78b077b0605a
Dustin Campbell <dustin@teamcampbell.org>
2018-04-27 16:03:12 -0700

    Add an analyzer that reports a diagnostic when MSBuildWorkspace likely needs upgrading

---
+++ src/Microsoft.CodeAnalysis.Analyzers/CSharp/CSharpUpgradeMSBuildWorkspaceAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Analyzers;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.CSharp.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class CSharpUpgradeMSBuildWorkspaceAnalyzer : UpgradeMSBuildWorkspaceAnalyzer
+    {
+        private CSharpUpgradeMSBuildWorkspaceAnalyzer(bool performAssemblyChecks)
+            : base(performAssemblyChecks)
+        {
+        }
+
+        public CSharpUpgradeMSBuildWorkspaceAnalyzer()
+            : this(performAssemblyChecks: true)
+        {
+        }
+
+        internal static CSharpUpgradeMSBuildWorkspaceAnalyzer CreateForTests()
+            => new CSharpUpgradeMSBuildWorkspaceAnalyzer(performAssemblyChecks: false);
+
+        protected override void RegisterIdentifierAnalysis(CompilationStartAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzerIdentifier, SyntaxKind.IdentifierName);
+        }
+
+        protected override void RegisterIdentifierAnalysis(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzerIdentifier, SyntaxKind.IdentifierName);
+        }
+
+        private void AnalyzerIdentifier(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is IdentifierNameSyntax identifierName &&
+                identifierName.Identifier.ToString() == MSBuildWorkspace)
+            {
+                var symbolInfo = context.SemanticModel.GetSymbolInfo(identifierName);
+                if (symbolInfo.Symbol == null)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(UpgradeMSBuildWorkspaceDiagnosticRule, identifierName.GetLocation()));
+                }
+            }
+        }
+    }
+}
---
+++ src/Microsoft.CodeAnalysis.Analyzers/VisualBasic/VisualBasicUpgradeMSBuildWorkspaceAnalyzer.vb
@@ -0,0 +1,49 @@
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports Microsoft.CodeAnalysis.Analyzers
+Imports Microsoft.CodeAnalysis.Diagnostics
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+
+Namespace Microsoft.CodeAnalysis.VisualBasic.Analyzers
+    <DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+    Public Class VisualBasicUpgradeMSBuildWorkspaceAnalyzer
+        Inherits UpgradeMSBuildWorkspaceAnalyzer
+
+        Private Sub New(performAssemblyChecks As Boolean)
+            MyBase.New(performAssemblyChecks)
+        End Sub
+
+        Public Sub New()
+            Me.New(performAssemblyChecks:=True)
+        End Sub
+
+        Friend Shared Function CreateForTests() As VisualBasicUpgradeMSBuildWorkspaceAnalyzer
+            Return New VisualBasicUpgradeMSBuildWorkspaceAnalyzer(performAssemblyChecks:=False)
+        End Function
+
+        Protected Overrides Sub RegisterIdentifierAnalysis(context As CompilationStartAnalysisContext)
+            context.RegisterSyntaxNodeAction(AddressOf AnalyzerIdentifier, SyntaxKind.IdentifierName)
+        End Sub
+
+        Protected Overrides Sub RegisterIdentifierAnalysis(context As AnalysisContext)
+            context.RegisterSyntaxNodeAction(AddressOf AnalyzerIdentifier, SyntaxKind.IdentifierName)
+        End Sub
+
+        Private Sub AnalyzerIdentifier(context As SyntaxNodeAnalysisContext)
+            If TypeOf context.Node IsNot IdentifierNameSyntax Then
+                Return
+            End If
+
+            Dim identifierName = DirectCast(context.Node, IdentifierNameSyntax)
+
+            If identifierName.Identifier.ToString() <> MSBuildWorkspace Then
+                Return
+            End If
+
+            Dim symbolInfo = context.SemanticModel.GetSymbolInfo(identifierName)
+            If symbolInfo.Symbol Is Nothing Then
+                context.ReportDiagnostic(Diagnostic.Create(UpgradeMSBuildWorkspaceDiagnosticRule, identifierName.GetLocation()))
+            End If
+        End Sub
+    End Class
+End Namespace
