fa25ffadd17b51965d6646c2cb684bedd30666ba
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2018-04-27 19:05:09 -0700

    Lowring indexes and ranges

---
+++ src/Compilers/CSharp/Portable/Lowering/LocalRewriter/LocalRewriter_Range.cs
@@ -0,0 +1,177 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Diagnostics;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Roslyn.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.CodeAnalysis.CSharp
+{
+    internal sealed partial class LocalRewriter
+    {
+        public override BoundNode VisitRangeExpression(BoundRangeExpression node)
+        {
+            if (node is null)
+            {
+                return null;
+            }
+
+            if (node.LeftOperand is null)
+            {
+                if (node.RightOperand is null)
+                {
+                    // Case 1:              ".."
+                    // translates to:       Range.All()
+
+                    MethodSymbol method = (MethodSymbol)_compilation.GetWellKnownTypeMember(WellKnownMember.System_Range__All);
+                    return MakeCall(node.Syntax, rewrittenReceiver: null, method, ImmutableArray<BoundExpression>.Empty, node.Type);
+                }
+
+                BoundExpression rightOperand = VisitExpression(node.RightOperand);
+                rightOperand = NullableAlwaysHasValue(rightOperand) ?? rightOperand;
+
+                if (node.RightOperand.Type.IsNullableType())
+                {
+                    // Case 2:              "..nullableRight"
+                    // translates to:       nullableRight.HasValue ? new Nullable(Range.ToEnd(nullableRight.GetValueOrDefault())) : default
+
+                    MethodSymbol method = (MethodSymbol)_compilation.GetWellKnownTypeMember(WellKnownMember.System_Range__ToEnd);
+                    return LiftRangeExpression(node.Syntax, node.Type, method, rightOperand);
+                }
+                else
+                {
+                    // Case 3:              "..right"
+                    // translates to:       Range.ToEnd(right)
+
+                    MethodSymbol method = (MethodSymbol)_compilation.GetWellKnownTypeMember(WellKnownMember.System_Range__ToEnd);
+                    ImmutableArray<BoundExpression> arguments = ImmutableArray.Create(rightOperand);
+                    return MakeCall(node.Syntax, rewrittenReceiver: null, method, arguments, node.Type);
+                }
+            }
+            else
+            {
+                BoundExpression leftOperand = VisitExpression(node.LeftOperand);
+                leftOperand = NullableAlwaysHasValue(leftOperand) ?? leftOperand;
+
+                if (node.RightOperand is null)
+                {
+                    if (leftOperand.Type.IsNullableType())
+                    {
+                        // Case 4:              "nullableLeft.."
+                        // translates to:       nullableLeft.HasValue ? new Nullable(Range.FromStart(nullableLeft.GetValueOrDefault())) : default
+
+                        MethodSymbol method = (MethodSymbol)_compilation.GetWellKnownTypeMember(WellKnownMember.System_Range__FromStart);
+                        return LiftRangeExpression(node.Syntax, node.Type, method, leftOperand);
+                    }
+                    else
+                    {
+                        // Case 5:              "left.."
+                        // translates to:       Range.FromStart(left)
+
+                        MethodSymbol method = (MethodSymbol)_compilation.GetWellKnownTypeMember(WellKnownMember.System_Range__FromStart);
+                        ImmutableArray<BoundExpression> arguments = ImmutableArray.Create(leftOperand);
+                        return MakeCall(node.Syntax, rewrittenReceiver: null, method, arguments, node.Type);
+                    }
+                }
+
+                BoundExpression rightOperand = VisitExpression(node.RightOperand);
+                rightOperand = NullableAlwaysHasValue(rightOperand) ?? rightOperand;
+
+                if (!leftOperand.Type.IsNullableType() && !rightOperand.Type.IsNullableType())
+                {
+                    // Case 6:              "left..right"
+                    // translates to:       Range.Create(left, right)
+
+                    MethodSymbol method = (MethodSymbol)_compilation.GetWellKnownTypeMember(WellKnownMember.System_Range__Create);
+                    ImmutableArray<BoundExpression> arguments = ImmutableArray.Create(leftOperand, rightOperand);
+                    return MakeCall(node.Syntax, rewrittenReceiver: null, method, arguments, node.Type);
+                }
+                else
+                {
+                    // Case 7:              "nullableLeft..nullableRight"
+                    // translates to:       nullableLeft.HasValue && nullableRight.HasValue ? new Nullable(Range.Create(nullableLeft.GetValueOrDefault(), nullableRight.GetValueOrDefault())) : default
+
+                    // Case 8:              "nullableLeft..right"
+                    // translates to:       nullableLeft.HasValue ? new Nullable(Range.Create(nullableLeft.GetValueOrDefault(), right)) : default
+
+                    // Case 9:              "left..nullableRight"
+                    // translates to:       nullableRight.HasValue ? new Nullable(Range.Create(left, nullableRight.GetValueOrDefault())) : default
+
+                    MethodSymbol method = (MethodSymbol)_compilation.GetWellKnownTypeMember(WellKnownMember.System_Range__Create);
+                    return LiftRangeExpression(node.Syntax, node.Type, method, leftOperand, rightOperand);
+                }
+            }
+        }
+
+        private BoundNode LiftRangeExpression(SyntaxNode syntax, TypeSymbol type, MethodSymbol method, params BoundExpression[] operands)
+        {
+            Debug.Assert(type.IsNullableType());
+            Debug.Assert(operands.Any(operand => operand.Type.IsNullableType()));
+            Debug.Assert(operands.Length == 1 || operands.Length == 2);
+
+            ArrayBuilder<BoundExpression> sideeffects = ArrayBuilder<BoundExpression>.GetInstance();
+            ArrayBuilder<LocalSymbol> locals = ArrayBuilder<LocalSymbol>.GetInstance();
+            ArrayBuilder<BoundExpression> arguments = ArrayBuilder<BoundExpression>.GetInstance();
+
+            // left.HasValue && right.HasValue
+            BoundExpression condition = null;
+            for (var i = 0; i < operands.Length; i++)
+            {
+                BoundExpression operand = operands[i];
+                if (operand.Type.IsNullableType())
+                {
+                    operand = CaptureExpressionInTempIfNeeded(operand, sideeffects, locals);
+
+                    if (condition is null)
+                    {
+                        condition = MakeOptimizedHasValue(operand.Syntax, operand);
+                    }
+                    else
+                    {
+                        TypeSymbol boolType = _compilation.GetSpecialType(SpecialType.System_Boolean);
+                        BoundExpression operandHasValue = MakeOptimizedHasValue(operand.Syntax, operand);
+                        condition = MakeBinaryOperator(syntax, BinaryOperatorKind.BoolAnd, condition, operandHasValue, boolType, method: null);
+                    }
+
+                    arguments.Add(MakeOptimizedGetValueOrDefault(operand.Syntax, operand));
+                }
+                else
+                {
+                    arguments.Add(operand);
+                }
+            }
+
+            Debug.Assert(condition != null);
+
+            // method(left.GetValueOrDefault(), right.GetValueOrDefault())
+            BoundExpression rangeCall = MakeCall(syntax, rewrittenReceiver: null, method, arguments.ToImmutableAndFree(), type);
+
+            // new Nullable(method(left.GetValueOrDefault(), right.GetValueOrDefault()))
+            MethodSymbol nullableCtor = (MethodSymbol)_compilation.GetSpecialTypeMember(SpecialMember.System_Nullable_T__ctor).SymbolAsMember((NamedTypeSymbol)type);
+            BoundExpression consequence = new BoundObjectCreationExpression(syntax, nullableCtor, binderOpt: null, rangeCall);
+
+            // default
+            BoundExpression alternative = new BoundDefaultExpression(syntax, constantValueOpt: null, type);
+
+            // left.HasValue && right.HasValue ? new Nullable(method(left.GetValueOrDefault(), right.GetValueOrDefault())) : default
+            BoundExpression conditionalExpression = RewriteConditionalOperator(
+                syntax: syntax,
+                rewrittenCondition: condition,
+                rewrittenConsequence: consequence,
+                rewrittenAlternative: alternative,
+                constantValueOpt: null,
+                rewrittenType: type,
+                isRef: false);
+
+            return new BoundSequence(
+                syntax: syntax,
+                locals: locals.ToImmutableAndFree(),
+                sideEffects: sideeffects.ToImmutableAndFree(),
+                value: conditionalExpression,
+                type: type);
+        }
+    }
+}
---
+++ src/Compilers/CSharp/Test/Emit/CodeGen/IndexAndRangeTests.cs
@@ -0,0 +1,492 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.CodeGen
+{
+    public class IndexAndRangeTests : CSharpTestBase
+    {
+        [Fact]
+        public void LowerIndex_Int()
+        {
+            var compilation = CreateCompilationWithIndex(@"
+using System;
+public static class Util
+{
+    public static Index Convert(int a) => ^a;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.Convert", @"
+{
+  // Code size        8 (0x8)
+  .maxstack  2
+  IL_0000:  ldarg.0
+  IL_0001:  ldc.i4.1
+  IL_0002:  newobj     ""System.Index..ctor(int, bool)""
+  IL_0007:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerIndex_NullableInt()
+        {
+            var compilation = CreateCompilationWithIndex(@"
+using System;
+public static class Util
+{
+    public static Index? Convert(int? a) => ^a;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.Convert", @"
+{
+  // Code size       40 (0x28)
+  .maxstack  2
+  .locals init (int? V_0,
+                System.Index? V_1)
+  IL_0000:  ldarg.0
+  IL_0001:  stloc.0
+  IL_0002:  ldloca.s   V_0
+  IL_0004:  call       ""bool int?.HasValue.get""
+  IL_0009:  brtrue.s   IL_0015
+  IL_000b:  ldloca.s   V_1
+  IL_000d:  initobj    ""System.Index?""
+  IL_0013:  ldloc.1
+  IL_0014:  ret
+  IL_0015:  ldloca.s   V_0
+  IL_0017:  call       ""int int?.GetValueOrDefault()""
+  IL_001c:  ldc.i4.1
+  IL_001d:  newobj     ""System.Index..ctor(int, bool)""
+  IL_0022:  newobj     ""System.Index?..ctor(System.Index)""
+  IL_0027:  ret
+}");
+        }
+
+        [Fact]
+        public void PrintIndexExpressions()
+        {
+            var compilation = CreateCompilationWithIndex(@"
+using System;
+class Program
+{
+    static void Main()
+    {
+        int nonNullable = 1;
+        int? nullableValue = 2;
+        int? nullableDefault = default;
+
+        Index a = nonNullable;
+        Console.WriteLine(""a: "" + Print(a));
+
+        Index b = ^nonNullable;
+        Console.WriteLine(""b: "" + Print(b));
+
+        // --------------------------------------------------------
+        
+        Index? c = nullableValue;
+        Console.WriteLine(""c: "" + Print(c));
+
+        Index? d = ^nullableValue;
+        Console.WriteLine(""d: "" + Print(d));
+
+        // --------------------------------------------------------
+        
+        Index? e = nullableDefault;
+        Console.WriteLine(""e: "" + Print(e));
+
+        Index? f = ^nullableDefault;
+        Console.WriteLine(""f: "" + Print(f));
+    }
+    static string Print(Index? arg)
+    {
+        if (arg.HasValue)
+        {
+            return $""value: '{arg.Value.Value}', fromEnd: '{arg.Value.FromEnd}'"";
+        }
+        else
+        {
+            return ""default"";
+        }
+    }
+}", options: TestOptions.ReleaseExe);
+
+            CompileAndVerify(compilation, expectedOutput: @"
+a: value: '1', fromEnd: 'False'
+b: value: '1', fromEnd: 'True'
+c: value: '2', fromEnd: 'False'
+d: value: '2', fromEnd: 'True'
+e: default
+f: default");
+        }
+
+        [Fact]
+        public void LowerRange_Create_Index_Index()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range Create(Index start, Index end) => start..end;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.Create", @"
+{
+  // Code size        8 (0x8)
+  .maxstack  2
+  IL_0000:  ldarg.0
+  IL_0001:  ldarg.1
+  IL_0002:  call       ""System.Range System.Range.Create(System.Index, System.Index)""
+  IL_0007:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerRange_Create_Index_NullableIndex()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range? Create(Index start, Index? end) => start..end;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.Create", @"
+{
+  // Code size       40 (0x28)
+  .maxstack  2
+  .locals init (System.Index? V_0,
+                System.Range? V_1)
+  IL_0000:  ldarg.1
+  IL_0001:  stloc.0
+  IL_0002:  ldloca.s   V_0
+  IL_0004:  call       ""bool System.Index?.HasValue.get""
+  IL_0009:  brtrue.s   IL_0015
+  IL_000b:  ldloca.s   V_1
+  IL_000d:  initobj    ""System.Range?""
+  IL_0013:  ldloc.1
+  IL_0014:  ret
+  IL_0015:  ldarg.0
+  IL_0016:  ldloca.s   V_0
+  IL_0018:  call       ""System.Index System.Index?.GetValueOrDefault()""
+  IL_001d:  call       ""System.Range System.Range.Create(System.Index, System.Index)""
+  IL_0022:  newobj     ""System.Range?..ctor(System.Range)""
+  IL_0027:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerRange_Create_NullableIndex_Index()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range? Create(Index? start, Index end) => start..end;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.Create", @"
+{
+  // Code size       40 (0x28)
+  .maxstack  2
+  .locals init (System.Index? V_0,
+                System.Range? V_1)
+  IL_0000:  ldarg.0
+  IL_0001:  stloc.0
+  IL_0002:  ldloca.s   V_0
+  IL_0004:  call       ""bool System.Index?.HasValue.get""
+  IL_0009:  brtrue.s   IL_0015
+  IL_000b:  ldloca.s   V_1
+  IL_000d:  initobj    ""System.Range?""
+  IL_0013:  ldloc.1
+  IL_0014:  ret
+  IL_0015:  ldloca.s   V_0
+  IL_0017:  call       ""System.Index System.Index?.GetValueOrDefault()""
+  IL_001c:  ldarg.1
+  IL_001d:  call       ""System.Range System.Range.Create(System.Index, System.Index)""
+  IL_0022:  newobj     ""System.Range?..ctor(System.Range)""
+  IL_0027:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerRange_Create_NullableIndex_NullableIndex()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range? Create(Index? start, Index? end) => start..end;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.Create", @"
+{
+  // Code size       56 (0x38)
+  .maxstack  2
+  .locals init (System.Index? V_0,
+                System.Index? V_1,
+                System.Range? V_2)
+  IL_0000:  ldarg.0
+  IL_0001:  stloc.0
+  IL_0002:  ldarg.1
+  IL_0003:  stloc.1
+  IL_0004:  ldloca.s   V_0
+  IL_0006:  call       ""bool System.Index?.HasValue.get""
+  IL_000b:  ldloca.s   V_1
+  IL_000d:  call       ""bool System.Index?.HasValue.get""
+  IL_0012:  and
+  IL_0013:  brtrue.s   IL_001f
+  IL_0015:  ldloca.s   V_2
+  IL_0017:  initobj    ""System.Range?""
+  IL_001d:  ldloc.2
+  IL_001e:  ret
+  IL_001f:  ldloca.s   V_0
+  IL_0021:  call       ""System.Index System.Index?.GetValueOrDefault()""
+  IL_0026:  ldloca.s   V_1
+  IL_0028:  call       ""System.Index System.Index?.GetValueOrDefault()""
+  IL_002d:  call       ""System.Range System.Range.Create(System.Index, System.Index)""
+  IL_0032:  newobj     ""System.Range?..ctor(System.Range)""
+  IL_0037:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerRange_ToEnd_Index()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range ToEnd(Index end) => ..end;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.ToEnd", @"
+{
+  // Code size        7 (0x7)
+  .maxstack  1
+  IL_0000:  ldarg.0
+  IL_0001:  call       ""System.Range System.Range.ToEnd(System.Index)""
+  IL_0006:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerRange_ToEnd_NullableIndex()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range? ToEnd(Index? end) => ..end;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.ToEnd", @"
+{
+  // Code size       39 (0x27)
+  .maxstack  1
+  .locals init (System.Index? V_0,
+                System.Range? V_1)
+  IL_0000:  ldarg.0
+  IL_0001:  stloc.0
+  IL_0002:  ldloca.s   V_0
+  IL_0004:  call       ""bool System.Index?.HasValue.get""
+  IL_0009:  brtrue.s   IL_0015
+  IL_000b:  ldloca.s   V_1
+  IL_000d:  initobj    ""System.Range?""
+  IL_0013:  ldloc.1
+  IL_0014:  ret
+  IL_0015:  ldloca.s   V_0
+  IL_0017:  call       ""System.Index System.Index?.GetValueOrDefault()""
+  IL_001c:  call       ""System.Range System.Range.ToEnd(System.Index)""
+  IL_0021:  newobj     ""System.Range?..ctor(System.Range)""
+  IL_0026:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerRange_FromStart_Index()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range FromStart(Index start) => start..;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.FromStart", @"
+{
+  // Code size        7 (0x7)
+  .maxstack  1
+  IL_0000:  ldarg.0
+  IL_0001:  call       ""System.Range System.Range.FromStart(System.Index)""
+  IL_0006:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerRange_FromStart_NullableIndex()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range? FromStart(Index? start) => start..;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.FromStart", @"
+{
+  // Code size       39 (0x27)
+  .maxstack  1
+  .locals init (System.Index? V_0,
+                System.Range? V_1)
+  IL_0000:  ldarg.0
+  IL_0001:  stloc.0
+  IL_0002:  ldloca.s   V_0
+  IL_0004:  call       ""bool System.Index?.HasValue.get""
+  IL_0009:  brtrue.s   IL_0015
+  IL_000b:  ldloca.s   V_1
+  IL_000d:  initobj    ""System.Range?""
+  IL_0013:  ldloc.1
+  IL_0014:  ret
+  IL_0015:  ldloca.s   V_0
+  IL_0017:  call       ""System.Index System.Index?.GetValueOrDefault()""
+  IL_001c:  call       ""System.Range System.Range.FromStart(System.Index)""
+  IL_0021:  newobj     ""System.Range?..ctor(System.Range)""
+  IL_0026:  ret
+}");
+        }
+
+        [Fact]
+        public void LowerRange_All()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+public static class Util
+{
+    public static Range All() => ..;
+}");
+
+            CompileAndVerify(compilation).VerifyIL("Util.All", @"
+{
+  // Code size        6 (0x6)
+  .maxstack  1
+  IL_0000:  call       ""System.Range System.Range.All()""
+  IL_0005:  ret
+}");
+        }
+
+        [Fact]
+        public void PrintRangeExpressions()
+        {
+            var compilation = CreateCompilationWithIndexAndRange(@"
+using System;
+class Program
+{
+    static void Main()
+    {
+        Index nonNullable = 1;
+        Index? nullableValue = 2;
+        Index? nullableDefault = default;
+
+        Range a = nonNullable..nonNullable;
+        Console.WriteLine(""a: "" + Print(a));
+
+        Range? b = nonNullable..nullableValue;
+        Console.WriteLine(""b: "" + Print(b));
+
+        Range? c = nonNullable..nullableDefault;
+        Console.WriteLine(""c: "" + Print(c));
+
+        // --------------------------------------------------------
+
+        Range? d = nullableValue..nonNullable;
+        Console.WriteLine(""d: "" + Print(d));
+
+        Range? e = nullableValue..nullableValue;
+        Console.WriteLine(""e: "" + Print(e));
+
+        Range? f = nullableValue..nullableDefault;
+        Console.WriteLine(""f: "" + Print(f));
+
+        // --------------------------------------------------------
+
+        Range? g = nullableDefault..nonNullable;
+        Console.WriteLine(""g: "" + Print(g));
+
+        Range? h = nullableDefault..nullableValue;
+        Console.WriteLine(""h: "" + Print(h));
+
+        Range? i = nullableDefault..nullableDefault;
+        Console.WriteLine(""i: "" + Print(i));
+
+        // --------------------------------------------------------
+
+        Range? j = ..nonNullable;
+        Console.WriteLine(""j: "" + Print(j));
+
+        Range? k = ..nullableValue;
+        Console.WriteLine(""k: "" + Print(k));
+
+        Range? l = ..nullableDefault;
+        Console.WriteLine(""l: "" + Print(l));
+
+        // --------------------------------------------------------
+
+        Range? m = nonNullable..;
+        Console.WriteLine(""m: "" + Print(m));
+
+        Range? n = nullableValue..;
+        Console.WriteLine(""n: "" + Print(n));
+
+        Range? o = nullableDefault..;
+        Console.WriteLine(""o: "" + Print(o));
+
+        // --------------------------------------------------------
+
+        Range? p = ..;
+        Console.WriteLine(""p: "" + Print(p));
+
+    }
+    static string Print(Index? arg)
+    {
+        if (arg.HasValue)
+        {
+            return $""value: '{arg.Value.Value}', fromEnd: '{arg.Value.FromEnd}'"";
+        }
+        else
+        {
+            return ""default"";
+        }
+    }
+    static string Print(Range? arg)
+    {
+        if (arg.HasValue)
+        {
+            return $""value: '{Print(arg.Value.Start)}', fromEnd: '{Print(arg.Value.End)}'"";
+        }
+        else
+        {
+            return ""default"";
+        }
+    }
+}", options: TestOptions.ReleaseExe);
+
+            CompileAndVerify(compilation, expectedOutput: @"
+a: value: 'value: '1', fromEnd: 'False'', fromEnd: 'value: '1', fromEnd: 'False''
+b: value: 'value: '1', fromEnd: 'False'', fromEnd: 'value: '2', fromEnd: 'False''
+c: default
+d: value: 'value: '2', fromEnd: 'False'', fromEnd: 'value: '1', fromEnd: 'False''
+e: value: 'value: '2', fromEnd: 'False'', fromEnd: 'value: '2', fromEnd: 'False''
+f: default
+g: default
+h: default
+i: default
+j: value: 'value: '0', fromEnd: 'False'', fromEnd: 'value: '1', fromEnd: 'False''
+k: value: 'value: '0', fromEnd: 'False'', fromEnd: 'value: '2', fromEnd: 'False''
+l: default
+m: value: 'value: '1', fromEnd: 'False'', fromEnd: 'value: '0', fromEnd: 'True''
+n: value: 'value: '2', fromEnd: 'False'', fromEnd: 'value: '0', fromEnd: 'True''
+o: default
+p: value: 'value: '0', fromEnd: 'False'', fromEnd: 'value: '0', fromEnd: 'True''");
+        }
+    }
+}
