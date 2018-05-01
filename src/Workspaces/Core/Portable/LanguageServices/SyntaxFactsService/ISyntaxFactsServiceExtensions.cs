4bc8cad165e17b4ccf4f9dc7852230ced178611b
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-04-30 21:46:37 -0700

    Move Unparenthesize code to a common location.

--- src/Workspaces/Core/Portable/LanguageServices/SyntaxFactsService/ISyntaxFactsServiceExtensions.cs
+++ src/Workspaces/Core/Portable/LanguageServices/SyntaxFactsService/ISyntaxFactsServiceExtensions.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using Microsoft.CodeAnalysis.LanguageServices;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.LanguageServices
@@ -41,10 +39,46 @@ namespace Microsoft.CodeAnalysis.LanguageServices
             syntaxFacts.GetPartsOfAssignmentStatement(statement, out left, out _, out right);
         }
 
+        public static SyntaxNode Unparenthesize(
+            this ISyntaxFactsService syntaxFacts, SyntaxNode node)
+        {
+            syntaxFacts.GetPartsOfParenthesizedExpression(node,
+                out var openParenToken, out var expression, out var closeParenToken);
+
+            var leadingTrivia = openParenToken.LeadingTrivia
+                .Concat(openParenToken.TrailingTrivia)
+                .Where(t => !syntaxFacts.IsElastic(t))
+                .Concat(expression.GetLeadingTrivia());
+
+            var trailingTrivia = expression.GetTrailingTrivia()
+                .Concat(closeParenToken.LeadingTrivia)
+                .Where(t => !syntaxFacts.IsElastic(t))
+                .Concat(closeParenToken.TrailingTrivia);
+
+            var resultNode = expression
+                .WithLeadingTrivia(leadingTrivia)
+                .WithTrailingTrivia(trailingTrivia);
+
+            return resultNode;
+        }
+
         public static bool SpansPreprocessorDirective(this ISyntaxFactsService service, SyntaxNode node)
             => service.SpansPreprocessorDirective(SpecializedCollections.SingletonEnumerable(node));
 
         public static bool IsWhitespaceOrEndOfLineTrivia(this ISyntaxFactsService syntaxFacts, SyntaxTrivia trivia)
             => syntaxFacts.IsWhitespaceTrivia(trivia) || syntaxFacts.IsEndOfLineTrivia(trivia);
+
+        public static void GetPartsOfBinaryExpression(
+            this ISyntaxFactsService syntaxFacts, SyntaxNode node, 
+            out SyntaxNode left, out SyntaxNode right)
+        {
+            syntaxFacts.GetPartsOfBinaryExpression(node, out left, out _, out right);
+        }
+
+        public static SyntaxNode GetExpressionOfParenthesizedExpression(this ISyntaxFactsService syntaxFacts, SyntaxNode node)
+        {
+            syntaxFacts.GetPartsOfParenthesizedExpression(node, out _, out var expression, out _);
+            return expression;
+        }
     }
 }
