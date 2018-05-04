1b77aef569009878c9f923723f6461dedaa28136
Mikkel Nylander Bundgaard <mikkelbu@gmail.com>
2018-05-04 23:56:58 +0200

    Change code blocks to inline code to make the paragraph read nicer
    Relates to dotnet/roslyn-api-docs#43

--- src/Compilers/Core/Portable/Diagnostic/Location.cs
+++ src/Compilers/Core/Portable/Diagnostic/Location.cs
@@ -70,7 +70,7 @@ namespace Microsoft.CodeAnalysis
 
         /// <summary>
         /// Gets the location in terms of path, line and column after applying source line mapping directives
-        /// (<code>#line</code> in C# or <code>#ExternalSource</code> in VB). 
+        /// (`#line` in C# or `#ExternalSource` in VB).
         /// </summary>
         /// <returns>
         /// <see cref="FileLinePositionSpan"/> that contains file, line and column information,
