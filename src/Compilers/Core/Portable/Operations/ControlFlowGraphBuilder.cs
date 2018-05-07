6e0ef7c294a001adb53ea5ecd463aa532a0fc499
Cheryl Borley <chborley@microsoft.com>
2018-05-07 10:03:38 -0700

    Move VisitTypeOf out of #region

--- src/Compilers/Core/Portable/Operations/ControlFlowGraphBuilder.cs
+++ src/Compilers/Core/Portable/Operations/ControlFlowGraphBuilder.cs
@@ -3804,6 +3804,11 @@ oneMoreTime:
             return new LiteralExpression(semanticModel: null, operation.Syntax, operation.Type, operation.ConstantValue, operation.IsImplicit);
         }
 
+        public override IOperation VisitTypeOf(ITypeOfOperation operation, int? captureIdForResult)
+        {
+            return new TypeOfExpression(operation.TypeOperand, semanticModel: null, operation.Syntax, operation.Type, operation.ConstantValue, operation.IsImplicit);
+        }
+
         private T Visit<T>(T node) where T : IOperation
         {
             return (T)Visit(node, argument: null);
@@ -3928,11 +3933,6 @@ oneMoreTime:
         public override IOperation VisitSizeOf(ISizeOfOperation operation, int? captureIdForResult)
         {
             return new SizeOfExpression(operation.TypeOperand, semanticModel: null, operation.Syntax, operation.Type, operation.ConstantValue, operation.IsImplicit);
-        }
-
-        public override IOperation VisitTypeOf(ITypeOfOperation operation, int? captureIdForResult)
-        {
-            return new TypeOfExpression(operation.TypeOperand, semanticModel: null, operation.Syntax, operation.Type, operation.ConstantValue, operation.IsImplicit);
         }
 
         public override IOperation VisitAnonymousFunction(IAnonymousFunctionOperation operation, int? captureIdForResult)
