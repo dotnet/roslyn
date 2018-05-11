7e343fb51e1f5c79fcaa5fc124fd5df4a14a24f0
Sam Harwell <sam.harwell@microsoft.com>
2018-05-11 13:31:38 -0500

    Install the VSSDK analyzers in VSIX package projects and fix diagnostics

--- src/VisualStudio/VisualStudioDiagnosticsToolWindow/VisualStudioDiagnosticsWindowPackage.cs
+++ src/VisualStudio/VisualStudioDiagnosticsToolWindow/VisualStudioDiagnosticsWindowPackage.cs
@@ -55,6 +55,8 @@ namespace Roslyn.VisualStudio.DiagnosticsWindow
         /// </summary>
         private void ShowToolWindow(object sender, EventArgs e)
         {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
             // Get the instance number 0 of this tool window. This window is single instance so this instance
             // is actually the only one.
             // The last flag is set to true so that if the tool window does not exists it will be created.
