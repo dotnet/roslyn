f7ba0a15199dfaa225fd7a60de8c082974ddbba3
Andy Gocke <angocke@microsoft.com>
2018-05-14 17:02:25 -0700

    Add editorconfig parser and unit tests (#26253)
    This is the same parser used by Visual Studio for editorconfig with a
    minor modification to use case-insensitive comparison for property keys.

---
+++ src/Compilers/Core/CodeAnalysisTest/EditorConfigTests.cs
@@ -0,0 +1,257 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using Roslyn.Test.Utilities;
+using Roslyn.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.UnitTests
+{
+    public class EditorConfigTests
+    {
+        private static EditorConfig ParseConfigFile(string text) => EditorConfig.Parse(text, "");
+
+        [Fact]
+        public void SimpleCase()
+        {
+            var config = EditorConfig.Parse(@"
+root = true
+
+# Comment1
+# Comment2
+##################################
+
+my_global_prop = my_global_val
+
+[*.cs]
+my_prop = my_val
+
+", "Z:\\bogus");
+
+            Assert.Equal("", config.GlobalSection.Name);
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_global_prop", "my_global_val"),
+                        KeyValuePair.Create("root", "true") },
+                properties);
+
+            var namedSections = config.NamedSections;
+            Assert.Equal("*.cs", namedSections[0].Name);
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop", "my_val")},
+                namedSections[0].Properties);
+            
+            Assert.True(config.IsRoot);
+            Assert.Equal("Z:\\bogus", config.Directory);
+        }
+
+        [Fact]
+        public void MissingClosingBracket()
+        {
+            var config = ParseConfigFile(@"
+[*.cs
+my_prop = my_val");
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop", "my_val")},
+                properties);
+
+            Assert.Equal(0, config.NamedSections.Length);
+        }
+
+        [Fact]
+        public void EmptySection()
+        {
+            var config = EditorConfig.Parse(@"
+[]
+my_prop = my_val", "");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(new[] { KeyValuePair.Create("my_prop", "my_val")}, properties);
+            Assert.Equal(0, config.NamedSections.Length);
+        }
+
+        [Fact]
+        public void CaseInsensitivePropKey()
+        {
+            var config = ParseConfigFile(@"
+my_PROP = my_VAL");
+            var properties = config.GlobalSection.Properties;
+
+            Assert.True(properties.TryGetValue("my_PrOp", out var val));
+            Assert.Equal(val, "my_VAL");
+            Assert.Equal("my_prop", properties.Keys.Single());
+        }
+
+        [Fact]
+        public void NonReservedKeyPreservedCaseVal()
+        {
+            var config = ParseConfigFile(string.Join(Environment.NewLine,
+                EditorConfig.ReservedKeys.Select(k => "MY_" + k + " = MY_VAL")));
+            AssertEx.SetEqual(
+                EditorConfig.ReservedKeys.Select(k => KeyValuePair.Create("my_" + k, "MY_VAL")).ToList(),
+                config.GlobalSection.Properties);
+        }
+
+        [Fact]
+        public void DuplicateKeys()
+        {
+            var config = ParseConfigFile(@"
+my_prop = my_val
+my_prop = my_other_val");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(new[] { KeyValuePair.Create("my_prop", "my_other_val") }, properties);
+        }
+
+        [Fact]
+        public void DuplicateKeysCasing()
+        {
+            var config = ParseConfigFile(@"
+my_prop = my_val
+my_PROP = my_other_val");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(new[] { KeyValuePair.Create("my_prop", "my_other_val") }, properties);
+        }
+
+        [Fact]
+        public void MissingKey()
+        {
+            var config = ParseConfigFile(@"
+= my_val1
+my_prop = my_val2");
+
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop", "my_val2") },
+                properties);
+        }
+
+        [Fact]
+        public void MissingVal()
+        {
+            var config = ParseConfigFile(@"
+my_prop1 =
+my_prop2 = my_val");
+
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop1", ""),
+                        KeyValuePair.Create("my_prop2", "my_val") },
+                properties);
+        }
+
+        [Fact]
+        public void SpacesInProperties()
+        {
+            var config = ParseConfigFile(@"
+my prop1 = my_val1
+my_prop2 = my val2");
+
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop2", "my val2") },
+                properties);
+        }
+
+        [Fact]
+        public void EndOfLineComments()
+        {
+            var config = ParseConfigFile(@"
+my_prop2 = my val2 # Comment");
+
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_prop2", "my val2") },
+                properties);
+        }
+
+        [Fact]
+        public void SymbolsStartKeys()
+        {
+            var config = ParseConfigFile(@"
+@!$abc = my_val1
+@!$\# = my_val2");
+
+            var properties = config.GlobalSection.Properties;
+            Assert.Equal(0, properties.Count);
+        }
+
+        [Fact]
+        public void EqualsAndColon()
+        {
+            var config = ParseConfigFile(@"
+my:key1 = my_val
+my_key2 = my:val");
+
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my", "key1 = my_val"),
+                        KeyValuePair.Create("my_key2", "my:val")},
+                properties);
+        }
+
+        [Fact]
+        public void SymbolsInProperties()
+        {
+            var config = ParseConfigFile(@"
+my@key1 = my_val
+my_key2 = my@val");
+
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("my_key2", "my@val")},
+                properties);
+        }
+
+        [Fact]
+        public void LongLines()
+        {
+            // PROTOTYPE(editorconfig): This example is described in the Python ConfigParser as
+            // allowing line continuation via the RFC 822 specification, section 3.1.1 LONG
+            // HEADER FIELDS. It's unclear whether it's intended for editorconfig to permit
+            // this interpretation. The VS parser does not. We should probably check other
+            // parsers for completeness.
+            var config = ParseConfigFile(@"
+long: this value continues
+   in the next line");
+
+            var properties = config.GlobalSection.Properties;
+            AssertEx.SetEqual(
+                new[] { KeyValuePair.Create("long", "this value continues")},
+                properties);
+        }
+
+        [Fact]
+        public void CaseInsensitiveRoot()
+        {
+            var config = ParseConfigFile(@"
+RoOt = TruE");
+            Assert.True(config.IsRoot);
+        }
+
+        [Fact]
+        public void ReservedValues()
+        {
+            int index = 0;
+            var config = ParseConfigFile(string.Join(Environment.NewLine,
+                EditorConfig.ReservedValues.Select(v => "MY_KEY" + (index++) + " = " + v.ToUpperInvariant())));
+            index = 0;
+            AssertEx.SetEqual(
+                EditorConfig.ReservedValues.Select(v => KeyValuePair.Create("my_key" + (index++), v)).ToList(),
+                config.GlobalSection.Properties);
+        }
+
+        [Fact]
+        public void ReservedKeys()
+        {
+            var config = ParseConfigFile(string.Join(Environment.NewLine,
+                EditorConfig.ReservedKeys.Select(k => k + " = MY_VAL")));
+            AssertEx.SetEqual(
+                EditorConfig.ReservedKeys.Select(k => KeyValuePair.Create(k, "my_val")).ToList(),
+                config.GlobalSection.Properties);
+        }
+    }
+}
