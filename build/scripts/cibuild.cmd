f5f472db539a75301832fdd21796d6cb1a81a617
Jared Parsons <jaredpparsons@gmail.com>
2018-05-14 20:58:28 -0700

    Groovy cleanup
    This is cleaning up some code needed to bootstrap groovy changes in an
    earlier PR:
    
    https://github.com/dotnet/roslyn/pull/26762

--- build/scripts/cibuild.cmd
+++ build/scripts/cibuild.cmd
@@ -1,7 +1,2 @@
 @echo off
-if "%1"=="-testBuildCorrectness" ( 
-    REM Temporary work around until the netci.groovy change takes
-    call %~dp0\test-build-correctness.cmd -cibuild -release
-) else (
-    powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build.ps1" -cibuild -build -restore -bootstrap -binaryLog %*
-)
+powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build.ps1" -cibuild -build -restore -bootstrap -binaryLog %*
