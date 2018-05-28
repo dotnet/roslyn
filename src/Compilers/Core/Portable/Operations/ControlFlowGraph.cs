a1f8987644822fbaa8957e9faadb79fa38f34ef4
AlekseyTs <alekseyt@microsoft.com>
2018-05-27 21:19:44 -0700

    Add handling for local functions in control flow graph.

--- src/Compilers/Core/Portable/Operations/ControlFlowGraph.cs
+++ src/Compilers/Core/Portable/Operations/ControlFlowGraph.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Operations
 {
@@ -12,7 +14,14 @@ namespace Microsoft.CodeAnalysis.Operations
     /// </summary>
     public sealed partial class ControlFlowGraph
     {
-        internal ControlFlowGraph(ImmutableArray<BasicBlock> blocks, Region root)
+        // PROTOTYPE(dataflow): This dictionary will hold on to the original IOperation tree and that will keep SemanticModel alive too.
+        //                      Is this a problem? Should we simply build all sub-graphs eagerly?
+        private ImmutableDictionary<IMethodSymbol, (Region, IOperation, int)> _methodsMap;
+        private ControlFlowGraph[] _lazySubGraphs;
+
+        internal ControlFlowGraph(ImmutableArray<BasicBlock> blocks, Region root,
+                                  ImmutableArray<IMethodSymbol> methods,
+                                  ImmutableDictionary<IMethodSymbol, (Region region, IOperation operation, int ordinal)> methodsMap)
         {
             Debug.Assert(!blocks.IsDefault);
             Debug.Assert(blocks.First().Kind == BasicBlockKind.Entry);
@@ -24,6 +33,8 @@ namespace Microsoft.CodeAnalysis.Operations
 
             Blocks = blocks;
             Root = root;
+            Methods = methods;
+            _methodsMap = methodsMap;
         }
 
         /// <summary>
@@ -35,5 +46,48 @@ namespace Microsoft.CodeAnalysis.Operations
         /// Root (<see cref="RegionKind.Root"/>) region for the graph.
         /// </summary>
         public Region Root { get; }
+
+        /// <summary>
+        /// PROTOTYPE(dataflow): Add documentation
+        /// </summary>
+        public ImmutableArray<IMethodSymbol> Methods { get; }
+
+        /// <summary>
+        /// PROTOTYPE(dataflow): Add documentation
+        /// </summary>
+        public ControlFlowGraph this[IMethodSymbol method]
+        {
+            get
+            {
+                (Region enclosing, IOperation operation, int ordinal) = _methodsMap[method];
+                Debug.Assert(method == Methods[ordinal]);
+
+                if (_lazySubGraphs == null)
+                {
+                    Interlocked.CompareExchange(ref _lazySubGraphs, new ControlFlowGraph[Methods.Length], null);
+                }
+
+                if (_lazySubGraphs[ordinal] == null)
+                {
+                    ControlFlowGraph graph;
+
+                    switch (operation.Kind)
+                    {
+                        case OperationKind.LocalFunction:
+                            var localFunction = (ILocalFunctionOperation)operation;
+                            Debug.Assert(method == localFunction.Symbol);
+                            graph = ControlFlowGraphBuilder.Create(operation, enclosing);
+                            break;
+
+                        default:
+                            throw ExceptionUtilities.UnexpectedValue(operation.Kind);
+                    }
+
+                    Interlocked.CompareExchange(ref _lazySubGraphs[ordinal], graph, null);
+                }
+
+                return _lazySubGraphs[ordinal];
+            }
+        }
     }
 }
