a8806aa3392b444aa588cf50d0cd9b1d665e0726
AdamSpeight2008 <adam.speight@virgin.net>
2018-06-01 10:03:46 +0100

    Use the right type in cast.

--- src/EditorFeatures/VisualBasic/Highlighting/KeywordHighlighters/RegionHighlighter.vb
+++ src/EditorFeatures/VisualBasic/Highlighting/KeywordHighlighters/RegionHighlighter.vb
@@ -12,13 +12,14 @@ Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.KeywordHighlighting
 
         Protected Overloads Overrides Iterator Function GetHighlights(directive As DirectiveTriviaSyntax, cancellationToken As CancellationToken) As IEnumerable(Of TextSpan)
             If cancellationToken.IsCancellationRequested Then Return
-            If TypeOf directive IsNot RegionDirectiveTriviaSyntax AndAlso TypeOf directive IsNot EndRegionDirectiveTriviaSyntax Then Return
-            Dim match = directive.GetMatchingStartOrEndDirective(cancellationToken)
-            If match Is Nothing Then Return
-            Dim region = DirectCast(If(TypeOf directive Is RegionDirectiveTriviaSyntax, directive, match), EndRegionDirectiveTriviaSyntax)
-            Dim endRegion = DirectCast(If(TypeOf directive Is EndRegionDirectiveTriviaSyntax, directive, match), EndRegionDirectiveTriviaSyntax)
-            Yield TextSpan.FromBounds(region.HashToken.SpanStart, region.RegionKeyword.Span.End)
-            Yield TextSpan.FromBounds(endRegion.HashToken.SpanStart, endRegion.RegionKeyword.Span.End)
+            If TypeOf directive Is RegionDirectiveTriviaSyntax OrElse TypeOf directive Is EndRegionDirectiveTriviaSyntax Then
+                Dim match = directive.GetMatchingStartOrEndDirective(cancellationToken)
+                If match Is Nothing Then Return
+                Dim region = DirectCast(If(TypeOf directive Is RegionDirectiveTriviaSyntax, directive, match), RegionDirectiveTriviaSyntax)
+                Dim endRegion = DirectCast(If(TypeOf directive Is EndRegionDirectiveTriviaSyntax, directive, match), EndRegionDirectiveTriviaSyntax)
+                Yield TextSpan.FromBounds(region.HashToken.SpanStart, region.RegionKeyword.Span.End)
+                Yield TextSpan.FromBounds(endRegion.HashToken.SpanStart, endRegion.RegionKeyword.Span.End)
+            End If
         End Function
     End Class
 End Namespace
