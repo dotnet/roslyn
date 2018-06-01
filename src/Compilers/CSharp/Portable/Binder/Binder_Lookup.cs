28f0a80ea46fd644a2ad32eff3ca2c0002b4132f
Lauren Rosenberg <t-laros@microsoft.com>
2018-06-01 12:37:30 -0700

    Re-added attribute check

--- src/Compilers/CSharp/Portable/Binder/Binder_Lookup.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Lookup.cs
@@ -1204,14 +1204,16 @@ namespace Microsoft.CodeAnalysis.CSharp
 
             bool IsBadIvtSpecification()
             {
+                IEnumerable<ImmutableArray<byte>> keys = unwrappedSymbol.ContainingAssembly.GetInternalsVisibleToPublicKeys(this.Compilation.AssemblyName);
                 // Ensures that during binding we don't ask for public key which results in attribute binding and stack overflow.
                 // If looking up attributes, don't ask for public key.
                 if ((unwrappedSymbol.DeclaredAccessibility == Accessibility.Internal ||
                     unwrappedSymbol.DeclaredAccessibility == Accessibility.ProtectedAndInternal ||
                     unwrappedSymbol.DeclaredAccessibility == Accessibility.ProtectedOrInternal)
-                    && !options.IsAttributeTypeLookup())
+                    && !options.IsAttributeTypeLookup()
+                    && keys.Any())
                 {
-                    foreach (ImmutableArray<byte> key in unwrappedSymbol.ContainingAssembly.GetInternalsVisibleToPublicKeys(this.Compilation.AssemblyName))
+                    foreach (ImmutableArray<byte> key in keys)
                     {
                         if (key.SequenceEqual(this.Compilation.Assembly.Identity.PublicKey))
                         {
