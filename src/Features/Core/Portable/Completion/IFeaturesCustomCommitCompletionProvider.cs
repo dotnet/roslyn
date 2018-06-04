33facbb3553cb2e23fd74e99665fd0c366d08c8f
David Poeschl <dpoeschl@gmail.com>
2018-06-04 12:57:00 -0700

    Begin to explain IFeaturesCustomCommitCompletionProvider

--- src/Features/Core/Portable/Completion/IFeaturesCustomCommitCompletionProvider.cs
+++ src/Features/Core/Portable/Completion/IFeaturesCustomCommitCompletionProvider.cs
@@ -5,6 +5,12 @@ using System.Threading.Tasks;
 
 namespace Microsoft.CodeAnalysis.Completion
 {
+    /// <summary>
+    /// The ICustomCommitCompletionProvider in the EditorFeatures layer marks completion providers
+    /// that do fancy things with the caret. In the move to Editor Completion, we use this
+    /// interface to mark <see cref="CompletionProvider" />s that need to do more than insert the
+    /// InsertText.
+    /// </summary>
     interface IFeaturesCustomCommitCompletionProvider
     {
         Task<CompletionChange> GetChangeAsync(Document document, CompletionItem item, char? commitKey = default, CancellationToken cancellationToken = default);
