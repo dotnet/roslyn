327769804788363dfcac031a24aae9a4df3ec21b
Sam Harwell <sam.harwell@microsoft.com>
2018-06-05 07:40:28 -0500

    Updated documentation related to working with ReportDiagnostic and DiagnosticSeverity

--- src/Compilers/Core/Portable/Diagnostic/DiagnosticSeverityExtensions.cs
+++ src/Compilers/Core/Portable/Diagnostic/DiagnosticSeverityExtensions.cs
@@ -1,11 +1,25 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis
 {
+    /// <summary>
+    /// Provides extension methods for working with <see cref="DiagnosticSeverity"/> values.
+    /// </summary>
     public static class DiagnosticSeverityExtensions
     {
+        /// <summary>
+        /// Returns the equivalent <see cref="ReportDiagnostic"/> for a <see cref="DiagnosticSeverity"/> value.
+        /// </summary>
+        /// <param name="diagnosticSeverity">The <see cref="DiagnosticSeverity"/> value.</param>
+        /// <returns>
+        /// The equivalent <see cref="ReportDiagnostic"/> for the <see cref="DiagnosticSeverity"/> value.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// If <paramref name="diagnosticSeverity"/> is not one of the expected values.
+        /// </exception>
         public static ReportDiagnostic ToReportDiagnostic(this DiagnosticSeverity diagnosticSeverity)
         {
             switch (diagnosticSeverity)
--- src/Compilers/Core/Portable/Diagnostic/ReportDiagnosticExtensions.cs
+++ src/Compilers/Core/Portable/Diagnostic/ReportDiagnosticExtensions.cs
@@ -1,11 +1,27 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis
 {
+    /// <summary>
+    /// Provides extension methods for working with <see cref="ReportDiagnostic"/> values.
+    /// </summary>
     public static class ReportDiagnosticExtensions
     {
+        /// <summary>
+        /// Returns the equivalent <see cref="DiagnosticSeverity"/> for a <see cref="ReportDiagnostic"/> value.
+        /// </summary>
+        /// <param name="reportDiagnostic">The <see cref="ReportDiagnostic"/> value.</param>
+        /// <returns>
+        /// The equivalent <see cref="DiagnosticSeverity"/> for a <see cref="ReportDiagnostic"/> value; otherwise,
+        /// <see langword="null"/> if <see cref="DiagnosticSeverity"/> does not contain a direct equivalent for
+        /// <paramref name="reportDiagnostic"/>.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// If <paramref name="reportDiagnostic"/> is not one of the expected values.
+        /// </exception>
         public static DiagnosticSeverity? ToDiagnosticSeverity(this ReportDiagnostic reportDiagnostic)
         {
             switch (reportDiagnostic)
@@ -31,6 +47,17 @@ namespace Microsoft.CodeAnalysis
             }
         }
 
+        /// <summary>
+        /// Applies a default severity to a <see cref="ReportDiagnostic"/> value.
+        /// </summary>
+        /// <param name="reportDiagnostic">The <see cref="ReportDiagnostic"/> value.</param>
+        /// <param name="defaultSeverity">The default severity.</param>
+        /// <returns>
+        /// <para>If <paramref name="reportDiagnostic"/> is <see cref="ReportDiagnostic.Default"/>, returns
+        /// <paramref name="defaultSeverity"/>.</para>
+        /// <para>-or-</para>
+        /// <para>Otherwise, returns <paramref name="reportDiagnostic"/> if it has a non-default value.</para>
+        /// </returns>
         public static ReportDiagnostic WithDefaultSeverity(this ReportDiagnostic reportDiagnostic, DiagnosticSeverity defaultSeverity)
         {
             if (reportDiagnostic != ReportDiagnostic.Default)
