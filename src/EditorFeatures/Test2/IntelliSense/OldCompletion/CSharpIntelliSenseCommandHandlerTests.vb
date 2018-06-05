17aaa2655fc5186659e6bfaf662c5f2bcbad0666
Ivan Basov <basov@hotmail.com>
2018-06-05 15:40:26 -0700

    move intellisense tests

---
+++ src/EditorFeatures/Test2/IntelliSense/OldCompletion/CSharpIntelliSenseCommandHandlerTests.vb
@@ -0,0 +1,110 @@
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+
+Namespace Microsoft.CodeAnalysis.Editor.UnitTests.IntelliSense.OldCompletion
+    ''' <summary>
+    ''' This is a test file corresponding to the old Completion service which we are going to turn off after migration to the new one.
+    ''' We should keep this file up-to-date and on par with the new file until then.
+    ''' </summary>
+    <[UseExportProvider]>
+    Public Class CSharpIntelliSenseCommandHandlerTests
+        <WpfFact>
+        Public Async Function TestOpenParenDismissesCompletionAndBringsUpSignatureHelp1() As Task
+            Using state = OldCompletionTestState.CreateCSharpTestState(
+                              <Document>
+class C
+{
+    void Goo()
+    {
+        $$
+    }
+}
+                              </Document>)
+
+                state.SendTypeChars("Go")
+                Await state.AssertCompletionSession()
+                Await state.AssertNoSignatureHelpSession()
+                state.SendTypeChars("(")
+                Await state.AssertNoCompletionSession()
+                Await state.AssertSignatureHelpSession()
+                Await state.AssertSelectedSignatureHelpItem(displayText:="void C.Goo()")
+                Assert.Contains("Goo(", state.GetLineTextFromCaretPosition(), StringComparison.Ordinal)
+            End Using
+        End Function
+
+        <WorkItem(543913, "http://vstfdevdiv:8080/DevDiv2/DevDiv/_workitems/edit/543913")>
+        <WpfFact>
+        Public Async Function TestEscapeDismissesCompletionFirst() As Task
+            Using state = OldCompletionTestState.CreateCSharpTestState(
+                              <Document>
+class C
+{
+    void Goo()
+    {
+        $$
+    }
+}
+                              </Document>)
+
+                state.SendTypeChars("Goo(a")
+                Await state.AssertCompletionSession()
+                Await state.AssertSignatureHelpSession()
+                Await state.WaitForAsynchronousOperationsAsync()
+                state.SendEscape()
+                Await state.AssertNoCompletionSession()
+                Await state.AssertSignatureHelpSession()
+                Await state.WaitForAsynchronousOperationsAsync()
+                state.SendEscape()
+                Await state.AssertNoCompletionSession()
+                Await state.AssertNoSignatureHelpSession()
+            End Using
+        End Function
+
+        <WorkItem(531149, "http://vstfdevdiv:8080/DevDiv2/DevDiv/_workitems/edit/531149")>
+        <WpfFact>
+        Public Async Function TestCutDismissesCompletion() As Task
+            Using state = OldCompletionTestState.CreateCSharpTestState(
+                              <Document>
+class C
+{
+    void Goo()
+    {
+        $$
+    }
+}
+                              </Document>)
+                state.SendTypeChars("Goo(a")
+                Await state.AssertCompletionSession()
+                Await state.AssertSignatureHelpSession()
+                Await state.WaitForAsynchronousOperationsAsync()
+                state.SendCut()
+                Await state.AssertNoCompletionSession()
+                Await state.AssertSignatureHelpSession()
+            End Using
+        End Function
+
+        <WorkItem(531149, "http://vstfdevdiv:8080/DevDiv2/DevDiv/_workitems/edit/531149")>
+        <WpfFact>
+        Public Async Function TestPasteDismissesCompletion() As Task
+            Using state = OldCompletionTestState.CreateCSharpTestState(
+                              <Document>
+class C
+{
+    void Goo()
+    {
+        $$
+    }
+}
+                              </Document>)
+
+                state.SendTypeChars("Goo(a")
+                Await state.AssertCompletionSession()
+                Await state.AssertSignatureHelpSession()
+                Await state.WaitForAsynchronousOperationsAsync()
+                state.SendPaste()
+                Await state.AssertNoCompletionSession()
+                Await state.AssertSignatureHelpSession()
+            End Using
+        End Function
+    End Class
+End Namespace
