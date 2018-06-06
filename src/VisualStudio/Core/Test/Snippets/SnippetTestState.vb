e924113c3107764924d015a0c54678442486dce8
Ivan Basov <basov@hotmail.com>
2018-06-06 14:17:01 -0700

    expect to fix 41 of 54 failures

--- src/VisualStudio/Core/Test/Snippets/SnippetTestState.vb
+++ src/VisualStudio/Core/Test/Snippets/SnippetTestState.vb
@@ -9,6 +9,7 @@ Imports Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense.Completion
 Imports Microsoft.CodeAnalysis.Editor.Shared.Options
 Imports Microsoft.CodeAnalysis.Editor.UnitTests
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.IntelliSense
+Imports Microsoft.CodeAnalysis.Editor.VisualBasic.LineCommit
 Imports Microsoft.CodeAnalysis.Shared.TestHooks
 Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Microsoft.VisualStudio.Commanding
@@ -28,8 +29,8 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Snippets
     Friend NotInheritable Class SnippetTestState
         Inherits AbstractCommandHandlerTestState
 
-        Public Sub New(workspaceElement As XElement, languageName As String, startActiveSession As Boolean, extraParts As IEnumerable(Of Type), Optional workspaceKind As String = Nothing)
-            MyBase.New(workspaceElement, extraParts:=CreatePartCatalog(extraParts), workspaceKind:=workspaceKind)
+        Public Sub New(workspaceElement As XElement, languageName As String, startActiveSession As Boolean, extraParts As IEnumerable(Of Type), excludedTypes As IEnumerable(Of Type), Optional workspaceKind As String = Nothing)
+            MyBase.New(workspaceElement, excludedTypes:=If(excludedTypes Is Nothing, Nothing, New List(Of Type)(excludedTypes)), extraParts:=CreatePartCatalog(extraParts), workspaceKind:=workspaceKind)
 
             Workspace.Options = Workspace.Options.WithChangedOption(InternalFeatureOnOffOptions.Snippets, True)
 
@@ -52,6 +53,10 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Snippets
 
             SnippetExpansionClient = New MockSnippetExpansionClient(startActiveSession)
             TextView.Properties.AddProperty(GetType(AbstractSnippetExpansionClient), SnippetExpansionClient)
+        End Sub
+
+        Public Sub New(workspaceElement As XElement, languageName As String, startActiveSession As Boolean, extraParts As IEnumerable(Of Type), Optional workspaceKind As String = Nothing)
+            Me.New(workspaceElement, languageName, startActiveSession, extraParts, excludedTypes:=Enumerable.Empty(Of Type), workspaceKind)
         End Sub
 
         Public ReadOnly SnippetCommandHandler As AbstractSnippetCommandHandler
@@ -85,7 +90,7 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Snippets
                                    </Project>
                                </Workspace>
 
-            Return New SnippetTestState(workspaceXml, languageName, startActiveSession, extraParts)
+            Return New SnippetTestState(workspaceXml, languageName, startActiveSession, extraParts, excludedTypes:=New List(Of Type) From {GetType(CommitConnectionListener)})
         End Function
 
         Public Shared Function CreateSubmissionTestState(markup As String, languageName As String, Optional startActiveSession As Boolean = False, Optional extraParts As IEnumerable(Of Type) = Nothing) As SnippetTestState
