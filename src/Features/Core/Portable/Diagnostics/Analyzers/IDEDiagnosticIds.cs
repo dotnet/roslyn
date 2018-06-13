45c416b89a7a9bd3cedce333eb09d1338d0a506c
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-06-13 11:14:58 -0700

    Fix comment.

--- src/Features/Core/Portable/Diagnostics/Analyzers/IDEDiagnosticIds.cs
+++ src/Features/Core/Portable/Diagnostics/Analyzers/IDEDiagnosticIds.cs
@@ -16,7 +16,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
         public const string PopulateSwitchDiagnosticId = "IDE0010";
         public const string AddBracesDiagnosticId = "IDE0011";
 
-        // IDE0012-IDE0015 deprecated and replaced with PreferBuiltInTypeDiagnosticId and PreferFrameworkTypeDiagnosticId
+        // IDE0012-IDE0015 deprecated and replaced with PreferBuiltInOrFrameworkTypeDiagnosticId
         //public const string PreferIntrinsicPredefinedTypeInDeclarationsDiagnosticId = "IDE0012";
         //public const string PreferIntrinsicPredefinedTypeInMemberAccessDiagnosticId = "IDE0013";
         //public const string PreferFrameworkTypeInDeclarationsDiagnosticId = "IDE0014";
