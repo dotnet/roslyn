8473e9b13228928e9c49648ae0501a6eab4b4e5a
Tom Meschter <tom.meschter@microsoft.com>
2018-06-15 12:31:16 -0700

    Remove CheckLocStatus target
    Instead of adding a target to work around the fact that XliffTasks is
    only present in projects with shipping binaries (and thus we can't use
    the `EnsureAllResourcesTranslated` target from XliffTasks directly) make
    it available in _all_ projects, but turn it off (via the
    `EnableXlfLocalization` property) in the non-shipping ones.

--- build/scripts/build.ps1
+++ build/scripts/build.ps1
@@ -484,7 +484,7 @@ function Build-DeployToSymStore() {
 }
 
 function Build-CheckLocStatus() {
-    Run-MSBuild "Roslyn.sln" "/t:CheckLocStatus" -logFileName "RoslynCheckLocStatus"
+    Run-MSBuild "Roslyn.sln" "/t:EnsureAllResourcesTranslated" -logFileName "RoslynCheckLocStatus"
 }
 
 # These are tests that don't follow our standard restore, build, test pattern. They customize
