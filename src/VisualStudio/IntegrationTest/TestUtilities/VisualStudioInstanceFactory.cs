f6e489ec425055bc62d550caf16685ed2d87308e
Sam Harwell <sam.harwell@microsoft.com>
2018-06-15 08:04:58 -0500

    Create a new DTE in the current context for each test

--- src/VisualStudio/IntegrationTest/TestUtilities/VisualStudioInstanceFactory.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/VisualStudioInstanceFactory.cs
@@ -185,14 +185,17 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities
             }
             else
             {
-                // We are going to reuse the currently running instance, so ensure that we grab the host Process and Dte
+                // We are going to reuse the currently running instance, so ensure that we grab the host Process and DTE
                 // before cleaning up any hooks or remoting services created by the previous instance. We will then
                 // create a new VisualStudioInstance from the previous to ensure that everything is in a 'clean' state.
+                //
+                // We create a new DTE instance in the current context since the COM object could have been separated
+                // from its RCW during the previous test.
 
                 Debug.Assert(_currentlyRunningInstance != null);
 
                 hostProcess = _currentlyRunningInstance.HostProcess;
-                dte = _currentlyRunningInstance.Dte;
+                dte = await IntegrationHelper.WaitForNotNullAsync(() => IntegrationHelper.TryLocateDteForProcess(hostProcess)).ConfigureAwait(false);
                 supportedPackageIds = _currentlyRunningInstance.SupportedPackageIds;
                 installationPath = _currentlyRunningInstance.InstallationPath;
 
