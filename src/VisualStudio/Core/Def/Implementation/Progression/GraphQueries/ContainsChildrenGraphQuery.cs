b7766766d38635925cc9999237b463464d1e18a8
David Poeschl <dpoeschl@gmail.com>
2018-06-20 15:40:59 -0700

    Check path extension with EndsWith rather than GetExtension
    Reduces the implicit double check for invalid characters in the path.

--- src/VisualStudio/Core/Def/Implementation/Progression/GraphQueries/ContainsChildrenGraphQuery.cs
+++ src/VisualStudio/Core/Def/Implementation/Progression/GraphQueries/ContainsChildrenGraphQuery.cs
@@ -42,32 +42,33 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.Progression
                             var uri = node.Id.GetNestedValueByName<Uri>(CodeGraphNodeIdName.File);
                             if (uri != null)
                             {
+                                // Since a solution load is not yet completed, there is no document available to answer this query.
+                                // The solution explorer presumes that if somebody doesn't answer for a file, they never will. 
+                                // See Providers\GraphContextAttachedCollectionSource.cs for more. Therefore we should answer by setting
+                                // ContainsChildren property to either true or false, so any following updates will be tractable.
+                                // We will set it to false since the solution explorer assumes the default for this query response is 'false'.
+
+                                // Todo: we may need fallback to check if this node actually represents a C# or VB language 
+                                // even when its extension fails to say so. One option would be to call DTEWrapper.IsRegisteredForLangService,
+                                // which may not be called here however since deadlock could happen.
+
                                 // The Uri returned by `GetNestedValueByName()` above isn't necessarily absolute and the `OriginalString` is
                                 // the only property that doesn't throw if the UriKind is relative, so `OriginalString` must be used instead
                                 // of `AbsolutePath`.
                                 var path = uri.OriginalString;
 
-                                // We have seen cases where the URI in the graph node has the following
-                                // form, including the quotes (which are invalid path characters):
+                                // Recorded in https://github.com/dotnet/roslyn/issues/27805, we
+                                // have seen crashes because the URI in the graph node has the
+                                // following form, including the quotes (which are invalid path
+                                // characters):
                                 //     C:\path\to\"some path\App.config"
-                                // So, we choose to be defensive in the presence of invalid path characters.
-                                if (path.IndexOfAny(Path.GetInvalidPathChars()) == -1)
+                                // So we avoid calling Path.GetExtension here. Alternatively, we
+                                // could check for illegal path characters directly first, but then
+                                // that check would actually happen twice because GetExtension will
+                                // also perform the check.
+                                if (path.EndsWith(".cs", StringComparison.OrdinalIgnoreCase) || path.EndsWith(".vb", StringComparison.OrdinalIgnoreCase))
                                 {
-                                    // Since a solution load is not yet completed, there is no document available to answer this query.
-                                    // The solution explorer presumes that if somebody doesn't answer for a file, they never will. 
-                                    // See Providers\GraphContextAttachedCollectionSource.cs for more. Therefore we should answer by setting
-                                    // ContainsChildren property to either true or false, so any following updates will be tractable.
-                                    // We will set it to false since the solution explorer assumes the default for this query response is 'false'.
-
-                                    // Todo: we may need fallback to check if this node actually represents a C# or VB language 
-                                    // even when its extension fails to say so. One option would be to call DTEWrapper.IsRegisteredForLangService,
-                                    // which may not be called here however since deadlock could happen.
-
-                                    string ext = Path.GetExtension(path);
-                                    if (ext.Equals(".cs", StringComparison.OrdinalIgnoreCase) || ext.Equals(".vb", StringComparison.OrdinalIgnoreCase))
-                                    {
-                                        graphBuilder.AddDeferredPropertySet(node, DgmlNodeProperties.ContainsChildren, false);
-                                    }
+                                    graphBuilder.AddDeferredPropertySet(node, DgmlNodeProperties.ContainsChildren, false);
                                 }
                             }
                         }
