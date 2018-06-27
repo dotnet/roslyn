bda3e1912b7a4f6e301d0c16daf44155c2cd1f05
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-06-26 20:31:19 -0700

    Extract code for classifying a document span into its own helper class.

---
+++ src/EditorFeatures/Core/Implementation/Classification/EditorClassifier.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.Editor
+{
+}
