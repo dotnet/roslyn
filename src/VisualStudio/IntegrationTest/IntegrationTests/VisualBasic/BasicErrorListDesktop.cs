78cf54704b289eb133d6b1e5fa885eca7c06f22e
Sam Harwell <sam.harwell@microsoft.com>
2018-06-28 10:20:22 -0500

    Fixup BasicErrorListDesktop

--- src/VisualStudio/IntegrationTest/IntegrationTests/VisualBasic/BasicErrorListDesktop.cs
+++ src/VisualStudio/IntegrationTest/IntegrationTests/VisualBasic/BasicErrorListDesktop.cs
@@ -3,7 +3,7 @@
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Test.Utilities;
 using Microsoft.VisualStudio.IntegrationTest.Utilities;
-using Roslyn.Test.Utilities;
+using Microsoft.VisualStudio.IntegrationTest.Utilities.Harness;
 using Xunit;
 
 namespace Roslyn.VisualStudio.IntegrationTests.VisualBasic
@@ -16,13 +16,13 @@ namespace Roslyn.VisualStudio.IntegrationTests.VisualBasic
         {
         }
 
-        [WpfFact, Trait(Traits.Feature, Traits.Features.ErrorList)]
+        [IdeFact, Trait(Traits.Feature, Traits.Features.ErrorList)]
         public override async Task ErrorListAsync()
         {
             await base.ErrorListAsync();
         }
 
-        [WpfFact, Trait(Traits.Feature, Traits.Features.ErrorList)]
+        [IdeFact, Trait(Traits.Feature, Traits.Features.ErrorList)]
         public override async Task ErrorsDuringMethodBodyEditingAsync()
         {
             await base.ErrorsDuringMethodBodyEditingAsync();
