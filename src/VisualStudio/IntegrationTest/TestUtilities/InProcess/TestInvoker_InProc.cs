d9f66b2b75a388cc33b5c8c8f02441e2ed49b686
Sam Harwell <sam.harwell@microsoft.com>
2018-06-29 07:52:53 -0500

    Remove dead code

--- src/VisualStudio/IntegrationTest/TestUtilities/InProcess/TestInvoker_InProc.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/InProcess/TestInvoker_InProc.cs
@@ -1,12 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Threading;
-using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.VisualStudio.IntegrationTest.Utilities.Harness;
 using Xunit.Abstractions;
 using Xunit.Sdk;
@@ -25,41 +18,6 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.InProcess
         public InProcessIdeTestAssemblyRunner CreateTestAssemblyRunner(ITestAssembly testAssembly, IXunitTestCase[] testCases, IMessageSink diagnosticMessageSink, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
         {
             return new InProcessIdeTestAssemblyRunner(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions);
-        }
-
-        public Tuple<decimal, Exception> InvokeTest(
-            ITest test,
-            IMessageBus messageBus,
-            Type testClass,
-            object[] constructorArguments,
-            MethodInfo testMethod,
-            object[] testMethodArguments)
-        {
-            var aggregator = new ExceptionAggregator();
-            var beforeAfterAttributes = new BeforeAfterTestAttribute[0];
-            var cancellationTokenSource = new CancellationTokenSource();
-
-            var synchronizationContext = new DispatcherSynchronizationContext(Application.Current.Dispatcher, DispatcherPriority.Background);
-            var result = Task.Factory.StartNew(
-                async () =>
-                {
-                    var invoker = new XunitTestInvoker(
-                        test,
-                        messageBus,
-                        testClass,
-                        constructorArguments,
-                        testMethod,
-                        testMethodArguments,
-                        beforeAfterAttributes,
-                        aggregator,
-                        cancellationTokenSource);
-                    return await invoker.RunAsync();
-                },
-                CancellationToken.None,
-                TaskCreationOptions.None,
-                new SynchronizationContextTaskScheduler(synchronizationContext)).Unwrap().GetAwaiter().GetResult();
-
-            return Tuple.Create(result, aggregator.ToException());
         }
     }
 }
--- src/VisualStudio/IntegrationTest/TestUtilities/OutOfProcess/TestInvoker_OutOfProc.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/OutOfProcess/TestInvoker_OutOfProc.cs
@@ -1,8 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Linq;
-using System.Reflection;
 using Microsoft.VisualStudio.IntegrationTest.Utilities.Harness;
 using Microsoft.VisualStudio.IntegrationTest.Utilities.InProcess;
 using Xunit.Abstractions;
@@ -26,58 +23,6 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.OutOfProcess
         public InProcessIdeTestAssemblyRunner CreateTestAssemblyRunner(ITestAssembly testAssembly, IXunitTestCase[] testCases, IMessageSink diagnosticMessageSink, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
         {
             return TestInvokerInProc.CreateTestAssemblyRunner(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions);
-        }
-
-        public Tuple<decimal, Exception> InvokeTest(
-            ITest test,
-            IMessageBus messageBus,
-            Type testClass,
-            object[] constructorArguments,
-            MethodInfo testMethod,
-            object[] testMethodArguments)
-        {
-            if (constructorArguments != null)
-            {
-                if (constructorArguments.OfType<ITestOutputHelper>().Any())
-                {
-                    constructorArguments = (object[])constructorArguments.Clone();
-                    for (var i = 0; i < constructorArguments.Length; i++)
-                    {
-                        if (constructorArguments[i] is ITestOutputHelper testOutputHelper)
-                        {
-                            constructorArguments[i] = new TestOutputHelperWrapper(testOutputHelper);
-                        }
-                    }
-                }
-            }
-
-            return TestInvokerInProc.InvokeTest(
-                test,
-                messageBus,
-                testClass,
-                constructorArguments,
-                testMethod,
-                testMethodArguments);
-        }
-
-        private class TestOutputHelperWrapper : MarshalByRefObject, ITestOutputHelper
-        {
-            private readonly ITestOutputHelper _testOutputHelper;
-
-            public TestOutputHelperWrapper(ITestOutputHelper testOutputHelper)
-            {
-                _testOutputHelper = testOutputHelper;
-            }
-
-            public void WriteLine(string message)
-            {
-                _testOutputHelper.WriteLine(message);
-            }
-
-            public void WriteLine(string format, params object[] args)
-            {
-                _testOutputHelper.WriteLine(format, args);
-            }
         }
     }
 }
