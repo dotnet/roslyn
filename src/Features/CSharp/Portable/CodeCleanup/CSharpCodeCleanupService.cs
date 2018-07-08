d8783a0dc3356ef564b80c269d139f85ee0bf364
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-07-07 22:59:37 -0700

    Include the description up front when computing and applying the fixes.

--- src/Features/CSharp/Portable/CodeCleanup/CSharpCodeCleanupService.cs
+++ src/Features/CSharp/Portable/CodeCleanup/CSharpCodeCleanupService.cs
@@ -25,11 +25,6 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeCleanup
     [ExportLanguageService(typeof(ICodeCleanupService), LanguageNames.CSharp), Shared]
     internal class CSharpCodeCleanupService : ICodeCleanupService
     {
-        /// <summary>
-        /// Maps format document code cleanup options to DiagnosticId[]
-        /// </summary>
-        private static ImmutableArray<Tuple<PerLanguageOption<bool>, ImmutableArray<string>>> _optionDiagnosticsMappings = GetCodeCleanupOptionMapping();
-
         private readonly ICodeFixService _codeFixServiceOpt;
 
         [ImportingConstructor]
@@ -42,90 +37,91 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeCleanup
             _codeFixServiceOpt = codeFixService;
         }
 
-        private static ImmutableArray<Tuple<PerLanguageOption<bool>, ImmutableArray<string>>> GetCodeCleanupOptionMapping()
-        {
-            return ImmutableArray.Create<Tuple<PerLanguageOption<bool>, ImmutableArray<string>>>
-            (
-                Tuple.Create(
-                    CodeCleanupOptions.ApplyImplicitExplicitTypePreferences,
-                    ImmutableArray.Create(IDEDiagnosticIds.UseImplicitTypeDiagnosticId,
-                                          IDEDiagnosticIds.UseExplicitTypeDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.ApplyThisQualificationPreferences,
-                    ImmutableArray.Create(IDEDiagnosticIds.AddQualificationDiagnosticId,
-                                          IDEDiagnosticIds.RemoveQualificationDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.ApplyLanguageFrameworkTypePreferences,
-                    ImmutableArray.Create(IDEDiagnosticIds.PreferFrameworkTypeInDeclarationsDiagnosticId,
-                                          IDEDiagnosticIds.PreferFrameworkTypeInMemberAccessDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.AddRemoveBracesForSingleLineControlStatements,
-                    ImmutableArray.Create(IDEDiagnosticIds.AddBracesDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.AddAccessibilityModifiers,
-                    ImmutableArray.Create(IDEDiagnosticIds.AddAccessibilityModifiersDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.SortAccessibilityModifiers,
-                    ImmutableArray.Create(IDEDiagnosticIds.OrderModifiersDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.MakePrivateFieldReadonlyWhenPossible,
-                    ImmutableArray.Create(IDEDiagnosticIds.MakeFieldReadonlyDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.RemoveUnnecessaryCasts,
-                    ImmutableArray.Create(IDEDiagnosticIds.RemoveUnnecessaryCastDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.ApplyExpressionBlockBodyPreferences,
-                    ImmutableArray.Create(IDEDiagnosticIds.UseExpressionBodyForConstructorsDiagnosticId,
-                                          IDEDiagnosticIds.UseExpressionBodyForMethodsDiagnosticId,
-                                          IDEDiagnosticIds.UseExpressionBodyForConversionOperatorsDiagnosticId,
-                                          IDEDiagnosticIds.UseExpressionBodyForOperatorsDiagnosticId,
-                                          IDEDiagnosticIds.UseExpressionBodyForPropertiesDiagnosticId,
-                                          IDEDiagnosticIds.UseExpressionBodyForIndexersDiagnosticId,
-                                          IDEDiagnosticIds.UseExpressionBodyForAccessorsDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.ApplyInlineOutVariablePreferences,
-                    ImmutableArray.Create(IDEDiagnosticIds.InlineDeclarationDiagnosticId)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.RemoveUnusedVariables,
-                    ImmutableArray.Create(CSharpRemoveUnusedVariableCodeFixProvider.CS0168,
-                                          CSharpRemoveUnusedVariableCodeFixProvider.CS0219)
-                ),
-                Tuple.Create(
-                    CodeCleanupOptions.ApplyObjectCollectionInitializationPreferences,
-                    ImmutableArray.Create(IDEDiagnosticIds.UseObjectInitializerDiagnosticId,
-                                          IDEDiagnosticIds.UseCollectionInitializerDiagnosticId)
-                )
+        /// <summary>
+        /// Maps format document code cleanup options to DiagnosticId[]
+        /// </summary>
+        private static ImmutableArray<(string description, PerLanguageOption<bool> option, ImmutableArray<string> diagnosticIds)> _optionDiagnosticsMappings =
+            ImmutableArray.Create(
+                (CSharpFeaturesResources.Apply_implicit_explicit_type_preferences,
+                 CodeCleanupOptions.ApplyImplicitExplicitTypePreferences,
+                 ImmutableArray.Create(IDEDiagnosticIds.UseImplicitTypeDiagnosticId,
+                                       IDEDiagnosticIds.UseExplicitTypeDiagnosticId)),
+                (CSharpFeaturesResources.Apply_this_qualification_preferences,
+                 CodeCleanupOptions.ApplyThisQualificationPreferences,
+                 ImmutableArray.Create(IDEDiagnosticIds.AddQualificationDiagnosticId,
+                                       IDEDiagnosticIds.RemoveQualificationDiagnosticId)),
+                (CSharpFeaturesResources.Apply_language_framework_type_preferences,
+                 CodeCleanupOptions.ApplyLanguageFrameworkTypePreferences,
+                 ImmutableArray.Create(IDEDiagnosticIds.PreferFrameworkTypeInDeclarationsDiagnosticId,
+                                      IDEDiagnosticIds.PreferFrameworkTypeInMemberAccessDiagnosticId)),
+                (CSharpFeaturesResources.Add_remove_braces_for_single_line_control_statements,
+                 CodeCleanupOptions.AddRemoveBracesForSingleLineControlStatements,
+                 ImmutableArray.Create(IDEDiagnosticIds.AddBracesDiagnosticId)),
+                (CSharpFeaturesResources.Add_accessibility_modifiers,
+                 CodeCleanupOptions.AddAccessibilityModifiers,
+                 ImmutableArray.Create(IDEDiagnosticIds.AddAccessibilityModifiersDiagnosticId)),
+                (CSharpFeaturesResources.Sort_accessibility_modifiers,
+                 CodeCleanupOptions.SortAccessibilityModifiers,
+                 ImmutableArray.Create(IDEDiagnosticIds.OrderModifiersDiagnosticId)),
+                (CSharpFeaturesResources.Make_private_field_readonly_when_possible,
+                 CodeCleanupOptions.MakePrivateFieldReadonlyWhenPossible,
+                 ImmutableArray.Create(IDEDiagnosticIds.MakeFieldReadonlyDiagnosticId)),
+                (CSharpFeaturesResources.Remove_unnecessary_casts,
+                 CodeCleanupOptions.RemoveUnnecessaryCasts,
+                 ImmutableArray.Create(IDEDiagnosticIds.RemoveUnnecessaryCastDiagnosticId)),
+                (CSharpFeaturesResources.Apply_expression_block_body_preferences,
+                 CodeCleanupOptions.ApplyExpressionBlockBodyPreferences,
+                 ImmutableArray.Create(IDEDiagnosticIds.UseExpressionBodyForConstructorsDiagnosticId,
+                                       IDEDiagnosticIds.UseExpressionBodyForMethodsDiagnosticId,
+                                       IDEDiagnosticIds.UseExpressionBodyForConversionOperatorsDiagnosticId,
+                                       IDEDiagnosticIds.UseExpressionBodyForOperatorsDiagnosticId,
+                                       IDEDiagnosticIds.UseExpressionBodyForPropertiesDiagnosticId,
+                                       IDEDiagnosticIds.UseExpressionBodyForIndexersDiagnosticId,
+                                       IDEDiagnosticIds.UseExpressionBodyForAccessorsDiagnosticId)),
+                (CSharpFeaturesResources.Apply_inline_out_variable_preferences,
+                 CodeCleanupOptions.ApplyInlineOutVariablePreferences,
+                 ImmutableArray.Create(IDEDiagnosticIds.InlineDeclarationDiagnosticId)),
+                (CSharpFeaturesResources.Remove_unused_variables,
+                 CodeCleanupOptions.RemoveUnusedVariables,
+                 ImmutableArray.Create(CSharpRemoveUnusedVariableCodeFixProvider.CS0168,
+                                       CSharpRemoveUnusedVariableCodeFixProvider.CS0219)),
+                (CSharpFeaturesResources.Apply_object_collection_initialization_preferences,
+                 CodeCleanupOptions.ApplyObjectCollectionInitializationPreferences,
+                 ImmutableArray.Create(IDEDiagnosticIds.UseObjectInitializerDiagnosticId,
+                                       IDEDiagnosticIds.UseCollectionInitializerDiagnosticId))
             );
-        }
 
         public async Task<Document> CleanupAsync(
             Document document, IProgressTracker progressTracker, CancellationToken cancellationToken)
         {
             var docOptions = await document.GetOptionsAsync(cancellationToken).ConfigureAwait(false);
 
-            // add one item for the 'Remove Usings', one for 'Sort using' and one item for the
-            // 'format' actions we'll do last
-            progressTracker.AddItems(3);
+            // add one item for the 'format' action we'll do last
+            progressTracker.AddItems(1);
+
+            // and one for 'remove/sort usings' if we're going to run that.
+            var organizeUsings = false;
+            if (docOptions.GetOption(CodeCleanupOptions.RemoveUnusedImports) ||
+                docOptions.GetOption(CodeCleanupOptions.SortImports))
+            {
+                organizeUsings = true;
+                progressTracker.AddItems(1);
+            }
 
             if (_codeFixServiceOpt != null)
             {
                 document = await ApplyCodeFixesAsync(
-                    document, progressTracker, docOptions, cancellationToken).ConfigureAwait(false);
+                    document, docOptions, progressTracker, cancellationToken).ConfigureAwait(false);
             }
 
             // do the remove usings after code fix, as code fix might remove some code which can results in unused usings.
-            document = await RemoveSortUsingsAsync(
-                document, progressTracker, docOptions, cancellationToken).ConfigureAwait(false);
+            if (organizeUsings)
+            {
+                progressTracker.Description = CSharpFeaturesResources.Organize_Usings;
+                document = await RemoveSortUsingsAsync(
+                    document, docOptions, cancellationToken).ConfigureAwait(false);
+                progressTracker.ItemCompleted();
+            }
 
             progressTracker.Description = FeaturesResources.Formatting_document;
             var result = await Formatter.FormatAsync(document).ConfigureAwait(false);
@@ -135,84 +131,95 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeCleanup
         }
 
         private async Task<Document> RemoveSortUsingsAsync(
-            Document document, IProgressTracker tracker,  DocumentOptionSet docOptions, CancellationToken cancellationToken)
+            Document document, DocumentOptionSet docOptions, CancellationToken cancellationToken)
         {
             if (docOptions.GetOption(CodeCleanupOptions.RemoveUnusedImports))
             {
                 var removeUsingsService = document.GetLanguageService<IRemoveUnnecessaryImportsService>();
                 if (removeUsingsService != null)
                 {
-                    tracker.Description = CSharpFeaturesResources.Organize_Usings;
                     document = await removeUsingsService.RemoveUnnecessaryImportsAsync(document, cancellationToken).ConfigureAwait(false);
                 }
             }
 
-            // mark the remove usings command as done.
-            tracker.ItemCompleted();
-
             if (docOptions.GetOption(CodeCleanupOptions.SortImports))
             {
-                tracker.Description = CSharpFeaturesResources.Organize_Usings;
                 document = await OrganizeImportsService.OrganizeImportsAsync(document, cancellationToken).ConfigureAwait(false);
             }
-
-            // Mark the sort usings command as done.
-            tracker.ItemCompleted();
 
             return document;
         }
 
         private async Task<Document> ApplyCodeFixesAsync(
-            Document document, IProgressTracker progressTracker,
-            DocumentOptionSet docOptions, CancellationToken cancellationToken)
+            Document document, DocumentOptionSet docOptions,
+            IProgressTracker progressTracker, CancellationToken cancellationToken)
         {
-            var diagnosticIDs = GetEnabledDiagnosticIds(docOptions);
+            var enabledOptions = GetEnabledOptions(docOptions);
 
-            // Add a progress item for each diagnostic we're going to try to cleanup.
-            progressTracker.AddItems(diagnosticIDs.Length);
+            // Add a progress item for each enabled option we're going to fixup.
+            progressTracker.AddItems(enabledOptions.Length);
 
-            var fixAllService = document.Project.Solution.Workspace.Services.GetService<IFixAllGetFixesService>();
-
-            var dummy = new ProgressTracker();
-            foreach (var diagnosticId in GetEnabledDiagnosticIds(docOptions))
+            foreach (var (description, diagnosticIds) in enabledOptions)
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
-                var syntaxTree = await document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
-                var textSpan = new TextSpan(0, syntaxTree.Length);
+                progressTracker.Description = description;
+                document = await ApplyCodeFixesForSpecificDiagnosticIds(
+                    document, diagnosticIds, cancellationToken).ConfigureAwait(false);
 
-                var fixCollection = await _codeFixServiceOpt.GetFixesAsync(document, textSpan, diagnosticId, cancellationToken).ConfigureAwait(false);
-                if (fixCollection != null && fixCollection.Fixes.Length > 0)
-                {
-                    progressTracker.Description = fixCollection.Fixes[0].Action.Title;
-
-                    var fixAll = fixCollection.FixAllState;
-                    var solution = await fixAllService.GetFixAllChangedSolutionAsync(
-                        fixAll.CreateFixAllContext(dummy, cancellationToken)).ConfigureAwait(false);
-
-                    document = solution.GetDocument(document.Id);
-                }
-
-                // Mark this progress item as being completed.
+                // Mark this option as being completed.
                 progressTracker.ItemCompleted();
             }
 
             return document;
         }
 
-        private ImmutableArray<string> GetEnabledDiagnosticIds(DocumentOptionSet docOptions)
+        private async Task<Document> ApplyCodeFixesForSpecificDiagnosticIds(
+            Document document, ImmutableArray<string> diagnosticIds, CancellationToken cancellationToken)
         {
-            var diagnosticIds = ArrayBuilder<string>.GetInstance();
-
-            foreach (var tuple in _optionDiagnosticsMappings)
+            foreach (var diagnosticId in diagnosticIds)
             {
-                if (docOptions.GetOption(tuple.Item1))
+                document = await ApplyCodeFixesForSpecificDiagnosticId(
+                    document, diagnosticId, cancellationToken).ConfigureAwait(false);
+            }
+
+            return document;
+        }
+
+        private async Task<Document> ApplyCodeFixesForSpecificDiagnosticId(
+            Document document, string diagnosticId, CancellationToken cancellationToken)
+        {
+            var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+            var textSpan = new TextSpan(0, text.Length);
+
+            var fixCollection = await _codeFixServiceOpt.GetFixesAsync(
+                document, textSpan, diagnosticId, cancellationToken).ConfigureAwait(false);
+            if (fixCollection == null)
+            {
+                return document;
+            }
+
+            var fixAllService = document.Project.Solution.Workspace.Services.GetService<IFixAllGetFixesService>();
+
+            var solution = await fixAllService.GetFixAllChangedSolutionAsync(
+                fixCollection.FixAllState.CreateFixAllContext(new ProgressTracker(), cancellationToken)).ConfigureAwait(false);
+
+            return solution.GetDocument(document.Id);
+        }
+
+        private ImmutableArray<(string description, ImmutableArray<string> diagnosticIds)> GetEnabledOptions(DocumentOptionSet docOptions)
+        {
+            var result = ArrayBuilder<(string description, ImmutableArray<string> diagnosticIds)>.GetInstance();
+
+            foreach (var (description, option, diagnosticIds) in _optionDiagnosticsMappings)
+            {
+                if (docOptions.GetOption(option))
                 {
-                    diagnosticIds.AddRange(tuple.Item2);
+                    result.AddRange((description, diagnosticIds));
                 }
             }
 
-            return diagnosticIds.ToImmutableAndFree();
+            return result.ToImmutableAndFree();
         }
     }
 }
