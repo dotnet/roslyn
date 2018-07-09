47112d64bef0bd2da679b8489ded220560133478
Jason Malinowski <jason.malinowski@microsoft.com>
2018-07-09 12:52:28 -0700

    Remove whitespace that was accidentally added

--- src/EditorFeatures/TestUtilities/TestWaitIndicator.cs
+++ src/EditorFeatures/TestUtilities/TestWaitIndicator.cs
@@ -12,12 +12,12 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.Utilities
     [Export(typeof(IWaitIndicator))]
     [Export(typeof(VisualStudioIndicator.IWaitIndicator))]
     public sealed class TestWaitIndicator : IWaitIndicator, VisualStudioIndicator.IWaitIndicator
-    {	
-        public static readonly TestWaitIndicator Default = new TestWaitIndicator();	
-	
-        private readonly IWaitContext _waitContext;	
-        private readonly Microsoft.VisualStudio.Language.Intellisense.Utilities.IWaitContext _platformWaitContext = new UncancellableWaitContext();	
-	
+    {
+        public static readonly TestWaitIndicator Default = new TestWaitIndicator();
+
+        private readonly IWaitContext _waitContext;
+        private readonly Microsoft.VisualStudio.Language.Intellisense.Utilities.IWaitContext _platformWaitContext = new UncancellableWaitContext();
+
         public TestWaitIndicator()
             : this(new UncancellableWaitContext())
         {
@@ -48,33 +48,33 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.Utilities
         }
 
         VisualStudioIndicator.IWaitContext VisualStudioIndicator.IWaitIndicator.StartWait(string title, string message, bool allowCancel)
-        {	
-            return _platformWaitContext;	
-        }	
-
-        VisualStudioIndicator.WaitIndicatorResult VisualStudioIndicator.IWaitIndicator.Wait(string title, string message, bool allowCancel, Action<VisualStudioIndicator.IWaitContext> action) 
-        {	
-            try	
-            {		
-                action(_platformWaitContext);	
-            }	
-            catch (OperationCanceledException)	
-            {	
-                return VisualStudioIndicator.WaitIndicatorResult.Canceled;	
-            }	
-	
-            return VisualStudioIndicator.WaitIndicatorResult.Completed;	
+        {
+            return _platformWaitContext;
         }
-	
-        private sealed class UncancellableWaitContext : IWaitContext, VisualStudioIndicator.IWaitContext	
-        {	
-            public CancellationToken CancellationToken	
-            {	
-                get { return CancellationToken.None; }	
-            }	
-	
-            public IProgressTracker ProgressTracker { get; } = new ProgressTracker();	
-	
+
+        VisualStudioIndicator.WaitIndicatorResult VisualStudioIndicator.IWaitIndicator.Wait(string title, string message, bool allowCancel, Action<VisualStudioIndicator.IWaitContext> action)
+        {
+            try
+            {
+                action(_platformWaitContext);
+            }
+            catch (OperationCanceledException)
+            {
+                return VisualStudioIndicator.WaitIndicatorResult.Canceled;
+            }
+
+            return VisualStudioIndicator.WaitIndicatorResult.Completed;
+        }
+
+        private sealed class UncancellableWaitContext : IWaitContext, VisualStudioIndicator.IWaitContext
+        {
+            public CancellationToken CancellationToken
+            {
+                get { return CancellationToken.None; }
+            }
+
+            public IProgressTracker ProgressTracker { get; } = new ProgressTracker();
+
             public void UpdateProgress()
             {
             }
