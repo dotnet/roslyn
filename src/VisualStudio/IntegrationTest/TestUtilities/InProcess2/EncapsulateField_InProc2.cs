5073f4b30dc7835ea86ef0383bc3ac711cc2b16b
Sam Harwell <sam.harwell@microsoft.com>
2018-07-10 22:37:09 -0500

    Wait for light bulb session before invoking refactorings

--- src/VisualStudio/IntegrationTest/TestUtilities/InProcess2/EncapsulateField_InProc2.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/InProcess2/EncapsulateField_InProc2.cs
@@ -14,7 +14,14 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.InProcess2
 
         public string DialogName => "Preview Changes - Encapsulate Field";
 
-        public async Task InvokeAsync()
-            => await TestServices.Editor.SendKeysAsync(new KeyPress(VirtualKey.R, ShiftState.Ctrl), new KeyPress(VirtualKey.E, ShiftState.Ctrl));
+        public async Task InvokeAsync(bool waitForLightBulbSession = true)
+        {
+            if (waitForLightBulbSession)
+            {
+                await TestServices.Editor.WaitForLightBulbSessionAsync();
+            }
+
+            await TestServices.Editor.SendKeysAsync(new KeyPress(VirtualKey.R, ShiftState.Ctrl), new KeyPress(VirtualKey.E, ShiftState.Ctrl));
+        }
     }
 }
