ca76708c799d30a16b833f6ce13ec6a613443e71
Alireza Habibi <habibi.alireza@outlook.com>
2018-07-13 16:26:49 +0430

    Add formatting tests.

--- src/Workspaces/CSharpTest/Formatting/FormattingTests.cs
+++ src/Workspaces/CSharpTest/Formatting/FormattingTests.cs
@@ -4865,6 +4865,49 @@ var(x,y)=(1,2);
 
         [Fact]
         [Trait(Traits.Feature, Traits.Features.Formatting)]
+        public async Task SpacingInTupleArrayCreation3()
+        {
+            var code = @"class C
+{
+    void bar()
+    {
+        (string a, string b)[] ab = new(string a, string b)[0];
+    }
+}";
+            var expectedCode = @"class C
+{
+    void bar()
+    {
+        (string a, string b)[] ab = new (string a, string b)[0];
+    }
+}";
+
+            await AssertFormatAsync(expectedCode, code);
+        }
+
+        [Fact]
+        [Trait(Traits.Feature, Traits.Features.Formatting)]
+        public async Task SpacingInTargetTypedNew()
+        {
+            var code = @"class C
+{
+    void bar()
+    {
+        C ab = new ();
+    }
+}";
+            var expectedCode = @"class C
+{
+    void bar()
+    {
+        C ab = new();
+    }
+}";
+
+            await AssertFormatAsync(expectedCode, code);
+        }
+        [Fact]
+        [Trait(Traits.Feature, Traits.Features.Formatting)]
         public async Task SpacingInTupleExtension()
         {
             var code = @"static class Class5
