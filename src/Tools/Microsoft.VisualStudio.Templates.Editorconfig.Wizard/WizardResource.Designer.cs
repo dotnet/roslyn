5ce661c981480a9ea50c07b88c962946bb8dadbb
Jonathon Marolf <jmarolf@microsoft.com>
2018-07-18 12:29:59 -0700

    Fix access to resources
    Similar to issue descriped in https://github.com/dotnet/project-system/pull/3741
    When strings were updated for loc designer file was not properly regenerated

--- src/Tools/Microsoft.VisualStudio.Templates.Editorconfig.Wizard/WizardResource.Designer.cs
+++ src/Tools/Microsoft.VisualStudio.Templates.Editorconfig.Wizard/WizardResource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Templates.Editorconfig.Wizard {
+namespace Microsoft.VisualStudio.Templates.Editorconfig.Wizard {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Templates.Editorconfig.Wizard {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Templates.Editorconfig.Wizard.WizardResource", typeof(WizardResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.VisualStudio.Templates.Editorconfig.Wizard.WizardResource", typeof(WizardResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
