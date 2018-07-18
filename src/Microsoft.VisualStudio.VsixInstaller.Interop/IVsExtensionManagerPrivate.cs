79608b19a53a79454dc601e034821039a9e21af9
Sam Harwell <sam.harwell@microsoft.com>
2018-07-18 09:03:47 -0500

    Add Microsoft.VisualStudio.VsixInstaller projects

---
+++ src/Microsoft.VisualStudio.VsixInstaller.Interop/IVsExtensionManagerPrivate.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for more information.
+
+namespace Microsoft.Internal.VisualStudio.Shell.Interop
+{
+    using System;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+
+    [ComImport]
+    [Guid("753E55C6-E779-4A7A-BCD1-FD87181D52C0")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IVsExtensionManagerPrivate
+    {
+        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall)]
+        int GetEnabledExtensionContentLocations(
+            [In] [MarshalAs(UnmanagedType.LPWStr)] string szContentTypeName,
+            [In] uint cContentLocations,
+            [Out] [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.BStr, SizeParamIndex = 1)] string[] rgbstrContentLocations,
+            [Out] [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.BStr, SizeParamIndex = 1)] string[] rgbstrUniqueExtensionStrings,
+            out uint pcContentLocations);
+
+        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall)]
+        int GetEnabledExtensionContentLocationsWithNames(
+            [In] [MarshalAs(UnmanagedType.LPWStr)] string szContentTypeName,
+            [In] uint cContentLocations,
+            [Out] [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.BStr, SizeParamIndex = 1)] string[] rgbstrContentLocations,
+            [Out] [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.BStr, SizeParamIndex = 1)] string[] rgbstrUniqueExtensionStrings,
+            [Out] [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.BStr, SizeParamIndex = 1)] string[] rgbstrExtensionNames,
+            out uint pcContentLocations);
+
+        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall)]
+        int GetDisabledExtensionContentLocations(
+            [In] [MarshalAs(UnmanagedType.LPWStr)] string szContentTypeName,
+            [In] uint cContentLocations,
+            [Out] [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.BStr, SizeParamIndex = 1)] string[] rgbstrContentLocations,
+            [Out] [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.BStr, SizeParamIndex = 1)] string[] rgbstrUniqueExtensionStrings,
+            out uint pcContentLocations);
+
+        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall)]
+        int GetLastConfigurationChange([Out] [MarshalAs(UnmanagedType.LPArray)] DateTime[] pTimestamp);
+
+        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall)]
+        int LogAllInstalledExtensions();
+
+        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall)]
+        int GetUniqueExtensionString(
+            [In] [MarshalAs(UnmanagedType.LPWStr)] string szExtensionIdentifier,
+            [MarshalAs(UnmanagedType.BStr)] out string pbstrUniqueString);
+    }
+}
