482fd15e248c1958e8f80fdcb46df86e2e477f30
Lauren Rosenberg <t-laros@microsoft.com>
2018-07-23 15:41:48 -0700

    Fixed import

--- src/Compilers/CSharp/Portable/Binder/SwitchBinder.cs
+++ src/Compilers/CSharp/Portable/Binder/SwitchBinder.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Threading;
-using Microsoft.CodeAnalysis.CSharp.Lowering.LocalRewriter;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.PooledObjects;
