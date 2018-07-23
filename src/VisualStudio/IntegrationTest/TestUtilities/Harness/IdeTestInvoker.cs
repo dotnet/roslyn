220cc4695ae2244fbd1b4e37eb726b35fbc17128
Sam Harwell <sam.harwell@microsoft.com>
2018-07-22 21:20:26 -0500

    Capture screenshots of test failures during initialization and cleanup

--- src/VisualStudio/IntegrationTest/TestUtilities/Harness/IdeTestInvoker.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/Harness/IdeTestInvoker.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
@@ -15,6 +16,20 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.Harness
         public IdeTestInvoker(ITest test, IMessageBus messageBus, Type testClass, object[] constructorArguments, MethodInfo testMethod, object[] testMethodArguments, IReadOnlyList<BeforeAfterTestAttribute> beforeAfterAttributes, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource)
             : base(test, messageBus, testClass, constructorArguments, testMethod, testMethodArguments, beforeAfterAttributes, aggregator, cancellationTokenSource)
         {
+        }
+
+        protected override object CreateTestClass()
+        {
+            var testClass = base.CreateTestClass();
+
+            // Wrap the test class to provide hooks for initialization and cleanup
+            return new TestClassWrapper(testClass);
+        }
+
+        protected override Task<decimal> InvokeTestMethodAsync(object testClassInstance)
+        {
+            // Unwrap the test class for invoking the test method
+            return base.InvokeTestMethodAsync(((TestClassWrapper)testClassInstance).TestClass);
         }
 
         protected override object CallTestMethod(object testClassInstance)
@@ -41,6 +56,64 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.Harness
                 // it here.
                 InProcessIdeTestAssemblyRunner.SaveScreenshot(ex);
                 throw;
+            }
+        }
+
+        internal sealed class TestClassWrapper : IAsyncLifetime, IDisposable
+        {
+            public TestClassWrapper(object testClass)
+            {
+                TestClass = testClass;
+            }
+
+            public object TestClass
+            {
+                get;
+            }
+
+            public async Task InitializeAsync()
+            {
+                try
+                {
+                    await ((TestClass as IAsyncLifetime)?.InitializeAsync() ?? Task.CompletedTask);
+                }
+                catch (Exception ex) when (!InProcessIdeTestAssemblyRunner.IsCapturedFirstChanceException(ex))
+                {
+                    // This exception failed a test, but wasn't by the first-chance exception handler. Make sure to
+                    // record it here.
+                    InProcessIdeTestAssemblyRunner.SaveScreenshot(ex);
+                    throw;
+                }
+            }
+
+            public async Task DisposeAsync()
+            {
+                try
+                {
+                    await ((TestClass as IAsyncLifetime)?.DisposeAsync() ?? Task.CompletedTask);
+                }
+                catch (Exception ex) when (!InProcessIdeTestAssemblyRunner.IsCapturedFirstChanceException(ex))
+                {
+                    // This exception failed a test, but wasn't by the first-chance exception handler. Make sure to
+                    // record it here.
+                    InProcessIdeTestAssemblyRunner.SaveScreenshot(ex);
+                    throw;
+                }
+            }
+
+            public void Dispose()
+            {
+                try
+                {
+                    (TestClass as IDisposable)?.Dispose();
+                }
+                catch (Exception ex) when (!InProcessIdeTestAssemblyRunner.IsCapturedFirstChanceException(ex))
+                {
+                    // This exception failed a test, but wasn't by the first-chance exception handler. Make sure to
+                    // record it here.
+                    InProcessIdeTestAssemblyRunner.SaveScreenshot(ex);
+                    throw;
+                }
             }
         }
     }
