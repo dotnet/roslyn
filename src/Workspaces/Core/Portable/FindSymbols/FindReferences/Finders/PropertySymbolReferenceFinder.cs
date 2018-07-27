4fcb2f859701ab874693e8db8820582710f94ea1
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-07-26 20:08:29 -0400

    Capitalize.

--- src/Workspaces/Core/Portable/FindSymbols/FindReferences/Finders/PropertySymbolReferenceFinder.cs
+++ src/Workspaces/Core/Portable/FindSymbols/FindReferences/Finders/PropertySymbolReferenceFinder.cs
@@ -96,7 +96,7 @@ namespace Microsoft.CodeAnalysis.FindSymbols.Finders
             if (options.AssociatePropertyReferencesWithSpecificAccessor)
             {
                 // We want to associate property references to a specific accessor (if an accessor
-                // is being referenced).  Check if this reference would match an accessor. if so, do
+                // is being referenced).  Check if this reference would match an accessor. If so, do
                 // not add it.  It will be added by PropertyAccessorSymbolReferenceFinder.
                 var semanticFacts = document.GetLanguageService<ISemanticFactsService>();
 
