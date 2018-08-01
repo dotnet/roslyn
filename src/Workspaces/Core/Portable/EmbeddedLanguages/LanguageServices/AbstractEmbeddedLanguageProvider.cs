58103f6f80af096248e43893f19a216fce58c0c9
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-08-01 11:44:13 -0400

    Add an extension point to allow services to light up on string-literals (classification only for now)

---
+++ src/Workspaces/Core/Portable/EmbeddedLanguages/LanguageServices/AbstractEmbeddedLanguageProvider.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.EmbeddedLanguages.VirtualChars;
+using Microsoft.CodeAnalysis.LanguageServices;
+
+namespace Microsoft.CodeAnalysis.EmbeddedLanguages.LanguageServices
+{
+    /// <summary>
+    /// Abstract implementation of the C# and VB embedded language providers.
+    /// </summary>
+    internal abstract class AbstractEmbeddedLanguageProvider : IEmbeddedLanguageProvider
+    {
+        private readonly ImmutableArray<IEmbeddedLanguage> _embeddedLanguages;
+         
+        protected AbstractEmbeddedLanguageProvider(
+            int stringLiteralToken,
+            int interpolatedTextToken,
+            ISyntaxFactsService syntaxFacts,
+            ISemanticFactsService semanticFacts,
+            IVirtualCharService virtualCharService)
+        {
+            _embeddedLanguages = ImmutableArray.Create<IEmbeddedLanguage>();
+        }
+
+        public ImmutableArray<IEmbeddedLanguage> GetEmbeddedLanguages()
+            => _embeddedLanguages;
+    }
+}
