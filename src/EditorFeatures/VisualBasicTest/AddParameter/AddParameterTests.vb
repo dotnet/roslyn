472f16c1c4266f075ad7ec8ad08e98e931ab1af9
Martin Strecker <MaStr11@users.noreply.github.com>
2018-08-04 11:51:21 +0100

    Added tests for VB.

--- src/EditorFeatures/VisualBasicTest/AddParameter/AddParameterTests.vb
+++ src/EditorFeatures/VisualBasicTest/AddParameter/AddParameterTests.vb
@@ -830,7 +830,7 @@ Public Class C
         M(new System.Exception(), 2)
     End Sub
 End Class"
-            Await TestInRegularAndScriptAsync(code, Fix)
+            Await TestInRegularAndScriptAsync(code, fix)
         End Function
 
         <WorkItem(21446, "https://github.com/dotnet/roslyn/issues/21446")>
@@ -1039,6 +1039,110 @@ Public Class C
 End Class
 "
             Await TestMissingAsync(code)
+        End Function
+
+        <WorkItem(29061, "https://github.com/dotnet/roslyn/issues/29061")>
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddParameter)>
+        Public Async Function TestConstructorInitializer_OfferFixForConstructorWithDiagnostic() As Task
+            ' Error BC30057: Too many arguments to 'Public Sub New()'.
+            Dim code =
+"
+Public Class C
+    Public Sub New()
+        Me.New([|1|])
+    End Sub
+End Class
+"
+            ' The fixed code has error BC30298: Constructor 'Public Sub New(v As Integer)' cannot call itself: 
+            ' Public Sub New(v As Integer)' calls 'Public Sub New(v As Integer)'.
+            ' The correct behavior would be to not offer a fix.
+            Dim fix =
+"
+Public Class C
+    Public Sub New(v As Integer)
+        Me.New(1)
+    End Sub
+End Class
+"
+            Await TestInRegularAndScriptAsync(code, fix)
+        End Function
+
+        <WorkItem(29061, "https://github.com/dotnet/roslyn/issues/29061")>
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddParameter)>
+        Public Async Function TestConstructorInitializer_OfferFixForOtherConstructors() As Task
+            ' Error BC30516: Overload resolution failed because no accessible 'New' accepts this number of arguments.
+            Dim code =
+"
+Public Class C
+    Public Sub New(i As Integer)
+    End Sub
+    
+    Public Sub New()
+        Me.[|New|](1,1)
+    End Sub
+End Class
+"
+            Dim fix0 =
+"
+Public Class C
+    Public Sub New(i As Integer, v As Integer)
+    End Sub
+    
+    Public Sub New()
+        Me.New(1,1)
+    End Sub
+End Class
+"
+            ' This fix is wrong for several reasons. The root cause is, that the fix is offered for the constructor
+            ' with the diagnostic on it causing "error BC30298: Constructor '..' cannot call itself" sooner or later.
+            Dim fix1 =
+"
+Public Class C
+    Public Sub New(i As Integer)
+    End Sub
+    
+    Public Sub New(v As Integer)
+        Me.New(1,1)
+    End Sub
+End Class
+"
+            Await TestInRegularAndScriptAsync(code, fix0, index:=0)
+            Await TestInRegularAndScriptAsync(code, fix1, index:=1)
+        End Function
+
+        <WorkItem(29061, "https://github.com/dotnet/roslyn/issues/29061")>
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddParameter)>
+        Public Async Function TestConstructorInitializer_OfferFixForBaseConstrcutors() As Task
+            ' error BC30057: Too many arguments to 'Public Sub New()'.
+            Dim code =
+"
+Public Class B
+    Public Sub New()
+    End Sub
+End Class
+
+Public Class C
+    Inherits B
+    Public Sub New()
+        MyBase.New([|1|])
+    End Sub
+End Class
+"
+            Dim fix0 =
+"
+Public Class B
+    Public Sub New(v As Integer)
+    End Sub
+End Class
+
+Public Class C
+    Inherits B
+    Public Sub New()
+        MyBase.New(1)
+    End Sub
+End Class
+"
+            Await TestInRegularAndScriptAsync(code, fix0, index:=0)
         End Function
     End Class
 End Namespace
