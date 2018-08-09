31689a857ae510dcc7aa46206109efe87c522539
Charles Stoner <chucks@microsoft.com>
2018-08-09 10:49:31 -0700

    Disable bootstrap build

--- build/scripts/cibuild.cmd
+++ build/scripts/cibuild.cmd
@@ -1,2 +1,3 @@
 @echo off
-powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build.ps1" -cibuild -build -restore -bootstrap -binaryLog %*
+REM PROTOTYPE(NullableDogfood): Removed -bootstrap since `master` does not support annotations.
+powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build.ps1" -cibuild -build -restore -binaryLog %*
