bfb34a3b29b6e09bbac91807af06901e65a87289
Paul Ming <paulming@microsoft.com>
2018-08-10 13:01:58 -0700

    TaintedData abstract value

---
+++ src/Utilities/FlowAnalysis/Analysis/TaintedDataAnalysis/TaintedDataAbstractValue.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Analyzer.Utilities.FlowAnalysis.Analysis.TaintedDataAnalysis
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+
+    /// <summary>
+    /// Abstract tainted data value shared by a set of one of more <see cref="AnalysisEntity"/> instances tracked by <see cref="TaintedDataAnalysis"/>.
+    /// </summary>
+    internal class TaintedDataAbstractValue : CacheBasedEquatable<TaintedDataAbstractValue>
+    {
+        public TaintedDataAbstractValue(ImmutableHashSet<AnalysisEntity> analysisEntities, TaintedDataAbstractValueKind kind)
+        {
+            this.AnalysisEntities = analysisEntities;
+            this.Kind = kind;
+        }
+
+        /// <summary>
+        /// The set of <see cref="AnalysisEntity"/> that this abstract value is representing.
+        /// </summary>
+        public ImmutableHashSet<AnalysisEntity> AnalysisEntities { get; }
+
+        /// <summary>
+        /// The abstract value that this is representing.
+        /// </summary>
+        public TaintedDataAbstractValueKind Kind { get; }
+
+        protected override int ComputeHashCode()
+        {
+            int hashCode = HashUtilities.Combine(Kind.GetHashCode(), this.AnalysisEntities.Count.GetHashCode());
+            foreach (AnalysisEntity entity in this.AnalysisEntities)
+            {
+                hashCode = HashUtilities.Combine(entity.GetHashCode(), hashCode);
+            }
+
+            return hashCode;
+        }
+    }
+}
---
+++ src/Utilities/FlowAnalysis/Analysis/TaintedDataAnalysis/TaintedDataAbstractValueKind.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Analyzer.Utilities.FlowAnalysis.Analysis.TaintedDataAnalysis
+{
+    /// <summary>
+    /// Kind for <see cref="TaintedDataAbstractValue"/>.
+    /// </summary>
+    internal enum TaintedDataAbstractValueKind
+    {
+        /// <summary>
+        /// Indicates the data may or may not be tainted.
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// Indicates the data is definitely untainted.
+        /// </summary>
+        NotTainted,
+
+        /// <summary>
+        /// Indicates that data is definitely tainted.
+        /// </summary>
+        Tainted,
+    }
+}
