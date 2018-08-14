a1d141863ed732489f7b74481884700a7f2e6cd9
Ashwini <ashwini@unity3d.com>
2018-08-14 11:13:55 -0400

    Gitlab CI configuration

--- external/unity/RoslynBuilder/RoslynBuilder/PrepareArtifactsForBuildsZipStep.cs
+++ external/unity/RoslynBuilder/RoslynBuilder/PrepareArtifactsForBuildsZipStep.cs
@@ -30,7 +30,7 @@ namespace RoslynBuilder
 			{
 				"We build roslyn from this repository: https://github.com/Unity-Technologies/Roslyn",
 				"We use unity-master branch",
-				"It is built on katana - don't build it on your local machine"
+				"It is built on the Gitlab mirror https://gitlab.internal.unity3d.com/vm/roslyn pipeline - don't build it on your local machine"
 			});
 		}
 	}
