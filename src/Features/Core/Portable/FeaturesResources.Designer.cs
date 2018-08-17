4b8785a5b392534698e8ebc8b531a7fb35e7041b
Manish Vasani <mavasani@microsoft.com>
2018-08-17 06:51:22 -0700

    Add refactoring to convert foreach loop into linq invocations (Select, SelectMany and Where invocations)
    Adds one of the refactorings mentioned in #8953
    
    ```
    //
    // INPUT:
    //   foreach (var n1 in c1)
    //      foreach (var n2 in c2)
    //          if (n1 > n2)
    //              yield return n1 + n2;
    //
    // OUTPUT:
    //   c1.SelectMany(n1 => c2.Where(n2 => n1 > n2).Select(n2 => n1 + n2))
    //
    ```

--- src/Features/Core/Portable/FeaturesResources.Designer.cs
+++ src/Features/Core/Portable/FeaturesResources.Designer.cs
@@ -934,6 +934,15 @@ namespace Microsoft.CodeAnalysis {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Convert to linq.
+        /// </summary>
+        internal static string Convert_to_linq {
+            get {
+                return ResourceManager.GetString("Convert_to_linq", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Convert to query.
         /// </summary>
         internal static string Convert_to_query {
@@ -941,7 +950,7 @@ namespace Microsoft.CodeAnalysis {
                 return ResourceManager.GetString("Convert_to_query", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Convert to struct.
         /// </summary>
