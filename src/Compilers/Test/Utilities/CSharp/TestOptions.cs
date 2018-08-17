d42f9e083f9a408eeff2c9ac14e2da3c31047a7b
Manish Vasani <mavasani@microsoft.com>
2018-08-17 16:29:07 -0700

    Restore the machinery for enabling language features by feature flags
    Fixes #29291

--- src/Compilers/Test/Utilities/CSharp/TestOptions.cs
+++ src/Compilers/Test/Utilities/CSharp/TestOptions.cs
@@ -89,6 +89,27 @@ namespace Microsoft.CodeAnalysis.CSharp.Test.Utilities
             return options;
         }
 
+        internal static CSharpParseOptions WithExperimental(this CSharpParseOptions options, params MessageID[] features)
+        {
+            if (features.Length == 0)
+            {
+                throw new InvalidOperationException("Need at least one feature to enable");
+            }
+
+            var list = new List<KeyValuePair<string, string>>();
+            foreach (var feature in features)
+            {
+                var name = feature.RequiredFeature();
+                if (name == null)
+                {
+                    throw new InvalidOperationException($"{feature} is not a valid experimental feature");
+                }
+                list.Add(new KeyValuePair<string, string>(name, "true"));
+            }
+
+            return options.WithFeatures(options.Features.Concat(list));
+        }
+
         public static CSharpParseOptions WithFlowAnalysisFeature(this CSharpParseOptions options)
         {
             return options.WithFeatures(options.Features.Concat(new[] { new KeyValuePair<string, string>("flow-analysis", "true") }));
