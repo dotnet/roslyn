a91253a4fa1a0cdca2720d159da5d49f2392d783
Lillo <rafa_lillo@outlook.com>
2018-08-22 08:58:44 -0300

    add comments

--- src/Features/CSharp/Portable/Completion/KeywordRecommenders/AssemblyKeywordRecommender.cs
+++ src/Features/CSharp/Portable/Completion/KeywordRecommenders/AssemblyKeywordRecommender.cs
@@ -25,11 +25,32 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.KeywordRecommenders
             if (token.Parent.Kind() == SyntaxKind.AttributeList)
             {
                 var attributeList = token.Parent;
-                var compilationUnitSyntax = attributeList.Parent;
-                return compilationUnitSyntax is CompilationUnitSyntax || compilationUnitSyntax.Parent is CompilationUnitSyntax;
+                var parentSyntax = attributeList.Parent;
+                switch (parentSyntax)
+                {
+                    case CompilationUnitSyntax _:
+                    // There are some case the parente of attributeList is (Class/Interface/Enum/Struct)DeclarationSyntax, like:
+                    // [$$
+                    // class Goo {
+                    // for these cases is necessary check if they Parent is CompilationUnitSyntax
+                    case BaseTypeDeclarationSyntax baseType when baseType.Parent is CompilationUnitSyntax:
+                    // There is some case the parente of attributeList is IncompleteMemberSyntax(See test: TestInOuterAttribute), like:
+                    // [$$
+                    // for that case is necessary check if they Parent is CompilationUnitSyntax
+                    case IncompleteMemberSyntax incompleteMember when incompleteMember.Parent is CompilationUnitSyntax:
+                        return true;
+                    default:
+                        return false;
+                }
+                
+                // TODO: Remove before merge
+                //return compilationUnitSyntax is CompilationUnitSyntax || compilationUnitSyntax.Parent is CompilationUnitSyntax;
             }
 
             var skippedTokensTriviaSyntax = token.Parent;
+            // This case happens when:
+            // [$$
+            // namespace Goo {
             return skippedTokensTriviaSyntax is SkippedTokensTriviaSyntax;
         }
     }
