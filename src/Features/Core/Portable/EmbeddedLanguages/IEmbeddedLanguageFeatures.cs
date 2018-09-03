28b05a9aef09736409d8978cbdafd7b7bc111a79
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-09-03 15:31:16 -0700

    Simplify.

--- src/Features/Core/Portable/EmbeddedLanguages/IEmbeddedLanguageFeatures.cs
+++ src/Features/Core/Portable/EmbeddedLanguages/IEmbeddedLanguageFeatures.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeStyle;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.DocumentHighlighting;
 using Microsoft.CodeAnalysis.EmbeddedLanguages.LanguageServices;
@@ -20,6 +21,6 @@ namespace Microsoft.CodeAnalysis.Features.EmbeddedLanguages
         /// <summary>
         /// An optional analyzer that produces diagnostics for an embedded language string.
         /// </summary>
-        DiagnosticAnalyzer DiagnosticAnalyzer { get; }
+        AbstractCodeStyleDiagnosticAnalyzer DiagnosticAnalyzer { get; }
     }
 }
