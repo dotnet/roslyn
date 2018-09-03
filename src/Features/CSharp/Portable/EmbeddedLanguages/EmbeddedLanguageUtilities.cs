7a082677a9f2b4dd2091c5149befb44b3b0d24dd
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-09-03 15:25:30 -0700

    Add comment

--- src/Features/CSharp/Portable/EmbeddedLanguages/EmbeddedLanguageUtilities.cs
+++ src/Features/CSharp/Portable/EmbeddedLanguages/EmbeddedLanguageUtilities.cs
@@ -8,10 +8,16 @@ namespace Microsoft.CodeAnalysis.CSharp.Features.EmbeddedLanguages
     {
         public static string EscapeText(string text, SyntaxToken token)
         {
+            // This function is called when Completion needs to escape something its going to
+            // insert into the user's string token.  This means that we only have to escape
+            // things that completion could insert.  In this case, the only regex character
+            // that is relevant is the \ character, and it's only relevant if we insert into
+            // a normal string and not a verbatim string.  There are no other regex characters
+            // that completion will produce that need any escaping. 
             Debug.Assert(token.Kind() == SyntaxKind.StringLiteralToken);
             return token.IsVerbatimStringLiteral()
                 ? text
-                : text.Replace("\\", "\\\\");
+                : text.Replace(@"\", @"\\");
         }
     }
 }
