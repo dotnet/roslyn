10a91d3d9d5480e579c8cb48934daabf36567f73
Tomas Matousek <tomat@microsoft.com>
2018-09-04 08:56:50 -0700

    Retarget

--- src/Compilers/Core/CodeAnalysisTest/AnalyzerFileReferenceTests.cs
+++ src/Compilers/Core/CodeAnalysisTest/AnalyzerFileReferenceTests.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 // This file references the DesktopAnalyzerAssemblyLoader, which is only present in desktop
-#if NET46
+#if NET472
 
 using System;
 using System.Collections.Generic;
