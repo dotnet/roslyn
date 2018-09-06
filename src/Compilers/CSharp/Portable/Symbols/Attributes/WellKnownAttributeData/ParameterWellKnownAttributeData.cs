d04118f0335c8cade723d25ff37653578b375bbc
Julien Couvreur <julien.couvreur@gmail.com>
2018-09-06 16:29:33 -0700

    Typo

--- src/Compilers/CSharp/Portable/Symbols/Attributes/WellKnownAttributeData/ParameterWellKnownAttributeData.cs
+++ src/Compilers/CSharp/Portable/Symbols/Attributes/WellKnownAttributeData/ParameterWellKnownAttributeData.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-namespace Microsoft.CodeAnalysis.Symbols
+namespace Microsoft.CodeAnalysis.CSharp.Symbols
 {
     /// <summary>
     /// Information decoded from well-known custom attributes applied on a parameter.
