b444a0bfc5cac9b8ee62d075cb6aa3c5155ac5b6
Fredric Silberberg <frsilb@microsoft.com>
2018-09-07 14:18:16 -0700

    Fix warning level.

--- src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
@@ -324,6 +324,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 case ErrorCode.WRN_AttributesOnBackingFieldsNotAvailable:
                 case ErrorCode.WRN_TupleBinopLiteralNameMismatch:
                 case ErrorCode.WRN_TypeParameterSameAsOuterMethodTypeParameter:
+                case ErrorCode.WRN_DefaultLiteralConvertedToNullIsNotIntended:
                     return 1;
                 default:
                     return 0;
