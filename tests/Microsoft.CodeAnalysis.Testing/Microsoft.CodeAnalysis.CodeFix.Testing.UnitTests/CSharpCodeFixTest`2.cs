d26ddc9fd3c108c8bb68202981d2f4fca891a37a
Sam Harwell <sam@tunnelvisionlabs.com>
2018-09-09 15:45:22 -0500

    Support testing for the absence of code fixes
    Fixes #219

---
+++ tests/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.CodeFix.Testing.UnitTests/CSharpCodeFixTest`2.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.Testing
+{
+    internal class CSharpCodeFixTest<TAnalyzer, TCodeFix> : CodeFixTest<DefaultVerifier>
+        where TAnalyzer : DiagnosticAnalyzer, new()
+        where TCodeFix : CodeFixProvider, new()
+    {
+        public override string Language => LanguageNames.CSharp;
+
+        protected override string DefaultFileExt => "cs";
+
+        protected override CompilationOptions CreateCompilationOptions()
+            => new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+        protected override IEnumerable<DiagnosticAnalyzer> GetDiagnosticAnalyzers()
+        {
+            yield return new TAnalyzer();
+        }
+
+        protected override IEnumerable<CodeFixProvider> GetCodeFixProviders()
+        {
+            yield return new TCodeFix();
+        }
+    }
+}
