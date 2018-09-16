928dd8350710c20252236fb0d4ac2fa434800a42
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-09-15 18:01:29 -0700

    Add test.

--- src/EditorFeatures/CSharpTest/UseCompoundAssignment/UseCompoundAssignmentTests.cs
+++ src/EditorFeatures/CSharpTest/UseCompoundAssignment/UseCompoundAssignmentTests.cs
@@ -174,5 +174,25 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.UseCompoundAssignment
     }
 }");
         }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCompoundAssignment)]
+        public async Task TestLeftShiftExpression()
+        {
+            await TestInRegularAndScriptAsync(
+@"public class C
+{
+    void M(int a)
+    {
+        a [||]= a << 10;
+    }
+}",
+@"public class C
+{
+    void M(int a)
+    {
+        a <<= 10;
+    }
+}");
+        }
     }
 }
