d7072b60d7c137310f74c80ff93ff443431910d1
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-09-16 20:17:05 -0700

    Add tests.

---
+++ src/EditorFeatures/VisualBasicTest/InvertConditional/InvertIfTests.vb
@@ -0,0 +1,67 @@
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports System.Threading.Tasks
+Imports Microsoft.CodeAnalysis.CodeRefactorings
+Imports Microsoft.CodeAnalysis.VisualBasic.InvertConditional
+Imports Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.CodeRefactorings
+Imports Microsoft.CodeAnalysis.Test.Utilities
+Imports Xunit
+
+Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.InvertConditional
+    Partial Public Class InvertConditionalTests
+        Inherits AbstractVisualBasicCodeActionTest
+
+        Protected Overrides Function CreateCodeRefactoringProvider(workspace As Workspace, parameters As TestParameters) As CodeRefactoringProvider
+            Return New VisualBasicInvertConditionalCodeRefactoringProvider()
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInvertConditional)>
+        Public Async Function InvertConditional1() As Task
+            Await TestInRegularAndScriptAsync(
+"class C
+    sub M(x as boolean, a as integer, b as integer)
+        dim c = [||]if(x, a, b)
+    end sub
+end class",
+"class C
+    sub M(x as boolean, a as integer, b as integer)
+        dim c = if(Not x, b, a)
+    end sub
+end class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInvertConditional)>
+        Public Async Function InvertConditional2() As Task
+            Await TestInRegularAndScriptAsync(
+"class C
+    sub M(x as boolean, a as integer, b as integer)
+        dim c = [||]if(not x, a, b)
+    end sub
+end class",
+"class C
+    sub M(x as boolean, a as integer, b as integer)
+        dim c = if(x, b, a)
+    end sub
+end class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInvertConditional)>
+        Public Async Function TestTrivia() As Task
+            Await TestInRegularAndScriptAsync(
+"class C
+    sub M(x as boolean, a as integer, b as integer)
+        dim c = [||]if(x,
+            a,
+            b)
+    end sub
+end class",
+"class C
+    sub M(x as boolean, a as integer, b as integer)
+        dim c = if(Not x,
+            b,
+            a)
+    end sub
+end class")
+        End Function
+    End Class
+End Namespace
