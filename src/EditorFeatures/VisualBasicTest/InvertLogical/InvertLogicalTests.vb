dcb436f962a3ae230cca20016fc258747ebf0e2a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-09-17 12:27:22 -0700

    Add tests.

--- src/EditorFeatures/VisualBasicTest/InvertLogical/InvertLogicalTests.vb
+++ src/EditorFeatures/VisualBasicTest/InvertLogical/InvertLogicalTests.vb
@@ -118,5 +118,25 @@ end class",
     end sub
 end class")
         End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInvertLogical)>
+        Public Async Function TestMissingOnSimpleOr() As Task
+            Await TestMissingAsync(
+"class C
+    sub M(x as boolean, a as integer, b as integer)
+        dim c = a > 10 [||]or b < 20
+    end sub
+end class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInvertLogical)>
+        Public Async Function TestMissingOnSimpleAnd() As Task
+            Await TestMissingAsync(
+"class C
+    sub M(x as boolean, a as integer, b as integer)
+        dim c = a > 10 [||]or b < 20
+    end sub
+end class")
+        End Function
     End Class
 End Namespace
