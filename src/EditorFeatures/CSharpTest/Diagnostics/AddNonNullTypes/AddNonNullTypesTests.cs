3773fbca9d0e6c0bd78b2818076141b76de419c7
Julien Couvreur <jcouv@microsoft.com>
2018-09-21 21:54:20 -0700

    Also trigger on suppression operator

--- src/EditorFeatures/CSharpTest/Diagnostics/AddNonNullTypes/AddNonNullTypesTests.cs
+++ src/EditorFeatures/CSharpTest/Diagnostics/AddNonNullTypes/AddNonNullTypesTests.cs
@@ -34,6 +34,46 @@ class C
         }
 
         [Fact]
+        public async Task TestNullableInNonNullTypesFalseContext()
+        {
+            // https://github.com/dotnet/roslyn/issues/30099 We should detect present of NonNullTypes(false) and either not trigger, or toggle to `true`
+            await TestInRegularAndScriptAsync(
+@"
+[System.Runtime.CompilerServices.NonNullTypes(false)]
+class C
+{
+    [|string?|] M() { }
+}",
+@"
+[System.Runtime.CompilerServices.NonNullTypes(false)]
+[System.Runtime.CompilerServices.NonNullTypes]
+class C
+{
+    string? M() { }
+}");
+        }
+
+        [Fact]
+        public async Task TestNullableWithUsing()
+        {
+            await TestInRegularAndScriptAsync(
+@"
+using System.Runtime.CompilerServices;
+class C
+{
+    [|string?|] M() { }
+}",
+@"
+using System.Runtime.CompilerServices;
+
+[NonNullTypes]
+class C
+{
+    string? M() { }
+}");
+        }
+
+        [Fact]
         public async Task TestNullableOnMethodInInterface()
         {
             await TestInRegularAndScriptAsync(
@@ -70,6 +110,23 @@ class C
     {
         string? M();
     }
+}");
+        }
+
+        [Fact]
+        public async Task TestSuppression()
+        {
+            await TestInRegularAndScriptAsync(
+@"
+class C
+{
+    string M() { return [|null!|]; }
+}",
+@"
+[System.Runtime.CompilerServices.NonNullTypes]
+class C
+{
+    string M() { return null!; }
 }");
         }
     }
--- src/Features/CSharp/Portable/AddNonNullTypes/CSharpAddNonNullTypesCodeFixProvider.cs
+++ src/Features/CSharp/Portable/AddNonNullTypes/CSharpAddNonNullTypesCodeFixProvider.cs
@@ -18,13 +18,18 @@ namespace Microsoft.CodeAnalysis.CSharp.AddNonNullTypes
     /// offer to add a type-level NonNullTypes attribute.
     /// </summary>
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = PredefinedCodeFixProviderNames.AddNonNullTypes), Shared]
+#pragma warning disable RS1016 // This provider intentionally does not provide a FixAll
     internal class CSharpAddNonNullTypesCodeFixProvider : CodeFixProvider
+#pragma warning restore RS1016
     {
         // warning CS8632: The annotation for nullable reference types should only be used in code within a '[NonNullTypes(true)]' context.
         private const string CS8632 = nameof(CS8632);
 
+        // warning CS8629: The suppression operator (!) should be used in code with a '[NonNullTypes(true/false)]' context.
+        private const string CS8629 = nameof(CS8629);
+
         public sealed override ImmutableArray<string> FixableDiagnosticIds { get; }
-            = ImmutableArray.Create(CS8632);
+            = ImmutableArray.Create(CS8632, CS8629);
 
         public CSharpAddNonNullTypesCodeFixProvider()
         {
