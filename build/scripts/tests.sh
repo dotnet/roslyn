15b0199f0ca4ae95250c5753aaac1fada12ed18c
Katelyn Gadd <kg@luminance.org>
2018-09-25 13:48:17 -0700

    Refactor tests.sh for clarity and to eliminate shellcheck warnings. Add support for using mono --debug to run tests. Put usage instructions at the top of the script.

--- build/scripts/tests.sh
+++ build/scripts/tests.sh
@@ -2,11 +2,20 @@
 # Copyright (c) .NET Foundation and contributors. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+# USAGE:
+# build/scripts/tests.sh (Debug|Release) (dotnet|mono|mono-debug) [test assembly name] [xunit args]
+# If specified, the test assembly name must be a substring match for one or more test assemblies.
+# Note that it's a substring match so '.dll' would match all unit test DLLs and run them all.
+# Any xunit args specified after the assembly name will be passed directly to the test runner
+#  so you can run individual tests, i.e. -method "*.Query_01"
+
 set -e
 set -u
 
 build_configuration=${1:-Debug}
 runtime=${2:-dotnet}
+single_test_assembly=${3:-}
+xunit_args=(${@:4})
 
 this_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${this_dir}"/build-utils.sh
@@ -20,10 +29,9 @@ xunit_console_version="$(get_package_version xunitrunnerconsole)"
 dotnet_runtime_version="$(get_tool_version dotnetRuntime)"
 
 if [[ "${runtime}" == "dotnet" ]]; then
-    target_framework=netcoreapp2.1
     file_list=( "${unittest_dir}"/*/netcoreapp2.1/*.UnitTests.dll )
     xunit_console="${nuget_dir}"/xunit.runner.console/"${xunit_console_version}"/tools/netcoreapp2.0/xunit.console.dll
-elif [[ "${runtime}" == "mono" ]]; then
+elif [[ "${runtime}" =~ ^(mono|mono-debug)$ ]]; then
     file_list=(
         "${unittest_dir}/Microsoft.CodeAnalysis.CSharp.Symbol.UnitTests/net46/Microsoft.CodeAnalysis.CSharp.Symbol.UnitTests.dll"
         "${unittest_dir}/Microsoft.CodeAnalysis.CSharp.Syntax.UnitTests/net46/Microsoft.CodeAnalysis.CSharp.Syntax.UnitTests.dll"
@@ -55,41 +63,39 @@ mkdir -p "${log_dir}"
 exit_code=0
 for file_name in "${file_list[@]}"
 do
-    log_file="${log_dir}"/"$(basename "${file_name%.*}.xml")"
+    file_base_name=$(basename file_name)
+    log_file="${log_dir}/${file_base_name%.*}.xml"
     deps_json="${file_name%.*}".deps.json
     runtimeconfig_json="${file_name%.*}".runtimeconfig.json
 
-    # If the user specifies a test assembly on the command line, only run that one
-    # "${3:-}" => take second arg, empty string if unset
-    if [[ ("${3:-}" != "") && (! "${file_name}" =~ "${3:-}") ]]
+    if [[ ("${single_test_assembly}" != "") && (! "${file_name}" =~ "${single_test_assembly}") ]]
     then
         echo "Skipping ${file_name}"
         continue
     fi
 
-    echo Running "${runtime} ${file_name[@]}"
+    echo Running "${runtime} ${file_name}"
     if [[ "${runtime}" == "dotnet" ]]; then
         # Disable the VB Semantic tests while we investigate the core dump issue
         # https://github.com/dotnet/roslyn/issues/29660
-        if [[ "${file_name[@]}" == *'Microsoft.CodeAnalysis.VisualBasic.Semantic.UnitTests.dll' ]] 
+        if [[ "${file_name}" == *'Microsoft.CodeAnalysis.VisualBasic.Semantic.UnitTests.dll' ]] 
         then
-            echo "Skipping ${file_name[@]}"
+            echo "Skipping ${file_name}"
             continue
         fi
         runner="dotnet exec --fx-version ${dotnet_runtime_version} --depsfile ${deps_json} --runtimeconfig ${runtimeconfig_json}"
     elif [[ "${runtime}" == "mono" ]]; then
         runner=mono
+    elif [[ "${runtime}" == "mono-debug" ]]; then
+        runner="mono --debug"
     fi
 
     # https://github.com/dotnet/roslyn/issues/29380
-    # Pass additional arguments on to xunit_console directly.
-    # This allows you to (for example) run a single test method of a single test assembly, like so:
-    # ./build/scripts/tests.sh Debug dotnet Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests -method "*.Query_01"
-    if ${runner} "${xunit_console}" "${file_name[@]}" -xml "${log_file}" -parallel none "${@:4}"
+    if ${runner} "${xunit_console}" "${file_name}" -xml "${log_file}" -parallel none "${xunit_args[@]}"
     then
-        echo "Assembly ${file_name[@]} passed"
+        echo "Assembly ${file_name} passed"
     else
-        echo "Assembly ${file_name[@]} failed"
+        echo "Assembly ${file_name} failed"
         exit_code=1
     fi
 done
