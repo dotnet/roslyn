461e397be017bde6704b032ffe9b899e4f476649
Šimon Koníček <neme112@outlook.com>
2018-09-25 15:54:24 +0200

    Adding an explanatory comment to ProjectPropertyStorage

--- src/VisualStudio/Core/Def/Implementation/ProjectSystem/VisualStudioWorkspaceImpl.ProjectPropertyStorage.cs
+++ src/VisualStudio/Core/Def/Implementation/ProjectSystem/VisualStudioWorkspaceImpl.ProjectPropertyStorage.cs
@@ -11,6 +11,20 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
     {
         private abstract class ProjectPropertyStorage
         {
+            // For CPS projects, we prefer to use IVsBuildPropertyStorage since that allows us to easily modify the project
+            // properties independent of configuration. However this is problematic in the legacy project system, where setting
+            // the build property would indeed change its value, however that change would only be visible after the project is
+            // unloaded and reloaded. The language service would not be updated (the error we're trying to fix would still
+            // be visible) and any checkbox in the project properties dialog would not be updated. The language service issue
+            // could be worked around by using VSProject.Refresh, but that does not fix the project properties dialog. Therefore
+            // for the legacy project system, we choose to use ConfigurationManager to iterate & update each configuration,
+            // which works correctly, even though it is a little less convinient because it creates a more verbose project file
+            // (although we're dealing with a legacy project file... it's already verbose anyway).
+
+            // It's important to note that the property name may differ in these two implementations. The build property name
+            // corresponds to the name of the property in the project file (for example LangVersion), whereas the configuration
+            // property name comes from an interface such as CSharpProjectConfigurationProperties3 (for example LanguageVersion).
+
             public static ProjectPropertyStorage Create(Project project, IServiceProvider serviceProvider)
             {
                 var solution = (IVsSolution)serviceProvider.GetService(typeof(SVsSolution));
