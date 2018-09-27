587c44f5468f203cf1b13a117ddfaed4d877b3bd
Paul M Cohen <paul1956@users.noreply.github.com>
2018-09-27 12:20:46 -0700

    Added 2 additional tests that separate V15 from V15.5. V15 is the default for compiler even when running 15.5.

--- src/Compilers/VisualBasic/Test/Syntax/Parser/ParseDirectives.vb
+++ src/Compilers/VisualBasic/Test/Syntax/Parser/ParseDirectives.vb
@@ -2377,6 +2377,28 @@ End Module]]>,
     End Sub
 
     <Fact()>
+    Public Sub ParseWarningDirective_LineContinuation2V15_5()
+        Assert.Equal(37306, ERRID.ERR_CommentsAfterLineContinuationNotAvailable1)
+        Dim tree = ParseAndVerify(code:=<![CDATA[#Enable Warning _ 'Comment]]>,
+                                  options:=New VisualBasicParseOptions(LanguageVersion.VisualBasic15_5),
+                                    <errors>
+                                        <error id="37306" message="Please use language version 16 or greater to use comments after line continuation character." start="24" end="25"/>
+                                    </errors>)
+        tree.VerifyOccurrenceCount(SyntaxKind.EnableWarningDirectiveTrivia, 2)
+
+        Dim root = tree.GetRoot()
+        Dim skippedTokens = root.DescendantNodes(descendIntoTrivia:=True).OfType(Of SkippedTokensTriviaSyntax)
+        Assert.Empty(skippedTokens)
+
+        Dim enableNode = DirectCast(root.GetFirstDirective(), EnableWarningDirectiveTriviaSyntax)
+        Assert.Equal(SyntaxKind.EnableKeyword, enableNode.EnableKeyword.Kind)
+        Assert.False(enableNode.EnableKeyword.IsMissing)
+        Assert.Equal(SyntaxKind.WarningKeyword, enableNode.WarningKeyword.Kind)
+        Assert.False(enableNode.WarningKeyword.IsMissing)
+        Assert.Empty(enableNode.ErrorCodes)
+    End Sub
+
+    <Fact()>
     Public Sub ParseWarningDirective_LineContinuation2V16()
         Dim tree = ParseAndVerify((<![CDATA[#Enable Warning _  'Comment]]>), New VisualBasicParseOptions(LanguageVersion.VisualBasic16))
         tree.VerifyOccurrenceCount(SyntaxKind.EnableWarningDirectiveTrivia, 2)
@@ -2423,6 +2445,30 @@ End Module]]>,
         Assert.Equal(37306, ERRID.ERR_CommentsAfterLineContinuationNotAvailable1)
         Dim tree = ParseAndVerify(<![CDATA[#Enable Warning bc41007 _ 'Comment]]>,
                                   New VisualBasicParseOptions(LanguageVersion.VisualBasic15),
+            <errors>
+                <error id="37306" message="Please use language version 16 or greater to use comments after line continuation character." start="24" end="25"/>
+            </errors>)
+        tree.VerifyOccurrenceCount(SyntaxKind.EnableWarningDirectiveTrivia, 2)
+
+        Dim root = tree.GetRoot()
+        Dim skippedTokens = root.DescendantNodes(descendIntoTrivia:=True).OfType(Of SkippedTokensTriviaSyntax)
+        Assert.Empty(skippedTokens)
+
+        Dim enableNode = DirectCast(root.GetFirstDirective(), EnableWarningDirectiveTriviaSyntax)
+        Assert.Equal(SyntaxKind.EnableKeyword, enableNode.EnableKeyword.Kind)
+        Assert.False(enableNode.EnableKeyword.IsMissing)
+        Assert.Equal(SyntaxKind.WarningKeyword, enableNode.WarningKeyword.Kind)
+        Assert.False(enableNode.WarningKeyword.IsMissing)
+        Assert.Equal(1, enableNode.ErrorCodes.Count)
+        Assert.False(enableNode.ErrorCodes(0).IsMissing)
+        Assert.Equal(SyntaxKind.IdentifierName, enableNode.ErrorCodes(0).Kind)
+    End Sub
+
+    <Fact()>
+    Public Sub ParseWarningDirective_LineContinuation4V15_5()
+        Assert.Equal(37306, ERRID.ERR_CommentsAfterLineContinuationNotAvailable1)
+        Dim tree = ParseAndVerify(<![CDATA[#Enable Warning bc41007 _ 'Comment]]>,
+                                  New VisualBasicParseOptions(LanguageVersion.VisualBasic15_5),
             <errors>
                 <error id="37306" message="Please use language version 16 or greater to use comments after line continuation character." start="24" end="25"/>
             </errors>)
