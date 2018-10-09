c2eb9f749bd11d344f52be89c2edeb2405f4143b
Joey Robichaud <jorobich@microsoft.com>
2018-10-09 11:17:31 -0700

    Fix broken call to RemoteAddImportService

--- src/Features/Core/Portable/AddImport/AbstractAddImportFeatureService.cs
+++ src/Features/Core/Portable/AddImport/AbstractAddImportFeatureService.cs
@@ -63,6 +63,7 @@ namespace Microsoft.CodeAnalysis.AddImport
                     document.Id,
                     span,
                     diagnosticId,
+                    maxResults,
                     placeSystemNamespaceFirst,
                     searchReferenceAssemblies,
                     packageSources
