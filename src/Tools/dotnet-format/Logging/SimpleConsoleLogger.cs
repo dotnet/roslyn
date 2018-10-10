0f340a52c25aba3e0e7886d214df4e8d815e1627
Joey Robichaud <joseph.robichaud@microsoft.com>
2018-10-10 12:52:59 -0700

    Added DotNet global tool for code formatting. (#29403)
    Added DotNet global tool for code formatting.
    
    Tool can be run on a project, solution, or folder containing a
    project or solution. Tool pulls formatting configuration from an
    .editorconfig if present, otherwise formats with roslyn defaults.

---
+++ src/Tools/dotnet-format/Logging/SimpleConsoleLogger.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.CommandLine;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions.Internal;
+
+namespace Microsoft.CodeAnalysis.Tools.Logging
+{
+    internal class SimpleConsoleLogger : ILogger
+    {
+        private readonly IConsole _console;
+        private readonly LogLevel _logLevel;
+
+        private static readonly ImmutableDictionary<LogLevel, ConsoleColor> _logLevelColorMap = new Dictionary<LogLevel, ConsoleColor>
+        {
+            [LogLevel.Critical] = ConsoleColor.Red,
+            [LogLevel.Error] = ConsoleColor.Red,
+            [LogLevel.Warning] = ConsoleColor.Yellow,
+            [LogLevel.Information] = ConsoleColor.White,
+            [LogLevel.Debug] = ConsoleColor.Gray,
+            [LogLevel.Trace] = ConsoleColor.Gray,
+            [LogLevel.None] = ConsoleColor.White,
+        }.ToImmutableDictionary();
+
+        public SimpleConsoleLogger(IConsole console, LogLevel logLevel)
+        {
+            _console = console;
+            _logLevel = logLevel;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+            {
+                return;
+            }
+
+            var messageColor = _logLevelColorMap[logLevel];
+            _console.ForegroundColor = messageColor;
+
+            var message = formatter(state, exception);
+            _console.Out.WriteLine($"  {message}");
+
+            _console.ResetColor();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return (int)logLevel >= (int)_logLevel;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return NullScope.Instance;
+        }
+    }
+}
