64e974a5dba6f5144610d32a49f0f737b4cd85f7
Scott Caldwell <scott.michael.caldwell@gmail.com>
2018-10-12 17:50:13 -0400

    Add unit test

--- src/EditorFeatures/CSharpTest/MakeFieldReadonly/MakeFieldReadonlyTests.cs
+++ src/EditorFeatures/CSharpTest/MakeFieldReadonly/MakeFieldReadonlyTests.cs
@@ -1102,5 +1102,16 @@ class MyClass
     }
 }");
         }
+
+        [WorkItem(26364, "https://github.com/dotnet/roslyn/issues/26364")]
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsMakeFieldReadonly)]
+        public async Task FieldIsFixed()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"unsafe struct S
+{
+    [|private fixed byte b[8];|]
+}");
+        }
     }
 }
