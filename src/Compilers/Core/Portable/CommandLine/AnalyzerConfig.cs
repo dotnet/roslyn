4c001fe57b21aa4fbf03031dc64becc82133d99f
Andy Gocke <angocke@microsoft.com>
2018-10-29 14:46:17 -0700

    Move AnalyzerConfigOptionsResult to the top level and put in a new file

--- src/Compilers/Core/Portable/CommandLine/AnalyzerConfig.cs
+++ src/Compilers/Core/Portable/CommandLine/AnalyzerConfig.cs
@@ -87,47 +87,6 @@ namespace Microsoft.CodeAnalysis
         /// </summary>
         public bool IsRoot => GlobalSection.Properties.TryGetValue("root", out string val) && val == "true";
 
-
-        /// <summary>
-        /// Holds results from <see cref="GetAnalyzerConfigOptions{TStringList, TACList}(TStringList, TACList)"/>.
-        /// </summary>
-        public readonly struct AnalyzerConfigOptionsResult
-        {
-            /// <summary>
-            /// Options that customize diagnostic severity as reported by the compiler. If there
-            /// are no options for a given source path, that entry in the array is null.
-            /// </summary>
-            /// <remarks>
-            /// The item index corresponds to the input source path to
-            /// <see cref="GetAnalyzerConfigOptions{TStringList, TACList}(TStringList, TACList)" />
-            /// </remarks>
-            public ImmutableArray<TreeOptions> TreeOptions { get; }
-            /// <summary>
-            /// Options that do not have any special compiler behavior and are passed to analyzers as-is.
-            /// If there are no options for a given source path, that entry in the array is null.
-            /// </summary>
-            /// <remarks>
-            /// The item index corresponds to the input source path to
-            /// <see cref="GetAnalyzerConfigOptions{TStringList, TACList}(TStringList, TACList)"/>.
-            /// </remarks>
-            public ImmutableArray<AnalyzerOptions> AnalyzerOptions { get; }
-
-            /// <summary>
-            /// Any produced diagnostics while processing AnalyzerConfig files.
-            /// </summary>
-            public ImmutableArray<Diagnostic> Diagnostics { get; }
-
-            internal AnalyzerConfigOptionsResult(
-                ImmutableArray<TreeOptions> treeOptions,
-                ImmutableArray<AnalyzerOptions> analyzerOptions,
-                ImmutableArray<Diagnostic> diagnostics)
-            {
-                TreeOptions = treeOptions;
-                AnalyzerOptions = analyzerOptions;
-                Diagnostics = diagnostics;
-            }
-        }
-
         /// <summary>
         /// Takes a list of paths to source files and a list of AnalyzeConfigs and produces a
         /// resultant dictionary of diagnostic configurations for each of the source paths.
