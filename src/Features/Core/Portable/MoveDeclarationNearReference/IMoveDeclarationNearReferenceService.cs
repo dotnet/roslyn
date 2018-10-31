700e8ef75af513da3c2f41a620a34d27602299e1
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-10-31 14:20:45 -0700

    Cleanup.

--- src/Features/Core/Portable/MoveDeclarationNearReference/IMoveDeclarationNearReferenceService.cs
+++ src/Features/Core/Portable/MoveDeclarationNearReference/IMoveDeclarationNearReferenceService.cs
@@ -15,10 +15,9 @@ namespace Microsoft.CodeAnalysis.MoveDeclarationNearReference
         Task<bool> CanMoveDeclarationNearReferenceAsync(Document document, SyntaxNode localDeclarationStatement, CancellationToken cancellationToken);
 
         /// <summary>
-        /// Moves true if <paramref name="localDeclarationStatement"/> closer to its first
-        /// reference. Only applicable if <see cref="CanMoveDeclarationNearReferenceAsync"/>
-        /// returned <code>true</code>.  If not, then the original document will be returned
-        /// unchanged.
+        /// Moves <paramref name="localDeclarationStatement"/> closer to its first reference. Only
+        /// applicable if <see cref="CanMoveDeclarationNearReferenceAsync"/> returned
+        /// <code>true</code>.  If not, then the original document will be returned unchanged.
         /// </summary>
         Task<Document> MoveDeclarationNearReferenceAsync(Document document, SyntaxNode localDeclarationStatement, CancellationToken cancellationToken);
     }
