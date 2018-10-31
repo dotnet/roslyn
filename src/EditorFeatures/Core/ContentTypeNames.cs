d424caa438611731b6063478792e2db6915cbbf1
HeeJae Chang <hechang@microsoft.com>
2018-10-31 13:41:16 -0700

    added typeforward to the content type names

--- src/EditorFeatures/Core/ContentTypeNames.cs
+++ src/EditorFeatures/Core/ContentTypeNames.cs
@@ -1,16 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-namespace Microsoft.CodeAnalysis.Editor
-{
-    internal static class ContentTypeNames
-    {
-        public const string CSharpContentType = Text.ContentTypeNames.CSharpContentType;
-        public const string CSharpSignatureHelpContentType = Text.ContentTypeNames.CSharpSignatureHelpContentType;
-        public const string RoslynContentType = Text.ContentTypeNames.RoslynContentType;
-        public const string VisualBasicContentType = Text.ContentTypeNames.VisualBasicContentType;
-        public const string VisualBasicSignatureHelpContentType = Text.ContentTypeNames.VisualBasicSignatureHelpContentType;
-        public const string XamlContentType = Text.ContentTypeNames.XamlContentType;
-        public const string JavaScriptContentTypeName = Text.ContentTypeNames.JavaScriptContentTypeName;
-        public const string TypeScriptContentTypeName = Text.ContentTypeNames.TypeScriptContentTypeName;
-    }
-}
+using System.Runtime.CompilerServices;
+
+// Microsoft.CodeAnalysis.Editor.ContentTypeNames has been moved to Microsoft.CodeAnalysis.Editor.Text.dll
+[assembly: TypeForwardedTo(typeof(Microsoft.CodeAnalysis.Editor.ContentTypeNames))]
