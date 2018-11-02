c2f26cda7f07e6146f996634682e645d06d1581c
Gen Lu <gel@microsoft.com>
2018-11-01 17:32:17 -0700

    Add comments for default namespace in workspace

--- src/VisualStudio/Core/Def/Implementation/ProjectSystem/Legacy/AbstractLegacyProject.cs
+++ src/VisualStudio/Core/Def/Implementation/ProjectSystem/Legacy/AbstractLegacyProject.cs
@@ -308,6 +308,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem.L
         /// Get the default namespace of the project ("" if not defined, which means global namespace),
         /// or null if it is unknown or not applicable. 
         /// </summary>
+        /// <remarks>
+        /// This only has meaning in C# and is explicitly set to null in VB.
+        /// </remarks>>
         private static string GetDefaultNamespace(IVsHierarchy hierarchy, string language)
         {
             // While both csproj and vbproj might define <rootnamespace> property in the project file, 
