2d3bd6c27d3d1875d8e203f589f7c9bc8f641bcc
KristianJakubik <jakubikkristian@gmail.com>
2018-11-04 15:45:48 +0100

    Set up diagnostic class 2

---
+++ src/Features/CSharp/Portable/MakeStructFieldsWritable/CSharpMakeStructFieldsWritableDiagnosticAnalyzers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis.CodeQuality;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.CSharp.MakeStructFieldsWritable
+{
+    internal sealed class CSharpMakeStructFieldsWritableDiagnosticAnalyzers : AbstractCodeQualityDiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor s_diagnosticDescriptor = CreateDescriptor(
+            IDEDiagnosticIds.MakeStructFieldsWritable,
+            new LocalizableResourceString("test", FeaturesResources.ResourceManager, typeof(FeaturesResources)),
+            new LocalizableResourceString("test", FeaturesResources.ResourceManager, typeof(FeaturesResources)),
+            isUnneccessary: true);
+
+        public CSharpMakeStructFieldsWritableDiagnosticAnalyzers()
+            : base(ImmutableArray.Create(s_diagnosticDescriptor), GeneratedCodeAnalysisFlags.ReportDiagnostics)
+        {
+        }
+
+
+        public override DiagnosticAnalyzerCategory GetAnalyzerCategory()
+        {
+            return DiagnosticAnalyzerCategory.SemanticDocumentAnalysis;
+        }
+
+        public override bool OpenFileOnly(Workspace workspace) => true;
+
+        protected override void InitializeWorker(AnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
