0e998035951465bf1809d8a3bc18290011d8b4a3
Jonathon Marolf <jmarolf@microsoft.com>
2018-11-15 15:42:58 -0800

    Update test version that we will run training against

--- build/scripts/createrunsettings.ps1
+++ build/scripts/createrunsettings.ps1
@@ -19,7 +19,7 @@ try {
     $optProfToolExe = Join-Path $optProfToolDir "tools\roslyn.optprof.runsettings.generator.exe"
     $configFile = Join-Path $repoDir "build\config\optprof.json"
     $outputFolder = Join-Path $configDir "Insertion\RunSettings"
-    $optProfArgs = "--configFile $configFile --outputFolder $outputFolder --buildNumber 28302.01 "
+    $optProfArgs = "--configFile $configFile --outputFolder $outputFolder --buildNumber 28315.3001 "
     
     Exec-Console $optProfToolExe $optProfArgs
     exit 0
