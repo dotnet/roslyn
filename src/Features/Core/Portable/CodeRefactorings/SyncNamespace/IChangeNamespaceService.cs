27633bf4faa8083425f241dd519cb6f5b1542061
Gen Lu <gel@microsoft.com>
2018-11-20 12:02:10 -0800

    Initial step to separate sync namespace refactoring and change namespace service

---
+++ src/Features/Core/Portable/CodeRefactorings/SyncNamespace/IChangeNamespaceService.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.LanguageServices;
+
+namespace Microsoft.CodeAnalysis.ChangeNamespace
+{
+    internal interface IChangeNamespaceService : ILanguageService
+    {
+        Task<Solution> ChangeNamespaceAsync(Solution solution, ImmutableArray<DocumentId> documentIds, string declaredNamespace, string targetNamespace, CancellationToken cancellationToken);
+
+        bool TryGetReplacementReferenceSyntax(SyntaxNode reference, ImmutableArray<string> newNamespaceParts, ISyntaxFactsService syntaxFacts, out SyntaxNode old, out SyntaxNode @new);
+    }
+}
