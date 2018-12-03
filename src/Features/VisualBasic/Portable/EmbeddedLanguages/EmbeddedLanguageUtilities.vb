b8ee8826f9e6347df573dbb26b11d45fccd70585
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-12-03 14:45:27 -0800

    Blank line.

--- src/Features/VisualBasic/Portable/EmbeddedLanguages/EmbeddedLanguageUtilities.vb
+++ src/Features/VisualBasic/Portable/EmbeddedLanguages/EmbeddedLanguageUtilities.vb
@@ -1,6 +1,5 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-
 Namespace Microsoft.CodeAnalysis.VisualBasic.Features.EmbeddedLanguages
     Friend Module EmbeddedLanguageUtilities
         Public Function EscapeText(text As String, token As SyntaxToken) As String
