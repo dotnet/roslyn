3456d09f29760066b5778cd48025d4ed6d67dd2c
Ivan Basov <basov@hotmail.com>
2018-12-04 12:07:04 -0800

    fixing NamedParameterCompletionProvider to properly support the existing completion

--- src/Features/CSharp/Portable/Completion/CompletionProviders/NamedParameterCompletionProvider.cs
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/NamedParameterCompletionProvider.cs
@@ -257,7 +257,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
             return Task.FromResult<TextChange?>(new TextChange(
                 selectedItem.Span,
                 // Do not insert colon on <Tab> so that user can complete out a variable name that does not currently exist.
-                ch == '\t' ? selectedItem.DisplayText : selectedItem.GetEntireDisplayText()));
+                // ch == null is to support the old completion only.
+                (ch == null || ch == '\t') ? selectedItem.DisplayText : selectedItem.GetEntireDisplayText()));
         }
     }
 }
