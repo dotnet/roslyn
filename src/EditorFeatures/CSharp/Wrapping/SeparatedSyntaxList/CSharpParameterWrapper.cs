f98c463365120a1786c3c2ad0edbb5dba027813c
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-12-05 19:32:12 -0800

    Fix warning.

--- src/EditorFeatures/CSharp/Wrapping/SeparatedSyntaxList/CSharpParameterWrapper.cs
+++ src/EditorFeatures/CSharp/Wrapping/SeparatedSyntaxList/CSharpParameterWrapper.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Composition;
 using System.Linq;
-using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp.CodeGeneration;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.Utilities;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp.Editor.Wrapping.SeparatedSyntaxList
