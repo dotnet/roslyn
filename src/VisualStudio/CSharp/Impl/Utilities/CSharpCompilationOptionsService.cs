ec87d24c6a7fdc9938a4378c08a7b86e49af3b0c
Šimon Koníček <neme112@outlook.com>
2018-12-06 21:29:40 +0100

    Moving IParseOptionsService & ICompilationOptionsService to Microsoft.VisualStudio.LanguageServices

--- src/VisualStudio/CSharp/Impl/Utilities/CSharpCompilationOptionsService.cs
+++ src/VisualStudio/CSharp/Impl/Utilities/CSharpCompilationOptionsService.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Composition;
-using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.VisualStudio.LanguageServices.Utilities;
 
-namespace Microsoft.CodeAnalysis.CSharp
+namespace Microsoft.VisualStudio.LanguageServices.CSharp.Utilities
 {
     [ExportLanguageService(typeof(ICompilationOptionsService), LanguageNames.CSharp), Shared]
     internal class CSharpCompilationOptionsService : ICompilationOptionsService
--- src/VisualStudio/Core/Def/Utilities/ICompilationOptionsService.cs
+++ src/VisualStudio/Core/Def/Utilities/ICompilationOptionsService.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-namespace Microsoft.CodeAnalysis.Host
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.VisualStudio.LanguageServices.Utilities
 {
     internal interface ICompilationOptionsService : ILanguageService
     {
