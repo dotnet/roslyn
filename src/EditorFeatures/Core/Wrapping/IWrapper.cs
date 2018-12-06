8905c548358564a0d99890d0a359b96b056c147d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-12-05 19:51:33 -0800

    Update comment.

--- src/EditorFeatures/Core/Wrapping/IWrapper.cs
+++ src/EditorFeatures/Core/Wrapping/IWrapper.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 namespace Microsoft.CodeAnalysis.Editor.Wrapping
 {
     /// <summary>
-    /// Interface for types that can wrap some sort of language construct.  The main refactoring
+    /// Interface for types that can wrap some sort of language construct.
+    /// </summary>
+    /// <remarks>
+    /// The main refactoring
     /// keeps walking up nodes until it finds the first IWrapper that can handle that node.  That
     /// way the user is not inundated with lots of wrapping options for all the nodes their cursor
     /// is contained within.
-    /// </summary>
+    /// </remarks>
+    /// <seealso cref="AbstractWrappingCodeRefactoringProvider"/>
     internal interface IWrapper
     {
         /// <summary>
