7e908b01c92697bc9147d204a0ac179543da06cb
Sam Harwell <Sam.Harwell@microsoft.com>
2018-12-13 09:35:24 -0600

    Add a public API for Inheritance Margin

---
+++ src/Tools/ExternalAccess/InheritanceMargin/DependentTypeFinder.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.InheritanceMargin
+{
+    public static class DependentTypeFinder
+    {
+        public static async Task<ImmutableArray<INamedTypeSymbol>> FindImmediatelyDerivedAndImplementingTypesAsync(INamedTypeSymbol type, Solution solution, CancellationToken cancellationToken)
+        {
+            var types = await FindSymbols.DependentTypeFinder.FindImmediatelyDerivedAndImplementingTypesAsync(type, solution, cancellationToken).ConfigureAwait(false);
+            return types.SelectAsArray(symbolAndProjectId => symbolAndProjectId.Symbol);
+        }
+    }
+}
