a31c37d8b223e434b4a50d4370042fca0f04a035
Drew Noakes <git@drewnoakes.com>
2018-12-14 00:04:13 +0000

    Fix NRE in banned API analyzer when symbol null

--- src/Roslyn.Diagnostics.Analyzers/Core/SymbolIsBannedAnalyzer.cs
+++ src/Roslyn.Diagnostics.Analyzers/Core/SymbolIsBannedAnalyzer.cs
@@ -245,7 +245,7 @@ namespace Roslyn.Diagnostics.Analyzers
                 {
                     VerifyType(reportDiagnostic, typeSymbol, syntaxNode);
                 }
-                else
+                else if (symbol != null)
                 {
                     VerifySymbol(reportDiagnostic, symbol, syntaxNode);
                 }
