62535b0334d5ef51796dc4ed9a2ba88f2d296105
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2018-12-20 00:02:28 -0800

    Add test.

--- src/EditorFeatures/CSharpTest/Wrapping/CallWrappingTests.cs
+++ src/EditorFeatures/CSharpTest/Wrapping/CallWrappingTests.cs
@@ -1,9 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.Editor.CSharp.Wrapping;
@@ -110,6 +106,23 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Wrapping
 @"class C {
     void Bar() {
         the.quick.brown[1, 2, 3].fox.jumped[1][2][3];
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsWrapping)]
+        public async Task TestChunkMustHaveDottedSection()
+        {
+            await TestAllWrappingCasesAsync(
+@"class C {
+    void Bar() {
+        [||]the().quick.brown[1, 2, 3].fox.jumped[1][2][3];
+    }
+}",
+@"class C {
+    void Bar() {
+        the().quick.brown[1, 2, 3]
+             .fox.jumped[1][2][3];
     }
 }");
         }
