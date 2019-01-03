4dbf81fd3fc426ed2ccc77897e9796d59719ffd7
Andy Gocke <angocke@microsoft.com>
2019-01-02 23:35:01 -0500

    Synthesize record ctor body (#32079)
    Add code to synthesize a record constructor body.

---
+++ src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenRecords.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.CodeGen
+{
+    [CompilerTrait(CompilerFeature.Records)]
+    public sealed class CodeGenRecords : EmitMetadataTestBase
+    {
+        [Fact]
+        public void SimpleRecordWithProperties()
+        {
+            CompileAndVerify(@"
+using System;
+class Point(int x, int y);
+class C
+{
+
+    public static void Main()
+    {
+        var p = new Point(1, 3);
+        Console.WriteLine(p.x + "" "" + p.y);
+    }
+}", expectedOutput: "1 3");
+        }
+
+        [Fact]
+        public void RecordWithInitializer()
+        {
+            CompileAndVerify(@"
+using System;
+class Point(int x, int y)
+{
+    public int Z { get; } = 5;
+}
+class C
+{
+
+    public static void Main()
+    {
+        var p = new Point(1, 3);
+        Console.WriteLine(p.x + "" "" + p.y + "" "" + p.Z);
+    }
+}", expectedOutput: "1 3 5");
+        }
+    }
+}
