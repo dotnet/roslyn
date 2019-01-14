31bd038714a52022cd9028866af8048f6c5b68a3
Ivan Basov <basov@hotmail.com>
2019-01-14 11:38:05 -0800

    Completion window tooltip for constants/extension methods always show… (#32307)

--- src/EditorFeatures/Core/Implementation/Classification/ClassificationTags.cs
+++ src/EditorFeatures/Core/Implementation/Classification/ClassificationTags.cs
@@ -1,76 +1,13 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Roslyn.Utilities;
+using System;
 
 namespace Microsoft.CodeAnalysis.Classification
 {
     internal static class ClassificationTags
     {
+        [Obsolete("Use ToClassificationTypeName")]
         public static string GetClassificationTypeName(string textTag)
-        {
-            switch (textTag)
-            {
-                case TextTags.Keyword:
-                    return ClassificationTypeNames.Keyword;
-
-                case TextTags.Class:
-                    return ClassificationTypeNames.ClassName;
-
-                case TextTags.Delegate:
-                    return ClassificationTypeNames.DelegateName;
-
-                case TextTags.Enum:
-                    return ClassificationTypeNames.EnumName;
-
-                case TextTags.Interface:
-                    return ClassificationTypeNames.InterfaceName;
-
-                case TextTags.Module:
-                    return ClassificationTypeNames.ModuleName;
-
-                case TextTags.Struct:
-                    return ClassificationTypeNames.StructName;
-
-                case TextTags.TypeParameter:
-                    return ClassificationTypeNames.TypeParameterName;
-
-                case TextTags.Alias:
-                case TextTags.Assembly:
-                case TextTags.Field:
-                case TextTags.ErrorType:
-                case TextTags.Event:
-                case TextTags.Label:
-                case TextTags.Local:
-                case TextTags.Method:
-                case TextTags.Namespace:
-                case TextTags.Parameter:
-                case TextTags.Property:
-                case TextTags.RangeVariable:
-                    return ClassificationTypeNames.Identifier;
-
-                case TextTags.NumericLiteral:
-                    return ClassificationTypeNames.NumericLiteral;
-
-                case TextTags.StringLiteral:
-                    return ClassificationTypeNames.StringLiteral;
-
-                case TextTags.Space:
-                case TextTags.LineBreak:
-                    return ClassificationTypeNames.WhiteSpace;
-
-                case TextTags.Operator:
-                    return ClassificationTypeNames.Operator;
-
-                case TextTags.Punctuation:
-                    return ClassificationTypeNames.Punctuation;
-
-                case TextTags.AnonymousTypeIndicator:
-                case TextTags.Text:
-                    return ClassificationTypeNames.Text;
-
-                default:
-                    throw ExceptionUtilities.UnexpectedValue(textTag);
-            }
-        }
+            => textTag.ToClassificationTypeName();
     }
 }
