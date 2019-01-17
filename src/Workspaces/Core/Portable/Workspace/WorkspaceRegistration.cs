f85dcc0f470d0e0ebb282232304e952a745900ee
HeeJae Chang <hechang@microsoft.com>
2019-01-17 11:35:57 -0800

    updated comment.

--- src/Workspaces/Core/Portable/Workspace/WorkspaceRegistration.cs
+++ src/Workspaces/Core/Portable/Workspace/WorkspaceRegistration.cs
@@ -39,6 +39,13 @@ namespace Microsoft.CodeAnalysis
                 //
                 // we have this issue to track proper fix for preview 3 or later
                 // https://github.com/dotnet/roslyn/issues/32551
+                //
+                // issue we are working around is the fact this event can happen concurrently
+                // if RegisteryText happens and then UnregisterText happens in perfect timing, 
+                // RegisterText got slightly delayed since it is async event, and UnregisterText happens 
+                // at the same time since it is a synchronous event, and they my happens in 2 different threads, 
+                // cause this event to be raised concurrently.
+                // that can cause some listener to mess up its internal state like the issue linked above
                 WorkspaceChanged?.Invoke(this, EventArgs.Empty);
             }
         }
