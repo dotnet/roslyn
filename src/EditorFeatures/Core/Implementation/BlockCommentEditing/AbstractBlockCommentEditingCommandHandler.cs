ecefe8331bb63495737a4237b6a45ccb96a1ba72
Jason Malinowski <jason.malinowski@microsoft.com>
2019-01-18 09:42:26 -0800

    Remove AbstractBlockCommentingEditingCommandHandler's legacy implementation
    Now that TypeScript has moved to the newer editor commanding APIs,
    the legacy implementation is no longer needed.

--- src/EditorFeatures/Core/Implementation/BlockCommentEditing/AbstractBlockCommentEditingCommandHandler.cs
+++ src/EditorFeatures/Core/Implementation/BlockCommentEditing/AbstractBlockCommentEditingCommandHandler.cs
@@ -9,15 +9,7 @@ using VSEditorCommands = Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
 
 namespace Microsoft.CodeAnalysis.Editor.Implementation.BlockCommentEditing
 {
-    /// <summary>
-    /// This class implements both legacy and modern editor command handler becuase TypeScript
-    /// uses it to implement legacy Microsoft.CodeAnalysis.Editor.ICommandHandler based command.
-    /// Once TypeScript migrates to the modern editor commanding (tracked by
-    /// https://devdiv.visualstudio.com/DevDiv/_workitems/edit/548409), the part implementing
-    /// Microsoft.CodeAnalysis.Editor.ICommandHandler can be deleted.
-    /// </summary>
     internal abstract class AbstractBlockCommentEditingCommandHandler : BaseAbstractBlockCommentEditingCommandHandler,
-        ICommandHandler<ReturnKeyCommandArgs>,
         VSCommanding.ICommandHandler<VSEditorCommands.ReturnKeyCommandArgs>
     {
         protected AbstractBlockCommentEditingCommandHandler(
@@ -27,24 +19,6 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.BlockCommentEditing
         {
         }
 
-        #region Legacy ICommandHandler
-
-        public CommandState GetCommandState(ReturnKeyCommandArgs args, Func<CommandState> nextHandler) => nextHandler();
-
-        public void ExecuteCommand(ReturnKeyCommandArgs args, Action nextHandler)
-        {
-            if (TryHandleReturnKey(args.SubjectBuffer, args.TextView))
-            {
-                return;
-            }
-
-            nextHandler();
-        }
-
-        #endregion
-
-        #region Modern editor ICommandHandler
-
         public string DisplayName => EditorFeaturesResources.Block_Comment_Editing;
 
         public VSCommanding.CommandState GetCommandState(VSEditorCommands.ReturnKeyCommandArgs args) => VSCommanding.CommandState.Unspecified;
@@ -53,7 +27,5 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.BlockCommentEditing
         {
             return TryHandleReturnKey(args.SubjectBuffer, args.TextView);
         }
-
-        #endregion
     }
 }
