ccc3c0d079aa0a538300a07720e427d465b1322c
David Barbet <dabarbet@microsoft.com>
2019-01-23 17:41:18 -0800

    Remove unnecessary imports.

--- src/Features/CSharp/Portable/AddBraces/CSharpAddBracesDiagnosticAnalyzer.cs
+++ src/Features/CSharp/Portable/AddBraces/CSharpAddBracesDiagnosticAnalyzer.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Collections.Immutable;
 using System.Diagnostics;
-using System.Threading;
 using Microsoft.CodeAnalysis.CodeStyle;
 using Microsoft.CodeAnalysis.CSharp.CodeStyle;
 using Microsoft.CodeAnalysis.CSharp.Extensions;
@@ -58,7 +56,6 @@ namespace Microsoft.CodeAnalysis.CSharp.Diagnostics.AddBraces
             }
 
             var embeddedStatement = statement.GetEmbeddedStatement();
-
             switch (embeddedStatement.Kind())
             {
                 case SyntaxKind.Block:
