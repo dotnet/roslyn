2b9d5352abcc77b869dcfdc7047ad4080cff6375
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-01-30 15:11:12 -0800

    Add feature to suggest moving to simplified using statements

---
+++ src/Features/CSharp/Portable/UseSimpleUsingStatement/UseSimpleUsingStatementCodeFixProvider.cs
@@ -0,0 +1,203 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.CSharp.UseSimpleUsingDeclaration
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UseSimpleUsingStatementCodeFixProvider)), Shared]
+    internal class UseSimpleUsingStatementCodeFixProvider : SyntaxEditorBasedCodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } =
+            ImmutableArray.Create(IDEDiagnosticIds.UseSimpleUsingStatementDiagnosticId);
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            context.RegisterCodeFix(new MyCodeAction(
+                c => FixAsync(context.Document, context.Diagnostics[0], c)),
+                context.Diagnostics);
+
+            return Task.CompletedTask;
+        }
+
+        protected override Task FixAllAsync(
+            Document document, ImmutableArray<Diagnostic> diagnostics,
+            SyntaxEditor editor, CancellationToken cancellationToken)
+        {
+            var usingStatements = diagnostics.Select(d => (UsingStatementSyntax)d.AdditionalLocations[0].FindNode(cancellationToken)).ToSet();
+
+            var rewriter = new Rewriter(usingStatements);
+            var root = editor.OriginalRoot;
+            var updatedRoot = rewriter.Visit(root);
+
+            editor.ReplaceNode(root, updatedRoot);
+
+            return Task.CompletedTask;
+        }
+
+        private class Rewriter : CSharpSyntaxRewriter
+        {
+            private readonly ISet<UsingStatementSyntax> _usingStatements;
+
+            public Rewriter(ISet<UsingStatementSyntax> usingStatements)
+            {
+                _usingStatements = usingStatements;
+            }
+
+            private static SyntaxList<StatementSyntax> Expand(UsingStatementSyntax usingStatement)
+            {
+                var builder = ArrayBuilder<StatementSyntax>.GetInstance();
+                builder.Add(Convert(usingStatement));
+                if (usingStatement.Statement is BlockSyntax block)
+                {
+                    builder.AddRange(block.Statements);
+                }
+                else
+                {
+                    builder.Add(usingStatement.Statement);
+                }
+
+                var statements = new SyntaxList<StatementSyntax>(builder);
+                builder.Free();
+                return statements;
+            }
+
+            private bool ShouldExpand(StatementSyntax beforeStatement, StatementSyntax afterStatement)
+                => _usingStatements.Contains(beforeStatement) &&
+                   afterStatement is UsingStatementSyntax usingStatement &&
+                   usingStatement.Declaration != null;
+
+            private static LocalDeclarationStatementSyntax Convert(UsingStatementSyntax usingStatement)
+            {
+                return SyntaxFactory.LocalDeclarationStatement(
+                    usingStatement.AwaitKeyword,
+                    usingStatement.UsingKeyword,
+                    modifiers: default,
+                    usingStatement.Declaration,
+                    SyntaxFactory.Token(SyntaxKind.SemicolonToken));
+            }
+
+            public override SyntaxNode VisitElseClause(ElseClauseSyntax node)
+            {
+                // First, descend into the else-clause so we fixup any using statements contained
+                // inside of it.
+                var rewrittenElse = (ElseClauseSyntax)base.VisitElseClause(node);
+
+                // Now, if the else-clause previous pointed at a using statement we wanted to
+                // rewrite, and it still points at a using statement, then expand that using
+                // statemnet into the else-clause.
+                var rewrittenStatement = rewrittenElse.Statement;
+                if (ShouldExpand(node.Statement, rewrittenStatement))
+                {
+                    // Can't expand a using-statement directly inside an else-clause.
+                    // have to add a block around it to make sure scoping is preserved
+                    // properly.
+                    var expanded = Expand((UsingStatementSyntax)rewrittenStatement);
+                    return rewrittenElse.WithStatement(SyntaxFactory.Block(expanded))
+                                        .WithAdditionalAnnotations(Formatter.Annotation);
+                }
+
+                return rewrittenElse;
+            }
+
+            private bool ExpandAppropriateStatements(
+                SyntaxList<StatementSyntax> originalStatements,
+                SyntaxList<StatementSyntax> rewrittenStatements,
+                ArrayBuilder<StatementSyntax> finalStatements)
+            {
+                var changed = false;
+                if (originalStatements.Count == rewrittenStatements.Count)
+                {
+                    for (int i = 0, n = rewrittenStatements.Count; i < n; i++)
+                    {
+                        var rewrittenStatement = rewrittenStatements[i];
+                        if (ShouldExpand(originalStatements[i], rewrittenStatement))
+                        {
+                            var expanded = Expand((UsingStatementSyntax)rewrittenStatement);
+                            finalStatements.AddRange(expanded);
+                            changed = true;
+                        }
+                        else
+                        {
+                            finalStatements.Add(rewrittenStatement);
+                        }
+                    }
+                }
+
+                return changed;
+            }
+
+            public override SyntaxNode VisitBlock(BlockSyntax node)
+            {
+                var finalStatements = ArrayBuilder<StatementSyntax>.GetInstance();
+                var result = VisitBlockWorker(node, finalStatements);
+                finalStatements.Free();
+
+                return result;
+            }
+
+            private SyntaxNode VisitBlockWorker(BlockSyntax node, ArrayBuilder<StatementSyntax> finalStatements)
+            {
+                var rewrittenBlock = (BlockSyntax)base.VisitBlock(node);
+
+                var changed = ExpandAppropriateStatements(node.Statements, rewrittenBlock.Statements, finalStatements);
+
+                if (!changed)
+                {
+                    // Didn't update any children.  Just return as is.
+                    return rewrittenBlock;
+                }
+
+                return rewrittenBlock.WithStatements(new SyntaxList<StatementSyntax>(finalStatements))
+                                     .WithAdditionalAnnotations(Formatter.Annotation);
+            }
+
+            public override SyntaxNode VisitSwitchSection(SwitchSectionSyntax node)
+            {
+                var finalStatements = ArrayBuilder<StatementSyntax>.GetInstance();
+                var result = VisitSwitchSectionWorker(node, finalStatements);
+                finalStatements.Free();
+
+                return result;
+            }
+
+            private SyntaxNode VisitSwitchSectionWorker(SwitchSectionSyntax node, ArrayBuilder<StatementSyntax> finalStatements)
+            {
+                var rewrittenSwitchSection = (SwitchSectionSyntax)base.VisitSwitchSection(node);
+
+                var changed = ExpandAppropriateStatements(node.Statements, rewrittenSwitchSection.Statements, finalStatements);
+
+                if (!changed)
+                {
+                    // Didn't update any children.  Just return as is.
+                    return rewrittenSwitchSection;
+                }
+
+                return rewrittenSwitchSection.WithStatements(new SyntaxList<StatementSyntax>(finalStatements))
+                                             .WithAdditionalAnnotations(Formatter.Annotation);
+            }
+        }
+
+        private class MyCodeAction : CodeAction.DocumentChangeAction
+        {
+            public MyCodeAction(Func<CancellationToken, Task<Document>> createChangedDocument)
+                : base(FeaturesResources.Use_simple_using_statement, createChangedDocument, FeaturesResources.Use_simple_using_statement)
+            {
+            }
+        }
+    }
+}
---
+++ src/Features/CSharp/Portable/UseSimpleUsingStatement/UseSimpleUsingStatementDiagnosticAnalyzer.cs
@@ -0,0 +1,136 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.Extensions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.CSharp.UseSimpleUsingStatement
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class UseSimpleUsingStatementDiagnosticAnalyzer : AbstractBuiltInCodeStyleDiagnosticAnalyzer
+    {
+        public UseSimpleUsingStatementDiagnosticAnalyzer()
+            : base(IDEDiagnosticIds.UseSimpleUsingStatementDiagnosticId,
+                   new LocalizableResourceString(nameof(FeaturesResources.Use_simple_using_statement), FeaturesResources.ResourceManager, typeof(FeaturesResources)),
+                   new LocalizableResourceString(nameof(FeaturesResources.using_statment_can_be_simplified), FeaturesResources.ResourceManager, typeof(FeaturesResources)))
+        {
+        }
+
+        public override bool OpenFileOnly(Workspace workspace) => false;
+        public override DiagnosticAnalyzerCategory GetAnalyzerCategory()
+            => DiagnosticAnalyzerCategory.SemanticSpanAnalysis;
+
+        protected override void InitializeWorker(AnalysisContext context)
+            => context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.UsingStatement);
+
+        private void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+        {
+            var usingStatement = (UsingStatementSyntax)context.Node;
+            if (usingStatement.Declaration == null)
+            {
+                return;
+            }
+
+            var syntaxTree = context.Node.SyntaxTree;
+            //var options = (CSharpParseOptions)syntaxTree.Options;
+            //if (options.LanguageVersion < LanguageVersion.CSharp8)
+            //{
+            //    return;
+            //}
+
+            var cancellationToken = context.CancellationToken;
+            var optionSet = context.Options.GetDocumentOptionSetAsync(syntaxTree, cancellationToken).GetAwaiter().GetResult();
+            if (optionSet == null)
+            {
+                return;
+            }
+
+            var option = optionSet.GetOption(CSharpCodeStyleOptions.PreferStaticLocalFunction);
+            if (!option.Value)
+            {
+                return;
+            }
+
+            var parent = usingStatement.Parent;
+            var okParent = parent is BlockSyntax ||
+                           parent is SwitchSectionSyntax;
+            if (!okParent)
+            {
+                return;
+            }
+
+            // Has to be one of the following forms:
+            // 1. Using statement is the last statement in the parent.
+            // 2. Using statement is not the last statment in parent, but is followed by 
+            //    something that is unaffected by simplifying the using statement.  i.e.
+            //    `return`/`break`/`continue`.  *Note*.  `return expr` would *not* be ok.
+            //    In that case, `expr` would now be evaluated *before* the using disposed
+            //    the resource, instead of afterwards.  Effectly, the statement following
+            //    cannot actually execute any code that might depend on the .Dispose method
+            //    being called or not.
+            var statements = GetStatements(parent, usingStatement);
+            if (CanConvertUsingStatement(statements, usingStatement))
+            {
+                context.ReportDiagnostic(DiagnosticHelper.Create(
+                    Descriptor,
+                    usingStatement.UsingKeyword.GetLocation(),
+                    option.Notification.Severity,
+                    additionalLocations: ImmutableArray.Create(usingStatement.GetLocation()),
+                    properties: null));
+            }
+        }
+
+        private bool CanConvertUsingStatement(
+            SyntaxList<StatementSyntax> statements,
+            UsingStatementSyntax usingStatement)
+        {
+            var index = statements.IndexOf(usingStatement);
+            if (index == statements.Count - 1)
+            {
+                // very last statement in the block.  Can be converted.
+                return true;
+            }
+
+            // Not the last statement, get the next statement and examine that.
+            var nextStatement = statements[index + 1];
+            if (nextStatement is BreakStatementSyntax ||
+                nextStatement is ContinueStatementSyntax)
+            {
+                // using statemnet followed by break/continue.  Can conver this as executing 
+                // the break/continue will cause the code to exit the using scope, causing 
+                // Dispose to be called at the same place as before.
+                return true;
+            }
+
+            if (nextStatement is ReturnStatementSyntax returnStatement &&
+                returnStatement.Expression == null)
+            {
+                // using statemnet followed by `return`.  Can conver this as executing 
+                // the `return` will cause the code to exit the using scope, causing 
+                // Dispose to be called at the same place as before.
+                //
+                // Note: the expr has to be null.  If it was non-null, then the expr would
+                // now execute before hte using called 'Dispose' instead of after, potentially
+                // changing semantics.
+                return true;
+            }
+
+            // Add any additional cases here in the future.
+            return false;
+        }
+
+        private SyntaxList<StatementSyntax> GetStatements(SyntaxNode parent, UsingStatementSyntax usingStatement)
+        {
+            switch (parent)
+            {
+                case BlockSyntax block: return block.Statements;
+                case SwitchSectionSyntax switchSection: return switchSection.Statements;
+                default: throw ExceptionUtilities.UnexpectedValue(parent);
+            }
+        }
+    }
+}
