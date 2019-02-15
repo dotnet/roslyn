b26e16d479024fa2e9382ed6bbe5ddf2d32ec2df
Jason Malinowski <jason.malinowski@microsoft.com>
2019-02-14 16:49:08 -0800

    Don't accidentally share a named semaphore between test processes
    WpfSharedTestData had two static members: Instance and
    TestSerializationGateName. TestSerializationGateName is the name of a
    system-wide Semaphore that was used to execute WpfFacts one at time.
    The intent is this was a GUID so it wouldn't actually be shared.
    Unfortunately, the Instance member is initialized first; and the
    non-static Semaphore field is initialized with the still uninitialized
    TestSerializationGateName. This means our Semaphore would always be
    named with a GUID of all-zeros, causing us to share the semaphore
    between all running xUnit processes. This was terribly bad for
    two reasons:
    
    1. It kills test performance. We run tests in parallel in separate
       processes to ensure isolation, but this sharing of the semaphore
       meant that all of those processes are only running one test at a
       time, defeating all running of tests in parallel.
    2. If one test process crashes, the Semaphore is never freed, meaning
       all your other test processes will deadlock and never complete.
    
    It's unclear why this code is using a Semaphore, but since we have
    no need to share the Semaphore between processes, we can use a
    SemaphoreSlim. This also has the added benefit of properly supporting
    a WaitAsync that isn't implemented by launching off a Thread just to
    wait for the system semaphore.

--- src/EditorFeatures/TestUtilities/Threading/WpfTestSharedData.cs
+++ src/EditorFeatures/TestUtilities/Threading/WpfTestSharedData.cs
@@ -14,20 +14,12 @@ namespace Roslyn.Test.Utilities
         internal static readonly WpfTestSharedData Instance = new WpfTestSharedData();
 
         /// <summary>
-        /// The name of a <see cref="Semaphore"/> used to ensure that only a single
-        /// <see cref="WpfFactAttribute"/>-attributed test runs at once. This requirement must be made because,
-        /// currently, <see cref="WpfTestCase"/>'s logic sets various static state before a method runs. If two tests
-        /// run interleaved on the same scheduler (i.e. if one yields with an await) then all bets are off.
-        /// </summary>
-        internal static readonly Guid TestSerializationGateName = Guid.NewGuid();
-
-        /// <summary>
         /// Holds the last 10 test cases executed: more recent test cases will occur later in the 
         /// list. Useful for debugging deadlocks that occur because state leak between runs. 
         /// </summary>
         private readonly List<string> _recentTestCases = new List<string>();
 
-        public Semaphore TestSerializationGate = new Semaphore(1, 1, TestSerializationGateName.ToString("N"));
+        public readonly SemaphoreSlim TestSerializationGate = new SemaphoreSlim(1, 1);
 
         private WpfTestSharedData()
         {
