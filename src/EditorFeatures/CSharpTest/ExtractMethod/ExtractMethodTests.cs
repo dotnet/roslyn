9538ef403ec2ee3131e43e874db8dbb499bacf6d
Thijs Brobbel <thijsbrobbel@gmail.com>
2019-02-17 21:50:06 +0100

    Add (failing) unit test for issue #33242:
    InvalidCastException on extract method in expression bodied constructor.

--- src/EditorFeatures/CSharpTest/ExtractMethod/ExtractMethodTests.cs
+++ src/EditorFeatures/CSharpTest/ExtractMethod/ExtractMethodTests.cs
@@ -11083,5 +11083,31 @@ interface Program
 }";
             await TestExtractMethodAsync(code, expected);
         }
+
+        [WorkItem(33242, "https://github.com/dotnet/roslyn/issues/33242")]
+        [Fact, Trait(Traits.Feature, Traits.Features.ExtractMethod)]
+        public async Task ExtractMethodInExpressionBodiedConstructors()
+        {
+            var code = @"
+class Foo
+{
+    private readonly string _bar;
+
+    private Foo(string bar) => _bar = [|bar|];
+}";
+            var expected = @"
+class Foo
+{
+    private readonly string _bar;
+
+    private Foo(string bar) => _bar = GetBar(bar);
+
+    private static string GetBar(string bar)
+    {
+        return bar;
+    }
+}";
+            await TestExtractMethodAsync(code, expected);
+        }
     }
 }
