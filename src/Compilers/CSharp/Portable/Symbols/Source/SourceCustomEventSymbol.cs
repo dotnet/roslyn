1d51d61b825d70097e0ae2ffa22e2b6a6a7194d9
AlekseyTs <AlekseyTs@users.noreply.github.com>
2019-02-19 10:01:57 -0800

    Add PROTOTYPE comments to follow up on a previous change. (#33440)
    This is a follow up on https://github.com/dotnet/roslyn/pull/33281.

--- src/Compilers/CSharp/Portable/Symbols/Source/SourceCustomEventSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourceCustomEventSymbol.cs
@@ -172,6 +172,12 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 if (!_explicitInterfaceImplementations.IsDefaultOrEmpty && ContainingType.IsInterface)
                 {
                     Debug.Assert(_explicitInterfaceImplementations.Length == 1);
+                    // PROTOTYPE(DefaultInterfaceImplementation): This is better than 'private' accessibility, but might still be not where
+                    //                                            we want to be for implementations of internal members, because those can be 
+                    //                                            inaccessible for base-access from a different assembly that otherwise has 
+                    //                                            access to the original implemented member. That scenario has the internals
+                    //                                            being exposed to other assemblies by using InternalsVisibleTo attribute. 
+                    //                                            See DefaultInterfaceImplementationTests.ExplicitBase_152 unit-test, for example.
                     return _explicitInterfaceImplementations[0].DeclaredAccessibility;
                 }
 
