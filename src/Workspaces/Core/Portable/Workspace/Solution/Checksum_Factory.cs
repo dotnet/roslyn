ecdcd95b43668bc8a7b64fbe9bbbdb573860796c
Heejae Chang <heejaechang@outlook.com>
2019-02-20 00:17:21 -0800

    pool incrementalHash so that we don't create one unnecessarily (#33496)
    * pool incrementalHash so that we don't create one unnecessarily
    
    * renamed to s_incrementalHashPool

--- src/Workspaces/Core/Portable/Workspace/Solution/Checksum_Factory.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/Checksum_Factory.cs
@@ -5,6 +5,7 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Security.Cryptography;
 using System.Threading;
+using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Serialization;
 using Roslyn.Utilities;
 
@@ -14,41 +15,44 @@ namespace Microsoft.CodeAnalysis
     // all these are just helper methods
     internal partial class Checksum
     {
+        private static readonly ObjectPool<IncrementalHash> s_incrementalHashPool =
+            new ObjectPool<IncrementalHash>(() => IncrementalHash.CreateHash(HashAlgorithmName.SHA1), size: 20);
+
         public static Checksum Create(Stream stream)
         {
-            using (var hash = IncrementalHash.CreateHash(HashAlgorithmName.SHA1))
+            using (var pooledHash = s_incrementalHashPool.GetPooledObject())
+            using (var pooledBuffer = SharedPools.ByteArray.GetPooledObject())
             {
-                using (var pooledBuffer = SharedPools.ByteArray.GetPooledObject())
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var hash = pooledHash.Object;
+
+                var buffer = pooledBuffer.Object;
+                var bufferLength = buffer.Length;
+                int bytesRead;
+                do
                 {
-                    stream.Seek(0, SeekOrigin.Begin);
-
-                    var buffer = pooledBuffer.Object;
-                    var bufferLength = buffer.Length;
-                    int bytesRead;
-                    do
+                    bytesRead = stream.Read(buffer, 0, bufferLength);
+                    if (bytesRead > 0)
                     {
-                        bytesRead = stream.Read(buffer, 0, bufferLength);
-                        if (bytesRead > 0)
-                        {
-                            hash.AppendData(buffer, 0, bytesRead);
-                        }
+                        hash.AppendData(buffer, 0, bytesRead);
                     }
-                    while (bytesRead > 0);
-
-                    var bytes = hash.GetHashAndReset();
-
-                    // if bytes array is bigger than certain size, checksum
-                    // will truncate it to predetermined size. for more detail,
-                    // see the Checksum type
-                    //
-                    // the truncation can happen since different hash algorithm or 
-                    // same algorithm on different platform can have different hash size
-                    // which might be bigger than the Checksum HashSize.
-                    //
-                    // hash algorithm used here should remain functionally correct even
-                    // after the truncation
-                    return Checksum.From(bytes);
                 }
+                while (bytesRead > 0);
+
+                var bytes = hash.GetHashAndReset();
+
+                // if bytes array is bigger than certain size, checksum
+                // will truncate it to predetermined size. for more detail,
+                // see the Checksum type
+                //
+                // the truncation can happen since different hash algorithm or 
+                // same algorithm on different platform can have different hash size
+                // which might be bigger than the Checksum HashSize.
+                //
+                // hash algorithm used here should remain functionally correct even
+                // after the truncation
+                return Checksum.From(bytes);
             }
         }
 
