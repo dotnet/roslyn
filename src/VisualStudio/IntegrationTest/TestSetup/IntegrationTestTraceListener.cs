6be90e3d9a85327d48d29e04096c7aa2c53ef053
Sam Harwell <Sam.Harwell@microsoft.com>
2019-02-22 11:24:58 -0600

    Only FailFast on failures, not all debug messages

--- src/VisualStudio/IntegrationTest/TestSetup/IntegrationTestTraceListener.cs
+++ src/VisualStudio/IntegrationTest/TestSetup/IntegrationTestTraceListener.cs
@@ -6,16 +6,84 @@ using Microsoft.CodeAnalysis.ErrorReporting;
 
 namespace Microsoft.VisualStudio.IntegrationTest.Setup
 {
+    using Debugger = System.Diagnostics.Debugger;
+
     internal class IntegrationTestTraceListener : TraceListener
     {
+        public override void Fail(string message, string detailMessage)
+        {
+            if (!string.IsNullOrEmpty(detailMessage))
+            {
+                Exit(message + " " + detailMessage);
+            }
+            else
+            {
+                Exit(message);
+            }
+        }
+
+        public override void Write(object o)
+        {
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, null, o?.ToString());
+            }
+        }
+
+        public override void Write(object o, string category)
+        {
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, category, o?.ToString());
+            }
+        }
+
         public override void Write(string message)
         {
-            Exit(message);
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, null, message);
+            }
+        }
+
+        public override void Write(string message, string category)
+        {
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, category, message);
+            }
+        }
+
+        public override void WriteLine(object o)
+        {
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, null, o?.ToString() + Environment.NewLine);
+            }
+        }
+
+        public override void WriteLine(object o, string category)
+        {
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, category, o?.ToString() + Environment.NewLine);
+            }
         }
 
         public override void WriteLine(string message)
         {
-            Exit(message);
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, null, message + Environment.NewLine);
+            }
+        }
+
+        public override void WriteLine(string message, string category)
+        {
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, category, message + Environment.NewLine);
+            }
         }
 
         private static void Exit(string message)
