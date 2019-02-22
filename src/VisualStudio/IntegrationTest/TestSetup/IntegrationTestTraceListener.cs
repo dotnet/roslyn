cb124f6b11ed75de05c68eb3a299ab3641fe2d23
Sam Harwell <Sam.Harwell@microsoft.com>
2019-02-22 09:08:29 -0600

    Install a TraceListener for integration testing
    Fixes #33599

---
+++ src/VisualStudio/IntegrationTest/TestSetup/IntegrationTestTraceListener.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis.ErrorReporting;
+
+namespace Microsoft.VisualStudio.IntegrationTest.Setup
+{
+    internal class IntegrationTestTraceListener : TraceListener
+    {
+        public override void Write(string message)
+        {
+            Exit(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Exit(message);
+        }
+
+        private static void Exit(string message)
+        {
+            FatalError.Report(new Exception(message));
+        }
+
+        internal static void Install()
+        {
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new IntegrationTestTraceListener());
+        }
+    }
+}
