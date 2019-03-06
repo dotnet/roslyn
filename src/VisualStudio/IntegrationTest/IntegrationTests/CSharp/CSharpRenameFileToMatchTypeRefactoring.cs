812332ca102b5381abeb34b905d80aa4380dc4b6
David Poeschl <dpoeschl@gmail.com>
2019-03-06 14:17:10 -0800

    Initial Rename File To Match Type Name integration tests

---
+++ src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpRenameFileToMatchTypeRefactoring.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Microsoft.VisualStudio.IntegrationTest.Utilities;
+using Roslyn.Test.Utilities;
+using Xunit;
+using ProjectUtils = Microsoft.VisualStudio.IntegrationTest.Utilities.Common.ProjectUtils;
+
+namespace Roslyn.VisualStudio.IntegrationTests.CSharp
+{
+    [Collection(nameof(SharedIntegrationHostFixture))]
+    public class CSharpRenameFileToMatchTypeRefactoring : AbstractEditorTest
+    {
+        protected override string LanguageName => LanguageNames.CSharp;
+
+        public CSharpRenameFileToMatchTypeRefactoring(VisualStudioInstanceFactory instanceFactory)
+            : base(instanceFactory, nameof(CSharpGenerateFromUsage))
+        {
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)]
+        public void RenameFileToMatchType_ExistingCode()
+        {
+            var project = new ProjectUtils.Project(ProjectName);
+
+            SetUpEditor(@"class $$MismatchedClassName { }");
+
+            VisualStudio.Editor.InvokeCodeActionList();
+            VisualStudio.Editor.Verify.CodeAction("Rename file to MismatchedClassName.cs", applyFix: true);
+
+            // Ensure the file is still open in the editor, and that the file name change was made & saved
+            VisualStudio.Editor.Verify.TextContains("class MismatchedClassName { }");
+            VisualStudio.SolutionExplorer.Verify.FileContents(project, "MismatchedClassName.cs", @"class MismatchedClassName { }");
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.CodeActionsMoveType)]
+        public void RenameFileToMatchType_UndoStackPreserved()
+        {
+            var project = new ProjectUtils.Project(ProjectName);
+
+            SetUpEditor(@"$$class MismatchedClassName { }");
+            VisualStudio.Editor.SendKeys("public ");
+
+            VisualStudio.Editor.InvokeCodeActionList();
+            VisualStudio.Editor.Verify.CodeAction("Rename file to MismatchedClassName.cs", applyFix: true);
+
+            // Ensure the file is still open in the editor, and that the file name change was made & saved
+            VisualStudio.Editor.Verify.CurrentLineText("public class MismatchedClassName { }");
+            VisualStudio.SolutionExplorer.Verify.FileContents(project, "MismatchedClassName.cs", @"public class MismatchedClassName { }");
+
+            // Undo just undoes text changes. The file rename is not undone, and the changes are not saved after the undo
+            VisualStudio.Editor.Undo();
+            VisualStudio.Editor.Verify.CurrentLineText("class MismatchedClassName { }");
+            VisualStudio.SolutionExplorer.Verify.FileContents(project, "MismatchedClassName.cs", @"public class MismatchedClassName { }");
+        }
+    }
+}
