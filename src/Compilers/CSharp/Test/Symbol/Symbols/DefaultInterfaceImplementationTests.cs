8ad79fe2f58b9bbafea1a4bb02075f02fa41df0c
AlekseyTs <alekseyt@microsoft.com>
2019-03-07 16:12:29 -0800

    Disable crashing test on Mono

--- src/Compilers/CSharp/Test/Symbol/Symbols/DefaultInterfaceImplementationTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/DefaultInterfaceImplementationTests.cs
@@ -31083,7 +31083,7 @@ class Test1 : I2, I3
                 );
         }
 
-        [Fact]
+        [ConditionalFact(typeof(ClrOnly), Reason = ConditionalSkipReason.MonoDefaultInterfaceMethods)]
         [WorkItem(20084, "https://github.com/dotnet/roslyn/issues/20084")]
         public void EventImplementationInDerived_14()
         {
