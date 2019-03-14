5e1b2097daa0270c4c5277f18c83bc8b2d254116
TIHan <lol.tihan@gmail.com>
2019-03-14 16:19:18 -0700

    Added DiagnosticAnalyzerService

---
+++ src/Tools/ExternalAccess/FSharp/Diagnostics/FSharpDiagnosticAnalyzerService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Diagnostics
+{
+    [Export(typeof(IDiagnosticAnalyzerService))]
+    [Shared]
+    internal sealed class FSharpDiagnosticAnalyzerService : IDiagnosticAnalyzerService
+    {
+        private readonly Microsoft.CodeAnalysis.Diagnostics.IDiagnosticAnalyzerService _delegatee;
+
+        [ImportingConstructor]
+        public FSharpDiagnosticAnalyzerService(Microsoft.CodeAnalysis.Diagnostics.IDiagnosticAnalyzerService delegatee)
+        {
+            _delegatee = delegatee;
+        }
+
+        public void Reanalyze(Workspace workspace, IEnumerable<ProjectId> projectIds = null, IEnumerable<DocumentId> documentIds = null, bool highPriority = false)
+        {
+            _delegatee.Reanalyze(workspace, projectIds, documentIds, highPriority);
+        }
+    }
+}
---
+++ src/Tools/ExternalAccess/FSharp/Diagnostics/IDiagnosticAnalyzerService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Diagnostics
+{
+    public interface IDiagnosticAnalyzerService
+    {
+        /// <summary>
+        /// re-analyze given projects and documents
+        /// </summary>
+        void Reanalyze(Workspace workspace, IEnumerable<ProjectId> projectIds = null, IEnumerable<DocumentId> documentIds = null, bool highPriority = false);
+    }
+}
