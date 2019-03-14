f8840144355cd6380aeec65ce7a360b074aba038
Henrik Walker Moe <henrik.walker.moe@bekk.no>
2019-03-14 18:13:56 +0100

    Adds failing tests with mising using statements

--- src/VisualStudio/Core/Test/Options/BasicEditorConfigGeneratorTests.vb
+++ src/VisualStudio/Core/Test/Options/BasicEditorConfigGeneratorTests.vb
@@ -35,6 +35,10 @@ insert_final_newline = false
 
 #### .NET Coding Conventions ####
 
+# Organize usings
+dotnet_separate_import_directive_groups = true:silent
+dotnet_sort_system_directives_first = true:silent
+
 # this. and Me. preferences
 dotnet_style_qualification_for_event = false:silent
 dotnet_style_qualification_for_field = false:silent
