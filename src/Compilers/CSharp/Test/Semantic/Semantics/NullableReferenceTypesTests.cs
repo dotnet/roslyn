d88ed0344ec69483378d8132ae7fa74302a5a85b
Julien Couvreur <julien.couvreur@gmail.com>
2019-03-13 18:32:05 -0700

    Mitigation for GetTypeInfo on suppressed expression

--- src/Compilers/CSharp/Test/Semantic/Semantics/NullableReferenceTypesTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/NullableReferenceTypesTests.cs
@@ -1018,6 +1018,47 @@ public class C
             //Assert.Null(model.GetConstantValue(suppressions[1]).Value);
         }
 
+        [Fact]
+        public void SuppressNullableWarning_GetTypeInfo()
+        {
+            var source =
+@"class C<T>
+{
+    void M(string s, string? s2, C<string> c, C<string?> c2)
+    {
+        _ = s!;
+        _ = s2!;
+        _ = c!;
+        _ = c2!;
+    }
+}";
+            var comp = CreateCompilation(source, options: WithNonNullTypesTrue());
+            comp.VerifyDiagnostics();
+
+            var tree = comp.SyntaxTrees.Single();
+            var model = comp.GetSemanticModel(tree);
+            var suppressions = tree.GetRoot().DescendantNodes().OfType<PostfixUnaryExpressionSyntax>().ToArray();
+
+            // Need to verify the semantic model
+            // Tracked by https://github.com/dotnet/roslyn/issues/32661
+
+            var s = model.GetTypeInfo(suppressions[0]);
+            Assert.Equal("System.String", s.Type.ToTestDisplayString());
+            Assert.Equal("System.String", s.ConvertedType.ToTestDisplayString());
+
+            var s2 = model.GetTypeInfo(suppressions[1]);
+            Assert.Equal("System.String", s2.Type.ToTestDisplayString());
+            Assert.Equal("System.String", s2.ConvertedType.ToTestDisplayString());
+
+            var c = model.GetTypeInfo(suppressions[2]);
+            Assert.Equal("C<System.String>", c.Type.ToTestDisplayString());
+            Assert.Equal("C<System.String>", c.ConvertedType.ToTestDisplayString());
+
+            var c2 = model.GetTypeInfo(suppressions[3]);
+            Assert.Equal("C<System.String?>", c2.Type.ToTestDisplayString());
+            Assert.Equal("C<System.String?>", c2.ConvertedType.ToTestDisplayString());
+        }
+
         [Fact, WorkItem(31370, "https://github.com/dotnet/roslyn/issues/31370")]
         public void SuppressNullableWarning_MethodGroupInNameof()
         {
