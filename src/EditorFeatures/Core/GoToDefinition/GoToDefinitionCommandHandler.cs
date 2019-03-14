53143c5847ea2d57e36d1997ba7181cc037a16ca
David Barbet <dabarbet@microsoft.com>
2019-03-14 14:25:19 -0700

    Move Go to implementation to new fully loaded document API.

--- src/EditorFeatures/Core/GoToDefinition/GoToDefinitionCommandHandler.cs
+++ src/EditorFeatures/Core/GoToDefinition/GoToDefinitionCommandHandler.cs
@@ -23,12 +23,6 @@ namespace Microsoft.CodeAnalysis.Editor.GoToDefinition
     {
         public string DisplayName => EditorFeaturesResources.Go_to_Definition;
 
-        private (Document, IGoToDefinitionService) GetDocumentAndService(ITextSnapshot snapshot)
-        {
-            var document = snapshot.GetOpenDocumentInCurrentContextWithChanges();
-            return (document, document?.GetLanguageService<IGoToDefinitionService>());
-        }
-
         public VSCommanding.CommandState GetCommandState(GoToDefinitionCommandArgs args)
         {
             var document = args.SubjectBuffer.CurrentSnapshot.GetOpenDocumentInCurrentContextWithChanges();
