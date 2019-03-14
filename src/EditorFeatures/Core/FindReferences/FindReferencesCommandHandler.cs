08146c2f2744ff3322a75b163911495f60a26b1b
David Barbet <dabarbet@microsoft.com>
2019-03-14 15:49:18 -0700

    Move Find references implementation to new fully loaded document API.

--- src/EditorFeatures/Core/FindReferences/FindReferencesCommandHandler.cs
+++ src/EditorFeatures/Core/FindReferences/FindReferencesCommandHandler.cs
@@ -62,18 +62,22 @@ namespace Microsoft.CodeAnalysis.Editor.FindReferences
             var snapshotSpans = args.TextView.Selection.GetSnapshotSpansOnBuffer(args.SubjectBuffer);
             if (snapshotSpans.Count == 1)
             {
-                var selectedSpan = snapshotSpans[0];
-                var snapshot = args.SubjectBuffer.CurrentSnapshot;
-                var document = snapshot.GetOpenDocumentInCurrentContextWithChanges();
-                if (document != null)
+                using (context.OperationContext.AddScope(allowCancellation: true, EditorFeaturesResources.Find_References))
                 {
-                    // Do a find-refs at the *start* of the selection.  That way if the
-                    // user has selected a symbol that has another symbol touching it
-                    // on the right (i.e.  Goo++  ), then we'll do the find-refs on the
-                    // symbol selected, not the symbol following.
-                    if (TryExecuteCommand(selectedSpan.Start, document, context))
+                    var selectedSpan = snapshotSpans[0];
+                    var snapshot = args.SubjectBuffer.CurrentSnapshot;
+                    var document = snapshot.GetFullyLoadedOpenDocumentInCurrentContextWithChangesAsync(
+                        context.OperationContext).WaitAndGetResult(context.OperationContext.UserCancellationToken);
+                    if (document != null)
                     {
-                        return true;
+                        // Do a find-refs at the *start* of the selection.  That way if the
+                        // user has selected a symbol that has another symbol touching it
+                        // on the right (i.e.  Goo++  ), then we'll do the find-refs on the
+                        // symbol selected, not the symbol following.
+                        if (TryExecuteCommand(selectedSpan.Start, document, context))
+                        {
+                            return true;
+                        }
                     }
                 }
             }
