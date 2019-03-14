f52655e8c95b0bb8934e8279508bef75166d6cae
TIHan <lol.tihan@gmail.com>
2019-03-14 10:59:21 -0700

    Initial shim work

---
+++ src/Tools/ExternalAccess/FSharp/ClassificationTags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Classification
+{
+    public static class ClassificationTags
+    {
+        public static string GetClassificationTypeName(string textTag) => textTag.ToClassificationTypeName();
+    }
+}
