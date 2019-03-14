c408bea13d86b9990acb7d511215d94b90d90e1f
TIHan <lol.tihan@gmail.com>
2019-03-14 15:49:29 -0700

    Added contenttypelangservice

---
+++ src/Tools/ExternalAccess/FSharp/Editor/ContentTypeLanguageService.cs
@@ -0,0 +1,23 @@
+﻿using System.Composition;
+using Microsoft.CodeAnalysis.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Editor
+{
+    [ExportContentTypeLanguageService(LanguageNames.FSharp, LanguageNames.FSharp), Shared]
+    internal class FSharpContentTypeLanguageService : IContentTypeLanguageService
+    {
+        private readonly IContentTypeRegistryService _contentTypeRegistry;
+
+        [ImportingConstructor]
+        public FSharpContentTypeLanguageService(IContentTypeRegistryService contentTypeRegistry)
+        {
+            _contentTypeRegistry = contentTypeRegistry;
+        }
+
+        public IContentType GetDefaultContentType()
+        {
+            return _contentTypeRegistry.GetContentType(LanguageNames.FSharp);
+        }
+    }
+}
---
+++ src/Tools/ExternalAccess/FSharp/Editor/IContentTypeLanguageService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis.Host;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Editor
+{
+    /// <summary>
+    /// Service to provide the default content type for a language.
+    /// </summary>
+    internal interface IContentTypeLanguageService : ILanguageService
+    {
+        IContentType GetDefaultContentType();
+    }
+}
