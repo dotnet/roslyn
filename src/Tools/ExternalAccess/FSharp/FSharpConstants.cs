8da058c3f3da1b0a70c35372e8bdd3e4fd59fc5a
TIHan <lol.tihan@gmail.com>
2019-03-15 15:44:04 -0700

    Added IVTs back just for LanguageServices

---
+++ src/Tools/ExternalAccess/FSharp/FSharpConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp
+{
+    internal static class FSharpConstants
+    {
+        public const string ContentTypeName = "F#";
+        public const string LanguageServiceGuidString = "BC6DD5A5-D4D6-4dab-A00D-A51242DBAF1B";
+    }
+}
