90f092cb6ad946e7b365a1a633f87a490d401730
Ivan Basov <basov@hotmail.com>
2019-03-14 18:14:20 -0700

    Completion missing members of lambda parameter in fault tolerance case

--- src/Workspaces/CSharp/Portable/Recommendations/CSharpRecommendationService.cs
+++ src/Workspaces/CSharp/Portable/Recommendations/CSharpRecommendationService.cs
@@ -1,10 +1,15 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Extensions.ContextQuery;
 using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Recommendations;
 
 namespace Microsoft.CodeAnalysis.CSharp.Recommendations
