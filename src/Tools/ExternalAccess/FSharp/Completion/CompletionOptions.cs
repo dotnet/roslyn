434faa2778801e9a19051adbe55149cdec380753
TIHan <lol.tihan@gmail.com>
2019-03-15 12:41:16 -0700

    More apis

---
+++ src/Tools/ExternalAccess/FSharp/Completion/CompletionOptions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.CodeAnalysis.Options;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Completion
+{
+    public static class CompletionOptions
+    {
+        public static readonly PerLanguageOption<bool> BlockForCompletionItems = Microsoft.CodeAnalysis.Completion.CompletionOptions.BlockForCompletionItems
+    }
+}
