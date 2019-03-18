e42802a8164c2e850b946ae265ec700430add9d7
Andrew Hall (METAL) <andrha@microsoft.com>
2019-03-18 16:08:57 -0700

    Remove unused usings

--- src/Features/Core/Portable/MoveToNamespace/MoveToNamespaceAnalysisResult.cs
+++ src/Features/Core/Portable/MoveToNamespace/MoveToNamespaceAnalysisResult.cs
@@ -1,10 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Text;
-
 namespace Microsoft.CodeAnalysis.MoveToNamespace
 {
     internal partial class MoveToNamespaceAnalysisResult
