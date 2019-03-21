154b47500660e2c5b3bcb793b40d7ce8da411e78
Sam Harwell <Sam.Harwell@microsoft.com>
2019-03-21 08:06:54 -0500

    Fix VSTHRD003 in SymbolInlineRenameInfo

--- src/EditorFeatures/Core.Wpf/InlineRename/AbstractEditorInlineRenameService.cs
+++ src/EditorFeatures/Core.Wpf/InlineRename/AbstractEditorInlineRenameService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.CodeAnalysis.FindSymbols;
 using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.Navigation;
@@ -19,10 +20,12 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.InlineRename
 {
     internal abstract partial class AbstractEditorInlineRenameService : IEditorInlineRenameService
     {
+        private readonly IThreadingContext _threadingContext;
         private readonly IEnumerable<IRefactorNotifyService> _refactorNotifyServices;
 
-        protected AbstractEditorInlineRenameService(IEnumerable<IRefactorNotifyService> refactorNotifyServices)
+        protected AbstractEditorInlineRenameService(IThreadingContext threadingContext, IEnumerable<IRefactorNotifyService> refactorNotifyServices)
         {
+            _threadingContext = threadingContext;
             _refactorNotifyServices = refactorNotifyServices;
         }
 
@@ -48,10 +51,11 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.InlineRename
                 return new FailureInlineRenameInfo(EditorFeaturesResources.You_must_rename_an_identifier);
             }
 
-            return GetRenameInfo(_refactorNotifyServices, document, triggerToken, cancellationToken);
+            return GetRenameInfo(_threadingContext, _refactorNotifyServices, document, triggerToken, cancellationToken);
         }
 
         internal static IInlineRenameInfo GetRenameInfo(
+            IThreadingContext threadingContext,
             IEnumerable<IRefactorNotifyService> refactorNotifyServices,
             Document document, SyntaxToken triggerToken, CancellationToken cancellationToken)
         {
@@ -217,6 +221,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.InlineRename
             }
 
             return new SymbolInlineRenameInfo(
+                threadingContext,
                 refactorNotifyServices, document, triggerToken.Span,
                 symbolAndProjectId, forceRenameOverloads, cancellationToken);
         }
