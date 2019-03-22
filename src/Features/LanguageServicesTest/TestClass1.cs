7243951b4fa10a0f6e862f510367a81b7b22dcf2
David Barbet <dabarbet@microsoft.com>
2019-03-22 15:07:12 -0700

    Add projects for LSP implementation.

---
+++ src/Features/LanguageServicesTest/TestClass1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.Protocol.LanguageServices;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace LanguageServicesTest
+{
+    public class TestClass1
+    {
+        [WpfFact, Trait(Traits.Feature, "TestFeature")]
+        public void Class1Test()
+        {
+            var class1 = new Class1();
+
+            Assert.Equal(1, class1.GetInt());
+        }
+
+    }
+}
