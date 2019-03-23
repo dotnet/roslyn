e29f322de650f7f4504c7141ff493af4f7a70988
Sam Harwell <sam@tunnelvisionlabs.com>
2019-03-23 11:27:52 -0500

    Implement ImportingConstructorShouldBeObsolete

--- src/Roslyn.Diagnostics.Analyzers/Core/ImportingConstructorShouldBeObsolete.cs
+++ src/Roslyn.Diagnostics.Analyzers/Core/ImportingConstructorShouldBeObsolete.cs
@@ -1,12 +1,22 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Roslyn.Diagnostics.Analyzers
 {
+    /// <summary>
+    /// The importing constructor for a MEF-exported type should be marked obsolete.
+    ///
+    /// <code>
+    /// [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+    /// </code>
+    /// </summary>
     [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
     public sealed class ImportingConstructorShouldBeObsolete : DiagnosticAnalyzer
     {
@@ -32,6 +42,98 @@ namespace Roslyn.Diagnostics.Analyzers
         {
             context.EnableConcurrentExecution();
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(compilationContext =>
+            {
+                var obsoleteAttribute = compilationContext.Compilation.GetTypeByMetadataName("System.ObsoleteAttribute");
+                var exportAttributeV1 = compilationContext.Compilation.GetTypeByMetadataName("System.ComponentModel.Composition.ExportAttribute");
+                var importingConstructorAttributeV1 = compilationContext.Compilation.GetTypeByMetadataName("System.ComponentModel.Composition.ImportingConstructorAttribute");
+                var exportAttributeV2 = compilationContext.Compilation.GetTypeByMetadataName("System.Composition.ExportAttribute");
+                var importingConstructorAttributeV2 = compilationContext.Compilation.GetTypeByMetadataName("System.Composition.ImportingConstructorAttribute");
+
+                if (exportAttributeV1 is null && exportAttributeV2 is null)
+                {
+                    // We don't need to check assemblies unless they're referencing MEF, so we're done
+                    return;
+                }
+
+                compilationContext.RegisterSymbolAction(symbolContext =>
+                {
+                    var namedType = (INamedTypeSymbol)symbolContext.Symbol;
+                    var namedTypeAttributes = namedType.GetApplicableAttributes();
+
+                    AnalyzeSymbolForAttribute(ref symbolContext, obsoleteAttribute, exportAttributeV1, importingConstructorAttributeV1, namedType, namedTypeAttributes);
+                    AnalyzeSymbolForAttribute(ref symbolContext, obsoleteAttribute, exportAttributeV2, importingConstructorAttributeV2, namedType, namedTypeAttributes);
+                }, SymbolKind.NamedType);
+            });
+        }
+
+        private static void AnalyzeSymbolForAttribute(ref SymbolAnalysisContext context, INamedTypeSymbol obsoleteAttribute, INamedTypeSymbol exportAttributeOpt, INamedTypeSymbol importingConstructorAttribute, INamedTypeSymbol namedType, IEnumerable<AttributeData> namedTypeAttributes)
+        {
+            if (exportAttributeOpt is null)
+            {
+                return;
+            }
+
+            var exportAttributeApplication = namedTypeAttributes.FirstOrDefault(ad => ad.AttributeClass.DerivesFrom(exportAttributeOpt));
+            if (exportAttributeApplication is null)
+            {
+                return;
+            }
+
+            foreach (var constructor in namedType.Constructors)
+            {
+                if (constructor.IsImplicitlyDeclared)
+                {
+                    continue;
+                }
+
+                var constructorAttributes = constructor.GetAttributes();
+                if (!constructorAttributes.Any(ad => ad.AttributeClass.DerivesFrom(importingConstructorAttribute)))
+                {
+                    continue;
+                }
+
+                var foundObsoleteAttribute = false;
+                foreach (var attributeData in constructorAttributes)
+                {
+                    if (!attributeData.AttributeClass.Equals(obsoleteAttribute))
+                    {
+                        continue;
+                    }
+
+                    foundObsoleteAttribute = true;
+                    if (attributeData.ConstructorArguments.Length != 2)
+                    {
+                        // '{0}' is MEF-exported and should have a single importing constructor of the correct form
+                        context.ReportDiagnostic(Diagnostic.Create(Rule, exportAttributeApplication.ApplicationSyntaxReference.GetSyntax().GetLocation(), namedType.Name));
+                        break;
+                    }
+
+                    if (!Equals(attributeData.ConstructorArguments[0].Value, "This exported object must be obtained through the MEF export provider."))
+                    {
+                        // '{0}' is MEF-exported and should have a single importing constructor of the correct form
+                        context.ReportDiagnostic(Diagnostic.Create(Rule, exportAttributeApplication.ApplicationSyntaxReference.GetSyntax().GetLocation(), namedType.Name));
+                        break;
+                    }
+
+                    if (!Equals(attributeData.ConstructorArguments[1].Value, true))
+                    {
+                        // '{0}' is MEF-exported and should have a single importing constructor of the correct form
+                        context.ReportDiagnostic(Diagnostic.Create(Rule, exportAttributeApplication.ApplicationSyntaxReference.GetSyntax().GetLocation(), namedType.Name));
+                        break;
+                    }
+
+                    break;
+                }
+
+                if (!foundObsoleteAttribute)
+                {
+                    // '{0}' is MEF-exported and should have a single importing constructor of the correct form
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, exportAttributeApplication.ApplicationSyntaxReference.GetSyntax().GetLocation(), namedType.Name));
+                    break;
+                }
+            }
         }
     }
 }
--- src/Roslyn.Diagnostics.Analyzers/UnitTests/ImportingConstructorShouldBeObsoleteTests.cs
+++ src/Roslyn.Diagnostics.Analyzers/UnitTests/ImportingConstructorShouldBeObsoleteTests.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis.Diagnostics;
-using Roslyn.Diagnostics.CSharp.Analyzers;
-using Roslyn.Diagnostics.VisualBasic.Analyzers;
-using VerifyCS = Microsoft.CodeAnalysis.CSharp.Testing.XUnit.CodeFixVerifier<
+using System.Threading.Tasks;
+using Test.Utilities;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
     Roslyn.Diagnostics.Analyzers.ImportingConstructorShouldBeObsolete,
     Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
-using VerifyVB = Microsoft.CodeAnalysis.VisualBasic.Testing.XUnit.CodeFixVerifier<
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
     Roslyn.Diagnostics.Analyzers.ImportingConstructorShouldBeObsolete,
     Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
 
@@ -14,5 +14,319 @@ namespace Roslyn.Diagnostics.Analyzers.UnitTests
 {
     public class ImportingConstructorShouldBeObsoleteTests
     {
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task SingleExpectedConstructor_CSharp(string mefNamespace)
+        {
+            var source = $@"
+using System;
+using {mefNamespace};
+
+[Export]
+class C {{
+    [ImportingConstructor]
+    [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+    public C() {{ }}
+}}
+
+static class MefConstruction {{
+    internal const string ImportingConstructorMessage = ""This exported object must be obtained through the MEF export provider."";
+}}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task SingleExpectedConstructor_VisualBasic(string mefNamespace)
+        {
+            var source = $@"
+Imports System
+Imports {mefNamespace}
+
+<Export>
+Class C
+    <ImportingConstructor>
+    <Obsolete(MefConstruction.ImportingConstructorMessage, True)>
+    Public Sub New()
+    End Sub
+End Class
+
+Module MefConstruction
+    Friend Const ImportingConstructorMessage As String = ""This exported object must be obtained through the MEF export provider.""
+End Module
+";
+
+            await new VerifyVB.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task ObsoleteButNotError_CSharp(string mefNamespace)
+        {
+            var source = $@"
+using System;
+using {mefNamespace};
+
+[Export]
+class C {{
+    [ImportingConstructor]
+    [Obsolete(MefConstruction.ImportingConstructorMessage, error: false)]
+    public C() {{ }}
+}}
+
+static class MefConstruction {{
+    internal const string ImportingConstructorMessage = ""This exported object must be obtained through the MEF export provider."";
+}}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                    ExpectedDiagnostics = { VerifyCS.Diagnostic().WithSpan(5, 2, 5, 8).WithArguments("C") },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task ObsoleteButNotError_VisualBasic(string mefNamespace)
+        {
+            var source = $@"
+Imports System
+Imports {mefNamespace}
+
+<Export>
+Class C
+    <ImportingConstructor>
+    <Obsolete(MefConstruction.ImportingConstructorMessage, False)>
+    Public Sub New()
+    End Sub
+End Class
+
+Module MefConstruction
+    Friend Const ImportingConstructorMessage As String = ""This exported object must be obtained through the MEF export provider.""
+End Module
+";
+
+            await new VerifyVB.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                    ExpectedDiagnostics = { VerifyVB.Diagnostic().WithSpan(5, 2, 5, 8).WithArguments("C") },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task MessageArgumentOmitted_CSharp(string mefNamespace)
+        {
+            var source = $@"
+using System;
+using {mefNamespace};
+
+[Export]
+class C {{
+    [ImportingConstructor]
+    [Obsolete]
+    public C() {{ }}
+}}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                    ExpectedDiagnostics = { VerifyCS.Diagnostic().WithSpan(5, 2, 5, 8).WithArguments("C") },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task MessageArgumentOmitted_VisualBasic(string mefNamespace)
+        {
+            var source = $@"
+Imports System
+Imports {mefNamespace}
+
+<Export>
+Class C
+    <ImportingConstructor>
+    <Obsolete>
+    Public Sub New()
+    End Sub
+End Class
+";
+
+            await new VerifyVB.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                    ExpectedDiagnostics = { VerifyVB.Diagnostic().WithSpan(5, 2, 5, 8).WithArguments("C") },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task ErrorArgumentOmitted_CSharp(string mefNamespace)
+        {
+            var source = $@"
+using System;
+using {mefNamespace};
+
+[Export]
+class C {{
+    [ImportingConstructor]
+    [Obsolete(MefConstruction.ImportingConstructorMessage)]
+    public C() {{ }}
+}}
+
+static class MefConstruction {{
+    internal const string ImportingConstructorMessage = ""This exported object must be obtained through the MEF export provider."";
+}}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                    ExpectedDiagnostics = { VerifyCS.Diagnostic().WithSpan(5, 2, 5, 8).WithArguments("C") },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task ErrorArgumentOmitted_VisualBasic(string mefNamespace)
+        {
+            var source = $@"
+Imports System
+Imports {mefNamespace}
+
+<Export>
+Class C
+    <ImportingConstructor>
+    <Obsolete(MefConstruction.ImportingConstructorMessage)>
+    Public Sub New()
+    End Sub
+End Class
+
+Module MefConstruction
+    Friend Const ImportingConstructorMessage As String = ""This exported object must be obtained through the MEF export provider.""
+End Module
+";
+
+            await new VerifyVB.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                    ExpectedDiagnostics = { VerifyVB.Diagnostic().WithSpan(5, 2, 5, 8).WithArguments("C") },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task IncorrectMessage_CSharp(string mefNamespace)
+        {
+            var source = $@"
+using System;
+using {mefNamespace};
+
+[Export]
+class C {{
+    [ImportingConstructor]
+    [Obsolete(MefConstruction.ImportingConstructorMessage)]
+    public C() {{ }}
+}}
+
+static class MefConstruction {{
+    internal const string ImportingConstructorMessage = ""INCORRECT MESSAGE"";
+}}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                    ExpectedDiagnostics = { VerifyCS.Diagnostic().WithSpan(5, 2, 5, 8).WithArguments("C") },
+                },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("System.Composition")]
+        [InlineData("System.ComponentModel.Composition")]
+        public async Task IncorrectMessage_VisualBasic(string mefNamespace)
+        {
+            var source = $@"
+Imports System
+Imports {mefNamespace}
+
+<Export>
+Class C
+    <ImportingConstructor>
+    <Obsolete(MefConstruction.ImportingConstructorMessage)>
+    Public Sub New()
+    End Sub
+End Class
+
+Module MefConstruction
+    Friend Const ImportingConstructorMessage As String = ""INCORRECT MESSAGE""
+End Module
+";
+
+            await new VerifyVB.Test
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { AdditionalMetadataReferences.SystemCompositionReference, AdditionalMetadataReferences.SystemComponentModelCompositionReference },
+                    ExpectedDiagnostics = { VerifyVB.Diagnostic().WithSpan(5, 2, 5, 8).WithArguments("C") },
+                },
+            }.RunAsync();
+        }
     }
 }
