cc712bc1e211cd8b6e95f6e6027f78805643d435
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-03-28 14:46:30 +0800

    Use XmlReader For Schema Read

--- src/Utilities/Compiler/WellKnownTypeNames.cs
+++ src/Utilities/Compiler/WellKnownTypeNames.cs
@@ -280,5 +280,7 @@ namespace Analyzer.Utilities
         public const string SystemXmlXmlTextWriter = "System.Xml.XmlTextWriter";
         public const string SystemWindowsMarkupXamlReader = "System.Windows.Markup.XamlReader";
         public const string SystemWebConfigurationHttpRuntimeSection = "System.Web.Configuration.HttpRuntimeSection";
+        public const string SystemDataDataSet = "System.Data.DataSet";
+        public const string SystemXmlXmlReader = "System.Xml.XmlReader";
     }
 }
