b6285348f977b7267006fed2500a94c376f311b7
Heejae Chang <heejaechang@outlook.com>
2019-03-29 12:10:16 -0700

    move StreamJsonRpc to 2.x from 1.3.x (#33960)
    * move StreamJsonRpc to 2.0
    
    * use object and collection initializer
    
    * changed to throw rather than return
    
    * share JsonRpc creation when it can share code
    
    * add linked files to share code
    
    * more moving around so that we can have linked file working
    
    * addressed PR feedback
    
    * changed name to WatsonReporter
    
    * moved to latest streamjson version
    
    * added a way for partner to add thier own json converter to roslyn service hub service base type
    
    * fixed test break.
    
    it is caused by behavior changes between StreamJsonRpc 1.x and 2.x
    
    * added comment follow PR feedbacks
    
    * updated comment following feedbacks

--- src/Workspaces/Remote/ServiceHub/Shared/Extensions.cs
+++ src/Workspaces/Remote/ServiceHub/Shared/Extensions.cs
@@ -2,12 +2,14 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.ErrorReporting;
 using Microsoft.VisualStudio.Telemetry;
+using Newtonsoft.Json;
 using Roslyn.Utilities;
 using StreamJsonRpc;
 
@@ -15,6 +17,24 @@ namespace Microsoft.CodeAnalysis.Remote
 {
     internal static partial class Extensions
     {
+        public static JsonRpc CreateStreamJsonRpc(
+            this Stream stream,
+            object target,
+            TraceSource logger,
+            IEnumerable<JsonConverter> jsonConverters = null)
+        {
+            jsonConverters = jsonConverters ?? SpecializedCollections.EmptyEnumerable<JsonConverter>();
+
+            var jsonFormatter = new JsonMessageFormatter();
+            jsonFormatter.JsonSerializer.Converters.AddRange(jsonConverters.Concat(AggregateJsonConverter.Instance));
+
+            return new JsonRpc(new HeaderDelimitedMessageHandler(stream, jsonFormatter), target)
+            {
+                CancelLocallyInvokedMethodsWhenConnectionIsClosed = true,
+                TraceSource = logger
+            };
+        }
+
         public static async Task InvokeAsync(
             this JsonRpc rpc, string targetName, IReadOnlyList<object> arguments,
             Func<Stream, CancellationToken, Task> funcWithDirectStreamAsync, CancellationToken cancellationToken)
