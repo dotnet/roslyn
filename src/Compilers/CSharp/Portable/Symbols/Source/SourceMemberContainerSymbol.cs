f34d3e4d14261ec8bd97b66f9a765b3bef415c2a
Rikki Gibson <rigibson@microsoft.com>
2019-04-03 15:26:32 -0700

    Fix unconditional binding of pointer types

--- src/Compilers/CSharp/Portable/Symbols/Source/SourceMemberContainerSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourceMemberContainerSymbol.cs
@@ -1889,7 +1889,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                     {
                         continue;
                     }
-                    var type = field.Type;
+                    var type = field.NonPointerType();
                     if (((object)type != null) &&
                         (type.TypeKind == TypeKind.Struct) &&
                         BaseTypeAnalysis.StructDependsOn((NamedTypeSymbol)type, this) &&
