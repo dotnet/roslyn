030e442570800882b5debfa01cbd4a83a590a5f3
Sam Harwell <Sam.Harwell@microsoft.com>
2019-04-05 12:27:22 -0500

    Support debugging benchmarks

--- src/Tools/IdeBenchmarks/Program.cs
+++ src/Tools/IdeBenchmarks/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 namespace IdeBenchmarks
@@ -8,7 +9,13 @@ namespace IdeBenchmarks
     {
         private static void Main(string[] args)
         {
-            new BenchmarkSwitcher(typeof(Program).Assembly).Run(args);
+#if DEBUG
+            var config = new DebugInProcessConfig();
+#else
+            IConfig config = null;
+#endif
+
+            new BenchmarkSwitcher(typeof(Program).Assembly).Run(args, config);
         }
     }
 }
