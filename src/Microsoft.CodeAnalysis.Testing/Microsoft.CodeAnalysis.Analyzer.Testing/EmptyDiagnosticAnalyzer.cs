764e6d1079eae7d2df9bad74e98bd5b63fb47ea2
Sam Harwell <Sam.Harwell@microsoft.com>
2019-04-07 10:02:09 -0500

    Provide a default EmptyDiagnosticAnalyzer
    The empty analyzer is useful for testing code fixes for compiler
    warnings and errors.

---
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/EmptyDiagnosticAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.Testing
+{
+    /// <summary>
+    /// Defines a <see cref="DiagnosticAnalyzer"/> which does not report any diagnostics.
+    /// </summary>
+#pragma warning disable RS1001 // Missing diagnostic analyzer attribute.
+    public sealed class EmptyDiagnosticAnalyzer : DiagnosticAnalyzer
+#pragma warning restore RS1001 // Missing diagnostic analyzer attribute.
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+            => ImmutableArray<DiagnosticDescriptor>.Empty;
+
+        public override void Initialize(AnalysisContext context)
+        {
+        }
+    }
+}
