a8ac15a3d35cbd98031f23c2151b700767e12156
Cheryl Borley <chborley@microsoft.com>
2019-04-09 11:17:55 -0700

    Don't complete when multiple chars selected

--- src/Test/Utilities/Portable/MarkedSource/MarkupTestFile.cs
+++ src/Test/Utilities/Portable/MarkedSource/MarkupTestFile.cs
@@ -228,7 +228,7 @@ namespace Roslyn.Test.Utilities
         public static void GetPositionAndSpans(string input, out string output, out int cursorPosition, out ImmutableArray<TextSpan> spans)
         {
             GetPositionAndSpans(input, out output, out int? pos, out spans);
-            cursorPosition = pos.Value;
+            cursorPosition = pos ?? default;
         }
 
         public static void GetPosition(string input, out string output, out int? cursorPosition)
