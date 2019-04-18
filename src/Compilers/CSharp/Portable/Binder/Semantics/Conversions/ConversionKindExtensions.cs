c300c8c54a8964bc1c7a8dae0810045d07035998
Andrew Hall (METAL) <andrha@microsoft.com>
2019-04-17 17:21:02 -0700

    Add new ConversionKind to IsImplicitConversion test

--- src/Compilers/CSharp/Portable/Binder/Semantics/Conversions/ConversionKindExtensions.cs
+++ src/Compilers/CSharp/Portable/Binder/Semantics/Conversions/ConversionKindExtensions.cs
@@ -21,6 +21,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             switch (conversionKind)
             {
                 case ConversionKind.NoConversion:
+                case ConversionKind.UnsetConversionKind:
                     return false;
 
                 case ConversionKind.Identity:
