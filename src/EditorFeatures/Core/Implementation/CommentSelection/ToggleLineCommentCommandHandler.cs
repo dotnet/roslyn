23102cd8297fb0fd5b759b689c75ac7d874523d0
David Barbet <dabarbet@microsoft.com>
2019-04-19 15:16:29 -0700

    Add telemetry to toggle line comment.

--- src/EditorFeatures/Core/Implementation/CommentSelection/ToggleLineCommentCommandHandler.cs
+++ src/EditorFeatures/Core/Implementation/CommentSelection/ToggleLineCommentCommandHandler.cs
@@ -11,6 +11,7 @@ using Microsoft.CodeAnalysis.CommentSelection;
 using Microsoft.CodeAnalysis.Editor.Shared.Extensions;
 using Microsoft.CodeAnalysis.Experiments;
 using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Internal.Log;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Text;
@@ -70,19 +71,26 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.CommentSelection
         internal async override Task<CommentSelectionResult> CollectEditsAsync(Document document, ICommentSelectionService service,
             ITextBuffer subjectBuffer, NormalizedSnapshotSpanCollection selectedSpans, ValueTuple command, CancellationToken cancellationToken)
         {
-            var experimentationService = document.Project.Solution.Workspace.Services.GetRequiredService<IExperimentationService>();
-            if (!experimentationService.IsExperimentEnabled(WellKnownExperimentNames.RoslynToggleLineComment))
+            using (Logger.LogBlock(FunctionId.CommandHandler_ToggleLineComment, KeyValueLogMessage.Create(LogType.UserAction, m =>
             {
+                m[LanguageNameString] = document.Project.Language;
+                m[LengthString] = subjectBuffer.CurrentSnapshot.Length;
+            }), cancellationToken))
+            {
+                var experimentationService = document.Project.Solution.Workspace.Services.GetRequiredService<IExperimentationService>();
+                if (!experimentationService.IsExperimentEnabled(WellKnownExperimentNames.RoslynToggleLineComment))
+                {
+                    return s_emptyCommentSelectionResult;
+                }
+
+                var commentInfo = await service.GetInfoAsync(document, selectedSpans.First().Span.ToTextSpan(), cancellationToken).ConfigureAwait(false);
+                if (commentInfo.SupportsSingleLineComment)
+                {
+                    return ToggleLineComment(commentInfo, selectedSpans);
+                }
+
                 return s_emptyCommentSelectionResult;
             }
-
-            var commentInfo = await service.GetInfoAsync(document, selectedSpans.First().Span.ToTextSpan(), cancellationToken).ConfigureAwait(false);
-            if (commentInfo.SupportsSingleLineComment)
-            {
-                return ToggleLineComment(commentInfo, selectedSpans);
-            }
-
-            return s_emptyCommentSelectionResult;
         }
 
         private CommentSelectionResult ToggleLineComment(CommentSelectionInfo commentInfo,
