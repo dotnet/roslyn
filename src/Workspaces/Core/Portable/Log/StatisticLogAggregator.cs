d8cab9447c15a68cb18791ea4ed12de8f7f90197
Gen Lu <gel@microsoft.com>
2019-05-01 15:33:28 -0700

    Add StatisticLogAggregator

---
+++ src/Workspaces/Core/Portable/Log/StatisticLogAggregator.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.CodeAnalysis.Internal.Log
+{
+    internal sealed class StatisticLogAggregator : AbstractLogAggregator<StatisticLogAggregator.StatisticCounter>
+    {
+        protected override StatisticCounter CreateCounter()
+        {
+            return new StatisticCounter();
+        }
+
+        public void AddDataPoint(object key, int value)
+        {
+            var counter = GetCounter(key);
+            counter.AddDataPoint(value);
+        }
+
+        public StatisticResult GetStaticticResult(object key)
+        {
+            if (TryGetCounter(key, out var counter))
+            {
+                return counter.GetStatisticResult();
+            }
+
+            return default;
+        }
+
+        internal sealed class StatisticCounter
+        {
+            private int _count;
+            private int _maximum;
+            private int _mininum;
+
+            private int _total;
+
+            public void AddDataPoint(int value)
+            {
+                _count++;
+                _total += value;
+
+                if (value > _maximum)
+                {
+                    _maximum = value;
+                }
+
+                if (value < _mininum)
+                {
+                    _mininum = value;
+                }
+            }
+
+            public StatisticResult GetStatisticResult()
+            {
+                if (_count == 0)
+                {
+                    return default;
+                }
+                else
+                {
+                    return new StatisticResult(_maximum, _mininum, median: null, mean: _total / _count, range: _maximum - _mininum, mode: null, count: _count);
+                }
+            }
+        }
+    }
+}
