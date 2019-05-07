a24d4f33de52ce48520ed87c9d173d55a8aadaa2
David Barbet <dabarbet@microsoft.com>
2019-05-07 12:30:10 -0700

    Fixup diagnostics.

--- src/VisualStudio/LiveShare/Impl/Client/Diagnostics/RoslynRemoteDiagnosticsService.Exports.cs
+++ src/VisualStudio/LiveShare/Impl/Client/Diagnostics/RoslynRemoteDiagnosticsService.Exports.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Composition;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Host.Mef;
 
 namespace Microsoft.VisualStudio.LanguageServices.LiveShare.Client
@@ -25,16 +24,4 @@ namespace Microsoft.VisualStudio.LanguageServices.LiveShare.Client
         {
         }
     }
-
-#if !VS_16_0
-    [ExportLanguageService(typeof(IRemoteDiagnosticsService), StringConstants.TypeScriptLanguageName, WorkspaceKind.AnyCodeRoslynWorkspace), Shared]
-    internal class TypeScriptLspRemoteDiagnosticsService : RoslynRemoteDiagnosticsService
-    {
-        [ImportingConstructor]
-        public TypeScriptLspRemoteDiagnosticsService(RoslynLSPClientServiceFactory roslynLSPClientServiceFactory)
-            : base(roslynLSPClientServiceFactory)
-        {
-        }
-    }
-#endif
 }
