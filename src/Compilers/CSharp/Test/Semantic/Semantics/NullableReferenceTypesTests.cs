e3e286f10cadb50eb476faa95df8e515297d25b3
Julien Couvreur <julien.couvreur@gmail.com>
2019-05-07 12:39:06 -0700

    Address feedback

--- src/Compilers/CSharp/Test/Semantic/Semantics/NullableReferenceTypesTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/NullableReferenceTypesTests.cs
@@ -95864,7 +95864,7 @@ class Program
         }
 
         [Fact, WorkItem(34827, "https://github.com/dotnet/roslyn/issues/34827")]
-        public void Verify34827()
+        public void ArrayConversionToInterface()
         {
             string source = @"
 using System.Collections.Generic;
@@ -95874,9 +95874,11 @@ class C
     {
         IEnumerable<object> x1 = new[] { ""string"", null }; // 1
         IEnumerable<object?> x2 = new[] { ""string"", null };
-        IList<string> x3 = new[] { ""string"", null }; // 2
-        ICollection<string?> x4 = new[] { ""string"", null };
-        IReadOnlyList<string?> x5 = new[] { ""string"" };
+        IEnumerable<object?> x3 = new[] { ""string"" };
+        IList<string> x4 = new[] { ""string"", null }; // 2
+        ICollection<string?> x5 = new[] { ""string"", null };
+        ICollection<string?> x6 = new[] { ""string"" };
+        IReadOnlyList<string?> x7 = new[] { ""string"" };
     }
 }";
             var comp = CreateNullableCompilation(source);
@@ -95884,14 +95886,14 @@ class C
                 // (7,34): warning CS8619: Nullability of reference types in value of type 'string?[]' doesn't match target type 'IEnumerable<object>'.
                 //         IEnumerable<object> x1 = new[] { "string", null }; // 1
                 Diagnostic(ErrorCode.WRN_NullabilityMismatchInAssignment, @"new[] { ""string"", null }").WithArguments("string?[]", "System.Collections.Generic.IEnumerable<object>").WithLocation(7, 34),
-                // (9,28): warning CS8619: Nullability of reference types in value of type 'string?[]' doesn't match target type 'IList<string>'.
-                //         IList<string> x3 = new[] { "string", null }; // 2
-                Diagnostic(ErrorCode.WRN_NullabilityMismatchInAssignment, @"new[] { ""string"", null }").WithArguments("string?[]", "System.Collections.Generic.IList<string>").WithLocation(9, 28)
+                // (10,28): warning CS8619: Nullability of reference types in value of type 'string?[]' doesn't match target type 'IList<string>'.
+                //         IList<string> x4 = new[] { "string", null }; // 2
+                Diagnostic(ErrorCode.WRN_NullabilityMismatchInAssignment, @"new[] { ""string"", null }").WithArguments("string?[]", "System.Collections.Generic.IList<string>").WithLocation(10, 28)
                 );
         }
 
         [Fact, WorkItem(34827, "https://github.com/dotnet/roslyn/issues/34827")]
-        public void Verify34827_Nested()
+        public void ArrayConversionToInterface_Nested()
         {
             string source = @"
 using System.Collections.Generic;
@@ -95899,21 +95901,26 @@ class C
 {
     void M()
     {
-        IEnumerable<object[]> x1 = new[] { new[] { ""string"" }, null }; // 1
-        IEnumerable<object[]?> x2 = new[] { new[] { ""string"" }, null };
-        IEnumerable<IEnumerable<string>> x3 = new[] { new[] { ""string"" }, null }; // 2
-        IEnumerable<IEnumerable<string>?> x4 = new[] { new[] { ""string"" }, null };
-        IEnumerable<IEnumerable<string>?> x5 = new[] { new[] { ""string"" } };
+        IEnumerable<object[]> x1 = new[] { new[] { ""string"", null } }; // 1
+        IEnumerable<object[]?> x2 = new[] { new[] { ""string"", null } }; // 2
+        IEnumerable<object?[]> x3 = new[] { new[] { ""string"", null } };
+        IEnumerable<object?[]?> x4 = new[] { new[] { ""string"" } };
+        IEnumerable<IEnumerable<string>> x5 = new[] { new[] { ""string"" , null } }; // 3
+        IEnumerable<IEnumerable<string?>> x6 = new[] { new[] { ""string"" , null } };
+        IEnumerable<IEnumerable<string?>> x7 = new[] { new[] { ""string"" } };
     }
 }";
             var comp = CreateNullableCompilation(source);
             comp.VerifyDiagnostics(
-                // (7,36): warning CS8619: Nullability of reference types in value of type 'string[]?[]' doesn't match target type 'IEnumerable<object[]>'.
-                //         IEnumerable<object[]> x1 = new[] { new[] { "string" }, null }; // 1
-                Diagnostic(ErrorCode.WRN_NullabilityMismatchInAssignment, @"new[] { new[] { ""string"" }, null }").WithArguments("string[]?[]", "System.Collections.Generic.IEnumerable<object[]>").WithLocation(7, 36),
-                // (9,47): warning CS8619: Nullability of reference types in value of type 'string[]?[]' doesn't match target type 'IEnumerable<IEnumerable<string>>'.
-                //         IEnumerable<IEnumerable<string>> x3 = new[] { new[] { "string" }, null }; // 2
-                Diagnostic(ErrorCode.WRN_NullabilityMismatchInAssignment, @"new[] { new[] { ""string"" }, null }").WithArguments("string[]?[]", "System.Collections.Generic.IEnumerable<System.Collections.Generic.IEnumerable<string>>").WithLocation(9, 47)
+                // (7,36): warning CS8619: Nullability of reference types in value of type 'string?[][]' doesn't match target type 'IEnumerable<object[]>'.
+                //         IEnumerable<object[]> x1 = new[] { new[] { "string", null } }; // 1
+                Diagnostic(ErrorCode.WRN_NullabilityMismatchInAssignment, @"new[] { new[] { ""string"", null } }").WithArguments("string?[][]", "System.Collections.Generic.IEnumerable<object[]>").WithLocation(7, 36),
+                // (8,37): warning CS8619: Nullability of reference types in value of type 'string?[][]' doesn't match target type 'IEnumerable<object[]?>'.
+                //         IEnumerable<object[]?> x2 = new[] { new[] { "string", null } }; // 2
+                Diagnostic(ErrorCode.WRN_NullabilityMismatchInAssignment, @"new[] { new[] { ""string"", null } }").WithArguments("string?[][]", "System.Collections.Generic.IEnumerable<object[]?>").WithLocation(8, 37),
+                // (11,47): warning CS8619: Nullability of reference types in value of type 'string?[][]' doesn't match target type 'IEnumerable<IEnumerable<string>>'.
+                //         IEnumerable<IEnumerable<string>> x5 = new[] { new[] { "string" , null } }; // 3
+                Diagnostic(ErrorCode.WRN_NullabilityMismatchInAssignment, @"new[] { new[] { ""string"" , null } }").WithArguments("string?[][]", "System.Collections.Generic.IEnumerable<System.Collections.Generic.IEnumerable<string>>").WithLocation(11, 47)
                 );
         }
     }
