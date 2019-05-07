09139a3d4b2c21f16e1530a79873576d3c4a8352
David Barbet <dabarbet@microsoft.com>
2019-05-07 11:36:58 -0700

    Update license comments, fixup classifications.

--- src/VisualStudio/LiveShare/Impl/Client/SignatureHelp/RoslynSignatureHelpProvider.cs
+++ src/VisualStudio/LiveShare/Impl/Client/SignatureHelp/RoslynSignatureHelpProvider.cs
@@ -1,6 +1,4 @@
-﻿//
-//  Copyright (c) Microsoft Corporation. All rights reserved.
-//
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
 using System.Collections.Generic;
@@ -8,7 +6,6 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Cascade.Common;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.SignatureHelp;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
@@ -18,13 +15,10 @@ namespace Microsoft.VisualStudio.LanguageServices.LiveShare.Client
     class RoslynSignatureHelpProvider : ISignatureHelpProvider
     {
         private readonly RoslynLSPClientServiceFactory roslynLSPClientServiceFactory;
-        private readonly IVsConfigurationSettings configurationSettings;
 
-        public RoslynSignatureHelpProvider(RoslynLSPClientServiceFactory roslynLSPClientServiceFactory,
-            IVsConfigurationSettings configurationSettings)
+        public RoslynSignatureHelpProvider(RoslynLSPClientServiceFactory roslynLSPClientServiceFactory)
         {
             this.roslynLSPClientServiceFactory = roslynLSPClientServiceFactory ?? throw new ArgumentNullException(nameof(roslynLSPClientServiceFactory));
-            this.configurationSettings = configurationSettings ?? throw new ArgumentNullException(nameof(configurationSettings));
         }
         public bool IsTriggerCharacter(char ch)
         {
