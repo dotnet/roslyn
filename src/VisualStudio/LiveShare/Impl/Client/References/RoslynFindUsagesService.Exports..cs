bbed0f5d5e7370d91010ae72b504e04e7059a442
David Barbet <dabarbet@microsoft.com>
2019-05-07 17:43:56 -0700

    Fix threading context.

--- src/VisualStudio/LiveShare/Impl/Client/References/RoslynFindUsagesService.Exports..cs
+++ src/VisualStudio/LiveShare/Impl/Client/References/RoslynFindUsagesService.Exports..cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System.Composition;
+/*using System.Composition;
 using Microsoft.CodeAnalysis.Editor.FindUsages;
 using Microsoft.CodeAnalysis.Host.Mef;
 
@@ -25,4 +25,4 @@ namespace Microsoft.VisualStudio.LanguageServices.LiveShare.Client
         {
         }
     }
-}
+}*/
