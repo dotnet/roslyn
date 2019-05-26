c3a33ea3e4f5285d9fcf01c0210b25a173b2a350
TIHan <lol.tihan@gmail.com>
2019-05-26 04:54:13 -0700

    Finishing the last of shims

--- src/Tools/ExternalAccess/FSharp/Diagnostics/FSharpIDEDiagnosticIds.cs
+++ src/Tools/ExternalAccess/FSharp/Diagnostics/FSharpIDEDiagnosticIds.cs
@@ -7,5 +7,6 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Diagnostics
     internal static class FSharpIDEDiagnosticIds
     {
         public static string SimplifyNamesDiagnosticId => IDEDiagnosticIds.SimplifyNamesDiagnosticId;
+        public static string RemoveUnnecessaryImportsDiagnosticId => IDEDiagnosticIds.RemoveUnnecessaryImportsDiagnosticId;
     }
 }
