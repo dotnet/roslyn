5d61f32039350de8d98df10759a9b28d6894d9ca
TIHan <lol.tihan@gmail.com>
2019-05-25 22:43:35 -0700

    Added IFSharpDocumentNavigationService

---
+++ src/Tools/ExternalAccess/FSharp/Internal/Navigation/InternalFSharpNavigableItem.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.ExternalAccess.FSharp.Navigation;
+using Microsoft.CodeAnalysis.Navigation;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Internal.Navigation
+{
+    internal class InternalFSharpNavigableItem : INavigableItem
+    {
+        public InternalFSharpNavigableItem(FSharpNavigableItem item)
+        {
+            Document = item.Document;
+            SourceSpan = item.SourceSpan;
+        }
+
+        public Glyph Glyph => Glyph.BasicFile;
+
+        public ImmutableArray<TaggedText> DisplayTaggedParts => ImmutableArray<TaggedText>.Empty;
+
+        public bool DisplayFileLocation => true;
+
+        public bool IsImplicitlyDeclared => false;
+
+        public Document Document { get; private set; }
+
+        public TextSpan SourceSpan { get; private set; }
+
+        public ImmutableArray<INavigableItem> ChildItems => ImmutableArray<INavigableItem>.Empty;
+    }
+}
---
+++ src/Tools/ExternalAccess/FSharp/Navigation/FSharpNavigableItem.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Navigation
+{
+    internal class FSharpNavigableItem
+    {
+        public FSharpNavigableItem(Document document, TextSpan sourceSpan)
+        {
+            Document = document;
+            SourceSpan = sourceSpan;
+        }
+
+        public Document Document { get; private set; }
+
+        public TextSpan SourceSpan { get; private set; }
+    }
+}
