28ec1848bf4aa448639be3d7b02e8d7528b6d099
Sam Harwell <Sam.Harwell@microsoft.com>
2019-05-28 11:15:11 -0500

    Remove unnecessary import directives

--- src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.Analyzer.Testing.NUnit/AnalyzerVerifier`1.vb
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.Analyzer.Testing.NUnit/AnalyzerVerifier`1.vb
@@ -2,7 +2,6 @@
 
 Imports Microsoft.CodeAnalysis.Diagnostics
 Imports Microsoft.CodeAnalysis.Testing.Verifiers
-Imports Microsoft.CodeAnalysis.VisualBasic.Testing
 
 Public Class AnalyzerVerifier(Of TAnalyzer As {DiagnosticAnalyzer, New})
     Inherits VisualBasicAnalyzerVerifier(Of TAnalyzer, NUnitVerifier)
