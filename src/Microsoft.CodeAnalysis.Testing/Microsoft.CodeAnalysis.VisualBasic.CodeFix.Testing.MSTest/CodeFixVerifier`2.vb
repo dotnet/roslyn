b82e5d9a484f24dc37c5e21c2a8caca063ef2590
Sam Harwell <Sam.Harwell@microsoft.com>
2019-05-28 11:15:11 -0500

    Remove unnecessary import directives

--- src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.CodeFix.Testing.MSTest/CodeFixVerifier`2.vb
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.CodeFix.Testing.MSTest/CodeFixVerifier`2.vb
@@ -3,7 +3,6 @@
 Imports Microsoft.CodeAnalysis.CodeFixes
 Imports Microsoft.CodeAnalysis.Diagnostics
 Imports Microsoft.CodeAnalysis.Testing.Verifiers
-Imports Microsoft.CodeAnalysis.VisualBasic.Testing
 
 Public Class CodeFixVerifier(Of TAnalyzer As {DiagnosticAnalyzer, New}, TCodeFix As {CodeFixProvider, New})
     Inherits VisualBasicCodeFixVerifier(Of TAnalyzer, TCodeFix, MSTestVerifier)
