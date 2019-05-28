fbd55efe4a38cea8d3081a24e3e338740ac937f4
Sam Harwell <Sam.Harwell@microsoft.com>
2019-05-28 11:19:42 -0500

    Initial support for project references

---
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/ProjectState.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.CodeAnalysis.Testing
+{
+    public abstract class ProjectState
+    {
+        private readonly string _defaultPrefix;
+        private readonly string _defaultExtension;
+
+        protected ProjectState(string name, string defaultPrefix, string defaultExtension)
+        {
+            Name = name;
+            _defaultPrefix = defaultPrefix;
+            _defaultExtension = defaultExtension;
+
+            Sources = new SourceFileList(defaultPrefix, defaultExtension);
+        }
+
+        public string Name { get; }
+
+        public string AssemblyName => Name;
+
+        public abstract string Language { get; }
+
+        /// <summary>
+        /// Gets the set of source files for analyzer or code fix testing. Files may be added to this list using one of
+        /// the <see cref="SourceFileList.Add(string)"/> methods.
+        /// </summary>
+        public SourceFileList Sources { get; }
+
+        public MetadataReferenceCollection AdditionalReferences { get; } = new MetadataReferenceCollection();
+    }
+}
---
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.CSharp.Analyzer.Testing/CSharpProjectState.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Testing;
+
+namespace Microsoft.CodeAnalysis.CSharp.Testing
+{
+    public class CSharpProjectState : ProjectState
+    {
+        public CSharpProjectState(string name)
+            : base(name, defaultPrefix: "Test", defaultExtension: "cs")
+        {
+        }
+
+        public override string Language => LanguageNames.CSharp;
+    }
+}
