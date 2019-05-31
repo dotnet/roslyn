1b12bd38cfdf044f977c30042ba47dbc46aa052b
HeeJae Chang <hechang@microsoft.com>
2019-05-31 12:57:01 -0700

    made test passed

--- src/VisualStudio/TestUtilities2/VisualStudioTestExportProvider.vb
+++ src/VisualStudio/TestUtilities2/VisualStudioTestExportProvider.vb
@@ -5,6 +5,8 @@ Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Microsoft.VisualStudio.Composition
 Imports Microsoft.VisualStudio.LanguageServices.CSharp.CodeModel
 Imports Microsoft.VisualStudio.LanguageServices.VisualBasic.CodeModel
+Imports Microsoft.CodeAnalysis.Host
+Imports Microsoft.CodeAnalysis.Host.Mef
 
 Namespace Microsoft.VisualStudio.LanguageServices.UnitTests
     Public Module VisualStudioTestExportProvider
@@ -12,9 +14,21 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests
             Dim additionalAssemblies = {GetType(CSharpCodeModelService).Assembly,
                                         GetType(VisualBasicCodeModelService).Assembly}
 
-            Factory = ExportProviderCache.GetOrCreateExportProviderFactory(TestExportProvider.EntireAssemblyCatalogWithCSharpAndVisualBasic.WithParts(ExportProviderCache.GetOrCreateAssemblyCatalog(additionalAssemblies)))
+            Factory = ExportProviderCache.GetOrCreateExportProviderFactory(
+                TestExportProvider.EntireAssemblyCatalogWithCSharpAndVisualBasic.WithParts(
+                    ExportProviderCache.GetOrCreateAssemblyCatalog(additionalAssemblies)).WithPart(GetType(MockWorkspaceEventListenerProvider)))
         End Sub
 
         Public ReadOnly Property Factory As IExportProviderFactory
+
+        ' mock default workspace event listener so that we don't try to enable solution crawler and etc
+        ' implicitly
+        <ExportWorkspaceServiceFactory(GetType(IWorkspaceEventListenerProvider), ServiceLayer.Host), System.Composition.Shared>
+        Friend Class MockWorkspaceEventListenerProvider
+            Implements IWorkspaceServiceFactory
+            Public Function CreateService(workspaceServices As HostWorkspaceServices) As IWorkspaceService Implements IWorkspaceServiceFactory.CreateService
+                Return Nothing
+            End Function
+        End Class
     End Module
 End Namespace
