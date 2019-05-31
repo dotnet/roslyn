107937434eb5e917058f4047cc4c65d6cc0c37fb
Lauren Rosenberg <t-laro@microsoft.com>
2019-05-31 14:16:58 -0700

    Additional tests

--- src/Compilers/CSharp/Test/Semantic/Semantics/NullCheckedVariableTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/NullCheckedVariableTests.cs
@@ -43,5 +43,35 @@ abstract class C
                     // delegate void Del(int x!, int y);
                     Diagnostic(ErrorCode.ERR_MustNullCheckInImplementation, "int x!").WithArguments("int x!").WithLocation(4, 27));
         }
+
+        [Fact]
+        public void NullCheckedInterfaceMethod()
+        {
+            var source = @"
+interface C
+{
+    public int M(int x!);
+}";
+            CreateCompilation(source).VerifyDiagnostics(
+                    // (4, 18): error CS8713: Parameter 'int x!' can only have exclamation - point null checking in implementation methods.
+                    // delegate void Del(int x!, int y);
+                    Diagnostic(ErrorCode.ERR_MustNullCheckInImplementation, "int x!").WithArguments("int x!").WithLocation(4, 18));
+        }
+
+        [Fact]
+        public void NullCheckedExternMethod()
+        {
+            var source = @"
+using System.Runtime.InteropServices;
+class C
+{
+    [DllImport(""User32.dll"")]
+    public static extern int M(int x!);
+}";
+            CreateCompilation(source).VerifyDiagnostics(
+                    // (6, 32): error CS8713: Parameter 'int x!' can only have exclamation - point null checking in implementation methods.
+                    // delegate void Del(int x!, int y);
+                    Diagnostic(ErrorCode.ERR_MustNullCheckInImplementation, "int x!").WithArguments("int x!").WithLocation(6, 32));
+        }
     }
 }
