55c1c7e160c9986ca55820b048ec9c390dbd7e9e
HeeJae Chang <hechang@microsoft.com>
2019-05-31 01:40:39 -0700

    update comment

--- src/VisualStudio/Core/Def/Implementation/TaskList/ExternalErrorDiagnosticUpdateSource.cs
+++ src/VisualStudio/Core/Def/Implementation/TaskList/ExternalErrorDiagnosticUpdateSource.cs
@@ -180,13 +180,10 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TaskList
                     return;
                 }
 
-                // explicitly start solution crawler if it didn't start yet.
-                // we have a bug where we don't start solution cralwer until a file is opened (package is loaded)
-                // https://github.com/dotnet/roslyn/issues/35514
-                // until that is fixed, we need to do this for the case where user build solution right after opening solution
-                // without any file opened. "EnsureRegistration" is no-op if same workspace is already registered.
-                //
-                // here we give initializeLazily: false so that solution crawler is fully initialized when we do de-dup live and build errors
+                // explicitly start solution crawler if it didn't start yet. since solution crawler is lazy, 
+                // user might have built solution before workspace fires its first event yet (which is when solution crawler is initialized)
+                // here we give initializeLazily: false so that solution crawler is fully initialized when we do de-dup live and build errors,
+                // otherwise, we will think none of error we have here belong to live errors since diagnostic service is not initialized yet.
                 var registrationService = (SolutionCrawlerRegistrationService)_workspace.Services.GetService<ISolutionCrawlerRegistrationService>();
                 registrationService.EnsureRegistration(_workspace, initializeLazily: false);
 
