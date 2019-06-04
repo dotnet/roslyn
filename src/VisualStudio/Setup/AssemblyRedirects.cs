fac8b38aadb3859ff28c4e534aa3ed1951cbbe3a
David Barbet <dabarbet@microsoft.com>
2019-06-04 16:37:41 -0700

    Provide code base instead of binding redirect when bundling the protocol with roslyn.

--- src/VisualStudio/Setup/AssemblyRedirects.cs
+++ src/VisualStudio/Setup/AssemblyRedirects.cs
@@ -66,8 +66,8 @@ using Roslyn.VisualStudio.Setup;
 
 [assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\Microsoft.CodeAnalysis.FlowAnalysis.Utilities.dll")]
 
-[assembly: ProvideRoslynBindingRedirection("Microsoft.VisualStudio.LanguageServer.Protocol.dll")]
-[assembly: ProvideRoslynBindingRedirection("Microsoft.VisualStudio.LanguageServer.Protocol.Extensions.dll")]
+[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\Microsoft.VisualStudio.LanguageServer.Protocol.dll")]
+[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\Microsoft.VisualStudio.LanguageServer.Protocol.Extensions.dll")]
 
 [assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\ICSharpCode.Decompiler.dll")]
 
