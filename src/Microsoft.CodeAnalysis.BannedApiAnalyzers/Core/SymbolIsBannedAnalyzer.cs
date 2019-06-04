5e07860087a4587f8baf2e952a79982a073d5c3f
Daniel Paoliello <danpao@microsoft.com>
2019-06-04 12:39:20 -0700

    BannedApiAnalyzer should recognize uses of banned types as type arguments, array elements or pointers. Issue Details: BannedApiAnalyzer only looked at the original definition for a type, and not what its type arguments were. This means that, if `T:C` was banned, then users would still be permitted to use `List<C>`, `C[]` and `C*`.
    Fix Details:
    * Check the type arguments for `INamedTypeSymbol`, and use the `ConstructedFrom` property to get the original definition (this retains the generic instatiation of any containing types, whereas `OriginalDefinition` uses the original definition).
    * Check array element types.
    * Check pointer pointed-at types.
    * Add callback for array creation and address-of operations.

--- src/Microsoft.CodeAnalysis.BannedApiAnalyzers/Core/SymbolIsBannedAnalyzer.cs
+++ src/Microsoft.CodeAnalysis.BannedApiAnalyzers/Core/SymbolIsBannedAnalyzer.cs
@@ -114,6 +114,14 @@ namespace Microsoft.CodeAnalysis.BannedApiAnalyzers
                             VerifySymbol(context.ReportDiagnostic, memberReference.Member, context.Operation.Syntax);
                             VerifyType(context.ReportDiagnostic, memberReference.Member.ContainingType, context.Operation.Syntax);
                             break;
+
+                        case IArrayCreationOperation arrayCreation:
+                            VerifyType(context.ReportDiagnostic, arrayCreation.Type, context.Operation.Syntax);
+                            break;
+
+                        case IAddressOfOperation addressOf:
+                            VerifyType(context.ReportDiagnostic, addressOf.Type, context.Operation.Syntax);
+                            break;
                     }
                 },
                 OperationKind.ObjectCreation,
@@ -121,7 +129,9 @@ namespace Microsoft.CodeAnalysis.BannedApiAnalyzers
                 OperationKind.EventReference,
                 OperationKind.FieldReference,
                 OperationKind.MethodReference,
-                OperationKind.PropertyReference);
+                OperationKind.PropertyReference,
+                OperationKind.ArrayCreation,
+                OperationKind.AddressOf);
 
             compilationContext.RegisterSyntaxNodeAction(
                 context => VerifyDocumentationSyntax(context.ReportDiagnostic, GetReferenceSyntaxNodeFromXmlCref(context.Node), context),
@@ -206,12 +216,20 @@ namespace Microsoft.CodeAnalysis.BannedApiAnalyzers
                 }
             }
 
-            void VerifyType(Action<Diagnostic> reportDiagnostic, ITypeSymbol type, SyntaxNode syntaxNode)
+            bool VerifyType(Action<Diagnostic> reportDiagnostic, ITypeSymbol type, SyntaxNode syntaxNode)
             {
-                type = type.OriginalDefinition;
-
                 do
                 {
+                    if (!VerifyTypeArguments(reportDiagnostic, type, syntaxNode, out type))
+                    {
+                        return false;
+                    }
+                    if (type == null)
+                    {
+                        // Type will be null for arrays and pointers.
+                        return true;
+                    }
+
                     if (entryBySymbol.TryGetValue(type, out var entry))
                     {
                         reportDiagnostic(
@@ -220,12 +238,48 @@ namespace Microsoft.CodeAnalysis.BannedApiAnalyzers
                                 syntaxNode.GetLocation(),
                                 type.ToDisplayString(SymbolDisplayFormat),
                                 string.IsNullOrWhiteSpace(entry.Message) ? "" : ": " + entry.Message));
-                        break;
+                        return false;
                     }
 
                     type = type.ContainingType;
                 }
                 while (!(type is null));
+
+                return true;
+            }
+
+            bool VerifyTypeArguments(Action<Diagnostic> reportDiagnostic, ITypeSymbol type, SyntaxNode syntaxNode, out ITypeSymbol originalDefinition)
+            {
+                switch (type)
+                {
+                    case INamedTypeSymbol namedTypeSymbol:
+                        originalDefinition = namedTypeSymbol.ConstructedFrom;
+                        foreach (var typeArgument in namedTypeSymbol.TypeArguments)
+                        {
+                            if (typeArgument.TypeKind != TypeKind.TypeParameter &&
+                                typeArgument.TypeKind != TypeKind.Error &&
+                                !VerifyType(reportDiagnostic, typeArgument, syntaxNode))
+                            {
+                                return false;
+                            }
+                        }
+                        break;
+
+                    case IArrayTypeSymbol arrayTypeSymbol:
+                        originalDefinition = null;
+                        return VerifyType(reportDiagnostic, arrayTypeSymbol.ElementType, syntaxNode);
+
+                    case IPointerTypeSymbol pointerTypeSymbol:
+                        originalDefinition = null;
+                        return VerifyType(reportDiagnostic, pointerTypeSymbol.PointedAtType, syntaxNode);
+
+                    default:
+                        originalDefinition = type.OriginalDefinition;
+                        break;
+
+                }
+
+                return true;
             }
 
             void VerifySymbol(Action<Diagnostic> reportDiagnostic, ISymbol symbol, SyntaxNode syntaxNode)
