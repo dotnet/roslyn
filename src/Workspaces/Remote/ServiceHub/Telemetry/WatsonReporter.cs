3085171996769f208fd757d6a9ba2010866dbf3a
Heejae Chang <heejaechang@outlook.com>
2019-06-04 18:11:38 -0700

    mark NFW from OOP critical (#36156)
    * mark NFW from OOP critical
    
    currently, whenever OOP throws an exception, we show infobar saying "restart VS". whenever that happens, we report NFW. now those NFW will be marked as critical and we will stop reporting NFW after that in VS.
    
    basically making critical NFW behavior same as fatal watson in VS. and management will treat critical NFW same as fatal watson.
    
    also, now we will show callstack like code fix exception when info bar is shown
    
    * xml document fix
    
    * fix build break.
    
    * changed button to hyper-link to follow same pattern as other info bar (code fix)
    
    show remote exception in callstack view.
    
    * introduced enum for Watson Severity

--- src/Workspaces/Remote/ServiceHub/Telemetry/WatsonReporter.cs
+++ src/Workspaces/Remote/ServiceHub/Telemetry/WatsonReporter.cs
@@ -39,9 +39,10 @@ namespace Microsoft.CodeAnalysis.ErrorReporting
         /// Report Non-Fatal Watson
         /// </summary>
         /// <param name="exception">Exception that triggered this non-fatal error</param>
-        public static void Report(Exception exception)
+        /// <param name="severity">indicate <see cref="WatsonSeverity"/> of NFW</param>
+        public static void Report(Exception exception, WatsonSeverity severity = WatsonSeverity.Default)
         {
-            Report("Roslyn NonFatal Watson", exception);
+            Report("Roslyn NonFatal Watson", exception, severity);
         }
 
         /// <summary>
@@ -49,9 +50,10 @@ namespace Microsoft.CodeAnalysis.ErrorReporting
         /// </summary>
         /// <param name="description">any description you want to save with this watson report</param>
         /// <param name="exception">Exception that triggered this non-fatal error</param>
-        public static void Report(string description, Exception exception)
+        /// <param name="severity">indicate <see cref="WatsonSeverity"/> of NFW</param>
+        public static void Report(string description, Exception exception, WatsonSeverity severity = WatsonSeverity.Default)
         {
-            Report(description, exception, s_defaultCallback);
+            Report(description, exception, s_defaultCallback, severity);
         }
 
         /// <summary>
@@ -62,8 +64,10 @@ namespace Microsoft.CodeAnalysis.ErrorReporting
         /// <param name="callback">Callback to include extra data with the NFW. Note that we always collect
         /// a dump of the current process, but this can be used to add further information or files to the
         /// CAB.</param>
-        public static void Report(string description, Exception exception, Func<IFaultUtility, int> callback)
+        /// <param name="severity">indicate <see cref="WatsonSeverity"/> of NFW</param>
+        public static void Report(string description, Exception exception, Func<IFaultUtility, int> callback, WatsonSeverity severity = WatsonSeverity.Default)
         {
+            var critical = severity == WatsonSeverity.Critical;
             var emptyCallstack = exception.SetCallstackIfEmpty();
 
             // if given exception is non recoverable exception,
@@ -83,9 +87,18 @@ namespace Microsoft.CodeAnalysis.ErrorReporting
                 return;
             }
 
+            // in OOP, we don't fire Critical NFW, rather we fire General which is 1 level higher than Diagnostic
+            // and we keep fire NFW even after critical report. 
+            // critical NFW regarding OOP from VS side will let us to prioritize NFW to fix, and NFW from here should provide
+            // extra dump/info to take a look.
+            // whenever there is an exception in OOP, we fire NFW in both VS and OOP side. VS will report it as critical NFW
+            // and OOP will fire normal NFW. we only mark VS side critical since we don't want to double report same issue
+            // and don't want to shutdown NFW in OOP
+            // one can correlate NFW from VS and OOP through remote callstack in VS NFW
             var faultEvent = new FaultEvent(
                 eventName: FunctionId.NonFatalWatson.GetEventName(),
                 description: description,
+                critical ? FaultSeverity.General : FaultSeverity.Diagnostic,
                 exceptionObject: exception,
                 gatherEventDetails: arg =>
                 {
@@ -107,5 +120,18 @@ namespace Microsoft.CodeAnalysis.ErrorReporting
         {
             return exception is OutOfMemoryException;
         }
+    }
+
+    internal enum WatsonSeverity
+    {
+        /// <summary>
+        /// Indicate that this watson is informative and not urgent
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Indicate that this watson is critical and need to be addressed soon
+        /// </summary>
+        Critical,
     }
 }
