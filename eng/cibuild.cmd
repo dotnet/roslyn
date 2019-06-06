e1fa39034d64ef3e1de6d6c8d89813b77a5eb9d5
Nick Guerrera <nicholg@microsoft.com>
2019-06-06 13:29:05 -0700

    TEMP: Add tracing
    (This commit will be rebased away)

--- eng/cibuild.cmd
+++ eng/cibuild.cmd
@@ -1,2 +1,8 @@
 @echo off
+
+REM !!!!TEMPORARY DEBUGGING!!!
+setlocal
+set COREHOST_TRACE=1
+REM !!!!!!!!!!!!!!!!!!!!!!!!!!
+
 powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0build.ps1""" -ci -restore -build -bootstrap -pack -sign -publish -binaryLog %*"
