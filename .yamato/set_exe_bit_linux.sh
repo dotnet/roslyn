089473af2bf9e1d5a47bbc9ec07bdcbb3e2d23d8
ashwini <ashwini@unity3d.com>
2019-06-06 15:00:47 -0400

    Add Yamato configs

---
+++ .yamato/set_exe_bit_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod -v +x Artifacts/Builds/Binaries/Linux/csc
+ls -l Artifacts/Builds/Binaries/Linux/csc
+chmod -v +x Artifacts/Builds/Binaries/Linux/VBCSCompiler
+ls -l Artifacts/Builds/Binaries/Linux/VBCSCompiler
---
+++ .yamato/set_exe_bit_osx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod -v +x Artifacts/Builds/Binaries/Mac/csc
+ls -l Artifacts/Builds/Binaries/Mac/csc
+chmod -v +x Artifacts/Builds/Binaries/Mac/VBCSCompiler
+ls -l Artifacts/Builds/Binaries/Mac/VBCSCompiler
