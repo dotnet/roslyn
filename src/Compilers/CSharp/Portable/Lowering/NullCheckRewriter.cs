5c6a89679a938c789c2996481dd915863091ab3c
Lauren Rosenberg <t-laro@microsoft.com>
2019-06-17 15:30:39 -0700

    Added copyright header

--- src/Compilers/CSharp/Portable/Lowering/NullCheckRewriter.cs
+++ src/Compilers/CSharp/Portable/Lowering/NullCheckRewriter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+using System.Collections.Generic;
 using Microsoft.CodeAnalysis.CodeGen;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using System.Collections.Immutable;
