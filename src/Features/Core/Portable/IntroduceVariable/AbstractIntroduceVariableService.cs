11a801b81ee4add2d1065bbf5fe10632e7f7e273
Andrew Hall (METAL) <andrha@microsoft.com>
2019-06-17 15:24:45 -0700

    Move TypeInfoExtensions

--- src/Features/Core/Portable/IntroduceVariable/AbstractIntroduceVariableService.cs
+++ src/Features/Core/Portable/IntroduceVariable/AbstractIntroduceVariableService.cs
@@ -6,6 +6,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.Internal.Log;
