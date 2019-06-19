4e0751a95f30358ef6d55a616408fc2bb24c865c
Andy Gocke <angocke@microsoft.com>
2019-06-19 10:59:20 -0700

    Set scriptroot in tools.sh

--- eng/common/tools.sh
+++ eng/common/tools.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+source="${BASH_SOURCE[0]}"
+
+# resolve $source until the file is no longer a symlink
+while [[ -h "$source" ]]; do
+  scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
+  source="$(readlink "$source")"
+  # if $source was a relative symlink, we need to resolve it relative to the path where the
+  # symlink file was located
+  [[ $source != /* ]] && source="$scriptroot/$source"
+done
+scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
+
 # Initialize variables if they aren't already defined.
 
 # CI mode - set to true on CI server for PR validation build or official build.
