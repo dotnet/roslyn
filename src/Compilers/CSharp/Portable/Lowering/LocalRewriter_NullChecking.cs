cfc57a05703dc6073794e68427d49de672a944b4
Lauren Rosenberg <t-laro@microsoft.com>
2019-06-24 11:22:41 -0700

    Remove and sort usings

--- src/Compilers/CSharp/Portable/Lowering/LocalRewriter_NullChecking.cs
+++ src/Compilers/CSharp/Portable/Lowering/LocalRewriter_NullChecking.cs
@@ -1,11 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-using System.Collections.Generic;
-using Microsoft.CodeAnalysis.CodeGen;
-using Microsoft.CodeAnalysis.CSharp.Symbols;
-using System.Collections.Immutable;
-using Microsoft.CodeAnalysis.PooledObjects;
 using System.Linq;
-using System.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.PooledObjects;
 
 namespace Microsoft.CodeAnalysis.CSharp
 {
