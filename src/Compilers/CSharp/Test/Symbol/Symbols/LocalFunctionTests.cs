c437ee8ce8bb6b799fac0fd1a0151bf536a6fd37
yair halberstadt <yairhalberstadt@hotmail.co.uk>
2019-06-24 21:42:02 +0100

    Add tests for requires instance receiver.

--- src/Compilers/CSharp/Test/Symbol/Symbols/LocalFunctionTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/LocalFunctionTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Roslyn.Test.Utilities;
@@ -69,6 +70,31 @@ class C
             var localSyntax = tree.GetCompilationUnitRoot().DescendantNodes().OfType<LocalFunctionStatementSyntax>().Single();
             var local = semanticModel.GetDeclaredSymbol(localSyntax);
             Assert.False(local.IsStatic);
+        }
+
+        [Fact]
+        public void LocalFunctionDoesNotRequireInstanceReceiver()
+        {
+            var source = @"
+class C
+{
+    void M()
+    {
+        void local() {}
+        static void staticLocal() {}
+        local();
+        staticLocal();
+    }
+}";
+            var compilation = CreateCompilation(source).VerifyDiagnostics();
+            var tree = compilation.SyntaxTrees[0];
+            var semanticModel = compilation.GetSemanticModel(tree);
+
+            var localsSyntax = tree.GetCompilationUnitRoot().DescendantNodes().OfType<LocalFunctionStatementSyntax>().ToArray();
+            var local = (MethodSymbol)semanticModel.GetDeclaredSymbol(localsSyntax[0]);
+            Assert.False(local.RequiresInstanceReceiver);
+            var staticLocal = (MethodSymbol)semanticModel.GetDeclaredSymbol(localsSyntax[0]);
+            Assert.False(staticLocal.RequiresInstanceReceiver);
         }
     }
 }
