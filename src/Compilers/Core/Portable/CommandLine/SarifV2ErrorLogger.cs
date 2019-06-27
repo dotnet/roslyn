7e1a82f833193e4b17afaf9d3df38ad0d203a5ee
Larry Golding <larrygolding@comcast.net>
2019-06-27 15:41:21 -0700

    Move tool writing to the end so we can accumulate the rules.

--- src/Compilers/Core/Portable/CommandLine/SarifV2ErrorLogger.cs
+++ src/Compilers/Core/Portable/CommandLine/SarifV2ErrorLogger.cs
@@ -9,26 +9,23 @@ namespace Microsoft.CodeAnalysis
 {
     internal sealed class SarifV2ErrorLogger : StreamErrorLogger, IDisposable
     {
+
+        private readonly string _toolName;
+        private readonly string _toolFileVersion;
+        private readonly Version _toolAssemblyVersion;
+
         public SarifV2ErrorLogger(Stream stream, string toolName, string toolFileVersion, Version toolAssemblyVersion, CultureInfo culture)
             : base(stream)
         {
+            _toolName = toolName;
+            _toolFileVersion = toolFileVersion;
+            _toolAssemblyVersion = toolAssemblyVersion;
+
             _writer.WriteObjectStart(); // root
             _writer.Write("$schema", "http://json.schemastore.org/sarif-2.1.0");
             _writer.Write("version", "2.1.0");
             _writer.WriteArrayStart("runs");
             _writer.WriteObjectStart(); // run
-
-            _writer.WriteObjectStart("tool");
-            _writer.WriteObjectStart("driver");
-            _writer.Write("name", toolName);
-            _writer.Write("fileVersion", toolFileVersion);
-            _writer.Write("version", toolAssemblyVersion.ToString());
-            _writer.Write("semanticVersion", toolAssemblyVersion.ToString(fieldCount: 3));
-
-            WriteRules();
-
-            _writer.WriteObjectEnd(); // driver
-            _writer.WriteObjectEnd(); // tool
 
             _writer.WriteArrayStart("results");
         }
@@ -44,10 +41,28 @@ namespace Microsoft.CodeAnalysis
         public override void Dispose()
         {
             _writer.WriteArrayEnd(); //results
+
+            WriteTool();
+
             _writer.WriteObjectEnd(); // run
             _writer.WriteArrayEnd();  // runs
             _writer.WriteObjectEnd(); // root
             base.Dispose();
+        }
+
+        private void WriteTool()
+        {
+            _writer.WriteObjectStart("tool");
+            _writer.WriteObjectStart("driver");
+            _writer.Write("name", _toolName);
+            _writer.Write("fileVersion", _toolFileVersion);
+            _writer.Write("version", _toolAssemblyVersion.ToString());
+            _writer.Write("semanticVersion", _toolAssemblyVersion.ToString(fieldCount: 3));
+
+            WriteRules();
+
+            _writer.WriteObjectEnd(); // driver
+            _writer.WriteObjectEnd(); // tool
         }
 
         private void WriteRules()
