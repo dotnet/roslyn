0e6b7939fa87d401725b79766d6a45068a458a32
Larry Golding <larrygolding@comcast.net>
2019-06-27 15:35:43 -0700

    Produce SARIF v2 skeleton.

--- src/Compilers/Core/Portable/CommandLine/SarifV2ErrorLogger.cs
+++ src/Compilers/Core/Portable/CommandLine/SarifV2ErrorLogger.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections;
 using System.Globalization;
 using System.IO;
 
@@ -11,15 +12,48 @@ namespace Microsoft.CodeAnalysis
         public SarifV2ErrorLogger(Stream stream, string toolName, string toolFileVersion, Version toolAssemblyVersion, CultureInfo culture)
             : base(stream)
         {
+            _writer.WriteObjectStart(); // root
+            _writer.Write("$schema", "http://json.schemastore.org/sarif-2.1.0");
+            _writer.Write("version", "2.1.0");
+            _writer.WriteArrayStart("runs");
+            _writer.WriteObjectStart(); // run
+
+            _writer.WriteObjectStart("tool");
+            _writer.WriteObjectStart("driver");
+            _writer.Write("name", toolName);
+            _writer.Write("fileVersion", toolFileVersion);
+            _writer.Write("version", toolAssemblyVersion.ToString());
+            _writer.Write("semanticVersion", toolAssemblyVersion.ToString(fieldCount: 3));
+
+            WriteRules();
+
+            _writer.WriteObjectEnd(); // driver
+            _writer.WriteObjectEnd(); // tool
+
+            _writer.WriteArrayStart("results");
         }
 
         public override void LogDiagnostic(Diagnostic diagnostic)
         {
+            _writer.WriteObjectStart(); // result
+            _writer.Write("ruleId", diagnostic.Id);
+
+            _writer.WriteObjectEnd(); // result
         }
 
         public override void Dispose()
         {
+            _writer.WriteArrayEnd(); //results
+            _writer.WriteObjectEnd(); // run
+            _writer.WriteArrayEnd();  // runs
+            _writer.WriteObjectEnd(); // root
             base.Dispose();
+        }
+
+        private void WriteRules()
+        {
+            _writer.WriteArrayStart("rules");
+            _writer.WriteArrayEnd(); // rules
         }
     }
 }
