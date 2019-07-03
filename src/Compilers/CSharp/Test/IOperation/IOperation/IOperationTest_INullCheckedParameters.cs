16b62fc6326355455138cf75f12becc39c9c1c98
Lauren Rosenberg <t-laro@microsoft.com>
2019-07-03 14:40:27 -0700

    Added test file

---
+++ src/Compilers/CSharp/Test/IOperation/IOperation/IOperationTest_INullCheckedParameters.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp.UnitTests;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests
+{
+    public partial class IOperationTests : SemanticModelTestBase
+    {
+        [CompilerTrait(CompilerFeature.IOperation, CompilerFeature.Dataflow)]
+        [Fact]
+        public void NullCheckedMethodDeclarationIOp()
+        {
+            var source = @"
+public class C
+{
+    public void M(string input!) { }
+}";
+            var compilation = CreateCompilation(source);
+
+            compilation.VerifyDiagnostics();
+
+            var tree = compilation.SyntaxTrees.Single();
+            var node1 = tree.GetRoot().DescendantNodes().OfType<BaseMethodDeclarationSyntax>().Single();
+
+            compilation.VerifyOperationTree(node1, expectedOperationTree: @"
+IMethodBodyOperation (OperationKind.MethodBody, Type: null) (Syntax: 'public void ...  input) { }')
+    BlockBody: 
+    IBlockOperation (0 statements) (OperationKind.Block, Type: null) (Syntax: '{ }')
+    ExpressionBody: 
+    null");
+
+            VerifyFlowGraph(compilation, node1, expectedFlowGraph: @"
+Block[B0] - Entry
+    Statements (0)
+    Next (Regular) Block[B1]
+Block[B1] - Exit
+    Predecessors: [B0]
+    Statements (0)");
+        }
+    }
+}
