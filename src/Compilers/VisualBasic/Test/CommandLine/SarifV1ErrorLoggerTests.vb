9618cebe83be5170ea8ea7e983d295b1edccd566
Larry Golding <larrygolding@comcast.net>
2019-07-06 13:55:33 -0700

    DRY out VB command line tests.

--- src/Compilers/VisualBasic/Test/CommandLine/SarifV1ErrorLoggerTests.vb
+++ src/Compilers/VisualBasic/Test/CommandLine/SarifV1ErrorLoggerTests.vb
@@ -1,6 +1,5 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-Imports System.Globalization
 Imports System.IO
 Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Microsoft.CodeAnalysis.VisualBasic.UnitTests
@@ -8,7 +7,6 @@ Imports Xunit
 
 Imports Microsoft.CodeAnalysis.CommonDiagnosticAnalyzers
 Imports Microsoft.CodeAnalysis.DiagnosticExtensions
-Imports Roslyn.Test.Utilities.SharedResourceHelpers
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.CommandLine.UnitTests
 
@@ -16,34 +14,14 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.CommandLine.UnitTests
     Public Class SarifV1ErrorLoggerTests
         Inherits SarifErrorLoggerTests
 
-        Private ReadOnly _baseDirectory As String = TempRoot.Root
+        Protected Readonly Overrides Property VersionSpecificArguments() As String()
+            Get
+                Return {}
+            End Get
+        End Property
 
-        <Fact>
-        Public Sub NoDiagnostics()
-            Dim helloWorldVB As String = <text>
-Imports System
-Class C
-    Shared Sub Main(args As String())
-        Console.WriteLine("Hello, world")
-    End Sub
-End Class
-</text>.Value
-
-            Dim hello = Temp.CreateFile().WriteAllText(helloWorldVB).Path
-            Dim errorLogDir = Temp.CreateDirectory()
-            Dim errorLogFile = Path.Combine(errorLogDir.Path, "ErrorLog.txt")
-
-            Dim cmd = New MockVisualBasicCompiler(Nothing, _baseDirectory,
-                {"/nologo",
-                 $"/errorlog:{errorLogFile}",
-                 hello})
-            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
-
-            Dim exitCode = cmd.Run(outWriter, Nothing)
-            Assert.Equal("", outWriter.ToString().Trim())
-            Assert.Equal(0, exitCode)
-
-            Dim actualOutput = File.ReadAllText(errorLogFile).Trim()
+        Friend Overrides Function GetExpectedOutputForNoDiagnostics(
+            cmd As CommonCompiler) As String
 
             Dim expectedHeader = GetExpectedErrorLogHeader(cmd)
             Dim expectedIssues = "
@@ -53,42 +31,17 @@ End Class
   ]
 }"
 
-            Dim expectedText = expectedHeader + expectedIssues
-            Assert.Equal(expectedText, actualOutput)
-
-            CleanupAllGeneratedFiles(hello)
-            CleanupAllGeneratedFiles(errorLogFile)
-        End Sub
+            Return expectedHeader + expectedIssues
+        End Function
 
         <Fact>
-        Public Sub SimpleCompilerDiagnostics()
-            Dim source As String = <text>
-Public Class C
-    Public Sub Method()
-        Dim x As Integer
-    End Sub
-End Class
-</text>.Value
+        Public Sub NoDiagnostics()
+            NoDiagnosticsImpl()
+        End Sub
 
-            Dim sourceFilePath = Temp.CreateFile().WriteAllText(source).Path
-            Dim errorLogDir = Temp.CreateDirectory()
-            Dim errorLogFile = Path.Combine(errorLogDir.Path, "ErrorLog.txt")
-
-            Dim cmd = New MockVisualBasicCompiler(Nothing, _baseDirectory,
-                {"/nologo",
-                "/preferreduilang:en",
-                 $"/errorlog:{errorLogFile}",
-                 sourceFilePath})
-            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
-
-            Dim exitCode = cmd.Run(outWriter, Nothing)
-            Dim actualConsoleOutput = outWriter.ToString().Trim()
-
-            Assert.Contains("BC42024", actualConsoleOutput)
-            Assert.Contains("BC30420", actualConsoleOutput)
-            Assert.NotEqual(0, exitCode)
-
-            Dim actualOutput = File.ReadAllText(errorLogFile).Trim()
+        Friend Overrides Function GetExpectedOutputForSimpleCompilerDiagnostics(
+            cmd As CommonCompiler,
+            sourceFilePath As String) As String
 
             Dim expectedHeader = GetExpectedErrorLogHeader(cmd)
             Dim expectedIssues = String.Format("
@@ -152,45 +105,17 @@ End Class
   ]
 }}", AnalyzerForErrorLogTest.GetUriForPath(sourceFilePath), Path.GetFileNameWithoutExtension(sourceFilePath))
 
-            Dim expectedText = expectedHeader + expectedIssues
-            Assert.Equal(expectedText, actualOutput)
-
-            CleanupAllGeneratedFiles(sourceFilePath)
-            CleanupAllGeneratedFiles(errorLogFile)
-        End Sub
+            Return expectedHeader + expectedIssues
+        End Function
 
         <Fact>
-        Public Sub SimpleCompilerDiagnostics_Suppressed()
-            Dim source As String = <text>
-Public Class C
-    Public Sub Method()
-#Disable Warning BC42024
-        Dim x As Integer
-#Enable Warning BC42024
-    End Sub
-End Class
-</text>.Value
+        Public Sub SimpleCompilerDiagnostics()
+            SimpleCompilerDiagnosticsImpl()
+        End Sub
 
-            Dim sourceFilePath = Temp.CreateFile().WriteAllText(source).Path
-            Dim errorLogDir = Temp.CreateDirectory()
-            Dim errorLogFile = Path.Combine(errorLogDir.Path, "ErrorLog.txt")
-
-            Dim cmd = New MockVisualBasicCompiler(Nothing, _baseDirectory,
-                {"/nologo",
-                "/preferreduilang:en",
-                 $"/errorlog:{errorLogFile}",
-                 sourceFilePath})
-            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
-
-            Dim exitCode = cmd.Run(outWriter, Nothing)
-            Dim actualConsoleOutput = outWriter.ToString().Trim()
-
-            ' Suppressed diagnostics are only report in the error log, not the console output.
-            Assert.DoesNotContain("BC42024", actualConsoleOutput)
-            Assert.Contains("BC30420", actualConsoleOutput)
-            Assert.NotEqual(0, exitCode)
-
-            Dim actualOutput = File.ReadAllText(errorLogFile).Trim()
+        Friend Overrides Function GetExpectedOutputForSimpleCompilerDiagnosticsSuppressed(
+            cmd As CommonCompiler,
+            sourceFilePath As String) As String
 
             Dim expectedHeader = GetExpectedErrorLogHeader(cmd)
             Dim expectedIssues = String.Format("
@@ -257,54 +182,26 @@ End Class
   ]
 }}", AnalyzerForErrorLogTest.GetUriForPath(sourceFilePath), Path.GetFileNameWithoutExtension(sourceFilePath))
 
-            Dim expectedText = expectedHeader + expectedIssues
-            Assert.Equal(expectedText, actualOutput)
-
-            CleanupAllGeneratedFiles(sourceFilePath)
-            CleanupAllGeneratedFiles(errorLogFile)
-        End Sub
+            Return expectedHeader + expectedIssues
+        End Function
 
         <Fact>
-        Public Sub AnalyzerDiagnosticsWithAndWithoutLocation()
-            Dim source As String = <text>
-Imports System
-Class C
-End Class
-</text>.Value
+        Public Sub SimpleCompilerDiagnosticsSuppressed()
+            SimpleCompilerDiagnosticsSuppressedImpl()
+        End Sub
 
-            Dim sourceFilePath = Temp.CreateFile().WriteAllText(source).Path
-            Dim outputDir = Temp.CreateDirectory()
-            Dim errorLogFile = Path.Combine(outputDir.Path, "ErrorLog.txt")
-            Dim outputFilePath = Path.Combine(outputDir.Path, "test.dll")
-
-            Dim cmd = New MockVisualBasicCompiler(Nothing, _baseDirectory,
-                {"/nologo",
-                 "/preferreduilang:en",
-                 "/t:library",
-                 $"/out:{outputFilePath}",
-                 $"/errorlog:{errorLogFile}",
-                 sourceFilePath},
-                analyzer:=New AnalyzerForErrorLogTest())
-            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
-
-            Dim exitCode = cmd.Run(outWriter, Nothing)
-            Dim actualConsoleOutput = outWriter.ToString().Trim()
-
-            Assert.Contains(AnalyzerForErrorLogTest.Descriptor1.Id, actualConsoleOutput)
-            Assert.Contains(AnalyzerForErrorLogTest.Descriptor2.Id, actualConsoleOutput)
-            Assert.NotEqual(0, exitCode)
-
-            Dim actualOutput = File.ReadAllText(errorLogFile).Trim()
+        Friend Overrides Function GetExpectedOutputForAnalyzerDiagnosticsWithAndWithoutLocation(
+            cmd As MockVisualBasicCompiler) As String
 
             Dim expectedHeader = GetExpectedErrorLogHeader(cmd)
             Dim expectedIssues = AnalyzerForErrorLogTest.GetExpectedV1ErrorLogResultsAndRulesText(cmd.Compilation)
 
-            Dim expectedText = expectedHeader + expectedIssues
-            Assert.Equal(expectedText, actualOutput)
+            Return expectedHeader + expectedIssues
+        End Function
 
-            CleanupAllGeneratedFiles(sourceFilePath)
-            CleanupAllGeneratedFiles(outputFilePath)
-            CleanupAllGeneratedFiles(errorLogFile)
+        <Fact>
+        Public Sub AnalyzerDiagnosticsWithAndWithoutLocation()
+            AnalyzerDiagnosticsWithAndWithoutLocationImpl()
         End Sub
     End Class
 End Namespace
--- src/Compilers/VisualBasic/Test/CommandLine/SarifV2ErrorLoggerTests.vb
+++ src/Compilers/VisualBasic/Test/CommandLine/SarifV2ErrorLoggerTests.vb
@@ -1,16 +1,12 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-Imports System.Collections.Generic
 Imports System.Globalization
 Imports System.IO
 Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Microsoft.CodeAnalysis.VisualBasic.UnitTests
 Imports Xunit
-Imports Xunit.Abstractions
 
 Imports Microsoft.CodeAnalysis.CommonDiagnosticAnalyzers
-Imports Microsoft.CodeAnalysis.DiagnosticExtensions
-Imports Roslyn.Test.Utilities.SharedResourceHelpers
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.CommandLine.UnitTests
 
@@ -18,42 +14,18 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.CommandLine.UnitTests
     Public Class SarifV2ErrorLoggerTests
         Inherits SarifErrorLoggerTests
 
-        Private ReadOnly _baseDirectory As String = TempRoot.Root
-        Private Readonly _output As ITestOutputHelper
+        Protected Readonly Overrides Property VersionSpecificArguments() As String()
+            Get
+                Return {
+                    "/sarifversion:2"
+                }
+            End Get
+        End Property
 
-        Public Sub New(output As ITestOutputHelper)
-            Me._output = output
-        End Sub
+        Friend Overrides Function GetExpectedOutputForNoDiagnostics(
+            cmd As CommonCompiler) As String
 
-        <Fact>
-        Public Sub NoDiagnostics()
-            Dim helloWorldVB As String = <text>
-Imports System
-Class C
-    Shared Sub Main(args As String())
-        Console.WriteLine("Hello, world")
-    End Sub
-End Class
-</text>.Value
-
-            Dim hello = Temp.CreateFile().WriteAllText(helloWorldVB).Path
-            Dim errorLogDir = Temp.CreateDirectory()
-            Dim errorLogFile = Path.Combine(errorLogDir.Path, "ErrorLog.txt")
-
-            Dim cmd = New MockVisualBasicCompiler(Nothing, _baseDirectory,
-                {"/nologo",
-                 $"/errorlog:{errorLogFile}",
-                 "/sarifversion:2",
-                 hello})
-            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
-
-            Dim exitCode = cmd.Run(outWriter, Nothing)
-            Assert.Equal("", outWriter.ToString().Trim())
-            Assert.Equal(0, exitCode)
-
-            Dim actualOutput = File.ReadAllText(errorLogFile).Trim()
-
-            Dim expectedOutput As String = "{{
+            Dim expectedOutput = "{{
   ""$schema"": ""http://json.schemastore.org/sarif-2.1.0"",
   ""version"": ""2.1.0"",
   ""runs"": [
@@ -74,43 +46,17 @@ End Class
   ]
 }}"
 
-            expectedOutput = FormatOutputText(expectedOutput, cmd)
-            Assert.Equal(expectedOutput, actualOutput)
-
-            CleanupAllGeneratedFiles(hello)
-            CleanupAllGeneratedFiles(errorLogFile)
-        End Sub
+            Return FormatOutputText(expectedOutput, cmd)
+        End Function
 
         <Fact>
-        Public Sub SimpleCompilerDiagnostics()
-            Dim source As String = <text>
-Public Class C
-    Public Sub Method()
-        Dim x As Integer
-    End Sub
-End Class
-</text>.Value
+        Public Sub NoDiagnostics()
+            NoDiagnosticsImpl()
+        End Sub
 
-            Dim sourceFilePath = Temp.CreateFile().WriteAllText(source).Path
-            Dim errorLogDir = Temp.CreateDirectory()
-            Dim errorLogFile = Path.Combine(errorLogDir.Path, "ErrorLog.txt")
-
-            Dim cmd = New MockVisualBasicCompiler(Nothing, _baseDirectory,
-                {"/nologo",
-                "/preferreduilang:en",
-                 $"/errorlog:{errorLogFile}",
-                 "/sarifversion:2",
-                 sourceFilePath})
-            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
-
-            Dim exitCode = cmd.Run(outWriter, Nothing)
-            Dim actualConsoleOutput = outWriter.ToString().Trim()
-
-            Assert.Contains("BC42024", actualConsoleOutput)
-            Assert.Contains("BC30420", actualConsoleOutput)
-            Assert.NotEqual(0, exitCode)
-
-            Dim actualOutput = File.ReadAllText(errorLogFile).Trim()
+        Friend Overrides Function GetExpectedOutputForSimpleCompilerDiagnostics(
+            cmd As CommonCompiler,
+            sourceFilePath As String) As String
 
             Dim expectedOutput = "{{
   ""$schema"": ""http://json.schemastore.org/sarif-2.1.0"",
@@ -196,51 +142,21 @@ End Class
   ]
 }}"
 
-            expectedOutput = FormatOutputText(
+            Return FormatOutputText(
                 expectedOutput,
                 cmd,
                 AnalyzerForErrorLogTest.GetUriForPath(sourceFilePath),
                 Path.GetFileNameWithoutExtension(sourceFilePath))
-
-            Assert.Equal(expectedOutput, actualOutput)
-
-            CleanupAllGeneratedFiles(sourceFilePath)
-            CleanupAllGeneratedFiles(errorLogFile)
-        End Sub
+        End Function
 
         <Fact>
-        Public Sub SimpleCompilerDiagnostics_Suppressed()
-            Dim source As String = <text>
-Public Class C
-    Public Sub Method()
-#Disable Warning BC42024
-        Dim x As Integer
-#Enable Warning BC42024
-    End Sub
-End Class
-</text>.Value
+        Public Sub SimpleCompilerDiagnostics()
+            SimpleCompilerDiagnosticsImpl()
+        End Sub
 
-            Dim sourceFilePath = Temp.CreateFile().WriteAllText(source).Path
-            Dim errorLogDir = Temp.CreateDirectory()
-            Dim errorLogFile = Path.Combine(errorLogDir.Path, "ErrorLog.txt")
-
-            Dim cmd = New MockVisualBasicCompiler(Nothing, _baseDirectory,
-                {"/nologo",
-                "/preferreduilang:en",
-                 $"/errorlog:{errorLogFile}",
-                 "/sarifversion:2",
-                 sourceFilePath})
-            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
-
-            Dim exitCode = cmd.Run(outWriter, Nothing)
-            Dim actualConsoleOutput = outWriter.ToString().Trim()
-
-            ' Suppressed diagnostics are only report in the error log, not the console output.
-            Assert.DoesNotContain("BC42024", actualConsoleOutput)
-            Assert.Contains("BC30420", actualConsoleOutput)
-            Assert.NotEqual(0, exitCode)
-
-            Dim actualOutput = File.ReadAllText(errorLogFile).Trim()
+        Friend Overrides Function GetExpectedOutputForSimpleCompilerDiagnosticsSuppressed(
+            cmd As CommonCompiler,
+            sourceFilePath As String) As String
 
             Dim expectedOutput = "{{
   ""$schema"": ""http://json.schemastore.org/sarif-2.1.0"",
@@ -331,50 +247,20 @@ End Class
   ]
 }}"
 
-            expectedOutput = FormatOutputText(
+            Return FormatOutputText(
                 expectedOutput,
                 cmd,
                 AnalyzerForErrorLogTest.GetUriForPath(sourceFilePath),
                 Path.GetFileNameWithoutExtension(sourceFilePath))
-
-            Assert.Equal(expectedOutput, actualOutput)
-
-            CleanupAllGeneratedFiles(sourceFilePath)
-            CleanupAllGeneratedFiles(errorLogFile)
-        End Sub
+        End Function
 
         <Fact>
-        Public Sub AnalyzerDiagnosticsWithAndWithoutLocation()
-            Dim source As String = <text>
-Imports System
-Class C
-End Class
-</text>.Value
+        Public Sub SimpleCompilerDiagnosticsSuppressed()
+            SimpleCompilerDiagnosticsSuppressedImpl()
+        End Sub
 
-            Dim sourceFilePath = Temp.CreateFile().WriteAllText(source).Path
-            Dim outputDir = Temp.CreateDirectory()
-            Dim errorLogFile = Path.Combine(outputDir.Path, "ErrorLog.txt")
-            Dim outputFilePath = Path.Combine(outputDir.Path, "test.dll")
-
-            Dim cmd = New MockVisualBasicCompiler(Nothing, _baseDirectory,
-                {"/nologo",
-                 "/preferreduilang:en",
-                 "/t:library",
-                 $"/out:{outputFilePath}",
-                 $"/errorlog:{errorLogFile}",
-                 "/sarifversion:2",
-                 sourceFilePath},
-                analyzer:=New AnalyzerForErrorLogTest())
-            Dim outWriter = New StringWriter(CultureInfo.InvariantCulture)
-
-            Dim exitCode = cmd.Run(outWriter, Nothing)
-            Dim actualConsoleOutput = outWriter.ToString().Trim()
-
-            Assert.Contains(AnalyzerForErrorLogTest.Descriptor1.Id, actualConsoleOutput)
-            Assert.Contains(AnalyzerForErrorLogTest.Descriptor2.Id, actualConsoleOutput)
-            Assert.NotEqual(0, exitCode)
-
-            Dim actualOutput = File.ReadAllText(errorLogFile).Trim()
+        Friend Overrides Function GetExpectedOutputForAnalyzerDiagnosticsWithAndWithoutLocation(
+            cmd As MockVisualBasicCompiler) As String
 
             Dim expectedOutput =
 "{{
@@ -397,17 +283,16 @@ End Class
     }}
   ]
 }}"
-            expectedOutput = FormatOutputText(
+            Return FormatOutputText(
                 expectedOutput,
                 cmd,
                 AnalyzerForErrorLogTest.GetExpectedV2ErrorLogResultsText(cmd.Compilation),
                 AnalyzerForErrorLogTest.GetExpectedV2ErrorLogRulesText())
+        End Function
 
-            Assert.Equal(expectedOutput, actualOutput)
-
-            CleanupAllGeneratedFiles(sourceFilePath)
-            CleanupAllGeneratedFiles(outputFilePath)
-            CleanupAllGeneratedFiles(errorLogFile)
+        <Fact>
+        Public Sub AnalyzerDiagnosticsWithAndWithoutLocation()
+            AnalyzerDiagnosticsWithAndWithoutLocationImpl()
         End Sub
 
         Private Function FormatOutputText(s As String, compiler as CommonCompiler, ParamArray additionalArguments() As Object) As String
