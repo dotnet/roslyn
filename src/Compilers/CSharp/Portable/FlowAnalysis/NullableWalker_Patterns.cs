70ee1c9b8e1db1ba3e2c77ddafc8077bcde02a06
Neal Gafter <neal@gafter.com>
2019-07-08 12:47:16 -0700

    Additional tests for target-typed switch.

--- src/Compilers/CSharp/Portable/FlowAnalysis/NullableWalker_Patterns.cs
+++ src/Compilers/CSharp/Portable/FlowAnalysis/NullableWalker_Patterns.cs
@@ -478,6 +478,12 @@ namespace Microsoft.CodeAnalysis.CSharp
             return null;
         }
 
+        public override BoundNode VisitUnconvertedSwitchExpression(BoundUnconvertedSwitchExpression node)
+        {
+            VisitSwitchExpressionCore(node, inferType: true);
+            return null;
+        }
+
         private void VisitSwitchExpressionCore(BoundSwitchExpression node, bool inferType)
         {
             // first, learn from any null tests in the patterns
