2c1bf30eae05fcba4ca8017db41187f2defae3ca
Petr Houska <t-pehouk@microsoft.com>
2019-07-10 17:19:39 -0700

    Fix test after changing method header logic.

--- src/EditorFeatures/CSharpTest/PullMemberUp/CSharpPullMemberUpTests.cs
+++ src/EditorFeatures/CSharpTest/PullMemberUp/CSharpPullMemberUpTests.cs
@@ -2270,25 +2270,29 @@ namespace PushUpTest
         #region Selections and caret position
         [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsPullMemberUp)]
         [WorkItem(35180, "https://github.com/dotnet/roslyn/issues/35180")]
-        public async Task TestNoRefactoringCaretInArgs()
+        public async Task TestArgsIsPartOfHeader()
         {
-            var input = @"
+            var testText = @"
+using System;
+
 namespace PushUpTest
 {
+    class TestAttribute : Attribute { }
+    class Test2Attribute : Attribute { }
     public class A
-    { 
+    {
     }
 
     public class B : A
     {
+        [Test]
+        [Test2]
         void C([||])
         {
-
         }
     }
 }";
-
-            await TestQuickActionNotProvidedAsync(input);
+            await TestQuickActionNotProvidedAsync(testText);
         }
 
         [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsPullMemberUp)]
