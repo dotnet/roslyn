1d16b8e69a84acde14d9f93c9eb93486a5c5fbd9
Manish Vasani <mavasani@microsoft.com>
2019-07-12 10:02:37 -0700

    Improve handling of async methods and returns values in DFA
    We now track tasks wrapping values, so that awaiting a task from an async method correctly translates to the abstract value wrapped in the task. Additionally, we only track values from tasks that have not escaped via assigning to a field/property.
    
    Fixes https://github.com/dotnet/roslyn/issues/36643 and https://github.com/dotnet/roslyn/issues/37065

--- src/Utilities/FlowAnalysis/FlowAnalysis/Framework/DataFlow/DataFlowAnalysisResultBuilder.cs
+++ src/Utilities/FlowAnalysis/FlowAnalysis/Framework/DataFlow/DataFlowAnalysisResultBuilder.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using Analyzer.Utilities.Extensions;
 using Analyzer.Utilities.PooledObjects;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow.PointsToAnalysis;
 
 namespace Microsoft.CodeAnalysis.FlowAnalysis.DataFlow
 {
@@ -47,6 +48,7 @@ namespace Microsoft.CodeAnalysis.FlowAnalysis.DataFlow
             TAnalysisData exceptionPathsExitBlockDataOpt,
             TAnalysisData mergedDataForUnhandledThrowOperationsOpt,
             Dictionary<ThrownExceptionInfo, TAnalysisData> analysisDataForUnhandledThrowOperationsOpt,
+            Dictionary<PointsToAbstractValue, TAbstractAnalysisValue> taskWrappedValuesMapOpt,
             ControlFlowGraph cfg,
             TAbstractAnalysisValue defaultUnknownValue)
             where TBlockAnalysisResult : AbstractBlockAnalysisResult
@@ -73,7 +75,8 @@ namespace Microsoft.CodeAnalysis.FlowAnalysis.DataFlow
             return new DataFlowAnalysisResult<TBlockAnalysisResult, TAbstractAnalysisValue>(resultBuilder.ToImmutableDictionaryAndFree(), stateMap,
                 predicateValueKindMap, returnValueAndPredicateKindOpt, interproceduralResultsMap,
                 entryBlockOutputResult, exitBlockOutputResult, exceptionPathsExitBlockOutputResultOpt,
-                mergedStateForUnhandledThrowOperationsOpt, analysisDataForUnhandledThrowOperationsOpt, cfg, defaultUnknownValue);
+                mergedStateForUnhandledThrowOperationsOpt, analysisDataForUnhandledThrowOperationsOpt,
+                taskWrappedValuesMapOpt, cfg, defaultUnknownValue);
         }
 
         public void Dispose()
