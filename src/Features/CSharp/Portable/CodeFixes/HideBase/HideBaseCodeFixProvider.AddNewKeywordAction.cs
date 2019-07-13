dbc31f87819c743307ea2521b15575495edc599d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-13 15:34:52 -0700

    Remove unnecessary parameters

--- src/Features/CSharp/Portable/CodeFixes/HideBase/HideBaseCodeFixProvider.AddNewKeywordAction.cs
+++ src/Features/CSharp/Portable/CodeFixes/HideBase/HideBaseCodeFixProvider.AddNewKeywordAction.cs
@@ -25,13 +25,13 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.HideBase
             {
                 var root = await _document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
 
-                var newNode = GetNewNode(_document, _node, cancellationToken);
+                var newNode = GetNewNode(_node);
                 var newRoot = root.ReplaceNode(_node, newNode);
 
                 return _document.WithSyntaxRoot(newRoot);
             }
 
-            private SyntaxNode GetNewNode(Document document, SyntaxNode node, CancellationToken cancellationToken)
+            private SyntaxNode GetNewNode(SyntaxNode node)
             {
                 var generator = SyntaxGenerator.GetGenerator(_document);
                 return generator.WithModifiers(node, generator.GetModifiers(node).WithIsNew(true));
