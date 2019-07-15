82a709e72397efad6c6c1afef3be66f15bf97db9
Tomáš Matoušek <tmat@users.noreply.github.com>
2019-07-15 16:55:57 -0700

    EnC support for DIM (#37115)
    * Emit tests
    
    * Report rude edits for unsupported DIM operations
    
    * Allow EnC tests to specify TFMs to build against
    
    * EnC: test against netcoreapp30
    
    * Test fixes
    
    * Fix merge

--- src/EditorFeatures/CSharpTest/EditAndContinue/StatementEditingTests.cs
+++ src/EditorFeatures/CSharpTest/EditAndContinue/StatementEditingTests.cs
@@ -4741,7 +4741,7 @@ class Program
         }
 
         [Fact]
-        public void LocalFunctions_InLocalFunction_NoChangeInignature()
+        public void LocalFunctions_InLocalFunction_NoChangeInSignature()
         {
             var src1 = "int x() { int y(int a) => a; return y(b); }";
             var src2 = "int x() { int y() => c; return y(); }";
@@ -4752,7 +4752,7 @@ class Program
         }
 
         [Fact]
-        public void LocalFunctions_InLocalFunction_ChangeInignature()
+        public void LocalFunctions_InLocalFunction_ChangeInSignature()
         {
             var src1 = "int x() { int y(int a) => a; return y(b); }";
             var src2 = "int x(int z) { int y() => c; return y(); }";
@@ -6767,6 +6767,55 @@ class Program
             edits.VerifyRudeDiagnostics();
         }
 
+        [WorkItem(37128, "https://github.com/dotnet/roslyn/issues/37128")]
+        [Fact]
+        public void LocalFunction_AddToInterfaceMethod()
+        {
+            var src1 = @"
+using System;
+interface I
+{
+    static int X = M(() => 1);
+    static int M() => 1;
+
+    static void F()
+    {
+        void g() { }
+    }
+}
+";
+            var src2 = @"
+using System;
+interface I
+{
+    static int X = M(() => { void f3() {} return 2; });
+    static int M() => 1;
+
+    static void F()
+    {
+        int f1() => 1;
+        f1();
+
+        void g() { void f2() {} f2(); }
+
+        var l = new Func<int>(() => 1);
+        l();
+    }
+}
+";
+            var edits = GetTopEdits(src1, src2);
+
+            // lambdas are ok as they are emitted to a nested type
+            edits.VerifySemanticDiagnostics(
+                targetFrameworks: new[] { TargetFramework.NetCoreApp30 },
+                expectedDiagnostics: new[]
+                {
+                    Diagnostic(RudeEditKind.InsertLocalFunctionIntoInterfaceMethod, "f1"),
+                    Diagnostic(RudeEditKind.InsertLocalFunctionIntoInterfaceMethod, "f2"),
+                    Diagnostic(RudeEditKind.InsertLocalFunctionIntoInterfaceMethod, "f3")
+                });
+        }
+
         #endregion
 
         #region Queries
@@ -8676,13 +8725,8 @@ class C
 ";
             var edits = GetTopEdits(src1, src2);
 
-            CSharpEditAndContinueTestHelpers.InstanceMinAsync.VerifySemantics(
-                editScript: edits,
-                activeStatements: ActiveStatementsDescription.Empty,
-                additionalNewSources: null,
-                additionalOldSources: null,
-                expectedSemanticEdits: null,
-                expectedDeclarationError: null,
+            edits.VerifySemantics(
+                targetFrameworks: new[] { TargetFramework.MinimalAsync },
                 expectedDiagnostics: new[]
                 {
                     Diagnostic(RudeEditKind.UpdatingStateMachineMethodMissingAttribute, "static async Task<int> F()", "System.Runtime.CompilerServices.AsyncStateMachineAttribute")
@@ -8717,13 +8761,7 @@ class C
 ";
             var edits = GetTopEdits(src1, src2);
 
-            CSharpEditAndContinueTestHelpers.InstanceMinAsync.VerifySemantics(
-                edits,
-                ActiveStatementsDescription.Empty,
-                null,
-                null,
-                null,
-                null);
+            edits.VerifySemantics(targetFrameworks: new[] { TargetFramework.MinimalAsync });
         }
 
         [Fact]
--- src/Features/Core/Portable/EditAndContinue/RudeEditDiagnosticDescriptors.cs
+++ src/Features/Core/Portable/EditAndContinue/RudeEditDiagnosticDescriptors.cs
@@ -81,6 +81,8 @@ namespace Microsoft.CodeAnalysis.EditAndContinue
             { GetDescriptorPair(RudeEditKind.UpdatingStateMachineMethodMissingAttribute, FeaturesResources.Attribute_0_is_missing_Updating_an_async_method_or_an_iterator_will_prevent_the_debug_session_from_continuing) },
             { GetDescriptorPair(RudeEditKind.SwitchBetweenLambdaAndLocalFunction, FeaturesResources.Switching_between_lambda_and_local_function_will_prevent_the_debug_session_from_continuing ) },
             { GetDescriptorPair(RudeEditKind.InsertMethodWithExplicitInterfaceSpecifier, FeaturesResources.Adding_method_with_explicit_interface_specifier_will_prevernt_the_debug_session_from_continuing) },
+            { GetDescriptorPair(RudeEditKind.InsertIntoInterface,                       FeaturesResources.Adding_0_into_an_interface_will_prevent_the_debug_session_from_continuing) },
+            { GetDescriptorPair(RudeEditKind.InsertLocalFunctionIntoInterfaceMethod,    FeaturesResources.Adding_0_into_an_interface_method_will_prevent_the_debug_session_from_continuing) },
             { GetDescriptorPair(RudeEditKind.InternalError,                             FeaturesResources.Modifying_source_file_will_prevent_the_debug_session_from_continuing_due_to_internal_error) },
             { GetDescriptorPair(RudeEditKind.RUDE_EDIT_COMPLEX_QUERY_EXPRESSION,        FeaturesResources.Modifying_0_which_contains_an_Aggregate_Group_By_or_Join_query_clauses_will_prevent_the_debug_session_from_continuing) },
 
