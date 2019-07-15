9e461896af17fc06dd423013d1312c96676d9758
Lauren Rosenberg <t-laro@microsoft.com>
2019-07-15 09:40:03 -0700

    Removed duplicate test

--- src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenNullCheckedParameterTests.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenNullCheckedParameterTests.cs
@@ -1221,44 +1221,6 @@ class C
         }
 
         [Fact]
-        public void TestNullCheckedEnumeratorReturningIterator()
-        {
-            var source = @"
-using System.Collections.Generic;
-class C
-{
-    public static void Main()
-    {
-        IEnumerator<char> e = GetChars(""hello"");
-        IEnumerator<char> GetChars(string s!)
-        {
-            foreach (var c in s)
-            {
-                yield return c;
-            }
-        }
-    }
-}";
-            var compilation = CompileAndVerify(source);
-            compilation.VerifyIL("C.<Main>g__GetChars|0_0(string)", @"
-{
-    // Code size       28 (0x1c)
-    .maxstack  3
-    IL_0000:  ldarg.0
-    IL_0001:  brtrue.s   IL_000e
-    IL_0003:  ldstr      ""s""
-    IL_0008:  newobj     ""System.ArgumentNullException..ctor(string)""
-    IL_000d:  throw
-    IL_000e:  ldc.i4.0
-    IL_000f:  newobj     ""C.<<Main>g__GetChars|0_0>d..ctor(int)""
-    IL_0014:  dup
-    IL_0015:  ldarg.0
-    IL_0016:  stfld      ""string C.<<Main>g__GetChars|0_0>d.s""
-    IL_001b:  ret
-}");
-        }
-
-        [Fact]
         public void TestNullCheckedIteratorInLocalFunction()
         {
             var source = @"
