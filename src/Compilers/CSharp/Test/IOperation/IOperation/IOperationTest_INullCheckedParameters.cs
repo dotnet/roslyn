d117c6dbae87f2dc544ddfe45af0b0cb5c0ddb69
Lauren Rosenberg <t-laro@microsoft.com>
2019-07-16 10:43:45 -0700

    created first test

--- src/Compilers/CSharp/Test/IOperation/IOperation/IOperationTest_INullCheckedParameters.cs
+++ src/Compilers/CSharp/Test/IOperation/IOperation/IOperationTest_INullCheckedParameters.cs
@@ -25,19 +25,63 @@ public class C
             var node1 = tree.GetRoot().DescendantNodes().OfType<BaseMethodDeclarationSyntax>().Single();
 
             compilation.VerifyOperationTree(node1, expectedOperationTree: @"
-IMethodBodyOperation (OperationKind.MethodBody, Type: null) (Syntax: 'public void ...  input!) { }')
-    BlockBody: 
-    IBlockOperation (0 statements) (OperationKind.Block, Type: null) (Syntax: '{ }')
-    ExpressionBody: 
+IMethodBodyOperation (OperationKind.MethodBody, Type: null) (Syntax: 'public void ... input!) { }')
+  BlockBody: 
+    IBlockOperation (1 statements) (OperationKind.Block, Type: null) (Syntax: '{ }')
+      IConditionalOperation (OperationKind.Conditional, Type: null, IsImplicit) (Syntax: '{ }')
+        Condition: 
+          IBinaryOperation (BinaryOperatorKind.Equals) (OperationKind.Binary, Type: System.Boolean, IsImplicit) (Syntax: '{ }')
+            Left: 
+              IParameterReferenceOperation: input (OperationKind.ParameterReference, Type: System.String, IsImplicit) (Syntax: '{ }')
+            Right: 
+              ILiteralOperation (OperationKind.Literal, Type: System.String, Constant: ConstantValueNull(null: Null), IsImplicit) (Syntax: '{ }')
+        WhenTrue: 
+          IExpressionStatementOperation (OperationKind.ExpressionStatement, Type: System.Void, IsImplicit) (Syntax: '{ }')
+            Expression: 
+              IThrowOperation (OperationKind.Throw, Type: null, IsImplicit) (Syntax: '{ }')
+                IObjectCreationOperation (Constructor: System.ArgumentNullException..ctor(System.String paramName)) (OperationKind.ObjectCreation, Type: System.ArgumentNullException, IsImplicit) (Syntax: '{ }')
+                  Arguments(1):
+                      IArgumentOperation (ArgumentKind.Explicit, Matching Parameter: paramName) (OperationKind.Argument, Type: null, IsImplicit) (Syntax: '{ }')
+                        ILiteralOperation (OperationKind.Literal, Type: System.String, Constant: ""input"", IsImplicit) (Syntax: '{ }')
+                        InConversion: CommonConversion (Exists: True, IsIdentity: True, IsNumeric: False, IsReference: False, IsUserDefined: False) (MethodSymbol: null)
+                        OutConversion: CommonConversion (Exists: True, IsIdentity: True, IsNumeric: False, IsReference: False, IsUserDefined: False) (MethodSymbol: null)
+                  Initializer: 
+                    IObjectOrCollectionInitializerOperation (OperationKind.ObjectOrCollectionInitializer, Type: System.ArgumentNullException, IsImplicit) (Syntax: '{ }')
+                      Initializers(0)
+        WhenFalse: 
+          null
+  ExpressionBody: 
     null");
 
             VerifyFlowGraph(compilation, node1, expectedFlowGraph: @"
-Block[B0] - Entry
-    Statements (0)
-    Next (Regular) Block[B1]
-Block[B1] - Exit
-    Predecessors: [B0]
-    Statements (0)");
+    Block[B0] - Entry
+        Statements (0)
+        Next (Regular) Block[B1]
+    Block[B1] - Block
+        Predecessors: [B0]
+        Statements (0)
+        Jump if False (Regular) to Block[B3]
+            IBinaryOperation (BinaryOperatorKind.Equals) (OperationKind.Binary, Type: System.Boolean, IsImplicit) (Syntax: '{ }')
+              Left: 
+                IParameterReferenceOperation: input (OperationKind.ParameterReference, Type: System.String, IsImplicit) (Syntax: '{ }')
+              Right: 
+                ILiteralOperation (OperationKind.Literal, Type: System.String, Constant: ConstantValueNull(null: Null), IsImplicit) (Syntax: '{ }')
+        Next (Regular) Block[B2]
+    Block[B2] - Block
+        Predecessors: [B1]
+        Statements (0)
+        Next (Throw) Block[null]
+            IObjectCreationOperation (Constructor: System.ArgumentNullException..ctor(System.String paramName)) (OperationKind.ObjectCreation, Type: System.ArgumentNullException, IsImplicit) (Syntax: '{ }')
+              Arguments(1):
+                  IArgumentOperation (ArgumentKind.Explicit, Matching Parameter: paramName) (OperationKind.Argument, Type: null, IsImplicit) (Syntax: '{ }')
+                    ILiteralOperation (OperationKind.Literal, Type: System.String, Constant: """"input"""", IsImplicit) (Syntax: '{ }')
+                    InConversion: CommonConversion (Exists: True, IsIdentity: True, IsNumeric: False, IsReference: False, IsUserDefined: False) (MethodSymbol: null)
+                    OutConversion: CommonConversion (Exists: True, IsIdentity: True, IsNumeric: False, IsReference: False, IsUserDefined: False) (MethodSymbol: null)
+              Initializer: 
+                null
+    Block[B3] - Exit
+        Predecessors: [B1]
+        Statements (0)");
         }
     }
 }
