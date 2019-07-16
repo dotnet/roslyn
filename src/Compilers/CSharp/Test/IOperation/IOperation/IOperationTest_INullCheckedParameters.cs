4c26f08fb4da75bc98fc719955e22e3c28e77f56
Lauren Rosenberg <t-laro@microsoft.com>
2019-07-16 15:28:53 -0700

    Additional test for operators

--- src/Compilers/CSharp/Test/IOperation/IOperation/IOperationTest_INullCheckedParameters.cs
+++ src/Compilers/CSharp/Test/IOperation/IOperation/IOperationTest_INullCheckedParameters.cs
@@ -233,5 +233,91 @@ Block[B3] - Exit
     Predecessors: [B1]
     Statements (0)");
         }
+
+        [Fact]
+        public void TestIOp_NullCheckedOperator()
+        {
+            var source = @"
+public class Box
+{
+    public static int operator+ (Box b!, Box c)  
+    { 
+        return 2;
+    }
+}";
+            var compilation = CreateCompilation(source);
+
+            compilation.VerifyDiagnostics();
+
+            var tree = compilation.SyntaxTrees.Single();
+            var node1 = tree.GetRoot().DescendantNodes().OfType<BaseMethodDeclarationSyntax>().Single();
+
+            compilation.VerifyOperationTree(node1, expectedOperationTree: @"
+IMethodBodyOperation (OperationKind.MethodBody, Type: null) (Syntax: 'public stat ... }')
+  BlockBody: 
+    IBlockOperation (2 statements) (OperationKind.Block, Type: null) (Syntax: '{ ... }')
+      IConditionalOperation (OperationKind.Conditional, Type: null, IsImplicit) (Syntax: '{ ... }')
+        Condition: 
+          IBinaryOperation (BinaryOperatorKind.Equals) (OperationKind.Binary, Type: System.Boolean, IsImplicit) (Syntax: '{ ... }')
+            Left: 
+              IParameterReferenceOperation: b (OperationKind.ParameterReference, Type: Box, IsImplicit) (Syntax: '{ ... }')
+            Right: 
+              ILiteralOperation (OperationKind.Literal, Type: Box, Constant: ConstantValueNull(null: Null), IsImplicit) (Syntax: '{ ... }')
+        WhenTrue: 
+          IExpressionStatementOperation (OperationKind.ExpressionStatement, Type: System.Void, IsImplicit) (Syntax: '{ ... }')
+            Expression: 
+              IThrowOperation (OperationKind.Throw, Type: null, IsImplicit) (Syntax: '{ ... }')
+                IObjectCreationOperation (Constructor: System.ArgumentNullException..ctor(System.String paramName)) (OperationKind.ObjectCreation, Type: System.ArgumentNullException, IsImplicit) (Syntax: '{ ... }')
+                  Arguments(1):
+                      IArgumentOperation (ArgumentKind.Explicit, Matching Parameter: paramName) (OperationKind.Argument, Type: null, IsImplicit) (Syntax: '{ ... }')
+                        ILiteralOperation (OperationKind.Literal, Type: System.String, Constant: ""b"", IsImplicit) (Syntax: '{ ... }')
+                        InConversion: CommonConversion (Exists: True, IsIdentity: True, IsNumeric: False, IsReference: False, IsUserDefined: False) (MethodSymbol: null)
+                        OutConversion: CommonConversion (Exists: True, IsIdentity: True, IsNumeric: False, IsReference: False, IsUserDefined: False) (MethodSymbol: null)
+                  Initializer: 
+                    IObjectOrCollectionInitializerOperation (OperationKind.ObjectOrCollectionInitializer, Type: System.ArgumentNullException, IsImplicit) (Syntax: '{ ... }')
+                      Initializers(0)
+        WhenFalse: 
+          null
+      IReturnOperation (OperationKind.Return, Type: null) (Syntax: 'return 2;')
+        ReturnedValue: 
+          ILiteralOperation (OperationKind.Literal, Type: System.Int32, Constant: 2) (Syntax: '2')
+  ExpressionBody: 
+    null");
+
+            VerifyFlowGraph(compilation, node1, expectedFlowGraph: @"
+Block[B0] - Entry
+    Statements (0)
+    Next (Regular) Block[B1]
+Block[B1] - Block
+    Predecessors: [B0]
+    Statements (0)
+    Jump if False (Regular) to Block[B3]
+        IBinaryOperation (BinaryOperatorKind.Equals) (OperationKind.Binary, Type: System.Boolean, IsImplicit) (Syntax: '{ ... }')
+          Left: 
+            IParameterReferenceOperation: b (OperationKind.ParameterReference, Type: Box, IsImplicit) (Syntax: '{ ... }')
+          Right: 
+            ILiteralOperation (OperationKind.Literal, Type: Box, Constant: ConstantValueNull(null: Null), IsImplicit) (Syntax: '{ ... }')
+    Next (Regular) Block[B2]
+Block[B2] - Block
+    Predecessors: [B1]
+    Statements (0)
+    Next (Throw) Block[null]
+        IObjectCreationOperation (Constructor: System.ArgumentNullException..ctor(System.String paramName)) (OperationKind.ObjectCreation, Type: System.ArgumentNullException, IsImplicit) (Syntax: '{ ... }')
+          Arguments(1):
+              IArgumentOperation (ArgumentKind.Explicit, Matching Parameter: paramName) (OperationKind.Argument, Type: null, IsImplicit) (Syntax: '{ ... }')
+                ILiteralOperation (OperationKind.Literal, Type: System.String, Constant: ""b"", IsImplicit) (Syntax: '{ ... }')
+                InConversion: CommonConversion (Exists: True, IsIdentity: True, IsNumeric: False, IsReference: False, IsUserDefined: False) (MethodSymbol: null)
+                OutConversion: CommonConversion (Exists: True, IsIdentity: True, IsNumeric: False, IsReference: False, IsUserDefined: False) (MethodSymbol: null)
+          Initializer: 
+            null
+Block[B3] - Block
+    Predecessors: [B1]
+    Statements (0)
+    Next (Return) Block[B4]
+        ILiteralOperation (OperationKind.Literal, Type: System.Int32, Constant: 2) (Syntax: '2')
+Block[B4] - Exit
+    Predecessors: [B3]
+    Statements (0)");
+        }
     }
 }
