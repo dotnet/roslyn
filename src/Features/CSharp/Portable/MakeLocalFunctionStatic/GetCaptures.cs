3a6740846d031627dc129e00773e3bc48c5f6959
Lavanya Guruswamy <t-laguru@microsoft.com>
2019-07-19 11:35:29 -0700

    initial change

---
+++ src/Features/CSharp/Portable/MakeLocalFunctionStatic/GetCaptures.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.CodeAnalysis.CSharp.MakeLocalFunctionStatic
+{
+    internal class MakeLocalFunctionStaticService: ILanguageService
+    {
+        
+        public Task<Solution> CreateParameterSymbol(Document document, LocalFunctionStatementSyntax localfunction, CancellationToken cancellationToken)
+        {
+
+
+
+        }
+    }
+
+
+
+
+}
