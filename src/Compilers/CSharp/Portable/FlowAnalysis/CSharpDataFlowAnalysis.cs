20cf6b380141804e6ce2ea5ec66b2bdd1f5bfdb2
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-21 11:59:53 -0700

    Sort usings.

--- src/Compilers/CSharp/Portable/FlowAnalysis/CSharpDataFlowAnalysis.cs
+++ src/Compilers/CSharp/Portable/FlowAnalysis/CSharpDataFlowAnalysis.cs
@@ -1,12 +1,11 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.PooledObjects;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Threading;
-using System;
 using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.PooledObjects;
 
 namespace Microsoft.CodeAnalysis.CSharp
 {
