89cde962bb58b02936d845ae72b7b534131c9df5
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-23 10:50:25 -0700

    Remove imports

--- src/Compilers/VisualBasic/Test/Semantic/FlowAnalysis/ImplicitVariableTests.vb
+++ src/Compilers/VisualBasic/Test/Semantic/FlowAnalysis/ImplicitVariableTests.vb
@@ -1,16 +1,5 @@
 ﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-Imports System
-Imports System.Linq
-Imports System.Xml.Linq
-Imports Microsoft.CodeAnalysis
-Imports Microsoft.CodeAnalysis.Test.Utilities
-Imports Microsoft.CodeAnalysis.Text
-Imports Microsoft.CodeAnalysis.VisualBasic
-Imports Microsoft.CodeAnalysis.VisualBasic.Symbols
-Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
-Imports Xunit
-
 Namespace Microsoft.CodeAnalysis.VisualBasic.UnitTests.FlowAnalysis
 
     Public Class ImplicitVariableTests : Inherits FlowTestBase
