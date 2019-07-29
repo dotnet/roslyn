5d04d4fde3cda557ae94652c4e322719b154c74c
David Barbet <dabarbet@microsoft.com>
2019-07-29 14:08:26 -0700

    Add handler and client for syntax classifications over LSP.

---
+++ src/Tools/ExternalAccess/LiveShare/Classification/RoslynSyntaxClassificationServiceFactory.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.LiveShare.Classification
+{
+    internal abstract class RoslynSyntaxClassificationServiceFactory : ILanguageServiceFactory
+    {
+        private readonly AbstractLspClientServiceFactory _roslynLspClientServiceFactory;
+        private readonly RemoteLanguageServiceWorkspace _remoteLanguageServiceWorkspace;
+        private readonly ClassificationTypeMap _classificationTypeMap;
+        private readonly IThreadingContext _threadingContext;
+
+        public RoslynSyntaxClassificationServiceFactory(AbstractLspClientServiceFactory roslynLspClientServiceFactory, RemoteLanguageServiceWorkspace remoteLanguageServiceWorkspace,
+            ClassificationTypeMap classificationTypeMap, IThreadingContext threadingContext)
+        {
+            _roslynLspClientServiceFactory = roslynLspClientServiceFactory ?? throw new ArgumentNullException(nameof(roslynLspClientServiceFactory));
+            _remoteLanguageServiceWorkspace = remoteLanguageServiceWorkspace ?? throw new ArgumentNullException(nameof(remoteLanguageServiceWorkspace));
+            _classificationTypeMap = classificationTypeMap ?? throw new ArgumentNullException(nameof(classificationTypeMap));
+            _threadingContext = threadingContext ?? throw new ArgumentNullException(nameof(threadingContext));
+        }
+
+        public ILanguageService CreateLanguageService(HostLanguageServices languageServices)
+        {
+            return new RoslynSyntaxClassificationService(_roslynLspClientServiceFactory, _remoteLanguageServiceWorkspace,
+                languageServices.GetOriginalLanguageService<ISyntaxClassificationService>(), _classificationTypeMap, _threadingContext);
+        }
+    }
+}
