5f6d8998605e5e73d999bcdaae78798da28a2a7d
Charles Stoner <chucks@microsoft.com>
2019-07-30 10:32:40 -0700

    Remove AttributeTargets.Module from NullableContextAttribute

--- src/Compilers/CSharp/Test/Emit/Attributes/AttributeTests_Nullable.cs
+++ src/Compilers/CSharp/Test/Emit/Attributes/AttributeTests_Nullable.cs
@@ -1734,6 +1734,37 @@ public class Program
         }
 
         [Fact]
+        public void EmitAttribute_NestedEnum()
+        {
+            var source =
+@"#nullable enable
+public class Program
+{
+    public enum E
+    {
+        A,
+        B
+    }
+    public object F1;
+    public object F2;
+    public object F3;
+}";
+            var comp = CreateCompilation(source);
+            var expected =
+@"[NullableContext(1)] [Nullable(0)] Program
+    System.Object! F1
+    System.Object! F2
+    System.Object! F3
+    Program()
+    [NullableContext(0)] Program.E
+        A
+        B
+        E()
+";
+            AssertNullableAttributes(comp, expected);
+        }
+
+        [Fact]
         public void EmitAttribute_LambdaReturnType()
         {
             var source =
