e598f5e663015995d948a3d6ca3a60b9f39dc1d0
David Poeschl <dpoeschl@gmail.com>
2019-07-30 13:32:28 -0700

    Even more descriptive automation text for the Inline Rename Dashboard

--- src/EditorFeatures/Core/EditorFeaturesResources.Designer.cs
+++ src/EditorFeatures/Core/EditorFeaturesResources.Designer.cs
@@ -198,7 +198,7 @@ namespace Microsoft.CodeAnalysis.Editor {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to An inline rename session is active for identifier &apos;{0}&apos;. Invoke inline rename again to access additional options..
+        ///   Looks up a localized string similar to An inline rename session is active for identifier &apos;{0}&apos;. Invoke inline rename again to access additional options. You may continue to edit the identifier being renamed at any time..
         /// </summary>
         internal static string An_inline_rename_session_is_active_for_identifier_0 {
             get {
