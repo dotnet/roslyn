0e39a0aed375fa5bd90b5291cc7757965795ede8
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-08-02 01:41:12 -0700

    Revert

--- src/Compilers/CSharp/Portable/Binder/LocalBinderFactory.cs
+++ src/Compilers/CSharp/Portable/Binder/LocalBinderFactory.cs
@@ -1,11 +1,12 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-using System.Diagnostics;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Roslyn.Utilities;
+using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
 
 namespace Microsoft.CodeAnalysis.CSharp
 {
