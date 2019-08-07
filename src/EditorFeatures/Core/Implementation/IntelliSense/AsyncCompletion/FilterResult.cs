e0af906a0cce1f455226fba57c19a00a8b9c4f5d
Ivan Basov <basov@hotmail.com>
2019-08-06 18:55:55 -0700

    code review feedback

--- src/EditorFeatures/Core/Implementation/IntelliSense/AsyncCompletion/FilterResult.cs
+++ src/EditorFeatures/Core/Implementation/IntelliSense/AsyncCompletion/FilterResult.cs
@@ -1,12 +1,13 @@
 ﻿
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using Roslyn.Utilities;
+using System;
 using Microsoft.CodeAnalysis.Completion;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense.AsyncCompletion
 {
-    internal struct FilterResult
+    internal struct FilterResult : IComparable<FilterResult>
     {
         public readonly CompletionItem CompletionItem;
         public readonly bool MatchedFilterText;
@@ -19,50 +20,43 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense.AsyncComplet
             FilterText = filterText;
         }
 
-        public static int Compare(FilterResult result1, FilterResult result2)
+        public int CompareTo(FilterResult other)
         {
-            var item1 = result1.CompletionItem;
-            var item2 = result2.CompletionItem;
+            var item1 = this.CompletionItem;
+            var item2 = other.CompletionItem;
 
-            var prefixLength1 = item1.FilterText.GetCaseInsensitivePrefixLength(result1.FilterText);
-            var prefixLength2 = item2.FilterText.GetCaseInsensitivePrefixLength(result2.FilterText);
+            var prefixLength1 = item1.FilterText.GetCaseInsensitivePrefixLength(this.FilterText);
+            var prefixLength2 = item2.FilterText.GetCaseInsensitivePrefixLength(other.FilterText);
 
             // Prefer the item that matches a longer prefix of the filter text.
             if (prefixLength1 != prefixLength2)
             {
                 return prefixLength1.CompareTo(prefixLength2);
             }
-            else
+
+            // If the lengths are the same, prefer the one with the higher match priority.
+            // But only if it's an item that would have been hard selected.  We don't want
+            // to aggressively select an item that was only going to be softly offered.
+            var item1Priority = item1.Rules.SelectionBehavior == CompletionItemSelectionBehavior.HardSelection
+                ? item1.Rules.MatchPriority : MatchPriority.Default;
+            var item2Priority = item2.Rules.SelectionBehavior == CompletionItemSelectionBehavior.HardSelection
+                ? item2.Rules.MatchPriority : MatchPriority.Default;
+
+            if (item1Priority != item2Priority)
             {
-                // If the lengths are the same, prefer the one with the higher match priority.
-                // But only if it's an item that would have been hard selected.  We don't want
-                // to aggressively select an item that was only going to be softly offered.
-                var item1Priority = item1.Rules.SelectionBehavior == CompletionItemSelectionBehavior.HardSelection
-                    ? item1.Rules.MatchPriority : MatchPriority.Default;
-                var item2Priority = item2.Rules.SelectionBehavior == CompletionItemSelectionBehavior.HardSelection
-                    ? item2.Rules.MatchPriority : MatchPriority.Default;
-
-                if (item1Priority != item2Priority)
-                {
-                    return item1Priority > item2Priority ? 1 : -1;
-                }
-
-                prefixLength1 = item1.FilterText.GetCaseSensitivePrefixLength(result1.FilterText);
-                prefixLength2 = item2.FilterText.GetCaseSensitivePrefixLength(result2.FilterText);
-
-                // If there are "Abc" vs "abc", we should prefer the case typed by user.
-                if (prefixLength1 != prefixLength2)
-                {
-                    return prefixLength1 > prefixLength2 ? 1 : -1;
-                }
-
-                if (result1.CompletionItem.IsPreferredItem() != result2.CompletionItem.IsPreferredItem())
-                {
-                    return result1.CompletionItem.IsPreferredItem() ? 1 : -1;
-                }
-
-                return 0;
+                return item1Priority.CompareTo(item2Priority);
             }
+
+            prefixLength1 = item1.FilterText.GetCaseSensitivePrefixLength(this.FilterText);
+            prefixLength2 = item2.FilterText.GetCaseSensitivePrefixLength(other.FilterText);
+
+            // If there are "Abc" vs "abc", we should prefer the case typed by user.
+            if (prefixLength1 != prefixLength2)
+            {
+                return prefixLength1.CompareTo(prefixLength2);
+            }
+
+            return this.CompletionItem.IsPreferredItem().CompareTo(other.CompletionItem.IsPreferredItem());
         }
     }
 }
