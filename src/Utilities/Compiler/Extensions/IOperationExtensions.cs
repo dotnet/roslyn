4e8b21d4d8181edb59ee71e02b034764190e3d86
paulomorgado <470455+paulomorgado@users.noreply.github.com>
2019-08-09 20:45:41 +0100

    added extensions for walking up and down conversions

--- src/Utilities/Compiler/Extensions/IOperationExtensions.cs
+++ src/Utilities/Compiler/Extensions/IOperationExtensions.cs
@@ -664,6 +664,36 @@ namespace Analyzer.Utilities.Extensions
 
             return operation;
         }
+
+        /// <summary>
+        /// Walks down consequtive conversion operations until an operand is reached that isn't a conversion operation.
+        /// </summary>
+        /// <param name="operation">The starting operation.</param>
+        /// <returns>The inner non conversion operation or the starting operation if it wasn't a conversion operation.</returns>
+        public static IOperation WalkDownConversion(this IOperation operation)
+        {
+            while (operation is IConversionOperation conversionOperation)
+            {
+                operation = conversionOperation.Operand;
+            }
+
+            return operation;
+        }
+
+        /// <summary>
+        /// Walks up consequtive conversion operations until a parent is reached that isn't a conversion operation.
+        /// </summary>
+        /// <param name="operation">The starting operation.</param>
+        /// <returns>The outer non conversion operation or the starting operation if it wasn't a conversion operation.</returns>
+        public static IOperation WalkUpConversion(this IOperation operation)
+        {
+            while (operation is IConversionOperation conversionOperation)
+            {
+                operation = conversionOperation.Parent;
+            }
+
+            return operation;
+        }
     }
 }
 
