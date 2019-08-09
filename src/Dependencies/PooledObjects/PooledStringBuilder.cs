6e0cb9be51a34039cc5e0b76d1e58b63c1081eca
Petr Houska <t-pehouk@microsoft.com>
2019-08-08 20:27:25 -0700

    Cleanup.

--- src/Dependencies/PooledObjects/PooledStringBuilder.cs
+++ src/Dependencies/PooledObjects/PooledStringBuilder.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using System.Diagnostics;
 using System.Text;
 
