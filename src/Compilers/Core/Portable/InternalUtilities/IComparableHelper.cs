9c9aef70db42b40e80a1bb7008417987a893baf6
Ivan Basov <basov@hotmail.com>
2019-08-09 18:11:21 -0700

    Comparable helper to compare by components

---
+++ src/Compilers/Core/Portable/InternalUtilities/IComparableHelper.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+namespace Roslyn.Utilities
+{
+    internal static class IComparableHelper
+    {
+        public static int CompareTo<T>(T first, T second, params Func<T, IComparable>[] comparingComponents)
+        {
+            foreach (var component in comparingComponents)
+            {
+                var comparison = component(first).CompareTo(component(second));
+                if (comparison != 0)
+                {
+                    return comparison;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
