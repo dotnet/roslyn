82f312c5159c6542587ccfb79d4eefbf5ec3ee39
Jan Hindermann <jan@dfragment.net>
2019-08-14 19:21:11 +0200

    Add positive test for '#R' directive recommender

--- src/EditorFeatures/VisualBasicTest/Recommendations/PreprocessorDirectives/ReferenceDirectiveKeywordRecommenderTests.vb
+++ src/EditorFeatures/VisualBasicTest/Recommendations/PreprocessorDirectives/ReferenceDirectiveKeywordRecommenderTests.vb
@@ -7,5 +7,11 @@ Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.Recommendations.Pr
         Public Async Function NotInFileTest() As Task
             Await VerifyRecommendationsMissingAsync(<File>|</File>, "#R")
         End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.KeywordRecommending)>
+        <WorkItem(37911, "https://github.com/dotnet/roslyn/issues/37911")>
+        Public Async Function AppearsInScriptingContext() As Task
+            Await VerifyRecommendationsContainAsync(<File Script="True">|</File>, "#R")
+        End Function
     End Class
 End Namespace
