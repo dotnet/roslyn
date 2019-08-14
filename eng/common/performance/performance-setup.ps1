743cf338c2ba385ccad9ebc631b22031726fabf3
dotnet-maestro <@dotnet-maestro>
2019-08-14 12:19:50 +0000

    Update dependencies from https://github.com/dotnet/arcade build 20190812.7
    - Microsoft.DotNet.Arcade.Sdk - 1.0.0-beta.19412.7

--- eng/common/performance/performance-setup.ps1
+++ eng/common/performance/performance-setup.ps1
@@ -33,7 +33,7 @@ if ($Framework.StartsWith("netcoreapp")) {
 }
 
 if ($Internal) {
-    $Queue = "Windows.10.Amd64.ClientRS5.Perf"
+    $Queue = "Windows.10.Amd64.19H1.Tiger.Perf"
     $PerfLabArguments = "--upload-to-perflab-container"
     $ExtraBenchmarkDotNetArguments = ""
     $Creator = ""
