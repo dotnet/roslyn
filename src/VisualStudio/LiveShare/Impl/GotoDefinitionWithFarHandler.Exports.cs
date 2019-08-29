62d992d31c032179a4781819419e8c2c291bd088
David Barbet <dabarbet@microsoft.com>
2019-08-29 16:49:13 -0700

    Use shim for c#/vb liveshare gotodef, leave typescript as custom implementation requiring UI.

---
+++ src/VisualStudio/LiveShare/Impl/GotoDefinitionWithFarHandler.Exports.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.LiveShare.LanguageServices;
+using Microsoft.CodeAnalysis.Editor;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using System;
+
+namespace Microsoft.VisualStudio.LanguageServices.LiveShare
+{
+    [ExportLspRequestHandler(LiveShareConstants.RoslynContractName, Methods.TextDocumentDefinitionName)]
+    [Obsolete("Used for backwards compatibility with old liveshare clients.")]
+    internal class RoslynGoToDefinitionHandler : GotoDefinitionWithFarHandler
+    {
+        [ImportingConstructor]
+        public RoslynGoToDefinitionHandler([Import(AllowDefault = true)] IMetadataAsSourceFileService metadataAsSourceService) : base(metadataAsSourceService)
+        {
+        }
+    }
+
+    [ExportLspRequestHandler(LiveShareConstants.TypeScriptContractName, Methods.TextDocumentDefinitionName)]
+    internal class TypeScriptGoToDefinitionHandler : GotoDefinitionWithFarHandler
+    {
+        [ImportingConstructor]
+        public TypeScriptGoToDefinitionHandler([Import(AllowDefault = true)] IMetadataAsSourceFileService metadataAsSourceService) : base(metadataAsSourceService)
+        {
+        }
+    }
+}
---
+++ src/VisualStudio/LiveShare/Impl/Shims/GoToDefinitionHandlerShim.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis.LanguageServer.Handler;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Microsoft.VisualStudio.LiveShare.LanguageServices;
+
+namespace Microsoft.VisualStudio.LanguageServices.LiveShare.Shims
+{
+    internal class GoToDefinitionHandlerShim : AbstractLiveShareHandlerShim<TextDocumentPositionParams, object>
+    {
+        public GoToDefinitionHandlerShim(IEnumerable<Lazy<IRequestHandler, IRequestHandlerMetadata>> requestHandlers) : base(requestHandlers, Methods.TextDocumentDefinitionName)
+        {
+        }
+    }
+
+    [ExportLspRequestHandler(LiveShareConstants.CSharpContractName, Methods.TextDocumentDefinitionName)]
+    internal class CSharpFindImplementationsHandlerShim : GoToDefinitionHandlerShim
+    {
+        [ImportingConstructor]
+        public CSharpFindImplementationsHandlerShim([ImportMany] IEnumerable<Lazy<IRequestHandler, IRequestHandlerMetadata>> requestHandlers) : base(requestHandlers)
+        {
+        }
+    }
+
+    [ExportLspRequestHandler(LiveShareConstants.VisualBasicContractName, Methods.TextDocumentDefinitionName)]
+    internal class VisualBasicFindImplementationsHandlerShim : GoToDefinitionHandlerShim
+    {
+        [ImportingConstructor]
+        public VisualBasicFindImplementationsHandlerShim([ImportMany] IEnumerable<Lazy<IRequestHandler, IRequestHandlerMetadata>> requestHandlers) : base(requestHandlers)
+        {
+        }
+    }
+}
