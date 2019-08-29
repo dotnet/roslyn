8ed3070424e10b547d42fedd903e55dd2f6e8d9a
Petr Houska <t-pehouk@microsoft.com>
2019-08-29 16:54:55 -0700

    Fix underselection problem in invertlogical refactoring.

--- src/EditorFeatures/CSharpTest/InvertLogical/InvertLogicalTests.cs
+++ src/EditorFeatures/CSharpTest/InvertLogical/InvertLogicalTests.cs
@@ -268,5 +268,19 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.InvertLogical
     }
 }");
         }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsInvertLogical)]
+        [WorkItem(35525, "https://github.com/dotnet/roslyn/issues/35525")]
+        public async Task MissingSelectedSubtree()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int a, int b, int c)
+    {
+        var x = !(a <= 10 && [|b >= 20 && c != 30|]);
+    }
+}");
+        }
     }
 }
