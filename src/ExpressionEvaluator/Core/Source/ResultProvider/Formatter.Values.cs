385dabc97cdcbe05d08a1614b3583dcbd09d185c
Ivan Basov <basov@hotmail.com>
2019-08-30 15:28:26 -0700

    Formatter throws a exception where HostObjectValue is null

--- src/ExpressionEvaluator/Core/Source/ResultProvider/Formatter.Values.cs
+++ src/ExpressionEvaluator/Core/Source/ResultProvider/Formatter.Values.cs
@@ -53,6 +53,12 @@ namespace Microsoft.CodeAnalysis.ExpressionEvaluator
                 }
                 else if (lmrType.IsCharacter())
                 {
+                    // check if HostObjectValue is null, since any of these types might actually be a synthetic value as well.
+                    if (value.HostObjectValue == null)
+                    {
+                        return null;
+                    }
+
                     return IncludeObjectId(
                         value,
                         FormatLiteral((char)value.HostObjectValue, options | ObjectDisplayOptions.IncludeCodePoints),
@@ -102,6 +108,12 @@ namespace Microsoft.CodeAnalysis.ExpressionEvaluator
             }
             else if (lmrType.IsIntPtr())
             {
+                // check if HostObjectValue is null, since any of these types might actually be a synthetic value as well.
+                if (value.HostObjectValue == null)
+                {
+                    return null;
+                }
+
                 if (IntPtr.Size == 8)
                 {
                     var intPtr = ((IntPtr)value.HostObjectValue).ToInt64();
@@ -115,6 +127,12 @@ namespace Microsoft.CodeAnalysis.ExpressionEvaluator
             }
             else if (lmrType.IsUIntPtr())
             {
+                // check if HostObjectValue is null, since any of these types might actually be a synthetic value as well.
+                if (value.HostObjectValue == null)
+                {
+                    return null;
+                }
+
                 if (UIntPtr.Size == 8)
                 {
                     var uIntPtr = ((UIntPtr)value.HostObjectValue).ToUInt64();
@@ -161,6 +179,12 @@ namespace Microsoft.CodeAnalysis.ExpressionEvaluator
             {
                 options |= ObjectDisplayOptions.UseHexadecimalNumbers;
             }
+            // check if HostObjectValue is null, since any of these types might actually be a synthetic value as well.
+            if (value.HostObjectValue == null)
+            {
+                return null;
+            }
+
             var charTemp = FormatLiteral((char)value.HostObjectValue, options);
             Debug.Assert(charTemp != null);
             return charTemp;
@@ -173,7 +197,7 @@ namespace Microsoft.CodeAnalysis.ExpressionEvaluator
 
         private string GetUnderlyingString(DkmClrValue value, DkmInspectionContext inspectionContext)
         {
-            RawStringDataItem dataItem = value.GetDataItem<RawStringDataItem>();
+            var dataItem = value.GetDataItem<RawStringDataItem>();
             if (dataItem != null)
             {
                 return dataItem.RawString;
@@ -241,11 +265,15 @@ namespace Microsoft.CodeAnalysis.ExpressionEvaluator
             Debug.Assert(value != null);
 
             object underlyingValue = value.HostObjectValue;
-            Debug.Assert(underlyingValue != null);
+            // check if HostObjectValue is null, since any of these types might actually be a synthetic value as well.
+            if (value.HostObjectValue == null)
+            {
+                return null;
+            }
 
             string displayString;
 
-            ArrayBuilder<EnumField> fields = ArrayBuilder<EnumField>.GetInstance();
+            var fields = ArrayBuilder<EnumField>.GetInstance();
             FillEnumFields(fields, lmrType);
             // We will normalize/extend all enum values to ulong to ensure that we are always comparing the full underlying value.
             ulong valueForComparison = ConvertEnumUnderlyingTypeToUInt64(underlyingValue, Type.GetTypeCode(lmrType));
@@ -405,18 +433,21 @@ namespace Microsoft.CodeAnalysis.ExpressionEvaluator
         private string FormatPrimitive(DkmClrValue value, ObjectDisplayOptions options, DkmInspectionContext inspectionContext)
         {
             Debug.Assert(value != null);
+            // check if HostObjectValue is null, since any of these types might actually be a synthetic value as well.
+            if (value.HostObjectValue == null)
+            {
+                return null;
+            }
 
             object obj;
             if (value.Type.GetLmrType().IsDateTime())
             {
-                DkmClrValue dateDataValue = value.GetPropertyValue("Ticks", inspectionContext);
-                Debug.Assert(dateDataValue.HostObjectValue != null);
+                var dateDataValue = value.GetPropertyValue("Ticks", inspectionContext);
 
                 obj = new DateTime((long)dateDataValue.HostObjectValue);
             }
             else
             {
-                Debug.Assert(value.HostObjectValue != null);
                 obj = value.HostObjectValue;
             }
 
