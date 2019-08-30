35b0f32b2f988c42ded915470cc53fc47e3eb977
Joey Robichaud <jorobich@microsoft.com>
2019-08-30 13:11:34 -0700

    Register DocumentOptionsProvider in Workspace base class

--- src/VisualStudio/TestUtilities2/ProjectSystemShim/Framework/TestEnvironment.vb
+++ src/VisualStudio/TestUtilities2/ProjectSystemShim/Framework/TestEnvironment.vb
@@ -86,8 +86,7 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.ProjectSystemShim.Fr
 
             <ImportingConstructor>
             Public Sub New(exportProvider As Composition.ExportProvider)
-                MyBase.New(exportProvider,
-                           exportProvider.GetExportedValue(Of MockServiceProvider))
+                MyBase.New(exportProvider, exportProvider.GetExportedValue(Of MockServiceProvider))
             End Sub
 
             Public Overrides Sub DisplayReferencedSymbols(solution As Microsoft.CodeAnalysis.Solution, referencedSymbols As IEnumerable(Of ReferencedSymbol))
