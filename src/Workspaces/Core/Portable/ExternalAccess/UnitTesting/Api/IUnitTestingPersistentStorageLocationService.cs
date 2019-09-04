f44d9a7fb6fc7a5c6fe639d5694471da4f87df61
Shen Chen <shech@microsoft.com>
2019-09-04 15:50:18 -0700

    Add PersistentStorageLocationService wrapper

---
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/IUnitTestingPersistentStorageLocationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
+{
+    internal interface IUnitTestingPersistentStorageLocationService
+    {
+        event EventHandler<UnitTestingPersistentStorageLocationChangingEventArgsWrapper> StorageLocationChanging;
+        bool IsSupported(Workspace workspace);
+        string TryGetStorageLocation(SolutionId solutionId);
+    }
+}
---
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/UnitTestingPersistentStorageLocationChangingEventArgsWrapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
+{
+    internal readonly struct UnitTestingPersistentStorageLocationChangingEventArgsWrapper
+    {
+        internal UnitTestingPersistentStorageLocationChangingEventArgsWrapper(
+            PersistentStorageLocationChangingEventArgs underlyingObject)
+            => UnderlyingObject = underlyingObject;
+
+        internal PersistentStorageLocationChangingEventArgs UnderlyingObject { get; }
+    }
+}
