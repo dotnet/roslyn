173f76bc68dba45cbba39130fc8ded079b06ae5a
Shen Chen <shech@microsoft.com>
2019-09-04 15:34:39 -0700

    Add KeepAliveSessionWrapper

---
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/UnitTestingKeepAliveSessionWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Remote;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
+{
+    internal readonly struct UnitTestingKeepAliveSessionWrapper
+    {
+        internal UnitTestingKeepAliveSessionWrapper(KeepAliveSession underlyingObject)
+            => UnderlyingObject = underlyingObject;
+
+        internal KeepAliveSession UnderlyingObject { get; }
+
+        public Task<bool> TryInvokeAsync(string targetName, IReadOnlyList<object> arguments, CancellationToken cancellationToken)
+            => UnderlyingObject.TryInvokeAsync(targetName, arguments, cancellationToken);
+
+        public Task<bool> TryInvokeAsync(string targetName, Solution solution, IReadOnlyList<object> arguments, CancellationToken cancellationToken)
+            => UnderlyingObject.TryInvokeAsync(targetName, solution, arguments, cancellationToken);
+
+        public Task<T> TryInvokeAsync<T>(string targetName, IReadOnlyList<object> arguments, CancellationToken cancellationToken)
+            => UnderlyingObject.TryInvokeAsync<T>(targetName, arguments, cancellationToken);
+    }
+}
