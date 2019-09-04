4e3fdc3040763512823456f7aff466071789d6cf
Shen Chen <shech@microsoft.com>
2019-09-04 16:36:10 -0700

    Add extension wrapper

---
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/UnitTestingRemoteHostClientWrapper.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Remote;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
+{
+    internal readonly struct UnitTestingRemoteHostClientExtensionWrapper
+    {
+        internal UnitTestingRemoteHostClientExtensionWrapper(RemoteHostClient underlyingObject)
+            => UnderlyingObject = underlyingObject ?? throw new ArgumentNullException(nameof(underlyingObject));
+
+        internal RemoteHostClient UnderlyingObject { get; }
+
+        public async Task<UnitTestingKeepAliveSessionWrapper> TryCreateKeepAliveSessionAsync(string serviceName, CancellationToken cancellationToken)
+        {
+            var keepAliveSession = await UnderlyingObject.TryCreateKeepAliveSessionAsync(serviceName, cancellationToken).ConfigureAwait(false);
+            return new UnitTestingKeepAliveSessionWrapper(keepAliveSession);
+        }
+    }
+}
---
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/UnitTestingWorkspaceWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Remote;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
+{
+    internal readonly struct UnitTestingWorkspaceWrapper
+    {
+        internal UnitTestingWorkspaceWrapper(Workspace underlyingObject)
+            => UnderlyingObject = underlyingObject ?? throw new ArgumentNullException(nameof(underlyingObject));
+
+        internal Workspace UnderlyingObject { get; }
+
+        public async Task<UnitTestingRemoteHostClientExtensionWrapper> TryGetRemoteHostClientAsync(CancellationToken cancellationToken)
+        {
+            var remoteHostClient = await UnderlyingObject.TryGetRemoteHostClientAsync(cancellationToken).ConfigureAwait(false);
+            return new UnitTestingRemoteHostClientExtensionWrapper(remoteHostClient);
+        }
+    }
+}
