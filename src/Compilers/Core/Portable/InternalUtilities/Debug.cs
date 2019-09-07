58e0c4c0a9608414c36dd2abc53681780924d227
Andy Gocke <angocke@microsoft.com>
2019-09-07 15:04:48 -0700

    Add nullable-annotated Debug.Assert
    Adds the definitions for the nullable attributes and creates an
    annotated Debug.Assert

---
+++ src/Compilers/Core/Portable/InternalUtilities/Debug.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+#nullable enable
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Roslyn.Utilities
+{
+    internal static class RoslynDebug
+    {
+        [Conditional("DEBUG")]
+        public static void Assert([DoesNotReturnIf(false)]bool b) => Debug.Assert(b);
+
+        [Conditional("DEBUG")]
+        public static void Assert([DoesNotReturnIf(false)]bool b, string message)
+            => Debug.Assert(b, message);
+    }
+}
