3abd1ce63b0b6e4649f1a1cc151a870b5e83278e
Ivan Basov <basov@hotmail.com>
2019-09-09 17:41:52 -0700

    Update src/VisualStudio/Core/Test/DebuggerIntelliSense/TestState.vb
    Co-Authored-By: Jason Malinowski <jason@jason-m.com>

--- src/VisualStudio/Core/Test/DebuggerIntelliSense/TestState.vb
+++ src/VisualStudio/Core/Test/DebuggerIntelliSense/TestState.vb
@@ -198,7 +198,7 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.DebuggerIntelliSense
             Dim completionItems = session.GetComputedItems(CancellationToken.None)
             ' During the computation we can explicitly dismiss the session or we can return no items.
             ' Each of these conditions mean that there is no active completion.
-            Assert.True(session.IsDismissed OrElse completionItems.Items.Count() = 0, "AssertNoCompletionSession")
+            Assert.True(session.IsDismissed OrElse completionItems.Items.Count() = 0, NameOf(AssertNoCompletionSession))
         End Function
 
         Public Async Function AssertCompletionSession() As Task
