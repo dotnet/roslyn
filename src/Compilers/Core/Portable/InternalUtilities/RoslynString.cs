a625ecd9eeca0251f0c3ff21d96ddc2f71e3fd0c
Sam Harwell <Sam.Harwell@microsoft.com>
2019-09-26 09:09:16 -0700

    Define RoslynString

---
+++ src/Compilers/Core/Portable/InternalUtilities/RoslynString.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+#nullable enable
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Roslyn.Utilities
+{
+    internal static class RoslynString
+    {
+        /// <inheritdoc cref="string.IsNullOrEmpty(string)"/>
+        public static bool IsNullOrEmpty([NotNullWhen(returnValue: false)] this string? value)
+            => string.IsNullOrEmpty(value);
+    }
+}
