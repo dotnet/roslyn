8790fbde972c9723e4d4dc37e208721697d53cf6
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-01 11:51:18 -0700

    Add tests

--- src/EditorFeatures/CSharpTest/UseSystemHashCode/UseSimpleUsingStatementTests.cs
+++ src/EditorFeatures/CSharpTest/UseSystemHashCode/UseSimpleUsingStatementTests.cs
@@ -166,7 +166,7 @@ class C
 
     string S { get; }
 
-    public override int $$GetHashCode()
+    public override int GetHashCode()
     {
         return System.HashCode.Combine(i, S);
     }
@@ -206,7 +206,7 @@ class C
 
     string S { get; }
 
-    public override int $$GetHashCode()
+    public override int GetHashCode()
     {
         return System.HashCode.Combine(i, S);
     }
@@ -500,7 +500,7 @@ class C
 
     string S { get; }
 
-    public override int $$GetHashCode()
+    public override int GetHashCode()
     {
         return System.HashCode.Combine(i, S);
     }
@@ -537,7 +537,101 @@ class C
 
     string S { get; }
 
+    public override int GetHashCode()
+    {
+        return System.HashCode.Combine(i, S);
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseSimpleUsingStatement)]
+        public async Task TestInt64Pattern()
+        {
+            await TestInRegularAndScriptAsync(
+@"namespace System { public struct HashCode { } }
+
+class C
+{
+    int j;
+
     public override int $$GetHashCode()
+    {
+        long hashCode = -468965076;
+        hashCode = (hashCode * -1521134295 + j.GetHashCode()).GetHashCode();
+        return hashCode;
+    }
+}",
+@"namespace System { public struct HashCode { } }
+
+class C
+{
+    int j;
+
+    public override int GetHashCode()
+    {
+        return System.HashCode.Combine(j);
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseSimpleUsingStatement)]
+        public async Task TestInt64Pattern2()
+        {
+            await TestInRegularAndScriptAsync(
+@"namespace System { public struct HashCode { } }
+
+class C
+{
+    int j;
+
+    public override int $$GetHashCode()
+    {
+        long hashCode = -468965076;
+        hashCode = (hashCode * -1521134295 + j.GetHashCode()).GetHashCode();
+        return (int)hashCode;
+    }
+}",
+@"namespace System { public struct HashCode { } }
+
+class C
+{
+    int j;
+
+    public override int GetHashCode()
+    {
+        return System.HashCode.Combine(j);
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseSimpleUsingStatement)]
+        public async Task TestTuple()
+        {
+            await TestInRegularAndScriptAsync(
+@"using System.Collections.Generic;
+namespace System { public struct HashCode { } }
+
+class C 
+{
+    int i;
+
+    string S { get; }
+
+    public override int $$GetHashCode()
+    {
+        return (i, S).GetHashCode();
+    }
+}",
+@"using System.Collections.Generic;
+namespace System { public struct HashCode { } }
+
+class C 
+{
+    int i;
+
+    string S { get; }
+
+    public override int GetHashCode()
     {
         return System.HashCode.Combine(i, S);
     }
