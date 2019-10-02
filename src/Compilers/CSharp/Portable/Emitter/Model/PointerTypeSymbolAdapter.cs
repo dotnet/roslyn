b80e5284c927fade4be65087086f2b725a65a1ce
Sam Harwell <Sam.Harwell@microsoft.com>
2019-10-02 10:14:02 -0700

    Annotate implementations of IPointerTypeSymbol

--- src/Compilers/CSharp/Portable/Emitter/Model/PointerTypeSymbolAdapter.cs
+++ src/Compilers/CSharp/Portable/Emitter/Model/PointerTypeSymbolAdapter.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+#nullable enable
+
 using System.Reflection.Metadata;
 using Microsoft.CodeAnalysis.CSharp.Emit;
 using Microsoft.CodeAnalysis.Emit;
@@ -33,7 +35,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             get { return false; }
         }
 
-        Cci.ITypeDefinition Cci.ITypeReference.GetResolvedType(EmitContext context)
+        Cci.ITypeDefinition? Cci.ITypeReference.GetResolvedType(EmitContext context)
         {
             return null;
         }
@@ -48,47 +50,47 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             get { return default(TypeDefinitionHandle); }
         }
 
-        Cci.IGenericMethodParameterReference Cci.ITypeReference.AsGenericMethodParameterReference
+        Cci.IGenericMethodParameterReference? Cci.ITypeReference.AsGenericMethodParameterReference
         {
             get { return null; }
         }
 
-        Cci.IGenericTypeInstanceReference Cci.ITypeReference.AsGenericTypeInstanceReference
+        Cci.IGenericTypeInstanceReference? Cci.ITypeReference.AsGenericTypeInstanceReference
         {
             get { return null; }
         }
 
-        Cci.IGenericTypeParameterReference Cci.ITypeReference.AsGenericTypeParameterReference
+        Cci.IGenericTypeParameterReference? Cci.ITypeReference.AsGenericTypeParameterReference
         {
             get { return null; }
         }
 
-        Cci.INamespaceTypeDefinition Cci.ITypeReference.AsNamespaceTypeDefinition(EmitContext context)
+        Cci.INamespaceTypeDefinition? Cci.ITypeReference.AsNamespaceTypeDefinition(EmitContext context)
         {
             return null;
         }
 
-        Cci.INamespaceTypeReference Cci.ITypeReference.AsNamespaceTypeReference
+        Cci.INamespaceTypeReference? Cci.ITypeReference.AsNamespaceTypeReference
         {
             get { return null; }
         }
 
-        Cci.INestedTypeDefinition Cci.ITypeReference.AsNestedTypeDefinition(EmitContext context)
+        Cci.INestedTypeDefinition? Cci.ITypeReference.AsNestedTypeDefinition(EmitContext context)
         {
             return null;
         }
 
-        Cci.INestedTypeReference Cci.ITypeReference.AsNestedTypeReference
+        Cci.INestedTypeReference? Cci.ITypeReference.AsNestedTypeReference
         {
             get { return null; }
         }
 
-        Cci.ISpecializedNestedTypeReference Cci.ITypeReference.AsSpecializedNestedTypeReference
+        Cci.ISpecializedNestedTypeReference? Cci.ITypeReference.AsSpecializedNestedTypeReference
         {
             get { return null; }
         }
 
-        Cci.ITypeDefinition Cci.ITypeReference.AsTypeDefinition(EmitContext context)
+        Cci.ITypeDefinition? Cci.ITypeReference.AsTypeDefinition(EmitContext context)
         {
             return null;
         }
@@ -98,7 +100,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             visitor.Visit((Cci.IPointerTypeReference)this);
         }
 
-        Cci.IDefinition Cci.IReference.AsDefinition(EmitContext context)
+        Cci.IDefinition? Cci.IReference.AsDefinition(EmitContext context)
         {
             return null;
         }
