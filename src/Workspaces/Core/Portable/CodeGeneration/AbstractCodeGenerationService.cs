87c9a09e1f41063cdb47ac9b227c0a9b4d230b5f
yair halberstadt <yairhalberstadt@gmail.com>
2019-10-02 16:55:28 +0100

    Use AddImportsAnnotation not Annotation

--- src/Workspaces/Core/Portable/CodeGeneration/AbstractCodeGenerationService.cs
+++ src/Workspaces/Core/Portable/CodeGeneration/AbstractCodeGenerationService.cs
@@ -66,7 +66,7 @@ namespace Microsoft.CodeAnalysis.CodeGeneration
         private TNode WithAnnotations<TNode>(TNode node, CodeGenerationOptions options) where TNode : SyntaxNode
         {
             return options?.AddImports ?? true
-                ? node.WithAdditionalAnnotations(Simplifier.Annotation)
+                ? node.WithAdditionalAnnotations(Simplifier.AddImportsAnnotation)
                 : node;
         }
 
