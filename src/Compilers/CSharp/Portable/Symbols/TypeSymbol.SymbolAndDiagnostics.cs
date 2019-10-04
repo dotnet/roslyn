b5f30b16ca816d3bc6c9c176c9572e599d027481
Sam Harwell <Sam.Harwell@microsoft.com>
2019-10-04 09:07:30 -0700

    Annotate TypeSymbol

--- src/Compilers/CSharp/Portable/Symbols/TypeSymbol.SymbolAndDiagnostics.cs
+++ src/Compilers/CSharp/Portable/Symbols/TypeSymbol.SymbolAndDiagnostics.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+#nullable enable
+
 using System.Collections.Immutable;
+
+#pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
 
 namespace Microsoft.CodeAnalysis.CSharp.Symbols
 {
@@ -14,10 +18,10 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
         {
             public static readonly SymbolAndDiagnostics Empty = new SymbolAndDiagnostics(null, ImmutableArray<Diagnostic>.Empty);
 
-            public readonly Symbol Symbol;
+            public readonly Symbol? Symbol;
             public readonly ImmutableArray<Diagnostic> Diagnostics;
 
-            public SymbolAndDiagnostics(Symbol symbol, ImmutableArray<Diagnostic> diagnostics)
+            public SymbolAndDiagnostics(Symbol? symbol, ImmutableArray<Diagnostic> diagnostics)
             {
                 this.Symbol = symbol;
                 this.Diagnostics = diagnostics;
