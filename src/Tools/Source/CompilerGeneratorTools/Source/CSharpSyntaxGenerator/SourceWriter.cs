7c36fe2dfe8421b679da8f1dfca68f9842457c22
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-04 17:17:54 -0700

    Simplify switch generation

--- src/Tools/Source/CompilerGeneratorTools/Source/CSharpSyntaxGenerator/SourceWriter.cs
+++ src/Tools/Source/CompilerGeneratorTools/Source/CSharpSyntaxGenerator/SourceWriter.cs
@@ -737,18 +737,11 @@ namespace CSharpSyntaxGenerator
                         {
                             kinds.Add(new Kind { Name = "None" });
                         }
-                        for (int j = 0; j < kinds.Count; j++)
+                        foreach (var kind in kinds)
                         {
-                            var kind = kinds[j];
-                            Write("case SyntaxKind.{0}:", kind.Name);
-                            if (j == kinds.Count - 1)
-                            {
-                                WriteLine(" break;");
-                            }
-                            else
-                            {
-                                WriteLine();
-                            }
+                            WriteLine("case SyntaxKind.{0}:{1}",
+                                kind.Name,
+                                kind == kinds.Last() ? " break;" : "");
                         }
 
                         WriteLine("default: throw new ArgumentException(nameof({0}));", pname);
