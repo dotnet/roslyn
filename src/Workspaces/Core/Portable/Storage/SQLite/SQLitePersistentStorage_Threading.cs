1f04d794fb97c43ebd187057723d589082a5bc6a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-06 15:26:01 -0700

    Docs

--- src/Workspaces/Core/Portable/Storage/SQLite/SQLitePersistentStorage_Threading.cs
+++ src/Workspaces/Core/Portable/Storage/SQLite/SQLitePersistentStorage_Threading.cs
@@ -65,11 +65,16 @@ namespace Microsoft.CodeAnalysis.SQLite
                 }
             }
 
-            private static readonly Func<object, TResult> Callback = b =>
+            // Non-allocating/capturing callback for our task scheduler to execute. uses a pooled
+            // strongbox to pass in data safely through the 'arg' parameter of StartNew.
+            private static readonly Func<object, TResult> s_callback = b =>
             {
+                // Grab the func and arg we want to execute from the StrongBox passed in.
+                // Then return the StrongBox back to the pool as we have no more need for it.
                 var innerBox = (StrongBox<(Func<TArg, TResult> func, TArg arg)>)b;
                 var (func, arg) = innerBox.Value;
                 ReturnBox(innerBox);
+
                 return func(arg);
             };
 
@@ -80,7 +85,7 @@ namespace Microsoft.CodeAnalysis.SQLite
                 var box = GetBox();
                 box.Value = (func, arg);
 
-                return Task.Factory.StartNew(Callback, box, cancellationToken, TaskCreationOptions.None, scheduler);
+                return Task.Factory.StartNew(s_callback, box, cancellationToken, TaskCreationOptions.None, scheduler);
             }
         }
 
