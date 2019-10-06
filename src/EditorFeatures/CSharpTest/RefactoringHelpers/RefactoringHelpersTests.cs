a1d3e973882798ac1a25601256f5fbdbf075575c
Petr Houška <houskape@gmail.com>
2019-10-07 00:18:58 +0200

    Initial bugfix for #38708

--- src/EditorFeatures/CSharpTest/RefactoringHelpers/RefactoringHelpersTests.cs
+++ src/EditorFeatures/CSharpTest/RefactoringHelpers/RefactoringHelpersTests.cs
@@ -513,6 +513,23 @@ class C
 
         #endregion
 
+        #region IsUnderselected
+        [Fact]
+        [WorkItem(38708, "https://github.com/dotnet/roslyn/issues/38708")]
+        public async Task TestUnderselectionOnSemicolon()
+        {
+            var testText = @"
+class Program
+{
+    static void Main()
+    {
+        {|result:Main()|}[|;|]
+    }
+}";
+            await TestNotUnderselectedAsync<ExpressionSyntax>(testText);
+        }
+        #endregion
+
         #region Attributes
         [Fact]
         [WorkItem(37584, "https://github.com/dotnet/roslyn/issues/37584")]
