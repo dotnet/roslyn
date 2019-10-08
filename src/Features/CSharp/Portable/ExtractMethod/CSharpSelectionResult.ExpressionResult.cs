dab1fc5db5a4a881610b3e84725204005f7ce34f
Andrew Hall (METAL) <andrha@microsoft.com>
2019-10-08 11:08:55 -0700

    Fix to use ConvertedNullability. Remove type for object check since it would cause incorrect behavior for casting of object to another type. Make sure to get type info of the cast expression, not the expression type

--- src/Features/CSharp/Portable/ExtractMethod/CSharpSelectionResult.ExpressionResult.cs
+++ src/Features/CSharp/Portable/ExtractMethod/CSharpSelectionResult.ExpressionResult.cs
@@ -74,14 +74,14 @@ namespace Microsoft.CodeAnalysis.CSharp.ExtractMethod
                     // 2. if it doesn't, even if the cast itself wasn't included in the selection, we will treat it 
                     //    as it was in the selection
                     var regularType = GetRegularExpressionType(model, node);
-                    if (regularType != null && !regularType.IsObjectType())
+                    if (regularType != null)
                     {
                         return regularType;
                     }
 
                     if (node.Parent is CastExpressionSyntax castExpression)
                     {
-                        return model.GetTypeInfo(castExpression.Type).GetTypeWithAnnotatedNullability();
+                        return model.GetTypeInfo(castExpression).GetTypeWithAnnotatedNullability();
                     }
                 }
 
