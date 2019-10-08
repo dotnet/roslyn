d0cb72b49b1ce3dd0a552c47b82fa06463ea93e8
Jason Malinowski <jason.malinowski@microsoft.com>
2019-10-08 16:21:52 -0700

    Add some nullable annotations in options system
    There appeared to be one bug: if a document was opened that had a null
    file path, there was a risk that we would crash because we would be
    passing that around to the editor API in the legacy case.

--- src/Workspaces/Core/Portable/Options/IDocumentOptions.cs
+++ src/Workspaces/Core/Portable/Options/IDocumentOptions.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+#nullable enable
+
 namespace Microsoft.CodeAnalysis.Options
 {
     /// <summary>
@@ -7,6 +9,6 @@ namespace Microsoft.CodeAnalysis.Options
     /// </summary>
     interface IDocumentOptions
     {
-        bool TryGetDocumentOption(OptionKey option, out object value);
+        bool TryGetDocumentOption(OptionKey option, out object? value);
     }
 }
