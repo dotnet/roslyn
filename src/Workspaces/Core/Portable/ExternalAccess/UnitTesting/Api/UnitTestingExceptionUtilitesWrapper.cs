60a39cc06d5bada9877160690ee797b1c6b85962
Shen Chen <shech@microsoft.com>
2019-10-15 14:09:17 -0700

    Remove the dependency on ServicehubBase

---
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/UnitTestingExceptionUtilitesWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
+{
+    internal static class UnitTestingExceptionUtilitesWrapper
+    {
+        public static Exception Unreachable => ExceptionUtilities.Unreachable;
+    }
+}
--- src/Workspaces/Remote/ServiceHub/ExternalAccess/UnitTesting/Api/UnitTestingPinnedSolutionInfoWrapper.cs
+++ src/Workspaces/Remote/ServiceHub/ExternalAccess/UnitTesting/Api/UnitTestingPinnedSolutionInfoWrapper.cs
@@ -1,17 +1,20 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using Microsoft.CodeAnalysis.Execution;
+using Microsoft.CodeAnalysis.Remote;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
 {
     internal readonly struct UnitTestingPinnedSolutionInfoWrapper
     {
-        internal UnitTestingPinnedSolutionInfoWrapper(PinnedSolutionInfo underlyingObject)
-            => UnderlyingObject = underlyingObject ?? throw new ArgumentNullException(nameof(underlyingObject));
-
         public UnitTestingPinnedSolutionInfoWrapper(object underlyingObject)
-            => UnderlyingObject = (PinnedSolutionInfo)underlyingObject;
+        {
+            var reader = ((JObject)underlyingObject).CreateReader();
+            var serializer = JsonSerializer.Create(new JsonSerializerSettings() { Converters = new[] { AggregateJsonConverter.Instance } });
+            UnderlyingObject = serializer.Deserialize<PinnedSolutionInfo>(reader);
+        }
 
         internal PinnedSolutionInfo UnderlyingObject { get; }
     }
