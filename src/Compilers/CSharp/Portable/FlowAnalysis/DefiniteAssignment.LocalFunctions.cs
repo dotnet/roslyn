4017ba9b52caeeafd3d3996f3e096406833c6f34
Andy Gocke <angocke@microsoft.com>
2019-10-14 19:27:24 -0700

    Move much of local function data flow to AbstractFlowPass
    Our design for local function flow analysis is now pretty well decided
    -- local functions are assumed to preserve state transitions through
    calls within the method.
    
    For definite assignment, this means that variables which are definitely
    assigned inside the local function are assigned on calls to the local
    function. This functionality has mostly been moved to AbstractFlowPass.
    
    The remaining functionality, namely producing diagnostics based on the
    flow state, is analysis-specific, so I've refactored this to remain in
    definite assignment, but use stubs that are present in AbstractFlowPass.

--- src/Compilers/CSharp/Portable/FlowAnalysis/DefiniteAssignment.LocalFunctions.cs
+++ src/Compilers/CSharp/Portable/FlowAnalysis/DefiniteAssignment.LocalFunctions.cs
@@ -1,60 +1,33 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using Microsoft.CodeAnalysis.CSharp.Symbols;
-using Roslyn.Utilities;
-using System;
-using System.Collections.Immutable;
 using System.Diagnostics;
 
 namespace Microsoft.CodeAnalysis.CSharp
 {
     internal partial class DefiniteAssignmentPass
     {
-        private readonly SmallDictionary<LocalFunctionSymbol, LocalFuncUsages> _localFuncVarUsages =
-            new SmallDictionary<LocalFunctionSymbol, LocalFuncUsages>();
-
-        private class LocalFuncUsages
+        internal sealed class LocalFunctionState : AbstractLocalFunctionState
         {
             public BitVector ReadVars = BitVector.Empty;
-            public LocalState WrittenVars;
+            public ref LocalState WrittenVars => ref StateFromBottom;
 
-            public LocalFuncUsages(LocalState unreachableState)
-            {
-                // If we have yet to analyze the local function
-                // definition, assume it definitely assigns everything
-                WrittenVars = unreachableState;
-            }
-
-            public bool LocalFuncVisited { get; set; } = false;
+            public LocalFunctionState(LocalState unreachableState)
+                : base(unreachableState)
+            { }
         }
 
+        protected override LocalFunctionState CreateLocalFunctionState() => new LocalFunctionState(UnreachableState());
+
         /// <summary>
-        /// At the local function's use site, checks that all variables read
-        /// are assigned and assigns all variables that are definitely assigned
-        /// to be definitely assigned.
+        /// At the local function's use site, checks that all variables read are assigned.
         /// </summary>
-        private void ReplayReadsAndWrites(LocalFunctionSymbol localFunc,
-                                          SyntaxNode syntax,
-                                          bool writes)
+        protected override void VisitLocalFunctionUse(LocalFunctionSymbol localFunc, LocalFunctionState localFunctionState, SyntaxNode syntax)
         {
             _usedLocalFunctions.Add(localFunc);
 
-            var usages = GetOrCreateLocalFuncUsages(localFunc);
+            var reads = localFunctionState.ReadVars;
 
-            // First process the reads
-            ReplayReads(ref usages.ReadVars, syntax);
-
-            // Now the writes
-            if (writes)
-            {
-                Meet(ref this.State, ref usages.WrittenVars);
-            }
-
-            usages.LocalFuncVisited = true;
-        }
-
-        private void ReplayReads(ref BitVector reads, SyntaxNode syntax)
-        {
             // Start at slot 1 (slot 0 just indicates reachability)
             for (int slot = 1; slot < reads.Capacity; slot++)
             {
@@ -72,7 +45,7 @@ namespace Microsoft.CodeAnalysis.CSharp
         /// </summary>
         /// <remarks>
         /// Specifying the slot manually may be necessary if the symbol is a field,
-        /// in which case <see cref="LocalDataFlowPass{TLocalState}.VariableSlot(Symbol, int)"/>
+        /// in which case <see cref="LocalDataFlowPass{TLocalState, TLocalFunctionState}.VariableSlot(Symbol, int)"/>
         /// will not know which containing slot to look for.
         /// </remarks>
         private void CheckIfAssignedDuringLocalFunctionReplay(Symbol symbol, SyntaxNode node, int slot)
@@ -116,103 +89,9 @@ namespace Microsoft.CodeAnalysis.CSharp
             }
         }
 
-        public override BoundNode VisitLocalFunctionStatement(BoundLocalFunctionStatement localFunc)
-        {
-            var oldSymbol = this.currentSymbol;
-            var localFuncSymbol = localFunc.Symbol;
-            this.currentSymbol = localFuncSymbol;
-
-            var oldPending = SavePending(); // we do not support branches into a lambda
-
-            // SPEC: The entry point to a local function is always reachable.
-            // Captured variables are definitely assigned if they are definitely assigned on
-            // all branches into the local function.
-
-            var savedState = this.State;
-            this.State = this.TopState();
-
-            if (!localFunc.WasCompilerGenerated) EnterParameters(localFuncSymbol.Parameters);
-
-            // Captured variables are definitely assigned if they are assigned on
-            // all branches into the local function, so we store all reads from
-            // possibly unassigned captured variables and later report definite
-            // assignment errors if any of the captured variables is not assigned
-            // on a particular branch.
-            //
-            // Assignments to captured variables are also recorded, as calls to local functions
-            // definitely assign captured variables if the variables are definitely assigned at
-            // all branches out of the local function.
-
-            var usages = GetOrCreateLocalFuncUsages(localFuncSymbol);
-            var oldReads = usages.ReadVars.Clone();
-            usages.ReadVars.Clear();
-
-            var oldPending2 = SavePending();
-
-            // If this is an iterator, there's an implicit branch before the first statement
-            // of the function where the enumerable is returned.
-            if (localFuncSymbol.IsIterator)
-            {
-                PendingBranches.Add(new PendingBranch(null, this.State, null));
-            }
-
-            VisitAlways(localFunc.Body);
-            RestorePending(oldPending2); // process any forward branches within the lambda body
-            ImmutableArray<PendingBranch> pendingReturns = RemoveReturns();
-            RestorePending(oldPending);
-
-            Location location = null;
-
-            if (!localFuncSymbol.Locations.IsDefaultOrEmpty)
-            {
-                location = localFuncSymbol.Locations[0];
-            }
-
-            LeaveParameters(localFuncSymbol.Parameters, localFunc.Syntax, location);
-
-            // Intersect the state of all branches out of the local function
-            LocalState stateAtReturn = this.State;
-            foreach (PendingBranch pending in pendingReturns)
-            {
-                this.State = pending.State;
-                BoundNode branch = pending.Branch;
-
-                // Pass the local function identifier as a location if the branch
-                // is null or compiler generated.
-                LeaveParameters(localFuncSymbol.Parameters,
-                  branch?.Syntax,
-                  branch?.WasCompilerGenerated == false ? null : location);
-
-                Join(ref stateAtReturn, ref this.State);
-            }
-
-            // Check for changes to the possibly unassigned and assigned sets
-            // of captured variables
-            if (RecordChangedVars(ref usages.WrittenVars,
-                                  ref stateAtReturn,
-                                  ref oldReads,
-                                  ref usages.ReadVars) &&
-                usages.LocalFuncVisited)
-            {
-                // If the sets have changed and we already used the results
-                // of this local function in another computation, the previous
-                // calculations may be invalid. We need to analyze until we
-                // reach a fixed-point. The previous writes are always valid,
-                // so they are stored in usages.WrittenVars, while the reads
-                // may be invalidated by new writes, so we throw the results out.
-                stateChangedAfterUse = true;
-                usages.LocalFuncVisited = false;
-            }
-
-            this.State = savedState;
-            this.currentSymbol = oldSymbol;
-
-            return null;
-        }
-
         private void RecordReadInLocalFunction(int slot)
         {
-            var localFunc = GetNearestLocalFunctionOpt(currentSymbol);
+            var localFunc = GetNearestLocalFunctionOpt(CurrentSymbol);
 
             Debug.Assert(localFunc != null);
 
@@ -243,30 +122,6 @@ namespace Microsoft.CodeAnalysis.CSharp
             }
         }
 
-        private bool RecordChangedVars(ref LocalState oldWrites,
-                                       ref LocalState newWrites,
-                                       ref BitVector oldReads,
-                                       ref BitVector newReads)
-        {
-            bool anyChanged = Join(ref oldWrites, ref newWrites);
-
-            anyChanged |= RecordCapturedChanges(ref oldReads, ref newReads);
-
-            return anyChanged;
-        }
-
-        private bool RecordCapturedChanges(ref BitVector oldState,
-                                           ref BitVector newState)
-        {
-            // Build a list of variables that are both captured and assigned
-            var capturedMask = GetCapturedBitmask(ref newState);
-            var capturedAndSet = newState;
-            capturedAndSet.IntersectWith(capturedMask);
-
-            // Union and check to see if there are any changes
-            return oldState.UnionWith(capturedAndSet);
-        }
-
         private BitVector GetCapturedBitmask(ref BitVector state)
         {
             BitVector mask = BitVector.Empty;
@@ -294,19 +149,9 @@ namespace Microsoft.CodeAnalysis.CSharp
             // A variable is captured in a local function iff its
             // container is higher in the tree than the nearest
             // local function
-            var nearestLocalFunc = GetNearestLocalFunctionOpt(currentSymbol);
+            var nearestLocalFunc = GetNearestLocalFunctionOpt(CurrentSymbol);
 
             return !(nearestLocalFunc is null) && Symbol.IsCaptured(rootSymbol, nearestLocalFunc);
-        }
-
-        private LocalFuncUsages GetOrCreateLocalFuncUsages(LocalFunctionSymbol localFunc)
-        {
-            LocalFuncUsages usages;
-            if (!_localFuncVarUsages.TryGetValue(localFunc, out usages))
-            {
-                usages = _localFuncVarUsages[localFunc] = new LocalFuncUsages(UnreachableState());
-            }
-            return usages;
         }
 
         private static LocalFunctionSymbol GetNearestLocalFunctionOpt(Symbol symbol)
@@ -321,6 +166,38 @@ namespace Microsoft.CodeAnalysis.CSharp
                 symbol = symbol.ContainingSymbol;
             }
             return null;
+        }
+
+        protected override LocalFunctionState LocalFunctionStart(LocalFunctionState startState)
+        {
+            // Captured variables are definitely assigned if they are assigned on
+            // all branches into the local function, so we store all reads from
+            // possibly unassigned captured variables and later report definite
+            // assignment errors if any of the captured variables is not assigned
+            // on a particular branch.
+
+            var savedState = new LocalFunctionState(UnreachableState());
+            savedState.ReadVars = startState.ReadVars.Clone();
+            startState.ReadVars.Clear();
+            return savedState;
+        }
+
+        /// <summary>
+        /// State changes are handled by the base class. We override to find captured variables that
+        /// have been read before they were assigned and determine if the set has changed.
+        /// </summary>
+        protected override bool LocalFunctionEnd(
+            LocalFunctionState savedState,
+            LocalFunctionState currentState,
+            ref LocalState stateAtReturn)
+        {
+            // Build a list of variables that are both captured and read before assignment
+            var capturedMask = GetCapturedBitmask(ref currentState.ReadVars);
+            var capturedAndRead = currentState.ReadVars;
+            capturedAndRead.IntersectWith(capturedMask);
+
+            // Union and check to see if there are any changes
+            return savedState.ReadVars.UnionWith(capturedAndRead);
         }
     }
 }
