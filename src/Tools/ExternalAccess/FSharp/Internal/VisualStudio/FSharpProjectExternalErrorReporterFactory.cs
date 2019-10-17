21ace95431c033b26fb579866cbd3ddbec39b4e7
Tomas Matousek <tomat@microsoft.com>
2019-10-17 16:12:15 -0700

    Add FSharpProjectExternalErrorReporterFactory

---
+++ src/Tools/ExternalAccess/FSharp/Internal/VisualStudio/FSharpProjectExternalErrorReporterFactory.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.LanguageServices;
+using Microsoft.VisualStudio.LanguageServices.Implementation.TaskList;
+using Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem;
+using Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem.Extensions;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Internal.VisualStudio
+{
+    internal static class FSharpProjectExternalErrorReporterFactory
+    {
+        public static IVsLanguageServiceBuildErrorReporter2 Create(ProjectId projectId, string errorCodePrefix, IServiceProvider serviceProvider)
+            => new ProjectExternalErrorReporter(projectId, errorCodePrefix, (VisualStudioWorkspaceImpl)serviceProvider.GetMefService<VisualStudioWorkspace>());
+    }
+}
