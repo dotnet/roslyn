085990f1c0ae30f500e2e42d8e5851baa1b3bc9c
Shen Chen <Cosifne@users.noreply.github.com>
2019-10-23 15:09:49 -0700

    Remove the workspace IVT of SourceBasedTestDiscovery and LiveUnitTesting (#38466)
    * Add solution crawler wrapper types
    
    * Add Experimentation wrapper
    
    * Add UnitTestingPinnedSolutionInfoWrapper
    
    * Add KeepAliveSessionWrapper
    
    * Add PersistentStorageLocationService wrapper
    
    * Add UnitTestingWorkspaceTaskSchedule wrapper
    
    * Add extension wrapper
    
    * Change Workspace wrapper to an extension class
    
    * Add more wrapper types
    
    * Rename and remove files
    
    * Add immutable array extensions
    
    * remove lut ivt
    
    * Remove the dependency on ServicehubBase
    
    * Remove all the dependency for LUT
    
    * Rename and remove unused wrappers
    
    * Add comments
    
    * Rename wrappers and accessors
    
    * More renaming

---
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/UnitTestingKeepAliveSessionWrapper.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Remote;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
+{
+    internal readonly struct UnitTestingKeepAliveSessionWrapper
+    {
+        internal UnitTestingKeepAliveSessionWrapper(KeepAliveSession underlyingObject)
+            => UnderlyingObject = underlyingObject ?? throw new ArgumentNullException(nameof(underlyingObject));
+
+        internal KeepAliveSession UnderlyingObject { get; }
+
+        public Task<T> TryInvokeAsync<T>(string targetName, Solution solution, IReadOnlyList<object> arguments, CancellationToken cancellationToken)
+            => UnderlyingObject.TryInvokeAsync<T>(targetName, solution, arguments, cancellationToken);
+
+        public Task<bool> TryInvokeAsync(string targetName, IReadOnlyList<object> arguments, CancellationToken cancellationToken)
+            => UnderlyingObject.TryInvokeAsync(targetName, arguments, cancellationToken);
+
+        public Task<bool> TryInvokeAsync(string targetName, Solution solution, IReadOnlyList<object> arguments, CancellationToken cancellationToken)
+            => UnderlyingObject.TryInvokeAsync(targetName, solution, arguments, cancellationToken);
+
+        public Task<T> TryInvokeAsync<T>(string targetName, IReadOnlyList<object> arguments, CancellationToken cancellationToken)
+            => UnderlyingObject.TryInvokeAsync<T>(targetName, arguments, cancellationToken);
+    }
+}
