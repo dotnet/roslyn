82854e6f8bb7f6c2fa98c061253a7c784c3c8401
Manish Vasani <mavasani@microsoft.com>
2019-11-07 16:37:42 -0800

    Add some more missing nullable annotations found after building the entire solution

--- src/Utilities/Compiler/CodeMetrics/ComputationalComplexityMetrics.cs
+++ src/Utilities/Compiler/CodeMetrics/ComputationalComplexityMetrics.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Operations;
--- src/Utilities/Compiler/Extensions/CompilationExtensions.cs
+++ src/Utilities/Compiler/Extensions/CompilationExtensions.cs
@@ -1,5 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Linq;
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis;
 
 namespace Analyzer.Utilities.Extensions
