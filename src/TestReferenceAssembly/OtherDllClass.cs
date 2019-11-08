fc91aac46b3756ed82ea29d5eefcd1610a2b6d80
Manish Vasani <mavasani@microsoft.com>
2019-11-07 16:12:53 -0800

    Disable nullable in TestReferenceAssembly

--- src/TestReferenceAssembly/OtherDllClass.cs
+++ src/TestReferenceAssembly/OtherDllClass.cs
@@ -1,5 +1,8 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+// TODO(dotpaul): Enable nullable analysis.
+#nullable disable
+
 #pragma warning disable CA1801 // Remove unused parameter
 #pragma warning disable IDE0060 // Remove unused parameter
 
