2d9dfa9fc8e0058f3ca22c8f4bf57c3e50b349d3
Ivan Basov <basov@hotmail.com>
2019-11-12 15:54:12 -0800

    form alignment. host control

--- src/VisualStudio/Core/Def/Implementation/ChangeSignature/AddParameterDialogViewModel.cs
+++ src/VisualStudio/Core/Def/Implementation/ChangeSignature/AddParameterDialogViewModel.cs
@@ -1,20 +1,16 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Windows.Input;
 using Microsoft.VisualStudio.LanguageServices.Implementation.Utilities;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature
 {
     internal class AddParameterDialogViewModel : AbstractNotifyPropertyChanged
     {
-        public AddParameterDialogViewModel(ParameterTypeEditorControl parameterTypeEditorControl)
-        {
-            TypeNameEditorControl = parameterTypeEditorControl;
-        }
-
         public string ParameterName { get; set; }
         public string CallsiteValue { get; set; }
 
-        public ParameterTypeEditorControl TypeNameEditorControl { get; }
+        public string TypeName { get; set; }
 
         internal bool TrySubmit()
         {
