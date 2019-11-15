d4f30485f19e975cdee9b04209d622e9f5ce9a6b
Andrew Arnott <andrewarnott@gmail.com>
2019-11-14 21:43:54 -0700

    Capture exit codes within init.cmd from ps1 script

--- init.cmd
+++ init.cmd
@@ -1,3 +1,4 @@
-@set PS1UnderCmd=1
-powershell.exe -ExecutionPolicy bypass -Command "& '%~dpn0.ps1'" %*
-@set PS1UnderCmd=
+@echo off
+SETLOCAL
+set PS1UnderCmd=1
+powershell.exe -NoProfile -NoLogo -ExecutionPolicy bypass -Command "try { & '%~dpn0.ps1' %*; exit $LASTEXITCODE } catch { write-host $_; exit 1 }"
