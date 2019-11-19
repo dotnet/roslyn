542e210ecb49bfba4fc953c3f1204ac383120957
Kris <kj.tokarz@gmail.com>
2019-11-18 19:02:42 -0500

    Add additional tests to confirm the other refactorings are offered
    These tests confirm that one when one of the refactoring offerings has
    been choosen if the user asks for more refactorings they are offered
    the other two options to rewrite the initializer.

--- src/EditorFeatures/CSharpTest/Wrapping/InitializerExpressionWrappingTests.cs
+++ src/EditorFeatures/CSharpTest/Wrapping/InitializerExpressionWrappingTests.cs
@@ -90,5 +90,69 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Wrapping
 }"
 );
         }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsWrapping)]
+        public async Task TestShortInitializerExpressionRefactorings()
+        {
+            await TestAllWrappingCasesAsync(
+@"class C {
+    void Bar() {
+        var test = new[]
+        [||]{
+            1,
+            2
+        };
+    }
+}",
+@"class C {
+    void Bar() {
+        var test = new[] { 1, 2 };
+    }
+}",
+@"class C {
+    void Bar() {
+        var test = new[]
+        {
+            1, 2
+        };
+    }
+}");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsWrapping)]
+        public async Task TestLongIntializerExpressionRefactorings()
+        {
+            await TestAllWrappingCasesAsync(
+@"class C {
+    void Bar() {
+        var test = new[]
+        [||]{
+            ""the"", ""quick"", ""brown"", ""fox"", ""jumps"", ""over"", ""the"", ""lazy"", ""dog""
+        };
+     }
+}",
+@"class C {
+    void Bar() {
+        var test = new[]
+        {
+            ""the"",
+            ""quick"",
+            ""brown"",
+            ""fox"",
+            ""jumps"",
+            ""over"",
+            ""the"",
+            ""lazy"",
+            ""dog""
+        };
+     }
+}",
+@"class C {
+    void Bar() {
+        var test = new[] { ""the"", ""quick"", ""brown"", ""fox"", ""jumps"", ""over"", ""the"", ""lazy"", ""dog"" };
+     }
+}"
+);
+        }
     }
 }
