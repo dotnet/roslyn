33b354d75216cd5643021dc0be6fb1343b4cee9c
Andrew Hall (METAL) <andrha@microsoft.com>
2019-11-21 14:03:33 -0800

    Use document as argument instead of documentId. Update name of API

--- src/Workspaces/Core/Portable/Workspace/Host/DocumentService/IDocumentRefactoringService.cs
+++ src/Workspaces/Core/Portable/Workspace/Host/DocumentService/IDocumentRefactoringService.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,17 +8,9 @@ namespace Microsoft.CodeAnalysis.Host
     public interface IDocumentRefactoringService : IWorkspaceService
     {
         /// <summary>
-        /// Attempts to sync a named type name and namespace based on file path. <paramref name="previousFilePath" /> is
-        /// used to determine how the policy might be applied, assuming that matching types with previous
-        /// indicates a user wants the type to match with the new location or name.
-        /// May fail if:
-        /// - The appropriate type for naming policy cannot be found
-        /// - The type is partial and spread across multiple files
-        /// - The solution changes during the operation
+        /// Updates the contents of a document after some document info change, such as file name or path. May
+        /// prompt a user for confirmation on the changes.
         /// </summary>
-        /// <returns> 
-        /// Returns false if the symbol update failed, true if it succeeded
-        /// </returns>
-        Task<bool> TryUpdateNamedTypeToMatchFilePath(DocumentId documentId, string previousFilePath, CancellationToken cancellation = default);
+        Task UpdateAfterInfoChangeAsync(Document current, Document previous, CancellationToken cancellation = default);
     }
 }
