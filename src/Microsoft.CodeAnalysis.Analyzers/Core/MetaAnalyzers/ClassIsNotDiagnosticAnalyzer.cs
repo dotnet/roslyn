1c3c577b7c8c3110ea0f2092df27f6ab0c727631
Evangelink <evangelink@gmail.com>
2019-11-29 15:28:53 +0100

    New analyzer for Class is not a DiagnosticAnalyzer

---
+++ src/Microsoft.CodeAnalysis.Analyzers/Core/MetaAnalyzers/ClassIsNotDiagnosticAnalyzer.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.Analyzers.MetaAnalyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class ClassIsNotDiagnosticAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly LocalizableString s_localizableTitleNotDiagnosticAnalyzer = new LocalizableResourceString(nameof(CodeAnalysisDiagnosticsResources.ClassIsNotDiagnosticAnalyzerTitle), CodeAnalysisDiagnosticsResources.ResourceManager, typeof(CodeAnalysisDiagnosticsResources));
+        private static readonly LocalizableString s_localizableMessageNotDiagnosticAnalyzer = new LocalizableResourceString(nameof(CodeAnalysisDiagnosticsResources.ClassIsNotDiagnosticAnalyzerMessage), CodeAnalysisDiagnosticsResources.ResourceManager, typeof(CodeAnalysisDiagnosticsResources));
+        private static readonly LocalizableString s_localizableDescriptionNotDiagnosticAnalyzer = new LocalizableResourceString(nameof(CodeAnalysisDiagnosticsResources.ClassIsNotDiagnosticAnalyzerDescription), CodeAnalysisDiagnosticsResources.ResourceManager, typeof(CodeAnalysisDiagnosticsResources));
+
+        public static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticIds.TypeIsNotDiagnosticAnalyzerRuleId,
+            s_localizableTitleNotDiagnosticAnalyzer,
+            s_localizableMessageNotDiagnosticAnalyzer,
+            DiagnosticCategory.MicrosoftCodeAnalysisCorrectness,
+            DiagnosticHelpers.DefaultDiagnosticSeverity,
+            isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+            description: s_localizableDescriptionNotDiagnosticAnalyzer,
+            customTags: WellKnownDiagnosticTags.Telemetry);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterCompilationStartAction(csac =>
+            {
+                var diagnosticAnalyzer = csac.Compilation.GetOrCreateTypeByMetadataName(DiagnosticAnalyzerCorrectnessAnalyzer.DiagnosticAnalyzerTypeFullName);
+                var diagnosticAnalyzerAttribute = csac.Compilation.GetOrCreateTypeByMetadataName(DiagnosticAnalyzerCorrectnessAnalyzer.DiagnosticAnalyzerAttributeFullName);
+
+                if (diagnosticAnalyzer == null || diagnosticAnalyzerAttribute == null)
+                {
+                    // We don't need to check assemblies unless they're referencing Microsoft.CodeAnalysis which defines DiagnosticAnalyzer.
+                    return;
+                }
+
+                csac.RegisterSymbolAction(sac =>
+                {
+                    var namedType = (INamedTypeSymbol)sac.Symbol;
+
+                    if (namedType.TypeKind == TypeKind.Class &&
+                        namedType.GetAttributes().Any(a => a.AttributeClass.Equals(diagnosticAnalyzerAttribute)) &&
+                        !namedType.GetBaseTypes().Any(type => type.Equals(diagnosticAnalyzer)))
+                    {
+                        sac.ReportDiagnostic(namedType.Locations[0].CreateDiagnostic(Rule));
+                    }
+                }, SymbolKind.NamedType);
+            });
+        }
+    }
+}
---
+++ src/Microsoft.CodeAnalysis.Analyzers/UnitTests/MetaAnalyzers/ClassIsNotDiagnosticAnalyzerTests.cs
@@ -0,0 +1,164 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.CodeAnalysis.Analyzers.MetaAnalyzers.ClassIsNotDiagnosticAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.CodeAnalysis.Analyzers.MetaAnalyzers.ClassIsNotDiagnosticAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.CodeAnalysis.Analyzers.UnitTests.MetaAnalyzers
+{
+    public class ClassIsNotDiagnosticAnalyzerTests
+    {
+        [Fact]
+        public async Task ClassNotDiagnosticAnalyzer_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+namespace RoslynSandbox
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class Foo
+    {
+    }
+}",
+                VerifyCS.Diagnostic().WithLocation(8, 20));
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Diagnostics
+
+Namespace RoslynSandbox
+    <DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+    Friend Class Foo
+    End Class
+End Namespace",
+                VerifyVB.Diagnostic().WithLocation(7, 18));
+        }
+
+        [Fact]
+        public async Task StaticClassNotDiagnosticAnalyzer_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+namespace RoslynSandbox
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal static class Foo
+    {
+    }
+}",
+                VerifyCS.Diagnostic().WithLocation(8, 27));
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Diagnostics
+
+Namespace RoslynSandbox
+    <DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+    Friend Module Foo
+    End Module
+End Namespace");
+        }
+
+        [Fact]
+        public async Task ClassDiagnosticAnalyzer_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+namespace RoslynSandbox
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class Foo : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new System.NotImplementedException();
+
+        public override void Initialize(AnalysisContext context) => throw new System.NotImplementedException();
+    }
+}");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System.Collections.Immutable
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Diagnostics
+
+Namespace RoslynSandbox
+    <DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+    Friend Class Foo
+        Inherits DiagnosticAnalyzer
+
+        Public Overrides ReadOnly Property SupportedDiagnostics As ImmutableArray(Of DiagnosticDescriptor)
+            Get
+                Throw New System.NotImplementedException()
+            End Get
+        End Property
+
+        Public Overrides Sub Initialize(ByVal context As AnalysisContext)
+            Throw New System.NotImplementedException()
+        End Sub
+    End Class
+End Namespace");
+        }
+
+        [Fact]
+        public async Task ClassInheritsClassDiagnosticAnalyzer_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+namespace RoslynSandbox
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    internal abstract class FooBase : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new System.NotImplementedException();
+
+        public override void Initialize(AnalysisContext context) => throw new System.NotImplementedException();
+    }
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class Foo : FooBase
+    {
+    }
+}");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System.Collections.Immutable
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Diagnostics
+
+Namespace RoslynSandbox
+    Friend MustInherit Class FooBase
+        Inherits DiagnosticAnalyzer
+
+        Public Overrides ReadOnly Property SupportedDiagnostics As ImmutableArray(Of DiagnosticDescriptor)
+            Get
+                Throw New System.NotImplementedException()
+            End Get
+        End Property
+
+        Public Overrides Sub Initialize(ByVal context As AnalysisContext)
+            Throw New System.NotImplementedException()
+        End Sub
+    End Class
+
+
+    <DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+    Friend Class Foo
+        Inherits FooBase
+    End Class
+End Namespace");
+        }
+    }
+}
