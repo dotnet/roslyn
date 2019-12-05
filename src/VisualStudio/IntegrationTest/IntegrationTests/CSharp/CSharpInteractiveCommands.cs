8819170f27b976aeaffb7147e172baea15d87ac3
Joey Robichaud <joseph.robichaud@microsoft.com>
2019-12-05 09:04:12 -0800

    Skip failing C# REPL integration tests.
    After CI image was updated with 16.4 Preview 5 some REPL tests started
    failing consistently. https://github.com/dotnet/roslyn/issues/40160

--- src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpInteractiveCommands.cs
+++ src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpInteractiveCommands.cs
@@ -46,7 +46,7 @@ namespace Roslyn.VisualStudio.IntegrationTests.CSharp
             VisualStudio.InteractiveWindow.WaitForLastReplOutput("5");
         }
 
-        [WpfFact]
+        [WpfFact(Skip = "https://github.com/dotnet/roslyn/issues/40160")]
         public void VerifyNewLineAndIndent()
         {
             VisualStudio.InteractiveWindow.InsertCode("3 + ");
@@ -73,7 +73,7 @@ namespace Roslyn.VisualStudio.IntegrationTests.CSharp
             VisualStudio.InteractiveWindow.Verify.ReplPromptConsistency("<![CDATA[1 + 2 + 3]]>", "6");
         }
 
-        [WpfFact]
+        [WpfFact(Skip = "https://github.com/dotnet/roslyn/issues/40160")]
         public void VerifyCancelInput()
         {
             VisualStudio.InteractiveWindow.InsertCode("1 + 4");
@@ -82,7 +82,7 @@ namespace Roslyn.VisualStudio.IntegrationTests.CSharp
             VisualStudio.InteractiveWindow.Verify.LastReplInput(string.Empty);
         }
 
-        [WpfFact]
+        [WpfFact(Skip = "https://github.com/dotnet/roslyn/issues/40160")]
         public void VerifyUndoAndRedo()
         {
             VisualStudio.InteractiveWindow.ClearReplText();
@@ -95,7 +95,7 @@ namespace Roslyn.VisualStudio.IntegrationTests.CSharp
             VisualStudio.InteractiveWindow.WaitForLastReplOutput("6");
         }
 
-        [WpfFact]
+        [WpfFact(Skip = "https://github.com/dotnet/roslyn/issues/40160")]
         public void CutDeletePasteSelectAll()
         {
             ClearInteractiveWindow();
@@ -132,7 +132,7 @@ namespace Roslyn.VisualStudio.IntegrationTests.CSharp
         //     of these tests and convert them to unit-tests.
         //     -->
         //<!-- TODO(https://github.com/dotnet/roslyn/issues/4235)
-        [WpfFact]
+        [WpfFact(Skip = "https://github.com/dotnet/roslyn/issues/40160")]
         public void VerifyReturnIndentCurrentLine()
         {
             VisualStudio.InteractiveWindow.ClearScreen();
