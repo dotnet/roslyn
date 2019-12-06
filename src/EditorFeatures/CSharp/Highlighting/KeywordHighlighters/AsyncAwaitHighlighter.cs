e55e787a84e4b2b51fdfdb229d3d16fbc6183c05
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-12-06 12:49:03 -0800

    Use stack

--- src/EditorFeatures/CSharp/Highlighting/KeywordHighlighters/AsyncAwaitHighlighter.cs
+++ src/EditorFeatures/CSharp/Highlighting/KeywordHighlighters/AsyncAwaitHighlighter.cs
@@ -4,12 +4,14 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Extensions;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editor.Implementation.Highlighting;
+using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.KeywordHighlighting.KeywordHighlighters
@@ -17,6 +19,9 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.KeywordHighlighting.KeywordHighli
     [ExportHighlighter(LanguageNames.CSharp)]
     internal class AsyncAwaitHighlighter : AbstractKeywordHighlighter
     {
+        private static readonly ObjectPool<Stack<SyntaxNode>> s_stackPool
+            = new ObjectPool<Stack<SyntaxNode>>(() => new Stack<SyntaxNode>());
+
         [ImportingConstructor]
         public AsyncAwaitHighlighter()
         {
@@ -28,11 +33,38 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.KeywordHighlighting.KeywordHighli
         protected override IEnumerable<TextSpan> GetHighlightsForNode(SyntaxNode node, CancellationToken cancellationToken)
         {
             var spans = new List<TextSpan>();
-            foreach (var child in node.DescendantNodesAndSelf(n => !n.IsReturnableConstruct()))
+
+            foreach (var current in WalkChildren(node))
             {
-                HighlightRelatedKeywords(node, spans);
+                cancellationToken.ThrowIfCancellationRequested();
+                HighlightRelatedKeywords(current, spans);
             }
+
             return spans;
+        }
+
+        private IEnumerable<SyntaxNode> WalkChildren(SyntaxNode node)
+        {
+            using (var pooledObject = s_stackPool.GetPooledObject())
+            {
+                var stack = pooledObject.Object;
+                stack.Push(node);
+
+                while (stack.Count > 0)
+                {
+                    var current = stack.Pop();
+                    yield return current;
+
+                    foreach (var child in current.ChildNodes())
+                    {
+                        // Only recurse if we have anything to do
+                        if (!child.IsReturnableConstruct())
+                        {
+                            stack.Push(child);
+                        }
+                    }
+                }
+            }
         }
 
         private static void HighlightRelatedKeywords(SyntaxNode node, List<TextSpan> spans)
