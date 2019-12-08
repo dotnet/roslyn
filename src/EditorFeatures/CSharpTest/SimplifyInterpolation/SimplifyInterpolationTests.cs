af60e7523a80ea792b5d3992a2d1c146a049b615
jnm2 <me@jnm2.com>
2019-12-07 21:21:32 -0500

    Finished tests I can think of now

--- src/EditorFeatures/CSharpTest/SimplifyInterpolation/SimplifyInterpolationTests.cs
+++ src/EditorFeatures/CSharpTest/SimplifyInterpolation/SimplifyInterpolationTests.cs
@@ -199,5 +199,437 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.SimplifyInterpolation
     }
 }");
         }
+
+        [Fact]
+        public async Task PadLeftWithIntegerLiteral()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].PadLeft(|}3{|Unnecessary:)|}} suffix"";
+    }
+}",
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,-3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadRightWithIntegerLiteral()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].PadRight(|}3{|Unnecessary:)|}} suffix"";
+    }
+}",
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftWithComplexConstantExpressionRequiringParentheses()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        const int someConstant = 1;
+        _ = $""prefix {someValue{|Unnecessary:[||].PadLeft(|}(byte)3.3 + someConstant{|Unnecessary:)|}} suffix"";
+    }
+}",
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,-((byte)3.3 + someConstant)} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftWithSpaceChar()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].PadLeft(|}3{|Unnecessary:, ' ')|}} suffix"";
+    }
+}",
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,-3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadRightWithSpaceChar()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].PadRight(|}3{|Unnecessary:, ' ')|}} suffix"";
+    }
+}",
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftWithNonSpaceChar()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].PadLeft(3, '\t')} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadRightWithNonSpaceChar()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].PadRight(3, '\t')} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadRightWithComplexConstantExpression()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        const int someConstant = 1;
+        _ = $""prefix {someValue{|Unnecessary:[||].PadRight(|}(byte)3.3 + someConstant{|Unnecessary:)|}} suffix"";
+    }
+}",
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,(byte)3.3 + someConstant} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ToStringWithNoParameterWhenFormattingComponentIsSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].ToString():foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ToStringWithStringLiteralParameterWhenFormattingComponentIsSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].ToString(""bar""):foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ToStringWithNoParameterWhenAlignmentComponentIsSpecified()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].ToString()|},3} suffix"";
+    }
+}",
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ToStringWithStringLiteralParameterWhenAlignmentComponentIsSpecified()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].ToString(""|}some format code{|Unnecessary:"")|},3} suffix"";
+    }
+}",
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,3:some format code} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ToStringWithNoParameterWhenBothComponentsAreSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].ToString(),3:foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ToStringWithStringLiteralParameterWhenBothComponentsAreSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].ToString(""some format code""),3:foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftWhenFormattingComponentIsSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].PadLeft(3):foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadRightWhenFormattingComponentIsSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].PadRight(3):foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftWhenAlignmentComponentIsSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].PadLeft(3),3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadRightWhenAlignmentComponentIsSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].PadRight(3),3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftWhenBothComponentsAreSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].PadLeft(3),3:foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadRightWhenBothComponentsAreSpecified()
+        {
+            await TestMissingInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue[||].PadRight(3),3:foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ToStringWithoutFormatThenPadLeft()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].ToString().PadLeft(|}3{|Unnecessary:)|}} suffix"";
+    }
+}", @"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,-3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task ToStringWithFormatThenPadLeft()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].ToString(""|}foo{|Unnecessary:"").PadLeft(|}3{|Unnecessary:)|}} suffix"";
+    }
+}", @"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,-3:foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftThenToStringWithoutFormat()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue{|Unnecessary:[||].PadLeft(|}3{|Unnecessary:).ToString()|}} suffix"";
+    }
+}", @"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,-3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftThenToStringWithFormat()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue.PadLeft(3){|Unnecessary:[||].ToString(""|}foo{|Unnecessary:"")|}} suffix"";
+    }
+}", @"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue.PadLeft(3):foo} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftThenToStringWithoutFormatWhenAlignmentComponentIsSpecified()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue.PadLeft(3){|Unnecessary:[||].ToString()|},3} suffix"";
+    }
+}", @"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue,3} suffix"";
+    }
+}");
+        }
+
+        [Fact]
+        public async Task PadLeftThenToStringWithFormatWhenAlignmentComponentIsSpecified()
+        {
+            await TestInRegularAndScriptAsync(
+@"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue.PadLeft(3){|Unnecessary:[||].ToString(""|}foo{|Unnecessary:"")|},3} suffix"";
+    }
+}", @"class C
+{
+    void M(int someValue)
+    {
+        _ = $""prefix {someValue.PadLeft(3),3:foo} suffix"";
+    }
+}");
+        }
     }
 }
