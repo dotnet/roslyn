d965195dee7cb5e8effd179621d7d4e2e45ebd6b
Tomas Matousek <tomat@microsoft.com>
2019-12-09 17:08:03 -0800

    More annotations

---
+++ src/Workspaces/Core/Portable/Utilities/ValuesSources/OptionalValueSource.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+#nullable enable
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Roslyn.Utilities
+{
+    /// <summary>
+    /// A class that abstracts the accessing of a value that might not be available anymore when queried.
+    /// </summary>
+    internal abstract class OptionalValueSource<T> where T : class
+    {
+        public abstract bool TryGetValue(out T value);
+        public abstract T? GetValue(CancellationToken cancellationToken = default);
+        public abstract Task<T?> GetValueAsync(CancellationToken cancellationToken = default);
+
+        public bool HasValue => TryGetValue(out _);
+    }
+}
---
+++ src/Workspaces/Core/Portable/Utilities/ValuesSources/WeakValueSource.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+#nullable enable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Roslyn.Utilities
+{
+    /// <summary>
+    /// A <see cref="ValueSource{T}"/> that keeps a weak reference to a value.
+    /// </summary>
+    internal sealed class WeakValueSource<T> : OptionalValueSource<T>
+        where T : class
+    {
+        private readonly WeakReference<T> _weakValue;
+
+        public WeakValueSource(T value)
+        {
+            _weakValue = new WeakReference<T>(value);
+        }
+
+        public override bool TryGetValue(out T value)
+            => _weakValue.TryGetTarget(out value);
+
+        public override T? GetValue(CancellationToken cancellationToken)
+            => _weakValue.TryGetTarget(out var value) ? value : null;
+
+        public override Task<T?> GetValueAsync(CancellationToken cancellationToken)
+            => Task.FromResult(GetValue(cancellationToken));
+    }
+}
