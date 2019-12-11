b2bd1aa9cc32a661f9a270ec6ddc6a020ab41a55
Manish Vasani <mavasani@microsoft.com>
2019-12-10 20:41:53 -0800

    Fix build

--- src/Tools/AnalyzerRunner/DiagnosticAnalyzerRunner.cs
+++ src/Tools/AnalyzerRunner/DiagnosticAnalyzerRunner.cs
@@ -150,7 +150,7 @@ namespace AnalyzerRunner
             {
                 Compilation compilation = await processedProject.GetCompilationAsync(cancellationToken).ConfigureAwait(false);
 
-                var workspaceAnalyzerOptions = new WorkspaceAnalyzerOptions(project.AnalyzerOptions, project.Solution.Options, project.Solution);
+                var workspaceAnalyzerOptions = new WorkspaceAnalyzerOptions(project.AnalyzerOptions, project.Solution);
                 CompilationWithAnalyzers compilationWithAnalyzers = compilation.WithAnalyzers(languageAnalyzers, new CompilationWithAnalyzersOptions(workspaceAnalyzerOptions, null, analyzerOptionsInternal.RunConcurrent, logAnalyzerExecutionTime: true, reportSuppressedDiagnostics: analyzerOptionsInternal.ReportSuppressedDiagnostics));
 
                 SyntaxTree tree = await project.GetDocument(documentId).GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
