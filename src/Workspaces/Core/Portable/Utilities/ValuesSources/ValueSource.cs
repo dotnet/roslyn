3145ec983c65fefb5692b9a725d5b2269460fd7d
Rikki Gibson <rikkigibson@gmail.com>
2019-12-11 10:41:38 -0800

    Revert "Document load failures (#40044)"
    This reverts commit 6ac8006ec160efd4f8951088dae9870926a97426.

--- src/Workspaces/Core/Portable/Utilities/ValuesSources/ValueSource.cs
+++ src/Workspaces/Core/Portable/Utilities/ValuesSources/ValueSource.cs
@@ -1,8 +1,5 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-#nullable enable
-
-using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,12 +10,18 @@ namespace Roslyn.Utilities
     /// </summary>
     internal abstract class ValueSource<T>
     {
-        public abstract bool TryGetValue([MaybeNullWhen(false)]out T value);
+        public abstract bool TryGetValue(out T value);
         public abstract T GetValue(CancellationToken cancellationToken = default);
         public abstract Task<T> GetValueAsync(CancellationToken cancellationToken = default);
 
-        public bool HasValue => TryGetValue(out _);
+        public bool HasValue
+        {
+            get
+            {
+                return this.TryGetValue(out var tmp);
+            }
+        }
 
-        public static readonly ConstantValueSource<T> Empty = new ConstantValueSource<T>(default!);
+        public static readonly ConstantValueSource<T> Empty = new ConstantValueSource<T>(default);
     }
 }
