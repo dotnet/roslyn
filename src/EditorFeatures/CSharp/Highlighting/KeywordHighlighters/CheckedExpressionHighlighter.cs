7847ddb28df8e7ecdaf04f7d80d5a4c150c6511e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-12-13 13:41:52 -0800

    Simplify

--- src/EditorFeatures/CSharp/Highlighting/KeywordHighlighters/CheckedExpressionHighlighter.cs
+++ src/EditorFeatures/CSharp/Highlighting/KeywordHighlighters/CheckedExpressionHighlighter.cs
@@ -18,16 +18,7 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.KeywordHighlighting.KeywordHighli
         {
         }
 
-        protected override void AddHighlights(
-            CheckedExpressionSyntax checkedExpressionSyntax, List<TextSpan> highlights, CancellationToken cancellationToken)
-        {
-            switch (checkedExpressionSyntax.Kind())
-            {
-                case SyntaxKind.CheckedExpression:
-                case SyntaxKind.UncheckedExpression:
-                    highlights.Add(checkedExpressionSyntax.Keyword.Span);
-                    break;
-            }
-        }
+        protected override void AddHighlights(CheckedExpressionSyntax checkedExpressionSyntax, List<TextSpan> highlights, CancellationToken cancellationToken)
+            => highlights.Add(checkedExpressionSyntax.Keyword.Span);
     }
 }
