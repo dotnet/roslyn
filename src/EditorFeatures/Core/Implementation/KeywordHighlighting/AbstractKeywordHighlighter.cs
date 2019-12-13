be92d8d1f7e13a26d8ff92f9e50e00ffa71d3f69
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-12-13 14:02:53 -0800

    Simplify

--- src/EditorFeatures/Core/Implementation/KeywordHighlighting/AbstractKeywordHighlighter.cs
+++ src/EditorFeatures/Core/Implementation/KeywordHighlighting/AbstractKeywordHighlighter.cs
@@ -3,10 +3,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
-using Microsoft.CodeAnalysis.PooledObjects;
-using System;
 
 namespace Microsoft.CodeAnalysis.Editor.Implementation.Highlighting
 {
