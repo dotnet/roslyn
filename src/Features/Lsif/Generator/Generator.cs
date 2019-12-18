294d627fb6f04477217814dfea9cb3d2a6039477
Jason Malinowski <jason.malinowski@microsoft.com>
2019-12-17 16:22:02 -0800

    Write out ranges for all bindable tokens
    This is doing the binding, but isn't actually writing out anything
    interesting with it yet.

--- src/Features/Lsif/Generator/Generator.cs
+++ src/Features/Lsif/Generator/Generator.cs
@@ -2,8 +2,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.CommandLine;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.Lsif.Generator.LsifGraph;
 using Microsoft.CodeAnalysis.Lsif.Generator.Writing;
 
@@ -41,10 +43,39 @@ namespace Microsoft.CodeAnalysis.Lsif.Generator
         private Task<Id<LsifGraph.Document>> GenerateForDocument(SemanticModel semanticModel, HostLanguageServices languageServices)
         {
             var syntaxTree = semanticModel.SyntaxTree;
+            var sourceText = semanticModel.SyntaxTree.GetText();
+            var syntaxFactsService = languageServices.GetRequiredService<ISyntaxFactsService>();
+
             var documentVertex = new LsifGraph.Document(new Uri(syntaxTree.FilePath), GetLanguageKind(semanticModel.Language));
 
             _lsifJsonWriter.Write(documentVertex);
             _lsifJsonWriter.Write(new Event(Event.EventKind.Begin, documentVertex.GetId()));
+
+            // We will walk the file token-by-token, making a range for each one and then attaching information for it
+            var rangeVertices = new List<Id<LsifGraph.Range>>();
+
+            foreach (var syntaxToken in syntaxTree.GetRoot().DescendantTokens(descendIntoTrivia: true))
+            {
+                if (syntaxFactsService.IsBindableToken(syntaxToken))
+                {
+                    var bindableParent = syntaxFactsService.GetBindableParent(syntaxToken);
+
+                    if (bindableParent != null)
+                    {
+                        var symbolInfo = semanticModel.GetSymbolInfo(bindableParent);
+
+                        if (symbolInfo.Symbol != null)
+                        {
+                            var rangeVertex = LsifGraph.Range.FromTextSpan(syntaxToken.Span, sourceText);
+
+                            _lsifJsonWriter.Write(rangeVertex);
+                            rangeVertices.Add(rangeVertex.GetId());
+                        }
+                    }
+                }
+            }
+
+            _lsifJsonWriter.Write(Edge.Create("contains", documentVertex.GetId(), rangeVertices));
             _lsifJsonWriter.Write(new Event(Event.EventKind.End, documentVertex.GetId()));
 
             return Task.FromResult(documentVertex.GetId());
---
+++ src/Features/Lsif/Generator/LsifGraph/Range.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.Lsif.Generator.LsifGraph
+{
+    internal sealed class Range : Vertex
+    {
+        public Position Start { get; }
+        public Position End { get; }
+
+        public Range(Position start, Position end)
+            : base(label: "range")
+        {
+            Start = start;
+            End = end;
+        }
+
+        public static Range FromTextSpan(TextSpan textSpan, SourceText sourceText)
+        {
+            var linePositionSpan = sourceText.Lines.GetLinePositionSpan(textSpan);
+
+            return new Range(start: FromLinePositionSpan(linePositionSpan.Start), end: FromLinePositionSpan(linePositionSpan.End));
+        }
+
+        private static Position FromLinePositionSpan(LinePosition linePosition)
+        {
+            return new Position { Line = linePosition.Line, Character = linePosition.Character };
+        }
+    }
+}
