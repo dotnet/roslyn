7a2022892dec1eda83708d375547e0e5e0f9eed1
Tomáš Matoušek <tmat@users.noreply.github.com>
2019-12-19 14:19:01 -0800

    Fix nullable annotations for ValueSource (#40222)
    * Fix nullable annotations for ValueSource
    
    * Refactor WeakValueSource
    
    * Replace OptionalValueSource<T> with ValueSource<Optional<T>>

--- src/Workspaces/Core/Portable/Workspace/Host/SyntaxTreeFactory/AbstractSyntaxTreeFactoryService.AbstractRecoverableSyntaxRoot.cs
+++ src/Workspaces/Core/Portable/Workspace/Host/SyntaxTreeFactory/AbstractSyntaxTreeFactoryService.AbstractRecoverableSyntaxRoot.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+#nullable enable
+
 using System;
 using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -42,23 +45,22 @@ namespace Microsoft.CodeAnalysis.Host
                 DiagnosticOptions = diagnosticOptions;
             }
 
-            internal bool TryGetText(out SourceText text)
+            internal bool TryGetText([MaybeNullWhen(false)]out SourceText text)
             {
-                if (this.TextSource.TryGetValue(out var textAndVersion))
+                if (TextSource.TryGetValue(out var textAndVersion))
                 {
                     text = textAndVersion.Text;
                     return true;
                 }
-                else
-                {
-                    text = null;
-                    return false;
-                }
+
+                // Suppressing nullable warning due to https://github.com/dotnet/roslyn/issues/40266
+                text = null!;
+                return false;
             }
 
             internal async Task<SourceText> GetTextAsync(CancellationToken cancellationToken)
             {
-                var textAndVersion = await this.TextSource.GetValueAsync(cancellationToken).ConfigureAwait(false);
+                var textAndVersion = await TextSource.GetValueAsync(cancellationToken).ConfigureAwait(false);
                 return textAndVersion.Text;
             }
 
@@ -97,10 +99,10 @@ namespace Microsoft.CodeAnalysis.Host
             }
         }
 
-        internal sealed class RecoverableSyntaxRoot<TRoot> : RecoverableWeakValueSource<TRoot>
+        internal sealed class RecoverableSyntaxRoot<TRoot> : WeaklyCachedRecoverableValueSource<TRoot>
             where TRoot : SyntaxNode
         {
-            private ITemporaryStreamStorage _storage;
+            private ITemporaryStreamStorage? _storage;
 
             private readonly IRecoverableSyntaxTree<TRoot> _containingTree;
             private readonly AbstractSyntaxTreeFactoryService _service;
@@ -130,7 +132,7 @@ namespace Microsoft.CodeAnalysis.Host
             public RecoverableSyntaxRoot<TRoot> WithSyntaxTree(IRecoverableSyntaxTree<TRoot> containingTree)
             {
                 // at this point, we should either have strongly held root or _storage should not be null
-                if (this.TryGetValue(out var root))
+                if (TryGetValue(out var root))
                 {
                     // we have strongly held root
                     return new RecoverableSyntaxRoot<TRoot>(_service, root, containingTree);
@@ -151,7 +153,7 @@ namespace Microsoft.CodeAnalysis.Host
                 root.SerializeTo(stream, cancellationToken);
                 stream.Position = 0;
 
-                _storage = _service.LanguageServices.WorkspaceServices.GetService<ITemporaryStorageService>().CreateTemporaryStreamStorage(cancellationToken);
+                _storage = _service.LanguageServices.WorkspaceServices.GetRequiredService<ITemporaryStorageService>().CreateTemporaryStreamStorage(cancellationToken);
                 await _storage.WriteStreamAsync(stream, cancellationToken).ConfigureAwait(false);
             }
 
@@ -170,7 +172,6 @@ namespace Microsoft.CodeAnalysis.Host
             {
                 Contract.ThrowIfNull(_storage);
 
-                var tickCount = Environment.TickCount;
                 using (RoslynEventSource.LogInformationalBlock(FunctionId.Workspace_Recoverable_RecoverRoot, _containingTree.FilePath, cancellationToken))
                 {
                     using var stream = _storage.ReadStream(cancellationToken);
