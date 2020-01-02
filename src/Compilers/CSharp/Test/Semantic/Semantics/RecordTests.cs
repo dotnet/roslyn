7f3601e801af7afd84b74da527b4726c8157fa8e
Andy Gocke <angocke@microsoft.com>
2020-01-01 23:08:24 -0500

    Update test baselines

--- src/Compilers/CSharp/Test/Semantic/Semantics/RecordTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/RecordTests.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Microsoft.CodeAnalysis.Test.Utilities;
 using Xunit;
 
@@ -15,7 +16,7 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Semantics
 class Point(int x, int y);
 ";
             var src2 = @"
-data class Point;
+data class Point { }
 ";
             var src3 = @"
 data class Point(int x, int y);
@@ -24,12 +25,15 @@ data class Point(int x, int y);
             comp.VerifyDiagnostics(
                 // (2,12): error CS8652: The feature 'records' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
                 // class Point(int x, int y);
-                Diagnostic(ErrorCode.ERR_FeatureInPreview, "(int x, int y)").WithArguments("records").WithLocation(2, 12)
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "(int x, int y)").WithArguments("records").WithLocation(2, 12),
+                // (2,26): error CS8652: The feature 'records' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+                // class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, ";").WithArguments("records").WithLocation(2, 26)
             );
             comp = CreateCompilation(src2);
             comp.VerifyDiagnostics(
                 // (2,1): error CS8652: The feature 'records' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
-                // data class Point;
+                // data class Point { }
                 Diagnostic(ErrorCode.ERR_FeatureInPreview, "data").WithArguments("records").WithLocation(2, 1)
             );
             comp = CreateCompilation(src3);
@@ -39,8 +43,17 @@ data class Point(int x, int y);
                 Diagnostic(ErrorCode.ERR_FeatureInPreview, "data").WithArguments("records").WithLocation(2, 1),
                 // (2,17): error CS8652: The feature 'records' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
                 // data class Point(int x, int y);
-                Diagnostic(ErrorCode.ERR_FeatureInPreview, "(int x, int y)").WithArguments("records").WithLocation(2, 17)
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "(int x, int y)").WithArguments("records").WithLocation(2, 17),
+                // (2,31): error CS8652: The feature 'records' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+                // data class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, ";").WithArguments("records").WithLocation(2, 31)
             );
+
+            comp = CreateCompilation(src1, parseOptions: TestOptions.RegularPreview);
+            comp.VerifyDiagnostics();
+            comp = CreateCompilation(src2, parseOptions: TestOptions.RegularPreview);
+            comp.VerifyDiagnostics();
+            comp = CreateCompilation(src3, parseOptions: TestOptions.RegularPreview);
         }
     }
 }
