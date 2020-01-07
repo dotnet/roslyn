732751bbbd2172dc17519485ff59a97b396f499b
Sam Harwell <Sam.Harwell@microsoft.com>
2020-01-07 09:53:22 -0800

    Update AnalyzerRunner to include a netcoreapp3.0 target

--- src/Tools/AnalyzerRunner/Program.cs
+++ src/Tools/AnalyzerRunner/Program.cs
@@ -48,6 +48,15 @@ namespace AnalyzerRunner
             // QueryVisualStudioInstances returns Visual Studio installations on .NET Framework, and .NET Core SDK
             // installations on .NET Core. We use the one with the most recent version.
             var msBuildInstance = MSBuildLocator.QueryVisualStudioInstances().OrderByDescending(x => x.Version).First();
+
+#if NETCOREAPP
+            // Since we do not inherit msbuild.deps.json when referencing the SDK copy
+            // of MSBuild and because the SDK no longer ships with version matched assemblies, we
+            // register an assembly loader that will load assemblies from the msbuild path with
+            // equal or higher version numbers than requested.
+            LooseVersionAssemblyLoader.Register(msBuildInstance.MSBuildPath);
+#endif
+
             MSBuildLocator.RegisterInstance(msBuildInstance);
 
             var incrementalAnalyzerRunner = new IncrementalAnalyzerRunner(options);
@@ -71,6 +80,12 @@ namespace AnalyzerRunner
             Stopwatch stopwatch = Stopwatch.StartNew();
             var properties = new Dictionary<string, string>
             {
+#if NETCOREAPP
+                // This property ensures that XAML files will be compiled in the current AppDomain
+                // rather than a separate one. Any tasks isolated in AppDomains or tasks that create
+                // AppDomains will likely not work due to https://github.com/Microsoft/MSBuildLocator/issues/16.
+                { "AlwaysCompileMarkupFilesInSeparateDomain", bool.FalseString },
+#endif
                 // Use the latest language version to force the full set of available analyzers to run on the project.
                 { "LangVersion", "latest" },
             };
