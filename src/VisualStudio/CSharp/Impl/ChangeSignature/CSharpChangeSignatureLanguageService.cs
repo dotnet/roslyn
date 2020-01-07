eda5830d4a1b76b6342bcf4d900c775f4396acc4
Allison Chou <allichou@microsoft.com>
2020-01-06 17:09:59 -0800

    Add new files

---
+++ src/VisualStudio/CSharp/Impl/ChangeSignature/CSharpChangeSignatureLanguageService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.VisualStudio.LanguageServices.Implementation;
+using Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature;
+using Microsoft.VisualStudio.LanguageServices.Implementation.IntellisenseControls;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Projection;
+using Microsoft.VisualStudio.Utilities;
+using static Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature.ChangeSignatureDialogViewModel;
+
+namespace Microsoft.VisualStudio.LanguageServices.CSharp.ChangeSignature
+{
+    [ExportLanguageService(typeof(IChangeSignatureLanguageService), LanguageNames.CSharp), Shared]
+    internal class CSharpChangeSignatureLanguageService : ChangeSignatureLanguageService, IChangeSignatureLanguageService
+    {
+        public async Task<IntellisenseTextBoxViewModel[]> CreateViewModelsAsync(
+            string[] rolesCollectionType,
+            string[] rolesCollectionName,
+            int insertPosition,
+            Document document,
+            string documentText,
+            IContentType contentType,
+            IntellisenseTextBoxViewModelFactory intellisenseTextBoxViewModelFactory,
+            CancellationToken cancellationToken)
+        {
+            var rolesCollections = new[] { rolesCollectionType, rolesCollectionName };
+
+            return await intellisenseTextBoxViewModelFactory.CreateIntellisenseTextBoxViewModelsAsync(
+               document, contentType, documentText.Insert(insertPosition, ", "),
+               CreateTrackingSpans, rolesCollections, cancellationToken).ConfigureAwait(false);
+
+            ITrackingSpan[] CreateTrackingSpans(IProjectionSnapshot snapshot)
+            {
+                // Adjust the context point to ensure that the right information is in scope.
+                // For example, we may need to move the point to the end of the last statement in a method body
+                // in order to be able to access all local variables.
+                // For C#, + 1 to support inserted comma
+                return CreateTrackingSpansHelper(snapshot, contextPoint: insertPosition + 1, spaceBetweenTypeAndName: 1);
+            }
+        }
+
+        public void GeneratePreviewGrammar(AddedParameterViewModel addedParameterViewModel, List<SymbolDisplayPart> displayParts)
+        {
+            displayParts.Add(new SymbolDisplayPart(SymbolDisplayPartKind.Keyword, null, addedParameterViewModel.Type));
+            displayParts.Add(new SymbolDisplayPart(SymbolDisplayPartKind.Space, null, " "));
+            displayParts.Add(new SymbolDisplayPart(SymbolDisplayPartKind.ParameterName, null, addedParameterViewModel.Parameter)); ;
+        }
+    }
+}
---
+++ src/VisualStudio/VisualBasic/Impl/ChangeSignature/VisualBasicChangeSignatureLanguageService.vb
@@ -0,0 +1,45 @@
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports System.Composition
+Imports System.Threading
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Host.Mef
+Imports Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature
+Imports Microsoft.VisualStudio.LanguageServices.Implementation.IntellisenseControls
+Imports Microsoft.VisualStudio.Text
+Imports Microsoft.VisualStudio.Text.Projection
+Imports Microsoft.VisualStudio.Utilities
+
+Namespace Microsoft.VisualStudio.LanguageServices.VisualBasic.ChangeSignature
+    <ExportLanguageService(GetType(IChangeSignatureLanguageService), LanguageNames.VisualBasic), [Shared]>
+    Friend Class VisualBasicChangeSignatureLanguageService
+        Inherits ChangeSignatureLanguageService
+        Implements IChangeSignatureLanguageService
+        Public Async Function CreateViewModelsAsync(rolesCollectionType() As String,
+                                              rolesCollectionName() As String,
+                                              insertPosition As Integer, document As Document,
+                                              documentText As String, contentType As IContentType,
+                                              intellisenseTextBoxViewModelFactory As IntellisenseTextBoxViewModelFactory,
+                                              cancellationToken As CancellationToken) As Task(Of IntellisenseTextBoxViewModel()) Implements IChangeSignatureLanguageService.CreateViewModelsAsync
+            Dim rolesCollections = {rolesCollectionName, rolesCollectionType}
+
+            ' We insert '[]' so that we're always able to generate the type even if the name field is empty. We insert '~' to avoid Intellisense activation for the name field, since completion for
+            ' names does not apply to VB.
+            Return Await intellisenseTextBoxViewModelFactory.CreateIntellisenseTextBoxViewModelsAsync(
+                document,
+                contentType,
+                documentText.Insert(insertPosition, ", [~] As "),
+                Function(snapshot As IProjectionSnapshot)
+                    ' + 4 to support inserted ', [~'
+                    Return CreateTrackingSpansHelper(snapshot, contextPoint:=insertPosition + 4, spaceBetweenTypeAndName:=5)
+                End Function,
+                rolesCollections,
+                cancellationToken).ConfigureAwait(False)
+        End Function
+
+        Public Sub GeneratePreviewGrammar(addedParameterViewModel As ChangeSignatureDialogViewModel.AddedParameterViewModel, displayParts As List(Of SymbolDisplayPart)) Implements IChangeSignatureLanguageService.GeneratePreviewGrammar
+            displayParts.Add(New SymbolDisplayPart(SymbolDisplayPartKind.ParameterName, Nothing, addedParameterViewModel.Parameter))
+            displayParts.Add(New SymbolDisplayPart(SymbolDisplayPartKind.Keyword, Nothing, " As " + addedParameterViewModel.Type))
+        End Sub
+    End Class
+End Namespace
