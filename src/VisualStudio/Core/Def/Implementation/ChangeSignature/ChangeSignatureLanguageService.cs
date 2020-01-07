add8b2ef33a0a69bc83c1d98c2cd977e2c43b76c
Allison Chou <allichou@microsoft.com>
2020-01-06 17:09:59 -0800

    Add new files

---
+++ src/VisualStudio/Core/Def/Implementation/ChangeSignature/ChangeSignatureLanguageService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Projection;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature
+{
+    internal class ChangeSignatureLanguageService
+    {
+        protected ITrackingSpan[] CreateTrackingSpansHelper(IProjectionSnapshot snapshot, int contextPoint, int spaceBetweenTypeAndName)
+        {
+            // Get the previous span/text.
+            var previousStatementSpan = snapshot.CreateTrackingSpanFromStartToIndex(contextPoint, SpanTrackingMode.EdgeNegative);
+
+            // Get the appropriate ITrackingSpan for the window the user is typing in.
+            // mappedSpan1 is the 'Type' field for C# and 'Name' for VB.
+            var mappedSpan1 = snapshot.CreateTrackingSpan(contextPoint, 0, SpanTrackingMode.EdgeExclusive);
+
+            // Space could be either ' ' for C# or '] AS ' for VB
+            var spaceSpan = snapshot.CreateTrackingSpan(contextPoint, spaceBetweenTypeAndName, SpanTrackingMode.EdgeExclusive);
+
+            // mappedSpan2 is the 'Name' field for C# and 'Type' for VB.
+            var mappedSpan2 = snapshot.CreateTrackingSpan(contextPoint + spaceBetweenTypeAndName, 0, SpanTrackingMode.EdgeExclusive);
+
+            // Build the tracking span that includes the rest of the file.
+            var restOfFileSpan = snapshot.CreateTrackingSpanFromIndexToEnd(contextPoint + spaceBetweenTypeAndName, SpanTrackingMode.EdgePositive);
+
+            // This array as a whole should encompass the span of the entire file.
+            return new ITrackingSpan[] { previousStatementSpan, mappedSpan1, spaceSpan, mappedSpan2, restOfFileSpan };
+        }
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/ChangeSignature/IChangeSignatureLanguageService.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.VisualStudio.LanguageServices.Implementation.IntellisenseControls;
+using Microsoft.VisualStudio.Utilities;
+using static Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature.ChangeSignatureDialogViewModel;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature
+{
+    internal interface IChangeSignatureLanguageService : ILanguageService
+    {
+        Task<IntellisenseTextBoxViewModel[]> CreateViewModelsAsync(
+            string[] rolesCollectionType,
+            string[] rolesCollectionName,
+            int insertPosition,
+            Document document,
+            string documentText,
+            IContentType contentType,
+            IntellisenseTextBoxViewModelFactory intellisenseTextBoxViewModelFactory,
+            CancellationToken cancellationToken);
+
+        void GeneratePreviewGrammar(AddedParameterViewModel addedParameterViewModel, List<SymbolDisplayPart> displayParts);
+    }
+}
