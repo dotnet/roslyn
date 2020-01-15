0542f188a283df2f869bac675a1a2c3dca677601
Joey Robichaud <joseph.robichaud@microsoft.com>
2020-01-15 13:40:35 -0800

    Add color scheme reader for reading the scheme XML

---
+++ src/VisualStudio/Core/Def/Implementation/ColorSchemes/ColorSchemeApplier.ColorScheme.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+#nullable enable
+
+using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.VisualStudio.LanguageServices.ColorSchemes
+{
+    internal partial class ColorSchemeApplier
+    {
+        private class ColorScheme
+        {
+            public ImmutableArray<ColorTheme> Themes { get; }
+
+            public ColorScheme(ImmutableArray<ColorTheme> themes)
+            {
+                Themes = themes;
+            }
+        }
+
+        private class ColorTheme
+        {
+            public string Name { get; }
+            public Guid Guid { get; }
+            public ColorCategory Category { get; }
+
+            public ColorTheme(string name, Guid guid, ColorCategory category)
+            {
+                Name = name;
+                Guid = guid;
+                Category = category;
+            }
+        }
+
+        private class ColorCategory
+        {
+            public string Name { get; }
+            public Guid Guid { get; }
+            public ImmutableArray<ColorItem> Colors { get; }
+
+            public ColorCategory(string name, Guid guid, ImmutableArray<ColorItem> colors)
+            {
+                Name = name;
+                Guid = guid;
+                Colors = colors;
+            }
+        }
+
+        private class ColorItem
+        {
+            public string Name { get; }
+            public int BackgroundType { get; }
+            public uint? Background { get; }
+            public int ForegroundType { get; }
+            public uint? Foreground { get; }
+
+            public ColorItem(string name, int backgroundType, uint? background, int foregroundType, uint? foreground)
+            {
+                Name = name;
+
+                Debug.Assert(backgroundType == (int)__VSCOLORTYPE.CT_INVALID || background.HasValue);
+                BackgroundType = backgroundType;
+                Background = background;
+
+                Debug.Assert(foregroundType == (int)__VSCOLORTYPE.CT_INVALID || foreground.HasValue);
+                ForegroundType = foregroundType;
+                Foreground = foreground;
+            }
+        }
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/ColorSchemes/ColorSchemeApplier.ColorSchemeReader.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+#nullable enable
+
+using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.Shell.Interop;
+using Roslyn.Utilities;
+
+namespace Microsoft.VisualStudio.LanguageServices.ColorSchemes
+{
+    internal partial class ColorSchemeApplier
+    {
+        private static class ColorSchemeReader
+        {
+            public static ColorScheme ReadColorScheme(Stream schemeStream)
+            {
+                var schemeDocument = XDocument.Load(schemeStream);
+
+                var themes = schemeDocument
+                    .Descendants("Theme")
+                    .Select(ReadColorTheme);
+
+                return new ColorScheme(themes.ToImmutableArray());
+            }
+
+            private static ColorTheme ReadColorTheme(XElement themeElement)
+            {
+                var themeName = (string)themeElement.Attribute("Name");
+                var themeGuid = Guid.Parse((string)themeElement.Attribute("GUID"));
+
+                var categoryElement = themeElement.Descendants("Category").Single();
+                var category = ReadColorCategory(categoryElement);
+
+                return new ColorTheme(themeName, themeGuid, category);
+            }
+
+            private static ColorCategory ReadColorCategory(XElement categoryElement)
+            {
+                var categoryName = (string)categoryElement.Attribute("Name");
+                var categoryGuid = Guid.Parse((string)categoryElement.Attribute("GUID"));
+
+                var colorItems = categoryElement
+                    .Descendants("Color")
+                    .Select(ReadColorItem)
+                    .OfType<ColorItem>();
+
+                return new ColorCategory(categoryName, categoryGuid, colorItems.ToImmutableArray());
+            }
+
+            private static ColorItem? ReadColorItem(XElement colorElement)
+            {
+                var name = (string)colorElement.Attribute("Name");
+
+                var backgroundElement = colorElement.Descendants("Background").SingleOrDefault();
+                (var backgroundType, var backgroundColor) = ReadColor(backgroundElement);
+
+                var foregroundElement = colorElement.Descendants("Foreground").SingleOrDefault();
+                (var foregroundType, var foregroundColor) = ReadColor(foregroundElement);
+
+                if (backgroundElement is null && foregroundElement is null)
+                {
+                    return null;
+                }
+
+                return new ColorItem(name, backgroundType, backgroundColor, foregroundType, foregroundColor);
+            }
+
+            private static (int Type, uint? Color) ReadColor(XElement colorElement)
+            {
+                if (colorElement is null)
+                {
+                    return (0, null);
+                }
+
+                var colorType = (string)colorElement.Attribute("Type");
+                var sourceColor = (string)colorElement.Attribute("Source");
+
+                int type;
+                uint? color;
+
+                if (colorType == "CT_RAW")
+                {
+                    type = (int)__VSCOLORTYPE.CT_RAW;
+
+                    // Convert ARGB to BGR by ignoring the alpha channel and reversing byte order.
+                    var r = sourceColor.Substring(2, 2);
+                    var g = sourceColor.Substring(4, 2);
+                    var b = sourceColor.Substring(6, 2);
+                    color = uint.Parse($"{b}{g}{r}", System.Globalization.NumberStyles.HexNumber);
+                }
+                else if (colorType == "CT_SYSCOLOR")
+                {
+                    type = (int)__VSCOLORTYPE.CT_SYSCOLOR;
+
+                    color = uint.Parse(sourceColor, System.Globalization.NumberStyles.HexNumber);
+                }
+                else
+                {
+                    throw ExceptionUtilities.UnexpectedValue(colorType);
+                }
+
+                return (type, color);
+            }
+        }
+    }
+}
