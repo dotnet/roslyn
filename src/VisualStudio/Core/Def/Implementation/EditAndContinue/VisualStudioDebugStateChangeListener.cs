d794dbee7a90a5d1c6641e30d2226221577a01f3
Tomáš Matoušek <tmat@users.noreply.github.com>
2020-01-20 12:45:01 -0700

    EnC simplify reporting module diagnostics. (#40999)
    Previously we reported diagnostics caused by the debuggee not allowing edits in the corresponding module in EnC diagnostic analyzer.
    A module does not support edits if any of its instances does not support the edits. There are scenarios in which module instances may be loaded while in break mode.
    E.g. if the user attaches to a secondary process while debugging a primary process, the secondary process might have a module instance loaded of the same module loaded to the primary process.
    To handle these cases we hooked up debugger module load/unload events and invalidated the reported diagnostics.
    
    This approach has a few problems:
    1) EnC analyzer is now non-deterministic, as it can give different answers for the same inputs depending on the state of the debuggee
    2) Turns out that the debugger does not deliver module load/unload events in all cases (e.g. when the secondary process is killed).
    3) It's a bit complicated and would get even more complicated once we moved EnC to OOP/Cloud
    
    Instead, we don't report these errors until the user triggers the "continue" action. At this point the VS and all processes being debugged are blocked until the changes analyzed and applied.
    Since this situation is not very common the slight regression in interactivity is acceptable.

--- src/VisualStudio/Core/Def/Implementation/EditAndContinue/VisualStudioDebugStateChangeListener.cs
+++ src/VisualStudio/Core/Def/Implementation/EditAndContinue/VisualStudioDebugStateChangeListener.cs
@@ -24,59 +24,6 @@ namespace Microsoft.VisualStudio.LanguageServices.EditAndContinue
         // EnC service or null if EnC is disabled for the debug session.
         private IEditAndContinueWorkspaceService? _encService;
 
-        /// <summary>
-        /// Concord component. Singleton created on demand during debugging session and discarded as soon as the session ends.
-        /// </summary>
-        private sealed class DebuggerService : IDkmCustomMessageForwardReceiver, IDkmModuleInstanceLoadNotification, IDkmModuleInstanceUnloadNotification
-        {
-            private IEditAndContinueWorkspaceService? _encService;
-
-            /// <summary>
-            /// Message source id as specified in ManagedEditAndContinueService.vsdconfigxml.
-            /// </summary>
-            public static readonly Guid MessageSourceId = new Guid("730432E7-1B68-4B3A-BD6A-BD4C13E0566B");
-
-            DkmCustomMessage? IDkmCustomMessageForwardReceiver.SendLower(DkmCustomMessage customMessage)
-            {
-                // Initialize the listener before OnModuleInstanceLoad/OnModuleInstanceUnload can be triggered.
-                // These events are only called when managed debugging is being used due to RuntimeId=DkmRuntimeId.Clr filter in vsdconfigxml.
-                _encService = (IEditAndContinueWorkspaceService)customMessage.Parameter1;
-                return null;
-            }
-
-            /// <summary>
-            /// <see cref="IDkmModuleInstanceLoadNotification"/> is implemented by components that want to listen
-            /// for the ModuleInstanceLoad event. When this notification fires, the target process
-            /// will be suspended and can be examined. ModuleInstanceLoad is fired when a module is
-            /// loaded by a target process. Among other things, this event is used for symbol
-            /// providers to load symbols, and for the breakpoint manager to set breakpoints.
-            /// ModuleInstanceLoad fires for all modules, even if there are no symbols loaded.
-            /// </summary>
-            void IDkmModuleInstanceLoadNotification.OnModuleInstanceLoad(DkmModuleInstance moduleInstance, DkmWorkList workList, DkmEventDescriptorS eventDescriptor)
-            {
-                if (moduleInstance is DkmClrModuleInstance clrModuleInstance)
-                {
-                    Contract.ThrowIfNull(_encService);
-                    _encService.OnManagedModuleInstanceLoaded(clrModuleInstance.Mvid);
-                }
-            }
-
-            /// <summary>
-            /// <see cref="IDkmModuleInstanceUnloadNotification"/> is implemented by components that want to listen
-            /// for the ModuleInstanceUnload event. When this notification fires, the target process
-            /// will be suspended and can be examined. ModuleInstanceUnload is sent when the monitor
-            /// detects that a module has unloaded from within the target process.
-            /// </summary>
-            void IDkmModuleInstanceUnloadNotification.OnModuleInstanceUnload(DkmModuleInstance moduleInstance, DkmWorkList workList, DkmEventDescriptor eventDescriptor)
-            {
-                if (moduleInstance is DkmClrModuleInstance clrModuleInstance)
-                {
-                    Contract.ThrowIfNull(_encService);
-                    _encService.OnManagedModuleInstanceUnloaded(clrModuleInstance.Mvid);
-                }
-            }
-        }
-
         [ImportingConstructor]
         public VisualStudioDebugStateChangeListener(VisualStudioWorkspace workspace)
         {
@@ -94,19 +41,6 @@ namespace Microsoft.VisualStudio.LanguageServices.EditAndContinue
             if ((options & DebugSessionOptions.EditAndContinueDisabled) == 0)
             {
                 _encService = _workspace.Services.GetRequiredService<IEditAndContinueWorkspaceService>();
-
-                // hook up a callbacks (the call blocks until the message is processed):
-                using (DebuggerComponent.ManagedEditAndContinueService())
-                {
-                    DkmCustomMessage.Create(
-                        Connection: null,
-                        Process: null,
-                        SourceId: DebuggerService.MessageSourceId,
-                        MessageCode: 0,
-                        Parameter1: _encService,
-                        Parameter2: null).SendLower();
-                }
-
                 _encService.StartDebuggingSession();
             }
             else
