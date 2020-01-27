028f3e6053b77eba8a693394247d4827b7c412c1
Rikki Gibson <rigibson@microsoft.com>
2020-01-27 13:48:57 -0800

    Simplify AreContainingSymbolLocalsZeroed

--- src/Compilers/CSharp/Portable/Symbols/Source/SourceMethodSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourceMethodSymbol.cs
@@ -33,26 +33,12 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             }
         }
 
-        protected bool AreContainingSymbolLocalsZeroed
+        protected bool AreContainingSymbolLocalsZeroed => ContainingSymbol switch
         {
-            get
-            {
-                if (ContainingSymbol is SourceMethodSymbol method)
-                {
-                    return method.AreLocalsZeroed;
-                }
-                else if (ContainingType is SourceNamedTypeSymbol type)
-                {
-                    return type.AreLocalsZeroed;
-                }
-                else
-                {
-                    // Sometimes a source method symbol can be contained in a non-source symbol.
-                    // For example in EE. We aren't concerned with respecting SkipLocalsInit in such cases.
-                    return true;
-                }
-            }
-        }
+            SourceMethodSymbol m => m.AreLocalsZeroed,
+            SourceNamedTypeSymbol t => t.AreLocalsZeroed,
+            _ => true
+        };
 
         internal void ReportAsyncParameterErrors(DiagnosticBag diagnostics, Location location)
         {
