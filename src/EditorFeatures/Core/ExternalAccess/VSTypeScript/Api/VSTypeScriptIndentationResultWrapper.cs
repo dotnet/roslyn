bcceba9c53a8df62d7b01212026c116e5002df7c
Tomas Matousek <tomat@microsoft.com>
2020-02-07 14:51:46 -0800

    Feedback

--- src/EditorFeatures/Core/ExternalAccess/VSTypeScript/Api/VSTypeScriptIndentationResultWrapper.cs
+++ src/EditorFeatures/Core/ExternalAccess/VSTypeScript/Api/VSTypeScriptIndentationResultWrapper.cs
@@ -1,4 +1,6 @@
-﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 #nullable enable
 #pragma warning disable CS0618 // Type or member is obsolete (https://github.com/dotnet/roslyn/issues/35872)
