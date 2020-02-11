678fb26eefe431e3b92981674ac0bb1ff4f3fa0d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-02-11 11:10:02 -0800

    Move feature off of SyntaxGenerator.
    SyntaxGEnerator will be updated to produce tehse nodes, but in a more abstract manner (i.e. without tokens/trivia).
    This is not what this feature wants, so we're hardcoding the behavior it needs directly in the feature itself.

--- src/Features/CSharp/Portable/UseNullPropagation/CSharpUseNullPropagationCodeFixProvider.cs
+++ src/Features/CSharp/Portable/UseNullPropagation/CSharpUseNullPropagationCodeFixProvider.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using System.Composition;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -18,11 +20,16 @@ namespace Microsoft.CodeAnalysis.CSharp.UseNullPropagation
             InvocationExpressionSyntax,
             MemberAccessExpressionSyntax,
             ConditionalAccessExpressionSyntax,
-            ElementAccessExpressionSyntax>
+            ElementAccessExpressionSyntax,
+            ElementBindingExpressionSyntax,
+            BracketedArgumentListSyntax>
     {
         [ImportingConstructor]
         public CSharpUseNullPropagationCodeFixProvider()
         {
         }
+
+        protected override ElementBindingExpressionSyntax ElementBindingExpression(BracketedArgumentListSyntax argumentList)
+            => SyntaxFactory.ElementBindingExpression(argumentList);
     }
 }
