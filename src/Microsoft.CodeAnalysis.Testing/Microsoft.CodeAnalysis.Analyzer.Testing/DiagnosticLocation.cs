44d37b7a30216d6b4827ef898e6f36cfa65ef142
Sam Harwell <Sam.Harwell@microsoft.com>
2020-02-17 12:49:48 -0800

    Updated documentation

--- src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/DiagnosticLocation.cs
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/DiagnosticLocation.cs
@@ -2,16 +2,32 @@
 
 namespace Microsoft.CodeAnalysis.Testing
 {
+    /// <summary>
+    /// Represents an expected <see cref="Location"/> appearing in <see cref="Diagnostic.Location"/> or
+    /// <see cref="Diagnostic.AdditionalLocations"/>.
+    /// </summary>
     public readonly struct DiagnosticLocation
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiagnosticLocation"/> structure with the specified location and
+        /// options.
+        /// </summary>
+        /// <param name="span">The location of the diagnostic.</param>
+        /// <param name="options">The options to consider when validating this location.</param>
         public DiagnosticLocation(FileLinePositionSpan span, DiagnosticLocationOptions options)
         {
             Span = span;
             Options = options;
         }
 
+        /// <summary>
+        /// Gets the file and span of the location.
+        /// </summary>
         public FileLinePositionSpan Span { get; }
 
+        /// <summary>
+        /// Gets the options for validating the location.
+        /// </summary>
         public DiagnosticLocationOptions Options { get; }
     }
 }
