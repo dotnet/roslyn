8864ebcf54c95a7cad9d46d8358b719b49949d5e
Joey Robichaud <joseph.robichaud@microsoft.com>
2020-02-20 14:52:05 -0800

    Add link to explanatory comment

--- src/Workspaces/Core/Portable/Options/EditorConfig/EditorConfigStorageLocationExtensions.cs
+++ src/Workspaces/Core/Portable/Options/EditorConfig/EditorConfigStorageLocationExtensions.cs
@@ -10,7 +10,7 @@ namespace Microsoft.CodeAnalysis.Options
     {
         public static bool TryGetOption(this IEditorConfigStorageLocation editorConfigStorageLocation, AnalyzerConfigOptions analyzerConfigOptions, Type type, out object value)
         {
-            // This is a workaround until we have an API for enumeratings AnalyzerConfigOptions.
+            // This is a workaround until we have an API for enumeratings AnalyzerConfigOptions. See https://github.com/dotnet/roslyn/issues/41840
             var backingField = analyzerConfigOptions.GetType().GetField("_backing", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
             var backing = backingField?.GetValue(analyzerConfigOptions);
 
