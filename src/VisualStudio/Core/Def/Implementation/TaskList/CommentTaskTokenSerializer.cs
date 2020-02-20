8200921808301937c7a6731a3a206b9626bfcfb7
Manish Vasani <mavasani@microsoft.com>
2020-02-20 12:15:41 -0800

    Port additional change from #40513

--- src/VisualStudio/Core/Def/Implementation/TaskList/CommentTaskTokenSerializer.cs
+++ src/VisualStudio/Core/Def/Implementation/TaskList/CommentTaskTokenSerializer.cs
@@ -16,17 +16,17 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TaskList
     [Export(typeof(IOptionPersister))]
     internal class CommentTaskTokenSerializer : IOptionPersister
     {
-        private readonly VisualStudioWorkspace _workspace;
         private readonly ITaskList _taskList;
+        private readonly IGlobalOptionService _globalOptionService;
 
         private string _lastCommentTokenCache = null;
 
         [ImportingConstructor]
         public CommentTaskTokenSerializer(
-            VisualStudioWorkspace workspace,
+            IGlobalOptionService globalOptionService,
             [Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
         {
-            _workspace = workspace;
+            _globalOptionService = globalOptionService;
 
             // The SVsTaskList may not be available or doesn't actually implement ITaskList
             // in the "devenv /build" scenario
@@ -68,8 +68,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TaskList
 
             var commentString = GetTaskTokenList(_taskList);
 
-            var optionSet = _workspace.Options;
-            var optionValue = optionSet.GetOption(TodoCommentOptions.TokenList);
+            var optionValue = _globalOptionService.GetOption(TodoCommentOptions.TokenList);
             if (optionValue == commentString)
             {
                 return;
@@ -79,7 +78,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TaskList
             _lastCommentTokenCache = commentString;
 
             // let people to know that comment string has changed
-            _workspace.SetOptions(optionSet.WithChangedOption(TodoCommentOptions.TokenList, _lastCommentTokenCache));
+            _globalOptionService.RefreshOption(TodoCommentOptions.TokenList, _lastCommentTokenCache);
         }
 
         private static string GetTaskTokenList(ITaskList taskList)
