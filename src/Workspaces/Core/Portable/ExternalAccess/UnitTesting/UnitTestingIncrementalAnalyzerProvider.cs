c6443141b36bad224526193d5739a94c80866bc0
Artur Spychaj <drognanar@gmail.com>
2020-02-20 17:54:17 -0800

    Remove the static UnitTestingIncrementalAnalyzer

--- src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/UnitTestingIncrementalAnalyzerProvider.cs
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/UnitTestingIncrementalAnalyzerProvider.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api;
 using Microsoft.CodeAnalysis.SolutionCrawler;
 
@@ -10,11 +11,26 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting
     internal class UnitTestingIncrementalAnalyzerProvider : IIncrementalAnalyzerProvider
     {
         private readonly IUnitTestingIncrementalAnalyzerProviderImplementation _incrementalAnalyzerProvider;
+        private IIncrementalAnalyzer _analyzer;
+        private Workspace _workspace;
 
         public UnitTestingIncrementalAnalyzerProvider(IUnitTestingIncrementalAnalyzerProviderImplementation incrementalAnalyzerProvider)
             => _incrementalAnalyzerProvider = incrementalAnalyzerProvider;
 
         public IIncrementalAnalyzer CreateIncrementalAnalyzer(Workspace workspace)
-            => UnitTestingIncrementalAnalyzer.GetInstance(_incrementalAnalyzerProvider.CreateIncrementalAnalyzer(workspace));
+        {
+            if (_workspace != null && _workspace != workspace)
+            {
+                throw new ArgumentException(nameof(workspace));
+            }
+
+            if (_analyzer == null)
+            {
+                _analyzer = new UnitTestingIncrementalAnalyzer(_incrementalAnalyzerProvider.CreateIncrementalAnalyzer(workspace));
+                _workspace = workspace;
+            }
+
+            return _analyzer;
+        }
     }
 }
