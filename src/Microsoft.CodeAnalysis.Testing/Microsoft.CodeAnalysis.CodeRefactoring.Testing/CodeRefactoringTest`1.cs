ef4a0ec60b621ed655be455a37af61f735415801
Sam Harwell <Sam.Harwell@microsoft.com>
2020-02-24 09:12:28 -0800

    Add projects for testing CodeRefactoringProvider

---
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.CodeRefactoring.Testing/CodeRefactoringTest`1.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.Testing
+{
+    public abstract class CodeRefactoringTest<TVerifier> : AnalyzerTest<TVerifier>
+        where TVerifier : IVerifier, new()
+    {
+        /// <summary>
+        /// Gets the syntax kind enumeration type for the current code refactoring test.
+        /// </summary>
+        public abstract Type SyntaxKindType { get; }
+
+        protected override IEnumerable<DiagnosticAnalyzer> GetDiagnosticAnalyzers()
+            => Enumerable.Empty<DiagnosticAnalyzer>();
+
+        /// <summary>
+        /// Returns the code refactorings being tested - to be implemented in non-abstract class.
+        /// </summary>
+        /// <returns>The <see cref="CodeRefactoringProvider"/> to be used.</returns>
+        protected abstract IEnumerable<CodeRefactoringProvider> GetCodeRefactoringProviders();
+    }
+}
