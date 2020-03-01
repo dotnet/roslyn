f6d0c76f7f44a1ac7e69bcaf512dd736a8612f63
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-03-01 11:52:51 -0800

    Simplify

--- src/Features/Core/Portable/NavigateTo/AbstractNavigateToSearchService.InProcess.cs
+++ src/Features/Core/Portable/NavigateTo/AbstractNavigateToSearchService.InProcess.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.FindSymbols;
