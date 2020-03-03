8ff5d6d2c4f95956f9932fc1aaa0ffb46ddbc33d
David Barbet <dabarbet@microsoft.com>
2020-03-02 18:24:16 -0800

    Disable unimported types completion in liveshare until we have support in the LSP client.

--- src/Features/LanguageServer/ProtocolUnitTests/Completion/CompletionTests.cs
+++ src/Features/LanguageServer/ProtocolUnitTests/Completion/CompletionTests.cs
@@ -5,6 +5,7 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Completion;
 using Roslyn.Test.Utilities;
 using Xunit;
 using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
@@ -30,6 +31,30 @@ namespace Microsoft.CodeAnalysis.LanguageServer.UnitTests.Completion
 
             var results = (LSP.CompletionItem[])await RunGetCompletionsAsync(solution, locations["caret"].Single(), clientCapabilities);
             AssertJsonEquals(expected, results.First());
+        }
+
+        [Fact]
+        public async Task TestGetCompletionsDoesNotIncludeUnimportedTypesAsync()
+        {
+            var markup =
+@"class A
+{
+    void M()
+    {
+        {|caret:|}
+    }
+}";
+            var (solution, locations) = CreateTestSolution(markup);
+
+            // Make sure the unimported types option is on by default.
+            solution = solution.WithOptions(solution.Options.WithChangedOption(CompletionServiceOptions.IsExpandedCompletion, true));
+
+            var expected = CreateCompletionItem("A", LSP.CompletionItemKind.Class, new string[] { "Class", "Internal" }, CreateCompletionParams(locations["caret"].Single()));
+            var clientCapabilities = new LSP.VSClientCapabilities { SupportsVisualStudioExtensions = true };
+
+            var results = (LSP.CompletionItem[])await RunGetCompletionsAsync(solution, locations["caret"].Single(), clientCapabilities);
+
+            Assert.False(results.Any(item => "Console" == item.Label));
         }
 
         private static async Task<object> RunGetCompletionsAsync(Solution solution, LSP.Location caret, LSP.ClientCapabilities clientCapabilities = null)
