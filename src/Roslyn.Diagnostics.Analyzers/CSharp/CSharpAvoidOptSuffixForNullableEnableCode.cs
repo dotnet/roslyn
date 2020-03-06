4e5a73b22e2c6bce3d2db9bae0c2f59fe0f39983
Amaury Levé <evangelink@gmail.com>
2020-03-06 15:22:08 +0100

    RS0037: Avoid Opt suffix in nullable-enabled code

---
+++ src/Roslyn.Diagnostics.Analyzers/CSharp/CSharpAvoidOptSuffixForNullableEnableCode.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslyn.Diagnostics.Analyzers;
+
+namespace Roslyn.Diagnostics.CSharp.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class CSharpAvoidOptSuffixForNullableEnableCode : AvoidOptSuffixForNullableEnableCode
+    {
+        protected override bool IsNullableEnabledContext(CompilationOptions compilationOptions, IEnumerable<ParseOptions?> parseOptions)
+        {
+            var hasAnyCSharp8Part = parseOptions.OfType<CSharpParseOptions>().Any(option => option.LanguageVersion >= LanguageVersion.CSharp8);
+
+            return hasAnyCSharp8Part && IsNullableEnabledContext(compilationOptions);
+        }
+
+        private static bool IsNullableEnabledContext(CompilationOptions compilationOptions)
+            => compilationOptions is CSharpCompilationOptions csharpCompilationOptions &&
+                csharpCompilationOptions.NullableContextOptions == NullableContextOptions.Enable;
+    }
+}
---
+++ src/Roslyn.Diagnostics.Analyzers/UnitTests/AvoidOptSuffixForNullableEnableCodeTests.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Roslyn.Diagnostics.CSharp.Analyzers.CSharpAvoidOptSuffixForNullableEnableCode,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Roslyn.Diagnostics.Analyzers.UnitTests
+{
+    public class AvoidOptSuffixForNullableEnableCodeTests
+    {
+        [Fact]
+        public async Task RS0037_CSharp8_NullableEnabledCode_Diagnostic()
+        {
+            await new VerifyCS.Test
+            {
+                TestCode =
+                        @"
+#nullable enable
+
+public class Class1
+{
+    private Class1? _instanceOpt;
+
+    public void Method1(string? sOpt)
+    {
+    }
+}",
+                LanguageVersion = LanguageVersion.CSharp8
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task RS0037_CSharp8_NonNullableEnabledCode_NoDiagnostic()
+        {
+            await new VerifyCS.Test
+            {
+                TestCode =
+                        @"
+public class Class1
+{
+    private Class1? _instanceOpt;
+
+    public void Method1(string? sOpt)
+    {
+    }
+}",
+                LanguageVersion = LanguageVersion.CSharp8
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task RS0037_PriorToCSharp8_NoDiagnostic()
+        {
+            await new VerifyCS.Test
+            {
+                TestCode =
+                        @"
+public class Class1
+{
+    private Class1 _instanceOpt;
+
+    public void Method1(string sOpt)
+    {
+    }
+}",
+                LanguageVersion = LanguageVersion.CSharp7_3
+            }.RunAsync();
+        }
+    }
+}
