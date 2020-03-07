77a247a9f89e8d67dfd987ec19711e1306bde9fa
Manish Vasani <mavasani@microsoft.com>
2020-03-07 11:38:09 -0800

    Enforce IDE0073 (file header analyzer) as build warning

--- src/Test.Utilities/TestValidationMode.cs
+++ src/Test.Utilities/TestValidationMode.cs
@@ -1,4 +1,6 @@
-﻿namespace Test.Utilities
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Test.Utilities
 {
     public enum TestValidationMode
     {
