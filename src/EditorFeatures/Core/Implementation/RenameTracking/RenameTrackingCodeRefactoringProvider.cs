563ba3a9547457570968a5c33df0a50aa15f43bd
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-03-07 12:12:40 -0800

    Ensure user is on same line as renamed identifier to show refactoring.

--- src/EditorFeatures/Core/Implementation/RenameTracking/RenameTrackingCodeRefactoringProvider.cs
+++ src/EditorFeatures/Core/Implementation/RenameTracking/RenameTrackingCodeRefactoringProvider.cs
@@ -7,6 +7,7 @@
 using System.Collections.Generic;
 using System.Composition;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -41,24 +42,37 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.RenameTracking
             _refactorNotifyServices = refactorNotifyServices;
         }
 
+        // Internal for testing purposes
+        internal async Task<Diagnostic?> TryGetDiagnosticAsync(Document document, CancellationToken cancellationToken)
+        {
+            var syntaxTree = await document.GetRequiredSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
+            return RenameTrackingTaggerProvider.TryGetDiagnostic(
+                syntaxTree, DiagnosticDescriptor, cancellationToken);
+        }
+
         public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
         {
-            var (document, _, cancellationToken) = context;
+            var (document, span, cancellationToken) = context;
 
             // Ensure rename can still be invoked in this document. We reanalyze the document for
             // diagnostics when rename tracking is manually dismissed, but the existence of our
             // diagnostic may still be cached, so we have to double check before actually providing
             // any fixes.
-            if (RenameTrackingTaggerProvider.CanInvokeRename(document))
-            {
-                var syntaxTree = await document.GetRequiredSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
-                var diagnostic = RenameTrackingTaggerProvider.TryGetDiagnostic(
-                    syntaxTree, DiagnosticDescriptor, cancellationToken);
+            if (!RenameTrackingTaggerProvider.CanInvokeRename(document))
+                return;
 
-                var action = RenameTrackingTaggerProvider.CreateCodeAction(
-                    document, diagnostic, _refactorNotifyServices, _undoHistoryRegistry);
-                context.RegisterRefactoring(action);
-            }
+            var diagnostic = await TryGetDiagnosticAsync(document, cancellationToken).ConfigureAwait(false);
+            if (diagnostic == null)
+                return;
+
+            // user needs to be on the same line as the diagnostic location.
+            var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+            if (!text.AreOnSameLine(span.Start, diagnostic.Location.SourceSpan.Start))
+                return;
+
+            var action = RenameTrackingTaggerProvider.CreateCodeAction(
+                document, diagnostic, _refactorNotifyServices, _undoHistoryRegistry);
+            context.RegisterRefactoring(action);
         }
     }
 }
