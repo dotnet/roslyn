0990320d5f7c1afb427aa271eaf2ab2179946deb
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-03-07 18:46:20 -0800

    Add feature to fixup accidental assignments to the same variable.

---
+++ src/Features/Core/Portable/RemoveUnusedParametersAndValues/AbstractFixAssignmentToSameVariableCodeFixProvider.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+
+namespace Microsoft.CodeAnalysis.RemoveUnusedParametersAndValues
+{
+    internal abstract class AbstractFixAssignmentToSameVariableCodeFixProvider :
+        SyntaxEditorBasedCodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } =
+            ImmutableArray.Create(IDEDiagnosticIds.ValueAssignedIsUnusedDiagnosticId);
+
+        internal override CodeFixCategory CodeFixCategory
+            => CodeFixCategory.CodeQuality;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var document = context.Document;
+            var diagnostic = context.Diagnostics.First();
+            var cancellationToken = context.CancellationToken;
+
+            var node = diagnostic.Location.FindNode(getInnermostNodeForTie: true, cancellationToken);
+
+            return Task.CompletedTask;
+        }
+
+        protected override Task FixAllAsync(
+            Document document, ImmutableArray<Diagnostic> diagnostics,
+            SyntaxEditor editor, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
