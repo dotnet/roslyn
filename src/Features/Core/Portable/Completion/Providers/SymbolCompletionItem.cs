fdb09d514d2fc1844ea6e57ed5ac90ffa23e2bd1
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2020-03-09 00:29:34 -0700

    Update src/Features/Core/Portable/Completion/Providers/SymbolCompletionItem.cs

--- src/Features/Core/Portable/Completion/Providers/SymbolCompletionItem.cs
+++ src/Features/Core/Portable/Completion/Providers/SymbolCompletionItem.cs
@@ -317,8 +317,8 @@ namespace Microsoft.CodeAnalysis.Completion.Providers
         internal static SymbolKind? GetKind(CompletionItem item)
             => item.Properties.TryGetValue("SymbolKind", out var kind) ? (SymbolKind?)int.Parse(kind) : null;
 
-        internal static string GetSymbolIsGeneric(CompletionItem item)
-            => item.Properties.TryGetValue("IsGeneric", out var isGeneric) ? isGeneric : null;
+        internal static bool GetSymbolIsGeneric(CompletionItem item)
+            => item.Properties.TryGetValue("IsGeneric", out var v) && bool.TryParse(v, out var isGeneric) && isGeneric;
 
         public static async Task<CompletionDescription> GetDescriptionAsync(
             CompletionItem item, ImmutableArray<ISymbol> symbols, Document document, SemanticModel semanticModel, CancellationToken cancellationToken)
