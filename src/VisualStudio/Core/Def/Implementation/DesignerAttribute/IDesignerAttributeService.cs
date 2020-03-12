226f15aa3547dd4c9778cd0327b4186e93b3f8aa
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-03-12 09:24:20 -0700

    Docs

--- src/VisualStudio/Core/Def/Implementation/DesignerAttribute/IDesignerAttributeService.cs
+++ src/VisualStudio/Core/Def/Implementation/DesignerAttribute/IDesignerAttributeService.cs
@@ -7,6 +7,10 @@ using Microsoft.CodeAnalysis.Host;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.DesignerAttribute
 {
+    /// <summary>
+    /// In process service responsible for listening to OOP notifications whether or not a file is
+    /// designable  and then notifying the respective project systems about that information.
+    /// </summary>
     internal interface IDesignerAttributeService : IWorkspaceService
     {
         /// <summary>
