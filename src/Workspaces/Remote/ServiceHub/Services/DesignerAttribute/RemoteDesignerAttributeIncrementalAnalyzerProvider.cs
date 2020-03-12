b4746be3057fc7620d262776f154f1b5432ad0cc
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-03-12 11:07:54 -0700

    Add doc

--- src/Workspaces/Remote/ServiceHub/Services/DesignerAttribute/RemoteDesignerAttributeIncrementalAnalyzerProvider.cs
+++ src/Workspaces/Remote/ServiceHub/Services/DesignerAttribute/RemoteDesignerAttributeIncrementalAnalyzerProvider.cs
@@ -8,6 +8,11 @@ using Microsoft.CodeAnalysis.SolutionCrawler;
 
 namespace Microsoft.CodeAnalysis.Remote
 {
+    /// <remarks>Note: this is explicitly <b>not</b> exported.  We don't want the <see
+    /// cref="RemoteWorkspace"/> to automatically load this.  Instead, VS waits until it is ready
+    /// and then calls into OOP to tell it to start analyzing the solution.  At that point we'll get
+    /// created and added to the solution crawler.
+    /// </remarks>
     internal class RemoteDesignerAttributeIncrementalAnalyzerProvider : IIncrementalAnalyzerProvider
     {
         private readonly RemoteEndPoint _endPoint;
