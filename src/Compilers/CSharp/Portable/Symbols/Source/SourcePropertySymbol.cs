d8c31a18464300deec38ccf7af6b5ee133100526
Julien Couvreur <jcouv@users.noreply.github.com>
2020-03-15 07:48:53 -0700

    Add support for MemberNotNull/When attributes (#41675)
    * Add support for MemberNotNull/When attributes
    
    * Fix overflowing test
    
    * MemberNotNull on override and with try/finally
    
    * Fix regression
    
    * Update tests
    
    * Honor MemberNotNull on overrides
    
    * WIP on enforcement
    
    * Warn on referencing member from base type
    
    * Cleanup
    
    * Resolve conflicts
    
    * Fix inheritance, PE and property scenarios
    
    * Fix a test
    
    * Address feedback from Neal (on NotNullWhen)
    
    * Fix more scenarios with try/finally
    
    * Fix analysis of split invocation
    
    * Fix bootstrapping issues
    
    * Add test
    
    * Apply MemberNotNull on calls with instance receivers
    
    * Check reachability instead
    
    * Handle staticness mismatch
    
    * Resolve merge conflict
    
    * Address 2 remaining comments
    
    * Fix another merge conflict for bootstrapping
    
    * Add tests and fix assertion crash
    
    * Fix generate file
    
    * Use struct instead of tuple

--- src/Compilers/CSharp/Portable/Symbols/Source/SourcePropertySymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourcePropertySymbol.cs
@@ -491,6 +491,15 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             }
         }
 
+        internal sealed override ImmutableArray<string> NotNullMembers =>
+            GetDecodedWellKnownAttributeData()?.NotNullMembers ?? ImmutableArray<string>.Empty;
+
+        internal sealed override ImmutableArray<string> NotNullWhenTrueMembers =>
+            GetDecodedWellKnownAttributeData()?.NotNullWhenTrueMembers ?? ImmutableArray<string>.Empty;
+
+        internal sealed override ImmutableArray<string> NotNullWhenFalseMembers =>
+            GetDecodedWellKnownAttributeData()?.NotNullWhenFalseMembers ?? ImmutableArray<string>.Empty;
+
         internal bool IsExpressionBodied
             => (_propertyFlags & Flags.IsExpressionBodied) != 0;
 
@@ -1371,7 +1380,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             }
             else if (attribute.IsTargetAttribute(this, AttributeDescription.SkipLocalsInitAttribute))
             {
-                attribute.DecodeSkipLocalsInitAttribute<PropertyWellKnownAttributeData>(DeclaringCompilation, ref arguments);
+                CSharpAttributeData.DecodeSkipLocalsInitAttribute<PropertyWellKnownAttributeData>(DeclaringCompilation, ref arguments);
             }
             else if (attribute.IsTargetAttribute(this, AttributeDescription.DynamicAttribute))
             {
@@ -1417,6 +1426,16 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             else if (attribute.IsTargetAttribute(this, AttributeDescription.NotNullAttribute))
             {
                 arguments.GetOrCreateData<PropertyWellKnownAttributeData>().HasNotNullAttribute = true;
+            }
+            else if (attribute.IsTargetAttribute(this, AttributeDescription.MemberNotNullAttribute))
+            {
+                MessageID.IDS_FeatureMemberNotNull.CheckFeatureAvailability(arguments.Diagnostics, arguments.AttributeSyntaxOpt);
+                CSharpAttributeData.DecodeMemberNotNullAttribute<PropertyWellKnownAttributeData>(ContainingType, ref arguments);
+            }
+            else if (attribute.IsTargetAttribute(this, AttributeDescription.MemberNotNullWhenAttribute))
+            {
+                MessageID.IDS_FeatureMemberNotNull.CheckFeatureAvailability(arguments.Diagnostics, arguments.AttributeSyntaxOpt);
+                CSharpAttributeData.DecodeMemberNotNullWhenAttribute<PropertyWellKnownAttributeData>(ContainingType, ref arguments);
             }
         }
 
@@ -1468,8 +1487,17 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
         internal SourceAttributeData NotNullAttributeIfExists
             => FindAttribute(AttributeDescription.NotNullAttribute);
 
+        internal ImmutableArray<SourceAttributeData> MemberNotNullAttributeIfExists
+            => FindAttributes(AttributeDescription.MemberNotNullAttribute);
+
+        internal ImmutableArray<SourceAttributeData> MemberNotNullWhenAttributeIfExists
+            => FindAttributes(AttributeDescription.MemberNotNullWhenAttribute);
+
         private SourceAttributeData FindAttribute(AttributeDescription attributeDescription)
             => (SourceAttributeData)GetAttributes().First(a => a.IsTargetAttribute(this, attributeDescription));
+
+        private ImmutableArray<SourceAttributeData> FindAttributes(AttributeDescription attributeDescription)
+            => GetAttributes().Where(a => a.IsTargetAttribute(this, attributeDescription)).Cast<SourceAttributeData>().ToImmutableArray();
 
         internal override void PostDecodeWellKnownAttributes(ImmutableArray<CSharpAttributeData> boundAttributes, ImmutableArray<AttributeSyntax> allAttributeSyntaxNodes, DiagnosticBag diagnostics, AttributeLocation symbolPart, WellKnownAttributeData decodedData)
         {
