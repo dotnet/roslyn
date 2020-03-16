6aeec2f8a6cbbc970db1753f7f0082dbb048a4a9
Sam Harwell <Sam.Harwell@microsoft.com>
2020-03-16 14:38:58 -0700

    Add helpers for refactoring tests

--- src/Test.Utilities/CSharpCodeRefactoringVerifier`1+Test.cs
+++ src/Test.Utilities/CSharpCodeRefactoringVerifier`1+Test.cs
@@ -1,24 +1,20 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Testing;
-using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
 using Microsoft.CodeAnalysis.Testing.Verifiers;
 
 namespace Test.Utilities
 {
-    public static partial class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
-        where TAnalyzer : DiagnosticAnalyzer, new()
-        where TCodeFix : CodeFixProvider, new()
+    public static partial class CSharpCodeRefactoringVerifier<TRefactoring>
+        where TRefactoring : CodeRefactoringProvider, new()
     {
-        public class Test : CSharpCodeFixTest<TAnalyzer, TCodeFix, XUnitVerifier>
+        public class Test : CSharpCodeRefactoringTest<TRefactoring, XUnitVerifier>
         {
-            internal static readonly ImmutableDictionary<string, ReportDiagnostic> NullableWarnings = GetNullableWarningsFromCompiler();
-
             public Test()
             {
                 ReferenceAssemblies = AdditionalMetadataReferences.Default;
@@ -36,19 +32,8 @@ namespace Test.Utilities
                 });
             }
 
-            private static ImmutableDictionary<string, ReportDiagnostic> GetNullableWarningsFromCompiler()
-            {
-                string[] args = { "/warnaserror:nullable" };
-                var commandLineArguments = CSharpCommandLineParser.Default.Parse(args, baseDirectory: Environment.CurrentDirectory, sdkDirectory: Environment.CurrentDirectory);
-                var nullableWarnings = commandLineArguments.CompilationOptions.SpecificDiagnosticOptions;
-
-                // Workaround for https://github.com/dotnet/roslyn/issues/41610
-                nullableWarnings = nullableWarnings
-                    .SetItem("CS8632", ReportDiagnostic.Error)
-                    .SetItem("CS8669", ReportDiagnostic.Error);
-
-                return nullableWarnings;
-            }
+            private static ImmutableDictionary<string, ReportDiagnostic> NullableWarnings
+                => CSharpCodeFixVerifier<EmptyDiagnosticAnalyzer, EmptyCodeFixProvider>.Test.NullableWarnings;
 
             public LanguageVersion LanguageVersion { get; set; } = LanguageVersion.CSharp7_3;
         }
