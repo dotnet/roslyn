29e85244deead64f7fe521da3b5ef38e40bfa771
Manish Vasani <mavasani@microsoft.com>
2020-03-16 09:31:29 -0700

    Address feedback

--- src/EditorFeatures/VisualBasicTest/Diagnostics/GenerateType/GenerateTypeTests_Dialog.vb
+++ src/EditorFeatures/VisualBasicTest/Diagnostics/GenerateType/GenerateTypeTests_Dialog.vb
@@ -3,9 +3,6 @@
 ' See the LICENSE file in the project root for more information.
 
 Option Strict Off
-' Licensed to the .NET Foundation under one or more agreements.
-' The .NET Foundation licenses this file to you under the MIT license.
-' See the LICENSE file in the project root for more information.
 
 Imports System.Collections.Immutable
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Extensions
