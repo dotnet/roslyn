591e5df76bad982d3a2add64346dd6f5c3c925c7
Sam Harwell <Sam.Harwell@microsoft.com>
2020-03-15 17:48:56 -0700

    Implement RS0038 (Prefer null literal)

---
+++ src/Roslyn.Diagnostics.Analyzers/CSharp/PreferNullLiteralCodeFixProvider.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Editing;
+using Roslyn.Diagnostics.Analyzers;
+
+namespace Roslyn.Diagnostics.CSharp.Analyzers
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(PreferNullLiteralCodeFixProvider))]
+    [Shared]
+    public class PreferNullLiteralCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(PreferNullLiteral.Rule.Id);
+
+        public override FixAllProvider GetFixAllProvider()
+            => WellKnownFixAllProviders.BatchFixer;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        RoslynDiagnosticsAnalyzersResources.PreferNullLiteralCodeFix,
+                        cancellationToken => ReplaceWithNullLiteralAsync(context.Document, diagnostic.Location, cancellationToken),
+                        equivalenceKey: nameof(PreferNullLiteralCodeFixProvider)),
+                    diagnostic);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private async Task<Document> ReplaceWithNullLiteralAsync(Document document, Location location, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var syntax = root.FindNode(location.SourceSpan, getInnermostNodeForTie: true);
+
+            var syntaxGenerator = SyntaxGenerator.GetGenerator(document);
+            var newSyntax = syntaxGenerator.NullLiteralExpression();
+
+            return document.WithSyntaxRoot(root.ReplaceNode(syntax, newSyntax));
+        }
+    }
+}
---
+++ src/Roslyn.Diagnostics.Analyzers/UnitTests/PreferNullLiteralTests.cs
@@ -0,0 +1,174 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Roslyn.Diagnostics.CSharp.Analyzers.PreferNullLiteral,
+    Roslyn.Diagnostics.CSharp.Analyzers.PreferNullLiteralCodeFixProvider>;
+
+namespace Roslyn.Diagnostics.Analyzers.UnitTests
+{
+    public class PreferNullLiteralTests
+    {
+        [Theory]
+        [InlineData("default")]
+        [InlineData("default(object)")]
+        public async Task PreferNullLiteral_Class(string defaultValueExpression)
+        {
+            var source = $@"
+class Type
+{{
+    object Method()
+    {{
+        return [|{defaultValueExpression}|];
+    }}
+}}
+";
+            var fixedSource = @"
+class Type
+{
+    object Method()
+    {
+        return null;
+    }
+}
+";
+
+            await VerifyCS.VerifyCodeFixAsync(source, fixedSource);
+        }
+
+        [Theory]
+        [InlineData("default")]
+        [InlineData("default(object)")]
+        public async Task PreferNullLiteral_DefaultParameterValue(string defaultValueExpression)
+        {
+            var source = $@"
+class Type
+{{
+    void Method(object value = [|{defaultValueExpression}|])
+    {{
+    }}
+}}
+";
+            var fixedSource = @"
+class Type
+{
+    void Method(object value = null)
+    {
+    }
+}
+";
+
+            await VerifyCS.VerifyCodeFixAsync(source, fixedSource);
+        }
+
+        [Fact]
+        public async Task PreferNullLiteral_Struct()
+        {
+            var source = @"
+class Type
+{
+    int Method()
+    {
+        return default;
+    }
+}
+";
+
+            await VerifyCS.VerifyCodeFixAsync(source, source);
+        }
+
+        [Fact]
+        public async Task PreferNullLiteral_StructConvertedToReferenceType()
+        {
+            var source = @"
+class Type
+{
+    object Method()
+    {
+        return default(int);
+    }
+}
+";
+
+            await VerifyCS.VerifyCodeFixAsync(source, source);
+        }
+
+        [Fact]
+        public async Task PreferNullLiteral_UnconstrainedGeneric()
+        {
+            var source = @"
+class Type
+{
+    T Method<T>()
+    {
+        return default;
+    }
+}
+";
+
+            await VerifyCS.VerifyCodeFixAsync(source, source);
+        }
+
+        [Fact]
+        public async Task PreferNullLiteral_GenericConstrainedToReferenceType()
+        {
+            var source = @"
+class Type
+{
+    T Method<T>()
+        where T : class
+    {
+        return [|default|];
+    }
+}
+";
+            var fixedSource = @"
+class Type
+{
+    T Method<T>()
+        where T : class
+    {
+        return null;
+    }
+}
+";
+
+            await VerifyCS.VerifyCodeFixAsync(source, fixedSource);
+        }
+
+        [Fact]
+        public async Task PreferNullLiteral_GenericConstrainedToInterface()
+        {
+            var source = @"
+class Type
+{
+    T Method<T>()
+        where T : System.IComparable
+    {
+        return default;
+    }
+}
+";
+
+            await VerifyCS.VerifyCodeFixAsync(source, source);
+        }
+
+        [Fact]
+        public async Task PreferNullLiteral_GenericConstrainedToValueType()
+        {
+            var source = @"
+class Type
+{
+    T Method<T>()
+        where T : struct
+    {
+        return default;
+    }
+}
+";
+
+            await VerifyCS.VerifyCodeFixAsync(source, source);
+        }
+    }
+}
