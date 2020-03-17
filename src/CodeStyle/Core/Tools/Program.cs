88e731a4eaeb6999799e57738a4b41bbb72795de
Manish Vasani <mavasani@microsoft.com>
2020-03-17 11:19:17 -0700

    Disable all the analyzers by default for CI in CodeStyle NuGet package
    All the analyzers in CodeStyle NuGet package are suggestions or hidden diagnostics by default. Hence, executing them in CI by default is an unnecessary performance overhead during build. This is especially critical when we attempt to move the CodeStyle NuGet package into the SDK as outlined in the User Experience [here](https://github.com/dotnet/roslyn/issues/38480#issuecomment-578903218).
    This change auto-generates and embeds a .props file into the CodeStyle NuGet package that disables all analyzers in the package by default when executing command line builds. Current approach generates a `NoWarn`, but once #42219 is implemented, we will replace it with a global/base editorconfig which can ship with the NuGet package.

---
+++ src/CodeStyle/Core/Tools/Program.cs
@@ -0,0 +1,87 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeStylePackagePropsFileGenerator
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine($"Excepted 2 arguments, found {args.Length}: {string.Join(";", args)}");
+                return 1;
+            }
+
+            var assemblyList = args[0].Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).ToImmutableArray();
+            string propsFile = args[1];
+
+            var ruleIds = GetAllRuleIds(assemblyList);
+            Directory.CreateDirectory(Path.GetDirectoryName(propsFile));
+            File.WriteAllText(propsFile, GetPropsFileContents(ruleIds));
+
+            return 0;
+
+            static SortedSet<string> GetAllRuleIds(ImmutableArray<string> assemblyList)
+            {
+                var ruleIds = new SortedSet<string>();
+                foreach (var assemblyPath in assemblyList)
+                {
+                    if (!File.Exists(assemblyPath))
+                    {
+                        throw new Exception($"'{assemblyPath}' does not exist");
+                    }
+
+                    var analyzerFileReference = new AnalyzerFileReference(assemblyPath, AnalyzerAssemblyLoader.Instance);
+                    var analyzers = analyzerFileReference.GetAnalyzersForAllLanguages();
+
+                    foreach (var analyzer in analyzers)
+                    {
+                        foreach (var rule in analyzer.SupportedDiagnostics)
+                        {
+                            ruleIds.Add(rule.Id);
+                        }
+                    }
+                }
+
+                return ruleIds;
+            }
+
+            static string GetPropsFileContents(IEnumerable<string> ruleIds)
+            {
+                var allRuleIds = string.Join(";", ruleIds);
+                return
+$@"<Project DefaultTargets=""Build"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <!-- This property group prevents the rule ids implemented in this package from being executed by default in CI -->
+  <PropertyGroup Condition=""'$(DesignTimeBuild)' != 'true' and '$(BuildingProject)' != 'false'"">
+    <NoWarn>$(NoWarn);{allRuleIds}</NoWarn>
+  </PropertyGroup>
+</Project>";
+            }
+        }
+
+        private sealed class AnalyzerAssemblyLoader : IAnalyzerAssemblyLoader
+        {
+            public static IAnalyzerAssemblyLoader Instance = new AnalyzerAssemblyLoader();
+
+            private AnalyzerAssemblyLoader() { }
+            public void AddDependencyLocation(string fullPath)
+            {
+            }
+
+            public Assembly LoadFromPath(string fullPath)
+            {
+                return Assembly.LoadFrom(fullPath);
+            }
+        }
+    }
+}
