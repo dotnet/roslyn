358f90bd444c78191755a6c01c8eb4155c6bbc45
Sam Harwell <Sam.Harwell@microsoft.com>
2020-03-18 08:33:06 -0700

    Add suppressor RS0039 (Relax test naming)
    Asynchronous test methods do not require the 'Async' suffix.

---
+++ src/Roslyn.Diagnostics.Analyzers/Core/Extensions/IMethodSymbolExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Concurrent;
+using Microsoft.CodeAnalysis;
+
+namespace Roslyn.Diagnostics.Analyzers.Extensions
+{
+    internal static class IMethodSymbolExtensions
+    {
+        public static bool IsTestMethod(this IMethodSymbol method, ConcurrentDictionary<INamedTypeSymbol, bool> knownTestAttributes, INamedTypeSymbol factAttribute)
+        {
+            foreach (var attribute in method.GetAttributes())
+            {
+                if (attribute.AttributeClass.IsTestAttribute(knownTestAttributes, factAttribute))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
