8a6dc70f7c0032c4d46a772d1d56c0b637c663a3
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2020-03-18 18:28:33 -0700

    Update src/Workspaces/Core/Portable/Storage/AbstractPersistentStorageService.cs
    Co-Authored-By: Sam Harwell <sam@tunnelvisionlabs.com>

--- src/Workspaces/Core/Portable/Storage/AbstractPersistentStorageService.cs
+++ src/Workspaces/Core/Portable/Storage/AbstractPersistentStorageService.cs
@@ -213,7 +213,7 @@ namespace Microsoft.CodeAnalysis.Storage
             {
                 // This should only be called from a caller that has a non-null storage that it
                 // already has a reference on.  So .TryAddReference cannot fail.
-                return new PersistentStorageReferenceCountedDisposableWrapper(storage.TryAddReference() ?? throw new InvalidOperationException());
+                return new PersistentStorageReferenceCountedDisposableWrapper(storage.TryAddReference() ?? throw ExceptionUtilities.Unreachable);
             }
 
             public void Dispose()
