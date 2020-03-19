7be624d0746a8faf6bfc65c4dd0415147b8eced3
David Barbet <dibarbet@gmail.com>
2020-03-19 15:33:42 -0700

    Export typescript handlers with correct mef version

--- src/VisualStudio/LiveShare/Impl/Shims/TypeScriptHandlerShims.cs
+++ src/VisualStudio/LiveShare/Impl/Shims/TypeScriptHandlerShims.cs
@@ -6,7 +6,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Composition;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
