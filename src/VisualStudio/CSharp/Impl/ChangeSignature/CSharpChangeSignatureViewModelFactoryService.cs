743a7d86ee9f8defbe5cb01ba3b988e558140370
David Poeschl <dpoeschl@gmail.com>
2020-03-22 13:57:02 -0700

    Remove IntelliSenseTextBox

--- src/VisualStudio/CSharp/Impl/ChangeSignature/CSharpChangeSignatureViewModelFactoryService.cs
+++ src/VisualStudio/CSharp/Impl/ChangeSignature/CSharpChangeSignatureViewModelFactoryService.cs
@@ -4,15 +4,8 @@ using System;
 using System.Composition;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Editor;
 using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature;
-using Microsoft.VisualStudio.LanguageServices.Implementation.IntellisenseControls;
-using Microsoft.VisualStudio.Text;
-using Microsoft.VisualStudio.Text.Classification;
-using Microsoft.VisualStudio.Text.Operations;
-using Microsoft.VisualStudio.Utilities;
 using static Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature.ChangeSignatureDialogViewModel;
 
 namespace Microsoft.VisualStudio.LanguageServices.CSharp.ChangeSignature
@@ -22,26 +15,9 @@ namespace Microsoft.VisualStudio.LanguageServices.CSharp.ChangeSignature
     {
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-        public CSharpChangeSignatureViewModelFactoryService(
-            IntellisenseTextBoxViewModelFactory intellisenseTextBoxViewModelFactory,
-            IContentTypeRegistryService contentTypeRegistryService,
-            IEditorOperationsFactoryService editorOperationsFactoryService,
-            IClassificationFormatMapService classificationFormatMapService,
-            IVsEditorAdaptersFactoryService editorAdapterFactory)
-            : base(intellisenseTextBoxViewModelFactory, contentTypeRegistryService, editorOperationsFactoryService, classificationFormatMapService, editorAdapterFactory)
+        public CSharpChangeSignatureViewModelFactoryService()
         {
         }
-
-        // Adjust the context point to ensure that the right information is in scope.
-        // For example, we may need to move the point to the end of the last statement in a method body
-        // in order to be able to access all local variables.
-        // + 1 to support inserted comma
-        protected override ITrackingSpan[] CreateSpansMethod(ITextSnapshot textSnapshot, int insertPosition)
-            => CreateTrackingSpansHelper(textSnapshot, contextPoint: insertPosition + 1, spaceBetweenTypeAndName: 1);
-
-        protected override string TextToInsert => ", ";
-
-        protected override string ContentTypeName => ContentTypeNames.CSharpContentType;
 
         public override SymbolDisplayPart[] GeneratePreviewDisplayParts(AddedParameterViewModel addedParameterViewModel)
             => new[] {
--- src/VisualStudio/VisualBasic/Impl/ChangeSignature/VisualBasicChangeSignatureViewModelFactoryService.vb
+++ src/VisualStudio/VisualBasic/Impl/ChangeSignature/VisualBasicChangeSignatureViewModelFactoryService.vb
@@ -2,16 +2,9 @@
 
 Imports System.Composition
 Imports Microsoft.CodeAnalysis
-Imports Microsoft.CodeAnalysis.Editor
 Imports Microsoft.CodeAnalysis.Host.Mef
 Imports Microsoft.CodeAnalysis.VisualBasic
-Imports Microsoft.VisualStudio.Editor
 Imports Microsoft.VisualStudio.LanguageServices.Implementation.ChangeSignature
-Imports Microsoft.VisualStudio.LanguageServices.Implementation.IntellisenseControls
-Imports Microsoft.VisualStudio.Text
-Imports Microsoft.VisualStudio.Text.Classification
-Imports Microsoft.VisualStudio.Text.Operations
-Imports Microsoft.VisualStudio.Utilities
 
 Namespace Microsoft.VisualStudio.LanguageServices.VisualBasic.ChangeSignature
     <ExportLanguageService(GetType(IChangeSignatureViewModelFactoryService), LanguageNames.VisualBasic), [Shared]>
@@ -19,33 +12,8 @@ Namespace Microsoft.VisualStudio.LanguageServices.VisualBasic.ChangeSignature
         Inherits ChangeSignatureViewModelFactoryService
 
         <ImportingConstructor>
-        Public Sub New(
-            intellisenseTextBoxViewModelFactory As IntellisenseTextBoxViewModelFactory,
-            contentTypeRegistryService As IContentTypeRegistryService,
-            editorOperationsFactoryService As IEditorOperationsFactoryService,
-            classificationFormatMapService As IClassificationFormatMapService,
-            editorAdapterFactory As IVsEditorAdaptersFactoryService)
-            MyBase.New(intellisenseTextBoxViewModelFactory, contentTypeRegistryService, editorOperationsFactoryService, classificationFormatMapService, editorAdapterFactory)
+        Public Sub New()
         End Sub
-
-        Protected Overrides Function CreateSpansMethod(textSnapshot As ITextSnapshot, insertPosition As Integer) As ITrackingSpan()
-            ' + 4 to support inserted ', [~'
-            Return CreateTrackingSpansHelper(textSnapshot, contextPoint:=insertPosition + 4, spaceBetweenTypeAndName:=5)
-        End Function
-
-        ' We insert '[]' so that we're always able to generate the type even if the name field is empty. 
-        ' We insert '~' to avoid Intellisense activation for the name field, since completion for names does not apply to VB.
-        Protected Overrides ReadOnly Property TextToInsert As String
-            Get
-                Return ", [~] As "
-            End Get
-        End Property
-
-        Protected Overrides ReadOnly Property ContentTypeName As String
-            Get
-                Return ContentTypeNames.VisualBasicContentType
-            End Get
-        End Property
 
         Public Overrides Function GeneratePreviewDisplayParts(addedParameterViewModel As ChangeSignatureDialogViewModel.AddedParameterViewModel) As SymbolDisplayPart()
             Return {
