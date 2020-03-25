005e0494895c3ef672a36cfe57459ffe695f52af
Amadeusz Wieczorek <amwieczo@microsoft.com>
2020-03-25 15:56:38 -0700

    add header

--- src/VisualStudio/Core/Def/Implementation/FindUsages/RichNavFilterScopes.cs
+++ src/VisualStudio/Core/Def/Implementation/FindUsages/RichNavFilterScopes.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel.Composition;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel.Composition;
 using Microsoft.Internal.VisualStudio.Shell.ErrorList;
 using Microsoft.VisualStudio.Shell.TableControl;
 using Microsoft.VisualStudio.Shell.TableManager;
