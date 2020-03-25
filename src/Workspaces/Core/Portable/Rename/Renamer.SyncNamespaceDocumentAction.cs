cede8793ecc84a144811a999b36bb0b2b7d609b1
Andrew Hall (METAL) <andrha@microsoft.com>
2020-03-24 17:18:07 -0700

    Add cancellation token to the async API

--- src/Workspaces/Core/Portable/Rename/Renamer.SyncNamespaceDocumentAction.cs
+++ src/Workspaces/Core/Portable/Rename/Renamer.SyncNamespaceDocumentAction.cs
@@ -42,7 +42,7 @@ namespace Microsoft.CodeAnalysis.Rename
                 throw new NotImplementedException();
             }
 
-            internal override async Task<Solution> GetModifiedSolutionAsync(Solution solution, CancellationToken cancellationToken = default)
+            internal override async Task<Solution> GetModifiedSolutionAsync(Solution solution, CancellationToken cancellationToken)
             {
                 solution = solution.WithDocumentFolders(DocumentId, _newFolders);
 
