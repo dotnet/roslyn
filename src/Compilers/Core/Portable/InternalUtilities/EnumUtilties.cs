4ab040546cc52f40cc52daed1c2678676a871c96
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-03-25 23:52:24 -0700

    Revert

--- src/Compilers/Core/Portable/InternalUtilities/EnumUtilties.cs
+++ src/Compilers/Core/Portable/InternalUtilities/EnumUtilties.cs
@@ -25,20 +25,30 @@ namespace Roslyn.Utilities
 
             unchecked
             {
-                return specialType switch
+                switch (specialType)
                 {
-                    SpecialType.System_SByte => (ulong)(sbyte)value,
-                    SpecialType.System_Int16 => (ulong)(short)value,
-                    SpecialType.System_Int32 => (ulong)(int)value,
-                    SpecialType.System_Int64 => (ulong)(long)value,
-                    SpecialType.System_Byte => (byte)value,
-                    SpecialType.System_UInt16 => (ushort)value,
-                    SpecialType.System_UInt32 => (uint)value,
-                    SpecialType.System_UInt64 => (ulong)value,
-                    // not using ExceptionUtilities.UnexpectedValue() because this is used by the Services layer
-                    // which doesn't have those utilities.
-                    _ => throw new InvalidOperationException(string.Format("{0} is not a valid underlying type for an enum", specialType)),
-                };
+                    case SpecialType.System_SByte:
+                        return (ulong)(sbyte)value;
+                    case SpecialType.System_Int16:
+                        return (ulong)(short)value;
+                    case SpecialType.System_Int32:
+                        return (ulong)(int)value;
+                    case SpecialType.System_Int64:
+                        return (ulong)(long)value;
+                    case SpecialType.System_Byte:
+                        return (byte)value;
+                    case SpecialType.System_UInt16:
+                        return (ushort)value;
+                    case SpecialType.System_UInt32:
+                        return (uint)value;
+                    case SpecialType.System_UInt64:
+                        return (ulong)value;
+
+                    default:
+                        // not using ExceptionUtilities.UnexpectedValue() because this is used by the Services layer
+                        // which doesn't have those utilities.
+                        throw new InvalidOperationException(string.Format("{0} is not a valid underlying type for an enum", specialType));
+                }
             }
         }
 
