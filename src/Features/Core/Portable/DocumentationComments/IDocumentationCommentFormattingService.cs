507f8f20a6d6815b83b97f8426f56e2a00d39239
Alexander Gayko <a.gayko@googlemail.com>
2020-03-31 20:00:09 +0200

    added missing using clause. Tests pass now.

--- src/Features/Core/Portable/DocumentationComments/IDocumentationCommentFormattingService.cs
+++ src/Features/Core/Portable/DocumentationComments/IDocumentationCommentFormattingService.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
+using System.Threading;
 using Microsoft.CodeAnalysis.Host;
 
 namespace Microsoft.CodeAnalysis.DocumentationComments
