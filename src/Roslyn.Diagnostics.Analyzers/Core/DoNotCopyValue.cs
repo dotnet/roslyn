154688460b8b83ba748d5fc1bd927e6686f4a6cf
Sam Harwell <Sam.Harwell@microsoft.com>
2020-04-02 10:03:56 -0700

    Remove unnecessary using directive

--- src/Roslyn.Diagnostics.Analyzers/Core/DoNotCopyValue.cs
+++ src/Roslyn.Diagnostics.Analyzers/Core/DoNotCopyValue.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Threading.Tasks;
 using Analyzer.Utilities;
 using Analyzer.Utilities.Extensions;
 using Microsoft.CodeAnalysis;
