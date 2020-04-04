2d96be7108f7cdc94b2d724eb442418f63b3dc33
Manish Vasani <mavasani@microsoft.com>
2020-04-03 19:48:48 -0700

    Fix build warning

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/LanguageServices/SyntaxGeneratorInternalExtensions/SyntaxGeneratorInternal.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/LanguageServices/SyntaxGeneratorInternalExtensions/SyntaxGeneratorInternal.cs
@@ -54,9 +54,9 @@ namespace Microsoft.CodeAnalysis.Editing
         internal abstract SyntaxNode YieldReturnStatement(SyntaxNode expression);
 
         /// <summary>
-        /// <see langword="true"/> if the language requires a <see cref="TypeExpression(ITypeSymbol)"/>
+        /// <see langword="true"/> if the language requires a "TypeExpression"
         /// (including <see langword="var"/>) to be stated when making a 
-        /// <see cref="LocalDeclarationStatement(ITypeSymbol, string, SyntaxNode, bool)"/>.
+        /// <see cref="LocalDeclarationStatement(SyntaxNode, SyntaxToken, SyntaxNode, bool)"/>.
         /// <see langword="false"/> if the language allows the type node to be entirely elided.
         /// </summary>
         internal abstract bool RequiresLocalDeclarationType();
