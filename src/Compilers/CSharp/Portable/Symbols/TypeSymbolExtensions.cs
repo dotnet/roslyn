762c5ea0530a6ca47ad0c5cc97bc03920c37f265
Fredric Silberberg <frsilb@microsoft.com>
2020-04-09 22:00:27 -0700

    Remove prototype comment after addressing.

--- src/Compilers/CSharp/Portable/Symbols/TypeSymbolExtensions.cs
+++ src/Compilers/CSharp/Portable/Symbols/TypeSymbolExtensions.cs
@@ -1072,7 +1072,6 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             return type is SourceNamedTypeSymbol { IsPartial: true };
         }
 
-        // PROTOTYPE(func-ptr): Follow up on usages of this API and add FunctionPointers where necessary, and type/kind checks.
         public static bool IsPointerType(this TypeSymbol type)
         {
             return type is PointerTypeSymbol;
