ee873abcc14d95c816890ef469425cae83fc3467
jnm2 <me@jnm2.com>
2020-04-10 21:21:52 -0400

    Simplify RootModuleType.cs

--- src/Compilers/Core/Portable/PEWriter/RootModuleType.cs
+++ src/Compilers/Core/Portable/PEWriter/RootModuleType.cs
@@ -4,12 +4,11 @@
 
 #nullable enable
 
-using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis.Emit;
 using Roslyn.Utilities;
-using EmitContext = Microsoft.CodeAnalysis.Emit.EmitContext;
 
 namespace Microsoft.Cci
 {
@@ -18,288 +17,108 @@ namespace Microsoft.Cci
     /// </summary>
     internal class RootModuleType : INamespaceTypeDefinition
     {
-        public TypeDefinitionHandle TypeDef
-        {
-            get { return default(TypeDefinitionHandle); }
-        }
+        public TypeDefinitionHandle TypeDef => default;
 
-        public ITypeDefinition ResolvedType
-        {
-            get { return this; }
-        }
+        public ITypeDefinition ResolvedType => this;
 
-        public IEnumerable<ICustomAttribute> GetAttributes(EmitContext context)
-        {
-            return SpecializedCollections.EmptyEnumerable<ICustomAttribute>();
-        }
+        public IEnumerable<ICustomAttribute> GetAttributes(EmitContext context) => SpecializedCollections.EmptyEnumerable<ICustomAttribute>();
 
-        public bool MangleName
-        {
-            get { return false; }
-        }
+        public bool MangleName => false;
 
-        public string Name
-        {
-            get { return "<Module>"; }
-        }
+        public string Name => "<Module>";
 
-        public ushort Alignment
-        {
-            get { return 0; }
-        }
+        public ushort Alignment => 0;
 
-        public ITypeReference? GetBaseClass(EmitContext context)
-        {
-            return null;
-        }
+        public ITypeReference? GetBaseClass(EmitContext context) => null;
 
-        public IEnumerable<IEventDefinition> GetEvents(EmitContext context)
-        {
-            return SpecializedCollections.EmptyEnumerable<IEventDefinition>();
-        }
+        public IEnumerable<IEventDefinition> GetEvents(EmitContext context) => SpecializedCollections.EmptyEnumerable<IEventDefinition>();
 
-        public IEnumerable<MethodImplementation> GetExplicitImplementationOverrides(EmitContext context)
-        {
-            return SpecializedCollections.EmptyEnumerable<MethodImplementation>();
-        }
+        public IEnumerable<MethodImplementation> GetExplicitImplementationOverrides(EmitContext context) => SpecializedCollections.EmptyEnumerable<MethodImplementation>();
 
-        public IEnumerable<IFieldDefinition> GetFields(EmitContext context)
-        {
-            return SpecializedCollections.EmptyEnumerable<IFieldDefinition>();
-        }
+        public IEnumerable<IFieldDefinition> GetFields(EmitContext context) => SpecializedCollections.EmptyEnumerable<IFieldDefinition>();
 
-        public bool HasDeclarativeSecurity
-        {
-            get { return false; }
-        }
+        public bool HasDeclarativeSecurity => false;
 
-        public IEnumerable<Cci.TypeReferenceWithAttributes> Interfaces(EmitContext context)
-        {
-            return SpecializedCollections.EmptyEnumerable<Cci.TypeReferenceWithAttributes>();
-        }
+        public IEnumerable<TypeReferenceWithAttributes> Interfaces(EmitContext context) => SpecializedCollections.EmptyEnumerable<TypeReferenceWithAttributes>();
 
-        public bool IsAbstract
-        {
-            get { return false; }
-        }
+        public bool IsAbstract => false;
 
-        public bool IsBeforeFieldInit
-        {
-            get { return false; }
-        }
+        public bool IsBeforeFieldInit => false;
 
-        public bool IsComObject
-        {
-            get { return false; }
-        }
+        public bool IsComObject => false;
 
-        public bool IsGeneric
-        {
-            get { return false; }
-        }
+        public bool IsGeneric => false;
 
-        public bool IsInterface
-        {
-            get { return false; }
-        }
+        public bool IsInterface => false;
 
-        public bool IsDelegate
-        {
-            get { return false; }
-        }
+        public bool IsDelegate => false;
 
-        public bool IsRuntimeSpecial
-        {
-            get { return false; }
-        }
+        public bool IsRuntimeSpecial => false;
 
-        public bool IsSerializable
-        {
-            get { return false; }
-        }
+        public bool IsSerializable => false;
 
-        public bool IsSpecialName
-        {
-            get { return false; }
-        }
+        public bool IsSpecialName => false;
 
-        public bool IsWindowsRuntimeImport
-        {
-            get { return false; }
-        }
+        public bool IsWindowsRuntimeImport => false;
 
-        public bool IsSealed
-        {
-            get { return false; }
-        }
+        public bool IsSealed => false;
 
-        public LayoutKind Layout
-        {
-            get { return LayoutKind.Auto; }
-        }
+        public LayoutKind Layout => LayoutKind.Auto;
 
-        public IEnumerable<IMethodDefinition> GetMethods(EmitContext context)
-        {
-            return SpecializedCollections.EmptyEnumerable<IMethodDefinition>();
-        }
+        public IEnumerable<IMethodDefinition> GetMethods(EmitContext context) => SpecializedCollections.EmptyEnumerable<IMethodDefinition>();
 
-        public IEnumerable<INestedTypeDefinition> GetNestedTypes(EmitContext context)
-        {
-            return SpecializedCollections.EmptyEnumerable<INestedTypeDefinition>();
-        }
+        public IEnumerable<INestedTypeDefinition> GetNestedTypes(EmitContext context) => SpecializedCollections.EmptyEnumerable<INestedTypeDefinition>();
 
-        public IEnumerable<IPropertyDefinition> GetProperties(EmitContext context)
-        {
-            return SpecializedCollections.EmptyEnumerable<IPropertyDefinition>();
-        }
+        public IEnumerable<IPropertyDefinition> GetProperties(EmitContext context) => SpecializedCollections.EmptyEnumerable<IPropertyDefinition>();
 
-        public uint SizeOf
-        {
-            get { return 0; }
-        }
+        public uint SizeOf => 0;
 
-        public CharSet StringFormat
-        {
-            get { return CharSet.Ansi; }
-        }
+        public CharSet StringFormat => CharSet.Ansi;
 
-        public bool IsPublic
-        {
-            get { return false; }
-        }
+        public bool IsPublic => false;
 
-        public bool IsNested
-        {
-            get { return false; }
-        }
+        public bool IsNested => false;
 
-        IEnumerable<IGenericTypeParameter> ITypeDefinition.GenericParameters
-        {
-            get { throw ExceptionUtilities.Unreachable; }
-        }
+        IEnumerable<IGenericTypeParameter> ITypeDefinition.GenericParameters => throw ExceptionUtilities.Unreachable;
 
-        ushort ITypeDefinition.GenericParameterCount
-        {
-            get
-            {
-                return 0;
-            }
-        }
+        ushort ITypeDefinition.GenericParameterCount => 0;
 
-        IEnumerable<SecurityAttribute> ITypeDefinition.SecurityAttributes
-        {
-            get { throw ExceptionUtilities.Unreachable; }
-        }
+        IEnumerable<SecurityAttribute> ITypeDefinition.SecurityAttributes => throw ExceptionUtilities.Unreachable;
 
-        void IReference.Dispatch(MetadataVisitor visitor)
-        {
-            throw ExceptionUtilities.Unreachable;
-        }
+        void IReference.Dispatch(MetadataVisitor visitor) => throw ExceptionUtilities.Unreachable;
 
-        bool ITypeReference.IsEnum
-        {
-            get { throw ExceptionUtilities.Unreachable; }
-        }
+        bool ITypeReference.IsEnum => throw ExceptionUtilities.Unreachable;
 
-        bool ITypeReference.IsValueType
-        {
-            get { throw ExceptionUtilities.Unreachable; }
-        }
+        bool ITypeReference.IsValueType => throw ExceptionUtilities.Unreachable;
 
-        ITypeDefinition ITypeReference.GetResolvedType(EmitContext context)
-        {
-            return this;
-        }
+        ITypeDefinition ITypeReference.GetResolvedType(EmitContext context) => this;
 
-        PrimitiveTypeCode ITypeReference.TypeCode
-        {
-            get { throw ExceptionUtilities.Unreachable; }
-        }
+        PrimitiveTypeCode ITypeReference.TypeCode => throw ExceptionUtilities.Unreachable;
 
-        ushort INamedTypeReference.GenericParameterCount
-        {
-            get { throw ExceptionUtilities.Unreachable; }
-        }
+        ushort INamedTypeReference.GenericParameterCount => throw ExceptionUtilities.Unreachable;
 
-        IUnitReference INamespaceTypeReference.GetUnit(EmitContext context)
-        {
-            throw ExceptionUtilities.Unreachable;
-        }
+        IUnitReference INamespaceTypeReference.GetUnit(EmitContext context) => throw ExceptionUtilities.Unreachable;
 
-        string INamespaceTypeReference.NamespaceName
-        {
-            get
-            {
-                return string.Empty;
-            }
-        }
+        string INamespaceTypeReference.NamespaceName => string.Empty;
 
-        IGenericMethodParameterReference? ITypeReference.AsGenericMethodParameterReference
-        {
-            get
-            {
-                return null;
-            }
-        }
+        IGenericMethodParameterReference? ITypeReference.AsGenericMethodParameterReference => null;
 
-        IGenericTypeInstanceReference? ITypeReference.AsGenericTypeInstanceReference
-        {
-            get
-            {
-                return null;
-            }
-        }
+        IGenericTypeInstanceReference? ITypeReference.AsGenericTypeInstanceReference => null;
 
-        IGenericTypeParameterReference? ITypeReference.AsGenericTypeParameterReference
-        {
-            get
-            {
-                return null;
-            }
-        }
+        IGenericTypeParameterReference? ITypeReference.AsGenericTypeParameterReference => null;
 
-        INamespaceTypeDefinition ITypeReference.AsNamespaceTypeDefinition(EmitContext context)
-        {
-            return this;
-        }
+        INamespaceTypeDefinition ITypeReference.AsNamespaceTypeDefinition(EmitContext context) => this;
 
-        INamespaceTypeReference ITypeReference.AsNamespaceTypeReference
-        {
-            get
-            {
-                return this;
-            }
-        }
+        INamespaceTypeReference ITypeReference.AsNamespaceTypeReference => this;
 
-        INestedTypeDefinition? ITypeReference.AsNestedTypeDefinition(EmitContext context)
-        {
-            return null;
-        }
+        INestedTypeDefinition? ITypeReference.AsNestedTypeDefinition(EmitContext context) => null;
 
-        INestedTypeReference? ITypeReference.AsNestedTypeReference
-        {
-            get
-            {
-                return null;
-            }
-        }
+        INestedTypeReference? ITypeReference.AsNestedTypeReference => null;
 
-        ISpecializedNestedTypeReference? ITypeReference.AsSpecializedNestedTypeReference
-        {
-            get
-            {
-                return null;
-            }
-        }
+        ISpecializedNestedTypeReference? ITypeReference.AsSpecializedNestedTypeReference => null;
 
-        ITypeDefinition ITypeReference.AsTypeDefinition(EmitContext context)
-        {
-            return this;
-        }
+        ITypeDefinition ITypeReference.AsTypeDefinition(EmitContext context) => this;
 
-        IDefinition IReference.AsDefinition(EmitContext context)
-        {
-            return this;
-        }
+        IDefinition IReference.AsDefinition(EmitContext context) => this;
     }
 }
