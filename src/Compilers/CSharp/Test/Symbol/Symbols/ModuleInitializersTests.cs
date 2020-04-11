a51bac27632edb912c9756e63eb707f0dafbc58c
jnm2 <me@jnm2.com>
2020-04-10 21:09:49 -0400

    Test for simple success case

--- src/Compilers/CSharp/Test/Symbol/Symbols/ModuleInitializersTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/ModuleInitializersTests.cs
@@ -58,5 +58,48 @@ namespace System.Runtime.CompilerServices { class ModuleInitializerAttribute : S
                 Diagnostic(ErrorCode.ERR_FeatureInPreview, "ModuleInitializer").WithArguments("module initializers").WithLocation(5, 6)
                 );
         }
+
+        [Fact]
+        public void ModuleTypeStaticConstructorIsNotEmittedWhenNoMethodIsMarkedWithModuleInitializerAttribute()
+        {
+            string source = @"
+using System.Runtime.CompilerServices;
+
+class C
+{
+    internal static void M() { }
+}
+
+namespace System.Runtime.CompilerServices { class ModuleInitializerAttribute : System.Attribute { } }
+";
+            var verifier = CompileAndVerify(source, parseOptions: s_parseOptions);
+
+            verifier.VerifyMemberInIL("<Module>..cctor", expected: false);
+        }
+
+        [Fact]
+        public void ModuleTypeStaticConstructorCallsMethodMarkedWithModuleInitializerAttribute()
+        {
+            string source = @"
+using System.Runtime.CompilerServices;
+
+class C
+{
+    [ModuleInitializer]
+    internal static void M() { }
+}
+
+namespace System.Runtime.CompilerServices { class ModuleInitializerAttribute : System.Attribute { } }
+";
+            var verifier = CompileAndVerify(source, parseOptions: s_parseOptions);
+
+            verifier.VerifyIL("<Module>..cctor", @"
+{
+  // Code size        6 (0x6)
+  .maxstack  0
+  IL_0000:  call       ""void C.M()""
+  IL_0005:  ret
+}");
+        }
     }
 }
