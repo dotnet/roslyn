0a4b1125afe80b0446fccdbd05a0e4562a6d2546
Tomas Matousek <tomat@microsoft.com>
2020-04-13 10:03:47 -0700

    Type forwarder

---
+++ src/Features/Core/Portable/Diagnostics/Analyzers/DocumentDiagnosticAnalyzer.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+// for backward compat with TypeScript (https://github.com/dotnet/roslyn/issues/43313)
+[assembly: TypeForwardedTo(typeof(DocumentDiagnosticAnalyzer))]
