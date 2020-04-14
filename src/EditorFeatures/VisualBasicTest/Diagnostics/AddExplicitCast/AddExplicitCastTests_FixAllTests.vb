a693f31da0d6c731eb51bda58e4faeeb13ecd50b
Julia Feng <y87feng@gmail.com>
2020-04-13 20:06:02 -0700

    fixall tests

--- src/EditorFeatures/VisualBasicTest/Diagnostics/AddExplicitCast/AddExplicitCastTests_FixAllTests.vb
+++ src/EditorFeatures/VisualBasicTest/Diagnostics/AddExplicitCast/AddExplicitCastTests_FixAllTests.vb
@@ -6,7 +6,7 @@ Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.Diagnostics.AddExp
     Partial Public Class AddExplicitCastTests
 
         <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddExplicitCast)>
-        Public Async Function TestFixAllInDocument() As Task
+        Public Async Function TestFixAllInDocumentBC30512() As Task
             Dim input = <Workspace>
                             <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
                                 <Document><![CDATA[
@@ -379,7 +379,7 @@ End Class]]>
         End Function
 
         <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddExplicitCast)>
-        Public Async Function TestFixAllInProject() As Task
+        Public Async Function TestFixAllInProjectBC30512() As Task
             Dim input = <Workspace>
                             <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
                                 <Document><![CDATA[
@@ -754,7 +754,7 @@ End Class]]>
         End Function
 
         <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddExplicitCast)>
-        Public Async Function TestFixAllInSolution() As Task
+        Public Async Function TestFixAllInSolutionBC30512() As Task
             Dim input = <Workspace>
                             <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
                                 <Document><![CDATA[
@@ -1119,6 +1119,621 @@ Public Class Program3
 
     Private Sub returnD2(ByVal b As Base)
         Dim d As Derived = CType(b, Derived)
+    End Sub
+End Class]]>
+                                   </Document>
+                               </Project>
+                           </Workspace>.ToString()
+
+            Await TestInRegularAndScriptAsync(input, expected)
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddExplicitCast)>
+        Public Async Function TestFixAllInDocumentBC30519() As Task
+            Dim input = <Workspace>
+                            <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program1
+    Interface Base1
+    End Interface
+
+    Interface Base2
+        Inherits Base1
+    End Interface
+
+    Interface Base3
+    End Interface
+
+    Class Derived1
+        Implements Base2, Base3
+    End Class
+
+    Class Derived2
+        Inherits Derived1
+    End Class
+
+    Private Sub Foo4(ByVal i As Integer, ByVal j As String, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo4(ByVal j As String, ByVal i As Integer, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived2, ByVal Optional x As Integer = 1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived1, ParamArray d2list As Derived2())
+    End Sub
+
+    Private Sub M2(ByVal b1 As Base1, ByVal b2 As Base2, ByVal b3 As Base3, ByVal d1 As Derived1, ByVal d2 As Derived2)
+        Foo4(1, "", b1)
+        {|FixAllInDocument:Foo4(i:=1, j:="", b1)|}
+        Foo5("", 1, b1)
+        Foo5(d:=b1, i:=1, j:="", x:=1)
+        Foo5(1, "", x:=1, d:=b1)
+        Foo5(1, "", d:=b1, b2, b3, d1)
+        Foo5("", 1, d:=b1, b2, b3, d1)
+        Dim d2list = New Derived2() {}
+        Foo5(j:="", i:=1, d:=b2, d2list)
+        Dim d1list = New Derived1() {}
+        Foo5(j:="", i:=1, d:=b2, d1list)
+    End Sub
+End Class]]>
+                                </Document>
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program2
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                </Document>
+                            </Project>
+                            <Project Language="Visual Basic" AssemblyName="Assembly2" CommonReferences="true">
+                                <ProjectReference>Assembly1</ProjectReference>
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program3
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                </Document>
+                            </Project>
+                        </Workspace>.ToString()
+
+            Dim expected = <Workspace>
+                               <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program1
+    Interface Base1
+    End Interface
+
+    Interface Base2
+        Inherits Base1
+    End Interface
+
+    Interface Base3
+    End Interface
+
+    Class Derived1
+        Implements Base2, Base3
+    End Class
+
+    Class Derived2
+        Inherits Derived1
+    End Class
+
+    Private Sub Foo4(ByVal i As Integer, ByVal j As String, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo4(ByVal j As String, ByVal i As Integer, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived2, ByVal Optional x As Integer = 1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived1, ParamArray d2list As Derived2())
+    End Sub
+
+    Private Sub M2(ByVal b1 As Base1, ByVal b2 As Base2, ByVal b3 As Base3, ByVal d1 As Derived1, ByVal d2 As Derived2)
+        Foo4(1, "", b1)
+        Foo4(i:=1, j:="", CType(b1, Derived1))
+        Foo5("", 1, b1)
+        Foo5(d:=CType(b1, Derived2), i:=1, j:="", x:=1)
+        Foo5(CType(1, String), "", x:=1, d:=b1)
+        Foo5(1, "", d:=b1, b2, b3, d1)
+        Foo5("", 1, d:=b1, b2, b3, d1)
+        Dim d2list = New Derived2() {}
+        Foo5(j:="", i:=1, d:=CType(b2, Derived1), d2list)
+        Dim d1list = New Derived1() {}
+        Foo5(j:="", i:=1, d:=CType(b2, Derived1), d1list)
+    End Sub
+End Class]]>
+                                   </Document>
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program2
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                   </Document>
+                               </Project>
+                               <Project Language="Visual Basic" AssemblyName="Assembly2" CommonReferences="true">
+                                   <ProjectReference>Assembly1</ProjectReference>
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program3
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                   </Document>
+                               </Project>
+                           </Workspace>.ToString()
+
+            Await TestInRegularAndScriptAsync(input, expected)
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddExplicitCast)>
+        Public Async Function TestFixAllInProjectBC30519() As Task
+            Dim input = <Workspace>
+                            <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program1
+    Interface Base1
+    End Interface
+
+    Interface Base2
+        Inherits Base1
+    End Interface
+
+    Interface Base3
+    End Interface
+
+    Class Derived1
+        Implements Base2, Base3
+    End Class
+
+    Class Derived2
+        Inherits Derived1
+    End Class
+
+    Private Sub Foo4(ByVal i As Integer, ByVal j As String, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo4(ByVal j As String, ByVal i As Integer, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived2, ByVal Optional x As Integer = 1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived1, ParamArray d2list As Derived2())
+    End Sub
+
+    Private Sub M2(ByVal b1 As Base1, ByVal b2 As Base2, ByVal b3 As Base3, ByVal d1 As Derived1, ByVal d2 As Derived2)
+        Foo4(1, "", b1)
+        {|FixAllInProject:Foo4(i:=1, j:="", b1)|}
+        Foo5("", 1, b1)
+        Foo5(d:=b1, i:=1, j:="", x:=1)
+        Foo5(1, "", x:=1, d:=b1)
+        Foo5(1, "", d:=b1, b2, b3, d1)
+        Foo5("", 1, d:=b1, b2, b3, d1)
+        Dim d2list = New Derived2() {}
+        Foo5(j:="", i:=1, d:=b2, d2list)
+        Dim d1list = New Derived1() {}
+        Foo5(j:="", i:=1, d:=b2, d1list)
+    End Sub
+End Class]]>
+                                </Document>
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program2
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                </Document>
+                            </Project>
+                            <Project Language="Visual Basic" AssemblyName="Assembly2" CommonReferences="true">
+                                <ProjectReference>Assembly1</ProjectReference>
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program3
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                </Document>
+                            </Project>
+                        </Workspace>.ToString()
+
+            Dim expected = <Workspace>
+                               <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program1
+    Interface Base1
+    End Interface
+
+    Interface Base2
+        Inherits Base1
+    End Interface
+
+    Interface Base3
+    End Interface
+
+    Class Derived1
+        Implements Base2, Base3
+    End Class
+
+    Class Derived2
+        Inherits Derived1
+    End Class
+
+    Private Sub Foo4(ByVal i As Integer, ByVal j As String, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo4(ByVal j As String, ByVal i As Integer, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived2, ByVal Optional x As Integer = 1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived1, ParamArray d2list As Derived2())
+    End Sub
+
+    Private Sub M2(ByVal b1 As Base1, ByVal b2 As Base2, ByVal b3 As Base3, ByVal d1 As Derived1, ByVal d2 As Derived2)
+        Foo4(1, "", b1)
+        Foo4(i:=1, j:="", CType(b1, Derived1))
+        Foo5("", 1, b1)
+        Foo5(d:=CType(b1, Derived2), i:=1, j:="", x:=1)
+        Foo5(CType(1, String), "", x:=1, d:=b1)
+        Foo5(1, "", d:=b1, b2, b3, d1)
+        Foo5("", 1, d:=b1, b2, b3, d1)
+        Dim d2list = New Derived2() {}
+        Foo5(j:="", i:=1, d:=CType(b2, Derived1), d2list)
+        Dim d1list = New Derived1() {}
+        Foo5(j:="", i:=1, d:=CType(b2, Derived1), d1list)
+    End Sub
+End Class]]>
+                                   </Document>
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program2
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(CType(b, Derived), 1)
+    End Sub
+End Class]]>
+                                   </Document>
+                               </Project>
+                               <Project Language="Visual Basic" AssemblyName="Assembly2" CommonReferences="true">
+                                   <ProjectReference>Assembly1</ProjectReference>
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program3
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                   </Document>
+                               </Project>
+                           </Workspace>.ToString()
+
+            Await TestInRegularAndScriptAsync(input, expected)
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddExplicitCast)>
+        Public Async Function TestFixAllInSolutionBC30519() As Task
+            Dim input = <Workspace>
+                            <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program1
+    Interface Base1
+    End Interface
+
+    Interface Base2
+        Inherits Base1
+    End Interface
+
+    Interface Base3
+    End Interface
+
+    Class Derived1
+        Implements Base2, Base3
+    End Class
+
+    Class Derived2
+        Inherits Derived1
+    End Class
+
+    Private Sub Foo4(ByVal i As Integer, ByVal j As String, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo4(ByVal j As String, ByVal i As Integer, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived2, ByVal Optional x As Integer = 1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived1, ParamArray d2list As Derived2())
+    End Sub
+
+    Private Sub M2(ByVal b1 As Base1, ByVal b2 As Base2, ByVal b3 As Base3, ByVal d1 As Derived1, ByVal d2 As Derived2)
+        Foo4(1, "", b1)
+        {|FixAllInSolution:Foo4(i:=1, j:="", b1)|}
+        Foo5("", 1, b1)
+        Foo5(d:=b1, i:=1, j:="", x:=1)
+        Foo5(1, "", x:=1, d:=b1)
+        Foo5(1, "", d:=b1, b2, b3, d1)
+        Foo5("", 1, d:=b1, b2, b3, d1)
+        Dim d2list = New Derived2() {}
+        Foo5(j:="", i:=1, d:=b2, d2list)
+        Dim d1list = New Derived1() {}
+        Foo5(j:="", i:=1, d:=b2, d1list)
+    End Sub
+End Class]]>
+                                </Document>
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program2
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                </Document>
+                            </Project>
+                            <Project Language="Visual Basic" AssemblyName="Assembly2" CommonReferences="true">
+                                <ProjectReference>Assembly1</ProjectReference>
+                                <Document><![CDATA[
+Option Strict On
+Public Class Program3
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(b, 1)
+    End Sub
+End Class]]>
+                                </Document>
+                            </Project>
+                        </Workspace>.ToString()
+
+            Dim expected = <Workspace>
+                               <Project Language="Visual Basic" AssemblyName="Assembly1" CommonReferences="true">
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program1
+    Interface Base1
+    End Interface
+
+    Interface Base2
+        Inherits Base1
+    End Interface
+
+    Interface Base3
+    End Interface
+
+    Class Derived1
+        Implements Base2, Base3
+    End Class
+
+    Class Derived2
+        Inherits Derived1
+    End Class
+
+    Private Sub Foo4(ByVal i As Integer, ByVal j As String, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo4(ByVal j As String, ByVal i As Integer, ByVal d As Derived1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived2, ByVal Optional x As Integer = 1)
+    End Sub
+
+    Private Sub Foo5(ByVal j As String, ByVal i As Integer, ByVal d As Derived1, ParamArray d2list As Derived2())
+    End Sub
+
+    Private Sub M2(ByVal b1 As Base1, ByVal b2 As Base2, ByVal b3 As Base3, ByVal d1 As Derived1, ByVal d2 As Derived2)
+        Foo4(1, "", b1)
+        Foo4(i:=1, j:="", CType(b1, Derived1))
+        Foo5("", 1, b1)
+        Foo5(d:=CType(b1, Derived2), i:=1, j:="", x:=1)
+        Foo5(CType(1, String), "", x:=1, d:=b1)
+        Foo5(1, "", d:=b1, b2, b3, d1)
+        Foo5("", 1, d:=b1, b2, b3, d1)
+        Dim d2list = New Derived2() {}
+        Foo5(j:="", i:=1, d:=CType(b2, Derived1), d2list)
+        Dim d1list = New Derived1() {}
+        Foo5(j:="", i:=1, d:=CType(b2, Derived1), d1list)
+    End Sub
+End Class]]>
+                                   </Document>
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program2
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(CType(b, Derived), 1)
+    End Sub
+End Class]]>
+                                   </Document>
+                               </Project>
+                               <Project Language="Visual Basic" AssemblyName="Assembly2" CommonReferences="true">
+                                   <ProjectReference>Assembly1</ProjectReference>
+                                   <Document><![CDATA[
+Option Strict On
+Public Class Program3
+    Class Base
+    End Class
+
+    Class Derived
+        Inherits Base
+    End Class
+
+    Public Sub Foo(ByRef d As Derived, i As Integer)
+    End Sub
+
+    Public Sub Foo(ByRef d As Derived, d2 As Derived)
+    End Sub
+
+    Public Sub M()
+        Dim b As Base = New Derived
+        Foo(CType(b, Derived), 1)
     End Sub
 End Class]]>
                                    </Document>
