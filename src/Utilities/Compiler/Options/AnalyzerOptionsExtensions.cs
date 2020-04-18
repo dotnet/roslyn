88c96b644d2cb1ffbb1f084136c60cbc1c81cf2c
Amaury Levé <evangelink@gmail.com>
2020-04-18 23:29:32 +0200

    Fix broken build

--- src/Utilities/Compiler/Options/AnalyzerOptionsExtensions.cs
+++ src/Utilities/Compiler/Options/AnalyzerOptionsExtensions.cs
@@ -377,15 +377,6 @@ namespace Analyzer.Utilities
         public static SymbolNamesWithValueOption<Unit> GetInheritanceExcludedSymbolNamesOption(
             this AnalyzerOptions options,
             DiagnosticDescriptor rule,
-            ISymbol symbol,
-            Compilation compilation,
-            string defaultForcedValue,
-            CancellationToken cancellationToken)
-        => options.GetInheritanceExcludedSymbolNamesOption(rule, symbol.Locations[0].SourceTree, compilation, defaultForcedValue, cancellationToken);
-
-        public static SymbolNamesWithValueOption<Unit> GetInheritanceExcludedSymbolNamesOption(
-            this AnalyzerOptions options,
-            DiagnosticDescriptor rule,
             SyntaxTree tree,
             Compilation compilation,
             string defaultForcedValue,
@@ -395,9 +386,10 @@ namespace Analyzer.Utilities
         public static SymbolNamesWithValueOption<Unit> GetAdditionalUseResultsMethodsOption(
             this AnalyzerOptions options,
             DiagnosticDescriptor rule,
+            SyntaxTree tree,
             Compilation compilation,
             CancellationToken cancellationToken)
-            => options.GetSymbolNamesWithValueOption<Unit>(EditorConfigOptionNames.AdditionalUseResultsMethods, rule, compilation, cancellationToken, namePrefixOpt: "M:");
+            => options.GetSymbolNamesWithValueOption<Unit>(EditorConfigOptionNames.AdditionalUseResultsMethods, rule, tree, compilation, cancellationToken, namePrefixOpt: "M:");
 
         private static SymbolNamesWithValueOption<TValue> GetSymbolNamesWithValueOption<TValue>(
             this AnalyzerOptions options,
