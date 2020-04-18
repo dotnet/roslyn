43b08c3932fe05ac315a94ccd04bfbfc9f6e1772
Rikki Gibson <rigibson@microsoft.com>
2020-04-18 13:39:57 -0700

    "build directory" -> "build current project"

--- scripts/vscode-run-tests.ps1
+++ scripts/vscode-run-tests.ps1
@@ -1,55 +1,34 @@
 param (
-    [Parameter(Mandatory=$true)][string]$filePath,
-    [Parameter(Mandatory=$true)][string]$framework,
-    [Parameter(Mandatory=$false)][string]$filter
+  [Parameter(Mandatory = $true)][string]$filePath,
+  [Parameter(Mandatory = $true)][string]$framework,
+  [Parameter(Mandatory = $false)][string]$filter
 )
 
 Set-StrictMode -version 3.0
-$ErrorActionPreference="Stop"
+$ErrorActionPreference = "Stop"
 
 . (Join-Path $PSScriptRoot "../eng/build-utils.ps1")
 
-Push-Location
-
-$originalCwd = Get-Location
 $fileInfo = Get-ItemProperty $filePath
-Set-Location $fileInfo.Directory
+$projectFileInfo = Get-ProjectFile $fileInfo
+if ($projectFileInfo) {
+  $dotnetPath = Resolve-Path (Ensure-DotNetSdk) -Relative
+  $projectDir = Resolve-Path $projectFileInfo.Directory -Relative
 
-try {
-    while ($true) {
-        # search up from the current file for a folder containing a csproj
-        $files = Get-ChildItem *.csproj
-        if ($files) {
-            Set-Location $originalCwd
-            $dotnetPath = Resolve-Path (Ensure-DotNetSdk) -Relative
+  $filterArg = if ($filter) { " --filter $filter" } else { "" }
+  $logFileName = if ($filter) { $fileInfo.Name } else { $projectFileInfo.Name }
 
-            $projectFileInfo = $files[0]
-            $projectDir = Resolve-Path $projectFileInfo.Directory -Relative
+  $resultsPath = Join-Path $PSScriptRoot ".." "artifacts/TestResults"
+  $resultsPath = Resolve-Path (New-Item -ItemType Directory -Force -Path $resultsPath) -Relative
 
-            $filterArg = if ($filter) {" --filter $filter"} else {""}
-            $logFileName = if ($filter) {$fileInfo.Name} else {$projectFileInfo.Name}
+  $invocation = "$dotnetPath test $projectDir$filterArg --framework $framework --logger `"html;LogFileName=$logfileName.html`" --results-directory $resultsPath"
+  Write-Output "> $invocation"
+  Invoke-Expression $invocation
 
-            $resultsPath = Join-Path (Resolve-Path "$PSScriptRoot/.." -Relative) "/artifacts/TestResults"
-            New-Item -ItemType Directory -Force -Path $resultsPath | Out-Null
-
-            $invocation = "$dotnetPath test $projectDir$filterArg --framework $framework --logger `"html;LogFileName=$logfileName.html`" --results-directory $resultsPath"
-            Write-Output "> $invocation"
-            Invoke-Expression $invocation
-
-            exit 0
-        }
-        else {
-            $location = Get-Location
-            Set-Location ..
-            if ((Get-Location).Path -eq $location.Path) {
-                # our location didn't change. We must be at the drive root, so give up
-                Write-Host "Failed to run tests. $fileInfo is not part of a C# project."
-
-                exit 1
-            }
-        }
-    }
+  exit 0
 }
-finally {
-    Pop-Location
+else {
+  Write-Host "Failed to run tests. $fileInfo is not part of a C# project."
+
+  exit 1
 }
