10f383f889fa3f70ba44146970c40bb90e81cbe3
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-04-20 18:05:46 -0700

    Simplify

--- src/Features/Core/Portable/CodeRefactorings/SyncNamespace/AbstractChangeNamespaceService.cs
+++ src/Features/Core/Portable/CodeRefactorings/SyncNamespace/AbstractChangeNamespaceService.cs
@@ -479,12 +479,8 @@ namespace Microsoft.CodeAnalysis.ChangeNamespace
             cancellationToken.ThrowIfCancellationRequested();
             var progress = new StreamingProgressCollector();
             await SymbolFinder.FindReferencesAsync(
-                symbol,
-                document.Project.Solution,
-                documents: null,
-                progress: progress,
-                options: FindReferencesSearchOptions.Default,
-                cancellationToken: cancellationToken).ConfigureAwait(false);
+                symbol, document.Project.Solution, progress, documents: null,
+                FindReferencesSearchOptions.Default, cancellationToken).ConfigureAwait(false);
 
             return progress.GetReferencedSymbols();
         }
