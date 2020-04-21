977bc469c65953455dab243768cc6b1b1af5f7a7
Milla Zuniga <t-miziga@microsoft.com>
2020-04-21 11:23:51 -0700

    comment for conditional statement difference of compilers

--- src/Scripting/CSharpTest/ObjectFormatterTests.cs
+++ src/Scripting/CSharpTest/ObjectFormatterTests.cs
@@ -77,6 +77,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Scripting.Hosting.UnitTests
         public void AnonymousType()
         {
             string anon = s_formatter.FormatObject(new { X = 1, Y = 2 });
+
+            // conditional statement for difference of old vs. new compilers
             if (anon.StartsWith("\\"))
             {
                 anon = anon.Substring(1);
