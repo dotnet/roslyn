0daa32b9edd04b9db71adec832c3f291499959be
Tomáš Matoušek <tmat@users.noreply.github.com>
2020-04-22 18:21:42 -0700

    Replace GeneratePkgDef with impl that does not load assemblies (#43302)

--- src/VisualStudio/IntegrationTest/TestSetup/IntegrationTestServicePackage.cs
+++ src/VisualStudio/IntegrationTest/TestSetup/IntegrationTestServicePackage.cs
@@ -12,10 +12,6 @@ using Task = System.Threading.Tasks.Task;
 namespace Microsoft.VisualStudio.IntegrationTest.Setup
 {
     [Guid("D02DAC01-DDD0-4ECC-8687-79A554852B14")]
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [ProvideMenuResource("Menus.ctmenu", version: 1)]
-    [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class IntegrationTestServicePackage : AsyncPackage
     {
         private static readonly Guid s_compilerPackage = new Guid("31C0675E-87A4-4061-A0DD-A4E510FCCF97");
