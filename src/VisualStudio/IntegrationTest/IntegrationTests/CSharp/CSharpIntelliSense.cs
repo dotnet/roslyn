f664ac310fcd2ee31b1c33365f683350688c01fe
Sam Harwell <Sam.Harwell@microsoft.com>
2020-04-23 11:33:02 -0700

    Dismiss completion sessions in SetUpEditor

--- src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpIntelliSense.cs
+++ src/VisualStudio/IntegrationTest/IntegrationTests/CSharp/CSharpIntelliSense.cs
@@ -168,6 +168,7 @@ public static class NavigateTo
             VisualStudio.Editor.Verify.CurrentLineText("namespace Goo$$", assertCaretPosition: true);
 
             ClearEditor();
+            Assert.False(VisualStudio.Editor.IsCompletionActive());
             VisualStudio.Editor.SetUseSuggestionMode(true);
 
             VisualStudio.Editor.SendKeys("nam");
