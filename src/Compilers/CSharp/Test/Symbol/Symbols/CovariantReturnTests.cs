5210155566f6c7b5d57b741a9cce2b552b00619a
Neal Gafter <ngafter@microsoft.com>
2020-04-24 18:18:37 -0700

    - Emit language version suggestion for property overrides when applicable. - Add test for override properties with setters. - Test relaxed rules for override in source vs a metadata base type.

--- src/Compilers/CSharp/Test/Symbol/Symbols/CovariantReturnTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/CovariantReturnTests.cs
@@ -2,7 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
+using Microsoft.CodeAnalysis.Test.Utilities;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Symbols
@@ -13,11 +15,11 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Symbols
         public void CovariantReturns_01()
         {
             var source = @"
-class Base
+public class Base
 {
     public virtual object M() => null;
 }
-class Derived : Base
+public class Derived : Base
 {
     public override string M() => null;
 }
@@ -35,11 +37,11 @@ class Derived : Base
         public void CovariantReturns_02()
         {
             var source = @"
-class Base
+public class Base
 {
     public virtual T M<T, U>() where T : class where U : class, T => null;
 }
-class Derived : Base
+public class Derived : Base
 {
     public override U M<T, U>() => null;
 }
@@ -57,11 +59,11 @@ class Derived : Base
         public void CovariantReturns_03()
         {
             var source = @"
-class Base<T> where T : class
+public class Base<T> where T : class
 {
     public virtual T M() => null;
 }
-class Derived<T, U> : Base<T> where T : class where U : class, T
+public class Derived<T, U> : Base<T> where T : class where U : class, T
 {
     public override U M() => null;
 }
@@ -79,12 +81,12 @@ class Derived<T, U> : Base<T> where T : class where U : class, T
         public void CovariantReturns_04()
         {
             var source = @"
-class N { }
-class Base
+public class N { }
+public class Base
 {
     public virtual N M() => null;
 }
-class Derived<T> : Base where T : N
+public class Derived<T> : Base where T : N
 {
     public override T M() => null;
 }
@@ -102,19 +104,19 @@ class Derived<T> : Base where T : N
         public void CovariantReturns_05()
         {
             var source = @"
-class Base
+public class Base
 {
     public virtual object M => null;
 }
-class Derived : Base
+public class Derived : Base
 {
     public override string M => null;
 }
 ";
             CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
-                // (8,28): error CS1715: 'Derived.M': type must be 'object' to match overridden member 'Base.M'
+                // (8,28): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
                 //     public override string M => null;
-                Diagnostic(ErrorCode.ERR_CantChangeTypeOnOverride, "M").WithArguments("Derived.M", "Base.M", "object").WithLocation(8, 28)
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "M").WithArguments("covariant returns").WithLocation(8, 28)
                 );
             CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
                 );
@@ -124,19 +126,19 @@ class Derived : Base
         public void CovariantReturns_06()
         {
             var source = @"
-class Base<T> where T : class
+public class Base<T> where T : class
 {
     public virtual T M => null;
 }
-class Derived<T, U> : Base<T> where T : class where U : class, T
+public class Derived<T, U> : Base<T> where T : class where U : class, T
 {
     public override U M => null;
 }
 ";
             CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
-                // (8,23): error CS1715: 'Derived<T, U>.M': type must be 'T' to match overridden member 'Base<T>.M'
+                // (8,23): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
                 //     public override U M => null;
-                Diagnostic(ErrorCode.ERR_CantChangeTypeOnOverride, "M").WithArguments("Derived<T, U>.M", "Base<T>.M", "T").WithLocation(8, 23)
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "M").WithArguments("covariant returns").WithLocation(8, 23)
                 );
             CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
                 );
@@ -146,20 +148,20 @@ class Derived<T, U> : Base<T> where T : class where U : class, T
         public void CovariantReturns_07()
         {
             var source = @"
-class N { }
-class Base
+public class N { }
+public class Base
 {
     public virtual N M => null;
 }
-class Derived<T> : Base where T : N
+public class Derived<T> : Base where T : N
 {
     public override T M => null;
 }
 ";
             CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
-                // (9,23): error CS1715: 'Derived<T>.M': type must be 'N' to match overridden member 'Base.M'
+                // (9,23): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
                 //     public override T M => null;
-                Diagnostic(ErrorCode.ERR_CantChangeTypeOnOverride, "M").WithArguments("Derived<T>.M", "Base.M", "N").WithLocation(9, 23)
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "M").WithArguments("covariant returns").WithLocation(9, 23)
                 );
             CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
                 );
@@ -169,19 +171,19 @@ class Derived<T> : Base where T : N
         public void CovariantReturns_08()
         {
             var source = @"
-class Base
+public class Base
 {
     public virtual object this[int i] => null;
 }
-class Derived : Base
+public class Derived : Base
 {
     public override string this[int i] => null;
 }
 ";
             CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
-                // (8,28): error CS1715: 'Derived.this[int]': type must be 'object' to match overridden member 'Base.this[int]'
+                // (8,28): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
                 //     public override string this[int i] => null;
-                Diagnostic(ErrorCode.ERR_CantChangeTypeOnOverride, "this").WithArguments("Derived.this[int]", "Base.this[int]", "object").WithLocation(8, 28)
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "this").WithArguments("covariant returns").WithLocation(8, 28)
                 );
             CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
                 );
@@ -191,19 +193,19 @@ class Derived : Base
         public void CovariantReturns_09()
         {
             var source = @"
-class Base<T> where T : class
+public class Base<T> where T : class
 {
     public virtual T this[int i] => null;
 }
-class Derived<T, U> : Base<T> where T : class where U : class, T
+public class Derived<T, U> : Base<T> where T : class where U : class, T
 {
     public override U this[int i] => null;
 }
 ";
             CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
-                // (8,23): error CS1715: 'Derived<T, U>.this[int]': type must be 'T' to match overridden member 'Base<T>.this[int]'
+                // (8,23): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
                 //     public override U this[int i] => null;
-                Diagnostic(ErrorCode.ERR_CantChangeTypeOnOverride, "this").WithArguments("Derived<T, U>.this[int]", "Base<T>.this[int]", "T").WithLocation(8, 23)
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "this").WithArguments("covariant returns").WithLocation(8, 23)
                 );
             CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
                 );
@@ -213,20 +215,20 @@ class Derived<T, U> : Base<T> where T : class where U : class, T
         public void CovariantReturns_10()
         {
             var source = @"
-class N { }
-class Base
+public class N { }
+public class Base
 {
     public virtual N this[int i] => null;
 }
-class Derived<T> : Base where T : N
+public class Derived<T> : Base where T : N
 {
     public override T this[int i] => null;
 }
 ";
             CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
-                // (9,23): error CS1715: 'Derived<T>.this[int]': type must be 'N' to match overridden member 'Base.this[int]'
+                // (9,23): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
                 //     public override T this[int i] => null;
-                Diagnostic(ErrorCode.ERR_CantChangeTypeOnOverride, "this").WithArguments("Derived<T>.this[int]", "Base.this[int]", "N").WithLocation(9, 23)
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "this").WithArguments("covariant returns").WithLocation(9, 23)
                 );
             CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
                 );
@@ -237,12 +239,12 @@ class Derived<T> : Base where T : N
         {
             var source = @"
 using System;
-class Base
+public class Base
 {
     public virtual event Func<object> E;
     private void SuppressUnusedWarning() => E?.Invoke();
 }
-class Derived : Base
+public class Derived : Base
 {
     public override event Func<string> E;
     private void SuppressUnusedWarning() => E?.Invoke();
@@ -260,16 +262,166 @@ class Derived : Base
                 );
         }
 
+        [Fact]
+        public void CovariantReturns_WritableProperties()
+        {
+            var source = @"
+using System;
+public class Base
+{
+    public virtual Func<object> P { get; set; }
+}
+public class Derived : Base
+{
+    public override Func<string> P { get; set; }
+}
+";
+            CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
+                // (9,38): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+                //     public override Func<string> P { get; set; }
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "get").WithArguments("covariant returns").WithLocation(9, 38),
+                // (9,43): error CS0115: 'Derived.P.set': no suitable method found to override
+                //     public override Func<string> P { get; set; }
+                Diagnostic(ErrorCode.ERR_OverrideNotExpected, "set").WithArguments("Derived.P.set").WithLocation(9, 43)
+                );
+            CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
+                // (9,43): error CS0115: 'Derived.P.set': no suitable method found to override
+                //     public override Func<string> P { get; set; }
+                Diagnostic(ErrorCode.ERR_OverrideNotExpected, "set").WithArguments("Derived.P.set").WithLocation(9, 43)
+                );
+        }
+
+        [Fact]
+        public void CovariantReturns_MetadataVsSource_01()
+        {
+            var s0 = @"
+public class Base
+{
+    public virtual object M() => null;
+}
+";
+            var baseMetadata = CreateCompilation(s0).EmitToImageReference();
+            var source = @"
+public class Derived : Base
+{
+    public override string M() => null;
+}
+";
+            CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns, references: new[] { baseMetadata }).VerifyDiagnostics(
+                // (4,28): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+                //     public override string M() => null;
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "M").WithArguments("covariant returns").WithLocation(4, 28)
+                );
+            CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns, references: new[] { baseMetadata }).VerifyDiagnostics(
+                );
+        }
+
+        [Fact]
+        public void CovariantReturns_MetadataVsSource_02()
+        {
+            var s0 = @"
+public class Base
+{
+    public virtual object M => null;
+}
+";
+            var baseMetadata = CreateCompilation(s0).EmitToImageReference();
+            var source = @"
+public class Derived : Base
+{
+    public override string M => null;
+}
+";
+            CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns, references: new[] { baseMetadata }).VerifyDiagnostics(
+                // (4,28): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+                //     public override string M => null;
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "M").WithArguments("covariant returns").WithLocation(4, 28)
+                );
+            CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns, references: new[] { baseMetadata }).VerifyDiagnostics(
+                );
+        }
+
+        [Fact]
+        public void CovariantReturns_MetadataVsSource_03()
+        {
+            var s0 = @"
+public class Base
+{
+    public virtual object this[int i] => null;
+}
+";
+            var baseMetadata = CreateCompilation(s0).EmitToImageReference();
+            var source = @"
+public class Derived : Base
+{
+    public override string this[int i] => null;
+}
+";
+            CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns, references: new[] { baseMetadata }).VerifyDiagnostics(
+                // (4,28): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+                //     public override string this[int i] => null;
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "this").WithArguments("covariant returns").WithLocation(4, 28)
+                );
+            CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns, references: new[] { baseMetadata }).VerifyDiagnostics(
+                );
+        }
+
+        [Fact]
+        public void CovariantReturns_11()
+        {
+            var source = @"
+public abstract class Base
+{
+    public abstract object M();
+}
+public class Derived : Base
+{
+    public override string M() => null;
+}
+";
+            CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
+                // (8,28): error CS8652: The feature 'covariant returns' is currently in Preview and *unsupported*. To use Preview features, use the 'preview' language version.
+                //     public override string M() => null;
+                Diagnostic(ErrorCode.ERR_FeatureInPreview, "M").WithArguments("covariant returns").WithLocation(8, 28)
+                );
+            CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
+                );
+        }
+
+        [Fact]
+        public void CovariantReturns_WrongReturnType()
+        {
+            var source = @"
+public class Base
+{
+    public virtual string M() => null;
+}
+public class Derived : Base
+{
+    public override object M() => null;
+}
+";
+            CreateCompilation(source, parseOptions: TestOptions.WithoutCovariantReturns).VerifyDiagnostics(
+                // (8,28): error CS0508: 'Derived.M()': return type must be 'string' to match overridden member 'Base.M()'
+                //     public override object M() => null;
+                Diagnostic(ErrorCode.ERR_CantChangeReturnTypeOnOverride, "M").WithArguments("Derived.M()", "Base.M()", "string").WithLocation(8, 28)
+                );
+            CreateCompilation(source, parseOptions: TestOptions.WithCovariantReturns).VerifyDiagnostics(
+                // (8,28): error CS0508: 'Derived.M()': return type must be 'string' to match overridden member 'Base.M()'
+                //     public override object M() => null;
+                Diagnostic(ErrorCode.ERR_CantChangeReturnTypeOnOverride, "M").WithArguments("Derived.M()", "Base.M()", "string").WithLocation(8, 28)
+                );
+        }
+
+
         // PROTOTYPE: Future tests to be added:
         // - What is expected for public Derived : Base { public new string M => null;
         // - What is expected for public Derived : Base { public string M => null;
         // - What is expected for public Derived2: Derived { public new object/string M => null;
         // - Please add a test where Base has a property, Derived hides it with a different return type, and Derived2 tries to override with either return type.
         //   - These are also applicable to virtual methods.
-        // - I assume that an abstract method can be implemented with a covariant return. Please add a test.
         // - Please add a test with nested variance involved (returning CIn<object> vs.CIn<string>, or COut<object> vs.COut<string>). Also consider nullability variance (COut<object?>vs.COut<string!>` and some permutations).
         // - Test with an override that doesn't have an implicit reference conversion from base. For instance, numeric types, types convertible via user-defined operators, etc.
-        // - Test with wrong variance in override (Base.Method() returns string but override Derived.Method() returns object)
         // - Test other implicit reference conversion scenarios, such as Interface Base.Method() and TypeThatImplementsInterface Derived.Method(), to lock-in the proper check
         // - Test some DIM scenarios (no changed behavior)
         // - Test that UD conversions don't count (not an implicit reference conversion)
