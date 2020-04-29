1b17eae2aaa77c8c5f057dc657fc2a9d93fadb59
Tomáš Matoušek <tmat@users.noreply.github.com>
2020-04-29 14:34:08 -0700

    Move RazorRemoteHostClient functionality to ExternalAccess.Razor (#43770)
    * Move RazorRemoteHostClient functionality to ExternalAccess.Razor
    
    * Add file

--- src/Tools/ExternalAccess/Razor/RazorRemoteHostClient.cs
+++ src/Tools/ExternalAccess/Razor/RazorRemoteHostClient.cs
@@ -2,45 +2,56 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Experiments;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Remote;
 
-namespace Microsoft.VisualStudio.LanguageServices.Razor
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
 {
-    [Obsolete("Use Microsoft.CodeAnalysis.ExternalAccess.Razor.RazorRemoteHostClient instead")]
-    internal static class RazorLanguageServiceClientFactory
+    internal sealed class RazorRemoteHostClient
     {
-        public static async Task<RazorLanguageServiceClient> CreateAsync(Workspace workspace, CancellationToken cancellationToken = default)
+        private readonly RemoteHostClient _client;
+        private readonly string _serviceName;
+
+        internal RazorRemoteHostClient(RemoteHostClient client, string serviceName)
+        {
+            _client = client;
+            _serviceName = serviceName;
+        }
+
+        public static async Task<RazorRemoteHostClient?> CreateAsync(Workspace workspace, CancellationToken cancellationToken = default)
         {
             var clientFactory = workspace.Services.GetRequiredService<IRemoteHostClientService>();
             var client = await clientFactory.TryGetRemoteHostClientAsync(cancellationToken).ConfigureAwait(false);
-            return client == null ? null : new RazorLanguageServiceClient(client, GetServiceName(workspace));
+            return client == null ? null : new RazorRemoteHostClient(client, GetServiceName(workspace));
         }
 
+        public Task<Optional<T>> TryRunRemoteAsync<T>(string targetName, Solution? solution, IReadOnlyList<object?> arguments, CancellationToken cancellationToken)
+            => _client.TryRunRemoteAsync<T>(_serviceName, targetName, solution, arguments, callbackTarget: null, cancellationToken);
+
         #region support a/b testing. after a/b testing, we can remove all this code
-        private static string s_serviceNameDoNotAccessDirectly = null;
+
+        private static string? s_lazyServiceName = null;
 
         private static string GetServiceName(Workspace workspace)
         {
-            if (s_serviceNameDoNotAccessDirectly == null)
+            if (s_lazyServiceName == null)
             {
                 var x64 = workspace.Options.GetOption(OOP64Bit);
                 if (!x64)
                 {
-                    x64 = workspace.Services.GetService<IExperimentationService>().IsExperimentEnabled(
+                    x64 = workspace.Services.GetRequiredService<IExperimentationService>().IsExperimentEnabled(
                         WellKnownExperimentNames.RoslynOOP64bit);
                 }
 
                 Interlocked.CompareExchange(
-                    ref s_serviceNameDoNotAccessDirectly, x64 ? "razorLanguageService64" : "razorLanguageService", null);
+                    ref s_lazyServiceName, x64 ? "razorLanguageService64" : "razorLanguageService", null);
             }
 
-            return s_serviceNameDoNotAccessDirectly;
+            return s_lazyServiceName;
         }
 
         public static readonly Option<bool> OOP64Bit = new Option<bool>(
@@ -51,6 +62,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Razor
         {
             internal const string LocalRegistryPath = @"Roslyn\Internal\OnOff\Features\";
         }
+
         #endregion
     }
 }
