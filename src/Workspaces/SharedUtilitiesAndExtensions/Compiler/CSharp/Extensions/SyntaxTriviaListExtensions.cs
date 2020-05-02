489a63541175b11ae5547e3230b2c8717b7cbd97
Ivan Shimko <vana.shimko@gmail.com>
2020-05-02 21:30:45 +0300

    Update CollapseSequentialWhitespaceTrivia comment

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Extensions/SyntaxTriviaListExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Extensions/SyntaxTriviaListExtensions.cs
@@ -92,8 +92,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Extensions
         }
 
         /// <summary>
-        /// Removes multiple <see cref="SyntaxToken"/> with kind <see cref="SyntaxKind.WhitespaceTrivia"/>
-        /// arranged in a sequence from the <paramref name="triviaList"/>
+        /// Returns modified <paramref name="triviaList"/> with removed multiple <see cref="SyntaxToken"/>
+        /// with kind <see cref="SyntaxKind.WhitespaceTrivia"/> arranged in a sequence 
         /// preserving only the first one: [space1][space2][space3] -> [space1]. 
         /// </summary>
         public static SyntaxTriviaList CollapseSequentialWhitespaceTrivia(this SyntaxTriviaList triviaList)
