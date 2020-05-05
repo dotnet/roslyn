daa9ab6a6042cb1343aa64e84b36fdf48b4d5460
Ivan Shimko <vana.shimko@gmail.com>
2020-05-05 09:10:08 +0300

    Bring back "fix all" test

--- src/EditorFeatures/CSharpTest/Diagnostics/RemoveNewModifier/RemoveNewModifierCodeFixProviderTests.cs
+++ src/EditorFeatures/CSharpTest/Diagnostics/RemoveNewModifier/RemoveNewModifierCodeFixProviderTests.cs
@@ -14,8 +14,8 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics.RemoveNewMo
     [Trait(Traits.Feature, Traits.Features.CodeActionsRemoveNewModifier)]
     public class RemoveNewModifierCodeFixProviderTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
     {
-        internal override (DiagnosticAnalyzer, CodeFixProvider) CreateDiagnosticProviderAndFixer(Workspace workspace) =>
-            (null, new RemoveNewModifierCodeFixProvider());
+        internal override (DiagnosticAnalyzer, CodeFixProvider) CreateDiagnosticProviderAndFixer(Workspace workspace)
+            => (null, new RemoveNewModifierCodeFixProvider());
 
         [Theory]
         [InlineData(
@@ -84,6 +84,49 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics.RemoveNewMo
             "/* start */ int [|Test|];")]
         public Task TestRemoveNewFromModifiersWithComplexTrivia(string original, string expected)
             => TestRemoveNewModifierCodeFixAsync(original, expected);
+
+        [Fact]
+        public Task TestRemoveNewFromModifiersFixAll()
+            => TestInRegularAndScriptAsync(@"	
+using System;	
+class B	
+{	
+    public int ValidNew;	
+}	
+class C : B	
+{	
+    public new int ValidNew;	
+    public new void {|FixAllInDocument:M|}() { }	
+    public new int F;	
+    public new event Action E;	
+    public new int P { get; }	
+    public new int this[int p] => p;	
+    new class C2 { }	
+    new struct S2 { }	
+    new interface I2 { }	
+    new delegate void D2();	
+    new enum E2 { }	
+}",
+                @"	
+using System;	
+class B	
+{	
+    public int ValidNew;	
+}	
+class C : B	
+{	
+    public new int ValidNew;	
+    public void M() { }	
+    public int F;	
+    public event Action E;	
+    public int P { get; }	
+    public int this[int p] => p;	
+    class C2 { }	
+    struct S2 { }	
+    interface I2 { }	
+    delegate void D2();	
+    enum E2 { }	
+}");
 
         private Task TestRemoveNewModifierCodeFixAsync(string original, string expected)
         {
