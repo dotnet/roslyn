4385ecdf7fb77113e0c11fd0c950bb9d5895b33b
Sam Harwell <Sam.Harwell@microsoft.com>
2020-05-06 12:00:45 -0700

    Simplify code setting pasted text span for remote calls

--- src/Workspaces/Remote/Core/Services/RemotePasteTrackingService.cs
+++ src/Workspaces/Remote/Core/Services/RemotePasteTrackingService.cs
@@ -7,9 +7,11 @@
 using System;
 using System.Collections.Generic;
 using System.Composition;
+using System.Diagnostics;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.PasteTracking;
 using Microsoft.CodeAnalysis.Text;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Remote
 {
@@ -17,7 +19,7 @@ namespace Microsoft.CodeAnalysis.Remote
     [Shared]
     internal sealed class RemotePasteTrackingService : IPasteTrackingService
     {
-        private readonly Dictionary<SourceTextContainer, TextSpan> _trackedSpans = new Dictionary<SourceTextContainer, TextSpan>();
+        private readonly Dictionary<SourceTextContainer, (TextSpan? span, int referenceCount)> _trackedSpans = new Dictionary<SourceTextContainer, (TextSpan? span, int referenceCount)>();
 
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
@@ -29,30 +31,74 @@ namespace Microsoft.CodeAnalysis.Remote
         {
             lock (_trackedSpans)
             {
-                return _trackedSpans.TryGetValue(sourceTextContainer, out textSpan);
-            }
-        }
-
-        internal void ClearPastedTextSpan(SourceTextContainer container)
-        {
-            lock (_trackedSpans)
-            {
-                _trackedSpans.Remove(container);
-            }
-        }
-
-        internal void SetPastedTextSpan(SourceTextContainer container, TextSpan? pastedTextSpan)
-        {
-            lock (_trackedSpans)
-            {
-                if (pastedTextSpan is object)
+                var result = _trackedSpans.TryGetValue(sourceTextContainer, out var spanAndCount);
+                if (!result || spanAndCount.referenceCount == 0 || spanAndCount.span is null)
                 {
-                    _trackedSpans[container] = pastedTextSpan.Value;
+                    textSpan = default;
+                    return false;
                 }
-                else
+
+                textSpan = spanAndCount.span.Value;
+                return result;
+            }
+        }
+
+        private void AddPastedTextSpanReference(SourceTextContainer container, TextSpan? pastedTextSpan)
+        {
+            lock (_trackedSpans)
+            {
+                if (!_trackedSpans.TryGetValue(container, out var spanAndCount))
                 {
+                    _trackedSpans[container] = (pastedTextSpan, 1);
+                    return;
+                }
+
+                if (spanAndCount.span != pastedTextSpan)
+                {
+                    // Cannot track two different pasted spans for the same container
+                    throw ExceptionUtilities.UnexpectedValue(pastedTextSpan);
+                }
+
+                Debug.Assert(spanAndCount.referenceCount > 0);
+                _trackedSpans[container] = (spanAndCount.span, spanAndCount.referenceCount + 1);
+            }
+        }
+
+        private void ReleasePastedTextSpanReference(SourceTextContainer container)
+        {
+            lock (_trackedSpans)
+            {
+                if (!_trackedSpans.TryGetValue(container, out var spanAndCount))
+                    throw ExceptionUtilities.UnexpectedValue(container);
+
+                Debug.Assert(spanAndCount.referenceCount > 0);
+                if (spanAndCount.referenceCount <= 1)
                     _trackedSpans.Remove(container);
-                }
+                else
+                    _trackedSpans[container] = (spanAndCount.span, spanAndCount.referenceCount - 1);
+            }
+        }
+
+        internal Releaser SetPastedTextSpanForRemoteCall(SourceTextContainer container, TextSpan? pastedTextSpan)
+        {
+            AddPastedTextSpanReference(container, pastedTextSpan);
+            return new Releaser(this, container);
+        }
+
+        internal readonly struct Releaser : IDisposable
+        {
+            private readonly RemotePasteTrackingService _service;
+            private readonly SourceTextContainer _container;
+
+            public Releaser(RemotePasteTrackingService service, SourceTextContainer container)
+            {
+                _service = service;
+                _container = container;
+            }
+
+            public void Dispose()
+            {
+                _service.ReleasePastedTextSpanReference(_container);
             }
         }
     }
--- src/Workspaces/Remote/ServiceHub/Services/CodeAnalysisService_CodeRefactoring.cs
+++ src/Workspaces/Remote/ServiceHub/Services/CodeAnalysisService_CodeRefactoring.cs
@@ -2,12 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.PasteTracking;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Remote
@@ -21,24 +24,18 @@ namespace Microsoft.CodeAnalysis.Remote
                 using (UserOperationBooster.Boost())
                 {
                     var solution = await GetSolutionAsync(solutionInfo, cancellationToken).ConfigureAwait(false);
-                    var document = solution.GetDocument(documentId);
+                    var document = solution.GetRequiredDocument(documentId);
 
                     var mefHostExportProvider = (IMefHostExportProvider)solution.Workspace.Services.HostServices;
                     var service = mefHostExportProvider.GetExports<ICodeRefactoringService>().Single().Value;
-                    var pasteTrackingService = (RemotePasteTrackingService)mefHostExportProvider.GetExports<IPasteTrackingService>().SingleOrDefault()?.Value;
 
+                    // Make sure the paste tracking service for this process has the correct text span
+                    var pasteTrackingService = (RemotePasteTrackingService?)mefHostExportProvider.GetExports<IPasteTrackingService>().SingleOrDefault()?.Value;
                     var sourceText = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
                     var container = sourceText.Container;
+                    using var _ = pasteTrackingService?.SetPastedTextSpanForRemoteCall(container, pastedTextSpan);
 
-                    try
-                    {
-                        pasteTrackingService?.SetPastedTextSpan(container, pastedTextSpan);
-                        return await service.HasRefactoringsAsync(document, textSpan, pastedTextSpan, cancellationToken).ConfigureAwait(false);
-                    }
-                    finally
-                    {
-                        pasteTrackingService?.ClearPastedTextSpan(container);
-                    }
+                    return await service.HasRefactoringsAsync(document, textSpan, pastedTextSpan, cancellationToken).ConfigureAwait(false);
                 }
             }, cancellationToken);
         }
