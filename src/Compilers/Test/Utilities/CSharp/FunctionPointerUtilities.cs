cae7f81659369762011683f094d645c916d1a1b8
Fredric Silberberg <frsilb@microsoft.com>
2020-05-07 10:49:56 -0700

    Function pointer symbols cannot be referenced by name.

--- src/Compilers/Test/Utilities/CSharp/FunctionPointerUtilities.cs
+++ src/Compilers/Test/Utilities/CSharp/FunctionPointerUtilities.cs
@@ -32,6 +32,7 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
                 Assert.False(symbol.IsStatic);
                 Assert.False(symbol.IsAbstract);
                 Assert.False(symbol.IsSealed);
+                Assert.False(symbol.CanBeReferencedByName);
 
                 Assert.True(symbol.IsValueType);
                 Assert.True(symbol.CanBeAssignedNull());
