86a9b729df5807f7d5592a7b5325e01e564e8741
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-05-06 19:43:16 -0700

    Add assembly and module

---
+++ src/Compilers/Core/Portable/SourceGeneration/CodeGeneration/CodeGenerator_AssemblySymbol.cs
@@ -0,0 +1,91 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Microsoft.CodeAnalysis.SourceGeneration
+{
+    internal static partial class CodeGenerator
+    {
+        public static IAssemblySymbol Assembly(IModuleSymbol module, ImmutableArray<AttributeData> attributes = default)
+            => new AssemblySymbol(module, attributes);
+
+        public static IAssemblySymbol With(
+            this IAssemblySymbol assembly,
+            Optional<IModuleSymbol> module = default,
+            Optional<ImmutableArray<AttributeData>> attributes = default)
+        {
+            return new AssemblySymbol(
+                module.GetValueOr(assembly.Modules.First()),
+                attributes.GetValueOr(assembly.GetAttributes()));
+        }
+
+        private class AssemblySymbol : Symbol, IAssemblySymbol
+        {
+            private readonly ImmutableArray<IModuleSymbol> _modules;
+            private readonly ImmutableArray<AttributeData> _attributes;
+
+            public AssemblySymbol(
+                IModuleSymbol module,
+                ImmutableArray<AttributeData> attributes)
+            {
+                _modules = ImmutableArray.Create(module);
+                _attributes = attributes;
+            }
+
+            public override SymbolKind Kind => SymbolKind.Assembly;
+
+            public IEnumerable<IModuleSymbol> Modules
+                => _modules;
+
+            public override ImmutableArray<AttributeData> GetAttributes()
+                => _attributes;
+
+            public override void Accept(SymbolVisitor visitor)
+                => visitor.VisitAssembly(this);
+
+            public override TResult Accept<TResult>(SymbolVisitor<TResult> visitor)
+                => visitor.VisitAssembly(this);
+
+            #region default implementation
+
+            public bool IsInteractive => throw new NotImplementedException();
+
+            public AssemblyIdentity Identity => throw new NotImplementedException();
+
+            public ICollection<string> TypeNames => throw new NotImplementedException();
+
+            public ICollection<string> NamespaceNames => throw new NotImplementedException();
+
+            public bool MightContainExtensionMethods => throw new NotImplementedException();
+
+            public INamespaceSymbol GlobalNamespace => throw new NotImplementedException();
+
+            public bool GivesAccessTo(IAssemblySymbol toAssembly)
+            {
+                throw new NotImplementedException();
+            }
+
+            public INamedTypeSymbol GetTypeByMetadataName(string fullyQualifiedMetadataName)
+            {
+                throw new NotImplementedException();
+            }
+
+            public INamedTypeSymbol ResolveForwardedType(string fullyQualifiedMetadataName)
+            {
+                throw new NotImplementedException();
+            }
+
+            public AssemblyMetadata GetMetadata()
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ src/Compilers/Core/Portable/SourceGeneration/CodeGeneration/CodeGenerator_ModuleSymbol.cs
@@ -0,0 +1,68 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+
+namespace Microsoft.CodeAnalysis.SourceGeneration
+{
+    internal static partial class CodeGenerator
+    {
+        public static IModuleSymbol Module(INamespaceSymbol globalNamespace, ImmutableArray<AttributeData> attributes = default)
+            => new ModuleSymbol(globalNamespace, attributes);
+
+        public static IModuleSymbol With(
+            this IModuleSymbol module,
+            Optional<INamespaceSymbol> globalNamespace = default,
+            Optional<ImmutableArray<AttributeData>> attributes = default)
+        {
+            return new ModuleSymbol(
+                globalNamespace.GetValueOr(module.GlobalNamespace),
+                attributes.GetValueOr(module.GetAttributes()));
+        }
+
+        private class ModuleSymbol : Symbol, IModuleSymbol
+        {
+            private readonly ImmutableArray<AttributeData> _attributes;
+
+            public ModuleSymbol(
+                INamespaceSymbol globalNamespace,
+                ImmutableArray<AttributeData> attributes)
+            {
+                GlobalNamespace = globalNamespace;
+                _attributes = attributes;
+            }
+
+            public override SymbolKind Kind => SymbolKind.NetModule;
+            public INamespaceSymbol GlobalNamespace { get; }
+
+            public override ImmutableArray<AttributeData> GetAttributes()
+                => _attributes;
+
+            public override void Accept(SymbolVisitor visitor)
+                => visitor.VisitModule(this);
+
+            public override TResult Accept<TResult>(SymbolVisitor<TResult> visitor)
+                => visitor.VisitModule(this);
+
+            #region default implementation
+
+            public ImmutableArray<AssemblyIdentity> ReferencedAssemblies => throw new NotImplementedException();
+
+            public ImmutableArray<IAssemblySymbol> ReferencedAssemblySymbols => throw new NotImplementedException();
+
+            public ModuleMetadata GetMetadata()
+            {
+                throw new NotImplementedException();
+            }
+
+            public INamespaceSymbol GetModuleNamespace(INamespaceSymbol namespaceSymbol)
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+        }
+    }
+}
