6a7786e55f90cf5fdf5cd564e15a41ed62bf0a93
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-05-06 20:15:10 -0700

    Pointer types

---
+++ src/Compilers/Core/Portable/SourceGeneration/CodeGeneration/CodeGenerator_PointerTypeSymbol.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+
+namespace Microsoft.CodeAnalysis.SourceGeneration
+{
+    internal static partial class CodeGenerator
+    {
+        public static IPointerTypeSymbol Pointer(ITypeSymbol pointedAtType)
+            => new PointerTypeSymbol(pointedAtType);
+
+        public static IPointerTypeSymbol With(
+            this IPointerTypeSymbol pointer,
+            Optional<ITypeSymbol> pointedAtType = default)
+        {
+            return new PointerTypeSymbol(
+                pointedAtType.GetValueOr(pointer.PointedAtType));
+        }
+
+        private class PointerTypeSymbol : TypeSymbol, IPointerTypeSymbol
+        {
+            public PointerTypeSymbol(ITypeSymbol pointedAtType)
+            {
+                PointedAtType = pointedAtType;
+            }
+
+            public override SymbolKind Kind => SymbolKind.PointerType;
+            public override TypeKind TypeKind => TypeKind.Pointer;
+
+            public ITypeSymbol PointedAtType { get; }
+
+            public override void Accept(SymbolVisitor visitor)
+                => visitor.VisitPointerType(this);
+
+            public override TResult Accept<TResult>(SymbolVisitor<TResult> visitor)
+                => visitor.VisitPointerType(this);
+
+            #region default implementation
+
+            public ImmutableArray<CustomModifier> CustomModifiers => throw new NotImplementedException();
+
+            #endregion
+        }
+    }
+}
