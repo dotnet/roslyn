b034801fd263eef544105a596b5259d15a36fc63
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-05-07 23:58:54 -0700

    Add methods

---
+++ src/Compilers/Core/Portable/SourceGeneration/CodeGeneration/CodeGenerator_MethodSymbol.cs
@@ -0,0 +1,141 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+
+namespace Microsoft.CodeAnalysis.SourceGeneration
+{
+    internal static partial class CodeGenerator
+    {
+        public static IMethodSymbol Method(
+            ITypeSymbol returnType,
+            string name,
+            ImmutableArray<AttributeData> attributes = default,
+            Accessibility declaredAccessibility = default,
+            SymbolModifiers modifiers = default,
+            ImmutableArray<IMethodSymbol> explicitInterfaceImplementations = default,
+            ImmutableArray<ITypeSymbol> typeArguments = default,
+            ImmutableArray<IParameterSymbol> parameters = default)
+        {
+            return new MethodSymbol(
+                MethodKind.Ordinary,
+                attributes,
+                declaredAccessibility,
+                modifiers,
+                returnType,
+                explicitInterfaceImplementations,
+                name,
+                typeArguments,
+                parameters);
+        }
+
+        public static IMethodSymbol With(
+            this IMethodSymbol method,
+            Optional<MethodKind> methodKind = default,
+            Optional<ImmutableArray<AttributeData>> attributes = default,
+            Optional<Accessibility> declaredAccessibility = default,
+            Optional<SymbolModifiers> modifiers = default,
+            Optional<ITypeSymbol> returnType = default,
+            Optional<ImmutableArray<IMethodSymbol>> explicitInterfaceImplementations = default,
+            Optional<string> name = default,
+            Optional<ImmutableArray<ITypeSymbol>> typeArguments = default,
+            Optional<ImmutableArray<IParameterSymbol>> parameters = default)
+        {
+            return new MethodSymbol(
+                methodKind.GetValueOr(method.MethodKind),
+                attributes.GetValueOr(method.GetAttributes()),
+                declaredAccessibility.GetValueOr(method.DeclaredAccessibility),
+                modifiers.GetValueOr(method.GetModifiers()),
+                returnType.GetValueOr(method.ReturnType),
+                explicitInterfaceImplementations.GetValueOr(method.ExplicitInterfaceImplementations),
+                name.GetValueOr(method.Name),
+                typeArguments.GetValueOr(method.TypeArguments),
+                parameters.GetValueOr(method.Parameters));
+        }
+
+        private class MethodSymbol : Symbol, IMethodSymbol
+        {
+            private readonly ImmutableArray<AttributeData> _attributes;
+
+            public MethodSymbol(
+                MethodKind methodKind,
+                ImmutableArray<AttributeData> attributes,
+                Accessibility declaredAccessibility,
+                SymbolModifiers modifiers,
+                ITypeSymbol returnType,
+                ImmutableArray<IMethodSymbol> explicitInterfaceImplementations,
+                string name,
+                ImmutableArray<ITypeSymbol> typeArguments,
+                ImmutableArray<IParameterSymbol> parameters)
+            {
+                MethodKind = methodKind;
+                _attributes = attributes;
+                DeclaredAccessibility = declaredAccessibility;
+                Modifiers = modifiers;
+                ReturnType = returnType;
+                ExplicitInterfaceImplementations = explicitInterfaceImplementations;
+                Name = name;
+                TypeArguments = typeArguments;
+                Parameters = parameters;
+            }
+
+            public override Accessibility DeclaredAccessibility { get; }
+            public override SymbolKind Kind => SymbolKind.Method;
+            public override SymbolModifiers Modifiers { get; }
+            public override string Name { get; }
+
+            public ImmutableArray<IMethodSymbol> ExplicitInterfaceImplementations { get; }
+            public ImmutableArray<IParameterSymbol> Parameters { get; }
+            public ImmutableArray<ITypeSymbol> TypeArguments { get; }
+            public ITypeSymbol ReturnType { get; }
+            public MethodKind MethodKind { get; }
+
+            public override void Accept(SymbolVisitor visitor)
+                => visitor.VisitMethod(this);
+
+            public override TResult Accept<TResult>(SymbolVisitor<TResult> visitor)
+                => visitor.VisitMethod(this);
+
+            #region default implementation
+
+            IMethodSymbol IMethodSymbol.OriginalDefinition => throw new NotImplementedException();
+            public bool HidesBaseMethodsByName => throw new NotImplementedException();
+            public bool IsAsync => throw new NotImplementedException();
+            public bool IsCheckedBuiltin => throw new NotImplementedException();
+            public bool IsConditional => throw new NotImplementedException();
+            public bool IsExtensionMethod => throw new NotImplementedException();
+            public bool IsGenericMethod => throw new NotImplementedException();
+            public bool IsReadOnly => throw new NotImplementedException();
+            public bool IsVararg => throw new NotImplementedException();
+            public bool ReturnsByRef => throw new NotImplementedException();
+            public bool ReturnsByRefReadonly => throw new NotImplementedException();
+            public bool ReturnsVoid => throw new NotImplementedException();
+            public DllImportData GetDllImportData() => throw new NotImplementedException();
+            public IMethodSymbol Construct(ImmutableArray<ITypeSymbol> typeArguments, ImmutableArray<NullableAnnotation> typeArgumentNullableAnnotations) => throw new NotImplementedException();
+            public IMethodSymbol Construct(params ITypeSymbol[] typeArguments) => throw new NotImplementedException();
+            public IMethodSymbol ConstructedFrom => throw new NotImplementedException();
+            public IMethodSymbol OverriddenMethod => throw new NotImplementedException();
+            public IMethodSymbol PartialDefinitionPart => throw new NotImplementedException();
+            public IMethodSymbol PartialImplementationPart => throw new NotImplementedException();
+            public IMethodSymbol ReducedFrom => throw new NotImplementedException();
+            public IMethodSymbol ReduceExtensionMethod(ITypeSymbol receiverType) => throw new NotImplementedException();
+            public ImmutableArray<AttributeData> GetReturnTypeAttributes() => throw new NotImplementedException();
+            public ImmutableArray<CustomModifier> RefCustomModifiers => throw new NotImplementedException();
+            public ImmutableArray<CustomModifier> ReturnTypeCustomModifiers => throw new NotImplementedException();
+            public ImmutableArray<ITypeParameterSymbol> TypeParameters => throw new NotImplementedException();
+            public ImmutableArray<NullableAnnotation> TypeArgumentNullableAnnotations => throw new NotImplementedException();
+            public INamedTypeSymbol AssociatedAnonymousDelegate => throw new NotImplementedException();
+            public int Arity => throw new NotImplementedException();
+            public ISymbol AssociatedSymbol => throw new NotImplementedException();
+            public ITypeSymbol GetTypeInferredDuringReduction(ITypeParameterSymbol reducedFromTypeParameter) => throw new NotImplementedException();
+            public ITypeSymbol ReceiverType => throw new NotImplementedException();
+            public NullableAnnotation ReceiverNullableAnnotation => throw new NotImplementedException();
+            public NullableAnnotation ReturnNullableAnnotation => throw new NotImplementedException();
+            public RefKind RefKind => throw new NotImplementedException();
+
+            #endregion
+        }
+    }
+}
