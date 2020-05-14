2e8525920f7f8ea5105cb466fdabcbcc2e8752c0
Alexander Gayko <a.gayko@googlemail.com>
2020-05-14 02:11:08 +0200

    deleted three unused methods that were a remnant from copy-pasting.

--- src/Features/Core/Portable/DocumentationComments/AbstractDocumentationCommentFormattingService.cs
+++ src/Features/Core/Portable/DocumentationComments/AbstractDocumentationCommentFormattingService.cs
@@ -260,59 +260,6 @@ namespace Microsoft.CodeAnalysis.DocumentationComments
             }
         }
 
-        private static string GetDocumentation(ISymbol symbol, Compilation compilation, CancellationToken cancellationToken)
-            => symbol switch
-            {
-                IParameterSymbol parameter => GetParameterDocumentation(parameter, compilation, cancellationToken),
-                ITypeParameterSymbol typeParam => typeParam.ContainingSymbol.GetDocumentationComment(compilation, expandIncludes: true, expandInheritdoc: true, cancellationToken: cancellationToken).GetTypeParameterText(symbol.Name),
-                IMethodSymbol method => GetMethodDocumentation(method, compilation, cancellationToken),
-                IAliasSymbol alias => alias.Target.GetDocumentationComment(compilation, expandIncludes: true, expandInheritdoc: true, cancellationToken: cancellationToken).SummaryText,
-                _ => symbol.GetDocumentationComment(compilation, expandIncludes: true, expandInheritdoc: true, cancellationToken: cancellationToken).SummaryText,
-            };
-
-        private static string GetParameterDocumentation(IParameterSymbol parameter, Compilation compilation, CancellationToken cancellationToken)
-        {
-            var containingSymbol = parameter.ContainingSymbol;
-            if (containingSymbol.ContainingSymbol.IsDelegateType() && containingSymbol is IMethodSymbol methodSymbol)
-            {
-                // There are two ways to invoke a delegate that we care about here: the Invoke()/BeginInvoke() methods. (Direct invocation is equivalent to an Invoke() call.)
-                // DynamicInvoke() takes an object array, and EndInvoke() takes a System.IAsyncResult, so we can (and should) ignore those here.
-
-                var symbolName = methodSymbol.Name;
-                if (symbolName == WellKnownMemberNames.DelegateBeginInvokeName && parameter.Ordinal >= (methodSymbol.Parameters.Length - 2))
-                {
-                    // Return null (similar to DocumentationComment.GetParameterText()) for the last two implicit parameters (usually called "callback" and "@object").
-                    // We can't rely on those names because they might be renamed to avoid collision with a user-defined delegate parameter of the same name,
-                    // and we have to treat them separately, because a user might add e.g. a '<param name="callback">' tag to the delegate, which would be displayed in Signature Help for that implicit parameter.
-                    return null;
-                }
-
-                if (symbolName == WellKnownMemberNames.DelegateInvokeName || symbolName == WellKnownMemberNames.DelegateBeginInvokeName)
-                {
-                    // We know that containingSymbol is the [Begin]Invoke() method of a delegate type, so we need to go up a level and take the method's containing symbol (i.e. the delegate), which contains the documentation.
-                    containingSymbol = containingSymbol.ContainingSymbol;
-                }
-            }
-
-            // Get the comments from the original definition of the containing symbol.
-            return containingSymbol.OriginalDefinition.GetDocumentationComment(compilation, expandIncludes: true, expandInheritdoc: true, cancellationToken: cancellationToken).GetParameterText(parameter.Name);
-        }
-
-        private static string GetMethodDocumentation(IMethodSymbol method, Compilation compilation, CancellationToken cancellationToken)
-        {
-            switch (method.MethodKind)
-            {
-                case MethodKind.EventAdd:
-                case MethodKind.EventRaise:
-                case MethodKind.EventRemove:
-                case MethodKind.PropertyGet:
-                case MethodKind.PropertySet:
-                    return method.AssociatedSymbol.GetDocumentationComment(compilation, expandIncludes: true, expandInheritdoc: true, cancellationToken: cancellationToken).SummaryText;
-                default:
-                    return method.GetDocumentationComment(compilation, expandIncludes: true, expandInheritdoc: true, cancellationToken: cancellationToken).SummaryText;
-            }
-        }
-
         public string Format(string rawXmlText, Compilation compilation = null)
         {
             if (rawXmlText == null)
