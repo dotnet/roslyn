66bbe82840aa4ee56253df59097051f8bac8abb1
AlekseyTs <AlekseyTs@users.noreply.github.com>
2020-05-20 10:24:26 -0700

    Follow-up on Test Plan review for Top-level statements (#44419)
    Related to #41704.
    Add tests, remove PROTOTYPE comments

--- src/Compilers/CSharp/Portable/FlowAnalysis/DataFlowsOutWalker.cs
+++ src/Compilers/CSharp/Portable/FlowAnalysis/DataFlowsOutWalker.cs
@@ -37,8 +37,8 @@ namespace Microsoft.CodeAnalysis.CSharp
                 var result = walker.Analyze(ref badRegion);
 #if DEBUG
                 // Assert that DataFlowsOut only contains variables that were assigned to inside the region
-                // PROTOTYPE(TopLevelStatements): https://github.com/dotnet/roslyn/issues/41600 blocks some tests with local functions. 
-                //                            Enable the following assert once the issue is fixed.
+                // https://github.com/dotnet/roslyn/issues/41600 blocks some tests with local functions. 
+                // Enable the following assert once the issue is fixed.
                 //Debug.Assert(badRegion || !result.Any((variable) => !walker._assignedInside.Contains(variable)));
 #endif
                 return badRegion ? new HashSet<Symbol>() : result;
