6a2f989fd2ff844a55515a71ec02dc17147672bd
Shen Chen <shech@microsoft.com>
2020-05-28 12:01:12 -0700

    Obselete some more APIs

--- src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/UnitTestingImmutableArrayExtensions.cs
+++ src/Workspaces/Core/Portable/ExternalAccess/UnitTesting/Api/UnitTestingImmutableArrayExtensions.cs
@@ -2,12 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
 {
+    [Obsolete]
     internal static class UnitTestingImmutableArrayExtensions
     {
         public static ImmutableArray<byte> ToImmutable(this MemoryStream stream)
