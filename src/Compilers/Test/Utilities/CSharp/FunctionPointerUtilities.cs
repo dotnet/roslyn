b204d56137074a96b86fa27df6b7ff0f6b3ac2e3
Fredric Silberberg <frsilb@microsoft.com>
2020-05-28 12:01:47 -0700

    Adjust reported conversion for explicit conversions to match method groups.

--- src/Compilers/Test/Utilities/CSharp/FunctionPointerUtilities.cs
+++ src/Compilers/Test/Utilities/CSharp/FunctionPointerUtilities.cs
@@ -181,7 +181,13 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
                 case PrefixUnaryExpressionSyntax { RawKind: (int)SyntaxKind.AddressOfExpression, Operand: var operand }:
                     // Members should only be accessible from the underlying operand
                     Assert.Empty(semanticInfo.MemberGroup);
-                    Assert.Equal(expectedSymbol is null ? ConversionKind.NoConversion : ConversionKind.MethodGroup, semanticInfo.ImplicitConversion.Kind);
+                    var expectedConversionKind = (expectedType, expectedConvertedType, expectedSymbol) switch
+                    {
+                        (null, null, object _) => ConversionKind.Identity,
+                        (_, _, null) => ConversionKind.NoConversion,
+                        (_, _, _) => ConversionKind.MethodGroup,
+                    };
+                    Assert.Equal(expectedConversionKind, semanticInfo.ImplicitConversion.Kind);
 
                     semanticInfo = model.GetSemanticInfoSummary(operand);
                     Assert.Null(semanticInfo.Type);
