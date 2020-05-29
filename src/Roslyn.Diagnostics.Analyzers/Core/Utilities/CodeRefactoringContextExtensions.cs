7d62a6667db7cb83c08923797f69673ecf49d1de
Sam Harwell <Sam.Harwell@microsoft.com>
2020-05-29 11:30:22 -0700

    Port IRefactoringHelpers from dotnet/roslyn

---
+++ src/Roslyn.Diagnostics.Analyzers/Core/Utilities/CodeRefactoringContextExtensions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Analyzer.Utilities
+{
+    internal static class CodeRefactoringContextExtensions
+    {
+        internal static Task<TSyntaxNode?> TryGetRelevantNodeAsync<TSyntaxNode>(this CodeRefactoringContext context, IRefactoringHelpers helpers)
+            where TSyntaxNode : SyntaxNode
+            => TryGetRelevantNodeAsync<TSyntaxNode>(context.Document, helpers, context.Span, context.CancellationToken);
+
+        internal static Task<ImmutableArray<TSyntaxNode>> GetRelevantNodesAsync<TSyntaxNode>(this CodeRefactoringContext context, IRefactoringHelpers helpers)
+            where TSyntaxNode : SyntaxNode
+            => GetRelevantNodesAsync<TSyntaxNode>(context.Document, helpers, context.Span, context.CancellationToken);
+
+        internal static async Task<TSyntaxNode?> TryGetRelevantNodeAsync<TSyntaxNode>(
+            this Document document,
+            IRefactoringHelpers helpers,
+            TextSpan span,
+            CancellationToken cancellationToken)
+            where TSyntaxNode : SyntaxNode
+        {
+            var potentialNodes = await GetRelevantNodesAsync<TSyntaxNode>(document, helpers, span, cancellationToken).ConfigureAwait(false);
+            return potentialNodes.FirstOrDefault();
+        }
+
+        internal static Task<ImmutableArray<TSyntaxNode>> GetRelevantNodesAsync<TSyntaxNode>(
+            this Document document,
+            IRefactoringHelpers helpers,
+            TextSpan span,
+            CancellationToken cancellationToken) where TSyntaxNode : SyntaxNode
+        {
+            return helpers.GetRelevantNodesAsync<TSyntaxNode>(document, span, cancellationToken);
+        }
+    }
+}
---
+++ src/Roslyn.Diagnostics.Analyzers/Core/Utilities/Extensions/SyntaxTreeExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Analyzer.Utilities.Extensions
+{
+    internal static class SyntaxTreeExtensions
+    {
+        public static bool OverlapsHiddenPosition([NotNullWhen(returnValue: true)] this SyntaxTree? tree, TextSpan span, CancellationToken cancellationToken)
+        {
+            if (tree == null)
+            {
+                return false;
+            }
+
+            var text = tree.GetText(cancellationToken);
+
+            return text.OverlapsHiddenPosition(
+                span,
+                (position, cancellationToken2) =>
+                {
+                    // implements the ASP.NET IsHidden rule
+                    var lineVisibility = tree.GetLineVisibility(position, cancellationToken2);
+                    return lineVisibility == LineVisibility.Hidden || lineVisibility == LineVisibility.BeforeFirstLineDirective;
+                },
+                cancellationToken);
+        }
+    }
+}
