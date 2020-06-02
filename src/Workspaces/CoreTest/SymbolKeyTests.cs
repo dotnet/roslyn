120769ebf9039a8951694c3d8ee66c8f5b12d154
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2020-06-01 19:32:03 -0700

    Update src/Workspaces/CoreTest/SymbolKeyTests.cs
    Co-authored-by: Jason Malinowski <jason@jason-m.com>

--- src/Workspaces/CoreTest/SymbolKeyTests.cs
+++ src/Workspaces/CoreTest/SymbolKeyTests.cs
@@ -817,7 +817,7 @@ class C
             Assert.Equal(propType.Name, found.Name);
             Assert.Equal(propType.Kind, found.Kind);
 
-            var method = found as IErrorTypeSymbol;
+            var method = (IErrorTypeSymbol)found;
             Assert.True(SymbolEquivalenceComparer.Instance.Equals(propType, found));
         }
 
