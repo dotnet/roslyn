bd3d5aed04a70d20feeebf85bc671929268e5cba
Sam Harwell <Sam.Harwell@microsoft.com>
2020-06-03 09:12:48 -0700

    Add regression test for #327

---
+++ tests/VisualStudio.Roslyn.SDK/Roslyn.SDK.UnitTests/ResourceLocationTests.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Testing
+{
+    public class ResourceLocationTests
+    {
+        [Fact]
+        [WorkItem(327, "https://github.com/dotnet/roslyn-sdk/issues/327")]
+        public void TestDgmlHelperResources()
+        {
+            Assert.NotNull(Roslyn.SyntaxVisualizer.DgmlHelper.My.Resources.Resources.SyntaxNodeLabel);
+        }
+    }
+}
