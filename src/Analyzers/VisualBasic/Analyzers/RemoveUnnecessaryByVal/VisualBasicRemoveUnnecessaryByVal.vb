1f494a0f5f3a68dd8d41b154abde348fb18f1ee9
Youssef Victor <youssefvictor00@gmail.com>
2020-06-03 19:41:23 +0200

    Feedback

--- src/Analyzers/VisualBasic/Analyzers/RemoveUnnecessaryByVal/VisualBasicRemoveUnnecessaryByVal.vb
+++ src/Analyzers/VisualBasic/Analyzers/RemoveUnnecessaryByVal/VisualBasicRemoveUnnecessaryByVal.vb
@@ -3,48 +3,52 @@
 ' See the LICENSE file in the project root for more information.
 
 Imports System.Collections.Immutable
-Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.CodeStyle
 Imports Microsoft.CodeAnalysis.Diagnostics
-Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 
-<DiagnosticAnalyzer(LanguageNames.VisualBasic)>
-Friend Class VisualBasicRemoveUnnecessaryByVal
-    Inherits DiagnosticAnalyzer
-    Implements IBuiltInAnalyzer ' Should I implement IBuiltInAnalyzer??
+Namespace Microsoft.CodeAnalysis.VisualBasic.RemoveUnnecessaryByVal
 
-    Friend Const DiagnosticFixableId As String = "RemoveUnnecessaryByVal"
-    Private Shared ReadOnly s_descriptor As DiagnosticDescriptor = New DiagnosticDescriptor(id:="RemoveUnnecessaryByVal",
-                                                                                            title:="",
-                                                                                            messageFormat:="",
-                                                                                            category:="",
-                                                                                            defaultSeverity:=DiagnosticSeverity.Hidden,
-                                                                                            isEnabledByDefault:=True,
-                                                                                            description:="",
-                                                                                            helpLinkUri:="",
-                                                                                            WellKnownDiagnosticTags.Unnecessary)
+    <DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+    Friend Class VisualBasicRemoveUnnecessaryByVal
+        Inherits AbstractCodeStyleDiagnosticAnalyzer
 
-    Public Overrides ReadOnly Property SupportedDiagnostics As ImmutableArray(Of DiagnosticDescriptor)
-        Get
-            Return ImmutableArray.Create(s_descriptor)
-        End Get
-    End Property
+        Friend Const DiagnosticFixableId As String = IDEDiagnosticIds.RemoveUnnecessaryByValDiagnosticId
+        Private Shared ReadOnly s_descriptor As DiagnosticDescriptor = New DiagnosticDescriptor(id:=DiagnosticFixableId,
+            title:="",
+            messageFormat:="",
+            category:="",
+            defaultSeverity:=DiagnosticSeverity.Hidden,
+            isEnabledByDefault:=True,
+            description:="",
+            helpLinkUri:="",
+            WellKnownDiagnosticTags.Unnecessary)
 
-    Public Overrides Sub Initialize(context As AnalysisContext)
-        context.EnableConcurrentExecution()
-        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze Or GeneratedCodeAnalysisFlags.ReportDiagnostics)
-        context.RegisterSyntaxNodeAction(
+        Public Overrides ReadOnly Property SupportedDiagnostics As ImmutableArray(Of DiagnosticDescriptor) = ImmutableArray.Create(s_descriptor)
+
+        Public Sub New()
+            MyBase.New(s_descriptor.Id, s_descriptor.Title, s_descriptor.MessageFormat)
+        End Sub
+
+        Protected Overrides Sub InitializeWorker(context As AnalysisContext)
+            context.EnableConcurrentExecution()
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze Or GeneratedCodeAnalysisFlags.ReportDiagnostics)
+
+            context.RegisterSyntaxNodeAction(
             Sub(syntaxContext As SyntaxNodeAnalysisContext)
-                syntaxContext.ReportDiagnostic(DiagnosticHelper.Create(
-                                               s_descriptor, syntaxContext.Node.GetLocation(), ReportDiagnostic.Hidden, Nothing, Nothing))
-            End Sub, SyntaxKind.ByValKeyword)
-    End Sub
+                Dim location = GetByValLocation(CType(syntaxContext.Node, ParameterSyntax).Modifiers)
+                If location IsNot Nothing Then
+                    syntaxContext.ReportDiagnostic(DiagnosticHelper.Create(
+                                               s_descriptor, location, ReportDiagnostic.Hidden, Nothing, Nothing))
+                End If
+            End Sub, SyntaxKind.Parameter)
+        End Sub
 
-    Public Function GetAnalyzerCategory() As DiagnosticAnalyzerCategory Implements IBuiltInAnalyzer.GetAnalyzerCategory
-        ' What should be returned here?
-        Return DiagnosticAnalyzerCategory.None
-    End Function
-
-    Public Function OpenFileOnly(options As AnalyzerConfigOptions) As Boolean Implements IBuiltInAnalyzer.OpenFileOnly
-        Return False ' Is that correct? I don't know how OpenFileOnly exactly works, but removing ByVal can be done in all files not only the open one.
-    End Function
-End Class
+        Private Function GetByValLocation(modifiers As SyntaxTokenList) As Location
+            For Each modifier In modifiers
+                If modifier.IsKind(SyntaxKind.ByValKeyword) Then Return modifier.GetLocation()
+            Next
+            Return Nothing
+        End Function
+    End Class
+End Namespace
