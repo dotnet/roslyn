2196cad943f0f8e5de337c195b57524cefb2bc70
Youssef Victor <youssefvictor00@gmail.com>
2020-06-03 23:57:30 +0200

    Add tests and fixer

---
+++ src/Analyzers/VisualBasic/CodeFixes/RemoveUnnecessaryByVal/VisualBasicRemoveUnnecessaryByValCodeFixProvider.vb
@@ -0,0 +1,43 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports System.Collections.Immutable
+Imports System.Threading
+Imports Microsoft.CodeAnalysis.CodeActions
+Imports Microsoft.CodeAnalysis.CodeFixes
+Imports Microsoft.CodeAnalysis.Diagnostics
+Imports Microsoft.CodeAnalysis.Editing
+
+Namespace Microsoft.CodeAnalysis.VisualBasic.RemoveUnnecessaryByVal
+    <ExportCodeFixProvider(LanguageNames.VisualBasic)>
+    Friend Class VisualBasicRemoveUnnecessaryByValCodeFixProvider
+        Inherits CodeFixProvider
+
+        Public Overrides ReadOnly Property FixableDiagnosticIds As ImmutableArray(Of String) = ImmutableArray.Create(IDEDiagnosticIds.RemoveUnnecessaryByValDiagnosticId)
+
+        Public Overrides Async Function RegisterCodeFixesAsync(context As CodeFixContext) As Task
+            Dim root = Await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(False)
+            Dim node = root.FindNode(context.Span)
+
+            context.RegisterCodeFix(New MyCodeAction(
+                VisualBasicAnalyzersResources.Remove_ByVal,
+                Function(ct) RemoveByVal(context.Document, node, ct)),
+                context.Diagnostics)
+        End Function
+
+        Private Async Function RemoveByVal(document As Document, node As SyntaxNode, cancellationToken As CancellationToken) As Task(Of Document)
+            Dim editor = Await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(False)
+            editor.RemoveNode(node)
+            Return editor.GetChangedDocument()
+        End Function
+
+        Private Class MyCodeAction
+            Inherits CustomCodeActions.DocumentChangeAction
+
+            Friend Sub New(title As String, createChangedDocument As Func(Of CancellationToken, Task(Of Document)))
+                MyBase.New(title, createChangedDocument)
+            End Sub
+        End Class
+    End Class
+End Namespace
---
+++ src/Analyzers/VisualBasic/Tests/RemoveUnnecessaryByVal/RemoveUnnecessaryByValTests.vb
@@ -0,0 +1,52 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports Microsoft.CodeAnalysis.CodeFixes
+Imports Microsoft.CodeAnalysis.Diagnostics
+Imports Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.Diagnostics
+Imports Microsoft.CodeAnalysis.VisualBasic.OrderModifiers
+
+Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.RemoveUnnecessaryByVal
+
+    Public Class RemoveUnnecessaryByValTests
+        Inherits AbstractVisualBasicDiagnosticProviderBasedUserDiagnosticTest
+
+        Friend Overrides Function CreateDiagnosticProviderAndFixer(workspace As Workspace) As (DiagnosticAnalyzer, CodeFixProvider)
+            Return (New VisualBasicRemoveUnnecessaryByValDiagnosticAnalyzer(),
+                    New VisualBasicRemoveUnnecessaryByValCodeFixProvider())
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveByVal)>
+        Public Async Function TestRemoveByVal() As Task
+            TestInRegularAndScript1Async(
+"Public Class Program
+    Public Sub MySub([|ByVal|] arg As String)
+    End Sub
+End Class
+",
+"Public Class Program
+    Public Sub MySub(arg As String)
+    End Sub
+End Class
+")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveByVal)>
+        Public Async Function TestRemoveByValLowerCase() As Task
+            TestInRegularAndScript1Async(
+"Public Class Program
+    Public Sub MySub([|byval|] arg As String)
+    End Sub
+End Class
+",
+"Public Class Program
+    Public Sub MySub(arg As String)
+    End Sub
+End Class
+")
+        End Function
+
+    End Class
+
+End Namespace
