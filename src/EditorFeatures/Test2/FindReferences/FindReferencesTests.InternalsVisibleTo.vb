149f4ca3234e2427a345f98e447e2da352f22adf
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-06-08 15:03:23 -0700

    Add tests.

--- src/EditorFeatures/Test2/FindReferences/FindReferencesTests.InternalsVisibleTo.vb
+++ src/EditorFeatures/Test2/FindReferences/FindReferencesTests.InternalsVisibleTo.vb
@@ -101,7 +101,7 @@ Namespace Microsoft.CodeAnalysis.Editor.UnitTests.FindReferences
         End Function
 
         <WpfTheory, CombinatorialData, Trait(Traits.Feature, Traits.Features.FindReferences)>
-        Public Async Function TestInternalMethodSeenInDownstreamProjectWithIVT(kind As TestKind, host As TestHost) As Task
+        Public Async Function TestInternalMethodSeenInDownstreamProjectWithIVT_SimpleString(kind As TestKind, host As TestHost) As Task
             Dim input =
 <Workspace>
     <Project Language="C#" CommonReferences="true" Name="P1">
@@ -109,6 +109,172 @@ Namespace Microsoft.CodeAnalysis.Editor.UnitTests.FindReferences
         using System.Runtime.CompilerServices;
 
         [assembly: InternalsVisibleTo("P2")]
+
+        public class C
+        {
+            internal static void $${|Definition:Goo|}()
+            {
+            }
+        }
+        </Document>
+    </Project>
+    <Project Language="C#" CommonReferences="true" AssemblyName="P2">
+        <ProjectReference>P1</ProjectReference>
+        <Document>
+        class X
+        {
+            void Bar()
+            {
+                C.[|Goo|]();
+            }
+        }
+        </Document>
+    </Project>
+</Workspace>
+            Await TestAPIAndFeature(input, kind, host)
+        End Function
+
+        <WpfTheory, CombinatorialData, Trait(Traits.Feature, Traits.Features.FindReferences)>
+        Public Async Function TestInternalMethodSeenInDownstreamProjectWithIVT_WithAttributeSuffix(kind As TestKind, host As TestHost) As Task
+            Dim input =
+<Workspace>
+    <Project Language="C#" CommonReferences="true" Name="P1">
+        <Document>
+        using System.Runtime.CompilerServices;
+
+        [assembly: InternalsVisibleToAttribute("P2")]
+
+        public class C
+        {
+            internal static void $${|Definition:Goo|}()
+            {
+            }
+        }
+        </Document>
+    </Project>
+    <Project Language="C#" CommonReferences="true" AssemblyName="P2">
+        <ProjectReference>P1</ProjectReference>
+        <Document>
+        class X
+        {
+            void Bar()
+            {
+                C.[|Goo|]();
+            }
+        }
+        </Document>
+    </Project>
+</Workspace>
+            Await TestAPIAndFeature(input, kind, host)
+        End Function
+
+        <WpfTheory, CombinatorialData, Trait(Traits.Feature, Traits.Features.FindReferences)>
+        Public Async Function TestInternalMethodSeenInDownstreamProjectWithIVT_QualifiedAttributeName(kind As TestKind, host As TestHost) As Task
+            Dim input =
+<Workspace>
+    <Project Language="C#" CommonReferences="true" Name="P1">
+        <Document>
+        [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("P2")]
+
+        public class C
+        {
+            internal static void $${|Definition:Goo|}()
+            {
+            }
+        }
+        </Document>
+    </Project>
+    <Project Language="C#" CommonReferences="true" AssemblyName="P2">
+        <ProjectReference>P1</ProjectReference>
+        <Document>
+        class X
+        {
+            void Bar()
+            {
+                C.[|Goo|]();
+            }
+        }
+        </Document>
+    </Project>
+</Workspace>
+            Await TestAPIAndFeature(input, kind, host)
+        End Function
+
+        <WpfTheory, CombinatorialData, Trait(Traits.Feature, Traits.Features.FindReferences)>
+        Public Async Function TestInternalMethodSeenInDownstreamProjectWithIVT_VerbatimString(kind As TestKind, host As TestHost) As Task
+            Dim input =
+<Workspace>
+    <Project Language="C#" CommonReferences="true" Name="P1">
+        <Document>
+        using System.Runtime.CompilerServices;
+
+        [assembly: InternalsVisibleTo(@"P2")]
+
+        public class C
+        {
+            internal static void $${|Definition:Goo|}()
+            {
+            }
+        }
+        </Document>
+    </Project>
+    <Project Language="C#" CommonReferences="true" AssemblyName="P2">
+        <ProjectReference>P1</ProjectReference>
+        <Document>
+        class X
+        {
+            void Bar()
+            {
+                C.[|Goo|]();
+            }
+        }
+        </Document>
+    </Project>
+</Workspace>
+            Await TestAPIAndFeature(input, kind, host)
+        End Function
+
+        <WpfTheory, CombinatorialData, Trait(Traits.Feature, Traits.Features.FindReferences)>
+        Public Async Function TestInternalMethodSeenInDownstreamProjectWithIVT_UsingAlias(kind As TestKind, host As TestHost) As Task
+            Dim input =
+<Workspace>
+    <Project Language="C#" CommonReferences="true" Name="P1">
+        <Document>
+        using X = System.Runtime.CompilerServices.InternalsVisibleToAttribute;
+
+        [assembly: X(@"P2")]
+
+        public class C
+        {
+            internal static void $${|Definition:Goo|}()
+            {
+            }
+        }
+        </Document>
+    </Project>
+    <Project Language="C#" CommonReferences="true" AssemblyName="P2">
+        <ProjectReference>P1</ProjectReference>
+        <Document>
+        class X
+        {
+            void Bar()
+            {
+                C.[|Goo|]();
+            }
+        }
+        </Document>
+    </Project>
+</Workspace>
+            Await TestAPIAndFeature(input, kind, host)
+        End Function
+
+        <WpfTheory, CombinatorialData, Trait(Traits.Feature, Traits.Features.FindReferences)>
+        Public Async Function TestInternalMethodSeenInDownstreamProjectWithIVT_NonLiteralString(kind As TestKind, host As TestHost) As Task
+            Dim input =
+<Workspace>
+    <Project Language="C#" CommonReferences="true" Name="P1">
+        <Document>
+        [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("P" + "2")]
 
         public class C
         {
