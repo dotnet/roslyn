6f609837fd0bd7348028280e0fa7782811765542
Chris Sienkiewicz <chsienki@microsoft.com>
2020-06-10 15:32:01 -0700

    Add a simple abstract base class

---
+++ src/Compilers/Core/Portable/SourceGeneration/AbstractSourceGenerator.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis.SourceGeneration
+{
+    /// <summary>
+    /// A base class that can be used to implement a Source Generator 
+    /// </summary>
+    public abstract class AbstractSourceGenerator : ISourceGenerator
+    {
+        public abstract void Execute(SourceGeneratorContext context);
+
+        public virtual void Initialize(InitializationContext context)
+        {
+        }
+    }
+}
