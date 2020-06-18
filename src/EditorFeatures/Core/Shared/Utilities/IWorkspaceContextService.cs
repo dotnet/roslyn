15ee001a8aa7324f81672aaa5a52a43a83417459
David Barbet <dabarbet@microsoft.com>
2020-06-18 16:14:26 -0700

    Add workspace service for determining if in remote context

---
+++ src/EditorFeatures/Core/Shared/Utilities/IWorkspaceContextService.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+
+namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities
+{
+    internal interface IWorkspaceContextService : IWorkspaceService
+    {
+        /// <summary>
+        /// Used to determine if running as a client in a cloud connected environment.
+        /// </summary>
+        bool IsInRemoteClientContext();
+    }
+
+    [ExportWorkspaceService(typeof(IWorkspaceContextService), ServiceLayer.Editor), Shared]
+    internal sealed class DefaultWorkspaceContextService : IWorkspaceContextService
+    {
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public DefaultWorkspaceContextService()
+        {
+        }
+
+        public bool IsInRemoteClientContext()
+        {
+            return false;
+        }
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/VisualStudioWorkspaceContextService.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation
+{
+    [ExportWorkspaceService(typeof(IWorkspaceContextService), ServiceLayer.Host), Shared]
+    internal sealed class VisualStudioWorkspaceContextService : IWorkspaceContextService
+    {
+        /// <summary>
+        /// Guid for UI context set by liveshare upon joining as a client to a session.
+        /// </summary>
+        private static readonly Guid s_sessionJoinedUIContextGuid = Guid.Parse("c6f0e3cb-a3c3-49bd-bad2-7aad8690c15b");
+        private static readonly UIContext s_sessionJoinedUIContext = UIContext.FromUIContextGuid(s_sessionJoinedUIContextGuid);
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public VisualStudioWorkspaceContextService()
+        {
+        }
+
+        public bool IsInRemoteClientContext()
+        {
+            return s_sessionJoinedUIContext.IsActive;
+        }
+    }
+}
