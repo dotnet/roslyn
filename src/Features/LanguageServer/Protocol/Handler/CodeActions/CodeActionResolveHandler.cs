ac2d87d0be1b5d35bff1fd765d3f51e820ff3f3c
Allison Chou <allichou@microsoft.com>
2020-06-19 13:57:38 -0700

    Add new files

---
+++ src/Features/LanguageServer/Protocol/Handler/CodeActions/CodeActionResolveHandler.cs
@@ -0,0 +1,139 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.LanguageServer.CustomProtocol;
+using Microsoft.CodeAnalysis.LanguageServer.Handler.CodeActions;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Newtonsoft.Json.Linq;
+using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler
+{
+    /// <summary>
+    /// Handles the get code actions command.
+    /// </summary>
+    [ExportLspMethod(MSLSPMethods.TextDocumentCodeActionResolveName), Shared]
+    internal class CodeActionResolveHandler : AbstractRequestHandler<LSP.VSCodeAction, LSP.VSCodeAction>
+    {
+        private readonly ICodeFixService _codeFixService;
+        private readonly ICodeRefactoringService _codeRefactoringService;
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public CodeActionResolveHandler(
+            ICodeFixService codeFixService,
+            ICodeRefactoringService codeRefactoringService,
+            ILspSolutionProvider solutionProvider)
+            : base(solutionProvider)
+        {
+            _codeFixService = codeFixService;
+            _codeRefactoringService = codeRefactoringService;
+        }
+
+        public override async Task<LSP.VSCodeAction> HandleRequestAsync(
+            LSP.VSCodeAction codeAction,
+            LSP.ClientCapabilities clientCapabilities,
+            string? clientName,
+            CancellationToken cancellationToken)
+        {
+            CodeActionResolveData data;
+            if (codeAction.Data is CodeActionResolveData codeActionResolveData)
+            {
+                data = codeActionResolveData;
+            }
+            else
+            {
+                data = ((JToken)codeAction.Data).ToObject<CodeActionResolveData>();
+            }
+
+            var document = SolutionProvider.GetDocument(data.CodeActionParams.TextDocument, clientName);
+            var codeActions = await CodeActionsHandler.GetCodeActionsAsync(
+                document,
+                _codeFixService,
+                _codeRefactoringService,
+                data.CodeActionParams.Range,
+                cancellationToken).ConfigureAwait(false);
+
+            var codeActionToResolve = codeActions?.FirstOrDefault(a => a.Key.Title == codeAction.Title);
+            if (codeActionToResolve == null)
+            {
+                return codeAction;
+            }
+
+            // add check here
+            var operations = await codeActionToResolve.Value.Key.GetOperationsAsync(cancellationToken).ConfigureAwait(false);
+            if (operations.IsEmpty)
+            {
+                return codeAction;
+            }
+
+            var applyChangesOperations = operations.Where(operation => operation is ApplyChangesOperation);
+            if (applyChangesOperations.Any())
+            {
+                var workspaceEdit = new LSP.WorkspaceEdit { Changes = new Dictionary<string, LSP.TextEdit[]>() };
+                foreach (ApplyChangesOperation applyChangesOperation in applyChangesOperations)
+                {
+                    var solution = document!.Project.Solution;
+                    var changes = applyChangesOperation.ChangedSolution.GetChanges(solution);
+                    var changedDocuments = changes.GetProjectChanges().SelectMany(pc => pc.GetChangedDocuments());
+
+                    foreach (var docId in changedDocuments)
+                    {
+                        var newDoc = applyChangesOperation.ChangedSolution.GetDocument(docId);
+                        var oldDoc = solution.GetDocument(docId);
+                        if (oldDoc == null || newDoc == null)   // TODO: perhaps consider changing logic here
+                        {
+                            continue;
+                        }
+
+                        var oldText = await oldDoc.GetTextAsync(cancellationToken).ConfigureAwait(false);
+                        var newText = await newDoc.GetTextChangesAsync(oldDoc).ConfigureAwait(false);
+
+                        var edits = newText.Select(text => new LSP.TextEdit
+                        {
+                            NewText = text.NewText,
+                            Range = ProtocolConversions.TextSpanToRange(text.Span, oldText)
+                        });
+
+                        workspaceEdit.Changes.Add(newDoc.FilePath, edits.ToArray());
+                    }
+                }
+
+                codeAction.Edit = workspaceEdit;
+            }
+
+            var commandOperations = operations.Where(operation => !(operation is ApplyChangesOperation));
+            if (commandOperations.Any())
+            {
+                codeAction.Command = new LSP.Command
+                {
+                    CommandIdentifier = CodeActionsHandler.RunCodeActionCommandName,
+                    Title = codeAction.Title,
+                    Arguments = new object[]
+                    {
+                        new RunCodeActionParams
+                        {
+                            CodeActionParams = data.CodeActionParams,
+                            Title = codeAction.Title
+                        }
+                    }
+                };
+            }
+
+            return codeAction;
+        }
+    }
+}
