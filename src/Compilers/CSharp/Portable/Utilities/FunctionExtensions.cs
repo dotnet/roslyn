fabd343ee86689f2fbcc0e04d34912cf087c322a
AdamSpeight2008 <adam.speight@virgin.net>
2020-06-20 02:29:32 +0100

    C# Implementation.

--- src/Compilers/CSharp/Portable/Utilities/FunctionExtensions.cs
+++ src/Compilers/CSharp/Portable/Utilities/FunctionExtensions.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
@@ -12,9 +13,34 @@ namespace Microsoft.CodeAnalysis.CSharp
 {
     internal static partial class FunctionExtensions
     {
+#if (NETCOREAPP)
         public static HashSet<T> TransitiveClosure<T>(
             this Func<T, IEnumerable<T>> relation,
             T item)
+        {
+            var closure = PooledObjects.PooledHashSet<T>.GetInstance();
+            var stack = new Stack<T>();
+            stack.Push(item);
+            while (stack.Count > 0)
+            {
+                T current = stack.Pop();
+                foreach (var newItem in relation(current))
+                {
+                    if (closure.Add(newItem))
+                    {
+                        stack.Push(newItem);
+                    }
+                }
+            }
+            var result = closure.ToHashSet<T>();
+            closure.Free();
+            return result;
+        }
+
+#else
+        public static HashSet<T> TransitiveClosure<T>(
+     this Func<T, IEnumerable<T>> relation,
+     T item)
         {
             var closure = new HashSet<T>();
             var stack = new Stack<T>();
@@ -33,5 +59,6 @@ namespace Microsoft.CodeAnalysis.CSharp
 
             return closure;
         }
+#endif
     }
 }
