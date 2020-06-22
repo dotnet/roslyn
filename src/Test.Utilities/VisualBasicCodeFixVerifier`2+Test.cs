1e853c02d23c2c91cf0fba5fa3549774926cd565
Sam Harwell <Sam.Harwell@microsoft.com>
2020-06-22 07:14:52 -0700

    Use solution transforms

--- src/Test.Utilities/VisualBasicCodeFixVerifier`2+Test.cs
+++ src/Test.Utilities/VisualBasicCodeFixVerifier`2+Test.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Testing.Verifiers;
@@ -28,10 +29,11 @@ namespace Test.Utilities
 
                     if (AnalyzerConfigDocument != null)
                     {
-                        solution = project.AddAnalyzerConfigDocument(
+                        solution = solution.AddAnalyzerConfigDocument(
+                            DocumentId.CreateNewId(projectId, debugName: ".editorconfig"),
                             ".editorconfig",
                             SourceText.From($"is_global = true" + Environment.NewLine + AnalyzerConfigDocument),
-                            filePath: @"z:\.editorconfig").Project.Solution;
+                            filePath: @"z:\.editorconfig");
                     }
 
                     return solution;
