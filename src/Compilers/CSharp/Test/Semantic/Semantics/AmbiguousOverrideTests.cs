f724fd897fc39474d00eac26361f4cb6e13975b1
Neal Gafter <ngafter@microsoft.com>
2020-06-26 14:17:16 -0700

    Remove unneeded using added by overeager IDE

--- src/Compilers/CSharp/Test/Semantic/Semantics/AmbiguousOverrideTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/AmbiguousOverrideTests.cs
@@ -5,7 +5,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using ICSharpCode.Decompiler.IL;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Microsoft.CodeAnalysis.Test.Utilities;
