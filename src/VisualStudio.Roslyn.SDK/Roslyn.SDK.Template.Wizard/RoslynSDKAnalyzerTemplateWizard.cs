b0dfd241e45baa0808b45d6ec2fd89d9bb684b9e
Sam Harwell <Sam.Harwell@microsoft.com>
2020-06-26 12:09:02 -0700

    Remove unnecessary configuration of CopyLocal
    These assemblies do not need to be marked CopyLocal in an SDK-style VSIX
    project.

--- src/VisualStudio.Roslyn.SDK/Roslyn.SDK.Template.Wizard/RoslynSDKAnalyzerTemplateWizard.cs
+++ src/VisualStudio.Roslyn.SDK/Roslyn.SDK.Template.Wizard/RoslynSDKAnalyzerTemplateWizard.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using EnvDTE;
-using VSLangProj;
 
 public class RoslynSDKAnalyzerTemplateWizard : RoslynSDKChildTemplateWizard
 {
@@ -9,27 +8,6 @@ public class RoslynSDKAnalyzerTemplateWizard : RoslynSDKChildTemplateWizard
 
     public override void OnProjectFinishedGenerating(Project project)
     {
-        // We don't want to copy roslyn binaries to the output folder because they will be 
-        // included in the VSIX. The only way to solve this is to have th wizard mark the
-        // assemblies as copy local false.
         Project = project;
-#pragma warning disable VSTHRD010 // Invoke single-threaded types on Main thread
-        if (project.Object is VSProject vsProject)
-#pragma warning restore VSTHRD010 // Invoke single-threaded types on Main thread
-        {
-            if (vsProject.References != null)
-            {
-                foreach (Reference reference in vsProject.References)
-                {
-                    if (reference.Name.Contains("Microsoft.CodeAnalysis") ||
-                        reference.Name.Contains("System.Collections.Immutable") ||
-                        reference.Name.Contains("System.Composition") ||
-                        reference.Name.Contains("System.Reflection.Metadata"))
-                    {
-                        reference.CopyLocal = false;
-                    }
-                }
-            }
-        }
     }
 }
