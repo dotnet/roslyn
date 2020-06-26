c323a77c83caf1c6ec3b9df56ee4e36ced7d9330
AlekseyTs <alekseyt@microsoft.com>
2020-06-26 13:34:39 -0700

    Extract some base types for future implementation sharing

--- src/Compilers/CSharp/Portable/Symbols/Synthesized/Records/SynthesizedRecordObjEquals.cs
+++ src/Compilers/CSharp/Portable/Symbols/Synthesized/Records/SynthesizedRecordObjEquals.cs
@@ -6,25 +6,18 @@
 
 using System.Collections.Immutable;
 using System.Diagnostics;
-using System.Globalization;
-using System.Threading;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp.Symbols
 {
-    internal sealed class SynthesizedRecordObjEquals : SourceOrdinaryMethodSymbolBase
+    internal sealed class SynthesizedRecordObjEquals : SynthesizedRecordObjectMethod
     {
         private readonly MethodSymbol _typedRecordEquals;
-        private readonly int _memberOffset;
 
         public SynthesizedRecordObjEquals(SourceMemberContainerTypeSymbol containingType, MethodSymbol typedRecordEquals, int memberOffset, DiagnosticBag diagnostics)
-            : base(containingType, "Equals", containingType.Locations[0], (CSharpSyntaxNode)containingType.SyntaxReferences[0].GetSyntax(), MethodKind.Ordinary,
-                   isIterator: false, isExtensionMethod: false, isPartial: false, hasBody: true, diagnostics)
+            : base(containingType, "Equals", memberOffset, diagnostics)
         {
-            var compilation = containingType.DeclaringCompilation;
             _typedRecordEquals = typedRecordEquals;
-            _memberOffset = memberOffset;
         }
 
         protected override DeclarationModifiers MakeDeclarationModifiers(DeclarationModifiers allowedModifiers, DiagnosticBag diagnostics)
@@ -33,14 +26,6 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             Debug.Assert((result & ~allowedModifiers) == 0);
             return result;
         }
-
-        protected override bool HasAnyBody => true;
-
-        internal override bool IsExpressionBodied => false;
-
-        public override bool IsImplicitlyDeclared => true;
-
-        protected override Location ReturnTypeLocation => Locations[0];
 
         protected override (TypeWithAnnotations ReturnType, ImmutableArray<ParameterSymbol> Parameters, bool IsVararg, ImmutableArray<TypeParameterConstraintClause> DeclaredConstraintsForOverrideOrImplement) MakeParametersAndBindReturnType(DiagnosticBag diagnostics)
         {
@@ -56,68 +41,6 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
         }
 
         protected override int GetParameterCountFromSyntax() => 1;
-
-        protected override MethodSymbol? FindExplicitlyImplementedMethod(DiagnosticBag diagnostics) => null;
-
-        protected override void MethodChecks(DiagnosticBag diagnostics)
-        {
-            base.MethodChecks(diagnostics);
-
-            var overridden = OverriddenMethod?.OriginalDefinition;
-
-            if (overridden is null || (overridden is SynthesizedRecordObjEquals && overridden.DeclaringCompilation == DeclaringCompilation))
-            {
-                return;
-            }
-
-            MethodSymbol leastOverridden = GetLeastOverriddenMethod(accessingTypeOpt: null);
-
-            if (leastOverridden is object &&
-                leastOverridden.ReturnType.SpecialType == SpecialType.System_Boolean &&
-                leastOverridden.ContainingType.SpecialType != SpecialType.System_Object)
-            {
-                diagnostics.Add(ErrorCode.ERR_DoesNotOverrideMethodFromObject, Locations[0], this);
-            }
-        }
-
-        protected override ImmutableArray<TypeParameterSymbol> MakeTypeParameters(CSharpSyntaxNode node, DiagnosticBag diagnostics) => ImmutableArray<TypeParameterSymbol>.Empty;
-
-        public override ImmutableArray<TypeParameterConstraintClause> GetTypeParameterConstraintClauses() => ImmutableArray<TypeParameterConstraintClause>.Empty;
-
-
-        protected override void PartialMethodChecks(DiagnosticBag diagnostics)
-        {
-        }
-
-        protected override void ExtensionMethodChecks(DiagnosticBag diagnostics)
-        {
-        }
-
-        protected override void CompleteAsyncMethodChecksBetweenStartAndFinish()
-        {
-        }
-
-        protected override TypeSymbol? ExplicitInterfaceType => null;
-
-        protected override void CheckConstraintsForExplicitInterfaceType(ConversionsBase conversions, DiagnosticBag diagnostics)
-        {
-        }
-
-        protected override SourceMemberMethodSymbol? BoundAttributesSource => null;
-
-        internal override OneOrMany<SyntaxList<AttributeListSyntax>> GetAttributeDeclarations() => OneOrMany.Create(default(SyntaxList<AttributeListSyntax>));
-
-        public override string? GetDocumentationCommentXml(CultureInfo? preferredCulture = null, bool expandIncludes = false, CancellationToken cancellationToken = default) => null;
-
-        public override bool IsVararg => false;
-
-        public override RefKind RefKind => RefKind.None;
-
-        internal override LexicalSortKey GetLexicalSortKey() => LexicalSortKey.GetSynthesizedMemberKey(_memberOffset);
-
-        internal override bool GenerateDebugInfo => false;
-
-        internal override bool SynthesizesLoweredBoundBody => true;
 
         internal override void GenerateMethodBody(TypeCompilationState compilationState, DiagnosticBag diagnostics)
         {
