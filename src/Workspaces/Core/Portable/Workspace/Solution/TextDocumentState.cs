a0fdc69a2e316d08dc5de67240b7355dc8a49adb
Sam Harwell <Sam.Harwell@microsoft.com>
2020-06-26 12:53:30 -0700

    Expand comment in CreateRecoverableText

--- src/Workspaces/Core/Portable/Workspace/Solution/TextDocumentState.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/TextDocumentState.cs
@@ -100,7 +100,12 @@ namespace Microsoft.CodeAnalysis
         {
             var result = new RecoverableTextAndVersion(CreateStrongText(text), services.TemporaryStorage);
 
-            // Ensure the recoverable text is saved in temporary storage
+            // This RecoverableTextAndVersion is created directly from a TextAndVersion instance. In its initial state,
+            // the RecoverableTextAndVersion keeps a strong reference to the initial TextAndVersion, and only
+            // transitions to a weak reference backed by temporary storage after the first time GetValue (or
+            // GetValueAsync) is called. Since we know we are creating a RecoverableTextAndVersion for the purpose of
+            // avoiding problematic address space overhead, we call GetValue immediately to force the object to weakly
+            // hold its data from the start.
             result.GetValue();
 
             return result;
