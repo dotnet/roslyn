712609208e57ffd80814b831f00babc2d6de8bdf
Sam Harwell <Sam.Harwell@microsoft.com>
2020-06-27 15:30:25 -0700

    Allow independent methods to have the same parameter name
    Fixes #45492

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Services/SyntaxFacts/CSharpSyntaxFacts.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Services/SyntaxFacts/CSharpSyntaxFacts.cs
@@ -182,6 +182,9 @@ namespace Microsoft.CodeAnalysis.CSharp.LanguageServices
         public SyntaxNode GetParameterList(SyntaxNode node)
             => node.GetParameterList();
 
+        public bool IsParameterList(SyntaxNode node)
+            => node.IsKind(SyntaxKind.ParameterList, SyntaxKind.BracketedParameterList);
+
         public SyntaxToken GetIdentifierOfGenericName(SyntaxNode genericName)
         {
             return genericName is GenericNameSyntax csharpGenericName
