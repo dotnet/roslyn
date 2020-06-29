5d7dc2f175650c1a30d4941ec4eae579fa081bd7
Jason Malinowski <jason.malinowski@microsoft.com>
2020-06-29 13:40:24 -0700

    Revert "Move SymbolSearch down to EditorFeatures (#45505)"
    This reverts commit f755e8a8ed1d5200153990a099e6ad4c461eb592. That
    change meant our EditorFeatures code was using DllImports in the
    cross-platform binary, and we weren't sure what that would do in
    VS for Mac if that code ran. It also brought a dependency on
    Microsoft.VisualStudio.RemoteControl, which also seems to depend on
    various Windows specific APIs.
    
    We'll finish up the decision of what to do in
    https://github.com/dotnet/roslyn/pull/45115

--- src/EditorFeatures/Core.Wpf/SymbolSearch/IAddReferenceDatabaseWrapper.cs
+++ src/EditorFeatures/Core.Wpf/SymbolSearch/IAddReferenceDatabaseWrapper.cs
@@ -2,6 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Elfie.Model;
 
 namespace Microsoft.CodeAnalysis.SymbolSearch
