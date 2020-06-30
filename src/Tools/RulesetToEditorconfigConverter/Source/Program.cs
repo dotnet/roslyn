6561a51952a18c14a6baf57af250ca8384539530
Sam Harwell <Sam.Harwell@microsoft.com>
2020-06-29 17:40:13 -0700

    Convert RulesetToEditorConfigConverter to use top-level statements

--- src/Tools/RulesetToEditorconfigConverter/Source/Program.cs
+++ src/Tools/RulesetToEditorconfigConverter/Source/Program.cs
@@ -1,42 +1,37 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+#pragma warning disable IDE0051 // Remove unused private members
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes
+
 using System;
 using System.IO;
+using Microsoft.CodeAnalysis.RulesetToEditorconfig;
 
-namespace Microsoft.CodeAnalysis.RulesetToEditorconfig
+if (args.Length < 1 || args.Length > 2)
 {
-    internal static class Program
-    {
-        public static int Main(string[] args)
-        {
-            if (args.Length < 1 || args.Length > 2)
-            {
-                ShowUsage();
-                return 1;
-            }
+    ShowUsage();
+    return 1;
+}
 
-            var rulesetFilePath = args[0];
-            var editorconfigFilePath = args.Length == 2 ?
-                args[1] :
-                Path.Combine(Environment.CurrentDirectory, ".editorconfig");
-            try
-            {
-                Converter.GenerateEditorconfig(rulesetFilePath, editorconfigFilePath);
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return 2;
-            }
+var rulesetFilePath = args[0];
+var editorconfigFilePath = args.Length == 2 ?
+    args[1] :
+    Path.Combine(Environment.CurrentDirectory, ".editorconfig");
+try
+{
+    Converter.GenerateEditorconfig(rulesetFilePath, editorconfigFilePath);
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+    return 2;
+}
 
-            Console.WriteLine($"Successfully converted to '{editorconfigFilePath}'");
-            return 0;
+Console.WriteLine($"Successfully converted to '{editorconfigFilePath}'");
+return 0;
 
-            static void ShowUsage()
-            {
-                Console.WriteLine("Usage: RulesetToEditorconfigConverter.exe <%ruleset_file%> [<%path_to_editorconfig%>]");
-                return;
-            }
-        }
-    }
+static void ShowUsage()
+{
+    Console.WriteLine("Usage: RulesetToEditorconfigConverter.exe <%ruleset_file%> [<%path_to_editorconfig%>]");
+    return;
 }
