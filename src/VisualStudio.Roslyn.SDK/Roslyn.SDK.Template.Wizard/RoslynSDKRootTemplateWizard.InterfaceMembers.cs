7e6cf2ff09ddc733e2ac9f358a803636e506b11f
Sam Harwell <Sam.Harwell@microsoft.com>
2020-06-29 22:17:03 -0700

    Create a NuGet.Config file as part of solution
    Closes #512

--- src/VisualStudio.Roslyn.SDK/Roslyn.SDK.Template.Wizard/RoslynSDKRootTemplateWizard.InterfaceMembers.cs
+++ src/VisualStudio.Roslyn.SDK/Roslyn.SDK.Template.Wizard/RoslynSDKRootTemplateWizard.InterfaceMembers.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Xml.Linq;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TemplateWizard;
@@ -20,25 +16,6 @@ public partial class RoslynSDKRootTemplateWizard : IWizard
     {
         ThreadHelper.ThrowIfNotOnUIThread();
 
-        WriteOutBetaNugetSource("dotnet.myget.org roslyn", "https://dotnet.myget.org/F/roslyn/api/v3/index.json");
-        WriteOutBetaNugetSource("dotnet.myget.org roslyn-analyzers", "https://dotnet.myget.org/F/roslyn-analyzers/api/v3/index.json");
         OnRunStarted(automationObject as DTE, replacementsDictionary, runKind, customParams);
-    }
-
-    private void WriteOutBetaNugetSource(string key, string value)
-    {
-        var appDataFolder = Environment.GetEnvironmentVariable("APPDATA");
-        var nugetConfigPath = Path.Combine(appDataFolder, @"NuGet\NuGet.Config");
-        var document = XDocument.Load(nugetConfigPath);
-        var packageSources = document.Root.Descendants().Where(x => x.Name.LocalName == "packageSources");
-        var sources = packageSources.Elements(XName.Get("add"));
-        if (!sources.Where(x => x.Attribute(XName.Get("value")).Value == value).Any())
-        {
-            var newSource = new XElement(XName.Get("add"));
-            newSource.SetAttributeValue(XName.Get("key"), key);
-            newSource.SetAttributeValue(XName.Get("value"), value);
-            sources.Last().AddAfterSelf(newSource);
-            document.Save(nugetConfigPath);
-        }
     }
 }
