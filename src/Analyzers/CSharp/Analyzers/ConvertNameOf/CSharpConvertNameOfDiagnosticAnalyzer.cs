08c753386e0ec85e33fd8f54d3e697427fc2fd6c
Madalyn Redding <t-mare@microsoft.com>
2020-07-01 16:12:45 -0400

    Basic analyzer in place -- cleaning up code

--- src/Analyzers/CSharp/Analyzers/ConvertNameOf/CSharpConvertNameOfDiagnosticAnalyzer.cs
+++ src/Analyzers/CSharp/Analyzers/ConvertNameOf/CSharpConvertNameOfDiagnosticAnalyzer.cs
@@ -45,8 +45,6 @@ namespace Microsoft.CodeAnalysis.CSharp.ConvertNameOf
             //var cancellationToken = syntaxContext.CancellationToken;
             var node = typeofOp.Operation.Syntax;
 
-            // Verify it's a typeof expression
-
             // nameof was added in CSharp 6.0, so don't offer it for any languages before that time
             if (((CSharpParseOptions)syntaxTree.Options).LanguageVersion < LanguageVersion.CSharp6)
             {
@@ -55,26 +53,18 @@ namespace Microsoft.CodeAnalysis.CSharp.ConvertNameOf
 
             // TODO: Check for compiler errors on the declaration
 
-            var parent = node.Parent;
-
             // We know that it is a typeof() instance, but we only want to offer the fix if it is a .Name access
+            var parent = node.Parent;
             if (!(node.Parent.IsKind(SyntaxKind.SimpleMemberAccessExpression) && parent.IsNameMemberAccess()))
             {
                 return;
             }
 
-            // Analyze the argument to determine if argument is generic
-            if (ArgumentIsGeneric(typeofOp.Operation))
+            // Analyze the argument to determine if argument is generic or a primitive 
+            if (ArgumentIsGeneric(typeofOp.Operation) || ArgumentIsPrimitive(typeofOp.Operation))
             {
                 return;
             }
-
-            // Analyze the argument to determine if argument is primitive
-            if (ArgumentIsPrimitive(typeofOp.Operation))
-            {
-                return;
-            }
-
 
             // Current case can be effectively changed to a nameof instance so report a diagnostic
             var location = Location.Create(syntaxTree, parent.Span);
@@ -93,8 +83,6 @@ namespace Microsoft.CodeAnalysis.CSharp.ConvertNameOf
 
         private static bool ArgumentIsGeneric(IOperation op)
         {
-            // TODO: verify it is a typeof operation
-
             // Cast it to a ITypeOfOperation
             var tOp = (ITypeOfOperation)op;
 
@@ -107,11 +95,7 @@ namespace Microsoft.CodeAnalysis.CSharp.ConvertNameOf
 
         private static bool ArgumentIsPrimitive(IOperation op)
         {
-            // TODO: verify it is a typeof operation
-            //kind is named type for string
-
-            // Cast it to a ITypeOfOperation
-            var tOp = (ITypeOfOperation)op;
+            // Get the argument's syntax node
             var child = op.Syntax.ChildNodes().ElementAt(0);
 
             if (child.IsKind(SyntaxKind.PredefinedType))
