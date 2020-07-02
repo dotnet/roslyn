91d4f1d1741f7fee88dcb9508de5145a002db569
David Barbet <dabarbet@microsoft.com>
2020-07-01 18:28:41 -0700

    Fix check to only apply to remote workspace

--- src/EditorFeatures/Core/Shared/Utilities/IWorkspaceContextService.cs
+++ src/EditorFeatures/Core/Shared/Utilities/IWorkspaceContextService.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using System;
 using System.Composition;
 using Microsoft.CodeAnalysis.Host;
@@ -17,18 +19,24 @@ namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities
         bool IsInRemoteClientContext();
     }
 
-    [ExportWorkspaceService(typeof(IWorkspaceContextService), ServiceLayer.Editor), Shared]
     internal sealed class DefaultWorkspaceContextService : IWorkspaceContextService
     {
+        public bool IsInRemoteClientContext() => false;
+    }
+
+    [ExportWorkspaceServiceFactory(typeof(IWorkspaceContextService), ServiceLayer.Editor), Shared]
+    internal sealed class DefaultWorkspaceContextServiceFactory : IWorkspaceServiceFactory
+    {
+
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-        public DefaultWorkspaceContextService()
+        public DefaultWorkspaceContextServiceFactory()
         {
         }
 
-        public bool IsInRemoteClientContext()
+        public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
         {
-            return false;
+            return new DefaultWorkspaceContextService();
         }
     }
 }
--- src/VisualStudio/Core/Def/Implementation/VisualStudioWorkspaceContextService.cs
+++ src/VisualStudio/Core/Def/Implementation/VisualStudioWorkspaceContextService.cs
@@ -2,32 +2,51 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using System;
 using System.Composition;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
+using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.VisualStudio.Shell;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation
 {
-    [ExportWorkspaceService(typeof(IWorkspaceContextService), ServiceLayer.Host), Shared]
     internal sealed class VisualStudioWorkspaceContextService : IWorkspaceContextService
     {
         /// <summary>
         /// Guid for UI context set by liveshare upon joining as a client to a session.
         /// </summary>
         private static readonly Guid s_sessionJoinedUIContextGuid = Guid.Parse("c6f0e3cb-a3c3-49bd-bad2-7aad8690c15b");
-        private static readonly UIContext s_sessionJoinedUIContext = UIContext.FromUIContextGuid(s_sessionJoinedUIContextGuid);
+        private readonly UIContext _sessionJoinedUIContext;
 
+        public VisualStudioWorkspaceContextService()
+        {
+            _sessionJoinedUIContext = UIContext.FromUIContextGuid(s_sessionJoinedUIContextGuid);
+        }
+
+        public bool IsInRemoteClientContext() => _sessionJoinedUIContext.IsActive;
+    }
+
+    [ExportWorkspaceServiceFactory(typeof(IWorkspaceContextService), ServiceLayer.Host), Shared]
+    internal sealed class VisualStudioWorkspaceContextServiceFactory : IWorkspaceServiceFactory
+    {
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-        public VisualStudioWorkspaceContextService()
+        public VisualStudioWorkspaceContextServiceFactory()
         {
         }
 
-        public bool IsInRemoteClientContext()
+        public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
         {
-            return s_sessionJoinedUIContext.IsActive;
+            if (workspaceServices.Workspace.Kind == WorkspaceKind.AnyCodeRoslynWorkspace)
+            {
+                return new VisualStudioWorkspaceContextService();
+            }
+
+            return new DefaultWorkspaceContextService();
         }
     }
 }
