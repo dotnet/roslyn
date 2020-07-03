5c503983843928e305ecf4a3413fb9a46bdd8846
David Wengier <git@wengier.com>
2020-07-03 11:53:20 +1000

    Work around SyntaxGenerator issue

--- src/Features/CSharp/Portable/RemoveAsyncModifier/ConvertReturnsToTaskRewriter.cs
+++ src/Features/CSharp/Portable/RemoveAsyncModifier/ConvertReturnsToTaskRewriter.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
@@ -25,23 +26,51 @@ namespace Microsoft.CodeAnalysis.CSharp.RemoveAsyncModifier
             {
                 if (node.Expression is null)
                 {
-                    return node.WithExpression((ExpressionSyntax)_generator.MemberAccessExpression(_generator.TypeExpression(_taskType), nameof(Task.CompletedTask)));
+                    // return; ==> return Task.CompletedTask;
+                    return node.WithExpression((ExpressionSyntax)_generator.MemberAccessExpression(TypeExpressionForStaticMemberAccess(_generator, _taskType), nameof(Task.CompletedTask)));
                 }
+                // return <expr>; ==> return Task.FromResult(<expr>);
                 return node.WithExpression(WrapWithTaskFromResult(node.Expression));
             }
 
             public override SyntaxNode VisitArrowExpressionClause(ArrowExpressionClauseSyntax node)
             {
+                // For expression bodied methods and local functions just wrap the whole thing in Task.FromResult
                 return node.WithExpression(WrapWithTaskFromResult(node.Expression));
             }
 
             public ExpressionSyntax WrapWithTaskFromResult(ExpressionSyntax expression)
             {
+                // <expr>; ==> Task.FromResult(<expr>);
                 return (ExpressionSyntax)
                     _generator.InvocationExpression(
                         _generator.MemberAccessExpression(
-                            _generator.TypeExpression(_taskType), nameof(Task.FromResult)),
+                            TypeExpressionForStaticMemberAccess(_generator, _taskType), nameof(Task.FromResult)),
                             expression);
+            }
+
+            // Workaround for https://github.com/dotnet/roslyn/issues/43950
+            // Copied from https://github.com/dotnet/roslyn-analyzers/blob/f24a5b42c85be6ee572f3a93bef223767fbefd75/src/Utilities/Workspaces/SyntaxGeneratorExtensions.cs#L68-L74
+            private static SyntaxNode TypeExpressionForStaticMemberAccess(SyntaxGenerator generator, INamedTypeSymbol typeSymbol)
+            {
+                var qualifiedNameSyntaxKind = generator.QualifiedName(generator.IdentifierName("ignored"), generator.IdentifierName("ignored")).RawKind;
+                var memberAccessExpressionSyntaxKind = generator.MemberAccessExpression(generator.IdentifierName("ignored"), "ignored").RawKind;
+
+                var typeExpression = generator.TypeExpression(typeSymbol);
+                return QualifiedNameToMemberAccess(qualifiedNameSyntaxKind, memberAccessExpressionSyntaxKind, typeExpression, generator);
+
+                // Local function
+                static SyntaxNode QualifiedNameToMemberAccess(int qualifiedNameSyntaxKind, int memberAccessExpressionSyntaxKind, SyntaxNode expression, SyntaxGenerator generator)
+                {
+                    if (expression.RawKind == qualifiedNameSyntaxKind)
+                    {
+                        var left = QualifiedNameToMemberAccess(qualifiedNameSyntaxKind, memberAccessExpressionSyntaxKind, expression.ChildNodes().First(), generator);
+                        var right = expression.ChildNodes().Last();
+                        return generator.MemberAccessExpression(left, right);
+                    }
+
+                    return expression;
+                }
             }
         }
     }
