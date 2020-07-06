fbea8aa06e7eded330ae18ae9a8c651c0a0d6955
xdtao <danieltaox@gmail.com>
2020-07-06 14:01:18 -0500

    stub ConvertNameOfTests

---
+++ src/Analyzers/CSharp/Tests/ConvertNameOf/ConvertNameOfTests.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.ConvertNameOf;
+using Microsoft.CodeAnalysis.CSharp.CSharpConvertNameOfCodeFixProvider;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.ConvertNameOf
+{
+    public partial class ConvertNameOfTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
+    {
+        internal override (DiagnosticAnalyzer, CodeFixProvider) CreateDiagnosticProviderAndFixer(Workspace workspace)
+            => (new CSharpConvertNameOfDiagnosticAnalyzer(), new CSharpConvertNameOfCodeFixProvider());
+
+       
+
+    }
+}
