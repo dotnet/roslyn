4e7ca321a3277d4f7001c3df7cb8f1c390690b76
David Wengier <dawengie@microsoft.com>
2020-07-11 16:34:03 +1000

    Add Visual Basic implementation and test cases

---
+++ src/EditorFeatures/VisualBasicTest/Diagnostics/RemoveAsyncModifier/RemoveAsyncModifierTests.vb
@@ -0,0 +1,171 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports Microsoft.CodeAnalysis.Testing
+Imports VerifyVB = Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions.VisualBasicCodeFixVerifier(
+    Of Microsoft.CodeAnalysis.Testing.EmptyDiagnosticAnalyzer,
+    Microsoft.CodeAnalysis.VisualBasic.RemoveAsyncModifier.VisualBasicRemoveAsyncModifierCodeFixProvider)
+
+Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.Diagnostics.RemoteAsyncModifier
+    Public Class RemoveAsyncModifierTests
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveAsyncModifier)>
+        Public Async Function Function_Task() As Task
+            Await VerifyVB.VerifyCodeFixAsync(
+"Imports System.Threading.Tasks
+
+Class C
+    Async Function {|BC42356:Goo|}() As Task
+        System.Console.WriteLine(1)
+    End Function
+End Class",
+"Imports System.Threading.Tasks
+
+Class C
+    Function {|BC42356:Goo|}() As Task
+        System.Console.WriteLine(1)
+        Return Task.CompletedTask
+    End Function
+End Class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveAsyncModifier)>
+        Public Async Function Function_TaskOfT() As Task
+            Await VerifyVB.VerifyCodeFixAsync(
+"Imports System.Threading.Tasks
+
+Class C
+    Async Function {|BC42356:Goo|}() As Task(of Integer)
+        Return 1
+    End Function
+End Class",
+"Imports System.Threading.Tasks
+
+Class C
+    Function {|BC42356:Goo|}() As Task(of Integer)
+        Return Task.FromResult(1)
+    End Function
+End Class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveAsyncModifier)>
+        Public Async Function SingleLineLambda_Task() As Task
+            Await VerifyVB.VerifyCodeFixAsync(
+"Imports System
+Imports System.Threading.Tasks
+
+Class C
+    Sub Goo()
+        dim f as Func(of Task) =
+            Async {|BC42356:Function|}() 1
+    End Sub
+End Class",
+"Imports System
+Imports System.Threading.Tasks
+
+Class C
+    Sub Goo()
+        dim f as Func(of Task) =
+            Function() Task.FromResult(1)
+    End Sub
+End Class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveAsyncModifier)>
+        Public Async Function SingleLineLambda_TaskOfT() As Task
+            Await VerifyVB.VerifyCodeFixAsync(
+"Imports System
+Imports System.Threading.Tasks
+
+Class C
+    Sub Goo()
+        dim f as Func(of Task(Of Integer)) =
+            Async {|BC42356:Function|}() 1
+    End Sub
+End Class",
+"Imports System
+Imports System.Threading.Tasks
+
+Class C
+    Sub Goo()
+        dim f as Func(of Task(Of Integer)) =
+            Function() Task.FromResult(1)
+    End Sub
+End Class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveAsyncModifier)>
+        Public Async Function MultiLineLambda_Task() As Task
+            Await VerifyVB.VerifyCodeFixAsync(
+"Imports System
+Imports System.Threading.Tasks
+
+Class C
+    Sub Goo()
+        dim f as Func(of Task) =
+            Async {|BC42356:Function|}()
+                Console.WriteLine(1)
+            End Function
+    End Sub
+End Class",
+"Imports System
+Imports System.Threading.Tasks
+
+Class C
+    Sub Goo()
+        dim f as Func(of Task) =
+            Function()
+                Console.WriteLine(1)
+                Return Task.CompletedTask
+            End Function
+    End Sub
+End Class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveAsyncModifier)>
+        Public Async Function MultiLineLambda_TaskOfT() As Task
+            Await VerifyVB.VerifyCodeFixAsync(
+"Imports System
+Imports System.Threading.Tasks
+
+Class C
+    Sub Goo()
+        dim f as Func(of Task(Of Integer)) =
+            Async {|BC42356:Function|}()
+                Return 1
+            End Function
+    End Sub
+End Class",
+"Imports System
+Imports System.Threading.Tasks
+
+Class C
+    Sub Goo()
+        dim f as Func(of Task(Of Integer)) =
+            Function()
+                Return Task.FromResult(1)
+            End Function
+    End Sub
+End Class")
+        End Function
+
+        <Fact, Trait(Traits.Feature, Traits.Features.CodeActionsRemoveAsyncModifier)>
+        Public Async Function Sub_Task_Missing() As Task
+            Dim source = "
+Imports System
+
+Class C
+    Async Sub Goo()
+        System.Console.WriteLine(1)
+    End Sub
+End Class"
+
+            Dim test = New VerifyVB.Test()
+            test.TestState.Sources.Add(source)
+            test.FixedState.Sources.Add(source)
+            test.FixedState.ExpectedDiagnostics.Add(DiagnosticResult.CompilerWarning("BC42356").WithLocation(0).WithArguments("Integer", "System.Threading.Tasks.Task"))
+            Await test.RunAsync()
+        End Function
+    End Class
+End Namespace
---
+++ src/Features/VisualBasic/Portable/RemoveAsyncModifier/VisualBasicRemoveAsyncModifierCodeFixProvider.vb
@@ -0,0 +1,108 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports System.Collections.Immutable
+Imports System.Composition
+Imports System.Diagnostics.CodeAnalysis
+Imports Microsoft.CodeAnalysis.CodeFixes
+Imports Microsoft.CodeAnalysis.Editing
+Imports Microsoft.CodeAnalysis.MakeMethodAsynchronous.AbstractMakeMethodAsynchronousCodeFixProvider
+Imports Microsoft.CodeAnalysis.RemoveAsyncModifier
+Imports Microsoft.CodeAnalysis.VisualBasic.MakeMethodSynchronous
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+
+Namespace Microsoft.CodeAnalysis.VisualBasic.RemoveAsyncModifier
+    <ExportCodeFixProvider(LanguageNames.VisualBasic, Name:=PredefinedCodeFixProviderNames.RemoveAsyncModifier), [Shared]>
+    <ExtensionOrder(After:=PredefinedCodeFixProviderNames.MakeMethodSynchronous)>
+    Friend Class VisualBasicRemoveAsyncModifierCodeFixProvider
+        Inherits AbstractRemoveAsyncModifierCodeFixProvider(Of ReturnStatementSyntax, ExpressionSyntax)
+
+        Private Const BC42356 As String = NameOf(BC42356) ' This async method lacks 'Await' operators and so will run synchronously.
+
+        Private Shared ReadOnly s_diagnosticIds As ImmutableArray(Of String) = ImmutableArray.Create(BC42356)
+
+        <ImportingConstructor>
+        <SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification:="Used in test code: https://github.com/dotnet/roslyn/issues/42814")>
+        Public Sub New()
+        End Sub
+
+        Public Overrides ReadOnly Property FixableDiagnosticIds As ImmutableArray(Of String)
+            Get
+                Return s_diagnosticIds
+            End Get
+        End Property
+
+        Protected Overrides Function IsAsyncSupportingFunctionSyntax(node As SyntaxNode) As Boolean
+            Return node.IsAsyncSupportedFunctionSyntax()
+        End Function
+
+        Protected Overrides Function ShouldOfferFix(declaredSymbol As ISymbol, knownTypes As KnownTypes) As Boolean
+            ' We only apply to functions because async subs will be offered the Make Method Synchronous fixer which would do the same job
+            Dim methodSymbol = TryCast(declaredSymbol, IMethodSymbol)
+            If methodSymbol IsNot Nothing Then
+                Return Not methodSymbol.ReturnsVoid
+            End If
+
+            Return True
+        End Function
+
+        Protected Overrides Function RemoveAsyncModifier(methodSymbolOpt As IMethodSymbol, node As SyntaxNode, knownTypes As KnownTypes) As SyntaxNode
+            Dim methodBlock = TryCast(node, MethodBlockSyntax)
+            If methodBlock IsNot Nothing Then
+                Dim subOrFunctionStatement = methodBlock.SubOrFunctionStatement
+                Dim newSubOrFunctionStatement = RemoveAsyncModifierHelpers.RemoveAsyncKeyword(subOrFunctionStatement)
+                Return methodBlock.WithSubOrFunctionStatement(newSubOrFunctionStatement)
+            End If
+
+            Dim multiLineLambda = TryCast(node, MultiLineLambdaExpressionSyntax)
+            If multiLineLambda IsNot Nothing Then
+                Return RemoveAsyncModifierHelpers.FixMultiLineLambdaExpression(multiLineLambda)
+            End If
+
+            Dim singleLineLambda = TryCast(node, SingleLineLambdaExpressionSyntax)
+            If singleLineLambda IsNot Nothing Then
+                Return RemoveAsyncModifierHelpers.FixSingleLineLambdaExpression(singleLineLambda)
+            End If
+
+            Return Nothing
+        End Function
+
+        Protected Overrides Function AnalyzeControlFlow(semanticModel As SemanticModel, node As SyntaxNode) As ControlFlowAnalysis
+            Dim multiLineLambda = TryCast(node, MultiLineLambdaExpressionSyntax)
+            If multiLineLambda IsNot Nothing Then
+                Return semanticModel.AnalyzeControlFlow(multiLineLambda.Statements.First(), multiLineLambda.Statements.Last())
+            End If
+
+            Return semanticModel.AnalyzeControlFlow(node)
+        End Function
+
+        Protected Overrides Function GetLastChildOfBlock(node As SyntaxNode) As SyntaxNode
+            Dim methodBlock = TryCast(node, MethodBlockSyntax)
+            If methodBlock IsNot Nothing Then
+                Return methodBlock.Statements.Last()
+            End If
+
+            Dim multiLineLambda = TryCast(node, MultiLineLambdaExpressionSyntax)
+            If multiLineLambda IsNot Nothing Then
+                Return multiLineLambda.Statements.Last()
+            End If
+
+            Return Nothing
+        End Function
+
+        Protected Overrides Function ConvertToBlockBody(node As SyntaxNode, expressionBody As SyntaxNode, editor As SyntaxEditor) As SyntaxNode
+            Throw New InvalidOperationException("Single line Async Sub is not supported by this code fixer")
+        End Function
+
+        Protected Overrides Function TryGetExpressionBody(methodSymbolOpt As SyntaxNode, ByRef expression As SyntaxNode) As Boolean
+            Dim singleLineLambda = TryCast(methodSymbolOpt, SingleLineLambdaExpressionSyntax)
+            If singleLineLambda IsNot Nothing Then
+                expression = singleLineLambda.Body
+                Return True
+            End If
+
+            Return False
+        End Function
+    End Class
+End Namespace
