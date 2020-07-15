7a7466a723f0abd2ab08ce6787bf2432082eac19
Tomáš Matoušek <tmat@users.noreply.github.com>
2020-07-14 18:10:23 -0700

    Disable interpreting of strings as DateTime during deserialization (#45891)

--- src/Workspaces/Remote/ServiceHub/ExternalAccess/UnitTesting/Api/UnitTestingServiceBase.cs
+++ src/Workspaces/Remote/ServiceHub/ExternalAccess/UnitTesting/Api/UnitTestingServiceBase.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 #nullable enable
@@ -27,13 +27,7 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
             => base.StartService();
 
         protected Task<Solution> GetSolutionAsync(JObject solutionInfo, CancellationToken cancellationToken)
-        {
-            var reader = solutionInfo.CreateReader();
-            var serializer = JsonSerializer.Create(new JsonSerializerSettings() { Converters = new[] { AggregateJsonConverter.Instance } });
-            var pinnedSolutionInfo = serializer.Deserialize<PinnedSolutionInfo>(reader);
-
-            return CreateSolutionService(pinnedSolutionInfo).GetSolutionAsync(pinnedSolutionInfo, cancellationToken);
-        }
+            => GetSolutionImplAsync(solutionInfo, cancellationToken);
 
         protected new Task<T> RunServiceAsync<T>(Func<Task<T>> callAsync, CancellationToken cancellationToken)
             => base.RunServiceAsync(callAsync, cancellationToken);
