6e51025907d836d9228df67484fb20b8654e33f5
Sam Harwell <Sam.Harwell@microsoft.com>
2020-07-15 19:03:50 -0700

    Fix usage of GetService extension

--- src/VisualStudio/Core/Def/Utilities/IServiceProviderExtensions.cs
+++ src/VisualStudio/Core/Def/Utilities/IServiceProviderExtensions.cs
@@ -18,19 +18,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Utilities
         }
 
         /// <summary>
-        /// Returns the specified interface from the service. This is useful when the service and interface differ
-        /// </summary>
-        public static TInterfaceType GetServiceTheWrongWay<TInterfaceType, TServiceType>(this IServiceProvider sp)
-            where TInterfaceType : class
-            where TServiceType : class
-        {
-            return (TInterfaceType)sp.GetService(typeof(TServiceType));
-        }
-
-        /// <summary>
         /// Returns the specified service type from the service.
         /// </summary>
         public static TServiceType GetService<TServiceType>(this IServiceProvider sp) where TServiceType : class
-            => sp.GetServiceTheWrongWay<TServiceType, TServiceType>();
+            => sp.GetService<TServiceType, TServiceType>();
     }
 }
