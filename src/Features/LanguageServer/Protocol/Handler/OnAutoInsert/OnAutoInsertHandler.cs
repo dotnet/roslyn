12de815208edab136c51c6ebb3601c8a23934274
David Wengier <git@wengier.com>
2020-07-24 14:41:38 +1000

    Add basic handler and tests for OnAutoInsert to add documentation comments

---
+++ src/Features/LanguageServer/Protocol/Handler/OnAutoInsert/OnAutoInsertHandler.cs
@@ -0,0 +1,76 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
+
+using System;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Editor;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler
+{
+    [Shared]
+    [ExportLspMethod(LSP.MSLSPMethods.OnAutoInsertName)]
+    internal class OnAutoInsertHandler : AbstractRequestHandler<LSP.DocumentOnAutoInsertParams, LSP.DocumentOnAutoInsertResponseItem>
+    {
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public OnAutoInsertHandler(ILspSolutionProvider solutionProvider) : base(solutionProvider)
+        {
+        }
+
+        public override Task<LSP.DocumentOnAutoInsertResponseItem> HandleRequestAsync(LSP.DocumentOnAutoInsertParams request, LSP.ClientCapabilities clientCapabilities, string? clientName,
+            CancellationToken cancellationToken)
+            => OnAutoInsertAsync(request, clientName, cancellationToken);
+
+        private async Task<LSP.DocumentOnAutoInsertResponseItem> OnAutoInsertAsync(LSP.DocumentOnAutoInsertParams autoInsertParams, string? clientName, CancellationToken cancellationToken)
+        {
+            var document = SolutionProvider.GetDocument(autoInsertParams.TextDocument, clientName);
+
+            if (document != null)
+            {
+                var service = document.GetRequiredLanguageService<IDocumentationCommentSnippetService>();
+
+                var syntaxTree = await document.GetRequiredSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
+                var sourceText = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+                var options = await document.GetOptionsAsync(cancellationToken).ConfigureAwait(false);
+
+                var linePosition = ProtocolConversions.PositionToLinePosition(autoInsertParams.Position);
+                var position = sourceText.Lines.GetPosition(linePosition);
+
+                var exteriorTriviaText = "///";
+
+                var result = autoInsertParams.Character == "\n"
+                    ? service.GetDocumentationCommentSnippetOnEnterTyped(syntaxTree, sourceText, position, options, exteriorTriviaText, cancellationToken)
+                    : service.GetDocumentationCommentSnippetOnCharacterTyped(syntaxTree, sourceText, position, options, cancellationToken);
+
+                if (result != null)
+                {
+                    return new LSP.DocumentOnAutoInsertResponseItem
+                    {
+                        TextEditFormat = LSP.InsertTextFormat.Snippet,
+                        TextEdit = new LSP.TextEdit
+                        {
+                            NewText = result.SnippetText.Insert(result.CaretOffset, "$0"),
+                            Range = new LSP.Range
+                            {
+                                // GetDocumentationCommentOnCharacterTyped returns the text to insert _after_ the triple-slash so we
+                                // can just insert it at the current position
+                                Start = autoInsertParams.Position,
+                                End = autoInsertParams.Position
+                            }
+                        }
+                    };
+                }
+            }
+
+            return new LSP.DocumentOnAutoInsertResponseItem();
+        }
+    }
+}
---
+++ src/Features/LanguageServer/ProtocolUnitTests/OnAutoInsert/OnAutoInsertTests.cs
@@ -0,0 +1,99 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Roslyn.Test.Utilities;
+using Xunit;
+using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.UnitTests.OnAutoInsert
+{
+    public class OnAutoInsertTests : AbstractLanguageServerProtocolTests
+    {
+        [Fact, Trait(Traits.Feature, Traits.Features.DocumentationComments)]
+        public async Task OnAutoInsert_CommentCharacter()
+        {
+            var markup =
+@"class A
+{
+    ///{|type:|}
+    void M()
+    {
+    }
+}";
+            var expected =
+@"class A
+{
+    /// <summary>
+    /// $0
+    /// </summary>
+    void M()
+    {
+    }
+}";
+            using var workspace = CreateTestWorkspace(markup, out var locations);
+            var characterTyped = "/";
+            var locationTyped = locations["type"].Single();
+            var documentText = await workspace.CurrentSolution.GetDocuments(locationTyped.Uri).Single().GetTextAsync();
+
+            var result = await RunOnAutoInsertAsync(workspace.CurrentSolution, characterTyped, locationTyped);
+            Assert.Equal(InsertTextFormat.Snippet, result.TextEditFormat);
+            var actualText = ApplyTextEdits(new[] { result.TextEdit }, documentText);
+            Assert.Equal(expected, actualText);
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.DocumentationComments)]
+        public async Task OnAutoInsert_EnterKey()
+        {
+            var markup =
+@"class A
+{
+    /// <summary>
+    /// Foo
+    /// </summary>
+{|type:|}
+    void M()
+    {
+    }
+}";
+            var expected =
+@"class A
+{
+    /// <summary>
+    /// Foo
+    /// </summary>
+    /// $0
+    void M()
+    {
+    }
+}";
+            using var workspace = CreateTestWorkspace(markup, out var locations);
+            var characterTyped = "\n";
+            var locationTyped = locations["type"].Single();
+            var documentText = await workspace.CurrentSolution.GetDocuments(locationTyped.Uri).Single().GetTextAsync();
+
+            var result = await RunOnAutoInsertAsync(workspace.CurrentSolution, characterTyped, locationTyped);
+            Assert.Equal(InsertTextFormat.Snippet, result.TextEditFormat);
+            var actualText = ApplyTextEdits(new[] { result.TextEdit }, documentText);
+            Assert.Equal(expected, actualText);
+        }
+
+        private static async Task<LSP.DocumentOnAutoInsertResponseItem> RunOnAutoInsertAsync(Solution solution, string characterTyped, LSP.Location locationTyped)
+            => await GetLanguageServer(solution)
+            .ExecuteRequestAsync<LSP.DocumentOnAutoInsertParams, LSP.DocumentOnAutoInsertResponseItem>(MSLSPMethods.OnAutoInsertName,
+                CreateDocumentOnAutoInsertParams(characterTyped, locationTyped), new LSP.ClientCapabilities(), null, CancellationToken.None);
+
+        private static LSP.DocumentOnAutoInsertParams CreateDocumentOnAutoInsertParams(string characterTyped, LSP.Location locationTyped)
+            => new LSP.DocumentOnAutoInsertParams()
+            {
+                Position = locationTyped.Range.Start,
+                Character = characterTyped,
+                TextDocument = CreateTextDocumentIdentifier(locationTyped.Uri)
+            };
+    }
+}
