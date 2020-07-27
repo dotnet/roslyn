a446d80b3a7bafce6da39dc80e02aa300cc3de44
David Wengier <git@wengier.com>
2020-07-27 18:04:53 +1000

    Document

--- src/EditorFeatures/Core/Implementation/DocumentationComments/DocumentationCommentSnippet.cs
+++ src/EditorFeatures/Core/Implementation/DocumentationComments/DocumentationCommentSnippet.cs
@@ -10,8 +10,19 @@ namespace Microsoft.CodeAnalysis.Editor
 {
     internal class DocumentationCommentSnippet
     {
+        /// <summary>
+        /// The span in the original text that should be replaced with the documentation comment.
+        /// </summary>
         public TextSpan SpanToReplace { get; }
+
+        /// <summary>
+        /// The documentation comment text to replace the span with
+        /// </summary>
         public string SnippetText { get; }
+
+        /// <summary>
+        /// The offset within <see cref="SnippetText"/> where the caret should be positioned after replacement
+        /// </summary>
         public int CaretOffset { get; }
 
         internal DocumentationCommentSnippet(TextSpan spanToReplace, string snippetText, int caretOffset)
