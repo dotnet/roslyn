17bc279896e39a572d500daa0e94ee49a53cc5cb
Javier Matos Denizac <javier.matosd@gmail.com>
2020-07-28 14:47:37 -0400

    Initial Setup

---
+++ src/Analyzers/CSharp/CodeFixes/SimplifyLinqExpressions/CSharpSimplifyLinqExpressionsCodeFixProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.SimplifyLinqExpressions
+{
+    class CSharpSimplifyLinqExpressionsCodeFixProvider
+    {
+    }
+}
