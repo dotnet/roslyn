76aac1054fa8f19ec69c4479d64d1ecbd2c1348e
Javier Matos Denizac <javier.matosd@gmail.com>
2020-07-28 14:47:37 -0400

    Initial Setup

---
+++ src/Analyzers/CSharp/Tests/SimplifyLinqExpressions/SimplifyLinqExpressionsTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis.CSharp.Analyzers.UnitTests.SimplifyLinqExpressions
+{
+    class SimplifyLinqExpressionsTests
+    {
+    }
+}
