6249321df53ab2faeb8d30df2dca4247d7ce8733
Javier Matos Denizac <javier.matosd@gmail.com>
2020-08-05 17:11:28 -0400

    Analyzer passes all tests

--- src/Analyzers/CSharp/Tests/SimplifyLinqExpressions/SimplifyLinqExpressionsFixAllTests.cs
+++ src/Analyzers/CSharp/Tests/SimplifyLinqExpressions/SimplifyLinqExpressionsFixAllTests.cs
@@ -14,23 +14,33 @@ namespace Microsoft.CodeAnalysis.CSharp.Analyzers.UnitTests.SimplifyLinqExpressi
         public async Task FixAllInDocument()
         {
             await TestInRegularAndScriptAsync(
-@"class C
+@"
+using System;
+using System.Linq;
+using System.Collections.Generic;
+ 
+class C
 {
     IEnumerable<string> test = new List<string> { 'hello', 'world', '!' };
-    var test1 = {|FixAllInDocument:test.Where(x => x == '!').Any()|};
-    var test2 = test.Where(x => x == '!').SingleOrDefault();
-    var test3 = test.Where(x => x == '!').Last();
-    var test4 = test.Where(x => x == '!').Count();
-    var test5 = test.Where(x => x == '!').FirstOrDefault();
+    var test1 = {|FixAllInDocument:test.Where(x => x.Equals('!')).Any()|};
+    var test2 = test.Where(x => x.Equals('!')).SingleOrDefault();
+    var test3 = test.Where(x => x.Equals('!')).Last();
+    var test4 = test.Where(x => x.Equals('!')).Count();
+    var test5 = test.Where(x => x.Equals('!')).FirstOrDefault();
 }",
-@"class C
+@"
+using System;
+using System.Linq;
+using System.Collections.Generic;
+ 
+class C
 {
     IEnumerable<string> test = new List<string> { 'hello', 'world', '!' };
-    var test1 = test.Any(x => x == '!');
-    var test2 = test.SingleOrDefault(x => x == '!');
-    var test3 = test.Last(x => x == '!');
-    var test4 = test.Count(x => x == '!');
-    var test5 = test.FirstOrDefault(x => x == '!');
+    var test1 = test.Any(x => x.Equals('!'));
+    var test2 = test.SingleOrDefault(x => x.Equals('!'));
+    var test3 = test.Last(x => x.Equals('!'));
+    var test4 = test.Count(x => x.Equals('!'));
+    var test5 = test.FirstOrDefault(x => x.Equals('!'));
 }");
         }
     }
