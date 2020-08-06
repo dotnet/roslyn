55cc22bddcbcab763198d92857a4cb39c7737bcc
Joey Robichaud <jorobich@microsoft.com>
2020-08-06 12:09:28 -0700

    Update GacFileResolver.IsAvailable to check if the platform is Mono

--- src/Compilers/Shared/GlobalAssemblyCacheHelpers/GacFileResolver.cs
+++ src/Compilers/Shared/GlobalAssemblyCacheHelpers/GacFileResolver.cs
@@ -21,8 +21,8 @@ namespace Microsoft.CodeAnalysis.Scripting.Hosting
         /// Returns true if GAC is available on the current platform.
         /// </summary>
         public static bool IsAvailable
-            // Check a type from System.Core since Mono doesn't load mscorlib from the GAC.
-            => typeof(Enumerable).Assembly.GlobalAssemblyCache;
+            // Since mscorlib may not be loaded from the GAC on Mono, also check if the platform is Mono which supports a GAC.
+            => typeof(object).Assembly.GlobalAssemblyCache || PlatformInformation.IsRunningOnMono;
 
         /// <summary>
         /// Architecture filter used when resolving assembly references.
@@ -38,7 +38,7 @@ namespace Microsoft.CodeAnalysis.Scripting.Hosting
         /// Creates an instance of a <see cref="GacFileResolver"/>, if available on the platform (check <see cref="IsAvailable"/>).
         /// </summary>
         /// <param name="architectures">Supported architectures used to filter GAC assemblies.</param>
-        /// <param name="preferredCulture">A culture to use when choosing the best assembly from 
+        /// <param name="preferredCulture">A culture to use when choosing the best assembly from
         /// among the set filtered by <paramref name="architectures"/></param>
         /// <exception cref="PlatformNotSupportedException">The platform doesn't support GAC.</exception>
         public GacFileResolver(
