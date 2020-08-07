677a1c5ca7e43595c7e3a7bee8ac3c70a27aad73
Javier Matos Denizac <javier.matosd@gmail.com>
2020-08-07 11:04:36 -0400

    Removed blank lines

--- src/Analyzers/CSharp/Tests/SimplifyLinqExpressions/SimplifyLinqExpressionsTests.cs
+++ src/Analyzers/CSharp/Tests/SimplifyLinqExpressions/SimplifyLinqExpressionsTests.cs
@@ -587,7 +587,6 @@ namespace demo
             await TestMissingInRegularAndScriptAsync(source);
         }
 
-
         [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsSimplifyLinqExpressions)]
         public async Task TestArgumentsInSecondCall()
 
