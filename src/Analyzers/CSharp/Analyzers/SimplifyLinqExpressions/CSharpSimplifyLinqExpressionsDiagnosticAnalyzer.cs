5a46a97154195a9c9e12cc23dbfbdb2b842b3567
Javier Matos Denizac <javier.matosd@gmail.com>
2020-08-10 17:05:01 -0400

    Update to Analyzer and Update to Analyzer resources

--- src/Analyzers/CSharp/Analyzers/SimplifyLinqExpressions/CSharpSimplifyLinqExpressionsDiagnosticAnalyzer.cs
+++ src/Analyzers/CSharp/Analyzers/SimplifyLinqExpressions/CSharpSimplifyLinqExpressionsDiagnosticAnalyzer.cs
@@ -11,6 +11,7 @@ using Microsoft.CodeAnalysis.Operations;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq.Expressions;
 #if CODE_STYLE
 using OptionSet = Microsoft.CodeAnalysis.Diagnostics.AnalyzerConfigOptions;
 #endif
@@ -20,11 +21,21 @@ namespace Microsoft.CodeAnalysis.CSharp.SimplifyLinqExpressions
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     internal sealed class CSharpSimplifyLinqExpressionsDiagnosticAnalyzer : AbstractBuiltInCodeStyleDiagnosticAnalyzer
     {
+        internal readonly ImmutableArray<string> s_validLinqCalls = ImmutableArray.Create(
+                nameof(Enumerable.First),
+                nameof(Enumerable.Last),
+                nameof(Enumerable.Single),
+                nameof(Enumerable.Any),
+                nameof(Enumerable.Count),
+                nameof(Enumerable.SingleOrDefault),
+                nameof(Enumerable.FirstOrDefault),
+                nameof(Enumerable.LastOrDefault)
+            );
 
         public CSharpSimplifyLinqExpressionsDiagnosticAnalyzer()
             : base(IDEDiagnosticIds.SimplifyLinqExpressionsDiagnosticId,
-                  option: null,
-                  title: new LocalizableResourceString(nameof(CSharpAnalyzersResources.Simplify_linq_expressions), CSharpAnalyzersResources.ResourceManager, typeof(CSharpAnalyzersResources)))
+                   option: null,
+                   title: new LocalizableResourceString(nameof(CSharpAnalyzersResources.Simplify_linq_expression), CSharpAnalyzersResources.ResourceManager, typeof(CSharpAnalyzersResources)))
         {
         }
 
@@ -40,17 +51,22 @@ namespace Microsoft.CodeAnalysis.CSharp.SimplifyLinqExpressions
         {
             var whereMethods = ImmutableHashSet.CreateBuilder<IMethodSymbol>();
             var linqMethods = ImmutableHashSet.CreateBuilder<IMethodSymbol>();
-            var validLinqCalls = new List<string> { "First", "Last", "Single", "Any", "Count", "SingleOrDefault", "FirstOrDefault", "LastOrDefault" };
 
-            var enumNamedType = context.Compilation.GetTypeByMetadataName("System.Linq.Enumerable");
-            var queryNamedType = context.Compilation.GetTypeByMetadataName("System.Linq.Queryable");
-            var enumMethods = enumNamedType?.GetMembers("Where").OfType<IMethodSymbol>();
-            var queryMethods = queryNamedType?.GetMembers("Where").OfType<IMethodSymbol>();
+            var enumNamedType = context.Compilation.GetTypeByMetadataName(typeof(System.Linq.Enumerable).FullName);
+            var queryNamedType = context.Compilation.GetTypeByMetadataName(typeof(System.Linq.Queryable).FullName);
+            var enumMethods = enumNamedType.GetMembers(nameof(Enumerable.Where)).OfType<IMethodSymbol>();
+            var queryMethods = queryNamedType.GetMembers(nameof(Enumerable.Where)).OfType<IMethodSymbol>();
+
+            if (enumMethods == null || queryMethods == null)
+            {
+                return;
+            }
+
             AddIfNotNull(whereMethods, enumMethods);
             AddIfNotNull(whereMethods, queryMethods);
 
             // add all valid linq calls
-            foreach (var id in validLinqCalls)
+            foreach (var id in s_validLinqCalls)
             {
                 enumMethods = enumNamedType?.GetMembers(id).OfType<IMethodSymbol>();
                 queryMethods = queryNamedType?.GetMembers(id).OfType<IMethodSymbol>();
@@ -65,6 +81,8 @@ namespace Microsoft.CodeAnalysis.CSharp.SimplifyLinqExpressions
                         context, whereMethods.ToImmutable(), linqMethods.ToImmutable()), OperationKind.Invocation);
             }
 
+            return;
+
             static void AddIfNotNull(ImmutableHashSet<IMethodSymbol>.Builder set, IEnumerable<IMethodSymbol>? others)
             {
                 if (others != null)
@@ -78,25 +96,38 @@ namespace Microsoft.CodeAnalysis.CSharp.SimplifyLinqExpressions
         {
             var invocationOperation = (IInvocationOperation)context.Operation;
             var semanticModel = context.Operation.SemanticModel;
-            // Check that .Where(...) is not user defined
-            var child = invocationOperation.Children.FirstOrDefault(c => c is IArgumentOperation);
-            var whereClause = child?.Children.FirstOrDefault(c => c is IInvocationOperation);
-            var isLambda = whereClause?.Children.Any(c => c.Syntax.IsKind(SyntaxKind.Argument) &&
-                                                    ((ArgumentSyntax)c.Syntax).Expression.IsKind(SyntaxKind.SimpleLambdaExpression)) ?? false;
 
-            if (whereClause == null ||
-                !(whereClause is IInvocationOperation method) ||
-                method.TargetMethod.OriginalDefinition == null ||
-                !isLambda ||
-                !whereMethods.Contains(method.TargetMethod.OriginalDefinition))
+            var firstChildArgument = invocationOperation.Children.FirstOrDefault(c => c is IArgumentOperation);
+
+            if (firstChildArgument is null)
+            {
+                return;
+            }
+
+            var whereClause = firstChildArgument.Children.FirstOrDefault(c => c is IInvocationOperation);
+
+            if (whereClause is null)
+            {
+                return;
+            }
+
+            var isLambda = whereClause.Children.Any(c => c.Syntax is ArgumentSyntax argument && argument.Expression is SimpleLambdaExpressionSyntax);
+
+            if (!isLambda)
+            {
+                return;
+            }
+
+            if (whereClause is IInvocationOperation method && !whereMethods.Contains(method.TargetMethod.OriginalDefinition))
             {
                 return;
             }
 
             // check that the Where clause is followed by a call to a valid method i.e. one of First, FirstOrDefault, Single, SingleOrDefault, etc..
             // and that it is also not user defined
-            var originalDefinition = (invocationOperation.TargetMethod.ReducedFrom ?? invocationOperation.TargetMethod).OriginalDefinition;
-            if (!linqMethods.Contains(originalDefinition))
+            var invocationDefinition = invocationOperation.TargetMethod.OriginalDefinition;
+
+            if (!linqMethods.Contains(invocationDefinition))
             {
                 return;
             }
@@ -110,6 +141,14 @@ namespace Microsoft.CodeAnalysis.CSharp.SimplifyLinqExpressions
 
             var operation = context.Operation;
             var node = operation.Syntax;
+
+            // check that the nodes expression is a MemberAccessExpressionSyntax
+            if (!((InvocationExpressionSyntax)node is InvocationExpressionSyntax invocation &&
+                (MemberAccessExpressionSyntax)invocation.Expression is MemberAccessExpressionSyntax))
+            {
+                return;
+            }
+
             var location = node.GetLocation();
             context.ReportDiagnostic(
                 DiagnosticHelper.Create(Descriptor, location, Descriptor.GetEffectiveSeverity(context.Compilation.Options),
