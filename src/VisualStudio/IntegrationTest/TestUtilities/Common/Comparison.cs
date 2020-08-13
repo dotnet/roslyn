ee48dc817e9e3c9eb34d717493155d271ac23855
Manish Vasani <mavasani@microsoft.com>
2020-08-13 16:11:01 -0700

    Simplify check

--- src/VisualStudio/IntegrationTest/TestUtilities/Common/Comparison.cs
+++ src/VisualStudio/IntegrationTest/TestUtilities/Common/Comparison.cs
@@ -10,8 +10,7 @@ namespace Microsoft.VisualStudio.IntegrationTest.Utilities.Common
     internal static class Comparison
     {
         public static bool AreStringValuesEqual(string str1, string str2)
-            => string.IsNullOrEmpty(str1) && string.IsNullOrEmpty(str2)
-            || str1 == str2;
+            => (str1 ?? "") == (str2 ?? "");
 
         public static bool AreArraysEqual<T>(T[] array1, T[] array2) where T : IEquatable<T>
         {
