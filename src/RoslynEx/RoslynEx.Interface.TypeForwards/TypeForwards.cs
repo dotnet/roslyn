723527b39dfb49fa6da3f74bd8907692e57fb5e5
Petr Onderka <petro@postsharp.net>
2020-08-14 14:20:37 +0200

    Added type forwards project

---
+++ src/RoslynEx/RoslynEx.Interface.TypeForwards/TypeForwards.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+using RoslynEx;
+
+[assembly: TypeForwardedTo(typeof(ISourceTransformer))]
+[assembly: TypeForwardedTo(typeof(SourceTransformerContext))]
+[assembly: TypeForwardedTo(typeof(TransformerAttribute))]
