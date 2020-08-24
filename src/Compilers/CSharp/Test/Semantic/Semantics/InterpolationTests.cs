f5a47703e80d385573ed151ee47537d29733430c
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-08-24 17:17:39 +0200

    Update InterpolationTests.cs

--- src/Compilers/CSharp/Test/Semantic/Semantics/InterpolationTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/InterpolationTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Microsoft.CodeAnalysis.Test.Utilities;
 using Roslyn.Test.Utilities;
 using System.Linq;
