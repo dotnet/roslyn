06fba384569f6de2e22c59fc17282c931b3b4243
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-08-26 18:55:05 -0700

    rename parameters

--- src/Workspaces/Core/Portable/Storage/SQLite/v1/SQLitePersistentStorage_DocumentSerialization.cs
+++ src/Workspaces/Core/Portable/Storage/SQLite/v1/SQLitePersistentStorage_DocumentSerialization.cs
@@ -14,11 +14,11 @@ namespace Microsoft.CodeAnalysis.SQLite.v1
 {
     internal partial class SQLitePersistentStorage
     {
-        protected override Task<Checksum> ReadChecksumAsync(DocumentKey documentKey, Document? documentOpt, string name, CancellationToken cancellationToken)
-            => _documentAccessor.ReadChecksumAsync((documentKey, documentOpt, name), cancellationToken);
+        protected override Task<Checksum> ReadChecksumAsync(DocumentKey documentKey, Document? bulkLoadSnapshot, string name, CancellationToken cancellationToken)
+            => _documentAccessor.ReadChecksumAsync((documentKey, bulkLoadSnapshot, name), cancellationToken);
 
-        protected override Task<Stream> ReadStreamAsync(DocumentKey documentKey, Document? documentOpt, string name, Checksum? checksum, CancellationToken cancellationToken)
-            => _documentAccessor.ReadStreamAsync((documentKey, documentOpt, name), checksum, cancellationToken);
+        protected override Task<Stream> ReadStreamAsync(DocumentKey documentKey, Document? bulkLoadSnapshot, string name, Checksum? checksum, CancellationToken cancellationToken)
+            => _documentAccessor.ReadStreamAsync((documentKey, bulkLoadSnapshot, name), checksum, cancellationToken);
 
         public override Task<bool> WriteStreamAsync(Document document, string name, Stream stream, Checksum? checksum, CancellationToken cancellationToken)
             => _documentAccessor.WriteStreamAsync(((DocumentKey)document, document, name), stream, checksum, cancellationToken);
@@ -28,7 +28,7 @@ namespace Microsoft.CodeAnalysis.SQLite.v1
         /// retrieving data from <see cref="DocumentDataTableName"/>.
         /// </summary>
         private class DocumentAccessor : Accessor<
-            (DocumentKey documentKey, Document? documentOpt, string name),
+            (DocumentKey documentKey, Document? bulkLoadSnapshot, string name),
             (DocumentId, string),
             long>
         {
@@ -38,11 +38,11 @@ namespace Microsoft.CodeAnalysis.SQLite.v1
 
             protected override string DataTableName => DocumentDataTableName;
 
-            protected override (DocumentId, string) GetWriteQueueKey((DocumentKey documentKey, Document? documentOpt, string name) key)
+            protected override (DocumentId, string) GetWriteQueueKey((DocumentKey documentKey, Document? bulkLoadSnapshot, string name) key)
                 => (key.documentKey.Id, key.name);
 
-            protected override bool TryGetDatabaseId(SqlConnection connection, (DocumentKey documentKey, Document? documentOpt, string name) key, out long dataId)
-                => Storage.TryGetDocumentDataId(connection, key.documentKey, key.documentOpt, key.name, out dataId);
+            protected override bool TryGetDatabaseId(SqlConnection connection, (DocumentKey documentKey, Document? bulkLoadSnapshot, string name) key, out long dataId)
+                => Storage.TryGetDocumentDataId(connection, key.documentKey, key.bulkLoadSnapshot, key.name, out dataId);
 
             protected override void BindFirstParameter(SqlStatement statement, long dataId)
                 => statement.BindInt64Parameter(parameterIndex: 1, value: dataId);
