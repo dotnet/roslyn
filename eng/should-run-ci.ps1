f9a21e2de5b9a95088bc3093b55c29597d40877b
Rikki Gibson <rigibson@microsoft.com>
2020-08-28 16:54:03 -0700

    well

--- eng/should-run-ci.ps1
+++ eng/should-run-ci.ps1
@@ -2,4 +2,5 @@
 
 Write-Host "Yes this script really exists and is being run"
 ShouldRunCI -AsOutput
-Write-Host "##vso[task.setvariable variable=ShouldRunCI]$_ShouldRunCI"
+$str = $_ShouldRunCI ? "true" : "false"
+Write-Host "##vso[task.setvariable variable=ShouldRunCI]$str"
