30719ffaa417c3a3ac00654df25a50c7d52e9802
Rikki Gibson <rigibson@microsoft.com>
2020-08-28 16:41:09 -0700

    WIP

--- eng/should-run-ci.ps1
+++ eng/should-run-ci.ps1
@@ -1,4 +1,4 @@
 . (Join-Path $PSScriptRoot "build-utils.ps1")
 
+Write-Host "Yes this script really exists and is being run"
 ShouldRunCI -AsOutput
-Write-Host "##vso[task.setvariable variable=ShouldRunCI]$_ShouldRunCI"
