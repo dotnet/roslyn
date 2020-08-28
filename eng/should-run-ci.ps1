49f1f842d654ca248bff38a40f985c43eb008a93
Rikki Gibson <rigibson@microsoft.com>
2020-08-28 16:27:21 -0700

    WIP

--- eng/should-run-ci.ps1
+++ eng/should-run-ci.ps1
@@ -1,9 +1,4 @@
 . (Join-Path $PSScriptRoot "build-utils.ps1")
 
 ShouldRunCI -AsOutput
-if ($_ShouldRunCI) {
-  exit 0;
-}
-else {
-  exit 1;
-}
+Write-Host "##vso[task.setvariable variable=ShouldRunCI]$_ShouldRunCI"
