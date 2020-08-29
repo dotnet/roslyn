d563865743075b03fdd998b84649b0c2a9429e93
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-08-29 20:20:11 +0200

    Address feedback

--- src/Compilers/CSharp/Portable/Syntax/SyntaxKind.cs
+++ src/Compilers/CSharp/Portable/Syntax/SyntaxKind.cs
@@ -13,135 +13,135 @@ namespace Microsoft.CodeAnalysis.CSharp
         List = GreenNode.ListKind,
 
         // punctuation
-        /// <summary>Represents <c>~<c> token.</summary>
+        /// <summary>Represents <c>~</c> token.</summary>
         TildeToken = 8193,
-        /// <summary>Represents <c>!<c> token.</summary>
+        /// <summary>Represents <c>!</c> token.</summary>
         ExclamationToken = 8194,
-        /// <summary>Represents <c>$<c> token.</summary>
+        /// <summary>Represents <c>$</c> token.</summary>
         DollarToken = 8195,
-        /// <summary>Represents <c>%<c> token.</summary>
+        /// <summary>Represents <c>%</c> token.</summary>
         PercentToken = 8196,
-        /// <summary>Represents <c>^<c> token.</summary>
+        /// <summary>Represents <c>^</c> token.</summary>
         CaretToken = 8197,
-        /// <summary>Represents <c>&amp;<c> token.</summary>
+        /// <summary>Represents <c>&amp;</c> token.</summary>
         AmpersandToken = 8198,
-        /// <summary>Represents <c>*<c> token.</summary>
+        /// <summary>Represents <c>*</c> token.</summary>
         AsteriskToken = 8199,
-        /// <summary>Represents <c>(<c> token.</summary>
+        /// <summary>Represents <c>(</c> token.</summary>
         OpenParenToken = 8200,
-        /// <summary>Represents <c>)<c> token.</summary>
+        /// <summary>Represents <c>)</c> token.</summary>
         CloseParenToken = 8201,
-        /// <summary>Represents <c>-<c> token.</summary>
+        /// <summary>Represents <c>-</c> token.</summary>
         MinusToken = 8202,
-        /// <summary>Represents <c>+<c> token.</summary>
+        /// <summary>Represents <c>+</c> token.</summary>
         PlusToken = 8203,
-        /// <summary>Represents <c>=<c> token.</summary>
+        /// <summary>Represents <c>=</c> token.</summary>
         EqualsToken = 8204,
-        /// <summary>Represents <c>{<c> token.</summary>
+        /// <summary>Represents <c>{</c> token.</summary>
         OpenBraceToken = 8205,
-        /// <summary>Represents <c>}<c> token.</summary>
+        /// <summary>Represents <c>}</c> token.</summary>
         CloseBraceToken = 8206,
-        /// <summary>Represents <c>[<c> token.</summary>
+        /// <summary>Represents <c>[</c> token.</summary>
         OpenBracketToken = 8207,
-        /// <summary>Represents <c>]<c> token.</summary>
+        /// <summary>Represents <c>]</c> token.</summary>
         CloseBracketToken = 8208,
-        /// <summary>Represents <c>|<c> token.</summary>
+        /// <summary>Represents <c>|</c> token.</summary>
         BarToken = 8209,
-        /// <summary>Represents <c>\<c> token.</summary>
+        /// <summary>Represents <c>\</c> token.</summary>
         BackslashToken = 8210,
-        /// <summary>Represents <c>:<c> token.</summary>
+        /// <summary>Represents <c>:</c> token.</summary>
         ColonToken = 8211,
-        /// <summary>Represents <c>;<c> token.</summary>
+        /// <summary>Represents <c>;</c> token.</summary>
         SemicolonToken = 8212,
-        /// <summary>Represents <c>"<c> token.</summary>
+        /// <summary>Represents <c>"</c> token.</summary>
         DoubleQuoteToken = 8213,
-        /// <summary>Represents <c>'<c> token.</summary>
+        /// <summary>Represents <c>'</c> token.</summary>
         SingleQuoteToken = 8214,
-        /// <summary>Represents <c>&lt;<c> token.</summary>
+        /// <summary>Represents <c>&lt;</c> token.</summary>
         LessThanToken = 8215,
-        /// <summary>Represents <c>,<c> token.</summary>
+        /// <summary>Represents <c>,</c> token.</summary>
         CommaToken = 8216,
-        /// <summary>Represents <c>&gt;<c> token.</summary>
+        /// <summary>Represents <c>&gt;</c> token.</summary>
         GreaterThanToken = 8217,
-        /// <summary>Represents <c>.<c> token.</summary>
+        /// <summary>Represents <c>.</c> token.</summary>
         DotToken = 8218,
-        /// <summary>Represents <c>?<c> token.</summary>
+        /// <summary>Represents <c>?</c> token.</summary>
         QuestionToken = 8219,
-        /// <summary>Represents <c>#<c> token.</summary>
+        /// <summary>Represents <c>#</c> token.</summary>
         HashToken = 8220,
-        /// <summary>Represents <c>/<c> token.</summary>
+        /// <summary>Represents <c>/</c> token.</summary>
         SlashToken = 8221,
-        /// <summary>Represents <c>..<c> token.</summary>
+        /// <summary>Represents <c>..</c> token.</summary>
         DotDotToken = 8222,
 
         // additional xml tokens
-        /// <summary>Represents <c>/&gt;<c> token.</summary>
+        /// <summary>Represents <c>/&gt;</c> token.</summary>
         SlashGreaterThanToken = 8232, // xml empty element end
-        /// <summary>Represents <c>&lt;/<c> token.</summary>
+        /// <summary>Represents <c>&lt;/</c> token.</summary>
         LessThanSlashToken = 8233, // element end tag start token
-        /// <summary>Represents <c>&lt;!--<c> token.</summary>
+        /// <summary>Represents <c>&lt;!--</c> token.</summary>
         XmlCommentStartToken = 8234, // <!--
-        /// <summary>Represents <c>--&gt;<c> token.</summary>
+        /// <summary>Represents <c>--&gt;</c> token.</summary>
         XmlCommentEndToken = 8235, // -->
-        /// <summary>Represents <c>&lt;![CDATA[<c> token.</summary>
+        /// <summary>Represents <c>&lt;![CDATA[</c> token.</summary>
         XmlCDataStartToken = 8236, // <![CDATA[
-        /// <summary>Represents <c>]]&gt;<c> token.</summary>
+        /// <summary>Represents <c>]]&gt;</c> token.</summary>
         XmlCDataEndToken = 8237, // ]]>
-        /// <summary>Represents <c>&lt;?<c> token.</summary>
+        /// <summary>Represents <c>&lt;?</c> token.</summary>
         XmlProcessingInstructionStartToken = 8238, // <?
-        /// <summary>Represents <c>?&gt;<c> token.</summary>
+        /// <summary>Represents <c>?&gt;</c> token.</summary>
         XmlProcessingInstructionEndToken = 8239, // ?>
 
         // compound punctuation
-        /// <summary>Represents <c>||<c> token.</summary>
+        /// <summary>Represents <c>||</c> token.</summary>
         BarBarToken = 8260,
-        /// <summary>Represents <c>&amp;&amp;<c> token.</summary>
+        /// <summary>Represents <c>&amp;&amp;</c> token.</summary>
         AmpersandAmpersandToken = 8261,
-        /// <summary>Represents <c>--<c> token.</summary>
+        /// <summary>Represents <c>--</c> token.</summary>
         MinusMinusToken = 8262,
-        /// <summary>Represents <c>++<c> token.</summary>
+        /// <summary>Represents <c>++</c> token.</summary>
         PlusPlusToken = 8263,
-        /// <summary>Represents <c>::<c> token.</summary>
+        /// <summary>Represents <c>::</c> token.</summary>
         ColonColonToken = 8264,
-        /// <summary>Represents <c>??<c> token.</summary>
+        /// <summary>Represents <c>??</c> token.</summary>
         QuestionQuestionToken = 8265,
-        /// <summary>Represents <c>-&gt;<c> token.</summary>
+        /// <summary>Represents <c>-&gt;</c> token.</summary>
         MinusGreaterThanToken = 8266,
-        /// <summary>Represents <c>!=<c> token.</summary>
+        /// <summary>Represents <c>!=</c> token.</summary>
         ExclamationEqualsToken = 8267,
-        /// <summary>Represents <c>==<c> token.</summary>
+        /// <summary>Represents <c>==</c> token.</summary>
         EqualsEqualsToken = 8268,
-        /// <summary>Represents <c>=&gt;<c> token.</summary>
+        /// <summary>Represents <c>=&gt;</c> token.</summary>
         EqualsGreaterThanToken = 8269,
-        /// <summary>Represents <c>&lt;=<c> token.</summary>
+        /// <summary>Represents <c>&lt;=</c> token.</summary>
         LessThanEqualsToken = 8270,
-        /// <summary>Represents <c>&lt;&lt;<c> token.</summary>
+        /// <summary>Represents <c>&lt;&lt;</c> token.</summary>
         LessThanLessThanToken = 8271,
-        /// <summary>Represents <c>&lt;&lt;=<c> token.</summary>
+        /// <summary>Represents <c>&lt;&lt;=</c> token.</summary>
         LessThanLessThanEqualsToken = 8272,
-        /// <summary>Represents <c>&gt;=<c> token.</summary>
+        /// <summary>Represents <c>&gt;=</c> token.</summary>
         GreaterThanEqualsToken = 8273,
-        /// <summary>Represents <c>&gt;&gt;<c> token.</summary>
+        /// <summary>Represents <c>&gt;&gt;</c> token.</summary>
         GreaterThanGreaterThanToken = 8274,
-        /// <summary>Represents <c>&gt;&gt;=<c> token.</summary>
+        /// <summary>Represents <c>&gt;&gt;=</c> token.</summary>
         GreaterThanGreaterThanEqualsToken = 8275,
-        /// <summary>Represents <c>/=<c> token.</summary>
+        /// <summary>Represents <c>/=</c> token.</summary>
         SlashEqualsToken = 8276,
-        /// <summary>Represents <c>*=<c> token.</summary>
+        /// <summary>Represents <c>*=</c> token.</summary>
         AsteriskEqualsToken = 8277,
-        /// <summary>Represents <c>|=<c> token.</summary>
+        /// <summary>Represents <c>|=</c> token.</summary>
         BarEqualsToken = 8278,
-        /// <summary>Represents <c>&amp;=<c> token.</summary>
+        /// <summary>Represents <c>&amp;=</c> token.</summary>
         AmpersandEqualsToken = 8279,
-        /// <summary>Represents <c>+=<c> token.</summary>
+        /// <summary>Represents <c>+=</c> token.</summary>
         PlusEqualsToken = 8280,
-        /// <summary>Represents <c>-=<c> token.</summary>
+        /// <summary>Represents <c>-=</c> token.</summary>
         MinusEqualsToken = 8281,
-        /// <summary>Represents <c>^=<c> token.</summary>
+        /// <summary>Represents <c>^=</c> token.</summary>
         CaretEqualsToken = 8282,
-        /// <summary>Represents <c>%=<c> token.</summary>
+        /// <summary>Represents <c>%=</c> token.</summary>
         PercentEqualsToken = 8283,
-        /// <summary>Represents <c>??=<c> token.</summary>
+        /// <summary>Represents <c>??=</c> token.</summary>
         QuestionQuestionEqualsToken = 8284,
 
         // Keywords
@@ -433,11 +433,11 @@ namespace Microsoft.CodeAnalysis.CSharp
         /// <summary>Represents <see langword="r"/>.</summary>
         ReferenceKeyword = 8481,
 
-        /// <summary>Represents <code>$"</code> token.</summary>
+        /// <summary>Represents <c>$"</c> token.</summary>
         InterpolatedStringStartToken = 8482,            // $"
-        /// <summary>Represents <code>"</code> token that is closing <code>$"</code>.</summary>
+        /// <summary>Represents <c>"</c> token that is closing <code>$"</code>.</summary>
         InterpolatedStringEndToken = 8483,              // "
-        /// <summary>Represents <code>$@</code> or <code>@$</code> token.</summary>
+        /// <summary>Represents <c>$@</c> or <c>@$</c> token.</summary>
         InterpolatedVerbatimStringStartToken = 8484,    // $@" or @$"
 
         // additional preprocessor keywords (continued)
@@ -457,11 +457,11 @@ namespace Microsoft.CodeAnalysis.CSharp
         // Other
         /// <summary>Represents <see langword="var"/>.</summary>
         VarKeyword = 8490,
-        /// <summary>Represents <code>_</code> token.</summary>
+        /// <summary>Represents <c>_</c> token.</summary>
         UnderscoreToken = 8491,
-        /// <summary>Represents <code>TODO: What's this?</code> token.</summary>
+        /// <summary>Represents <c>TODO: What's this?</c> token.</summary>
         OmittedTypeArgumentToken = 8492,
-        /// <summary>Represents <code>TODO: What's this?</code> token.</summary>
+        /// <summary>Represents <c>TODO: What's this?</c> token.</summary>
         OmittedArraySizeExpressionToken = 8493,
         /// <summary>Represents a token that comes after the end of a directive such as <code>#endif</code>.</summary>
         EndOfDirectiveToken = 8494,
