0bc03d78ce954c179993bc79236fe977e51e0464
Manish Vasani <mavasani@microsoft.com>
2020-08-31 16:35:41 -0700

    Remove unnecessary using

--- src/Utilities/FlowAnalysis/Extensions/OperationBlocksExtensions.cs
+++ src/Utilities/FlowAnalysis/Extensions/OperationBlocksExtensions.cs
@@ -3,7 +3,6 @@
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.FlowAnalysis;
-using Microsoft.CodeAnalysis.Operations;
 
 namespace Analyzer.Utilities.Extensions
 {
