3396e33430eeeab4c1b7a75f0eed9cb24f929241
Marius Ungureanu <therzok@gmail.com>
2020-09-02 22:22:21 +0300

    Add missing file

---
+++ src/Workspaces/Remote/ServiceHub/ProcessExtensions.cs
@@ -0,0 +1,30 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.Remote
+{
+    /// <summary>
+    /// Boost performance of any servicehub service which is invoked by user explicit actions
+    /// </summary>
+    internal static class ProcessExtensions
+    {
+        public static bool TrySetPriorityClass(this Process process, ProcessPriorityClass priorityClass)
+        {
+            try
+            {
+                process.PriorityClass = priorityClass;
+                return true;
+            }
+            catch (Exception e) when (e is PlatformNotSupportedException || e is Win32Exception)
+            {
+                // the runtime does not support changing process priority
+                return false;
+            }
+        }
+    }
+}
