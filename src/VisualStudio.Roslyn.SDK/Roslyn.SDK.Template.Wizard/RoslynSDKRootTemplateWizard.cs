1aa1e022b546ea8b5be688dfd77d79e96294a836
Sam Harwell <Sam.Harwell@microsoft.com>
2020-09-02 11:11:15 -0700

    Switch package feeds to dotnet-tools

--- src/VisualStudio.Roslyn.SDK/Roslyn.SDK.Template.Wizard/RoslynSDKRootTemplateWizard.cs
+++ src/VisualStudio.Roslyn.SDK/Roslyn.SDK.Template.Wizard/RoslynSDKRootTemplateWizard.cs
@@ -14,8 +14,7 @@ public partial class RoslynSDKRootTemplateWizard
   <packageSources>
     <clear />
     <add key=""nuget.org"" value=""https://api.nuget.org/v3/index.json"" />
-    <add key=""roslyn"" value=""https://dotnet.myget.org/F/roslyn/api/v3/index.json"" />
-    <add key=""roslyn-analyzers"" value=""https://dotnet.myget.org/F/roslyn-analyzers/api/v3/index.json"" />
+    <add key=""dotnet-tools"" value=""https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json"" />
   </packageSources>
   <disabledPackageSources>
     <clear />
