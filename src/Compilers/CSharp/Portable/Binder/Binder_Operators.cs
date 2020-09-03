13655dd84f99625c50cb0b75085dc102184606ab
Charles Stoner <chucks@microsoft.com>
2020-09-02 22:36:15 -0700

    Add helper methods

--- src/Compilers/CSharp/Portable/Binder/Binder_Operators.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Operators.cs
@@ -166,13 +166,7 @@ namespace Microsoft.CodeAnalysis.CSharp
 
             BinaryOperatorSignature bestSignature = best.Signature;
 
-            switch (bestSignature.Kind & BinaryOperatorKind.TypeMask)
-            {
-                case BinaryOperatorKind.NInt:
-                case BinaryOperatorKind.NUInt:
-                    CheckFeatureAvailability(node, MessageID.IDS_FeatureNativeInt, diagnostics);
-                    break;
-            }
+            CheckNativeIntegerFeatureAvailability(bestSignature.Kind, node, diagnostics);
 
             if (CheckOverflowAtRuntime)
             {
@@ -579,13 +573,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                     break;
             }
 
-            switch (resultOperatorKind & BinaryOperatorKind.TypeMask)
-            {
-                case BinaryOperatorKind.NInt:
-                case BinaryOperatorKind.NUInt:
-                    CheckFeatureAvailability(node, MessageID.IDS_FeatureNativeInt, diagnostics);
-                    break;
-            }
+            CheckNativeIntegerFeatureAvailability(resultOperatorKind, node, diagnostics);
 
             TypeSymbol resultType = signature.ReturnType;
             BoundExpression resultLeft = left;
@@ -2122,13 +2110,7 @@ namespace Microsoft.CodeAnalysis.CSharp
 
             var signature = best.Signature;
 
-            switch (signature.Kind & UnaryOperatorKind.TypeMask)
-            {
-                case UnaryOperatorKind.NInt:
-                case UnaryOperatorKind.NUInt:
-                    CheckFeatureAvailability(node, MessageID.IDS_FeatureNativeInt, diagnostics);
-                    break;
-            }
+            CheckNativeIntegerFeatureAvailability(signature.Kind, node, diagnostics);
 
             HashSet<DiagnosticInfo> useSiteDiagnostics = null;
             var resultConversion = Conversions.ClassifyConversionFromType(signature.ReturnType, operandType, ref useSiteDiagnostics);
@@ -2501,13 +2483,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             var resultMethod = signature.Method;
             var resultConstant = FoldUnaryOperator(node, resultOperatorKind, resultOperand, resultType.SpecialType, diagnostics);
 
-            switch (resultOperatorKind & UnaryOperatorKind.TypeMask)
-            {
-                case UnaryOperatorKind.NInt:
-                case UnaryOperatorKind.NUInt:
-                    CheckFeatureAvailability(node, MessageID.IDS_FeatureNativeInt, diagnostics);
-                    break;
-            }
+            CheckNativeIntegerFeatureAvailability(resultOperatorKind, node, diagnostics);
 
             return new BoundUnaryOperator(
                 node,
@@ -4022,6 +3998,28 @@ namespace Microsoft.CodeAnalysis.CSharp
             else
             {
                 return ConstantValue.Bad;
+            }
+        }
+
+        private static void CheckNativeIntegerFeatureAvailability(BinaryOperatorKind operatorKind, SyntaxNode syntax, DiagnosticBag diagnostics)
+        {
+            switch (operatorKind & BinaryOperatorKind.TypeMask)
+            {
+                case BinaryOperatorKind.NInt:
+                case BinaryOperatorKind.NUInt:
+                    CheckFeatureAvailability(syntax, MessageID.IDS_FeatureNativeInt, diagnostics);
+                    break;
+            }
+        }
+
+        private static void CheckNativeIntegerFeatureAvailability(UnaryOperatorKind operatorKind, SyntaxNode syntax, DiagnosticBag diagnostics)
+        {
+            switch (operatorKind & UnaryOperatorKind.TypeMask)
+            {
+                case UnaryOperatorKind.NInt:
+                case UnaryOperatorKind.NUInt:
+                    CheckFeatureAvailability(syntax, MessageID.IDS_FeatureNativeInt, diagnostics);
+                    break;
             }
         }
     }
