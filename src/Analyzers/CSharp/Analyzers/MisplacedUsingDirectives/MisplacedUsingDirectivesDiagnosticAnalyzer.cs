a83f04a8e12c7a510e9773a16a867412503f4288
Vritant Bhardwaj <vrbhardw@microsoft.com>
2020-09-02 22:40:59 -0700

    clean changes

--- src/Analyzers/CSharp/Analyzers/MisplacedUsingDirectives/MisplacedUsingDirectivesDiagnosticAnalyzer.cs
+++ src/Analyzers/CSharp/Analyzers/MisplacedUsingDirectives/MisplacedUsingDirectivesDiagnosticAnalyzer.cs
@@ -66,9 +66,7 @@ namespace Microsoft.CodeAnalysis.CSharp.MisplacedUsingDirectives
 
         private static void AnalyzeCompilationUnitNode(SyntaxNodeAnalysisContext context)
         {
-            var option = context.Options.GetOption(
-                CSharpCodeStyleOptions.PreferredUsingDirectivePlacement,
-                context.Node.SyntaxTree, context.CancellationToken);
+            var option = context.Options.GetOption(CSharpCodeStyleOptions.PreferredUsingDirectivePlacement, context.Node.SyntaxTree, context.CancellationToken);
 
             var compilationUnit = (CompilationUnitSyntax)context.Node;
 
