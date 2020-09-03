80a071a7043a7ba8324c973aa920af4347ea3ab7
Martin Strecker <MaStr11@users.noreply.github.com>
2020-09-03 18:54:31 +0100

    Add code comments to the switch expression

--- src/Analyzers/CSharp/CodeFixes/UseIndexOrRangeOperator/CSharpUseRangeOperatorCodeFixProvider.cs
+++ src/Analyzers/CSharp/CodeFixes/UseIndexOrRangeOperator/CSharpUseRangeOperatorCodeFixProvider.cs
@@ -106,18 +106,21 @@ namespace Microsoft.CodeAnalysis.CSharp.UseIndexOrRangeOperator
 
             if (result.MemberInfo.OverloadedMethodOpt == null)
             {
-                var expression = invocation.Expression switch
-                {
-                    MemberAccessExpressionSyntax memberAccess => memberAccess.Expression,
-                    MemberBindingExpressionSyntax _ => null,
-                    _ => invocation.Expression
-                };
-
                 var argList = invocation.ArgumentList;
                 var argumentList = BracketedArgumentList(
                         Token(SyntaxKind.OpenBracketToken).WithTriviaFrom(argList.OpenParenToken),
                         arguments,
                         Token(SyntaxKind.CloseBracketToken).WithTriviaFrom(argList.CloseParenToken));
+                // The method invocation will be replaced by a range operator. We need to find the right replacement.
+                var expression = invocation.Expression switch
+                {
+                    // a.b() -> a[..] element access with expression "a"
+                    MemberAccessExpressionSyntax memberAccess => memberAccess.Expression,
+                    // a?.b() -> a?[..] the invocation b() is referenced to WhenNotNull 
+                    // of a ConditionalAccessExpression. b() is replaced by the ElementAccessExpression [..]
+                    MemberBindingExpressionSyntax _ => null,
+                    _ => invocation.Expression
+                };
                 return expression == null
                     ? (ExpressionSyntax)ElementBindingExpression(argumentList)
                     : ElementAccessExpression(expression, argumentList);
