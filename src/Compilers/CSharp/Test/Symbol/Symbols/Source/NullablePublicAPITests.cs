7e57a2b4495f6410077c82295a7d0bf9b9fb7030
Fredric Silberberg <frsilb@microsoft.com>
2020-09-04 15:51:10 -0700

    Correctly get binder when a local function is the first thing in top level statements
    We had a bug where nullable analysis was getting the incorrect binder when attempting to populate initial information about all top-level statements if the first thing in the top level was a local function. This ensures that we use the correct binder for these cases.
    
    I've investigated other areas where we call `GetAdjustedNodePosition`, and I do not believe that any of them have this issue. It seems like this particular case is unique in that preferring the innermost binder was actually not what we wanted to do here, or the usage wasn't possible to hit with this scenario.

--- src/Compilers/CSharp/Test/Symbol/Symbols/Source/NullablePublicAPITests.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/Source/NullablePublicAPITests.cs
@@ -4712,5 +4712,47 @@ class C
             Assert.Equal(PublicNullableAnnotation.NotAnnotated, typeInfo.Type.NullableAnnotation);
             Assert.Equal(PublicNullableFlowState.NotNull, typeInfo.Nullability.FlowState);
         }
+
+        [Fact, WorkItem(47467, "https://github.com/dotnet/roslyn/issues/47467")]
+        public void GetDeclaredSymbolTopLevelStatementsWithLocalFunctionFirst_1()
+        {
+            var comp = CreateCompilation(@"void M() {}", options: TestOptions.ReleaseExe.WithNullableContextOptions(NullableContextOptions.Enable));
+
+            var tree = comp.SyntaxTrees[0];
+            var localFunction = tree.GetRoot().DescendantNodes().OfType<LocalFunctionStatementSyntax>().Single();
+            var model = comp.GetSemanticModel(tree);
+
+            AssertEx.Equal("void M()", model.GetDeclaredSymbol(localFunction).ToTestDisplayString());
+        }
+
+        [Fact, WorkItem(47467, "https://github.com/dotnet/roslyn/issues/47467")]
+        public void GetDeclaredSymbolTopLevelStatementsWithLocalFunctionFirst_2()
+        {
+            var comp = CreateCompilation(@"void M() {}
+M();
+", options: TestOptions.ReleaseExe.WithNullableContextOptions(NullableContextOptions.Enable));
+
+            var tree = comp.SyntaxTrees[0];
+            var localFunction = tree.GetRoot().DescendantNodes().OfType<LocalFunctionStatementSyntax>().Single();
+            var model = comp.GetSemanticModel(tree);
+
+            AssertEx.Equal("void M()", model.GetDeclaredSymbol(localFunction).ToTestDisplayString());
+        }
+
+        [Fact, WorkItem(47467, "https://github.com/dotnet/roslyn/issues/47467")]
+        public void GetDeclaredSymbolTopLevelStatementsWithLocalFunctionFirst_3()
+        {
+            var comp = CreateCompilation(@"
+// Comment
+void M() {}
+M();
+", options: TestOptions.ReleaseExe.WithNullableContextOptions(NullableContextOptions.Enable));
+
+            var tree = comp.SyntaxTrees[0];
+            var localFunction = tree.GetRoot().DescendantNodes().OfType<LocalFunctionStatementSyntax>().Single();
+            var model = comp.GetSemanticModel(tree);
+
+            AssertEx.Equal("void M()", model.GetDeclaredSymbol(localFunction).ToTestDisplayString());
+        }
     }
 }
