654d9beb3d4f8e18b2d5db324bd964040090837d
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-09-05 17:30:00 +0200

    Update DiagnosticTest.cs

--- src/Compilers/CSharp/Test/Syntax/Diagnostics/DiagnosticTest.cs
+++ src/Compilers/CSharp/Test/Syntax/Diagnostics/DiagnosticTest.cs
@@ -347,6 +347,7 @@ class X
                         case ErrorCode.WRN_SyncAndAsyncEntryPoints:
                         case ErrorCode.WRN_ParameterIsStaticClass:
                         case ErrorCode.WRN_ReturnTypeIsStaticClass:
+                        case ErrorCode.WRN_EqualityWithNaN:
                             // These are the warnings introduced with the warning "wave" shipped with dotnet 5 and C# 9.
                             Assert.Equal(5, ErrorFacts.GetWarningLevel(errorCode));
                             break;
@@ -406,6 +407,7 @@ class X
                     ErrorCode.WRN_ParameterIsStaticClass,
                     ErrorCode.WRN_ReturnTypeIsStaticClass,
                     ErrorCode.WRN_RecordNamedDisallowed,
+                    ErrorCode.WRN_EqualityWithNaN,
                 };
 
                 Assert.Contains(error, nullableUnrelatedWarnings);
