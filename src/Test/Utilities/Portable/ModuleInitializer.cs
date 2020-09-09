68a0d5e222210e5e34d4d7ec14a5def87088b44d
Sam Harwell <Sam.Harwell@microsoft.com>
2020-09-09 11:51:23 -0700

    Only include the ThrowingTraceListener module initializer for NETCOREAPP

--- src/Test/Utilities/Portable/ModuleInitializer.cs
+++ src/Test/Utilities/Portable/ModuleInitializer.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NETCOREAPP
+
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Microsoft.CodeAnalysis;
@@ -18,3 +20,5 @@ namespace Roslyn.Test.Utilities
         }
     }
 }
+
+#endif
