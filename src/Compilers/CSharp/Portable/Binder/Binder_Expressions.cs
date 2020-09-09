112a05c5cd5eb4c981dd291158a103e8c3d11db6
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-09-09 18:40:47 +0200

    Split long line

--- src/Compilers/CSharp/Portable/Binder/Binder_Expressions.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Expressions.cs
@@ -1483,7 +1483,9 @@ namespace Microsoft.CodeAnalysis.CSharp
                 {
                     // Don't bind nameof(nint) or nameof(nuint) so that ERR_NameNotInContext is reported.
                     // The check for ArgumentSyntax is to allow the binding for something like nameof(nint.Equals).
-                    var type = IsInsideNameof && identifier.Parent is ArgumentSyntax && identifier.Parent?.Parent?.Parent is InvocationExpressionSyntax invocation && (invocation.Expression as IdentifierNameSyntax)?.Identifier.ContextualKind() == SyntaxKind.NameOfKeyword
+                    var type = IsInsideNameof && identifier.Parent is ArgumentSyntax &&
+                        identifier.Parent?.Parent?.Parent is InvocationExpressionSyntax invocation &&
+                        (invocation.Expression as IdentifierNameSyntax)?.Identifier.ContextualKind() == SyntaxKind.NameOfKeyword
                         ? null
                         : BindNativeIntegerSymbolIfAny(identifier, diagnostics);
 
