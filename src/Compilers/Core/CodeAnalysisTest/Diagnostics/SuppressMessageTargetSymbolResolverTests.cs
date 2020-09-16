dotnet/roslyn

commit 3c3750158b620cfdeca45891245468cf423a1597

Address feedback

src/Compilers/Core/Portable/DiagnosticAnalyzer/SuppressMessageAttributeState.TargetSymbolResolver.cs
@@ -55,6 +55,11 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                 return id;
             }
 
+            /// <summary>
+            /// Attempts to resolve the "Target" argument of the global SuppressMessageAttribute to symbols in compilation.
+            /// </summary>
+            /// <param name="resolvedWithDocCommentIdFormat">Indicates if resolved "Target" argument is in Roslyn's <see cref="DocumentationCommentId"/> format.</param>
+            /// <returns>Resolved symbols for the the "Target" argument of the global SuppressMessageAttribute.</returns>
             public ImmutableArray<ISymbol> Resolve(out bool resolvedWithDocCommentIdFormat)
             {
                 resolvedWithDocCommentIdFormat = false;
@@ -103,7 +108,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                     var candidateMembers = containingSymbol.GetMembers(segment);
                     if (candidateMembers.Length == 0)
                     {
-                        return results.ToImmutableAndFree();
+                        break;
                     }
 
                     if (segmentIsNamedTypeName.HasValue)
@@ -134,7 +139,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                         if (parameters == null)
                         {
                             // Failed to resolve parameter list
-                            return results.ToImmutableAndFree();
+                            break;
                         }
                     }
                     else if (nextChar == '.' || nextChar == '+')
@@ -158,7 +163,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                         {
                             // If we cannot resolve the name on the left of the delimiter, we have no 
                             // hope of finding the symbol.
-                            return results.ToImmutableAndFree();
+                            break;
                         }
 
                         if (containingSymbol.Kind == SymbolKind.NamedType)
@@ -185,7 +190,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                             results.Add(method);
                         }
 
-                        return results.ToImmutableAndFree();
+                        break;
                     }
 
                     ISymbol singleResult;
@@ -225,9 +230,9 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                     {
                         results.Add(singleResult);
                     }
-
-                    return results.ToImmutableAndFree();
                 }
+
+                return results.ToImmutableAndFree();
             }
 
             private string ParseNextNameSegment()

src/Analyzers/CSharp/Tests/CSharpAnalyzers.UnitTests.projitems
@@ -25,7 +25,7 @@
     <Compile Include="$(MSBuildThisFileDirectory)MisplacedUsingDirectives\MisplacedUsingDirectivesCodeFixProviderTests.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)OrderModifiers\OrderModifiersCompilerErrorTests.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)OrderModifiers\OrderModifiersTests.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)RemoveUnnecessarySuppressions\RemoveLegacySuppressionsTests.cs" />
+    <Compile Include="$(MSBuildThisFileDirectory)UpdateLegacySuppressions\UpdateLegacySuppressionsTests.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)RemoveUnnecessarySuppressions\RemoveUnnecessarySuppressionsTests.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)RemoveUnnecessaryParentheses\RemoveUnnecessaryPatternParenthesesTests.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)RemoveUnreachableCode\RemoveUnreachableCodeTests.cs" />
@@ -95,5 +95,8 @@
   </ItemGroup>
   <ItemGroup Condition="'$(DefaultLanguageSourceExtension)' != '' AND '$(BuildingInsideVisualStudio)' != 'true'">
     <ExpectedCompile Include="$(MSBuildThisFileDirectory)**\*$(DefaultLanguageSourceExtension)" />
+  </ItemGroup>
+  <ItemGroup>
+    <Folder Include="$(MSBuildThisFileDirectory)UpdateLegacySuppressions\" />
   </ItemGroup>
 </Project>

src/Analyzers/CSharp/Tests/UpdateLegacySuppressions/UpdateLegacySuppressionsTests.cs
@@ -11,13 +11,13 @@ using Roslyn.Test.Utilities;
 using Xunit;
 using VerifyCS = Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions.CSharpCodeFixVerifier<
     Microsoft.CodeAnalysis.CSharp.RemoveUnnecessarySuppressions.CSharpRemoveUnnecessarySuppressionsDiagnosticAnalyzer,
-    Microsoft.CodeAnalysis.RemoveUnnecessarySuppressions.RemoveLegacySuppressionsCodeFixProvider>;
+    Microsoft.CodeAnalysis.UpdateLegacySuppressions.UpdateLegacySuppressionsCodeFixProvider>;
 
-namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.RemoveUnnecessarySuppressions
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.UpdateLegacySuppressions
 {
-    [Trait(Traits.Feature, Traits.Features.CodeActionsRemoveUnnecessarySuppressions)]
+    [Trait(Traits.Feature, Traits.Features.CodeActionsUpdateLegacySuppressions)]
     [WorkItem(44362, "https://github.com/dotnet/roslyn/issues/44362")]
-    public class RemoveLegacySuppressionsTests
+    public class UpdateLegacySuppressionsTests
     {
         [Fact]
         public void TestStandardProperties()

src/Analyzers/Core/Analyzers/AnalyzersResources.resx
@@ -320,6 +320,6 @@
     <value>Avoid legacy format target in 'SuppressMessageAttribute'</value>
   </data>
   <data name="Avoid_legacy_format_target_0_in_SuppressMessageAttribute" xml:space="preserve">
-    <value>Avoid legacy format target '{0}' in 'SuppressMessageAttribute''SuppressMessageAttribute'</value>
+    <value>Avoid legacy format target '{0}' in 'SuppressMessageAttribute'</value>
   </data>
 </root>

src/Analyzers/Core/Analyzers/RemoveUnnecessarySuppressions/AbstractRemoveUnnecessarySuppressionsDiagnosticAnalyzer.cs
@@ -76,19 +76,20 @@ namespace Microsoft.CodeAnalysis.RemoveUnnecessarySuppressions
                     return;
                 }
 
-                if (SuppressMessageAttributeState.HasInvalidScope(namedAttributeArguments, out var targetScope))
+                if (!SuppressMessageAttributeState.HasValidScope(namedAttributeArguments, out var targetScope))
                 {
                     reportDiagnostic(Diagnostic.Create(s_invalidScopeDescriptor, attributeSyntax.GetLocation()));
                     return;
                 }
 
-                if (_state.HasInvalidOrMissingTarget(namedAttributeArguments, targetScope, out var targetHasDocCommentIdFormat,
+                if (!_state.HasValidTarget(namedAttributeArguments, targetScope, out var targetHasDocCommentIdFormat,
                         out var targetSymbolString, out var targetValueOperation, out var resolvedSymbols))
                 {
                     reportDiagnostic(Diagnostic.Create(s_invalidOrMissingTargetDescriptor, attributeSyntax.GetLocation()));
                     return;
                 }
 
+                // We want to flag valid target which uses legacy format to update to Roslyn based DocCommentId format.
                 if (resolvedSymbols.Length > 0 && !targetHasDocCommentIdFormat)
                 {
                     RoslynDebug.Assert(!string.IsNullOrEmpty(targetSymbolString));
@@ -107,10 +108,7 @@ namespace Microsoft.CodeAnalysis.RemoveUnnecessarySuppressions
                         }
                     }
 
-#pragma warning disable CS8620 // Mismatch in nullability of 'properties' parameter and argument types - Parameter type for 'properties' has been updated to 'ImmutableDictionary<string, string?>?' in newer version of Microsoft.CodeAnalysis (3.7.x).
-                    reportDiagnostic(Diagnostic.Create(LegacyFormatTargetDescriptor, targetValueOperation.Syntax.GetLocation(), properties, targetSymbolString));
-#pragma warning restore CS8620
-
+                    reportDiagnostic(Diagnostic.Create(LegacyFormatTargetDescriptor, targetValueOperation.Syntax.GetLocation(), properties!, targetSymbolString));
                     return;
                 }
             }

src/Analyzers/Core/Analyzers/RemoveUnnecessarySuppressions/SuppressMessageAttributeState.cs
@@ -81,7 +81,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
             return namedAttributeArguments.Length > 0;
         }
 
-        public static bool HasInvalidScope(ImmutableArray<(string name, IOperation value)> namedAttributeArguments, out TargetScope targetScope)
+        public static bool HasValidScope(ImmutableArray<(string name, IOperation value)> namedAttributeArguments, out TargetScope targetScope)
         {
             if (!TryGetNamedArgument(namedAttributeArguments, SuppressMessageScope, out var scopeString, out _) ||
                 RoslynString.IsNullOrEmpty(scopeString))
@@ -92,13 +92,13 @@ namespace Microsoft.CodeAnalysis.Diagnostics
             else if (!s_targetScopesMap.TryGetValue(scopeString, out targetScope))
             {
                 targetScope = TargetScope.None;
-                return true;
+                return false;
             }
 
-            return false;
+            return true;
         }
 
-        public bool HasInvalidOrMissingTarget(
+        public bool HasValidTarget(
             ImmutableArray<(string name, IOperation value)> namedAttributeArguments,
             TargetScope targetScope,
             out bool targetHasDocCommentIdFormat,
@@ -114,7 +114,7 @@ namespace Microsoft.CodeAnalysis.Diagnostics
             if (targetScope == TargetScope.Resource)
             {
                 // Legacy scope which we do not handle.
-                return false;
+                return true;
             }
 
             if (!TryGetNamedArgument(namedAttributeArguments, SuppressMessageTarget, out targetSymbolString, out targetValueOperation))
@@ -125,12 +125,12 @@ namespace Microsoft.CodeAnalysis.Diagnostics
             if (targetScope == TargetScope.Module)
             {
                 // Compilation wide suppression with a non-null target is considered invalid.
-                return targetSymbolString != null;
+                return targetSymbolString == null;
             }
 
             var resolver = new TargetSymbolResolver(_compilation, targetScope, targetSymbolString);
             resolvedSymbols = resolver.Resolve(out targetHasDocCommentIdFormat);
-            return resolvedSymbols.IsEmpty;
+            return !resolvedSymbols.IsEmpty;
         }
 
         private static bool TryGetNamedArgument(

src/Analyzers/Core/Analyzers/xlf/AnalyzersResources.cs.xlf
@@ -38,8 +38,8 @@
         <note />
       </trans-unit>
       <trans-unit id="Avoid_legacy_format_target_0_in_SuppressMessageAttribute">
-        <source>Avoid legacy format target '{0}' in 'SuppressMessageAttribute''SuppressMessageAttribute'</source>
-        <target state="new">Avoid legacy format target '{0}' in 'SuppressMessageAttribute''SuppressMessageAttribute'</target>
+        <source>Avoid legacy format target '{0}' in 'SuppressMessageAttribute'</source>
+        <target state="new">Avoid legacy format target '{0}' in 'SuppressMessageAttribute'</target>
         <note />
       </trans-unit>
       <trans-unit id="Avoid_legacy_format_target_in_SuppressMessageAttribute">

src/Analyzers/Core/Analyzers/xlf/AnalyzersResources.de.xlf
@@ -38,8 +38,8 @@
         <note />
       </trans-unit>
       <trans-unit id="Avoid_legacy_format_target_0_in_SuppressMessageAttribute">
-        <source>Avoid legacy format target '{0}' in 'SuppressMessageAttribute''SuppressMessageAttribute'</source>
-        <target state="new">Avoid legacy format target '{0}' in 'SuppressMessageAttribute''SuppressMessageAttribute'</target>
+        <source>Avoid legacy format target '{0}' in 'SuppressMessageAttribute'</source>
+        <target state="new">Avoid legacy format target '{0}' in 'SuppressMessageAttribute'</target>
         <note />
       </trans-unit>
       <trans-unit id="Avoid_legacy_format_target_in_SuppressMessageAttribute">

commit 9d2d36feb314357cad8e440f00e167fd0f45b318

Fix regression in SuppressMessageAttributeState.TargetSymbolResolver
Fixes VSO Watson [#1141257](https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1141257)

Regression was introduced in https://github.com/dotnet/roslyn/commit/3c3750158b620cfdeca45891245468cf423a1597#diff-464dab23fb56bf98bfcf831dd11f4288L228-L229. I forgot to add a break statement during some refactoring from PR feedback.

Verified that added unit test hangs prior to the fix

src/Compilers/Core/Portable/DiagnosticAnalyzer/SuppressMessageAttributeState.TargetSymbolResolver.cs
@@ -230,6 +230,8 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                     {
                         results.Add(singleResult);
                     }
+
+                    break;
                 }
 
                 return results.ToImmutableAndFree();

src/Compilers/Core/CodeAnalysisTest/Diagnostics/SuppressMessageTargetSymbolResolverTests.cs
@@ -366,6 +366,20 @@ End Class
                 "C.s");
         }
 
+        [Fact, WorkItem(1141257, "https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1141257")]
+        public void TestResolveEnumFieldWithoutName()
+        {
+            var source = @"
+enum E
+{
+    $$,
+}
+";
+            var syntaxTree = CreateSyntaxTree(source, LanguageNames.CSharp);
+            var compilation = CreateCompilation(syntaxTree, LanguageNames.CSharp, "");
+            _ = SuppressMessageAttributeState.ResolveTargetSymbols(compilation, "E.", SuppressMessageAttributeState.TargetScope.Member);
+        }
+
         [Fact]
         public void TestResolveProperty1()
         {