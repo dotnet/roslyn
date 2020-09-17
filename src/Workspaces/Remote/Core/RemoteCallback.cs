97a1e7c9d4dae662e569d96a9c9f808bb78b7fa5
Tomas Matousek <tomat@microsoft.com>
2020-09-17 14:40:59 -0700

    Improve callback error handling.

--- src/Workspaces/Remote/Core/RemoteCallback.cs
+++ src/Workspaces/Remote/Core/RemoteCallback.cs
@@ -79,45 +79,55 @@ namespace Microsoft.CodeAnalysis.Remote
             }
         }
 
-        // TODO: https://github.com/microsoft/vs-streamjsonrpc/issues/246
+        // Remote calls can only throw 4 types of exceptions that correspond to
         //
-        // We need to get to a state when remote calls can only throw 4 types of exceptions that correspond to
         //   1) Connection issue (connection dropped for any reason)
         //   2) Serialization issue - bug in serialization of arguments (types are not serializable, etc.)
         //   3) Remote exception - an exception was thrown by the callee
         //   4) Cancelation
-        // When a connection is dropped and CancelLocallyInvokedMethodsWhenConnectionIsClosed is set the connection dropped exception [1] should not be thrown.
-        // Instead a the cancellation token should be signaled and OperationCancelledException should be thrown ([4]).
         //
-        // Until the above issue in JSON-RPC is fixed we do a best guess on what the issue is.
-
         private bool ReportUnexpectedException(Exception exception, CancellationToken cancellationToken)
         {
-            if (exception is RemoteInvocationException or MessagePackSerializationException or IOException)
+            if (exception is IOException)
             {
-                // indicates bug on client side or in serialization, propagate the exception
-                return FatalError.ReportWithoutCrashAndPropagate(exception);
+                // propagate intermittent exceptions without reporting telemetry:
+                return false;
             }
 
-            if (cancellationToken.IsCancellationRequested)
+            if (exception is OperationCanceledException)
             {
-                // If cancelation is requested and we see a different exception the handler will throw OperationCancelledException.
-                return exception is not OperationCanceledException;
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return false;
+                }
+
+                // It is not guaranteed that RPC only throws OCE when our token is signaled.
+                // Signal the cancelation source that our token is linked to and throw new cancellation
+                // exception in OnUnexpectedException.
+                ClientDisconnectedSource.Cancel();
+
+                return true;
             }
 
-            // We assume that any other exception indicates lost connection (it might not),
-            // cancel any ongoing work since the client can't receive the results.
-            // This should be handled by JSON-RPC but it's not guaranteed due to https://github.com/microsoft/vs-streamjsonrpc/issues/246.
-            ClientDisconnectedSource.Cancel();
+            // When a connection is dropped and CancelLocallyInvokedMethodsWhenConnectionIsClosed is
+            // set ConnectionLostException should not be thrown. Instead the cancellation token should be
+            // signaled and OperationCancelledException should be thrown.
+            // Seems to not work in all cases currently, so we need to cancel ourselves (bug https://github.com/microsoft/vs-streamjsonrpc/issues/551).
+            // Once this issue is fixed we can remov this if statement and fall back to reporting NFW
+            // as any observation of ConnectionLostException indicates a bug (e.g. https://github.com/microsoft/vs-streamjsonrpc/issues/549).
+            if (exception is ConnectionLostException)
+            {
+                ClientDisconnectedSource.Cancel();
 
-            // catch the exception, cancellation exception will be thrown by the handler.
-            return true;
+                return true;
+            }
+
+            // Indicates bug on client side or in serialization, report NFW and propagate the exception.
+            return FatalError.ReportWithoutCrashAndPropagate(exception);
         }
 
         private static Exception OnUnexpectedException(CancellationToken cancellationToken)
         {
-            // Remote call may fail with different exception even when our cancellation token is signaled
-            // (e.g. on shutdown if the connection is dropped):
             cancellationToken.ThrowIfCancellationRequested();
 
             // If this is hit the cancellation token passed to the service implementation did not use the correct token.
