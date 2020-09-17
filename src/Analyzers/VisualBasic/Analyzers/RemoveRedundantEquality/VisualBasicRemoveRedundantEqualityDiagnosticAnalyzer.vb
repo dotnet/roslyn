02228d1100a98ab629903d88cd414d27202a9145
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-09-17 14:52:38 +0200

    Update VisualBasicRemoveRedundantEqualityDiagnosticAnalyzer.vb

--- src/Analyzers/VisualBasic/Analyzers/RemoveRedundantEquality/VisualBasicRemoveRedundantEqualityDiagnosticAnalyzer.vb
+++ src/Analyzers/VisualBasic/Analyzers/RemoveRedundantEquality/VisualBasicRemoveRedundantEqualityDiagnosticAnalyzer.vb
@@ -2,6 +2,7 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
+Imports Microsoft.CodeAnalysis.RemoveRedundantEquality
 Imports Microsoft.CodeAnalysis.VisualBasic.LanguageServices
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.RemoveRedundantEquality
