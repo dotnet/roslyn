b9c48e1f2d78ef3410f4df4f531eafbd56be1016
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-09-19 20:14:40 +0200

    Exclude constant fields from record's PrintMembers

--- src/Compilers/CSharp/Portable/Symbols/Synthesized/Records/SynthesizedRecordPrintMembers.cs
+++ src/Compilers/CSharp/Portable/Symbols/Synthesized/Records/SynthesizedRecordPrintMembers.cs
@@ -214,7 +214,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                     return false;
                 }
 
-                if (m.Kind is SymbolKind.Field)
+                if (m is SymbolField { IsConst: false })
                 {
                     return true;
                 }
