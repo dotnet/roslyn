02f41d94f0bb7e431ac70894826e4918e8d973c7
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-09-22 12:03:40 -0700

    Fix code

--- src/Features/Core/Portable/Diagnostics/DiagnosticService.cs
+++ src/Features/Core/Portable/Diagnostics/DiagnosticService.cs
@@ -232,14 +232,12 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                 }
                 else
                 {
-                    using var pool = SharedPools.Default<List<Data>>().GetPooledObject();
-
                     AppendMatchingData(source, workspace, projectId, documentId, id, buffer);
                     Debug.Assert(buffer.Count == 0 || buffer.Count == 1);
 
                     if (buffer.Count == 1)
                     {
-                        var diagnostics = pool.Object[0].Diagnostics;
+                        var diagnostics = buffer[0].Diagnostics;
                         return includeSuppressedDiagnostics
                             ? diagnostics
                             : diagnostics.NullToEmpty().WhereAsArray(d => !d.IsSuppressed);
