9ec08a85b839405cc2bd56d6589d85b02df1a7f6
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2020-09-26 14:13:38 -0700

    Update src/EditorFeatures/Test/Diagnostics/IDEDiagnosticIDConfigurationTests.cs

--- src/EditorFeatures/Test/Diagnostics/IDEDiagnosticIDConfigurationTests.cs
+++ src/EditorFeatures/Test/Diagnostics/IDEDiagnosticIDConfigurationTests.cs
@@ -401,7 +401,7 @@ csharp_style_prefer_not_pattern = true:suggestion
 csharp_style_implicit_object_creation_when_type_is_apparent = true:suggestion
 
 # IDE0091
-dotnet_diagnostic.IDE0091.severity = %value%
+dotnet_diagnostic.IDE0100.severity = %value%
 
 # IDE1005
 csharp_style_conditional_delegate_call = true:suggestion
