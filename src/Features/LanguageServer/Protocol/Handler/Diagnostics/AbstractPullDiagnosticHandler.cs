125591a2a49415e97a0a5bc8fbe2cb9e10039dce
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-09-28 16:06:38 -0700

    Cleanup

--- src/Features/LanguageServer/Protocol/Handler/Diagnostics/AbstractPullDiagnosticHandler.cs
+++ src/Features/LanguageServer/Protocol/Handler/Diagnostics/AbstractPullDiagnosticHandler.cs
@@ -11,7 +11,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Diagnostics
 {
@@ -130,7 +132,7 @@ namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Diagnostics
             var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
             var _ = ArrayBuilder<VSDiagnostic>.GetInstance(out var diagnostics);
             foreach (var diagnostic in _diagnosticService.GetDiagnostics(document, includeSuppressedDiagnostics: false, cancellationToken))
-                diagnostics.Add(DiagnosticUtilities.Convert(text, diagnostic));
+                diagnostics.Add(ConvertDiagnostic(text, diagnostic));
 
             var report = RecordDiagnosticReport(document, diagnostics.ToArray());
             Report(progress, reports, report);
@@ -196,6 +198,47 @@ namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Diagnostics
                 if (document != null)
                     documentToDiagnosticParams[document] = previousResult;
             }
+        }
+        public static VSDiagnostic ConvertDiagnostic(SourceText text, DiagnosticData diagnosticData)
+        {
+            Contract.ThrowIfNull(diagnosticData.Message, $"Got a document diagnostic that did not have a {nameof(diagnosticData.Message)}");
+            Contract.ThrowIfNull(diagnosticData.DataLocation, $"Got a document diagnostic that did not have a {nameof(diagnosticData.DataLocation)}");
+
+            return new VSDiagnostic
+            {
+                Code = diagnosticData.Id,
+                Message = diagnosticData.Message,
+                Severity = ProtocolConversions.DiagnosticSeverityToLspDiagnositcSeverity(diagnosticData.Severity),
+                Range = ProtocolConversions.LinePositionToRange(DiagnosticData.GetLinePositionSpan(diagnosticData.DataLocation, text, useMapped: true)),
+                Tags = ConvertTags(diagnosticData),
+                DiagnosticType = diagnosticData.Category,
+            };
+        }
+
+        private static DiagnosticTag[] ConvertTags(DiagnosticData diagnosticData)
+        {
+            using var _ = ArrayBuilder<DiagnosticTag>.GetInstance(out var result);
+
+            if (diagnosticData.Severity == DiagnosticSeverity.Hidden)
+            {
+                result.Add(VSDiagnosticTags.HiddenInEditor);
+                result.Add(VSDiagnosticTags.HiddenInErrorList);
+            }
+
+            foreach (var tag in diagnosticData.CustomTags)
+            {
+                switch (tag)
+                {
+                    case WellKnownDiagnosticTags.Unnecessary:
+                        result.Add(DiagnosticTag.Unnecessary);
+                        break;
+                    case WellKnownDiagnosticTags.Build:
+                        result.Add(VSDiagnosticTags.BuildError);
+                        break;
+                }
+            }
+
+            return result.ToArray();
         }
     }
 }
