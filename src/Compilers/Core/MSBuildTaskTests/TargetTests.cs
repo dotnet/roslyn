ffd36e3a7cc89275313dfa9f2795db7972f89a47
Jared Parsons <jaredpparsons@gmail.com>
2020-09-28 10:50:08 -0700

    Manual fixup

--- src/Compilers/Core/MSBuildTaskTests/TargetTests.cs
+++ src/Compilers/Core/MSBuildTaskTests/TargetTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 // uncomment the below define to dump binlogs of each test
+
 //#define DUMP_MSBUILD_BIN_LOG
 
 using System;
