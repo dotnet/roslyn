0725852a20e7c680e8bf7f57cf2e5dbafd209c10
Martin Strecker <MaStr11@users.noreply.github.com>
2020-09-29 20:02:05 +0100

    Add comment with source for built-in conversions

--- src/Features/CSharp/Portable/Completion/CompletionProviders/OperatorsAndIndexer/ConversionOperatorExtensions.cs
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/OperatorsAndIndexer/ConversionOperatorExtensions.cs
@@ -10,6 +10,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
     {
         public static SpecialType[]? GetBuiltInNumericConversions(this ITypeSymbol container)
         {
+            // Source: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/conversions#explicit-numeric-conversions
             return container.SpecialType switch
             {
                 SpecialType.System_SByte => new[] {
