361ec4d9def8903f8478e06487ab0c2972b5a62b
Jared Parsons <jaredpparsons@gmail.com>
2020-09-29 12:15:21 -0700

    Mop up PR for nullability default change
    This cleans up some issues identified in the nullability default change
    PR (#47961)

--- src/Features/Lsif/Generator/Writing/LineModeLsifJsonWriter.cs
+++ src/Features/Lsif/Generator/Writing/LineModeLsifJsonWriter.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.IO;
 using Microsoft.CodeAnalysis.LanguageServerIndexFormat.Generator.Graph;
 using Newtonsoft.Json;
