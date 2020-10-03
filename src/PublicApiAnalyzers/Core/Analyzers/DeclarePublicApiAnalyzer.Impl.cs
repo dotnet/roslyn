e54ea5c77bf6c271c29ac51b843b227847e23c14
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-10-03 13:09:56 +0200

    Apply suggestion

--- src/PublicApiAnalyzers/Core/Analyzers/DeclarePublicApiAnalyzer.Impl.cs
+++ src/PublicApiAnalyzers/Core/Analyzers/DeclarePublicApiAnalyzer.Impl.cs
@@ -182,7 +182,7 @@ namespace Microsoft.CodeAnalysis.PublicApiAnalyzers
                 if (symbol.Kind == SymbolKind.NamedType)
                 {
                     var namedType = (INamedTypeSymbol)symbol;
-                    if ((namedType.InstanceConstructors.Length == 1 && namedType.TypeKind == TypeKind.Class)
+                    if ((namedType.TypeKind == TypeKind.Class && namedType.InstanceConstructors.Length == 1)
                         || namedType.TypeKind == TypeKind.Struct)
                     {
                         var implicitConstructor = namedType.InstanceConstructors.FirstOrDefault(x => x.IsImplicitlyDeclared);
