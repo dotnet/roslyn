ae25cd97a519a47433cf5f08d95f62df2286c08b
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-10-05 23:38:38 +0800

    Add more test cases.

--- src/Analyzers/CSharp/Tests/UseCompoundAssignment/UseCompoundAssignmentTests.cs
+++ src/Analyzers/CSharp/Tests/UseCompoundAssignment/UseCompoundAssignmentTests.cs
@@ -917,11 +917,77 @@ struct InsertionPoint
         }
 
         [WorkItem(38054, "https://github.com/dotnet/roslyn/issues/38054")]
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCompoundAssignment)]
+        public async Task TestIncrementChar()
+        {
+            await TestInRegularAndScript1Async(
+@"public class C
+{
+    void M(char a)
+    {
+        a [||]= a + 1;
+    }
+}",
+@"public class C
+{
+    void M(char a)
+    {
+        a++;
+    }
+}");
+        }
+
+        [WorkItem(38054, "https://github.com/dotnet/roslyn/issues/38054")]
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCompoundAssignment)]
+        public async Task TestIncrementEnum()
+        {
+            await TestInRegularAndScript1Async(
+@"public enum E {}
+public class C
+{
+    void M(E a)
+    {
+        a [||]= a + 1;
+    }
+}",
+@"public enum E {}
+public class C
+{
+    void M(E a)
+    {
+        a++;
+    }
+}");
+        }
+
+        [WorkItem(38054, "https://github.com/dotnet/roslyn/issues/38054")]
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCompoundAssignment)]
+        public async Task TestIncrementDecimal()
+        {
+            await TestInRegularAndScript1Async(
+@"public class C
+{
+    void M(decimal a)
+    {
+        a [||]= a + 1.0m;
+    }
+}",
+@"public class C
+{
+    void M(decimal a)
+    {
+        a++;
+    }
+}");
+        }
+
+        [WorkItem(38054, "https://github.com/dotnet/roslyn/issues/38054")]
         [Theory, Trait(Traits.Feature, Traits.Features.CodeActionsUseCompoundAssignment)]
         [InlineData("byte")]
         [InlineData("short")]
         [InlineData("long")]
         [InlineData("float")]
+        [InlineData("decimal")]
         public async Task TestIncrementLiteralConversion(string typeName)
         {
             await TestInRegularAndScript1Async(
@@ -939,6 +1005,31 @@ $@"public class C
         a++;
     }}
 }}");
+        }
+
+        [WorkItem(38054, "https://github.com/dotnet/roslyn/issues/38054")]
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseCompoundAssignment)]
+        public async Task TestIncrementLoopVariable()
+        {
+            await TestInRegularAndScript1Async(
+@"public class C
+{
+    void M()
+    {
+        for (int i = 0; i < 10; i [||]= i + 1)
+        {
+        }
+    }
+}",
+@"public class C
+{
+    void M()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+        }
+    }
+}");
         }
     }
 }
