459351b6ee593c7cd3e731e80ae9e8d42b49f652
Tomas Matousek <tomat@microsoft.com>
2020-10-05 16:24:25 -0700

    Fix

--- src/Compilers/Core/Portable/InternalUtilities/ValueTaskFactory.cs
+++ src/Compilers/Core/Portable/InternalUtilities/ValueTaskFactory.cs
@@ -2,12 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace Roslyn.Utilities
 {
     /// <summary>
-    /// Implements <see cref="ValueTask{TResult}"/> members that are only available in .NET 5.
+    /// Implements <see cref="ValueTask"/> and <see cref="ValueTask{TResult}"/> static members that are only available in .NET 5.
     /// </summary>
     internal static class ValueTaskFactory
     {
