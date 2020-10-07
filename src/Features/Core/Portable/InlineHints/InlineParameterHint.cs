cbf2abafff816a2968b7e96f7390f50f9292f7ba
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-10-06 19:38:14 -0700

    NRT

--- src/Features/Core/Portable/InlineHints/InlineParameterHint.cs
+++ src/Features/Core/Portable/InlineHints/InlineParameterHint.cs
@@ -2,18 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace Microsoft.CodeAnalysis.InlineHints
 {
     internal readonly struct InlineParameterHint
     {
-        public readonly IParameterSymbol Parameter;
+        public readonly IParameterSymbol? Parameter;
         public readonly int Position;
         public readonly InlineParameterHintKind Kind;
 
         public InlineParameterHint(
-            IParameterSymbol parameter,
+            IParameterSymbol? parameter,
             int position,
             InlineParameterHintKind kind)
         {
