507e298a3250fe71168abe8b99f65069479f8440
AlekseyTs <AlekseyTs@users.noreply.github.com>
2020-10-07 10:02:42 -0700

    Change number for ERR_UnexpectedVarianceStaticMember

--- src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorCode.cs
@@ -1916,9 +1916,9 @@ namespace Microsoft.CodeAnalysis.CSharp
 
         ERR_InitCannotBeReadonly = 8903,
 
-        #endregion diagnostics introduced for C# 9.0
+        ERR_UnexpectedVarianceStaticMember = 8904,
 
-        ERR_UnexpectedVarianceStaticMember = 9100,
+        #endregion diagnostics introduced for C# 9.0
 
         // Note: you will need to re-generate compiler code after adding warnings (eng\generate-compiler-code.cmd)
     }
