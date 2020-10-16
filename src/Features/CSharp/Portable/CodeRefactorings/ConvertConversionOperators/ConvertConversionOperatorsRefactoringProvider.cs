0a72c899d29015a13595006c311d11cc16fbb9d3
Martin Strecker <MaStr11@users.noreply.github.com>
2020-10-16 14:36:27 +0100

    WIP: Implement C# CodeAction

--- src/Features/CSharp/Portable/CodeRefactorings/ConvertConversionOperators/ConvertConversionOperatorsRefactoringProvider.cs
+++ src/Features/CSharp/Portable/CodeRefactorings/ConvertConversionOperators/ConvertConversionOperatorsRefactoringProvider.cs
@@ -2,22 +2,62 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
+using System.Collections.Immutable;
 using System.Composition;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CodeRefactorings.ConvertConversionOperators;
+using Microsoft.CodeAnalysis.CSharp.CodeGeneration;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.ConvertConversionOperators
 {
     /// <inheritdoc/>
     [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = PredefinedCodeRefactoringProviderNames.ConvertConversionOperators), Shared]
-    internal partial class CSharpConvertConversionOperatorsRefactoringProvider : AbstractConvertConversionOperatorsRefactoringProvider<InvocationExpressionSyntax>
+    internal partial class CSharpConvertConversionOperatorsRefactoringProvider : AbstractConvertConversionOperatorsRefactoringProvider<CastExpressionSyntax, BinaryExpressionSyntax>
     {
         [ImportingConstructor]
         [SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
         public CSharpConvertConversionOperatorsRefactoringProvider()
         {
+        }
+
+        protected override ImmutableArray<BinaryExpressionSyntax> FilterAsExpressionCandidates(ImmutableArray<BinaryExpressionSyntax> asExpression)
+        {
+            return asExpression.WhereAsArray(binaryExpression => binaryExpression.IsKind(SyntaxKind.AsExpression));
+        }
+
+        protected override async Task<Document> ConvertFromAsToCastAsync(Document document, BinaryExpressionSyntax asExpression, CancellationToken cancellationToken)
+        {
+            var generator = new CSharpSyntaxGenerator();
+            var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var expression = asExpression.Left;
+            if (asExpression.Right is not TypeSyntax typeNode)
+            {
+                return document;
+            }
+
+            var castExpression = CastExpression(typeNode, expression.WithoutTrailingTrivia());
+            var newRoot = generator.ReplaceNode(root, asExpression, castExpression);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        protected override async Task<Document> ConvertFromCastToAsAsync(Document document, CastExpressionSyntax castExpression, CancellationToken cancellationToken)
+        {
+            var generator = new CSharpSyntaxGenerator();
+            var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var typeNode = castExpression.Type;
+            var expression = castExpression.Expression;
+
+            var asExpression = BinaryExpression(SyntaxKind.AsExpression, expression, typeNode);
+            var newRoot = generator.ReplaceNode(root, castExpression, asExpression);
+            return document.WithSyntaxRoot(newRoot);
         }
 
         protected override string GetTitle()
--- src/Features/Core/Portable/CodeRefactorings/ConvertConversionsOperators/AbstractConvertConversionOperatorsRefactoringProvider.cs
+++ src/Features/Core/Portable/CodeRefactorings/ConvertConversionsOperators/AbstractConvertConversionOperatorsRefactoringProvider.cs
@@ -2,13 +2,18 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using System;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.Shared.Extensions;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CodeRefactorings.ConvertConversionOperators
 {
@@ -22,24 +27,65 @@ namespace Microsoft.CodeAnalysis.CodeRefactorings.ConvertConversionOperators
     /// Or:
     ///     visa versa
     /// </summary>
-    internal abstract class AbstractConvertConversionOperatorsRefactoringProvider<TInvocationExpressionSyntax> : CodeRefactoringProvider
-        where TInvocationExpressionSyntax : SyntaxNode
+    internal abstract class AbstractConvertConversionOperatorsRefactoringProvider<TCastExpressionSyntax, TAsExpressionSyntax> : CodeRefactoringProvider
+        where TCastExpressionSyntax : SyntaxNode
+        where TAsExpressionSyntax : SyntaxNode
     {
         protected abstract string GetTitle();
         public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
         {
-            var (document, _, cancellationToken) = context;
 
-            var model = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-            var syntaxFacts = document.GetLanguageService<ISyntaxFactsService>();
+            var castExpressions = await context.GetRelevantNodesAsync<TCastExpressionSyntax>().ConfigureAwait(false);
+            var asExpressions = await context.GetRelevantNodesAsync<TAsExpressionSyntax>().ConfigureAwait(false);
 
-            var awaitable = await context.TryGetRelevantNodeAsync<TInvocationExpressionSyntax>().ConfigureAwait(false);
+            castExpressions = FilterCastExpressionCandidates(castExpressions);
+            asExpressions = FilterAsExpressionCandidates(asExpressions);
 
-            //context.RegisterRefactoring(
-            //    new MyCodeAction(
-            //        GetTitle(),
-            //        c => AddAwaitAsync(document, awaitable, withConfigureAwait: false, c)),
-            //    awaitable.Span);
+            if (castExpressions.IsEmpty && asExpressions.IsEmpty)
+            {
+                return;
+            }
+
+            var nodesToRegister = castExpressions.Cast<SyntaxNode>().Union(asExpressions).ToImmutableArray();
+
+            foreach (var node in nodesToRegister)
+            {
+                Func<CancellationToken, Task<Document>> createChangedDocument = node switch
+                {
+                    TCastExpressionSyntax castExpression => cancellationToken => ConvertFromCastToAsAsync(context.Document, castExpression, cancellationToken),
+                    TAsExpressionSyntax asExpression => cancellationToken => ConvertFromAsToCastAsync(context.Document, asExpression, cancellationToken),
+                    _ => throw new InvalidOperationException("Unsupported node type"),
+                };
+                context.RegisterRefactoring(
+                    new MyCodeAction(
+                        GetTitle(),
+                        createChangedDocument
+                    ), node.Span);
+            }
+        }
+
+        protected virtual ImmutableArray<TCastExpressionSyntax> FilterCastExpressionCandidates(ImmutableArray<TCastExpressionSyntax> castExpressions)
+            => castExpressions;
+
+        protected virtual ImmutableArray<TAsExpressionSyntax> FilterAsExpressionCandidates(ImmutableArray<TAsExpressionSyntax> asExpressions)
+            => asExpressions;
+
+        protected abstract Task<Document> ConvertFromCastToAsAsync(
+            Document document,
+            TCastExpressionSyntax castExpression,
+            CancellationToken cancellationToken);
+
+        protected abstract Task<Document> ConvertFromAsToCastAsync(
+            Document document,
+            TAsExpressionSyntax asExpression,
+            CancellationToken cancellationToken);
+
+        private class MyCodeAction : CodeAction.DocumentChangeAction
+        {
+            public MyCodeAction(string title, Func<CancellationToken, Task<Document>> createChangedDocument)
+                : base(title, createChangedDocument)
+            {
+            }
         }
     }
 }
