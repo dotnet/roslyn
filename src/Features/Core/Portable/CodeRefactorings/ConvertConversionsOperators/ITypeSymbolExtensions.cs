a9fc012f4d91b56ce3d3930cbfb36fdbb3b9b6f7
Martin Strecker <MaStr11@users.noreply.github.com>
2020-10-19 16:02:08 +0100

    Simplify IsReferenceTypeOrTypeParameter

--- src/Features/Core/Portable/CodeRefactorings/ConvertConversionsOperators/ITypeSymbolExtensions.cs
+++ src/Features/Core/Portable/CodeRefactorings/ConvertConversionsOperators/ITypeSymbolExtensions.cs
@@ -12,22 +12,6 @@ namespace Microsoft.CodeAnalysis.CodeRefactorings.ConvertConversionOperators
                 null => false,
                 { Kind: SymbolKind.ErrorType } => false,
                 { IsReferenceType: true } => true,
-                { IsValueType: true } => false,
-                ITypeParameterSymbol typeParameter => typeParameter.IsRefernceTypeParameter(),
-                _ => true,
-            };
-
-        public static bool IsRefernceTypeParameter(this ITypeParameterSymbol typeParameter)
-            => typeParameter switch
-            {
-                { HasValueTypeConstraint: true } => false,
-                { HasReferenceTypeConstraint: true } => true,
-                { ConstraintTypes: var constrainedTypes } => constrainedTypes.Any(t => t switch
-                    {
-                        { TypeKind: TypeKind.Class } => true,
-                        ITypeParameterSymbol nested => nested.IsRefernceTypeParameter(),
-                        _ => false,
-                    }),
                 _ => false,
             };
     }
