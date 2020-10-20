b8f72fa79844a2dc69be68a60952b1c8561943fd
Allison Chou <allichou@microsoft.com>
2020-10-19 21:04:59 -0700

    Add new files

---
+++ src/Features/Core/Portable/MetadataAsSource/IMetadataAsSourceFileLSPService.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+
+#nullable enable
+
+namespace Microsoft.CodeAnalysis.MetadataAsSource
+{
+    internal interface IMetadataAsSourceFileLSPService : IWorkspaceService
+    {
+        /// <summary>
+        /// Gets and opens the generated file for the given symbol.
+        /// </summary>
+        /// <remarks>
+        /// Currently used solely by LSP go-to-def. Silently opens the file, meaning that the host user may not even realize
+        /// the file has been opened as we intentionally do not make it visible.
+        /// </remarks>
+        Task<MetadataAsSourceFile?> GetAndOpenGeneratedFileAsync(ISymbol symbol, Project project, CancellationToken cancellationToken);
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/Workspace/VisualStudioMetadataAsSourceFileLSPServiceFactory.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.MetadataAsSource;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation
+{
+    [ExportWorkspaceServiceFactory(typeof(IMetadataAsSourceFileLSPService), ServiceLayer.Host), Shared]
+    internal class VisualStudioMetadataAsSourceFileLSPServiceFactory : IWorkspaceServiceFactory
+    {
+        private readonly IMetadataAsSourceFileLSPService _singleton;
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public VisualStudioMetadataAsSourceFileLSPServiceFactory(
+            SVsServiceProvider serviceProvider)
+        {
+            _singleton = new VisualStudioMetadataAsSourceFileLSPService(serviceProvider);
+        }
+
+        public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
+            => _singleton;
+    }
+}
