7cc2991323e304b0bead5d8c2bd8cf0763a2dce8
Tomáš Matoušek <tmat@users.noreply.github.com>
2020-10-22 16:39:05 -0700

    Add test covering documents with no active statement spans (#48799)
    * Add test covering documents with no active statement spans
    
    * Fix
    
    * Fix fire and forget task

--- src/EditorFeatures/Core/Implementation/EditAndContinue/ActiveStatementTrackingService.cs
+++ src/EditorFeatures/Core/Implementation/EditAndContinue/ActiveStatementTrackingService.cs
@@ -67,7 +67,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.EditAndContinue
             }
 
             // fire and forget on a background thread:
-            _ = newSession.TrackActiveSpansAsync();
+            _ = Task.Run(() => newSession.TrackActiveSpansAsync()).ReportNonFatalErrorAsync();
 
             TrackingChanged?.Invoke();
         }
@@ -161,19 +161,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.EditAndContinue
                 }
             }
 
-            internal Task TrackActiveSpansAsync()
-            {
-                try
-                {
-                    return Task.Run(() => TrackActiveSpansAsync(_cancellationSource.Token), _cancellationSource.Token);
-                }
-                catch (TaskCanceledException)
-                {
-                    return Task.CompletedTask;
-                }
-            }
-
-            private async Task TrackActiveSpansAsync(CancellationToken cancellationToken)
+            internal async Task TrackActiveSpansAsync()
             {
                 try
                 {
@@ -183,7 +171,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.EditAndContinue
                         return;
                     }
 
-                    var baseActiveStatementSpans = await _encService.GetBaseActiveStatementSpansAsync(openDocumentIds, cancellationToken).ConfigureAwait(false);
+                    var baseActiveStatementSpans = await _encService.GetBaseActiveStatementSpansAsync(openDocumentIds, _cancellationSource.Token).ConfigureAwait(false);
                     if (baseActiveStatementSpans.IsDefault)
                     {
                         // Edit session not in progress.
