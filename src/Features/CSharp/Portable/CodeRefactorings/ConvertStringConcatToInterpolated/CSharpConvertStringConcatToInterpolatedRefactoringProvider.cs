f473da3476823b5b743ad92d34786b047faa7b81
Martin Strecker <MaStr11@users.noreply.github.com>
2020-11-07 14:51:26 +0000

    Add some code comments and some small refactorings.

--- src/Features/CSharp/Portable/CodeRefactorings/ConvertStringConcatToInterpolated/CSharpConvertStringConcatToInterpolatedRefactoringProvider.cs
+++ src/Features/CSharp/Portable/CodeRefactorings/ConvertStringConcatToInterpolated/CSharpConvertStringConcatToInterpolatedRefactoringProvider.cs
@@ -46,13 +46,16 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.ConvertStringConcatToIn
             var binaryExpression = await context.TryGetRelevantNodeAsync<BinaryExpressionSyntax>().ConfigureAwait(false);
             if (binaryExpression?.IsKind(SyntaxKind.AddExpression) == true)
             {
+                // Walk up to get the full sequence of conjunctive concatenations
                 while (binaryExpression.Parent is BinaryExpressionSyntax { RawKind: (int)SyntaxKind.AddExpression } parent)
                 {
                     binaryExpression = parent;
                 }
+
                 var semanticModel = await context.Document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
                 if (IsStringConcatenation(semanticModel, binaryExpression, cancellationToken))
                 {
+                    // Collect the expression parts ("a" + "b" + expr) into an array { "a", "b", expr }
                     using var _ = ArrayBuilder<ExpressionSyntax>.GetInstance(out var builder);
                     WalkBinaryExpression(semanticModel, builder, binaryExpression, cancellationToken);
                     var concatParts = builder.ToImmutable();
@@ -106,20 +109,26 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.ConvertStringConcatToIn
             var interpolated = InterpolatedStringExpression(
                 Token(SyntaxKind.InterpolatedStringStartToken),
                 List(interpolatedStringContent));
+            // expressions may have superfluous parenthesis after the conversion to an interpolated string:
+            // "a" + (1 + 1) -> $"a{(1 + 1)}"
+            // if there is any (InterpolationSyntax.Expression is ParenthesizedExpressionSyntax) we need to
+            // recompile and check via parenthesized.CanRemoveParentheses(semanticModel) if it can be removed.
             var anyPotentiallyRemovableParenthesis = interpolated.Contents.Any(IsParenthesizedInterpolation);
             var annotation = new SyntaxAnnotation("InterpolatedStringExpressionWithPotentiallyRemovableParanthesisKind");
             if (anyPotentiallyRemovableParenthesis)
             {
+                // annotate "interpolated" because we need to find it after the changes are applied
                 interpolated = interpolated.WithAdditionalAnnotations(annotation);
             }
-            editor.ReplaceNode(binaryExpression, interpolated.WithAdditionalAnnotations(annotation));
+            editor.ReplaceNode(binaryExpression, interpolated);
 
             if (anyPotentiallyRemovableParenthesis)
             {
+                // Take the changed document and go through interpolatedNode.Contents to check if any parenthesis can be removed.
                 var newDocument = document.WithSyntaxRoot(editor.GetChangedRoot());
                 var newRoot = await newDocument.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
                 var interpolatedNode = (InterpolatedStringExpressionSyntax)newRoot.GetAnnotatedNodes(annotation).Single();
-                var semanticModel = await newDocument.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+                var semanticModel = await newDocument.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
                 editor = new SyntaxEditor(newRoot, CSharpSyntaxGenerator.Instance);
                 foreach (var content in interpolatedNode.Contents.Where(IsParenthesizedInterpolation).Cast<InterpolationSyntax>())
                 {
@@ -147,19 +156,25 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.ConvertStringConcatToIn
                     case var expression when IsStringLiteralExpression(expression, out var literal):
                         if (result.LastOrDefault() is InterpolatedStringTextSyntax text)
                         {
+                            // Merge the string literal with the last InterpolatedStringTextSyntax
                             var newText = text.TextToken.ValueText + literal.Token.ValueText;
                             result[^1] = InterpolatedStringText(InterpolatedStringTextToken(newText));
                         }
                         else
                         {
-                            result.Add(InterpolatedStringText(StringLiteralTokenToInterpolatedStringTextToken(literal.Token)));
+                            result.Add(InterpolatedStringText(InterpolatedStringTextToken(literal.Token.ValueText)));
                         }
                         break;
                     case InterpolatedStringExpressionSyntax interpolated when !interpolated.StringStartToken.IsKind(SyntaxKind.InterpolatedVerbatimStringStartToken):
+                        // One of the concatParts is itself an interpolated string at should be in-lined:
+                        // "a" + $"b{expr}c" -> $"ab{expr}c"
+                        // Here "interpolated" is $"b{expr}c"
                         foreach (var contentPart in interpolated.Contents)
                         {
                             if (contentPart is InterpolatedStringTextSyntax textFromOther && result.LastOrDefault() is InterpolatedStringTextSyntax previousText)
                             {
+                                // The last added content was a text and the first part in interpolated is a text. We can merge these two.
+                                // "a" + $"b{expr}" -> "a" and "b" can be merged
                                 var newText = previousText.TextToken.ValueText + textFromOther.TextToken.ValueText;
                                 result[^1] = InterpolatedStringText(InterpolatedStringTextToken(newText));
                             }
@@ -170,6 +185,8 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.ConvertStringConcatToIn
                         }
                         break;
                     default:
+                        // "part" might be parenthesized here. Superfluous parenthesizes are removed later.
+                        // "+" usually has whitespace before and after. We remove that and other trivia.
                         result.Add(Interpolation(part.WithoutTrivia()));
                         break;
                 }
@@ -180,9 +197,6 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.ConvertStringConcatToIn
 
         private static SyntaxToken InterpolatedStringTextToken(string text)
             => Token(TriviaList(), SyntaxKind.InterpolatedStringTextToken, text, text, TriviaList());
-
-        private static SyntaxToken StringLiteralTokenToInterpolatedStringTextToken(SyntaxToken stringLiteralToken)
-            => InterpolatedStringTextToken(stringLiteralToken.ValueText);
 
         private static bool IsStringLiteralExpression(ExpressionSyntax? expression, [NotNullWhen(true)] out LiteralExpressionSyntax? literal)
         {
