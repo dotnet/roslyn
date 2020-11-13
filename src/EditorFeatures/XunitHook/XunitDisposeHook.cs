b69913893a02edf022db10d00b2727af6b8b8964
Sam Harwell <Sam.Harwell@microsoft.com>
2020-11-13 10:59:20 -0800

    Harden, document, cross-link XunitDisposeHook

--- src/EditorFeatures/XunitHook/XunitDisposeHook.cs
+++ src/EditorFeatures/XunitHook/XunitDisposeHook.cs
@@ -25,22 +25,31 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
                 if (appDomainManagerType is null)
                     continue;
 
+                // AppDomainManager_AppDomain.Dispose() calls AppDomain.Unload(), which is unfortunately not reliable
+                // when the test creates STA COM objects. Since this call to Unload() only occurs at the end of testing
+                // (immediately before the process is going to close anyway), we can simply hot-patch the executable
+                // code in Dispose() to return without taking any action.
+                //
+                // This is a workaround for https://github.com/xunit/xunit/issues/2097. The fix in
+                // https://github.com/xunit/xunit/pull/2192 was not viable because xunit v2 is no longer shipping
+                // updates. Once xunit v3 is available, it will no longer be necessary.
                 var method = appDomainManagerType.GetMethod("Dispose");
                 RuntimeHelpers.PrepareMethod(method.MethodHandle);
                 var functionPointer = method.MethodHandle.GetFunctionPointer();
-                if (IntPtr.Size == 4)
-                {
-                    // Overwrite the compiled method to just return
 
-                    // ret
-                    Marshal.WriteByte(functionPointer, 0xC3);
-                }
-                else
+                switch (RuntimeInformation.ProcessArchitecture)
                 {
-                    // Overwrite the compiled method to just return
+                    case Architecture.X86:
+                    case Architecture.X64:
+                        // 😱 Overwrite the compiled method to just return.
+                        // Note that the same sequence works for x86 and x64.
 
-                    // ret
-                    Marshal.WriteByte(functionPointer, 0xC3);
+                        // ret
+                        Marshal.WriteByte(functionPointer, 0xC3);
+                        break;
+
+                    default:
+                        throw new NotSupportedException();
                 }
             }
         }
