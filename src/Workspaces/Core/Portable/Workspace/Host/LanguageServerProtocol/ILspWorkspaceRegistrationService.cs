d3d800bf9f4560e1e2f6d25fb0538148b3762bab
David Wengier <david.wengier@microsoft.com>
2020-11-17 21:46:26 +1100

    Create workspace regsitration service

---
+++ src/Workspaces/Core/Portable/Workspace/Host/LanguageServerProtocol/ILspWorkspaceRegistrationService.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Immutable;
+
+namespace Microsoft.CodeAnalysis.Host.LanguageServerProtocol
+{
+    public interface ILspWorkspaceRegistrationService
+    {
+        ImmutableArray<Workspace> GetAllRegistrations();
+
+        void Register(Workspace workspace);
+
+        void Unregister(Workspace workspace);
+    }
+}
