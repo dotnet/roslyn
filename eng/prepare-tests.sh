cfce17c9948a940342943ec3a41e28e45b44e583
Rikki Gibson <rikkigibson@gmail.com>
2020-11-18 10:14:06 -0800

    WIP on removing checkout step in unix tests

---
+++ eng/prepare-tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Copyright (c) .NET Foundation and contributors. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
+
+# Stop script if subcommand fails
+set -e
+
+source="${BASH_SOURCE[0]}"
+
+# resolve $source until the file is no longer a symlink
+while [[ -h "$source" ]]; do
+  scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
+  source="$(readlink "$source")"
+  # if $source was a relative symlink, we need to resolve it relative to the path where the
+  # symlink file was located
+  [[ $source != /* ]] && source="$scriptroot/$source"
+done
+scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
+
+# Import Arcade functions
+. "$scriptroot/common/tools.sh"
+
+InitializeDotNetCli $true
+dotnet run --project src/Tools/PrepareTests/PrepareTests.csproj $repo_root $repo_root/artifacts/testPayload
