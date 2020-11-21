57751921bd90bfbafd8a2590d21f8d62c08ad61d
Jonathon Marolf <jmarolf@microsoft.com>
2020-11-20 19:48:07 -0800

    use regex to normalize line endings

--- src/Test.Utilities/StringExtensions.cs
+++ src/Test.Utilities/StringExtensions.cs
@@ -1,17 +1,12 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Text.RegularExpressions;
+
 namespace Test.Utilities
 {
     public static class StringExtensions
     {
         public static string NormalizeLineEndings(this string input)
-        {
-            if (input.Contains("\n") && !input.Contains("\r\n"))
-            {
-                input = input.Replace("\n", "\r\n");
-            }
-
-            return input;
-        }
+            => Regex.Replace(input, "(?<!\r)\n", "\r\n");
     }
 }
