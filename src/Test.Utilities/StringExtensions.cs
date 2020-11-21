bf7a7bc08e5b5da88ff1eab93ea68de789003a41
Jonathon Marolf <jmarolf@microsoft.com>
2020-11-20 17:56:47 -0800

    handle mixed newlines

--- src/Test.Utilities/StringExtensions.cs
+++ src/Test.Utilities/StringExtensions.cs
@@ -1,14 +1,17 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-using System;
-
 namespace Test.Utilities
 {
     public static class StringExtensions
     {
-        public static string NormalizeLineEndings(this string text)
+        public static string NormalizeLineEndings(this string input)
         {
-            return text.Replace(Environment.NewLine, "\r\n");
+            if (input.Contains("\n") && !input.Contains("\r\n"))
+            {
+                input = input.Replace("\n", "\r\n");
+            }
+
+            return input;
         }
     }
 }
