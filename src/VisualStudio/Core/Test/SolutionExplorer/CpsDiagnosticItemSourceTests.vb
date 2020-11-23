dotnet/roslyn

commit 7fa2c964f8433b928c5ff966427ce3798724cdd2

Null annotate most of the SolutionExplorer extension code
Easy to do while I'm here.

src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/BaseDiagnosticItemSource.cs
@@ -2,12 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -15,6 +14,7 @@ using Microsoft.CodeAnalysis.Internal.Log;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Shell;
+using Roslyn.Utilities;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplorer
 {
@@ -24,9 +24,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
 
         private readonly IDiagnosticAnalyzerService _diagnosticAnalyzerService;
 
-        private BulkObservableCollection<BaseDiagnosticItem> _diagnosticItems;
+        private BulkObservableCollection<BaseDiagnosticItem>? _diagnosticItems;
         private ReportDiagnostic _generalDiagnosticOption;
-        private ImmutableDictionary<string, ReportDiagnostic> _specificDiagnosticOptions;
+        private ImmutableDictionary<string, ReportDiagnostic>? _specificDiagnosticOptions;
         private AnalyzerConfigOptionsResult? _analyzerConfigOptions;
 
         public BaseDiagnosticItemSource(Workspace workspace, ProjectId projectId, IAnalyzersCommandHandler commandHandler, IDiagnosticAnalyzerService diagnosticAnalyzerService)
@@ -41,11 +41,12 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
         public ProjectId ProjectId { get; }
         protected IAnalyzersCommandHandler CommandHandler { get; }
 
-        public abstract AnalyzerReference AnalyzerReference { get; }
+        public abstract AnalyzerReference? AnalyzerReference { get; }
         protected abstract BaseDiagnosticItem CreateItem(DiagnosticDescriptor diagnostic, ReportDiagnostic effectiveSeverity, string language);
 
         public abstract object SourceItem { get; }
 
+        [MemberNotNullWhen(true, nameof(AnalyzerReference))]
         public bool HasItems
         {
             get
@@ -61,6 +62,12 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
                 }
 
                 var project = Workspace.CurrentSolution.GetProject(ProjectId);
+
+                if (project == null)
+                {
+                    return false;
+                }
+
                 return AnalyzerReference.GetAnalyzers(project.Language).Length > 0;
             }
         }
@@ -71,9 +78,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             {
                 if (_diagnosticItems == null)
                 {
-                    var project = Workspace.CurrentSolution.GetProject(ProjectId);
-                    _generalDiagnosticOption = project.CompilationOptions.GeneralDiagnosticOption;
-                    _specificDiagnosticOptions = project.CompilationOptions.SpecificDiagnosticOptions;
+                    var project = Workspace.CurrentSolution.GetRequiredProject(ProjectId);
+                    _generalDiagnosticOption = project.CompilationOptions!.GeneralDiagnosticOption;
+                    _specificDiagnosticOptions = project.CompilationOptions!.SpecificDiagnosticOptions;
                     _analyzerConfigOptions = project.GetAnalyzerConfigOptions();
 
                     _diagnosticItems = new BulkObservableCollection<BaseDiagnosticItem>();
@@ -100,6 +107,8 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             // descriptions or messages, and it would be strange if the node's name changed from one run of
             // VS to another. So we group the diagnostics by ID, sort them within a group, and take the first
             // one.
+
+            Contract.ThrowIfFalse(HasItems);
 
             return AnalyzerReference.GetAnalyzers(language)
                 .SelectMany(a => _diagnosticAnalyzerService.AnalyzerInfoCache.GetDiagnosticDescriptors(a))
@@ -150,9 +159,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             // Local functions.
             void OnProjectConfigurationChanged()
             {
-                var project = e.NewSolution.GetProject(ProjectId);
-                var newGeneralDiagnosticOption = project.CompilationOptions.GeneralDiagnosticOption;
-                var newSpecificDiagnosticOptions = project.CompilationOptions.SpecificDiagnosticOptions;
+                var project = e.NewSolution.GetRequiredProject(ProjectId);
+                var newGeneralDiagnosticOption = project.CompilationOptions!.GeneralDiagnosticOption;
+                var newSpecificDiagnosticOptions = project.CompilationOptions!.SpecificDiagnosticOptions;
                 var newAnalyzerConfigOptions = project.GetAnalyzerConfigOptions();
 
                 if (newGeneralDiagnosticOption != _generalDiagnosticOption ||
@@ -163,6 +172,8 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
                     _generalDiagnosticOption = newGeneralDiagnosticOption;
                     _specificDiagnosticOptions = newSpecificDiagnosticOptions;
                     _analyzerConfigOptions = newAnalyzerConfigOptions;
+
+                    Contract.ThrowIfNull(_diagnosticItems, "We only subscribe to events after we create the items, so this should not be null.");
 
                     foreach (var item in _diagnosticItems)
                     {

src/VisualStudio/Core/Impl/SolutionExplorer/AnalyzerItemTracker.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
@@ -23,10 +21,10 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
     internal class AnalyzerItemsTracker : IVsSelectionEvents
     {
         private readonly IServiceProvider _serviceProvider;
-        private IVsMonitorSelection _vsMonitorSelection = null;
+        private IVsMonitorSelection? _vsMonitorSelection = null;
         private uint _selectionEventsCookie = 0;
 
-        public event EventHandler SelectedHierarchyItemChanged;
+        public event EventHandler? SelectedHierarchyItemChanged;
 
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
@@ -56,9 +54,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             }
         }
 
-        public IVsHierarchy SelectedHierarchy { get; private set; }
+        public IVsHierarchy? SelectedHierarchy { get; private set; }
         public uint SelectedItemId { get; private set; } = VSConstants.VSITEMID_NIL;
-        public AnalyzersFolderItem SelectedFolder { get; private set; }
+        public AnalyzersFolderItem? SelectedFolder { get; private set; }
         public ImmutableArray<AnalyzerItem> SelectedAnalyzerItems { get; private set; } = ImmutableArray<AnalyzerItem>.Empty;
         public ImmutableArray<BaseDiagnosticItem> SelectedDiagnosticItems { get; private set; } = ImmutableArray<BaseDiagnosticItem>.Empty;
 
@@ -114,7 +112,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             return VSConstants.S_OK;
         }
 
-        private object[] GetSelectedObjects(ISelectionContainer selectionContainer)
+        private object[] GetSelectedObjects(ISelectionContainer? selectionContainer)
         {
             if (selectionContainer == null)
             {
@@ -135,7 +133,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             return selectedObjects;
         }
 
-        private IVsMonitorSelection GetMonitorSelection()
+        private IVsMonitorSelection? GetMonitorSelection()
         {
             if (_vsMonitorSelection == null)
             {

src/VisualStudio/Core/Impl/SolutionExplorer/AnalyzerNodeSetup.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Design;

src/VisualStudio/Core/Impl/SolutionExplorer/AnalyzerReferenceManager.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.ComponentModel.Composition;
 using System.Diagnostics;
@@ -11,6 +9,7 @@ using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using Roslyn.Utilities;
 using VSLangProj140;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplorer
@@ -23,17 +22,17 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
     internal class AnalyzerReferenceManager : IVsReferenceManagerUser
     {
         private readonly IServiceProvider _serviceProvider;
-        private IVsReferenceManager _referenceManager;
-
-        [Import]
-        private readonly AnalyzerItemsTracker _tracker = null;
+        private IVsReferenceManager? _referenceManager;
+        private readonly AnalyzerItemsTracker _tracker;
 
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
         public AnalyzerReferenceManager(
-            [Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+            [Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider,
+            AnalyzerItemsTracker analyzerItemsTracker)
         {
             _serviceProvider = serviceProvider;
+            _tracker = analyzerItemsTracker;
         }
 
         /// <summary>
@@ -62,7 +61,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
 
             // The items selected in Solution Explorer should correspond to exactly one
             // IVsHierarchy, otherwise we shouldn't have even tried to show the dialog.
-            Debug.Assert(_tracker.SelectedHierarchy != null);
+            Contract.ThrowIfNull(_tracker.SelectedHierarchy);
             if (_tracker.SelectedHierarchy.TryGetProject(out var project))
             {
 
@@ -94,7 +93,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
         public Array GetProviderContexts()
         {
             // Return just the File provider context so that just the browse tab shows up.
-            var context = GetReferenceManager().CreateProviderContext(VSConstants.FileReferenceProvider_Guid) as IVsFileReferenceProviderContext;
+            var context = (IVsFileReferenceProviderContext)GetReferenceManager().CreateProviderContext(VSConstants.FileReferenceProvider_Guid);
             context.BrowseFilter = string.Format("{0} (*.dll)\0*.dll\0", SolutionExplorerShim.Analyzer_Files);
             return new[] { context };
         }
@@ -104,6 +103,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             if (_referenceManager == null)
             {
                 _referenceManager = _serviceProvider.GetService(typeof(SVsReferenceManager)) as IVsReferenceManager;
+                Assumes.Present(_referenceManager);
             }
 
             return _referenceManager;

src/VisualStudio/Core/Impl/SolutionExplorer/BaseItem.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -44,111 +42,30 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             _name = name;
         }
 
-        public IEnumerable<string> Children
-        {
-            get
-            {
-                return SpecializedCollections.EmptyEnumerable<string>();
-            }
-        }
+        public IEnumerable<string> Children => SpecializedCollections.EmptyEnumerable<string>();
 
-        public bool IsExpandable
-        {
-            get
-            {
-                return true;
-            }
-        }
+        public bool IsExpandable => true;
 
-        public FontStyle FontStyle
-        {
-            get { return FontStyles.Normal; }
-        }
+        public FontStyle FontStyle => FontStyles.Normal;
+        public FontWeight FontWeight => FontWeights.Normal;
 
-        public FontWeight FontWeight
-        {
-            get { return FontWeights.Normal; }
-        }
+        public virtual ImageSource? Icon => null;
+        public virtual ImageMoniker IconMoniker => default;
+        public virtual ImageSource? ExpandedIcon => null;
+        public virtual ImageMoniker ExpandedIconMoniker => default;
 
-        public virtual ImageSource Icon
-        {
-            get { return null; }
-        }
-
-        public virtual ImageMoniker IconMoniker
-        {
-            get { return default; }
-        }
-
-        public virtual ImageSource ExpandedIcon
-        {
-            get { return null; }
-        }
-
-        public virtual ImageMoniker ExpandedIconMoniker
-        {
-            get { return default; }
-        }
-
-        public bool AllowIconTheming
-        {
-            get { return true; }
-        }
-
-        public bool AllowExpandedIconTheming
-        {
-            get { return true; }
-        }
-
-        public bool IsCut
-        {
-            get { return false; }
-        }
-
-        public ImageSource OverlayIcon
-        {
-            get { return null; }
-        }
-
-        public virtual ImageMoniker OverlayIconMoniker
-        {
-            get { return default; }
-        }
-
-        public ImageSource StateIcon
-        {
-            get { return null; }
-        }
-
-        public virtual ImageMoniker StateIconMoniker
-        {
-            get { return default; }
-        }
-
-        public string StateToolTipText
-        {
-            get { return null; }
-        }
-
-        public override string ToString()
-        {
-            return Text;
-        }
-
-        public string Text
-        {
-            get { return _name; }
-        }
-
-        public object ToolTipContent
-        {
-            get { return null; }
-        }
-
-        public string ToolTipText
-        {
-            get { return _name; }
-        }
+        public bool AllowIconTheming => true;
+        public bool AllowExpandedIconTheming => true;
+        public bool IsCut => false;
+        public ImageSource? OverlayIcon => null;
+        public virtual ImageMoniker OverlayIconMoniker => default;
+        public ImageSource? StateIcon => null;
+        public virtual ImageMoniker StateIconMoniker => default;
+        public string? StateToolTipText => null;
+        public override string ToString() => Text;
+        public string Text => _name;
+        public object? ToolTipContent => null;
+        public string ToolTipText => _name;
 
         private static readonly HashSet<Type> s_supportedPatterns = new HashSet<Type>()
         {
@@ -163,7 +80,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             typeof(ISupportDisposalNotification)
         };
 
-        public TPattern GetPattern<TPattern>() where TPattern : class
+        public TPattern? GetPattern<TPattern>() where TPattern : class
         {
             if (!IsDisposed)
             {
@@ -188,49 +105,21 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             return null;
         }
 
-        public bool CanPreview
-        {
-            get { return false; }
-        }
+        public bool CanPreview => false;
 
-        public virtual IInvocationController InvocationController
-        {
-            get
-            {
-                return null;
-            }
-        }
+        public virtual IInvocationController? InvocationController => null;
 
-        public virtual IContextMenuController ContextMenuController
-        {
-            get
-            {
-                return null;
-            }
-        }
+        public virtual IContextMenuController? ContextMenuController => null;
 
-        public IDragDropSourceController DragDropSourceController
-        {
-            get
-            {
-                return null;
-            }
-        }
+        public IDragDropSourceController? DragDropSourceController => null;
 
         public virtual object GetBrowseObject()
         {
             return this;
         }
 
-        public bool IsDisposed
-        {
-            get { return false; }
-        }
-
-        public int Priority
-        {
-            get { return 0; }
-        }
+        public bool IsDisposed => false;
+        public int Priority => 0;
 
         public int CompareTo(object obj)
         {

src/VisualStudio/Core/Impl/SolutionExplorer/BrowseObjectAttributes.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.ComponentModel;
 using System.Globalization;

src/VisualStudio/Core/Impl/SolutionExplorer/ContextMenuController.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.Collections.Generic;
 using System.Linq;

src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/BaseDiagnosticItem.BrowseObject.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.ComponentModel;
 using System.Globalization;
@@ -51,7 +49,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             }
 
             [BrowseObjectDisplayName(nameof(SolutionExplorerShim.Help_link))]
-            public string HelpLink
+            public string? HelpLink
             {
                 get
                 {

commit 2fffa6de21ef9600eb3e0e5f9959817155388db9

Fix the ability to expand the list of analyzers in a reference
This was broken by 7fa2c964f8433b928c5ff966427ce3798724cdd2, where I
asserted that GetDiagnosticItems was only called when we already knew
we would have items. This was broken since right before we called the
method the collection would get created, which changed the answer
it would return. A small refactoring ensures we create the list and
fill it in at once. This also makes it clear we aren't passing a
non-trivial LINQ query across method boundaries anyways in a way that
might result in multiple enumerations.

Fixes https://github.com/dotnet/roslyn/issues/49524

src/EditorFeatures/TestUtilities/Diagnostics/TestAnalyzerReferenceByLanguage.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -14,10 +12,13 @@ namespace Microsoft.CodeAnalysis.Diagnostics
     {
         private readonly IReadOnlyDictionary<string, ImmutableArray<DiagnosticAnalyzer>> _analyzersMap;
 
-        public TestAnalyzerReferenceByLanguage(IReadOnlyDictionary<string, ImmutableArray<DiagnosticAnalyzer>> analyzersMap)
-            => _analyzersMap = analyzersMap;
+        public TestAnalyzerReferenceByLanguage(IReadOnlyDictionary<string, ImmutableArray<DiagnosticAnalyzer>> analyzersMap, string? fullPath = null)
+        {
+            _analyzersMap = analyzersMap;
+            FullPath = fullPath;
+        }
 
-        public override string FullPath => null;
+        public override string? FullPath { get; }
         public override string Display => nameof(TestAnalyzerReferenceByLanguage);
         public override object Id => Display;
 

src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/BaseDiagnosticItemSource.cs
@@ -83,8 +83,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
                     _specificDiagnosticOptions = project.CompilationOptions!.SpecificDiagnosticOptions;
                     _analyzerConfigOptions = project.GetAnalyzerConfigOptions();
 
-                    _diagnosticItems = new BulkObservableCollection<BaseDiagnosticItem>();
-                    _diagnosticItems.AddRange(GetDiagnosticItems(project.Language, project.CompilationOptions, _analyzerConfigOptions));
+                    _diagnosticItems = CreateDiagnosticItems(project.Language, project.CompilationOptions, _analyzerConfigOptions);
 
                     Workspace.WorkspaceChanged += OnWorkspaceChangedLookForOptionsChanges;
                 }
@@ -97,7 +96,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             }
         }
 
-        private IEnumerable<BaseDiagnosticItem> GetDiagnosticItems(string language, CompilationOptions options, AnalyzerConfigOptionsResult? analyzerConfigOptions)
+        private BulkObservableCollection<BaseDiagnosticItem> CreateDiagnosticItems(string language, CompilationOptions options, AnalyzerConfigOptionsResult? analyzerConfigOptions)
         {
             // Within an analyzer assembly, an individual analyzer may report multiple different diagnostics
             // with the same ID. Or, multiple analyzers may report diagnostics with the same ID. Or a
@@ -110,7 +109,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
 
             Contract.ThrowIfFalse(HasItems);
 
-            return AnalyzerReference.GetAnalyzers(language)
+            var collection = new BulkObservableCollection<BaseDiagnosticItem>();
+            collection.AddRange(
+                AnalyzerReference.GetAnalyzers(language)
                 .SelectMany(a => _diagnosticAnalyzerService.AnalyzerInfoCache.GetDiagnosticDescriptors(a))
                 .GroupBy(d => d.Id)
                 .OrderBy(g => g.Key, StringComparer.CurrentCulture)
@@ -119,7 +120,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
                     var selectedDiagnostic = g.OrderBy(d => d, s_comparer).First();
                     var effectiveSeverity = selectedDiagnostic.GetEffectiveSeverity(options, analyzerConfigOptions);
                     return CreateItem(selectedDiagnostic, effectiveSeverity, language);
-                });
+                }));
+
+            return collection;
         }
 
         private void OnWorkspaceChangedLookForOptionsChanges(object sender, WorkspaceChangeEventArgs e)

src/VisualStudio/Core/Test/SolutionExplorer/CpsDiagnosticItemSourceTests.vb
@@ -1,0 +1,51 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports System.Collections.Immutable
+Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Diagnostics
+Imports Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces
+Imports Microsoft.CodeAnalysis.Test.Utilities
+Imports Microsoft.Internal.VisualStudio.PlatformUI
+Imports Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplorer
+Imports Microsoft.VisualStudio.LanguageServices.UnitTests.ProjectSystemShim.Framework
+Imports Microsoft.VisualStudio.Shell
+
+Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.SolutionExplorer
+    <UseExportProvider>
+    Public Class CpsDiagnosticItemSourceTests
+        <Fact, Trait(Traits.Feature, Traits.Features.Diagnostics)>
+        Public Sub AnalyzerHasDiagnostics()
+            Dim workspaceXml =
+                <Workspace>
+                    <Project Language="Visual Basic" CommonReferences="true">
+                    </Project>
+                </Workspace>
+
+            Using workspace = TestWorkspace.Create(workspaceXml)
+                Dim project = workspace.Projects.Single()
+
+                Dim analyzers = New Dictionary(Of String, ImmutableArray(Of DiagnosticAnalyzer))
+
+                ' The choice here of this analyzer to test with is arbitray -- there's nothing special about this
+                ' analyzer versus any other one.
+                analyzers.Add(LanguageNames.VisualBasic, ImmutableArray.Create(Of DiagnosticAnalyzer)(New Microsoft.CodeAnalysis.VisualBasic.UseAutoProperty.VisualBasicUseAutoPropertyAnalyzer()))
+
+                Const analyzerPath = "C:\Analyzer.dll"
+                workspace.OnAnalyzerReferenceAdded(project.Id, New TestAnalyzerReferenceByLanguage(analyzers, analyzerPath))
+
+                Dim source As IAttachedCollectionSource = New CpsDiagnosticItemSource(
+                    workspace,
+                    project.FilePath,
+                    project.Id,
+                    New MockHierarchyItem() With {.CanonicalName = "\net472\analyzerdependency\" + analyzerPath},
+                    New FakeAnalyzersCommandHandler, workspace.GetService(Of IDiagnosticAnalyzerService))
+
+                Assert.True(source.HasItems)
+                Dim diagnostic = Assert.IsAssignableFrom(Of ITreeDisplayItem)(Assert.Single(source.Items))
+                Assert.Contains(IDEDiagnosticIds.UseAutoPropertyDiagnosticId, diagnostic.Text)
+            End Using
+        End Sub
+    End Class
+End Namespace