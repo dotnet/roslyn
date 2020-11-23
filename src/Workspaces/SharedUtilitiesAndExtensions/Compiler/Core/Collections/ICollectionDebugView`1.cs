95902b01fda11b2710e8b26c9b0b65e40c2c4c52
Sam Harwell <Sam.Harwell@microsoft.com>
2020-11-23 11:59:21 -0800

    Add List<T> and supporting files
    These files are based on dotnet/runtime@cf258a14 (v5.0.0-rtm.20519.4).

---
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Collections/ICollectionDebugView`1.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+
+namespace System.Collections.Generic
+{
+    internal sealed class ICollectionDebugView<T>
+    {
+        private readonly ICollection<T> _collection;
+
+        public ICollectionDebugView(ICollection<T> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            _collection = collection;
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public T[] Items
+        {
+            get
+            {
+                T[] items = new T[_collection.Count];
+                _collection.CopyTo(items, 0);
+                return items;
+            }
+        }
+    }
+}
