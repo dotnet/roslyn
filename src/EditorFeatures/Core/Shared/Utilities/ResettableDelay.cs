f221fb4a734528047196d866f9a1a7c960bdd181
Jason Malinowski <jason.malinowski@microsoft.com>
2020-11-24 17:01:09 -0800

    Add support for cancellation to ResettableDelay

--- src/EditorFeatures/Core/Shared/Utilities/ResettableDelay.cs
+++ src/EditorFeatures/Core/Shared/Utilities/ResettableDelay.cs
@@ -25,7 +25,7 @@ namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities
         /// delay can be reset multiple times.
         /// </summary>
         /// <param name="delayInMilliseconds">The time to delay before completing the task</param>
-        public ResettableDelay(int delayInMilliseconds, IExpeditableDelaySource expeditableDelaySource)
+        public ResettableDelay(int delayInMilliseconds, IExpeditableDelaySource expeditableDelaySource, CancellationToken cancellationToken = default)
         {
             Contract.ThrowIfFalse(delayInMilliseconds >= 50, "Perf, only use delays >= 50ms");
             _delayInMilliseconds = delayInMilliseconds;
@@ -33,7 +33,7 @@ namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities
             _taskCompletionSource = new TaskCompletionSource<object?>();
             Reset();
 
-            _ = StartTimerAsync(expeditableDelaySource);
+            _ = StartTimerAsync(expeditableDelaySource, cancellationToken);
         }
 
         private ResettableDelay()
@@ -55,20 +55,28 @@ namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities
             _lastSetTime = Environment.TickCount;
         }
 
-        private async Task StartTimerAsync(IExpeditableDelaySource expeditableDelaySource)
+        private async Task StartTimerAsync(IExpeditableDelaySource expeditableDelaySource, CancellationToken cancellationToken)
         {
-            do
+            try
             {
-                // Keep delaying until at least delayInMilliseconds has elapsed since lastSetTime
-                if (!await expeditableDelaySource.Delay(TimeSpan.FromMilliseconds(_delayInMilliseconds), CancellationToken.None).ConfigureAwait(false))
+                do
                 {
-                    // The operation is being expedited.
-                    break;
+                    // Keep delaying until at least delayInMilliseconds has elapsed since lastSetTime
+                    if (!await expeditableDelaySource.Delay(TimeSpan.FromMilliseconds(_delayInMilliseconds), cancellationToken).ConfigureAwait(false))
+                    {
+                        // The operation is being expedited.
+                        break;
+                    }
                 }
-            }
-            while (Environment.TickCount - _lastSetTime < _delayInMilliseconds);
+                while (Environment.TickCount - _lastSetTime < _delayInMilliseconds);
 
-            _taskCompletionSource.SetResult(null);
+                _taskCompletionSource.SetResult(null);
+            }
+            catch (OperationCanceledException)
+            {
+                // Calling the "Try" variant because that's the only one that accepts the token to associate with the task
+                _taskCompletionSource.TrySetCanceled(cancellationToken);
+            }
         }
     }
 }
