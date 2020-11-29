f0d0ca0ae724957325e9ca1af70d4b6625225d56
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2020-11-28 19:37:14 -0800

    NRT

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Utilities/NoOpProgressTracker.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Utilities/NoOpProgressTracker.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace Microsoft.CodeAnalysis.Shared.Utilities
 {
     internal class NoOpProgressTracker : IProgressTracker
@@ -14,7 +12,7 @@ namespace Microsoft.CodeAnalysis.Shared.Utilities
         {
         }
 
-        public string Description { get => null; set { } }
+        public string? Description { get => null; set { } }
 
         public int CompletedItems => 0;
 
