fae2b8afbc299f33a517098aba3a3c991d0717bd
Rikki Gibson <rigibson@microsoft.com>
2020-12-07 12:31:33 -0800

    fix

--- src/Tools/Source/RunTests/TestRunner.cs
+++ src/Tools/Source/RunTests/TestRunner.cs
@@ -116,7 +116,7 @@ namespace RunTests
                 var rehydrateCommand = @".\rehydrate.cmd";
                 var workItem = @"
         <HelixWorkItem Include=""" + assemblyInfo.DisplayName + @""">
-            <PayloadDirectory>$(RepoRoot)" + Path.GetDirectoryName(assemblyInfo.AssemblyPath) + @"</PayloadDirectory>
+            <PayloadDirectory>" + msbuildTestPayloadRoot + Path.GetDirectoryName(assemblyInfo.AssemblyPath) + @"</PayloadDirectory>
             <Command>
                 dir
                 " + rehydrateCommand + @"
