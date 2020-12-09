b515d7e733832e489ec0f03b1a6e28b0565fa4a2
Andrew Arnott <andrewarnott@gmail.com>
2020-12-09 13:08:36 -0700

    Remove code to push to CI
    The feeds it was pushing to were obsolete anyway.

--- azure-pipelines/variables/ci_feed.ps1
+++
@@ -1,8 +0,0 @@
-$signType = $env:SIGNTYPE
-if (!$signType) { $signType = & "$PSScriptRoot/SignType.ps1" }
-
-if ($signType -eq 'Real') {
-    '09d8d03c-1ac8-456e-9274-4d2364527d99' # VSIDE-RealSigned-Release
-} else {
-    'da484c78-f942-44ef-b197-99e2a1bef53c' # VSIDE-TestSigned-Release
-}
