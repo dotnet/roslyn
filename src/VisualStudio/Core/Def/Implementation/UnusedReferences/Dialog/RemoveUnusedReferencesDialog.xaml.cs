9e831758c69d22b967484b00b06c4a6fef92d712
Joey Robichaud <joseph.robichaud@microsoft.com>
2020-12-08 17:10:52 -0800

    Add remove unused references dialog

---
+++ src/VisualStudio/Core/Def/Implementation/UnusedReferences/Dialog/RemoveUnusedReferencesDialog.xaml.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows;
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.UnusedReferences.Dialog
+{
+    /// <summary>
+    /// Interaction logic for RemoveUnusedReferencesDialog.xaml
+    /// </summary>
+    internal partial class RemoveUnusedReferencesDialog : DialogWindow
+    {
+        public string RemoveUnusedReferences => ServicesVSResources.Remove_Unused_References;
+        public string HelpText => ServicesVSResources.Choose_which_action_you_would_like_to_perform_on_the_unused_references;
+        public string Apply => ServicesVSResources.Apply;
+        public string Cancel => ServicesVSResources.Cancel;
+
+        public RemoveUnusedReferencesDialog(FrameworkElement element)
+        {
+            InitializeComponent();
+
+            TablePanel.Child = element;
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/UnusedReferences/Dialog/RemoveUnusedReferencesDialogProvider.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
+
+using System;
+using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.UnusedReferences;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.UnusedReferences.Dialog
+{
+    [Export(typeof(RemoveUnusedReferencesDialogProvider)), Shared]
+    internal class RemoveUnusedReferencesDialogProvider
+    {
+        private readonly UnusedReferencesTableProvider _tableProvider;
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public RemoveUnusedReferencesDialogProvider(UnusedReferencesTableProvider tableProvider)
+        {
+            _tableProvider = tableProvider;
+        }
+
+        public RemoveUnusedReferencesDialog CreateDialog(Project project, ImmutableArray<ReferenceUpdate> referenceUpdates)
+        {
+            var tableControl = _tableProvider.CreateTableControl();
+            tableControl.ShowGroupingLine = true;
+            tableControl.DoColumnsAutoAdjust = true;
+            tableControl.DoSortingAndGroupingWhileUnstable = true;
+
+            var dialog = new RemoveUnusedReferencesDialog(tableControl.Control);
+            _tableProvider.SetTableData(project, referenceUpdates);
+
+            // The table updates asychronously and is waiting for the UI thread. This allows the table
+            // to update and show our data.
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                await tableControl.ForceUpdateAsync().ConfigureAwait(true);
+            });
+
+            return dialog;
+        }
+    }
+}
