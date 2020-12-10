52ccbf4b7acdddb2ccdf3cd1117ea36767fda61e
Fredric Silberberg <frsilb@microsoft.com>
2020-12-10 15:54:37 -0800

    Allow pattern matching against function pointer types with nested type parameters.
    Fixes https://github.com/dotnet/roslyn/issues/49639.

--- src/Compilers/CSharp/Portable/Binder/Binder_Patterns.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Patterns.cs
@@ -345,7 +345,8 @@ namespace Microsoft.CodeAnalysis.CSharp
                     HashSet<DiagnosticInfo>? useSiteDiagnostics = null;
                     if (expression.ConstantValue == ConstantValue.Null)
                     {
-                        if (inputType.IsNonNullableValueType())
+                        // Function pointers are value types, but they can be assigned null, so they can be matched against null.
+                        if (inputType.IsNonNullableValueType() && inputType.TypeKind != TypeKind.FunctionPointer)
                         {
                             // We do not permit matching null against a struct type.
                             diagnostics.Add(ErrorCode.ERR_ValueCantBeNull, expression.Syntax.Location, inputType);
