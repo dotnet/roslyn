b7263b1a89655b207f749daf175082d47d37ab67
Manish Vasani <mavasani@microsoft.com>
2020-12-14 08:08:09 -0800

    Add tooling to CodeStyle layer to generate global configs
    Implements the post-build tooling support for https://github.com/dotnet/roslyn/issues/49250#issuecomment-736879786. The tool generates and packages the following files into the CodeStyle package:
    
    1) Global analyzer config files to enable/disable bucket of code style rules based on user specified AnalysisLevel/AnalysisMode
    2) Targets file to map the user specified AnalysisLevel/AnalysisMode to the appropriate global analyzer config and pass it down to the compiler
    
    Note that the post-build tool is primarily extracted from https://github.com/dotnet/roslyn-analyzers/blob/master/src/Tools/GenerateDocumentationAndConfigFiles/Program.cs.
    
    **TODO**: Add a new enum parameter `EnforceOnBuild` to CodeStyle analyzer constructor, which will determine the correct bucketing for code style analyzers for build enforcement. This will be done in a follow-up PR

---
+++ src/Analyzers/Core/Analyzers/EnforceOnBuild.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.CodeAnalysis.CodeStyle
+{
+    /// <summary>
+    /// Build enforcement recommendation for a code style analyzer.
+    /// </summary>
+    internal enum EnforceOnBuild
+    {
+        Never,
+        WhenExplicitlyEnabled,
+        Recommended,
+        HighlyRecommended,
+    }
+
+    internal static class EnforceOnBuildExtensions
+    {
+        public static string ToCustomTag(this EnforceOnBuild enforceOnBuild)
+            => $"{nameof(EnforceOnBuild)}_{enforceOnBuild}";
+    }
+}
