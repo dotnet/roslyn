fecb0d84d8814c4801df8cf2f875b113f9ead836
Paul M Cohen <paul1956@users.noreply.github.com>
2020-12-21 20:19:37 -0800

    Undo changes to IsVBKeyword

--- src/Tools/Source/CompilerGeneratorTools/Source/BoundTreeGenerator/BoundNodeClassWriter.cs
+++ src/Tools/Source/CompilerGeneratorTools/Source/BoundTreeGenerator/BoundNodeClassWriter.cs
@@ -2048,7 +2048,7 @@ namespace BoundTreeGenerator
                 case "getxmlnamespace":
                 case "global":
                 case "gosub":
-                case "goto":
+                case "goTo":
                 case "handles":
                 case "if":
                 case "implements":
@@ -2071,7 +2071,6 @@ namespace BoundTreeGenerator
                 case "mustoverride":
                 case "mybase":
                 case "myclass":
-                case "nameof":
                 case "namespace":
                 case "narrowing":
                 case "new":
@@ -2091,7 +2090,7 @@ namespace BoundTreeGenerator
                 case "overloads":
                 case "overridable":
                 case "overrides":
-                case "paramarray":
+                case "paramArray":
                 case "partial":
                 case "private":
                 case "property":
