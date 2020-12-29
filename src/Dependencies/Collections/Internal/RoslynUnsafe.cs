37de313dc61707efe19278a6e143d3df3bef4251
Sam Harwell <Sam.Harwell@microsoft.com>
2020-12-29 10:35:22 -0800

    Clean up code and comments in RoslynUnsafe

--- src/Dependencies/Collections/Internal/RoslynUnsafe.cs
+++ src/Dependencies/Collections/Internal/RoslynUnsafe.cs
@@ -13,30 +13,16 @@ namespace Microsoft.CodeAnalysis.Collections.Internal
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref T NullRef<T>()
-        {
-            return ref Unsafe.AsRef<T>(null);
-
-            // ldc.i4.0
-            // conv.u
-            // ret
-        }
+            => ref Unsafe.AsRef<T>(null);
 
         /// <summary>
         /// Returns if a given by-ref to type <typeparamref name="T"/> is a null reference.
         /// </summary>
         /// <remarks>
-        /// This check is conceptually similar to "(void*)(&amp;source) == nullptr".
+        /// This check is conceptually similar to <c>(void*)(&amp;source) == nullptr</c>.
         /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsNullRef<T>(ref T source)
-        {
-            return Unsafe.AsPointer(ref source) == null;
-
-            // ldarg.0
-            // ldc.i4.0
-            // conv.u
-            // ceq
-            // ret
-        }
+            => Unsafe.AsPointer(ref source) == null;
     }
 }
