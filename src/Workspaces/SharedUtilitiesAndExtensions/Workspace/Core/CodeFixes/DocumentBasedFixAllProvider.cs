7149dcced33d26377a72d82b3d93cb5ff7d9a20c
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-01-01 17:45:09 -0800

    Move onto shared helper

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/CodeFixes/DocumentBasedFixAllProvider.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/CodeFixes/DocumentBasedFixAllProvider.cs
@@ -2,13 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeActions;
-using Roslyn.Utilities;
-
-#pragma warning disable RS0005 // Do not use generic CodeAction.Create to create CodeAction
 
 namespace Microsoft.CodeAnalysis.CodeFixes
 {
@@ -24,31 +20,18 @@ namespace Microsoft.CodeAnalysis.CodeFixes
 
         public sealed override Task<CodeAction?> GetFixAsync(FixAllContext fixAllContext)
         {
-            var title = GetCodeActionTitle(fixAllContext);
-            switch (fixAllContext.Scope)
-            {
-                case FixAllScope.Document:
-                    return Task.FromResult<CodeAction?>(CodeAction.Create(
-                        title,
-                        c => GetDocumentFixesAsync(fixAllContext.WithCancellationToken(c)),
-                        nameof(DocumentBasedFixAllProvider)));
+            return FixAllContextHelper.GetFixAllCodeActionAsync(
+                fixAllContext,
+                GetCodeActionTitle(fixAllContext),
+                async (context, document, diagnostics) =>
+                {
+                    // if we didn't get a new root back, just return null to indicate we had no work to do here.
+                    var newRoot = await this.FixAllInDocumentAsync(context, document, diagnostics).ConfigureAwait(false);
+                    if (newRoot == null)
+                        return null;
 
-                case FixAllScope.Project:
-                    return Task.FromResult<CodeAction?>(CodeAction.Create(
-                        title,
-                        c => GetProjectFixesAsync(fixAllContext.WithCancellationToken(c), fixAllContext.Project),
-                        nameof(DocumentBasedFixAllProvider)));
-
-                case FixAllScope.Solution:
-                    return Task.FromResult<CodeAction?>(CodeAction.Create(
-                        title,
-                        c => GetSolutionFixesAsync(fixAllContext.WithCancellationToken(c)),
-                        nameof(DocumentBasedFixAllProvider)));
-
-                case FixAllScope.Custom:
-                default:
-                    return Task.FromResult<CodeAction?>(null);
-            }
+                    return document.WithSyntaxRoot(newRoot);
+                });
         }
 
         /// <summary>
@@ -63,38 +46,5 @@ namespace Microsoft.CodeAnalysis.CodeFixes
         /// <para><see langword="null"/>, if no changes were made to the document.</para>
         /// </returns>
         protected abstract Task<SyntaxNode?> FixAllInDocumentAsync(FixAllContext fixAllContext, Document document, ImmutableArray<Diagnostic> diagnostics);
-
-        private async Task<Document> GetDocumentFixesAsync(FixAllContext fixAllContext)
-        {
-            RoslynDebug.AssertNotNull(fixAllContext.Document);
-
-            var documentDiagnosticsToFix = await FixAllContextHelper.GetDocumentDiagnosticsToFixAsync(fixAllContext).ConfigureAwait(false);
-            if (!documentDiagnosticsToFix.TryGetValue(fixAllContext.Document, out var diagnostics))
-                return fixAllContext.Document;
-
-            var newRoot = await FixAllInDocumentAsync(fixAllContext, fixAllContext.Document, diagnostics).ConfigureAwait(false);
-            if (newRoot == null)
-                return fixAllContext.Document;
-
-            return fixAllContext.Document.WithSyntaxRoot(newRoot);
-        }
-
-        private Task<Solution> GetProjectFixesAsync(FixAllContext fixAllContext, Project project)
-            => FixAllContextHelper.FixAllInSolutionAsync(fixAllContext, ImmutableArray.Create(project.Id), GetFixAllInDocumentFunction());
-
-        private Task<Solution> GetSolutionFixesAsync(FixAllContext fixAllContext)
-            => FixAllContextHelper.FixAllInSolutionAsync(fixAllContext, GetFixAllInDocumentFunction());
-
-        private Func<FixAllContext, Document, ImmutableArray<Diagnostic>, Task<Document?>> GetFixAllInDocumentFunction()
-        {
-            return async (context, document, diagnostics) =>
-            {
-                var newRoot = await this.FixAllInDocumentAsync(context, document, diagnostics).ConfigureAwait(false);
-                if (newRoot == null)
-                    return null;
-
-                return document.WithSyntaxRoot(newRoot);
-            };
-        }
     }
 }
