a16ad35a715ef6d7a52c97633f58ebfa39c709c3
David Barbet <dabarbet@microsoft.com>
2021-01-05 18:06:25 -0800

    Add new integration test pipeline

--- src/VisualStudio/IntegrationTest/IntegrationTests/LanguageServerProtocol/LspGoToDefinition.cs
+++ src/VisualStudio/IntegrationTest/IntegrationTests/LanguageServerProtocol/LspGoToDefinition.cs
@@ -29,27 +29,7 @@ namespace Roslyn.VisualStudio.IntegrationTests.LanguageServerProtocol
         [WpfFact, Trait(Traits.Feature, Traits.Features.GoToDefinition), Trait(Traits.Editor, Traits.Editors.LanguageServerProtocol)]
         public void GoToDefinitionLSP()
         {
-            var project = new ProjectUtils.Project(ProjectName);
-            VisualStudio.SolutionExplorer.AddFile(project, "FileDef.cs");
-            VisualStudio.SolutionExplorer.OpenFile(project, "FileDef.cs");
-            VisualStudio.Editor.SetText(
-@"class SomeClass
-{
-}
-");
-            VisualStudio.SolutionExplorer.CloseCodeFile(project, "FileDef.cs", saveFile: true);
-            VisualStudio.SolutionExplorer.AddFile(project, "FileConsumer.cs");
-            VisualStudio.SolutionExplorer.OpenFile(project, "FileConsumer.cs");
-            VisualStudio.Editor.SetText(
-@"class SomeOtherClass
-{
-    SomeClass sc;
-}");
-            VisualStudio.Editor.PlaceCaret("SomeClass");
-            VisualStudio.Editor.GoToDefinition("FileDef.cs");
-            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
-            VisualStudio.Editor.Verify.TextContains(@"class SomeClass$$", assertCaretPosition: true);
-            Assert.True(VisualStudio.Shell.IsActiveTabProvisional());
+            Assert.True(false);
         }
     }
 }
