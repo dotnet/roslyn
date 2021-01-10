6f698eba176d9396baa1909bbd3993f7b3935b94
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-01-09 23:13:54 -0800

    Add VBside

---
+++ src/Analyzers/VisualBasic/Analyzers/NewLines/ConsecutiveStatementPlacement/VisualBasicConsecutiveStatementPlacementDiagnosticAnalyzer.vb
@@ -0,0 +1,48 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+Imports Microsoft.CodeAnalysis.Diagnostics
+Imports Microsoft.CodeAnalysis.NewLines.ConsecutiveStatementPlacement
+
+Namespace Microsoft.CodeAnalysis.VisualBasic.NewLines.ConsecutiveStatementPlacement
+    ''' <summary>
+    ''' Analyzer that finds code of the form:
+    ''' <code>
+    ''' If cond
+    ''' End If
+    ''' NextStatement()
+    ''' </code>
+    ''' 
+    ''' And requires it to be of the form:
+    ''' <code>
+    ''' If cond
+    ''' End If
+    ''' 
+    ''' NextStatement()
+    ''' </code>
+    ''' 
+    ''' Specifically, all blocks followed by another statement must have a blank line between them.
+    ''' </summary>
+    <DiagnosticAnalyzer(LanguageNames.VisualBasic)>
+    Friend NotInheritable Class VisualBasicConsecutiveStatementPlacementDiagnosticAnalyzer
+        Inherits AbstractConsecutiveStatementPlacementDiagnosticAnalyzer(Of ExecutableStatementSyntax)
+
+        Protected Overrides Function IsEndOfLine(trivia As SyntaxTrivia) As Boolean
+            Return trivia.Kind() = SyntaxKind.EndOfLineTrivia
+        End Function
+
+        Protected Overrides Function IsWhitespace(trivia As SyntaxTrivia) As Boolean
+            Return trivia.Kind() = SyntaxKind.WhitespaceTrivia
+        End Function
+
+        Protected Overrides Function IsBlockStatement(node As SyntaxNode) As Boolean
+            Return TypeOf node Is EndBlockStatementSyntax OrElse TypeOf node Is NextStatementSyntax
+        End Function
+
+        Protected Overrides Function GetDiagnosticLocation(block As SyntaxNode) As Location
+            Return block.GetLocation()
+        End Function
+    End Class
+End Namespace
