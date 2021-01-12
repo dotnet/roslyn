227c48526a1e8fe5b9326078f7b9c52cd6c8899c
Andrew Hall (METAL) <andrha@microsoft.com>
2021-01-11 16:46:20 -0800

    Remove custom batch fix. Add file headers

--- src/Analyzers/Core/CodeFixes/NamespaceSync/AbstractSyncNamespaceCodeFixProvider.cs
+++ src/Analyzers/Core/CodeFixes/NamespaceSync/AbstractSyncNamespaceCodeFixProvider.cs
@@ -1,21 +1,21 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.Collections.Immutable;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Shared.Extensions;
-using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CodeFixes.NamespaceSync
 {
     internal abstract class AbstractSyncNamespaceCodeFixProvider : CodeFixProvider
     {
         protected abstract CodeAction CreateCodeAction(Func<CancellationToken, Task<Solution>> createChangedSolution);
-        
+
         public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(IDEDiagnosticIds.NamespaceSyncAnalyzerDiagnosticId);
 
         public override Task RegisterCodeFixesAsync(CodeFixContext context)
@@ -46,91 +46,6 @@ namespace Microsoft.CodeAnalysis.CodeFixes.NamespaceSync
         }
 
         public override FixAllProvider? GetFixAllProvider()
-        {
-            return new MyFixAllProvider();
-        }
-
-        private sealed class MyFixAllProvider : FixAllProvider
-        {
-            private static string CodeActionTitle => "Sync namespaces";
-
-            public override Task<CodeAction?> GetFixAsync(FixAllContext fixAllContext)
-            {
-                CodeAction? fixAction;
-                switch (fixAllContext.Scope)
-                {
-                    case FixAllScope.Document:
-                        fixAction = new CustomCodeActions.SolutionChangeAction(
-                            CodeActionTitle,
-                            cancellationToken => GetDocumentFixesAsync(fixAllContext.WithCancellationToken(cancellationToken)),
-                            nameof(DocumentBasedFixAllProvider));
-                        break;
-
-                    case FixAllScope.Project:
-                        fixAction = new CustomCodeActions.SolutionChangeAction(
-                            CodeActionTitle,
-                            cancellationToken => GetProjectFixesAsync(fixAllContext.WithCancellationToken(cancellationToken), fixAllContext.Project),
-                            nameof(DocumentBasedFixAllProvider));
-                        break;
-
-                    case FixAllScope.Solution:
-                        fixAction = new CustomCodeActions.SolutionChangeAction(
-                            CodeActionTitle,
-                            cancellationToken => GetSolutionFixesAsync(fixAllContext.WithCancellationToken(cancellationToken)),
-                            nameof(DocumentBasedFixAllProvider));
-                        break;
-
-                    case FixAllScope.Custom:
-                    default:
-                        fixAction = null;
-                        break;
-                }
-
-                return Task.FromResult(fixAction);
-            }
-
-            private static Task<Solution> GetSolutionFixesAsync(FixAllContext fixAllContext)
-            {
-                var documents = fixAllContext.Solution.Projects.SelectMany(i => i.Documents).ToImmutableArray();
-                return GetSolutionFixesAsync(fixAllContext, documents);
-            }
-
-            private static Task<Solution> GetProjectFixesAsync(FixAllContext fixAllContext, Project project)
-                => GetSolutionFixesAsync(fixAllContext, project.Documents.ToImmutableArray());
-
-            private static Task<Solution> GetDocumentFixesAsync(FixAllContext fixAllContext)
-            {
-                RoslynDebug.AssertNotNull(fixAllContext.Document);
-                return FixAsync(fixAllContext.Document, fixAllContext.CancellationToken);
-            }
-
-            private static async Task<Solution> GetSolutionFixesAsync(FixAllContext fixAllContext, ImmutableArray<Document> documents)
-            {
-                var documentDiagnosticsToFix = await FixAllContextHelper.GetDocumentDiagnosticsToFixAsync(fixAllContext).ConfigureAwait(false);
-
-                using var _ = PooledHashSet<DocumentId>.GetInstance(out var documentIds);
-                foreach (var document in documents)
-                {
-                    // Don't bother examining any documents that aren't in the list of docs that
-                    // actually have diagnostics.
-                    if (!documentDiagnosticsToFix.TryGetValue(document, out var diagnostics))
-                        continue;
-
-                    documentIds.Add(document.Id);
-                }
-
-                var solution = fixAllContext.Solution;
-
-                // The fixes have to be applied one at a time since they can cause merge conflicts
-                // by modifying other documents referencing the definitions in the namespaces that are changing.
-                foreach (var documentId in documentIds)
-                {
-                    var document = solution.GetRequiredDocument(documentId);
-                    solution = await FixAsync(document, fixAllContext.CancellationToken).ConfigureAwait(false);
-                }
-
-                return solution;
-            }
-        }
+            => WellKnownFixAllProviders.BatchFixer;
     }
 }
