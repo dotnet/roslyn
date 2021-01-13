eb92dc972bf280845dfc0ae14ad778a4abcd90b0
David Wengier <david.wengier@microsoft.com>
2021-01-13 11:45:22 +1100

    Fix build

--- src/EditorFeatures/TestUtilities/SignatureHelp/SignatureHelpTestItem.cs
+++ src/EditorFeatures/TestUtilities/SignatureHelp/SignatureHelpTestItem.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace Microsoft.CodeAnalysis.Editor.UnitTests.SignatureHelp
 {
