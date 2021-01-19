eedc8efdb80f305e0462f0246961712c416bb482
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-01-18 22:48:33 -0800

    Add F# ovloads

--- src/Tools/ExternalAccess/FSharp/Navigation/IFSharpDocumentNavigationService.cs
+++ src/Tools/ExternalAccess/FSharp/Navigation/IFSharpDocumentNavigationService.cs
@@ -4,6 +4,7 @@
 
 #nullable disable
 
+using System.Threading;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Text;
@@ -16,30 +17,36 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Navigation
         /// Determines whether it is possible to navigate to the given position in the specified document.
         /// </summary>
         bool CanNavigateToSpan(Workspace workspace, DocumentId documentId, TextSpan textSpan);
+        bool CanNavigateToSpan(Workspace workspace, DocumentId documentId, TextSpan textSpan, CancellationToken cancellationToken);
 
         /// <summary>
         /// Determines whether it is possible to navigate to the given line/offset in the specified document.
         /// </summary>
         bool CanNavigateToLineAndOffset(Workspace workspace, DocumentId documentId, int lineNumber, int offset);
+        bool CanNavigateToLineAndOffset(Workspace workspace, DocumentId documentId, int lineNumber, int offset, CancellationToken cancellationToken);
 
         /// <summary>
         /// Determines whether it is possible to navigate to the given virtual position in the specified document.
         /// </summary>
         bool CanNavigateToPosition(Workspace workspace, DocumentId documentId, int position, int virtualSpace = 0);
+        bool CanNavigateToPosition(Workspace workspace, DocumentId documentId, int position, int virtualSpace, CancellationToken cancellationToken);
 
         /// <summary>
         /// Navigates to the given position in the specified document, opening it if necessary.
         /// </summary>
         bool TryNavigateToSpan(Workspace workspace, DocumentId documentId, TextSpan textSpan, OptionSet options = null);
+        bool TryNavigateToSpan(Workspace workspace, DocumentId documentId, TextSpan textSpan, OptionSet options, CancellationToken cancellationToken);
 
         /// <summary>
         /// Navigates to the given line/offset in the specified document, opening it if necessary.
         /// </summary>
         bool TryNavigateToLineAndOffset(Workspace workspace, DocumentId documentId, int lineNumber, int offset, OptionSet options = null);
+        bool TryNavigateToLineAndOffset(Workspace workspace, DocumentId documentId, int lineNumber, int offset, OptionSet options, CancellationToken cancellationToken);
 
         /// <summary>
         /// Navigates to the given virtual position in the specified document, opening it if necessary.
         /// </summary>
         bool TryNavigateToPosition(Workspace workspace, DocumentId documentId, int position, int virtualSpace = 0, OptionSet options = null);
+        bool TryNavigateToPosition(Workspace workspace, DocumentId documentId, int position, int virtualSpace, OptionSet options, CancellationToken cancellationToken);
     }
 }
