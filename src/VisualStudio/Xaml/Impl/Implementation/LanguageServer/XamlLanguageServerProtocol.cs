6b7ade2fec27fbc7d96945dfb0b4e4221c717a24
Lingling Tong <ltong@microsoft.com>
2021-01-25 10:56:56 -0800

    Add XamlLanguageServerFeedbackService interface

--- src/VisualStudio/Xaml/Impl/Implementation/LanguageServer/XamlLanguageServerProtocol.cs
+++ src/VisualStudio/Xaml/Impl/Implementation/LanguageServer/XamlLanguageServerProtocol.cs
@@ -7,11 +7,13 @@ using System.Collections.Generic;
 using System.Composition;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Editor.Xaml;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.LanguageServer;
 using Microsoft.CodeAnalysis.LanguageServer.Handler;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Microsoft.VisualStudio.LanguageServices.Xaml.Telemetry;
 
 namespace Microsoft.VisualStudio.LanguageServices.Xaml.LanguageServer
 {
@@ -23,25 +25,42 @@ namespace Microsoft.VisualStudio.LanguageServices.Xaml.LanguageServer
     internal sealed class XamlLanguageServerProtocol : AbstractRequestHandlerProvider
     {
         private readonly XamlProjectService _projectService;
+        private readonly IXamlLanguageServerFeedbackService? _feedbackService;
 
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-        public XamlLanguageServerProtocol([ImportMany] IEnumerable<Lazy<IRequestHandler, IRequestHandlerMetadata>> requestHandlers, XamlProjectService projectService)
+        public XamlLanguageServerProtocol(
+            [ImportMany] IEnumerable<Lazy<IRequestHandler, IRequestHandlerMetadata>> requestHandlers, 
+            XamlProjectService projectService,
+            [Import(AllowDefault = true)] IXamlLanguageServerFeedbackService? feedbackService)
             : base(requestHandlers, languageName: StringConstants.XamlLanguageName)
         {
             _projectService = projectService;
+            _feedbackService = feedbackService;
         }
 
-        protected override Task<ResponseType> ExecuteRequestAsync<RequestType, ResponseType>(RequestExecutionQueue queue, RequestType request, ClientCapabilities clientCapabilities, string? clientName, string methodName, bool mutatesSolutionState, IRequestHandler<RequestType, ResponseType> handler, CancellationToken cancellationToken)
+        protected override async Task<ResponseType> ExecuteRequestAsync<RequestType, ResponseType>(RequestExecutionQueue queue, RequestType request, ClientCapabilities clientCapabilities, string? clientName, string methodName, bool mutatesSolutionState, IRequestHandler<RequestType, ResponseType> handler, CancellationToken cancellationToken)
         {
             var textDocument = handler.GetTextDocumentIdentifier(request);
 
+            DocumentId? documentId = null;
             if (textDocument is { Uri: { IsAbsoluteUri: true } documentUri })
             {
-                _projectService.TrackOpenDocument(documentUri.LocalPath);
+                documentId = _projectService.TrackOpenDocument(documentUri.LocalPath);
             }
 
-            return base.ExecuteRequestAsync(queue, request, clientCapabilities, clientName, methodName, mutatesSolutionState, handler, cancellationToken);
+            using (var requestScope = _feedbackService?.CreateRequestScope(documentId, methodName))
+            {
+                try
+                {
+                    return await base.ExecuteRequestAsync(queue, request, clientCapabilities, clientName, methodName, mutatesSolutionState, handler, cancellationToken).ConfigureAwait(false);
+                }
+                catch(Exception e) when (e is not OperationCanceledException)
+                {
+                    requestScope?.RecordFailure(e);
+                    throw;
+                }
+            }
         }
     }
 }
