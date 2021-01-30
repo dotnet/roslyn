48d261c8248e5061dbcf72487297eac2d91e3fa3
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-01-30 00:42:50 -0800

    Break out files

--- src/Workspaces/Core/Portable/Storage/SQLite/v2/Database.cs
+++ src/Workspaces/Core/Portable/Storage/SQLite/v2/Database.cs
@@ -2,27 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Immutable;
-using System.Text;
 using Roslyn.Utilities;
-using SQLitePCL;
 
 namespace Microsoft.CodeAnalysis.SQLite.v2
 {
-    internal enum Table
-    {
-        Document,
-        Project,
-        Solution,
-    }
-
-    internal enum Column
-    {
-        Data,
-        Checksum,
-    }
-
     internal enum Database
     {
         /// <summary>
