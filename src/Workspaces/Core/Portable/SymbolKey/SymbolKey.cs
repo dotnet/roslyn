19f37b47867c1c5f0250bc9a97b81cbfc5c3508d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-01-29 23:33:45 -0800

    Fix comment

--- src/Workspaces/Core/Portable/SymbolKey/SymbolKey.cs
+++ src/Workspaces/Core/Portable/SymbolKey/SymbolKey.cs
@@ -15,57 +15,43 @@ namespace Microsoft.CodeAnalysis
 {
     /// <summary>
     /// <para>
-    /// A <see cref="SymbolKey"/> is a lightweight identifier for a symbol that can be used to 
-    /// resolve the "same" symbol across compilations.  Different symbols have different concepts 
-    /// of "same-ness". Same-ness is recursively defined as follows:
+    /// A <see cref="SymbolKey"/> is a lightweight identifier for a symbol that can be used to resolve the "same" symbol
+    /// across compilations.  Different symbols have different concepts of "same-ness". Same-ness is recursively defined
+    /// as follows:
     /// <list type="number">
-    ///   <item>Two <see cref="IArrayTypeSymbol"/>s are the "same" if they have 
-    ///         the "same" <see cref="IArrayTypeSymbol.ElementType"/> and 
-    ///         equal <see cref="IArrayTypeSymbol.Rank"/>.</item>
-    ///   <item>Two <see cref="IAssemblySymbol"/>s are the "same" if 
-    ///         they have equal <see cref="IAssemblySymbol.Identity"/>.Name</item>
-    ///   <item>Two <see cref="IEventSymbol"/>s are the "same" if they have 
-    ///         the "same" <see cref="ISymbol.ContainingType"/> and 
-    ///         equal <see cref="ISymbol.MetadataName"/>.</item>
-    ///   <item>Two <see cref="IMethodSymbol"/>s are the "same" if they have 
-    ///         the "same" <see cref="ISymbol.ContainingType"/>,
-    ///         equal <see cref="ISymbol.MetadataName"/>,
-    ///         equal <see cref="IMethodSymbol.Arity"/>, 
-    ///         the "same" <see cref="IMethodSymbol.TypeArguments"/>, and have
-    ///         the "same" <see cref="IParameterSymbol.Type"/>s and  
-    ///         equal <see cref="IParameterSymbol.RefKind"/>s.</item>
-    ///   <item>Two <see cref="IModuleSymbol"/>s are the "same" if they have
-    ///         the "same" <see cref="ISymbol.ContainingAssembly"/>.
-    ///         <see cref="ISymbol.MetadataName"/> is not used because module identity is not important in practice.</item>
-    ///   <item>Two <see cref="INamedTypeSymbol"/>s are the "same" if they have 
-    ///         the "same" <see cref="ISymbol.ContainingSymbol"/>,
-    ///         equal <see cref="ISymbol.MetadataName"/>,
-    ///         equal <see cref="INamedTypeSymbol.Arity"/> and 
-    ///         the "same" <see cref="INamedTypeSymbol.TypeArguments"/>. If the named type is a delegate,
-    ///         it must have the "same" <see cref="IParameterSymbol.Type"/>s and 
-    ///         equal <see cref="IParameterSymbol.RefKind"/>s.
-    ///   <item>Two <see cref="INamespaceSymbol"/>s are the "same" if they have 
-    ///         the "same" <see cref="ISymbol.ContainingSymbol"/> and
-    ///         equal <see cref="ISymbol.MetadataName"/>.
-    ///     If the <see cref="INamespaceSymbol"/> is the global namespace for a
-    ///     compilation, then it will only match another
-    ///     global namespace of another compilation.</item>
-    ///   <item>Two <see cref="IParameterSymbol"/>s are the "same" if they have
-    ///         the "same" <see cref="ISymbol.ContainingSymbol"/> and 
-    ///         equal <see cref="ISymbol.MetadataName"/>.</item>
-    ///   <item>Two <see cref="IPointerTypeSymbol"/>s are the "same" if they have 
-    ///         the "same" <see cref="IPointerTypeSymbol.PointedAtType"/>.</item>
-    ///   <item>Two <see cref="IPropertySymbol"/>s are the "same" if they have 
-    ///         the "same" the "same" <see cref="ISymbol.ContainingType"/>, 
-    ///         the "same" <see cref="ISymbol.MetadataName"/>, and have 
-    ///         the "same" <see cref="IParameterSymbol.Type"/>s and  
-    ///         the "same" <see cref="IParameterSymbol.RefKind"/>s.</item>
-    ///   <item>Two <see cref="ITypeParameterSymbol"/> are the "same" if they have
-    ///         the "same" <see cref="ISymbol.ContainingSymbol"/> and 
-    ///         the "same" <see cref="ISymbol.MetadataName"/>.</item>
-    ///   <item>Two <see cref="IFieldSymbol"/>s are the "same" if they have
-    ///         the "same" <see cref="ISymbol.ContainingSymbol"/> and 
-    ///         the "same" <see cref="ISymbol.MetadataName"/>.</item>
+    ///   <item>Two <see cref="IArrayTypeSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="IArrayTypeSymbol.ElementType"/> and  equal <see cref="IArrayTypeSymbol.Rank"/>.</item>
+    ///   <item>Two <see cref="IAssemblySymbol"/>s are the "same" if they have equal <see
+    ///   cref="IAssemblySymbol.Identity"/>.Name</item>
+    ///   <item>Two <see cref="IEventSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="ISymbol.ContainingType"/> and equal <see cref="ISymbol.MetadataName"/>.</item>
+    ///   <item>Two <see cref="IMethodSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="ISymbol.ContainingType"/>, equal <see cref="ISymbol.MetadataName"/>, equal <see
+    ///   cref="IMethodSymbol.Arity"/>,  the "same" <see cref="IMethodSymbol.TypeArguments"/>, and have the "same" <see
+    ///   cref="IParameterSymbol.Type"/>s and equal <see cref="IParameterSymbol.RefKind"/>s.</item>
+    ///   <item>Two <see cref="IModuleSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="ISymbol.ContainingAssembly"/>. <see cref="ISymbol.MetadataName"/> is not used because module identity is
+    ///   not important in practice.</item>
+    ///   <item>Two <see cref="INamedTypeSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="ISymbol.ContainingSymbol"/>, equal <see cref="ISymbol.MetadataName"/>, equal <see
+    ///   cref="INamedTypeSymbol.Arity"/> and the "same" <see cref="INamedTypeSymbol.TypeArguments"/>. If the named type
+    ///   is a delegate, it must have the "same" <see cref="IParameterSymbol.Type"/>s and equal <see
+    ///   cref="IParameterSymbol.RefKind"/>s.</item>
+    ///   <item>Two <see cref="INamespaceSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="ISymbol.ContainingSymbol"/> and equal <see cref="ISymbol.MetadataName"/>. If the <see
+    ///   cref="INamespaceSymbol"/> is the global namespace for a compilation, then it will only match another global
+    ///   namespace of another compilation.</item>
+    ///   <item>Two <see cref="IParameterSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="ISymbol.ContainingSymbol"/> and equal <see cref="ISymbol.MetadataName"/>.</item>
+    ///   <item>Two <see cref="IPointerTypeSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="IPointerTypeSymbol.PointedAtType"/>.</item>
+    ///   <item>Two <see cref="IPropertySymbol"/>s are the "same" if they have the "same" the "same" <see
+    ///   cref="ISymbol.ContainingType"/>, the "same" <see cref="ISymbol.MetadataName"/>, and have the "same" <see
+    ///   cref="IParameterSymbol.Type"/>s and the "same" <see cref="IParameterSymbol.RefKind"/>s.</item>
+    ///   <item>Two <see cref="ITypeParameterSymbol"/> are the "same" if they have the "same" <see
+    ///   cref="ISymbol.ContainingSymbol"/> and the "same" <see cref="ISymbol.MetadataName"/>.</item>
+    ///   <item>Two <see cref="IFieldSymbol"/>s are the "same" if they have the "same" <see
+    ///   cref="ISymbol.ContainingSymbol"/> and the "same" <see cref="ISymbol.MetadataName"/>.</item>
     /// </list>
     /// </para>
     /// <para>
