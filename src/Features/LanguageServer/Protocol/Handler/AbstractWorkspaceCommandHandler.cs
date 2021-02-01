2bd38c3fbf0bd95f92b27fd4d66aceb9c0eabf68
David Wengier <david.wengier@microsoft.com>
2021-02-02 10:51:42 +1100

    Move workspace command metadata to interfaces

---
+++ src/Features/LanguageServer/Protocol/Handler/AbstractWorkspaceCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler
+{
+    internal abstract class AbstractWorkspaceCommandHandler<ResponseType> : IWorkspaceCommandHandler, IRequestHandler<ExecuteCommandParams, ResponseType>
+    {
+        public string MethodName => $"{Methods.WorkspaceExecuteCommandName}/{CommandName}";
+
+        public abstract string CommandName { get; }
+        public abstract bool MutatesSolutionState { get; }
+        public abstract bool SkipBuildingLSPSolution { get; }
+
+        public abstract TextDocumentIdentifier? GetTextDocumentIdentifier(ExecuteCommandParams request);
+        public abstract Task<ResponseType> HandleRequestAsync(ExecuteCommandParams request, RequestContext context, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Features/LanguageServer/Protocol/Handler/IWorkspaceCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler
+{
+    internal interface IWorkspaceCommandHandler : IRequestHandler
+    {
+        /// <summary>
+        /// The name of the command to execute for a <see cref="Methods.WorkspaceExecuteCommandName"/> request.
+        /// </summary>
+        string CommandName { get; }
+    }
+}
