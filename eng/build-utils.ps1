5b62cfb0b0412e3204d3d569f59a0135acbe7271
Rikki Gibson <rigibson@microsoft.com>
2021-02-02 20:32:35 -0800

    Get-ChildItem returns a full path apparently

--- eng/build-utils.ps1
+++ eng/build-utils.ps1
@@ -333,7 +333,7 @@ function Make-BootstrapBuild([switch]$force32 = $false) {
 
   Run-MSBuild $projectPath "/restore /t:Pack /p:RoslynEnforceCodeStyle=false /p:RunAnalyzersDuringBuild=false /p:DotNetUseShippingVersions=true /p:InitialDefineConstants=BOOTSTRAP /p:PackageOutputPath=`"$dir`" /p:EnableNgenOptimization=false /p:PublishWindowsPdb=false $force32Flag" -logFileName "Bootstrap" -configuration $bootstrapConfiguration -runAnalyzers
   $packageFile = Get-ChildItem -Path $dir -Filter "$packageName.*.nupkg"
-  Unzip (Join-Path $dir $packageFile) $dir
+  Unzip $packageFile $dir
 
   Write-Host "Cleaning Bootstrap compiler artifacts"
   Run-MSBuild $projectPath "/t:Clean" -logFileName "BootstrapClean"
