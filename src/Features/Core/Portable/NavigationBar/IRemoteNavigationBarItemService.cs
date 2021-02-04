0588585b269c0ebd729a1a81aac27cc86fff6c24
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-02-03 20:46:17 -0800

    Move API up

--- src/Features/Core/Portable/NavigationBar/IRemoteNavigationBarItemService.cs
+++ src/Features/Core/Portable/NavigationBar/IRemoteNavigationBarItemService.cs
@@ -8,7 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Remote;
 
-namespace Microsoft.CodeAnalysis.Editor.Extensibility.NavigationBar
+namespace Microsoft.CodeAnalysis.NavigationBar
 {
     internal interface IRemoteNavigationBarItemService
     {
