326a523d6f70fcb2331e7a7c73b8cf84d50c9395
Ankita Khera <ankitakhera@microsoft.com>
2021-02-03 16:31:53 -0800

    Have abstract introduce parameter completed?

---
+++ src/Features/Core/Portable/IntroduceVariable/AbstractIntroduceParameterCodeAction.cs
@@ -0,0 +1,77 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.LanguageServices;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Simplification;
+
+namespace Microsoft.CodeAnalysis.IntroduceVariable
+{
+    internal partial class AbstractIntroduceParameterService<TService, TExpressionSyntax>
+    {
+        internal abstract class AbstractIntroduceParameterCodeAction : CodeAction
+        {
+            private readonly bool _allOccurrences;
+            private readonly TService _service;
+            private readonly TExpressionSyntax _expression;
+            private readonly SemanticDocument _semanticDocument;
+
+            internal AbstractIntroduceParameterCodeAction(
+                SemanticDocument document,
+                TService service,
+                TExpressionSyntax expression,
+                bool allOccurrences)
+            {
+                _semanticDocument = document;
+                _service = service;
+                _expression = expression;
+                _allOccurrences = allOccurrences;
+                Title = CreateDisplayText(expression);
+            }
+
+            public override string Title { get; }
+
+            protected override async Task<Document> GetChangedDocumentAsync(CancellationToken cancellationToken)
+            {
+                var changedDocument = await GetChangedDocumentCoreAsync(cancellationToken).ConfigureAwait(false);
+                return await Simplifier.ReduceAsync(changedDocument, cancellationToken: cancellationToken).ConfigureAwait(false);
+            }
+
+            private async Task<Document> GetChangedDocumentCoreAsync(CancellationToken cancellationToken)
+            {
+                return await _service.IntroduceParameterAsync(_semanticDocument, _expression, _allOccurrences, cancellationToken).ConfigureAwait(false);
+            }
+
+            private string CreateDisplayText(TExpressionSyntax expression)
+            {
+                var singleLineExpression = _semanticDocument.Document.GetLanguageService<ISyntaxFactsService>().ConvertToSingleLine(expression);
+                var nodeString = singleLineExpression.ToString();
+
+                return CreateDisplayText(nodeString);
+            }
+
+            private string CreateDisplayText(string nodeString)
+            {
+                var formatString = _allOccurrences
+                        ? FeaturesResources.Introduce_parameter_for_all_occurrences_of_0
+                        : FeaturesResources.Introduce_parameter_for_0;
+                return string.Format(formatString, nodeString);
+            }
+
+            protected ITypeSymbol GetExpressionType(
+                CancellationToken cancellationToken)
+            {
+                var semanticModel = _semanticDocument.SemanticModel;
+                var typeInfo = semanticModel.GetTypeInfo(_expression, cancellationToken);
+
+                return typeInfo.Type ?? typeInfo.ConvertedType ?? semanticModel.Compilation.GetSpecialType(SpecialType.System_Object);
+            }
+        }
+    }
+}
---
+++ src/Features/Core/Portable/IntroduceVariable/AbstractIntroduceParameterService.cs
@@ -0,0 +1,149 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.LanguageServices;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Shared.Utilities;
+using Microsoft.CodeAnalysis.Text;
+using static Microsoft.CodeAnalysis.CodeActions.CodeAction;
+
+#nullable disable
+
+namespace Microsoft.CodeAnalysis.IntroduceVariable
+{
+    internal abstract partial class AbstractIntroduceParameterService<TService, TExpressionSyntax> : CodeRefactoringProvider
+        where TService : AbstractIntroduceParameterService<TService, TExpressionSyntax>
+        where TExpressionSyntax : SyntaxNode
+    {
+        public TExpressionSyntax Expression { get; private set; }
+        protected abstract Task<Document> IntroduceParameterAsync(SemanticDocument document, TExpressionSyntax expression, bool allOccurrences, CancellationToken cancellationToken);
+        protected abstract IEnumerable<SyntaxNode> GetContainingExecutableBlocks(TExpressionSyntax expression);
+        protected virtual bool BlockOverlapsHiddenPosition(SyntaxNode block, CancellationToken cancellationToken)
+            => block.OverlapsHiddenPosition(cancellationToken);
+
+        public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            var (document, textSpan, cancellationToken) = context;
+            if (document.Project.Solution.Workspace.Kind == WorkspaceKind.MiscellaneousFiles)
+            {
+                return;
+            }
+
+            var action = await IntroduceParameterAsync(document, textSpan, cancellationToken).ConfigureAwait(false);
+            if (action != null)
+            {
+                context.RegisterRefactoring(action, textSpan);
+            }
+        }
+
+        public async Task<CodeAction> IntroduceParameterAsync(Document document, TextSpan textSpan, CancellationToken cancellationToken)
+        {
+            var semanticDocument = await SemanticDocument.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+            var syntaxFacts = document.GetLanguageService<ISyntaxFactsService>();
+
+            Expression = await document.TryGetRelevantNodeAsync<TExpressionSyntax>(textSpan, cancellationToken).ConfigureAwait(false);
+            if (Expression == null || CodeRefactoringHelpers.IsNodeUnderselected(Expression, textSpan))
+            {
+                return null;
+            }
+
+            var expression = syntaxFacts.GetExpressionOfExpressionStatement(Expression);
+
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+            var type = semanticModel.GetTypeInfo(expression, cancellationToken).Type;
+            if (type == null ||
+                type.SpecialType == SpecialType.System_Void)
+            {
+                return null;
+            }
+
+            if (expression != null)
+            {
+                var (title, actions) = AddActions(semanticDocument, cancellationToken);
+
+                if (actions.Length > 0)
+                {
+                    return new CodeActionWithNestedActions(title, actions, isInlinable: true);
+                }
+            }
+            return null;
+
+        }
+
+        private (string title, ImmutableArray<CodeAction> actions) AddActions(SemanticDocument semanticDocument, CancellationToken cancellationToken)
+        {
+            var actionsBuilder = new ArrayBuilder<CodeAction>();
+            var enclosingBlocks = GetContainingExecutableBlocks(Expression);
+            if (enclosingBlocks.Any())
+            {
+                // If we're inside a block, then don't even try the other options (like field,
+                // constructor initializer, etc.).  This is desirable behavior.  If we're in a 
+                // block in a field, then we're in a lambda, and we want to offer to generate
+                // a local, and not a field.
+                if (IsInBlockContext(semanticDocument, cancellationToken))
+                {
+                    var block = enclosingBlocks.FirstOrDefault();
+
+                    if (!BlockOverlapsHiddenPosition(block, cancellationToken))
+                    {
+                        actionsBuilder.Add(new IntroduceParameterCodeAction(semanticDocument, (TService)this, Expression, false));
+
+                        if (enclosingBlocks.All(b => !BlockOverlapsHiddenPosition(b, cancellationToken)))
+                        {
+                            actionsBuilder.Add(new IntroduceParameterCodeAction(semanticDocument, (TService)this, Expression, true));
+                        }
+                    }
+                }
+            }
+            return (FeaturesResources.Introduce_parameter, actionsBuilder.ToImmutable());
+        }
+
+
+
+        private bool IsInBlockContext(SemanticDocument semanticDocument,
+                CancellationToken cancellationToken)
+        {
+           /* if (!IsInTypeDeclarationOrValidCompilationUnit())
+            {
+                return false;
+            }*/
+
+            // If refer to a query property, then we use the query context instead.
+            var bindingMap = GetSemanticMap(semanticDocument, cancellationToken);
+            if (bindingMap.AllReferencedSymbols.Any(s => s is IRangeVariableSymbol))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public SemanticMap GetSemanticMap(SemanticDocument semanticDocument, CancellationToken cancellationToken)
+        {
+            var semanticMap = semanticDocument.SemanticModel.GetSemanticMap(Expression, cancellationToken);
+            return semanticMap;
+        }
+
+        private class IntroduceParameterCodeAction : AbstractIntroduceParameterCodeAction
+        {
+            internal IntroduceParameterCodeAction(
+                SemanticDocument document,
+                TService service,
+                TExpressionSyntax expression,
+                bool allOccurrences)
+                : base(document, service, expression, allOccurrences)
+            { }
+        }
+    }
+}
