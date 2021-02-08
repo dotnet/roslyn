9d871473f95d1449df9999f1e61047f7166db961
Sam Harwell <Sam.Harwell@microsoft.com>
2021-02-07 21:10:17 -0800

    Minimal changes necessary to build

--- src/Dependencies/Collections/Internal/ICollectionDebugView`1.cs
+++ src/Dependencies/Collections/Internal/ICollectionDebugView`1.cs
@@ -1,5 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 // NOTE: This code is derived from an implementation originally in dotnet/runtime:
 // https://github.com/dotnet/runtime/blob/v5.0.2/src/libraries/System.Private.CoreLib/src/System/Collections/Generic/ICollectionDebugView.cs
@@ -7,9 +8,13 @@
 // See the commentary in https://github.com/dotnet/roslyn/pull/50156 for notes on incorporating changes made to the
 // reference implementation.
 
+#pragma warning disable
+
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace System.Collections.Generic
+namespace Microsoft.CodeAnalysis.Collections.Internal
 {
     internal sealed class ICollectionDebugView<T>
     {
