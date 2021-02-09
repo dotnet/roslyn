d1d7fc9cdf896d255655bb2a3162765449ede76e
Sam Harwell <Sam.Harwell@microsoft.com>
2021-02-09 08:24:10 -0800

    Add origin notes

--- src/Dependencies/Collections/Internal/ArraySortHelper.cs
+++ src/Dependencies/Collections/Internal/ArraySortHelper.cs
@@ -1,6 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// NOTE: This code is derived from an implementation originally in dotnet/runtime:
+// https://github.com/dotnet/runtime/blob/v5.0.2/src/libraries/System.Private.CoreLib/src/System/Collections/Generic/ArraySortHelper.cs
+//
+// See the commentary in https://github.com/dotnet/roslyn/pull/50156 for notes on incorporating changes made to the
+// reference implementation.
+
 using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.CompilerServices;
