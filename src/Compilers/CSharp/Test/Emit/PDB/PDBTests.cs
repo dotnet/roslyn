8113a0ede1ef6be6effde1b7362f02a83e93cb4b
Petr Onderka <petro@postsharp.net>
2021-02-10 17:14:29 +0100

    More fixes

--- src/Compilers/CSharp/Test/Emit/PDB/PDBTests.cs
+++ src/Compilers/CSharp/Test/Emit/PDB/PDBTests.cs
@@ -552,9 +552,11 @@ public class C
             var d_int_g_int = d_int_g.Construct(stInt);
 
             var result = c1.Emit(new MemoryStream(), new MemoryStream(), debugEntryPoint: f2.GetPublicSymbol());
-            result.Diagnostics.Verify(
-                // error CS8096: Debug entry point must be a definition of a source method in the current compilation.
-                Diagnostic(ErrorCode.ERR_DebugEntryPointNotSourceMethodDefinition));
+            // <Caravela>: Caravela allows debugEntryPoint from a different compilation, because it needs to map it from original compilation to transformed compilation
+            //result.Diagnostics.Verify(
+            //    // error CS8096: Debug entry point must be a definition of a source method in the current compilation.
+            //    Diagnostic(ErrorCode.ERR_DebugEntryPointNotSourceMethodDefinition));
+            // </Caravela>
 
             result = c1.Emit(new MemoryStream(), new MemoryStream(), debugEntryPoint: d_t_g_int.GetPublicSymbol());
             result.Diagnostics.Verify(
