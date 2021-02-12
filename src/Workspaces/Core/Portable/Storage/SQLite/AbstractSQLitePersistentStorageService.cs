f182ee8dc46525d757cafdda519ea3fd26e5e029
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-02-11 16:06:56 -0800

    Author according to sam.

--- src/Workspaces/Core/Portable/Storage/SQLite/AbstractSQLitePersistentStorageService.cs
+++ src/Workspaces/Core/Portable/Storage/SQLite/AbstractSQLitePersistentStorageService.cs
@@ -25,26 +25,6 @@ namespace Microsoft.CodeAnalysis.SQLite
 
         private static bool TryInitializeLibrariesLazy()
         {
-            // Attempt to load the correct version of e_sqlite.dll.  That way when we call
-            // into SQLitePCL.Batteries_V2.Init it will be able to find it.
-            //
-            // Only do this on Windows when we can safely do the LoadLibrary call to this
-            // direct dll.  On other platforms, it is the responsibility of the host to ensure
-            // that the necessary sqlite library has already been loaded such that SQLitePCL.Batteries_V2
-            // will be able to call into it.
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                var myFolder = Path.GetDirectoryName(
-                    typeof(AbstractSQLitePersistentStorageService).Assembly.Location);
-                if (myFolder == null)
-                    return false;
-
-                var is64 = IntPtr.Size == 8;
-                var subfolder = is64 ? "x64" : "x86";
-
-                LoadLibrary(Path.Combine(myFolder, subfolder, "e_sqlite3.dll"));
-            }
-
             try
             {
                 // Necessary to initialize SQLitePCL.
