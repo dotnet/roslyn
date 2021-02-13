b948ec115279c42181e3d87a1dfc41be10c2505d
tmat <tomas.matousek@microsoft.com>
2021-02-13 13:28:54 -0800

    fix

--- src/EditorFeatures/CSharpTest/Completion/CompletionProviders/LoadDirectiveCompletionProviderTests.cs
+++ src/EditorFeatures/CSharpTest/Completion/CompletionProviders/LoadDirectiveCompletionProviderTests.cs
@@ -68,7 +68,6 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionPr
         [InlineData(" # load \"$$\"", true)]
         [InlineData(" # load \"\"$$", true)]
         [InlineData("$$ # load \"\"", false)]
-        [InlineData(" # load \"\"$$", false)]
         [InlineData(" # load $$\"\"", false)]
         public void ShouldTriggerCompletion(string textWithPositionMarker, bool expectedResult)
         {
