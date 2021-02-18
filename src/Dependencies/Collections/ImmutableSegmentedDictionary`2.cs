1d904e0def5e0d6352587ae9a04ba68119ecec59
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-02-17 23:56:10 -0800

    Disable

--- src/Dependencies/Collections/ImmutableSegmentedDictionary`2.cs
+++ src/Dependencies/Collections/ImmutableSegmentedDictionary`2.cs
@@ -2,6 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if CODE_STYLE
+#pragma warning disable CS8767
+#endif
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
