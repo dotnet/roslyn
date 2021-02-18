449c5edb57bff5ec925a3f4a5a20ea686e4e0ebf
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-02-18 13:48:19 -0800

    REmove pragmas

--- src/Dependencies/Collections/ImmutableSegmentedDictionary`2+Builder+KeyCollection.cs
+++ src/Dependencies/Collections/ImmutableSegmentedDictionary`2+Builder+KeyCollection.cs
@@ -2,11 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if CODE_STYLE
-#pragma warning disable CS8601
-#pragma warning disable CS8618
-#endif
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- src/Dependencies/Collections/ImmutableSegmentedDictionary`2.cs
+++ src/Dependencies/Collections/ImmutableSegmentedDictionary`2.cs
@@ -2,10 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if CODE_STYLE
-#pragma warning disable CS8767
-#endif
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
