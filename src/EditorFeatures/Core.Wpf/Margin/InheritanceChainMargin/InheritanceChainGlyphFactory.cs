8ffaf713ffb5cc74b734a4d71b6d9dda6c1331a3
Shen Chen <cos_ifne@hotmail.com>
2021-02-22 15:31:10 -0800

    Initial commit

---
+++ src/EditorFeatures/Core.Wpf/Margin/InheritanceChainMargin/InheritanceChainGlyphFactory.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+
+namespace Microsoft.CodeAnalysis.Editor.Margin.InheritanceChainMargin
+{
+    public class InheritanceChainGlyphFactory : IGlyphFactory
+    {
+        public UIElement GenerateGlyph(IWpfTextViewLine line, IGlyphTag tag)
+        {
+            return new InheritanceChainMargin();
+        }
+    }
+}
---
+++ src/EditorFeatures/Core.Wpf/Margin/InheritanceChainMargin/InheritanceChainMarginFactoryProvider.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.CodeAnalysis.Editor.Margin.InheritanceChainMargin
+{
+    [Export(typeof(IGlyphFactoryProvider))]
+    [Name(nameof(InheritanceChainMarginFactoryProvider))]
+    // TODO: Figure out the proper margin order
+    // [Order(After = "VsTextMarker")]
+    [ContentType(ContentTypeNames.CSharpContentType)]
+    internal class InheritanceChainMarginFactoryProvider : IGlyphFactoryProvider
+    {
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public InheritanceChainMarginFactoryProvider()
+        {
+        }
+
+        public IGlyphFactory GetGlyphFactory(IWpfTextView view, IWpfTextViewMargin margin)
+        {
+            return new InheritanceChainGlyphFactory();
+        }
+    }
+}
