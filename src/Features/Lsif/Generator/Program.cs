53156664ef4a90665439bd0b1e08fb9b957f2036
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2021-03-01 16:51:48 -0800

    Update src/Features/Lsif/Generator/Program.cs
    Co-authored-by: Jason Malinowski <jason@jason-m.com>

--- src/Features/Lsif/Generator/Program.cs
+++ src/Features/Lsif/Generator/Program.cs
@@ -131,7 +131,7 @@ namespace Microsoft.CodeAnalysis.LanguageServerIndexFormat.Generator
             var solutionLoadStopwatch = Stopwatch.StartNew();
 
             var msbuildWorkspace = MSBuildWorkspace.Create();
-            msbuildWorkspace.WorkspaceFailed += (s, e) => logFile.WriteLine("Error while loading the solution: " + e.Diagnostic.Message);
+            msbuildWorkspace.WorkspaceFailed += (s, e) => logFile.WriteLine("Error while loading: " + e.Diagnostic.Message);
 
             var solution = await openAsync(msbuildWorkspace);
 
