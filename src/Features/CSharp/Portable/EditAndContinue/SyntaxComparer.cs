05481ce301c7c821b749c6b39113d9980517f426
David Wengier <david.wengier@microsoft.com>
2021-03-02 16:09:13 +1100

    Classify records

--- src/Features/CSharp/Portable/EditAndContinue/SyntaxComparer.cs
+++ src/Features/CSharp/Portable/EditAndContinue/SyntaxComparer.cs
@@ -441,10 +441,10 @@ namespace Microsoft.CodeAnalysis.CSharp.EditAndContinue
                 case SyntaxKind.NamespaceDeclaration:
                     return Label.NamespaceDeclaration;
 
-                // Need to add support for records (tracked by https://github.com/dotnet/roslyn/issues/44877)
                 case SyntaxKind.ClassDeclaration:
                 case SyntaxKind.StructDeclaration:
                 case SyntaxKind.InterfaceDeclaration:
+                case SyntaxKind.RecordDeclaration:
                     return Label.TypeDeclaration;
 
                 case SyntaxKind.EnumDeclaration:
@@ -1498,10 +1498,10 @@ namespace Microsoft.CodeAnalysis.CSharp.EditAndContinue
                 case SyntaxKind.NamespaceDeclaration:
                     return ((NamespaceDeclarationSyntax)node).Name;
 
-                // Need to add support for records (tracked by https://github.com/dotnet/roslyn/issues/44877)
                 case SyntaxKind.ClassDeclaration:
                 case SyntaxKind.StructDeclaration:
                 case SyntaxKind.InterfaceDeclaration:
+                case SyntaxKind.RecordDeclaration:
                     return ((TypeDeclarationSyntax)node).Identifier;
 
                 case SyntaxKind.EnumDeclaration:
