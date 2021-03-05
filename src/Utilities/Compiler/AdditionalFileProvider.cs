ab9e61adf3066c5d1e2591dae1c87ecf748fe762
Youssef Victor <youssefvictor00@gmail.com>
2021-03-05 18:04:44 +0200

    IDE0090: FixAll

--- src/Utilities/Compiler/AdditionalFileProvider.cs
+++ src/Utilities/Compiler/AdditionalFileProvider.cs
@@ -30,7 +30,7 @@ namespace Analyzer.Utilities
         /// <param name="options">Options passed to a <see cref="DiagnosticAnalyzer"/>.</param>
         /// <returns>An instance of <see cref="AdditionalFileProvider"/>.</returns>
         public static AdditionalFileProvider FromOptions(AnalyzerOptions options)
-            => new AdditionalFileProvider(options.AdditionalFiles);
+            => new(options.AdditionalFiles);
 
         /// <summary>
         /// Returns the first additional file whose name is the specified <paramref name="fileName"/>.
