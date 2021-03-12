aa65c9f36896655be9caf382434a176be21a75d9
Marco Goertz <mgoertz@microsoft.com>
2021-03-12 11:55:56 -0800

    Updated XAML QuickInfo to show more info like C# by using ISymbolDisplayService and adding more documentation parts.

--- src/VisualStudio/Xaml/Impl/Implementation/LanguageServer/Extensions/SymbolExtensions.cs
+++ src/VisualStudio/Xaml/Impl/Implementation/LanguageServer/Extensions/SymbolExtensions.cs
@@ -5,28 +5,21 @@
 #nullable enable
 
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.DocumentationComments;
 using Microsoft.CodeAnalysis.Editor.Xaml;
+using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.VisualStudio.LanguageServices.Xaml.Implementation.LanguageServer.Extensions
 {
     internal static class SymbolExtensions
     {
-        private static readonly SymbolDisplayFormat s_descriptionStyle =
-            new SymbolDisplayFormat(
-                typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
-                delegateStyle: SymbolDisplayDelegateStyle.NameAndSignature,
-                genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters | SymbolDisplayGenericsOptions.IncludeVariance | SymbolDisplayGenericsOptions.IncludeTypeConstraints,
-                parameterOptions: SymbolDisplayParameterOptions.IncludeType | SymbolDisplayParameterOptions.IncludeName | SymbolDisplayParameterOptions.IncludeParamsRefOut,
-                miscellaneousOptions: SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers,
-                kindOptions: SymbolDisplayKindOptions.IncludeNamespaceKeyword | SymbolDisplayKindOptions.IncludeTypeKeyword);
-
-        public static async Task<IEnumerable<TaggedText>> GetDescriptionAsync(this ISymbol symbol, TextDocument document, int offset, CancellationToken cancellationToken)
+        public static async Task<IEnumerable<TaggedText>> GetDescriptionAsync(this ISymbol symbol, TextDocument document, CancellationToken cancellationToken)
         {
             if (symbol == null)
             {
@@ -40,7 +33,13 @@ namespace Microsoft.VisualStudio.LanguageServices.Xaml.Implementation.LanguageSe
                 return Enumerable.Empty<TaggedText>();
             }
 
-            // TODO: Should we get this from the code-behind document instead?
+            var symbolDisplayService = codeProject.LanguageServices.GetService<ISymbolDisplayService>();
+            if (symbolDisplayService == null)
+            {
+                return Enumerable.Empty<TaggedText>();
+            }
+
+            // Any code document will do
             var codeDocument = codeProject.Documents.FirstOrDefault();
             if (codeDocument == null)
             {
@@ -53,16 +52,51 @@ namespace Microsoft.VisualStudio.LanguageServices.Xaml.Implementation.LanguageSe
                 return Enumerable.Empty<TaggedText>();
             }
 
-            var textContentBuilder = new List<TaggedText>();
-            textContentBuilder.AddRange(symbol.ToDisplayParts(s_descriptionStyle).ToTaggedText());
+            var description = await symbolDisplayService.ToDescriptionPartsAsync(codeProject.Solution.Workspace, semanticModel, 0, ImmutableArray.Create(symbol), SymbolDescriptionGroups.MainDescription, cancellationToken).ConfigureAwait(false);
 
-            var documentation = symbol.GetDocumentationParts(semanticModel, offset, formatter, cancellationToken);
+            var builder = new List<TaggedText>();
+            builder.AddRange(description.ToTaggedText());
+
+            var documentation = symbol.GetDocumentationParts(semanticModel, 0, formatter, cancellationToken);
             if (documentation.Any())
             {
-                textContentBuilder.AddLineBreak();
-                textContentBuilder.AddRange(documentation);
+                builder.AddLineBreak();
+                builder.AddRange(documentation);
             }
-            return textContentBuilder;
+
+            var remarksDocumentation = symbol.GetRemarksDocumentationParts(semanticModel, 0, formatter, cancellationToken);
+            if (remarksDocumentation.Any())
+            {
+                builder.AddLineBreak();
+                builder.AddLineBreak();
+                builder.AddRange(remarksDocumentation);
+            }
+
+            var returnsDocumentation = symbol.GetReturnsDocumentationParts(semanticModel, 0, formatter, cancellationToken);
+            if (returnsDocumentation.Any())
+            {
+                builder.AddLineBreak();
+                builder.AddLineBreak();
+                builder.AddText(FeaturesResources.Returns_colon);
+                builder.AddLineBreak();
+                builder.Add(new TaggedText(TextTags.ContainerStart, "  "));
+                builder.AddRange(returnsDocumentation);
+                builder.Add(new TaggedText(TextTags.ContainerEnd, string.Empty));
+            }
+
+            var valueDocumentation = symbol.GetValueDocumentationParts(semanticModel, 0, formatter, cancellationToken);
+            if (valueDocumentation.Any())
+            {
+                builder.AddLineBreak();
+                builder.AddLineBreak();
+                builder.AddText(FeaturesResources.Value_colon);
+                builder.AddLineBreak();
+                builder.Add(new TaggedText(TextTags.ContainerStart, "  "));
+                builder.AddRange(valueDocumentation);
+                builder.Add(new TaggedText(TextTags.ContainerEnd, string.Empty));
+            }
+
+            return builder;
         }
     }
 }
