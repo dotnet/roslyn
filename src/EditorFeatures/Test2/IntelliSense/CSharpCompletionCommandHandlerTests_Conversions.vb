fae2b4e2696304ea9262c8621e48a0270b17ed9f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-03-13 10:09:23 -0800

    Add test

--- src/EditorFeatures/Test2/IntelliSense/CSharpCompletionCommandHandlerTests_Conversions.vb
+++ src/EditorFeatures/Test2/IntelliSense/CSharpCompletionCommandHandlerTests_Conversions.vb
@@ -29,5 +29,30 @@ class C
                 Assert.Equal(state.GetLineFromCurrentCaretPosition().End, state.GetCaretPoint().BufferPosition)
             End Using
         End Function
+
+        <WpfTheory, CombinatorialData, Trait(Traits.Feature, Traits.Features.Completion)>
+        Public Async Function BuiltInConversion_BetweenDots(showCompletionInArgumentLists As Boolean) As Task
+            Using state = TestStateFactory.CreateCSharpTestState(
+                <Document><![CDATA[
+using System.Text.RegularExpressions;
+class C
+{
+    void goo()
+    {
+        var x = 0;
+        var y = x.$$.ToString();
+    }
+}
+]]></Document>, showCompletionInArgumentLists:=showCompletionInArgumentLists)
+
+                state.SendInvokeCompletionList()
+                state.SendTypeChars("by")
+                Await state.AssertSelectedCompletionItem("(byte)")
+                state.SendTab()
+                Await state.AssertNoCompletionSession()
+                Assert.Equal("        var y = ((byte)x).ToString();", state.GetLineTextFromCaretPosition())
+                Assert.Equal(".", state.GetCaretPoint().BufferPosition.GetChar())
+            End Using
+        End Function
     End Class
 End Namespace
