5bf3abe38c5f594e681eda18da41f6e7edb920d8
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-03-15 12:44:34 -0700

    Less allocations

--- src/Features/Core/Portable/ReassignedVariable/AbstractReassignedVariableService.cs
+++ src/Features/Core/Portable/ReassignedVariable/AbstractReassignedVariableService.cs
@@ -11,6 +11,7 @@ using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.SQLite.Interop;
 using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
 
@@ -40,187 +41,189 @@ namespace Microsoft.CodeAnalysis.ReassignedVariable
 
             using var _1 = PooledDictionary<ISymbol, bool>.GetInstance(out var symbolToIsReassigned);
             using var _2 = ArrayBuilder<TextSpan>.GetInstance(out var result);
+            using var _3 = ArrayBuilder<SyntaxNode>.GetInstance(out var stack);
 
-            var identifiers = root.DescendantNodesAndSelf(span).OfType<TIdentifierNameSyntax>();
-            foreach (var identifier in identifiers)
+            // First, walk through all identifiers in the provided span.  If they refer to a local/param, then determine
+            // if that's a local/param that's reassigned or not.  Note that the local/param they refer to may not be in the
+            // span being asked about.
+            stack.Add(root.FindNode(span));
+
+            while (stack.Count > 0)
             {
-                if (await IsReassignedAsync(semanticFacts, syntaxFacts, semanticModel, identifier, symbolToIsReassigned, cancellationToken).ConfigureAwait(false))
-                    result.Add(identifier.Span);
-            }
+                var current = stack.Last();
+                stack.RemoveLast();
 
-            var parameterDecls = root.DescendantNodes(span).OfType<TParameterSyntax>();
-            foreach (var decl in parameterDecls)
-            {
-                var parameter = semanticModel.GetDeclaredSymbol(decl, cancellationToken) as IParameterSymbol;
-                if (parameter == null || parameter.Locations.Length == 0)
-                    continue;
+                if (current.Span.IntersectsWith(span))
+                {
+                    ProcessNode(current);
 
-                Contract.ThrowIfNull(parameter);
-
-                var isReassigned = await IsReassignedAsync(
-                    semanticFacts, syntaxFacts, semanticModel, symbolToIsReassigned, parameter, cancellationToken).ConfigureAwait(false);
-                if (isReassigned)
-                    result.Add(syntaxFacts.GetIdentifierOfParameter(decl).Span);
-            }
-
-            var variableDecls = root.DescendantNodes(span).OfType<TVariableDeclaratorSyntax>();
-            foreach (var decl in variableDecls)
-            {
-                var local = semanticModel.GetDeclaredSymbol(decl, cancellationToken);
-                if (local == null || local.Locations.Length == 0)
-                    continue;
-
-                var isReassigned = await IsReassignedAsync(
-                    semanticFacts, syntaxFacts, semanticModel, symbolToIsReassigned, local, cancellationToken).ConfigureAwait(false);
-                if (isReassigned)
-                    result.Add(syntaxFacts.GetIdentifierOfVariableDeclarator(decl).Span);
+                    foreach (var child in current.ChildNodesAndTokens())
+                    {
+                        if (child.IsNode)
+                            stack.Add(child.AsNode()!);
+                    }
+                }
             }
 
             return result.ToImmutable();
-        }
 
-        private Task<bool> IsReassignedAsync(
-            ISemanticFactsService semanticFacts,
-            ISyntaxFactsService syntaxFacts,
-            SemanticModel semanticModel,
-            TIdentifierNameSyntax identifier,
-            Dictionary<ISymbol, bool> symbolToIsReassigned,
-            CancellationToken cancellationToken)
-        {
-            var symbol = semanticModel.GetSymbolInfo(identifier, cancellationToken).Symbol;
-            if (symbol == null)
-                return SpecializedTasks.False;
-
-            return IsReassignedAsync(
-                semanticFacts, syntaxFacts, semanticModel, symbolToIsReassigned, symbol, cancellationToken);
-        }
-
-        private async Task<bool> IsReassignedAsync(
-            ISemanticFactsService semanticFacts,
-            ISyntaxFactsService syntaxFacts,
-            SemanticModel semanticModel,
-            Dictionary<ISymbol, bool> symbolToIsReassigned,
-            ISymbol symbol,
-            CancellationToken cancellationToken)
-        {
-            // Note: we don't need to test range variables, and they are never reassignable.
-            if (symbol is not IParameterSymbol and not ILocalSymbol)
-                return false;
-
-            if (!symbolToIsReassigned.TryGetValue(symbol, out var reassignedResult))
+            void ProcessNode(SyntaxNode node)
             {
-                reassignedResult = await ComputeIsAssignedAsync(
-                    semanticFacts, syntaxFacts, semanticModel, symbol, symbolToIsReassigned, cancellationToken).ConfigureAwait(false);
-                symbolToIsReassigned[symbol] = reassignedResult;
+                switch (node)
+                {
+                    case TIdentifierNameSyntax identifier:
+                        ProcessIdentifier(identifier);
+                        break;
+                    case TParameterSyntax parameter:
+                        ProcessParameter(parameter);
+                        break;
+                    case TVariableDeclaratorSyntax variable:
+                        ProcessVariable(variable);
+                        break;
+                }
             }
 
-            return reassignedResult;
-        }
-
-        private Task<bool> ComputeIsAssignedAsync(
-            ISemanticFactsService semanticFacts,
-            ISyntaxFactsService syntaxFacts,
-            SemanticModel semanticModel,
-            ISymbol symbol,
-            Dictionary<ISymbol, bool> symbolToIsReassigned,
-            CancellationToken cancellationToken)
-        {
-            return symbol switch
+            void ProcessIdentifier(TIdentifierNameSyntax identifier)
             {
-                IParameterSymbol parameter => ComputeParameterIsAssignedAsync(semanticModel, parameter, symbolToIsReassigned, cancellationToken),
-                ILocalSymbol local => ComputeLocalIsAssignedAsync(semanticFacts, syntaxFacts, semanticModel, local, cancellationToken),
-                _ => throw ExceptionUtilities.UnexpectedValue(symbol.Kind),
-            };
-        }
-
-        private async Task<bool> ComputeParameterIsAssignedAsync(
-            SemanticModel semanticModel,
-            IParameterSymbol parameter,
-            Dictionary<ISymbol, bool> symbolToIsReassigned,
-            CancellationToken cancellationToken)
-        {
-            // Parameters are an easy case.  We just need to get the method they're defined for, and see if they are
-            // even written to inside that method.
-
-            if (parameter.ContainingSymbol is not IMethodSymbol method)
-                return false;
-
-            if (method.DeclaringSyntaxReferences.Length == 0)
-                return false;
-
-            var methodDeclaration = await method.DeclaringSyntaxReferences.First().GetSyntaxAsync(cancellationToken).ConfigureAwait(false);
-
-            // Potentially a reference to a parameter in another file.
-            if (methodDeclaration.SyntaxTree != semanticModel.SyntaxTree)
-                return false;
-
-            var methodBlock = GetMethodBlock(methodDeclaration);
-            var dataFlow = AnalyzeMethodBodyDataFlow(semanticModel, methodBlock, cancellationToken);
-            if (dataFlow == null)
-                return false;
-
-            foreach (var methodParam in method.Parameters)
-                symbolToIsReassigned[methodParam] = dataFlow.WrittenInside.Contains(methodParam);
-
-            return symbolToIsReassigned.TryGetValue(parameter, out var result) && result;
-        }
-
-        private async Task<bool> ComputeLocalIsAssignedAsync(
-            ISemanticFactsService semanticFacts,
-            ISyntaxFactsService syntaxFacts,
-            SemanticModel semanticModel,
-            ILocalSymbol local,
-            CancellationToken cancellationToken)
-        {
-            // Locals are harder to determine than parameters.  Because a parameter always comes in assigned, we only
-            // have to see if there is a later assignment.  For locals though, they may start unassigned, and then only
-            // get assigned once later.  That would not count as a reassignment.  So we only want to consider something
-            // a reassignment, if it is *already* assigned, and then written again.
-
-            if (local.DeclaringSyntaxReferences.Length == 0)
-                return false;
-
-            var localDeclaration = await local.DeclaringSyntaxReferences.First().GetSyntaxAsync(cancellationToken).ConfigureAwait(false);
-
-            if (localDeclaration.SyntaxTree != semanticModel.SyntaxTree)
-            {
-                Contract.Fail("Local did not come from same file that we were analyzing?");
-                return false;
+                if (IsIdentifierReassigned(identifier))
+                    result.Add(identifier.Span);
             }
 
-            // Get the scope the local is declared in.
-            var parentScope = GetParentScope(localDeclaration);
-
-            // Now, walk the scope, looking for all usages of the local.  See if any are a reassignment.
-            foreach (var id in parentScope.DescendantNodes().OfType<TIdentifierNameSyntax>())
+            void ProcessParameter(TParameterSyntax parameterSyntax)
             {
-                // Ignore any nodes before the local decl.
-                if (id.SpanStart <= localDeclaration.SpanStart)
-                    continue;
-
-                // Ignore identifiers that don't match the local name.
-                var idToken = syntaxFacts.GetIdentifierOfSimpleName(id);
-                if (!syntaxFacts.StringComparer.Equals(idToken.ValueText, local.Name))
-                    continue;
-
-                // Ignore identifiers that bind to another symbol.
-                var symbol = semanticModel.GetSymbolInfo(id, cancellationToken).Symbol;
-                if (!local.Equals(symbol))
-                    continue;
-
-                // Ok, we have a reference to the local.  See if it was assigned on entry.  If not, we don't care about
-                // this reference. As an assignment here doesn't mean it was reassigned.
-                var dataFlow = semanticModel.AnalyzeDataFlow(id);
-                if (!dataFlow.DefinitelyAssignedOnEntry.Contains(local))
-                    continue;
-
-                // This was a variable that was already assigned prior to this location.  See if this location is
-                // considered a write.
-                if (semanticFacts.IsWrittenTo(semanticModel, id, cancellationToken))
-                    return true;
+                var parameter = semanticModel.GetDeclaredSymbol(parameterSyntax, cancellationToken);
+                if (IsSymbolReassigned(parameter))
+                    result.Add(syntaxFacts.GetIdentifierOfParameter(parameterSyntax).Span);
             }
 
-            return false;
+            void ProcessVariable(TVariableDeclaratorSyntax variable)
+            {
+                var local = semanticModel.GetDeclaredSymbol(variable, cancellationToken);
+                if (IsSymbolReassigned(local))
+                    result.Add(syntaxFacts.GetIdentifierOfVariableDeclarator(variable).Span);
+            }
+
+            bool IsIdentifierReassigned(TIdentifierNameSyntax identifier)
+            {
+                var symbol = semanticModel.GetSymbolInfo(identifier, cancellationToken).Symbol;
+                if (symbol == null)
+                    return false;
+
+                return IsSymbolReassigned(symbol);
+            }
+
+            bool IsSymbolReassigned(ISymbol symbol)
+            {
+                // Note: we don't need to test range variables, and they are never reassignable.
+                if (symbol is not IParameterSymbol and not ILocalSymbol)
+                    return false;
+
+                // reference to a symbol not even in this document.  ignore this.
+                if (symbol.Locations.Length <= 0 ||
+                    symbol.Locations[0].SourceTree != semanticModel.SyntaxTree)
+                {
+                    return false;
+                }
+
+                if (!symbolToIsReassigned.TryGetValue(symbol, out var reassignedResult))
+                {
+                    reassignedResult = ComputeIsAssigned(symbol);
+                    symbolToIsReassigned[symbol] = reassignedResult;
+                }
+
+                return reassignedResult;
+            }
+
+            bool ComputeIsAssigned(ISymbol symbol)
+            {
+                return symbol switch
+                {
+                    IParameterSymbol parameter => ComputeParameterIsAssigned(parameter),
+                    ILocalSymbol local => ComputeLocalIsAssigned(local),
+                    _ => throw ExceptionUtilities.UnexpectedValue(symbol.Kind),
+                };
+            }
+
+            bool ComputeParameterIsAssigned(IParameterSymbol parameter)
+            {
+                // Parameters are an easy case.  We just need to get the method they're defined for, and see if they are
+                // even written to inside that method.
+
+                if (parameter.ContainingSymbol is not IMethodSymbol method)
+                    return false;
+
+                if (method.DeclaringSyntaxReferences.Length == 0)
+                    return false;
+
+                var methodDeclaration = method.DeclaringSyntaxReferences.First().GetSyntax(cancellationToken);
+
+                // Potentially a reference to a parameter in another file.
+                if (methodDeclaration.SyntaxTree != semanticModel.SyntaxTree)
+                    return false;
+
+                var methodBlock = GetMethodBlock(methodDeclaration);
+                var dataFlow = AnalyzeMethodBodyDataFlow(semanticModel, methodBlock, cancellationToken);
+                if (dataFlow == null)
+                    return false;
+
+                foreach (var methodParam in method.Parameters)
+                    symbolToIsReassigned[methodParam] = dataFlow.WrittenInside.Contains(methodParam);
+
+                return symbolToIsReassigned.TryGetValue(parameter, out var result) && result;
+            }
+
+            bool ComputeLocalIsAssigned(ILocalSymbol local)
+            {
+                // Locals are harder to determine than parameters.  Because a parameter always comes in assigned, we only
+                // have to see if there is a later assignment.  For locals though, they may start unassigned, and then only
+                // get assigned once later.  That would not count as a reassignment.  So we only want to consider something
+                // a reassignment, if it is *already* assigned, and then written again.
+
+                if (local.DeclaringSyntaxReferences.Length == 0)
+                    return false;
+
+                var localDeclaration = local.DeclaringSyntaxReferences.First().GetSyntax(cancellationToken);
+
+                if (localDeclaration.SyntaxTree != semanticModel.SyntaxTree)
+                {
+                    Contract.Fail("Local did not come from same file that we were analyzing?");
+                    return false;
+                }
+
+                // Get the scope the local is declared in.
+                var parentScope = GetParentScope(localDeclaration);
+
+                // Now, walk the scope, looking for all usages of the local.  See if any are a reassignment.
+                foreach (var id in parentScope.DescendantNodes().OfType<TIdentifierNameSyntax>())
+                {
+                    // Ignore any nodes before the local decl.
+                    if (id.SpanStart <= localDeclaration.SpanStart)
+                        continue;
+
+                    // Ignore identifiers that don't match the local name.
+                    var idToken = syntaxFacts.GetIdentifierOfSimpleName(id);
+                    if (!syntaxFacts.StringComparer.Equals(idToken.ValueText, local.Name))
+                        continue;
+
+                    // Ignore identifiers that bind to another symbol.
+                    var symbol = semanticModel.GetSymbolInfo(id, cancellationToken).Symbol;
+                    if (!local.Equals(symbol))
+                        continue;
+
+                    // Ok, we have a reference to the local.  See if it was assigned on entry.  If not, we don't care about
+                    // this reference. As an assignment here doesn't mean it was reassigned.
+                    var dataFlow = semanticModel.AnalyzeDataFlow(id);
+                    if (!dataFlow.DefinitelyAssignedOnEntry.Contains(local))
+                        continue;
+
+                    // This was a variable that was already assigned prior to this location.  See if this location is
+                    // considered a write.
+                    if (semanticFacts.IsWrittenTo(semanticModel, id, cancellationToken))
+                        return true;
+                }
+
+                return false;
+            }
         }
     }
 }
