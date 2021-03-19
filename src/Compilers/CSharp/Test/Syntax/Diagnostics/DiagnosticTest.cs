174d1cab5ccea2017cc4ba5406d7fe4cf8a3e22b
Youssef Victor <youssefvictor00@gmail.com>
2021-03-19 19:56:54 +0000

    Fix NullableWarnings test

--- src/Compilers/CSharp/Test/Syntax/Diagnostics/DiagnosticTest.cs
+++ src/Compilers/CSharp/Test/Syntax/Diagnostics/DiagnosticTest.cs
@@ -419,6 +419,11 @@ class X
                     ErrorCode.WRN_AnalyzerReferencesFramework,
                     ErrorCode.WRN_UnreadRecordParameter,
                     ErrorCode.WRN_DoNotCompareFunctionPointers,
+                    ErrorCode.WRN_CallerArgumentExpressionParamForUnconsumedLocation,
+                    ErrorCode.WRN_CallerLineNumberPreferredOverCallerArgumentExpression,
+                    ErrorCode.WRN_CallerFilePathPreferredOverCallerArgumentExpression,
+                    ErrorCode.WRN_CallerMemberNamePreferredOverCallerArgumentExpression,
+                    ErrorCode.WRN_CallerArgumentExpressionAttributeHasInvalidParameterName,
                 };
 
                 Assert.Contains(error, nullableUnrelatedWarnings);
