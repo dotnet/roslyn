0b2c4c44fc579f1a15c6a2efffd43ec926a6ffed
Alireza Habibi <habibi.alireza@outlook.com>
2021-03-20 12:11:25 +0330

    Attempt to disable RS0016

---
+++ src/Compilers/CSharp/Portable/Syntax/RecursivePatternSyntax.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Microsoft.CodeAnalysis.CSharp.Syntax
+{
+    public partial class RecursivePatternSyntax
+    {
+        public RecursivePatternSyntax Update(TypeSyntax? type, PositionalPatternClauseSyntax? positionalPatternClause, PropertyPatternClauseSyntax? propertyPatternClause, VariableDesignationSyntax? designation)
+        {
+            return Update(type, positionalPatternClause, lengthPatternClause: null, propertyPatternClause, designation);
+        }
+    }
+}
+
+namespace Microsoft.CodeAnalysis.CSharp
+{
+    public partial class SyntaxFactory
+    {
+        public static RecursivePatternSyntax RecursivePattern(TypeSyntax? type, PositionalPatternClauseSyntax? positionalPatternClause, PropertyPatternClauseSyntax? propertyPatternClause, VariableDesignationSyntax? designation)
+        {
+            return RecursivePattern(type, positionalPatternClause, lengthPatternClause: null, propertyPatternClause, designation);
+        }
+    }
+}
