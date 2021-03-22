69b543e7da0f26ea25e6e7db6747f3287f29612b
Bernd Baumanns <baumannsbernd@gmail.com>
2021-03-22 21:29:41 +0100

    Update StackAllocArrayCreationExpressionSyntax.cs

--- src/Compilers/CSharp/Portable/Syntax/StackAllocArrayCreationExpressionSyntax.cs
+++ src/Compilers/CSharp/Portable/Syntax/StackAllocArrayCreationExpressionSyntax.cs
@@ -9,7 +9,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax
     public partial class StackAllocArrayCreationExpressionSyntax
     {
         public StackAllocArrayCreationExpressionSyntax Update(SyntaxToken stackAllocKeyword, TypeSyntax type)
-            => Update(StackAllocKeyword, type, initializer: Initializer);
+            => Update(StackAllocKeyword, type, Initializer);
     }
 }
 
