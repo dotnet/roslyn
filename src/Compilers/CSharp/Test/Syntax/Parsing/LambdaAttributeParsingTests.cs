36960b1d4c35e946b81917a838b4abb12ae1509f
Charles Stoner <chucks@microsoft.com>
2021-03-22 17:22:59 -0700

    Update PublicAPI.unshipped.txt

--- src/Compilers/CSharp/Test/Syntax/Parsing/LambdaAttributeParsingTests.cs
+++ src/Compilers/CSharp/Test/Syntax/Parsing/LambdaAttributeParsingTests.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Xunit;
 using Xunit.Abstractions;
@@ -14,12 +12,12 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests
     {
         public LambdaAttributeParsingTests(ITestOutputHelper output) : base(output) { }
 
-        protected override SyntaxTree ParseTree(string text, CSharpParseOptions options)
+        protected override SyntaxTree ParseTree(string text, CSharpParseOptions? options)
         {
             return SyntaxFactory.ParseSyntaxTree(text, options: options);
         }
 
-        protected override CSharpSyntaxNode ParseNode(string text, CSharpParseOptions options)
+        protected override CSharpSyntaxNode ParseNode(string text, CSharpParseOptions? options)
         {
             return SyntaxFactory.ParseExpression(text, options: options);
         }
