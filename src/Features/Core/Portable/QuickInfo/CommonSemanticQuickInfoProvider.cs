7507fd083b96500ff6962add13ce213d717504ba
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-03-23 17:18:13 -0700

    Extract out common QI code for Xaml to use.

--- src/Features/Core/Portable/QuickInfo/CommonSemanticQuickInfoProvider.cs
+++ src/Features/Core/Portable/QuickInfo/CommonSemanticQuickInfoProvider.cs
@@ -13,6 +13,7 @@ using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Shared.Utilities;
 using Microsoft.CodeAnalysis.Tags;
+using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.QuickInfo
@@ -146,7 +147,7 @@ namespace Microsoft.CodeAnalysis.QuickInfo
             return default;
         }
 
-        protected static async Task<QuickInfoItem> CreateContentAsync(
+        protected static Task<QuickInfoItem> CreateContentAsync(
             Workspace workspace,
             SyntaxToken token,
             SemanticModel semanticModel,
@@ -155,10 +156,7 @@ namespace Microsoft.CodeAnalysis.QuickInfo
             CancellationToken cancellationToken)
         {
             var descriptionService = workspace.Services.GetLanguageServices(token.Language).GetRequiredService<ISymbolDisplayService>();
-            var formatter = workspace.Services.GetLanguageServices(semanticModel.Language).GetRequiredService<IDocumentationCommentFormattingService>();
             var syntaxFactsService = workspace.Services.GetLanguageServices(semanticModel.Language).GetRequiredService<ISyntaxFactsService>();
-
-            var showWarningGlyph = supportedPlatforms != null && supportedPlatforms.HasValidAndInvalidProjects();
 
             var symbols = tokenInformation.Symbols;
 
@@ -171,159 +169,9 @@ namespace Microsoft.CodeAnalysis.QuickInfo
                     s2.Kind == SymbolKind.NamedType ? 1 : 0).ToImmutableArray();
             }
 
-            var groups = await descriptionService.ToDescriptionGroupsAsync(workspace, semanticModel, token.SpanStart, symbols, cancellationToken).ConfigureAwait(false);
-
-            var sections = ImmutableArray.CreateBuilder<QuickInfoSection>(initialCapacity: groups.Count);
-
-            var symbol = tokenInformation.Symbols.First();
-            if (tokenInformation.ShowAwaitReturn)
-            {
-                // We show a special message if the Task being awaited has no return
-                if (symbol is INamedTypeSymbol { SpecialType: SpecialType.System_Void })
-                {
-                    var builder = ImmutableArray.CreateBuilder<TaggedText>();
-                    builder.AddText(FeaturesResources.Awaited_task_returns_no_value);
-                    AddSection(QuickInfoSectionKinds.Description, builder.ToImmutable());
-                    return QuickInfoItem.Create(token.Span, sections: sections.ToImmutable());
-                }
-                else
-                {
-                    if (TryGetGroupText(SymbolDescriptionGroups.MainDescription, out var mainDescriptionTaggedParts))
-                    {
-                        // We'll take the existing message and wrap it with a message saying this was returned from the task.
-                        var defaultSymbol = "{0}";
-                        var symbolIndex = FeaturesResources.Awaited_task_returns_0.IndexOf(defaultSymbol);
-
-                        var builder = ImmutableArray.CreateBuilder<TaggedText>();
-                        builder.AddText(FeaturesResources.Awaited_task_returns_0.Substring(0, symbolIndex));
-                        builder.AddRange(mainDescriptionTaggedParts);
-                        builder.AddText(FeaturesResources.Awaited_task_returns_0[(symbolIndex + defaultSymbol.Length)..]);
-
-                        AddSection(QuickInfoSectionKinds.Description, builder.ToImmutable());
-                    }
-                }
-            }
-            else
-            {
-                if (TryGetGroupText(SymbolDescriptionGroups.MainDescription, out var mainDescriptionTaggedParts))
-                {
-                    AddSection(QuickInfoSectionKinds.Description, mainDescriptionTaggedParts);
-                }
-            }
-
-            if (groups.TryGetValue(SymbolDescriptionGroups.Documentation, out var docParts) && !docParts.IsDefaultOrEmpty)
-                AddSection(QuickInfoSectionKinds.DocumentationComments, docParts);
-
-            var remarksDocumentation = GetRemarksDocumentationContent(workspace, groups, semanticModel);
-            if (!remarksDocumentation.IsDefaultOrEmpty)
-            {
-                var builder = ImmutableArray.CreateBuilder<TaggedText>();
-                if (!docParts.IsDefaultOrEmpty)
-                    builder.AddLineBreak();
-
-                builder.AddRange(remarksDocumentation);
-                AddSection(QuickInfoSectionKinds.RemarksDocumentationComments, builder.ToImmutable());
-            }
-
-            if (groups.TryGetValue(SymbolDescriptionGroups.ReturnsDocumentation, out var returnsDocumentation) &&
-                !returnsDocumentation.IsDefaultOrEmpty)
-            {
-                var builder = ImmutableArray.CreateBuilder<TaggedText>();
-                builder.AddLineBreak();
-                builder.AddRange(returnsDocumentation);
-                AddSection(QuickInfoSectionKinds.ReturnsDocumentationComments, builder.ToImmutable());
-            }
-
-            if (groups.TryGetValue(SymbolDescriptionGroups.ValueDocumentation, out var valueDocumentation) &&
-                !valueDocumentation.IsDefaultOrEmpty)
-            {
-                var builder = ImmutableArray.CreateBuilder<TaggedText>();
-                builder.AddLineBreak();
-                builder.AddRange(valueDocumentation);
-                AddSection(QuickInfoSectionKinds.ValueDocumentationComments, builder.ToImmutable());
-            }
-
-            if (TryGetGroupText(SymbolDescriptionGroups.TypeParameterMap, out var typeParameterMapText))
-            {
-                var builder = ImmutableArray.CreateBuilder<TaggedText>();
-                builder.AddLineBreak();
-                builder.AddRange(typeParameterMapText);
-                AddSection(QuickInfoSectionKinds.TypeParameters, builder.ToImmutable());
-            }
-
-            if (TryGetGroupText(SymbolDescriptionGroups.AnonymousTypes, out var anonymousTypesText))
-            {
-                var builder = ImmutableArray.CreateBuilder<TaggedText>();
-                builder.AddLineBreak();
-                builder.AddRange(anonymousTypesText);
-                AddSection(QuickInfoSectionKinds.AnonymousTypes, builder.ToImmutable());
-            }
-
-            var usageTextBuilder = ImmutableArray.CreateBuilder<TaggedText>();
-            if (TryGetGroupText(SymbolDescriptionGroups.AwaitableUsageText, out var awaitableUsageText))
-            {
-                usageTextBuilder.AddRange(awaitableUsageText);
-            }
-
-            var nullableMessage = tokenInformation.NullableFlowState switch
-            {
-                NullableFlowState.MaybeNull => string.Format(FeaturesResources._0_may_be_null_here, symbol.Name),
-                NullableFlowState.NotNull => string.Format(FeaturesResources._0_is_not_null_here, symbol.Name),
-                _ => null
-            };
-
-            if (nullableMessage != null)
-            {
-                AddSection(QuickInfoSectionKinds.NullabilityAnalysis, ImmutableArray.Create(new TaggedText(TextTags.Text, nullableMessage)));
-            }
-
-            if (supportedPlatforms != null)
-            {
-                usageTextBuilder.AddRange(supportedPlatforms.ToDisplayParts().ToTaggedText());
-            }
-
-            if (usageTextBuilder.Count > 0)
-            {
-                AddSection(QuickInfoSectionKinds.Usage, usageTextBuilder.ToImmutable());
-            }
-
-            if (TryGetGroupText(SymbolDescriptionGroups.Exceptions, out var exceptionsText))
-            {
-                AddSection(QuickInfoSectionKinds.Exception, exceptionsText);
-            }
-
-            if (TryGetGroupText(SymbolDescriptionGroups.Captures, out var capturesText))
-            {
-                AddSection(QuickInfoSectionKinds.Captures, capturesText);
-            }
-
-            var tags = ImmutableArray.CreateRange(GlyphTags.GetTags(tokenInformation.Symbols.First().GetGlyph()));
-
-            if (showWarningGlyph)
-            {
-                tags = tags.Add(WellKnownTags.Warning);
-            }
-
-            return QuickInfoItem.Create(token.Span, tags, sections.ToImmutable());
-
-            bool TryGetGroupText(SymbolDescriptionGroups group, out ImmutableArray<TaggedText> taggedParts)
-                => groups.TryGetValue(group, out taggedParts) && !taggedParts.IsDefaultOrEmpty;
-
-            void AddSection(string kind, ImmutableArray<TaggedText> taggedParts)
-                => sections.Add(QuickInfoSection.Create(kind, taggedParts));
-        }
-
-        private static ImmutableArray<TaggedText> GetRemarksDocumentationContent(
-            Workspace workspace,
-            IDictionary<SymbolDescriptionGroups, ImmutableArray<TaggedText>> sections,
-            SemanticModel semanticModel)
-        {
-            if (!workspace.Options.GetOption(QuickInfoOptions.ShowRemarksInQuickInfo, semanticModel.Language))
-                return default;
-
-            return sections.TryGetValue(SymbolDescriptionGroups.RemarksDocumentation, out var parts)
-                ? parts
-                : default;
+            return QuickInfoUtilities.CreateQuickInfoItemAsync(
+                workspace, descriptionService, semanticModel, token.Span, symbols, supportedPlatforms,
+                tokenInformation.ShowAwaitReturn, tokenInformation.NullableFlowState, cancellationToken);
         }
 
         protected abstract bool GetBindableNodeForTokenIndicatingLambda(SyntaxToken token, [NotNullWhen(returnValue: true)] out SyntaxNode? found);
