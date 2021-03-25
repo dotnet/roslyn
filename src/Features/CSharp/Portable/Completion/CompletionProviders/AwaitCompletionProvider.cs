994932faefd768e8b824ce97fb1b278135c7a4ae
Youssef Victor <youssefvictor00@gmail.com>
2021-03-25 11:26:12 +0000

    Make single completion provider for await

--- src/Features/CSharp/Portable/Completion/CompletionProviders/AwaitCompletionProvider.cs
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/AwaitCompletionProvider.cs
@@ -8,6 +8,7 @@ using System.Composition;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.Completion.Providers;
 using Microsoft.CodeAnalysis.CSharp.Extensions;
 using Microsoft.CodeAnalysis.CSharp.Extensions.ContextQuery;
 using Microsoft.CodeAnalysis.Editing;
@@ -19,11 +20,19 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
 {
+    /// <summary>
+    /// A completion provider for offering <see langword="await"/> keyword.
+    /// This is implemented separately, not as a keyword recommender as it contains extra logic for making container method async.
+    /// </summary>
+    /// <remarks>
+    /// The container is made async if and only if the containing method is returning a Task-like type.
+    /// </remarks>
     [ExportCompletionProvider(nameof(AwaitCompletionProvider), LanguageNames.CSharp)]
-    [ExtensionOrder(After = nameof(KeywordCompletionProvider))]
     [Shared]
     internal sealed class AwaitCompletionProvider : LSPCompletionProvider
     {
+        private bool _shouldMakeContainerAsync = false;
+
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
         public AwaitCompletionProvider()
@@ -52,17 +61,36 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
                 var symbol = semanticModel.GetDeclaredSymbol(method, cancellationToken) as IMethodSymbol;
                 if (symbol is not null && IsTask(symbol.ReturnType))
                 {
-                    context.AddItem(CommonCompletionItem.Create("await", string.Empty, CompletionItemRules.Default, glyph: Glyph.Keyword, inlineDescription: CSharpFeaturesResources.Make_container_async));
+                    _shouldMakeContainerAsync = true;
+                    context.AddItem(GetCompletionItem());
+                    return;
                 }
             }
 
+            context.AddItem(GetCompletionItem());
+
+            // Local functions.
             static bool IsTask(ITypeSymbol returnType)
                 => returnType.Name is "Task" or "ValueTask";
+
+            CompletionItem GetCompletionItem() => CommonCompletionItem.Create(
+                displayText: SyntaxFacts.GetText(SyntaxKind.AwaitKeyword),
+                displayTextSuffix: "",
+                rules: CompletionItemRules.Default,
+                Glyph.Keyword,
+                description: RecommendedKeyword.CreateDisplayParts(SyntaxFacts.GetText(SyntaxKind.AwaitKeyword), string.Empty),
+                inlineDescription: _shouldMakeContainerAsync ? CSharpFeaturesResources.Make_container_async : null,
+                isComplexTextEdit: _shouldMakeContainerAsync);
         }
 
         internal override async Task<CompletionChange> GetChangeAsync(Document document, CompletionItem completionItem, TextSpan completionListSpan, char? commitKey, bool disallowAddingImports, CancellationToken cancellationToken)
         {
-            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            if (!_shouldMakeContainerAsync)
+            {
+                return await base.GetChangeAsync(document, completionItem, completionListSpan, commitKey, disallowAddingImports, cancellationToken).ConfigureAwait(false);
+            }
+
+            var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
             var declaration = root?.FindToken(completionListSpan.Start).GetAncestor(node => node.IsAsyncSupportingFunctionSyntax());
             if (root is null || declaration is null)
             {
@@ -74,7 +102,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
             var documentWithAsyncModifier = document.WithSyntaxRoot(root.ReplaceNode(declaration, AddAsyncModifier(document, declaration)));
             var formattedDocument = await Formatter.FormatAsync(documentWithAsyncModifier, Formatter.Annotation, cancellationToken: cancellationToken).ConfigureAwait(false);
 
-            var builder = ArrayBuilder<TextChange>.GetInstance();
+            using var _ = ArrayBuilder<TextChange>.GetInstance(out var builder);
+
             builder.AddRange(await formattedDocument.GetTextChangesAsync(document, cancellationToken).ConfigureAwait(false));
             builder.Add(new TextChange(completionListSpan, completionItem.DisplayText));
 
