38482e3638508520748122b4f7a811724b6e4e9e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2021-03-25 14:23:05 -0700

    Remove interface that is no longer user.

--- src/Features/Core/Portable/NavigateTo/INavigateToSearchService.cs
+++ src/Features/Core/Portable/NavigateTo/INavigateToSearchService.cs
@@ -18,14 +18,4 @@ namespace Microsoft.CodeAnalysis.NavigateTo
         Task<ImmutableArray<INavigateToSearchResult>> SearchProjectAsync(Project project, ImmutableArray<Document> priorityDocuments, string searchPattern, IImmutableSet<string> kinds, CancellationToken cancellationToken);
         Task<ImmutableArray<INavigateToSearchResult>> SearchDocumentAsync(Document document, string searchPattern, IImmutableSet<string> kinds, CancellationToken cancellationToken);
     }
-
-    [Obsolete("Use " + nameof(INavigateToSearchResult) + " instead", error: false)]
-    internal interface INavigateToSeINavigateToSearchService_RemoveInterfaceAboveAndRenameThisAfterInternalsVisibleToUsersUpdatearchService : ILanguageService
-    {
-        IImmutableSet<string> KindsProvided { get; }
-        bool CanFilter { get; }
-
-        Task<ImmutableArray<INavigateToSearchResult>> SearchProjectAsync(Project project, ImmutableArray<Document> priorityDocuments, string searchPattern, IImmutableSet<string> kinds, CancellationToken cancellationToken);
-        Task<ImmutableArray<INavigateToSearchResult>> SearchDocumentAsync(Document document, string searchPattern, IImmutableSet<string> kinds, CancellationToken cancellationToken);
-    }
 }
