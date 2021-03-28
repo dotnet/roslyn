e3df1f64568ee23491b1dda28a0ccf196264b015
Jonathon Marolf <jmarolf@microsoft.com>
2021-03-27 22:53:51 -0700

    fix text header

--- src/EditorFeatures/Test/EditorConfigSettings/Data/CodeStyleSettingsTest.cs
+++ src/EditorFeatures/Test/EditorConfigSettings/Data/CodeStyleSettingsTest.cs
@@ -1,13 +1,12 @@
-﻿extern alias WORKSPACES;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
+extern alias WORKSPACES;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
 using Microsoft.CodeAnalysis.CodeStyle;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Editor.EditorConfigSettings.Data;
