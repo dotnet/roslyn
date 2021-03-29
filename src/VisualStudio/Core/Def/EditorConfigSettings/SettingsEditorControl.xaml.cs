d1af11bea3f879152599bfb889a6ebba176bf8ca
Jonathon Marolf <jmarolf@microsoft.com>
2021-03-28 21:42:49 -0700

    simplify threading for updating code

--- src/VisualStudio/Core/Def/EditorConfigSettings/SettingsEditorControl.xaml.cs
+++ src/VisualStudio/Core/Def/EditorConfigSettings/SettingsEditorControl.xaml.cs
@@ -2,8 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Linq;
 using System.Windows.Controls;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.LanguageServices.EditorConfigSettings.Common;
 using Microsoft.VisualStudio.Shell.TableControl;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.VisualStudio.LanguageServices.EditorConfigSettings
 {
@@ -15,6 +22,9 @@ namespace Microsoft.VisualStudio.LanguageServices.EditorConfigSettings
         private readonly ISettingsEditorView _formattingView;
         private readonly ISettingsEditorView _codeStyleView;
         private readonly ISettingsEditorView _analyzerSettingsView;
+        private readonly Workspace _workspace;
+        private readonly string _filepath;
+        private readonly EditorTextUpdater _textUpdater;
 
         public static string Formatting => ServicesVSResources.Formatting;
         public static string CodeStyle => ServicesVSResources.Code_Style;
@@ -22,10 +32,17 @@ namespace Microsoft.VisualStudio.LanguageServices.EditorConfigSettings
 
         public SettingsEditorControl(ISettingsEditorView formattingView,
                                      ISettingsEditorView codeStyleView,
-                                     ISettingsEditorView analyzerSettingsView)
+                                     ISettingsEditorView analyzerSettingsView,
+                                     Workspace workspace,
+                                     string filepath,
+                                     IVsEditorAdaptersFactoryService editorAdaptersFactoryService,
+                                     IVsTextLines textLines)
         {
             InitializeComponent();
             DataContext = this;
+            _workspace = workspace;
+            _filepath = filepath;
+            _textUpdater = new EditorTextUpdater(editorAdaptersFactoryService, textLines);
             _formattingView = formattingView;
             FormattingTab.Content = _formattingView.SettingControl;
             _codeStyleView = codeStyleView;
@@ -36,9 +53,24 @@ namespace Microsoft.VisualStudio.LanguageServices.EditorConfigSettings
 
         internal void SynchronizeSettings()
         {
-            _formattingView.Synchronize();
-            _codeStyleView.Synchronize();
-            _analyzerSettingsView.Synchronize();
+            if (!IsKeyboardFocusWithin)
+            {
+                return;
+            }
+
+            var solution = _workspace.CurrentSolution;
+            var analyzerConfigDocument = solution.Projects
+                .Select(p => p.TryGetExistingAnalyzerConfigDocumentAtPath(_filepath)).FirstOrDefault();
+            if (analyzerConfigDocument is null)
+            {
+                return;
+            }
+
+            var originalText = analyzerConfigDocument.GetTextSynchronously(default);
+            var updatedText = _formattingView.UpdateEditorConfig(originalText);
+            updatedText = _codeStyleView.UpdateEditorConfig(updatedText);
+            updatedText = _analyzerSettingsView.UpdateEditorConfig(updatedText);
+            _textUpdater.UpdateText(updatedText.GetTextChanges(originalText))
         }
 
         internal IWpfTableControl[] GetTableControls()
