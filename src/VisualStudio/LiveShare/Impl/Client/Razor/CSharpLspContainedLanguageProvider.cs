17a985e85f327ddc76b502783aa3a0f1f70717d9
Sam Harwell <Sam.Harwell@microsoft.com>
2021-03-29 14:25:06 -0700

    Additional package updates

--- src/VisualStudio/LiveShare/Impl/Client/Razor/CSharpLspContainedLanguageProvider.cs
+++ src/VisualStudio/LiveShare/Impl/Client/Razor/CSharpLspContainedLanguageProvider.cs
@@ -2,13 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Runtime.CompilerServices;
-
-// This assembly is currently broken due to interop limitations. Force the runtime to reject it.
-[assembly: ReferenceAssembly]
-
-#if false // PROTOTYPE: needs to be enabled
-
 using System;
 using System.ComponentModel.Composition;
 using Microsoft.CodeAnalysis.Editor;
@@ -59,5 +52,3 @@ namespace Microsoft.VisualStudio.LanguageServices.LiveShare.Client.Razor
         }
     }
 }
-
-#endif
