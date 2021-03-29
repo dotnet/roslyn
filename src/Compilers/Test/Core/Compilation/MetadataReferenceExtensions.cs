dbaec20be7ac7e729d217b7a2e36c49d99a789ac
Jared Parsons <jaredpparsons@gmail.com>
2021-03-29 09:51:40 -0700

    Fix failing tests
    The determinism / round trip tests in emit were depending on a bug in
    our test framework. The metadata writer only emits references if there
    is a name associated with the reference. Before my PR all of our in
    memory references lacked image names and hence they were omitted from
    the PDB. Once I fixed that they started showing up and needed to be
    accounted for in the logic.

---
+++ src/Compilers/Test/Core/Compilation/MetadataReferenceExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Roslyn.Test.Utilities;
+
+namespace Microsoft.CodeAnalysis.Test.Utilities
+{
+    public static class MetadataReferenceExtensions
+    {
+        public static Guid GetModuleVersionId(this MetadataReference metadataReference)
+            => metadataReference is PortableExecutableReference peReference
+                ? peReference.GetModuleVersionId()
+                : throw new InvalidOperationException();
+
+        public static Guid GetModuleVersionId(this PortableExecutableReference peReference)
+        {
+            if (peReference.GetMetadata() is AssemblyMetadata assemblyMetadata &&
+                assemblyMetadata.GetModules() is { Length: 1 } modules)
+            {
+                return modules[0].GetModuleVersionId();
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
