77593904c53f9aa94fd6859e52a7da9d81296536
Jared Parsons <jaredpparsons@gmail.com>
2021-03-30 11:59:19 -0700

    Fixups

--- src/Compilers/Core/RebuildTest/RebuildCommandLineTests.VisualBasicRebuildCompiler.cs
+++ src/Compilers/Core/RebuildTest/RebuildCommandLineTests.VisualBasicRebuildCompiler.cs
@@ -2,25 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.PortableExecutable;
-using System.Text;
-using System.Threading;
-using BuildValidator;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
-using Microsoft.CodeAnalysis.Emit;
-using Microsoft.CodeAnalysis.Test.Utilities;
 using Microsoft.CodeAnalysis.VisualBasic;
-using Microsoft.Extensions.Logging;
-using Roslyn.Test.Utilities;
-using Xunit;
 
 namespace Microsoft.CodeAnalysis.Rebuild.UnitTests
 {
