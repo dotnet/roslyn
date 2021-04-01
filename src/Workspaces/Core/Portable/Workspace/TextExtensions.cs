67a0994666a229445abd4f1c1814bee6bb5a089e
Jason Malinowski <jason.malinowski@microsoft.com>
2021-04-01 11:24:54 -0700

    Update GetRelatedDocumentsWithChanges to work with open generated files
    This works the same as the GetOpenDocumentInCurrentContextWithChanges.
    This fixes navigation bars specifically which use this.

--- src/Workspaces/Core/Portable/Workspace/TextExtensions.cs
+++ src/Workspaces/Core/Portable/Workspace/TextExtensions.cs
@@ -27,6 +27,14 @@ namespace Microsoft.CodeAnalysis.Text
 
                 var solution = workspace.CurrentSolution;
 
+                if (workspace.TryGetOpenSourceGeneratedDocumentIdentity(documentId, out var documentIdentity))
+                {
+                    // For source generated documents, we won't count them as linked across multiple projects; this is because
+                    // the generated documents in each target may have different source so other features might be surprised if we
+                    // return the same documents but with different text. So in this case, we'll just return a single document.
+                    return ImmutableArray.Create(solution.WithFrozenSourceGeneratedDocument(documentIdentity, text));
+                }
+
                 var relatedIds = solution.GetRelatedDocumentIds(documentId);
                 solution = solution.WithDocumentText(relatedIds, text, PreservationMode.PreserveIdentity);
                 return relatedIds.SelectAsArray((id, solution) => solution.GetRequiredDocument(id), solution);
