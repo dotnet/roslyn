4e88fe2caf269ccb7ec6d86359547dee41c867a3
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-04-02 15:04:26 -0700

    Missing cancellation token

--- src/Features/Core/Portable/NavigateTo/AbstractNavigateToSearchService.InProcess.cs
+++ src/Features/Core/Portable/NavigateTo/AbstractNavigateToSearchService.InProcess.cs
@@ -159,7 +159,7 @@ namespace Microsoft.CodeAnalysis.NavigateTo
 
                     await ProcessIndexAsync(
                         documentKey.Id, document: null, patternName, patternContainer, kinds, onItemFound, index, cancellationToken).ConfigureAwait(false);
-                }));
+                }, cancellationToken));
             }
 
             await Task.WhenAll(tasks).ConfigureAwait(false);
