8f3ba68401c85a2ed0811d6cffb4f7d5bce717e3
Chris Sienkiewicz <chsienki@microsoft.com>
2021-04-06 06:01:37 -0700

    Initial API stubs for incremental generation (#52314)
    * Create stubs for public API shape

---
+++ src/Compilers/Core/Portable/SourceGeneration/IIncrementalGenerator.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.CodeAnalysis
+{
+    /// <summary>
+    /// The base interface required to implement an incremental generator
+    /// </summary>
+    /// <remarks>
+    /// The lifetime of a generator is controlled by the compiler.
+    /// State should not be stored directly on the generator, as there
+    /// is no guarantee that the same instance will be used on a 
+    /// subsequent generation pass.
+    /// </remarks>
+    public interface IIncrementalGenerator
+    {
+        /// <summary>
+        /// Called to initialize the generator and register generation steps via callbacks
+        /// on the <paramref name="context"/>
+        /// </summary>
+        /// <param name="context">The <see cref="IncrementalGeneratorInitializationContext"/> to register callbacks on</param>
+        void Initialize(IncrementalGeneratorInitializationContext context);
+    }
+}
