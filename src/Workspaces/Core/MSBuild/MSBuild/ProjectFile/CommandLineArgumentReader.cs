9cfe36ea6815d0230ea34f3cacd8f285102ed83d
Jonathon Marolf <jmarolf@microsoft.com>
2021-04-06 16:02:12 -0700

    make value as being nullable

--- src/Workspaces/Core/MSBuild/MSBuild/ProjectFile/CommandLineArgumentReader.cs
+++ src/Workspaces/Core/MSBuild/MSBuild/ProjectFile/CommandLineArgumentReader.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -39,7 +39,7 @@ namespace Microsoft.CodeAnalysis.MSBuild
             _builder.Add($"/{name}");
         }
 
-        protected void Add(string name, string value)
+        protected void Add(string name, string? value)
         {
             ValidateName(name);
 
