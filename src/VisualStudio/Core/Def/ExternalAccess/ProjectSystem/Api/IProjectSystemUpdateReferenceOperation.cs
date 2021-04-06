2b9e7526afa7f7b80d512fc7ce8a57736636a5e9
Joey Robichaud <joseph.robichaud@microsoft.com>
2021-04-06 11:15:04 -0700

    Add interfaces for reference update operations

---
+++ src/VisualStudio/Core/Def/ExternalAccess/ProjectSystem/Api/IProjectSystemUpdateReferenceOperation.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.LanguageServices.ExternalAccess.ProjectSystem.Api
+{
+    internal interface IProjectSystemUpdateReferenceOperation
+    {
+        /// <summary>
+        /// Applies a reference update operation to the project file.
+        /// </summary>
+        /// <returns>A boolean indicating success.</returns>
+        Task<bool> ApplyAsync();
+
+        /// <summary>
+        /// Reverts a reference update operation to the project file.
+        /// </summary>
+        /// <returns>A boolean indicating success.</returns>
+        Task<bool> RevertAsync();
+    }
+}
