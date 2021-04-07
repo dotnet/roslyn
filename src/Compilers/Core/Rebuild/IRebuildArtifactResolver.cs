d9c9867d8f67911ba97e6e8972f5deede960e7f5
Jared Parsons <jaredpparsons@gmail.com>
2021-04-07 14:09:35 -0700

    Factor out interface for source and references
    This adds the abstraction `IRebuildArtifactResolver` to the Rebuild
    layer. This interface abstracts out the finding of the source and
    references to the host process. The validator and tests were then
    changed to leverage this interface in testing.
    
    As a proof of this working I extended our tests to include `/pathmap`
    tests. These all failed before the change due to the file paths not
    lining up in the re-used `SyntaxTree` instances

---
+++ src/Compilers/Core/Rebuild/IRebuildArtifactResolver.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.Rebuild
+{
+    public interface IRebuildArtifactResolver
+    {
+        SourceText ResolveSourceText(SourceTextInfo sourceTextInfo);
+
+        MetadataReference ResolveMetadataReference(MetadataReferenceInfo metadataReferenceInfo);
+    }
+}
---
+++ src/Compilers/Core/RebuildTest/CompilationRebuildArtifactResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.Rebuild.UnitTests
+{
+    internal sealed class CompilationRebuildArtifactResolver : IRebuildArtifactResolver
+    {
+        internal Compilation Compilation { get; }
+
+        public CompilationRebuildArtifactResolver(Compilation compilation)
+        {
+            Compilation = compilation;
+        }
+
+        public MetadataReference ResolveMetadataReference(MetadataReferenceInfo metadataReferenceInfo) =>
+            Compilation
+                .References
+                .Single(x =>
+                    x.GetModuleVersionId() == metadataReferenceInfo.ModuleVersionId &&
+                    x.Properties.Aliases.SingleOrDefault() == metadataReferenceInfo.ExternAlias);
+
+        public SourceText ResolveSourceText(SourceTextInfo sourceTextInfo) =>
+            Compilation
+                .SyntaxTrees
+                .Select(x => x.GetText())
+                .Single(x => x.GetChecksum().SequenceEqual(sourceTextInfo.Hash));
+
+    }
+}
