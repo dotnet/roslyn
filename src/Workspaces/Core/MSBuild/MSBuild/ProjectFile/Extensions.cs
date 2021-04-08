aeeaa8f63902e6394d7ff1635a15178b77c447de
Jonathon Marolf <jmarolf@microsoft.com>
2021-04-08 14:34:06 -0700

    append char instead of string

--- src/Workspaces/Core/MSBuild/MSBuild/ProjectFile/Extensions.cs
+++ src/Workspaces/Core/MSBuild/MSBuild/ProjectFile/Extensions.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -84,7 +84,7 @@ namespace Microsoft.CodeAnalysis.MSBuild
             {
                 if (builder.Length > 0)
                 {
-                    builder.Append(" ");
+                    builder.Append(' ');
                 }
 
                 builder.Append(item.EvaluatedInclude);
