e85d7d2ad1e910989db4c5172c62b29956191100
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2021-04-12 16:33:34 -0700

    Update src/Compilers/Core/Portable/Syntax/SyntaxToken.cs
    Co-authored-by: Fred Silberberg <fred@silberberg.xyz>

--- src/Compilers/Core/Portable/Syntax/SyntaxToken.cs
+++ src/Compilers/Core/Portable/Syntax/SyntaxToken.cs
@@ -699,7 +699,7 @@ namespace Microsoft.CodeAnalysis
         /// Incrementally identical tokens can also appear within the same syntax tree, or syntax trees that did not arise
         /// from <see cref="SyntaxTree.WithChangedText"/>.  This can happen as the parser is allowed to construct parse
         /// trees using shared tokens for efficiency.  In all these cases though, it will still remain true that the incrementally
-        /// identical tokens will have different parents and may occur at different positions in their respective trees.
+        /// identical tokens could have different parents and may occur at different positions in their respective trees.
         /// </remarks>
         public bool IsIncrementallyIdenticalTo(SyntaxToken token)
             => this.Node != null && this.Node == token.Node;
