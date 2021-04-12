b72d137646133b893c1c9f17ae1b825c257fe7af
Andrew Hall (METAL) <andrha@microsoft.com>
2021-04-12 16:19:25 -0700

    Bold and truncate strings

--- src/EditorFeatures/Core/ValueTracking/ValueTrackedItem.cs
+++ src/EditorFeatures/Core/ValueTracking/ValueTrackedItem.cs
@@ -11,6 +11,7 @@ using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
+using System;
 
 namespace Microsoft.CodeAnalysis.ValueTracking
 {
@@ -45,6 +46,87 @@ namespace Microsoft.CodeAnalysis.ValueTracking
         {
             var subText = SourceText.GetSubText(Span);
             return subText.ToString();
+        }
+
+        public sealed class TruncatedClassifiedSpans
+        {
+            public ImmutableArray<ClassifiedSpan> Spans { get; }
+            public int Start => IsEmpty ? 0 : Spans[0].TextSpan.Start;
+            public int Length => IsEmpty ? 0 : Spans.Sum(c => c.TextSpan.Length);
+            public bool IsEmpty => Spans.IsDefaultOrEmpty;
+
+            public TruncatedClassifiedSpans(
+                ImmutableArray<ClassifiedSpan> spans)
+            {
+                Spans = spans;
+            }
+
+            public static readonly TruncatedClassifiedSpans Empty = new TruncatedClassifiedSpans(default);
+        }
+
+        /// <summary>
+        /// Gets the truncated version of the spans if the text length is greater than the max length provided. Returns true
+        /// if the out params represent some subset of <see cref="ClassifiedSpans"/>
+        /// </summary>
+        /// <param name="maxLength">The maximum text length before truncation happens</param>
+        /// <param name="beginning">If not empty, the chunk of spans that should always be shown at the beginning</param>
+        /// <param name="middle">If not empty, the chunk of spans that should be shown and may be surrounded by indicators of truncation</param>
+        /// <param name="end">If not empty, the chunk of spans that should always be shown at the end</param>
+        /// <remarks>
+        /// For trivial cases, beginning and end will not be used. This means the display can be "... [text] ...".
+        /// However, for some cases that loses important context for the line.
+        /// <para>
+        /// Example: <code>FullName = GetFirstName() + GetMiddleName() + GetLastName();</code>
+        /// Assume "GetMiddleName()" is the important symbol to be shown for this item, we want to show
+        /// something like this:
+        ///     "FullName = ... + GetMiddleName() + ...".
+        /// This keeps more context for the user to see. It's up to overrides of ValueTrackedItem to handle splitting the spans as needed. 
+        /// </para>
+        /// </remarks>
+        public virtual bool GetTruncatedClassifiedSpans(int maxLength, out TruncatedClassifiedSpans beginning, out TruncatedClassifiedSpans middle, out TruncatedClassifiedSpans end, out int totalLength)
+        {
+            beginning = TruncatedClassifiedSpans.Empty;
+            middle = TruncatedClassifiedSpans.Empty;
+            end = TruncatedClassifiedSpans.Empty;
+
+            if (ClassifiedSpans.IsDefaultOrEmpty)
+            {
+                totalLength = 0;
+                return false;
+            }
+
+            totalLength = ClassifiedSpans.Sum(c => c.TextSpan.Length);
+            if (totalLength <= maxLength || Span.Length >= maxLength)
+            {
+                middle = new TruncatedClassifiedSpans(ClassifiedSpans);
+                return false;
+            }
+
+            var builder = ImmutableArray.CreateBuilder<ClassifiedSpan>();
+            var beginningOfClassification = ClassifiedSpans[0].TextSpan.Start;
+            var minInclude = Span.Start - ((maxLength - Span.Length) / 2);
+            var maxInclude = Span.End + ((maxLength - Span.Length) / 2);
+
+            var position = beginningOfClassification;
+
+            foreach (var part in ClassifiedSpans)
+            {
+                if (position >= minInclude && position <= maxInclude)
+                {
+                    builder.Add(part);
+                }
+                else if (position > maxInclude)
+                {
+                    break;
+                }
+
+                position += part.TextSpan.Length;
+            }
+
+            middle = new TruncatedClassifiedSpans(
+                builder.ToImmutable());
+
+            return true;
         }
 
         public static async Task<ValueTrackedItem?> TryCreateAsync(Solution solution, Location location, ISymbol symbol, ValueTrackedItem? parent = null, CancellationToken cancellationToken = default)
