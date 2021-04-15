51c4cfc3eb8748effffb4dbe3c4a9f9133baffd0
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-04-15 14:28:35 -0700

    Add to intermediary list.

--- src/EditorFeatures/Core/Implementation/Classification/SyntacticClassificationTaggerProvider.TagComputer.cs
+++ src/EditorFeatures/Core/Implementation/Classification/SyntacticClassificationTaggerProvider.TagComputer.cs
@@ -431,7 +431,8 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Classification
                         span.Snapshot.AsText(), span.Span.ToTextSpan(), classifiedSpans, CancellationToken.None);
                 }
 
-                void AddSyntacticClassificationsForDocument(SnapshotSpan span, Document document, SyntaxNode? root)
+                void AddSyntacticClassificationsForDocument(
+                    SnapshotSpan span, Document document, SyntaxNode? root, List<ClassifiedSpan> classifiedSpans)
                 {
                     this.AssertIsForeground();
 
@@ -501,7 +502,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Classification
                     else
                     {
                         var tempList = ClassificationUtilities.GetOrCreateClassifiedSpanList();
-                        AddSyntacticClassificationsForDocument(span, lastProcessedDocument, lastProcessedRoot);
+                        AddSyntacticClassificationsForDocument(span, lastProcessedDocument, lastProcessedRoot, tempList);
 
                         var currentSnapshot = span.Snapshot;
                         var currentText = currentSnapshot.AsText();
