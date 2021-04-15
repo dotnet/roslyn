12c1c0d4f8caa7833c4919b1cfdc56a617f32967
David Wengier <david.wengier@microsoft.com>
2021-04-16 08:32:51 +1000

    Use flags enum and direct parsing

--- src/Features/Core/Portable/EditAndContinue/ManagedEditAndContinueCapabilities.cs
+++ src/Features/Core/Portable/EditAndContinue/ManagedEditAndContinueCapabilities.cs
@@ -2,49 +2,49 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-
 namespace Microsoft.CodeAnalysis.EditAndContinue
 {
     internal class ManagedEditAndContinueCapabilities
     {
-        internal static readonly ManagedEditAndContinueCapabilities Net5CoreCLR = new(ManagedEditAndContinueCapability.Baseline, ManagedEditAndContinueCapability.AddDefinitionToExistingType, ManagedEditAndContinueCapability.NewTypeDefinition);
-        internal static readonly ManagedEditAndContinueCapabilities Net6CoreCLR = new(ManagedEditAndContinueCapability.Baseline, ManagedEditAndContinueCapability.AddDefinitionToExistingType, ManagedEditAndContinueCapability.NewTypeDefinition, ManagedEditAndContinueCapability.RuntimeEdits);
-        internal static readonly ManagedEditAndContinueCapabilities Net5MonoVM = new();
-        internal static readonly ManagedEditAndContinueCapabilities Net6MonoVM = new(ManagedEditAndContinueCapability.Baseline, ManagedEditAndContinueCapability.RuntimeEdits);
+        internal static readonly ManagedEditAndContinueCapabilities Net5CoreCLR = new(ManagedEditAndContinueCapability.Baseline | ManagedEditAndContinueCapability.AddDefinitionToExistingType | ManagedEditAndContinueCapability.NewTypeDefinition);
+        internal static readonly ManagedEditAndContinueCapabilities Net6CoreCLR = new(ManagedEditAndContinueCapability.Baseline | ManagedEditAndContinueCapability.AddDefinitionToExistingType | ManagedEditAndContinueCapability.NewTypeDefinition | ManagedEditAndContinueCapability.RuntimeEdits);
+        internal static readonly ManagedEditAndContinueCapabilities Net5MonoVM = new(ManagedEditAndContinueCapability.None);
+        internal static readonly ManagedEditAndContinueCapabilities Net6MonoVM = new(ManagedEditAndContinueCapability.Baseline | ManagedEditAndContinueCapability.RuntimeEdits);
 
-        private readonly bool[] _capabilities;
+        private readonly ManagedEditAndContinueCapability _capabilities;
 
-        private ManagedEditAndContinueCapabilities(params ManagedEditAndContinueCapability[] capabilities)
-            : this("")
+        private ManagedEditAndContinueCapabilities(ManagedEditAndContinueCapability capabilities)
         {
-            foreach (var capability in capabilities)
-            {
-                _capabilities[(int)capability] = true;
-            }
+            _capabilities = capabilities;
         }
 
         internal ManagedEditAndContinueCapabilities(string? capabilities)
         {
-            _capabilities = new bool[(int)ManagedEditAndContinueCapability.Count];
+            var caps = ManagedEditAndContinueCapability.None;
 
-            if (string.IsNullOrEmpty(capabilities))
+            if (!string.IsNullOrEmpty(capabilities))
             {
-                return;
-            }
-
-            foreach (var capability in capabilities.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
-            {
-                if (Enum.TryParse(capability, out ManagedEditAndContinueCapability result) && (int)result < (int)ManagedEditAndContinueCapability.Count)
+                foreach (var capability in capabilities.Split(' '))
                 {
-                    _capabilities[(int)result] = true;
+                    caps |= ParseCapability(capability);
                 }
             }
+
+            _capabilities = caps;
         }
 
+        private static ManagedEditAndContinueCapability ParseCapability(string capability)
+            => capability switch
+            {
+                "Baseline" => ManagedEditAndContinueCapability.Baseline,
+                "AddDefinitionToExistingType" => ManagedEditAndContinueCapability.AddDefinitionToExistingType,
+                "NewTypeDefinition" => ManagedEditAndContinueCapability.NewTypeDefinition,
+                "RuntimeEdits" => ManagedEditAndContinueCapability.RuntimeEdits,
+
+                _ => ManagedEditAndContinueCapability.None
+            };
+
         public bool HasCapability(ManagedEditAndContinueCapability capability)
-        {
-            return (int)capability < (int)ManagedEditAndContinueCapability.Count && _capabilities[(int)capability];
-        }
+            => (_capabilities & capability) == capability;
     }
 }
--- src/Features/Core/Portable/EditAndContinue/ManagedEditAndContinueCapability.cs
+++ src/Features/Core/Portable/EditAndContinue/ManagedEditAndContinueCapability.cs
@@ -2,19 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 namespace Microsoft.CodeAnalysis.EditAndContinue
 {
     /// <summary>
     /// The capabilities that the runtime has with respect to edit and continue
     /// </summary>
+    [Flags]
     internal enum ManagedEditAndContinueCapability
     {
-        Baseline,
-        AddDefinitionToExistingType,
-        NewTypeDefinition,
-        RuntimeEdits,
+        None = 0,
 
-        // Must be last
-        Count
+        Baseline = 1 << 0,
+        AddDefinitionToExistingType = 1 << 1,
+        NewTypeDefinition = 1 << 2,
+        RuntimeEdits = 1 << 3,
     }
 }
