ab912487aa31470b27b76269956bc74673183765
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-04-21 14:58:12 -0700

    Remove IForegroundNotificationService.

--- src/EditorFeatures/Core/IForegroundNotificationService.cs
+++ src/EditorFeatures/Core/IForegroundNotificationService.cs
@@ -2,28 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
-using System.Threading;
-using Microsoft.CodeAnalysis.Shared.TestHooks;
 
 namespace Microsoft.CodeAnalysis.Editor
 {
-    /// <summary>
-    /// provide a way to call APIs from UI thread
-    /// </summary>
+    [Obsolete("This Interfaces exists for TypeScript tagger compatibility only and should not be used in new code.  TypeScript should move to overload of AsynchronousViewTaggerProvider that does not take this type.")]
     internal interface IForegroundNotificationService
     {
-        void RegisterNotification(Action action, IAsyncToken asyncToken, CancellationToken cancellationToken);
-
-        void RegisterNotification(Action action, int delayInMS, IAsyncToken asyncToken, CancellationToken cancellationToken);
-
-        /// <summary>
-        /// if action return true, the service will call it back again when it has time.
-        /// </summary>
-        void RegisterNotification(Func<bool> action, IAsyncToken asyncToken, CancellationToken cancellationToken);
-
-        void RegisterNotification(Func<bool> action, int delayInMS, IAsyncToken asyncToken, CancellationToken cancellationToken);
     }
 }
