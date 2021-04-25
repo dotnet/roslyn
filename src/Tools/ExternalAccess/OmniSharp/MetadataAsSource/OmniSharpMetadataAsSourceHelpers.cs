6a1c39ec69bce5a8afb6c946d1ea4f20f1fea1d2
Fredric Silberberg <fred@silberberg.xyz>
2021-04-25 15:11:42 -0700

    Add OmniSharpMetadataAsSourceHelpers

---
+++ src/Tools/ExternalAccess/OmniSharp/MetadataAsSource/OmniSharpMetadataAsSourceHelpers.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.MetadataAsSource;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.OmniSharp.MetadataAsSource
+{
+    internal static class OmniSharpMetadataAsSourceHelpers
+    {
+        public static string GetAssemblyInfo(IAssemblySymbol assemblySymbol)
+            => MetadataAsSourceHelpers.GetAssemblyInfo(assemblySymbol);
+
+        public static string GetAssemblyDisplay(Compilation compilation, IAssemblySymbol assemblySymbol)
+            => MetadataAsSourceHelpers.GetAssemblyDisplay(compilation, assemblySymbol);
+    }
+}
