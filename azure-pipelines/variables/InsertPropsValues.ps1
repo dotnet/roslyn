c177a9b9a9ea4e208420364f963dbf5c607b856d
Andrew Arnott <andrewarnott@live.com>
2021-04-27 08:18:59 -0600

    Fix InsertPropsValues.ps1

--- azure-pipelines/variables/InsertPropsValues.ps1
+++ azure-pipelines/variables/InsertPropsValues.ps1
@@ -1,14 +1,2 @@
-$BinPath = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\..\bin\Packages\$env:BUILDCONFIGURATION")
-
-$dirsToSearch = "$BinPath\NuGet\*.nupkg" |? { Test-Path $_ }
-$icv=@()
-if ($dirsToSearch) {
-    Get-ChildItem -Path $dirsToSearch |% {
-        if ($_.Name -match "^(.*)\.(\d+\.\d+\.\d+(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
-            $id = $Matches[1]
-            $version = $Matches[2]
-        }
-    }
-}
-
-Write-Output ([string]::join(',',$icv))
+# These values are commonly the same.
+& "$PSScriptRoot/InsertConfigValues.ps1"
