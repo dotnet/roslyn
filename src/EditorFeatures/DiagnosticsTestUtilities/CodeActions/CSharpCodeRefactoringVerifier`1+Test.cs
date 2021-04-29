6e3b2ca1b97ab02d9be67c93f6f1f5cd6dbae01e
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-04-29 13:08:34 -0700

    Sort

--- src/EditorFeatures/DiagnosticsTestUtilities/CodeActions/CSharpCodeRefactoringVerifier`1+Test.cs
+++ src/EditorFeatures/DiagnosticsTestUtilities/CodeActions/CSharpCodeRefactoringVerifier`1+Test.cs
@@ -2,17 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Testing.Verifiers;
-using Microsoft.CodeAnalysis.CodeRefactorings;
-using Microsoft.CodeAnalysis.Host.Mef;
-using System.Collections.Immutable;
-using Microsoft.CodeAnalysis.CodeActions;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Threading;
 
 #if !CODE_STYLE
 using System;
