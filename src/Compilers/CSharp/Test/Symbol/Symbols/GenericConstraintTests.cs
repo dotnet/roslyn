0d23db174b8b121633986c64dd2c35fd7a81290c
Fredric Silberberg <frsilb@microsoft.com>
2021-04-30 12:06:12 -0700

    Fix typo

--- src/Compilers/CSharp/Test/Symbol/Symbols/GenericConstraintTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/GenericConstraintTests.cs
@@ -7154,7 +7154,7 @@ public class C : A<C, C.D>
             DiagnosticDescription expectedDiagnostic = Diagnostic(ErrorCode.WRN_UnifyReferenceMajMin).WithArguments("mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", "assembly1", "mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", "mscorlib").WithLocation(1, 1);
 
             // These are unification use-site diagnostics. The original stackoverflow bug here came from checking constraints as part of
-            // unification diagnostic calcluation, so we want to verify that these are present to make sure we're testing the correct scenario.
+            // unification diagnostic calculation, so we want to verify that these are present to make sure we're testing the correct scenario.
             comp.VerifyDiagnostics(
                     expectedDiagnostic,
                     // warning CS1701: Assuming assembly reference 'mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' used by 'assembly1' matches identity 'mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' of 'mscorlib', you may need to supply runtime policy
