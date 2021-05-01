2ffb2bf37bc102f9d037554b73ac001e27be0384
Jonathon Marolf <jmarolf@microsoft.com>
2021-04-30 17:49:36 -0700

    handle params array in the argument list
    fixes #4842

--- src/Utilities/Compiler/Lightup/ITypeSymbolExtensions.cs
+++ src/Utilities/Compiler/Lightup/ITypeSymbolExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
 using Microsoft.CodeAnalysis;
 
 namespace Analyzer.Utilities.Lightup
@@ -13,10 +14,16 @@ namespace Analyzer.Utilities.Lightup
         private static readonly Func<ITypeSymbol, NullableAnnotation, ITypeSymbol> s_withNullableAnnotation
             = LightupHelpers.CreateSymbolWithPropertyAccessor<ITypeSymbol, NullableAnnotation>(typeof(ITypeSymbol), nameof(NullableAnnotation), fallbackResult: Lightup.NullableAnnotation.None);
 
+        private static readonly Func<ITypeSymbol, bool> s_isNativeIntegerType
+            = LightupHelpers.CreateSymbolPropertyAccessor<ITypeSymbol, bool>(typeof(ITypeSymbol), nameof(IsNativeIntegerType), fallbackResult: false);
+
         public static NullableAnnotation NullableAnnotation(this ITypeSymbol typeSymbol)
             => s_nullableAnnotation(typeSymbol);
 
         public static ITypeSymbol WithNullableAnnotation(this ITypeSymbol typeSymbol, NullableAnnotation nullableAnnotation)
             => s_withNullableAnnotation(typeSymbol, nullableAnnotation);
+
+        public static bool IsNativeIntegerType(this ITypeSymbol typeSymbol)
+            => s_isNativeIntegerType(typeSymbol);
     }
 }
