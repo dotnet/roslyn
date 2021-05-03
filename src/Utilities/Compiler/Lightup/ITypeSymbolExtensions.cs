a4cf6ec02c1c1e44b83944c9f2fc07544731cf3f
Jonathon Marolf <jmarolf@microsoft.com>
2021-05-03 14:25:00 -0700

    remove unused using

--- src/Utilities/Compiler/Lightup/ITypeSymbolExtensions.cs
+++ src/Utilities/Compiler/Lightup/ITypeSymbolExtensions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
 using System;
-using System.Reflection;
 using Microsoft.CodeAnalysis;
 
 namespace Analyzer.Utilities.Lightup
