bf2ccad0d816a156636ec6b1739b0690275a466b
Youssef Victor <youssefvictor00@gmail.com>
2021-05-04 16:41:01 +0200

    Fix

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Extensions/SyntaxNodeExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Extensions/SyntaxNodeExtensions.cs
@@ -966,7 +966,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Extensions
             {
                 case MemberDeclarationSyntax memberDecl: return memberDecl.WithModifiers(modifiers);
                 case AccessorDeclarationSyntax accessor: return accessor.WithModifiers(modifiers);
-                case AnonymousFunctionExpressionSyntax anonymous: anonymous.WithModifiers(modifiers);
+                case AnonymousFunctionExpressionSyntax anonymous: return anonymous.WithModifiers(modifiers);
                 case LocalFunctionStatementSyntax localFunction: return localFunction.WithModifiers(modifiers);
                 case LocalDeclarationStatementSyntax localDeclaration: return localDeclaration.WithModifiers(modifiers);
             }
