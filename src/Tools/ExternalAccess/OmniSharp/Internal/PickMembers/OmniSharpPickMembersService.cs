28955d0f02e5e369dbac824f167cd87c8fd1dfb3
Fredric Silberberg <fred@silberberg.xyz>
2021-05-04 22:07:14 -0700

    Fix up for 16.11

--- src/Tools/ExternalAccess/OmniSharp/Internal/PickMembers/OmniSharpPickMembersService.cs
+++ src/Tools/ExternalAccess/OmniSharp/Internal/PickMembers/OmniSharpPickMembersService.cs
@@ -24,10 +24,10 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.OmniSharp.Internal.PickMembers
             _omniSharpPickMembersService = omniSharpPickMembersService;
         }
 
-        public PickMembersResult PickMembers(string title, ImmutableArray<ISymbol> members, ImmutableArray<PickMembersOption> options = default, bool selectAll = true)
+        public PickMembersResult PickMembers(string title, ImmutableArray<ISymbol> members, ImmutableArray<PickMembersOption> options = default)
         {
-            var result = _omniSharpPickMembersService.PickMembers(title, members, options.SelectAsArray(o => new OmniSharpPickMembersOption(o)), selectAll);
-            return new(result.Members, result.Options.SelectAsArray(o => o.PickMembersOptionInternal), result.SelectedAll);
+            var result = _omniSharpPickMembersService.PickMembers(title, members, options.SelectAsArray(o => new OmniSharpPickMembersOption(o)), selectAll: true);
+            return new(result.Members, result.Options.SelectAsArray(o => o.PickMembersOptionInternal));
         }
     }
 }
