0d2997d3a1420408c7699e56e4e75b95afe65ff5
Fredric Silberberg <fred@silberberg.xyz>
2021-05-06 23:03:11 -0700

    Bugfixes for the OmniSharp ExternalAcces layer
    1. Make the completion provider class names constants so they can be used in pattern matching. I've added a unit test to make sure that these names are correct.
    2. Correctly handle default options arrays in OmniSharpPickMembersService.

---
+++ src/Tools/ExternalAccess/OmniSharpTest/OmniSharpCompletionProviderNamesTests.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Reflection;
+using Microsoft.CodeAnalysis.CSharp.Completion.Providers;
+using Microsoft.CodeAnalysis.ExternalAccess.OmniSharp.CSharp.Completion;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.OmniSharp.UnitTests
+{
+    public class OmniSharpCompletionProviderNamesTests
+    {
+        [Fact]
+        public void AllProviderNamesAreCorrect()
+        {
+            Assert.Equal(typeof(ObjectCreationCompletionProvider).FullName, OmniSharpCompletionProviderNames.ObjectCreationCompletionProvider);
+            Assert.Equal(typeof(OverrideCompletionProvider).FullName, OmniSharpCompletionProviderNames.OverrideCompletionProvider);
+            Assert.Equal(typeof(PartialMethodCompletionProvider).FullName, OmniSharpCompletionProviderNames.PartialMethodCompletionProvider);
+            Assert.Equal(typeof(InternalsVisibleToCompletionProvider).FullName, OmniSharpCompletionProviderNames.InternalsVisibleToCompletionProvider);
+            Assert.Equal(typeof(TypeImportCompletionProvider).FullName, OmniSharpCompletionProviderNames.TypeImportCompletionProvider);
+            Assert.Equal(typeof(ExtensionMethodImportCompletionProvider).FullName, OmniSharpCompletionProviderNames.ExtensionMethodImportCompletionProvider);
+
+            Assert.Equal(6, typeof(OmniSharpCompletionProviderNames).GetFields(BindingFlags.NonPublic | BindingFlags.Static).Length);
+        }
+    }
+}
