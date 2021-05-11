629e58e53ec85dee54b9d3e00bea4eb959290a8c
Jason Malinowski <jason.malinowski@microsoft.com>
2021-05-11 16:33:56 -0700

    Rename and update our tests on FindSymbolAtPositionAsync

--- src/EditorFeatures/Test/SymbolFinder/FindSymbolAtPositionTests.cs
+++ src/EditorFeatures/Test/SymbolFinder/FindSymbolAtPositionTests.cs
@@ -2,22 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces;
 using Microsoft.CodeAnalysis.FindSymbols;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Test.Utilities;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.UnitTests.Extensions
 {
     [UseExportProvider]
-    public class ISemanticSnapshotExtensionTests
+    public class FindSymbolAtPositionTests
     {
         [Fact]
-        public async Task TryGetSymbolTouchingPositionOnLeadingTrivia()
+        public async Task PositionOnLeadingTrivia()
         {
             using var workspace = TestWorkspace.CreateCSharp(
                 @"using System;
@@ -30,11 +29,9 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.Extensions
                         #pragma warning restore 612
                     }
                 }");
-            var position = workspace.Documents.Single(d => d.CursorPosition.HasValue).CursorPosition.Value;
-            var snapshot = workspace.Documents.Single().GetTextBuffer().CurrentSnapshot;
+            var position = workspace.Documents.Single(d => d.CursorPosition.HasValue).CursorPosition!.Value;
 
-            var document = workspace.CurrentSolution.GetDocument(workspace.Documents.Single().Id);
-            Assert.NotNull(document);
+            var document = workspace.CurrentSolution.GetRequiredDocument(workspace.Documents.Single().Id);
 
             var symbol = await SymbolFinder.FindSymbolAtPositionAsync(document, position);
             Assert.Null(symbol);
