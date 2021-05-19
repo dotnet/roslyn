7fb2adbb65add47fea69ed1c4896c08d5f12a376
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-05-19 10:30:08 -0700

    Extract out helper static

--- src/Features/Core/Portable/QuickInfo/QuickInfoServiceWithProviders.cs
+++ src/Features/Core/Portable/QuickInfo/QuickInfoServiceWithProviders.cs
@@ -46,12 +46,17 @@ namespace Microsoft.CodeAnalysis.QuickInfo
             return _providers;
         }
 
-        public override async Task<QuickInfoItem?> GetQuickInfoAsync(Document document, int position, CancellationToken cancellationToken)
+        public override Task<QuickInfoItem?> GetQuickInfoAsync(Document document, int position, CancellationToken cancellationToken)
         {
-            var extensionManager = _workspace.Services.GetRequiredService<IExtensionManager>();
+            return GetQuickInfoAsync(_workspace, GetProviders(), document, position, cancellationToken);
+        }
+
+        private static async Task<QuickInfoItem?> GetQuickInfoAsync(Workspace workspace, ImmutableArray<QuickInfoProvider> providers, Document document, int position, CancellationToken cancellationToken)
+        {
+            var extensionManager = workspace.Services.GetRequiredService<IExtensionManager>();
 
             // returns the first non-empty quick info found (based on provider order)
-            foreach (var provider in GetProviders())
+            foreach (var provider in providers)
             {
                 try
                 {
