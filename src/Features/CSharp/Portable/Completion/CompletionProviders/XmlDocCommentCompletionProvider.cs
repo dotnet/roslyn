d36653a9b13d65773a03964e45016ae0f1f29693
Youssef Victor <youssefvictor00@gmail.com>
2021-05-22 08:11:09 +0200

    Update XmlDocCommentCompletionProvider.cs

--- src/Features/CSharp/Portable/Completion/CompletionProviders/XmlDocCommentCompletionProvider.cs
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/XmlDocCommentCompletionProvider.cs
@@ -371,11 +371,15 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers
             var declaredParameters = declarationSymbol.GetParameters();
             if (declarationSymbol is INamedTypeSymbol { IsRecord: true } recordSymbol)
             {
+            var declaredParameters = declarationSymbol.GetParameters();
+            if (declarationSymbol is INamedTypeSymbol { IsRecord: true } recordSymbol)
+            {
                 Debug.Assert(declaredParameters.IsDefaultOrEmpty, "If GetParameters extension handles record, we can remove the handling here.");
                 // A bit hacky to determine the parameters of primary constructor associated with a given record.
                 // Simplifying is tracked by: https://github.com/dotnet/roslyn/issues/53092.
-                // Note: When the issue is handled, we can remove the logic here and handle things in GetParameters. BUT
-                // make sure none of the other callers doesn't need records to be handled.
+                // Note: When the issue is handled, we can remove the logic here and handle things in GetParameters extension. BUT
+                // if GetParameters extension method gets updated to handle records, we need to test EVERY usage
+                // of the extension method and make sure the change is applicable to all these usages.
                 var primaryConstructor = recordSymbol.InstanceConstructors.FirstOrDefault(
                     c => c.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() is RecordDeclarationSyntax);
 
