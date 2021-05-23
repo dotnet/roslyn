62a05415e0e454af7c7f4c58ffab14dc9fd4a6bf
Youssef Victor <youssefvictor00@gmail.com>
2021-05-23 16:42:40 +0200

    revert

--- src/Compilers/CSharp/Portable/Binder/BinderFactory.BinderFactoryVisitor.cs
+++ src/Compilers/CSharp/Portable/Binder/BinderFactory.BinderFactoryVisitor.cs
@@ -684,12 +684,6 @@ namespace Microsoft.CodeAnalysis.CSharp
                 {
                     extraInfo = NodeUsage.NamedTypeBodyOrTypeParameters;
                 }
-                else if (parent.SemicolonToken != default &&
-                    parent is RecordDeclarationSyntax &&
-                    LookupPosition.IsBetweenTokens(_position, parent.GetFirstToken(), parent.SemicolonToken.GetNextToken()))
-                {
-                    extraInfo = NodeUsage.NamedTypeBodyOrTypeParameters;
-                }
                 else if (LookupPosition.IsInTypeParameterList(_position, parent))
                 {
                     extraInfo = NodeUsage.NamedTypeBodyOrTypeParameters;
