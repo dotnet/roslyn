9516cbdcbb0e58b56ec0ea42b30fa5d7cb969043
Gen Lu <gel@microsoft.com>
2021-05-24 16:18:53 -0700

    Make ServiceHub.CoreComponents non-shipping

--- src/Workspaces/Remote/ServiceHub.CoreComponents/Program.cs
+++ src/Workspaces/Remote/ServiceHub.CoreComponents/Program.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -6,7 +6,9 @@ namespace Microsoft.CodeAnalysis.Remote.ServiceHub.CoreComponents
 {
     internal static class Program
     {
+#pragma warning disable IDE0060 // Remove unused parameter
         public static void Main(string[] args)
+#pragma warning restore IDE0060 // Remove unused parameter
         {
         }
     }
