0d79d5d76df2d3713e00bef6e8e4085d7a41d92d
Tomáš Matoušek <tmat@users.noreply.github.com>
2021-05-27 17:44:04 -0700

    Revert dotnet watch (#53738)
    * Revert "Fixes"
    
    This reverts commit 0b3eb4885f17409e275d53fcf0b12cbc8e96d01b.
    
    Revert "Fix Watch ExternalAccess"
    
    This reverts commit eabb1556b3ff626874eb2893310d414e9f8afdf9.
    
    * Revert "Prototype for dotnet-watch"
    
    This reverts commit 93a60f2917a966b1e17461041603825e87f33eec.

--- src/Features/Core/Portable/EditAndContinue/IEditAndContinueWorkspaceService.cs
+++ src/Features/Core/Portable/EditAndContinue/IEditAndContinueWorkspaceService.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,7 +20,7 @@ namespace Microsoft.CodeAnalysis.EditAndContinue
         void CommitSolutionUpdate(out ImmutableArray<DocumentId> documentsToReanalyze);
         void DiscardSolutionUpdate();
 
-        ValueTask OnSourceFileUpdatedAsync(Document document, CancellationToken cancellationToken);
+        void OnSourceFileUpdated(Document document);
 
         ValueTask StartDebuggingSessionAsync(Solution solution, IManagedEditAndContinueDebuggerService debuggerService, bool captureMatchingDocuments, CancellationToken cancellationToken);
         void BreakStateEntered(out ImmutableArray<DocumentId> documentsToReanalyze);
