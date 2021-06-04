c92d4cac72c339c89f34cf63790f43ae85e75866
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-06-04 11:42:25 -0700

    VBside

--- src/EditorFeatures/VisualBasic/NavigationBar/VisualBasicEditorNavigationBarItemService.vb
+++ src/EditorFeatures/VisualBasic/NavigationBar/VisualBasicEditorNavigationBarItemService.vb
@@ -4,13 +4,11 @@
 
 Imports System.Composition
 Imports System.Threading
-Imports Microsoft.CodeAnalysis
 Imports Microsoft.CodeAnalysis.Editor.Extensibility.NavigationBar
 Imports Microsoft.CodeAnalysis.Editor.Shared.Utilities
-Imports Microsoft.CodeAnalysis.Editor.VisualBasic.Utilities
 Imports Microsoft.CodeAnalysis.Host.Mef
 Imports Microsoft.CodeAnalysis.NavigationBar.RoslynNavigationBarItem
-Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+Imports Microsoft.VisualStudio.Text
 Imports Microsoft.VisualStudio.Text.Editor
 Imports Microsoft.VisualStudio.Text.Operations
 
@@ -40,41 +38,8 @@ Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.NavigationBar
             Return TypeOf DirectCast(item, WrappedNavigationBarItem).UnderlyingItem Is SymbolItem
         End Function
 
-        Protected Overrides Async Function GetSymbolNavigationPointAsync(document As Document, symbol As ISymbol, cancellationToken As CancellationToken) As Task(Of VirtualTreePoint?)
-            Dim location As Location = Await GetSourceNavigationLocationAsync(document, symbol, cancellationToken).ConfigureAwait(False)
-            If location Is Nothing Then
-                Return Nothing
-            End If
-
-            ' If the symbol is a method symbol, we'll figure out the right location which may be in
-            ' virtual space
-            If symbol.Kind = SymbolKind.Method Then
-                Dim methodBlock = location.FindToken(cancellationToken).GetAncestor(Of MethodBlockBaseSyntax)()
-
-                If methodBlock IsNot Nothing Then
-                    Return NavigationPointHelpers.GetNavigationPoint(location.SourceTree.GetText(cancellationToken), 4, methodBlock)
-                End If
-            End If
-
-            Return New VirtualTreePoint(location.SourceTree, location.SourceTree.GetText(cancellationToken), location.SourceSpan.Start)
-        End Function
-
-        Private Shared Async Function GetSourceNavigationLocationAsync(document As Document, symbol As ISymbol, cancellationToken As CancellationToken) As Task(Of Location)
-            Dim sourceLocations = symbol.Locations.Where(Function(l) l.IsInSource)
-
-            ' First figure out the location that we want to grab considering partial types
-            Dim syntaxTree = Await document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(False)
-            Dim location = sourceLocations.FirstOrDefault(Function(l) l.SourceTree.Equals(syntaxTree))
-
-            If location Is Nothing Then
-                location = sourceLocations.FirstOrDefault
-            End If
-
-            Return location
-        End Function
-
         Protected Overrides Async Function TryNavigateToItemAsync(
-                document As Document, item As WrappedNavigationBarItem, textView As ITextView, cancellationToken As CancellationToken) As Task(Of Boolean)
+                document As Document, item As WrappedNavigationBarItem, textView As ITextView, textSnapshot As ITextSnapshot, cancellationToken As CancellationToken) As Task(Of Boolean)
             Dim underlying = item.UnderlyingItem
 
             Dim generateCodeItem = TryCast(underlying, AbstractGenerateCodeItem)
@@ -83,7 +48,7 @@ Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.NavigationBar
                 Await GenerateCodeForItemAsync(document, generateCodeItem, textView, cancellationToken).ConfigureAwait(False)
                 Return True
             ElseIf symbolItem IsNot Nothing Then
-                Await NavigateToSymbolItemAsync(document, symbolItem, cancellationToken).ConfigureAwait(False)
+                NavigateToSymbolItem(document, item, symbolItem, textSnapshot, cancellationToken)
                 Return True
             End If
 
