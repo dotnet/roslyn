c5ff3afd36ae35e0ae94a345e50937bfe97aa9ca
Fredric Silberberg <frsilb@microsoft.com>
2021-06-08 14:41:06 -0700

    Expose IVSTypeScriptFormattingInteractionService
    Currently, TypeScript is implementing IEditorFormattingService itself, rather than going through an EA type. This meant that the change in https://github.com/dotnet/roslyn/pull/50222 broke them, as they were using VSTypeScriptEditorFormattingServiceWrapper as a wrapper to access their own implementation. This PR removes that wrapper, as it isn't needed, and adds a proper EA component for them to implement IFormattingInteractionService in a safe manner.

---
+++ src/EditorFeatures/Core/ExternalAccess/VSTypeScript/VSTypeScriptFormattingInteractionService.cs
@@ -0,0 +1,46 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.ExternalAccess.VSTypeScript.Api;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Shared.Utilities;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.VSTypeScript
+{
+    [ExportLanguageService(typeof(IFormattingInteractionService), InternalLanguageNames.TypeScript), Shared]
+    internal sealed class VSTypeScriptFormattingInteractionService : IFormattingInteractionService
+    {
+        private readonly IVSTypeScriptFormattingInteractionService _implementation;
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        internal VSTypeScriptFormattingInteractionService(IVSTypeScriptFormattingInteractionService implementation)
+            => _implementation = implementation;
+
+        bool SupportsFormatDocument => _implementation.SupportsFormatDocument;
+        bool SupportsFormatSelection => _implementation.SupportsFormatSelection;
+        bool SupportsFormatOnPaste => _implementation.SupportsFormatOnPaste;
+        bool SupportsFormatOnReturn => _implementation.SupportsFormatOnReturn;
+
+        bool SupportsFormattingOnTypedCharacter(Document document, char ch)
+            => _implementation.SupportsFormattingOnCharacterTyped(document, ch);
+
+        Task<ImmutableArray<TextChange>> GetFormattingChangesAsync(Document document, TextSpan? textSpan, DocumentOptionSet? documentOptions, CancellationToken cancellationToken)
+            => _implementation.GetFormattingChangesAsync(document, textSpan, documentOptions, cancellationToken);
+
+        Task<ImmutableArray<TextChange>> GetFormattingChangesOnPasteAsync(Document document, TextSpan textSpan, DocumentOptionSet? documentOptions, CancellationToken cancellationToken)
+            => _implementation.GetFormattingChangesOnPasteAsync(document, textSpan, documentOptions, cancellationToken);
+
+        Task<ImmutableArray<TextChange>> GetFormattingChangesAsync(Document document, char typedChar, int position, DocumentOptionSet? documentOptions, CancellationToken cancellationToken)
+            => _implementation.GetFormattingChangesAsync(document, typedChar, position, documentOptions, cancellationToken);
+
+        Task<ImmutableArray<TextChange>> GetFormattingChangesOnReturnAsync(Document document, int position, DocumentOptionSet? documentOptions, CancellationToken cancellationToken)
+            => _implementation.GetFormattingChangesOnReturnAsync(document, position, documentOptions, cancellationToken);
+    }
