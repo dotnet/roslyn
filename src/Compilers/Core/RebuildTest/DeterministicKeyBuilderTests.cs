05990b7acf1ce10a612b688a9dea6aff4440df97
Jared Parsons <jaredpparsons@gmail.com>
2021-06-09 12:50:24 -0700

    Regression test

--- src/Compilers/Core/RebuildTest/DeterministicKeyBuilderTests.cs
+++ src/Compilers/Core/RebuildTest/DeterministicKeyBuilderTests.cs
@@ -5,10 +5,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
+using Microsoft.CodeAnalysis.VisualBasic;
 using Roslyn.Test.Utilities;
 using Xunit;
 
@@ -16,6 +18,35 @@ namespace Microsoft.CodeAnalysis.Rebuild.UnitTests
 {
     public sealed class DeterministicKeyBuilderTests
     {
+        /// <summary>
+        /// This check monitors the set of properties and fields on the various option types
+        /// that contribute to the deterministic checksum of a <see cref="Compilation"/>. When
+        /// any of these tests change that means the new property or field needs to be evaluated
+        /// for inclusion into the checksum
+        /// </summary>
+        [Fact]
+        public void VerifyUpToDate()
+        {
+            verifyCount<ParseOptions>(11);
+            verifyCount<CSharpParseOptions>(10);
+            verifyCount<VisualBasicParseOptions>(10);
+            verifyCount<CompilationOptions>(62);
+            verifyCount<CSharpCompilationOptions>(9);
+            verifyCount<VisualBasicCompilationOptions>(22);
+
+            static void verifyCount<T>(int expected)
+            {
+                var type = typeof(T);
+                var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly | BindingFlags.Instance;
+                var fields = type.GetFields(flags);
+                var properties = type.GetProperties(flags);
+                var count = fields.Length + properties.Length;
+                Assert.Equal(expected, count);
+            }
+
+
+        }
+
         [Fact]
         public void Simple()
         {
