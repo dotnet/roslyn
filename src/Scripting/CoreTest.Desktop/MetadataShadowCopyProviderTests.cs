9495664c11f79bdfbe4ed94fabe44c6abf09aed9
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-06-08 20:17:26 -0700

    Fix IDE0059: Unnecessary assignment

--- src/Scripting/CoreTest.Desktop/MetadataShadowCopyProviderTests.cs
+++ src/Scripting/CoreTest.Desktop/MetadataShadowCopyProviderTests.cs
@@ -195,10 +195,9 @@ namespace Microsoft.CodeAnalysis.Scripting.Hosting.UnitTests
         {
             var dir = Temp.CreateDirectory();
             var dll = dir.CreateFile("a.dll").WriteAllBytes(TestResources.MetadataTests.InterfaceAndClass.CSClasses01);
-            var doc = dir.CreateFile("a.xml").WriteAllText("<hello>");
-
-            var sc1 = _provider.GetMetadataShadowCopy(dll.Path, MetadataImageKind.Assembly);
-            var sc2 = _provider.GetMetadataShadowCopy(dll.Path, MetadataImageKind.Assembly);
+            _ = dir.CreateFile("a.xml").WriteAllText("<hello>");
+            _ = _provider.GetMetadataShadowCopy(dll.Path, MetadataImageKind.Assembly);
+            _ = _provider.GetMetadataShadowCopy(dll.Path, MetadataImageKind.Assembly);
 
             var md1 = _provider.GetMetadata(dll.Path, MetadataImageKind.Assembly);
             Assert.NotNull(md1);
@@ -210,7 +209,7 @@ namespace Microsoft.CodeAnalysis.Scripting.Hosting.UnitTests
             var dll2 = dir2.CreateFile("a2.dll").WriteAllBytes(TestResources.MetadataTests.InterfaceAndClass.CSClasses01);
 
             Assert.Equal(1, _provider.CacheSize);
-            var sc3a = _provider.GetMetadataShadowCopy(dll2.Path, MetadataImageKind.Module);
+            _ = _provider.GetMetadataShadowCopy(dll2.Path, MetadataImageKind.Module);
             Assert.Equal(2, _provider.CacheSize);
         }
 
@@ -222,8 +221,8 @@ namespace Microsoft.CodeAnalysis.Scripting.Hosting.UnitTests
 
             var dir = Temp.CreateDirectory();
             var dll = dir.CreateFile("a.dll").WriteAllBytes(TestResources.MetadataTests.InterfaceAndClass.CSClasses01);
-            var docInvariant = dir.CreateFile("a.xml").WriteAllText("Invariant");
-            var docGreek = dir.CreateDirectory(elGR.Name).CreateFile("a.xml").WriteAllText("Greek");
+            _ = dir.CreateFile("a.xml").WriteAllText("Invariant");
+            _ = dir.CreateDirectory(elGR.Name).CreateFile("a.xml").WriteAllText("Greek");
 
             // invariant culture
             var provider = CreateProvider(CultureInfo.InvariantCulture);
