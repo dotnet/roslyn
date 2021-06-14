76262daae538bc70a2c8cb7e44a510288610d2c0
samuelawuah <65207187+samaw7@users.noreply.github.com>
2021-06-14 09:30:44 -0700

    Update to PR Commit 6

--- src/Compilers/CSharp/Portable/Emitter/Model/PEModuleBuilder.cs
+++ src/Compilers/CSharp/Portable/Emitter/Model/PEModuleBuilder.cs
@@ -281,6 +281,11 @@ namespace Microsoft.CodeAnalysis.CSharp.Emit
                         }
                     }
                 }
+                else if (!method.IsConstructor)
+                {
+                    atLeastOneMethodDoesNotHaveIL = true;
+                }
+
                 // This statement will only work if constructor is the last on the list, if the list changes
                 // then the whole method will break. Note to find a better way to check if constructor has no body and 
                 // any other method does not have body.
