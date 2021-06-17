77db8b246b4c586c688f9c1374a05731fe7a3794
Lingling Tong <ltong@microsoft.com>
2021-06-17 12:07:06 -0700

    Allow snippet format in XAML compeltion

--- src/VisualStudio/Xaml/Impl/Implementation/LanguageServer/Handler/Completion/CompletionHandler.cs
+++ src/VisualStudio/Xaml/Impl/Implementation/LanguageServer/Handler/Completion/CompletionHandler.cs
@@ -93,6 +93,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Xaml.LanguageServer.Handler
                 Kind = GetItemKind(xamlCompletion.Kind),
                 Description = xamlCompletion.Description,
                 Icon = xamlCompletion.Icon,
+                InsertTextFormat = xamlCompletion.InsertSnippet ? InsertTextFormat.Snippet : InsertTextFormat.Plaintext,
                 Data = new CompletionResolveData { ProjectGuid = documentId.ProjectId.Id, DocumentGuid = documentId.Id, Position = position, DisplayText = xamlCompletion.DisplayText }
             };
 
@@ -119,7 +120,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Xaml.LanguageServer.Handler
                 // Retriger completion after commit
                 item.Command = s_retriggerCompletionCommand;
             }
-
+ 
             return item;
         }
 
