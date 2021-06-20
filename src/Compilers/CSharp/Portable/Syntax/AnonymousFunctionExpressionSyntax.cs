29148ec3d0b54e8d36082409da6f18566466b758
Youssef1313 <youssefvictor00@gmail.com>
2021-06-20 13:17:17 +0200

    Fix

--- src/Compilers/CSharp/Portable/Syntax/AnonymousFunctionExpressionSyntax.cs
+++ src/Compilers/CSharp/Portable/Syntax/AnonymousFunctionExpressionSyntax.cs
@@ -23,5 +23,35 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax
             => WithAsyncKeywordCore(asyncKeyword);
 
         internal abstract AnonymousFunctionExpressionSyntax WithAsyncKeywordCore(SyntaxToken asyncKeyword);
+
+        /// <summary>
+        /// If the given <paramref name="asyncKeyword"/> is default, remove async if exists.
+        /// Otherwise, replace the existing <see cref="AsyncKeyword"/> or add a new one.
+        /// </summary>
+        /// <param name="asyncKeyword"></param>
+        /// <returns></returns>
+        private protected SyntaxTokenList UpdateAsyncKeyword(SyntaxToken asyncKeyword)
+        {
+            var existingAsync = AsyncKeyword;
+
+            // remove async keyword (if exists).
+            if (asyncKeyword == default)
+            {
+                if (existingAsync != default)
+                {
+                    return Modifiers.Remove(existingAsync);
+                }
+
+                return Modifiers;
+            }
+
+            // add or replace.
+            if (existingAsync == default)
+            {
+                return Modifiers.Add(asyncKeyword);
+            }
+
+            return Modifiers.Replace(existingAsync, asyncKeyword);
+        }
     }
 }
