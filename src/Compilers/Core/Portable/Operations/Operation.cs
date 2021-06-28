31999a9afbab60d72c653fc6d7fb9513a87a0c5c
Fredric Silberberg <frsilb@microsoft.com>
2021-06-28 16:02:46 -0700

    Fixup using order.

--- src/Compilers/Core/Portable/Operations/Operation.cs
+++ src/Compilers/Core/Portable/Operations/Operation.cs
@@ -2,14 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using Microsoft.CodeAnalysis.Operations;
 using Microsoft.CodeAnalysis.PooledObjects;
-using System.Diagnostics.CodeAnalysis;
-using System;
 
 namespace Microsoft.CodeAnalysis
 {
