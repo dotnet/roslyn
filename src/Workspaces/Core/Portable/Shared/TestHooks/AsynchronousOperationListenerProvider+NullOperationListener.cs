81a5b570dc6f6246000ef1c1eef71fa7e3ac40df
Stephen Toub <stoub@microsoft.com>
2021-07-01 13:43:27 -0400

    Reduce overheads of NullOperationListener.Delay
    This is showing up as a primary source of cancellation exceptions while creating projects in VS.

--- src/Workspaces/Core/Portable/Shared/TestHooks/AsynchronousOperationListenerProvider+NullOperationListener.cs
+++ src/Workspaces/Core/Portable/Shared/TestHooks/AsynchronousOperationListenerProvider+NullOperationListener.cs
@@ -6,6 +6,7 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Shared.TestHooks
 {
@@ -19,10 +20,42 @@ namespace Microsoft.CodeAnalysis.Shared.TestHooks
                 [CallerFilePath] string filePath = "",
                 [CallerLineNumber] int lineNumber = 0) => EmptyAsyncToken.Instance;
 
-            public async Task<bool> Delay(TimeSpan delay, CancellationToken cancellationToken)
+            public Task<bool> Delay(TimeSpan delay, CancellationToken cancellationToken)
             {
-                await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
-                return true;
+                // This could be as simple as:
+                //     await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
+                //     return true;
+                // However, whereas in general cancellation is expected to be rare and thus throwing
+                // an exception in response isn't very impactful, here it's expected to be the case
+                // more often than not as the operation is being used to delay an operation because
+                // it's expected something else is going to happen to obviate the need for that
+                // operation.  Thus, we can spend a little more code avoiding the additional throw
+                // for the common case of an exception occurring.
+
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return Task.FromCanceled<bool>(cancellationToken);
+                }
+
+                var t = Task.Delay(delay, cancellationToken);
+                if (t.IsCompleted)
+                {
+                    return t.Status == TaskStatus.RanToCompletion ?
+                        SpecializedTasks.True :
+                        Task.FromCanceled<bool>(cancellationToken);
+                }
+
+                return t.ContinueWith(
+                    _ => true,
+                    CancellationToken.None,
+                    TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.NotOnCanceled,
+                    TaskScheduler.Default);
+
+                // Note the above passes CancellationToken.None and TaskContinuationOptions.NotOnCanceled.
+                // That's cheaper than passing cancellationToken and with the same semantics except
+                // that if the returned task does end up being canceled, any operation canceled exception
+                // thrown won't contain the cancellationToken.  If that ends up being impactful, it can
+                // be switched to use `cancellationToken, TaskContinuationOptions.ExecuteSynchronously`.
             }
         }
     }
