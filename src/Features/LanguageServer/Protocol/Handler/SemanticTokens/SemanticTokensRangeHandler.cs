fb92c8554ac57825ee475a1d97dfdd00ddd99310
Allison Chou <allichou@microsoft.com>
2021-07-02 15:18:33 -0700

    [LSP] Revert semantic tokens change in main (#54570)

--- src/Features/LanguageServer/Protocol/Handler/SemanticTokens/SemanticTokensRangeHandler.cs
+++ src/Features/LanguageServer/Protocol/Handler/SemanticTokens/SemanticTokensRangeHandler.cs
@@ -43,14 +43,8 @@ namespace Microsoft.CodeAnalysis.LanguageServer.Handler.SemanticTokens
             RequestContext context,
             CancellationToken cancellationToken)
         {
-            // Temporary workaround for https://github.com/dotnet/roslyn/issues/54547.
-            // We should eventually go back to throwing here if context.Document is null.
-            if (context.Document is null)
-            {
-                return new LSP.SemanticTokens();
-            }
-
             Contract.ThrowIfNull(request.TextDocument, "TextDocument is null.");
+            Contract.ThrowIfNull(context.Document, "Document is null.");
 
             var resultId = _tokensCache.GetNextResultId();
 
