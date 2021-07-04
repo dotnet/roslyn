afc1ca7ab9337dfd016bd3fa0c9450723583b8d1
David Wengier <david.wengier@microsoft.com>
2021-07-04 17:17:35 +1000

    Fix condition

--- src/Compilers/CSharp/Portable/Compilation/SyntaxTreeSemanticModel.cs
+++ src/Compilers/CSharp/Portable/Compilation/SyntaxTreeSemanticModel.cs
@@ -2228,12 +2228,14 @@ namespace Microsoft.CodeAnalysis.CSharp
 
             // Global statements don't have their parent in common, but should belong to the same compilation unit
             // For everything else, the parents should be the same
-            if (firstStatement.Parent is GlobalStatementSyntax &&
+            bool isGlobalStatement = firstStatement.Parent is GlobalStatementSyntax;
+            if (isGlobalStatement &&
                 (lastStatement.Parent is not GlobalStatementSyntax || firstStatement.Parent.Parent != lastStatement.Parent.Parent))
             {
                 throw new ArgumentException("global statements not within the same compilation unit");
             }
-            else if (firstStatement.Parent == null || firstStatement.Parent != lastStatement.Parent)
+            else if (!isGlobalStatement &&
+                (firstStatement.Parent == null || firstStatement.Parent != lastStatement.Parent))
             {
                 throw new ArgumentException("statements not within the same statement list");
             }
