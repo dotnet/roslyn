c0213db70e6d68bb70a5f077dd99bf4ce9f1d56d
Sam Harwell <Sam.Harwell@microsoft.com>
2021-07-09 08:44:22 -0700

    Add origin notes

--- src/Compilers/Core/CodeAnalysisTest/Collections/ImmutableSegmentedHashSetTest.nonnetstandard.cs
+++ src/Compilers/Core/CodeAnalysisTest/Collections/ImmutableSegmentedHashSetTest.nonnetstandard.cs
@@ -1,6 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// NOTE: This code is derived from an implementation originally in dotnet/runtime:
+// https://github.com/dotnet/runtime/blob/v5.0.8/src/libraries/System.Collections.Immutable/tests/ImmutableHashSetTest.nonnetstandard.cs
+//
+// See the commentary in https://github.com/dotnet/roslyn/pull/50156 for notes on incorporating changes made to the
+// reference implementation.
+
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
