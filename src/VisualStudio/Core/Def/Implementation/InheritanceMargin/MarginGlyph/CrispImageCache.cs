4db338fc1611e348ff6548e69c1a117b1d8e6669
Shen Chen <shech@microsoft.com>
2021-07-14 10:21:01 -0700

    Add a static cache just for demo purpose

---
+++ src/VisualStudio/Core/Def/Implementation/InheritanceMargin/MarginGlyph/CrispImageCache.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.VisualStudio.Imaging;
+using Microsoft.VisualStudio.Imaging.Interop;
+using Roslyn.Utilities;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMargin.MarginGlyph
+{
+    internal static class CrispImageCache
+    {
+        private static ImageAttributes? s_attributes;
+        private static readonly Dictionary<ImageMoniker, BitmapSource> s_ImageCache = new();
+
+        public static BitmapSource GetBitmapSource(ImageMoniker imageMoniker, ImageAttributes currentAttributes)
+        {
+            if (!currentAttributes.Equals(s_attributes))
+            {
+                s_attributes = currentAttributes;
+                var imageLibrary = CrispImage.DefaultImageLibrary;
+                s_ImageCache[KnownMonikers.Implementing] = (BitmapSource)imageLibrary.GetImage(KnownMonikers.Implementing, currentAttributes);
+                s_ImageCache[KnownMonikers.Implemented] = (BitmapSource)imageLibrary.GetImage(KnownMonikers.Implemented, currentAttributes);
+                s_ImageCache[KnownMonikers.Overridden] = (BitmapSource)imageLibrary.GetImage(KnownMonikers.Overridden, currentAttributes);
+                s_ImageCache[KnownMonikers.Overriding] = (BitmapSource)imageLibrary.GetImage(KnownMonikers.Overriding, currentAttributes);
+                s_ImageCache[KnownMonikers.ImplementingOverridden] = (BitmapSource)imageLibrary.GetImage(KnownMonikers.ImplementingOverridden, currentAttributes);
+                s_ImageCache[KnownMonikers.ImplementingOverriding] = (BitmapSource)imageLibrary.GetImage(KnownMonikers.ImplementingOverriding, currentAttributes);
+            }
+
+            if (s_ImageCache.TryGetValue(imageMoniker, out var bitmapSource))
+            {
+                return bitmapSource;
+            }
+
+            throw ExceptionUtilities.UnexpectedValue(imageMoniker);
+        }
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/InheritanceMargin/MarginGlyph/CrispImageSourceConverter.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Imaging.Interop;
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMargin.MarginGlyph
+{
+    internal class CrispImageSourceConverter : MultiValueConverter<ImageMoniker, double, double, double, Color, ImageSource>
+    {
+        protected override ImageSource Convert(
+            ImageMoniker imageMoniker,
+            double height,
+            double width,
+            double dpi,
+            Color background,
+            object parameter,
+            CultureInfo culture)
+        {
+            var attributes = new ImageAttributes
+            {
+                StructSize = Marshal.SizeOf(typeof(ImageAttributes)),
+                Flags = unchecked((uint)(_ImageAttributesFlags.IAF_Size | _ImageAttributesFlags.IAF_Dpi | _ImageAttributesFlags.IAF_Type | _ImageAttributesFlags.IAF_Format | _ImageAttributesFlags.IAF_Background)),
+                LogicalHeight = (int)height,
+                LogicalWidth = (int)width,
+                Dpi = (int)dpi,
+                Format = (int)_UIDataFormat.DF_WPF,
+                ImageType = (int)_UIImageType.IT_Bitmap,
+                Background = background.ToRgba(),
+            };
+
+            return CrispImageCache.GetBitmapSource(imageMoniker, attributes);
+        }
+    }
+}
