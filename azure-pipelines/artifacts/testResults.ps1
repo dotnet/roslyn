8696c4f9a1a35cb5b07229085a4409890e2a33f4
Sam Harwell <Sam.Harwell@microsoft.com>
2021-07-14 18:00:18 -0700

    Add devenv heap dump to the list of collected artifacts

--- azure-pipelines/artifacts/testResults.ps1
+++ azure-pipelines/artifacts/testResults.ps1
@@ -2,7 +2,7 @@ if ($env:AGENT_TEMPDIRECTORY) {
     # The DotNetCoreCLI uses an alternate location to publish these files
     $guidRegex = '^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'
     @{
-        $env:AGENT_TEMPDIRECTORY = (Get-ChildItem $env:AGENT_TEMPDIRECTORY -Directory |? { $_.Name -match $guidRegex } |% { Get-ChildItem "$($_.FullName)\testhost*.dmp","$($_.FullName)\Sequence_*.xml" -Recurse });
+        $env:AGENT_TEMPDIRECTORY = (Get-ChildItem $env:AGENT_TEMPDIRECTORY -Directory |? { $_.Name -match $guidRegex } |% { Get-ChildItem "$($_.FullName)\testhost*.dmp","$($_.FullName)\devenv*.dmp","$($_.FullName)\Sequence_*.xml" -Recurse });
     }
 } else {
     $testRoot = Resolve-Path "$PSScriptRoot\..\..\test"
