417bad83863e1c6b5705e429eacc49c5dea08b4f
Allison Chou <allichou@microsoft.com>
2021-07-16 13:10:16 -0700

    Fix RPS issues in Razor options service (#54857)

--- src/Features/Core/Portable/ExternalAccess/Razor/Api/Extensions.cs
+++ src/Features/Core/Portable/ExternalAccess/Razor/Api/Extensions.cs
@@ -4,7 +4,7 @@
 
 using Microsoft.CodeAnalysis.Host;
 
-namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor.Api
 {
     internal static class Extensions
     {
