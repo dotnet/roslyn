608b515c6f379b8724880f443476c0945bc68dc1
tmat <tomas.matousek@microsoft.com>
2021-07-20 12:56:11 -0700

    Fix

--- src/Compilers/CSharp/Portable/Emitter/Model/PEModuleBuilder.cs
+++ src/Compilers/CSharp/Portable/Emitter/Model/PEModuleBuilder.cs
@@ -537,9 +537,10 @@ namespace Microsoft.CodeAnalysis.CSharp.Emit
                     continue;
                 }
 
+                // exported types are not emitted in EnC deltas (hence generation 0):
                 string fullEmittedName = MetadataHelpers.BuildQualifiedName(
                     ((Cci.INamespaceTypeReference)type.GetCciAdapter()).NamespaceName,
-                    Cci.MetadataWriter.GetMangledName(type.GetCciAdapter()));
+                    Cci.MetadataWriter.GetMangledName(type.GetCciAdapter(), generation: 0));
 
                 // First check against types declared in the primary module
                 if (ContainsTopLevelType(fullEmittedName))
