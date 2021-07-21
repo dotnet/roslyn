72dec5656b961b24f417345bc66deff84ee97c64
Youssef Victor <youssefvictor00@gmail.com>
2021-07-21 19:22:43 +0200

    Update CSharpUseNullCheckOverTypeCheckDiagnosticAnalyzer.cs

--- src/Analyzers/CSharp/Analyzers/UseIsNullCheck/CSharpUseNullCheckOverTypeCheckDiagnosticAnalyzer.cs
+++ src/Analyzers/CSharp/Analyzers/UseIsNullCheck/CSharpUseNullCheckOverTypeCheckDiagnosticAnalyzer.cs
@@ -18,6 +18,7 @@ namespace Microsoft.CodeAnalysis.CSharp.UseIsNullCheck
             : base(IDEDiagnosticIds.UseNullCheckOverTypeCheckDiagnosticId,
                    EnforceOnBuildValues.UseNullCheckOverTypeCheck,
                    CSharpCodeStyleOptions.PreferNullCheckOverTypeCheck,
+                   LanguageNames.CSharp,
                    new LocalizableResourceString(nameof(CSharpAnalyzersResources.Prefer_null_check_over_type_check), CSharpAnalyzersResources.ResourceManager, typeof(CSharpAnalyzersResources)),
                    new LocalizableResourceString(nameof(CSharpAnalyzersResources.Null_check_can_be_clarified), CSharpAnalyzersResources.ResourceManager, typeof(CSharpAnalyzersResources)))
         {
