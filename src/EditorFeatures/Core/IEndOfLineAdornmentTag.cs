f06f7d7ad3aa65710a04411593b26d5e3cc6acf7
Ankita Khera <ankitakhera@microsoft.com>
2021-07-26 09:33:46 -0700

    the end of line adornment tag that will be used by the editor team

---
+++ src/EditorFeatures/Core/IEndOfLineAdornmentTag.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace Microsoft.CodeAnalysis.Editor
+{
+    internal interface IEndOfLineAdornmentTag : ITag
+    {
+        string Type { get; }
+        double Width { get; }
+        double Height { get; }
+        double HorizontalOffset { get; }
+        double VerticalOffset { get; }
+    }
+}
