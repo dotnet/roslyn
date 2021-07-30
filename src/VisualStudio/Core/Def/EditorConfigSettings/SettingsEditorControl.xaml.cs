8e3a6618021f8fc3b4cb9242f6c15769099debbb
Jonathon Marolf <jmarolf@microsoft.com>
2021-07-29 17:21:36 -0700

    update UI text

--- src/VisualStudio/Core/Def/EditorConfigSettings/SettingsEditorControl.xaml.cs
+++ src/VisualStudio/Core/Def/EditorConfigSettings/SettingsEditorControl.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -28,7 +28,7 @@ namespace Microsoft.VisualStudio.LanguageServices.EditorConfigSettings
         private readonly IThreadingContext _threadingContext;
         private readonly EditorTextUpdater _textUpdater;
 
-        public static string Formatting => ServicesVSResources.Formatting;
+        public static string Whitespace => ServicesVSResources.Whitespace;
         public static string CodeStyle => ServicesVSResources.Code_Style;
         public static string Analyzers => ServicesVSResources.Analyzers;
 
