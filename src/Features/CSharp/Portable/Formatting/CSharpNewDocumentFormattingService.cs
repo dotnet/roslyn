53f8e6a09dd691552b80523729c9fd3fb27898f6
David Wengier <david.wengier@microsoft.com>
2021-08-05 16:48:12 +1000

    Create service for formatting new documents
    The service simply iterates through a number of providers and calls each one in turn, building up the new document as it goes.

---
+++ src/Features/CSharp/Portable/Formatting/CSharpNewDocumentFormattingService.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Host.Mef;
+
+namespace Microsoft.CodeAnalysis.CSharp.Formatting
+{
+    [ExportLanguageService(typeof(INewDocumentFormattingService), LanguageNames.CSharp)]
+    [Shared]
+    internal class CSharpNewDocumentFormattingService : AbstractNewDocumentFormattingService
+    {
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public CSharpNewDocumentFormattingService([ImportMany] IEnumerable<INewDocumentFormattingProvider> providers)
+            : base(providers)
+        {
+        }
+    }
+}
---
+++ src/Features/Core/Portable/Formatting/INewDocumentFormattingService.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.CodeAnalysis.Formatting
+{
+    internal interface INewDocumentFormattingService : ILanguageService
+    {
+        /// <summary>
+        /// Formats a new document that is being added to a project from the Add New Item dialog.
+        /// </summary>
+        /// <remarks>
+        /// Calls to this method will be on the UI thread so consider using .ConfigureAwait(true) to avoid unnecssary thread switching.
+        /// </remarks>
+        Task<Document> FormatNewDocumentAsync(Document document, CancellationToken cancellationToken);
+    }
+}
