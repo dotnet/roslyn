aa365306525d5d53c7d224260952fd23736342c1
Soren Dahl <t-sorendahl@microsoft.com>
2021-08-06 13:07:02 -0700

    Added a strongly typed version of GetBlockFromBegin which only applies to DeclarationStatementSyntaxes

--- src/Workspaces/VisualBasic/Portable/CodeGeneration/MethodGenerator.vb
+++ src/Workspaces/VisualBasic/Portable/CodeGeneration/MethodGenerator.vb
@@ -52,10 +52,9 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.CodeGeneration
         Public Shared Function GenerateMethodDeclaration(method As IMethodSymbol,
                                                          destination As CodeGenerationDestination,
                                                          options As CodeGenerationOptions) As StatementSyntax
-            Dim reusableSyntax = GetReuseableSyntaxNodeForSymbol(Of StatementSyntax)(method, options)
+            Dim reusableSyntax = GetReuseableSyntaxNodeForSymbol(Of DeclarationStatementSyntax)(method, options)
             If reusableSyntax IsNot Nothing Then
-                Return DirectCast(reusableSyntax.GetBlockFromBegin(), StatementSyntax)
-                'Return reusableSyntax
+                Return reusableSyntax.GetDeclarationBlockFromBegin()
             End If
 
             Dim declaration = GenerateMethodDeclarationWorker(method, destination, options)
