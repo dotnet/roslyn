93d0bd7e3b3747d4c6856529121a00c4a8a8eda2
Soren Dahl <t-sorendahl@microsoft.com>
2021-08-12 14:32:36 -0700

    Make service field readonly

--- src/Features/Core/Portable/MoveStaticMembers/MoveStaticMembersWithDialogCodeAction.cs
+++ src/Features/Core/Portable/MoveStaticMembers/MoveStaticMembersWithDialogCodeAction.cs
@@ -27,7 +27,7 @@ namespace Microsoft.CodeAnalysis.MoveStaticMembers
         private readonly Document _document;
         private readonly ISymbol? _selectedMember;
         private readonly INamedTypeSymbol _selectedType;
-        private IMoveStaticMembersOptionsService _service;
+        private readonly IMoveStaticMembersOptionsService _service;
 
         public TextSpan Span { get; }
         public override string Title => FeaturesResources.Move_static_members_to_another_type;
