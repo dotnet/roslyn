84383fa5f482ada0cdd51c2a4e94af4cd3886924
David Wengier <david.wengier@microsoft.com>
2021-08-13 09:53:18 +1000

    Call new document formatting service from extract type (#55522)

--- src/Features/Core/Portable/Shared/Utilities/ExtractTypeHelpers.cs
+++ src/Features/Core/Portable/Shared/Utilities/ExtractTypeHelpers.cs
@@ -53,7 +53,7 @@ namespace Microsoft.CodeAnalysis.Shared.Utilities
             ProjectId projectId,
             IEnumerable<string> folders,
             INamedTypeSymbol newSymbol,
-            ImmutableArray<SyntaxTrivia> fileBanner,
+            Document hintDocument,
             CancellationToken cancellationToken)
         {
             var newDocumentId = DocumentId.CreateNewId(projectId, debugName: fileName);
@@ -73,14 +73,19 @@ namespace Microsoft.CodeAnalysis.Shared.Utilities
                 options: options,
                 cancellationToken: cancellationToken).ConfigureAwait(false);
 
+            var formattingSerivce = newTypeDocument.GetLanguageService<INewDocumentFormattingService>();
+            if (formattingSerivce is not null)
+            {
+                newTypeDocument = await formattingSerivce.FormatNewDocumentAsync(newTypeDocument, hintDocument, cancellationToken).ConfigureAwait(false);
+            }
+
             var syntaxRoot = await newTypeDocument.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-            var rootWithBanner = syntaxRoot.WithPrependedLeadingTrivia(fileBanner);
 
             var typeAnnotation = new SyntaxAnnotation();
             var syntaxFacts = newTypeDocument.GetRequiredLanguageService<ISyntaxFactsService>();
 
-            var declarationNode = rootWithBanner.DescendantNodes().First(syntaxFacts.IsTypeDeclaration);
-            var annotatedRoot = rootWithBanner.ReplaceNode(declarationNode, declarationNode.WithAdditionalAnnotations(typeAnnotation));
+            var declarationNode = syntaxRoot.DescendantNodes().First(syntaxFacts.IsTypeDeclaration);
+            var annotatedRoot = syntaxRoot.ReplaceNode(declarationNode, declarationNode.WithAdditionalAnnotations(typeAnnotation));
 
             newTypeDocument = newTypeDocument.WithSyntaxRoot(annotatedRoot);
 
