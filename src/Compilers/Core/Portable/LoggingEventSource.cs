96e5355946d57c9a0d4cf9f2443e2d7864a37374
Chris Sienkiewicz <chsienki@microsoft.com>
2021-08-24 19:50:30 -0700

    Add logging event source

---
+++ src/Compilers/Core/Portable/LoggingEventSource.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis
+{
+    [EventSource(Name = "Microsoft-CodeAnalysis-General")]
+    internal sealed class LoggingEventSource : EventSource
+    {
+        public static readonly LoggingEventSource Instance = new LoggingEventSource();
+
+        public class Keywords
+        {
+            public const EventKeywords Performance = (EventKeywords)1;
+        }
+
+        public class Tasks
+        {
+            public const EventTask Generators = (EventTask)1;
+        }
+
+        [NonEvent]
+        public void ReportGeneratorRunTime(TimeSpan elapsed) => ReportGeneratorRunTime(elapsed.Ticks);
+
+        [Event(1, Message = "Generators ran for {0} ticks", Keywords = Keywords.Performance, Level = EventLevel.Informational, Task = Tasks.Generators)]
+        private void ReportGeneratorRunTime(long elapsedTicks) => WriteEvent(1, elapsedTicks);
+
+        [NonEvent]
+        public void ReportSingleGeneratorRunTime(ISourceGenerator generator, TimeSpan elapsed)
+        {
+            var type = generator.GetGeneratorType();
+            ReportSingleGeneratorRunTime(type.FullName!, type.Assembly.Location, elapsed.Ticks);
+        }
+
+        [Event(2, Message = "Generator {0} ran for {2} ticks", Keywords = Keywords.Performance, Level = EventLevel.Informational, Task = Tasks.Generators)]
+        private void ReportSingleGeneratorRunTime(string generatorName, string assemblyPath, long elapsedTicks) => WriteEvent(2, generatorName, assemblyPath, elapsedTicks);
+
+    }
+}
