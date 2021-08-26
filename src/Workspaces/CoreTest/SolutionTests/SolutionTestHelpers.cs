df37efe461b3307a667b7c2775071f194bb81a5a
Jason Malinowski <jason.malinowski@microsoft.com>
2021-08-25 18:26:16 -0700

    Freeze semantics for all branches when asked
    We had logic in Document.WithFrozenPartialSemantics which meant we would
    only freeze semantics if the Soluion in question was the "primary"
    branch, that is the actual instance from some Workspace.CurrentSolution.
    This seems terribly unwise: features like completion call
    GetOpenDocumentInCurrentContextWithChanges when doing things like
    computing entries or committing; if the text snapshots were out of
    sync then that would always be forking. In that case Freeze did nothing
    at all -- it would return the original Solution instance, so asking for
    semantics would build the full compilation like before. This is even
    worse with source generators, since we weren't freezing the generator
    state either and would rerun generators when later asked.
    
    The comment before the code to me isn't really explaining why this
    behavior is desirable: it's correct that if you have a forked Solution
    there is no background compiler pulling it forward. But it'll still
    have the state that was computed when it was forked, which should be
    enough for anybody opting into partial semantics in the first place.
    
    While writing a test, I also discovered our existing tests around
    frozen partial semantics and generators weren't actually testing
    anything, since the workspace didn't have partial semantics on in
    the first place. Worse off, they still didn't work unless we also
    remove the BranchId check.

--- src/Workspaces/CoreTest/SolutionTests/SolutionTestHelpers.cs
+++ src/Workspaces/CoreTest/SolutionTests/SolutionTestHelpers.cs
@@ -32,6 +32,18 @@ namespace Microsoft.CodeAnalysis.UnitTests
             return workspace;
         }
 
+        public static Workspace CreateWorkspaceWithPartalSemantics(Type[]? additionalParts = null)
+            => new WorkspaceWithPartialSemantics(FeaturesTestCompositions.Features.AddParts(additionalParts).GetHostServices());
+
+        private class WorkspaceWithPartialSemantics : Workspace
+        {
+            public WorkspaceWithPartialSemantics(HostServices hostServices) : base(hostServices, workspaceKind: nameof(WorkspaceWithPartialSemantics))
+            {
+            }
+
+            protected internal override bool PartialSemanticsEnabled => true;
+        }
+
         public static Project AddEmptyProject(Solution solution, string languageName = LanguageNames.CSharp)
         {
             var id = ProjectId.CreateNewId();
