3113de9dea0bde8a1bd38ffebcf1ebb8a266b16b
Allison Chou <allichou@microsoft.com>
2021-08-27 00:55:10 -0700

    Add file

---
+++ src/Features/LanguageServer/Protocol/Handler/SemanticTokens/RoslynSemanticTokensDelta.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
+using System.Runtime.Serialization;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler.SemanticTokens
+{
+    internal sealed class RoslynSemanticTokensDelta : LSP.SemanticTokensDelta
+    {
+        /// <summary>
+        /// True if the token set may be incomplete.
+        /// </summary>
+        /// <remarks>
+        /// Certain clients such as Razor need to know whether we're returning partial results.
+        /// This may occur if the full compilation is not yet available.
+        /// </remarks>
+        [DataMember(Name = "isPartial")]
+        public bool IsPartial { get; set; }
+    }
+}
