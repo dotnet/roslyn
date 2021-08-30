0d5e0cb3ed74404e67a7b1cb8105885f5955726d
David Wengier <david.wengier@microsoft.com>
2021-08-31 08:24:43 +1000

    Source symbol tests

--- src/Compilers/CSharp/Test/Symbol/Symbols/Metadata/PE/LoadingMetadataTokens.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/Metadata/PE/LoadingMetadataTokens.cs
@@ -13,7 +13,7 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Symbols.Metadata.PE
     public class LoadingMetadataTokens : CSharpTestBase
     {
         [Fact]
-        public void Test1()
+        public void LoadPESymbols()
         {
             var compilation = CreateCompilation(
 @"
@@ -72,6 +72,63 @@ public struct S
                 var struct1 = module.GlobalNamespace.GetTypeMember("S");
                 Assert.Equal(33554435, struct1.MetadataToken);
             });
+        }
+
+        [Fact]
+        public void LoadSourceSymbols()
+        {
+            var compilation = CreateCompilation(
+@"
+public class C
+{
+    public int f = 0;
+
+    public int P { get; set; }
+
+    public void M(int p)
+    {
+    }
+
+    public void GM<T>()
+    {
+    }
+
+    public event System.Action E;
+}
+
+public struct S
+{
+}");
+
+            var assembly = compilation.Assembly;
+            Assert.Equal(0, assembly.MetadataToken);
+
+            var class1 = compilation.GlobalNamespace.GetTypeMember("C");
+            Assert.Equal(0, class1.MetadataToken);
+
+            var field = class1.GetMember("f");
+            Assert.Equal(0, field.MetadataToken);
+
+            var property = class1.GetMember("P");
+            Assert.Equal(0, property.MetadataToken);
+
+            var method = class1.GetMember("M");
+            Assert.Equal(0, method.MetadataToken);
+
+            var parameter = method.GetParameters().Single();
+            Assert.Equal(0, parameter.MetadataToken);
+
+            var genericMethod = class1.GetMember("GM");
+            Assert.Equal(0, genericMethod.MetadataToken);
+
+            var typeParameter = genericMethod.GetMemberTypeParameters().Single();
+            Assert.Equal(0, typeParameter.MetadataToken);
+
+            var event1 = class1.GetMember("E");
+            Assert.Equal(0, event1.MetadataToken);
+
+            var struct1 = compilation.GlobalNamespace.GetTypeMember("S");
+            Assert.Equal(0, struct1.MetadataToken);
         }
     }
 }
