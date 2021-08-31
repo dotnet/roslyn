977360f2d6f05883e1073fb42a2abed1b19391b3
David Wengier <david.wengier@microsoft.com>
2021-08-31 14:50:59 +1000

    VB source symbol tests

--- src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/Metadata/PE/LoadingMetadataTokens.vb
+++ src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/Metadata/PE/LoadingMetadataTokens.vb
@@ -12,7 +12,7 @@ Namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Symbols.Metadata.PE
         Inherits BasicTestBase
 
         <Fact>
-        Public Sub Test1()
+        Public Sub LoadPESymbols()
             Dim compilation = CreateCompilation("
 Public Class C
 
@@ -71,6 +71,60 @@ End Structure
                                                            End Sub)
         End Sub
 
+        <Fact>
+        Public Sub LoadSourceSymbols()
+            Dim compilation = CreateCompilation("
+Public Class C
+
+    Public f As Integer = 0
+
+    Public Property P As Integer
+
+    Public Sub M(ByVal p As Integer)
+    End Sub
+
+    Public Sub GM(Of T)()
+    End Sub
+
+    Public Event E As System.Action
+
+End Class
+
+Public Structure S
+
+End Structure
+")
+
+            Dim assembly = compilation.Assembly
+            Assert.Equal(0, assembly.MetadataToken)
+
+            Dim class1 = compilation.GlobalNamespace.GetTypeMember("C")
+            Assert.Equal(0, class1.MetadataToken)
+
+            Dim field = class1.GetMember("f")
+            Assert.Equal(0, field.MetadataToken)
+
+            Dim [property] = class1.GetMember("P")
+            Assert.Equal(0, [property].MetadataToken)
+
+            Dim method = class1.GetMember("M")
+            Assert.Equal(0, method.MetadataToken)
+
+            Dim parameter = method.GetParameters().Single()
+            Assert.Equal(0, parameter.MetadataToken)
+
+            Dim genericMethod = class1.GetMember(Of MethodSymbol)("GM")
+            Assert.Equal(0, genericMethod.MetadataToken)
+
+            Dim typeParameter = genericMethod.TypeParameters.Single()
+            Assert.Equal(0, typeParameter.MetadataToken)
+
+            Dim event1 = class1.GetMember("E")
+            Assert.Equal(0, event1.MetadataToken)
+
+            Dim struct1 = compilation.GlobalNamespace.GetTypeMember("S")
+            Assert.Equal(0, struct1.MetadataToken)
+        End Sub
     End Class
 
 End Namespace
