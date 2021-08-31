f976ffd342056b2d9323ea6a5f58ae452e6d3725
Shen Chen <shech@microsoft.com>
2021-08-31 11:17:08 -0700

    Remove experimental suffix

--- src/VisualStudio/VisualBasic/Impl/Options/AdvancedOptionPageStrings.vb
+++ src/VisualStudio/VisualBasic/Impl/Options/AdvancedOptionPageStrings.vb
@@ -299,7 +299,6 @@ Namespace Microsoft.VisualStudio.LanguageServices.VisualBasic.Options
         Public ReadOnly Property Combine_inheritance_margin_with_indicator_margin As String =
             ServicesVSResources.Combine_inheritance_margin_with_indicator_margin
 
-        Public ReadOnly Property Inheritance_Margin_experimental As String =
-            ServicesVSResources.Inheritance_Margin_experimental
+        Public ReadOnly Property Inheritance_Margin As String = ServicesVSResources.Inheritance_Margin
     End Module
 End Namespace
