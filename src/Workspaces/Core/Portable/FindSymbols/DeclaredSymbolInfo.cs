f07ec590b2c531c1993637ae7154f95732be1550
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-09-03 18:27:55 -0700

    rename and doc

--- src/Workspaces/Core/Portable/FindSymbols/DeclaredSymbolInfo.cs
+++ src/Workspaces/Core/Portable/FindSymbols/DeclaredSymbolInfo.cs
@@ -130,14 +130,15 @@ namespace Microsoft.CodeAnalysis.FindSymbols
             int parameterCount = 0,
             int typeParameterCount = 0)
         {
-            const uint MaxFlagValue5 = 0b11111;
-            const uint MaxFlagValue4 = 0b1111;
+            // Max value that we can store depending on how many bits we have to store that particular value in.
+            const uint Max5BitValue = 0b11111;
+            const uint Max4BitValue = 0b1111;
 
-            Contract.ThrowIfTrue((uint)accessibility > MaxFlagValue4);
-            Contract.ThrowIfTrue((uint)kind > MaxFlagValue5);
+            Contract.ThrowIfTrue((uint)accessibility > Max4BitValue);
+            Contract.ThrowIfTrue((uint)kind > Max5BitValue);
 
-            parameterCount = Math.Min(parameterCount, (byte)MaxFlagValue4);
-            typeParameterCount = Math.Min(typeParameterCount, (byte)MaxFlagValue4);
+            parameterCount = Math.Min(parameterCount, (byte)Max4BitValue);
+            typeParameterCount = Math.Min(typeParameterCount, (byte)Max4BitValue);
 
             var flags =
                 (uint)kind |
