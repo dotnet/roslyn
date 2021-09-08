2ba4d54cd68ceb8d66f16461fd18e8ff4c64787c
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-09-08 15:06:42 -0700

    Introduce a specializd type to ensure we pass the right kind along.

---
+++ src/Workspaces/Core/Portable/FindSymbols/FindReferences/MetadataUnifyingSymbolHashSet.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis.FindSymbols
+{
+    internal sealed class MetadataUnifyingSymbolHashSet : HashSet<ISymbol>
+    {
+        public MetadataUnifyingSymbolHashSet() : base(MetadataUnifyingEquivalenceComparer.Instance)
+        {
+        }
+    }
+}
