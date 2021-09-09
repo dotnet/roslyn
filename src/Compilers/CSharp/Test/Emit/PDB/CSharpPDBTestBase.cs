60d40b1e394f6444893f1f9c8fba4092a37561a8
David Wengier <david.wengier@microsoft.com>
2021-09-09 11:54:27 +1000

    Tests

--- src/Compilers/CSharp/Test/Emit/PDB/CSharpPDBTestBase.cs
+++ src/Compilers/CSharp/Test/Emit/PDB/CSharpPDBTestBase.cs
@@ -4,10 +4,16 @@
 
 #nullable disable
 
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
 using System.Xml;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
+using Microsoft.CodeAnalysis.Debugging;
+using Microsoft.CodeAnalysis.Emit;
 using Microsoft.CodeAnalysis.Test.Utilities;
 using Microsoft.CodeAnalysis.Text;
 using Roslyn.Test.Utilities;
@@ -66,6 +72,57 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.PDB
             }
 
             return false;
+        }
+
+        public static void TestTypeDocuments(string[] sources, params (string typeName, string documentName)[] expected)
+        {
+            var trees = sources.Select((s, i) => SyntaxFactory.ParseSyntaxTree(s, path: $"{i + 1}.cs", encoding: Encoding.UTF8)).ToArray();
+            var compilation = CreateCompilation(trees, options: TestOptions.DebugDll);
+
+            var pdbStream = new MemoryStream();
+            var pe = compilation.EmitToArray(EmitOptions.Default.WithDebugInformationFormat(DebugInformationFormat.PortablePdb), pdbStream: pdbStream);
+            pdbStream.Position = 0;
+
+            var metadata = ModuleMetadata.CreateFromImage(pe);
+            var metadataReader = metadata.GetMetadataReader();
+
+            var provider = MetadataReaderProvider.FromPortablePdbStream(pdbStream);
+            var pdbReader = provider.GetMetadataReader();
+
+            var actual = from handle in pdbReader.CustomDebugInformation
+                         let entry = pdbReader.GetCustomDebugInformation(handle)
+                         where pdbReader.GetGuid(entry.Kind).Equals(PortableCustomDebugInfoKinds.TypeDocument)
+                         select (typeName: GetTypeName(entry.Parent), documentName: GetDocumentNames(entry.Value));
+
+            AssertEx.Equal(expected, actual, itemSeparator: ", ", itemInspector: i => $"(\"{i.typeName}\", \"{i.documentName}\")");
+
+            string GetTypeName(EntityHandle handle)
+            {
+                var typeHandle = (TypeDefinitionHandle)handle;
+                var type = metadataReader.GetTypeDefinition(typeHandle);
+                return metadataReader.GetString(type.Name);
+            }
+
+            string GetDocumentNames(BlobHandle value)
+            {
+                var result = new List<string>();
+
+                var reader = pdbReader.GetBlobReader(value);
+                while (reader.RemainingBytes > 0)
+                {
+                    var documentRow = reader.ReadCompressedInteger();
+                    if (documentRow > 0)
+                    {
+                        var doc = pdbReader.GetDocument(MetadataTokens.DocumentHandle(documentRow));
+                        result.Add(pdbReader.GetString(doc.Name));
+                    }
+                }
+
+                // Order can be different in net472 vs net5 :(
+                result.Sort();
+
+                return string.Join(", ", result);
+            }
         }
     }
 }
