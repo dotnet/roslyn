daae60016f341197d5eea9bc8addab919cdec5a7
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-09-14 15:07:58 -0700

    Fix uninitialized values during construction

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Services/SyntaxFacts/VisualBasicFileBanner.vb
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Services/SyntaxFacts/VisualBasicFileBanner.vb
@@ -19,7 +19,16 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.LanguageServices
         Protected Sub New()
         End Sub
 
-        Protected Overrides ReadOnly Property SyntaxFacts As ISyntaxFacts = VisualBasicSyntaxFacts.Instance
-        Protected Overrides ReadOnly Property DocumentationCommentService As IDocumentationCommentService = VisualBasicDocumentationCommentService.Instance
+        Protected Overrides ReadOnly Property SyntaxFacts As ISyntaxFacts
+            Get
+                Return VisualBasicSyntaxFacts.Instance
+            End Get
+        End Property
+
+        Protected Overrides ReadOnly Property DocumentationCommentService As IDocumentationCommentService
+            Get
+                Return VisualBasicDocumentationCommentService.Instance
+            End Get
+        End Property
     End Class
 End Namespace
