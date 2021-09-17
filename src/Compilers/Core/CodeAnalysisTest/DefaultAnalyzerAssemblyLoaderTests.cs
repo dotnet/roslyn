01486c8e9420aaa90bf7f98d9e09288085647db5
gel@microsoft.com <gel@microsoft.com>
2021-09-16 17:39:48 -0700

    Add a test

--- src/Compilers/Core/CodeAnalysisTest/DefaultAnalyzerAssemblyLoaderTests.cs
+++ src/Compilers/Core/CodeAnalysisTest/DefaultAnalyzerAssemblyLoaderTests.cs
@@ -434,6 +434,38 @@ Delta: Epsilon: Test E
                 allReferenceSimpleNames.Free();
             }
         }
+
+        [Fact]
+        public void AssemblyLoadingInNonDefaultContext_AnalyzerReferencesSystemCollectionsImmutable()
+        {
+            // Create a separate ALC as the compiler context, load the compiler assembly and a modified version of S.C.I into it,
+            // then use that to load and run `AssemblyLoadingInNonDefaultContextHelper` below. We expect the analyzer running in
+            // its own `DirectoryLoadContext` would use the bogus S.C.I loaded in the compiler load context instead of the real one
+            // in the default context.
+            var compilerContext = new System.Runtime.Loader.AssemblyLoadContext("compilerContext");
+            _ = compilerContext.LoadFromAssemblyPath(_testFixture.UserSystemCollectionsImmutable.Path);
+            _ = compilerContext.LoadFromAssemblyPath(typeof(DefaultAnalyzerAssemblyLoader).GetTypeInfo().Assembly.Location);
+
+            var testAssembly = compilerContext.LoadFromAssemblyPath(typeof(DefaultAnalyzerAssemblyLoaderTests).GetTypeInfo().Assembly.Location);
+            var testObject = testAssembly.CreateInstance(typeof(DefaultAnalyzerAssemblyLoaderTests).FullName!,
+                ignoreCase: false, BindingFlags.Default, binder: null, args: new object[] { _output, _testFixture }, null, null)!;
+
+            StringBuilder sb = new();
+            testObject.GetType().GetMethod(nameof(AssemblyLoadingInNonDefaultContextHelper), BindingFlags.Instance | BindingFlags.NonPublic)!.Invoke(testObject, new object[] { sb });
+            Assert.Equal("42", sb.ToString());
+        }
+
+        // This help does the samething as in `AssemblyLoading_AnalyzerReferencesSystemCollectionsImmutable_01` test above except the assertions.
+        private void AssemblyLoadingInNonDefaultContextHelper(StringBuilder sb)
+        {
+            var loader = new DefaultAnalyzerAssemblyLoader();
+            loader.AddDependencyLocation(_testFixture.UserSystemCollectionsImmutable.Path);
+            loader.AddDependencyLocation(_testFixture.AnalyzerReferencesSystemCollectionsImmutable1.Path);
+
+            Assembly analyzerAssembly = loader.LoadFromPath(_testFixture.AnalyzerReferencesSystemCollectionsImmutable1.Path);
+            var analyzer = analyzerAssembly.CreateInstance("Analyzer")!;
+            analyzer.GetType().GetMethod("Method")!.Invoke(analyzer, new object[] { sb });
+        }
 #endif
     }
 }
