2afc0fc882acb6ce056144368f5ed3907ed4ea91
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-09-21 10:20:19 -0700

    New reg value

--- src/EditorFeatures/Core/Implementation/Suggestions/SuggestionsOptions.cs
+++ src/EditorFeatures/Core/Implementation/Suggestions/SuggestionsOptions.cs
@@ -11,7 +11,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Suggestions
         private const string FeatureName = "SuggestionsOptions";
 
         public static readonly Option2<bool> Asynchronous = new(FeatureName, nameof(Asynchronous), defaultValue: true,
-            new RoamingProfileStorageLocation("TextEditor.Specific.Suggestions.Asynchronous2"));
+            new RoamingProfileStorageLocation("TextEditor.Specific.Suggestions.Asynchronous3"));
 
         public static readonly Option2<bool> AsynchronousQuickActionsDisableFeatureFlag = new(FeatureName, nameof(AsynchronousQuickActionsDisableFeatureFlag), defaultValue: false,
             new FeatureFlagStorageLocation("Roslyn.AsynchronousQuickActionsDisable"));
