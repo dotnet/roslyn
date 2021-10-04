ca589c55993e2914093b3ec3202dfbcd7dbe12b5
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-10-04 15:06:56 -0700

    Skip tracked test

--- src/EditorFeatures/Test2/Simplification/CastSimplificationTests.vb
+++ src/EditorFeatures/Test2/Simplification/CastSimplificationTests.vb
@@ -954,7 +954,7 @@ class X
             Await TestAsync(input, expected)
         End Function
 
-        <Fact, Trait(Traits.Feature, Traits.Features.Simplification)>
+        <Fact(Skip:="https://github.com/dotnet/roslyn/issues/56938"), Trait(Traits.Feature, Traits.Features.Simplification)>
         <WorkItem(529855, "http://vstfdevdiv:8080/DevDiv2/DevDiv/_workitems/edit/529855")>
         Public Async Function TestCSharp_Remove_CastInIsExpression() As Task
             Dim input =
