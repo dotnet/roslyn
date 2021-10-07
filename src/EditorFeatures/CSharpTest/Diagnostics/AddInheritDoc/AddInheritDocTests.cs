fa3239798fbec9f192357600267ed8592fa98868
Martin Strecker <Martin.Strecker@web.de>
2021-10-07 14:19:23 +0100

    WIP: First successful fix of CS1591

---
+++ src/EditorFeatures/CSharpTest/Diagnostics/AddInheritDoc/AddInheritDocTests.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.CodeFixes.AddInheritDoc;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Roslyn.Test.Utilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics.AddInheritDoc
+{
+    public class AddInheritDocTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
+    {
+        public AddInheritDocTests(ITestOutputHelper logger) : base(logger)
+        {
+        }
+
+        internal override (DiagnosticAnalyzer, CodeFixProvider) CreateDiagnosticProviderAndFixer(Workspace workspace)
+            => (null, new AddInheritDocCodeFixProvider());
+
+        private async Task TestAsync(string initialMarkup, string expectedMarkup)
+        {
+            await TestAsync(initialMarkup, expectedMarkup, CSharpParseOptions.Default.WithDocumentationMode(DocumentationMode.Diagnose));
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsAddInheritDoc)]
+        public async Task AddMissingInheritDocOnClass()
+        {
+            await TestAsync(
+            @"
+/// Some doc.
+public class BaseClass
+{
+}
+public class [|Derived|]: BaseClass
+{
+}",
+            @"
+/// Some doc.
+public class BaseClass
+{
+}
+///<inheritdoc/> public class Derived: BaseClass
+{
+}");
+        }
+    }
+}
---
+++ src/Features/CSharp/Portable/CodeFixes/AddInheritDoc/AddInheritDocCodeFixProvider.cs
@@ -0,0 +1,79 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Editing;
+
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.AddInheritDoc
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = PredefinedCodeFixProviderNames.AddInheritDoc), Shared]
+    internal sealed class AddInheritDocCodeFixProvider : SyntaxEditorBasedCodeFixProvider
+    {
+        /// <summary>
+        /// CS1591: Missing XML comment for publicly visible type or member 'Type_or_Member'
+        /// </summary>
+        private const string CS1591 = nameof(CS1591);
+
+        [ImportingConstructor]
+        [SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
+        public AddInheritDocCodeFixProvider()
+        {
+        }
+
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(CS1591);
+
+        internal override CodeFixCategory CodeFixCategory => CodeFixCategory.Compile;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.FirstOrDefault(d => d.Id == CS1591);
+            if (diagnostic != null)
+            {
+                context.RegisterCodeFix(new MyCodeAction("TODO", c => FixAsync(context.Document, diagnostic, c)), context.Diagnostics);
+            }
+            return Task.CompletedTask;
+        }
+
+        protected override Task FixAllAsync(Document document, ImmutableArray<Diagnostic> diagnostics, SyntaxEditor editor, CancellationToken cancellationToken)
+        {
+            foreach (var diagnostic in diagnostics)
+            {
+                var node = editor.OriginalRoot.FindNode(diagnostic.Location.SourceSpan);
+                // We can safely assume, that there is no leading doc comment, because that is what CS1591 is telling us.
+                // So we create a new ///<inheritdoc/> comment.
+                var lessThanToken = Token(SyntaxKind.LessThanToken).WithLeadingTrivia(DocumentationCommentExterior("///"));
+                var singleLineInheritDocComment = DocumentationCommentTrivia(
+                    kind: SyntaxKind.SingleLineDocumentationCommentTrivia,
+                    content: new SyntaxList<Syntax.XmlNodeSyntax>(XmlEmptyElement(lessThanToken, name: XmlName("inheritdoc"), attributes: default, slashGreaterThanToken: Token(SyntaxKind.SlashGreaterThanToken))),
+                    endOfComment: Token(SyntaxKind.EndOfDocumentationCommentToken));
+                var wrappedInheritedDoc = Trivia(singleLineInheritDocComment);
+                var existingLeadingTrivia = node.GetLeadingTrivia();
+                var newLeadingTrivia = existingLeadingTrivia.Add(wrappedInheritedDoc);
+                editor.ReplaceNode(node, node.WithLeadingTrivia(newLeadingTrivia));
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private class MyCodeAction : CodeAction.DocumentChangeAction
+        {
+            public MyCodeAction(string title, Func<CancellationToken, Task<Document>> createChangedDocument)
+                : base(title, createChangedDocument, equivalenceKey: title)
+            {
+            }
+        }
+    }
+}
