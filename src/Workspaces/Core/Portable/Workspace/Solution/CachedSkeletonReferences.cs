fea7fc9a31aeb25f61d58f6d2a9165d0b63508a5
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-10-14 12:21:24 -0700

    Temp

--- src/Workspaces/Core/Portable/Workspace/Solution/CachedSkeletonReferences.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/CachedSkeletonReferences.cs
@@ -46,6 +46,8 @@ namespace Microsoft.CodeAnalysis
         /// </summary>
         private static readonly ConditionalWeakTable<Compilation, SkeletonReferenceSet> s_compilationToReferenceMap = new();
 
+        private readonly ProjectId _projectId;
+
         private readonly SemaphoreSlim _gate = new(initialCount: 1);
 
         /// <summary>
@@ -58,13 +60,17 @@ namespace Microsoft.CodeAnalysis
         /// </summary>
         private SkeletonReferenceSet? _skeletonReferenceSet;
 
-        public static readonly CachedSkeletonReferences Empty =
-            new(version: null, skeletonReferenceSet: null);
+        public CachedSkeletonReferences(ProjectId projectId)
+            : this(projectId, version: null, skeletonReferenceSet: null)
+        {
+        }
 
         private CachedSkeletonReferences(
+            ProjectId projectId,
             VersionStamp? version,
             SkeletonReferenceSet? skeletonReferenceSet)
         {
+            _projectId = projectId;
             _version = version;
             _skeletonReferenceSet = skeletonReferenceSet;
         }
@@ -77,13 +83,13 @@ namespace Microsoft.CodeAnalysis
                 // can use this data if either the version changed, or they weren't able to build a skeleton for themselves.
                 // By passing along a copy we ensure that if they have a different version, they'll end up producing a new
                 // SkeletonReferenceSet where they'll store their own data in which will not affect prior ProjectStates.
-                return new CachedSkeletonReferences(_version, _skeletonReferenceSet);
+                return new CachedSkeletonReferences(_projectId, _version, _skeletonReferenceSet);
             }
         }
 
         public async Task<MetadataReference> GetOrBuildReferenceAsync(
-            SolutionState solution,
-            ProjectReference projectReference,
+            Workspace workspace,
+            MetadataReferenceProperties properties,
             Compilation finalCompilation,
             VersionStamp version,
             CancellationToken cancellationToken)
@@ -91,11 +97,11 @@ namespace Microsoft.CodeAnalysis
             // First see if we already have a cached reference for either finalCompilation or for projectReference.
             // If we have one for the latter, we'll make sure that it's version matches what we're asking for before
             // returning it.
-            solution.Workspace.LogTestMessage($"Looking to see if we already have a skeleton assembly for {projectReference.ProjectId} before we build one...");
-            var reference = await TryGetReferenceAsync(solution, projectReference, finalCompilation, version, cancellationToken).ConfigureAwait(false);
+            workspace.LogTestMessage($"Looking to see if we already have a skeleton assembly for {_projectId} before we build one...");
+            var reference = await TryGetReferenceAsync(workspace, properties, finalCompilation, version, cancellationToken).ConfigureAwait(false);
             if (reference != null)
             {
-                solution.Workspace.LogTestMessage($"A reference was found {projectReference.ProjectId} so we're skipping the build.");
+                workspace.LogTestMessage($"A reference was found {_projectId} so we're skipping the build.");
                 return reference;
             }
 
@@ -103,20 +109,23 @@ namespace Microsoft.CodeAnalysis
 
             // first, prepare image
             // * NOTE * image is cancellable, do not create it inside of conditional weak table.
-            var service = solution.Workspace.Services.GetService<ITemporaryStorageService>();
-            var image = MetadataOnlyImage.Create(solution.Workspace, service, finalCompilation, cancellationToken);
+            var service = workspace.Services.GetService<ITemporaryStorageService>();
+            var image = MetadataOnlyImage.Create(workspace, service, finalCompilation, cancellationToken);
 
             if (image.IsEmpty)
             {
                 // unfortunately, we couldn't create one. see if we have one from previous compilation., it might be
                 // out-of-date big time, but better than nothing.
-                reference = await TryGetReferenceAsync(solution, projectReference, finalCompilation, version: null, cancellationToken).ConfigureAwait(false);
+                reference = await TryGetReferenceAsync(workspace, properties, finalCompilation, version: null, cancellationToken).ConfigureAwait(false);
                 if (reference != null)
                 {
-                    solution.Workspace.LogTestMessage($"We failed to create metadata so we're using the one we just found from an earlier version.");
+                    workspace.LogTestMessage($"We failed to create metadata so we're using the one we just found from an earlier version.");
                     return reference;
                 }
             }
+
+            // We either had an image, or we didn't have an image and we didn't have a previous value we could reuse.
+            // Just store this image against this version for us, and any future ProjectStates that fork from us.
 
             var tempReferenceSet = new SkeletonReferenceSet(image);
             var referenceSet = s_compilationToReferenceMap.GetValue(finalCompilation, _ => tempReferenceSet);
@@ -127,40 +136,40 @@ namespace Microsoft.CodeAnalysis
                 image.Cleanup();
             }
 
-            await (_gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
+            // Store this for ourselves, and for any project states that clone from us from this point onwards.
+            using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
             {
                 _version = version;
                 _skeletonReferenceSet = referenceSet;
             }
 
-            return await referenceSet.GetMetadataReferenceAsync(
-                finalCompilation, projectReference.Aliases, projectReference.EmbedInteropTypes, cancellationToken).ConfigureAwait(false);
+            return await referenceSet.GetMetadataReferenceAsync(finalCompilation, properties, cancellationToken).ConfigureAwait(false);
         }
 
         /// <summary>
-        /// Tries to get the <see cref="MetadataReference"/> associated with the provided <paramref name="projectReference"/>
-        /// that produced the <see cref="Compilation"/> <paramref name="finalOrDeclarationCompilation"/>.
+        /// Tries to get the <see cref="MetadataReference"/> with the given <paramref name="properties"/>
+        /// for the <see cref="Compilation"/> <paramref name="finalOrDeclarationCompilation"/>.
         /// </summary>
         public Task<MetadataReference?> TryGetReferenceAsync(
-            SolutionState solution,
-            ProjectReference projectReference,
+            Workspace workspace,
+            MetadataReferenceProperties properties,
             Compilation finalOrDeclarationCompilation,
             VersionStamp version,
             CancellationToken cancellationToken)
         {
-            return TryGetReferenceAsync(solution, projectReference, finalOrDeclarationCompilation, (VersionStamp?)version, cancellationToken);
+            return TryGetReferenceAsync(workspace, properties, finalOrDeclarationCompilation, (VersionStamp?)version, cancellationToken);
         }
 
         /// <remarks>
-        /// <inheritdoc cref="TryGetReferenceAsync(SolutionState, ProjectReference, Compilation, VersionStamp, CancellationToken)"/>.
+        /// <inheritdoc cref="TryGetReferenceAsync(Workspace, MetadataReferenceProperties, Compilation, VersionStamp, CancellationToken)"/>.
         /// If <paramref name="version"/> is <see langword="null"/>, any <see cref="MetadataReference"/> for that
         /// project may be returned, even if it doesn't correspond to that compilation.  This is useful in error tolerance
         /// cases as building a skeleton assembly may easily fail.  In that case it's better to use the last successfully
         /// built skeleton than just have no semantic information for that project at all.
         /// </remarks> 
         private async Task<MetadataReference?> TryGetReferenceAsync(
-            SolutionState solution,
-            ProjectReference projectReference,
+            Workspace workspace,
+            MetadataReferenceProperties properties,
             Compilation finalOrDeclarationCompilation,
             VersionStamp? version,
             CancellationToken cancellationToken)
@@ -168,22 +177,22 @@ namespace Microsoft.CodeAnalysis
             // if we have one from snapshot cache, use it. it will make sure same compilation will get same metadata reference always.
             if (s_compilationToReferenceMap.TryGetValue(finalOrDeclarationCompilation, out var referenceSet))
             {
-                solution.Workspace.LogTestMessage($"Found already cached metadata in {nameof(s_compilationToReferenceMap)} for the exact compilation");
+                workspace.LogTestMessage($"Found already cached metadata in {nameof(s_compilationToReferenceMap)} for the exact compilation");
                 return await referenceSet.GetMetadataReferenceAsync(
-                    finalOrDeclarationCompilation, projectReference.Aliases, projectReference.EmbedInteropTypes, cancellationToken).ConfigureAwait(false);
+                    finalOrDeclarationCompilation, properties, cancellationToken).ConfigureAwait(false);
             }
 
             // okay, now use version based cache that can live multiple compilation as long as there is no semantic changes.
 
-            var result = await TryGetReferenceAsync(projectReference, finalOrDeclarationCompilation, version, cancellationToken).ConfigureAwait(false);
+            var result = await TryGetReferenceAsync(properties, finalOrDeclarationCompilation, version, cancellationToken).ConfigureAwait(false);
             if (result != null)
-                solution.Workspace.LogTestMessage($"Found already cached metadata for the branch and version {version}");
+                workspace.LogTestMessage($"Found already cached metadata for the branch and version {version}");
 
             return result;
         }
 
         private async Task<MetadataReference?> TryGetReferenceAsync(
-            ProjectReference projectReference,
+            MetadataReferenceProperties properties,
             Compilation finalOrDeclarationCompilation,
             VersionStamp? version,
             CancellationToken cancellationToken)
@@ -203,8 +212,7 @@ namespace Microsoft.CodeAnalysis
                 }
             }
 
-            return await set.GetMetadataReferenceAsync(
-                finalOrDeclarationCompilation, projectReference.Aliases, projectReference.EmbedInteropTypes, cancellationToken).ConfigureAwait(false);
+            return await set.GetMetadataReferenceAsync(finalOrDeclarationCompilation, properties, cancellationToken).ConfigureAwait(false);
         }
 
         private class SkeletonReferenceSet
@@ -221,27 +229,33 @@ namespace Microsoft.CodeAnalysis
             }
 
             public async Task<MetadataReference> GetMetadataReferenceAsync(
-                Compilation compilation, ImmutableArray<string> aliases, bool embedInteropTypes, CancellationToken cancellationToken)
+                Compilation compilation, MetadataReferenceProperties properties, CancellationToken cancellationToken)
             {
-                var key = new MetadataReferenceProperties(MetadataImageKind.Assembly, aliases, embedInteropTypes);
-
+                // lookup first and eagerly return cached value if we have it.
                 using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
                 {
-                    if (!_metadataReferences.TryGetValue(key, out var weakMetadata) ||
-                        !weakMetadata.TryGetTarget(out var metadataReference))
+                    if (_metadataReferences.TryGetValue(properties, out var weakMetadata) &&
+                        weakMetadata.TryGetTarget(out var metadataReference))
                     {
-                        // here we give out strong reference to compilation. so there is possibility that we end up making 2 compilations for same project alive.
-                        // one for final compilation and one for declaration only compilation. but the final compilation will be eventually kicked out from compilation cache
-                        // if there is no activity on the project. or the declaration compilation will go away if the project that depends on the reference doesn't have any
-                        // activity when it is kicked out from compilation cache. if there is an activity, then both will updated as activity happens.
-                        // so simply put, things will go away when compilations are kicked out from the cache or due to user activity.
-                        //
-                        // there is one case where we could have 2 compilations for same project alive. if a user opens a file that requires a skeleton assembly when the skeleton
-                        // assembly project didn't reach the final stage yet and then the user opens another document that is part of the skeleton assembly project 
-                        // and then never change it. declaration compilation will be alive by skeleton assembly and final compilation will be alive by background compiler.
-                        metadataReference = _image.CreateReference(aliases, embedInteropTypes, new DeferredDocumentationProvider(compilation));
-                        weakMetadata = new WeakReference<MetadataReference>(metadataReference);
-                        _metadataReferences[key] = weakMetadata;
+                        return metadataReference;
+                    }
+                }
+
+                // otherwise, create the metadata outside of the lock, and then try to assign it if no one else beat us
+                {
+                    var metadataReference = _image.CreateReference(properties.Aliases, properties.EmbedInteropTypes, new DeferredDocumentationProvider(compilation));
+                    var weakMetadata = new WeakReference<MetadataReference>(metadataReference);
+
+                    using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
+                    {
+                        if (_metadataReferences.TryGetValue(properties, out var innherWeakMetadata) &&
+                            weakMetadata.TryGetTarget(out var innerMetadataReference))
+                        {
+                            // someone beat us to writing this.
+                            return innerMetadataReference;
+                        }
+
+                        _metadataReferences[properties] = weakMetadata;
                     }
 
                     return metadataReference;
