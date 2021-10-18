fa5e6cdb4c48c953cefad3b97abb775e7586c986
Sam Harwell <Sam.Harwell@microsoft.com>
2021-10-18 07:29:29 -0700

    Implement 'Enable nullable reference types' refactoring

---
+++ src/EditorFeatures/CSharpTest/CodeActions/EnableNullable/EnableNullableTests.cs
@@ -0,0 +1,260 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Xunit;
+using VerifyCS = Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions.CSharpCodeRefactoringVerifier<
+    Microsoft.CodeAnalysis.CSharp.CodeRefactorings.EnableNullable.EnableNullableCodeRefactoringProvider>;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.CodeActions.EnableNullable
+{
+    public class EnableNullableTests
+    {
+        private static readonly Func<Solution, ProjectId, Solution> s_enableNullableInFixedSolution =
+            (solution, projectId) =>
+            {
+                var project = solution.GetRequiredProject(projectId);
+                var document = project.Documents.First();
+
+                // Only the input solution contains '#nullable enable'
+                if (!document.GetTextSynchronously(CancellationToken.None).ToString().Contains("#nullable enable"))
+                {
+                    var compilationOptions = (CSharpCompilationOptions)solution.GetRequiredProject(projectId).CompilationOptions!;
+                    solution = solution.WithProjectCompilationOptions(projectId, compilationOptions.WithNullableContextOptions(NullableContextOptions.Enable));
+                }
+
+                return solution;
+            };
+
+        [Fact]
+        public async Task EnabledOnNullableEnable()
+        {
+            var code1 = @"
+#nullable enable$$
+
+class Example
+{
+  string? value;
+}
+";
+            var code2 = @"
+class Example2
+{
+  string value;
+}
+";
+            var code3 = @"
+class Example3
+{
+#nullable enable
+  string? value;
+#nullable restore
+}
+";
+            var code4 = @"
+#nullable disable
+
+class Example4
+{
+  string value;
+}
+";
+
+            var fixedCode1 = @"
+#nullable disable
+
+#nullable restore
+
+class Example
+{
+  string? value;
+}
+";
+            var fixedCode2 = @"
+#nullable disable
+
+class Example2
+{
+  string value;
+}
+";
+            var fixedCode3 = @"
+#nullable disable
+
+class Example3
+{
+#nullable restore
+  string? value;
+#nullable disable
+}
+";
+            var fixedCode4 = @"
+#nullable disable
+
+#nullable disable
+
+class Example4
+{
+  string value;
+}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources =
+                    {
+                        code1,
+                        code2,
+                        code3,
+                        code4,
+                    },
+                },
+                FixedState =
+                {
+                    Sources =
+                    {
+                        fixedCode1,
+                        fixedCode2,
+                        fixedCode3,
+                        fixedCode4,
+                    },
+                },
+                SolutionTransforms = { s_enableNullableInFixedSolution },
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task PlacementAfterHeader()
+        {
+            var code1 = @"
+#nullable enable$$
+
+class Example
+{
+  string? value;
+}
+";
+            var code2 = @"// File header line 1
+// File header line 2
+
+class Example2
+{
+  string value;
+}
+";
+            var code3 = @"#region File Header
+// File header line 1
+// File header line 2
+#endregion
+
+class Example3
+{
+  string value;
+}
+";
+
+            var fixedCode1 = @"
+#nullable disable
+
+#nullable restore
+
+class Example
+{
+  string? value;
+}
+";
+            var fixedCode2 = @"// File header line 1
+// File header line 2
+
+#nullable disable
+
+class Example2
+{
+  string value;
+}
+";
+            var fixedCode3 = @"#nullable disable
+
+#region File Header
+// File header line 1
+// File header line 2
+#endregion
+
+class Example3
+{
+  string value;
+}
+";
+
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources =
+                    {
+                        code1,
+                        code2,
+                        code3,
+                    },
+                },
+                FixedState =
+                {
+                    Sources =
+                    {
+                        fixedCode1,
+                        fixedCode2,
+                        fixedCode3,
+                    },
+                },
+                SolutionTransforms = { s_enableNullableInFixedSolution },
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData(NullableContextOptions.Annotations)]
+        [InlineData(NullableContextOptions.Warnings)]
+        [InlineData(NullableContextOptions.Enable)]
+        public async Task DisabledIfSetInProject(NullableContextOptions nullableContextOptions)
+        {
+            var code = @"
+#nullable enable$$
+";
+
+            await new VerifyCS.Test
+            {
+                TestCode = code,
+                FixedCode = code,
+                SolutionTransforms =
+                {
+                    (solution, projectId) =>
+                    {
+                        var compilationOptions = (CSharpCompilationOptions)solution.GetRequiredProject(projectId).CompilationOptions!;
+                        return solution.WithProjectCompilationOptions(projectId, compilationOptions.WithNullableContextOptions(nullableContextOptions));
+                    },
+                },
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task DisabledOnNullableDisable()
+        {
+            var code = @"
+#nullable disable$$
+";
+
+            await new VerifyCS.Test
+            {
+                TestCode = code,
+                FixedCode = code,
+            }.RunAsync();
+        }
+    }
+}
---
+++ src/Features/CSharp/Portable/CodeRefactorings/EnableNullable/EnableNullableCodeRefactoringProvider.cs
@@ -0,0 +1,139 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+
+namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.EnableNullable
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = PredefinedCodeRefactoringProviderNames.EnableNullable)]
+    [Shared]
+    internal class EnableNullableCodeRefactoringProvider : CodeRefactoringProvider
+    {
+        private static readonly Func<DirectiveTriviaSyntax, bool> s_isNullableDirectiveTriviaPredicate =
+            static directive => directive.IsKind(SyntaxKind.NullableDirectiveTrivia);
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public EnableNullableCodeRefactoringProvider()
+        {
+        }
+
+        public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            var (document, textSpan, cancellationToken) = context;
+            if (!textSpan.IsEmpty)
+                return;
+
+            if (document.Project.CompilationOptions!.NullableContextOptions != NullableContextOptions.Disable)
+                return;
+
+            var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var token = root.FindToken(textSpan.Start, findInsideTrivia: true);
+            if (token.IsKind(SyntaxKind.EndOfDirectiveToken))
+                token = root.FindToken(textSpan.Start - 1, findInsideTrivia: true);
+
+            if (!token.IsKind(SyntaxKind.EnableKeyword))
+                return;
+
+            context.RegisterRefactoring(
+                new MyCodeAction(cancellationToken => EnableNullableReferenceTypesAsync(document.Project.Solution, document.Project.Id, cancellationToken)));
+        }
+
+        public static async Task<Solution> EnableNullableReferenceTypesAsync(Solution solution, ProjectId projectId, CancellationToken cancellationToken)
+        {
+            var project = solution.GetRequiredProject(projectId);
+            foreach (var document in project.Documents)
+            {
+                var updatedDocumentRoot = await EnableNullableReferenceTypesAsync(document, cancellationToken).ConfigureAwait(false);
+                solution = solution.WithDocumentSyntaxRoot(document.Id, updatedDocumentRoot);
+            }
+
+            var compilationOptions = (CSharpCompilationOptions)project.CompilationOptions!;
+            solution = solution.WithProjectCompilationOptions(projectId, compilationOptions.WithNullableContextOptions(NullableContextOptions.Enable));
+            return solution;
+        }
+
+        private static async Task<SyntaxNode> EnableNullableReferenceTypesAsync(Document document, CancellationToken cancellationToken)
+        {
+            var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var firstToken = root.GetFirstToken(includeDirectives: true);
+            if (firstToken.IsKind(SyntaxKind.None))
+            {
+                // The document has no content, so it's fine to change the nullable context
+                return root;
+            }
+
+            var firstNonDirectiveToken = root.GetFirstToken();
+            var firstDirective = root.GetFirstDirective(s_isNullableDirectiveTriviaPredicate);
+            if (firstNonDirectiveToken.IsKind(SyntaxKind.None) && firstDirective.IsKind(SyntaxKind.None))
+            {
+                // The document has no semantic content, and also has no nullable directives to update
+                return root;
+            }
+
+            // Update all prior nullable directives
+            var directives = new List<NullableDirectiveTriviaSyntax>();
+            for (var directive = firstDirective; directive.IsKind(SyntaxKind.NullableDirectiveTrivia); directive = directive.GetNextDirective(s_isNullableDirectiveTriviaPredicate))
+            {
+                directives.Add((NullableDirectiveTriviaSyntax)directive);
+            }
+
+            root = root.ReplaceNodes(
+                directives,
+                (originalNode, rewrittenNode) =>
+                {
+                    if (originalNode.SettingToken.IsKind(SyntaxKind.DisableKeyword))
+                    {
+                        // 'disable' keeps its meaning
+                        return rewrittenNode;
+                    }
+
+                    if (originalNode.SettingToken.IsKind(SyntaxKind.RestoreKeyword))
+                    {
+                        return rewrittenNode.WithSettingToken(SyntaxFactory.Token(SyntaxKind.DisableKeyword).WithTriviaFrom(rewrittenNode.SettingToken));
+                    }
+
+                    if (originalNode.SettingToken.IsKind(SyntaxKind.EnableKeyword))
+                    {
+                        return rewrittenNode.WithSettingToken(SyntaxFactory.Token(SyntaxKind.RestoreKeyword).WithTriviaFrom(rewrittenNode.SettingToken));
+                    }
+
+                    Debug.Fail("Unexpected state?");
+                    return rewrittenNode;
+                });
+
+            firstToken = root.GetFirstToken(includeDirectives: true);
+
+            // Add a new '#nullable disable' to the top of each file
+            var options = await document.GetOptionsAsync(cancellationToken).ConfigureAwait(false);
+            var newLine = SyntaxFactory.EndOfLine(options.GetOption(FormattingOptions2.NewLine));
+            var nullableDisableTrivia = SyntaxFactory.Trivia(SyntaxFactory.NullableDirectiveTrivia(SyntaxFactory.Token(SyntaxKind.DisableKeyword).WithPrependedLeadingTrivia(SyntaxFactory.ElasticSpace), isActive: true));
+            var updatedRoot = root.ReplaceToken(firstToken, firstToken.WithLeadingTrivia(firstToken.LeadingTrivia.Add(nullableDisableTrivia).Add(newLine).Add(newLine)));
+
+            // TODO: remove all '#nullable disable' directives in code with no nullable types
+
+            // TODO: remove all redundant directives
+
+            return updatedRoot;
+        }
+
+        private sealed class MyCodeAction : CodeActions.CodeAction.SolutionChangeAction
+        {
+            public MyCodeAction(Func<CancellationToken, Task<Solution>> createChangedSolution)
+                : base(CSharpFeaturesResources.Enable_nullable_reference_types, createChangedSolution, nameof(CSharpFeaturesResources.Enable_nullable_reference_types))
+            {
+            }
+        }
+    }
+}
