56400c6a9ce3a032c38e4a51a365a4bda18069a1
Manish Vasani <mavasani@microsoft.com>
2021-10-19 21:32:06 +0530

    Update src/Compilers/Core/Portable/DiagnosticAnalyzer/CompilationUnitCompletedEvent.cs

--- src/Compilers/Core/Portable/DiagnosticAnalyzer/CompilationUnitCompletedEvent.cs
+++ src/Compilers/Core/Portable/DiagnosticAnalyzer/CompilationUnitCompletedEvent.cs
@@ -21,6 +21,10 @@ namespace Microsoft.CodeAnalysis.Diagnostics
         /// Optional filter span for a synthesized CompilationUnitCompletedEvent generated for span-based semantic diagnostic computation.
         /// Such synthesized events are used primarily for performance improvements when running compiler analyzer in span-based mode in the IDE,
         /// such as computing diagnostics for the lightbulb for the current line.
+        /// Note that such a synthesized CompilationUnitCompletedEvent with non-null FilterSpan is not a true
+        /// compilation unit completed event, but just a stub event to drive span-based semantic model action callbacks
+        /// for analyzer execution. This event will eventually be followed by a true CompilationUnitCompletedEvent
+        /// with null FilterSpan when the entire compilation unit has actually completed.
         /// See https://github.com/dotnet/roslyn/issues/56843 for details.
         /// </summary>
         public TextSpan? FilterSpan { get; }
