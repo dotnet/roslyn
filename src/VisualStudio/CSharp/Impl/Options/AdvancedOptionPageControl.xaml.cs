eb790df633b7513e5b21421b285bfc276c704b67
Manish Vasani <mavasani@microsoft.com>
2021-10-20 09:00:01 -0700

    Fix formatting

--- src/VisualStudio/CSharp/Impl/Options/AdvancedOptionPageControl.xaml.cs
+++ src/VisualStudio/CSharp/Impl/Options/AdvancedOptionPageControl.xaml.cs
@@ -51,7 +51,7 @@ namespace Microsoft.VisualStudio.LanguageServices.CSharp.Options
             BindToOption<BackgroundAnalysisScope?>(Background_analysis_scope_active_file, SolutionCrawlerOptions.BackgroundAnalysisScopeOption, BackgroundAnalysisScope.ActiveFile, LanguageNames.CSharp, onNullBackgroundAnalysisScope);
             BindToOption<BackgroundAnalysisScope?>(Background_analysis_scope_open_files, SolutionCrawlerOptions.BackgroundAnalysisScopeOption, BackgroundAnalysisScope.OpenFilesAndProjects, LanguageNames.CSharp, onNullBackgroundAnalysisScope);
             BindToOption<BackgroundAnalysisScope?>(Background_analysis_scope_full_solution, SolutionCrawlerOptions.BackgroundAnalysisScopeOption, BackgroundAnalysisScope.FullSolution, LanguageNames.CSharp, onNullBackgroundAnalysisScope);
-            
+
             BindToOption(DisplayDiagnosticsInline, InlineDiagnosticsOptions.EnableInlineDiagnostics, LanguageNames.CSharp);
             BindToOption(at_the_end_of_the_line_of_code, InlineDiagnosticsOptions.Location, InlineDiagnosticsLocations.PlacedAtEndOfCode, LanguageNames.CSharp);
             BindToOption(on_the_right_edge_of_the_editor_window, InlineDiagnosticsOptions.Location, InlineDiagnosticsLocations.PlacedAtEndOfEditor, LanguageNames.CSharp);
