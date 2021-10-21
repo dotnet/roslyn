265355c4d8930c0cd593fe34a9448378536e00f3
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-10-20 21:53:41 -0700

    Keep around references

--- src/Workspaces/Core/Portable/Utilities/Documentation/DeferredDocumentationProvider.cs
+++ src/Workspaces/Core/Portable/Utilities/Documentation/DeferredDocumentationProvider.cs
@@ -11,15 +11,15 @@ namespace Microsoft.CodeAnalysis
     /// The documentation provider used to lookup xml docs for any metadata reference we pass out.  They'll
     /// all get the same xml doc comment provider (as different references to the same compilation don't
     /// see the xml docs any differently).  This provider does root a Compilation around.  However, this should
-    /// not be an issue in practice as the compilation we are rooting is a declaration-only-compilation, and
-    /// not the full compilation for a project.  This declaration-only-compilation has no references, and can
-    /// dump syntax trees whenever necessary.  What is does store is the compact decl-table which is safe and
-    /// cheap to hold onto long term.  When some downstream consumer of this metadata-reference then needs to
-    /// get xml-doc comments, it will resolve a doc-comment-id against this decl-only-compilation.  Resolution
-    /// is very cheap, only causing the necessary symbols referenced directly in the ID to be created.  As 
-    /// downstream consumers are only likely to resolve a small handful of these symbols in practice, this 
-    /// should not be expensive to hold onto.  Importantly, semantic models and complex method binding/caching
-    /// should never really happen with this compilation.
+    /// not be an issue in practice as the compilation we are rooting is a clone of the acutal compilation of
+    /// project, and not the compilation itself.  This clone doesn't share any symbols/semantics with the main
+    /// compilation, and it can dump syntax trees whenever necessary.  What is does store is the compact
+    /// decl-table which is safe and cheap to hold onto long term.  When some downstream consumer of this
+    /// metadata-reference then needs to get xml-doc comments, it will resolve a doc-comment-id against this
+    /// decl-only-compilation.  Resolution is very cheap, only causing the necessary symbols referenced directly
+    /// in the ID to be created.  As downstream consumers are only likely to resolve a small handful of these 
+    /// symbols in practice, this should not be expensive to hold onto.  Importantly, semantic models and 
+    /// complex method binding/caching should never really happen with this compilation.
     /// </summary>
     internal class DeferredDocumentationProvider : DocumentationProvider
     {
@@ -27,9 +27,9 @@ namespace Microsoft.CodeAnalysis
 
         public DeferredDocumentationProvider(Compilation compilation)
         {
-            // Ensure we are getting a clone of this compilation without any references so that we are only
-            // holding onto the decl table and not rooting a full compilation that might be quite expensive.
-            _compilation = compilation.WithReferences().Clone();
+            // Ensure we are getting a clone of this compilation so that we are only holding onto the decl table and
+            // not rooting a full compilation that might be quite expensive.
+            _compilation = compilation.Clone();
         }
 
         protected override string? GetDocumentationForSymbol(string documentationMemberID, CultureInfo preferredCulture, CancellationToken cancellationToken = default)
