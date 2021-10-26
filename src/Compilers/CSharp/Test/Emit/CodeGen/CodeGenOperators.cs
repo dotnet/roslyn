ff7e8aa12504a8097ff07e244767436080c0fbbf
Rikki Gibson <rigibson@microsoft.com>
2021-10-25 17:16:50 -0700

    Move new tests to Emit2

--- src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenOperators.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenOperators.cs
@@ -1,7 +1,4 @@
-﻿#if false
-// TODO: re-enable these tests once Emit2 is integrated
-
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -6664,4 +6661,3 @@ namespace BadImageFormatExceptionRepro
         }
     }
 }
-#endif
