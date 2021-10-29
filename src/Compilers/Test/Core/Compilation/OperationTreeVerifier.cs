4fad1bb4e20adadd52afbb2bdc41f9e87450de20
bernd <baumannsbernd@gmail.com>
2021-10-29 20:47:10 +0200

    Pointer -> Target

--- src/Compilers/Test/Core/Compilation/OperationTreeVerifier.cs
+++ src/Compilers/Test/Core/Compilation/OperationTreeVerifier.cs
@@ -829,7 +829,7 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
             LogString(nameof(IFunctionPointerInvocationOperation));
             LogCommonPropertiesAndNewLine(operation);
 
-            Visit(operation.Target, "Pointer");
+            Visit(operation.Target, "Target");
             VisitArguments(operation.Arguments);
         }
 
