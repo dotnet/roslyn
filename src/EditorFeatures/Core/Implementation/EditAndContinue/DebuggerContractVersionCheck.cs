d6c9065f7dc066781b40ec7338051a4f3584e67e
tmat <tomas.matousek@microsoft.com>
2021-10-29 15:27:31 -0700

    FIx

--- src/EditorFeatures/Core/Implementation/EditAndContinue/DebuggerContractVersionCheck.cs
+++ src/EditorFeatures/Core/Implementation/EditAndContinue/DebuggerContractVersionCheck.cs
@@ -4,7 +4,7 @@
 
 using System;
 using System.Runtime.CompilerServices;
-using Microsoft.VisualStudio.Debugger.Contracts;
+using Microsoft.VisualStudio.Debugger.Contracts.HotReload;
 
 namespace Microsoft.CodeAnalysis.EditAndContinue
 {
