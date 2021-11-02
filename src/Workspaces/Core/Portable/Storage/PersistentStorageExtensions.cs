f64ea364400671bf94d93ed9ec5c1e99216a70c8
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-11-02 11:38:21 -0700

    Disable sql when built from source

--- src/Workspaces/Core/Portable/Storage/PersistentStorageExtensions.cs
+++ src/Workspaces/Core/Portable/Storage/PersistentStorageExtensions.cs
@@ -4,8 +4,11 @@
 
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Options;
-using Microsoft.CodeAnalysis.SQLite.v2;
 using Microsoft.CodeAnalysis.Storage.CloudCache;
+
+#if !DOTNET_BUILD_FROM_SOURCE
+using Microsoft.CodeAnalysis.SQLite.v2;
+#endif
 
 namespace Microsoft.CodeAnalysis.Storage
 {
@@ -23,9 +26,11 @@ namespace Microsoft.CodeAnalysis.Storage
 
             return database switch
             {
+#if !DOTNET_BUILD_FROM_SOURCE
                 StorageDatabase.SQLite
                     => services.GetService<SQLitePersistentStorageService>() ??
                        NoOpPersistentStorageService.GetOrThrow(configuration),
+#endif
                 StorageDatabase.CloudCache
                     => services.GetService<ICloudCacheStorageService>() ??
                        NoOpPersistentStorageService.GetOrThrow(configuration),
