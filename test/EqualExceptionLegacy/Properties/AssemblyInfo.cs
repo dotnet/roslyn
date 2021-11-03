75d81101123c4b20290a131f12f9320427db491f
Sam Harwell <Sam.Harwell@microsoft.com>
2021-11-03 14:01:56 -0700

    Avoid restarting Visual Studio between each test class

--- test/EqualExceptionLegacy/Properties/AssemblyInfo.cs
+++ test/EqualExceptionLegacy/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -13,3 +14,5 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
