ba5e2969dd6bea1a4afe82c875f4d1919d9f1647
Sam Harwell <Sam.Harwell@microsoft.com>
2021-11-08 09:29:43 -0800

    Remove unnecessary nullable disable directives

--- src/Microsoft.VisualStudio.Extensibility.Testing.Xunit.Shared/Harness/VSConstants.cs
+++ src/Microsoft.VisualStudio.Extensibility.Testing.Xunit.Shared/Harness/VSConstants.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for more information.
 
-#nullable disable
-
 #pragma warning disable SA1310 // Field names should not contain underscore
 
 namespace Xunit.Harness
