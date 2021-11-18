66f2a1b113f8d21b4380449db875dbcb3d89ab04
David Barbet <dabarbet@microsoft.com>
2021-11-17 17:41:02 -0800

    Update LanguageServerTargetTests.cs

--- src/Features/LanguageServer/ProtocolUnitTests/LanguageServerTargetTests.cs
+++ src/Features/LanguageServer/ProtocolUnitTests/LanguageServerTargetTests.cs
@@ -66,7 +66,7 @@ namespace Microsoft.CodeAnalysis.LanguageServer.UnitTests
 
         private static async Task AssertServerQueueClosed(LanguageServerTarget server)
         {
-            await server.GetTestAccessor().GetQueueAccessor().WaitForProcessingToStop().ConfigureAwait(false);
+            await server.GetTestAccessor().GetQueueAccessor().WaitForProcessingToStopAsync().ConfigureAwait(false);
             Assert.True(server.HasShutdownStarted);
             Assert.True(server.GetTestAccessor().GetQueueAccessor().IsComplete());
         }
