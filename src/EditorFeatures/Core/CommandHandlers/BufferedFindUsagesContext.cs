703bb7e21fa8ed127ad64d1cbae0d742e3135387
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-11-20 12:15:17 -0800

    Use simple using statement

--- src/EditorFeatures/Core/CommandHandlers/BufferedFindUsagesContext.cs
+++ src/EditorFeatures/Core/CommandHandlers/BufferedFindUsagesContext.cs
@@ -47,74 +47,64 @@ internal sealed class BufferedFindUsagesContext : IFindUsagesContext, IStreaming
 
     public async Task<string?> GetMessageAsync(CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
-        {
-            Contract.ThrowIfTrue(_streamingPresenterContext != null, "Should not be called if we've switched over to the streaming presenter");
-            return _message;
-        }
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        Contract.ThrowIfTrue(_streamingPresenterContext != null, "Should not be called if we've switched over to the streaming presenter");
+        return _message;
     }
 
     public async Task<string?> GetInformationalMessageAsync(CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
-        {
-            Contract.ThrowIfTrue(_streamingPresenterContext != null, "Should not be called if we've switched over to the streaming presenter");
-            return _informationalMessage;
-        }
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        Contract.ThrowIfTrue(_streamingPresenterContext != null, "Should not be called if we've switched over to the streaming presenter");
+        return _informationalMessage;
     }
 
     public async Task<string?> GetSearchTitleAsync(CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
-        {
-            Contract.ThrowIfTrue(_streamingPresenterContext != null, "Should not be called if we've switched over to the streaming presenter");
-            return _searchTitle;
-        }
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        Contract.ThrowIfTrue(_streamingPresenterContext != null, "Should not be called if we've switched over to the streaming presenter");
+        return _searchTitle;
     }
 
     public async Task<ImmutableArray<DefinitionItem>> GetDefinitionsAsync(CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
-        {
-            Contract.ThrowIfNull(_definitions, "This should not be called if we switched over to the presenter to show results");
-            return _definitions.ToImmutable();
-        }
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        Contract.ThrowIfNull(_definitions, "This should not be called if we switched over to the presenter to show results");
+        return _definitions.ToImmutable();
     }
 
     public async Task AttachToStreamingPresenterAsync(IFindUsagesContext presenterContext, CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
-        {
-            Contract.ThrowIfTrue(_streamingPresenterContext != null, "Trying to set the presenter multiple times.");
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        Contract.ThrowIfTrue(_streamingPresenterContext != null, "Trying to set the presenter multiple times.");
 
-            // Push all values we've buffered into the new presenter context.
+        // Push all values we've buffered into the new presenter context.
 
-            await presenterContext.ProgressTracker.AddItemsAsync(_totalItemCount, cancellationToken).ConfigureAwait(false);
-            await presenterContext.ProgressTracker.ItemsCompletedAsync(_itemsCompleted, cancellationToken).ConfigureAwait(false);
+        await presenterContext.ProgressTracker.AddItemsAsync(_totalItemCount, cancellationToken).ConfigureAwait(false);
+        await presenterContext.ProgressTracker.ItemsCompletedAsync(_itemsCompleted, cancellationToken).ConfigureAwait(false);
 
-            if (_searchTitle != null)
-                await presenterContext.SetSearchTitleAsync(_searchTitle, cancellationToken).ConfigureAwait(false);
+        if (_searchTitle != null)
+            await presenterContext.SetSearchTitleAsync(_searchTitle, cancellationToken).ConfigureAwait(false);
 
-            if (_message != null)
-                await presenterContext.ReportMessageAsync(_message, cancellationToken).ConfigureAwait(false);
+        if (_message != null)
+            await presenterContext.ReportMessageAsync(_message, cancellationToken).ConfigureAwait(false);
 
-            if (_informationalMessage != null)
-                await presenterContext.ReportInformationalMessageAsync(_informationalMessage, cancellationToken).ConfigureAwait(false);
+        if (_informationalMessage != null)
+            await presenterContext.ReportInformationalMessageAsync(_informationalMessage, cancellationToken).ConfigureAwait(false);
 
-            Contract.ThrowIfNull(_definitions);
-            foreach (var definition in _definitions)
-                await presenterContext.OnDefinitionFoundAsync(definition, cancellationToken).ConfigureAwait(false);
+        Contract.ThrowIfNull(_definitions);
+        foreach (var definition in _definitions)
+            await presenterContext.OnDefinitionFoundAsync(definition, cancellationToken).ConfigureAwait(false);
 
-            // Now swap over to the presenter being the sink for all future callbacks, and clear any buffered data.
-            _streamingPresenterContext = presenterContext;
+        // Now swap over to the presenter being the sink for all future callbacks, and clear any buffered data.
+        _streamingPresenterContext = presenterContext;
 
-            _totalItemCount = -1;
-            _itemsCompleted = -1;
-            _searchTitle = null;
-            _message = null;
-            _informationalMessage = null;
-            _definitions = null;
-        }
+        _totalItemCount = -1;
+        _itemsCompleted = -1;
+        _searchTitle = null;
+        _message = null;
+        _informationalMessage = null;
+        _definitions = null;
     }
 
     #region IStreamingProgressTracker
@@ -123,31 +113,27 @@ internal sealed class BufferedFindUsagesContext : IFindUsagesContext, IStreaming
 
     async ValueTask IStreamingProgressTracker.AddItemsAsync(int count, CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        if (_streamingPresenterContext != null)
         {
-            if (_streamingPresenterContext != null)
-            {
-                await _streamingPresenterContext.ProgressTracker.AddItemsAsync(count, cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                _totalItemCount += count;
-            }
+            await _streamingPresenterContext.ProgressTracker.AddItemsAsync(count, cancellationToken).ConfigureAwait(false);
+        }
+        else
+        {
+            _totalItemCount += count;
         }
     }
 
     async ValueTask IStreamingProgressTracker.ItemsCompletedAsync(int count, CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        if (_streamingPresenterContext != null)
         {
-            if (_streamingPresenterContext != null)
-            {
-                await _streamingPresenterContext.ProgressTracker.ItemsCompletedAsync(count, cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                _itemsCompleted += count;
-            }
+            await _streamingPresenterContext.ProgressTracker.ItemsCompletedAsync(count, cancellationToken).ConfigureAwait(false);
+        }
+        else
+        {
+            _itemsCompleted += count;
         }
     }
 
@@ -157,68 +143,61 @@ internal sealed class BufferedFindUsagesContext : IFindUsagesContext, IStreaming
 
     async ValueTask IFindUsagesContext.ReportMessageAsync(string message, CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        if (_streamingPresenterContext != null)
         {
-            if (_streamingPresenterContext != null)
-            {
-                await _streamingPresenterContext.ReportMessageAsync(message, cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                _message = message;
-            }
+            await _streamingPresenterContext.ReportMessageAsync(message, cancellationToken).ConfigureAwait(false);
+        }
+        else
+        {
+            _message = message;
         }
     }
 
     async ValueTask IFindUsagesContext.ReportInformationalMessageAsync(string message, CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        if (_streamingPresenterContext != null)
         {
-            if (_streamingPresenterContext != null)
-            {
-                await _streamingPresenterContext.ReportInformationalMessageAsync(message, cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                _informationalMessage = message;
-            }
+            await _streamingPresenterContext.ReportInformationalMessageAsync(message, cancellationToken).ConfigureAwait(false);
+        }
+        else
+        {
+            _informationalMessage = message;
         }
     }
 
     async ValueTask IFindUsagesContext.SetSearchTitleAsync(string title, CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        if (_streamingPresenterContext != null)
         {
-            if (_streamingPresenterContext != null)
-            {
-                await _streamingPresenterContext.SetSearchTitleAsync(title, cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                _searchTitle = title;
-            }
+            await _streamingPresenterContext.SetSearchTitleAsync(title, cancellationToken).ConfigureAwait(false);
+        }
+        else
+        {
+            _searchTitle = title;
         }
     }
 
     async ValueTask IFindUsagesContext.OnDefinitionFoundAsync(DefinitionItem definition, CancellationToken cancellationToken)
     {
-        using (await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false))
+        using var _ = await _gate.DisposableWaitAsync(cancellationToken).ConfigureAwait(false);
+        if (_streamingPresenterContext != null)
         {
-            if (_streamingPresenterContext != null)
-            {
-                await _streamingPresenterContext.OnDefinitionFoundAsync(definition, cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                Contract.ThrowIfNull(_definitions);
-                _definitions.Add(definition);
-            }
+            await _streamingPresenterContext.OnDefinitionFoundAsync(definition, cancellationToken).ConfigureAwait(false);
+        }
+        else
+        {
+            Contract.ThrowIfNull(_definitions);
+            _definitions.Add(definition);
         }
     }
 
     ValueTask IFindUsagesContext.OnReferenceFoundAsync(SourceReferenceItem reference, CancellationToken cancellationToken)
     {
         // Entirely ignored.  These features do not show references.
+        Contract.Fail("GoToImpl/Base should never report a reference.");
         return ValueTaskFactory.CompletedTask;
     }
 
