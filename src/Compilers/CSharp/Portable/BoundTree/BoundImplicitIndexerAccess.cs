439867229d46b471e4f29076b79429f915353b30
Julien Couvreur <julien.couvreur@gmail.com>
2021-11-23 13:26:40 -0800

    Add BoundBadExpression case for safety

--- src/Compilers/CSharp/Portable/BoundTree/BoundImplicitIndexerAccess.cs
+++ src/Compilers/CSharp/Portable/BoundTree/BoundImplicitIndexerAccess.cs
@@ -33,7 +33,7 @@ namespace Microsoft.CodeAnalysis.CSharp
         private partial void Validate()
         {
             Debug.Assert(LengthOrCountAccess is BoundPropertyAccess or BoundLocal or BoundBadExpression);
-            Debug.Assert(IndexerOrSliceAccess is BoundIndexerAccess or BoundCall or BoundArrayAccess);
+            Debug.Assert(IndexerOrSliceAccess is BoundIndexerAccess or BoundCall or BoundArrayAccess or BoundBadExpression);
         }
     }
 }
