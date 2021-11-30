70e4b4401216cc3b991dde546b861ccc90536e52
David Wengier <david.wengier@microsoft.com>
2021-12-01 09:35:29 +1100

    Copy SourceLinkMap directly into Roslyn

--- src/Features/Core/Portable/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("CodeStyle", "IDE0055", Justification = "Code is from a source package", Scope = "type", Target = "Microsoft.SourceLink.Tools.SourceLinkMap")]
-[assembly: SuppressMessage("CodeStyle", "IDE0073", Justification = "Code is from a source package", Scope = "type", Target = "Microsoft.SourceLink.Tools.SourceLinkMap")]
