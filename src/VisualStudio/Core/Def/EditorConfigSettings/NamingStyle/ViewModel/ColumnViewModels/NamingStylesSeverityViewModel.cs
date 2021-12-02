a773abede493dae21d147b9d33ecf820e55984a9
Jonathon Marolf <jmarolf@microsoft.com>
2021-12-02 06:29:57 -0800

    Add views for naming styles
    This adds a new tab for the naming style options that were exposed via services in a previous commit

---
+++ src/VisualStudio/Core/Def/EditorConfigSettings/NamingStyle/ViewModel/ColumnViewModels/NamingStylesSeverityViewModel.cs
@@ -0,0 +1,62 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editor.EditorConfigSettings.Data;
+
+namespace Microsoft.VisualStudio.LanguageServices.EditorConfigSettings.NamingStyle.ViewModel
+{
+    internal class NamingStylesSeverityViewModel
+    {
+        private static readonly string[] s_severities = new[]
+        {
+            ServicesVSResources.Disabled,
+            ServicesVSResources.Suggestion,
+            ServicesVSResources.Warning,
+            ServicesVSResources.Error
+        };
+        private readonly NamingStyleSetting _setting;
+
+        public NamingStylesSeverityViewModel(NamingStyleSetting setting)
+        {
+            _setting = setting;
+            var selectedSeverityIndex = _setting.Severity switch
+            {
+                ReportDiagnostic.Hidden => 0,
+                ReportDiagnostic.Info => 1,
+                ReportDiagnostic.Warn => 2,
+                ReportDiagnostic.Error => 3,
+                _ => throw new InvalidOperationException(),
+            };
+
+            SelectedSeverityValue = Severities[selectedSeverityIndex];
+        }
+
+        internal void SelectionChanged(int selectedIndex)
+        {
+            var severity = selectedIndex switch
+            {
+                0 => ReportDiagnostic.Hidden,
+                1 => ReportDiagnostic.Info,
+                2 => ReportDiagnostic.Warn,
+                3 => ReportDiagnostic.Error,
+                _ => throw new InvalidOperationException(),
+            };
+            _setting.ChangeSeverity(severity);
+        }
+
+        public static string SeverityToolTip => ServicesVSResources.Severity;
+
+        public static string SeverityAutomationName => ServicesVSResources.Severity;
+
+        public string SelectedSeverityValue { get; set; }
+
+        public static string[] Severities { get; } = s_severities;
+    }
+}
