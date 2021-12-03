9ea47d01618616f74c4317601db145f052bc6869
AlekseyTs <AlekseyTs@users.noreply.github.com>
2021-12-03 11:47:56 -0800

    Revert unintentional formatting changes

--- src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
+++ src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
@@ -7784,7 +7784,7 @@ public class C
         [Fact, WorkItem(530359, "http://vstfdevdiv:8080/DevDiv2/DevDiv/_workitems/edit/530359")]
         public void NoStdLib02()
         {
-#region "source"
+            #region "source"
             var source = @"
 // <Title>A collection initializer can be declared with a user-defined IEnumerable that is declared in a user-defined System.Collections</Title>
 using System.Collections;
@@ -7836,9 +7836,9 @@ namespace System.Collections
     }
 }
 ";
-#endregion
+            #endregion
 
-#region "mslib"
+            #region "mslib"
             var mslib = @"
 namespace System
 {
@@ -7903,7 +7903,7 @@ namespace System
     }
 }
 ";
-#endregion
+            #endregion
 
             var src = Temp.CreateFile("NoStdLib02.cs");
             src.WriteAllText(source + mslib);
