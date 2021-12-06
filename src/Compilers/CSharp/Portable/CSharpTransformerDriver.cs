06918c08ae10b6c7f63e53d195a27e82b5effce1
Antonín Procházka <antonin@postsharp.net>
2021-12-06 17:02:36 +0100

    #29528 Licensing configured for Caravela.Try.

--- src/Compilers/CSharp/Portable/CSharpTransformerDriver.cs
+++ src/Compilers/CSharp/Portable/CSharpTransformerDriver.cs
@@ -4,12 +4,15 @@
 
 // <Caravela /> This code is used by Try.Caravela.
 
+using System;
 using System.Collections.Immutable;
 using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 using PostSharp.Backstage.Extensibility;
+using PostSharp.Backstage.Licensing;
+using PostSharp.Backstage.Licensing.Consumption;
 
 namespace Caravela.Compiler
 {
@@ -21,14 +24,36 @@ namespace Caravela.Compiler
         {
             var services = new ServiceCollection();
 
-            // TODO: Configure for Try.Caravela
             var serviceProviderBuilder = new ServiceProviderBuilder(
                 (type, instance) => services.AddService(type, instance),
                 () => services.GetServiceProvider());
 
+            serviceProviderBuilder.AddSingleton<IBackstageDiagnosticSink>(new CaravelaTryBackstageDiagnosticsSink());
+            serviceProviderBuilder.AddSingleton<ILicenseConsumptionManager>(new CaravelaTryLicenseConsumptionManager());
+
             var diagnostics = DiagnosticBag.GetInstance();
             var results = CSharpCompiler.RunTransformers(input, transformers, null, plugins, analyzerConfigProvider, diagnostics, manifestResources, assemblyLoader, serviceProviderBuilder.ServiceProvider, CancellationToken.None);
             return (results.TransformedCompilation, diagnostics.ToReadOnlyAndFree());
+        }
+
+        private class CaravelaTryLicenseConsumptionManager : ILicenseConsumptionManager
+        {
+            public bool CanConsumeFeatures(ILicenseConsumer consumer, LicensedFeatures requiredFeatures) => true;
+
+            public void ConsumeFeatures(ILicenseConsumer consumer, LicensedFeatures requiredFeatures) { }
+        }
+
+        private class CaravelaTryBackstageDiagnosticsSink : IBackstageDiagnosticSink
+        {
+            public void ReportError(string message, IDiagnosticsLocation? location = null)
+            {
+                throw new InvalidOperationException(message);
+            }
+
+            public void ReportWarning(string message, IDiagnosticsLocation? location = null)
+            {
+                throw new InvalidOperationException(message);
+            }
         }
     }
 }
