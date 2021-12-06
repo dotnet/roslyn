264eefb988528fc048b1ad68c7d6c4edb3074e7d
Jonathon Marolf <jmarolf@microsoft.com>
2021-12-06 11:12:45 -0800

    fix Span and Section being swapped from the base

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/EditorConfig/Parsing/EditorConfigOption`1.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/EditorConfig/Parsing/EditorConfigOption`1.cs
@@ -9,13 +9,13 @@ namespace Microsoft.CodeAnalysis.EditorConfig.Parsing
     /// <summary>
     /// An abstraction over an editorconfig option that reprsents some type <typeparamref name="T"/> and the span in which that option was defined.
     /// </summary>
-    internal record class EditorConfigOption<T>(T Value, TextSpan? Span, Section Section)
+    internal record class EditorConfigOption<T>(Section Section, TextSpan? Span, T Value)
         : EditorConfigOption(Section, Span)
     {
         public static implicit operator T(EditorConfigOption<T> option) => option.Value;
         public static implicit operator EditorConfigOption<T>((Section section, TextSpan? span, T value) tuple)
-            => new(tuple.value, tuple.span, tuple.section);
+            => new(tuple.section, tuple.span, tuple.value);
         public static implicit operator EditorConfigOption<T>((Section section, T value) tuple)
-            => new(tuple.value, null, tuple.section);
+            => new(tuple.section, null, tuple.value);
     }
 }
