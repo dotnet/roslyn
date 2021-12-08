cb26bda22bfe5c56796f7d1c4930c6df2f422663
Gael Fraiteur <gael@postsharp.net>
2021-12-08 11:11:54 +0100

    Updating engineering.

--- eng-Caravela/src/Program.cs
+++ eng-Caravela/src/Program.cs
@@ -4,7 +4,6 @@ using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using PostSharp.Engineering.BuildTools.NuGet;
 using Spectre.Console.Cli;
-using System;
 using System.Collections.Immutable;
 using System.IO;
 
@@ -12,29 +11,21 @@ namespace Build
 {
     internal class Program
     {
-        private static int Main(string[] args)
+        private static int Main( string[] args )
         {
-            var privateSource = new NugetSource("%INTERNAL_NUGET_PUSH_URL%", "%INTERNAL_NUGET_API_KEY%");
-            var publicSource = new NugetSource("https://api.nuget.org/v3/index.json", "%NUGET_ORG_API_KEY%");
 
-
-            // These packages are published to internal and private feeds.
-            var publicPackages = new ParametricString[]
+            var product = new Product
             {
-                "Caravela.Compiler.$(PackageVersion).nupkg",
-                "Caravela.Compiler.Sdk.$(PackageVersion).nupkg"
-            };
-
-            var publicPublishing = new NugetPublishTarget(
-                Pattern.Empty.Add(publicPackages),
-                privateSource,
-                publicSource);
-
-
-            // These packages are published to private feeds only.
-            var privatePackages = new ParametricString[]
-            {
-                "Caravela.RoslynUtilities.$(PackageVersion).nupkg",
+                PrivateArtifactsDirectory = "artifacts\\packages\\$(Configuration)\\Shipping",
+                ProductName = "Caravela.Compiler",
+                EngineeringDirectory = "eng-Caravela",
+                VersionsFile = "eng\\Versions.props",
+                GenerateArcadeProperties = true,
+                AdditionalDirectoriesToClean = ImmutableArray.Create( "artifacts" ),
+                Solutions = ImmutableArray.Create<Solution>( new RoslynSolution() ),
+                PublicArtifacts = Pattern.Create( "Caravela.Compiler.$(PackageVersion).nupkg", "Caravela.Compiler.Sdk.$(PackageVersion).nupkg" ),
+                PrivateArtifacts = Pattern.Create(
+                     "Caravela.RoslynUtilities.$(PackageVersion).nupkg",
                 "Caravela.Roslyn.CodeAnalysis.Common.$(PackageVersion).nupkg",
                 "Caravela.Roslyn.CodeAnalysis.CSharp.$(PackageVersion).nupkg",
                 "Caravela.Roslyn.CodeAnalysis.CSharp.Features.$(PackageVersion).nupkg",
@@ -48,52 +39,34 @@ namespace Build
                 // Visual Basic is needed by Caravela.Try
                 "Caravela.Roslyn.CodeAnalysis.VisualBasic.$(PackageVersion).nupkg",
                 "Caravela.Roslyn.CodeAnalysis.VisualBasic.Features.$(PackageVersion).nupkg",
-                "Caravela.Roslyn.CodeAnalysis.VisualBasic.Workspaces.$(PackageVersion).nupkg",
-            };
-
-
-            var privatePublishing = new NugetPublishTarget(
-                Pattern.Empty.Add(publicPackages).Add(privatePackages),
-                privateSource);
-
-            var product = new Product
-            {
-                PrivateArtifactsDirectory = "artifacts\\packages\\$(Configuration)\\Shipping",
-                ProductName = "Caravela.Compiler",
-                EngineeringDirectory = "eng-Caravela",
-                VersionsFile = "eng\\Versions.props",
-                GenerateArcadeProperties = true,
-                AdditionalDirectoriesToClean = ImmutableArray.Create("artifacts"),
-                Solutions = ImmutableArray.Create<Solution>(
-                    new RoslynSolution()),
-                PublishingTargets = ImmutableArray.Create<PublishingTarget>(publicPublishing, privatePublishing),
-                Dependencies = ImmutableArray.Create(Dependencies.PostSharpEngineering, Dependencies.PostSharpBackstageSettings),
-                SupportedProperties = ImmutableDictionary.Create<string, string>().Add("TestAll", "Supported by the 'test' command. Run all tests instead of just Caravela's unit tests."),
+                "Caravela.Roslyn.CodeAnalysis.VisualBasic.Workspaces.$(PackageVersion).nupkg" ),
+                Dependencies = ImmutableArray.Create( Dependencies.PostSharpEngineering, Dependencies.PostSharpBackstageSettings ),
+                SupportedProperties = ImmutableDictionary.Create<string, string>().Add( "TestAll", "Supported by the 'test' command. Run all tests instead of just Caravela's unit tests." ),
                 KeepEditorConfig = true,
             };
             product.BuildCompleted += OnBuildCompleted;
             var commandApp = new CommandApp();
-            commandApp.AddProductCommands(product);
+            commandApp.AddProductCommands( product );
 
-            return commandApp.Run(args);
+            return commandApp.Run( args );
         }
 
-        private static bool OnBuildCompleted((BuildContext Context, BuildOptions Options, string Directory) args)
+        private static bool OnBuildCompleted( (BuildContext Context, BuildOptions Options, string Directory) args )
         {
             // Rename the packages as a post-build step.
-            args.Context.Console.WriteHeading("Renaming packages");
+            args.Context.Console.WriteHeading( "Renaming packages" );
 
-            var success = RenamePackagesCommand.Execute(args.Context.Console, new RenamePackageSettings { Directory = args.Directory });
+            var success = RenamePackagesCommand.Execute( args.Context.Console, new RenamePackageSettings { Directory = args.Directory } );
 
-            if (success)
+            if ( success )
             {
                 // Delete original packages (those non-renamed) so they don't get uploaded.
-                foreach (var file in Directory.GetFiles(args.Directory, "Microsoft.*.nupkg"))
+                foreach ( var file in Directory.GetFiles( args.Directory, "Microsoft.*.nupkg" ) )
                 {
-                    File.Delete(file);
+                    File.Delete( file );
                 }
 
-                args.Context.Console.WriteSuccess("Renaming packages was successful.");
+                args.Context.Console.WriteSuccess( "Renaming packages was successful." );
             }
 
             return success;
