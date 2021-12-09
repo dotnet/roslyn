f407e3757919b98f1c3ef79e5af675f7c91311c0
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-12-08 18:24:28 -0800

    Add back compat tests

---
+++ src/Compilers/CSharp/Test/Syntax/Parsing/InterpolationTests.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Parsing
+{
+    public class InterpolationTests
+    {
+        [Fact]
+        public void APIBackCompatTest1()
+        {
+            Assert.Equal("{a}", SyntaxFactory.Interpolation(SyntaxFactory.IdentifierName("a")).ToFullString());
+        }
+
+        [Fact]
+        public void APIBackCompatTest2()
+        {
+            Assert.Equal("{a,b:c}", SyntaxFactory.Interpolation(
+                SyntaxFactory.IdentifierName("a"),
+                SyntaxFactory.InterpolationAlignmentClause(
+                    SyntaxFactory.Token(SyntaxKind.CommaToken),
+                    SyntaxFactory.IdentifierName("b")),
+                SyntaxFactory.InterpolationFormatClause(
+                    SyntaxFactory.Token(SyntaxKind.ColonToken),
+                    SyntaxFactory.Token(default, SyntaxKind.InterpolatedStringTextToken, "c", "c", default))).ToFullString());
+        }
+    }
+}
