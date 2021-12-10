6bda3a917506e2a51649653b099430e768ebb47b
akhera99 <ankitakhera@microsoft.com>
2021-12-09 16:32:04 -0800

    starting imp

---
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/Snippets/AbstractSnippetCompletionProvider.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Completion;
+
+namespace Microsoft.CodeAnalysis.CSharp.Completion.CompletionProviders.Snippets
+{
+    internal abstract class AbstractSnippetCompletionProvider : CommonCompletionProvider
+    {
+        public AbstractSnippetCompletionProvider()
+        {
+
+        }
+
+        public override async Task<CompletionChange> GetChangeAsync(Document document, CompletionItem item, char? commitKey = null, CancellationToken cancellationToken = default)
+        {
+
+        }
+
+        private async Task<Document> DetermineNewDocumentAsync(Document document, CompletionItem completionItem, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
