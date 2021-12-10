923173f7b9a46e0574eb5bf347762cb7741bda2d
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-12-09 22:09:34 -0800

    Fix feature with cahnge to parse tree shape

--- src/Features/CSharp/Portable/BraceCompletion/InterpolatedStringBraceCompletionService.cs
+++ src/Features/CSharp/Portable/BraceCompletion/InterpolatedStringBraceCompletionService.cs
@@ -59,32 +59,28 @@ namespace Microsoft.CodeAnalysis.CSharp.BraceCompletion
 
             var start = position - 1;
             if (start < 0)
-            {
                 return false;
-            }
 
             // Check if the user is typing an interpolated or interpolated verbatim string.
             // If the preceding character(s) are not '$' or '$@' then we can't be starting an interpolated string.
             if (text[start] == '@')
             {
-                start--;
-
-                if (start < 0)
-                {
+                if (--start < 0)
                     return false;
-                }
             }
 
             if (text[start] != '$')
-            {
                 return false;
-            }
 
             // Verify that we are actually in an location allowed for an interpolated string.
-            // Note that the quote has not yet been typed so we don't actually know if we're in an interpolated string.
             var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-            var token = root.FindTokenOnLeftOfPosition(start);
-            return root.SyntaxTree.IsExpressionContext(start, token, attributes: false, cancellationToken: cancellationToken)
+            var token = root.FindToken(start);
+            if (token.Kind() is not SyntaxKind.InterpolatedStringStartToken and not SyntaxKind.InterpolatedVerbatimStringStartToken)
+                return false;
+
+            token = token.GetPreviousToken();
+
+            return root.SyntaxTree.IsExpressionContext(start, token, attributes: true, cancellationToken)
                 || root.SyntaxTree.IsStatementContext(start, token, cancellationToken);
         }
     }
