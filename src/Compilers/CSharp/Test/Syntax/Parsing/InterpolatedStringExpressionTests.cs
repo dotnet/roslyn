4d5a0fed692e6e615fd38c026d11931aa69bef5f
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-12-10 11:35:09 -0800

    File scoped namespaces

--- src/Compilers/CSharp/Test/Syntax/Parsing/InterpolatedStringExpressionTests.cs
+++ src/Compilers/CSharp/Test/Syntax/Parsing/InterpolatedStringExpressionTests.cs
@@ -2,32 +2,26 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Xunit;
 
-namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Parsing
-{
-    public class InterpolatedStringExpressionTests
-    {
-        [Fact]
-        public void APIBackCompatTest1()
-        {
-            Assert.Equal("$\"\"", SyntaxFactory.InterpolatedStringExpression(SyntaxFactory.Token(SyntaxKind.InterpolatedStringStartToken)).ToFullString());
-        }
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Parsing;
 
-        [Fact]
-        public void APIBackCompatTest2()
-        {
-            Assert.Equal("$\"goo\"", SyntaxFactory.InterpolatedStringExpression(
-                SyntaxFactory.Token(SyntaxKind.InterpolatedStringStartToken),
-                SyntaxFactory.SingletonList<InterpolatedStringContentSyntax>(
-                    SyntaxFactory.InterpolatedStringText(SyntaxFactory.Token(
-                        default, SyntaxKind.InterpolatedStringTextToken, "goo", "goo", default)))).ToFullString());
-        }
+public class InterpolatedStringExpressionTests
+{
+    [Fact]
+    public void APIBackCompatTest1()
+    {
+        Assert.Equal("$\"\"", SyntaxFactory.InterpolatedStringExpression(SyntaxFactory.Token(SyntaxKind.InterpolatedStringStartToken)).ToFullString());
+    }
+
+    [Fact]
+    public void APIBackCompatTest2()
+    {
+        Assert.Equal("$\"goo\"", SyntaxFactory.InterpolatedStringExpression(
+            SyntaxFactory.Token(SyntaxKind.InterpolatedStringStartToken),
+            SyntaxFactory.SingletonList<InterpolatedStringContentSyntax>(
+                SyntaxFactory.InterpolatedStringText(SyntaxFactory.Token(
+                    default, SyntaxKind.InterpolatedStringTextToken, "goo", "goo", default)))).ToFullString());
     }
 }
