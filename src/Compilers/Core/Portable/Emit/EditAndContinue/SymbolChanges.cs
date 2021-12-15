381e2045637381edb2f37eb4e0ebf2c3074ec02b
Jared Parsons <jaredpparsons@gmail.com>
2021-12-15 07:56:55 -0800

    Update src/Compilers/Core/Portable/Emit/EditAndContinue/SymbolChanges.cs
    Co-authored-by: Sam Harwell <sam@tunnelvisionlabs.com>

--- src/Compilers/Core/Portable/Emit/EditAndContinue/SymbolChanges.cs
+++ src/Compilers/Core/Portable/Emit/EditAndContinue/SymbolChanges.cs
@@ -374,9 +374,9 @@ namespace Microsoft.CodeAnalysis.Emit
                     break;
             }
 
-            return symbol.ContainingSymbol is { Kind: SymbolKind.NetModule or SymbolKind.Assembly }
+            return symbol.ContainingSymbol is { Kind: SymbolKind.NetModule or SymbolKind.Assembly } containingSymbol
                 ? null
-                : symbol.ContainingSymbol;
+                : containingSymbol;
         }
     }
 }
