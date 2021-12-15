8d880cdf251d5bdb07d0e31d572d422d80c054cb
David Wengier <david.wengier@microsoft.com>
2021-12-15 16:15:42 +1100

    Don't restrict search to only source implementations

--- src/EditorFeatures/Core/FindUsages/AbstractFindUsagesService_FindImplementations.cs
+++ src/EditorFeatures/Core/FindUsages/AbstractFindUsagesService_FindImplementations.cs
@@ -110,7 +110,7 @@ namespace Microsoft.CodeAnalysis.Editor.FindUsages
 
             foreach (var linkedSymbol in linkedSymbols)
             {
-                var implementations = await FindSourceImplementationsWorkerAsync(
+                var implementations = await FindImplementationsWorkerAsync(
                     solution, linkedSymbol, cancellationToken).ConfigureAwait(false);
                 foreach (var implementation in implementations)
                 {
@@ -125,8 +125,7 @@ namespace Microsoft.CodeAnalysis.Editor.FindUsages
             {
                 foreach (var location in implementation.Locations)
                 {
-                    Contract.ThrowIfFalse(location.IsInSource);
-                    if (!seenLocations.Add((location.SourceTree.FilePath, location.SourceSpan)))
+                    if (location.IsInSource && !seenLocations.Add((location.SourceTree.FilePath, location.SourceSpan)))
                         return false;
                 }
 
@@ -134,26 +133,26 @@ namespace Microsoft.CodeAnalysis.Editor.FindUsages
             }
         }
 
-        private static async Task<ImmutableArray<ISymbol>> FindSourceImplementationsWorkerAsync(
+        private static async Task<ImmutableArray<ISymbol>> FindImplementationsWorkerAsync(
             Solution solution, ISymbol symbol, CancellationToken cancellationToken)
         {
             var implementations = await FindSourceAndMetadataImplementationsAsync(solution, symbol, cancellationToken).ConfigureAwait(false);
-            var sourceImplementations = new HashSet<ISymbol>(implementations.Where(s => s.IsFromSource()).Select(s => s.OriginalDefinition));
+            var result = new HashSet<ISymbol>(implementations.Select(s => s.OriginalDefinition));
 
             // For members, if we've found overrides of the original symbol, then filter out any abstract
             // members these inherit from.  The user has asked for literal implementations, and in the case
             // of an override, including the abstract as well isn't helpful.
-            var overrides = sourceImplementations.Where(s => s.IsOverride).ToImmutableArray();
+            var overrides = result.Where(s => s.IsOverride).ToImmutableArray();
             foreach (var ov in overrides)
             {
                 for (var overridden = ov.GetOverriddenMember(); overridden != null; overridden = overridden.GetOverriddenMember())
                 {
                     if (overridden.IsAbstract)
-                        sourceImplementations.Remove(overridden.OriginalDefinition);
+                        result.Remove(overridden.OriginalDefinition);
                 }
             }
 
-            return sourceImplementations.ToImmutableArray();
+            return result.ToImmutableArray();
         }
 
         private static async Task<ImmutableArray<ISymbol>> FindSourceAndMetadataImplementationsAsync(
