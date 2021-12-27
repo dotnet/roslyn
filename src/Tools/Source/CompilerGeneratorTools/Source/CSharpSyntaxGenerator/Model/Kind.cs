58a127c5b3d18e7378a837aee40075ba15e73427
Cyrus Najmabadi <cyrusn@microsoft.com>
2021-12-27 11:11:10 -0800

    NRT

--- src/Tools/Source/CompilerGeneratorTools/Source/CSharpSyntaxGenerator/Model/Kind.cs
+++ src/Tools/Source/CompilerGeneratorTools/Source/CSharpSyntaxGenerator/Model/Kind.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.Xml.Serialization;
 
@@ -12,15 +10,15 @@ namespace CSharpSyntaxGenerator
     public class Kind : IEquatable<Kind>
     {
         [XmlAttribute]
-        public string Name;
+        public string? Name;
 
-        public override bool Equals(object obj)
+        public override bool Equals(object? obj)
             => Equals(obj as Kind);
 
-        public bool Equals(Kind other)
+        public bool Equals(Kind? other)
             => Name == other?.Name;
 
         public override int GetHashCode()
-            => Name.GetHashCode();
+            => Name == null ? 0 : Name.GetHashCode();
     }
 }
