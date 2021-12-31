e39225a62311a6c4544dd263b8eeb227bded5519
Sam Harwell <Sam.Harwell@microsoft.com>
2021-12-31 07:28:31 -0800

    Store expected test outputs on disk for easy review

---
+++ src/Microsoft.VisualStudio.Extensibility.Testing.SourceGenerator.UnitTests/Resources/TestGenerationForVS2022/ShellInProcess1.g.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for more information.
+
+#nullable enable
+
+namespace Microsoft.VisualStudio.Extensibility.Testing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft;
+    using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Shell.Interop;
+    using Microsoft.VisualStudio.Threading;
+
+    [TestService]
+    internal partial class ShellInProcess
+    {
+        public new Task<TInterface> GetRequiredGlobalServiceAsync<TService, TInterface>(CancellationToken cancellationToken)
+            where TService : class
+            where TInterface : class
+        {
+            return base.GetRequiredGlobalServiceAsync<TService, TInterface>(cancellationToken);
+        }
+
+        public new Task<TService> GetComponentModelServiceAsync<TService>(CancellationToken cancellationToken)
+            where TService : class
+        {
+            return base.GetComponentModelServiceAsync<TService>(cancellationToken);
+        }
+
+        public async Task<string> GetActiveWindowCaptionAsync(CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            var monitorSelection = await GetRequiredGlobalServiceAsync<SVsShellMonitorSelection, IVsMonitorSelection>(cancellationToken);
+            ErrorHandler.ThrowOnFailure(monitorSelection.GetCurrentElementValue((uint)VSConstants.VSSELELEMID.SEID_WindowFrame, out var windowFrameObj));
+            var windowFrame = (IVsWindowFrame)windowFrameObj;
+
+            ErrorHandler.ThrowOnFailure(windowFrame.GetProperty((int)__VSFPROPID.VSFPROPID_Caption, out var captionObj));
+            return $"{captionObj}";
+        }
+
+        public async Task<Version> GetVersionAsync(CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            var shell = await GetRequiredGlobalServiceAsync<SVsShell, IVsShell>(cancellationToken);
+            shell.GetProperty((int)__VSSPROPID5.VSSPROPID_ReleaseVersion, out var versionProperty);
+
+            var fullVersion = versionProperty?.ToString() ?? string.Empty;
+            var firstSpace = fullVersion.IndexOf(' ');
+            if (firstSpace >= 0)
+            {
+                // e.g. "17.1.31907.60 MAIN"
+                fullVersion = fullVersion.Substring(0, firstSpace);
+            }
+
+            if (Version.TryParse(fullVersion, out var version))
+            {
+                return version;
+            }
+
+            throw new NotSupportedException($"Unexpected version format: {versionProperty}");
+        }
+    }
+}
