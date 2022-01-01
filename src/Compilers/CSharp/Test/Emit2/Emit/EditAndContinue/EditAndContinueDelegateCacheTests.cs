fd2d72761cee1110c539f43c86ce07b15fd068cb
Paul Chen <lancevdance@gmail.com>
2022-01-01 15:52:15 +0800

    Prepare for EnC tests.

---
+++ src/Compilers/CSharp/Test/Emit2/Emit/EditAndContinue/EditAndContinueDelegateCacheTests.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeGen;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
+using Microsoft.CodeAnalysis.CSharp.UnitTests;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.EditAndContinue.UnitTests;
+
+public class EditAndContinueDelegateCacheTests : EditAndContinueTestBase
+{
+}
