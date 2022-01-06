592e07f9003e4d9eb62beb02ee2dfab8b440c975
akhera99 <ankitakhera@microsoft.com>
2022-01-06 10:24:20 -0800

    still figuring things out

--- src/Features/CSharp/Portable/Completion/CompletionProviders/Snippets/AbstractSnippetCompletionProvider.cs
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/Snippets/AbstractSnippetCompletionProvider.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.CSharp.Completion.CompletionProviders.Snippets
 {
@@ -25,7 +26,40 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.CompletionProviders.Snippets
 
         private async Task<Document> DetermineNewDocumentAsync(Document document, CompletionItem completionItem, CancellationToken cancellationToken)
         {
+            var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
 
+            // The span we're going to replace
+            var line = text.Lines[SnippetCompletionProvider.GetLine(completionItem)];
+
+            // Annotate the line we care about so we can find it after adding usings
+            var tree = await document.GetRequiredSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
+            var token = GetToken(completionItem, tree, cancellationToken);
+            var annotatedRoot = tree.GetRoot(cancellationToken).ReplaceToken(token, token.WithAdditionalAnnotations(_otherAnnotation));
+            document = document.WithSyntaxRoot(annotatedRoot);
+
+            var memberContainingDocument = await GenerateMemberAndUsingsAsync(document, completionItem, line, cancellationToken).ConfigureAwait(false);
+            if (memberContainingDocument == null)
+            {
+                // Generating the new document failed because we somehow couldn't resolve
+                // the underlying symbol's SymbolKey. At this point, we won't be able to 
+                // make any changes, so just return the document we started with.
+                return document;
+            }
+
+            var insertionRoot = await GetTreeWithAddedSyntaxNodeRemovedAsync(memberContainingDocument, cancellationToken).ConfigureAwait(false);
+            var insertionText = await GenerateInsertionTextAsync(memberContainingDocument, cancellationToken).ConfigureAwait(false);
+
+            var destinationSpan = ComputeDestinationSpan(insertionRoot);
+
+            var finalText = insertionRoot.GetText(text.Encoding)
+                .Replace(destinationSpan, insertionText.Trim());
+
+            document = document.WithText(finalText);
+            var newRoot = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var declaration = GetSyntax(newRoot.FindToken(destinationSpan.End));
+
+            document = document.WithSyntaxRoot(newRoot.ReplaceNode(declaration, declaration.WithAdditionalAnnotations(_annotation)));
+            return await Formatter.FormatAsync(document, _annotation, cancellationToken: cancellationToken).ConfigureAwait(false);
         }
     }
 }
