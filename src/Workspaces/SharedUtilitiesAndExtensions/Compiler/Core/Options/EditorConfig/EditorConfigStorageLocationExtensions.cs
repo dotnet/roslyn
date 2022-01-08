9ca3e6ae27eafe70d0b60e8f0b15c7c7f2c851b1
tmat <tomas.matousek@microsoft.com>
2022-01-07 16:19:15 -0800

    Use reflection

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Options/EditorConfig/EditorConfigStorageLocationExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Options/EditorConfig/EditorConfigStorageLocationExtensions.cs
@@ -2,22 +2,29 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
+using System.Reflection;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Options
 {
     internal static class EditorConfigStorageLocationExtensions
     {
-        public static bool TryGetOption(this IEditorConfigStorageLocation editorConfigStorageLocation, AnalyzerConfigOptions analyzerConfigOptions, Type type, out object value)
+        public static bool TryGetOption(this IEditorConfigStorageLocation editorConfigStorageLocation, AnalyzerConfigOptions analyzerConfigOptions, Type type, out object? value)
         {
             // This is a workaround until we have an API for enumeratings AnalyzerConfigOptions. See https://github.com/dotnet/roslyn/issues/41840
-            if (analyzerConfigOptions is AnalyzerConfigOptionsDictionary optionsDictionary)
+            if (analyzerConfigOptions.GetType().FullName == typeof(AnalyzerConfigOptionsDictionary).FullName)
             {
-                return editorConfigStorageLocation.TryGetOption(optionsDictionary.Options, type, out value);
+                var optionsField = analyzerConfigOptions.GetType().GetField(nameof(AnalyzerConfigOptionsDictionary.Options), BindingFlags.NonPublic | BindingFlags.Instance);
+                Contract.ThrowIfNull(optionsField);
+
+                var options = optionsField.GetValue(analyzerConfigOptions);
+                Contract.ThrowIfNull(options);
+
+                return editorConfigStorageLocation.TryGetOption((ImmutableDictionary<string, string?>)options, type, out value);
             }
 
             value = null;
