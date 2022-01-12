862cb0b7ced5e79430e78d73f1df34cf0817b16d
David Wengier <david.wengier@microsoft.com>
2022-01-12 15:43:57 +1100

    Handle synthesized delegates without refKinds When originally adding support for sythensized delegates I made (wrong) assumptions that delegates were only synthesized for lambdas where there were ref parameters. Turns out any lambda with more than 16 parameters (incl return) also get synthesized delegates, and my delegate name parsing blew up. This fixes that.

--- src/Compilers/CSharp/Portable/Symbols/Synthesized/GeneratedNames.cs
+++ src/Compilers/CSharp/Portable/Symbols/Synthesized/GeneratedNames.cs
@@ -390,39 +390,43 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 return false;
             }
 
-            // The character after the prefix should be an open brace
-            if (name[DelegateNamePrefixLength] != '{')
-            {
-                return false;
-            }
-
             parameterCount = arity - (returnsVoid ? 0 : 1);
 
-            var lastBraceIndex = name.LastIndexOf('}');
-            if (lastBraceIndex < 0)
+            // If there are more than 16 type parameters then the synthesized delegate doesn't ref kinds encoded
+            // (and therefore no braces) so we use the end of the prefix instead.
+            var nameEndIndex = name.LastIndexOf('}');
+            if (nameEndIndex < 0)
             {
-                return false;
+                nameEndIndex = DelegateNamePrefixLength;
             }
-
-            // The ref kind string is between the two braces
-            var refKindString = name[DelegateNamePrefixLengthWithOpenBrace..lastBraceIndex];
-
-            if (!RefKindVector.TryParse(refKindString, arity, out byRefs))
+            else
             {
-                return false;
-            }
-
-            // If there is a generation index it will be directly after the brace, otherwise the brace
-            // is the last character
-            if (lastBraceIndex < name.Length - 1)
-            {
-                // Format is a '#' followed by the generation number
-                if (name[lastBraceIndex + 1] != '#')
+                // There should be a character after the prefix, and it should be an open brace
+                if (name.Length <= DelegateNamePrefixLength || name[DelegateNamePrefixLength] != '{')
                 {
                     return false;
                 }
 
-                if (!int.TryParse(name[(lastBraceIndex + 2)..], out generation))
+                // If there are braces, then the ref kind string is encoded between them
+                var refKindString = name[DelegateNamePrefixLengthWithOpenBrace..nameEndIndex];
+
+                if (!RefKindVector.TryParse(refKindString, arity, out byRefs))
+                {
+                    return false;
+                }
+            }
+
+            // If there is a generation index it will be directly after the brace, otherwise the brace
+            // is the last character
+            if (nameEndIndex < name.Length - 1)
+            {
+                // Format is a '#' followed by the generation number
+                if (name[nameEndIndex + 1] != '#')
+                {
+                    return false;
+                }
+
+                if (!int.TryParse(name[(nameEndIndex + 2)..], out generation))
                 {
                     return false;
                 }
