2d15a5dca8d19a87f31c8ef4399f810633feae81
Andrew Arnott <andrewarnott@live.com>
2022-01-13 08:09:07 -0700

    Enable publishing of artifacts in multiple passes
    When an artifact is not ready yet, but other artifacts are and must be prepared, we can now run `artifacts/_pipelines.ps1` multiple times in the pipeline.
    To opt out of an earlier pass, the artifact script need only produce 0 files. After an artifact script returns some files, that artifact will be published and will not be published again.
    
    This is similar to `variables/_pipelines.ps1` behavior, which we already run twice.

--- azure-pipelines/artifacts/_pipelines.ps1
+++ azure-pipelines/artifacts/_pipelines.ps1
@@ -7,4 +7,9 @@ param (
 
 & "$PSScriptRoot/_stage_all.ps1" -ArtifactNameSuffix $ArtifactNameSuffix |% {
     Write-Host "##vso[artifact.upload containerfolder=$($_.Name);artifactname=$($_.Name);]$($_.Path)"
+
+    # Set a variable which will out-live this script so that a subsequent attempt to collect and upload artifacts
+    # will skip this one from a check in the _all.ps1 script.
+    $varName = "ARTIFACTUPLOADED_$($_.Name.ToUpper())"
+    Write-Host "##vso[task.setvariable variable=$varName]true"
 }
