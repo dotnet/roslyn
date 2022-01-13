d5bba306a2685d2fefe94d5ba5874ffb28a93695
David Wengier <david.wengier@microsoft.com>
2022-01-13 11:58:54 +1100

    More tests

--- src/Compilers/CSharp/Test/Emit/Emit/GeneratedNamesTests.cs
+++ src/Compilers/CSharp/Test/Emit/Emit/GeneratedNamesTests.cs
@@ -12,6 +12,9 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Emit
         [Theory]
         [InlineData("<>A", true, 0, 0)]
         [InlineData("<>A{00010000}`8", true, 0, 8)]
+        [InlineData("<>A{00010000}#2`8", true, 2, 8)]
+        [InlineData("<>A{00010000,00000000}`16", true, 0, 16)]
+        [InlineData("<>A{00010000,00000000}#4`16", true, 4, 16)]
         [InlineData("<>A`5", true, 0, 5)]
         [InlineData("<>A`18", true, 0, 18)]
         [InlineData("<>F`1", false, 0, 0)]
@@ -21,7 +24,6 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Emit
         public void TryParseSynthesizedDelegateName_Success(string name, bool returnsVoid, int generation, int parameterCount)
         {
             Assert.True(GeneratedNames.TryParseSynthesizedDelegateName(name, out var actualByRefs, out var actualReturnsVoid, out var actualGeneration, out var actualParameterCount));
-
 
             Assert.Equal(returnsVoid, actualReturnsVoid);
             Assert.Equal(generation, actualGeneration);
@@ -40,6 +42,11 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.Emit
         [InlineData("<>ABCDEF")]
         [InlineData("<>A{Z}")]
         [InlineData("<>A#F")]
+        [InlineData("<>A{}")]
+        [InlineData("<>A{,}")]
+        [InlineData("<>A#")]
+        [InlineData("<>A{0,}")]
+        [InlineData("<>A{,1}")]
         public void TryParseSynthesizedDelegateName_Failure(string name)
         {
             Assert.False(GeneratedNames.TryParseSynthesizedDelegateName(name, out _, out _, out _, out _));
