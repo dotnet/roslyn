b8a272f6f84fc98ff392096ee304d10c28b41f7b
Andrew Arnott <andrewarnott@live.com>
2022-01-13 11:21:05 -0700

    Build SBOM manifest as part of build

--- azure-pipelines/artifacts/VSInsertion.ps1
+++ azure-pipelines/artifacts/VSInsertion.ps1
@@ -8,6 +8,9 @@ if (!$BuildConfiguration) {
 
 $PackagesRoot = "$RepoRoot/bin/Packages/$BuildConfiguration/NuGet"
 
+# This artifact is not ready if we're running on the devdiv AzDO account and we don't have an SBOM yet.
+if ($env:SYSTEM_COLLECTIONID -eq '011b8bdf-6d56-4f87-be0d-0092136884d9' -and -not (Test-Path $PackagesRoot/_manifest)) { return @{} }
+
 if (!(Test-Path $PackagesRoot))  { return @{} }
 
 @{
