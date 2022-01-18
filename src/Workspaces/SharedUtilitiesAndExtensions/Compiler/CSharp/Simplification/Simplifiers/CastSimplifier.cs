29b250abdcc6d5a51438f5c023b66c9d53ecb705
Youssef Victor <youssefvictor00@gmail.com>
2022-01-18 21:42:17 +0200

    Forward `cancellationToken`

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Simplification/Simplifiers/CastSimplifier.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Simplification/Simplifiers/CastSimplifier.cs
@@ -83,7 +83,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Simplification.Simplifiers
 
             // Special case for converting a method group to object. The compiler issues a warning if the cast is removed:
             // warning CS8974: Converting method group 'ToString' to non-delegate type 'object'. Did you intend to invoke the method?
-            if (semanticModel.GetOperation(cast.Expression)?.Kind == OperationKind.MethodReference)
+            if (semanticModel.GetOperation(cast.Expression, cancellationToken)?.Kind == OperationKind.MethodReference)
                 return false;
 
             return IsCastSafeToRemove(cast, cast.Expression, semanticModel, cancellationToken);
