ca68e89372d3cec48ae8223a10f7b4cc7dadcbb9
Shen Chen <cos_ifne@hotmail.com>
2022-01-18 14:36:49 -0800

    Move the extension method to private helper

--- src/EditorFeatures/Core/InheritanceMargin/InheritanceMarginServiceHelpers.cs
+++ src/EditorFeatures/Core/InheritanceMargin/InheritanceMarginServiceHelpers.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Editor.FindUsages;
-using Microsoft.CodeAnalysis.Editor.InheritanceMargin;
 using Microsoft.CodeAnalysis.FindSymbols;
 using Microsoft.CodeAnalysis.FindSymbols.FindReferences;
 using Microsoft.CodeAnalysis.FindUsages;
@@ -355,7 +354,8 @@ namespace Microsoft.CodeAnalysis.InheritanceMargin
             targetSymbol = symbolInSource ?? targetSymbol;
 
             // Right now the targets are not shown in a classified way.
-            var definition = await targetSymbol.ToSlimDefinitionItemAsync(
+            var definition = await ToSlimDefinitionItemAsync(
+                targetSymbol,
                 solution,
                 cancellationToken: cancellationToken).ConfigureAwait(false);
 
@@ -486,6 +486,60 @@ namespace Microsoft.CodeAnalysis.InheritanceMargin
                     transitive: true,
                     cancellationToken: cancellationToken).ConfigureAwait(false);
             }
+        }
+
+        /// <summary>
+        /// Create the DefinitionItem based on the numbers of locations for <paramref name="symbol"/>.
+        /// If there is only one location, create the DefinitionItem contains only the documentSpan or symbolKey to save memory.
+        /// Because in such case, when clicking InheritanceMarginGlpph, it will directly navigate to the symbol.
+        /// Otherwise, create the full non-classified DefinitionItem. Because in such case we want to display all the locations to the user
+        /// by reusing the FAR window.
+        /// </summary>
+        private static async Task<DefinitionItem> ToSlimDefinitionItemAsync(ISymbol symbol, Solution solution, CancellationToken cancellationToken)
+        {
+            var locations = symbol.Locations;
+            if (locations.IsEmpty)
+            {
+                return DefinitionItem.CreateNonNavigableItem(
+                    tags: GlyphTags.GetTags(symbol.GetGlyph()),
+                    displayParts: FindUsagesHelpers.GetDisplayParts(symbol));
+            }
+
+            if (locations.Length > 1)
+            {
+                return await symbol.ToNonClassifiedDefinitionItemAsync(
+                    solution,
+                    includeHiddenLocations: false,
+                    cancellationToken: cancellationToken).ConfigureAwait(false);
+            }
+
+            var location = locations[0];
+            if (location.IsInMetadata)
+            {
+                return DefinitionItem.CreateMetadataDefinition(
+                    tags: ImmutableArray<string>.Empty,
+                    displayParts: ImmutableArray<TaggedText>.Empty,
+                    nameDisplayParts: ImmutableArray<TaggedText>.Empty,
+                    solution,
+                    symbol);
+            }
+            else if (location.IsInSource && location.IsVisibleSourceLocation())
+            {
+                var document = solution.GetDocument(location.SourceTree);
+                if (document != null)
+                {
+                    var documentSpan = new DocumentSpan(document, location.SourceSpan);
+                    return DefinitionItem.Create(
+                        tags: ImmutableArray<string>.Empty,
+                        displayParts: ImmutableArray<TaggedText>.Empty,
+                        documentSpan,
+                        nameDisplayParts: ImmutableArray<TaggedText>.Empty);
+                }
+            }
+
+            return DefinitionItem.CreateNonNavigableItem(
+                tags: GlyphTags.GetTags(symbol.GetGlyph()),
+                displayParts: FindUsagesHelpers.GetDisplayParts(symbol));
         }
     }
 }
