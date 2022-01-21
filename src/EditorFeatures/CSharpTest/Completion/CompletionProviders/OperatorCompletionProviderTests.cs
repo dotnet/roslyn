ecc3027bf13457810332ea44f20bb7a52236bcf4
Sam Harwell <Sam.Harwell@microsoft.com>
2022-01-21 08:14:31 -0800

    Fix xUnit1016 (MemberData must reference a public member)

--- src/EditorFeatures/CSharpTest/Completion/CompletionProviders/OperatorCompletionProviderTests.cs
+++ src/EditorFeatures/CSharpTest/Completion/CompletionProviders/OperatorCompletionProviderTests.cs
@@ -23,7 +23,7 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionPr
         // The suggestion is e.g. "+". If the user actually types "+" the completion list is closed. Operators therefore do not support partially written items.
         protected override string? ItemPartiallyWritten(string? expectedItemOrNull) => "";
 
-        private static IEnumerable<object[]> BinaryArithmeticAndLogicalOperators()
+        public static IEnumerable<object[]> BinaryArithmeticAndLogicalOperators()
         {
             yield return new[] { "+" };
             yield return new[] { "&" };
@@ -37,7 +37,7 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionPr
             yield return new[] { "-" };
         }
 
-        private static IEnumerable<object[]> BinaryEqualityAndRelationalOperators()
+        public static IEnumerable<object[]> BinaryEqualityAndRelationalOperators()
         {
             yield return new[] { "==" };
             yield return new[] { ">" };
@@ -47,13 +47,13 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionPr
             yield return new[] { "<=" };
         }
 
-        private static IEnumerable<object[]> PostfixOperators()
+        public static IEnumerable<object[]> PostfixOperators()
         {
             yield return new[] { "++" };
             yield return new[] { "--" };
         }
 
-        private static IEnumerable<object[]> PrefixOperators()
+        public static IEnumerable<object[]> PrefixOperators()
         {
             yield return new[] { "!" };
             yield return new[] { "~" };
@@ -61,10 +61,10 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionPr
             yield return new[] { "+" };
         }
 
-        private static IEnumerable<object[]> BinaryOperators()
+        public static IEnumerable<object[]> BinaryOperators()
             => BinaryArithmeticAndLogicalOperators().Union(BinaryEqualityAndRelationalOperators());
 
-        private static IEnumerable<object[]> UnaryOperators()
+        public static IEnumerable<object[]> UnaryOperators()
             => PostfixOperators().Union(PrefixOperators());
 
         [Fact, Trait(Traits.Feature, Traits.Features.Completion)]
