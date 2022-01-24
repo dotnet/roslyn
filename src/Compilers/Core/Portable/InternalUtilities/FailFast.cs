f0684581bc349c9513639baf5e4962ade2fc9be3
Jared Parsons <jaredpparsons@gmail.com>
2022-01-24 09:40:57 -0800

    Remove netstandard1.3 from code base
    The remote debugger components moved to their minimum framework install
    to 4.6.2 where it was previously 2.0. That means we can remove the uses
    of `netstandard1.3` in our code base as they existed just to support
    this specific case.
    
    This means we can also remove our use of `net2.0` in the code base and
    replace it with `net462`. That is a more involved changed though and
    going to take that a piece at a time. Removing `netstandard1.3` is an
    important step because presently it's blocking us from updating to the
    latest System.Collections.Immutable as it dropped support for
    `netstandard1.3`

--- src/Compilers/Core/Portable/InternalUtilities/FailFast.cs
+++ src/Compilers/Core/Portable/InternalUtilities/FailFast.cs
@@ -20,9 +20,7 @@ namespace Microsoft.CodeAnalysis
 
         [DebuggerHidden]
         [DoesNotReturn]
-#if !NETSTANDARD1_3
         [MethodImpl(MethodImplOptions.Synchronized)]
-#endif
         internal static void OnFatalException(Exception exception)
         {
             // EDMAURER Now using the managed API to fail fast so as to default
@@ -49,9 +47,7 @@ namespace Microsoft.CodeAnalysis
 
         [DebuggerHidden]
         [DoesNotReturn]
-#if !NETSTANDARD1_3
         [MethodImpl(MethodImplOptions.Synchronized)]
-#endif
         internal static void Fail(string message)
         {
             DumpStackTrace(message: message);
@@ -82,7 +78,7 @@ namespace Microsoft.CodeAnalysis
                 }
             }
 
-#if !NET20 && !NETSTANDARD1_3
+#if !NET20
             Console.WriteLine("Stack trace of handler");
             var stackTrace = new StackTrace();
             Console.WriteLine(stackTrace.ToString());
