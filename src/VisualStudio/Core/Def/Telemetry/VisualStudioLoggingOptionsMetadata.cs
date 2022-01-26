6e0ec76a86def649e0af27ee94efe4e55c3238a4
tmat <tomas.matousek@microsoft.com>
2022-01-26 12:06:36 -0800

    Move FileLogger to VS layer

---
+++ src/VisualStudio/Core/Def/Telemetry/VisualStudioLoggingOptionsMetadata.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.Options;
+
+namespace Microsoft.CodeAnalysis.Diagnostics
+{
+    internal static class VisualStudioLoggingOptionsMetadata
+    {
+        private const string LocalRegistryPath = @"Roslyn\Internal\Diagnostics\";
+
+        public static readonly Option2<bool> EnableFileLoggingForDiagnostics = new("InternalDiagnosticsOptions", "EnableFileLoggingForDiagnostics", defaultValue: false,
+            storageLocation: new LocalUserProfileStorageLocation(LocalRegistryPath + "EnableFileLoggingForDiagnostics"));
+    }
+}
