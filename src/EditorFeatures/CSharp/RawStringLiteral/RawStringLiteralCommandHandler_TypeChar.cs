f88ba0363c20c92ff80173b517350f30e036fef5
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-01-26 10:39:56 -0800

    Simplify

--- src/EditorFeatures/CSharp/RawStringLiteral/RawStringLiteralCommandHandler_TypeChar.cs
+++ src/EditorFeatures/CSharp/RawStringLiteral/RawStringLiteralCommandHandler_TypeChar.cs
@@ -9,13 +9,10 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editor.Shared.Extensions;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
-using Microsoft.CodeAnalysis.Formatting;
-using Microsoft.CodeAnalysis.Indentation;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.Commanding;
 using Microsoft.VisualStudio.Text;
-using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
 using Roslyn.Utilities;
 
@@ -53,19 +50,37 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.RawStringLiteral
                 return false;
 
             var cancellationToken = CancellationToken.None;
-            return TryGenerateInitialEmptyRawString(textView, caret.Value, nextCommandHandler, cancellationToken) ||
-                TryGrowInitialEmptyRawString(textView, caret.Value, nextCommandHandler, cancellationToken) ||
-                TryGrowRawStringDelimeters(textView, caret.Value, nextCommandHandler, cancellationToken);
+            var textChangeOpt = TryGenerateInitialEmptyRawString(caret.Value, cancellationToken) ??
+                TryGrowInitialEmptyRawString(caret.Value, cancellationToken) ??
+                TryGrowRawStringDelimeters(caret.Value, cancellationToken);
+
+            if (textChangeOpt is not TextChange textChange)
+                return false;
+
+            // Looks good.  First, let the quote get added by the normal type char handlers.  Then make our text change.
+            // We do this in two steps so that undo can work properly.
+            nextCommandHandler();
+
+            using var transaction = CaretPreservingEditTransaction.TryCreate(
+                CSharpEditorResources.Grow_raw_string, textView, _undoHistoryRegistry, _editorOperationsFactoryService);
+
+            var edit = subjectBuffer.CreateEdit();
+            edit.Insert(textChange.Span.Start, textChange.NewText);
+            edit.Apply();
+
+            // ensure the caret is placed after where the original quote got added.
+            textView.Caret.MoveTo(new SnapshotPoint(subjectBuffer.CurrentSnapshot, caret.Value.Position + 1));
+
+            transaction?.Complete();
+            return true;
         }
 
         /// <summary>
         /// When typing <c>"</c> given a normal string like <c>""$$</c>, then update the text to be <c>"""$$"""</c>.
         /// Note that this puts the user in the position where TryGrowInitialEmptyRawString can now take effect.
         /// </summary>
-        private bool TryGenerateInitialEmptyRawString(
-            ITextView textView,
+        private static TextChange? TryGenerateInitialEmptyRawString(
             SnapshotPoint caret,
-            Action nextCommandHandler,
             CancellationToken cancellationToken)
         {
             var snapshot = caret.Snapshot;
@@ -73,7 +88,7 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.RawStringLiteral
 
             // if we have ""$$"   then typing `"` here should not be handled by this path but by TryGrowInitialEmptyRawString
             if (position + 1 < snapshot.Length && snapshot[position + 1] == '"')
-                return false;
+                return null;
 
             var start = position;
             while (start - 1 >= 0 && snapshot[start - 1] == '"')
@@ -81,43 +96,28 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.RawStringLiteral
 
             // must have exactly `""`
             if (position - start != 2)
-                return false;
+                return null;
 
             if (start - 1 >= 0 && snapshot[start - 1] == '$')
                 start--;
 
             // hitting `"` after `@""` shouldn't do anything
             if (start - 1 >= 0 && snapshot[start - 1] == '@')
-                return false;
+                return null;
 
             var document = snapshot.GetOpenDocumentInCurrentContextWithChanges();
             if (document == null)
-                return false;
+                return null;
 
             var root = document.GetRequiredSyntaxRootSynchronously(cancellationToken);
             var token = root.FindToken(start);
             if (token.SpanStart != start)
-                return false;
+                return null;
 
             if (token.Kind() is not SyntaxKind.StringLiteralToken and not SyntaxKind.InterpolatedStringStartToken)
-                return false;
+                return null;
 
-            // Looks good.  First, let the quote get added by the normal type char handlers.  Then add the additional
-            // three quotes to start the raw string.  We do this in two steps so that undo can work properly.
-            nextCommandHandler();
-
-            using var transaction = CaretPreservingEditTransaction.TryCreate(
-                CSharpEditorResources.Split_string, textView, _undoHistoryRegistry, _editorOperationsFactoryService);
-
-            var textBuffer = snapshot.TextBuffer;
-            var edit = textBuffer.CreateEdit();
-            edit.Insert(position + 1, "\"\"\"");
-            edit.Apply();
-
-            textView.Caret.MoveTo(new SnapshotPoint(textBuffer.CurrentSnapshot, position + 1));
-
-            transaction?.Complete();
-            return true;
+            return new TextChange(new TextSpan(position + 1, 0), "\"\"\"");
         }
 
         /// <summary>
@@ -126,10 +126,8 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.RawStringLiteral
         /// balanced.  This differs from TryGrowRawStringDelimeters in that the language will consider that initial
         /// <c>""""""</c> text to be a single delimeter, while we want to treat it as two.
         /// </summary>
-        private bool TryGrowInitialEmptyRawString(
-            ITextView textView,
+        private static TextChange? TryGrowInitialEmptyRawString(
             SnapshotPoint caret,
-            Action nextCommandHandler,
             CancellationToken cancellationToken)
         {
             var snapshot = caret.Snapshot;
@@ -146,51 +144,37 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.RawStringLiteral
             // Have to have an even number of quotes.
             var quoteLength = end - start;
             if (quoteLength % 2 == 1)
-                return false;
+                return null;
 
             // User position must be halfway through the quotes.
             if (position != (start + quoteLength / 2))
-                return false;
+                return null;
 
             // have to at least have `"""$$"""`
             if (quoteLength < 6)
-                return false;
+                return null;
 
             while (start - 1 >= 0 && snapshot[start - 1] == '$')
                 start--;
 
             var document = snapshot.GetOpenDocumentInCurrentContextWithChanges();
             if (document == null)
-                return false;
+                return null;
 
             var root = document.GetRequiredSyntaxRootSynchronously(cancellationToken);
             var token = root.FindToken(start);
             if (token.SpanStart != start)
-                return false;
+                return null;
 
             if (token.Kind() is not (SyntaxKind.SingleLineRawStringLiteralToken or
                                      SyntaxKind.MultiLineRawStringLiteralToken or
                                      SyntaxKind.InterpolatedSingleLineRawStringStartToken or
                                      SyntaxKind.InterpolatedMultiLineRawStringStartToken))
             {
-                return false;
+                return null;
             }
-            // Looks good.  First, let the quote get added by the normal type char handlers.  Then add the additional
-            // quote to pair with that.  We do this in two steps so that undo can work properly.
-            nextCommandHandler();
 
-            using var transaction = CaretPreservingEditTransaction.TryCreate(
-                CSharpEditorResources.Split_string, textView, _undoHistoryRegistry, _editorOperationsFactoryService);
-
-            var textBuffer = snapshot.TextBuffer;
-            var edit = textBuffer.CreateEdit();
-            edit.Insert(position + 1, "\"");
-            edit.Apply();
-
-            textView.Caret.MoveTo(new SnapshotPoint(textBuffer.CurrentSnapshot, position + 1));
-
-            transaction?.Complete();
-            return true;
+            return new TextChange(new TextSpan(position + 1, 0), "\"");
         }
 
         /// <summary>
@@ -198,20 +182,17 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.RawStringLiteral
         /// update the text to be: <c>"""" goo bar """"</c>.  i.e. grow both the start and end delimiters to keep the
         /// string properly balanced.
         /// </summary>
-        private bool TryGrowRawStringDelimeters(
-            ITextView textView,
+        private TextChange? TryGrowRawStringDelimeters(
             SnapshotPoint caret,
-            Action nextCommandHandler,
             CancellationToken cancellationToken)
         {
-
             var snapshot = caret.Snapshot;
             var position = caret.Position;
 
             // if we have """$$"   then typing `"` here should not grow the start/end quotes.  we only want to grow them
             // if the user is at the end of the start delimeter.
             if (position + 1 < snapshot.Length && snapshot[position + 1] == '"')
-                return false;
+                return null;
 
             var start = position;
             while (start - 1 >= 0 && snapshot[start - 1] == '"')
@@ -220,44 +201,29 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.RawStringLiteral
             // must have at least three quotes for this to be a raw string
             var quoteCount = position - start;
             if (quoteCount < 3)
-                return false;
+                return null;
 
             while (start - 1 >= 0 && snapshot[start - 1] == '$')
                 start--;
 
             var document = snapshot.GetOpenDocumentInCurrentContextWithChanges();
             if (document == null)
-                return false;
+                return null;
 
             var root = document.GetRequiredSyntaxRootSynchronously(cancellationToken);
             var token = root.FindToken(start);
             if (token.SpanStart != start)
-                return false;
+                return null;
 
             if (token.Kind() is not SyntaxKind.InterpolatedSingleLineRawStringStartToken and not SyntaxKind.InterpolatedMultiLineRawStringStartToken)
-                return false;
+                return null;
 
             var interpolatedString = (InterpolatedStringExpressionSyntax)token.GetRequiredParent();
             var endToken = interpolatedString.StringEndToken;
             if (!endToken.Text.EndsWith(new string('"', quoteCount)))
-                return false;
+                return null;
 
-            // Looks good.  First, let the quote get added by the normal type char handlers.  Then add the additional
-            // three quotes to start the raw string.  We do this in two steps so that undo can work properly.
-            nextCommandHandler();
-
-            using var transaction = CaretPreservingEditTransaction.TryCreate(
-                CSharpEditorResources.Split_string, textView, _undoHistoryRegistry, _editorOperationsFactoryService);
-
-            var textBuffer = snapshot.TextBuffer;
-            var edit = textBuffer.CreateEdit();
-            edit.Insert(endToken.Span.End, "\"");
-            edit.Apply();
-
-            textView.Caret.MoveTo(new SnapshotPoint(textBuffer.CurrentSnapshot, position + 1));
-
-            transaction?.Complete();
-            return true;
+            return new TextChange(new TextSpan(endToken.Span.End, 0), "\"");
         }
     }
 }
