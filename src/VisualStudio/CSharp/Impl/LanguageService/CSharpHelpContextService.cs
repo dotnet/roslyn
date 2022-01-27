c750dd451cbb569aa79a3da95f21839a540682d3
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-01-27 14:52:55 -0800

    Merge the interpolated end tokens into a single token

--- src/VisualStudio/CSharp/Impl/LanguageService/CSharpHelpContextService.cs
+++ src/VisualStudio/CSharp/Impl/LanguageService/CSharpHelpContextService.cs
@@ -135,11 +135,10 @@ namespace Microsoft.VisualStudio.LanguageServices.CSharp.LanguageService
         {
             if (token.IsKind(SyntaxKind.InterpolatedStringStartToken) ||
                 token.IsKind(SyntaxKind.InterpolatedStringEndToken) ||
+                token.IsKind(SyntaxKind.InterpolatedRawStringEndToken) ||
                 token.IsKind(SyntaxKind.InterpolatedStringTextToken) ||
                 token.IsKind(SyntaxKind.InterpolatedSingleLineRawStringStartToken) ||
-                token.IsKind(SyntaxKind.InterpolatedSingleLineRawStringEndToken) ||
-                token.IsKind(SyntaxKind.InterpolatedMultiLineRawStringStartToken) ||
-                token.IsKind(SyntaxKind.InterpolatedMultiLineRawStringEndToken))
+                token.IsKind(SyntaxKind.InterpolatedMultiLineRawStringStartToken))
             {
                 text = "$_CSharpKeyword";
                 return true;
