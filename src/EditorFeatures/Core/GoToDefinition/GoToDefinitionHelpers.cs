5f1297f68e43903be7f63fef46ac011e36e0f281
tmat <tomas.matousek@microsoft.com>
2022-01-28 13:45:16 -0800

    Revert "Revert "Remove dependency on EditorFeatures from Remote.ServiceHub project (#59059)""
    This reverts commit 3bbedda00a46ee01412b6455f699dc9f94577f2f.

--- src/EditorFeatures/Core/GoToDefinition/GoToDefinitionHelpers.cs
+++ src/EditorFeatures/Core/GoToDefinition/GoToDefinitionHelpers.cs
@@ -10,7 +10,6 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.Editor.FindUsages;
 using Microsoft.CodeAnalysis.Editor.Host;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.CodeAnalysis.FindSymbols;
--- src/Features/Core/Portable/StackTraceExplorer/StackFrameLocalMethodResolver.cs
+++ src/Features/Core/Portable/StackTraceExplorer/StackFrameLocalMethodResolver.cs
@@ -10,7 +10,7 @@ using Microsoft.CodeAnalysis.EmbeddedLanguages.StackFrame;
 using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 
-namespace Microsoft.CodeAnalysis.Editor.StackTraceExplorer
+namespace Microsoft.CodeAnalysis.StackTraceExplorer
 {
     internal class StackFrameLocalMethodResolver : AbstractStackTraceSymbolResolver
     {
