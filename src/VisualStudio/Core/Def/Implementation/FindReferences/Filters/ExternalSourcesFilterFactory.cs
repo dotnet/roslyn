1f5092e1dc27348b9ec7ae8d57d373de46b9f23a
David Wengier <david.wengier@microsoft.com>
2022-01-28 15:44:41 +1100

    Add filtering for the results window

---
+++ src/VisualStudio/Core/Def/Implementation/FindReferences/Filters/ExternalSourcesFilterFactory.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.Internal.VisualStudio.Shell.ErrorList;
+using Microsoft.VisualStudio.Shell.TableControl;
+using Microsoft.VisualStudio.Shell.TableManager;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.FindReferences.Filters
+{
+    [Export(typeof(IScopeFilterFactory))]
+    [TableManagerIdentifier("FindAllReferences*")]
+    [Name(nameof(ExternalSourcesFilterFactory))]
+    [Order(After = nameof(EntireSolutionWithoutMetadataFilterFactory))]
+    internal class ExternalSourcesFilterFactory : IScopeFilterFactory
+    {
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public ExternalSourcesFilterFactory()
+        {
+        }
+
+        public IErrorListFilterHandler CreateFilter(IWpfTableControl tableControl)
+        {
+            return new ExternalSourcesFilterHandler();
+        }
+
+        private class ExternalSourcesFilterHandler : ExternalSourcesFilterHandlerBase
+        {
+            public override int FilterId => 42;
+            public override string FilterDisplayName => ServicesVSResources.External_sources;
+
+            public override bool IncludeExact => false;
+            public override bool IncludeExactMetadata => true;
+
+            // TODO: Remove when https://github.com/dotnet/roslyn/issues/42847 is fixed
+            public override bool IncludeOther => false;
+        }
+    }
+}
---
+++ src/VisualStudio/Core/Def/Implementation/FindReferences/Filters/ExternalSourcesFilterHandlerBase.cs
@@ -0,0 +1,47 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Internal.VisualStudio.Shell.ErrorList;
+using Microsoft.VisualStudio.Shell.TableControl;
+using Microsoft.VisualStudio.Shell.TableManager;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.FindReferences.Filters
+{
+    internal abstract class ExternalSourcesFilterHandlerBase : FilterHandlerBase
+    {
+        public abstract bool IncludeExact { get; }
+        public abstract bool IncludeExactMetadata { get; }
+        public abstract bool IncludeOther { get; }
+
+        public override IEntryFilter GetFilter(out string displayText)
+        {
+            displayText = FilterDisplayName;
+            return new EntryFilter(IncludeExact, IncludeExactMetadata, IncludeOther);
+        }
+
+        private class EntryFilter : IEntryFilter
+        {
+            private readonly bool _includeExact;
+            private readonly bool _includeExactMetadata;
+            private readonly bool _includeOther;
+
+            public EntryFilter(bool includeExact, bool includeExactMetadata, bool includeOther)
+            {
+                _includeExact = includeExact;
+                _includeExactMetadata = includeExactMetadata;
+                _includeOther = includeOther;
+            }
+
+            public bool Match(ITableEntryHandle entry)
+            {
+                if (!entry.TryGetValue(StandardTableKeyNames.ItemOrigin, out ItemOrigin origin))
+                    origin = ItemOrigin.Exact;
+
+                return _includeExact && origin == ItemOrigin.Exact
+                    || _includeExactMetadata && origin == ItemOrigin.ExactMetadata
+                    || _includeOther && origin == ItemOrigin.Other;
+            }
+        }
+    }
+}
