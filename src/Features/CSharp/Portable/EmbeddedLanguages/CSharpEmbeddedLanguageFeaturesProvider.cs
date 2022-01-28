765518e8374a486d37e2b1e18862cf3287fad849
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-01-28 12:48:17 -0800

    Fix

--- src/Features/CSharp/Portable/EmbeddedLanguages/CSharpEmbeddedLanguageFeaturesProvider.cs
+++ src/Features/CSharp/Portable/EmbeddedLanguages/CSharpEmbeddedLanguageFeaturesProvider.cs
@@ -2,16 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System;
 using System.Composition;
 using Microsoft.CodeAnalysis.CSharp.EmbeddedLanguages.LanguageServices;
-<<<<<<< HEAD
 using Microsoft.CodeAnalysis.Editing;
-=======
 using Microsoft.CodeAnalysis.EmbeddedLanguages.LanguageServices;
->>>>>>> jsonTests
 using Microsoft.CodeAnalysis.Features.EmbeddedLanguages;
 using Microsoft.CodeAnalysis.Host.Mef;
 
