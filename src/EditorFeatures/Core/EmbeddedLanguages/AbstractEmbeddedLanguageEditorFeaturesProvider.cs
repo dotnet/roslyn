bbd0ec8531c77d9a35945397edffe66e8d572084
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-01-28 12:11:05 -0800

    Fix

--- src/EditorFeatures/Core/EmbeddedLanguages/AbstractEmbeddedLanguageEditorFeaturesProvider.cs
+++ src/EditorFeatures/Core/EmbeddedLanguages/AbstractEmbeddedLanguageEditorFeaturesProvider.cs
@@ -2,18 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Editor.EmbeddedLanguages.Json;
 using Microsoft.CodeAnalysis.Editor.EmbeddedLanguages.RegularExpressions;
 using Microsoft.CodeAnalysis.EmbeddedLanguages.LanguageServices;
 using Microsoft.CodeAnalysis.Features.EmbeddedLanguages;
-<<<<<<< HEAD
-=======
 using Microsoft.CodeAnalysis.Features.EmbeddedLanguages.DateAndTime;
-using Microsoft.CodeAnalysis.Features.EmbeddedLanguages.RegularExpressions;
->>>>>>> jsonTests
 
 namespace Microsoft.CodeAnalysis.Editor.EmbeddedLanguages
 {
@@ -27,19 +21,11 @@ namespace Microsoft.CodeAnalysis.Editor.EmbeddedLanguages
 
         protected AbstractEmbeddedLanguageEditorFeaturesProvider(EmbeddedLanguageInfo info) : base(info)
         {
-<<<<<<< HEAD
-            // No 'Fallback' language added here.  That's because the Fallback language doesn't
-            // support any of the IEmbeddedLanguageFeatures or IEmbeddedLanguageEditorFeatures
-            // capabilities.
-            Languages = ImmutableArray.Create<IEmbeddedLanguageEditorFeatures>(
-                new RegexEmbeddedLanguageEditorFeatures(this, info),
-                new JsonEmbeddedLanguageEditorFeatures(this, info));
-=======
             Languages = ImmutableArray.Create<IEmbeddedLanguage>(
                 new DateAndTimeEmbeddedLanguageEditorFeatures(info),
+                new JsonEmbeddedLanguageEditorFeatures(this, info),
                 new RegexEmbeddedLanguageEditorFeatures(this, info),
                 new FallbackEmbeddedLanguage(info));
->>>>>>> jsonTests
         }
     }
 }
