264d4761da8d01492f324591b7185e862e2c9799
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-01-29 01:21:01 -0800

    Add vb tests

--- src/EditorFeatures/VisualBasicTest/EmbeddedLanguages/ValidateJsonStringTests.vb
+++ src/EditorFeatures/VisualBasicTest/EmbeddedLanguages/ValidateJsonStringTests.vb
@@ -32,12 +32,11 @@ Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.EmbeddedLanguages
         Public Async Function TestWarning1() As Task
             Await TestDiagnosticInfoAsync("
 class Program
-{
-    void Main()
-    {
-        var r = /*lang=json,strict*/ ""[|new|] Json()""
-    }     
-}",
+    sub Main()
+        ' lang=json,strict
+        dim r = ""[|new|] Json()""
+    end sub     
+end class",
                 options:=OptionOn(),
                 diagnosticId:=AbstractJsonDiagnosticAnalyzer.DiagnosticId,
                 diagnosticSeverity:=DiagnosticSeverity.Warning,
@@ -48,12 +47,11 @@ class Program
         Public Async Function TestWarning2() As Task
             Await TestDiagnosticInfoAsync("
 class Program
-{
-    void Main()
-    {
-        var r = /*lang=json*/ ""[|}|]""
-    }     
-}",
+    sub Main()
+        ' lang=json
+        dim r = ""[|}|]""
+    end sub     
+end class",
                 options:=OptionOn(),
                 diagnosticId:=AbstractJsonDiagnosticAnalyzer.DiagnosticId,
                 diagnosticSeverity:=DiagnosticSeverity.Warning,
@@ -64,17 +62,15 @@ class Program
         <Fact, Trait(Traits.Feature, Traits.Features.ValidateJsonString)>
         Public Async Function TestJsonDocumentWithTrailingComma() As Task
             Await TestDiagnosticInfoAsync("<Workspace>
-    <Project Language=""C#"" CommonReferencesNet6=""true"">
+    <Project Language=""Visual Basic"" CommonReferencesNet6=""true"">
         <Document>
 imports System.Text.Json
 
 class Program
-{
-    void Main()
-    {
-        var r = JsonDocument.Parse(""[1[|,|]]"")
-    }     
-}
+    sub Main()
+        dim r = JsonDocument.Parse(""[1[|,|]]"")
+    end sub
+end class
         </Document>
     </Project>
 </Workspace>",
@@ -88,17 +84,15 @@ class Program
         <Fact, Trait(Traits.Feature, Traits.Features.ValidateJsonString)>
         Public Async Function TestJsonDocumentTrailingCommaDisallowed() As Task
             Await TestDiagnosticInfoAsync("<Workspace>
-    <Project Language=""C#"" CommonReferencesNet6=""true"">
+    <Project Language=""Visual Basic"" CommonReferencesNet6=""true"">
         <Document>
 imports System.Text.Json
 
 class Program
-{
-    void Main()
-    {
-        var r = JsonDocument.Parse(""[1[|,|]]"", new JsonDocumentOptions { AllowTrailingCommas = false })
-    }     
-}
+    sub Main()
+        dim r = JsonDocument.Parse(""[1[|,|]]"", new JsonDocumentOptions() with { .AllowTrailingCommas = false })
+    end sub
+end class
         </Document>
     </Project>
 </Workspace>",
@@ -107,41 +101,37 @@ class Program
                 diagnosticSeverity:=DiagnosticSeverity.Warning,
                 diagnosticMessage:=String.Format(FeaturesResources.JSON_issue_0,
                     FeaturesResources.Trailing_comma_not_allowed))
-        end Function
+        End Function
 
         <Fact, Trait(Traits.Feature, Traits.Features.ValidateJsonString)>
         Public Async Function TestJsonDocumentTrailingCommaAllowed() As Task
             Await TestDiagnosticMissingAsync("<Workspace>
-    <Project Language=""C#"" CommonReferencesNet6=""true"">
+    <Project Language=""Visual Basic"" CommonReferencesNet6=""true"">
         <Document>
 imports System.Text.Json
 
 class Program
-{
-    void Main()
-    {
-        var r = JsonDocument.Parse(""[1[|,|]]"", new JsonDocumentOptions { AllowTrailingCommas = true })
-    }     
-}
+    sub Main()
+        dim r = JsonDocument.Parse(""[1[|,|]]"", new JsonDocumentOptions() with { .AllowTrailingCommas = true })
+    end sub
+end class
         </Document>
     </Project>
 </Workspace>")
         End Function
 
         <Fact, Trait(Traits.Feature, Traits.Features.ValidateJsonString)>
-        Public Async Function TestJsonDocumentTrailingCommaAllowedImplicitObject() As Task
+        Public Async Function TestJsonDocumentTrailingCommaAllowedCaseChange() As Task
             Await TestDiagnosticMissingAsync("<Workspace>
-    <Project Language=""C#"" CommonReferencesNet6=""true"">
+    <Project Language=""Visual Basic"" CommonReferencesNet6=""true"">
         <Document>
 imports System.Text.Json
 
 class Program
-{
-    void Main()
-    {
-        var r = JsonDocument.Parse(""[1[|,|]]"", new() { AllowTrailingCommas = true })
-    }     
-}
+    sub Main()
+        dim r = JsonDocument.Parse(""[1[|,|]]"", new jsondocumentoptions() with { .allowTrailingCommas = true })
+    end sub
+end class
         </Document>
     </Project>
 </Workspace>")
@@ -150,17 +140,15 @@ class Program
         <Fact, Trait(Traits.Feature, Traits.Features.ValidateJsonString)>
         Public Async Function TestJsonDocumentWithComments() As Task
             Await TestDiagnosticInfoAsync("<Workspace>
-    <Project Language=""C#"" CommonReferencesNet6=""true"">
+    <Project Language=""Visual Basic"" CommonReferencesNet6=""true"">
         <Document>
 imports System.Text.Json
 
 class Program
-{
-    void Main()
-    {
-        var r = JsonDocument.Parse(""[1][|/*comment*/|]"")
-    }     
-}
+    sub Main()
+        dim r = JsonDocument.Parse(""[1][|/*comment*/|]"")
+    end sub
+end class
         </Document>
     </Project>
 </Workspace>",
@@ -174,17 +162,15 @@ class Program
         <Fact, Trait(Traits.Feature, Traits.Features.ValidateJsonString)>
         Public Async Function TestJsonDocumentCommentsDisallowed() As Task
             Await TestDiagnosticInfoAsync("<Workspace>
-    <Project Language=""C#"" CommonReferencesNet6=""true"">
+    <Project Language=""Visual Basic"" CommonReferencesNet6=""true"">
         <Document>
 imports System.Text.Json
 
 class Program
-{
-    void Main()
-    {
-        var r = JsonDocument.Parse(""[1][|/*comment*/|]"", new JsonDocumentOptions { CommentHandling = JsonCommentHandling.Disallow })
-    }     
-}
+    sub Main()
+        dim r = JsonDocument.Parse(""[1][|/*comment*/|]"", new JsonDocumentOptions() with { .CommentHandling = JsonCommentHandling.Disallow })
+    end sub
+end class
         </Document>
     </Project>
 </Workspace>",
@@ -198,36 +184,32 @@ class Program
         <Fact, Trait(Traits.Feature, Traits.Features.ValidateJsonString)>
         Public Async Function TestJsonDocumentCommentsAllowed() As Task
             Await TestDiagnosticMissingAsync("<Workspace>
-    <Project Language=""C#"" CommonReferencesNet6=""true"">
+    <Project Language=""Visual Basic"" CommonReferencesNet6=""true"">
         <Document>
 imports System.Text.Json
 
 class Program
-{
-    void Main()
-    {
-        var r = JsonDocument.Parse(""[1][|/*comment*/|]"", new JsonDocumentOptions { CommentHandling = JsonCommentHandling.Allow })
-    }     
-}
+    sub Main()
+        dim r = JsonDocument.Parse(""[1][|/*comment*/|]"", new JsonDocumentOptions() with { .CommentHandling = JsonCommentHandling.Allow })
+    end sub
+end class
         </Document>
     </Project>
 </Workspace>")
         End Function
 
         <Fact, Trait(Traits.Feature, Traits.Features.ValidateJsonString)>
-        Public Async Function TestJsonDocumentCommentsAllowedImplicitObject() As Task
+        Public Async Function TestJsonDocumentCommentsAllowedCaseInsensitive() As Task
             Await TestDiagnosticMissingAsync("<Workspace>
-    <Project Language=""C#"" CommonReferencesNet6=""true"">
+    <Project Language=""Visual Basic"" CommonReferencesNet6=""true"">
         <Document>
 imports System.Text.Json
 
 class Program
-{
-    void Main()
-    {
-        var r = JsonDocument.Parse(""[1][|/*comment*/|]"", new() { CommentHandling = JsonCommentHandling.Allow })
-    }     
-}
+    sub Main()
+        dim r = jsonDocument.parse(""[1][|/*comment*/|]"", new jsonDocumentOptions() with { .commentHandling = jsonCommentHandling.allow })
+    end sub
+end class
         </Document>
     </Project>
 </Workspace>")
