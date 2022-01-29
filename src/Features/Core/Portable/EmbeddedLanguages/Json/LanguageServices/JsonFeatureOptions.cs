3a7d4a977202f56558d2954dfbc549432304e7e2
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-01-29 02:08:40 -0800

    Simplify

--- src/Features/Core/Portable/EmbeddedLanguages/Json/LanguageServices/JsonFeatureOptions.cs
+++ src/Features/Core/Portable/EmbeddedLanguages/Json/LanguageServices/JsonFeatureOptions.cs
@@ -14,22 +14,19 @@ namespace Microsoft.CodeAnalysis.Features.EmbeddedLanguages.Json.LanguageService
 internal class JsonFeatureOptions
 {
     public static PerLanguageOption2<bool> ReportInvalidJsonPatterns =
-        new(
-            nameof(JsonFeatureOptions),
+        new(nameof(JsonFeatureOptions),
             nameof(ReportInvalidJsonPatterns),
             defaultValue: true,
             storageLocation: new RoamingProfileStorageLocation("TextEditor.%LANGUAGE%.Specific.ReportInvalidJsonPatterns"));
 
     public static PerLanguageOption2<bool> HighlightRelatedJsonComponentsUnderCursor =
-        new(
-            nameof(JsonFeatureOptions),
+        new(nameof(JsonFeatureOptions),
             nameof(HighlightRelatedJsonComponentsUnderCursor),
             defaultValue: true,
             storageLocation: new RoamingProfileStorageLocation("TextEditor.%LANGUAGE%.Specific.HighlightRelatedJsonComponentsUnderCursor"));
 
     public static PerLanguageOption2<bool> DetectAndOfferEditorFeaturesForProbableJsonStrings =
-        new(
-            nameof(JsonFeatureOptions),
+        new(nameof(JsonFeatureOptions),
             nameof(DetectAndOfferEditorFeaturesForProbableJsonStrings),
             defaultValue: true,
             storageLocation: new RoamingProfileStorageLocation("TextEditor.%LANGUAGE%.Specific.DetectAndOfferEditorFeaturesForProbableJsonStrings"));
