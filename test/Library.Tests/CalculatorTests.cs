51e93cfe21709edf3dd38e9e26c8bfa0e46799dd
Andrew Arnott <andrewarnott@live.com>
2022-02-01 10:52:26 -0700

    Update sample test, which was sporting dead code

--- test/Library.Tests/CalculatorTests.cs
+++ test/Library.Tests/CalculatorTests.cs
@@ -14,7 +14,7 @@ public class CalculatorTests
     public void AddOrSubtract()
     {
         // This tests aggregation of code coverage across test runs.
-#if NETCOREAPP2_1
+#if NETCOREAPP3_1
         Assert.Equal(3, Calculator.Add(1, 2));
 #else
         Assert.Equal(-1, Calculator.Subtract(1, 2));
