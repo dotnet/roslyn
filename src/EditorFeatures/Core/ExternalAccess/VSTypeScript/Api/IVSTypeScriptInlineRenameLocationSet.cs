2515b404c95ab97878294f0f3674c7adf88e7f6b
tmat <tomas.matousek@microsoft.com>
2022-02-02 16:52:16 -0800

    Remove legacy TypeScript EA API

--- src/EditorFeatures/Core/ExternalAccess/VSTypeScript/Api/IVSTypeScriptInlineRenameLocationSet.cs
+++ src/EditorFeatures/Core/ExternalAccess/VSTypeScript/Api/IVSTypeScriptInlineRenameLocationSet.cs
@@ -15,25 +15,6 @@ using Microsoft.CodeAnalysis.Rename;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.VSTypeScript.Api
 {
-    [Obsolete]
-    internal interface IVSTypeScriptInlineRenameLocationSet
-    {
-        /// <summary>
-        /// The set of locations that need to be updated with the replacement text that the user
-        /// has entered in the inline rename session.  These are the locations are all relative
-        /// to the solution when the inline rename session began.
-        /// </summary>
-        IList<VSTypeScriptInlineRenameLocationWrapper> Locations { get; }
-
-        /// <summary>
-        /// Returns the set of replacements and their possible resolutions if the user enters the
-        /// provided replacement text and options.  Replacements are keyed by their document id
-        /// and TextSpan in the original solution, and specify their new span and possible conflict
-        /// resolution.
-        /// </summary>
-        Task<IVSTypeScriptInlineRenameReplacementInfo> GetReplacementsAsync(string replacementText, OptionSet optionSet, CancellationToken cancellationToken);
-    }
-
     internal abstract class VSTypeScriptInlineRenameLocationSet : IInlineRenameLocationSet
     {
         /// <summary>
