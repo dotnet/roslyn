f403873b7904fefdca00556690692cfbbdaa55bb
Youssef1313 <youssefvictor00@gmail.com>
2022-02-04 13:18:52 +0200

    Add another test

--- src/Compilers/CSharp/Test/Semantic/Semantics/PropertyFieldKeywordTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/PropertyFieldKeywordTests.cs
@@ -1420,5 +1420,30 @@ class C
             Assert.Equal(SpecialType.System_Int32, fieldKeywordTypeInfo.Type.SpecialType);
             Assert.Equal(0, accessorBindingData.NumberOfPerformedAccessorBinding);
         }
+
+        [Fact]
+        public void SpeculativeSemanticModelTest2()
+        {
+            var comp = CreateCompilation(@"
+class C
+{
+    public int P { get => 0; }
+}
+");
+            var accessorBindingData = new SourcePropertySymbolBase.AccessorBindingData();
+            comp.TestOnlyCompilationData = accessorBindingData;
+
+            var tree = comp.SyntaxTrees[0];
+            var token = tree.GetRoot().DescendantTokens().Single(t => t.IsKind(SyntaxKind.NumericLiteralToken));
+
+            var model = comp.GetSemanticModel(comp.SyntaxTrees[0]);
+            var identifier = SyntaxFactory.IdentifierName(SyntaxFactory.Identifier(leading: default, contextualKind: SyntaxKind.FieldKeyword, text: "field", valueText: "field", trailing: default));
+            model.TryGetSpeculativeSemanticModel(token.SpanStart, identifier, out var speculativeModel);
+            var fieldKeywordTypeInfo = speculativeModel.GetTypeInfo(identifier);
+
+            Assert.Empty(comp.GetTypeByMetadataName("C").GetFieldsToEmit());
+            Assert.Equal(SpecialType.System_Int32, fieldKeywordTypeInfo.Type.SpecialType);
+            Assert.Equal(0, accessorBindingData.NumberOfPerformedAccessorBinding);
+        }
     }
 }
