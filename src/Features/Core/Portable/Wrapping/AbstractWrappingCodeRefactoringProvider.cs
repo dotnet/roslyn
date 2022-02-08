62dde579727355ebd1d9093c28c23950b6cd2ff1
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-02-08 13:16:28 -0800

    Cleanup

--- src/Features/Core/Portable/Wrapping/AbstractWrappingCodeRefactoringProvider.cs
+++ src/Features/Core/Portable/Wrapping/AbstractWrappingCodeRefactoringProvider.cs
@@ -2,13 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeRefactorings;
-using Microsoft.CodeAnalysis.RemoveUnusedVariable;
 
 namespace Microsoft.CodeAnalysis.Wrapping
 {
