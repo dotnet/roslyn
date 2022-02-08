516b58be4447cfc68d46e2213d50969cea4fb6df
Shen Chen <shech@microsoft.com>
2022-02-08 15:28:00 -0800

    Modify the CodeFixProvider to use BatchFixer

--- src/Analyzers/Core/CodeFixes/NamingStyle/NamingStyleCodeFixProvider.cs
+++ src/Analyzers/Core/CodeFixes/NamingStyle/NamingStyleCodeFixProvider.cs
@@ -40,64 +40,56 @@ namespace Microsoft.CodeAnalysis.CodeFixes.NamingStyles
         public override ImmutableArray<string> FixableDiagnosticIds { get; }
             = ImmutableArray.Create(IDEDiagnosticIds.NamingRuleId);
 
-        public override FixAllProvider? GetFixAllProvider()
-        {
-            // Currently Fix All is not supported for naming style violations.
-            return null;
-        }
+        public override FixAllProvider? GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            var diagnostic = context.Diagnostics.First();
-            var serializedNamingStyle = diagnostic.Properties[nameof(NamingStyle)];
-            var style = NamingStyle.FromXElement(XElement.Parse(serializedNamingStyle));
-
             var document = context.Document;
             var span = context.Span;
+            var syntaxFactsService = document.GetRequiredLanguageService<ISyntaxFactsService>();
+            var model = await document.GetRequiredSemanticModelAsync(context.CancellationToken).ConfigureAwait(false);
 
-            var root = await document.GetRequiredSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-            var node = root.FindNode(span);
-
-            if (document.GetRequiredLanguageService<ISyntaxFactsService>().IsIdentifierName(node))
+            foreach (var diagnostic in context.Diagnostics)
             {
-                // The location we get from the analyzer only contains the identifier token and when we get its containing node,
+                var serializedNamingStyle = diagnostic.Properties[nameof(NamingStyle)];
+                var style = NamingStyle.FromXElement(XElement.Parse(serializedNamingStyle));
+
+                var root = await document.GetRequiredSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+                var node = root.FindNode(span);
+
                 // it is usually the right one (such as a variable declarator, designation or a foreach statement)
                 // because there is no other node in between. But there is one case in a VB catch clause where the token
                 // is wrapped in an identifier name. So if what we found is an identifier, take the parent node instead.
                 // Note that this is the correct thing to do because GetDeclaredSymbol never works on identifier names.
-                node = node.Parent;
-            }
+                if (syntaxFactsService.IsIdentifierName(node))
+                    node = node.Parent;
 
-            if (node == null)
-                return;
+                if (node is null)
+                    continue;
 
-            var model = await document.GetRequiredSemanticModelAsync(context.CancellationToken).ConfigureAwait(false);
-            var symbol = model.GetDeclaredSymbol(node, context.CancellationToken);
+                var symbol = model.GetDeclaredSymbol(node, context.CancellationToken);
+                // TODO: We should always be able to find the symbol that generated this diagnostic,
+                // but this cannot always be done by simply asking for the declared symbol on the node 
+                // from the symbol's declaration location.
+                // See https://github.com/dotnet/roslyn/issues/16588
+                if (symbol is null)
+                    continue;
 
-            // TODO: We should always be able to find the symbol that generated this diagnostic,
-            // but this cannot always be done by simply asking for the declared symbol on the node 
-            // from the symbol's declaration location.
-            // See https://github.com/dotnet/roslyn/issues/16588
-
-            if (symbol == null)
-            {
-                return;
-            }
-
-            var fixedNames = style.MakeCompliant(symbol.Name);
-            foreach (var fixedName in fixedNames)
-            {
-                context.RegisterCodeFix(
-                    new FixNameCodeAction(
+                var fixedNames = style.MakeCompliant(symbol.Name);
+                foreach (var fixedName in fixedNames)
+                {
+                    context.RegisterCodeFix(
+                        new FixNameCodeAction(
 #if !CODE_STYLE
-                        document.Project.Solution,
-                        symbol,
-                        fixedName,
+                            document.Project.Solution,
+                            symbol,
+                            fixedName,
 #endif
-                        string.Format(CodeFixesResources.Fix_Name_Violation_colon_0, fixedName),
-                        c => FixAsync(document, symbol, fixedName, c),
-                        equivalenceKey: nameof(NamingStyleCodeFixProvider)),
-                    diagnostic);
+                            string.Format(CodeFixesResources.Fix_Name_Violation_colon_0, fixedName),
+                            c => FixAsync(document, symbol, fixedName, c),
+                            equivalenceKey: nameof(NamingStyleCodeFixProvider)),
+                        diagnostic);
+                }
             }
         }
 
@@ -141,11 +133,17 @@ namespace Microsoft.CodeAnalysis.CodeFixes.NamingStyles
                 _equivalenceKey = equivalenceKey;
             }
 
+            protected override Task<Solution?> GetChangedSolutionAsync(CancellationToken cancellationToken)
+            {
+                return _createChangedSolutionAsync(cancellationToken).AsNullable();
+            }
+
             protected override async Task<IEnumerable<CodeActionOperation>> ComputePreviewOperationsAsync(CancellationToken cancellationToken)
             {
                 return SpecializedCollections.SingletonEnumerable(
                     new ApplyChangesOperation(await _createChangedSolutionAsync(cancellationToken).ConfigureAwait(false)));
             }
+
             protected override async Task<IEnumerable<CodeActionOperation>> ComputeOperationsAsync(CancellationToken cancellationToken)
             {
                 var newSolution = await _createChangedSolutionAsync(cancellationToken).ConfigureAwait(false);
