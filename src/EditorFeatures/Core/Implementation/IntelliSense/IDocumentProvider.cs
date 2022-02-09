e4c2d06e72ee898d5e04f23bd573809aa8b502f6
Sam Harwell <Sam.Harwell@microsoft.com>
2022-02-08 17:13:39 -0800

    Enable nullable reference types for IDocumentProvider

--- src/EditorFeatures/Core/Implementation/IntelliSense/IDocumentProvider.cs
+++ src/EditorFeatures/Core/Implementation/IntelliSense/IDocumentProvider.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
@@ -14,7 +12,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense
 {
     internal interface IDocumentProvider
     {
-        Document GetDocument(ITextSnapshot snapshot, CancellationToken cancellationToken);
+        Document? GetDocument(ITextSnapshot snapshot, CancellationToken cancellationToken);
     }
 
     internal class DocumentProvider : ForegroundThreadAffinitizedObject, IDocumentProvider
@@ -24,7 +22,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense
         {
         }
 
-        public Document GetDocument(ITextSnapshot snapshot, CancellationToken cancellationToken)
+        public Document? GetDocument(ITextSnapshot snapshot, CancellationToken cancellationToken)
         {
             AssertIsBackground();
             return snapshot.AsText().GetDocumentWithFrozenPartialSemantics(cancellationToken);
