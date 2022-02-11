8e72ebe3e4e2b89a929eae2307620fae3446062a
Sam Harwell <Sam.Harwell@microsoft.com>
2022-02-11 11:05:53 -0800

    Port CSharpOrganizing to the new integration test project

---
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/WellKnownCommands.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.VisualStudio;
+
+namespace Roslyn.VisualStudio.IntegrationTests
+{
+    internal static class WellKnownCommands
+    {
+        public static class Edit
+        {
+            public static readonly (Guid commandGroup, uint commandId) RemoveAndSort = (VSConstants.CMDSETID.CSharpGroup_guid, 6419);
+        }
+    }
+}
