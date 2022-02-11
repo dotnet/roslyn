c0452389a077f6836454bc11af1c55ec23bc41a8
Julien Couvreur <julien.couvreur@gmail.com>
2022-02-11 00:21:16 -0800

    Check definite assignment

--- src/Compilers/CSharp/Test/Semantic/Semantics/PatternMatchingTests_ListPatterns.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/PatternMatchingTests_ListPatterns.cs
@@ -2766,6 +2766,8 @@ class X
     {
         var source = @"
 _ = new S() is [..var y];
+y.ToString();
+
 _ = new S() is [..];
 _ = new S() is [..[..]];
 _ = new S() is not [..];
@@ -2782,15 +2784,15 @@ struct S
             // (2,5): warning CS8794: An expression of type 'S' always matches the provided pattern.
             // _ = new S() is [..var y];
             Diagnostic(ErrorCode.WRN_IsPatternAlways, "new S() is [..var y]").WithArguments("S").WithLocation(2, 5),
-            // (3,5): warning CS8794: An expression of type 'S' always matches the provided pattern.
+            // (5,5): warning CS8794: An expression of type 'S' always matches the provided pattern.
             // _ = new S() is [..];
-            Diagnostic(ErrorCode.WRN_IsPatternAlways, "new S() is [..]").WithArguments("S").WithLocation(3, 5),
-            // (4,5): warning CS8794: An expression of type 'S' always matches the provided pattern.
+            Diagnostic(ErrorCode.WRN_IsPatternAlways, "new S() is [..]").WithArguments("S").WithLocation(5, 5),
+            // (6,5): warning CS8794: An expression of type 'S' always matches the provided pattern.
             // _ = new S() is [..[..]];
-            Diagnostic(ErrorCode.WRN_IsPatternAlways, "new S() is [..[..]]").WithArguments("S").WithLocation(4, 5),
-            // (5,5): error CS8518: An expression of type 'S' can never match the provided pattern.
+            Diagnostic(ErrorCode.WRN_IsPatternAlways, "new S() is [..[..]]").WithArguments("S").WithLocation(6, 5),
+            // (7,5): error CS8518: An expression of type 'S' can never match the provided pattern.
             // _ = new S() is not [..];
-            Diagnostic(ErrorCode.ERR_IsPatternImpossible, "new S() is not [..]").WithArguments("S").WithLocation(5, 5)
+            Diagnostic(ErrorCode.ERR_IsPatternImpossible, "new S() is not [..]").WithArguments("S").WithLocation(7, 5)
             );
     }
 
