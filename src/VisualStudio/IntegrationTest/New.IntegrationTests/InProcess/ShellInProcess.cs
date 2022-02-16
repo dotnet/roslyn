4da854c246ae5f4979837131e4b3d1cc8f209726
Sam Harwell <Sam.Harwell@microsoft.com>
2022-02-16 13:12:48 -0800

    Update to vs-extension-testing 0.1.127-beta

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/ShellInProcess.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/ShellInProcess.cs
@@ -2,71 +2,20 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.UnitTests;
-using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Threading;
 using IAsyncDisposable = System.IAsyncDisposable;
-using IOleCommandTarget = Microsoft.VisualStudio.OLE.Interop.IOleCommandTarget;
-using OLECMDEXECOPT = Microsoft.VisualStudio.OLE.Interop.OLECMDEXECOPT;
 
 namespace Microsoft.VisualStudio.Extensibility.Testing
 {
     internal partial class ShellInProcess
     {
-        public async Task<(Guid commandGroup, uint commandId)> PrepareCommandAsync(string command, CancellationToken cancellationToken)
-        {
-            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-
-            var commandWindow = await GetRequiredGlobalServiceAsync<SVsCommandWindow, IVsCommandWindow>(cancellationToken);
-            var result = new PREPARECOMMANDRESULT[1];
-            ErrorHandler.ThrowOnFailure(commandWindow.PrepareCommand(command, out var commandGroup, out var commandId, out var cmdArg, result));
-
-            if (cmdArg != IntPtr.Zero)
-            {
-                throw new NotSupportedException("Unable to create a disposable wrapper for command arguments (VARIANT).");
-            }
-
-            return (commandGroup, commandId);
-        }
-
-        public async Task ExecuteCommandAsync(string command, CancellationToken cancellationToken)
-        {
-            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-
-            var (commandGuid, commandId) = await PrepareCommandAsync(command, cancellationToken);
-            await ExecuteCommandAsync(commandGuid, commandId, cancellationToken);
-        }
-
-        public Task ExecuteCommandAsync<TEnum>(TEnum command, CancellationToken cancellationToken)
-            where TEnum : struct, Enum
-        {
-            var commandGuid = command switch
-            {
-                EditorConstants.EditorCommandID => EditorConstants.EditorCommandSet,
-                _ => typeof(TEnum).GUID,
-            };
-
-            return ExecuteCommandAsync(commandGuid, Convert.ToUInt32(command), cancellationToken);
-        }
-
-        public Task ExecuteCommandAsync((Guid commandGuid, uint commandId) command, CancellationToken cancellationToken)
-            => ExecuteCommandAsync(command.commandGuid, command.commandId, cancellationToken);
-
-        public async Task ExecuteCommandAsync(Guid commandGuid, uint commandId, CancellationToken cancellationToken)
-        {
-            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-
-            var dispatcher = await TestServices.Shell.GetRequiredGlobalServiceAsync<SUIHostCommandDispatcher, IOleCommandTarget>(cancellationToken);
-            ErrorHandler.ThrowOnFailure(dispatcher.Exec(commandGuid, commandId, (uint)OLECMDEXECOPT.OLECMDEXECOPT_DODEFAULT, IntPtr.Zero, IntPtr.Zero));
-        }
-
         public async Task<PauseFileChangesRestorer> PauseFileChangesAsync(CancellationToken cancellationToken)
         {
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
--- src/VisualStudio/IntegrationTest/New.IntegrationTests/WellKnownCommands.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/WellKnownCommands.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
+using System.ComponentModel.Design;
 using Microsoft.VisualStudio;
 
 namespace Roslyn.VisualStudio.IntegrationTests
@@ -11,7 +11,7 @@ namespace Roslyn.VisualStudio.IntegrationTests
     {
         public static class Edit
         {
-            public static readonly (Guid commandGroup, uint commandId) RemoveAndSort = (VSConstants.CMDSETID.CSharpGroup_guid, 6419);
+            public static readonly CommandID RemoveAndSort = new(VSConstants.CMDSETID.CSharpGroup_guid, 6419);
         }
     }
 }
