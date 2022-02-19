caa111e8a1395da12ca6fab4f5a45205244ff512
Logan Bussell <loganbussell@microsoft.com>
2022-02-18 16:39:33 -0800

    Revert changes for lifting some package versions for source-build

--- src/Tools/BuildBoss/ProjectCheckerUtil.cs
+++ src/Tools/BuildBoss/ProjectCheckerUtil.cs
@@ -158,7 +158,6 @@ namespace BuildBoss
                 yield return $"$({name}Version)";
                 yield return $"$({name}FixedVersion)";
                 yield return $"$(RefOnly{name}Version)";
-                yield return $"$(SourceBuildLifted{name}Version)";
             }
         }
 
