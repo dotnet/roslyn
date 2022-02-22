efb11dd72c3e88d2efadd79af8649f27cd96bc1b
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-02-22 11:35:40 -0800

    Port over

---
+++ src/Features/LanguageServer/Protocol/Handler/SpellChecking/DocumentPullDiagnosticHandlerProvider.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Host.Mef;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler.SpellChecking
+{
+    [ExportRoslynLanguagesLspRequestHandlerProvider(typeof(DocumentPullDiagnosticHandler)), Shared]
+    internal class DocumentPullDiagnosticHandlerProvider : AbstractRequestHandlerProvider
+    {
+        private readonly IDiagnosticService _diagnosticService;
+        private readonly IDiagnosticAnalyzerService _analyzerService;
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public DocumentPullDiagnosticHandlerProvider(
+            IDiagnosticService diagnosticService,
+            IDiagnosticAnalyzerService analyzerService)
+        {
+            _diagnosticService = diagnosticService;
+            _analyzerService = analyzerService;
+        }
+
+        public override ImmutableArray<IRequestHandler> CreateRequestHandlers(WellKnownLspServerKinds serverKind)
+        {
+            return ImmutableArray.Create<IRequestHandler>(new DocumentPullDiagnosticHandler(serverKind, _diagnosticService, _analyzerService));
+        }
+    }
+}
