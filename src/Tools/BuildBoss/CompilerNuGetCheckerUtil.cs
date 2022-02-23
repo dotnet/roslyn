9aead50e8b7052b6d9c811eca5a6bacbd285d788
Jared Parsons <jaredpparsons@gmail.com>
2022-02-23 08:50:59 -0800

    Remove net20 build logic

--- src/Tools/BuildBoss/CompilerNuGetCheckerUtil.cs
+++ src/Tools/BuildBoss/CompilerNuGetCheckerUtil.cs
@@ -169,8 +169,6 @@ namespace BuildBoss
             textWriter.WriteLine("Verifying contents of VS.ExternalAPIs.Roslyn");
             textWriter.WriteLine("\tRoot Folder");
             verifyFolder("");
-            textWriter.WriteLine("\tRemote Debugger net20");
-            verifyFolder(@"RemoteDebugger\net20");
             textWriter.WriteLine("\tRemote Debugger net50");
             verifyFolder(@"RemoteDebugger\net45");
             return allGood;
