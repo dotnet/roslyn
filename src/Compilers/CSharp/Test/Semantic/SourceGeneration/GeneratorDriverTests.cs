d489e71887fd19348014e9e86dc986a7cd79d38e
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2022-02-23 09:38:20 -0800

    Handle binary files in AdditionalTextComparer.GetHashCode (#59660)
    * Handle binary files in AdditionalTextComparer.GetHashCode
    
    This is a follow up to https://github.com/dotnet/roslyn/pull/59578, which missed the `GetHashCode` case.
    
    * Add test that hits both altered code paths and confirm doesn't throw
    
    Co-authored-by: Chris Sienkiewicz <chsienki@microsoft.com>

--- src/Compilers/CSharp/Test/Semantic/SourceGeneration/GeneratorDriverTests.cs
+++ src/Compilers/CSharp/Test/Semantic/SourceGeneration/GeneratorDriverTests.cs
@@ -2806,5 +2806,37 @@ class C { }
             driver = driver.RunGenerators(compilation);
             Assert.Single(referenceList, modifiedRef.Display);
         }
+
+        [WorkItem(59209, "https://github.com/dotnet/roslyn/issues/59209")]
+        public void Binary_Additional_Files_Do_Not_Throw_When_Compared()
+        {
+            var source = "class C{}";
+
+            var parseOptions = TestOptions.RegularPreview;
+            Compilation compilation = CreateCompilation(source, options: TestOptions.DebugDll, parseOptions: parseOptions);
+            compilation.VerifyDiagnostics();
+
+            var generator = new PipelineCallbackGenerator(ctx =>
+            {
+                ctx.RegisterSourceOutput(ctx.AdditionalTextsProvider, (context, text) =>
+                {
+                    context.AddSource(Path.GetFileName(text.Path), "");
+                });
+            });
+
+            var additionalText1 = new InMemoryAdditionalText.BinaryText("file1");
+            var additionalText2 = new InMemoryAdditionalText.BinaryText("file2");
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(new[] { generator.AsSourceGenerator() },
+                parseOptions: parseOptions,
+                additionalTexts: new[] { additionalText1, additionalText2 },
+                driverOptions: new GeneratorDriverOptions(IncrementalGeneratorOutputKind.None, trackIncrementalGeneratorSteps: true));
+
+            driver = driver.RunGenerators(compilation);
+            var result = driver.GetRunResult();
+
+            Assert.Equal(2, result.GeneratedTrees.Length);
+            driver = driver.RunGenerators(compilation);
+        }
     }
 }
