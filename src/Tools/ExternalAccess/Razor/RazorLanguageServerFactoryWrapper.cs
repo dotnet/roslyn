48a5908d4d8863d00da3f2eef9fb2bb23cca220d
Allison Chou <allichou@microsoft.com>
2022-02-23 16:08:20 -0800

    Add initial external access files

---
+++ src/Tools/ExternalAccess/Razor/RazorLanguageServerFactoryWrapper.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.LanguageServer;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using StreamJsonRpc;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
+{
+    [Export(typeof(IRazorLanguageServerFactoryWrapper))]
+    [Shared]
+    internal class RazorLanguageServerFactoryWrapper : IRazorLanguageServerFactoryWrapper
+    {
+        private readonly ILanguageServerFactory _languageServerFactory;
+
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        [ImportingConstructor]
+        public RazorLanguageServerFactoryWrapper(ILanguageServerFactory languageServerFactory)
+        {
+            if (languageServerFactory is null)
+            {
+                throw new ArgumentNullException(nameof(languageServerFactory));
+            }
+
+            _languageServerFactory = languageServerFactory;
+        }
+
+        public IRazorLanguageServerTarget Create(JsonRpc jsonRpc, IRazorCapabilitiesProvider razorCapabilitiesProvider)
+        {
+            var capabilitiesProvider = new RazorCapabilitiesProvider(razorCapabilitiesProvider);
+            var languageServer = _languageServerFactory.Create(jsonRpc, capabilitiesProvider, NoOpLspLogger.Instance);
+
+            return new RazorLanguageServerTargetWrapper(languageServer);
+        }
+
+        private class RazorCapabilitiesProvider : ICapabilitiesProvider
+        {
+            private readonly IRazorCapabilitiesProvider _razorCapabilitiesProvider;
+
+            public RazorCapabilitiesProvider(IRazorCapabilitiesProvider razorCapabilitiesProvider)
+            {
+                _razorCapabilitiesProvider = razorCapabilitiesProvider;
+            }
+
+            public ServerCapabilities GetCapabilities(ClientCapabilities clientCapabilities)
+                => _razorCapabilitiesProvider.GetCapabilities(clientCapabilities);
+        }
+    }
+}
