157a91b52e8c640acdd9ed1cc8c8f1567597b3a0
Allison Chou <allichou@microsoft.com>
2022-03-01 11:56:47 -0800

    Add new files

---
+++ src/Tools/ExternalAccess/Razor/RazorTestWorkspaceRegistrationService.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.LanguageServer;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
+{
+    [Export(typeof(RazorTestWorkspaceRegistrationService))]
+    [Export(typeof(LspWorkspaceRegistrationService))]
+    [Shared]
+    internal class RazorTestWorkspaceRegistrationService : LspWorkspaceRegistrationService
+    {
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public RazorTestWorkspaceRegistrationService()
+        {
+        }
+
+        public override string GetHostWorkspaceKind()
+        {
+            return WorkspaceKind.Host;
+        }
+
+        public override void Register(Workspace workspace)
+        {
+            base.Register(workspace);
+        }
+    }
+}
