132d8897c26f40aeb5889e7c2e73b7053542900e
tmat <tomas.matousek@microsoft.com>
2022-03-05 13:47:31 -0800

    DocumentationComments

--- src/EditorFeatures/VisualBasic/DocumentationComments/XmlTagCompletionCommandHandler.vb
+++ src/EditorFeatures/VisualBasic/DocumentationComments/XmlTagCompletionCommandHandler.vb
@@ -5,6 +5,7 @@
 Imports System.ComponentModel.Composition
 Imports System.Diagnostics.CodeAnalysis
 Imports System.Threading
+Imports Microsoft.CodeAnalysis.DocumentationComments
 Imports Microsoft.CodeAnalysis.Editor.Implementation.DocumentationComments
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 Imports Microsoft.VisualStudio.Commanding
