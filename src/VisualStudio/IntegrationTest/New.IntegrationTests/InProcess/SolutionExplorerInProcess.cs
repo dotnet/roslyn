301aacf0ea7ad40ca25f36d00907f037acb02882
Sam Harwell <Sam.Harwell@microsoft.com>
2022-03-07 08:14:11 -0800

    Simplify and document SolutionExplorerInProcess helpers

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/SolutionExplorerInProcess.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/SolutionExplorerInProcess.cs
@@ -395,6 +395,15 @@ namespace Microsoft.VisualStudio.Extensibility.Testing
             return (solutionDirectory, solutionFileFullPath, userOptionsFile);
         }
 
+        /// <returns>
+        /// If <paramref name="waitForBuildToFinish"/> is <see langword="true"/>, returns the build status line, which generally looks something like this:
+        ///
+        /// <code>
+        /// ========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+        /// </code>
+        ///
+        /// Otherwise, this method does not wait for the build to complete and returns <see langword="null"/>.
+        /// </returns>
         public async Task<string?> BuildSolutionAsync(bool waitForBuildToFinish, CancellationToken cancellationToken)
         {
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
@@ -411,32 +420,25 @@ namespace Microsoft.VisualStudio.Extensibility.Testing
             return null;
         }
 
+        /// <inheritdoc cref="WaitForBuildToFinishAsync(IVsOutputWindowPane, CancellationToken)"/>
         public async Task<string> WaitForBuildToFinishAsync(CancellationToken cancellationToken)
         {
             var buildOutputWindowPane = await GetBuildOutputWindowPaneAsync(cancellationToken);
             return await WaitForBuildToFinishAsync(buildOutputWindowPane, cancellationToken);
         }
 
+        /// <returns>
+        /// The summary line for the build, which generally looks something like this:
+        ///
+        /// <code>
+        /// ========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+        /// </code>
+        /// </returns>
         private async Task<string> WaitForBuildToFinishAsync(IVsOutputWindowPane buildOutputWindowPane, CancellationToken cancellationToken)
         {
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
-            var buildManager = await GetRequiredGlobalServiceAsync<SVsSolutionBuildManager, IVsSolutionBuildManager2>(cancellationToken);
-            using var semaphore = new SemaphoreSlim(1);
-            using var solutionEvents = new UpdateSolutionEvents(buildManager);
-
-            await semaphore.WaitAsync(cancellationToken);
-
-            void HandleUpdateSolutionDone(bool succeeded, bool modified, bool canceled) => semaphore.Release();
-            solutionEvents.OnUpdateSolutionDone += HandleUpdateSolutionDone;
-            try
-            {
-                await semaphore.WaitAsync(cancellationToken);
-            }
-            finally
-            {
-                solutionEvents.OnUpdateSolutionDone -= HandleUpdateSolutionDone;
-            }
+            await KnownUIContexts.SolutionExistsAndNotBuildingAndNotDebuggingContext;
 
             // Force the error list to update
             ErrorHandler.ThrowOnFailure(buildOutputWindowPane.FlushToTaskList());
@@ -449,7 +451,8 @@ namespace Microsoft.VisualStudio.Extensibility.Testing
                 return string.Empty;
             }
 
-            return lines[lines.Count - 2].Extent.GetText();
+            // The build summary line should be second to last in the output window
+            return lines[^2].Extent.GetText();
         }
 
         public async Task<IVsOutputWindowPane> GetBuildOutputWindowPaneAsync(CancellationToken cancellationToken)
@@ -562,162 +565,6 @@ namespace Microsoft.VisualStudio.Extensibility.Testing
                     return string.Equals(project.FileName, nameOrFileName, StringComparison.OrdinalIgnoreCase)
                         || string.Equals(project.Name, nameOrFileName, StringComparison.OrdinalIgnoreCase);
                 });
-        }
-
-        internal sealed class UpdateSolutionEvents : IVsUpdateSolutionEvents, IVsUpdateSolutionEvents2, IDisposable
-        {
-            private uint _cookie;
-            private readonly IVsSolutionBuildManager2 _solutionBuildManager;
-
-            internal delegate void UpdateSolutionDoneEvent(bool succeeded, bool modified, bool canceled);
-
-            internal delegate void UpdateSolutionBeginEvent(ref bool cancel);
-
-            internal delegate void UpdateSolutionStartUpdateEvent(ref bool cancel);
-
-            internal delegate void UpdateProjectConfigDoneEvent(IVsHierarchy projectHierarchy, IVsCfg projectConfig, int success);
-
-            internal delegate void UpdateProjectConfigBeginEvent(IVsHierarchy projectHierarchy, IVsCfg projectConfig);
-
-            public event UpdateSolutionDoneEvent? OnUpdateSolutionDone;
-
-            public event UpdateSolutionBeginEvent? OnUpdateSolutionBegin;
-
-            public event UpdateSolutionStartUpdateEvent? OnUpdateSolutionStartUpdate;
-
-            public event Action? OnActiveProjectConfigurationChange;
-
-            public event Action? OnUpdateSolutionCancel;
-
-            public event UpdateProjectConfigDoneEvent? OnUpdateProjectConfigDone;
-
-            public event UpdateProjectConfigBeginEvent? OnUpdateProjectConfigBegin;
-
-            internal UpdateSolutionEvents(IVsSolutionBuildManager2 solutionBuildManager)
-            {
-                ThreadHelper.ThrowIfNotOnUIThread();
-
-                _solutionBuildManager = solutionBuildManager;
-                ErrorHandler.ThrowOnFailure(solutionBuildManager.AdviseUpdateSolutionEvents(this, out _cookie));
-            }
-
-            int IVsUpdateSolutionEvents.UpdateSolution_Begin(ref int pfCancelUpdate)
-            {
-                var cancel = false;
-                OnUpdateSolutionBegin?.Invoke(ref cancel);
-                if (cancel)
-                {
-                    pfCancelUpdate = 1;
-                }
-
-                return 0;
-            }
-
-            int IVsUpdateSolutionEvents.UpdateSolution_Done(int fSucceeded, int fModified, int fCancelCommand)
-            {
-                OnUpdateSolutionDone?.Invoke(fSucceeded != 0, fModified != 0, fCancelCommand != 0);
-                return 0;
-            }
-
-            int IVsUpdateSolutionEvents.UpdateSolution_StartUpdate(ref int pfCancelUpdate)
-            {
-                return UpdateSolution_StartUpdate(ref pfCancelUpdate);
-            }
-
-            int IVsUpdateSolutionEvents.UpdateSolution_Cancel()
-            {
-                OnUpdateSolutionCancel?.Invoke();
-                return 0;
-            }
-
-            int IVsUpdateSolutionEvents.OnActiveProjectCfgChange(IVsHierarchy pIVsHierarchy)
-            {
-                return OnActiveProjectCfgChange();
-            }
-
-            int IVsUpdateSolutionEvents2.UpdateSolution_Begin(ref int pfCancelUpdate)
-            {
-                var cancel = false;
-                OnUpdateSolutionBegin?.Invoke(ref cancel);
-                if (cancel)
-                {
-                    pfCancelUpdate = 1;
-                }
-
-                return 0;
-            }
-
-            int IVsUpdateSolutionEvents2.UpdateSolution_Done(int fSucceeded, int fModified, int fCancelCommand)
-            {
-                OnUpdateSolutionDone?.Invoke(fSucceeded != 0, fModified != 0, fCancelCommand != 0);
-                return 0;
-            }
-
-            int IVsUpdateSolutionEvents2.UpdateSolution_StartUpdate(ref int pfCancelUpdate)
-            {
-                return UpdateSolution_StartUpdate(ref pfCancelUpdate);
-            }
-
-            int IVsUpdateSolutionEvents2.UpdateSolution_Cancel()
-            {
-                OnUpdateSolutionCancel?.Invoke();
-                return 0;
-            }
-
-            int IVsUpdateSolutionEvents2.OnActiveProjectCfgChange(IVsHierarchy pIVsHierarchy)
-            {
-                return OnActiveProjectCfgChange();
-            }
-
-            int IVsUpdateSolutionEvents2.UpdateProjectCfg_Begin(IVsHierarchy pHierProj, IVsCfg pCfgProj, IVsCfg pCfgSln, uint dwAction, ref int pfCancel)
-            {
-                OnUpdateProjectConfigBegin?.Invoke(pHierProj, pCfgProj);
-                return 0;
-            }
-
-            int IVsUpdateSolutionEvents2.UpdateProjectCfg_Done(IVsHierarchy pHierProj, IVsCfg pCfgProj, IVsCfg pCfgSln, uint dwAction, int fSuccess, int fCancel)
-            {
-                OnUpdateProjectConfigDone?.Invoke(pHierProj, pCfgProj, fSuccess);
-                return 0;
-            }
-
-            private int UpdateSolution_StartUpdate(ref int pfCancelUpdate)
-            {
-                var cancel = false;
-                OnUpdateSolutionStartUpdate?.Invoke(ref cancel);
-                if (cancel)
-                {
-                    pfCancelUpdate = 1;
-                }
-
-                return 0;
-            }
-
-            private int OnActiveProjectCfgChange()
-            {
-                OnActiveProjectConfigurationChange?.Invoke();
-                return 0;
-            }
-
-            void IDisposable.Dispose()
-            {
-                ThreadHelper.ThrowIfNotOnUIThread();
-
-                OnUpdateSolutionDone = null;
-                OnUpdateSolutionBegin = null;
-                OnUpdateSolutionStartUpdate = null;
-                OnActiveProjectConfigurationChange = null;
-                OnUpdateSolutionCancel = null;
-                OnUpdateProjectConfigDone = null;
-                OnUpdateProjectConfigBegin = null;
-
-                if (_cookie != 0)
-                {
-                    var tempCookie = _cookie;
-                    _cookie = 0;
-                    ErrorHandler.ThrowOnFailure(_solutionBuildManager.UnadviseUpdateSolutionEvents(tempCookie));
-                }
-            }
         }
     }
 }
