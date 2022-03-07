c2ad9d164175cc2e75689638f71b46bb5410c8c5
tmat <tomas.matousek@microsoft.com>
2022-03-07 09:18:27 -0800

    FIx

--- src/VisualStudio/VisualBasic/Impl/Options/AdvancedOptionPageStrings.vb
+++ src/VisualStudio/VisualBasic/Impl/Options/AdvancedOptionPageStrings.vb
@@ -3,7 +3,6 @@
 ' See the LICENSE file in the project root for more information.
 
 Imports Microsoft.CodeAnalysis.ColorSchemes
-Imports Microsoft.CodeAnalysis.Editor.ColorSchemes
 Imports Microsoft.CodeAnalysis.SolutionCrawler
 
 Namespace Microsoft.VisualStudio.LanguageServices.VisualBasic.Options
