e16ceabbb2e4c1428bf00ca7ba0114c889166b9a
David Wengier <david.wengier@microsoft.com>
2022-03-09 17:20:47 +1100

    Check setting before decompiling in LSP
    I missed this when fixing up the flag here. Sorry David B, you totally called me on this, and I was completely wrong when I answered your comment.

---
+++ src/Features/LanguageServer/Protocol/Features/Options/MetadataAsSourceOptionsStorage.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.Options;
+
+namespace Microsoft.CodeAnalysis.GoToDefinition
+{
+    internal static class GoToDefinitionOptionsStorage
+    {
+        public static bool GetNavigateToDecompiledSources(this IGlobalOptionService globalOptions)
+            => globalOptions.GetOption(NavigateToDecompiledSources);
+
+        public static Option2<bool> NavigateToDecompiledSources =
+            new("FeatureOnOffOptions", "NavigateToDecompiledSources", defaultValue: true,
+                storageLocation: new RoamingProfileStorageLocation($"TextEditor.NavigateToDecompiledSources"));
+    }
+}
