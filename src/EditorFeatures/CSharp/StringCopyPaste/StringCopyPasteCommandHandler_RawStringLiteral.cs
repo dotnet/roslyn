766d4890fb1c9383daf96e7d18d036e0989d74aa
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-03-10 16:03:06 -0800

    move

--- src/EditorFeatures/CSharp/StringCopyPaste/StringCopyPasteCommandHandler_RawStringLiteral.cs
+++ src/EditorFeatures/CSharp/StringCopyPaste/StringCopyPasteCommandHandler_RawStringLiteral.cs
@@ -66,113 +66,8 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.StringCopyPaste
             string newLine,
             string? quotesToAdd)
         {
-            var token = stringExpressionBeforePaste.Token;
-            var text = snapshotBeforePaste.AsText();
-            var endLine = text.Lines.GetLineFromPosition(token.Span.End);
-
-            // The indentation whitespace every line of the final raw string needs.
-            var indentationWhitespace = endLine.GetLeadingWhitespace();
-
-            using var _1 = ArrayBuilder<TextChange>.GetInstance(out var finalTextChanges);
-            using var _2 = PooledStringBuilder.GetInstance(out var buffer);
-
-            if (quotesToAdd != null)
-                finalTextChanges.Add(new TextChange(new TextSpan(stringExpressionBeforePaste.SpanStart, 0), quotesToAdd));
-
-            foreach (var change in snapshotBeforePaste.Version.Changes)
-            {
-                // Create a text object around the change text we're making.  This is a very simple way to get
-                // a nice view of the text lines in the change.
-                var changeText = SourceText.From(change.NewText);
-                buffer.Clear();
-
-                var commonIndentationPrefix = GetCommonIndentationPrefix(changeText);
-
-                for (int i = 0, n = changeText.Lines.Count; i < n; i++)
-                {
-                    var firstLine = i == 0;
-                    var lastLine = i == n - 1;
-
-                    // The actual full line that was pasted in.
-                    var fullInitialLine = changeText.ToString(changeText.Lines[i].SpanIncludingLineBreak);
-                    // The contents of the line, with all leading whitespace removed.
-                    var (lineLeadingWhitespace, lineWithoutLeadingWhitespace) = ExtractWhitespace(fullInitialLine);
-
-                    // This entire if-chain is only concerned with inserting the necessary whitespace a line should have.
-
-                    if (firstLine)
-                    {
-                        // The first line is often special.  It may be copied without any whitespace (e.g. the user
-                        // starts their selection at the start of text on that line, not the start of the line itself).
-                        // So we use some heuristics to try to decide what to do depending on how much whitespace we see
-                        // on that first copied line.
-
-                        text.GetLineAndOffset(change.OldSpan.Start, out var line, out var offset);
-
-                        // First, ensure that the indentation whitespace of the *inserted* first line is sufficient.
-                        if (line == text.Lines.GetLineFromPosition(stringExpressionBeforePaste.SpanStart).LineNumber)
-                        {
-                            // if the first chunk was pasted into the space after the first `"""` then we need to actually
-                            // insert a newline, then the indentation whitespace, then the first line of the change.
-                            buffer.Append(newLine);
-                            buffer.Append(indentationWhitespace);
-                        }
-                        else if (offset < indentationWhitespace.Length)
-                        {
-                            // On the first line, we were pasting into the indentation whitespace.  Ensure we add enough
-                            // whitespace so that the trimmed line starts at an acceptable position.
-                            buffer.Append(indentationWhitespace[offset..]);
-                        }
-
-                        // Now, we want to actually insert any whitespace the line itself should have *if* it's got more
-                        // than the common indentation whitespace.
-                        if (commonIndentationPrefix != null && lineLeadingWhitespace.StartsWith(commonIndentationPrefix))
-                            buffer.Append(lineLeadingWhitespace[commonIndentationPrefix.Length..]);
-                    }
-                    else if (!lastLine && lineWithoutLeadingWhitespace.Length > 0 && SyntaxFacts.IsNewLine(lineWithoutLeadingWhitespace[0]))
-                    {
-                        // if it's just an empty line, don't bother adding any whitespace at all.  This will just end up
-                        // inserting the blank line here.  We don't do this on the last line as we want to still insert
-                        // the right amount of indentation so that the user's caret is placed properly in the text.  We
-                        // could technically not insert the whitespace and attempt to place the caret using a virtual
-                        // position, but this adds a lot of complexity to this system, so we avoid that for now and go
-                        // for the simpler approach..
-                    }
-                    else
-                    {
-                        // On any other line we're adding, ensure we have enough indentation whitespace to proceed.
-                        // Add the necessary whitespace the literal needs, then add the line contents without 
-                        // the common whitespace included.
-                        buffer.Append(indentationWhitespace);
-                        if (commonIndentationPrefix != null)
-                            buffer.Append(lineLeadingWhitespace[commonIndentationPrefix.Length..]);
-                    }
-
-                    // After the necessary whitespace has been added, add the actual non-whitespace contents of the
-                    // line.
-                    buffer.Append(lineWithoutLeadingWhitespace);
-                }
-
-                finalTextChanges.Add(new TextChange(change.OldSpan.ToTextSpan(), buffer.ToString()));
-            }
-
-            if (quotesToAdd != null)
-                finalTextChanges.Add(new TextChange(new TextSpan(stringExpressionBeforePaste.Span.End, 0), quotesToAdd));
-
-            return finalTextChanges.ToImmutable();
-        }
-
-        // Pasting into multi line case.
-
-        private static ImmutableArray<TextChange> GetTextChangesForMultiLineRawStringLiteral(
-            ITextSnapshot snapshotBeforePaste,
-            LiteralExpressionSyntax stringExpressionBeforePaste,
-            string newLine,
-            string? quotesToAdd)
-        {
-            var token = stringExpressionBeforePaste.Token;
-            var text = snapshotBeforePaste.AsText();
-            var endLine = text.Lines.GetLineFromPosition(token.Span.End);
+            var textBeforePaste = snapshotBeforePaste.AsText();
+            var endLine = textBeforePaste.Lines.GetLineFromPosition(stringExpressionBeforePaste.Span.End);
 
             // The indentation whitespace every line of the final raw string needs.
             var indentationWhitespace = endLine.GetLeadingWhitespace();
@@ -215,10 +110,10 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.StringCopyPaste
                         // So we use some heuristics to try to decide what to do depending on how much whitespace we see
                         // on that first copied line.
 
-                        text.GetLineAndOffset(change.OldSpan.Start, out var line, out var offset);
+                        textBeforePaste.GetLineAndOffset(change.OldSpan.Start, out var line, out var offset);
 
                         // First, ensure that the indentation whitespace of the *inserted* first line is sufficient.
-                        if (line == text.Lines.GetLineFromPosition(stringExpressionBeforePaste.SpanStart).LineNumber)
+                        if (line == textBeforePaste.Lines.GetLineFromPosition(stringExpressionBeforePaste.SpanStart).LineNumber)
                         {
                             // if the first chunk was pasted into the space after the first `"""` then we need to actually
                             // insert a newline, then the indentation whitespace, then the first line of the change.
@@ -266,14 +161,138 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.StringCopyPaste
                         // before the last `"""` then we need potentially insert a newline, then enough indentation
                         // whitespace to keep delimiter in the right location.
 
-                        text.GetLineAndOffset(change.OldSpan.End, out var line, out var offset);
+                        textBeforePaste.GetLineAndOffset(change.OldSpan.End, out var line, out var offset);
 
-                        if (line == text.Lines.GetLineFromPosition(stringExpressionBeforePaste.Span.End).LineNumber)
+                        if (line == textBeforePaste.Lines.GetLineFromPosition(stringExpressionBeforePaste.Span.End).LineNumber)
                         {
                             if (currentChangeLine.Span.End == currentChangeLine.SpanIncludingLineBreak.End)
                                 buffer.Append(newLine);
 
-                            buffer.Append(text.ToString(new TextSpan(text.Lines[line].Start, offset)));
+                            buffer.Append(textBeforePaste.ToString(new TextSpan(textBeforePaste.Lines[line].Start, offset)));
+                        }
+                    }
+                }
+
+                finalTextChanges.Add(new TextChange(change.OldSpan.ToTextSpan(), buffer.ToString()));
+            }
+
+            if (quotesToAdd != null)
+                finalTextChanges.Add(new TextChange(new TextSpan(stringExpressionBeforePaste.Span.End, 0), quotesToAdd));
+
+            return finalTextChanges.ToImmutable();
+        }
+
+        // Pasting into multi line case.
+
+        private static ImmutableArray<TextChange> GetTextChangesForMultiLineRawStringLiteral(
+            ITextSnapshot snapshotBeforePaste,
+            LiteralExpressionSyntax stringExpressionBeforePaste,
+            string newLine,
+            string? quotesToAdd)
+        {
+            var textBeforePaste = snapshotBeforePaste.AsText();
+            var endLine = textBeforePaste.Lines.GetLineFromPosition(stringExpressionBeforePaste.Span.End);
+
+            // The indentation whitespace every line of the final raw string needs.
+            var indentationWhitespace = endLine.GetLeadingWhitespace();
+
+            using var _1 = ArrayBuilder<TextChange>.GetInstance(out var finalTextChanges);
+            using var _2 = PooledStringBuilder.GetInstance(out var buffer);
+
+            if (quotesToAdd != null)
+                finalTextChanges.Add(new TextChange(new TextSpan(stringExpressionBeforePaste.SpanStart, 0), quotesToAdd));
+
+            for (var i = 0; i < snapshotBeforePaste.Version.Changes.Count; i++)
+            {
+                var change = snapshotBeforePaste.Version.Changes[i];
+
+                // Create a text object around the change text we're making.  This is a very simple way to get
+                // a nice view of the text lines in the change.
+                var changeText = SourceText.From(change.NewText);
+                buffer.Clear();
+
+                var commonIndentationPrefix = GetCommonIndentationPrefix(changeText);
+
+                for (var j = 0; j < changeText.Lines.Count; j++)
+                {
+                    var firstChange = i == 0 && j == 0;
+                    var lastChange = (i == snapshotBeforePaste.Version.Changes.Count - 1) &&
+                                     (j == changeText.Lines.Count - 1);
+
+                    // The actual full line that was pasted in.
+                    var currentChangeLine = changeText.Lines[j];
+                    var fullChangeLineText = changeText.ToString(currentChangeLine.SpanIncludingLineBreak);
+                    // The contents of the line, with all leading whitespace removed.
+                    var (lineLeadingWhitespace, lineWithoutLeadingWhitespace) = ExtractWhitespace(fullChangeLineText);
+
+                    // This entire if-chain is only concerned with inserting the necessary whitespace a line should have.
+
+                    if (firstChange)
+                    {
+                        // The first line is often special.  It may be copied without any whitespace (e.g. the user
+                        // starts their selection at the start of text on that line, not the start of the line itself).
+                        // So we use some heuristics to try to decide what to do depending on how much whitespace we see
+                        // on that first copied line.
+
+                        textBeforePaste.GetLineAndOffset(change.OldSpan.Start, out var line, out var offset);
+
+                        // First, ensure that the indentation whitespace of the *inserted* first line is sufficient.
+                        if (line == textBeforePaste.Lines.GetLineFromPosition(stringExpressionBeforePaste.SpanStart).LineNumber)
+                        {
+                            // if the first chunk was pasted into the space after the first `"""` then we need to actually
+                            // insert a newline, then the indentation whitespace, then the first line of the change.
+                            buffer.Append(newLine);
+                            buffer.Append(indentationWhitespace);
+                        }
+                        else if (offset < indentationWhitespace.Length)
+                        {
+                            // On the first line, we were pasting into the indentation whitespace.  Ensure we add enough
+                            // whitespace so that the trimmed line starts at an acceptable position.
+                            buffer.Append(indentationWhitespace[offset..]);
+                        }
+
+                        // Now, we want to actually insert any whitespace the line itself should have *if* it's got more
+                        // than the common indentation whitespace.
+                        if (commonIndentationPrefix != null && lineLeadingWhitespace.StartsWith(commonIndentationPrefix))
+                            buffer.Append(lineLeadingWhitespace[commonIndentationPrefix.Length..]);
+                    }
+                    else if (!lastChange && lineWithoutLeadingWhitespace.Length > 0 && SyntaxFacts.IsNewLine(lineWithoutLeadingWhitespace[0]))
+                    {
+                        // if it's just an empty line, don't bother adding any whitespace at all.  This will just end up
+                        // inserting the blank line here.  We don't do this on the last line as we want to still insert
+                        // the right amount of indentation so that the user's caret is placed properly in the text.  We
+                        // could technically not insert the whitespace and attempt to place the caret using a virtual
+                        // position, but this adds a lot of complexity to this system, so we avoid that for now and go
+                        // for the simpler approach..
+                    }
+                    else
+                    {
+                        // On any other line we're adding, ensure we have enough indentation whitespace to proceed.
+                        // Add the necessary whitespace the literal needs, then add the line contents without 
+                        // the common whitespace included.
+                        buffer.Append(indentationWhitespace);
+                        if (commonIndentationPrefix != null)
+                            buffer.Append(lineLeadingWhitespace[commonIndentationPrefix.Length..]);
+                    }
+
+                    // After the necessary whitespace has been added, add the actual non-whitespace contents of the
+                    // line.
+                    buffer.Append(lineWithoutLeadingWhitespace);
+
+                    if (lastChange)
+                    {
+                        // Similar to the check we do for the first-change, if the last change was pasted into the space
+                        // before the last `"""` then we need potentially insert a newline, then enough indentation
+                        // whitespace to keep delimiter in the right location.
+
+                        textBeforePaste.GetLineAndOffset(change.OldSpan.End, out var line, out var offset);
+
+                        if (line == textBeforePaste.Lines.GetLineFromPosition(stringExpressionBeforePaste.Span.End).LineNumber)
+                        {
+                            if (currentChangeLine.Span.End == currentChangeLine.SpanIncludingLineBreak.End)
+                                buffer.Append(newLine);
+
+                            buffer.Append(textBeforePaste.ToString(new TextSpan(textBeforePaste.Lines[line].Start, offset)));
                         }
                     }
                 }
