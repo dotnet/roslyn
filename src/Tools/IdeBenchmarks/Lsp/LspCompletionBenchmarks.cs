d564dbec5aaaff89f73485a6edd4cab0dd41fdd0
David Barbet <dabarbet@microsoft.com>
2022-03-17 12:33:31 -0700

    cleanup

--- src/Tools/IdeBenchmarks/Lsp/LspCompletionBenchmarks.cs
+++ src/Tools/IdeBenchmarks/Lsp/LspCompletionBenchmarks.cs
@@ -2,29 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Classification;
-using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.LanguageServer;
 using Microsoft.CodeAnalysis.LanguageServer.UnitTests.Completion;
-using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.CodeAnalysis.Options;
-using Microsoft.CodeAnalysis.PooledObjects;
-using Microsoft.CodeAnalysis.Shared.Extensions;
-using Microsoft.CodeAnalysis.Storage;
 using Microsoft.CodeAnalysis.Test.Utilities;
-using Microsoft.CodeAnalysis.Text;
 using Roslyn.Test.Utilities;
 using Xunit;
 using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
@@ -36,9 +20,9 @@ namespace IdeBenchmarks.Lsp
     {
         private readonly UseExportProviderAttribute _useExportProviderAttribute = new UseExportProviderAttribute();
 
-        TestLspServer _testServer;
-        IGlobalOptionService _globalOptionService;
-        LSP.CompletionParams _completionParams;
+        private TestLspServer? _testServer;
+        private IGlobalOptionService? _globalOptionService;
+        private LSP.CompletionParams? _completionParams;
 
         [GlobalSetup]
         public void GlobalSetup()
@@ -86,8 +70,7 @@ class A
         T{|caret:|}
     }
 }";
-            var testServer = await CreateTestLspServerAsync(markup).ConfigureAwait(false);
-            _testServer = testServer;
+            _testServer = await CreateTestLspServerAsync(markup).ConfigureAwait(false);
 
             _completionParams = CreateCompletionParams(
                 _testServer.GetLocations("caret").Single(),
@@ -95,41 +78,27 @@ class A
                 triggerCharacter: "T",
                 triggerKind: LSP.CompletionTriggerKind.Invoked);
 
-            _globalOptionService = testServer.TestWorkspace.GetService<IGlobalOptionService>();
+            _globalOptionService = _testServer.TestWorkspace.GetService<IGlobalOptionService>();
         }
 
         [Benchmark]
         public void GetCompletionsWithTextEdits()
         {
-            _globalOptionService.SetGlobalOption(new OptionKey(LspOptions.LspCompletionFeatureFlag), true);
+            _globalOptionService!.SetGlobalOption(new OptionKey(LspOptions.LspCompletionFeatureFlag), true);
 
-            var results = CompletionTests.RunGetCompletionsAsync(_testServer, _completionParams).Result;
+            var results = CompletionTests.RunGetCompletionsAsync(_testServer!, _completionParams!).Result;
             Assert.Equal(1000, results.Items.Length);
             Assert.True(results.IsIncomplete);
-            Assert.All(results.Items, (item) => AssertTextEditOrComplexItem(item));
         }
 
         [Benchmark]
         public void GetCompletionsWithoutTextEdits()
         {
-            _globalOptionService.SetGlobalOption(new OptionKey(LspOptions.LspCompletionFeatureFlag), false);
+            _globalOptionService!.SetGlobalOption(new OptionKey(LspOptions.LspCompletionFeatureFlag), false);
 
-            var results = CompletionTests.RunGetCompletionsAsync(_testServer, _completionParams).Result;
+            var results = CompletionTests.RunGetCompletionsAsync(_testServer!, _completionParams!).Result;
             Assert.Equal(1000, results.Items.Length);
             Assert.True(results.IsIncomplete);
-            Assert.All(results.Items, (item) => Assert.Null(item.TextEdit));
-        }
-
-        private void AssertTextEditOrComplexItem(LSP.CompletionItem item)
-        {
-            if (item.TextEdit == null)
-            {
-                Assert.Null(item.InsertText);
-            }
-            else
-            {
-                Assert.NotNull(item.TextEdit);
-            }
         }
 
         [IterationCleanup]
