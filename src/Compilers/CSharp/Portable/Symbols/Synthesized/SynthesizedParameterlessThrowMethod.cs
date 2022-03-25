5a8dc97f4005b8b5ea6c088b1c56694b3505a7fe
Youssef Victor <youssefvictor00@gmail.com>
2022-03-25 21:59:57 +0200

    Use synthesized methods for compiler-generated switch exceptions (#59975)

--- src/Compilers/CSharp/Portable/Symbols/Synthesized/SynthesizedParameterlessThrowMethod.cs
+++ src/Compilers/CSharp/Portable/Symbols/Synthesized/SynthesizedParameterlessThrowMethod.cs
@@ -8,14 +8,17 @@ using Microsoft.CodeAnalysis.CodeGen;
 namespace Microsoft.CodeAnalysis.CSharp.Symbols
 {
     /// <summary>
-    /// Throws a System.ArgumentNullException with the given 'paramName'.
+    /// Throws an exception of a given type using a parameterless constructor.
     /// </summary>
-    internal sealed class SynthesizedThrowMethod : SynthesizedGlobalMethodSymbol
+    internal sealed class SynthesizedParameterlessThrowMethod : SynthesizedGlobalMethodSymbol
     {
-        internal SynthesizedThrowMethod(SourceModuleSymbol containingModule, PrivateImplementationDetails privateImplType, TypeSymbol returnType, TypeSymbol paramType)
-            : base(containingModule, privateImplType, returnType, PrivateImplementationDetails.SynthesizedThrowFunctionName)
+        private readonly MethodSymbol _exceptionConstructor;
+
+        internal SynthesizedParameterlessThrowMethod(SourceModuleSymbol containingModule, PrivateImplementationDetails privateImplType, TypeSymbol returnType, string synthesizedMethodName, MethodSymbol exceptionConstructor)
+            : base(containingModule, privateImplType, returnType, synthesizedMethodName)
         {
-            this.SetParameters(ImmutableArray.Create<ParameterSymbol>(SynthesizedParameterSymbol.Create(this, TypeWithAnnotations.Create(paramType), 0, RefKind.None, "paramName")));
+            _exceptionConstructor = exceptionConstructor;
+            this.SetParameters(ImmutableArray<ParameterSymbol>.Empty);
         }
 
         internal override void GenerateMethodBody(TypeCompilationState compilationState, BindingDiagnosticBag diagnostics)
@@ -25,11 +28,9 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
 
             try
             {
-                ParameterSymbol paramName = this.Parameters[0];
+                //throw new GivenException();
 
-                //throw new ArgumentNullException(paramName);
-
-                var body = F.Throw(F.New(F.WellKnownMethod(WellKnownMember.System_ArgumentNullException__ctorString), ImmutableArray.Create<BoundExpression>(F.Parameter(paramName))));
+                var body = F.Throw(F.New(_exceptionConstructor, ImmutableArray<BoundExpression>.Empty));
 
                 // NOTE: we created this block in its most-lowered form, so analysis is unnecessary
                 F.CloseMethod(body);
