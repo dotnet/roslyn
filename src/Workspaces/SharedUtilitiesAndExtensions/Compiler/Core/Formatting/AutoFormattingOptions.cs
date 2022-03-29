e8660e4e900f0cd1325869b39c1f493930d51467
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-03-29 11:19:35 -0700

    move some code to workspace layer

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Formatting/AutoFormattingOptions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Formatting/AutoFormattingOptions.cs
@@ -6,67 +6,14 @@ using System;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Options;
 
-#if !CODE_STYLE
-using System.Composition;
-using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.Options.Providers;
-#endif
-
 namespace Microsoft.CodeAnalysis.Formatting
 {
     /// <summary>
-    /// Solution-wide formatting options.
+    /// Solution-wide format-on-type options.
     /// </summary>
-    internal readonly record struct AutoFormattingOptions(
+    internal readonly partial record struct AutoFormattingOptions(
         bool FormatOnReturn,
         bool FormatOnTyping,
         bool FormatOnSemicolon,
-        bool FormatOnCloseBrace)
-    {
-#if !CODE_STYLE
-        public static AutoFormattingOptions From(Project project)
-            => From(project.Solution.Options, project.Language);
-
-        public static AutoFormattingOptions From(OptionSet options, string language)
-            => new(
-                FormatOnReturn: options.GetOption(Metadata.AutoFormattingOnReturn, language),
-                FormatOnTyping: options.GetOption(Metadata.AutoFormattingOnTyping, language),
-                FormatOnSemicolon: options.GetOption(Metadata.AutoFormattingOnSemicolon, language),
-                FormatOnCloseBrace: options.GetOption(Metadata.AutoFormattingOnCloseBrace, language));
-
-        [ExportSolutionOptionProvider, Shared]
-        internal sealed class Metadata : IOptionProvider
-        {
-            [ImportingConstructor]
-            [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-            public Metadata()
-            {
-            }
-
-            public ImmutableArray<IOption> Options { get; } = ImmutableArray.Create<IOption>(
-                AutoFormattingOnReturn,
-                AutoFormattingOnTyping,
-                AutoFormattingOnSemicolon,
-                AutoFormattingOnCloseBrace);
-
-            private const string FeatureName = "FormattingOptions";
-
-            internal static readonly PerLanguageOption2<bool> AutoFormattingOnReturn =
-                new(FeatureName, OptionGroup.Default, nameof(AutoFormattingOnReturn), defaultValue: true,
-                storageLocation: new RoamingProfileStorageLocation("TextEditor.%LANGUAGE%.Specific.Auto Formatting On Return"));
-
-            public static readonly PerLanguageOption2<bool> AutoFormattingOnTyping =
-                new(FeatureName, OptionGroup.Default, nameof(AutoFormattingOnTyping), defaultValue: true,
-                storageLocation: new RoamingProfileStorageLocation("TextEditor.%LANGUAGE%.Specific.Auto Formatting On Typing"));
-
-            public static readonly PerLanguageOption2<bool> AutoFormattingOnSemicolon =
-                new(FeatureName, OptionGroup.Default, nameof(AutoFormattingOnSemicolon), defaultValue: true,
-                storageLocation: new RoamingProfileStorageLocation("TextEditor.%LANGUAGE%.Specific.Auto Formatting On Semicolon"));
-
-            public static readonly PerLanguageOption2<bool> AutoFormattingOnCloseBrace = new(
-                "BraceCompletionOptions", nameof(AutoFormattingOnCloseBrace), defaultValue: true,
-                storageLocation: new RoamingProfileStorageLocation("TextEditor.%LANGUAGE%.Specific.Auto Formatting On Close Brace"));
-        }
-#endif
-    }
+        bool FormatOnCloseBrace);
 }
