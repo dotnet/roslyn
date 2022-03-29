462d5ee143a1838498de4cb358171b0446c27802
Sam Harwell <Sam.Harwell@microsoft.com>
2022-03-29 14:58:19 -0700

    Handle CompletedWithoutData state for light bulb
    Closes #60463

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/LightBulbHelper.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/LightBulbHelper.cs
@@ -61,6 +61,8 @@ namespace Roslyn.VisualStudio.IntegrationTests.InProcess
 
                 if (e.Status == QuerySuggestedActionCompletionStatus.Completed)
                     tcs.SetResult(e.ActionSets.ToList());
+                else if (e.Status == QuerySuggestedActionCompletionStatus.CompletedWithoutData)
+                    tcs.SetResult(new List<SuggestedActionSet>());
                 else if (e.Status == QuerySuggestedActionCompletionStatus.Canceled)
                     tcs.TrySetCanceled();
                 else
