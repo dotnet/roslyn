c8876af06b9acfadaa660ebf8894ab530f6cadfc
Youssef Victor <youssefvictor00@gmail.com>
2022-04-04 22:13:13 +0200

    Fix formatting

--- src/Analyzers/CSharp/CodeFixes/UseIsNullCheck/CSharpUseNullCheckOverTypeCheckCodeFixProvider.cs
+++ src/Analyzers/CSharp/CodeFixes/UseIsNullCheck/CSharpUseNullCheckOverTypeCheckCodeFixProvider.cs
@@ -40,7 +40,7 @@ namespace Microsoft.CodeAnalysis.CSharp.UseIsNullCheck
             var diagnostic = context.Diagnostics.First();
             context.RegisterCodeFix(
                 CodeAction.Create(
-                    CSharpAnalyzersResources.Prefer_null_check_over_type_check, 
+                    CSharpAnalyzersResources.Prefer_null_check_over_type_check,
                     c => FixAsync(context.Document, diagnostic, c),
                     nameof(CSharpAnalyzersResources.Prefer_null_check_over_type_check)),
                 context.Diagnostics);
