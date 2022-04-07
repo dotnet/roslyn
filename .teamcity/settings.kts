1643d4c7210eacc4498fc0d4f7bb85388b823c17
Jan Hlavac <jan@postsharp.net>
2022-04-07 15:38:44 +0200

    Version bump, TC settings update

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,12 +3,13 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
 // Both Swabra and swabra need to be imported
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.sshAgent
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.Swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.*
 
-version = "2019.2"
+version = "2021.2"
 
 project {
 
@@ -55,6 +56,8 @@ object DebugBuild : BuildType({
         vcs {
             watchChangesInDependencies = true
             branchFilter = "+:<default>"
+            // Build will not trigger automatically if the commit message contains comment value.
+            triggerRules = "-:comment=<<VERSION_BUMP>>:**"
         }        
 
     }
