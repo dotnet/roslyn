a99a5859ef81e42ef93c67ba72839a1f9dfbd461
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-04-07 15:58:52 -0700

    Lint

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Services/BlockFacts/IBlockFactsExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Services/BlockFacts/IBlockFactsExtensions.cs
@@ -2,12 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-
-#if CODE_STYLE
-#else
-using Microsoft.CodeAnalysis.Editing;
-#endif
-
 namespace Microsoft.CodeAnalysis.LanguageServices
 {
     internal static class IBlockFactsExtensions
