80b20adba9e2c65b64d618b5dbbc3b09420842b4
David Wengier <david.wengier@microsoft.com>
2022-04-09 21:25:12 +1000

    Correctness

--- src/Analyzers/CSharp/Tests/UseUTF8StringLiteral/UseUTF8StringLiteralTests.cs
+++ src/Analyzers/CSharp/Tests/UseUTF8StringLiteral/UseUTF8StringLiteralTests.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -853,7 +853,6 @@ public class C
                 LanguageVersion = LanguageVersion.Preview
             }.RunAsync();
         }
-
 
         [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsUseUTF8StringLiteral)]
         public async Task TestParamArray4()
