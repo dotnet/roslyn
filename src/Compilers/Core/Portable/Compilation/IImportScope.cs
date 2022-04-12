7a5423d7639d35fee5dc83310b7e0ce69d6baa3c
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-04-12 15:01:06 -0700

    Fix API

--- src/Compilers/Core/Portable/Compilation/IImportScope.cs
+++ src/Compilers/Core/Portable/Compilation/IImportScope.cs
@@ -11,8 +11,12 @@ namespace Microsoft.CodeAnalysis
     /// Represents a chain of symbols that are imported to a particular position in a source file.  Symbols may be
     /// imported, but may not necessarily be available at that location (for example, an alias symbol hidden by another
     /// symbol).  There is no guarantee that the same chain will be returned from successive calls to <see
-    /// cref="SemanticModel.GetImportScopes"/>.  Each symbol or xml-namespace import has a reference to the location the
-    /// import directive was declared at.
+    /// cref="SemanticModel.GetImportScopes"/>.  Each import has a reference to the location the import directive was
+    /// declared at.  For the <see cref="IAliasSymbol"/> import, the location can be found using either <see
+    /// cref="ISymbol.Locations"/> or <see cref="ISymbol.DeclaringSyntaxReferences"/> on the <see cref="IAliasSymbol"/>
+    /// itself.  For <see cref="Imports"/> or <see cref="XmlNamespaces"/> the location is found through <see
+    /// cref="ImportedNamespaceOrType.DeclaringSyntaxReference"/> or <see
+    /// cref="ImportedXmlNamespace.DeclaringSyntaxReference"/> respectively.
     /// </summary>
     /// <remarks>
     /// <list type="bullet">
@@ -35,33 +39,57 @@ namespace Microsoft.CodeAnalysis
         /// Aliases defined at this level of the chain.  This corresponds to <c>using X = TypeOrNamespace;</c> in C# or
         /// <c>Imports X = TypeOrNamespace</c> in Visual Basic.
         /// </summary>
-        ImmutableArray<(IAliasSymbol alias, SyntaxReference declaratingSyntaxReference)> Aliases { get; }
+        ImmutableArray<IAliasSymbol> Aliases { get; }
 
         /// <summary>
         /// Extern aliases defined at this level of the chain.  This corresponds to <c>extern alias X;</c> in C#.  It
         /// will be empty in Visual Basic.
         /// </summary>
-        ImmutableArray<(IAliasSymbol alias, SyntaxReference declaratingSyntaxReference)> ExternAliases { get; }
+        ImmutableArray<IAliasSymbol> ExternAliases { get; }
 
         /// <summary>
         /// Types or namespaces imported at this level of the chain.  This corresponds to <c>using Namespace;</c> or
         /// <c>using static Type;</c> in C#, or <c>Imports TypeOrNamespace</c> in Visual Basic.
         /// </summary>
-        ImmutableArray<(INamespaceOrTypeSymbol namespaceOrType, SyntaxReference declaratingSyntaxReference)> Imports { get; }
+        ImmutableArray<ImportedNamespaceOrType> Imports { get; }
 
         /// <summary>
         /// Xml namespaces imported at this level of the chain.  This corresponds to <c>Imports &lt;xmlns:prefix =
         /// "name"&gt;</c> in Visual Basic.  It will be empty in C#.
         /// </summary>
-        ImmutableArray<(string name, SyntaxReference declaratingSyntaxReference)> XmlNamespaces { get; }
+        ImmutableArray<ImportedXmlNamespace> XmlNamespaces { get; }
+    }
+
+    public readonly struct ImportedNamespaceOrType
+    {
+        public INamespaceOrTypeSymbol NamespaceOrType { get; }
+        public SyntaxReference? DeclaringSyntaxReference { get; }
+
+        internal ImportedNamespaceOrType(INamespaceOrTypeSymbol namespaceOrType, SyntaxReference? declaringSyntaxReference)
+        {
+            NamespaceOrType = namespaceOrType;
+            DeclaringSyntaxReference = declaringSyntaxReference;
+        }
+    }
+
+    public readonly struct ImportedXmlNamespace
+    {
+        public string XmlNamespace { get; }
+        public SyntaxReference? DeclaringSyntaxReference { get; }
+
+        internal ImportedXmlNamespace(string xmlNamespace, SyntaxReference? declaringSyntaxReference)
+        {
+            XmlNamespace = xmlNamespace;
+            DeclaringSyntaxReference = declaringSyntaxReference;
+        }
     }
 
     /// <summary>
     /// Simple POCO implementation of the import scope, usable by both C# and VB.
     /// </summary>
     internal sealed record SimpleImportScope(
-        ImmutableArray<(IAliasSymbol alias, SyntaxReference declaratingSyntaxReference)> Aliases,
-        ImmutableArray<(IAliasSymbol alias, SyntaxReference declaratingSyntaxReference)> ExternAliases,
-        ImmutableArray<(INamespaceOrTypeSymbol namespaceOrType, SyntaxReference declaratingSyntaxReference)> Imports,
-        ImmutableArray<(string name, SyntaxReference declaratingSyntaxReference)> XmlNamespaces) : IImportScope;
+        ImmutableArray<IAliasSymbol> Aliases,
+        ImmutableArray<IAliasSymbol> ExternAliases,
+        ImmutableArray<ImportedNamespaceOrType> Imports,
+        ImmutableArray<ImportedXmlNamespace> XmlNamespaces) : IImportScope;
 }
