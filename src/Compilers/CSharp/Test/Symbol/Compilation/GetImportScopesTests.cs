784355d9cd78643852f1d2322c29425d7a9876e4
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-04-18 12:50:27 -0700

    Add tests

---
+++ src/Compilers/CSharp/Test/Symbol/Compilation/GetImportScopesTests.cs
@@ -0,0 +1,114 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests
+{
+    public class GetImportScopesTests : SemanticModelTestBase
+    {
+        [Fact]
+        public void TestEmptyFile()
+        {
+            var text = @"/*pos*/";
+            var tree = Parse(text);
+            var comp = CreateCompilation(tree);
+            var model = comp.GetSemanticModel(tree);
+            var scopes = model.GetImportScopes(GetPositionForBinding(text));
+            Assert.Empty(scopes);
+        }
+
+        [Fact]
+        public void TestNoImportsBeforeMemberDeclaration()
+        {
+            var text = @"/*pos*/
+class C {}";
+            var tree = Parse(text);
+            var comp = CreateCompilation(tree);
+            var model = comp.GetSemanticModel(tree);
+            var scopes = model.GetImportScopes(GetPositionForBinding(text));
+            Assert.Empty(scopes);
+        }
+
+        [Fact]
+        public void TestBeforeImports()
+        {
+            var text = @"/*pos*/
+using System;";
+            var tree = Parse(text);
+            var comp = CreateCompilation(tree);
+            var model = comp.GetSemanticModel(tree);
+            var scopes = model.GetImportScopes(GetPositionForBinding(text));
+            Assert.Single(scopes);
+            Assert.Single(scopes.Single().Imports);
+        }
+
+        [Fact]
+        public void TestAfterImportsNoContent()
+        {
+            var text = @"
+using System;
+/*pos*/";
+            var tree = Parse(text);
+            var comp = CreateCompilation(tree);
+            var model = comp.GetSemanticModel(tree);
+            var scopes = model.GetImportScopes(GetPositionForBinding(text));
+            Assert.Single(scopes);
+            Assert.Single(scopes.Single().Imports);
+        }
+
+        [Fact]
+        public void TestAfterImportsBeforeMemberDeclaration()
+        {
+            var text = @"
+using System;
+/*pos*/
+class C
+{
+}";
+            var tree = Parse(text);
+            var comp = CreateCompilation(tree);
+            var model = comp.GetSemanticModel(tree);
+            var scopes = model.GetImportScopes(GetPositionForBinding(text));
+            Assert.Single(scopes);
+            Assert.Single(scopes.Single().Imports);
+        }
+
+        [Fact]
+        public void TestBeforeImportsTopLevelStatements()
+        {
+            var text = @"
+/*pos*/
+using System;
+
+return;";
+            var tree = Parse(text);
+            var comp = CreateCompilation(tree);
+            var model = comp.GetSemanticModel(tree);
+            var scopes = model.GetImportScopes(GetPositionForBinding(text));
+            Assert.Single(scopes);
+            Assert.Single(scopes.Single().Imports);
+        }
+
+        [Fact]
+        public void TestAfterImportsTopLevelStatements()
+        {
+            var text = @"
+using System;
+/*pos*/
+return;";
+            var tree = Parse(text);
+            var comp = CreateCompilation(tree);
+            var model = comp.GetSemanticModel(tree);
+            var scopes = model.GetImportScopes(GetPositionForBinding(text));
+            Assert.Single(scopes);
+            Assert.Single(scopes.Single().Imports);
+        }
+    }
+}
