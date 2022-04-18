53f7271fd9af225adaf761819ce84f21c2706629
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-04-18 12:53:57 -0700

    Add tests

--- src/Compilers/CSharp/Test/Symbol/Compilation/GetImportScopesTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Compilation/GetImportScopesTests.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.CSharp.UnitTests
@@ -47,6 +48,8 @@ using System;";
             var scopes = model.GetImportScopes(GetPositionForBinding(text));
             Assert.Single(scopes);
             Assert.Single(scopes.Single().Imports);
+            Assert.True(scopes.Single().Imports.Single().NamespaceOrType is INamespaceSymbol { ContainingNamespace.IsGlobalNamespace: true, Name: nameof(System) });
+            Assert.True(scopes.Single().Imports.Single().DeclaringSyntaxReference!.GetSyntax() is UsingDirectiveSyntax);
         }
 
         [Fact]
@@ -61,6 +64,8 @@ using System;
             var scopes = model.GetImportScopes(GetPositionForBinding(text));
             Assert.Single(scopes);
             Assert.Single(scopes.Single().Imports);
+            Assert.True(scopes.Single().Imports.Single().NamespaceOrType is INamespaceSymbol { ContainingNamespace.IsGlobalNamespace: true, Name: nameof(System) });
+            Assert.True(scopes.Single().Imports.Single().DeclaringSyntaxReference!.GetSyntax() is UsingDirectiveSyntax);
         }
 
         [Fact]
@@ -76,8 +81,7 @@ class C
             var comp = CreateCompilation(tree);
             var model = comp.GetSemanticModel(tree);
             var scopes = model.GetImportScopes(GetPositionForBinding(text));
-            Assert.Single(scopes);
-            Assert.Single(scopes.Single().Imports);
+            Assert.Empty(scopes);
         }
 
         [Fact]
@@ -94,10 +98,12 @@ return;";
             var scopes = model.GetImportScopes(GetPositionForBinding(text));
             Assert.Single(scopes);
             Assert.Single(scopes.Single().Imports);
+            Assert.True(scopes.Single().Imports.Single().NamespaceOrType is INamespaceSymbol { ContainingNamespace.IsGlobalNamespace: true, Name: nameof(System) });
+            Assert.True(scopes.Single().Imports.Single().DeclaringSyntaxReference!.GetSyntax() is UsingDirectiveSyntax);
         }
 
         [Fact]
-        public void TestAfterImportsTopLevelStatements()
+        public void TestAfterImportsTopLevelStatements1()
         {
             var text = @"
 using System;
@@ -107,8 +113,24 @@ return;";
             var comp = CreateCompilation(tree);
             var model = comp.GetSemanticModel(tree);
             var scopes = model.GetImportScopes(GetPositionForBinding(text));
+            Assert.Empty(scopes);
+        }
+
+        [Fact]
+        public void TestAfterImportsTopLevelStatements2()
+        {
+            var text = @"
+using System;
+
+return /*pos*/;";
+            var tree = Parse(text);
+            var comp = CreateCompilation(tree);
+            var model = comp.GetSemanticModel(tree);
+            var scopes = model.GetImportScopes(GetPositionForBinding(text));
             Assert.Single(scopes);
             Assert.Single(scopes.Single().Imports);
+            Assert.True(scopes.Single().Imports.Single().NamespaceOrType is INamespaceSymbol { ContainingNamespace.IsGlobalNamespace: true, Name: nameof(System) });
+            Assert.True(scopes.Single().Imports.Single().DeclaringSyntaxReference!.GetSyntax() is UsingDirectiveSyntax);
         }
     }
 }
