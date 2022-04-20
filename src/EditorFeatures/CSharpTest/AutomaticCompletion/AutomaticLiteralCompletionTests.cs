dotnet/roslyn

commit a838dcfc37a7f22018fddcf7d36b241254f14d58

Don't complete quotes if they complete a valid string (#60227)

src/Features/CSharp/Portable/BraceCompletion/StringLiteralBraceCompletionService.cs
@@ -7,6 +7,7 @@ using System.Composition;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.BraceCompletion;
+using Microsoft.CodeAnalysis.CSharp.Extensions;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.LanguageServices;
 using Microsoft.CodeAnalysis.Shared.Extensions;
@@ -53,6 +54,27 @@ namespace Microsoft.CodeAnalysis.CSharp.BraceCompletion
                 return SpecializedTasks.False;
             }
 
+            // If the single token that the user typed is a string literal that is more than just
+            // the one double quote character they typed, and the line doesn't have errors, then
+            // it means it is completing an existing token, from the start. For example given:
+            //
+            // var s = "te$$st";
+            //
+            // When the user types `" + "` to split the string into two literals, the first
+            // quote won't be completed (because its in a string literal), and with this check
+            // the second quote won't either.
+            //
+            // We don't do this optimization for verbatim strings because they are multi-line so
+            // the flow on effects from us getting it wrong are much greater, and it can really change
+            // the tree.
+            if (token.IsKind(SyntaxKind.StringLiteralToken) &&
+                !token.IsVerbatimStringLiteral() &&
+                token.Span.Length > 1 &&
+                !RestOfLineContainsDiagnostics(token))
+            {
+                return SpecializedTasks.False;
+            }
+
             if (token.SpanStart == position)
             {
                 return SpecializedTasks.True;
@@ -62,6 +84,22 @@ namespace Microsoft.CodeAnalysis.CSharp.BraceCompletion
             // doesn't match the position.  Check if we're in a verbatim string token @" where the token span start
             // is the @ character and the " is one past the token start.
             return Task.FromResult(token.SpanStart + 1 == position && token.IsVerbatimStringLiteral());
+        }
+
+        private static bool RestOfLineContainsDiagnostics(SyntaxToken token)
+        {
+            while (!token.TrailingTrivia.Contains(t => t.IsEndOfLine()))
+            {
+                if (token.ContainsDiagnostics)
+                    return true;
+
+                token = token.GetNextToken();
+            }
+
+            if (token.ContainsDiagnostics)
+                return true;
+
+            return false;
         }
     }
 }

src/EditorFeatures/CSharpTest/AutomaticCompletion/AutomaticLiteralCompletionTests.cs
@@ -429,6 +429,71 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.AutomaticCompletion
             CheckStart(session.Session, expectValidSession: false);
         }
 
+        [WpfFact, Trait(Traits.Feature, Traits.Features.AutomaticCompletion)]
+        [WorkItem(59178, "https://github.com/dotnet/roslyn/issues/59178")]
+        public void String_CompleteLiteral()
+        {
+            var code = @"class C
+{
+    void Method()
+    {
+        var s = ""this"" + $$that"";
+    }
+}";
+            using var session = CreateSessionDoubleQuote(code);
+            Assert.NotNull(session);
+            CheckStart(session.Session, expectValidSession: false);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.AutomaticCompletion)]
+        [WorkItem(59178, "https://github.com/dotnet/roslyn/issues/59178")]
+        public void String_BeforeOtherString1()
+        {
+            var code = @"class C
+{
+    void Method()
+    {
+        var s = $$ + "" + bar"";
+    }
+}";
+            using var session = CreateSessionDoubleQuote(code);
+            Assert.NotNull(session);
+            CheckStart(session.Session);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.AutomaticCompletion)]
+        [WorkItem(59178, "https://github.com/dotnet/roslyn/issues/59178")]
+        public void String_BeforeOtherString2()
+        {
+            var code = @"class C
+{
+    void Method()
+    {
+        var s = $$ + ""; } "";
+    }
+}";
+            using var session = CreateSessionDoubleQuote(code);
+            Assert.NotNull(session);
+            CheckStart(session.Session);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.AutomaticCompletion)]
+        [WorkItem(59178, "https://github.com/dotnet/roslyn/issues/59178")]
+        public void String_DontCompleteVerbatim()
+        {
+            var code = @"class C
+{
+    void Method()
+    {
+        var s = ""this"" + @$$that
+            and this"";
+    }
+}";
+            using var session = CreateSessionDoubleQuote(code);
+            Assert.NotNull(session);
+            CheckStart(session.Session);
+        }
+
         internal static Holder CreateSessionSingleQuote(string code)
         {
             return CreateSession(

commit 50ce5f8778c32e22d52e450f72908ec4cabf7328

Fix infinite loop introduced with https://github.com/dotnet/roslyn/pull/60227

src/Features/CSharp/Portable/BraceCompletion/StringLiteralBraceCompletionService.cs
@@ -88,7 +88,7 @@ namespace Microsoft.CodeAnalysis.CSharp.BraceCompletion
 
         private static bool RestOfLineContainsDiagnostics(SyntaxToken token)
         {
-            while (!token.TrailingTrivia.Contains(t => t.IsEndOfLine()))
+            while (token != default && !token.TrailingTrivia.Contains(t => t.IsEndOfLine()))
             {
                 if (token.ContainsDiagnostics)
                     return true;

src/EditorFeatures/CSharpTest/AutomaticCompletion/AutomaticLiteralCompletionTests.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using Microsoft.CodeAnalysis.AutomaticCompletion;
 using Microsoft.CodeAnalysis.Editor.UnitTests.AutomaticCompletion;
 using Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces;
@@ -492,6 +490,20 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.AutomaticCompletion
             using var session = CreateSessionDoubleQuote(code);
             Assert.NotNull(session);
             CheckStart(session.Session);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.AutomaticCompletion)]
+        [WorkItem(59178, "https://github.com/dotnet/roslyn/issues/59178")]
+        public void String_CompleteLiteral_EndOfFile()
+        {
+            var code = @"class C
+{
+    void Method()
+    {
+        var s = ""this"" + $$that""";
+            using var session = CreateSessionDoubleQuote(code);
+            Assert.NotNull(session);
+            CheckStart(session.Session, expectValidSession: false);
         }
 
         internal static Holder CreateSessionSingleQuote(string code)