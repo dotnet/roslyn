9043752d537d6b49eb4d457ec8f3e61f290dbcde
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-04-21 17:24:14 -0700

    Better hover text

--- src/VisualStudio/Core/Def/InheritanceMargin/MarginGlyph/InheritanceMarginGlyphViewModel.cs
+++ src/VisualStudio/Core/Def/InheritanceMargin/MarginGlyph/InheritanceMarginGlyphViewModel.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -37,30 +38,30 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMarg
                 if (_lazyToolTipTextBlock is null)
                 {
                     var members = _tag.MembersOnLine;
-                    if (members.Length == 1)
+
+                    if (members.All(static m => m.TopLevelDisplayText != null))
                     {
                         var member = _tag.MembersOnLine[0];
 
-                        if (member.TopLevelDisplayText is not null)
-                        {
-                            _lazyToolTipTextBlock = new[] { new TaggedText(TextTags.Text, member.TopLevelDisplayText) }.ToTextBlock(_classificationFormatMap, _classificationTypeMap);
-                        }
-                        else
-                        {
-                            // Here we want to show a classified text with loc text,
-                            // e.g. 'Bar' is inherited.
-                            // But the classified text are inlines, so can't directly use string.format to generate the string
-                            var inlines = member.DisplayTexts.ToInlines(_classificationFormatMap, _classificationTypeMap);
-                            var startOfThePlaceholder = ServicesVSResources._0_is_inherited.IndexOf("{0}", StringComparison.Ordinal);
-                            var prefixString = ServicesVSResources._0_is_inherited[..startOfThePlaceholder];
-                            var suffixString = ServicesVSResources._0_is_inherited[(startOfThePlaceholder + "{0}".Length)..];
-                            inlines.Insert(0, new Run(prefixString));
-                            inlines.Add(new Run(suffixString));
+                        _lazyToolTipTextBlock = new[] { new TaggedText(TextTags.Text, member.TopLevelDisplayText) }.ToTextBlock(_classificationFormatMap, _classificationTypeMap);
+                    }
+                    else if (members.Length == 1)
+                    {
+                        var member = _tag.MembersOnLine[0];
 
-                            _lazyToolTipTextBlock = inlines.ToTextBlock(_classificationFormatMap);
-                        }
+                        // Here we want to show a classified text with loc text,
+                        // e.g. 'Bar' is inherited.
+                        // But the classified text are inlines, so can't directly use string.format to generate the string
+                        var inlines = member.DisplayTexts.ToInlines(_classificationFormatMap, _classificationTypeMap);
+                        var startOfThePlaceholder = ServicesVSResources._0_is_inherited.IndexOf("{0}", StringComparison.Ordinal);
+                        var prefixString = ServicesVSResources._0_is_inherited[..startOfThePlaceholder];
+                        var suffixString = ServicesVSResources._0_is_inherited[(startOfThePlaceholder + "{0}".Length)..];
+                        inlines.Insert(0, new Run(prefixString));
+                        inlines.Add(new Run(suffixString));
 
+                        _lazyToolTipTextBlock = inlines.ToTextBlock(_classificationFormatMap);
                         _lazyToolTipTextBlock.FlowDirection = FlowDirection.LeftToRight;
+
                     }
                     else
                     {
@@ -117,7 +118,15 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.InheritanceMarg
             // ZoomLevel is 100 based. (e.g. 150%, 100%)
             // ScaleFactor is 1 based. (e.g. 1.5, 1)
             var scaleFactor = zoomLevel / 100;
-            if (members.Length == 1)
+            if (members.All(m => m.TopLevelDisplayText != null))
+            {
+                var member = tag.MembersOnLine[0];
+                var automationName = member.TopLevelDisplayText;
+
+                var menuItemViewModels = members.SelectMany(m => InheritanceMarginHelpers.CreateMenuItemViewModelsForSingleMember(m)).ToImmutableArray();
+                return new InheritanceMarginGlyphViewModel(tag, classificationTypeMap, classificationFormatMap, automationName!, scaleFactor, menuItemViewModels);
+            }
+            else if (members.Length == 1)
             {
                 var member = tag.MembersOnLine[0];
 
