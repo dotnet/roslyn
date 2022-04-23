9d9d98f8c4bb6aca424ec76938e9e5775e9f6ec9
Youssef Victor <youssefvictor00@gmail.com>
2022-04-23 22:31:56 +0200

    Fix assertion failure

--- src/Compilers/VisualBasic/Portable/Binding/Binder_Attributes.vb
+++ src/Compilers/VisualBasic/Portable/Binding/Binder_Attributes.vb
@@ -766,7 +766,9 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
                                    Not _binder.IsValidTypeForAttributeArgument(conv.Operand.Type) Then
 
                                     If Not conv.HasErrors Then
-                                        ReportDiagnostic(diagBag, conv.Operand.Syntax, ERRID.ERR_RequiredAttributeConstConversion2, conv.Operand.Type, conv.Type)
+                                        ' BC30934: Conversion from '{0}' to '{1}' cannot occur in a constant expression used as an argument to an attribute.
+                                        Dim firstArgument As Object = If(conv.Operand.Type Is Nothing, "Nothing", conv.Operand.Type)
+                                        ReportDiagnostic(diagBag, conv.Operand.Syntax, ERRID.ERR_RequiredAttributeConstConversion2, firstArgument, conv.Type)
                                     End If
                                     Return CreateErrorTypedConstant(node.Type)
                                 Else
