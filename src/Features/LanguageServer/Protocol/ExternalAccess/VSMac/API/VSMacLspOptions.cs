552a4a3fa5be0706782f8e6ca7ace9b6dcedb226
David Barbet <dabarbet@microsoft.com>
2022-04-25 13:03:55 -0700

    Allow VSMac to access LSP options

---
+++ src/Features/LanguageServer/Protocol/ExternalAccess/VSMac/API/VSMacLspOptions.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Text;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.Options.Providers;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.ExternalAccess.VSMac.API;
+
+internal class VSMacLspOptions
+{
+    /// <summary>
+    /// External access option to allow VSMac to turn on the LSP editor feature flag.
+    /// </summary>
+    public static readonly Option2<bool> VSMacLspEditorOption = LspOptions.LspEditorFeatureFlag;
+
+    /// <summary>
+    /// External access option to allow VSMac to turn on the semantic tokens feature flag.
+    /// </summary>
+    public static readonly Option2<bool> VSMacLspSemanticTokensOption = LspOptions.LspSemanticTokensFeatureFlag;
+}
