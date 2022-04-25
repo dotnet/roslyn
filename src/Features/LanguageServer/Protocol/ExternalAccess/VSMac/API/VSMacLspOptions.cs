54f0013603034b776d471380dd16313644f8c8ba
David Barbet <dabarbet@microsoft.com>
2022-04-25 14:54:14 -0700

    remove usings

--- src/Features/LanguageServer/Protocol/ExternalAccess/VSMac/API/VSMacLspOptions.cs
+++ src/Features/LanguageServer/Protocol/ExternalAccess/VSMac/API/VSMacLspOptions.cs
@@ -2,14 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Composition;
-using System.Text;
-using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.Options;
-using Microsoft.CodeAnalysis.Options.Providers;
 
 namespace Microsoft.CodeAnalysis.LanguageServer.ExternalAccess.VSMac.API;
 
