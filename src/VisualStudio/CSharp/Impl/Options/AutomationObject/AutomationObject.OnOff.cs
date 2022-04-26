e4ed96383d5b6063883e2c41b0b5023799a17e2e
David Wengier <david.wengier@microsoft.com>
2022-04-26 11:47:24 +1000

    Add to automation object

--- src/VisualStudio/CSharp/Impl/Options/AutomationObject/AutomationObject.OnOff.cs
+++ src/VisualStudio/CSharp/Impl/Options/AutomationObject/AutomationObject.OnOff.cs
@@ -4,6 +4,7 @@
 
 using Microsoft.CodeAnalysis.Editor.Shared.Options;
 using Microsoft.CodeAnalysis.MetadataAsSource;
+using Microsoft.CodeAnalysis.Structure;
 
 namespace Microsoft.VisualStudio.LanguageServices.CSharp.Options
 {
@@ -31,6 +32,24 @@ namespace Microsoft.VisualStudio.LanguageServices.CSharp.Options
         {
             get { return GetBooleanOption(FeatureOnOffOptions.Outlining); }
             set { SetBooleanOption(FeatureOnOffOptions.Outlining, value); }
+        }
+
+        public int CollapseImportsWhenFirstOpened
+        {
+            get { return GetBooleanOption(BlockStructureOptionsStorage.CollapseImportsWhenFirstOpened); }
+            set { SetBooleanOption(BlockStructureOptionsStorage.CollapseImportsWhenFirstOpened, value); }
+        }
+
+        public int CollapseRegionsWhenFirstOpened
+        {
+            get { return GetBooleanOption(BlockStructureOptionsStorage.CollapseRegionsWhenFirstOpened); }
+            set { SetBooleanOption(BlockStructureOptionsStorage.CollapseRegionsWhenFirstOpened, value); }
+        }
+
+        public int CollapseMetadataImplementationsWhenFirstOpened
+        {
+            get { return GetBooleanOption(BlockStructureOptionsStorage.CollapseMetadataImplementationsWhenFirstOpened); }
+            set { SetBooleanOption(BlockStructureOptionsStorage.CollapseMetadataImplementationsWhenFirstOpened, value); }
         }
 
         public int HighlightReferences
