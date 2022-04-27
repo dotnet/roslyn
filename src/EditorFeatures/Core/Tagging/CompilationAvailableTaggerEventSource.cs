a59c920b6b1fe9c49389e7675c432940ce39d8db
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-04-27 15:34:36 -0700

    Extract out common code to shared locatio

--- src/EditorFeatures/Core/Tagging/CompilationAvailableTaggerEventSource.cs
+++ src/EditorFeatures/Core/Tagging/CompilationAvailableTaggerEventSource.cs
@@ -8,6 +8,7 @@ using Microsoft.CodeAnalysis.Editor.Shared.Tagging;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Remote;
 using Microsoft.CodeAnalysis.Shared.TestHooks;
+using Microsoft.CodeAnalysis.Shared.Utilities;
 using Microsoft.CodeAnalysis.Tagging;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.Text;
@@ -29,7 +30,6 @@ namespace Microsoft.CodeAnalysis.Editor.Tagging
     internal sealed class CompilationAvailableTaggerEventSource : ITaggerEventSource
     {
         private readonly ITextBuffer _subjectBuffer;
-        private readonly IAsynchronousOperationListener _asyncListener;
 
         /// <summary>
         /// Other event sources we're composing over.  If they fire, we should reclassify.  However, after they fire, we
@@ -37,10 +37,7 @@ namespace Microsoft.CodeAnalysis.Editor.Tagging
         /// </summary>
         private readonly ITaggerEventSource _underlyingSource;
 
-        /// <summary>
-        /// Cancellation tokens controlling background computation of the compilation.
-        /// </summary>
-        private readonly ReferenceCountedDisposable<CancellationSeries> _cancellationSeries = new(new CancellationSeries());
+        private readonly CompilationAvailableEventSource _eventSource;
 
         public CompilationAvailableTaggerEventSource(
             ITextBuffer subjectBuffer,
@@ -48,7 +45,7 @@ namespace Microsoft.CodeAnalysis.Editor.Tagging
             params ITaggerEventSource[] eventSources)
         {
             _subjectBuffer = subjectBuffer;
-            _asyncListener = asyncListener;
+            _eventSource = new CompilationAvailableEventSource(asyncListener);
             _underlyingSource = TaggerEventSources.Compose(eventSources);
         }
 
@@ -58,14 +55,16 @@ namespace Microsoft.CodeAnalysis.Editor.Tagging
         {
             // When we are connected to, connect to all our underlying sources and have them notify us when they've changed.
             _underlyingSource.Connect();
-            _underlyingSource.Changed += OnEventSourceChanged;
+            _underlyingSource.Changed += OnUnderlyingSourceChanged;
+            _eventSource.OnCompilationAvailable += OnCompilationAvailable;
         }
 
         public void Disconnect()
         {
-            _underlyingSource.Changed -= OnEventSourceChanged;
+            _eventSource.OnCompilationAvailable -= OnCompilationAvailable;
+            _underlyingSource.Changed -= OnUnderlyingSourceChanged;
             _underlyingSource.Disconnect();
-            _cancellationSeries.Dispose();
+            _eventSource.Dispose();
         }
 
         public void Pause()
@@ -74,7 +73,7 @@ namespace Microsoft.CodeAnalysis.Editor.Tagging
         public void Resume()
             => _underlyingSource.Resume();
 
-        private void OnEventSourceChanged(object? sender, TaggerEventArgs args)
+        private void OnUnderlyingSourceChanged(object? sender, TaggerEventArgs args)
         {
             // First, notify anyone listening to us that something definitely changed.
             this.Changed?.Invoke(this, args);
@@ -83,55 +82,10 @@ namespace Microsoft.CodeAnalysis.Editor.Tagging
             if (document == null)
                 return;
 
-            if (!document.SupportsSemanticModel)
-                return;
-
-            using var cancellationSeries = _cancellationSeries.TryAddReference();
-            if (cancellationSeries is null)
-            {
-                // Already in the process of disposing this instance
-                return;
-            }
-
-            // Cancel any existing tasks that are computing the compilation and spawn a new one to compute
-            // it and notify any listening clients.
-            var cancellationToken = cancellationSeries.Target.CreateNext();
-
-            var token = _asyncListener.BeginAsyncOperation(nameof(OnEventSourceChanged));
-            var task = Task.Run(async () =>
-            {
-                // Support cancellation without throwing.
-                //
-                // We choose a long delay here so that we can avoid this work as long as the user is continually making
-                // changes to their code.  During that time, features that use this are already kicking off fast work
-                // with frozen-partial semantics and we'd like that to not have to contend with more expensive work
-                // kicked off in OOP to compute full compilations.
-                await _asyncListener.Delay(DelayTimeSpan.NonFocus, cancellationToken).NoThrowAwaitable(captureContext: false);
-                if (cancellationToken.IsCancellationRequested)
-                    return;
-
-                var client = await RemoteHostClient.TryGetClientAsync(document.Project, cancellationToken).ConfigureAwait(false);
-                if (client != null)
-                {
-                    var result = await client.TryInvokeAsync<IRemoteCompilationAvailableService>(
-                        document.Project,
-                        (service, solutionInfo, cancellationToken) => service.ComputeCompilationAsync(solutionInfo, document.Project.Id, cancellationToken),
-                        cancellationToken).ConfigureAwait(false);
-
-                    if (!result)
-                        return;
-                }
-                else
-                {
-                    // if we can't get the client, just compute the compilation locally and fire the event once we have it.
-                    await CompilationAvailableHelpers.ComputeCompilationInCurrentProcessAsync(document.Project, cancellationToken).ConfigureAwait(false);
-                }
-
-                // now that we know we have an full compilation, retrigger the tagger so it can show accurate results with the 
-                // full information about this project.
-                this.Changed?.Invoke(this, new TaggerEventArgs());
-            }, cancellationToken);
-            task.CompletesAsyncOperation(token);
+            _eventSource.EnsureCompilationAvailability(document.Project);
         }
+
+        private void OnCompilationAvailable()
+            => this.Changed?.Invoke(this, new TaggerEventArgs());
     }
 }
---
+++ src/Features/Core/Portable/Shared/Utilities/CompilationAvailableEventSource.cs
@@ -0,0 +1,91 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Remote;
+using Microsoft.CodeAnalysis.Shared.TestHooks;
+using Microsoft.CodeAnalysis.Tagging;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.Shared.Utilities
+{
+    internal class CompilationAvailableEventSource : IDisposable
+    {
+        private readonly IAsynchronousOperationListener _asyncListener;
+
+        /// <summary>
+        /// Cancellation tokens controlling background computation of the compilation.
+        /// </summary>
+        private readonly ReferenceCountedDisposable<CancellationSeries> _cancellationSeries = new(new CancellationSeries());
+
+        public event Action? OnCompilationAvailable;
+
+        public CompilationAvailableEventSource(
+            IAsynchronousOperationListener asyncListener)
+        {
+            _asyncListener = asyncListener;
+        }
+
+        public void Dispose()
+            => _cancellationSeries.Dispose();
+
+        public void EnsureCompilationAvailability(Project project)
+        {
+            if (project == null)
+                return;
+
+            if (!project.SupportsCompilation)
+                return;
+
+            using var cancellationSeries = _cancellationSeries.TryAddReference();
+            if (cancellationSeries is null)
+            {
+                // Already in the process of disposing this instance
+                return;
+            }
+
+            // Cancel any existing tasks that are computing the compilation and spawn a new one to compute
+            // it and notify any listening clients.
+            var cancellationToken = cancellationSeries.Target.CreateNext();
+
+            var token = _asyncListener.BeginAsyncOperation(nameof(EnsureCompilationAvailability));
+            var task = Task.Run(async () =>
+            {
+                // Support cancellation without throwing.
+                //
+                // We choose a long delay here so that we can avoid this work as long as the user is continually making
+                // changes to their code.  During that time, features that use this are already kicking off fast work
+                // with frozen-partial semantics and we'd like that to not have to contend with more expensive work
+                // kicked off in OOP to compute full compilations.
+                await _asyncListener.Delay(DelayTimeSpan.NonFocus, cancellationToken).NoThrowAwaitableInternal(captureContext: false);
+                if (cancellationToken.IsCancellationRequested)
+                    return;
+
+                var client = await RemoteHostClient.TryGetClientAsync(project, cancellationToken).ConfigureAwait(false);
+                if (client != null)
+                {
+                    var result = await client.TryInvokeAsync<IRemoteCompilationAvailableService>(
+                        project,
+                        (service, solutionInfo, cancellationToken) => service.ComputeCompilationAsync(solutionInfo, project.Id, cancellationToken),
+                        cancellationToken).ConfigureAwait(false);
+
+                    if (!result)
+                        return;
+                }
+                else
+                {
+                    // if we can't get the client, just compute the compilation locally and fire the event once we have it.
+                    await CompilationAvailableHelpers.ComputeCompilationInCurrentProcessAsync(project, cancellationToken).ConfigureAwait(false);
+                }
+
+                // now that we know we have an full compilation, retrigger the tagger so it can show accurate results with the 
+                // full information about this project.
+                this.OnCompilationAvailable?.Invoke();
+            }, cancellationToken);
+            task.CompletesAsyncOperation(token);
+        }
+    }
+}
