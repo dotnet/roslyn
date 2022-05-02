746c91944a40a50ce72d9aec9b31a7c63531d6f8
Joey Robichaud <joseph.robichaud@microsoft.com>
2022-05-02 12:51:17 -0700

    Remove unneeded helix project property

--- src/Tools/Source/RunTests/TestRunner.cs
+++ src/Tools/Source/RunTests/TestRunner.cs
@@ -111,7 +111,6 @@ namespace RunTests
         <DotNetCliVersion>" + globalJson.sdk.version + @"</DotNetCliVersion>
         <DotNetCliPackageType>sdk</DotNetCliPackageType>
         <EnableAzurePipelinesReporter>" + (isAzureDevOpsRun ? "true" : "false") + @"</EnableAzurePipelinesReporter>
-        <EnableXUnitReporter>" + (isAzureDevOpsRun ? "true" : "false") + @"</EnableXUnitReporter>
     </PropertyGroup>
 
     <ItemGroup>
