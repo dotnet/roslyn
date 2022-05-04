85fbdae162995bdc568b21b173cc7517d09774ef
Youssef Victor <youssefvictor00@gmail.com>
2022-05-04 09:49:15 +0200

    assert property is override when reporting `ERR_AutoPropertyMustOverrideSet`

--- src/Compilers/CSharp/Portable/Symbols/Source/SourcePropertySymbolBase.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourcePropertySymbolBase.cs
@@ -702,6 +702,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 // get-only auto property should not override settable properties
                 if (SetMethod is null && !this.IsReadOnly)
                 {
+                    Debug.Assert(this.IsOverride);
                     diagnostics.Add(ErrorCode.ERR_AutoPropertyMustOverrideSet, Location);
                 }
             }
