631989694ae4414bbf2cdafa31cf6cfe278077e7
Tomáš Matoušek <tmat@users.noreply.github.com>
2022-05-04 17:18:59 -0700

    Revert to pass isBlocking as a parameter (#61123)
    * Revert to pass isBlocking as a parameter
    
    * link

--- src/EditorFeatures/DiagnosticsTestUtilities/CodeActions/AbstractCodeActionTest.cs
+++ src/EditorFeatures/DiagnosticsTestUtilities/CodeActions/AbstractCodeActionTest.cs
@@ -108,7 +108,7 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions
                 CodeActionOptionsStorage.GetCodeActionOptionsProvider(workspace.GlobalOptions) :
                 CodeActionOptions.DefaultProvider;
 
-            var context = new CodeRefactoringContext(document, selectedOrAnnotatedSpan, (a, t) => actions.Add((a, t)), codeActionOptionsProvider, CancellationToken.None);
+            var context = new CodeRefactoringContext(document, selectedOrAnnotatedSpan, (a, t) => actions.Add((a, t)), codeActionOptionsProvider, isBlocking: false, CancellationToken.None);
             await provider.ComputeRefactoringsAsync(context);
             var result = actions.Count > 0 ? new CodeRefactoring(provider, actions.ToImmutable(), FixAllProviderInfo.Create(provider)) : null;
             actions.Free();
