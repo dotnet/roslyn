f1815fe29e81c86417728885489f5a1db8235d52
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-05-07 21:23:17 -0700

    Refactor

--- src/EditorFeatures/CSharp/StringCopyPaste/KnownSourcePasteProcessor.cs
+++ src/EditorFeatures/CSharp/StringCopyPaste/KnownSourcePasteProcessor.cs
@@ -151,27 +151,16 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.StringCopyPaste
             // At this point, we will now have the information necessary to actually insert the content and do things 
             // like give interpolations the proper number of braces for the final string we're making.
 
-            var dummyContentEdit = GetContentEditForRawString(insertInterpolations: false, dollarSignCount: -1, indentationWhitespace: "");
+            DetermineTopLevelChangesToMakeToRawString(
+                out var quotesToAdd, out var dollarSignsToAdd, out var convertToMultiLine);
 
-            var clonedBuffer = _textBufferFactoryService.CreateTextBuffer(
-                new SnapshotSpan(SnapshotBeforePaste, 0, SnapshotBeforePaste.Length), SnapshotBeforePaste.ContentType);
-            var snapshotBeforeDummyPaste = clonedBuffer.CurrentSnapshot;
+            return DetermineTotalEditsToMakeToRawString(
+                quotesToAdd, dollarSignsToAdd, convertToMultiLine, cancellationToken);
+        }
 
-            var edit = clonedBuffer.CreateEdit();
-            edit.Replace(_selectionSpanBeforePaste.ToSpan(), dummyContentEdit.NewText);
-
-            var snapshotAfterDummyPaste = edit.Apply();
-            var textAfterDummyPaste = snapshotAfterDummyPaste.AsText();
-
-            var contentSpansAfterDummyPaste = StringExpressionBeforePasteInfo.ContentSpans.SelectAsArray(
-                ts => MapSpan(ts, snapshotBeforeDummyPaste, snapshotAfterDummyPaste));
-
-            var quotesToAdd = GetQuotesToAddToRawString(textAfterDummyPaste, contentSpansAfterDummyPaste);
-            var dollarSignsToAdd = GetDollarSignsToAddToRawString(textAfterDummyPaste, contentSpansAfterDummyPaste);
-            //var openBracesToAdd = dollarSignsToAdd == null ? null : new string('{', dollarSignsToAdd.Length);
-            //var closeBracesToAdd = dollarSignsToAdd == null ? null : new string('}', dollarSignsToAdd.Length);
-
-            var convertToMultiLine = !IsAnyMultiLineRawStringExpression(StringExpressionBeforePaste) && RawContentMustBeMultiLine(textAfterDummyPaste, contentSpansAfterDummyPaste);
+        private ImmutableArray<TextChange> DetermineTotalEditsToMakeToRawString(
+            string? quotesToAdd, string? dollarSignsToAdd, bool convertToMultiLine, CancellationToken cancellationToken)
+        {
             var indentationWhitespace = StringExpressionBeforePaste.GetFirstToken().GetPreferredIndentation(DocumentBeforePaste, IndentationOptions, cancellationToken);
 
             var finalDollarSignCount = StringExpressionBeforePasteInfo.DelimiterDollarCount +
@@ -213,6 +202,32 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.StringCopyPaste
                 edits.Add(new TextChange(new TextSpan(StringExpressionBeforePasteInfo.EndDelimiterSpanWithoutSuffix.End, 0), quotesToAdd));
 
             return edits.ToImmutable();
+        }
+
+        private void DetermineTopLevelChangesToMakeToRawString(out string? quotesToAdd, out string? dollarSignsToAdd, out bool convertToMultiLine)
+        {
+            PerformInitialBasicPasteInRawString(out var textAfterBasicPaste, out var contentSpansAfterBasicPaste);
+
+            quotesToAdd = GetQuotesToAddToRawString(textAfterBasicPaste, contentSpansAfterBasicPaste);
+            dollarSignsToAdd = GetDollarSignsToAddToRawString(textAfterBasicPaste, contentSpansAfterBasicPaste);
+            convertToMultiLine = !IsAnyMultiLineRawStringExpression(StringExpressionBeforePaste) && RawContentMustBeMultiLine(textAfterBasicPaste, contentSpansAfterBasicPaste);
+        }
+
+        private void PerformInitialBasicPasteInRawString(out SourceText textAfterDummyPaste, out ImmutableArray<TextSpan> contentSpansAfterDummyPaste)
+        {
+            var dummyContentEdit = GetContentEditForRawString(insertInterpolations: false, dollarSignCount: -1, indentationWhitespace: "");
+
+            var clonedBuffer = _textBufferFactoryService.CreateTextBuffer(
+                new SnapshotSpan(SnapshotBeforePaste, 0, SnapshotBeforePaste.Length), SnapshotBeforePaste.ContentType);
+            var snapshotBeforeDummyPaste = clonedBuffer.CurrentSnapshot;
+
+            var edit = clonedBuffer.CreateEdit();
+            edit.Replace(_selectionSpanBeforePaste.ToSpan(), dummyContentEdit.NewText);
+
+            var snapshotAfterDummyPaste = edit.Apply();
+            textAfterDummyPaste = snapshotAfterDummyPaste.AsText();
+            contentSpansAfterDummyPaste = StringExpressionBeforePasteInfo.ContentSpans.SelectAsArray(
+                ts => MapSpan(ts, snapshotBeforeDummyPaste, snapshotAfterDummyPaste));
         }
 
         private void UpdateExistingInterpolationBraces(
