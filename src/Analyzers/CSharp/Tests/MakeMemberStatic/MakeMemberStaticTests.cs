00485ca70672bd9b06a65aa3487aeffc71e39d83
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-05-12 13:59:32 +0200

    Add tests for testing if static modifier is added in correct order

--- src/Analyzers/CSharp/Tests/MakeMemberStatic/MakeMemberStaticTests.cs
+++ src/Analyzers/CSharp/Tests/MakeMemberStatic/MakeMemberStaticTests.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.CodeStyle;
 using Microsoft.CodeAnalysis.CSharp.MakeMemberStatic;
 using Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions;
 using Microsoft.CodeAnalysis.Test.Utilities;
@@ -18,6 +19,20 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.MakeMemberStatic
 
     public class MakeMemberStaticTests
     {
+        private static async Task TestAsync(string testCode, string fixedCode, string? customModifierOrder = null)
+        {
+            await new VerifyCS.Test
+            {
+                TestCode = testCode,
+                FixedCode = fixedCode,
+                Options =
+                {
+                    { CSharpCodeStyleOptions.PreferredModifierOrder,
+                        customModifierOrder ?? CSharpCodeStyleOptions.PreferredModifierOrder.DefaultValue.Value }
+                }
+            }.RunAsync();
+        }
+
         [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsMakeMemberStatic)]
         public async Task TestField()
         {
@@ -128,6 +143,70 @@ public static class Foo
         static event System.Action E;
     }
 }");
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsMakeMemberStatic)]
+        public async Task TestModifierOrder_DefaultOrder()
+        {
+            var testCode = @"
+public static class Foo
+{
+    public void {|CS0708:Test|}() { }
+}
+";
+
+            var fixedCode = @"
+public static class Foo
+{
+    public static void Test() { }
+}
+";
+
+            await TestAsync(testCode, fixedCode);
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsMakeMemberStatic)]
+        public async Task TestModifierOrder_CustomOrder1()
+        {
+            var testCode = @"
+public static class Foo
+{
+    public void {|CS0708:Test|}() { }
+}
+";
+
+            var fixedCode = @"
+public static class Foo
+{
+    static public void Test() { }
+}
+";
+            var customModifierOrder = "static, public, private, protected, internal, extern, new, virtual, " +
+                "abstract, sealed, override, readonly, unsafe, volatile, async";
+
+            await TestAsync(testCode, fixedCode, customModifierOrder);
+        }
+
+        [Fact, Trait(Traits.Feature, Traits.Features.CodeActionsMakeMemberStatic)]
+        public async Task TestModifierOrder_CustomOrder2()
+        {
+            var testCode = @"
+public static class Foo
+{
+    public async System.Threading.Tasks.Task {|CS0708:Test|}() { }
+}
+";
+
+            var fixedCode = @"
+public static class Foo
+{
+    public async static System.Threading.Tasks.Task Test() { }
+}
+";
+            var customModifierOrder = "public, private, protected, internal, extern, new, virtual, abstract, " +
+                "sealed, override, readonly, unsafe, volatile, async, static";
+
+            await TestAsync(testCode, fixedCode, customModifierOrder);
         }
     }
 }
