3eeb82f127df15f888fbea49faceb778668702d4
Simona Koníčková <neme112@outlook.com>
2022-05-17 20:59:45 +0200

    Add back first test

--- src/Workspaces/CoreTest/WorkspaceServiceTests/DirectMemoryAccessStreamReaderTests.cs
+++ src/Workspaces/CoreTest/WorkspaceServiceTests/DirectMemoryAccessStreamReaderTests.cs
@@ -15,5 +15,47 @@ namespace Microsoft.CodeAnalysis.UnitTests.WorkspaceServiceTests
     [Trait(Traits.Feature, Traits.Features.Workspace)]
     public sealed class DirectMemoryAccessStreamReaderTests
     {
+        [Fact]
+        public void PeakRead()
+        {
+            using var _ = CreateReader("text", out var reader);
+            Assert.Equal('t', reader.Read());
+            Assert.Equal('e', reader.Peek());
+            Assert.Equal('e', reader.Read());
+            Assert.Equal('x', reader.Read());
+            Assert.Equal('t', reader.Read());
+            Assert.Equal(-1, reader.Peek());
+            Assert.Equal(-1, reader.Read());
+        }
+
+        private static Disposer CreateReader(string text, out DirectMemoryAccessStreamReader reader)
+        {
+            var handle = GCHandle.Alloc(text.ToCharArray(), GCHandleType.Pinned);
+            reader = DirectMemoryAccessStreamReader.TestAccessor.Create(handle.AddrOfPinnedObject(), text.Length);
+
+#pragma warning disable RS0042 // Do not copy value
+            return new Disposer(handle, reader);
+#pragma warning restore RS0042 // Do not copy value
+        }
+
+        private sealed class Disposer : IDisposable
+        {
+            private GCHandle _handle;
+            private readonly TextReader _textReader;
+
+            public Disposer(GCHandle handle, TextReader textReader)
+            {
+#pragma warning disable RS0042 // Do not copy value
+                _handle = handle;
+#pragma warning restore RS0042 // Do not copy value
+                _textReader = textReader;
+            }
+
+            public void Dispose()
+            {
+                _textReader.Dispose();
+                _handle.Free();
+            }
+        }
     }
 }
