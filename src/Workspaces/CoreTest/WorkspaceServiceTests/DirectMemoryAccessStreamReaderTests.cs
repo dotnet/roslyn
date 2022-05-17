c986e2d81dff0f907dd3aedfddd1dcd40f3131f4
Simona Koníčková <neme112@outlook.com>
2022-05-17 19:58:43 +0200

    Add back empty test class

---
+++ src/Workspaces/CoreTest/WorkspaceServiceTests/DirectMemoryAccessStreamReaderTests.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Xunit;
+using static Microsoft.CodeAnalysis.Host.TemporaryStorageServiceFactory;
+
+namespace Microsoft.CodeAnalysis.UnitTests.WorkspaceServiceTests
+{
+    [Trait(Traits.Feature, Traits.Features.Workspace)]
+    public sealed class DirectMemoryAccessStreamReaderTests
+    {
+    }
+}
