d5f0937c66cce0e38d153c9c998c431356ee7e5b
Rikki Gibson <rigibson@microsoft.com>
2022-05-20 10:51:02 -0700

    Address IDE team feedback

--- src/Analyzers/Core/Analyzers/IDEDiagnosticIds.cs
+++ src/Analyzers/Core/Analyzers/IDEDiagnosticIds.cs
@@ -167,6 +167,8 @@ namespace Microsoft.CodeAnalysis.Diagnostics
 
         public const string UseTupleSwapDiagnosticId = "IDE0180";
 
+        // Don't use "IDE0190". It corresponds to the deleted field UseParameterNullCheckingId which was previously shipped.
+
         public const string RemoveUnnecessaryLambdaExpressionDiagnosticId = "IDE0200";
 
         public const string UseTopLevelStatementsId = "IDE0210";
