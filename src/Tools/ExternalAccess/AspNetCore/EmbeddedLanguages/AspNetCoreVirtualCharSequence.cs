0db292c1dc053b4cb6533afbc310328cc899c9f2
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2022-05-26 08:39:29 -0700

    Expose VirtualChars to asp.net (through EA) to facilitate route classification

---
+++ src/Tools/ExternalAccess/AspNetCore/EmbeddedLanguages/AspNetCoreVirtualCharSequence.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.EmbeddedLanguages.VirtualChars;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.AspNetCore.EmbeddedLanguages
+{
+    internal readonly struct AspNetCoreVirtualCharSequence
+    {
+        private readonly VirtualCharSequence _virtualCharSequence;
+
+        internal AspNetCoreVirtualCharSequence(VirtualCharSequence virtualCharSequence)
+        {
+            _virtualCharSequence = virtualCharSequence;
+        }
+
+        public int Length => _virtualCharSequence.Length;
+        public AspNetCoreVirtualChar this[int index] => new(_virtualCharSequence[index]);
+    }
+}
