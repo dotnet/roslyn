bdf86e34896ac4244caf6961bab9a11ae5cdbea5
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-06-03 14:33:09 -0700

    Support assignments

--- src/Features/Core/Portable/ReplaceConditionalWithStatements/AbstractReplaceConditionalWithStatementsCodeRefactoringProvider.cs
+++ src/Features/Core/Portable/ReplaceConditionalWithStatements/AbstractReplaceConditionalWithStatementsCodeRefactoringProvider.cs
@@ -61,7 +61,7 @@ internal abstract class AbstractReplaceConditionalWithStatementsCodeRefactoringP
             {
                 context.RegisterRefactoring(CodeAction.Create(
                     FeaturesResources.Replace_conditional_expression_with_statements,
-                    c => ReplaceConditionalExpressionInAssignmentStatementAsync(document, conditionalExpression, topExpression, assignmentStatement, c)));
+                    c => ReplaceConditionalExpressionInAssignmentStatementAsync(document, conditionalExpression, assignmentStatement, c)));
                 return;
             }
         }
@@ -87,14 +87,48 @@ internal abstract class AbstractReplaceConditionalWithStatementsCodeRefactoringP
         }
     }
 
-    private Task<Document> ReplaceConditionalExpressionInAssignmentStatementAsync(
+    private static SyntaxNode TryCast(SyntaxGenerator generator, SyntaxNode whenTrue, ITypeSymbol? conditionalType)
+        => conditionalType is null or IErrorTypeSymbol
+            ? whenTrue
+            : generator.CastExpression(conditionalType, whenTrue);
+
+    private static async Task<Document> ReplaceConditionalExpressionInAssignmentStatementAsync(
         Document document,
         TConditionalExpressionSyntax conditionalExpression,
-        TExpressionSyntax topExpression,
         TStatementSyntax assignmentStatement,
         CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var semanticModel = await document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+        var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+        var generator = SyntaxGenerator.GetGenerator(document);
+        var syntaxFacts = document.GetRequiredLanguageService<ISyntaxFactsService>();
+
+        // When we have `a = x ? y : z`, then the type of 'y' and 'z' can influence each other.
+        // If we convert this into:
+        //
+        //   if (x)
+        //     a = y;
+        //   else
+        //     a = z;
+        //
+        // Then we need to preserve that meaning so that 'y' and 'z' have the same type/value, even after the
+        // transformation.
+        var conditionalType = semanticModel.GetTypeInfo(conditionalExpression, cancellationToken).Type;
+
+        syntaxFacts.GetPartsOfAssignmentStatement(assignmentStatement, out var left, out _);
+        syntaxFacts.GetPartsOfConditionalExpression(conditionalExpression, out var condition, out var whenTrue, out var whenFalse);
+        var ifStatement = generator.IfStatement(
+            condition,
+            new[] { generator.AssignmentStatement(left, TryCast(generator, whenTrue, conditionalType)) },
+            new[] { generator.AssignmentStatement(left, TryCast(generator, whenFalse, conditionalType)) }).WithTriviaFrom(assignmentStatement);
+
+        var editor = new SyntaxEditor(root, generator);
+        editor.ReplaceNode(
+            assignmentStatement,
+            ifStatement);
+
+        return document.WithSyntaxRoot(editor.GetChangedRoot());
     }
 
     private async Task<Document> ReplaceConditionalExpressionInLocalDeclarationStatementAsync(
@@ -166,10 +200,12 @@ internal abstract class AbstractReplaceConditionalWithStatementsCodeRefactoringP
         // When we have `return x ? y : z`, then the type of 'y' and 'z' can influence each other.
         // If we convert this into:
         //
-        // if (x)
-        //   return y;
-        // else
-        //   return z;
+        // object M()
+        // {
+        //   if (x)
+        //     return y;
+        //   else
+        //     return z;
         //
         // Then we need to preserve that meaning so that 'y' and 'z' have the same type/value, even after the
         // transformation.
@@ -188,9 +224,4 @@ internal abstract class AbstractReplaceConditionalWithStatementsCodeRefactoringP
 
         return document.WithSyntaxRoot(editor.GetChangedRoot());
     }
-
-    private static SyntaxNode TryCast(SyntaxGenerator generator, SyntaxNode whenTrue, ITypeSymbol? conditionalType)
-        => conditionalType is null or IErrorTypeSymbol
-            ? whenTrue
-            : generator.CastExpression(conditionalType, whenTrue);
 }
