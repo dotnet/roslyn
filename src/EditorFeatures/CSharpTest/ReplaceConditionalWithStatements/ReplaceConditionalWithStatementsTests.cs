8f9027108070a089a8db52f2e5113c9bd83968e7
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-06-04 10:58:23 -0700

    Add tests

---
+++ src/EditorFeatures/CSharpTest/ReplaceConditionalWithStatements/ReplaceConditionalWithStatementsTests.cs
@@ -0,0 +1,116 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.ReplaceConditionalWithStatements;
+using Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.ReplaceConditionalWithStatements;
+
+using VerifyCS = CSharpCodeRefactoringVerifier<CSharpReplaceConditionalWithStatementsCodeRefactoringProvider>;
+
+public class ReplaceConditionalWithStatementsTests
+{
+    [Fact]
+    public async Task TestAssignment1()
+    {
+        await VerifyCS.VerifyRefactoringAsync(
+            """
+            class C
+            {
+                void M(bool b)
+                {
+                    object a;
+                    a = $$b ? 0 : 1L;
+                }
+            }
+            """,
+            """
+            class C
+            {
+                void M(bool b)
+                {
+                    object a;
+                    if (b)
+                    {
+                        a = (long)0;
+                    }
+                    else
+                    {
+                        a = 1L;
+                    }
+                }
+            }
+            """);
+    }
+
+    [Fact]
+    public async Task TestLocalDeclarationStatement1()
+    {
+        await VerifyCS.VerifyRefactoringAsync(
+            """
+            class C
+            {
+                void M(bool b)
+                {
+                    object a = $$b ? 0 : 1L;
+                }
+            }
+            """,
+            """
+            class C
+            {
+                void M(bool b)
+                {
+                    object a;
+                    if (b)
+                    {
+                        a = (long)0;
+                    }
+                    else
+                    {
+                        a = 1L;
+                    }
+                }
+            }
+            """);
+    }
+
+    [Fact]
+    public async Task TestLocalDeclarationStatement_WithVar()
+    {
+        await VerifyCS.VerifyRefactoringAsync(
+            """
+            class C
+            {
+                void M(bool b)
+                {
+                    var a = $$b ? 0 : 1L;
+                }
+            }
+            """,
+            """
+            class C
+            {
+                void M(bool b)
+                {
+                    long a;
+                    if (b)
+                    {
+                        a = 0;
+                    }
+                    else
+                    {
+                        a = 1L;
+                    }
+                }
+            }
+            """);
+    }
+}
