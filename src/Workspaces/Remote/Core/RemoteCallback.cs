dd4e0d31e403b513c4cf8ffd98b460e8f4ddae47
tmat <tomas.matousek@microsoft.com>
2022-06-09 10:07:08 -0700

    Feedback

--- src/Workspaces/Remote/Core/RemoteCallback.cs
+++ src/Workspaces/Remote/Core/RemoteCallback.cs
@@ -94,7 +94,8 @@ namespace Microsoft.CodeAnalysis.Remote
                     return false;
                 }
 
-                return true;
+                // Log unexpected state where a cancellation exception occurs without being requested.
+                return FatalError.ReportAndCatch(exception);
             }
 
             // When a connection is dropped we can see ConnectionLostException even though CancelLocallyInvokedMethodsWhenConnectionIsClosed is set.
