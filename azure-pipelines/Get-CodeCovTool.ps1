f4be78a682e6022cd17d01aada709e7f4ee9b070
Steve Bush <stevebu@bushchang.com>
2022-06-15 09:02:52 -0700

    CodeCov binary uploader
    Replaces the bash CodeCov uploader with the latest binary version.  A PowerShell script downloads the latest codecov binary.  Another PowerShell script collects and uploads code coverage artifact files to CodeCov.io.  You can pass flags and a coverage report name as parameters.

---
+++ azure-pipelines/Get-CodeCovTool.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    Downloads the CodeCov.io uploader tool and returns the path to it.
+#>
+[CmdletBinding()]
+Param(
+)
+
+$toolsPath = & "$PSScriptRoot\Get-TempToolsPath.ps1"
+$binaryToolsPath = Join-Path $toolsPath codecov
+if (!(Test-Path $binaryToolsPath)) { $null = mkdir $binaryToolsPath }
+
+if ($IsMacOS)
+{
+    $codeCovPath = Join-Path $binaryToolsPath codecov
+    $codeCovUrl = "https://uploader.codecov.io/latest/macos/codecov"
+}
+elseif ($IsLinux)
+{
+    $codeCovPath = Join-Path $binaryToolsPath codecov
+    $codeCovUrl = "https://uploader.codecov.io/latest/linux/codecov"
+}
+else
+{
+    $codeCovPath = Join-Path $binaryToolsPath codecov.exe
+    $codeCovUrl = "https://uploader.codecov.io/latest/windows/codecov.exe"
+}
+
+if (!(Test-Path $codeCovPath)) {
+    Write-Host "Downloading latest codeconv..." -ForegroundColor Yellow
+    (New-Object System.Net.WebClient).DownloadFile($codeCovUrl, $codeCovPath)
+}
+
+if ($IsMacOS -or $IsLinux) {
+    chmod u+x $codeCovPath
+}
+
+return (Resolve-Path $codeCovPath).Path
