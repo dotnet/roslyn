bf5fc7e87538ad18333cc291bf9641d7e539917f
Andrew Arnott <andrewarnott@live.com>
2022-06-18 15:22:38 -0600

    fixup

--- azure-pipelines/publish-CodeCov.ps1
+++ azure-pipelines/publish-CodeCov.ps1
@@ -23,7 +23,7 @@ Param (
 $RepoRoot = (Resolve-Path "$PSScriptRoot/..").Path
 
 Get-ChildItem -Recurse -Path $PathToCodeCoverage -Filter "*.cobertura.xml" | % {
-    $relativeFilePath = Resolve-Path -relative $_
+    $relativeFilePath = Resolve-Path -relative $_.FullName
 
     Write-Host "Uploading: $relativeFilePath" -ForegroundColor Yellow
     & (& "$PSScriptRoot/Get-CodeCovTool.ps1") -t $CodeCovToken -f $relativeFilePath -R $RepoRoot -F $Flags -n $Name
