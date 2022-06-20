32edbfcd05980313339e8e57e2b2668a2b6c63b4
Andrew Arnott <andrew.arnott@microsoft.com>
2022-06-20 08:46:30 -0600

    Collect test results from the right directory
    The dump files used to be written to an agent TEMP directory. Somewhere along the lines they moved to the TestResults directory within the repo itself.
    
    Fixes AArnott/Library.Template#167

--- azure-pipelines/artifacts/testResults.ps1
+++ azure-pipelines/artifacts/testResults.ps1
@@ -4,17 +4,8 @@ Param(
 
 $result = @{}
 
-if ($env:AGENT_TEMPDIRECTORY) {
-    # The DotNetCoreCLI uses an alternate location to publish these files
-    $guidRegex = '^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'
-    $result[$env:AGENT_TEMPDIRECTORY] = (Get-ChildItem $env:AGENT_TEMPDIRECTORY -Directory |? { $_.Name -match $guidRegex } |% {
-        Get-ChildItem "$($_.FullName)\dotnet*.dmp","$($_.FullName)\*_crashdump.dmp","$($_.FullName)\testhost*.dmp","$($_.FullName)\Sequence_*.xml" -Recurse
-    });
-}
-else {
-    $testRoot = Resolve-Path "$PSScriptRoot\..\..\test"
-    $result[$testRoot] = (Get-ChildItem "$testRoot\TestResults" -Recurse -Directory | Get-ChildItem -Recurse -File)
-}
+$testRoot = Resolve-Path "$PSScriptRoot\..\..\test"
+$result[$testRoot] = (Get-ChildItem "$testRoot\TestResults" -Recurse -Directory | Get-ChildItem -Recurse -File)
 
 $testlogsPath = "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\test_logs"
 if (Test-Path $testlogsPath) {
