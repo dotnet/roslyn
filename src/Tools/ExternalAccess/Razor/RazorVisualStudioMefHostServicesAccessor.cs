296073bb89f9ca0eca917e32ba6c3734b37d114e
Allison Chou <allichou@microsoft.com>
2022-06-20 16:11:05 -0700

    Add Razor VS MEF host acccessor

---
+++ src/Tools/ExternalAccess/Razor/RazorVisualStudioMefHostServicesAccessor.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.VisualStudio.Composition;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
+{
+    internal class RazorVisualStudioMefHostServicesAccessor
+    {
+        public static HostServices Create(ExportProvider exportProvider)
+            => VisualStudioMefHostServices.Create(exportProvider);
+    }
+}
