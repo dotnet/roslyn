1b0696034182db540882896358601d10788d2628
Shen Chen <shech@microsoft.com>
2022-06-20 13:23:22 -0700

    Add the third test to navigate to metadata

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpInheritanceMarginTests.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpInheritanceMarginTests.cs
@@ -51,7 +51,7 @@ class Implementation : IBar
 
             await TestServices.InheritanceMargin.ClickTheGlyphOnLine(2, HangMitigatingCancellationToken);
 
-            // Move focus to menu item of 'class Implementation'
+            // Move focus to menu item of 'IBar', the destination is targeting 'class Implementation'
             await TestServices.Input.SendAsync(VirtualKey.Tab);
             // Navigate to the destination
             await TestServices.Input.SendAsync(VirtualKey.Enter);
@@ -80,6 +80,7 @@ class Implementation : IBar
 
             await TestServices.InheritanceMargin.ClickTheGlyphOnLine(4, HangMitigatingCancellationToken);
 
+            // The context menu contains two members, e1 and e2.
             // Move focus to menu item of 'event e1'
             await TestServices.Input.SendAsync(VirtualKey.Tab);
             // Expand the submenu
@@ -87,6 +88,31 @@ class Implementation : IBar
             // Navigate to the implemention
             await TestServices.Input.SendAsync(VirtualKey.Enter);
             await TestServices.EditorVerifier.TextContainsAsync(@"public event EventHandler e1$$, e2;", assertCaretPosition: true);
+        }
+
+        [IdeFact]
+        public async Task TestNavigateToMetadata()
+        {
+            var project = ProjectName;
+            await TestServices.InheritanceMargin.EnableOptionsAsync(LanguageName, cancellationToken: HangMitigatingCancellationToken);
+            await TestServices.SolutionExplorer.AddFileAsync(project, "Test.cs", cancellationToken: HangMitigatingCancellationToken);
+            await TestServices.SolutionExplorer.OpenFileAsync(project, "Test.cs", HangMitigatingCancellationToken);
+
+            await TestServices.InheritanceMargin.SetTextAndEnsureGlyphsAppearAsync(
+@"
+using System.Collections
+
+class Implementation : IEnumerable
+{
+}", expectedGlyphsNumberInMargin: 1, HangMitigatingCancellationToken);
+
+            await TestServices.InheritanceMargin.ClickTheGlyphOnLine(4, HangMitigatingCancellationToken);
+
+            // Move focus to menu item of 'class Implementation'
+            await TestServices.Input.SendAsync(VirtualKey.Tab);
+            // Navigate to 'IEnumerable'
+            await TestServices.Input.SendAsync(VirtualKey.Enter);
+            await TestServices.EditorVerifier.TextContainsAsync(@"public interface IEnumerable$$", assertCaretPosition: true);
         }
     }
 }
