91274fe7a496cb3b23be9687f6c3bea0e52c4bb4
Soren Dahl <t-dahlsoren@microsoft.com>
2022-06-22 16:37:21 -0700

    Changed node selection helper to break field declaration into variable declarators for C#
    Selects declarator closest to selection span

--- src/Features/CSharp/Portable/CodeRefactorings/NodeSelectionHelpers.cs
+++ src/Features/CSharp/Portable/CodeRefactorings/NodeSelectionHelpers.cs
@@ -4,6 +4,7 @@
 
 #nullable disable
 
+using System;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -18,8 +19,30 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings
             // MemberDeclaration: member that can be declared in type (those are the ones we can pull up) 
             // VariableDeclaratorSyntax: for fields the MemberDeclaration can actually represent multiple declarations, e.g. `int a = 0, b = 1;`.
             // ..Since the user might want to select & pull up only one of them (e.g. `int a = 0, [|b = 1|];` we also look for closest VariableDeclaratorSyntax.
-            return await context.TryGetRelevantNodeAsync<MemberDeclarationSyntax>().ConfigureAwait(false) as SyntaxNode ??
-                await context.TryGetRelevantNodeAsync<VariableDeclaratorSyntax>().ConfigureAwait(false);
+            var memberDeclaration = await context.TryGetRelevantNodeAsync<MemberDeclarationSyntax>().ConfigureAwait(false) as SyntaxNode;
+            if (memberDeclaration == null)
+            {
+                return await context.TryGetRelevantNodeAsync<VariableDeclaratorSyntax>().ConfigureAwait(false);
+            }
+            // since a FieldDeclarationSyntax doesn't have an associated Symbol (it can have multiple), we just get its variable declaration instead
+            if (memberDeclaration is FieldDeclarationSyntax fieldDeclaration)
+            {
+                // find the variable declarator closest to the start of the span
+                var (_, span, _) = context;
+                VariableDeclaratorSyntax closestDeclarator = null;
+                var leastDistance = int.MaxValue;
+                foreach (var candidate in fieldDeclaration.Declaration.Variables)
+                {
+                    var dist = Math.Min(Math.Abs(candidate.SpanStart - span.Start), Math.Abs(candidate.Span.End - span.Start));
+                    if (dist < leastDistance)
+                    {
+                        closestDeclarator = candidate;
+                        leastDistance = dist;
+                    }
+                }
+                return closestDeclarator;
+            }
+            return memberDeclaration;
         }
     }
 }
