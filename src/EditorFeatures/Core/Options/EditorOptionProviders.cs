88596b8bb044bdfa6fad2dab470ad02b9fd3bc05
tmat <tomas.matousek@microsoft.com>
2022-06-21 19:07:35 -0700

    Fix interactive buffers

--- src/EditorFeatures/Core/Options/EditorOptionProviders.cs
+++ src/EditorFeatures/Core/Options/EditorOptionProviders.cs
@@ -64,4 +64,16 @@ internal static class EditorOptionProviders
             IndentingStyle.Block => FormattingOptions2.IndentStyle.Block,
             _ => FormattingOptions2.IndentStyle.None,
         };
+
+    /// <summary>
+    /// Sets options stored in <see cref="IGlobalOptionService"/> that are read by command handlers from the text editor to given <see cref="IEditorOptions"/>.
+    /// </summary>
+    public static void SetEditorOptions(this IGlobalOptionService globalOptions, IEditorOptions editorOptions, string language)
+    {
+        editorOptions.SetOptionValue(DefaultOptions.IndentStyleId, globalOptions.GetOption(FormattingOptions2.SmartIndent, language).ToEditorIndentStyle());
+        editorOptions.SetOptionValue(DefaultOptions.NewLineCharacterOptionId, globalOptions.GetOption(FormattingOptions2.NewLine, language));
+        editorOptions.SetOptionValue(DefaultOptions.TabSizeOptionId, globalOptions.GetOption(FormattingOptions2.TabSize, language));
+        editorOptions.SetOptionValue(DefaultOptions.IndentSizeOptionId, globalOptions.GetOption(FormattingOptions2.IndentationSize, language));
+        editorOptions.SetOptionValue(DefaultOptions.ConvertTabsToSpacesOptionId, !globalOptions.GetOption(FormattingOptions2.UseTabs, language));
+    }
 }
