27024edd778f09586c876c9fec036b677fd79d5e
tmat <tomas.matousek@microsoft.com>
2022-06-22 12:56:13 -0700

    Comment

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Utilities/ParsedDocument.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Utilities/ParsedDocument.cs
@@ -11,6 +11,16 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis;
 
+/// <summary>
+/// Represents a <see cref="Document"/> content that has been parsed.
+/// </summary>
+/// <remarks>
+/// Used to front-load <see cref="SyntaxTree"/> parsing and <see cref="SourceText"/> retrieval to a caller that has knowledge of whether or not these operations
+/// should be performed synchronously or asynchronously. The <see cref="ParsedDocument"/> is then passed to a feature whose implementation is entirely synchronous.
+/// In general, any feature API that accepts <see cref="ParsedDocument"/> should be synchronous and not access the <see cref="Document"/> or <see cref="Solution"/> snapshots.
+/// In exceptional cases such API may be asynchronous as long as it completes synchronously in most common cases and async completion is rare. It is still desirable to improve the design
+/// of such feature to either not be invoked on a UI thread or be entirely synchronous.
+/// </remarks>
 internal readonly record struct ParsedDocument(DocumentId Id, SourceText Text, SyntaxNode Root, HostLanguageServices LanguageServices)
 {
     public SyntaxTree SyntaxTree => Root.SyntaxTree;
