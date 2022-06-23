81d5dc43d9b0b3675fa01a35c76757f8f9f2219f
Andrew Arnott <andrew.arnott@microsoft.com>
2022-06-22 19:34:34 -0600

    Handle 4-integer package versions in InsertConfigValues.ps1

--- azure-pipelines/variables/InsertConfigValues.ps1
+++ azure-pipelines/variables/InsertConfigValues.ps1
@@ -3,7 +3,7 @@ $InsertedPkgs = (& "$PSScriptRoot\..\artifacts\VSInsertion.ps1" -SbomNotRequired
 $icv=@()
 foreach ($kvp in $InsertedPkgs.GetEnumerator()) {
     $kvp.Value |% {
-        if ($_.Name -match "^(.*)\.(\d+\.\d+\.\d+(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
+        if ($_.Name -match "^(.*?)\.(\d+\.\d+\.\d+(?:\.\d+)?(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
             $id = $Matches[1]
             $version = $Matches[2]
             $icv += "$id=$version"
