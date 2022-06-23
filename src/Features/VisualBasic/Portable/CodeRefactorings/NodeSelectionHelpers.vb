0e29fd97ceec7b306dc645831f7096c8b0b4a22d
Soren Dahl <t-dahlsoren@microsoft.com>
2022-06-22 17:00:10 -0700

    WIP on VB selection

--- src/Features/VisualBasic/Portable/CodeRefactorings/NodeSelectionHelpers.vb
+++ src/Features/VisualBasic/Portable/CodeRefactorings/NodeSelectionHelpers.vb
@@ -2,7 +2,10 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
+Imports System.Collections.Immutable
+Imports System.Threading
 Imports Microsoft.CodeAnalysis.CodeRefactorings
+Imports Microsoft.CodeAnalysis.Text
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.CodeRefactorings
@@ -15,7 +18,21 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.CodeRefactorings
             ' Gets field variable declarations (not including the keywords like Public/Shared, etc), which are not methods
             Dim fieldDeclaration = Await context.TryGetRelevantNodeAsync(Of FieldDeclarationSyntax).ConfigureAwait(False)
             If fieldDeclaration IsNot Nothing Then
-                Return fieldDeclaration
+                ' Since field declarations can contain multiple variables (each of which are a "member"), we need to find one to choose
+                ' We'll do this by selecting the one closest to the start of the span
+                Dim span As TextSpan, cancellationToken As CancellationToken
+                context.Deconstruct(Nothing, span, cancellationToken)
+                Dim closestDeclarator As VariableDeclaratorSyntax = Nothing
+                Dim leastDistance = Integer.MaxValue
+                Dim candidates = (Await fieldDeclaration.Declarators.SelectManyAsArrayAsync(Of VariableDeclaratorSyntax, ModifiedIdentifierSyntax)(Function(vds As VariableDeclaratorSyntax, cancellationToken) vds.Names.AsImmutable()).ConfigureAwait(False)).
+                For Each candidate In fieldDeclaration.Declarators
+                    Dim dist = Math.Min(Math.Abs(candidate.SpanStart - span.Start), Math.Abs(candidate.Span.End - span.Start))
+                    If (dist < leastDistance) Then
+                        closestDeclarator = candidate
+                        leastDistance = dist
+                    End If
+                Next
+                Return closestDeclarator
             End If
             ' Gets the identifier + type of the field itself (ex. TestField As Integer), since it is nested in the variable declaration
             ' And so the token's parent is not a variable declaration
