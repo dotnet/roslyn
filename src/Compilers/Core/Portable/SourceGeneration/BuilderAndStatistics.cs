644ed1b5fe2ec38a28e24c5416d847b001779789
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-06-27 17:21:41 -0700

    Simplify

--- src/Compilers/Core/Portable/SourceGeneration/BuilderAndStatistics.cs
+++ src/Compilers/Core/Portable/SourceGeneration/BuilderAndStatistics.cs
@@ -20,21 +20,24 @@ namespace Microsoft.CodeAnalysis
     /// is pooled.  Used to ensure that we don't keep around builders for too long that are both enormous but also
     /// barely used in practice.
     /// </summary>
-    /// <param name="NumberOfTimesPooled">
-    /// The number of times this item has been added back to the pool.  Once this goes past some threshold we will start
-    /// checking if we're continually returning a large array that is mostly empty.  If so, we will try to lower the
-    /// capacity of the array to prevent wastage.
-    /// </param>
-    /// <param name="NumberOfTimesPooledWhenSparse">
-    /// The number of times we returned a large array to the pool that was barely filled.  If this is a significant
-    /// number of the total times pooled, then we will attempt to lower the capacity of the array.
-    /// </param>
     internal record struct BuilderAndStatistics<TValue>(
-        ImmutableArray<TValue>.Builder Builder,
-        int NumberOfTimesPooled,
-        int NumberOfTimesPooledWhenSparse)
+        ImmutableArray<TValue>.Builder Builder)
     {
         private static readonly ConcurrentQueue<BuilderAndStatistics<TValue>> s_pool = new();
+
+        /// <summary>
+        /// The number of times this item has been added back to the pool.  Once this goes past some threshold
+        /// we will start checking if we're continually returning a large array that is mostly empty.  If so, we
+        /// will try to lower the capacity of the array to prevent wastage.
+        /// </summary>
+        private int _numberOfTimesPooled = 0;
+
+        /// <summary>
+        /// The number of times we returned a large array to the pool that was barely filled.  If this is a
+        /// significant number of the total times pooled, then we will attempt to lower the capacity of the
+        /// array.
+        /// </summary>
+        private int _numberOfTimesPooledWhenSparse = 0;
 
         public static BuilderAndStatistics<TValue> Allocate()
             => s_pool.TryDequeue(out var item) ? item : new BuilderAndStatistics<TValue> { Builder = ImmutableArray.CreateBuilder<TValue>() };
@@ -79,29 +82,29 @@ namespace Microsoft.CodeAnalysis
             // want to shrink down.  To prevent shrinking and inflating over and over again, we only shrink when
             // we're highly confident we're going to stay small.
 
-            NumberOfTimesPooled++;
+            _numberOfTimesPooled++;
 
             // See if we're pooling something both large and sparse.
             if (Builder.Capacity > MinCapacityToConsiderThreshold &&
                 ((double)Builder.Count / Builder.Capacity) < SparseThresholdRatio)
             {
                 CodeAnalysisEventSource.Log.PooledWhenSparse(Builder.GetType(), Builder.Count, Builder.Capacity);
-                NumberOfTimesPooledWhenSparse++;
+                _numberOfTimesPooledWhenSparse++;
             }
 
             Builder.Clear();
 
             // See if this builder has been consistently sparse. If so then time to lower its capacity.
-            if (NumberOfTimesPooled > MinTimesPooledToConsiderStatistics &&
-                ((double)NumberOfTimesPooledWhenSparse / NumberOfTimesPooled) > ConsistentlySparseRatio)
+            if (_numberOfTimesPooled > MinTimesPooledToConsiderStatistics &&
+                ((double)_numberOfTimesPooledWhenSparse / _numberOfTimesPooled) > ConsistentlySparseRatio)
             {
                 CodeAnalysisEventSource.Log.HalvedCapacity(Builder.GetType(), Builder.Count, Builder.Capacity);
                 Builder.Capacity /= 2;
 
                 // Reset our statistics.  We'll wait another 100 pooling attempts to reassess if we need to
                 // adjust the capacity here.
-                NumberOfTimesPooled = 1;
-                NumberOfTimesPooledWhenSparse = 0;
+                _numberOfTimesPooled = 1;
+                _numberOfTimesPooledWhenSparse = 0;
             }
 
             s_pool.Enqueue(this);
