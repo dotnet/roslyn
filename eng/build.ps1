cf80d782f5927f522873b763d1c8fe49aacc1300
Sam Harwell <Sam.Harwell@microsoft.com>
2022-06-28 10:32:58 -0700

    Disable async save in integration tests

--- eng/build.ps1
+++ eng/build.ps1
@@ -573,6 +573,9 @@ function Deploy-VsixViaTool() {
   # Disable background download UI to avoid toasts
   &$vsRegEdit set "$vsDir" $hive HKCU "FeatureFlags\Setup\BackgroundDownload" Value dword 0
 
+  # Disable async save
+  &$vsRegEdit set "$vsDir" $hive HKCU "FeatureFlags\VS\RDT\AsyncSave" Value dword 0
+
   # Configure LSP
   $lspRegistryValue = [int]$lspEditor.ToBool()
   &$vsRegEdit set "$vsDir" $hive HKCU "FeatureFlags\Roslyn\LSP\Editor" Value dword $lspRegistryValue
