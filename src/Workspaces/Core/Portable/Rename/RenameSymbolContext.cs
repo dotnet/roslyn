5f0ab8b881edcd13f454954918e1533c4c2a176d
Shen Chen <shech@microsoft.com>
2022-07-07 15:07:38 -0700

    Cherry-pick the context files

---
+++ src/Workspaces/Core/Portable/Rename/RenameSymbolContext.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis.Rename.ConflictEngine;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.Rename
+{
+    /// <summary>
+    /// RenameSymbolContext contains all the immutable context information to rename the <paramref name="RenamedSymbol"/>.
+    /// </summary>
+    internal record RenameSymbolContext(
+        int Priority,
+        RenameAnnotation RenamableSymbolDeclarationAnnotation,
+        string ReplacementText,
+        string OriginalText,
+        ICollection<string> PossibleNameConflicts,
+        ISymbol RenamedSymbol,
+        IAliasSymbol? AliasSymbol,
+        bool ReplacementTextValid,
+        bool IsRenamingInStrings,
+        bool IsRenamingInComments);
+}
---
+++ src/Workspaces/Core/Portable/Rename/TextSpanRenameContext.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis.Rename
+{
+    /// <summary>
+    /// Represent the rename context information for the given <paramref name="RenameLocation"/>.
+    /// </summary>
+    /// <param name="SymbolContext">The linked rename symbol for this location.</param>
+    internal record TextSpanRenameContext(
+        RenameLocation RenameLocation,
+        RenameSymbolContext SymbolContext)
+    {
+        public int Priority => SymbolContext.Priority;
+    }
+}
