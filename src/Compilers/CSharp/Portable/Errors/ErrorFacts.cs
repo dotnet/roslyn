0673f5103c0cfbb3ac6b57072c93d986b6b8d43a
Rikki Gibson <rigibson@microsoft.com>
2022-07-18 11:41:03 -0700

    Update ErrorFacts.cs

--- src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
@@ -2207,6 +2207,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 case ErrorCode.ERR_FileTypeNameDisallowed:
                 case ErrorCode.ERR_FeatureNotAvailableInVersion11:
                 case ErrorCode.ERR_RefFieldInNonRefStruct:
+                case ErrorCode.WRN_AnalyzerReferencesNewerCompiler:
                     return false;
                 default:
                     // NOTE: All error codes must be explicitly handled in this switch statement
