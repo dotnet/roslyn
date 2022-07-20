0a30e4769d81e14120cd3e05d4a3c157c340dccc
Youssef1313 <youssefvictor00@gmail.com>
2022-07-20 07:24:01 +0200

    Fix build errors

--- src/Workspaces/CoreTest/WorkspaceServiceTests/GlobalOptionServiceTests.cs
+++ src/Workspaces/CoreTest/WorkspaceServiceTests/GlobalOptionServiceTests.cs
@@ -391,7 +391,12 @@ namespace Microsoft.CodeAnalysis.UnitTests.WorkspaceServices
 
             //  Test matrix using different OptionKey and OptionKey2 get/set operations.
             var optionKey = new OptionKey(option, language);
-            var optionKey2 = new OptionKey2(option, language);
+            var optionKey2 = option switch
+            {
+                IPerLanguageValuedOption perLanguageValuedOption => new OptionKey2(perLanguageValuedOption, language!),
+                ISingleValuedOption singleValued => new OptionKey2(singleValued),
+                _ => throw ExceptionUtilities.Unreachable,
+            };
 
             // Value return from "object GetOption(OptionKey)" should always be public CodeStyleOption type.
             var newPublicValue = newValue.AsPublicCodeStyleOption();
