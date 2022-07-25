d62a15b1f67972c0db36e7bd1918097fd7018bc1
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-07-25 10:44:19 -0700

    Add extension

---
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Workspace/SolutionExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.CodeAnalysis
+{
+    internal static class SolutionExtensions
+    {
+#if CODE_STYLE
+        /// <summary>
+        /// Remove this extension once Solution.Services is shipped and can be used by CODE_STYLE layer.
+        /// </summary>
+        public static HostWorkspaceServices GetServices(this Solution solution)
+        {
+            return solution.Workspace.Services;
+        }
+#else
+        public static HostWorkspaceServices GetServices(this Solution solution)
+            => solution.Services
+#endif
+    }
+}
