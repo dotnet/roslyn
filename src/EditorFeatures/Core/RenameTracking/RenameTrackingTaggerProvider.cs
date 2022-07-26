95cfe3d69c779ea65f48a26230c2116bc6cd9911
Sandy Armstrong <sandy@xamarin.com>
2022-07-26 15:19:57 -0700

    Throw instead of FailFast when rename tracking state is messed up

--- src/EditorFeatures/Core/RenameTracking/RenameTrackingTaggerProvider.cs
+++ src/EditorFeatures/Core/RenameTracking/RenameTrackingTaggerProvider.cs
@@ -86,7 +86,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.RenameTracking
                     textBuffer = text.Container.TryGetTextBuffer();
                     if (textBuffer == null)
                     {
-                        FailFast.Fail(string.Format("document with name {0} is open but textBuffer is null. Textcontainer is of type {1}. SourceText is: {2}",
+                        throw new InvalidOperationException(string.Format("document with name {0} is open but textBuffer is null. Textcontainer is of type {1}. SourceText is: {2}",
                                                             document.Name, text.Container.GetType().FullName, text.ToString()));
                     }
 
