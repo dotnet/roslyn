17dfa0e1b4bdadabecd928a9dc0c14da83f8c68a
adamperlin <adamp@nanosoft.com>
2022-07-28 16:55:02 -0700

    Fix nullable annotation issue

--- src/Compilers/CSharp/Portable/Binder/Binder.QueryUnboundLambdaState.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder.QueryUnboundLambdaState.cs
@@ -53,7 +53,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             // Query lambdas don't have default values as part of their syntax
             public override EqualsValueClauseSyntax DefaultValue(int index) => null;
             // Query unbound lambdas don't have associated parameter syntax
-            public override ParameterSyntax? ParamSyntax(int index) => null;
+            public override ParameterSyntax ParamSyntax(int index) => null;
             public override TypeWithAnnotations ParameterTypeWithAnnotations(int index) { throw new ArgumentException(); } // implicitly typed
 
             public override void GenerateAnonymousFunctionConversionError(BindingDiagnosticBag diagnostics, TypeSymbol targetType)
