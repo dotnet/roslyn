73886c31e8e15542f04a20acb2766407e994423b
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-07-29 15:25:50 -0700

    REstore

--- src/Workspaces/Remote/ServiceHub/Host/RemoteWorkspace.cs
+++ src/Workspaces/Remote/ServiceHub/Host/RemoteWorkspace.cs
@@ -170,7 +170,6 @@ namespace Microsoft.CodeAnalysis.Remote
         private async Task<Solution> ComputeDisconnectedSolutionAsync(
             AssetProvider assetProvider,
             Checksum solutionChecksum,
-            Solution currentSolution,
             CancellationToken cancellationToken)
         {
             try
