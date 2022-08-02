5ab8e39c85f7d570ceb0b04a0a225e3fd5fac78b
akhera99 <ankitakhera@microsoft.com>
2022-08-02 14:44:23 -0700

    feedback

--- src/Features/CSharp/Portable/Snippets/CSharpForEachLoopSnippetProvider.cs
+++ src/Features/CSharp/Portable/Snippets/CSharpForEachLoopSnippetProvider.cs
@@ -19,7 +19,7 @@ using Microsoft.CodeAnalysis.Snippets.SnippetProviders;
 namespace Microsoft.CodeAnalysis.CSharp.Snippets
 {
     [ExportSnippetProvider(nameof(ISnippetProvider), LanguageNames.CSharp), Shared]
-    internal class CSharpForEachLoopSnippetProvider : AbstractForEachLoopSnippetProvider
+    internal sealed class CSharpForEachLoopSnippetProvider : AbstractForEachLoopSnippetProvider
     {
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
