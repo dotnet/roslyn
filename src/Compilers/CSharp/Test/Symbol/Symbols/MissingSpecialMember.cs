7ab83bb21d1b1130fc3b64986543251e494c65f6
Charles Stoner <10732005+cston@users.noreply.github.com>
2022-08-02 21:25:32 -0700

    Support UnscopedRefAttribute (#62783)

--- src/Compilers/CSharp/Test/Symbol/Symbols/MissingSpecialMember.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/MissingSpecialMember.cs
@@ -617,6 +617,7 @@ namespace System
                     case WellKnownType.System_Runtime_CompilerServices_ScopedRefAttribute:
                     case WellKnownType.System_MemoryExtensions:
                     case WellKnownType.System_Runtime_CompilerServices_CompilerFeatureRequiredAttribute:
+                    case WellKnownType.System_Diagnostics_CodeAnalysis_UnscopedRefAttribute:
                         // Not yet in the platform.
                         continue;
                     case WellKnownType.Microsoft_CodeAnalysis_Runtime_Instrumentation:
@@ -981,6 +982,7 @@ namespace System
                     case WellKnownMember.System_MemoryExtensions__SequenceEqual_ReadOnlySpan_T:
                     case WellKnownMember.System_MemoryExtensions__AsSpan_String:
                     case WellKnownMember.System_Runtime_CompilerServices_CompilerFeatureRequiredAttribute__ctor:
+                    case WellKnownMember.System_Diagnostics_CodeAnalysis_UnscopedRefAttribute__ctor:
                         // Not yet in the platform.
                         continue;
                     case WellKnownMember.Microsoft_CodeAnalysis_Runtime_Instrumentation__CreatePayloadForMethodsSpanningSingleFile:
