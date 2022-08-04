9aef43c200f094a1b7d3ff0f38153fb4c6b27c4c
Ryan Brandenburg (HE/HIM) <rybrande@microsoft.com>
2022-08-03 17:21:17 -0700

    PR feedback

---
+++ src/Features/LanguageServer/CommonLanguageServerProtocol.Framework/ILifeCycleManager.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLanguageServerProtocol.Framework;
+
+/// <summary>
+/// Used to manage server lifecycle.
+/// </summary>
+public interface ILifeCycleManager
+{
+    /// <summary>
+    /// Shutdown the LanguageServer, for example because "shutdown" was called.
+    /// </summary>
+    void Shutdown();
+
+    /// <summary>
+    /// Exit the LanguageServer, for example because "exit" was called.
+    /// </summary>
+    void Exit();
+}
