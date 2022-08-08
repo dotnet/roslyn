6c6919800585d6d94b3d59874b8ac0f29862b726
Soren Dahl <t-dahlsoren@microsoft.com>
2022-08-08 13:27:44 -0700

    Fixed failing test cleanup

--- src/EditorFeatures/CSharpTest/ConvertToRecord/ConvertToRecordTests.cs
+++ src/EditorFeatures/CSharpTest/ConvertToRecord/ConvertToRecordTests.cs
@@ -14,6 +14,7 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.ConvertToRecord
 {
     using VerifyCS = CSharpCodeRefactoringVerifier<CSharpConvertToRecordRefactoringProvider>;
 
+    [UseExportProvider]
     [Trait(Traits.Feature, Traits.Features.CodeActionsConvertToRecord)]
     public class ConvertToRecordTests
     {
