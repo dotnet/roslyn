6075591b3bf80f06be586065d34a0d74d3986412
Daniel David Sanchez <t-danield@microsoft.com>
2022-08-09 13:28:36 -0700

    changed completion handler logic

--- src/EditorFeatures/Core/ExternalAccess/EditorConfig/Features/SettingsHelper.cs
+++ src/EditorFeatures/Core/ExternalAccess/EditorConfig/Features/SettingsHelper.cs
@@ -2,44 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Composition;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.LanguageServer;
-using Microsoft.CodeAnalysis.LanguageServer.Handler;
-using Microsoft.VisualStudio.LanguageServer.Protocol;
 using Microsoft.CodeAnalysis.Editor.EditorConfigSettings;
 using Microsoft.CodeAnalysis.Editor.EditorConfigSettings.Data;
 using Roslyn.Utilities;
 using System.Linq;
-using System.Collections.Immutable;
-using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.EditorConfigSettings.Data;
-using System.Collections.Generic;
-using Microsoft.CodeAnalysis.Options;
-using System.Net.WebSockets;
-using Microsoft.CodeAnalysis.Internal.Log;
-using Microsoft.CodeAnalysis.Editor.EditorConfigSettings.DataProvider;
-using Microsoft.CodeAnalysis.Collections.Internal;
-using System.Text.RegularExpressions;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using static Microsoft.CodeAnalysis.ExternalAccess.EditorConfig.Features.SettingsHelper;
+using System.Collections.Immutable;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.EditorConfig.Features
 {
     internal class SettingsHelper
     {
-        public struct SettingsSnapshots
-        {
-            public ImmutableArray<CodeStyleSetting>? codeStyleSnapshot;
-            public ImmutableArray<WhitespaceSetting>? whitespaceSnapshot;
-            public ImmutableArray<AnalyzerSetting>? analyzerSnapshot;
-        }
-
-        public static SettingsSnapshots GetSettingsSnapshots(Workspace workspace, string filePath)
+        public static ImmutableArray<IEditorConfigSettingInfo> GetSettingsSnapshots(Workspace workspace, string filePath)
         {
             var settingsAggregator = workspace.Services.GetRequiredService<ISettingsAggregator>();
 
@@ -47,16 +22,11 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.EditorConfig.Features
             var whitespaceProvider = settingsAggregator.GetSettingsProvider<WhitespaceSetting>(filePath);
             var analyzerProvider = settingsAggregator.GetSettingsProvider<AnalyzerSetting>(filePath);
 
-            var codeStyleSnapshot = codeStyleProvider?.GetCurrentDataSnapshot();
-            var whitespaceSnapshot = whitespaceProvider?.GetCurrentDataSnapshot();
-            var analyzerSnapshot = analyzerProvider?.GetCurrentDataSnapshot();
+            var codeStyleSnapshot = (IEditorConfigSettingInfo[])codeStyleProvider?.GetCurrentDataSnapshot().ToArray()!;
+            var whitespaceSnapshot = (IEditorConfigSettingInfo[])whitespaceProvider?.GetCurrentDataSnapshot().ToArray()!;
+            var analyzerSnapshot = (IEditorConfigSettingInfo[])analyzerProvider?.GetCurrentDataSnapshot().ToArray()!;
 
-            return new SettingsSnapshots
-            {
-                codeStyleSnapshot = codeStyleSnapshot,
-                whitespaceSnapshot = whitespaceSnapshot,
-                analyzerSnapshot = analyzerSnapshot,
-            };
+            return codeStyleSnapshot.Concat(whitespaceSnapshot).Concat(analyzerSnapshot).ToImmutableArray();
         }
     }
 }
