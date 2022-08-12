61599aa60aa9b87421aef50039768f82796cc392
Jared Parsons <jaredpparsons@gmail.com>
2022-08-12 11:33:14 -0700

    Fix generator error on command line (#63347)
    * Fix generator error on command line
    
    The latest compiler was throwing when generators took advantage of the
    new API `ForAttributeWithMetadataName` with an exception trying to
    compare `CompilationOption` values. The reason is that we didn't plumb
    `ReferenceEqualityComparer` in both the places it was needed. Doing that
    fixed the bug.
    
    The reason this was never caught during testing is that we create
    `CompilationOptions` differently in testing vs. the command line. The
    command line inserts a `LoggingMetadataFileReferenceResolver` instance
    which throws on all equality methods. The reasons for this are unknown
    but it is the behavior we have. The `CompilationOptions` used in testing
    do normal value based equality checks. This created a significant test
    gap for us.
    
    This change introduces a new `CompilationOptions` helper that throws on
    equality checks. Once that was introduced into the generator tests we
    saw everything fail as expected. After fixing the bug the tests all
    moved back to passing.
    
    closes #63318
    
    * PR feedback

--- src/Compilers/Test/Utilities/CSharp/TestOptions.cs
+++ src/Compilers/Test/Utilities/CSharp/TestOptions.cs
@@ -69,6 +69,9 @@ namespace Microsoft.CodeAnalysis.CSharp.Test.Utilities
         public static readonly CSharpCompilationOptions DebugDll = CreateTestOptions(OutputKind.DynamicallyLinkedLibrary, OptimizationLevel.Debug);
         public static readonly CSharpCompilationOptions DebugExe = CreateTestOptions(OutputKind.ConsoleApplication, OptimizationLevel.Debug);
 
+        public static readonly CSharpCompilationOptions DebugDllThrowing = DebugDll.WithMetadataReferenceResolver(new ThrowingMetadataReferenceResolver());
+        public static readonly CSharpCompilationOptions DebugExeThrowing = DebugExe.WithMetadataReferenceResolver(new ThrowingMetadataReferenceResolver());
+
         public static readonly CSharpCompilationOptions ReleaseWinMD = CreateTestOptions(OutputKind.WindowsRuntimeMetadata, OptimizationLevel.Release);
         public static readonly CSharpCompilationOptions DebugWinMD = CreateTestOptions(OutputKind.WindowsRuntimeMetadata, OptimizationLevel.Debug);
 
