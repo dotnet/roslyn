3f675c28e67ac47fea85e9eeeac85a83a11a2c8e
AraHaan <seandhunt_7@yahoo.com>
2022-08-14 17:26:42 -0400

    Update src/Features/LanguageServer/ProtocolUnitTests/Hover/HoverTests.cs

--- src/Features/LanguageServer/ProtocolUnitTests/Hover/HoverTests.cs
+++ src/Features/LanguageServer/ProtocolUnitTests/Hover/HoverTests.cs
@@ -84,7 +84,7 @@ namespace Microsoft.CodeAnalysis.LanguageServer.UnitTests.Hover
             using var testLspServer = await CreateTestLspServerAsync(markup, CapabilitiesWithVSExtensions);
             var expectedLocation = testLspServer.GetLocations("caret").Single();
             var results = await RunGetHoverAsync(testLspServer, expectedLocation).ConfigureAwait(false);
-            VerifyVSContent(results, $"string A.Method(int i)|A great method|{FeaturesResources.Exceptions_colon}|  System.NullReferenceException: Line 1|Line 2|Line3");
+            VerifyVSContent(results, $"string A.Method(int i)|A great method|{FeaturesResources.Exceptions_colon}|  System.NullReferenceException: Line 1.|Line 2.|Line3.");
         }
 
         [Fact]
