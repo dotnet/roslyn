705da2a550e80b857db813ec118620ce92c423b5
Daniel David Sanchez <t-danield@microsoft.com>
2022-08-15 09:57:51 -0700

    file creation

---
+++ src/EditorFeatures/Core/EditorConfigSettings/DataProvider/EditorConfigData.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis.EditAndContinue;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.EditorConfigSettings.DataProvider
+{
+    internal class EditorConfigData<T> where T : notnull
+    {
+        public string SettingName { get; }
+        public string SettingNameDocumentation { get; }
+        private readonly BidirectionalMap<string, T> ValueToSettingName;
+        private readonly ImmutableDictionary<T, string> ValueToValueDocumentation;
+
+        public EditorConfigData(string settingName, string settingNameDocumentation, BidirectionalMap<string, T> valueToSettingName, ImmutableDictionary<T, string> valueToValueDocumentation)
+        {
+            SettingName = settingName;
+            SettingNameDocumentation = settingNameDocumentation;
+            ValueToSettingName = valueToSettingName;
+            ValueToValueDocumentation = valueToValueDocumentation;
+        }
+
+        public string? GetEditorConfigStringFromValue(T value)
+        {
+            ValueToSettingName.TryGetKey(value, out var key);
+            return key;
+        }
+
+        public T? GetValueFromEditorConfigString(string key)
+        {
+            ValueToSettingName.TryGetValue(key, out var value);
+            return value;
+        }
+
+        public string? GetValueDocumentation(T key)
+        {
+            ValueToValueDocumentation.TryGetValue(key, out var value);
+            return value;
+        }
+    }
+}
---
+++ src/EditorFeatures/Core/EditorConfigSettings/DataProvider/WhitespaceEditorConfigSettings.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis.EditAndContinue;
+
+namespace Microsoft.CodeAnalysis.EditorConfigSettings.DataProvider
+{
+    //yield return WhitespaceSetting.Create(FormattingOptions2.UseTabs, EditorFeaturesResources.Use_Tabs, editorConfigOptions, visualStudioOptions, updater, FileName);
+    //yield return WhitespaceSetting.Create(FormattingOptions2.TabSize, EditorFeaturesResources.Tab_Size, editorConfigOptions, visualStudioOptions, updater, FileName);
+    //yield return WhitespaceSetting.Create(FormattingOptions2.IndentationSize, EditorFeaturesResources.Indentation_Size, editorConfigOptions, visualStudioOptions, updater, FileName);
+    //yield return WhitespaceSetting.Create(FormattingOptions2.NewLine, EditorFeaturesResources.New_Line, editorConfigOptions, visualStudioOptions, updater, FileName);
+    //yield return WhitespaceSetting.Create(FormattingOptions2.InsertFinalNewLine, EditorFeaturesResources.Insert_Final_Newline, editorConfigOptions, visualStudioOptions, updater, FileName);
+    //yield return WhitespaceSetting.Create(CodeStyleOptions2.OperatorPlacementWhenWrapping, EditorFeaturesResources.Operator_placement_when_wrapping, editorConfigOptions, visualStudioOptions, updater, FileName);
+    //private readonly BidirectionalMap<string, bool> _boolMap 
+    internal partial class WhitespaceEditorConfigSettings
+    {
+        //public static EditorConfigData<int> Use_Tabs = new("indent_style", EditorFeaturesResources.Use_Tabs);
+    }
+}
