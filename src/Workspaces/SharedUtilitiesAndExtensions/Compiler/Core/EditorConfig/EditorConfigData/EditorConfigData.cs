2c5ad7a61277cb36539f404e195db307c611b858
Daniel David Sanchez <t-danield@microsoft.com>
2022-08-18 10:03:33 -0700

    added folders

---
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/EditorConfig/EditorConfigData/EditorConfigData.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.Text;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.EditorConfigSettings
+{
+    internal interface IEditorConfigData
+    {
+        public string GetSettingName();
+        public string GetSettingNameDocumentation();
+        public bool GetAllowsMultipleValues();
+        public ImmutableArray<string> GetAllSettingValues();
+        public string? GetSettingValueDocumentation(string key);
+    }
+
+    internal abstract class EditorConfigData<T> : IEditorConfigData where T : notnull
+    {
+        public string SettingName { get; }
+        public string SettingNameDocumentation { get; }
+        public bool AllowsMultipleValues { get; }
+
+        public EditorConfigData(string settingName, string settingNameDocumentation, bool allowsMultipleValues = false)
+        {
+            SettingName = settingName;
+            SettingNameDocumentation = settingNameDocumentation;
+            AllowsMultipleValues = allowsMultipleValues;
+        }
+
+        public abstract string GetSettingName();
+        public abstract string GetSettingNameDocumentation();
+        public abstract bool GetAllowsMultipleValues();
+
+        public abstract ImmutableArray<string> GetAllSettingValues();
+        public abstract string? GetSettingValueDocumentation(string key);
+        public abstract string GetEditorConfigStringFromValue(T value);
+        public abstract Optional<T> GetValueFromEditorConfigString(string key);
+    }
+}
---
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/EditorConfig/EditorConfigData/StringEditorConfigData.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.EditorConfigSettings
+{
+    internal class StringEditorConfigData : EditorConfigData<string>
+    {
+        private readonly BidirectionalMap<string, string> ValueToSettingName;
+        //private readonly ImmutableDictionary<string, string>? ValueToValueDocumentation;
+        private readonly string DefaultEditorConfigString;
+        private readonly string DefaultValue;
+
+        public StringEditorConfigData(string settingName, string settingNameDocumentation, BidirectionalMap<string, string> valueToSettingName, string defaultEditorConfigString, string defaultValue)
+            : base(settingName, settingNameDocumentation)
+        {
+            ValueToSettingName = valueToSettingName;
+            DefaultEditorConfigString = defaultEditorConfigString;
+            DefaultValue = defaultValue;
+        }
+
+        public override string GetSettingName() => SettingName;
+
+        public override string GetSettingNameDocumentation() => SettingNameDocumentation;
+
+        public override bool GetAllowsMultipleValues() => AllowsMultipleValues;
+
+        public override ImmutableArray<string> GetAllSettingValues()
+        {
+            return ValueToSettingName.Keys.ToImmutableArray();
+        }
+
+        public override string? GetSettingValueDocumentation(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetEditorConfigStringFromValue(string value)
+        {
+            return ValueToSettingName.TryGetKey(value, out var key) ? key : DefaultEditorConfigString;
+        }
+
+        public override Optional<string> GetValueFromEditorConfigString(string key)
+        {
+            return ValueToSettingName.TryGetValue(key, out var value) ? value : DefaultValue;
+        }
+    }
+}
