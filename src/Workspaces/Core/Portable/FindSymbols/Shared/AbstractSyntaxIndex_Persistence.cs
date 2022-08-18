92d83330a1ba10b82495b01697c468c8d26fcd9d
Shen Chen <Cosifne@users.noreply.github.com>
2022-08-18 11:57:59 -0700

    Revert "Revert "Merge pull request #61568 from CyrusNajmabadi/localFuncindexing""

--- src/Workspaces/Core/Portable/FindSymbols/Shared/AbstractSyntaxIndex_Persistence.cs
+++ src/Workspaces/Core/Portable/FindSymbols/Shared/AbstractSyntaxIndex_Persistence.cs
@@ -19,7 +19,6 @@ namespace Microsoft.CodeAnalysis.FindSymbols
     internal partial class AbstractSyntaxIndex<TIndex> : IObjectWritable
     {
         private static readonly string s_persistenceName = typeof(TIndex).Name;
-
         private static readonly Checksum s_serializationFormatChecksum = Checksum.Create("31");
 
         /// <summary>
