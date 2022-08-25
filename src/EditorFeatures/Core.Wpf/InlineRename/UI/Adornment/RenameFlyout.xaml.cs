4145c9d6a4cfa6f093d46f98e8149d80cb1b0aaf
Andrew Hall <ryzngard@live.com>
2022-08-25 12:32:30 -0700

    Nothing to see here >.>

--- src/EditorFeatures/Core.Wpf/InlineRename/UI/Adornment/RenameFlyout.xaml.cs
+++ src/EditorFeatures/Core.Wpf/InlineRename/UI/Adornment/RenameFlyout.xaml.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
