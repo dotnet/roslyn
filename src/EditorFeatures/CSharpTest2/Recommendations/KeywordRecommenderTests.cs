c2b7cfeeeefbf14d6b6da3d1ad4c5c786b652d46
DoctorKrolic <mapmyp03@gmail.com>
2022-08-30 22:10:02 +0300

    Sort usings

--- src/EditorFeatures/CSharpTest2/Recommendations/KeywordRecommenderTests.cs
+++ src/EditorFeatures/CSharpTest2/Recommendations/KeywordRecommenderTests.cs
@@ -2,12 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.CodeAnalysis.CSharp.Completion.KeywordRecommenders;
-using System.Reflection;
 using System;
 using System.Collections.Generic;
-using Microsoft.CodeAnalysis.CSharp;
+using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Completion.KeywordRecommenders;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Recommendations
