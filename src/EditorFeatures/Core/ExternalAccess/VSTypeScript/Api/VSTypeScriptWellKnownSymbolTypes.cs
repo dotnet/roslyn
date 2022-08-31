d0c6023d26199c10b33f35b1a24a5459f8c497b9
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-08-31 13:32:45 -0700

    Add docs

--- src/EditorFeatures/Core/ExternalAccess/VSTypeScript/Api/VSTypeScriptWellKnownSymbolTypes.cs
+++ src/EditorFeatures/Core/ExternalAccess/VSTypeScript/Api/VSTypeScriptWellKnownSymbolTypes.cs
@@ -6,6 +6,9 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.VSTypeScript.Api
 {
     internal static class VSTypeScriptWellKnownSymbolTypes
     {
+        /// <summary>
+        /// Exists for binary compat.  Not actually used for any purpose.
+        /// </summary>
         public const string Definition = nameof(Definition);
     }
 }
