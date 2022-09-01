bb3e871c1d44cd3b310f78183e849e33aa855180
Fredric Silberberg <frsilb@microsoft.com>
2022-09-01 14:56:55 -0700

    Add default label test.

--- src/Compilers/CSharp/Test/Semantic/Semantics/UsingDeclarationTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/UsingDeclarationTests.cs
@@ -696,6 +696,41 @@ class C2
             verifier.VerifyDiagnostics();
         }
 
+        [Fact, WorkItem(63570, "https://github.com/dotnet/roslyn/issues/63570")]
+        public void UsingVariableInSwitchCase_04()
+        {
+            var source = """
+                using System;
+                StringSplitOptions temp = StringSplitOptions.RemoveEmptyEntries;
+                switch (temp)
+                {
+                    case StringSplitOptions.RemoveEmptyEntries:
+                    {
+                        using var streamReader = new C1();
+                        goto default;
+                    }
+
+                    default:
+                        Console.WriteLine("Default");
+                        break;
+                }
+                
+                class C1 : IDisposable
+                {
+                    public void Dispose()
+                    {
+                        Console.WriteLine("Disposed");
+                    }
+                }
+                """;
+
+            var verifier = CompileAndVerify(source, expectedOutput: """
+                Disposed
+                Default
+                """);
+            verifier.VerifyDiagnostics();
+        }
+
         [Fact]
         public void UsingVariableDiagnosticsInDeclarationAreOnlyEmittedOnce()
         {
