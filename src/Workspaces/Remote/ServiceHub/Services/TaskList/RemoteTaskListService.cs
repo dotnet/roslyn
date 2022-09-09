79bcb377029761f37fe665166f09f9142769645d
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-09 12:35:59 -0700

    Fix

--- src/Workspaces/Remote/ServiceHub/Services/TaskList/RemoteTaskListService.cs
+++ src/Workspaces/Remote/ServiceHub/Services/TaskList/RemoteTaskListService.cs
@@ -2,11 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.SolutionCrawler;
 using Microsoft.CodeAnalysis.TaskList;
-using Microsoft.CodeAnalysis.TodoComments;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Remote
