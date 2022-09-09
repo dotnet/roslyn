5d3e9f7aa3b83575935e662f1a6d1fbbe630dfdd
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-09 12:49:12 -0700

    move

--- src/Workspaces/Core/Portable/TaskList/TaskListItemDescriptor.cs
+++ src/Workspaces/Core/Portable/TaskList/TaskListItemDescriptor.cs
@@ -2,14 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Collections.Immutable;
 using System.Globalization;
-using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using Microsoft.CodeAnalysis.PooledObjects;
-using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.TaskList
 {
