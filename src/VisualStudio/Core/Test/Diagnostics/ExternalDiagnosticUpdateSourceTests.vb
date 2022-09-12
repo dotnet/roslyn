c9fe0585eb00f2f820ef1c5c6f4ee346a072d614
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-12 10:44:10 -0700

    Fix tests

--- src/VisualStudio/Core/Test/Diagnostics/ExternalDiagnosticUpdateSourceTests.vb
+++ src/VisualStudio/Core/Test/Diagnostics/ExternalDiagnosticUpdateSourceTests.vb
@@ -404,7 +404,7 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Diagnostics
                     customTags:=ImmutableArray(Of String).Empty,
                     properties:=ImmutableDictionary(Of String, String).Empty,
                     project.Id,
-                    location:=New DiagnosticDataLocation(documentId:=Nothing, sourceSpan:=Nothing, New FileLinePositionSpan("Test.txt", New LinePosition(4, 4), Nothing)),
+                    location:=New DiagnosticDataLocation(documentId:=Nothing, sourceSpan:=Nothing, New FileLinePositionSpan("Test.txt", New LinePosition(4, 4), New LinePosition(4, 4))),
                     language:=project.Language)
 
                     AddHandler service.DiagnosticsUpdated, Sub(o, args)
