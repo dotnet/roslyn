3b33bb5de593d9b5de684b5fb8e3e373d35cdc04
Jan Jones <janjones@microsoft.com>
2022-09-12 14:38:16 +0200

    Suggest `base` members first

--- src/Features/Core/Portable/GenerateConstructorFromMembers/AbstractGenerateConstructorFromMembersCodeRefactoringProvider.cs
+++ src/Features/Core/Portable/GenerateConstructorFromMembers/AbstractGenerateConstructorFromMembersCodeRefactoringProvider.cs
@@ -202,7 +202,11 @@ namespace Microsoft.CodeAnalysis.GenerateConstructorFromMembers
             // Find all the possible writable instance fields/properties.  If there are any, then
             // show a dialog to the user to select the ones they want.  Otherwise, if there are none
             // don't offer to generate anything.
-            var viableMembers = containingType.GetAccessibleMembersInThisAndBaseTypes<ISymbol>(containingType).WhereAsArray(IsWritableInstanceFieldOrProperty);
+            // First suggest `base` members, then `this` members.
+            var viableMembers = containingType.GetAccessibleMembersInBaseTypes<ISymbol>(containingType)
+                .Concat(containingType.GetMembers())
+                .Where(IsWritableInstanceFieldOrProperty)
+                .ToImmutableArray();
             if (viableMembers.Length == 0)
             {
                 return null;
