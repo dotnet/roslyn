69465e508428d7067fc9587f339bf22a88671a67
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-12 11:20:06 -0700

    File Scoped Namespaces

--- src/Features/Core/Portable/ExternalAccess/VSTypeScript/Api/IVSTypeScriptTodoCommentDataServiceImplementation.cs
+++ src/Features/Core/Portable/ExternalAccess/VSTypeScript/Api/IVSTypeScriptTodoCommentDataServiceImplementation.cs
@@ -8,40 +8,39 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.TaskList;
 using Microsoft.CodeAnalysis.TodoComments;
 
-namespace Microsoft.CodeAnalysis.ExternalAccess.VSTypeScript.Api
+namespace Microsoft.CodeAnalysis.ExternalAccess.VSTypeScript.Api;
+
+internal readonly struct VSTypeScriptTaskListItem
 {
-    internal readonly struct VSTypeScriptTaskListItem
+    public VSTypeScriptTaskListItem(VSTypeScriptTaskListItemDescriptorWrapper descriptor, string message, int position)
     {
-        public VSTypeScriptTaskListItem(VSTypeScriptTaskListItemDescriptorWrapper descriptor, string message, int position)
-        {
-            Descriptor = descriptor;
-            Message = message;
-            Position = position;
-        }
-
-        public VSTypeScriptTaskListItemDescriptorWrapper Descriptor { get; }
-        public string Message { get; }
-        public int Position { get; }
+        Descriptor = descriptor;
+        Message = message;
+        Position = position;
     }
 
-    internal readonly struct VSTypeScriptTaskListItemDescriptorWrapper
+    public VSTypeScriptTaskListItemDescriptorWrapper Descriptor { get; }
+    public string Message { get; }
+    public int Position { get; }
+}
+
+internal readonly struct VSTypeScriptTaskListItemDescriptorWrapper
+{
+    internal readonly TaskListItemDescriptor Descriptor;
+
+    public string Text => Descriptor.Text;
+
+    internal VSTypeScriptTaskListItemDescriptorWrapper(TaskListItemDescriptor descriptor)
     {
-        internal readonly TaskListItemDescriptor Descriptor;
-
-        public string Text => Descriptor.Text;
-
-        internal VSTypeScriptTaskListItemDescriptorWrapper(TaskListItemDescriptor descriptor)
-        {
-            Descriptor = descriptor;
-        }
-
-        public static ImmutableArray<VSTypeScriptTaskListItemDescriptorWrapper> Parse(ImmutableArray<string> items)
-            => TaskListItemDescriptor.Parse(items).SelectAsArray(d => new VSTypeScriptTaskListItemDescriptorWrapper(d));
+        Descriptor = descriptor;
     }
 
-    internal interface IVSTypeScriptTaskListServiceImplementation
-    {
-        Task<ImmutableArray<VSTypeScriptTaskListItem>> GetTaskListItemsAsync(
-            Document document, ImmutableArray<VSTypeScriptTaskListItemDescriptorWrapper> value, CancellationToken cancellationToken);
-    }
+    public static ImmutableArray<VSTypeScriptTaskListItemDescriptorWrapper> Parse(ImmutableArray<string> items)
+        => TaskListItemDescriptor.Parse(items).SelectAsArray(d => new VSTypeScriptTaskListItemDescriptorWrapper(d));
+}
+
+internal interface IVSTypeScriptTaskListServiceImplementation
+{
+    Task<ImmutableArray<VSTypeScriptTaskListItem>> GetTaskListItemsAsync(
+        Document document, ImmutableArray<VSTypeScriptTaskListItemDescriptorWrapper> value, CancellationToken cancellationToken);
 }
