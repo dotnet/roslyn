bcf77dc117df8a19c770200410643b3d43ae6766
AlekseyTs <AlekseyTs@users.noreply.github.com>
2022-09-13 08:16:40 -0700

    Formatting

--- src/Compilers/CSharp/Test/Semantic/Semantics/RefFieldTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/RefFieldTests.cs
@@ -16107,7 +16107,6 @@ ref struct A
                 Diagnostic(ErrorCode.ERR_RefReturnStructThis, "this").WithLocation(8, 9)
                 );
         }
-        
         [Theory]
         [InlineData(LanguageVersion.CSharp10)]
         [InlineData(LanguageVersion.CSharp11)]
