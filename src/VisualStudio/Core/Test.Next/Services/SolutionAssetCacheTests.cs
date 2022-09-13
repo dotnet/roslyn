011cf533a137ff283e7421f115b1383225af4cc1
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-13 12:47:41 -0700

    Update tests

--- src/VisualStudio/Core/Test.Next/Services/SolutionAssetCacheTests.cs
+++ src/VisualStudio/Core/Test.Next/Services/SolutionAssetCacheTests.cs
@@ -73,17 +73,45 @@ namespace Roslyn.VisualStudio.Next.UnitTests.Remote
             var storage = new SolutionAssetCache(
                 workspace, cleanupInterval: TimeSpan.FromMilliseconds(1), purgeAfter: TimeSpan.FromMilliseconds(2), gcAfter: TimeSpan.FromMilliseconds(5));
 
-            var data = new object();
+            var checksum1 = checksums.Checksum;
+            var data1 = new object();
 
-            Assert.Equal(data, storage.GetOrAdd(checksums.Checksum, data));
+            var checksum2 = Checksum.Create(ImmutableArray.CreateRange(Guid.NewGuid().ToByteArray()));
+            var data2 = new object();
+
+            Assert.Equal(data1, storage.GetOrAdd(checksum1, data1));
+            Assert.Equal(data2, storage.GetOrAdd(checksum2, data2));
+
+            var gotChecksum2 = true;
 
             for (var i = 0; i < 10; i++)
             {
                 await Task.Delay(10);
 
                 // We should always get this asset, as the solution is keeping it alive.
-                Assert.True(storage.TryGetAsset(checksums.Checksum, out object _));
+                Assert.True(storage.TryGetAsset(checksum1, out object current1));
+                Assert.Equal(data1, current1);
+
+                gotChecksum2 = storage.TryGetAsset(checksum2, out object _);
             }
+
+            // By the end, checksum2/data2 should be gone.
+            Assert.False(gotChecksum2);
+
+            // Now, add a project.  At this point, the original pinned object should go away.
+            workspace.SetCurrentSolution(solution => solution.AddProject("Project", "Assembly", LanguageNames.CSharp).Solution, WorkspaceChangeKind.ProjectAdded);
+
+            for (var i = 0; i < 10; i++)
+            {
+                await Task.Delay(10);
+
+                // Eventually, this asset should go away.
+                if (!storage.TryGetAsset(checksum1, out object _))
+                    return;
+            }
+
+            // it should not reach here
+            Assert.True(false, "asset not cleaned up");
         }
     }
 }
