6040df77d644a5f3d12db604d8ca15579579c854
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-14 10:39:20 -0700

    Docs

--- src/Features/Core/Portable/Navigation/ICrossLanguageSymbolNavigationService.cs
+++ src/Features/Core/Portable/Navigation/ICrossLanguageSymbolNavigationService.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace Microsoft.CodeAnalysis.Navigation
 {
+    /// <summary>
+    /// Service to allow 3rd party languages to handle navigating to some metadata symbol.  For example, this can allow
+    /// a language like F# to navigate to its source definition of a symbol that roslyn views as a metadata symbol.
+    /// </summary>
     internal interface ICrossLanguageSymbolNavigationService
     {
+        /// <summary>
+        /// Attempts to get location that can be navigated to for a particular symbol id.  The symbol id format is
+        /// defined at: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/xmldoc/#id-strings.  Should
+        /// return <see langword="null"/> if the 3rd party language cannot navigate to this particular symbol.
+        /// </summary>
         Task<INavigableLocation?> TryGetNavigableLocationAsync(string documentationCommentId, CancellationToken cancellationToken);
     }
 }
