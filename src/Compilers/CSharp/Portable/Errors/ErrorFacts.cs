c0746075031ceaf0e370ae84ca31bd9eb00523ea
AlekseyTs <AlekseyTs@users.noreply.github.com>
2022-09-14 14:08:00 -0700

    Do not report an error and eat `scoped` identifier following `ref` keyword.
    Fixes #63986.

--- src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
@@ -2212,7 +2212,6 @@ namespace Microsoft.CodeAnalysis.CSharp
                 case ErrorCode.ERR_BadParameterModifiersOrder:
                 case ErrorCode.WRN_AnalyzerReferencesNewerCompiler:
                 case ErrorCode.ERR_CannotMatchOnINumberBase:
-                case ErrorCode.ERR_MisplacedScoped:
                 case ErrorCode.ERR_ScopedTypeNameDisallowed:
                 case ErrorCode.ERR_UnscopedRefAttributeUnsupportedTarget:
                 case ErrorCode.ERR_RuntimeDoesNotSupportRefFields:
