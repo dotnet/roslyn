a58f93eef10aac0beac75acdbdc4f891ae4a9965
Daniel Balas <daniel@postsharp.net>
2022-09-16 13:43:53 +0200

    #31076 Repro.

--- src/Metalama/Metalama.Compiler.UnitTests/TreeTrackerTests.cs
+++ src/Metalama/Metalama.Compiler.UnitTests/TreeTrackerTests.cs
@@ -45,33 +45,71 @@ namespace Metalama.Compiler.UnitTests
             Assert.Same(originalBlock.Statements[0], TreeTracker.GetSourceSyntaxNode(block.Statements[0]));
             Assert.Same(block.Statements[1], TreeTracker.GetSourceSyntaxNode(block.Statements[1]));
         }
-        
+
         [Fact]
         public void CopyOriginalLocation()
         {
-             var originalBlock = (BlockSyntax)SyntaxFactory.ParseStatement("{ return 5; }");
-            
+            var originalBlock = (BlockSyntax)SyntaxFactory.ParseStatement("{ return 5; }");
+
             var annotatedBlock = TreeTracker.AnnotateNodeAndChildren(originalBlock);
 
-            var modifiedBlock = new ReturnRewriter().Visit(annotatedBlock);
+            var modifiedBlock = new CopyOriginalLocationRewriter().Visit(annotatedBlock);
 
+            // TODO: I don't think the following is a correct test (see the test below that also replaces trivia and fails).
             // Check that rewriting of trivias still works.
             _ = modifiedBlock.NormalizeWhitespace();
-
         }
-        
-        class ReturnRewriter : CSharpSyntaxRewriter
+
+        class CopyOriginalLocationRewriter : CSharpSyntaxRewriter
         {
             public override SyntaxNode? VisitReturnStatement(ReturnStatementSyntax node)
             {
                 var assignment = SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(
                     SyntaxKind.SimpleAssignmentExpression,
                     SyntaxFactory.IdentifierName("x"), node.Expression!));
-                
+
                 assignment = assignment.WithOriginalLocationAnnotationFrom(node);
 
                 return SyntaxFactory.Block(assignment,
                     SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName("x")));
+            }
+        }
+
+        // TODO: #31076.
+        [Fact]
+        public void CopyOriginalLocationTwice()
+        {
+            var originalBlock = (BlockSyntax)SyntaxFactory.ParseStatement("{ return 5; }");
+
+            var annotatedBlock = TreeTracker.AnnotateNodeAndChildren(originalBlock);
+
+            var modifiedBlock1 = new CopyOriginalLocationTwiceRewriter1().Visit(annotatedBlock);
+            var modifiedBlock2 = new CopyOriginalLocationTwiceRewriter2().Visit(modifiedBlock1);
+
+            // Check that rewriting of trivias still works.
+            _ = modifiedBlock2.NormalizeWhitespace();
+        }
+
+        class CopyOriginalLocationTwiceRewriter1 : CSharpSyntaxRewriter
+        {
+            public override SyntaxNode? VisitReturnStatement(ReturnStatementSyntax node)
+            {
+                var assignment = SyntaxFactory.ExpressionStatement(SyntaxFactory.AssignmentExpression(
+                    SyntaxKind.SimpleAssignmentExpression,
+                    SyntaxFactory.IdentifierName("x"), node.Expression!));
+
+                assignment = assignment.WithOriginalLocationAnnotationFrom(node);
+
+                return SyntaxFactory.Block(assignment,
+                    SyntaxFactory.ReturnStatement(SyntaxFactory.IdentifierName("x")));
+            }
+        }
+
+        class CopyOriginalLocationTwiceRewriter2 : CSharpSyntaxRewriter
+        {
+            public override SyntaxNode? VisitExpressionStatement(ExpressionStatementSyntax node)
+            {
+                return node.WithTrailingTrivia(node.GetTrailingTrivia().Add(SyntaxFactory.ElasticLineFeed));
             }
         }
     }
