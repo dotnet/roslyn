ad7de72e03d4a227ed9a63f7c9956864665c1d92
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-16 10:07:08 -0700

    Remove unused extension

--- src/Workspaces/Core/Portable/Shared/Extensions/FileLinePositionSpanExtensions.cs
+++ src/Workspaces/Core/Portable/Shared/Extensions/FileLinePositionSpanExtensions.cs
@@ -6,10 +6,5 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
 {
     internal static class FileLinePositionSpanExtensions
     {
-        /// <summary>
-        /// Get mapped file path if exist, otherwise return null.
-        /// </summary>
-        public static string? GetMappedFilePathIfExist(this FileLinePositionSpan fileLinePositionSpan)
-            => fileLinePositionSpan.HasMappedPath ? fileLinePositionSpan.Path : null;
     }
 }
