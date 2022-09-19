d90eacf79b0bb6d50901f9b34be457a9f96fab71
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-19 13:48:19 -0700

    Fork the solutoin crawler code to the unit testing EA lib

---
+++ src/Features/Core/Portable/ExternalAccess/UnitTesting/SolutionCrawler/IDocumentDifferenceService.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.SolutionCrawler
+{
+    internal class DocumentDifferenceResult
+    {
+        public InvocationReasons ChangeType { get; }
+        public SyntaxNode? ChangedMember { get; }
+
+        public DocumentDifferenceResult(InvocationReasons changeType, SyntaxNode? changedMember = null)
+        {
+            ChangeType = changeType;
+            ChangedMember = changedMember;
+        }
+    }
+
+    internal interface IDocumentDifferenceService : ILanguageService
+    {
+        Task<DocumentDifferenceResult?> GetDifferenceAsync(Document oldDocument, Document newDocument, CancellationToken cancellationToken);
+    }
+}
