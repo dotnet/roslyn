360284fd7c337a7c5ab9256bbf2132b2a7b1c020
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-19 13:22:19 -0700

    Cleanup

--- src/Features/Core/Portable/SolutionCrawler/ISolutionCrawlerService.cs
+++ src/Features/Core/Portable/SolutionCrawler/ISolutionCrawlerService.cs
@@ -3,10 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
-using Microsoft.CodeAnalysis.Remote;
 
 namespace Microsoft.CodeAnalysis.SolutionCrawler
 {
