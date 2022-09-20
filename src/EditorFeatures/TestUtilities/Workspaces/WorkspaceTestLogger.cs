af9d51c8197f45eb54cf530f8a4b333595279c3b
tmat <tomas.matousek@microsoft.com>
2022-09-20 15:14:22 -0700

    Use workspace services to implement test hooks, instead of directly manipulating workspace instance

---
+++ src/EditorFeatures/TestUtilities/Workspaces/WorkspaceTestLogger.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.CodeAnalysis.Host;
+
+internal class WorkspaceTestLogger : IWorkspaceTestLogger
+{
+    public ITestOutputHelper? OutputHelper { get; set; }
+
+    public void Log(string message)
+        => OutputHelper?.WriteLine(message);
+}
+
+internal static class WorkspaceTestLoggerExtensions
+{
+    public static void SetWorkspaceTestOutput(this SolutionServices services, ITestOutputHelper outputHelper)
+        => Assert.IsType<WorkspaceTestLogger>(services.GetRequiredService<IWorkspaceTestLogger>()).OutputHelper = outputHelper;
+}
+
