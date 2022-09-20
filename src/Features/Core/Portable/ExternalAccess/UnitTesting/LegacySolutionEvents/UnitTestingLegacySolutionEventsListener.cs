8b7fd47b4ccf8b26300caba3f1c73c5d1b2ba32a
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-20 12:06:37 -0700

    build

---
+++ src/Features/Core/Portable/ExternalAccess/UnitTesting/LegacySolutionEvents/UnitTestingLegacySolutionEventsListener.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.LegacySolutionEvents;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.LegacySolutionEvents
+{
+    [Export(typeof(ILegacySolutionEventsListener)), Shared]
+    internal class UnitTestingLegacySolutionEventsListener : ILegacySolutionEventsListener
+    {
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public UnitTestingLegacySolutionEventsListener()
+        {
+        }
+
+        public ValueTask OnWorkspaceChangedEventAsync(WorkspaceChangeEventArgs args, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask OnTextDocumentOpenedAsync(TextDocumentEventArgs args, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask OnTextDocumentClosedAsync(TextDocumentEventArgs args, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
