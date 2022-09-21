f5289049bfb61d04c58eeba0137dc6a39477eb5c
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-20 17:08:04 -0700

    Add back

--- src/Workspaces/Remote/Core/Serialization/MessagePackFormatters.cs
+++ src/Workspaces/Remote/Core/Serialization/MessagePackFormatters.cs
@@ -23,6 +23,7 @@ namespace Microsoft.CodeAnalysis.Remote
     internal sealed class MessagePackFormatters
     {
         internal static readonly ImmutableArray<IMessagePackFormatter> Formatters = ImmutableArray.Create<IMessagePackFormatter>(
+            ProjectIdFormatter.Instance,
             // ForceTypelessFormatter<T> needs to be listed here for each Roslyn abstract type T that is being serialized OOP.
             // TODO: add a resolver that provides these https://github.com/dotnet/roslyn/issues/60724
             new ForceTypelessFormatter<SimplifierOptions>(),
@@ -37,5 +38,74 @@ namespace Microsoft.CodeAnalysis.Remote
 
         internal static IFormatterResolver CreateResolver(ImmutableArray<IMessagePackFormatter> additionalFormatters, ImmutableArray<IFormatterResolver> additionalResolvers)
             => (additionalFormatters.IsEmpty && additionalResolvers.IsEmpty) ? DefaultResolver : CompositeResolver.Create(Formatters.AddRange(additionalFormatters), s_resolvers.AddRange(additionalResolvers));
+
+        /// <summary>
+        /// Specialized formatter used so we can cache and reuse <see cref="ProjectId"/> instances.  This is valuable as
+        /// it's very common for a set of results to reuse the same ProjectId across long sequences of results
+        /// containing <see cref="DocumentId"/>s.  This allows a single instance to be created and shared across that
+        /// entire sequence, saving on allocations.
+        /// </summary>
+        internal sealed class ProjectIdFormatter : IMessagePackFormatter<ProjectId?>
+        {
+            public static readonly ProjectIdFormatter Instance = new();
+
+            /// <summary>
+            /// Keep a copy of the most recent project ID to avoid duplicate instances when many consecutive IDs
+            /// reference the same project.
+            /// </summary>
+            /// <remarks>
+            /// Synchronization is not required for this field, since it's only intended to be an opportunistic (lossy)
+            /// cache.
+            /// </remarks>
+            private ProjectId? _previousProjectId;
+
+            public ProjectId? Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options)
+            {
+                try
+                {
+                    if (reader.TryReadNil())
+                    {
+                        return null;
+                    }
+
+                    Contract.ThrowIfFalse(reader.ReadArrayHeader() == 2);
+                    var id = GuidFormatter.Instance.Deserialize(ref reader, options);
+                    var debugName = reader.ReadString();
+
+                    var previousId = _previousProjectId;
+                    if (previousId is not null && previousId.Id == id && previousId.DebugName == debugName)
+                        return previousId;
+
+                    var currentId = ProjectId.CreateFromSerialized(id, debugName);
+                    _previousProjectId = currentId;
+                    return currentId;
+                }
+                catch (Exception e) when (e is not MessagePackSerializationException)
+                {
+                    throw new MessagePackSerializationException(e.Message, e);
+                }
+            }
+
+            public void Serialize(ref MessagePackWriter writer, ProjectId? value, MessagePackSerializerOptions options)
+            {
+                try
+                {
+                    if (value is null)
+                    {
+                        writer.WriteNil();
+                    }
+                    else
+                    {
+                        writer.WriteArrayHeader(2);
+                        GuidFormatter.Instance.Serialize(ref writer, value.Id, options);
+                        writer.Write(value.DebugName);
+                    }
+                }
+                catch (Exception e) when (e is not MessagePackSerializationException)
+                {
+                    throw new MessagePackSerializationException(e.Message, e);
+                }
+            }
+        }
     }
 }
