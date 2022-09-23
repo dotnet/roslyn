a5753ef237c72ced12e42a3a4f27602781a6dd1a
gel@microsoft.com <gel@microsoft.com>
2022-09-23 15:06:59 -0700

    Fix FSharpCompletionServiceWithProviders

--- src/Tools/ExternalAccess/FSharp/Completion/FSharpCompletionServiceWithProviders.cs
+++ src/Tools/ExternalAccess/FSharp/Completion/FSharpCompletionServiceWithProviders.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Completion;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Completion
@@ -17,5 +19,14 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Completion
             => GetRulesImpl();
 
         internal abstract CompletionRules GetRulesImpl();
+
+        // Disallow implementation since this method is banned in Roslyn
+        public sealed override ImmutableArray<CompletionItem> FilterItems(Document document, ImmutableArray<CompletionItem> items, string filterText)
+#pragma warning disable RS0030 // Do not used banned APIs
+            => base.FilterItems(document, items, filterText);
+#pragma warning restore RS0030 // Do not used banned APIs
+
+        internal sealed override void FilterItems(Document document, IReadOnlyList<MatchResult> matchResults, string filterText, IList<MatchResult> builder)
+            => CompletionService.FilterItemsDefault(CompletionHelper.GetHelper(document), matchResults, filterText, builder);
     }
 }
