4d29fe86166c8892b2d40e769425d8723599ced5
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-09-28 15:25:42 -0700

    initial work

---
+++ src/Features/Core/Portable/ExternalAccess/UnitTesting/API/UnitTestingSearchHelpers.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.UnitTesting.Api
+{
+    internal sealed class UnitTestingSearchQuery
+    {
+        public readonly string FullyQualifiedTypeName;
+        public readonly string? MethodName;
+        public readonly int MethodArity;
+        public readonly int MethodParameterCount;
+
+        public UnitTestingSearchQuery(string fullyQualifiedTypeName, string? methodName, int methodArity, int methodParameterCount)
+        {
+            FullyQualifiedTypeName = fullyQualifiedTypeName;
+            MethodName = methodName;
+            MethodArity = methodArity;
+            MethodParameterCount = methodParameterCount;
+        }
+    }
+
+    internal readonly struct UnitTestingDocumentSpan
+    {
+        private readonly DocumentSpan _documentSpan;
+
+        internal UnitTestingDocumentSpan(DocumentSpan documentSpan)
+        {
+            _documentSpan = documentSpan;
+        }
+
+        public Document Document => _documentSpan.Document;
+        public TextSpan SourceSpan => _documentSpan.SourceSpan;
+    }
+
+    internal static class UnitTestingSearchHelpers
+    {
+        public static async Task<ImmutableArray<UnitTestingDocumentSpan>> GetSourceLocations(
+            Solution solution, UnitTestingSearchQuery query, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
