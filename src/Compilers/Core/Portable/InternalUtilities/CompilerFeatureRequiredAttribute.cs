7c1a68544be88adcc36d77e33988ea230cdb87bd
tmat <tomas.matousek@microsoft.com>
2022-10-07 10:43:22 -0700

    Fix

--- src/Compilers/Core/Portable/InternalUtilities/CompilerFeatureRequiredAttribute.cs
+++ src/Compilers/Core/Portable/InternalUtilities/CompilerFeatureRequiredAttribute.cs
@@ -17,22 +17,22 @@ namespace System.Runtime.CompilerServices
         {
             FeatureName = featureName;
         }
- 
+
         /// <summary>
         /// The name of the compiler feature.
         /// </summary>
         public string FeatureName { get; }
- 
+
         /// <summary>
         /// If true, the compiler can choose to allow access to the location where this attribute is applied if it does not understand <see cref="FeatureName"/>.
         /// </summary>
         public bool IsOptional { get; init; }
- 
+
         /// <summary>
         /// The <see cref="FeatureName"/> used for the ref structs C# feature.
         /// </summary>
         public const string RefStructs = nameof(RefStructs);
- 
+
         /// <summary>
         /// The <see cref="FeatureName"/> used for the required members C# feature.
         /// </summary>
