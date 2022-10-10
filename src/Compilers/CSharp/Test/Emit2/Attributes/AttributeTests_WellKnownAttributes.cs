9696801f06e4a72f7f6f13fd52a938220f552c4a
Youssef1313 <youssefvictor00@gmail.com>
2022-10-10 21:52:56 +0200

    Fix early attribute decoding when an argument is interpolated string

--- src/Compilers/CSharp/Test/Emit2/Attributes/AttributeTests_WellKnownAttributes.cs
+++ src/Compilers/CSharp/Test/Emit2/Attributes/AttributeTests_WellKnownAttributes.cs
@@ -13803,5 +13803,44 @@ public struct S2 { }
                 Diagnostic(ErrorCode.ERR_InvalidNamedArgument, "CharSet=0").WithArguments("CharSet").WithLocation(7, 38)
                 );
         }
+
+        [Fact]
+        public void ObsoleteWithInterpolation()
+        {
+            var source = """
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // this does not raise a warning
+        var object1 = new LegacyObject1();
+
+        // but this does
+        var object2 = new LegacyObject2();
+    }
+}
+
+[Obsolete($"Do not use {nameof(LegacyObject1)}")]
+public class LegacyObject1
+{
+}
+
+[Obsolete("Do not use" + nameof(LegacyObject2))]
+public class LegacyObject2
+{
+}
+""";
+            var comp = CreateCompilation(source);
+            comp.VerifyDiagnostics(
+                // (8,27): warning CS0618: 'LegacyObject1' is obsolete: 'Do not use LegacyObject1'
+                //         var object1 = new LegacyObject1();
+                Diagnostic(ErrorCode.WRN_DeprecatedSymbolStr, "LegacyObject1").WithArguments("LegacyObject1", "Do not use LegacyObject1").WithLocation(8, 27),
+                // (11,27): warning CS0618: 'LegacyObject2' is obsolete: 'Do not useLegacyObject2'
+                //         var object2 = new LegacyObject2();
+                Diagnostic(ErrorCode.WRN_DeprecatedSymbolStr, "LegacyObject2").WithArguments("LegacyObject2", "Do not useLegacyObject2").WithLocation(11, 27)
+                );
+        }
     }
 }
