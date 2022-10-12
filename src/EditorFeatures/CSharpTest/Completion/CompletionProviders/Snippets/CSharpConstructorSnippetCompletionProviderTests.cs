432f1aa5ccb7c3e870737fe60db15e98e6711f1e
akhera99 <ankitakhera@microsoft.com>
2022-10-12 16:39:55 -0700

    tests

--- src/EditorFeatures/CSharpTest/Completion/CompletionProviders/Snippets/CSharpConstructorSnippetCompletionProviderTests.cs
+++ src/EditorFeatures/CSharpTest/Completion/CompletionProviders/Snippets/CSharpConstructorSnippetCompletionProviderTests.cs
@@ -8,13 +8,145 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Test.Utilities;
+using Roslyn.Test.Utilities;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionProviders.Snippets
 {
     [Trait(Traits.Feature, Traits.Features.Completion)]
-    internal class CSharpConstructorSnippetCompletionProviderTests : AbstractCSharpSnippetCompletionProviderTests
+    public class CSharpConstructorSnippetCompletionProviderTests : AbstractCSharpSnippetCompletionProviderTests
     {
         protected override string ItemToCommit => "ctor";
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task ConstructorSnippetMissingInNamespace()
+        {
+            var markupBeforeCommit =
+@"namespace Namespace
+{
+    $$
+}";
+
+            await VerifyItemIsAbsentAsync(markupBeforeCommit, ItemToCommit);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task ConstructorSnippetMissingInFilescopedNamespace()
+        {
+            var markupBeforeCommit =
+@"namespace Namespace;
+
+$$";
+
+            await VerifyItemIsAbsentAsync(markupBeforeCommit, ItemToCommit);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task ConstructorSnippetMissingInTopLevelContext()
+        {
+            var markupBeforeCommit =
+@"System.Console.WriteLine();
+$$";
+
+            await VerifyItemIsAbsentAsync(markupBeforeCommit, ItemToCommit);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task InsertConstructorSnippetInClassTest()
+        {
+            var markupBeforeCommit =
+@"class MyClass
+{
+    $$
+}";
+
+            var expectedCodeAfterCommit =
+@"class MyClass
+{
+    public MyClass()
+    {
+        $$
+    }
+}";
+            await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task InsertConstructorSnippetInStructTest()
+        {
+            var markupBeforeCommit =
+@"struct MyStruct
+{
+    $$
+}";
+
+            var expectedCodeAfterCommit =
+@"struct MyStruct
+{
+    public MyStruct()
+    {
+        $$
+    }
+}";
+            await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task InsertConstructorSnippetInRecordTest()
+        {
+            var markupBeforeCommit =
+@"record MyRecord
+{
+    $$
+}";
+
+            var expectedCodeAfterCommit =
+@"record MyRecord
+{
+    public MyRecord()
+    {
+        $$
+    }
+}";
+            await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task ConstructorSnippetMissingInInterface()
+        {
+            var markupBeforeCommit =
+@"interface MyInterface
+{
+    $$
+}";
+
+            await VerifyItemIsAbsentAsync(markupBeforeCommit, ItemToCommit);
+        }
+
+        [WpfFact, Trait(Traits.Feature, Traits.Features.Completion)]
+        public async Task InsertConstructorSnippetInNestedClassTest()
+        {
+            var markupBeforeCommit =
+@"class MyClass
+{
+    class MyClass1
+    {
+        $$
+    }
+}";
+
+            var expectedCodeAfterCommit =
+@"class MyClass
+{
+    class MyClass1
+    {
+        public MyClass1()
+        {
+            $$
+        }
+    }
+}";
+            await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
+        }
     }
 }
