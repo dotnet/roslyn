8681187373419392b12d85dabbd2d0e10c2dca16
Adam Perlin <adamp@nanosoft.com>
2022-10-12 00:57:37 -0700

    Update DefiniteAssignment Pass to Account for Local Default Parameter Values (#63850)
    * Account for use of local constants as default parameter values for local functions and lambdas in DefiniteAssignment pass
    
    * Fix nullable annotations context error
    
    * Slight cleanup
    
    * Modify parameter symbol to re-bind equals value syntax without caching
    
    * Address review feedback and include files meant for last commit
    
    * Only visit default value expression if it is constant
    
    * Add ConditionalFact annotation to flow tests for constant usage in lambda default parameters
    
    * Analyze default parameter values in lambdas/local functions for definite assignment even if non-constant
    
    * Remove definition of BindParameterEqualsValue from base type and move to SourceComplexParameterSymbolBase; add type check at use site in definite assignment pass
    
    * Update comment

--- src/Compilers/CSharp/Portable/Symbols/Source/SourceComplexParameterSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourceComplexParameterSymbol.cs
@@ -217,6 +217,21 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
         internal static SyntaxNode? GetDefaultValueSyntaxForIsNullableAnalysisEnabled(ParameterSyntax? parameterSyntax) =>
             parameterSyntax?.Default?.Value;
 
+        /// <summary>
+        /// Returns the bound default value syntax from the parameter, if it exists.
+        /// Note that this method will only return a non-null value if the
+        /// default value was supplied in syntax. If the value is supplied through the DefaultParameterValue
+        /// attribute, then ExplicitDefaultValue will be non-null but this method will return null.
+        /// However, if ExplicitDefaultValue is null, this method should always return null.
+        /// </summary>
+        public BoundParameterEqualsValue? BindParameterEqualsValue()
+        {
+            // Rebind default value expression, ignoring any diagnostics, in order to produce
+            // a bound node that can be used for passes such as definite assignment.
+            MakeDefaultExpression(BindingDiagnosticBag.Discarded, out var _, out var parameterEqualsValue);
+            return parameterEqualsValue;
+        }
+
         private ConstantValue DefaultSyntaxValue
         {
             get
