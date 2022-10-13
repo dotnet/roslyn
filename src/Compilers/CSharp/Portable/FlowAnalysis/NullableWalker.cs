38d32fb324678f3d601df4b30d8f62d76bc1d790
Jan Jones <jan.jones.cz@gmail.com>
2022-10-13 16:35:57 +0200

    Fix a typo

--- src/Compilers/CSharp/Portable/FlowAnalysis/NullableWalker.cs
+++ src/Compilers/CSharp/Portable/FlowAnalysis/NullableWalker.cs
@@ -2850,7 +2850,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             // Ignore var self-references (e.g., the RHS of `var x = x;`) to avoid cycles.
             // While inferring the type of a more complex construct (like lambda),
             // nullability analysis could be triggered against a reference of the local being inferred,
-            // querying its type and hence starting the same type inferrence recursively.
+            // querying its type and hence starting the same type inference recursively.
             if (local is SourceLocalSymbol { IsVar: true } && local.ForbiddenZone?.Contains(node.Syntax) == true)
             {
                 SetResultType(node, TypeWithState.ForType(node.Type));
