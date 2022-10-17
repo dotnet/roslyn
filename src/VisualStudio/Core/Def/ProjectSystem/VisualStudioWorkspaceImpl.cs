5c910f2856ca427949b8c602ca6439a53301ce9d
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-10-17 14:21:47 -0700

    Update to transformation pattern

--- src/VisualStudio/Core/Def/ProjectSystem/VisualStudioWorkspaceImpl.cs
+++ src/VisualStudio/Core/Def/ProjectSystem/VisualStudioWorkspaceImpl.cs
@@ -1630,21 +1630,18 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
             if (!solutionChanges.HasChange)
                 return;
 
-            // Clear out mutable state not associated with the solution snapshot (for example, which documents are
-            // currently open).
-            foreach (var documentId in solutionChanges.DocumentIdsRemoved)
-                this.ClearDocumentData(documentId);
-
-            var (oldSolution, newSolution) = SetCurrentSolutionEx(solutionChanges.Solution);
-
-            // This method returns the task that could be used to wait for the workspace changed event; we don't want
-            // to do that.
-            _ = RaiseWorkspaceChangedEventAsync(
+            this.SetCurrentSolution(
+                _ => solutionChanges.Solution,
                 solutionChanges.WorkspaceChangeKind,
-                oldSolution,
-                newSolution,
                 solutionChanges.WorkspaceChangeProjectId,
-                solutionChanges.WorkspaceChangeDocumentId);
+                solutionChanges.WorkspaceChangeDocumentId,
+                onBeforeUpdate: (_, _) =>
+                {
+                    // Clear out mutable state not associated with the solution snapshot (for example, which documents are
+                    // currently open).
+                    foreach (var documentId in solutionChanges.DocumentIdsRemoved)
+                        this.ClearDocumentData(documentId);
+                });
         }
 
         private readonly Dictionary<ProjectId, ProjectReferenceInformation> _projectReferenceInfoMap = new();
