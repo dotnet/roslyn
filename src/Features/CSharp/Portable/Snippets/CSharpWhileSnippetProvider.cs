0d982408dcce674654f81eb6693774cd9c60dbc7
DoctorKrolic <mapmyp03@gmail.com>
2022-10-21 19:52:51 +0300

    Implemented "while" semantic snippet

--- src/Features/CSharp/Portable/Snippets/CSharpWhileSnippetProvider.cs
+++ src/Features/CSharp/Portable/Snippets/CSharpWhileSnippetProvider.cs
@@ -20,18 +20,18 @@ using Microsoft.CodeAnalysis.Text;
 namespace Microsoft.CodeAnalysis.CSharp.Snippets
 {
     [ExportSnippetProvider(nameof(ISnippetProvider), LanguageNames.CSharp), Shared]
-    internal class CSharpIfSnippetProvider : AbstractIfSnippetProvider
+    internal class CSharpWhileSnippetProvider : AbstractWhileSnippetProvider
     {
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-        public CSharpIfSnippetProvider()
+        public CSharpWhileSnippetProvider()
         {
         }
 
         protected override int GetTargetCaretPosition(ISyntaxFactsService syntaxFacts, SyntaxNode caretTarget, SourceText sourceText)
         {
-            var ifStatement = (IfStatementSyntax)caretTarget;
-            var blockStatement = (BlockSyntax)ifStatement.Statement;
+            var whileStatement = (WhileStatementSyntax)caretTarget;
+            var blockStatement = (BlockSyntax)whileStatement.Statement;
 
             var triviaSpan = blockStatement.CloseBraceToken.LeadingTrivia.Span;
             var line = sourceText.Lines.GetLineFromPosition(triviaSpan.Start);
@@ -41,15 +41,15 @@ namespace Microsoft.CodeAnalysis.CSharp.Snippets
 
         protected override SyntaxNode GetStatementCondition(SyntaxNode node)
         {
-            var ifStatement = (IfStatementSyntax)node;
-            return ifStatement.Condition;
+            var whileStatement = (WhileStatementSyntax)node;
+            return whileStatement.Condition;
         }
 
         private static string GetIndentation(Document document, SyntaxNode node, SyntaxFormattingOptions syntaxFormattingOptions, CancellationToken cancellationToken)
         {
             var parsedDocument = ParsedDocument.CreateSynchronously(document, cancellationToken);
-            var ifStatementSyntax = (IfStatementSyntax)node;
-            var openBraceLine = parsedDocument.Text.Lines.GetLineFromPosition(ifStatementSyntax.Statement.SpanStart).LineNumber;
+            var whileStatementSyntax = (WhileStatementSyntax)node;
+            var openBraceLine = parsedDocument.Text.Lines.GetLineFromPosition(whileStatementSyntax.Statement.SpanStart).LineNumber;
 
             var indentationOptions = new IndentationOptions(syntaxFormattingOptions);
             var newLine = indentationOptions.FormattingOptions.NewLine;
@@ -70,12 +70,12 @@ namespace Microsoft.CodeAnalysis.CSharp.Snippets
             var syntaxFormattingOptions = await document.GetSyntaxFormattingOptionsAsync(fallbackOptions: null, cancellationToken).ConfigureAwait(false);
             var indentationString = GetIndentation(document, snippet, syntaxFormattingOptions, cancellationToken);
 
-            var ifStatementSyntax = (IfStatementSyntax)snippet;
-            var blockStatement = (BlockSyntax)ifStatementSyntax.Statement;
+            var whileStatementSyntax = (WhileStatementSyntax)snippet;
+            var blockStatement = (BlockSyntax)whileStatementSyntax.Statement;
             blockStatement = blockStatement.WithCloseBraceToken(blockStatement.CloseBraceToken.WithPrependedLeadingTrivia(SyntaxFactory.SyntaxTrivia(SyntaxKind.WhitespaceTrivia, indentationString)));
-            var newIfStatementSyntax = ifStatementSyntax.ReplaceNode(ifStatementSyntax.Statement, blockStatement);
+            var newIfStatementSyntax = whileStatementSyntax.ReplaceNode(whileStatementSyntax.Statement, blockStatement);
 
-            var newRoot = root.ReplaceNode(ifStatementSyntax, newIfStatementSyntax);
+            var newRoot = root.ReplaceNode(whileStatementSyntax, newIfStatementSyntax);
             return document.WithSyntaxRoot(newRoot);
         }
     }
--- src/Features/Core/Portable/Snippets/SnippetProviders/AbstractWhileSnippetProvider.cs
+++ src/Features/Core/Portable/Snippets/SnippetProviders/AbstractWhileSnippetProvider.cs
@@ -3,25 +3,26 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.LanguageService;
-using Microsoft.CodeAnalysis.Snippets.SnippetProviders;
 using Microsoft.CodeAnalysis.Text;
 
-namespace Microsoft.CodeAnalysis.Snippets
+namespace Microsoft.CodeAnalysis.Snippets.SnippetProviders
 {
-    internal abstract class AbstractIfSnippetProvider : AbstractConditionExpressionAndBlockBasedSnippetProvider
+    internal abstract class AbstractWhileSnippetProvider : AbstractConditionExpressionAndBlockBasedSnippetProvider
     {
-        public override string SnippetIdentifier => "if";
+        public override string SnippetIdentifier => "while";
 
-        public override string SnippetDescription => FeaturesResources.if_statement;
+        public override string SnippetDescription => FeaturesResources.while_statement;
 
-        protected override Func<SyntaxNode?, bool> GetSnippetContainerFunction(ISyntaxFacts syntaxFacts) => syntaxFacts.IsIfStatement;
+        protected override Func<SyntaxNode?, bool> GetSnippetContainerFunction(ISyntaxFacts syntaxFacts) => syntaxFacts.IsWhileStatement;
 
         protected override TextChange GenerateSnippetTextChange(Document document, int position)
         {
             var generator = SyntaxGenerator.GetGenerator(document);
-            var ifStatement = generator.IfStatement(generator.TrueLiteralExpression(), Array.Empty<SyntaxNode>());
+            var ifStatement = generator.WhileStatement(generator.TrueLiteralExpression(), Array.Empty<SyntaxNode>());
 
             return new TextChange(TextSpan.FromBounds(position, position), ifStatement.ToFullString());
         }
