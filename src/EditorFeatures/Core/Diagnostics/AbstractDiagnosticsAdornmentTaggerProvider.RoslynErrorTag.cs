97e8cdf2d125354ae2621626717065b42d2a785e
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-10-24 12:40:27 -0700

    Throw in unsupported Tag api

--- src/EditorFeatures/Core/Diagnostics/AbstractDiagnosticsAdornmentTaggerProvider.RoslynErrorTag.cs
+++ src/EditorFeatures/Core/Diagnostics/AbstractDiagnosticsAdornmentTaggerProvider.RoslynErrorTag.cs
@@ -62,10 +62,10 @@ namespace Microsoft.CodeAnalysis.Diagnostics
                     this._data.Message == other._data.Message;
             }
 
-            // Intentionally returning the base impl, we have never supported this facility, and there is no contract around
-            // placing these tags in sets or maps.
+            // Intentionally throwing, we have never supported this facility, and there is no contract around placing
+            // these tags in sets or maps.
             public override int GetHashCode()
-                => base.GetHashCode();
+                => throw new NotImplementedException();
         }
     }
 }
