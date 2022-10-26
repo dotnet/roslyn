ab6cecb0aa48b55fec83b5a8090c485d4a713b94
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-10-25 18:11:35 -0700

    Sort the options we give back in 'alias ambiguous type'

--- src/Analyzers/Core/CodeFixes/AliasAmbiguousType/AbstractAliasAmbiguousTypeCodeFixProvider.cs
+++ src/Analyzers/Core/CodeFixes/AliasAmbiguousType/AbstractAliasAmbiguousTypeCodeFixProvider.cs
@@ -3,8 +3,10 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.AddImport;
@@ -12,7 +14,9 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.LanguageService;
+using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.AliasAmbiguousType
 {
@@ -32,40 +36,88 @@ namespace Microsoft.CodeAnalysis.AliasAmbiguousType
             // Innermost: We are looking for an IdentifierName. IdentifierName is sometimes at the same span as its parent (e.g. SimpleBaseTypeSyntax).
             var diagnosticNode = root.FindNode(context.Span, getInnermostNodeForTie: true);
             if (!syntaxFacts.IsIdentifierName(diagnosticNode))
-            {
                 return;
-            }
 
             var semanticModel = await document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
             var symbolInfo = semanticModel.GetSymbolInfo(diagnosticNode, cancellationToken);
-            if (SymbolCandidatesContainsSupportedSymbols(symbolInfo))
+            if (!SymbolCandidatesContainsSupportedSymbols(symbolInfo))
+                return;
+
+            var addImportService = document.GetRequiredLanguageService<IAddImportsService>();
+            var syntaxGenerator = document.GetRequiredLanguageService<SyntaxGenerator>();
+            var compilation = semanticModel.Compilation;
+
+            var optionsProvider = context.GetOptionsProvider();
+            var allOptions = await document.GetAnalyzerConfigOptionsAsync(cancellationToken).ConfigureAwait(false);
+
+            var placementOption = await document.GetAddImportPlacementOptionsAsync(addImportService, optionsProvider, cancellationToken).ConfigureAwait(false);
+
+            using var _ = ArrayBuilder<CodeAction>.GetInstance(out var actions);
+            foreach (var symbol in Sort(symbolInfo.CandidateSymbols.Cast<ITypeSymbol>(), placementOption.PlaceSystemNamespaceFirst))
             {
-                var addImportService = document.GetRequiredLanguageService<IAddImportsService>();
-                var syntaxGenerator = document.GetRequiredLanguageService<SyntaxGenerator>();
-                var compilation = semanticModel.Compilation;
+                var typeName = symbol.Name;
+                var title = GetTextPreviewOfChange(typeName, symbol);
 
-                var options = await document.GetAddImportPlacementOptionsAsync(addImportService, context.GetOptionsProvider(), cancellationToken).ConfigureAwait(false);
+                actions.Add(CodeAction.Create(
+                    title,
+                    cancellationToken =>
+                    {
+                        var aliasDirective = syntaxGenerator.AliasImportDeclaration(typeName, symbol);
+                        var newRoot = addImportService.AddImport(compilation, root, diagnosticNode, aliasDirective, syntaxGenerator, placementOption, cancellationToken);
+                        return Task.FromResult(document.WithSyntaxRoot(newRoot));
+                    },
+                    title));
+            }
 
-                var codeActionsBuilder = ImmutableArray.CreateBuilder<CodeAction>(symbolInfo.CandidateSymbols.Length);
-                foreach (var symbol in symbolInfo.CandidateSymbols.Cast<ITypeSymbol>())
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    string.Format(CodeFixesResources.Alias_ambiguous_type_0, diagnosticNode.ToString()),
+                    actions.ToImmutable(),
+                    isInlinable: true),
+                context.Diagnostics.First());
+        }
+
+        private static IEnumerable<ITypeSymbol> Sort(IEnumerable<ITypeSymbol> types, bool sortSystemFirst)
+        {
+            var typeToNames = new Dictionary<ITypeSymbol, ImmutableArray<string>>();
+
+            return types.OrderBy((t1, t2) =>
+            {
+                var t1Names = GetNames(t1);
+                var t2Names = GetNames(t2);
+
+                for (int i = 0, n = Math.Min(t1Names.Length, t2Names.Length); i < n; i++)
                 {
-                    var typeName = symbol.Name;
-                    var title = GetTextPreviewOfChange(typeName, symbol);
+                    var t1Name = t1Names[i];
+                    var t2Name = t2Names[i];
 
-                    codeActionsBuilder.Add(CodeAction.Create(
-                        title,
-                        cancellationToken =>
-                        {
-                            var aliasDirective = syntaxGenerator.AliasImportDeclaration(typeName, symbol);
-                            var newRoot = addImportService.AddImport(compilation, root, diagnosticNode, aliasDirective, syntaxGenerator, options, cancellationToken);
-                            return Task.FromResult(document.WithSyntaxRoot(newRoot));
-                        },
-                        title));
+                    var comparer = i == 0 && sortSystemFirst ? SortSystemFirstComparer.Instance : StringComparer.Ordinal;
+
+                    var diff = comparer.Compare(t1Name, t2Name);
+                    if (diff != 0)
+                        return diff;
                 }
 
-                var groupingTitle = string.Format(CodeFixesResources.Alias_ambiguous_type_0, diagnosticNode.ToString());
-                var groupingCodeAction = CodeAction.Create(groupingTitle, codeActionsBuilder.ToImmutable(), isInlinable: true);
-                context.RegisterCodeFix(groupingCodeAction, context.Diagnostics.First());
+                return t1Names.Length - t2Names.Length;
+            });
+
+            ImmutableArray<string> GetNames(ITypeSymbol symbol)
+            {
+                return typeToNames.GetOrAdd(symbol, static symbol =>
+                {
+                    using var _ = ArrayBuilder<string>.GetInstance(out var result);
+
+                    for (ISymbol current = symbol; current != null; current = current.ContainingSymbol)
+                    {
+                        if (string.IsNullOrEmpty(current.Name))
+                            break;
+
+                        result.Add(current.Name);
+                    }
+
+                    result.ReverseContents();
+                    return result.ToImmutable();
+                });
             }
         }
 
@@ -76,5 +128,27 @@ namespace Microsoft.CodeAnalysis.AliasAmbiguousType
                // It is unlikely that the user wants that and so generic types are not supported.
                symbolInfo.CandidateSymbols.All(symbol => symbol.IsKind(SymbolKind.NamedType) &&
                                                          symbol.GetArity() == 0);
+
+        private sealed class SortSystemFirstComparer : IComparer<string>
+        {
+            public static readonly IComparer<string> Instance = new SortSystemFirstComparer();
+
+            public int Compare(string x, string y)
+            {
+                var xIsSystem = x == nameof(System);
+                var yIsSystem = y == nameof(System);
+
+                if (xIsSystem && yIsSystem)
+                    return 0;
+
+                if (xIsSystem)
+                    return -1;
+
+                if (yIsSystem)
+                    return 1;
+
+                return StringComparer.Ordinal.Compare(x, y);
+            }
+        }
     }
 }
