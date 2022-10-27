5a1224ed0dbc81edff71573489b2b36d43f2f4d7
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-10-27 08:35:53 -0700

    Fix issue in remove-unnecessary-cast with implicit-nullable-boxing casts

--- src/Analyzers/CSharp/Tests/RemoveUnnecessaryCast/RemoveUnnecessaryCastTests.cs
+++ src/Analyzers/CSharp/Tests/RemoveUnnecessaryCast/RemoveUnnecessaryCastTests.cs
@@ -12577,5 +12577,25 @@ class C
 ";
             await VerifyCS.VerifyCodeFixAsync(code, code);
         }
+
+        [Fact, WorkItem(65005, "https://github.com/dotnet/roslyn/issues/65005")]
+        public async Task DoNotRemoveImplicitNullableBoxingCast()
+        {
+            var code = @"
+using System;
+
+class C
+{
+    void Goo()
+    {
+        byte? temp = 10;
+        object? box = (int?)temp;
+
+        Console.WriteLine((int?)box);
+    }
+}
+";
+            await VerifyCS.VerifyCodeFixAsync(code, code);
+        }
     }
 }
