af31111236b21387cdde44e0f8ceedd8fea28757
DoctorKrolic <mapmyp03@gmail.com>
2022-11-03 18:46:01 +0300

    Reuse syntax root from the editor

--- src/Analyzers/Core/CodeFixes/MakeDeclarationPartial/AbstractMakePartialCodeFixProvider.cs
+++ src/Analyzers/Core/CodeFixes/MakeDeclarationPartial/AbstractMakePartialCodeFixProvider.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Editing;
-using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.MakeDeclarationPartial
 {
@@ -20,17 +19,19 @@ namespace Microsoft.CodeAnalysis.MakeDeclarationPartial
             return Task.CompletedTask;
         }
 
-        protected override async Task FixAllAsync(Document document, ImmutableArray<Diagnostic> diagnostics, SyntaxEditor editor, CodeActionOptionsProvider fallbackOptions, CancellationToken cancellationToken)
+        protected override Task FixAllAsync(Document document, ImmutableArray<Diagnostic> diagnostics, SyntaxEditor editor, CodeActionOptionsProvider fallbackOptions, CancellationToken cancellationToken)
         {
-            var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var syntaxRoot = editor.OriginalRoot;
             var generator = editor.Generator;
 
             foreach (var diagnostic in diagnostics)
             {
-                var declaration = root.FindNode(diagnostic.Location.SourceSpan);
+                var declaration = syntaxRoot.FindNode(diagnostic.Location.SourceSpan);
                 var fixedModifiers = generator.GetModifiers(declaration).WithPartial(true);
                 editor.ReplaceNode(declaration, generator.WithModifiers(declaration, fixedModifiers));
             }
+
+            return Task.CompletedTask;
         }
     }
 }
