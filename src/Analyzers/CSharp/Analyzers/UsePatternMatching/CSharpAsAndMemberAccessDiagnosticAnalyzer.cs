41b8d9e818e78678962cf35037438b156ef1bedc
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-11-09 10:24:00 -0800

    Add additional case for use-pattern-matching

---
+++ src/Analyzers/CSharp/Analyzers/UsePatternMatching/CSharpAsAndMemberAccessDiagnosticAnalyzer.cs
@@ -0,0 +1,131 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq.Expressions;
+using Microsoft.CodeAnalysis.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.Extensions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.CSharp.UsePatternMatching
+{
+    /// <summary>
+    /// Looks for code of the forms:
+    /// <code>
+    ///     (x as Y)?.Prop == constant
+    /// </code>
+    /// and converts it to:
+    /// <code>
+    ///     x is Y { Prop: constant }
+    /// </code>
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal partial class CSharpAsAndMemberAccessDiagnosticAnalyzer : AbstractBuiltInCodeStyleDiagnosticAnalyzer
+    {
+        public CSharpAsAndMemberAccessDiagnosticAnalyzer()
+            : base(IDEDiagnosticIds.UsePatternMatchingAsAndMemberAccessId,
+                   EnforceOnBuildValues.UsePatternMatchingAsAndMemberAccess,
+                   CSharpCodeStyleOptions.PreferPatternMatchingOverAsWithNullCheck,
+                   new LocalizableResourceString(
+                        nameof(CSharpAnalyzersResources.Use_pattern_matching), CSharpAnalyzersResources.ResourceManager, typeof(CSharpAnalyzersResources)))
+        {
+        }
+
+        public override DiagnosticAnalyzerCategory GetAnalyzerCategory()
+            => DiagnosticAnalyzerCategory.SemanticSpanAnalysis;
+
+        protected override void InitializeWorker(AnalysisContext context)
+        {
+            context.RegisterCompilationStartAction(context =>
+            {
+                // Recursive patterns (`is X { Prop: Y }`) is only available in C# 8.0 and above. Don't offer this
+                // refactoring in projects targeting a lesser version.
+                if (context.Compilation.LanguageVersion() < LanguageVersion.CSharp8)
+                    return;
+
+                context.RegisterSyntaxNodeAction(context => AnalyzeAsExpression(context), SyntaxKind.AsExpression);
+            });
+        }
+
+        private void AnalyzeAsExpression(SyntaxNodeAnalysisContext context)
+        {
+            var styleOption = context.GetCSharpAnalyzerOptions().PreferPatternMatchingOverAsWithNullCheck;
+            if (!styleOption.Value)
+            {
+                // Bail immediately if the user has disabled this feature.
+                return;
+            }
+
+            var cancellationToken = context.CancellationToken;
+            var semanticModel = context.SemanticModel;
+            var asExpression = (BinaryExpressionSyntax)context.Node;
+
+            // we're starting with `expr as T`
+
+            // has to be `(expr as T)`
+            if (asExpression.Parent is not ParenthesizedExpressionSyntax
+                {
+                    // Has to be `(expr as T)?...`
+                    Parent: ConditionalAccessExpressionSyntax conditionalAccess
+                })
+            {
+                return;
+            }
+
+            // After the `?` has to be `.X.Y.Z`
+
+            var whenNotNull = conditionalAccess.WhenNotNull;
+            while (whenNotNull is MemberAccessExpressionSyntax memberAccess)
+                whenNotNull = memberAccess.Expression;
+
+            // Ensure we have `.X`
+            if (whenNotNull is not MemberBindingExpressionSyntax)
+                return;
+
+            if (conditionalAccess.Parent is
+                    BinaryExpressionSyntax(
+                        SyntaxKind.EqualsExpression or
+                        SyntaxKind.NotEqualsExpression or
+                        SyntaxKind.GreaterThanExpression or
+                        SyntaxKind.GreaterThanEqualsToken or
+                        SyntaxKind.LessThanExpression or
+                        SyntaxKind.LessThanOrEqualExpression) binaryExpression &&
+                binaryExpression.Left == conditionalAccess)
+            {
+                // `(expr as T)?... == other_expr
+                //
+                // in this case we can only convert if other_expr is a constant.
+                var constantValue = semanticModel.GetConstantValue(binaryExpression.Right, cancellationToken);
+                if (!constantValue.HasValue)
+                    return;
+            }
+            else if (conditionalAccess.Parent is IsPatternExpressionSyntax)
+            {
+                // `(expr as T)?... is pattern
+                //
+                //  In this case we can always convert to a pattern.
+            }
+            else
+            {
+                return;
+            }
+
+            // Looks good!
+            //var additionalLocations = ImmutableArray.Create(
+            //    ifStatement.GetLocation(),
+            //    localDeclarationStatement.GetLocation());
+
+            // Put a diagnostic with the appropriate severity on the declaration-statement itself.
+            context.ReportDiagnostic(DiagnosticHelper.Create(
+                Descriptor,
+                asExpression.GetLocation(),
+                styleOption.Notification.Severity,
+                additionalLocations: null,
+                properties: null));
+        }
+    }
+}
--- src/Analyzers/Core/Analyzers/IDEDiagnosticIds.cs
+++ src/Analyzers/Core/Analyzers/IDEDiagnosticIds.cs
@@ -183,6 +183,8 @@ namespace Microsoft.CodeAnalysis.Diagnostics
 
         public const string MakeStructReadOnlyDiagnosticId = "IDE0250";
 
+        public const string UsePatternMatchingAsAndMemberAccessId = "IDE260";
+
         // Analyzer error Ids
         public const string AnalyzerChangedId = "IDE1001";
         public const string AnalyzerDependencyConflictId = "IDE1002";
