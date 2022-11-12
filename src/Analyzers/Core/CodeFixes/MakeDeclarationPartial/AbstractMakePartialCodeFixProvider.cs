bf6ae5d8e76ba9625a92a65c205349fa62cc57c8
DoctorKrolic <mapmyp03@gmail.com>
2022-11-12 19:02:41 +0300

    Remove comment

--- src/Analyzers/Core/CodeFixes/MakeDeclarationPartial/AbstractMakePartialCodeFixProvider.cs
+++ src/Analyzers/Core/CodeFixes/MakeDeclarationPartial/AbstractMakePartialCodeFixProvider.cs
@@ -13,7 +13,6 @@ namespace Microsoft.CodeAnalysis.MakeDeclarationPartial
 {
     internal abstract class AbstractMakeDeclarationPartialCodeFixProvider : SyntaxEditorBasedCodeFixProvider
     {
-        // This code fix addresses a very specific compiler error. It's unlikely there will be more than 1 of them at a time.
         protected AbstractMakeDeclarationPartialCodeFixProvider()
             : base(supportsFixAll: false)
         {
