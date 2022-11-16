5a27d502bb90ee1e3c01f00d407a5ae1ffe3ca8a
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-11-15 17:10:05 -0800

    Share code

--- src/VisualStudio/Core/Def/NavigateTo/RoslynSearchResultView.cs
+++ src/VisualStudio/Core/Def/NavigateTo/RoslynSearchResultView.cs
@@ -5,6 +5,8 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Editor;
+using Microsoft.CodeAnalysis.Editor.NavigateTo;
 using Microsoft.CodeAnalysis.Navigation;
 using Microsoft.CodeAnalysis.Shared.TestHooks;
 using Microsoft.VisualStudio.Core.Imaging;
@@ -18,55 +20,27 @@ namespace Microsoft.CodeAnalysis.NavigateTo
         private sealed class RoslynSearchResultView : CodeSearchResultViewBase
         {
             private readonly RoslynSearchItemsSourceProvider _provider;
-            private readonly RoslynCodeSearchResult _searchResult;
+            private readonly INavigateToSearchResult _searchResult;
 
             public RoslynSearchResultView(
                 RoslynSearchItemsSourceProvider provider,
-                RoslynCodeSearchResult searchResult,
+                INavigateToSearchResult searchResult,
                 HighlightedText title,
+                HighlightedText description,
                 ImageId primaryIcon)
-                : base(title, primaryIcon: primaryIcon)
+                : base(title, description, primaryIcon: primaryIcon)
             {
                 _provider = provider;
                 _searchResult = searchResult;
 
-                var filePath = _searchResult.SearchResult.NavigableItem.Document.FilePath;
+                var filePath = _searchResult.NavigableItem.Document.FilePath;
                 if (filePath != null)
                     this.FileLocation = new HighlightedText(filePath, Array.Empty<VisualStudio.Text.Span>());
             }
 
             public override void Invoke(CancellationToken cancellationToken)
-            {
-                var token = _provider._asyncListener.BeginAsyncOperation(nameof(NavigateTo));
-                NavigateToAsync().ReportNonFatalErrorAsync().CompletesAsyncOperation(token);
-            }
-
-            private async Task NavigateToAsync()
-            {
-                var document = _searchResult.SearchResult.NavigableItem.Document;
-                if (document == null)
-                    return;
-
-                var workspace = document.Project.Solution.Workspace;
-                var navigationService = workspace.Services.GetRequiredService<IDocumentNavigationService>();
-
-                // Document tabs opened by NavigateTo are carefully created as preview or regular tabs
-                // by them; trying to specifically open them in a particular kind of tab here has no
-                // effect.
-                //
-                // In the case of a stale item, don't require that the span be in bounds of the document
-                // as it exists right now.
-                using var context = _provider._threadOperationExecutor.BeginExecute(
-                    EditorFeaturesResources.Navigating_to_definition, EditorFeaturesResources.Navigating_to_definition, allowCancellation: true, showProgress: false);
-                await navigationService.TryNavigateToSpanAsync(
-                    _provider._threadingContext,
-                    workspace,
-                    document.Id,
-                    _searchResult.SearchResult.NavigableItem.SourceSpan,
-                    NavigationOptions.Default,
-                    allowInvalidSpan: _searchResult.SearchResult.NavigableItem.IsStale,
-                    context.UserCancellationToken).ConfigureAwait(false);
-            }
+                => NavigateToHelpers.NavigateTo(
+                    _searchResult, _provider._threadingContext, _provider._threadOperationExecutor, _provider._asyncListener);
         }
     }
 }
