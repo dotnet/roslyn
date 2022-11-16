3106afec05c47ca2e9171dd468205e04c22d2800
Julien Couvreur <jcouv@microsoft.com>
2022-11-15 17:58:30 -0800

    Removed unused parameters

--- src/Compilers/Core/Portable/DiagnosticAnalyzer/SuppressMessageAttributeState.cs
+++ src/Compilers/Core/Portable/DiagnosticAnalyzer/SuppressMessageAttributeState.cs
@@ -309,10 +309,10 @@ namespace Microsoft.CodeAnalysis.Diagnostics
             Debug.Assert(symbol is IAssemblySymbol || symbol is IModuleSymbol);
 
             var attributes = symbol.GetAttributes().Where(a => IsSuppressionAttribute(a));
-            DecodeGlobalSuppressMessageAttributes(compilation, symbol, globalSuppressions, attributes);
+            DecodeGlobalSuppressMessageAttributes(compilation, globalSuppressions, attributes);
         }
 
-        private static void DecodeGlobalSuppressMessageAttributes(Compilation compilation, ISymbol symbol, GlobalSuppressions globalSuppressions, IEnumerable<AttributeData> attributes)
+        private static void DecodeGlobalSuppressMessageAttributes(Compilation compilation, GlobalSuppressions globalSuppressions, IEnumerable<AttributeData> attributes)
         {
             foreach (var instance in attributes)
             {
