c4a42bc2e23e20dd5cf652193e9b4609bcb54350
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-11-21 14:26:44 -0800

    Remove errant code

--- src/Compilers/CSharp/Test/Syntax/Parsing/MemberDeclarationParsingTests.cs
+++ src/Compilers/CSharp/Test/Syntax/Parsing/MemberDeclarationParsingTests.cs
@@ -13,7 +13,6 @@ using Microsoft.CodeAnalysis.Test.Utilities;
 using Roslyn.Test.Utilities;
 using Xunit;
 using Xunit.Abstractions;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Microsoft.CodeAnalysis.CSharp.UnitTests
 {
