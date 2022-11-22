0a57bd2e96d59a633d7eabe5e120b6a2eb5b1874
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-11-21 17:50:30 -0800

    Simplify

--- src/Workspaces/Core/Portable/Storage/SQLite/v2/SQLitePersistentStorage_DocumentIds.cs
+++ src/Workspaces/Core/Portable/Storage/SQLite/v2/SQLitePersistentStorage_DocumentIds.cs
@@ -3,8 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Concurrent;
-using Microsoft.CodeAnalysis.Storage;
 using Microsoft.CodeAnalysis.SQLite.v2.Interop;
+using Microsoft.CodeAnalysis.Storage;
 
 namespace Microsoft.CodeAnalysis.SQLite.v2
 {
--- src/Workspaces/Core/Portable/Storage/SQLite/v2/SQLitePersistentStorage_ProjectSerialization.cs
+++ src/Workspaces/Core/Portable/Storage/SQLite/v2/SQLitePersistentStorage_ProjectSerialization.cs
@@ -2,12 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Immutable;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.Storage;
 using Microsoft.CodeAnalysis.SQLite.v2.Interop;
-using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.Storage;
 
 namespace Microsoft.CodeAnalysis.SQLite.v2
 {
