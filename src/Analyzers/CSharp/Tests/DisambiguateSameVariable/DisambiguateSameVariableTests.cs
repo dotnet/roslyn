1658eccbcc06806d4b772e9b6d5641c32730abb8
DoctorKrolic <mapmyp03@gmail.com>
2022-11-25 19:28:06 +0300

    Move DisambiguateSameVariableTests to VerifyCS

--- src/Analyzers/CSharp/Tests/DisambiguateSameVariable/DisambiguateSameVariableTests.cs
+++ src/Analyzers/CSharp/Tests/DisambiguateSameVariable/DisambiguateSameVariableTests.cs
@@ -2,79 +2,78 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.DisambiguateSameVariable;
-using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics;
+using Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions;
+using Microsoft.CodeAnalysis.Testing;
 using Roslyn.Test.Utilities;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.DisambiguateSameVariable
 {
-    public class DisambiguateSameVariableTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
+    using VerifyCS = CSharpCodeFixVerifier<
+        EmptyDiagnosticAnalyzer,
+        CSharpDisambiguateSameVariableCodeFixProvider>;
+
+    public class DisambiguateSameVariableTests
     {
-        public DisambiguateSameVariableTests(ITestOutputHelper logger)
-           : base(logger)
-        {
-        }
-
-        internal override (DiagnosticAnalyzer, CodeFixProvider) CreateDiagnosticProviderAndFixer(Workspace workspace)
-            => (null, new CSharpDisambiguateSameVariableCodeFixProvider());
-
         [Fact]
         public async Task TestParamToParamWithNoMatch()
         {
-            await TestMissingAsync(
+            var code =
 @"class C
 {
     void M(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
-}");
+}";
+
+            await VerifyCS.VerifyCodeFixAsync(code, code);
         }
 
         [Fact]
         public async Task TestLocalToLocalWithNoMatch()
         {
-            await TestMissingAsync(
+            var code =
 @"class C
 {
-    void M(int a)
+    void M()
     {
-        [|a = a|];
+        var a = 0;
+        {|CS1717:a = a|};
     }
-}");
+}";
+
+            await VerifyCS.VerifyCodeFixAsync(code, code);
         }
 
         [Fact]
         public async Task TestFieldToFieldWithNoMatch()
         {
-            await TestMissingAsync(
+            var code =
 @"class C
 {
     int a;
     void M()
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
-}");
+}";
+
+            await VerifyCS.VerifyCodeFixAsync(code, code);
         }
 
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamWithSameNamedField()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     int a;
     void M(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
 }",
 @"class C
@@ -90,27 +89,29 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.DisambiguateSameVariabl
         [Fact]
         public async Task TestFieldToFieldWithNonMatchingField()
         {
-            await TestMissingAsync(
+            var code =
 @"class C
 {
     int x;
     void M()
     {
-        [|a = a|];
+        {|CS0103:a|} = {|CS0103:a|};
     }
-}");
+}";
+
+            await VerifyCS.VerifyCodeFixAsync(code, code);
         }
 
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamWithUnderscoreNamedField()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     int _a;
     void M(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
 }",
 @"class C
@@ -126,13 +127,13 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.DisambiguateSameVariabl
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamWithCapitalizedField()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     int A;
     void M(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
 }",
 @"class C
@@ -148,13 +149,13 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.DisambiguateSameVariabl
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamWithProperty()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     int A { get; set; }
     void M(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
 }",
 @"class C
@@ -170,13 +171,13 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.DisambiguateSameVariabl
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamWithReadOnlyFieldInConstructor()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     readonly int a;
     public C(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
 }",
 @"class C
@@ -193,13 +194,13 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.DisambiguateSameVariabl
         public async Task TestParamToParamWithReadOnlyFieldOutsideOfConstructor()
         {
             // Not legal, but is at least something they might want.
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     readonly int a;
     void M(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
 }",
 @"class C
@@ -207,7 +208,7 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.DisambiguateSameVariabl
     readonly int a;
     void M(int a)
     {
-        this.a = a;
+        {|CS0191:this.a|} = a;
     }
 }");
         }
@@ -215,7 +216,7 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.DisambiguateSameVariabl
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamWithAccessibleFieldInBaseType()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"
 class Base
 {
@@ -226,7 +227,7 @@ class C : Base
 {
     public C(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
 }",
 @"
@@ -247,7 +248,7 @@ class C : Base
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamNotWithInaccessibleFieldInBaseType()
         {
-            await TestMissingAsync(
+            var code =
 @"
 class Base
 {
@@ -258,36 +259,40 @@ class C : Base
 {
     public C(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
-}");
+}";
+
+            await VerifyCS.VerifyCodeFixAsync(code, code);
         }
 
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamNotWithStaticField()
         {
-            await TestMissingAsync(
+            var code =
 @"
 class C
 {
     static int a;
     public C(int a)
     {
-        [|a = a|];
+        {|CS1717:a = a|};
     }
-}");
+}";
+
+            await VerifyCS.VerifyCodeFixAsync(code, code);
         }
 
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestParamToParamCompareWithSameNamedField()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     int a;
     void M(int a)
     {
-        if ([|a == a|])
+        if ({|CS1718:a == a|})
         {
         }
     }
@@ -307,14 +312,14 @@ class C
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestFixAll1()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     int a;
     void M(int a)
     {
-        {|FixAllInDocument:a = a|};
-        a = a;
+        {|CS1717:a = a|};
+        {|CS1717:a = a|};
     }
 }",
 @"class C
@@ -331,20 +336,20 @@ class C
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestFieldToFieldWithPropAvailableOffOfThis()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     int a;
-    int A { get; set; };
+    int A { get; set; }
     void M()
     {
-        [|this.a = this.a|];
+        {|CS1717:this.a = this.a|};
     }
 }",
 @"class C
 {
     int a;
-    int A { get; set; };
+    int A { get; set; }
     void M()
     {
         this.A = this.a;
@@ -355,20 +360,20 @@ class C
         [Fact, WorkItem(28290, "https://github.com/dotnet/roslyn/issues/28290")]
         public async Task TestFieldToFieldWithPropAvailableOffOfOtherInstance()
         {
-            await TestInRegularAndScript1Async(
+            await VerifyCS.VerifyCodeFixAsync(
 @"class C
 {
     int a;
-    int A { get; set; };
+    int A { get; set; }
     void M(C c)
     {
-        [|c.a = c.a|];
+        {|CS1717:c.a = c.a|};
     }
 }",
 @"class C
 {
     int a;
-    int A { get; set; };
+    int A { get; set; }
     void M(C c)
     {
         c.A = c.a;
