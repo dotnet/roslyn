3594256392b09f6943de31d699bb8998738976bd
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2022-11-29 09:27:25 -0800

    Move IDS_FeatureXXX checks out of the parser (#65500)

--- src/Compilers/CSharp/Test/Syntax/Parsing/TopLevelStatementsParsingTests.cs
+++ src/Compilers/CSharp/Test/Syntax/Parsing/TopLevelStatementsParsingTests.cs
@@ -3231,10 +3231,13 @@ record class Point(int x, int y);
 record class Point(int x, int y);
 ";
 
-            UsingTree(test, TestOptions.Regular8,
+            CreateCompilation(test, parseOptions: TestOptions.Regular8).VerifyDiagnostics(
                 // (2,1): error CS8400: Feature 'top-level statements' is not available in C# 8.0. Please use language version 9.0 or greater.
                 // record class Point(int x, int y);
                 Diagnostic(ErrorCode.ERR_FeatureNotAvailableInVersion8, "record ").WithArguments("top-level statements", "9.0").WithLocation(2, 1),
+                // (2,1): error CS0246: The type or namespace name 'record' could not be found (are you missing a using directive or an assembly reference?)
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_SingleTypeNameNotFound, "record").WithArguments("record").WithLocation(2, 1),
                 // (2,8): error CS1001: Identifier expected
                 // record class Point(int x, int y);
                 Diagnostic(ErrorCode.ERR_IdentifierExpected, "class").WithLocation(2, 8),
@@ -3249,8 +3252,39 @@ record class Point(int x, int y);
                 Diagnostic(ErrorCode.ERR_RbraceExpected, "(").WithLocation(2, 19),
                 // (2,19): error CS8803: Top-level statements must precede namespace and type declarations.
                 // record class Point(int x, int y);
-                Diagnostic(ErrorCode.ERR_TopLevelStatementAfterNamespaceOrType, "(int x, int y);").WithLocation(2, 19)
-                );
+                Diagnostic(ErrorCode.ERR_TopLevelStatementAfterNamespaceOrType, "(int x, int y);").WithLocation(2, 19),
+                // (2,19): error CS0201: Only assignment, call, increment, decrement, await, and new object expressions can be used as a statement
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_IllegalStatement, "(int x, int y)").WithLocation(2, 19),
+                // (2,20): error CS8185: A declaration is not allowed in this context.
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_DeclarationExpressionNotPermitted, "int x").WithLocation(2, 20),
+                // (2,20): error CS0165: Use of unassigned local variable 'x'
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_UseDefViolation, "int x").WithArguments("x").WithLocation(2, 20),
+                // (2,27): error CS8185: A declaration is not allowed in this context.
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_DeclarationExpressionNotPermitted, "int y").WithLocation(2, 27),
+                // (2,27): error CS0165: Use of unassigned local variable 'y'
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_UseDefViolation, "int y").WithArguments("y").WithLocation(2, 27));
+
+            UsingTree(test, TestOptions.Regular8,
+                // (2,8): error CS1001: Identifier expected
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_IdentifierExpected, "class").WithLocation(2, 8),
+                // (2,8): error CS1002: ; expected
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_SemicolonExpected, "class").WithLocation(2, 8),
+                // (2,19): error CS1514: { expected
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_LbraceExpected, "(").WithLocation(2, 19),
+                // (2,19): error CS1513: } expected
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_RbraceExpected, "(").WithLocation(2, 19),
+                // (2,19): error CS8803: Top-level statements must precede namespace and type declarations.
+                // record class Point(int x, int y);
+                Diagnostic(ErrorCode.ERR_TopLevelStatementAfterNamespaceOrType, "(int x, int y);").WithLocation(2, 19));
 
             N(SyntaxKind.CompilationUnit);
             {
