54c95f392f31e28878095dbd8d14456390007a20
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-11-28 16:17:28 -0800

    Simplify

--- src/Features/CSharp/Portable/FullyQualify/CSharpFullyQualifyCodeFixProvider.cs
+++ src/Features/CSharp/Portable/FullyQualify/CSharpFullyQualifyCodeFixProvider.cs
@@ -51,7 +51,7 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.FullyQualify
         {
         }
 
-        public override ImmutableArray<string> FixableDiagnosticIds =
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } =
             ImmutableArray.Create(CS0103, CS0104, CS0246, CS0305, CS0308, IDEDiagnosticIds.UnboundIdentifierId);
 
         protected override bool CanFullyQualify(Diagnostic diagnostic, SyntaxNode node, [NotNullWhen(true)] out SimpleNameSyntax? simpleName)
