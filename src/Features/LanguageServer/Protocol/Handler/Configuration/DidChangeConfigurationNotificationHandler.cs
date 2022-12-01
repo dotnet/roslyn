1a718124de37fb345c9fd3a6d77028680bdc24f7
Shen Chen <shech@microsoft.com>
2022-12-01 15:14:19 -0800

    Add stub to handle Configuration Change Notification

---
+++ src/Features/LanguageServer/Protocol/Handler/Configuration/DidChangeConfigurationNotificationHandler.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Roslyn.Utilities;
+using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Configuration
+{
+    internal class DidChangeConfigurationNotificationHandler : ILspServiceNotificationHandler
+    {
+        public bool MutatesSolutionState => throw new NotImplementedException();
+
+        public bool RequiresLSPSolution => throw new NotImplementedException();
+
+        public Task HandleNotificationAsync(RequestContext requestContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
