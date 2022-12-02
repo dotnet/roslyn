3b409b09dd3f1aa3f21d063aa81f37886c698784
Shen Chen <shech@microsoft.com>
2022-12-01 16:09:55 -0800

    Add the stub code for perisiter and persiterProvider

---
+++ src/Features/LanguageServer/Protocol/Handler/Configuration/LspOptionPersister.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis.Options;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Configuration
+{
+    internal class LspOptionPersister : IOptionPersister
+    {
+        public bool TryFetch(OptionKey optionKey, out object? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryPersist(OptionKey optionKey, object? value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
