33c9ef1be2dcabe99029806791ea38da110397e7
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-12-07 10:09:13 -0800

    Remove bogus assert

--- src/Features/CSharp/Portable/EmbeddedLanguages/EmbeddedLanguageUtilities.cs
+++ src/Features/CSharp/Portable/EmbeddedLanguages/EmbeddedLanguageUtilities.cs
@@ -2,10 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Diagnostics;
 using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.CSharp.Features.EmbeddedLanguages
 {
@@ -18,21 +17,22 @@ namespace Microsoft.CodeAnalysis.CSharp.Features.EmbeddedLanguages
                 SyntaxFactory.ElasticSpace);
             var newStringLiteral = stringLiteral.WithLeadingTrivia(
                 stringLiteral.LeadingTrivia.AddRange(triviaList));
-            editor.ReplaceNode(stringLiteral.Parent, stringLiteral.Parent.ReplaceToken(stringLiteral, newStringLiteral));
+            var parent = stringLiteral.GetRequiredParent();
+            editor.ReplaceNode(
+               parent,
+               parent.ReplaceToken(stringLiteral, newStringLiteral));
         }
 
         public static string EscapeText(string text, SyntaxToken token)
         {
-            // This function is called when Completion needs to escape something its going to
-            // insert into the user's string token.  This means that we only have to escape
-            // things that completion could insert.  In this case, the only regex character
-            // that is relevant is the \ character, and it's only relevant if we insert into
-            // a normal string and not a verbatim string.  There are no other regex characters
-            // that completion will produce that need any escaping. 
-            Debug.Assert(token.Kind() == SyntaxKind.StringLiteralToken);
-            return token.IsVerbatimStringLiteral()
-                ? text
-                : text.Replace(@"\", @"\\");
+            // This function is called when Completion needs to escape something its going to insert into the user's
+            // string token.  This means that we only have to escape things that completion could insert.  In this case,
+            // the only regex character that is relevant is the \ character, and it's only relevant if we insert into a
+            // normal string and not a verbatim string.  There are no other regex characters that completion will
+            // produce that need any escaping.
+            return token.Kind() is SyntaxKind.StringLiteralToken or SyntaxKind.Utf8StringLiteralToken  && !token.IsVerbatimStringLiteral()
+                ? text.Replace(@"\", @"\\")
+                : text;
         }
     }
 }
