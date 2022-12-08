03731913d817e5e05dd67e07b1e648bfeaa5ad84
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-12-08 12:01:16 -0800

    lint

--- src/Workspaces/Core/Portable/Workspace/Solution/DocumentState.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/DocumentState.cs
@@ -23,8 +23,7 @@ namespace Microsoft.CodeAnalysis
     {
         private static readonly Func<string?, PreservationMode, string> s_fullParseLog = (path, mode) => $"{path} : {mode}";
 
-        private static readonly ConditionalWeakTable<SyntaxTree, DocumentId> s_syntaxTreeToIdMap =
-            new();
+        private static readonly ConditionalWeakTable<SyntaxTree, DocumentId> s_syntaxTreeToIdMap = new();
 
         // properties inherited from the containing project:
         private readonly HostLanguageServices _languageServices;
