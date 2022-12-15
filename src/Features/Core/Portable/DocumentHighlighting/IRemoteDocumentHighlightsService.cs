be37674251126eb281d839d10d7fcaeff3f56d54
Cyrus Najmabadi <cyrusn@microsoft.com>
2022-12-14 16:28:58 -0800

    Cleanup

--- src/Features/Core/Portable/DocumentHighlighting/IRemoteDocumentHighlightsService.cs
+++ src/Features/Core/Portable/DocumentHighlighting/IRemoteDocumentHighlightsService.cs
@@ -6,9 +6,7 @@ using System.Collections.Immutable;
 using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.Remote;
 using Microsoft.CodeAnalysis.Shared.Extensions;
-using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.DocumentHighlighting
 {
