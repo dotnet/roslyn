19c7a802edfb0b28e8cc5534a558360c3ce16676
tmat <tomas.matousek@microsoft.com>
2022-12-16 10:48:59 -0800

    Avoid Solution.Options in BlockCommentEditingCommandHandler

--- src/EditorFeatures/CSharp/BlockCommentEditing/CloseBlockCommentCommandHandler.cs
+++ src/EditorFeatures/CSharp/BlockCommentEditing/CloseBlockCommentCommandHandler.cs
@@ -21,13 +21,13 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.BlockCommentEditing
     [Order(After = nameof(BlockCommentEditingCommandHandler))]
     internal sealed class CloseBlockCommentCommandHandler : ICommandHandler<TypeCharCommandArgs>
     {
-        private readonly IGlobalOptionService _globalOptions;
+        private readonly EditorOptionsService _editorOptions;
 
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-        public CloseBlockCommentCommandHandler(IGlobalOptionService globalOptions)
+        public CloseBlockCommentCommandHandler(EditorOptionsService editorOptions)
         {
-            _globalOptions = globalOptions;
+            _editorOptions = editorOptions;
         }
 
         public string DisplayName => EditorFeaturesResources.Block_Comment_Editing;
@@ -51,8 +51,8 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.BlockCommentEditing
                         if (line.End == position &&
                             line.IsEmptyOrWhitespace(0, line.Length - 2))
                         {
-                            if (_globalOptions.GetOption(FeatureOnOffOptions.AutoInsertBlockCommentStartString, LanguageNames.CSharp) &&
-                                BlockCommentEditingCommandHandler.IsCaretInsideBlockCommentSyntax(caret.Value, out _, out _))
+                            if (_editorOptions.GlobalOptions.GetOption(FeatureOnOffOptions.AutoInsertBlockCommentStartString, LanguageNames.CSharp) &&
+                                BlockCommentEditingCommandHandler.IsCaretInsideBlockCommentSyntax(caret.Value, args.SubjectBuffer, _editorOptions, out _, out _, executionContext.OperationContext.UserCancellationToken))
                             {
                                 args.SubjectBuffer.Replace(new VisualStudio.Text.Span(position - 1, 1), "/");
                                 return true;
