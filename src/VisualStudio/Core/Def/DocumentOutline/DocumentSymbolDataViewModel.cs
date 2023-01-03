882606852d27c0f7d00d3317525e61d00720cc4c
Jonathon Marolf <jmarolf@microsoft.com>
2023-01-03 11:33:00 -0800

    keep equals methods together

--- src/VisualStudio/Core/Def/DocumentOutline/DocumentSymbolDataViewModel.cs
+++ src/VisualStudio/Core/Def/DocumentOutline/DocumentSymbolDataViewModel.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -141,14 +141,14 @@ namespace Microsoft.VisualStudio.LanguageServices.DocumentOutline
             };
         }
 
-        public override bool Equals(object obj)
-            => Equals(obj as DocumentSymbolDataViewModel);
-
         public static bool operator ==(DocumentSymbolDataViewModel left, DocumentSymbolDataViewModel right)
-            => (object)left == right || (left is not null && left.Equals(right));
+            => left.Equals(right);
 
         public static bool operator !=(DocumentSymbolDataViewModel left, DocumentSymbolDataViewModel right)
             => !(left == right);
+
+        public override bool Equals(object obj)
+            => Equals(obj as DocumentSymbolDataViewModel);
 
         public bool Equals(DocumentSymbolDataViewModel? other)
             => (object)this == other ||
