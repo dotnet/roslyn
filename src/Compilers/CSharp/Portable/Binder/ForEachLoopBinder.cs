d8b22e6c8fa04f041db974d2fcc1fca236fc80a2
Julien Couvreur <julien.couvreur@gmail.com>
2023-01-05 00:01:06 -0800

    Disallow more locals of restricted type in async methods

--- src/Compilers/CSharp/Portable/Binder/ForEachLoopBinder.cs
+++ src/Compilers/CSharp/Portable/Binder/ForEachLoopBinder.cs
@@ -298,6 +298,11 @@ namespace Microsoft.CodeAnalysis.CSharp
                         SourceLocalSymbol local = this.IterationVariable;
                         local.SetTypeWithAnnotations(declType);
 
+                        if (CheckRestrictedTypeInAsyncMethod(this.ContainingMemberOrLambda, declType.Type, diagnostics, typeSyntax))
+                        {
+                            hasErrors = true;
+                        }
+
                         if (local.Scope == ScopedKind.ScopedValue && !declType.Type.IsErrorTypeOrRefLikeType())
                         {
                             diagnostics.Add(ErrorCode.ERR_ScopedRefAndRefStructOnly, typeSyntax.Location);
