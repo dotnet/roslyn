57b5de4e6ada2e3ce800e81a2b6c6ae83b25f696
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-01-09 08:28:38 -0800

    Add comment

--- src/EditorFeatures/VisualBasic/LanguageServer/VisualBasicLspBuildOnlyDiagnostics.vb
+++ src/EditorFeatures/VisualBasic/LanguageServer/VisualBasicLspBuildOnlyDiagnostics.vb
@@ -7,6 +7,8 @@ Imports Microsoft.CodeAnalysis.Host.Mef
 Imports Microsoft.CodeAnalysis.LanguageServer
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.LanguageServer
+    ' Keep in sync with IsBuildOnlyDiagnostic
+    ' src\Compilers\VisualBasic\Portable\Errors\ErrorFacts.vb
     <LspBuildOnlyDiagnostics(
         "VB31093", ' ERRID.ERR_TypeRefResolutionError3,
         "VB35000", ' ERRID.ERR_MissingRuntimeHelper,
