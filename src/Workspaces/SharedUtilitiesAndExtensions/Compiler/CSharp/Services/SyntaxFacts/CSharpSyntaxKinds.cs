c80a825a07aab159f33f4b8a34c932c6387bdf29
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-01-10 11:45:58 -0800

    Fix

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Services/SyntaxFacts/CSharpSyntaxKinds.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Services/SyntaxFacts/CSharpSyntaxKinds.cs
@@ -35,7 +35,7 @@ namespace Microsoft.CodeAnalysis.CSharp.LanguageService
         public int IfDirectiveTrivia => (int)SyntaxKind.IfDirectiveTrivia;
 
         public int CloseBraceToken => (int)SyntaxKind.CloseBraceToken;
-        public int CloseBracketToken => (int)SyntaxKind.CloseBraceToken;
+        public int? CloseBracketToken => (int)SyntaxKind.CloseBracketToken;
         public int CloseParenToken => (int)SyntaxKind.CloseParenToken;
         public int ColonToken => (int)SyntaxKind.ColonToken;
         public int CharacterLiteralToken => (int)SyntaxKind.CharacterLiteralToken;
@@ -44,7 +44,7 @@ namespace Microsoft.CodeAnalysis.CSharp.LanguageService
         public int InterpolatedStringTextToken => (int)SyntaxKind.InterpolatedStringTextToken;
         public int LessThanToken => (int)SyntaxKind.LessThanToken;
         public int OpenBraceToken => (int)SyntaxKind.OpenBraceToken;
-        public int OpenBracketToken => (int)SyntaxKind.OpenBraceToken;
+        public int? OpenBracketToken => (int)SyntaxKind.OpenBracketToken;
         public int OpenParenToken => (int)SyntaxKind.OpenParenToken;
         public int QuestionToken => (int)SyntaxKind.QuestionToken;
         public int StringLiteralToken => (int)SyntaxKind.StringLiteralToken;
