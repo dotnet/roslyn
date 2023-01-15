2b27982155543fd789afc0e7cae4140f043561c7
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-01-14 17:27:58 -0800

    Add error facts

--- src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
@@ -2277,6 +2277,9 @@ namespace Microsoft.CodeAnalysis.CSharp
                 case ErrorCode.ERR_UnscopedRefAttributeInterfaceImplementation:
                 case ErrorCode.ERR_UnrecognizedRefSafetyRulesAttributeVersion:
                 case ErrorCode.ERR_BadSpecialByRefUsing:
+                case ERR_BadRefInUsingAlias:
+                case ERR_BadUnsafeInUsingDirective:
+                case ERR_BadNullableReferenceTypeInUsingAlias:
                     return false;
                 default:
                     // NOTE: All error codes must be explicitly handled in this switch statement
