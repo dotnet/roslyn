c8c7100818bff4c4ce4c42ba1ce631210cfa6621
DoctorKrolic <mapmyp03@gmail.com>
2023-01-18 16:13:05 +0300

    Support fields as well

--- src/Analyzers/CSharp/CodeFixes/MakePropertyRequired/CSharpMakePropertyRequiredCodeFixProvider.cs
+++ src/Analyzers/CSharp/CodeFixes/MakePropertyRequired/CSharpMakePropertyRequiredCodeFixProvider.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Threading;
@@ -47,59 +48,96 @@ internal sealed class CSharpMakePropertyRequiredCodeFixProvider : SyntaxEditorBa
 
         var node = root.FindNode(span);
 
-        if (node is not PropertyDeclarationSyntax propertyDeclaration)
-            return;
-
-        var semanticModel = await document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-        var compilation = semanticModel.Compilation;
-
-        // `RequiredMember` attribute must be present in the metadata in order to emit required members
-        if (compilation.RequiredMemberAttributeType() is null)
-            return;
-
-        var propertySymbol = semanticModel.GetDeclaredSymbol(propertyDeclaration, cancellationToken);
-
-        if (propertySymbol is null)
-            return;
-
-        var setMethod = propertySymbol.SetMethod;
-
-        // Property must have a `set` or `init` accessor in order to be able to be required
-        if (setMethod is null)
-            return;
-
-        var containingTypeVisibility = propertySymbol.ContainingType.GetResultantVisibility();
-
-        var minimalAccessibility = (Accessibility)Math.Min((int)propertySymbol.DeclaredAccessibility, (int)setMethod.DeclaredAccessibility);
-
-        // Property itself and its set/init accessor must have
-        // at least equal visibility as the type they belong to
-        // in order to be able to be required
-        var propertyAndSetterCanBeAccessed = containingTypeVisibility switch
+        if (node is PropertyDeclarationSyntax propertyDeclaration)
         {
-            SymbolVisibility.Public => minimalAccessibility is Accessibility.Public,
-            SymbolVisibility.Internal => minimalAccessibility is >= Accessibility.Internal,
-            SymbolVisibility.Private => minimalAccessibility is >= Accessibility.Internal,
+            var semanticModel = await document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+
+            var propertySymbol = semanticModel.GetDeclaredSymbol(propertyDeclaration, cancellationToken);
+
+            if (propertySymbol is null)
+                return;
+
+            var setMethod = propertySymbol.SetMethod;
+
+            // Property must have a `set` or `init` accessor in order to be able to be required
+            if (setMethod is null)
+                return;
+
+            var containingTypeVisibility = propertySymbol.ContainingType.GetResultantVisibility();
+            var minimalAccessibility = (Accessibility)Math.Min((int)propertySymbol.DeclaredAccessibility, (int)setMethod.DeclaredAccessibility);
+
+            if (!CanBeAccessed(containingTypeVisibility, minimalAccessibility))
+                return;
+
+            RegisterCodeFix(context, CSharpCodeFixesResources.Make_property_required, nameof(CSharpCodeFixesResources.Make_property_required));
+        }
+        else if (node is VariableDeclaratorSyntax { Parent.Parent: FieldDeclarationSyntax })
+        {
+            var semanticModel = await document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+
+            var fieldSymbol = semanticModel.GetDeclaredSymbol(node, cancellationToken);
+
+            if (fieldSymbol is null)
+                return;
+
+            var containingTypeVisibility = fieldSymbol.ContainingType.GetResultantVisibility();
+            var accessibility = fieldSymbol.DeclaredAccessibility;
+
+            if (!CanBeAccessed(containingTypeVisibility, accessibility))
+                return;
+
+            RegisterCodeFix(context, CSharpCodeFixesResources.Make_field_required, nameof(CSharpCodeFixesResources.Make_field_required));
+        }
+
+        static bool CanBeAccessed(SymbolVisibility containingTypeVisibility, Accessibility accessibility) => containingTypeVisibility switch
+        {
+            SymbolVisibility.Public => accessibility is Accessibility.Public,
+            SymbolVisibility.Internal => accessibility is >= Accessibility.Internal,
+            SymbolVisibility.Private => accessibility is >= Accessibility.Internal,
             _ => throw ExceptionUtilities.Unreachable(),
         };
-
-        if (!propertyAndSetterCanBeAccessed)
-            return;
-
-        RegisterCodeFix(context, CSharpCodeFixesResources.Make_property_required, nameof(CSharpCodeFixesResources.Make_property_required));
     }
 
     protected override Task FixAllAsync(Document document, ImmutableArray<Diagnostic> diagnostics, SyntaxEditor editor, CodeActionOptionsProvider fallbackOptions, CancellationToken cancellationToken)
     {
         var root = editor.OriginalRoot;
         var generator = editor.Generator;
+        var visitedFieldDeclarations = new HashSet<FieldDeclarationSyntax>();
 
         foreach (var diagnostic in diagnostics)
         {
-            var propertyDeclaration = root.FindNode(diagnostic.Location.SourceSpan);
-            var declarationModifiers = generator.GetModifiers(propertyDeclaration);
-            var newDeclarationModifiers = declarationModifiers.WithIsRequired(true);
-            editor.ReplaceNode(propertyDeclaration, generator.WithModifiers(propertyDeclaration, newDeclarationModifiers));
+            var propertyDeclarationOrFieldVariableDeclarator = root.FindNode(diagnostic.Location.SourceSpan);
+
+            // If we are fixing field, do not apply new declaration modifiers just to variable declarator, but to the whole field declaration.
+            // This is observable when there are several variables in single filed declaration:
+            // `public string _myField, _myField1;` -> `public required string _myField, _myField1;`
+            // Without this branch the result would be:
+            // ```
+            // public required string _myField;
+            // public required string _myField2;
+            // ```
+            if (propertyDeclarationOrFieldVariableDeclarator is VariableDeclaratorSyntax { Parent.Parent: FieldDeclarationSyntax fieldDeclaration })
+            {
+                // Skip field declarations we already visited to not try changing the same declaration twice.
+                // Otherwise we get an exception in fix-all scenario like this:
+                // `public string _myField, _myField1`
+                // Here when visiting diagnostic for `_myField1` we already changed this field declaration.
+                // Trying to change it again throws in `SyntaxEditor` later, because it cannot find the node.
+                if (visitedFieldDeclarations.Contains(fieldDeclaration))
+                    continue;
+
+                var declarationModifiers = generator.GetModifiers(fieldDeclaration);
+                var newDeclarationModifiers = declarationModifiers.WithIsRequired(true);
+                editor.ReplaceNode(fieldDeclaration, generator.WithModifiers(fieldDeclaration, newDeclarationModifiers));
+
+                visitedFieldDeclarations.Add(fieldDeclaration);
+            }
+            else
+            {
+                var declarationModifiers = generator.GetModifiers(propertyDeclarationOrFieldVariableDeclarator);
+                var newDeclarationModifiers = declarationModifiers.WithIsRequired(true);
+                editor.ReplaceNode(propertyDeclarationOrFieldVariableDeclarator, generator.WithModifiers(propertyDeclarationOrFieldVariableDeclarator, newDeclarationModifiers));
+            }
         }
 
         return Task.CompletedTask;
--- src/Analyzers/CSharp/Tests/MakePropertyRequired/MakePropertyRequiredTests.cs
+++ src/Analyzers/CSharp/Tests/MakePropertyRequired/MakePropertyRequiredTests.cs
@@ -284,6 +284,140 @@ namespace Microsoft.CodeAnalysis.CSharp.Analyzers.UnitTests.MakePropertyRequired
         }
 
         [Fact]
+        public async Task SimpleField()
+        {
+            await new VerifyCS.Test
+            {
+                TestCode = """
+                    #nullable enable
+                
+                    class MyClass
+                    {
+                        public string {|CS8618:_myField|};
+                    }
+                    """,
+                FixedCode = """
+                    #nullable enable
+                
+                    class MyClass
+                    {
+                        public required string _myField;
+                    }
+                    """,
+                LanguageVersion = LanguageVersion.CSharp11,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net70
+            }.RunAsync();
+        }
+
+        [Theory]
+        [InlineData("public", "public", "public", true)]
+        [InlineData("public", "public", "private", false)]
+        [InlineData("public", "public", "protected", false)]
+        [InlineData("public", "public", "internal", false)]
+        [InlineData("public", "public", "private protected", false)]
+        [InlineData("public", "public", "protected internal", false)]
+        [InlineData("public", "private", "public", true)]
+        [InlineData("public", "private", "private", false)]
+        [InlineData("public", "private", "protected", false)]
+        [InlineData("public", "private", "internal", true)]
+        [InlineData("public", "private", "private protected", false)]
+        [InlineData("public", "private", "protected internal", true)]
+        [InlineData("public", "protected", "public", true)]
+        [InlineData("public", "protected", "private", false)]
+        [InlineData("public", "protected", "protected", false)]
+        [InlineData("public", "protected", "internal", false)]
+        [InlineData("public", "protected", "private protected", false)]
+        [InlineData("public", "protected", "protected internal", false)]
+        [InlineData("public", "internal", "public", true)]
+        [InlineData("public", "internal", "private", false)]
+        [InlineData("public", "internal", "protected", false)]
+        [InlineData("public", "internal", "internal", true)]
+        [InlineData("public", "internal", "private protected", false)]
+        [InlineData("public", "internal", "protected internal", true)]
+        [InlineData("public", "private protected", "public", true)]
+        [InlineData("public", "private protected", "private", false)]
+        [InlineData("public", "private protected", "protected", false)]
+        [InlineData("public", "private protected", "internal", true)]
+        [InlineData("public", "private protected", "private protected", false)]
+        [InlineData("public", "private protected", "protected internal", true)]
+        [InlineData("public", "protected internal", "public", true)]
+        [InlineData("public", "protected internal", "private", false)]
+        [InlineData("public", "protected internal", "protected", false)]
+        [InlineData("public", "protected internal", "internal", false)]
+        [InlineData("public", "protected internal", "private protected", false)]
+        [InlineData("public", "protected internal", "protected internal", false)]
+        [InlineData("internal", "public", "public", true)]
+        [InlineData("internal", "public", "private", false)]
+        [InlineData("internal", "public", "protected", false)]
+        [InlineData("internal", "public", "internal", true)]
+        [InlineData("internal", "public", "private protected", false)]
+        [InlineData("internal", "public", "protected internal", true)]
+        [InlineData("internal", "private", "public", true)]
+        [InlineData("internal", "private", "private", false)]
+        [InlineData("internal", "private", "protected", false)]
+        [InlineData("internal", "private", "internal", true)]
+        [InlineData("internal", "private", "private protected", false)]
+        [InlineData("internal", "private", "protected internal", true)]
+        [InlineData("internal", "protected", "public", true)]
+        [InlineData("internal", "protected", "private", false)]
+        [InlineData("internal", "protected", "protected", false)]
+        [InlineData("internal", "protected", "internal", true)]
+        [InlineData("internal", "protected", "private protected", false)]
+        [InlineData("internal", "protected", "protected internal", true)]
+        [InlineData("internal", "internal", "public", true)]
+        [InlineData("internal", "internal", "private", false)]
+        [InlineData("internal", "internal", "protected", false)]
+        [InlineData("internal", "internal", "internal", true)]
+        [InlineData("internal", "internal", "private protected", false)]
+        [InlineData("internal", "internal", "protected internal", true)]
+        [InlineData("internal", "private protected", "public", true)]
+        [InlineData("internal", "private protected", "private", false)]
+        [InlineData("internal", "private protected", "protected", false)]
+        [InlineData("internal", "private protected", "internal", true)]
+        [InlineData("internal", "private protected", "private protected", false)]
+        [InlineData("internal", "private protected", "protected internal", true)]
+        [InlineData("internal", "protected internal", "public", true)]
+        [InlineData("internal", "protected internal", "private", false)]
+        [InlineData("internal", "protected internal", "protected", false)]
+        [InlineData("internal", "protected internal", "internal", true)]
+        [InlineData("internal", "protected internal", "private protected", false)]
+        [InlineData("internal", "protected internal", "protected internal", true)]
+        public async Task TestEffectiveFieldAccessibility(string outerClassAccessibility, string containingTypeAccessibility, string fieldAccessibility, bool shouldOfferFix)
+        {
+            var code = $$"""
+                #nullable enable
+                
+                {{outerClassAccessibility}} class C
+                {
+                    {{containingTypeAccessibility}} class MyClass
+                    {
+                        {{fieldAccessibility}} string {|CS8618:_myField|};
+                    }
+                }
+                """;
+
+            var fixedCode = $$"""
+                #nullable enable
+                
+                {{outerClassAccessibility}} class C
+                {
+                    {{containingTypeAccessibility}} class MyClass
+                    {
+                        {{fieldAccessibility}} required string _myField;
+                    }
+                }
+                """;
+
+            await new VerifyCS.Test
+            {
+                TestCode = code,
+                FixedCode = shouldOfferFix ? fixedCode : code,
+                LanguageVersion = LanguageVersion.CSharp11,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net70
+            }.RunAsync();
+        }
+
+        [Fact]
         public async Task NotForLowerVersionOfCSharp()
         {
             var code = """
@@ -305,27 +439,6 @@ namespace Microsoft.CodeAnalysis.CSharp.Analyzers.UnitTests.MakePropertyRequired
         }
 
         [Fact]
-        public async Task NotIfNoRequiredAttributeInMetadata()
-        {
-            var code = """
-                #nullable enable
-                
-                class MyClass
-                {
-                    public string {|CS8618:MyProperty|} { get; set; }
-                }
-                """;
-
-            await new VerifyCS.Test
-            {
-                TestCode = code,
-                FixedCode = code,
-                LanguageVersion = LanguageVersion.CSharp11,
-                ReferenceAssemblies = ReferenceAssemblies.Net.Net60
-            }.RunAsync();
-        }
-
-        [Fact]
         public async Task NotOnConstructorDeclaration()
         {
             var code = """
@@ -336,27 +449,6 @@ namespace Microsoft.CodeAnalysis.CSharp.Analyzers.UnitTests.MakePropertyRequired
                     public string MyProperty { get; set; }
 
                     public {|CS8618:MyClass|}() { }
-                }
-                """;
-
-            await new VerifyCS.Test
-            {
-                TestCode = code,
-                FixedCode = code,
-                LanguageVersion = LanguageVersion.CSharp11,
-                ReferenceAssemblies = ReferenceAssemblies.Net.Net70
-            }.RunAsync();
-        }
-
-        [Fact]
-        public async Task NotOnFieldDeclaration()
-        {
-            var code = """
-                #nullable enable
-                
-                class MyClass
-                {
-                    public string {|CS8618:_myField|};
                 }
                 """;
 
@@ -391,7 +483,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Analyzers.UnitTests.MakePropertyRequired
         }
 
         [Fact]
-        public async Task FixAll()
+        public async Task FixAll1()
         {
             await new VerifyCS.Test
             {
@@ -411,6 +503,114 @@ namespace Microsoft.CodeAnalysis.CSharp.Analyzers.UnitTests.MakePropertyRequired
                     {
                         public required string MyProperty { get; set; }
                         public required string MyProperty1 { get; set; }
+                    }
+                    """,
+                LanguageVersion = LanguageVersion.CSharp11,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net70
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task FixAll2()
+        {
+            await new VerifyCS.Test
+            {
+                TestCode = """
+                    #nullable enable
+
+                    class MyClass
+                    {
+                        public string {|CS8618:_myField|};
+                        public string {|CS8618:_myField1|};
+                    }
+                    """,
+                FixedCode = """
+                    #nullable enable
+
+                    class MyClass
+                    {
+                        public required string _myField;
+                        public required string _myField1;
+                    }
+                    """,
+                LanguageVersion = LanguageVersion.CSharp11,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net70
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task FixAll3()
+        {
+            await new VerifyCS.Test
+            {
+                TestCode = """
+                    #nullable enable
+
+                    class MyClass
+                    {
+                        public string {|CS8618:_myField|}, {|CS8618:_myField1|};
+                    }
+                    """,
+                FixedCode = """
+                    #nullable enable
+
+                    class MyClass
+                    {
+                        public required string _myField, _myField1;
+                    }
+                    """,
+                LanguageVersion = LanguageVersion.CSharp11,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net70
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task FixAll4()
+        {
+            await new VerifyCS.Test
+            {
+                TestCode = """
+                    #nullable enable
+
+                    class MyClass
+                    {
+                        public string {|CS8618:_myField|}, {|CS8618:_myField1|}, {|CS8618:_myField2|};
+                    }
+                    """,
+                FixedCode = """
+                    #nullable enable
+
+                    class MyClass
+                    {
+                        public required string _myField, _myField1, _myField2;
+                    }
+                    """,
+                LanguageVersion = LanguageVersion.CSharp11,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net70
+            }.RunAsync();
+        }
+
+        [Fact]
+        public async Task FixAll5()
+        {
+            await new VerifyCS.Test
+            {
+                TestCode = """
+                    #nullable enable
+
+                    class MyClass
+                    {
+                        public string {|CS8618:_myField|}, {|CS8618:_myField1|};
+                        public string {|CS8618:_myField2|}, {|CS8618:_myField3|};
+                    }
+                    """,
+                FixedCode = """
+                    #nullable enable
+
+                    class MyClass
+                    {
+                        public required string _myField, _myField1;
+                        public required string _myField2, _myField3;
                     }
                     """,
                 LanguageVersion = LanguageVersion.CSharp11,
