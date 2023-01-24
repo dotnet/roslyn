0a0bca4ee371f6d52f0290c75551d0ceb594acf9
Shen Chen <shech@microsoft.com>
2023-01-23 16:06:51 -0800

    Add serializer for LiveShareDiagnosicMode and NormalDiagnosticMode

--- src/Features/LanguageServer/Protocol/Features/Options/InternalDiagnosticsOptions.cs
+++ src/Features/LanguageServer/Protocol/Features/Options/InternalDiagnosticsOptions.cs
@@ -8,20 +8,21 @@ namespace Microsoft.CodeAnalysis.Diagnostics;
 
 internal static class InternalDiagnosticsOptions
 {
+    private static readonly EditorConfigValueSerializer<DiagnosticMode> s_editorConfigValueSerializer = EditorConfigValueSerializer.CreateSerializerForEnum<DiagnosticMode>();
     /// <summary>
     /// Diagnostic mode setting for Razor.  This should always be <see cref="DiagnosticMode.LspPull"/> as there is no push support in Razor.
     /// This option is only for passing to the diagnostics service and can be removed when we switch all of Roslyn to LSP pull.
     /// </summary>
     public static readonly Option2<DiagnosticMode> RazorDiagnosticMode = new(
-        "InternalDiagnosticsOptions_RazorDiagnosticMode", defaultValue: DiagnosticMode.LspPull, serializer: EditorConfigValueSerializer.CreateSerializerForEnum<DiagnosticMode>());
+        "InternalDiagnosticsOptions_RazorDiagnosticMode", defaultValue: DiagnosticMode.LspPull, serializer: s_editorConfigValueSerializer);
 
     /// <summary>
     /// Diagnostic mode setting for Live Share.  This should always be <see cref="DiagnosticMode.LspPull"/> as there is no push support in Live Share.
     /// This option is only for passing to the diagnostics service and can be removed when we switch all of Roslyn to LSP pull.
     /// </summary>
     public static readonly Option2<DiagnosticMode> LiveShareDiagnosticMode = new(
-        "InternalDiagnosticsOptions_LiveShareDiagnosticMode", defaultValue: DiagnosticMode.LspPull);
+        "InternalDiagnosticsOptions_LiveShareDiagnosticMode", defaultValue: DiagnosticMode.LspPull, serializer: s_editorConfigValueSerializer);
 
     public static readonly Option2<DiagnosticMode> NormalDiagnosticMode = new(
-        "InternalDiagnosticsOptions_NormalDiagnosticMode", defaultValue: DiagnosticMode.Default);
+        "InternalDiagnosticsOptions_NormalDiagnosticMode", defaultValue: DiagnosticMode.Default, serializer: s_editorConfigValueSerializer);
 }
