2f8d23945a19ab2c43768d7569065794b622af76
Shen Chen <shech@microsoft.com>
2023-01-23 16:04:58 -0800

    Add serializer for RazorDiagnosticMode

--- src/Features/LanguageServer/Protocol/Features/Options/InternalDiagnosticsOptions.cs
+++ src/Features/LanguageServer/Protocol/Features/Options/InternalDiagnosticsOptions.cs
@@ -13,7 +13,7 @@ internal static class InternalDiagnosticsOptions
     /// This option is only for passing to the diagnostics service and can be removed when we switch all of Roslyn to LSP pull.
     /// </summary>
     public static readonly Option2<DiagnosticMode> RazorDiagnosticMode = new(
-        "InternalDiagnosticsOptions_RazorDiagnosticMode", defaultValue: DiagnosticMode.LspPull);
+        "InternalDiagnosticsOptions_RazorDiagnosticMode", defaultValue: DiagnosticMode.LspPull, serializer: EditorConfigValueSerializer.CreateSerializerForEnum<DiagnosticMode>());
 
     /// <summary>
     /// Diagnostic mode setting for Live Share.  This should always be <see cref="DiagnosticMode.LspPull"/> as there is no push support in Live Share.
