3164c223e7f9b13dbfd029df50c05cb17caffa83
tmat <tomas.matousek@microsoft.com>
2023-01-30 13:20:55 -0800

    Fix

--- src/Compilers/CSharp/Portable/Lowering/Instrumentation/CodeCoverageInstrumenter.cs
+++ src/Compilers/CSharp/Portable/Lowering/Instrumentation/CodeCoverageInstrumenter.cs
@@ -4,6 +4,7 @@
 
 using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Microsoft.Cci;
 using Microsoft.CodeAnalysis.CodeGen;
