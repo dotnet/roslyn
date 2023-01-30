9db48c7b3d57174bce9113b72cdb97abf7000a3a
Julien Couvreur <julien.couvreur@gmail.com>
2023-01-30 15:13:19 -0800

    Remove unused usings

--- src/Compilers/CSharp/Portable/Binder/Binder_Await.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Await.cs
@@ -2,11 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Roslyn.Utilities;
