af57feb58f5f85613da53ee85d8c6fc99814d583
Andrew Hall <andrha@microsoft.com>
2023-01-31 14:14:36 -0800

    Don't error if a member isn't found relating to code

--- src/Features/Core/Portable/PullMemberUp/MemberAndDestinationValidator.cs
+++ src/Features/Core/Portable/PullMemberUp/MemberAndDestinationValidator.cs
@@ -2,8 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.  
 
-#nullable disable
-
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 using Microsoft.CodeAnalysis.Shared.Extensions;
@@ -27,8 +26,13 @@ namespace Microsoft.CodeAnalysis.PullMemberUp
             return isDestinationInSourceAndNotGeneratedCode;
         }
 
-        public static bool IsMemberValid(ISymbol member)
+        public static bool IsMemberValid([NotNullWhen(true)] ISymbol? member)
         {
+            if (member is null)
+            {
+                return false;
+            }
+
             if (member.IsImplicitlyDeclared)
             {
                 return false;
