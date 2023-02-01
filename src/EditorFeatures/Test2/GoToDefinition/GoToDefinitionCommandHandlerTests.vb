76291f7c4528a768a0bbf1abb34b5ccd7628fe15
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-02-01 10:26:21 -0800

    Support goto-def when the user has selected the identifier

--- src/EditorFeatures/Test2/GoToDefinition/GoToDefinitionCommandHandlerTests.vb
+++ src/EditorFeatures/Test2/GoToDefinition/GoToDefinitionCommandHandlerTests.vb
@@ -2,6 +2,7 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
+Imports Microsoft.CodeAnalysis.Editor.[Shared].Extensions
 Imports Microsoft.CodeAnalysis.Editor.Shared.Utilities
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Utilities
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Utilities.GoToHelpers
@@ -11,6 +12,7 @@ Imports Microsoft.CodeAnalysis.Navigation
 Imports Microsoft.CodeAnalysis.Options
 Imports Microsoft.CodeAnalysis.Shared.TestHooks
 Imports Microsoft.CodeAnalysis.Text
+Imports Microsoft.VisualStudio.Text
 Imports Microsoft.VisualStudio.Text.Editor.Commanding.Commands
 Imports Microsoft.VisualStudio.Utilities
 Imports Roslyn.Utilities
@@ -108,6 +110,56 @@ int y = x$$</Document>
 
                 Assert.True(mockDocumentNavigationService._triedNavigationToSpan)
                 Assert.Equal(New TextSpan(4, 1), mockDocumentNavigationService._span)
+                Assert.Equal(document.Id, mockDocumentNavigationService._documentId)
+            End Using
+        End Function
+
+        <WpfTheory, Trait(Traits.Feature, Traits.Features.GoToDefinition)>
+        <CombinatorialData>
+        Public Async Function TestWithSelection(reversedSelection As Boolean) As Task
+            Dim definition =
+<Workspace>
+    <Project Language="C#" CommonReferences="true" AssemblyName="CSProj">
+        <Document FilePath="C.cs">
+class C
+{
+    int X;
+
+    void M()
+    {
+        _ = X%2; // Press F12 with caret between X and %
+    }
+}
+        </Document>
+    </Project>
+</Workspace>
+
+            Using workspace = TestWorkspace.Create(definition, composition:=GoToTestHelpers.Composition)
+
+                Dim document = workspace.Documents.First()
+                Dim view = document.GetTextView()
+
+                Dim mockDocumentNavigationService =
+                    DirectCast(workspace.Services.GetService(Of IDocumentNavigationService)(), MockDocumentNavigationService)
+
+                Dim provider = workspace.GetService(Of IAsynchronousOperationListenerProvider)()
+                Dim waiter = provider.GetWaiter(FeatureAttribute.GoToDefinition)
+                Dim handler = New GoToDefinitionCommandHandler(
+                    workspace.GetService(Of IGlobalOptionService),
+                    workspace.GetService(Of IThreadingContext),
+                    workspace.GetService(Of IUIThreadOperationExecutor),
+                    provider)
+
+                Dim snapshot = document.GetTextBuffer().CurrentSnapshot
+                Dim index = snapshot.GetText().IndexOf("X%")
+
+                view.SetSelection(New SnapshotSpan(snapshot, New Span(index, 1)), isReversed:=reversedSelection)
+
+                handler.ExecuteCommand(New GoToDefinitionCommandArgs(view, document.GetTextBuffer()), TestCommandExecutionContext.Create())
+                Await waiter.ExpeditedWaitAsync()
+
+                Assert.True(mockDocumentNavigationService._triedNavigationToSpan)
+                Assert.Equal(New TextSpan(22, 1), mockDocumentNavigationService._span)
                 Assert.Equal(document.Id, mockDocumentNavigationService._documentId)
             End Using
         End Function
