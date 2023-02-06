958420b5fa5598ab1b8a0d6dc66036568dcb4e97
Todd Grunke <toddgrun@microsoft.com>
2023-02-06 15:27:58 -0800

    Add comment about why RequiresPreviousQueueItemsCancelled is necessary

--- src/Features/LanguageServer/Microsoft.CommonLanguageServerProtocol.Framework/RequestConcurrency.cs
+++ src/Features/LanguageServer/Microsoft.CommonLanguageServerProtocol.Framework/RequestConcurrency.cs
@@ -19,6 +19,13 @@ namespace Microsoft.CommonLanguageServerProtocol.Framework
         /// <summary>
         /// Indicates this request requires previous requests to be cancelled before execution
         /// </summary>
+        /// <remarks>
+        /// This was added for WebTools consumption as they aren't resilient to previous
+        /// incomplete requests continuing execution during didChange notifications. As their
+        /// parse trees are mutable, a didChange notification requires all previous requests
+        /// to be completed before processing. This is similar to the O#
+        /// WithContentModifiedSupport(false) behavior.
+        /// </remarks>
         RequiresPreviousQueueItemsCancelled = 1,
 
         /// <summary>
