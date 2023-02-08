2139bd1d8b98a66bba41062d310ffb1e187e7483
Ryan Brandenburg <rybrande@microsoft.com>
2023-02-07 16:54:39 -0800

    Fix OnInitialized

--- src/Features/LanguageServer/ProtocolUnitTests/LanguageServerTargetTests.cs
+++ src/Features/LanguageServer/ProtocolUnitTests/LanguageServerTargetTests.cs
@@ -4,12 +4,15 @@
 
 using System;
 using System.Composition;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.LanguageServer.Handler;
 using Microsoft.CodeAnalysis.LanguageServer.Handler.DocumentChanges;
 using Microsoft.CodeAnalysis.Test.Utilities;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
 using Roslyn.Test.Utilities;
+using StreamJsonRpc;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -72,6 +75,47 @@ namespace Microsoft.CodeAnalysis.LanguageServer.UnitTests
 
             // Get a stateless request handler and verify each server has the same instance.
             Assert.Same(serverOne.GetRequiredLspService<DidOpenHandler>(), serverTwo.GetRequiredLspService<DidOpenHandler>());
+        }
+
+        [Fact]
+        public async Task LanguageServerSucceedsAfterInitializedCalled()
+        {
+            // Arrange
+            await using var server = await CreateTestLspServerAsync("");
+
+            var initializedParams = new InitializedParams();
+
+            await server.ExecuteRequestAsync<InitializedParams, object>(Methods.InitializedName, initializedParams, CancellationToken.None);
+
+            // Act & Assert
+            var didOpenParams = new DidOpenTextDocumentParams
+            {
+                TextDocument = new TextDocumentItem
+                {
+                    Text = "sometext",
+                    Uri = new Uri("C:\\location\\file.json"),
+                }
+            };
+
+            // We just want this to not throw.
+            await server.ExecuteRequestAsync<DidOpenTextDocumentParams, object>(Methods.TextDocumentDidOpenName, didOpenParams, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task LanguageServerRejectsRequestsBeforeInitialized()
+        {
+            await using var server = await CreateTestLspServerAsync("");
+
+            var didOpenParams = new DidOpenTextDocumentParams
+            {
+                TextDocument = new TextDocumentItem
+                {
+                    Text = "sometext",
+                    Uri = new Uri("C:\\location\\file.json"),
+                }
+            };
+            var ex = await Assert.ThrowsAsync<RemoteInvocationException>(async () => await server.ExecuteRequestAsync<DidOpenTextDocumentParams, object>(Methods.TextDocumentDidOpenName, didOpenParams, CancellationToken.None));;
+            Assert.Equal("'initialized' has not been called.", ex.Message);
         }
 
         [Fact]
