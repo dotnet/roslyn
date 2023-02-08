38517dbb71a1f3979add0e43665257b1872cb3f8
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-02-08 12:01:37 -0800

    REmove

--- src/VisualStudio/CSharp/Test/PersistentStorage/AbstractPersistentStorageTests.cs
+++ src/VisualStudio/CSharp/Test/PersistentStorage/AbstractPersistentStorageTests.cs
@@ -4,10 +4,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Composition;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
