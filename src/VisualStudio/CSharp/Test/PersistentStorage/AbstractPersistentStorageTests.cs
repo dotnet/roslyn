6e3826a7ab3ebe7135e2e7a4cb9ffccdc80f642d
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-02-08 12:00:45 -0800

    Give a better message in FAR when not invoked in an ok location

--- src/VisualStudio/CSharp/Test/PersistentStorage/AbstractPersistentStorageTests.cs
+++ src/VisualStudio/CSharp/Test/PersistentStorage/AbstractPersistentStorageTests.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Composition;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
