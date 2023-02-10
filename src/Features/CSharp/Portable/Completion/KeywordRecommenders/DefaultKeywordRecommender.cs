e2ada98c620482d0c5052b0b0f45d802d3f0da6d
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-02-10 12:44:04 -0800

    Fix case where the 'default' keyword was not recommended

--- src/Features/CSharp/Portable/Completion/KeywordRecommenders/DefaultKeywordRecommender.cs
+++ src/Features/CSharp/Portable/Completion/KeywordRecommenders/DefaultKeywordRecommender.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Extensions;
 using Microsoft.CodeAnalysis.CSharp.Extensions.ContextQuery;
 
 namespace Microsoft.CodeAnalysis.CSharp.Completion.KeywordRecommenders
@@ -22,7 +23,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Completion.KeywordRecommenders
                 context.IsGlobalStatementContext ||
                 context.IsAnyExpressionContext ||
                 context.TargetToken.IsSwitchLabelContext() ||
-                context.SyntaxTree.IsTypeParameterConstraintStartContext(position, context.LeftToken);
+                context.SyntaxTree.IsTypeParameterConstraintStartContext(position, context.LeftToken) ||
+                context.LeftToken.IsInCastExpressionTypeWhereExpressionIsMissingOrInNextLine();
         }
 
         private static bool IsValidPreProcessorContext(CSharpSyntaxContext context)
