92986c55c67e354c7c25581ff8a79d3fcf458b43
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-02-13 13:04:55 -0800

    Move the 'make method synchronous' fixer down to shared layer

--- src/Analyzers/VisualBasic/CodeFixes/MakeMethodSynchronous/VisualBasicMakeMethodSynchronousCodeFixProvider.vb
+++ src/Analyzers/VisualBasic/CodeFixes/MakeMethodSynchronous/VisualBasicMakeMethodSynchronousCodeFixProvider.vb
@@ -6,7 +6,6 @@ Imports System.Collections.Immutable
 Imports System.Composition
 Imports System.Diagnostics.CodeAnalysis
 Imports Microsoft.CodeAnalysis.CodeFixes
-Imports Microsoft.CodeAnalysis.MakeMethodAsynchronous.AbstractMakeMethodAsynchronousCodeFixProvider
 Imports Microsoft.CodeAnalysis.MakeMethodSynchronous
 Imports Microsoft.CodeAnalysis.VisualBasic.RemoveAsyncModifier
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
