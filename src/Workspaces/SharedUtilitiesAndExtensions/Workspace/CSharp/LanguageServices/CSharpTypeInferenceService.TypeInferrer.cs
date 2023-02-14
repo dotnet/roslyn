1f75795fc0c27b97809ed98f0cb72247173c8d20
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-02-14 12:45:23 -0800

    Fix enum preselection in switch expressions

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/CSharp/LanguageServices/CSharpTypeInferenceService.TypeInferrer.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/CSharp/LanguageServices/CSharpTypeInferenceService.TypeInferrer.cs
@@ -247,6 +247,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                     ReturnStatementSyntax returnStatement => InferTypeForReturnStatement(returnStatement, token),
                     SingleVariableDesignationSyntax singleVariableDesignationSyntax => InferTypeForSingleVariableDesignation(singleVariableDesignationSyntax),
                     SwitchLabelSyntax switchLabel => InferTypeInSwitchLabel(switchLabel, token),
+                    SwitchExpressionSyntax switchExpression => InferTypeInSwitchExpression(switchExpression, token),
                     SwitchStatementSyntax switchStatement => InferTypeInSwitchStatement(switchStatement, token),
                     ThrowStatementSyntax throwStatement => InferTypeInThrowStatement(throwStatement, token),
                     TupleExpressionSyntax tupleExpression => InferTypeInTupleExpression(tupleExpression, token),
@@ -2118,6 +2119,16 @@ namespace Microsoft.CodeAnalysis.CSharp
                     // if a prior arm gave us nothing useful, or we're the first arm, then try to infer looking at
                     // what type gets inferred for the switch expression itself.
                     return InferTypes(switchExpression);
+                }
+
+                return SpecializedCollections.EmptyEnumerable<TypeInferenceInfo>();
+            }
+
+            private IEnumerable<TypeInferenceInfo> InferTypeInSwitchExpression(SwitchExpressionSyntax switchExpression, SyntaxToken token)
+            {
+                if (token.Kind() is SyntaxKind.OpenBraceToken or SyntaxKind.CommaToken)
+                {
+                    return GetTypes(switchExpression.GoverningExpression);
                 }
 
                 return SpecializedCollections.EmptyEnumerable<TypeInferenceInfo>();
