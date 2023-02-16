caf4c2745d35113322ae37dae6eccd6292b0700b
Maryam Ariyan <maryam.ariyan@microsoft.com>
2023-02-15 21:44:03 -0800

    Roslyn changes to get data tip information

---
+++ src/Tools/ExternalAccess/Razor/RazorDebugDataTipInfo.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
+{
+    internal readonly struct RazorDebugDataTipInfo
+    {
+        public readonly TextSpan Span;
+        public readonly string Text;
+
+        public RazorDebugDataTipInfo(TextSpan span, string text)
+        {
+            Span = span;
+            Text = text;
+        }
+
+        public bool IsDefault
+            => Span.Length == 0 && Span.Start == 0 && Text == null;
+    }
+}
---
+++ src/Tools/ExternalAccess/Razor/RazorDebugInfoService.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Debugging;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
+{
+    internal class RazorDebugInfoService
+    {
+        public RazorDebugInfoService() { }
+        internal async Task<RazorDebugDataTipInfo> GetDataTipInfoAsync(
+            Document document, int position, CancellationToken cancellationToken)
+        {
+            var languageDebugInfo = document.Project.Services.GetService<ILanguageDebugInfoService>();
+            DebugDataTipInfo info = await languageDebugInfo!.GetDataTipInfoAsync(document, position, cancellationToken).ConfigureAwait(false);
+            return new RazorDebugDataTipInfo(info.Span, info.Text);
+        }
+    }
+}
+
+
+
