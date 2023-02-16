3a2a66337b297171af2667fabeeb3b7aee70e0d1
Shen Chen <shech@microsoft.com>
2023-02-16 11:05:37 -0800

    Onboard other code action options

--- src/Features/LanguageServer/Protocol/Features/Options/ImplementTypeOptionsStorage.cs
+++ src/Features/LanguageServer/Protocol/Features/Options/ImplementTypeOptionsStorage.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.CodeAnalysis.CodeGeneration;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Options;
 
--- src/Features/LanguageServer/Protocol/Handler/Configuration/DidChangeConfigurationNotificationHandler_OptionList.cs
+++ src/Features/LanguageServer/Protocol/Handler/Configuration/DidChangeConfigurationNotificationHandler_OptionList.cs
@@ -3,8 +3,11 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.ImplementType;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.SymbolSearch;
+using CodeActionOptionsStorage = Microsoft.CodeAnalysis.CodeActions.CodeActionOptionsStorage;
 
 namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Configuration
 {
@@ -13,7 +16,13 @@ namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Configuration
         private static readonly ImmutableArray<ISingleValuedOption> s_supportedGlobalOptions = ImmutableArray.Create<ISingleValuedOption>();
 
         private static readonly ImmutableArray<IPerLanguageValuedOption> s_supportedPerLanguageOptions = ImmutableArray.Create<IPerLanguageValuedOption>(
+            // Code Action options
             SymbolSearchOptionsStorage.SearchNuGetPackages,
-            SymbolSearchOptionsStorage.SearchReferenceAssemblies);
+            SymbolSearchOptionsStorage.SearchReferenceAssemblies,
+            ImplementTypeOptionsStorage.InsertionBehavior,
+            ImplementTypeOptionsStorage.PropertyGenerationBehavior,
+            CompletionOptionsStorage.HideAdvancedMembers,
+            CodeActionOptionsStorage.WrappingColumn,
+            CodeActionOptionsStorage.ConditionalExpressionWrappingLength);
     }
 }
