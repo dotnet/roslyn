48953d065486fc8f312ec35ba454079730ca5933
Shen Chen <shech@microsoft.com>
2023-02-17 12:15:13 -0800

    Rename keybidingResetOptions

--- src/VisualStudio/Core/Def/KeybindingReset/KeybindingResetOptions.cs
+++ src/VisualStudio/Core/Def/KeybindingReset/KeybindingResetOptions.cs
@@ -8,9 +8,9 @@ namespace Microsoft.VisualStudio.LanguageServices.KeybindingReset
 {
     internal sealed class KeybindingResetOptions
     {
-        public static readonly Option2<ReSharperStatus> ReSharperStatus = new("dotnet_key_Binding_reset_options_resharper_status", defaultValue: KeybindingReset.ReSharperStatus.NotInstalledOrDisabled);
-        public static readonly Option2<bool> NeedsReset = new("dotnet_key_binding_reset_options_needs_reset", defaultValue: false);
-        public static readonly Option2<bool> NeverShowAgain = new("dotnet_key_binding_reset_options_never_show_again", defaultValue: false);
-        public static readonly Option2<bool> EnabledFeatureFlag = new("dotnet_key_binding_reset_options_enabled_feature_flag", defaultValue: false);
+        public static readonly Option2<ReSharperStatus> ReSharperStatus = new("dotnet_resharper_key_binding_status", defaultValue: KeybindingReset.ReSharperStatus.NotInstalledOrDisabled);
+        public static readonly Option2<bool> NeedsReset = new("dotnet_key_binding_needs_reset", defaultValue: false);
+        public static readonly Option2<bool> NeverShowAgain = new("dotnet_key_binding_reset_never_show_again", defaultValue: false);
+        public static readonly Option2<bool> EnabledFeatureFlag = new("dotnet_key_binding_reset_enabled", defaultValue: false);
     }
 }
