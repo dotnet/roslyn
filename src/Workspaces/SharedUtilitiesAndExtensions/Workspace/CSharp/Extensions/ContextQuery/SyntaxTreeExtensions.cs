6ca4295d39e47654dffd9f0ff791e294ebdf7cdd
DoctorKrolic <mapmyp03@gmail.com>
2023-02-21 22:41:26 +0300

    Fix case with whitespace between the token and current position

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/CSharp/Extensions/ContextQuery/SyntaxTreeExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/CSharp/Extensions/ContextQuery/SyntaxTreeExtensions.cs
@@ -2369,7 +2369,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Extensions.ContextQuery
             // 1..|
             if (token.IsKind(SyntaxKind.DotDotToken) &&
                 token.Parent.IsKind(SyntaxKind.RangeExpression) &&
-                position == token.Span.End)
+                position >= token.Span.End)
             {
                 return true;
             }
