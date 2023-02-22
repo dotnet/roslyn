0024250122d879fb05ca5ce0ae89f540d79b5623
Shen Chen <shech@microsoft.com>
2023-02-21 23:22:44 -0800

    Rename xaml options

--- src/VisualStudio/Core/Def/Options/XamlOptions.cs
+++ src/VisualStudio/Core/Def/Options/XamlOptions.cs
@@ -15,6 +15,6 @@ namespace Microsoft.CodeAnalysis.Editor.Xaml
     /// </summary>
     internal sealed class XamlOptions
     {
-        public static readonly Option2<bool> EnableLspIntelliSenseFeatureFlag = new("dotnet_enable_lsp_intellisense_in_xaml", defaultValue: false);
+        public static readonly Option2<bool> EnableLspIntelliSenseFeatureFlag = new("xaml_enable_lsp_intellisense", defaultValue: false);
     }
 }
