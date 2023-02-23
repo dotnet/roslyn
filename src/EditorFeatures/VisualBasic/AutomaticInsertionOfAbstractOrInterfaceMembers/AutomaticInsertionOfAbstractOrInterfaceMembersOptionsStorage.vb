a67c18224cf418c9e4f92ef0a07bd0c647729d4c
Shen Chen <shech@microsoft.com>
2023-02-23 15:17:11 -0800

    Modify the comment

--- src/EditorFeatures/VisualBasic/AutomaticInsertionOfAbstractOrInterfaceMembers/AutomaticInsertionOfAbstractOrInterfaceMembersOptionsStorage.vb
+++ src/EditorFeatures/VisualBasic/AutomaticInsertionOfAbstractOrInterfaceMembers/AutomaticInsertionOfAbstractOrInterfaceMembersOptionsStorage.vb
@@ -6,7 +6,7 @@ Imports Microsoft.CodeAnalysis.Options
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.AutomaticInsertionOfAbstractOrInterfaceMembers
     Friend Class AutomaticInsertionOfAbstractOrInterfaceMembersOptionsStorage
-        ' This value Is only used by Visual Basic, And so Is using the old serialization name that was used by VB.
+        'This value is only used by Visual Basic, and so is using the old serialization name that was used by vb.
         Public Shared ReadOnly AutomaticInsertionOfAbstractOrInterfaceMembers As PerLanguageOption2(Of Boolean) = New PerLanguageOption2(Of Boolean)(
             "visual_basic_insert_abstract_or_interface_members_on_return", defaultValue:=True)
     End Class
