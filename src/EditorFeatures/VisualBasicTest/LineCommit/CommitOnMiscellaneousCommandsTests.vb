e99d9eea4c4b66aca5d2b1f4c4b13a4b68d1d425
Shen Chen <Cosifne@users.noreply.github.com>
2023-02-23 22:27:05 -0800

    Clean and rename FeatureOnOffOptions (#66881)
    * Clean inheritance margin options
    
    * Clean add import on paste options
    
    * Clean EndConstructGeneration options
    
    * Clean up visual_basic_automatic_insertion_of_abstract_or_interface_members_options_automatic_insertion_of_abstract_or_interface_members
    
    * Clean line seperator options
    
    * Clean outlining options
    
    * Move keyword highlighting options
    
    * Clean Reference highlighting
    
    * Clean AutoInsertBlockCommentStartString options
    
    * Clean pretty listing options
    
    * Clean string indentation options
    
    * Clean rename tracking preview optinons
    
    * Clean complete statement options
    
    * Clean  string copy paste options
    
    * Rename navigate async options
    
    * Rename rename tracking and refactoring verification options
    
    * Rename offer remove unused references options
    
    * Rename skip analyzers for implictly trigger builds
    
    * Fix errors
    
    * Revert "Rename rename tracking and refactoring verification options"
    because there is no storage in VS
    
    This reverts commit dbeee1d87d267a780aa9668ce41973924e1c96d8.
    
    * Rename add imports on paste
    
    * Rename always use default symbol server
    
    * Renmae insert block comment start string, complete statement on semicolon and fix string on paste
    
    * Rename vb automatic insertion of abstract or interface members
    
    * Rename vb generate enf construct
    
    * Rename inheritance margin options
    
    * Rename keyword highlighting
    
    * Rename line separator
    
    * Rename feature on off options
    
    * Rename outlining
    
    * Rename pretty listing
    
    * Rename reference highlighting
    
    * Rename rename_tracking_preview
    
    * Rename skip analyzers for implictly trigger build
    
    * Rename string indentation
    
    * Add enabled suffix to automatic insertion of abstract or interface member
    
    * Remove automatically word
    
    * Rename visual_basic_automatic_insertion_of_abstract_or_interface_members_enabled to visual_basic_insert_abstract_or_interface_members_on_return
    
    * Add Storage suffix to AddImportOnPasteOptions
    
    * Move AutomaticInsertionOfAbstractOrInterfaceMembers to VB project
    
    * Move BlockCommentEditingOptionsStorage to C# project
    
    * Move CompleteStatementOptions to C# project
    
    * Move and rename visual_basic_generate_end_construct
    
    * Rename InheritanceMargin options
    
    * Rename hightlight keyword
    
    * Move and rename LineCommitOptions to VB project
    
    * Rename display line separators
    
    * Rename highlight_refernences
    
    * Change and rename the RenameTrackingOptionStroage
    
    * Rename StringCopyPasteOptions
    
    * Rename String Indentation Options
    
    * Rename OutlineOptionsStorage
    
    * Rename CompletionStatementOptions to Storage
    
    * Rename AddImportOnPasteOptions to storage
    
    * Rename InheritanceMarginOptions to Storage
    
    * Rename KeywordHighlightingOption to storage
    
    * Remove empty directory
    
    * Rename OutlingingOptions to Storage
    
    * Rename OutliningOptions to Storage
    
    * Remove automatically from BlockCommentEditingOptions
    
    * Remove 's' in OutliningOptionsStorage
    
    * Modify the comment
    
    * Generalize to dotnet for NavigateToDecompiledSources and NavigateToSourceLinkAndEmbeddedSources

--- src/EditorFeatures/VisualBasicTest/LineCommit/CommitOnMiscellaneousCommandsTests.vb
+++ src/EditorFeatures/VisualBasicTest/LineCommit/CommitOnMiscellaneousCommandsTests.vb
@@ -2,9 +2,9 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
-Imports Microsoft.CodeAnalysis.Editor.Shared.Options
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Extensions
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Utilities
+Imports Microsoft.CodeAnalysis.Editor.VisualBasic.LineCommit
 Imports Microsoft.CodeAnalysis.Options
 Imports Microsoft.VisualStudio.Text.Editor.Commanding.Commands
 
@@ -72,7 +72,7 @@ End Class
 
                 Dim workspace = testData.Workspace
                 Dim globalOptions = workspace.GetService(Of IGlobalOptionService)
-                globalOptions.SetGlobalOption(FeatureOnOffOptions.PrettyListing, LanguageNames.VisualBasic, False)
+                globalOptions.SetGlobalOption(LineCommitOptionsStorage.PrettyListing, LanguageNames.VisualBasic, False)
 
                 testData.CommandHandler.ExecuteCommand(New PasteCommandArgs(testData.View, testData.Buffer),
                                                        Sub() testData.EditorOperations.InsertText("Class Program" & vbCrLf & "    Sub M(abc As Integer)" & vbCrLf & "        Dim a  = 7" & vbCrLf & "    End Sub" & vbCrLf & "End Class"),
@@ -131,7 +131,7 @@ End Class
                                                    </Workspace>)
                 Dim workspace = testData.Workspace
                 Dim globalOptions = workspace.GetService(Of IGlobalOptionService)
-                globalOptions.SetGlobalOption(FeatureOnOffOptions.PrettyListing, LanguageNames.VisualBasic, False)
+                globalOptions.SetGlobalOption(LineCommitOptionsStorage.PrettyListing, LanguageNames.VisualBasic, False)
 
                 testData.Buffer.Insert(57, "    ")
                 testData.CommandHandler.ExecuteCommand(New SaveCommandArgs(testData.View, testData.Buffer), Sub() Exit Sub, TestCommandExecutionContext.Create())
@@ -180,7 +180,7 @@ End Module
                 ' Turn off pretty listing
                 Dim workspace = testData.Workspace
                 Dim globalOptions = workspace.GetService(Of IGlobalOptionService)
-                globalOptions.SetGlobalOption(FeatureOnOffOptions.PrettyListing, LanguageNames.VisualBasic, False)
+                globalOptions.SetGlobalOption(LineCommitOptionsStorage.PrettyListing, LanguageNames.VisualBasic, False)
 
                 testData.CommandHandler.ExecuteCommand(New FormatDocumentCommandArgs(testData.View, testData.Buffer), Sub() Exit Sub, TestCommandExecutionContext.Create())
                 Assert.Equal("    Sub Main()", testData.Buffer.CurrentSnapshot.GetLineFromLineNumber(1).GetText())
