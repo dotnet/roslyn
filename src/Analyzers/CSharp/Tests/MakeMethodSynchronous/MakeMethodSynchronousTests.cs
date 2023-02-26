ae6f4ddba6056f6803976fa13a1fd55447bef0e0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-02-26 00:45:21 -0800

    Fix

--- src/Analyzers/CSharp/Tests/MakeMethodSynchronous/MakeMethodSynchronousTests.cs
+++ src/Analyzers/CSharp/Tests/MakeMethodSynchronous/MakeMethodSynchronousTests.cs
@@ -816,18 +816,10 @@ class C
         }
 
         [Theory]
-        [InlineData("", "Task<C>", """
-            C
-            """)]
-        [InlineData("", "Task<int>", """
-            int
-            """)]
-        [InlineData("", "Task", """
-            void
-            """)]
-        [InlineData("", "void", """
-            void
-            """)]
+        [InlineData("", "Task<C>", "\r\n    C")]
+        [InlineData("", "Task<int>", "\r\n    int")]
+        [InlineData("", "Task", "\r\n    void")]
+        [InlineData("", "void", "\r\n    void")]
         [InlineData("public", "Task<C>", " C")]
         [InlineData("public", "Task<int>", " int")]
         [InlineData("public", "Task", " void")]
@@ -854,7 +846,7 @@ using System.Threading.Tasks;
 class C
 {{
     // Leading trivia
-    {modifiers}/*1*/{expectedReturn} /*2*/ M2/*3*/() /*4*/
+    {modifiers}/*1*/ {expectedReturn} /*2*/ M2/*3*/() /*4*/
     {{
         throw new NotImplementedException();
     }}
