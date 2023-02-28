35450c1ff83406ebbd5dad89cd59fc79d3a16c2b
Shen Chen <shech@microsoft.com>
2023-02-27 16:09:17 -0800

    Add enable to classification and squiggles

--- src/EditorFeatures/Core/Diagnostics/DiagnosticsOptionsStorage.cs
+++ src/EditorFeatures/Core/Diagnostics/DiagnosticsOptionsStorage.cs
@@ -8,8 +8,8 @@ namespace Microsoft.CodeAnalysis.Diagnostics
 {
     internal static class DiagnosticsOptionsStorage
     {
-        public static readonly Option2<bool> Classification = new("dotnet_classification", defaultValue: true);
+        public static readonly Option2<bool> Classification = new("dotnet_enable_classification", defaultValue: true);
 
-        public static readonly Option2<bool> Squiggles = new("dotnet_squiggles", defaultValue: true);
+        public static readonly Option2<bool> Squiggles = new("dotnet_enable_squiggles", defaultValue: true);
     }
 }
