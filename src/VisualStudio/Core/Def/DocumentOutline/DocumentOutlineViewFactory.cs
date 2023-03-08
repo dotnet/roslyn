baf95e799a7e2f8e29daecc8668b91b7b8e17eea
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-03-08 10:21:58 -0800

    Add failure mode

--- src/VisualStudio/Core/Def/DocumentOutline/DocumentOutlineViewFactory.cs
+++ src/VisualStudio/Core/Def/DocumentOutline/DocumentOutlineViewFactory.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis.Editor.Shared.Extensions;
 using Microsoft.CodeAnalysis.Editor.Shared.Tagging;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
@@ -20,7 +21,7 @@ namespace Microsoft.VisualStudio.LanguageServices.DocumentOutline
     /// </summary>
     internal static class DocumentOutlineViewFactory
     {
-        public static DocumentOutlineView CreateView(
+        public static DocumentOutlineView? CreateView(
             ILanguageServiceBroker2 languageServiceBroker,
             IThreadingContext threadingContext,
             IAsynchronousOperationListener asyncListener,
@@ -28,29 +29,37 @@ namespace Microsoft.VisualStudio.LanguageServices.DocumentOutline
             IVsCodeWindow codeWindow)
         {
             threadingContext.ThrowIfNotOnUIThread();
-            var (textViewEventSource, textBuffer) = CreateEventSource(asyncListener, editorAdaptersFactoryService, codeWindow);
-            var viewModel = new DocumentOutlineViewModel(languageServiceBroker, asyncListener, textViewEventSource, textBuffer, threadingContext);
+            if (!TryCreateEventSource(asyncListener, editorAdaptersFactoryService, codeWindow, out var eventSource, out var textBuffer))
+                return null;
+
+            var viewModel = new DocumentOutlineViewModel(languageServiceBroker, asyncListener, eventSource, textBuffer, threadingContext);
             return new DocumentOutlineView(viewModel, editorAdaptersFactoryService, codeWindow, threadingContext);
         }
 
-        private static (CompilationAvailableTaggerEventSource, ITextBuffer) CreateEventSource(
+        private static bool TryCreateEventSource(
             IAsynchronousOperationListener asyncListener,
             IVsEditorAdaptersFactoryService editorAdaptersFactoryService,
-            IVsCodeWindow codeWindow)
+            IVsCodeWindow codeWindow,
+            [NotNullWhen(true)] out ITaggerEventSource? eventSource,
+            [NotNullWhen(true)] out ITextBuffer? subjectBuffer)
         {
+            eventSource = null;
+            subjectBuffer = null;
+
             if (ErrorHandler.Failed(codeWindow.GetLastActiveView(out var textView)))
             {
                 Debug.Fail("Unable to get the last active text view. IVsCodeWindow implementation we are given is invalid.");
+                return false;
             }
 
             var wpfTextView = editorAdaptersFactoryService.GetWpfTextView(textView);
             Assumes.NotNull(wpfTextView);
 
-            var subjectBuffer = wpfTextView.TextBuffer;
+            subjectBuffer = wpfTextView.TextBuffer;
             // Unfortunately, the DocumentSymbol LSP request as its implemented requires semantic information so 
             // we need to listen for compilation changes. This can be simplifies if Roslyn's implementation of the
             // DocumentSymbol request is ever changed to just operate off of syntax tree.
-            var textViewEventSource = new CompilationAvailableTaggerEventSource(
+            eventSource = new CompilationAvailableTaggerEventSource(
                 subjectBuffer,
                 asyncListener,
                 // Any time an edit happens, recompute as the document symbols may have changed.
@@ -61,7 +70,7 @@ namespace Microsoft.VisualStudio.LanguageServices.DocumentOutline
                 TaggerEventSources.OnWorkspaceChanged(subjectBuffer, asyncListener),
                 // Once we hook this buffer up to the workspace, then we can start computing the document symbols.
                 TaggerEventSources.OnWorkspaceRegistrationChanged(subjectBuffer));
-            return (textViewEventSource, subjectBuffer);
+            return true;
         }
     }
 }
