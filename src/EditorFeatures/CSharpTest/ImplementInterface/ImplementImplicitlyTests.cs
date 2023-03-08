28447e6075a059da6d64d1bfe9b6f383b9eea23a
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-03-07 17:44:48 -0800

    Update tests to raw strings

--- src/EditorFeatures/CSharpTest/ImplementInterface/ImplementImplicitlyTests.cs
+++ src/EditorFeatures/CSharpTest/ImplementInterface/ImplementImplicitlyTests.cs
@@ -33,154 +33,166 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.ImplementInterface
         public async Task TestSingleMember()
         {
             await TestInRegularAndScriptAsync(
-@"
-interface IGoo { void Goo1(); void Goo2(); }
-interface IBar { void Bar(); }
+                """
+                interface IGoo { void Goo1(); void Goo2(); }
+                interface IBar { void Bar(); }
 
-class C : IGoo, IBar
-{
-    void IGoo.[||]Goo1() { }
+                class C : IGoo, IBar
+                {
+                    void IGoo.[||]Goo1() { }
 
-    void IGoo.Goo2() { }
+                    void IGoo.Goo2() { }
 
-    void IBar.Bar() { }
-}",
-@"
-interface IGoo { void Goo1(); void Goo2(); }
-interface IBar { void Bar(); }
+                    void IBar.Bar() { }
+                }
+                """,
+                """
+                interface IGoo { void Goo1(); void Goo2(); }
+                interface IBar { void Bar(); }
 
-class C : IGoo, IBar
-{
-    public void Goo1() { }
+                class C : IGoo, IBar
+                {
+                    public void Goo1() { }
 
-    void IGoo.Goo2() { }
+                    void IGoo.Goo2() { }
 
-    void IBar.Bar() { }
-}", index: SingleMember);
+                    void IBar.Bar() { }
+                }
+                """, index: SingleMember);
         }
 
         [Fact]
         public async Task TestSameInterface()
         {
             await TestInRegularAndScriptAsync(
-@"
-interface IGoo { void Goo1(); void Goo2(); }
-interface IBar { void Bar(); }
+                """
+                interface IGoo { void Goo1(); void Goo2(); }
+                interface IBar { void Bar(); }
 
-class C : IGoo, IBar
-{
-    void IGoo.[||]Goo1() { }
+                class C : IGoo, IBar
+                {
+                    void IGoo.[||]Goo1() { }
 
-    void IGoo.Goo2() { }
+                    void IGoo.Goo2() { }
 
-    void IBar.Bar() { }
-}",
-@"
-interface IGoo { void Goo1(); void Goo2(); }
-interface IBar { void Bar(); }
+                    void IBar.Bar() { }
+                }
+                """,
+                """
+                interface IGoo { void Goo1(); void Goo2(); }
+                interface IBar { void Bar(); }
 
-class C : IGoo, IBar
-{
-    public void Goo1() { }
+                class C : IGoo, IBar
+                {
+                    public void Goo1() { }
 
-    public void Goo2() { }
+                    public void Goo2() { }
 
-    void IBar.Bar() { }
-}", index: SameInterface);
+                    void IBar.Bar() { }
+                }
+                """, index: SameInterface);
         }
 
         [Fact]
         public async Task TestAllInterfaces()
         {
             await TestInRegularAndScriptAsync(
-@"
-interface IGoo { void Goo1(); void Goo2(); }
-interface IBar { void Bar(); }
+                """
+                interface IGoo { void Goo1(); void Goo2(); }
+                interface IBar { void Bar(); }
 
-class C : IGoo, IBar
-{
-    void IGoo.[||]Goo1() { }
+                class C : IGoo, IBar
+                {
+                    void IGoo.[||]Goo1() { }
 
-    void IGoo.Goo2() { }
+                    void IGoo.Goo2() { }
 
-    void IBar.Bar() { }
-}",
-@"
-interface IGoo { void Goo1(); void Goo2(); }
-interface IBar { void Bar(); }
+                    void IBar.Bar() { }
+                }
+                """,
+                """
+                interface IGoo { void Goo1(); void Goo2(); }
+                interface IBar { void Bar(); }
 
-class C : IGoo, IBar
-{
-    public void Goo1() { }
+                class C : IGoo, IBar
+                {
+                    public void Goo1() { }
 
-    public void Goo2() { }
+                    public void Goo2() { }
 
-    public void Bar() { }
-}", index: AllInterfaces);
+                    public void Bar() { }
+                }
+                """, index: AllInterfaces);
         }
 
         [Fact]
         public async Task TestProperty()
         {
             await TestInRegularAndScriptAsync(
-@"
-interface IGoo { int Goo1 { get; } }
+                """
+                interface IGoo { int Goo1 { get; } }
 
-class C : IGoo
-{
-    int IGoo.[||]Goo1 { get { } }
-}",
-@"
-interface IGoo { int Goo1 { get; } }
+                class C : IGoo
+                {
+                    int IGoo.[||]Goo1 { get { } }
+                }
+                """,
+                """
+                interface IGoo { int Goo1 { get; } }
 
-class C : IGoo
-{
-    public int Goo1 { get { } }
-}", index: SingleMember);
+                class C : IGoo
+                {
+                    public int Goo1 { get { } }
+                }
+                """, index: SingleMember);
         }
 
         [Fact]
         public async Task TestEvent()
         {
             await TestInRegularAndScriptAsync(
-@"
-interface IGoo { event Action E; }
+                """
+                interface IGoo { event Action E; }
 
-class C : IGoo
-{
-    event Action IGoo.[||]E { add { } remove { } }
-}",
-@"
-interface IGoo { event Action E; }
+                class C : IGoo
+                {
+                    event Action IGoo.[||]E { add { } remove { } }
+                }
+                """,
+                """
+                interface IGoo { event Action E; }
 
-class C : IGoo
-{
-    public event Action E { add { } remove { } }
-}", index: SingleMember);
+                class C : IGoo
+                {
+                    public event Action E { add { } remove { } }
+                }
+                """, index: SingleMember);
         }
 
         [Fact]
         public async Task TestNotOnImplicitMember()
         {
             await TestMissingAsync(
-@"
-interface IGoo { void Goo1(); }
+                """
+                interface IGoo { void Goo1(); }
 
-class C : IGoo
-{
-    public void [||]Goo1() { }
-}");
+                class C : IGoo
+                {
+                    public void [||]Goo1() { }
+                }
+                """);
         }
 
         [Fact]
         public async Task TestNotOnUnboundExplicitImpl()
         {
             await TestMissingAsync(
-@"
-class C : IGoo
-{
-    void IGoo.[||]Goo1() { }
-}");
+                """
+                class C : IGoo
+                {
+                    void IGoo.[||]Goo1() { }
+                }
+                """);
         }
 
         [Fact]
@@ -189,61 +201,66 @@ class C : IGoo
             // Currently we don't do anything special here.  But we just test here to make sure we
             // don't blow up here.
             await TestInRegularAndScriptAsync(
-@"
-interface IGoo { void Goo1(); }
+                """
+                interface IGoo { void Goo1(); }
 
-class C : IGoo
-{
-    void IGoo.[||]Goo1() { }
+                class C : IGoo
+                {
+                    void IGoo.[||]Goo1() { }
 
-    private void Goo1() { }
-}",
-@"
-interface IGoo { void Goo1(); }
+                    private void Goo1() { }
+                }
+                """,
+                """
+                interface IGoo { void Goo1(); }
 
-class C : IGoo
-{
-    public void Goo1() { }
+                class C : IGoo
+                {
+                    public void Goo1() { }
 
-    private void Goo1() { }
-}", index: SingleMember);
+                    private void Goo1() { }
+                }
+                """, index: SingleMember);
         }
 
         [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/48027")]
         public async Task TestSingleMemberAndContainingTypeHasNoInterface()
         {
             await TestMissingAsync(
-@"
-using System;
-using System.Collections;
+                """
+                using System;
+                using System.Collections;
 
-class C
-{
-    IEnumerator IEnumerable.[||]GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-}");
+                class C
+                {
+                    IEnumerator IEnumerable.[||]GetEnumerator()
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                """);
         }
 
         [Fact]
         public async Task TestPreserveReadOnly()
         {
             await TestInRegularAndScriptAsync(
-@"
-interface IGoo { void Goo1(); }
+                """
+                interface IGoo { void Goo1(); }
 
-class C : IGoo
-{
-    readonly void IGoo.[||]Goo1() { }
-}",
-@"
-interface IGoo { void Goo1(); }
+                class C : IGoo
+                {
+                    readonly void IGoo.[||]Goo1() { }
+                }
+                """,
+                """
+                interface IGoo { void Goo1(); }
 
-class C : IGoo
-{
-    public readonly void Goo1() { }
-}");
+                class C : IGoo
+                {
+                    public readonly void Goo1() { }
+                }
+                """);
         }
     }
 }
