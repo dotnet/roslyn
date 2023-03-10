d3cf816828ea714f0007514d5584fd2c73dae87f
akhera99 <ankitakhera@microsoft.com>
2023-03-10 14:34:03 -0800

    wip

--- src/Features/LanguageServer/Protocol/Handler/InlayHints/InlayHintsHandler.cs
+++ src/Features/LanguageServer/Protocol/Handler/InlayHints/InlayHintsHandler.cs
@@ -3,22 +3,25 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.Composition;
-using System.Linq;
-using System.Text;
+using System.Reflection.Metadata;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.LanguageServer.Handler.CodeLens;
+using Microsoft.CodeAnalysis.InlineHints;
+using Microsoft.CodeAnalysis.LanguageService;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
+using StreamJsonRpc;
 using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
 
 namespace Microsoft.CodeAnalysis.LanguageServer.Handler.InlayHints
 {
     [ExportCSharpVisualBasicStatelessLspService(typeof(InlayHintsHandler)), Shared]
     [Method(LSP.Methods.TextDocumentInlayHint)]
-    internal sealed class InlayHintsHandler : ILspServiceDocumentRequestHandler<LSP.InlayHintParams>
+    internal sealed class InlayHintsHandler : ILspServiceDocumentRequestHandler<LSP.InlayHintParams, LSP.InlayHint[]?>
     {
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
@@ -26,18 +29,63 @@ namespace Microsoft.CodeAnalysis.LanguageServer.Handler.InlayHints
         {
         }
 
-        public bool MutatesSolutionState => throw new NotImplementedException();
+        public bool MutatesSolutionState => false;
 
-        public bool RequiresLSPSolution => throw new NotImplementedException();
+        public bool RequiresLSPSolution => true;
 
-        public TextDocumentIdentifier GetTextDocumentIdentifier(object request)
+        public TextDocumentIdentifier GetTextDocumentIdentifier(InlayHintParams request)
+            => request.TextDocument;
+
+        public async Task<InlayHint[]?> HandleRequestAsync(InlayHintParams request, RequestContext context, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var document = context.GetRequiredDocument();
+            var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+            var textSpan = ProtocolConversions.RangeToTextSpan(request.Range, text);
+
+            using var _ = ArrayBuilder<LSP.InlayHint>.GetInstance(out var inlayHints);
+            await AddParameterHintsToBuilderAsync(document, textSpan, inlayHints, cancellationToken).ConfigureAwait(false);
+            await AddTypeHintsToBuilderAsync(document, textSpan, inlayHints, cancellationToken).ConfigureAwait(false);
+            return inlayHints.ToArray();
         }
 
-        public TextDocumentIdentifier GetTextDocumentIdentifier(object request)
+        private async Task AddParameterHintsToBuilderAsync(Document document, TextSpan textSpan, ArrayBuilder<LSP.InlayHint> inlayHints, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var inlineParameterService = document.GetRequiredLanguageService<IInlineParameterNameHintsService>();
+            var options = new InlineParameterHintsOptions()
+            {
+                EnabledForParameters = true
+            };
+
+            var displayOptions = new SymbolDescriptionOptions();
+            var parameterHints = await inlineParameterService.GetInlineHintsAsync(document, textSpan, options, displayOptions, cancellationToken).ConfigureAwait(false);
+
+            foreach (var parameterHint in parameterHints)
+            {
+                var inlayHint = new LSP.InlayHint
+                {
+
+                };
+            }
+        }
+
+        private async Task AddTypeHintsToBuilderAsync(Document document, TextSpan textSpan, ArrayBuilder<InlayHint> inlayHints, CancellationToken cancellationToken)
+        {
+            var inlineTypeService = document.GetRequiredLanguageService<IInlineTypeHintsService>();
+            var options = new InlineTypeHintsOptions()
+            {
+                EnabledForTypes = true
+            };
+
+            var displayOptions = new SymbolDescriptionOptions();
+            var typeHints = await inlineTypeService.GetInlineHintsAsync(document, textSpan, options, displayOptions, cancellationToken).ConfigureAwait(false);
+
+            foreach (var typeHint in typeHints)
+            {
+                var inlayHint = new LSP.InlayHint
+                {
+
+                };
+            }
         }
     }
 }
