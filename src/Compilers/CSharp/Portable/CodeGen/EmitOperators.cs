270c816c8b1e56df757125621b3ad3d7cdc8b3f6
Jan Jones <janjones@microsoft.com>
2023-03-14 12:10:25 +0100

    Improve wording

--- src/Compilers/CSharp/Portable/CodeGen/EmitOperators.cs
+++ src/Compilers/CSharp/Portable/CodeGen/EmitOperators.cs
@@ -530,7 +530,7 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeGen
             {
                 EmitExpression(condition, used: true);
 
-                // Convert to 1 or 0 (although `condition` is of type `bool`, it can return any integer).
+                // Convert to 1 or 0 (although `condition` is of type `bool`, it can contain any integer).
                 _builder.EmitOpCode(ILOpCode.Ldc_i4_0);
                 _builder.EmitOpCode(sense ? ILOpCode.Cgt_un : ILOpCode.Ceq);
                 return true;
