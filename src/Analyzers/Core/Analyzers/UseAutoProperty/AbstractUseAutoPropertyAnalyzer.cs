7e3036a3ed9a67e63c7b51878cd98573b8296a85
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-03-17 11:39:13 -0700

    Doc

--- src/Analyzers/Core/Analyzers/UseAutoProperty/AbstractUseAutoPropertyAnalyzer.cs
+++ src/Analyzers/Core/Analyzers/UseAutoProperty/AbstractUseAutoPropertyAnalyzer.cs
@@ -30,6 +30,9 @@ namespace Microsoft.CodeAnalysis.UseAutoProperty
         where TVariableDeclarator : SyntaxNode
         where TExpression : SyntaxNode
     {
+        /// <summary>
+        /// ConcurrentStack as that's the only concurrent collection that supports 'Clear' in netstandard2.
+        /// </summary>
         private static readonly ObjectPool<ConcurrentStack<AnalysisResult>> s_analysisResultPool = new(() => new());
         private static readonly ObjectPool<ConcurrentSet<IFieldSymbol>> s_fieldSetPool = new(() => new());
         private static readonly ObjectPool<ConcurrentSet<SyntaxNode>> s_nodeSetPool = new(() => new());
