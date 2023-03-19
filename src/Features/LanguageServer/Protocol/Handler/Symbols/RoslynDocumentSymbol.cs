adf1f21b48e8c017907ddff0095bf41f260af094
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2023-03-19 12:27:18 -0700

    Update src/Features/LanguageServer/Protocol/Handler/Symbols/RoslynDocumentSymbol.cs
    Co-authored-by: Jason Malinowski <jason@jason-m.com>

--- src/Features/LanguageServer/Protocol/Handler/Symbols/RoslynDocumentSymbol.cs
+++ src/Features/LanguageServer/Protocol/Handler/Symbols/RoslynDocumentSymbol.cs
@@ -9,7 +9,7 @@ using Newtonsoft.Json;
 namespace Microsoft.CodeAnalysis.LanguageServer.Handler
 {
     /// <summary>
-    /// Derived version of DocumentSymbol used so we can pass additional information we do not want lost for our
+    /// Derived version of <see cref="DocumentSymbol" /> used so we can pass additional information we do not want lost for our
     /// internal consumers.  For example, the richer <see cref="Glyph"/> which is a superset of <see
     /// cref="DocumentSymbol.Kind"/>.
     /// </summary>
