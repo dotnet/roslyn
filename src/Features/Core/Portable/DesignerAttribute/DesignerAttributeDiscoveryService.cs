e65f21dd8bb83a96aa3bcafb386438fc8624ebeb
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-03-19 22:52:06 -0700

    Clean up

--- src/Features/Core/Portable/DesignerAttribute/DesignerAttributeDiscoveryService.cs
+++ src/Features/Core/Portable/DesignerAttribute/DesignerAttributeDiscoveryService.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel;
 using System.Composition;
