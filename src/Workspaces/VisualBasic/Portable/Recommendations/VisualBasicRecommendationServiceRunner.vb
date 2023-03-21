d7a46d93124d89ad3f9cbbe15c828df4b6552d46
DoctorKrolic <mapmyp03@gmail.com>
2023-03-21 22:48:41 +0300

    Fix VB

--- src/Workspaces/VisualBasic/Portable/Recommendations/VisualBasicRecommendationServiceRunner.vb
+++ src/Workspaces/VisualBasic/Portable/Recommendations/VisualBasicRecommendationServiceRunner.vb
@@ -172,7 +172,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Recommendations
                     Return ImmutableArray(Of ISymbol).Empty
                 End If
 
-                If TypeOf node.GetAncestor(Of AsClauseSyntax)().Parent Is EnumStatementSyntax Then
+                If TypeOf node.GetAncestor(Of AsClauseSyntax)()?.Parent Is EnumStatementSyntax Then
                     Return GetSymbolsForEnumBaseList(leftHandSymbol)
                 End If
 
