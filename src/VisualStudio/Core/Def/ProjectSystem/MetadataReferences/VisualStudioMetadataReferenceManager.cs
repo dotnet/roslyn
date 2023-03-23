b3ab5c92f67eeee111f61f8817f1dbc880125e55
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-03-23 14:49:19 -0700

    Simplify code now that there is no value source here

--- src/VisualStudio/Core/Def/ProjectSystem/MetadataReferences/VisualStudioMetadataReferenceManager.cs
+++ src/VisualStudio/Core/Def/ProjectSystem/MetadataReferences/VisualStudioMetadataReferenceManager.cs
@@ -43,7 +43,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
         /// the remote process, and it can map that same memory in directly, instead of needing the host to send the
         /// entire contents of the assembly over the channel to the OOP process.
         /// </summary>
-        private static readonly ConditionalWeakTable<ValueSource<AssemblyMetadata>, IReadOnlyList<TemporaryStorageService.TemporaryStreamStorage>> s_valueSourceToStorages = new();
+        private static readonly ConditionalWeakTable<AssemblyMetadata, IReadOnlyList<TemporaryStorageService.TemporaryStreamStorage>> s_metadataToStorages = new();
 
         private readonly MetadataCache _metadataCache = new();
         private readonly ImmutableArray<string> _runtimeDirectories;
@@ -89,12 +89,12 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
             }
         }
 
-        public IEnumerable<ITemporaryStreamStorageInternal>? GetStorages(string fullPath, DateTime snapshotTimestamp)
+        public IReadOnlyList<ITemporaryStreamStorageInternal>? GetStorages(string fullPath, DateTime snapshotTimestamp)
         {
             var key = new FileKey(fullPath, snapshotTimestamp);
             // check existing metadata
-            if (_metadataCache.TryGetSource(key, out var source) &&
-                s_valueSourceToStorages.TryGetValue(source, out var storages))
+            if (_metadataCache.TryGetMetadata(key, out var source) &&
+                s_metadataToStorages.TryGetValue(source, out var storages))
             {
                 return storages;
             }
@@ -140,19 +140,19 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
             if (_metadataCache.TryGetMetadata(key, out var metadata))
                 return metadata;
 
-            var (newMetadata, newMetadataValueSource) = GetMetadataWorker();
+            var newMetadata = GetMetadataWorker();
 
-            if (!_metadataCache.GetOrAddMetadata(key, newMetadataValueSource, out metadata))
+            if (!_metadataCache.GetOrAddMetadata(key, newMetadata, out metadata))
                 newMetadata.Dispose();
 
             return metadata;
 
-            (AssemblyMetadata newMetadata, ValueSource<AssemblyMetadata> newMetadataValueSource) GetMetadataWorker()
+            AssemblyMetadata GetMetadataWorker()
             {
                 if (VsSmartScopeCandidate(key.FullPath))
                 {
                     var newMetadata = CreateAssemblyMetadataFromMetadataImporter(key);
-                    return (newMetadata, ValueSource.Constant(newMetadata));
+                    return newMetadata;
                 }
                 else
                 {
@@ -169,11 +169,9 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
 
                     var storagesArray = storages.ToImmutable();
 
-                    var valueSource = ValueSource.Constant(newMetadata);
+                    s_metadataToStorages.Add(newMetadata, storagesArray);
 
-                    s_valueSourceToStorages.Add(valueSource, storagesArray);
-
-                    return (newMetadata, valueSource);
+                    return newMetadata;
                 }
             }
         }
