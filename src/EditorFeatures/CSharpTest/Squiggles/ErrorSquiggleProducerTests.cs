a1cee58ff5afb574202bc176774e45b0ad77ebe7
Shen Chen <shech@microsoft.com>
2023-03-30 15:12:26 -0700

    Fix nullable warnings in test

--- src/EditorFeatures/CSharpTest/Squiggles/ErrorSquiggleProducerTests.cs
+++ src/EditorFeatures/CSharpTest/Squiggles/ErrorSquiggleProducerTests.cs
@@ -156,6 +156,7 @@ class Program
                     new ClassifiedTextRun(ClassificationTypeNames.Text, CSharpAnalyzersResources.Using_directive_is_unnecessary)));
 
             Assert.Equal(PredefinedErrorTypeNames.Suggestion, first.Tag.ErrorType);
+            AssertEx.NotNull(first.Tag.ToolTipContent);
             ToolTipAssert.EqualContent(expectedToolTip, first.Tag.ToolTipContent);
             Assert.Equal(40, first.Span.Start);
             Assert.Equal(25, first.Span.Length);
@@ -169,6 +170,7 @@ class Program
                     new ClassifiedTextRun(ClassificationTypeNames.Text, CSharpAnalyzersResources.Using_directive_is_unnecessary)));
 
             Assert.Equal(PredefinedErrorTypeNames.Suggestion, second.Tag.ErrorType);
+            AssertEx.NotNull(second.Tag.ToolTipContent);
             ToolTipAssert.EqualContent(expectedToolTip, second.Tag.ToolTipContent);
             Assert.Equal(82, second.Span.Start);
             Assert.Equal(60, second.Span.Length);
@@ -182,6 +184,7 @@ class Program
                     new ClassifiedTextRun(ClassificationTypeNames.Text, "messageFormat")));
 
             Assert.Equal(PredefinedErrorTypeNames.Warning, third.Tag.ErrorType);
+            AssertEx.NotNull(third.Tag.ToolTipContent);
             ToolTipAssert.EqualContent(expectedToolTip, third.Tag.ToolTipContent);
             Assert.Equal(152, third.Span.Start);
             Assert.Equal(7, third.Span.Length);
@@ -195,6 +198,7 @@ class Program
                     new ClassifiedTextRun(ClassificationTypeNames.Text, AnalyzersResources.Name_can_be_simplified)));
 
             Assert.Equal(PredefinedErrorTypeNames.SyntaxError, fourth.Tag.ErrorType);
+            AssertEx.NotNull(fourth.Tag.ToolTipContent);
             ToolTipAssert.EqualContent(expectedToolTip, fourth.Tag.ToolTipContent);
             Assert.Equal(196, fourth.Span.Start);
             Assert.Equal(5, fourth.Span.Length);
@@ -230,6 +234,7 @@ class Program
                     new ClassifiedTextRun(ClassificationTypeNames.WhiteSpace, " "),
                     new ClassifiedTextRun(ClassificationTypeNames.Text, firstDiagnostic.Message)));
 
+            AssertEx.NotNull(firstSpan.Tag.ToolTipContent);
             ToolTipAssert.EqualContent(expectedToolTip, firstSpan.Tag.ToolTipContent);
         }
 
