77734b25529820788a9f22f34909a69091063f5f
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-03-30 16:51:36 -0700

    Add more tests

--- src/Workspaces/CoreTest/ChecksumTests.cs
+++ src/Workspaces/CoreTest/ChecksumTests.cs
@@ -24,6 +24,13 @@ namespace Microsoft.CodeAnalysis.UnitTests
             var checksumB = Checksum.TestAccessor.CreateUsingSpans(checksum1, checksum2);
 
             Assert.Equal(checksumA, checksumB);
+
+            Assert.NotEqual(checksum1, checksum2);
+
+            Assert.NotEqual(checksum1, checksumA);
+            Assert.NotEqual(checksum1, checksumB);
+            Assert.NotEqual(checksum2, checksumA);
+            Assert.NotEqual(checksum2, checksumB);
         }
 
         [Fact]
@@ -37,6 +44,17 @@ namespace Microsoft.CodeAnalysis.UnitTests
             var checksumB = Checksum.TestAccessor.CreateUsingSpans(checksum1, checksum2, checksum3);
 
             Assert.Equal(checksumA, checksumB);
+
+            Assert.NotEqual(checksum1, checksum2);
+            Assert.NotEqual(checksum2, checksum3);
+            Assert.NotEqual(checksum3, checksum1);
+
+            Assert.NotEqual(checksum1, checksumA);
+            Assert.NotEqual(checksum1, checksumB);
+            Assert.NotEqual(checksum2, checksumA);
+            Assert.NotEqual(checksum2, checksumB);
+            Assert.NotEqual(checksum3, checksumA);
+            Assert.NotEqual(checksum3, checksumB);
         }
 #endif
     }
