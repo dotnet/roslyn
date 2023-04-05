e2e4e226b77cf330d2c37f48a61ff1909ec05677
Chris Sienkiewicz <chsienki@microsoft.com>
2023-04-04 17:00:41 -0700

    Analyzer load failure suppress (#58376)
    * Allow analyzer load failure supression from a global config.

--- src/Compilers/Core/Portable/CommandLine/CommonCompiler.cs
+++ src/Compilers/Core/Portable/CommandLine/CommonCompiler.cs
@@ -115,6 +115,7 @@ namespace Microsoft.CodeAnalysis
         protected abstract void ResolveAnalyzersFromArguments(
             List<DiagnosticInfo> diagnostics,
             CommonMessageProvider messageProvider,
+            CompilationOptions compilationOptions,
             bool skipAnalyzers,
             out ImmutableArray<DiagnosticAnalyzer> analyzers,
             out ImmutableArray<ISourceGenerator> generators);
@@ -858,7 +859,7 @@ namespace Microsoft.CodeAnalysis
             }
 
             var diagnosticInfos = new List<DiagnosticInfo>();
-            ResolveAnalyzersFromArguments(diagnosticInfos, MessageProvider, Arguments.SkipAnalyzers, out var analyzers, out var generators);
+            ResolveAnalyzersFromArguments(diagnosticInfos, MessageProvider, compilation.Options, Arguments.SkipAnalyzers, out var analyzers, out var generators);
             var additionalTextFiles = ResolveAdditionalFilesFromArguments(diagnosticInfos, MessageProvider, touchedFilesLogger);
             if (ReportDiagnostics(diagnosticInfos, consoleOutput, errorLogger, compilation))
             {
