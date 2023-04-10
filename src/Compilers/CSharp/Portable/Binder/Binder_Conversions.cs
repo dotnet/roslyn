a2dbc3e8df6632bd79319aab8d8df9d413650e39
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-04-10 13:18:05 -0700

    Continued removal of location allocations

--- src/Compilers/CSharp/Portable/Binder/Binder_Conversions.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Conversions.cs
@@ -1477,7 +1477,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             CheckValidScopedMethodConversion(syntax, selectedMethod, delegateOrFuncPtrType, isExtensionMethod, diagnostics);
             if (!isAddressOf)
             {
-                ReportDiagnosticsIfUnmanagedCallersOnly(diagnostics, selectedMethod, location, isDelegateConversion: true);
+                ReportDiagnosticsIfUnmanagedCallersOnly(diagnostics, selectedMethod, syntax, isDelegateConversion: true);
             }
             ReportDiagnosticsIfObsolete(diagnostics, selectedMethod, syntax, hasBaseReceiver: false);
 
