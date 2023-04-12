52bb76d1bd0e337bfb6a27ccee0354c0aa72107d
Jason Malinowski <jason.malinowski@microsoft.com>
2023-04-12 16:55:41 -0700

    When figuring out directories to watch, also look to DOTNET_ROOT

--- src/Workspaces/Core/Portable/Workspace/ProjectSystem/FileWatchedPortableExecutableReferenceFactory.cs
+++ src/Workspaces/Core/Portable/Workspace/ProjectSystem/FileWatchedPortableExecutableReferenceFactory.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -46,29 +47,39 @@ namespace Microsoft.CodeAnalysis.ProjectSystem
 
             _fileReferenceChangeContext = new Lazy<IFileChangeContext>(() =>
             {
-                var watchedDirectories = new List<WatchedDirectory>();
+                var referenceDirectories = new HashSet<string>();
 
                 // On each platform, there is a place that reference assemblies for the framework are installed. These are rarely going to be changed
                 // but are the most common places that we're going to create file watches. Rather than either creating a huge number of file watchers
                 // for every single file, or eventually realizing we should just watch these directories, we just create the single directory watchers now.
+                // We'll collect this from two places: constructing it from known environment variables, and also for the defaults where those environment
+                // variables would usually point, as a fallback.
+
+                if (Environment.GetEnvironmentVariable("DOTNET_ROOT") is string dotnetRoot)
+                {
+                    referenceDirectories.Add(Path.Combine(dotnetRoot));
+                }
+
                 if (PlatformInformation.IsWindows)
                 {
-                    var referenceAssembliesPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Reference Assemblies", "Microsoft", "Framework");
-                    watchedDirectories.Add(new WatchedDirectory(referenceAssembliesPath, ".dll"));
+                    referenceDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Reference Assemblies", "Microsoft", "Framework"));
+                    referenceDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "dotnet", "packs"));
                 }
                 else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                 {
-                    watchedDirectories.Add(new WatchedDirectory("/usr/lib/dotnet/packs", ".dll"));
+                    referenceDirectories.Add("/usr/lib/dotnet/packs");
+                    referenceDirectories.Add("/usr/share/dotnet/packs");
                 }
                 else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 {
-                    watchedDirectories.Add(new WatchedDirectory("/usr/local/share/dotnet/packs", ".dll"));
+                    referenceDirectories.Add("/usr/local/share/dotnet/packs");
                 }
 
                 // TODO: set this to watch the NuGet directory as well; there's some concern that watching the entire directory
                 // might make restores take longer because we'll be watching changes that may not impact your project.
 
-                var fileReferenceChangeContext = fileChangeWatcher.CreateContext(watchedDirectories.ToArray());
+                var directoriesToWatch = referenceDirectories.Select(static d => new WatchedDirectory(d, ".dll")).ToArray();
+                var fileReferenceChangeContext = fileChangeWatcher.CreateContext(directoriesToWatch);
                 fileReferenceChangeContext.FileChanged += FileReferenceChangeContext_FileChanged;
                 return fileReferenceChangeContext;
             });
