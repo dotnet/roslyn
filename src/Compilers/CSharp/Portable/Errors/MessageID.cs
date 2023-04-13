93cd10a5fdff679ecab004afb9e594c0be240b11
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2023-04-12 22:41:05 -0700

    Remove unnecessary Location object allocations when checking lang versions. (#67773)
    * Continued removal of location allocations
    
    * Continued removal of location allocations
    
    * Only compute once
    
    * Defer location allocation
    
    * Defer more location creation
    
    * Apply suggestions from code review
    
    * Update src/Compilers/CSharp/Portable/Binder/Binder.cs
    
    * Remove unnecessary loc allocs
    
    * Revert
    
    * revert
    
    * revert
    
    * revert

--- src/Compilers/CSharp/Portable/Errors/MessageID.cs
+++ src/Compilers/CSharp/Portable/Errors/MessageID.cs
@@ -316,29 +316,85 @@ namespace Microsoft.CodeAnalysis.CSharp
 
         internal static bool CheckFeatureAvailability(
             this MessageID feature,
-            BindingDiagnosticBag diagnostics,
+            DiagnosticBag diagnostics,
             SyntaxNode syntax,
             Location? location = null)
         {
-            var diag = GetFeatureAvailabilityDiagnosticInfo(feature, (CSharpParseOptions)syntax.SyntaxTree.Options);
-            if (diag is object)
-            {
-                diagnostics.Add(diag, location ?? syntax.GetLocation());
-                return false;
-            }
-            return true;
+            return CheckFeatureAvailability(
+                feature,
+                diagnostics,
+                syntax.SyntaxTree.Options,
+                static tuple => tuple.location ?? tuple.syntax.Location,
+                (syntax, location));
         }
 
         internal static bool CheckFeatureAvailability(
             this MessageID feature,
             DiagnosticBag diagnostics,
+            SyntaxToken syntax,
+            Location? location = null)
+        {
+            return CheckFeatureAvailability(
+                feature,
+                diagnostics,
+                syntax.SyntaxTree!.Options,
+                static tuple => tuple.location ?? tuple.syntax.GetLocation(),
+                (syntax, location));
+        }
+
+        internal static bool CheckFeatureAvailability(
+            this MessageID feature,
+            BindingDiagnosticBag diagnostics,
             SyntaxNode syntax,
             Location? location = null)
         {
-            var diag = GetFeatureAvailabilityDiagnosticInfo(feature, (CSharpParseOptions)syntax.SyntaxTree.Options);
-            if (diag is object)
+            return CheckFeatureAvailability(
+                feature,
+                diagnostics,
+                syntax.SyntaxTree.Options,
+                static tuple => tuple.location ?? tuple.syntax.Location,
+                (syntax, location));
+        }
+
+        internal static bool CheckFeatureAvailability(
+            this MessageID feature,
+            BindingDiagnosticBag diagnostics,
+            SyntaxToken syntax,
+            Location? location = null)
+        {
+            return CheckFeatureAvailability(
+                feature,
+                diagnostics,
+                syntax.SyntaxTree!.Options,
+                static tuple => tuple.location ?? tuple.syntax.GetLocation(),
+                (syntax, location));
+        }
+
+        private static bool CheckFeatureAvailability<TData>(
+            this MessageID feature,
+            DiagnosticBag diagnostics,
+            ParseOptions parseOptions,
+            Func<TData, Location> getLocation,
+            TData data)
+        {
+            if (GetFeatureAvailabilityDiagnosticInfo(feature, (CSharpParseOptions)parseOptions) is { } diagInfo)
             {
-                diagnostics.Add(diag, location ?? syntax.GetLocation());
+                diagnostics.Add(diagInfo, getLocation(data));
+                return false;
+            }
+            return true;
+        }
+
+        private static bool CheckFeatureAvailability<TData>(
+            this MessageID feature,
+            BindingDiagnosticBag diagnostics,
+            ParseOptions parseOptions,
+            Func<TData, Location> getLocation,
+            TData data)
+        {
+            if (GetFeatureAvailabilityDiagnosticInfo(feature, (CSharpParseOptions)parseOptions) is { } diagInfo)
+            {
+                diagnostics.Add(diagInfo, getLocation(data));
                 return false;
             }
             return true;
