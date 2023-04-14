2221e47419918ddfa41351f30afecc9501055276
Julien Couvreur <julien.couvreur@gmail.com>
2023-04-14 10:12:58 -0700

    Use proper type for `BoundAwaitOperator` in VB statement

--- src/Compilers/VisualBasic/Portable/Lowering/AsyncRewriter/AsyncRewriter.AsyncMethodToClassRewriter.Await.vb
+++ src/Compilers/VisualBasic/Portable/Lowering/AsyncRewriter/AsyncRewriter.AsyncMethodToClassRewriter.Await.vb
@@ -2,16 +2,10 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
-Imports System.Collections.Generic
 Imports System.Collections.Immutable
-Imports System.Threading
-Imports Microsoft.Cci
 Imports Microsoft.CodeAnalysis
-Imports Microsoft.CodeAnalysis.Collections
 Imports Microsoft.CodeAnalysis.PooledObjects
-Imports Microsoft.CodeAnalysis.Text
 Imports Microsoft.CodeAnalysis.VisualBasic.Symbols
-Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 
 Namespace Microsoft.CodeAnalysis.VisualBasic
 
@@ -99,7 +93,6 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
                                                     Me.F.AssignmentExpression(Me.F.Local(resultTemp, True), rewrittenGetResult),
                                                     clearAwaiterTemp,
                                                     Me.F.Local(resultTemp, False))
-
                 Else
                     ' STMT:   $awaiterTemp.GetResult()
                     ' STMT:   $awaiterTemp = Nothing
