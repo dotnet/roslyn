5dba742f5a7f5e72ab2b4cfbcd59ccd56f431af2
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2023-04-14 13:36:53 -0700

    Update src/Compilers/Core/Portable/InternalUtilities/Hash.cs

--- src/Compilers/Core/Portable/InternalUtilities/Hash.cs
+++ src/Compilers/Core/Portable/InternalUtilities/Hash.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.PooledObjects;
 
 namespace Roslyn.Utilities
 {
