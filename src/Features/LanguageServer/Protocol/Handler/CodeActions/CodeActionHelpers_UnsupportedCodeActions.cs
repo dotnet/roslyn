288572e3ec87af1026f23bd8672fa74ee406c221
Shen Chen <shech@microsoft.com>
2023-04-27 11:45:41 -0700

    Disable extract method ane enable nullable

--- src/Features/LanguageServer/Protocol/Handler/CodeActions/CodeActionHelpers_UnsupportedCodeActions.cs
+++ src/Features/LanguageServer/Protocol/Handler/CodeActions/CodeActionHelpers_UnsupportedCodeActions.cs
@@ -2,24 +2,34 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.UnifiedSuggestions;
 
 namespace Microsoft.CodeAnalysis.LanguageServer.Handler.CodeActions
 {
     internal static partial class CodeActionHelpers
     {
+        private static readonly ImmutableArray<string> s_notSupportFixerAndRefactoringProviderNames = ImmutableArray.Create(
+            // Extract method requires inline rename support, not supported by lsp.
+            PredefinedCodeRefactoringProviderNames.ExtractMethod,
+            PredefinedCodeRefactoringProviderNames.EnableNullable);
+
         private static bool IsCodeActionNotSupportedByLSP(IUnifiedSuggestedAction suggestedAction)
         {
+            var originalCodeAction = suggestedAction.OriginalCodeAction;
             // Filter out code actions with options since they'll show dialogs and we can't remote the UI and the options.
-            return suggestedAction.OriginalCodeAction is CodeActionWithOptions
+            return originalCodeAction is CodeActionWithOptions
             // Skip code actions that requires non-document changes.  We can't apply them in LSP currently.
             // https://github.com/dotnet/roslyn/issues/48698
-            || suggestedAction.OriginalCodeAction.Tags.Contains(CodeAction.RequiresNonDocumentChange)
+            || originalCodeAction.Tags.Contains(CodeAction.RequiresNonDocumentChange)
             // Filter the configure and suppress fixer if it is not VS LSP, because it would generate many nested code actions.
             // Tracking issue: https://github.com/microsoft/language-server-protocol/issues/994 
-            || suggestedAction.OriginalCodeAction is AbstractConfigurationActionWithNestedActions;
+            || originalCodeAction is AbstractConfigurationActionWithNestedActions
+            || s_notSupportFixerAndRefactoringProviderNames.Any(originalCodeAction.CustomTags.Contains);
         }
     }
 }
