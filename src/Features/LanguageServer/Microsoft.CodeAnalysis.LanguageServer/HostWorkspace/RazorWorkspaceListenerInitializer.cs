4530a8d642bcf2ee771326acc000a830043dca92
David Wengier <git@wengier.com>
2023-05-02 11:50:08 +1000

    More PR feedback

--- src/Features/LanguageServer/Microsoft.CodeAnalysis.LanguageServer/HostWorkspace/RazorWorkspaceListenerInitializer.cs
+++ src/Features/LanguageServer/Microsoft.CodeAnalysis.LanguageServer/HostWorkspace/RazorWorkspaceListenerInitializer.cs
@@ -23,9 +23,12 @@ internal sealed class RazorWorkspaceListenerInitializer
     private const string _projectRazorJsonFileName = "project.razor.vscode.json";
 
     private readonly ILogger _logger;
-    private readonly Lazy<RazorWorkspaceListener> _razorWorkspaceListener;
-    private ImmutableHashSet<ProjectId> _projectIdWithDynamicFiles = ImmutableHashSet<ProjectId>.Empty;
+    private readonly HashSet<ProjectId> _projectIdWithDynamicFiles = new();
     private readonly object _initializeGate = new();
+    private readonly Lazy<LanguageServerWorkspaceFactory> _workspaceFactory;
+    private readonly ILoggerFactory _loggerFactory;
+
+    private RazorWorkspaceListener? _razorWorkspaceListener;
 
     [ImportingConstructor]
     [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
@@ -33,68 +36,51 @@ internal sealed class RazorWorkspaceListenerInitializer
     {
         _logger = loggerFactory.CreateLogger(nameof(RazorWorkspaceListenerInitializer));
 
-        _razorWorkspaceListener = new Lazy<RazorWorkspaceListener>(() =>
-        {
-            var razorWorkspaceListener = new RazorWorkspaceListener(loggerFactory);
-            var workspace = workspaceFactory.Value.Workspace;
-            razorWorkspaceListener.EnsureInitialized(workspace, _projectRazorJsonFileName);
-
-            return razorWorkspaceListener;
-        });
+        _workspaceFactory = workspaceFactory;
+        _loggerFactory = loggerFactory;
     }
 
     internal void Initialize()
     {
-        // Only initialize once
-        if (_razorWorkspaceListener.IsValueCreated)
-        {
-            return;
-        }
-
-        ImmutableHashSet<ProjectId> projectsToProcess;
         lock (_initializeGate)
         {
-            if (_razorWorkspaceListener.IsValueCreated)
+            // Only initialize once
+            if (_razorWorkspaceListener is not null)
             {
                 return;
             }
 
             _logger.LogTrace("Initializing the Razor workspace listener");
-            _ = _razorWorkspaceListener.Value;
-            projectsToProcess = Interlocked.Exchange(ref _projectIdWithDynamicFiles, ImmutableHashSet<ProjectId>.Empty);
+            _razorWorkspaceListener = new RazorWorkspaceListener(_loggerFactory);
+            _razorWorkspaceListener.EnsureInitialized(_workspaceFactory.Value.Workspace, _projectRazorJsonFileName);
         }
 
-        foreach (var projectId in projectsToProcess)
+        foreach (var projectId in _projectIdWithDynamicFiles)
         {
             _logger.LogTrace("{projectId} notifying a dynamic file for the first time", projectId);
-            _razorWorkspaceListener.Value.NotifyDynamicFile(projectId);
+            _razorWorkspaceListener.NotifyDynamicFile(projectId);
         }
+
+        // May as well clear out the collection, it will never get used again anyway.
+        _projectIdWithDynamicFiles.Clear();
     }
 
     internal void NotifyDynamicFile(ProjectId projectId)
     {
-        if (!_razorWorkspaceListener.IsValueCreated)
+        lock (_initializeGate)
         {
-            // We haven't been initialized by the extension yet, so just store the project id, to tell Razor later
-            _logger.LogTrace("{projectId} queuing up a dynamic file notify for later", projectId);
-            ImmutableInterlocked.Update(ref _projectIdWithDynamicFiles, (col, arg) => col.Add(arg), projectId);
+            if (_razorWorkspaceListener is null)
+            {
+                // We haven't been initialized by the extension yet, so just store the project id, to tell Razor later
+                _logger.LogTrace("{projectId} queuing up a dynamic file notify for later", projectId);
+                _projectIdWithDynamicFiles.Add(projectId);
 
-            return;
+                return;
+            }
         }
 
         // We've been initialized, so just pass the information along
         _logger.LogTrace("{projectId} forwarding on a dynamic file notification because we're initialized", projectId);
-        _razorWorkspaceListener.Value.NotifyDynamicFile(projectId);
-
-        // It's possible that we were initialized after the IsValueCreated check above, which could leave stale project Ids in
-        // our hashset, so we'll see if we need to clear it out just in case
-        if (!_projectIdWithDynamicFiles.IsEmpty)
-        {
-            var projects = Interlocked.Exchange(ref _projectIdWithDynamicFiles, ImmutableHashSet<ProjectId>.Empty);
-            foreach (var pId in projects)
-            {
-                _razorWorkspaceListener.Value.NotifyDynamicFile(pId);
-            }
-        }
+        _razorWorkspaceListener.NotifyDynamicFile(projectId);
     }
 }
