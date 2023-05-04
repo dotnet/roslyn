b54a0d1961bef02b1ecbfecce2b720ed49a1617e
Sam Harwell <Sam.Harwell@microsoft.com>
2023-05-04 17:32:31 -0500

    Make sure tree view items have an accessibility name

---
+++ src/VisualStudio/Core/Def/DocumentOutline/DocumentOutlineStrings.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.VisualStudio.LanguageServices.DocumentOutline
+{
+    internal static class DocumentOutlineStrings
+    {
+        public static string Document_Outline
+            => ServicesVSResources.Document_Outline;
+    }
+}
