7bae31a714773a6c23cda5e5c882bfc88d897279
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-05-09 11:27:35 -0700

    Add candidates

--- src/Analyzers/CSharp/Analyzers/UsePrimaryConstructor/CSharpUsePrimaryConstructorDiagnosticAnalyzer.cs
+++ src/Analyzers/CSharp/Analyzers/UsePrimaryConstructor/CSharpUsePrimaryConstructorDiagnosticAnalyzer.cs
@@ -82,11 +82,33 @@ namespace Microsoft.CodeAnalysis.CSharp.UsePrimaryConstructor
 
             private IMethodSymbol _primaryConstructor = null!;
             private ConstructorDeclarationSyntax _primaryConstructorDeclaration = null!;
-            private readonly PooledHashSet<string> _candidateMembersToRemove = PooledHashSet<string>.GetInstance();
+
+            private readonly PooledDictionary<ISymbol, IParameterSymbol> _candidateMembersToRemove = PooledDictionary<ISymbol, IParameterSymbol>.GetInstance();
+            private readonly PooledHashSet<ISymbol> _membersThatCannotBeRemoved = PooledHashSet<ISymbol>.GetInstance();
 
             public Analyzer(CSharpUsePrimaryConstructorDiagnosticAnalyzer diagnosticAnalyzer)
             {
                 _diagnosticAnalyzer = diagnosticAnalyzer;
+            }
+
+            private void OnSymbolEnd(SymbolAnalysisContext context)
+            {
+                // See if the constructor analysis found a viable constructor to convert to a primary constructor.
+                if (_primaryConstructor is not null)
+                {
+                    // Pass along a mapping of field/property name to the constructor parameter name that will replace it.
+                    context.ReportDiagnostic(DiagnosticHelper.Create(
+                        _diagnosticAnalyzer.Descriptor,
+                        _primaryConstructorDeclaration.Identifier.GetLocation(),
+                        _styleOption.Notification.Severity,
+                        ImmutableArray.Create(_primaryConstructorDeclaration.GetLocation()),
+                        _candidateMembersToRemove
+                            .Where(kvp => !_membersThatCannotBeRemoved.Contains(kvp.Key))
+                            .ToImmutableDictionary(static kvp => kvp.Key.Name, static kvp => (string?)kvp.Value.Name)));
+                }
+
+                _candidateMembersToRemove.Free();
+                _membersThatCannotBeRemoved.Free();
             }
 
             public void AnalyzeNamedTypeStart(SymbolStartAnalysisContext context)
@@ -119,90 +141,71 @@ namespace Microsoft.CodeAnalysis.CSharp.UsePrimaryConstructor
 
                 // Need to see if there is a single constructor that either calls `base(...)` or has no constructor
                 // initializer (and thus implicitly calls `base()`), and that all other constructors call `this(...)`.
-                if (!TryFindPrimaryConstructorCandidate(cancellationToken))
+                if (!TryFindPrimaryConstructorCandidate())
                     return;
 
-                Debug.Assert(_primaryConstructor != null);
-                Debug.Assert(_primaryConstructorDeclaration != null);
+                Contract.ThrowIfNull(_primaryConstructor);
+                Contract.ThrowIfNull(_primaryConstructorDeclaration);
 
                 // Constructor has to have a real body (can't be extern/etc.).
                 if (_primaryConstructorDeclaration is { Body: null, ExpressionBody: null })
                     return;
 
-
-                context.RegisterSyntaxNodeAction(AnalyzeConstructorDeclaration, SyntaxKind.ConstructorDeclaration);
-            }
-
-            private void OnSymbolEnd(SymbolAnalysisContext context)
-            {
-                // See if the constructor analysis invalidated us.
-                if (_primaryConstructor is not null)
+                // Now ensure the constructor body is something that could be converted to a primary constructor (i.e.
+                // only assignments to instance fields/props on this).
+                var semanticModel = context.Compilation.GetSemanticModel(_primaryConstructorDeclaration.SyntaxTree);
+                if (!AnalyzeConstructorBody())
                 {
-                    context.ReportDiagnostic(DiagnosticHelper.Create(
-                        _diagnosticAnalyzer.Descriptor,
-                        _primaryConstructorDeclaration.Identifier.GetLocation(),
-                        _styleOption.Notification.Severity,
-                        ImmutableArray.Create(_primaryConstructorDeclaration.GetLocation()),
-                        properties: _candidateMembersToRemove.ToImmutableDictionary(static k => k, static k => (string?)k)));
+                    // Anything else is invalid.  Clear this out so that OnSymbolEnd knows to do nothing.
+                    _primaryConstructor = null!;
+                    _primaryConstructorDeclaration = null!;
+
+                    return;
                 }
 
-                _candidateMembersToRemove.Free();
-            }
+                // Look to see if we have trivial `_x = x` or `this.x = x` assignments.  If so, then the field/prop
+                // could be a candidate for removal (as long as we determine that all use sites of the field/prop would
+                // be able to use the captured primary constructor parameter.
 
-            private bool TryFindPrimaryConstructorCandidate(CancellationToken cancellationToken)
-            {
-                var constructors = _namedType.InstanceConstructors;
-
-                foreach (var constructor in constructors)
-                {
-                    // Needs to just have a single declaration
-                    if (constructor.DeclaringSyntaxReferences is not [var constructorReference])
-                        return false;
-
-                    if (constructorReference.GetSyntax(cancellationToken) is not ConstructorDeclarationSyntax constructorDeclaration)
-                        return false;
-
-                    if (constructorDeclaration.Initializer is null or (kind: SyntaxKind.BaseConstructorInitializer))
-                    {
-                        // Can only have one candidate
-                        if (_primaryConstructor != null)
-                            return false;
-
-                        _primaryConstructor = constructor;
-                        _primaryConstructorDeclaration = constructorDeclaration;
-                    }
-                    else
-                    {
-                        Debug.Assert(constructorDeclaration.Initializer.Kind() == SyntaxKind.ThisConstructorInitializer);
-                    }
-                }
-
-                return _primaryConstructor != null;
-            }
-
-            private void AnalyzeConstructorDeclaration(SyntaxNodeAnalysisContext context)
-            {
-                var cancellationToken = context.CancellationToken;
-                var semanticModel = context.SemanticModel;
-                var constructorDeclaration = (ConstructorDeclarationSyntax)context.Node;
-
-                // skip constructors until we find the candidate.
-                if (constructorDeclaration != _primaryConstructorDeclaration)
-                    return;
-
-                // constructor must be empty, except for assignments to instance fields/properties on this type.
-                if (AnalyzeConstructorBody())
-                    return;
-
-                // Anything else is invalid.  Clear this out so that OnSymbolEnd knows to do nothing.
-                _primaryConstructor = null!;
-                _primaryConstructorDeclaration = null!;
+                if (_candidateMembersToRemove.Count > 0)
+                    context.RegisterOperationAction(AnalyzeFieldOrPropertyReference, OperationKind.FieldReference, OperationKind.PropertyReference);
 
                 return;
 
+                bool TryFindPrimaryConstructorCandidate()
+                {
+                    var constructors = _namedType.InstanceConstructors;
+
+                    foreach (var constructor in constructors)
+                    {
+                        // Needs to just have a single declaration
+                        if (constructor.DeclaringSyntaxReferences is not [var constructorReference])
+                            return false;
+
+                        if (constructorReference.GetSyntax(cancellationToken) is not ConstructorDeclarationSyntax constructorDeclaration)
+                            return false;
+
+                        if (constructorDeclaration.Initializer is null or (kind: SyntaxKind.BaseConstructorInitializer))
+                        {
+                            // Can only have one candidate
+                            if (_primaryConstructor != null)
+                                return false;
+
+                            _primaryConstructor = constructor;
+                            _primaryConstructorDeclaration = constructorDeclaration;
+                        }
+                        else
+                        {
+                            Debug.Assert(constructorDeclaration.Initializer.Kind() == SyntaxKind.ThisConstructorInitializer);
+                        }
+                    }
+
+                    return _primaryConstructor != null;
+                }
+
                 bool AnalyzeConstructorBody()
                 {
-                    return constructorDeclaration switch
+                    return _primaryConstructorDeclaration switch
                     {
                         { ExpressionBody.Expression: AssignmentExpressionSyntax assignmentExpression } => IsAssignmentToInstanceMember(assignmentExpression),
                         { Body: { } body } => AnalyzeBlockBody(body),
@@ -249,11 +252,11 @@ namespace Microsoft.CodeAnalysis.CSharp.UsePrimaryConstructor
                         return false;
 
                     // Has to bind to a field/prop on this type.
-                    var symbol = semanticModel.GetSymbolInfo(leftIdentifier, cancellationToken).GetAnySymbol();
-                    if (symbol is not IFieldSymbol and not IPropertySymbol)
+                    var leftSymbol = semanticModel.GetSymbolInfo(leftIdentifier, cancellationToken).GetAnySymbol();
+                    if (leftSymbol is not IFieldSymbol and not IPropertySymbol)
                         return false;
 
-                    if (!_namedType.Equals(symbol.ContainingType))
+                    if (!_namedType.Equals(leftSymbol.ContainingType))
                         return false;
 
                     // Left side looks good.  Now check the right side.  It cannot reference 'this' (as that is not
@@ -266,8 +269,52 @@ namespace Microsoft.CodeAnalysis.CSharp.UsePrimaryConstructor
                     }
 
                     // Looks good, both the left and right sides are legal.
+
+                    // If we have an assignment of the form `private_member = param`, then that member can be a candidate for removal.
+                    if (leftSymbol.DeclaredAccessibility == Accessibility.Private &&
+                        semanticModel.GetSymbolInfo(assignmentExpression.Right, cancellationToken).GetAnySymbol() is IParameterSymbol parameter)
+                    {
+                        _candidateMembersToRemove[leftSymbol.OriginalDefinition] = parameter;
+                    }
+
                     return true;
                 }
+            }
+
+            private void AnalyzeFieldOrPropertyReference(OperationAnalysisContext context)
+            {
+                var operation = (IMemberReferenceOperation)context.Operation;
+                var semanticModel = operation.SemanticModel;
+                Contract.ThrowIfNull(semanticModel);
+
+                var member = operation.Member.OriginalDefinition;
+                var instance = operation.Instance;
+
+                // static field/property.  not something we're interested in.
+                if (instance is null)
+                    return;
+
+                // we only care about reference to members in our candidate-removal set.  Can ignore everything else.
+                if (!_candidateMembersToRemove.TryGetValue(member, out var parameter))
+                    return;
+
+                // not a reference off of 'this'.  e.g. `== other.fieldName`, we could not remove this member.
+                if (instance is not IInstanceReferenceOperation)
+                {
+                    _membersThatCannotBeRemoved.Add(member);
+                    return;
+                }
+
+                // it's either `this.field` or just `field`.  We can replace with a reference to 'paramName' *if* that
+                // name in the current location doesn't bind to something else.
+                var symbols = semanticModel.LookupSymbols(operation.Syntax.SpanStart, name: parameter.Name);
+                if (symbols.Length > 0)
+                {
+                    _membersThatCannotBeRemoved.Add(member);
+                    return;
+                }
+
+                // looks good.  We should be able to remove this.
             }
         }
     }
