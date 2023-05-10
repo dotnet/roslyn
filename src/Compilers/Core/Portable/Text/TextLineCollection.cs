0897ee43e9f5265e19292350488debc1b15159be
Becca McHenry <mchenry.rebecca@gmail.com>
2023-05-10 09:33:41 -0500

    remove unused using

--- src/Compilers/Core/Portable/Text/TextLineCollection.cs
+++ src/Compilers/Core/Portable/Text/TextLineCollection.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Text
