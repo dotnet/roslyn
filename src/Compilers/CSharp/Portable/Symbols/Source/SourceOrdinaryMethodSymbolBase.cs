7223446957e167af717d41018117bb8f423ac5a9
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-05-11 08:24:03 -0700

    Change to abstract override

--- src/Compilers/CSharp/Portable/Symbols/Source/SourceOrdinaryMethodSymbolBase.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/SourceOrdinaryMethodSymbolBase.cs
@@ -166,6 +166,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
 
         protected abstract void CompleteAsyncMethodChecksBetweenStartAndFinish();
 
+        public abstract override ImmutableArray<TypeParameterSymbol> TypeParameters { get; }
+
         public abstract override string GetDocumentationCommentXml(CultureInfo preferredCulture = null, bool expandIncludes = false, CancellationToken cancellationToken = default(CancellationToken));
 
         public override string Name
