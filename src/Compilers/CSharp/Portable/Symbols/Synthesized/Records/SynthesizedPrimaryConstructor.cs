2e34e566e372703d7a2724a6c480aefb01859393
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-05-12 09:45:43 -0700

    Change to true

--- src/Compilers/CSharp/Portable/Symbols/Synthesized/Records/SynthesizedPrimaryConstructor.cs
+++ src/Compilers/CSharp/Portable/Symbols/Synthesized/Records/SynthesizedPrimaryConstructor.cs
@@ -31,7 +31,9 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 RefKind.None,
                 containingType.IsAbstract ? DeclarationModifiers.Protected : DeclarationModifiers.Public,
                 returnsVoid: true,
-                hasAnyBody: false,
+                // We consider synthesized constructors to have a body, since they effectively span the entire type, and
+                // can do things like create constructor assignments that write into the fields/props of the type.
+                hasAnyBody: true,
                 isExpressionBodied: false,
                 isExtensionMethod: false,
                 isVarArg: GetSyntax().ParameterList.Parameters.Any(static p => p.IsArgList),
