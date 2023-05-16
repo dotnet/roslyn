7aa11af676112190ac854bf1a00de3b1ba1b9af8
Fredric Silberberg <frsilb@microsoft.com>
2023-05-16 15:35:24 -0700

    Support a --skip-duplicate flag to pass to nuget if a partial republish is needed for errors.

--- src/Tools/dotnet-roslyn-tools/Tool/Commands/NuGetPublishCommand.cs
+++ src/Tools/dotnet-roslyn-tools/Tool/Commands/NuGetPublishCommand.cs
@@ -21,6 +21,7 @@ internal class NuGetPublishCommand
     internal static readonly Option<string> SourceOption = new Option<string>(new[] { "--source", "-s" }, () => "https://www.nuget.org", "Package source (URL, UNC/folder path or package source name) to use.");
     internal static readonly Option<string> ApiKeyOption = new Option<string>(new[] { "--api-key", "-k" }, "The API key for the server.") { IsRequired = true };
     internal static readonly Option<bool> UnlistedOption = new Option<bool>(new[] { "--unlisted", "-u" }, "Whether to publish the packages as unlisted.");
+    internal static readonly Option<bool> SkipDuplicateOption = new Option<bool>(new[] { "--skip-duplicate" }, "Whether to skip packages that have already been published.");
 
     public static Symbol GetCommand()
     {
@@ -30,7 +31,8 @@ internal class NuGetPublishCommand
             SourceOption,
             ApiKeyOption,
             UnlistedOption,
-            VerbosityOption
+            VerbosityOption,
+            SkipDuplicateOption
         };
         command.Handler = s_nuGetPublishCommandHandler;
         return command;
@@ -47,8 +49,9 @@ internal class NuGetPublishCommand
             var source = context.ParseResult.GetValueForOption(SourceOption)!;
             var apiKey = context.ParseResult.GetValueForOption(ApiKeyOption)!;
             var unlisted = context.ParseResult.GetValueForOption(UnlistedOption)!;
+            var skipDuplicate = context.ParseResult.GetValueForOption(SkipDuplicateOption);
 
-            return await NuGetPublish.PublishAsync(repoName, source, apiKey, unlisted, logger);
+            return await NuGetPublish.PublishAsync(repoName, source, apiKey, unlisted, skipDuplicate, logger);
         }
     }
 }
