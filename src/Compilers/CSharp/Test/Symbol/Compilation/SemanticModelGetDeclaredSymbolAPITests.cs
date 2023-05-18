b7c776c7dc12e2e2a74513d3546b305e40851b26
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-05-18 08:15:02 -0700

    use IsAssignableFrom

--- src/Compilers/CSharp/Test/Symbol/Compilation/SemanticModelGetDeclaredSymbolAPITests.cs
+++ src/Compilers/CSharp/Test/Symbol/Compilation/SemanticModelGetDeclaredSymbolAPITests.cs
@@ -4128,7 +4128,7 @@ static class S
             SymbolInfo info = new SymbolInfo();
             info = model.GetSymbolInfo(call);
 
-            Assert.True(info.Symbol.GetSymbol() is SourceOrdinaryMethodSymbol);
+            Assert.IsAssignableFrom<SourceOrdinaryMethodSymbol>(info.Symbol.GetSymbol());
 
             src = @"
 static class S
