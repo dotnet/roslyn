6ef852f6d95442efb9671eba81f00aabd3e2d480
Andrew Arnott <andrew.arnott@microsoft.com>
2023-05-22 08:31:09 -0600

    (Hopefully) fix `nbgv` failures with `--` argument separator
    I believe `dotnet tool run` may be stealing the `--project` parameter non-deterministically (perhaps due to string hashing inputs that change in every process). By adding `--` in front of all parameters for the `nbgv` tool, I believe and hope this will resolve the issue by forcing the command line parser to interpret all subsequent command line arguments to be directed to `nbgv`.

--- azure-pipelines/variables/InsertVersionsValues.ps1
+++ azure-pipelines/variables/InsertVersionsValues.ps1
@@ -6,28 +6,6 @@ return
 
 $MacroName = 'LibraryNoDotsVersion'
 $SampleProject = "$PSScriptRoot\..\..\src\LibraryName"
-try {
-    return [string]::join(',',(@{
-        ($MacroName) = & { (dotnet tool run nbgv get-version --project $SampleProject --format json | ConvertFrom-Json).AssemblyVersion };
-    }.GetEnumerator() |% { "$($_.key)=$($_.value)" }))
-}
-catch {
-    Write-Error $_
-    Write-Error "Failed. Diagnostic data proceeds:"
-    [string]::join(',',(@{
-        ($MacroName) = & {
-            Write-Host "dotnet tool run nbgv get-version --project `"$SampleProject`" --format json"
-            $result1 = dotnet tool run nbgv get-version --project $SampleProject --format json
-            Write-Host $result1
-            $result2 = dotnet tool run nbgv get-version --project $SampleProject --format json | ConvertFrom-Json
-            Write-Host $result2
-            Write-Host $result2.AssemblyVersion
-            Write-Host (dotnet tool run nbgv get-version --project $SampleProject --format json | ConvertFrom-Json).AssemblyVersion
-            Write-Output (dotnet tool run nbgv get-version --project $SampleProject --format json | ConvertFrom-Json).AssemblyVersion
-        };
-    }.GetEnumerator() |% { "$($_.key)=$($_.value)" }))
-
-    Write-Host "one more..."
-    $r = & { (dotnet tool run nbgv get-version --project $SampleProject --format json | ConvertFrom-Json).AssemblyVersion };
-    Write-Host $r
-}
+[string]::join(',',(@{
+    ($MacroName) = & { (dotnet tool run nbgv -- get-version --project $SampleProject --format json | ConvertFrom-Json).AssemblyVersion };
+}.GetEnumerator() |% { "$($_.key)=$($_.value)" }))
