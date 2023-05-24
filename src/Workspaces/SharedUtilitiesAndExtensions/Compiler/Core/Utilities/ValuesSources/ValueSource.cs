d412c3d79239ccd2fd6cc9d8cee27ab28caf9294
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-05-24 09:18:17 -0700

    Rename and move type

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Utilities/ValuesSources/ValueSource.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Utilities/ValuesSources/ValueSource.cs
@@ -17,14 +17,5 @@ namespace Roslyn.Utilities
         public abstract bool TryGetValue([MaybeNullWhen(false)] out T value);
         public abstract T GetValue(CancellationToken cancellationToken = default);
         public abstract Task<T> GetValueAsync(CancellationToken cancellationToken = default);
-
-        public static ValueSource<T> Constant(T value)
-            => new AsyncLazy<T>(value);
-    }
-
-    internal static class ValueSourceExtensions
-    {
-        internal static T GetValueOrDefault<T>(this Optional<T> optional) where T : struct
-            => optional.Value;
     }
 }
