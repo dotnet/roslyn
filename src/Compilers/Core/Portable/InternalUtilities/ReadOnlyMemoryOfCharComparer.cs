d41b95d88532625cad31bcbfca0628f4f4e3be3c
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-06-01 11:19:26 -0700

    Break out type

---
+++ src/Compilers/Core/Portable/InternalUtilities/ReadOnlyMemoryOfCharComparer.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis
+{
+    internal sealed class ReadOnlyMemoryOfCharComparer : IEqualityComparer<ReadOnlyMemory<char>>
+    {
+        public static ReadOnlyMemoryOfCharComparer Instance { get; } = new();
+
+        private ReadOnlyMemoryOfCharComparer()
+        {
+        }
+
+        public bool Equals(ReadOnlyMemory<char> x, ReadOnlyMemory<char> y)
+            => x.Span.SequenceEqual(y.Span);
+
+        public int GetHashCode(ReadOnlyMemory<char> obj)
+        {
+#if NET
+            return string.GetHashCode(obj.Span);
+#else
+            return Hash.GetFNVHashCode(obj.Span);
+#endif
+        }
+    }
+
+    /// <summary>
+    /// Very cheap trivial comparer that never matches the keys,
+    /// should only be used in empty dictionaries.
+    /// </summary>
+    internal sealed class EmptyReadOnlyMemoryOfCharComparer : IEqualityComparer<ReadOnlyMemory<char>>
+    {
+        public static readonly EmptyReadOnlyMemoryOfCharComparer Instance = new EmptyReadOnlyMemoryOfCharComparer();
+
+        private EmptyReadOnlyMemoryOfCharComparer()
+        {
+        }
+
+        public bool Equals(ReadOnlyMemory<char> a, ReadOnlyMemory<char> b)
+        {
+            Debug.Assert(false, "Are we using empty comparer with nonempty dictionary?");
+            return false;
+        }
+
+        public int GetHashCode(ReadOnlyMemory<char> s)
+        {
+            // dictionary will call this often
+            return 0;
+        }
+    }
+}
--- src/Compilers/Core/Portable/MetadataReader/MetadataTypeName.cs
+++ src/Compilers/Core/Portable/MetadataReader/MetadataTypeName.cs
@@ -12,51 +12,6 @@ using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis
 {
-    internal sealed class ReadOnlyMemoryOfCharComparer : IEqualityComparer<ReadOnlyMemory<char>>
-    {
-        public static ReadOnlyMemoryOfCharComparer Instance { get; } = new();
-
-        private ReadOnlyMemoryOfCharComparer()
-        {
-        }
-
-        public bool Equals(ReadOnlyMemory<char> x, ReadOnlyMemory<char> y)
-            => x.Span.SequenceEqual(y.Span);
-
-        public int GetHashCode(ReadOnlyMemory<char> obj)
-        {
-#if NET
-            return string.GetHashCode(obj.Span);
-#else
-            return Hash.GetFNVHashCode(obj.Span);
-#endif
-        }
-    }
-
-    /// <summary>
-    /// Very cheap trivial comparer that never matches the keys,
-    /// should only be used in empty dictionaries.
-    /// </summary>
-    internal sealed class EmptyReadOnlyMemoryOfCharComparer : IEqualityComparer<ReadOnlyMemory<char>>
-    {
-        public static readonly EmptyReadOnlyMemoryOfCharComparer Instance = new EmptyReadOnlyMemoryOfCharComparer();
-
-        private EmptyReadOnlyMemoryOfCharComparer()
-        {
-        }
-
-        public bool Equals(ReadOnlyMemory<char> a, ReadOnlyMemory<char> b)
-        {
-            Debug.Assert(false, "Are we using empty comparer with nonempty dictionary?");
-            return false;
-        }
-
-        public int GetHashCode(ReadOnlyMemory<char> s)
-        {
-            // dictionary will call this often
-            return 0;
-        }
-    }
 
     /// <summary>
     /// Helper structure to encapsulate/cache various information about metadata name of a type and 
