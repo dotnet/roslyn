39993d4cf7fac1d5d50ff9ab81b7f104b4f2ab9f
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-06-05 15:22:16 -0700

    Working

--- src/EditorFeatures/Core/Structure/StructureTag.cs
+++ src/EditorFeatures/Core/Structure/StructureTag.cs
@@ -19,25 +19,6 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Structure
     {
         private readonly AbstractStructureTaggerProvider _tagProvider;
 
-        public StructureTag(AbstractStructureTaggerProvider tagProvider, BlockSpan blockSpan, ITextSnapshot snapshot)
-        {
-            Snapshot = snapshot;
-            OutliningSpan = blockSpan.TextSpan.ToSpan();
-            Type = ConvertType(blockSpan.Type);
-            IsCollapsible = blockSpan.IsCollapsible;
-            IsDefaultCollapsed = blockSpan.IsDefaultCollapsed;
-            IsImplementation = blockSpan.AutoCollapse;
-            HeaderSpan = StructureUtilities.DetermineHeaderSpan(
-                blockSpan.TextSpan,
-                blockSpan.HintSpan,
-                snapshot.AsText()).ToSnapshotSpan(snapshot);
-            CollapsedText = blockSpan.BannerText;
-            CollapsedHintFormSpan = blockSpan.HintSpan.ToSpan();
-            _tagProvider = tagProvider;
-
-            if (blockSpan.)
-        }
-
         /// <summary>
         /// The contents of the buffer to show if we mouse over the collapsed indicator.
         /// </summary>
@@ -48,14 +29,48 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Structure
         public ITextSnapshot Snapshot { get; }
         public Span? OutliningSpan { get; }
         public Span? HeaderSpan { get; }
-        public Span? GuideLineSpan => null;
         public Span? PrimaryHeaderSpan { get; }
-
+        public Span? GuideLineSpan => null;
         public int? GuideLineHorizontalAnchorPoint => null;
         public string Type { get; }
         public bool IsCollapsible { get; }
         public bool IsDefaultCollapsed { get; }
         public bool IsImplementation { get; }
+
+        public StructureTag(AbstractStructureTaggerProvider tagProvider, BlockSpan blockSpan, ITextSnapshot snapshot)
+        {
+            Snapshot = snapshot;
+            OutliningSpan = blockSpan.TextSpan.ToSpan();
+            Type = ConvertType(blockSpan.Type);
+            IsCollapsible = blockSpan.IsCollapsible;
+            IsDefaultCollapsed = blockSpan.IsDefaultCollapsed;
+            IsImplementation = blockSpan.AutoCollapse;
+            HeaderSpan = DetermineHeaderSpan(blockSpan.TextSpan, blockSpan.HintSpan, snapshot);
+            PrimaryHeaderSpan = blockSpan.PrimarySpans is { } primarySpans
+                ? DetermineHeaderSpan(primarySpans.textSpan, primarySpans.hintSpan, snapshot)
+                : null;
+
+            CollapsedText = blockSpan.BannerText;
+            CollapsedHintFormSpan = blockSpan.HintSpan.ToSpan();
+            _tagProvider = tagProvider;
+        }
+
+        private static Span DetermineHeaderSpan(TextSpan textSpan, TextSpan hintSpan, ITextSnapshot snapshot)
+        {
+            if (hintSpan.Start < textSpan.Start)
+            {
+                // The HeaderSpan is what is used for drawing the guidelines and also what is shown if
+                // you mouse over a guideline. We will use the text from the hint start to the collapsing
+                // start; in the case this spans mutiple lines the editor will clip it for us and suffix an
+                // ellipsis at the end.
+                return Span.FromBounds(hintSpan.Start, textSpan.Start);
+            }
+            else
+            {
+                var hintLine = snapshot.GetLineFromPosition(hintSpan.Start);
+                return AbstractStructureTaggerProvider.TrimLeadingWhitespace(hintLine.Extent);
+            }
+        }
 
         // Editor uses this here:
         // https://devdiv.visualstudio.com/DefaultCollection/DevDiv/_git/VS-Platform?path=/src/Editor/Text/Impl/Structure/StructureSpanningTree/StructureSpanningTree.cs&version=GBmain&line=308&lineEnd=309&lineStartColumn=1&lineEndColumn=1&lineStyle=plain&_a=contents
@@ -66,7 +81,8 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Structure
                Hash.Combine(this.IsDefaultCollapsed,
                Hash.Combine(this.IsImplementation,
                Hash.Combine(this.OutliningSpan.GetHashCode(),
-               Hash.Combine(this.HeaderSpan.GetHashCode(), this.GuideLineSpan.GetHashCode())))))));
+               Hash.Combine(this.HeaderSpan.GetHashCode(),
+               Hash.Combine(this.PrimaryHeaderSpan.GetHashCode(), this.GuideLineSpan.GetHashCode()))))))));
 
         public override bool Equals(object? obj)
             => Equals(obj as StructureTag);
@@ -81,6 +97,7 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Structure
                 this.IsImplementation == other.IsImplementation &&
                 _tagProvider.SpanEquals(this.Snapshot, this.OutliningSpan, other.Snapshot, other.OutliningSpan) &&
                 _tagProvider.SpanEquals(this.Snapshot, this.HeaderSpan, other.Snapshot, other.HeaderSpan) &&
+                _tagProvider.SpanEquals(this.Snapshot, this.PrimaryHeaderSpan, other.Snapshot, other.PrimaryHeaderSpan) &&
                 _tagProvider.SpanEquals(this.Snapshot, this.GuideLineSpan, other.Snapshot, other.GuideLineSpan);
         }
 
