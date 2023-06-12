6caa86e8b5f31bbfe68ad6b5373ad27c8bd7af15
Todd Grunke <toddgrun@microsoft.com>
2023-06-12 15:16:55 -0700

    Test PR to track down test failures as I don't want to pollute main telemetry PR with these testing commits

---
+++ src/VisualStudio/Core/Def/Telemetry/VisualStudioTelemetryLogManager.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.Internal.Log;
+using Microsoft.CodeAnalysis.Telemetry;
+using Microsoft.VisualStudio.Telemetry;
+
+namespace Microsoft.VisualStudio.LanguageServices.Telemetry
+{
+    internal sealed class VisualStudioTelemetryLogManager
+    {
+        private readonly TelemetrySession _session;
+        private readonly ILogger _telemetryLogger;
+
+        private ImmutableDictionary<FunctionId, VisualStudioTelemetryLog> _logs = ImmutableDictionary<FunctionId, VisualStudioTelemetryLog>.Empty;
+
+        public VisualStudioTelemetryLogManager(TelemetrySession session, ILogger telemetryLogger)
+        {
+            _session = session;
+            _telemetryLogger = telemetryLogger;
+        }
+
+        public ITelemetryLog? GetLog(FunctionId functionId)
+        {
+            if (!_session.IsOptedIn)
+                return null;
+
+            return ImmutableInterlocked.GetOrAdd(ref _logs, functionId, functionId => new VisualStudioTelemetryLog(_telemetryLogger, functionId));
+        }
+    }
+}
