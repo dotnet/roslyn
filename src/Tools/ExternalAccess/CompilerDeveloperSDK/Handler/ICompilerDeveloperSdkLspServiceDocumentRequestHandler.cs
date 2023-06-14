0a9c614f53f976551aa689a8e9f4cd0b9a5782fa
Fredric Silberberg <frsilb@microsoft.com>
2023-06-14 16:08:04 -0700

    Add EA package for the compiler developer SDK
    As part of experimenting with a public API for the C# language server, we're adding an EA reference for a compiler developer SDK to help drive the API design without exposing publicly for the initial stages.

---
+++ src/Tools/ExternalAccess/CompilerDeveloperSDK/Handler/ICompilerDeveloperSdkLspServiceDocumentRequestHandler.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.LanguageServer.Handler;
+using Microsoft.CommonLanguageServerProtocol.Framework;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.CompilerDeveloperSDK;
+
+internal interface ICompilerDeveloperSdkLspServiceDocumentRequestHandler<TRequest, TResponse> :
+    ILspServiceDocumentRequestHandler<TRequest, TResponse>
+{
+    bool ISolutionRequiredHandler.RequiresLSPSolution => true;
+
+    Task<TResponse> IRequestHandler<TRequest, TResponse, LspRequestContext>.HandleRequestAsync(TRequest request, LspRequestContext context, CancellationToken cancellationToken)
+        => HandleRequestAsync(request, new RequestContext(context), cancellationToken);
+
+    Task<TResponse> HandleRequestAsync(TRequest request, RequestContext context, CancellationToken cancellationToken);
+}
