9637d0ed20cf8537cbd1e082294f3d9919acbe75
Fredric Silberberg <frsilb@microsoft.com>
2023-06-16 15:35:35 -0700

    More IsBuildOnlyDiagnostic followpu

--- src/Compilers/CSharp/Test/Syntax/Diagnostics/DiagnosticTest.cs
+++ src/Compilers/CSharp/Test/Syntax/Diagnostics/DiagnosticTest.cs
@@ -2950,6 +2950,7 @@ class Program
                     case ErrorCode.WRN_NullabilityMismatchInReturnTypeOnInterceptor:
                     case ErrorCode.WRN_NullabilityMismatchInParameterTypeOnInterceptor:
                     case ErrorCode.ERR_InterceptorCannotInterceptNameof:
+                    case ErrorCode.ERR_SymbolDefinedInAssembly:
                         Assert.True(isBuildOnly, $"Check failed for ErrorCode.{errorCode}");
                         break;
 
