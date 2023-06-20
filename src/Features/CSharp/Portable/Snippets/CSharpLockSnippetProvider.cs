233fa30c2b648421633f7155013fbabefb1f471f
DoctorKrolic <70431552+DoctorKrolic@users.noreply.github.com>
2023-06-20 21:21:58 +0300

    Semantic snippets - `lock` snippet and a lot of refactoring (#68673)
    * Extract common logic into helper class
    
    * Actually add `lock` snippet
    
    * Extract more common logic into helper class
    
    * Rename
    
    * Remove unused parameters
    
    * Add alternative constructor to `SnippetPlaceholder`

--- src/Features/CSharp/Portable/Snippets/CSharpLockSnippetProvider.cs
+++ src/Features/CSharp/Portable/Snippets/CSharpLockSnippetProvider.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Immutable;
 using System.Composition;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,18 +17,29 @@ using Microsoft.CodeAnalysis.Text;
 namespace Microsoft.CodeAnalysis.CSharp.Snippets
 {
     [ExportSnippetProvider(nameof(ISnippetProvider), LanguageNames.CSharp), Shared]
-    internal sealed class CSharpWhileLoopSnippetProvider : AbstractWhileLoopSnippetProvider
+    internal sealed class CSharpLockSnippetProvider : AbstractLockSnippetProvider
     {
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-        public CSharpWhileLoopSnippetProvider()
+        public CSharpLockSnippetProvider()
         {
+        }
+
+        public override string Identifier => "lock";
+
+        public override string Description => CSharpFeaturesResources.lock_statement;
+
+        protected override ImmutableArray<SnippetPlaceholder> GetPlaceHolderLocationsList(SyntaxNode node, ISyntaxFacts syntaxFacts, CancellationToken cancellationToken)
+        {
+            var lockStatement = (LockStatementSyntax)node;
+            var expression = lockStatement.Expression;
+            return ImmutableArray.Create(new SnippetPlaceholder(expression.ToString(), expression.SpanStart));
         }
 
         protected override int GetTargetCaretPosition(ISyntaxFactsService syntaxFacts, SyntaxNode caretTarget, SourceText sourceText)
         {
-            var whileStatement = (WhileStatementSyntax)caretTarget;
-            var blockStatement = (BlockSyntax)whileStatement.Statement;
+            var lockStatement = (LockStatementSyntax)caretTarget;
+            var blockStatement = (BlockSyntax)lockStatement.Statement;
 
             var triviaSpan = blockStatement.CloseBraceToken.LeadingTrivia.Span;
             var line = sourceText.Lines.GetLineFromPosition(triviaSpan.Start);
@@ -35,15 +47,9 @@ namespace Microsoft.CodeAnalysis.CSharp.Snippets
             return line.Span.End;
         }
 
-        protected override SyntaxNode GetCondition(SyntaxNode node)
-        {
-            var whileStatement = (WhileStatementSyntax)node;
-            return whileStatement.Condition;
-        }
-
         protected override Task<Document> AddIndentationToDocumentAsync(Document document, CancellationToken cancellationToken)
         {
-            return СSharpSnippetIndentationHelpers.AddBlockIndentationToDocumentAsync<WhileStatementSyntax>(
+            return СSharpSnippetIndentationHelpers.AddBlockIndentationToDocumentAsync<LockStatementSyntax>(
                 document,
                 FindSnippetAnnotation,
                 static s => (BlockSyntax)s.Statement,
