ef1b48d70414a1e00114e434a2e4ae07a62da92e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-06-21 19:36:33 -0700

    remove using

--- src/Compilers/CSharp/Portable/Compilation/CSharpSemanticModel.cs
+++ src/Compilers/CSharp/Portable/Compilation/CSharpSemanticModel.cs
@@ -13,7 +13,6 @@ using System.Threading;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.PooledObjects;
-using Microsoft.CodeAnalysis.Shared.Collections;
 using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
 
