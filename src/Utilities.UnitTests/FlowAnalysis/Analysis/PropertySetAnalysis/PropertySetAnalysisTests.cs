3b4274daf05e54a6a1028a1309ec11f389e9be29
Manish Vasani <mavasani@microsoft.com>
2023-06-22 12:06:08 +0530

    Fix more tests

--- src/Utilities.UnitTests/FlowAnalysis/Analysis/PropertySetAnalysis/PropertySetAnalysisTests.cs
+++ src/Utilities.UnitTests/FlowAnalysis/Analysis/PropertySetAnalysis/PropertySetAnalysisTests.cs
@@ -1305,7 +1305,7 @@ class TestClass
                 .AddMetadataReference(projectId, AdditionalMetadataReferences.SystemWebExtensions)
 #endif
                 .WithProjectCompilationOptions(projectId, options)
-                .WithProjectParseOptions(projectId, null)
+                .WithProjectParseOptions(projectId, new CSharpParseOptions())
                 .GetProject(projectId);
 
             // Enable Flow-Analysis feature on the project
