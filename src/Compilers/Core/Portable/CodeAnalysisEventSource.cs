33b54762955e418913ac04cf09da2581c5496dfb
Chris Sienkiewicz <chsienki@microsoft.com>
2023-06-28 14:12:35 -0700

    Instrument most nodes

--- src/Compilers/Core/Portable/CodeAnalysisEventSource.cs
+++ src/Compilers/Core/Portable/CodeAnalysisEventSource.cs
@@ -16,12 +16,14 @@ namespace Microsoft.CodeAnalysis
         public static class Keywords
         {
             public const EventKeywords Performance = (EventKeywords)1;
+            public const EventKeywords Correctness = (EventKeywords)2;
         }
 
         public static class Tasks
         {
             public const EventTask GeneratorDriverRunTime = (EventTask)1;
             public const EventTask SingleGeneratorRunTime = (EventTask)2;
+            public const EventTask BuildStateTable = (EventTask)3;
         }
 
         private CodeAnalysisEventSource() { }
@@ -40,5 +42,8 @@ namespace Microsoft.CodeAnalysis
 
         [Event(5, Message = "Generator '{0}' failed with exception: {1}", Level = EventLevel.Error)]
         internal void GeneratorException(string generatorName, string exception) => WriteEvent(5, generatorName, exception);
+
+        [Event(6, Message = "Node {0} transformed", Keywords = Keywords.Correctness, Level = EventLevel.Verbose, Task = Tasks.BuildStateTable)]
+        internal void NodeTransform(int nodeHashCode, string name, string tableType, int previousTable, string previousTableContent, int newTable, string newTableContent, int input1, int input2) => WriteEvent(6, nodeHashCode, name, tableType, previousTable, previousTableContent, newTable, newTableContent, input1, input2);
     }
 }
