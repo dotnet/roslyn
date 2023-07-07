917a44b8c748c8fb20294a0264cc71735350746a
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-07-07 10:10:10 -0700

    Initial helper methods

--- src/EditorFeatures/Core/Organizing/OrganizeDocumentCommandHandler.cs
+++ src/EditorFeatures/Core/Organizing/OrganizeDocumentCommandHandler.cs
@@ -9,6 +9,7 @@ using System.ComponentModel.Composition;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Editor.BackgroundWorkIndicator;
 using Microsoft.CodeAnalysis.Editor.Commanding.Commands;
 using Microsoft.CodeAnalysis.Editor.Shared.Extensions;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
@@ -18,10 +19,13 @@ using Microsoft.CodeAnalysis.OrganizeImports;
 using Microsoft.CodeAnalysis.Organizing;
 using Microsoft.CodeAnalysis.RemoveUnnecessaryImports;
 using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Shared.TestHooks;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.Commanding;
 using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Editor.Commanding;
+using Microsoft.VisualStudio.Threading;
 using Microsoft.VisualStudio.Utilities;
 using Roslyn.Utilities;
 
@@ -39,40 +43,24 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Organizing
     {
         private readonly IThreadingContext _threadingContext;
         private readonly IGlobalOptionService _globalOptions;
+        private readonly IAsynchronousOperationListener _listener;
 
         [ImportingConstructor]
         [SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
-        public OrganizeDocumentCommandHandler(IThreadingContext threadingContext, IGlobalOptionService globalOptions)
+        public OrganizeDocumentCommandHandler(
+            IThreadingContext threadingContext,
+            IGlobalOptionService globalOptions,
+            IAsynchronousOperationListenerProvider listenerProvider)
         {
             _threadingContext = threadingContext;
             _globalOptions = globalOptions;
+            _listener = listenerProvider.GetListener(FeatureAttribute.OrganizeDocument);
         }
 
         public string DisplayName => EditorFeaturesResources.Organize_Document;
 
         public CommandState GetCommandState(OrganizeDocumentCommandArgs args)
             => GetCommandState(args, _ => EditorFeaturesResources.Organize_Document, needsSemantics: true);
-
-        public bool ExecuteCommand(OrganizeDocumentCommandArgs args, CommandExecutionContext context)
-        {
-            using (context.OperationContext.AddScope(allowCancellation: true, EditorFeaturesResources.Organizing_document))
-            {
-                var cancellationToken = context.OperationContext.UserCancellationToken;
-                var document = args.SubjectBuffer.CurrentSnapshot.GetFullyLoadedOpenDocumentInCurrentContextWithChanges(
-                    context.OperationContext, _threadingContext);
-                if (document != null)
-                {
-                    var newDocument = OrganizingService.OrganizeAsync(document, cancellationToken: cancellationToken).WaitAndGetResult(cancellationToken);
-                    if (document != newDocument)
-                    {
-                        var changes = newDocument.GetTextChangesAsync(document, cancellationToken).WaitAndGetResult(cancellationToken);
-                        args.SubjectBuffer.ApplyChanges(changes);
-                    }
-                }
-            }
-
-            return true;
-        }
 
         public CommandState GetCommandState(SortImportsCommandArgs args)
             => GetCommandState(args, o => o.SortImportsDisplayStringWithAccelerator, needsSemantics: false);
@@ -113,6 +101,94 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Organizing
             }
 
             return false;
+        }
+
+        private void ExecuteCommandWorker(
+            EditorCommandArgs commandArgs,
+            CommandExecutionContext context,
+            string dialogText,
+            Func<ITextSnapshot, IUIThreadOperationContext, Task<Document>> getCurrentDocumentAsync,
+            Func<Document, CancellationToken, Task<Document>> getChangedDocumentAsync)
+        {
+            // We're showing our own UI, ensure the editor doesn't show anything itself.
+            context.OperationContext.TakeOwnership();
+
+            var subjectBuffer = commandArgs.SubjectBuffer;
+            var textView = commandArgs.TextView;
+
+            var caretPoint = textView.GetCaretPoint(subjectBuffer);
+            if (caretPoint is null)
+                return;
+
+            if (!subjectBuffer.TryGetWorkspace(out var workspace))
+                return;
+
+            var token = _listener.BeginAsyncOperation(nameof(ExecuteCommandWorker));
+
+            var snapshotSpan = textView.GetTextElementSpan(caretPoint.Value);
+            ExecuteAsync(workspace, commandArgs, snapshotSpan, dialogText, getCurrentDocumentAsync, getChangedDocumentAsync)
+                .ReportNonFatalErrorAsync()
+                .CompletesAsyncOperation(token);
+        }
+
+        private async Task ExecuteAsync(
+            Workspace workspace,
+            EditorCommandArgs commandArgs,
+            SnapshotSpan snapshotSpan,
+            string dialogText,
+            Func<ITextSnapshot, IUIThreadOperationContext, Task<Document>> getCurrentDocumentAsync,
+            Func<Document, CancellationToken, Task<Document>> getChangedDocumentAsync)
+        {
+            _threadingContext.ThrowIfNotOnUIThread();
+
+            var indicatorFactory = workspace.Services.GetRequiredService<IBackgroundWorkIndicatorFactory>();
+            using var backgroundWorkContext = indicatorFactory.Create(
+                commandArgs.TextView,
+                snapshotSpan,
+                dialogText,
+                cancelOnEdit: true,
+                cancelOnFocusLost: true);
+
+            var cancellationToken = backgroundWorkContext.UserCancellationToken;
+
+            await TaskScheduler.Default;
+
+            var currentDocument = await getCurrentDocumentAsync(snapshotSpan.Snapshot, backgroundWorkContext).ConfigureAwait(false);
+            var newDocument = await getChangedDocumentAsync(currentDocument, cancellationToken).ConfigureAwait(false);
+
+            if (currentDocument == newDocument)
+                return;
+
+            var changes = newDocument.GetTextChangesAsync(currentDocument, cancellationToken).WaitAndGetResult(cancellationToken);
+
+            // Required to switch back to the UI thread to call TryApplyChanges
+            await _threadingContext.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            // We're about to make an edit ourselves.  so disable the cancellation that happens on editing.
+            backgroundWorkContext.CancelOnEdit = false;
+
+            commandArgs.SubjectBuffer.ApplyChanges(changes);
+        }
+
+        public bool ExecuteCommand(OrganizeDocumentCommandArgs args, CommandExecutionContext context)
+        {
+            using (context.OperationContext.AddScope(allowCancellation: true, EditorFeaturesResources.Organizing_document))
+            {
+                var cancellationToken = context.OperationContext.UserCancellationToken;
+                var document = args.SubjectBuffer.CurrentSnapshot.GetFullyLoadedOpenDocumentInCurrentContextWithChanges(
+                    context.OperationContext, _threadingContext);
+                if (document != null)
+                {
+                    var newDocument = OrganizingService.OrganizeAsync(document, cancellationToken: cancellationToken).WaitAndGetResult(cancellationToken);
+                    if (document != newDocument)
+                    {
+                        var changes = newDocument.GetTextChangesAsync(document, cancellationToken).WaitAndGetResult(cancellationToken);
+                        args.SubjectBuffer.ApplyChanges(changes);
+                    }
+                }
+            }
+
+            return true;
         }
 
         public bool ExecuteCommand(SortImportsCommandArgs args, CommandExecutionContext context)
