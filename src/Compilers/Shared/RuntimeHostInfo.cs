893cd559a51fc3e9a867cda28e45f3887f75c340
Jared Parsons <jared@popcornbear.org>
2023-07-07 13:35:58 -0700

    fix

--- src/Compilers/Shared/RuntimeHostInfo.cs
+++ src/Compilers/Shared/RuntimeHostInfo.cs
@@ -7,7 +7,6 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.IO.Pipes;
-using Microsoft.CodeAnalysis.BuildTasks;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis
