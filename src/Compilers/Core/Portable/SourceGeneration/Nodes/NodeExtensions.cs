35aac0db9b5b60bac5f3902c7adeb9815775859b
Chris Sienkiewicz <chsienki@microsoft.com>
2023-07-19 16:03:39 -0700

    Fix nullability warning

--- src/Compilers/Core/Portable/SourceGeneration/Nodes/NodeExtensions.cs
+++ src/Compilers/Core/Portable/SourceGeneration/Nodes/NodeExtensions.cs
@@ -18,7 +18,7 @@ namespace Microsoft.CodeAnalysis
 
                 CodeAnalysisEventSource.Log.NodeTransform(self.GetHashCode(),
                                                           name ?? "<anonymous>",
-                                                          typeof(TSelf).FullName,
+                                                          typeof(TSelf).FullName ?? "<unknown>",
                                                           previousTable?.GetHashCode() ?? -1,
                                                           previousTable?.GetPackedStates() ?? "",
                                                           newTableOpt?.GetHashCode() ?? -1,
