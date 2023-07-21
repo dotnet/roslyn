c259bc9f75b0805d2117ad439aeadf9cd2e022ee
Jason Malinowski <jason.malinowski@microsoft.com>
2023-07-21 16:29:17 -0700

    Remove WorkspaceConfigurationService.Clear()
    For some reason we have a method that tests use to clear the cached
    value if requisite options change. Rather than doing that we can
    just clear it automatically; without this the option is also frozen
    for the lifetime of the process.

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpSourceGenerators.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpSourceGenerators.cs
@@ -95,12 +95,6 @@ internal static class Program
 
             if (invokeFromSourceGeneratedFile)
             {
-                var workspace = await TestServices.Shell.GetComponentModelServiceAsync<VisualStudioWorkspace>(HangMitigatingCancellationToken);
-
-                // clear configuration options already read by initialization above, so that the global option update below is effective:
-                var configurationService = (WorkspaceConfigurationService)workspace.Services.GetRequiredService<IWorkspaceConfigurationService>();
-                configurationService.Clear();
-
                 await TestServices.Editor.GoToDefinitionAsync(HangMitigatingCancellationToken);
                 Assert.Equal($"{HelloWorldGenerator.GeneratedEnglishClassName}.cs {ServicesVSResources.generated_suffix}", await TestServices.Shell.GetActiveWindowCaptionAsync(HangMitigatingCancellationToken));
             }
