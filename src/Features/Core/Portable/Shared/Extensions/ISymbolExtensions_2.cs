3082e94308bffc5acb91c6d1462ebeb4438d235a
Rekkonnect <kalfas.alex@gmail.com>
2023-07-26 20:50:37 +0300

    Adjust preprocessing symbol glyph

--- src/Features/Core/Portable/Shared/Extensions/ISymbolExtensions_2.cs
+++ src/Features/Core/Portable/Shared/Extensions/ISymbolExtensions_2.cs
@@ -153,9 +153,8 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions
                 case SymbolKind.TypeParameter:
                     return Glyph.TypeParameter;
 
-                // Temporary fix
                 case SymbolKind.Preprocessing:
-                    return Glyph.Label;
+                    return Glyph.Keyword;
 
                 default:
                     throw new ArgumentException(FeaturesResources.The_symbol_does_not_have_an_icon, nameof(symbol));
