684c4f47ca53a0252dac12fc4a7893fd71b4664c
Sam Harwell <Sam.Harwell@microsoft.com>
2023-08-04 13:38:51 -0500

    Avoid enumeration overhead by using ImmutableArray<T> instead of ImmutableHashSet<T>

--- src/Utilities/Compiler/WellKnownTypeProvider.cs
+++ src/Utilities/Compiler/WellKnownTypeProvider.cs
@@ -26,14 +26,14 @@ namespace Analyzer.Utilities
         {
             Compilation = compilation;
             _fullNameToTypeMap = new ConcurrentDictionary<string, INamedTypeSymbol?>(StringComparer.Ordinal);
-            _referencedAssemblies = new Lazy<ImmutableHashSet<IAssemblySymbol>>(
+            _referencedAssemblies = new Lazy<ImmutableArray<IAssemblySymbol>>(
                 () =>
                 {
                     return ImmutableHashSet.Create<IAssemblySymbol>(
                         SymbolEqualityComparer.Default,
                         Compilation.Assembly.Modules
                             .SelectMany(m => m.ReferencedAssemblySymbols)
-                            .ToArray());
+                            .ToArray()).ToImmutableArray();
                 },
                 LazyThreadSafetyMode.ExecutionAndPublication);
         }
@@ -56,7 +56,7 @@ namespace Analyzer.Utilities
         /// foreach (Compilation.Assembly.Modules)
         ///     foreach (Module.ReferencedAssemblySymbols)
         /// </remarks>
-        private readonly Lazy<ImmutableHashSet<IAssemblySymbol>> _referencedAssemblies;
+        private readonly Lazy<ImmutableArray<IAssemblySymbol>> _referencedAssemblies;
 
         /// <summary>
         /// Mapping of full name to <see cref="INamedTypeSymbol"/>.
