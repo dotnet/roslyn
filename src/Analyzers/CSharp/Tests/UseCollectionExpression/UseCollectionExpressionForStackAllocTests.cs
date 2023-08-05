c6b39f23c6a9b459bcab83b2d6dbf95bc0234132
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-08-05 13:20:42 -0700

    tests

--- src/Analyzers/CSharp/Tests/UseCollectionExpression/UseCollectionExpressionForStackAllocTests.cs
+++ src/Analyzers/CSharp/Tests/UseCollectionExpression/UseCollectionExpressionForStackAllocTests.cs
@@ -548,4 +548,344 @@ public class UseCollectionExpressionForStackAllocTests
             ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
         }.RunAsync();
     }
+
+    [Fact]
+    public async Task TestNoInitializer_ZeroSize()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    void M()
+                    {
+                        ReadOnlySpan<int> r = [|[|stackalloc|] int[0]|];
+                    }
+                }
+                """,
+            FixedCode = """
+                using System;
+
+                class C
+                {
+                    void M()
+                    {
+                        ReadOnlySpan<int> r = [];
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_NotEnoughFollowingStatements()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    void M()
+                    {
+                        ReadOnlySpan<int> r = stackalloc int[1];
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_WrongFollowingStatement()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    void M()
+                    {
+                        ReadOnlySpan<int> r = stackalloc int[1];
+                        return;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_NotLocalStatementInitializer()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    void M()
+                    {
+                        Span<int> r = Goo(stackalloc int[1]);
+                        r[0] = 1;
+                    }
+
+                    Span<int> Goo(Span<int> input) => default;
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_ExpressionStatementNotAssignment()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    void M(int i)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[1]|];
+                        i++;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_AssignmentNotElementAccess()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[1]|];
+                        i = j;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_ElementAccessNotToIdentifier()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[1]|];
+                        C.array[0] = j;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_IdentifierNotEqualToVariableName()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[1]|];
+                        array[0] = j;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_ArgumentNotConstant()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[1]|];
+                        r[i] = j;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_ConstantArgumentNotCorrect1()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[1]|];
+                        r[1] = j;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_ConstantArgumentNotCorrect2()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[2]|];
+                        r[0] = i;
+                        r[0] = j;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_OneElement()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[1]|];
+                        r[0] = i;
+                    }
+                }
+                """,
+            FixedCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [i];
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
+
+    [Fact]
+    public async Task TestNoInitializer_OneElement_MultipleFollowingStatements()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [|[|stackalloc|] int[1]|];
+                        r[0] = i;
+                        r[0] = j;
+                    }
+                }
+                """,
+            FixedCode = """
+                using System;
+
+                class C
+                {
+                    static int[] array;
+
+                    void M(int i, int j)
+                    {
+                        Span<int> r = [i];
+                        r[0] = j;
+                    }
+                }
+                """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
+        }.RunAsync();
+    }
 }
