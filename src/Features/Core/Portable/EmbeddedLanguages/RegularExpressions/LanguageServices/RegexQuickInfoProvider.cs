6d57638076144983f6e80af15a1dc00f96f98d5b
Chanel Frazier <t-cfrazier@microsoft.com>
2023-08-04 17:05:42 -0700

    Delete multiple lines

--- src/Features/Core/Portable/EmbeddedLanguages/RegularExpressions/LanguageServices/RegexQuickInfoProvider.cs
+++ src/Features/Core/Portable/EmbeddedLanguages/RegularExpressions/LanguageServices/RegexQuickInfoProvider.cs
@@ -2,15 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-
-
 using System;
 using System.Collections.Immutable;
 using System.Composition;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.QuickInfo;
-
-
 
 namespace Microsoft.CodeAnalysis.EmbeddedLanguages.RegularExpressions.LanguageServices
 {
@@ -22,7 +18,6 @@ namespace Microsoft.CodeAnalysis.EmbeddedLanguages.RegularExpressions.LanguageSe
         public RegexQuickInfoProvider()
         {
         }
-
 
         public QuickInfoItem? GetQuickInfo(QuickInfoContext context, SemanticModel semanticModel, SyntaxToken token)
         {
