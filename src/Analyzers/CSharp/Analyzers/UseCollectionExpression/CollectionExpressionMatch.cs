fc9e7cea42b6e3b0be3e71f422c5cd11c0a7c43b
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-08-04 17:03:17 -0700

    SHare docs

--- src/Analyzers/CSharp/Analyzers/UseCollectionExpression/CollectionExpressionMatch.cs
+++ src/Analyzers/CSharp/Analyzers/UseCollectionExpression/CollectionExpressionMatch.cs
@@ -3,18 +3,11 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.UseCollectionInitializer;
 
 namespace Microsoft.CodeAnalysis.UseCollectionExpression;
 
-/// <summary>
-/// Represents statements following some expression that should be converted into collection-expression elements.
-/// </summary>
-/// <param name="Statement">The statement that follows that contains the values to add to the new
-/// collection-initializer or collection-expression</param>
-/// <param name="UseSpread">Whether or not a spread (<c>.. x</c>) element should be created for this statement. This is
-/// needed as the statement could be cases like <c>expr.Add(x)</c> vs. <c>expr.AddRange(x)</c>. This property indicates
-/// that the latter should become a spread, without the consumer having to reexamine the statement to see what form it
-/// is.</param>
+/// <inheritdoc cref="Match{TStatementSyntax}"/>
 internal readonly record struct CollectionExpressionMatch(
     StatementSyntax Statement,
     bool UseSpread);
