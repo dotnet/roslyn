4560dfe7f409a0b8e16a51bfc21ac9d9d6d99c8a
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-08-09 15:38:59 -0700

    add base type

---
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/CodeFixes/ForkingSyntaxEditorBasedCodeFixProvider.cs
@@ -0,0 +1,78 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.LanguageService;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+
+namespace Microsoft.CodeAnalysis.CodeFixes;
+
+internal abstract class ForkingSyntaxEditorBasedCodeFixProvider<TDiagnosticNode>
+    : SyntaxEditorBasedCodeFixProvider
+    where TDiagnosticNode : SyntaxNode
+{
+    protected abstract Task FixAsync(
+        Document document, Diagnostic diagnostic, SyntaxEditor editor, CodeActionOptionsProvider fallbackOptions, CancellationToken cancellationToken);
+
+    protected sealed override bool IncludeDiagnosticDuringFixAll(Diagnostic diagnostic)
+        => !diagnostic.Descriptor.ImmutableCustomTags().Contains(WellKnownDiagnosticTags.Unnecessary);
+
+    protected sealed override async Task FixAllAsync(
+        Document document,
+        ImmutableArray<Diagnostic> diagnostics,
+        SyntaxEditor editor,
+        CodeActionOptionsProvider fallbackOptions,
+        CancellationToken cancellationToken)
+    {
+        var syntaxFacts = document.GetRequiredLanguageService<ISyntaxFactsService>();
+        var originalRoot = editor.OriginalRoot;
+
+        var originalNodes = new Stack<(TDiagnosticNode diagnosticNode, Diagnostic diagnostic)>();
+        foreach (var diagnostic in diagnostics)
+        {
+            var diagnosticNode = (TDiagnosticNode)originalRoot.FindNode(
+                diagnostic.AdditionalLocations[0].SourceSpan, getInnermostNodeForTie: true);
+            originalNodes.Push((diagnosticNode, diagnostic));
+        }
+
+        var solutionServices = document.Project.Solution.Services;
+
+        // We're going to be continually editing this tree.  Track all the nodes we care about so we can find them
+        // across each edit.
+        var semanticDocument = await SemanticDocument.CreateAsync(
+            document.WithSyntaxRoot(originalRoot.TrackNodes(originalNodes.Select(static t => t.diagnosticNode))),
+            cancellationToken).ConfigureAwait(false);
+
+        while (originalNodes.Count > 0)
+        {
+            var (originalDiagnosticNode, diagnostic) = originalNodes.Pop();
+            var currentRoot = semanticDocument.Root;
+            var diagnosticNode = currentRoot.GetCurrentNodes(originalDiagnosticNode).Single();
+
+            var subEditor = new SyntaxEditor(currentRoot, solutionServices);
+
+            await FixAsync(
+                semanticDocument.Document,
+                diagnostic,
+                subEditor,
+                fallbackOptions,
+                cancellationToken).ConfigureAwait(false);
+
+            var changedRoot = subEditor.GetChangedRoot();
+            if (currentRoot == changedRoot)
+                continue;
+
+            semanticDocument = await semanticDocument.WithSyntaxRootAsync(
+                changedRoot, cancellationToken).ConfigureAwait(false);
+        }
+
+        editor.ReplaceNode(originalRoot, semanticDocument.Root);
+    }
+}
