0417bf72a9e86a10651b0ec981ee8fdcfa1b6619
DoctorKrolic <mapmyp03@gmail.com>
2023-08-16 12:19:42 +0300

    Make `foreach` snippet work with `await` and async enumerables

--- src/EditorFeatures/CSharpTest/Completion/CompletionProviders/Snippets/CSharpForEachSnippetCompletionProviderTests.cs
+++ src/EditorFeatures/CSharpTest/Completion/CompletionProviders/Snippets/CSharpForEachSnippetCompletionProviderTests.cs
@@ -14,6 +14,130 @@ namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Completion.CompletionPr
     {
         protected override string ItemToCommit => "foreach";
 
+        private const string IAsyncEnumerable = """
+            namespace System
+            {
+                public interface IAsyncDisposable
+                {
+                    System.Threading.Tasks.ValueTask DisposeAsync();
+                }
+            }
+
+            namespace System.Runtime.CompilerServices
+            {
+                using System.Threading.Tasks;
+
+                public sealed class AsyncMethodBuilderAttribute : Attribute
+                {
+                    public AsyncMethodBuilderAttribute(Type builderType) { }
+                    public Type BuilderType { get; }
+                }
+
+                public struct AsyncValueTaskMethodBuilder
+                {
+                    public ValueTask Task => default;
+
+                    public static AsyncValueTaskMethodBuilder Create() => default;
+                    public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+                        where TAwaiter : INotifyCompletion
+                        where TStateMachine : IAsyncStateMachine {}
+
+                    public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+                        where TAwaiter : ICriticalNotifyCompletion
+                        where TStateMachine : IAsyncStateMachine {}
+                    public void SetException(Exception exception) {}
+                    public void SetResult() {}
+                    public void SetStateMachine(IAsyncStateMachine stateMachine) {}
+                    public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine {}
+                }
+
+                public readonly struct ValueTaskAwaiter : ICriticalNotifyCompletion, INotifyCompletion
+                {
+                    public bool IsCompleted => default;
+
+                    public void GetResult() { }
+                    public void OnCompleted(Action continuation) { }
+                    public void UnsafeOnCompleted(Action continuation) { }
+                }
+
+                public readonly struct ValueTaskAwaiter<TResult> : ICriticalNotifyCompletion, INotifyCompletion
+                {
+                    public bool IsCompleted => default;
+                    public TResult GetResult() => default;
+                    public void OnCompleted(Action continuation) { }
+                    public void UnsafeOnCompleted(Action continuation) { }
+                }
+            }
+
+            namespace System.Threading.Tasks
+            {
+                using System.Runtime.CompilerServices;
+
+                [AsyncMethodBuilder(typeof(AsyncValueTaskMethodBuilder))]
+                public readonly struct ValueTask : IEquatable<ValueTask>
+                {
+                    public ValueTask(Task task) {}
+                    public ValueTask(IValueTaskSource source, short token) {}
+
+                    public bool IsCompleted => default;
+                    public bool IsCompletedSuccessfully => default;
+                    public bool IsFaulted => default;
+                    public bool IsCanceled => default;
+
+                    public Task AsTask() => default;
+                    public ConfiguredValueTaskAwaitable ConfigureAwait(bool continueOnCapturedContext) => default;
+                    public override bool Equals(object obj) => default;
+                    public bool Equals(ValueTask other) => default;
+                    public ValueTaskAwaiter GetAwaiter() => default;
+                    public override int GetHashCode() => default;
+                    public ValueTask Preserve() => default;
+
+                    public static bool operator ==(ValueTask left, ValueTask right) => default;
+                    public static bool operator !=(ValueTask left, ValueTask right) => default;
+                }
+
+                [AsyncMethodBuilder(typeof(AsyncValueTaskMethodBuilder<>))]
+                public readonly struct ValueTask<TResult> : IEquatable<ValueTask<TResult>>
+                {
+                    public ValueTask(TResult result) {}
+                    public ValueTask(Task<TResult> task) {}
+                    public ValueTask(IValueTaskSource<TResult> source, short token) {}
+
+                    public bool IsFaulted => default;
+                    public bool IsCompletedSuccessfully => default;
+                    public bool IsCompleted => default;
+                    public bool IsCanceled => default;
+                    public TResult Result => default;
+
+                    public Task<TResult> AsTask() => default;
+                    public ConfiguredValueTaskAwaitable<TResult> ConfigureAwait(bool continueOnCapturedContext) => default;
+
+                    public bool Equals(ValueTask<TResult> other) => default;
+                    public override bool Equals(object obj) => default;
+                    public ValueTaskAwaiter<TResult> GetAwaiter() => default;
+                    public override int GetHashCode() => default;
+                    public ValueTask<TResult> Preserve() => default;
+                    public override string ToString() => default;
+                    public static bool operator ==(ValueTask<TResult> left, ValueTask<TResult> right) => default;
+                    public static bool operator !=(ValueTask<TResult> left, ValueTask<TResult> right) => default;
+                }
+            }
+
+            namespace System.Collections.Generic
+            {
+                public interface IAsyncEnumerable<out T>
+                {
+                    IAsyncEnumerator<T> GetAsyncEnumerator();
+                }
+
+                public interface IAsyncEnumerator<out T> : IAsyncDisposable
+                {
+                    System.Threading.Tasks.ValueTask<bool> MoveNextAsync();
+                    T Current { get; }
+                }
+            }
+            """;
+
         [WpfFact]
         public async Task InsertForEachSnippetInMethodTest()
         {
@@ -425,6 +549,142 @@ static void Main(string[] args)
                     $$
                 }
                 """;
+
+            await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
+        }
+
+        [WpfTheory]
+        [InlineData("")]
+        [InlineData("async ")]
+        public async Task InsertForEachSnippetAfterSingleAwaitKeywordInMethodBodyTest(string asyncKeyword)
+        {
+            var markupBeforeCommit = $$"""
+                class C
+                {
+                    {{asyncKeyword}}void M()
+                    {
+                        await $$
+                    }
+                }
+                """ + IAsyncEnumerable;
+
+            var expectedCodeAfterCommit = $$"""
+                class C
+                {
+                    {{asyncKeyword}}void M()
+                    {
+                        await foreach (var item in collection)
+                        {
+                            $$
+                        }
+                    }
+                }
+                """ + IAsyncEnumerable;
+
+            await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
+        }
+
+        [WpfFact]
+        public async Task InsertForEachSnippetAfterSingleAwaitKeywordInGlobalStatementTest()
+        {
+            var markupBeforeCommit = """
+                await $$
+
+                """ + IAsyncEnumerable;
+
+            var expectedCodeAfterCommit = """
+                await foreach (var item in collection)
+                {
+                    $$
+                }
+
+                """ + IAsyncEnumerable;
+
+            await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
+        }
+
+        [WpfFact]
+        public async Task NoForEachStatementAfterAwaitKeywordWhenWontResultInStatementTest()
+        {
+            var markupBeforeCommit = """
+                var result = await $$
+                """ + IAsyncEnumerable;
+
+            await VerifyItemIsAbsentAsync(markupBeforeCommit, ItemToCommit);
+        }
+
+        [WpfTheory]
+        [InlineData("")]
+        [InlineData("async ")]
+        public async Task PreferAsyncEnumerableVariableInScopeForAwaitForEachTest(string asyncKeyword)
+        {
+            var markupBeforeCommit = $$"""
+                using System.Collections.Generic;
+
+                class C
+                {
+                    {{asyncKeyword}}void M()
+                    {
+                        IEnumerable<int> enumerable;
+                        IAsyncEnumerable<int> asyncEnumerable;
+
+                        await $$
+                    }
+                }
+                """ + IAsyncEnumerable;
+
+            var expectedCodeAfterCommit = $$"""
+                using System.Collections.Generic;
+                
+                class C
+                {
+                    {{asyncKeyword}}void M()
+                    {
+                        IEnumerable<int> enumerable;
+                        IAsyncEnumerable<int> asyncEnumerable;
+                
+                        await foreach (var item in asyncEnumerable)
+                        {
+                            $$
+                        }
+                    }
+                }
+                """ + IAsyncEnumerable;
+
+            await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
+        }
+
+        [WpfTheory]
+        [InlineData("")]
+        [InlineData("async ")]
+        public async Task InsertAwaitForEachSnippetForPostfixAsyncEnumerableTest(string asyncKeyword)
+        {
+            var markupBeforeCommit = $$"""
+                using System.Collections.Generic;
+
+                class C
+                {
+                    {{asyncKeyword}}void M(IAsyncEnumerable<int> asyncEnumerable)
+                    {
+                        asyncEnumerable.$$
+                    }
+                }
+                """ + IAsyncEnumerable;
+
+            var expectedCodeAfterCommit = $$"""
+                using System.Collections.Generic;
+                
+                class C
+                {
+                    {{asyncKeyword}}void M(IAsyncEnumerable<int> asyncEnumerable)
+                    {
+                        await foreach (var item in asyncEnumerable)
+                        {
+                            $$
+                        }
+                    }
+                }
+                """ + IAsyncEnumerable;
 
             await VerifyCustomCommitProviderAsync(markupBeforeCommit, ItemToCommit, expectedCodeAfterCommit);
         }
