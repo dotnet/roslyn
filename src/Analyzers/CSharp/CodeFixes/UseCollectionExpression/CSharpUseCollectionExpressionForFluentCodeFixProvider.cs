f8ccc0099cb9ff59a31cf501431e3613a6aeef52
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-08-18 08:29:43 -0700

    Add fixer

--- src/Analyzers/CSharp/CodeFixes/UseCollectionExpression/CSharpUseCollectionExpressionForFluentCodeFixProvider.cs
+++ src/Analyzers/CSharp/CodeFixes/UseCollectionExpression/CSharpUseCollectionExpressionForFluentCodeFixProvider.cs
@@ -13,19 +13,19 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.LanguageService;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
-using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis.UseCollectionInitializer;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp.UseCollectionExpression;
 
-using static SyntaxFactory;
 using static CSharpCollectionExpressionRewriter;
 using static CSharpUseCollectionExpressionForFluentDiagnosticAnalyzer;
+using static SyntaxFactory;
 
 [ExportCodeFixProvider(LanguageNames.CSharp, Name = PredefinedCodeFixProviderNames.UseCollectionExpressionForFluent), Shared]
 internal partial class CSharpUseCollectionExpressionForFluentCodeFixProvider
@@ -50,7 +50,6 @@ internal partial class CSharpUseCollectionExpressionForFluentCodeFixProvider
         CancellationToken cancellationToken)
     {
         var syntaxFacts = document.GetRequiredLanguageService<ISyntaxFactsService>();
-        var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
 
         var semanticModel = await document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
         var state = new UpdateExpressionState<ExpressionSyntax, StatementSyntax>(
@@ -69,7 +68,7 @@ internal partial class CSharpUseCollectionExpressionForFluentCodeFixProvider
         var semanticDocument = await SemanticDocument.CreateAsync(document, cancellationToken).ConfigureAwait(false);
 
         // Get the expressions that we're going to fill the new collection expression with.
-        var arguments = GetArguments(analysisResult.Matches);
+        var arguments = await GetArgumentsAsync(document, fallbackOptions, analysisResult.Matches, cancellationToken).ConfigureAwait(false);
 
         var dummyObjectAnnotation = new SyntaxAnnotation();
         var dummyObjectCreation = ImplicitObjectCreationExpression(ArgumentList(arguments), initializer: analysisResult.ExistingInitializer)
@@ -93,118 +92,64 @@ internal partial class CSharpUseCollectionExpressionForFluentCodeFixProvider
 
         editor.ReplaceNode(invocationExpression, collectionExpression);
 
-        static SeparatedSyntaxList<ArgumentSyntax> GetArguments(
-            SourceText text, ImmutableArray<CollectionExpressionMatch<ExpressionSyntax>> matches)
+        static async Task<SeparatedSyntaxList<ArgumentSyntax>> GetArgumentsAsync(
+            Document document,
+            CodeActionOptionsProvider fallbackOptions,
+            ImmutableArray<CollectionExpressionMatch<ArgumentSyntax>> matches,
+            CancellationToken cancellationToken)
         {
+            if (matches.IsEmpty)
+                return default;
+
+            var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+#if CODE_STYLE
+            var formattingOptions = SyntaxFormattingOptions.CommonDefaults;
+#else
+            var formattingOptions = await document.GetSyntaxFormattingOptionsAsync(
+                fallbackOptions, cancellationToken).ConfigureAwait(false);
+#endif
+
             using var _ = ArrayBuilder<SyntaxNodeOrToken>.GetInstance(out var nodesAndTokens);
 
-            var comma = Token(SyntaxKind.CommaToken).WithoutTrivia().WithTrailingTrivia(Space);
-            foreach (var (expression, _) in matches)
-            {
-                // After each expression, place a comma
-                if (nodesAndTokens.Count == 0)
-                    nodesAndTokens.Add(comma);
+            // Get the first argument.  If it was originally like `Add(arg)` then keep it in that form in `new(arg)`.
+            // However, if it was on it's own line originally, then preserve that in the new form as well.
+            AddOriginallyFirstArgument(matches[0].Node);
 
-                // If the original 
-                var firstToken = expression.GetFirstToken();
-                if (text.AreOnSameLine(firstToken.GetPreviousToken(), firstToken))
+            // Now go through and add the rest of the arguments.
+            for (int i = 1, n = matches.Length; i < n; i++)
+            {
+                var argument = matches[i].Node;
+                var argumentList = (ArgumentListSyntax)argument.GetRequiredParent();
+                var originalArgumentListChildren = argumentList.Arguments.GetWithSeparators();
+                var index = originalArgumentListChildren.IndexOf(argument);
+
+                // if this was not the first argument in its original list.  for example: `.Add(1, 2, 3)`, then add its
+                // preceding comma as well.  That way we preserve its original relationship in the rewritten code.
+                if (index > 0)
                 {
-                    nodesAndTokens.Add(Argument(expression));
+                    nodesAndTokens.Add(originalArgumentListChildren[index - 1]);
+                    nodesAndTokens.Add(argument);
                 }
                 else
                 {
-                    nodesAndTokens.Add(Argument(expression.WithPrependedLeadingTrivia(CarriageReturnLineFeed)));
+                    AddOriginallyFirstArgument(argument);
                 }
             }
 
             return SeparatedList<ArgumentSyntax>(nodesAndTokens);
+
+            void AddOriginallyFirstArgument(ArgumentSyntax firstArgument)
+            {
+                var firstToken = firstArgument.GetFirstToken();
+                if (text.AreOnSameLine(firstToken.GetPreviousToken(), firstToken))
+                {
+                    nodesAndTokens.Add(firstArgument);
+                }
+                else
+                {
+                    nodesAndTokens.Add(firstArgument.WithPrependedLeadingTrivia(EndOfLine(formattingOptions.NewLine)));
+                }
+            }
         }
-
-#if false
-
-        // Two major cases to handle.  First, the original expression was something like `new[] { 1, 2, 3
-        // }.Concat(...).ToArray()` where there was an existing initializer we want to add the new elements to:
-
-        if (analysisResult.ExistingInitializer != null)
-        {
-
-        }
-
-        // We want to replace the final invocation (`builder.ToImmutable()`) with `new()`.  That way we can call into
-        // the collection-rewriter to swap out that object-creation with the new collection-expression.
-
-        // First, mark all the nodes we care about, so we can find them once we do the replacement with the
-        // object-creation expression.
-        var dummyObjectAnnotation = new SyntaxAnnotation();
-        var newDocument = await CreateTrackedDocumentAsync(
-            document, analysisResult, dummyObjectAnnotation, cancellationToken).ConfigureAwait(false);
-
-        var root = await newDocument.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-        var dummyObjectCreation = (ImplicitObjectCreationExpressionSyntax)root.GetAnnotatedNodes(dummyObjectAnnotation).Single();
-
-        // Move the original match over to this rewritten tree.
-        analysisResult = TrackAnalysisResult(root, analysisResult);
-
-        // Get the new collection expression.
-        var collectionExpression = await CSharpCollectionExpressionRewriter.CreateCollectionExpressionAsync(
-            newDocument,
-            fallbackOptions,
-            dummyObjectCreation,
-            analysisResult.Matches.SelectAsArray(m => new CollectionExpressionMatch<StatementSyntax>(m.Statement, m.UseSpread)),
-            static o => o.Initializer,
-            static (o, i) => o.WithInitializer(i),
-            cancellationToken).ConfigureAwait(false);
-
-        var subEditor = new SyntaxEditor(root, document.Project.Solution.Services);
-
-        // Remove the actual declaration of the builder.
-        subEditor.RemoveNode(analysisResult.LocalDeclarationStatement);
-
-        // Remove all the nodes mutating the builder.
-        foreach (var (statement, _) in analysisResult.Matches)
-            subEditor.RemoveNode(statement);
-
-        // Finally, replace the invocation where we convert the builder to a collection with the new collection expression.
-        subEditor.ReplaceNode(dummyObjectCreation, collectionExpression);
-
-        editor.ReplaceNode(editor.OriginalRoot, subEditor.GetChangedRoot());
-
-        return;
-
-        // Move the nodes in analysisResult over to the tracked result in the root passed in.
-        static AnalysisResult TrackAnalysisResult(SyntaxNode root, AnalysisResult analysisResult)
-            => new(//analysisResult.DiagnosticLocation,
-                   analysisResult.ExistingInitializer is null ? null : root.GetCurrentNode(analysisResult.ExistingInitializer)!,
-                   root.GetCurrentNode(analysisResult.CreationExpression)!,
-                   analysisResult.Matches.SelectAsArray(m => new CollectionExpressionMatch<ExpressionSyntax>(root.GetCurrentNode(m.Node)!, m.UseSpread)));
-
-        // Creates a new document with all of the relevant nodes in analysisResult tracked so that we can find them
-        // across mutations we're making.
-        static async Task<Document> CreateTrackedDocumentAsync(
-            Document document,
-            AnalysisResult analysisResult,
-            SyntaxAnnotation annotation,
-            CancellationToken cancellationToken)
-        {
-            using var _ = ArrayBuilder<SyntaxNode>.GetInstance(out var nodesToTrack);
-
-            var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-
-            nodesToTrack.AddIfNotNull(analysisResult.ExistingInitializer);
-            nodesToTrack.Add(analysisResult.CreationExpression);
-            foreach (var (statement, _) in analysisResult.Matches)
-                nodesToTrack.Add(statement);
-
-            var newRoot = root.TrackNodes(nodesToTrack);
-            var creationExpression = newRoot.GetCurrentNode(analysisResult.CreationExpression)!;
-
-            var dummyObjectCreation = ImplicitObjectCreationExpression()
-                .WithTriviaFrom(creationExpression)
-                .WithAdditionalAnnotations(annotation);
-
-            var newDocument = document.WithSyntaxRoot(newRoot.ReplaceNode(creationExpression, dummyObjectCreation));
-            return newDocument;
-        }
-#endif
     }
 }
