a319ba956f2ee173dbe3ca489ec2570c0297ce1e
tmat <tomas.matousek@microsoft.com>
2023-08-23 08:50:01 -0700

    Move EditAndContinueTest to PDB utilities

--- src/Test/PdbUtilities/EditAndContinue/EditAndContinueTest.cs
+++ src/Test/PdbUtilities/EditAndContinue/EditAndContinueTest.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-extern alias PDB;
-
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -13,7 +11,6 @@ using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using Microsoft.CodeAnalysis.Emit;
 using Microsoft.CodeAnalysis.Test.Utilities;
-using PDB::Roslyn.Test.Utilities;
 using Roslyn.Test.Utilities;
 using Xunit;
 
