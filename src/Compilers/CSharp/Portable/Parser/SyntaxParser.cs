caec4f680917ae50a28467290f0cf8869091c00d
Todd Grunke <toddgrun@microsoft.com>
2023-08-24 15:08:33 -0700

    Remove extraneous whitespace that the ci build complained about

--- src/Compilers/CSharp/Portable/Parser/SyntaxParser.cs
+++ src/Compilers/CSharp/Portable/Parser/SyntaxParser.cs
@@ -37,7 +37,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax.InternalSyntax
 
         private static readonly ObjectPool<BlendedNode[]> s_blendedNodesPool = new ObjectPool<BlendedNode[]>(() => new BlendedNode[32], 2);
         private static readonly ObjectPool<ArrayElement<SyntaxToken>[]> s_lexedTokensPool = new ObjectPool<ArrayElement<SyntaxToken>[]>(() => new ArrayElement<SyntaxToken>[CachedTokenArraySize], 2);
-        
+
         // Array size held in token pool. This should be large enough to prevent most allocations, but
         //  not so large as to be wasteful when not in use.
         private const int CachedTokenArraySize = 4096;
