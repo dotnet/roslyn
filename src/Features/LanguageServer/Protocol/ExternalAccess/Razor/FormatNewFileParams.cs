c5a7ca749d35b624defa5f5c0aae9a7d32a1907f
David Wengier <david.wengier@microsoft.com>
2023-09-11 16:55:46 +1000

    Allow Razor to format a new document it wants to create

---
+++ src/Features/LanguageServer/Protocol/ExternalAccess/Razor/FormatNewFileParams.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.Serialization;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.ExternalAccess.Razor;
+
+[DataContract]
+internal record FormatNewFileParams
+{
+    [DataMember(Name = "document")]
+    public required TextDocumentIdentifier Document { get; set; }
+
+    [DataMember(Name = "project")]
+    public required TextDocumentIdentifier Project { get; set; }
+
+    [DataMember(Name = "contents")]
+    public required string Contents { get; set; }
+}
