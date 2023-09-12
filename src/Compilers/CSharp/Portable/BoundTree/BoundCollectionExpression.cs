e0a0737f31c9815b200f21a204f78ebff0de360d
Charles Stoner <10732005+cston@users.noreply.github.com>
2023-09-12 16:38:18 -0700

    Fix formatting

--- src/Compilers/CSharp/Portable/BoundTree/BoundCollectionExpression.cs
+++ src/Compilers/CSharp/Portable/BoundTree/BoundCollectionExpression.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Linq;
-
 namespace Microsoft.CodeAnalysis.CSharp
 {
     internal partial class BoundCollectionExpressionBase
@@ -17,7 +15,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 {
                     hasSpreadElements = true;
                     if (spreadElement.LengthOrCount is null)
-        {
+                    {
                         return null;
                     }
                 }
