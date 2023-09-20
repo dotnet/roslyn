3bb6760b8a05a9c8b181931c04fe4086e6afc946
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-09-19 17:28:33 -0700

    Lint

--- src/Analyzers/CSharp/Analyzers/UseObjectInitializer/CSharpUseNamedMemberInitializerAnalyzer.cs
+++ src/Analyzers/CSharp/Analyzers/UseObjectInitializer/CSharpUseNamedMemberInitializerAnalyzer.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp.UseCollectionInitializer;
 using Microsoft.CodeAnalysis.UseObjectInitializer;
@@ -19,6 +20,6 @@ internal sealed class CSharpUseNamedMemberInitializerAnalyzer :
         VariableDeclaratorSyntax,
         CSharpUseNamedMemberInitializerAnalyzer>
 {
-    protected override bool IsInitializerOfLocalDeclarationStatement(LocalDeclarationStatementSyntax localDeclarationStatement, BaseObjectCreationExpressionSyntax rootExpression, out VariableDeclaratorSyntax variableDeclarator)
+    protected override bool IsInitializerOfLocalDeclarationStatement(LocalDeclarationStatementSyntax localDeclarationStatement, BaseObjectCreationExpressionSyntax rootExpression, [NotNullWhen(true)] out VariableDeclaratorSyntax? variableDeclarator)
         => CSharpObjectCreationHelpers.IsInitializerOfLocalDeclarationStatement(localDeclarationStatement, rootExpression, out variableDeclarator);
 }
