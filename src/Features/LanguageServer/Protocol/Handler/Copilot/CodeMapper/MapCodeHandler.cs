58f9dd8d555df385b14ea86ecb7ae7f3c85b86f9
gel@microsoft.com <gel@microsoft.com>
2023-09-22 16:20:13 -0700

    empty impl of lsp codemapper

---
+++ src/Features/LanguageServer/Protocol/Handler/Copilot/CodeMapper/MapCodeHandler.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Roslyn.Utilities;
+using LSP = Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler;
+
+[ExportCSharpVisualBasicStatelessLspService(typeof(MapCodeHandler)), Shared]
+[Method(LSP.MapperMethods.TextDocumentMapCodeName)]
+internal sealed partial class MapCodeHandler : ILspServiceDocumentRequestHandler<MapCodeParams, LSP.WorkspaceEdit?>
+{
+    [ImportingConstructor]
+    [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+    public MapCodeHandler()
+    {
+    }
+
+    public bool MutatesSolutionState => false;
+    public bool RequiresLSPSolution => true;
+
+
+    public TextDocumentIdentifier GetTextDocumentIdentifier(MapCodeParams request)
+        => request.TextDocument;
+
+    public Task<WorkspaceEdit?> HandleRequestAsync(MapCodeParams request, RequestContext context, CancellationToken cancellationToken)
+    {
+        Contract.ThrowIfNull(context.Document);
+        Contract.ThrowIfNull(context.Solution);
+
+        return null;
+    }
+}
---
+++ src/Features/LanguageServer/Protocol/Handler/Copilot/CodeMapper/MapCodeParams.cs
@@ -0,0 +1,62 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.Serialization;
+
+namespace Microsoft.VisualStudio.LanguageServer.Protocol
+{
+    /// <summary>
+    /// LSP Params for textDocument/mapCode calls.
+    /// </summary>
+    [DataContract]
+    internal class MapCodeParams : ITextDocumentParams
+    {
+        /// <summary>
+        /// Identifier for the document the contents are supposed to be mapped into.
+        /// </summary>
+        [DataMember(Name = "textDocument")]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public TextDocumentIdentifier TextDocument
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Strings of code/text to map into TextDocument.
+        /// </summary>
+        [DataMember(Name = "contents")]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public string[] Contents
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Prioritized Locations to be used when applying heuristics. For example, cursor location,
+        /// related classes (in other documents), viewport, etc. Earlier items should be considered
+        /// higher priority.
+        /// </summary>
+        [DataMember(Name = "focusLocations")]
+        public Location[]? FocusLocations
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Changes that should be applied to the workspace by the mapper before performing
+        /// the mapping operation.
+        /// </summary>
+        [DataMember(Name = "updates")]
+        public WorkspaceEdit? Updates
+        {
+            get;
+            set;
+        }
+    }
+}
