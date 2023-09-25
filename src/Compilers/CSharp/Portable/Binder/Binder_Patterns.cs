4f8940fee43fe946e748d4a27d899025ac75f9de
Jan Jones <janjones@microsoft.com>
2023-09-25 12:45:46 +0200

    Handle constant `null`s in pointer pattern matching

--- src/Compilers/CSharp/Portable/Binder/Binder_Patterns.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Patterns.cs
@@ -1630,7 +1630,7 @@ namespace Microsoft.CodeAnalysis.CSharp
 
             BoundExpression value = BindExpressionForPattern(inputType, node.Expression, ref hasErrors, diagnostics, out var constantValueOpt, out _, out Conversion patternConversion);
             ExpressionSyntax innerExpression = SkipParensAndNullSuppressions(node.Expression, diagnostics, ref hasErrors);
-            RoslynDebug.Assert(value.Type is { });
+            var type = value.Type ?? inputType;
             BinaryOperatorKind operation = tokenKindToBinaryOperatorKind(node.OperatorToken.Kind());
             if (operation == BinaryOperatorKind.Equal)
             {
@@ -1638,7 +1638,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 hasErrors = true;
             }
 
-            BinaryOperatorKind opType = RelationalOperatorType(value.Type.EnumUnderlyingTypeOrSelf());
+            BinaryOperatorKind opType = RelationalOperatorType(type.EnumUnderlyingTypeOrSelf());
             switch (opType)
             {
                 case BinaryOperatorKind.Float:
@@ -1654,7 +1654,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 case BinaryOperatorKind.Error:
                     if (!hasErrors)
                     {
-                        diagnostics.Add(ErrorCode.ERR_UnsupportedTypeForRelationalPattern, node.Location, value.Type.ToDisplayString());
+                        diagnostics.Add(ErrorCode.ERR_UnsupportedTypeForRelationalPattern, node.Location, type.ToDisplayString());
                         hasErrors = true;
                     }
                     break;
@@ -1673,7 +1673,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 hasErrors = true;
             }
 
-            return new BoundRelationalPattern(node, operation | opType, value, constantValueOpt, inputType, value.Type, hasErrors);
+            return new BoundRelationalPattern(node, operation | opType, value, constantValueOpt, inputType, type, hasErrors);
 
             static BinaryOperatorKind tokenKindToBinaryOperatorKind(SyntaxKind kind) => kind switch
             {
