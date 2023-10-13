68398ccd397de5d8c2348230780348be67fdd709
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-10-13 12:28:30 -0700

    In progress

--- src/Features/CSharp/Portable/ConvertPrimaryToRegularConstructor/ConvertPrimaryToRegularConstructorCodeRefactoringProvider.cs
+++ src/Features/CSharp/Portable/ConvertPrimaryToRegularConstructor/ConvertPrimaryToRegularConstructorCodeRefactoringProvider.cs
@@ -24,8 +24,14 @@ using System.Collections.Generic;
 using Microsoft.CodeAnalysis.FindSymbols;
 using System.Linq;
 using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CodeGeneration;
+using Microsoft.CodeAnalysis.Diagnostics.Analyzers.NamingStyles;
+using Microsoft.CodeAnalysis.Shared.Utilities;
+using Microsoft.CodeAnalysis.CSharp.CodeGeneration;
 
 namespace Microsoft.CodeAnalysis.CSharp.ConvertPrimaryToRegularConstructor;
+
+using static SyntaxFactory;
 
 [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = PredefinedCodeRefactoringProviderNames.ConvertPrimaryToRegularConstructor), Shared]
 [method: ImportingConstructor]
@@ -52,12 +58,12 @@ internal sealed class ConvertPrimaryToRegularConstructorCodeRefactoringProvider(
 
         context.RegisterRefactoring(CodeAction.Create(
             CSharpFeaturesResources.Convert_to_regular_constructor,
-            cancellationToken => ConvertAsync(document, typeDeclaration, typeDeclaration.ParameterList, cancellationToken),
+            cancellationToken => ConvertAsync(document, typeDeclaration, typeDeclaration.ParameterList, context.Options, cancellationToken),
             nameof(CSharpFeaturesResources.Convert_to_regular_constructor)));
     }
 
     private static async Task<Solution> ConvertAsync(
-        Document document, TypeDeclarationSyntax typeDeclaration, ParameterListSyntax parameterList, CancellationToken cancellationToken)
+        Document document, TypeDeclarationSyntax typeDeclaration, ParameterListSyntax parameterList, CodeActionOptionsProvider options, CancellationToken cancellationToken)
     {
         // 1. Create constructor
         // 2. Remove base arguments
@@ -72,10 +78,26 @@ internal sealed class ConvertPrimaryToRegularConstructorCodeRefactoringProvider(
         var solution = document.Project.Solution;
         var solutionEditor = new SolutionEditor(solution);
 
+        var syntaxGenerator = CSharpSyntaxGenerator.Instance;
         var parameters = parameterList.Parameters.SelectAsArray(p => semanticModel.GetRequiredDeclaredSymbol(p, cancellationToken));
 
         var parameterToNonDocCommentLocations = await GetParameterLocationsAsync(includeDocCommentLocations: false).ConfigureAwait(false);
-        var synthesizedFields = GetSynthesizedFields();
+        var parameterToSynthesizedFields = await GetSynthesizedFieldsAsync().ConfigureAwait(false);
+
+        var baseType = typeDeclaration.BaseList?.Types is [PrimaryConstructorBaseTypeSyntax type, ..] ? type : null;
+        var methodTargetingAttributes = typeDeclaration.AttributeLists.Where(list => list.Target?.Identifier.ValueText == "method");
+        var constructorDeclaration = CreateConstructorDeclaration();
+
+        // Now start editing the document
+        var mainDocumentEditor = await solutionEditor.GetDocumentEditorAsync(document.Id, cancellationToken).ConfigureAwait(false);
+
+        mainDocumentEditor.RemoveNode(parameterList);
+        if (baseType != null)
+            mainDocumentEditor.ReplaceNode(baseType, SimpleBaseType(baseType.Type).WithTriviaFrom(baseType));
+
+        foreach (var attributeList in methodTargetingAttributes)
+            mainDocumentEditor.RemoveNode(attributeList);
+
 
         return solutionEditor.GetChangedSolution();
 
@@ -142,26 +164,83 @@ internal sealed class ConvertPrimaryToRegularConstructorCodeRefactoringProvider(
             }
         }
 
-        ImmutableDictionary<IParameterSymbol, IFieldSymbol> GetSynthesizedFields()
+        async Task<ImmutableDictionary<IParameterSymbol, IFieldSymbol>> GetSynthesizedFieldsAsync()
         {
             using var _ = PooledDictionary<IParameterSymbol, IFieldSymbol>.GetInstance(out var result);
 
             foreach (var parameter in parameters)
             {
-                var referencedLocations = parameterToNonDocCommentLocations[parameter];
-                if (referencedLocations.Count == 0)
+                var existingField = namedType.GetMembers().OfType<IFieldSymbol>().FirstOrDefault(
+                    f => f.IsImplicitlyDeclared && parameter.Locations.Contains(f.Locations.FirstOrDefault()!));
+                if (existingField == null)
                     continue;
 
-                // if the parameter is only referenced in a single location, and that location is initializing a
-                // field/property already, we don't need to create a field for it.
-                if (referencedLocations.Count == 1 && referencedLocations.Single().assignedFieldOrProperty != null)
-                    continue;
+                var synthesizedField = CodeGenerationSymbolFactory.CreateFieldSymbol(
+                    existingField,
+                    name: await MakeFieldNameAsync(parameter.Name).ConfigureAwait(false));
 
-                // it was referenced outside of a field/prop initializer.  Need to synthesize a field for it.
+                result.Add(parameter, synthesizedField);
+                //var referencedLocations = parameterToNonDocCommentLocations[parameter];
+                //if (referencedLocations.Count == 0)
+                //    continue;
+
+                //// if the parameter is only referenced in a single location, and that location is initializing a
+                //// field/property already, we don't need to create a field for it.
+                //if (referencedLocations.Count == 1 && referencedLocations.Single().assignedFieldOrProperty != null)
+                //    continue;
+
+                //// it was referenced outside of a field/prop initializer.  Need to synthesize a field for it.
+                //result.Add(
+                //    parameter,
+                //    CodeGenerationSymbolFactory.CreateFieldSymbol(
+                //        )
 
             }
 
             return result.ToImmutableDictionary();
+        }
+
+        async Task<string> MakeFieldNameAsync(string parameterName)
+        {
+            var rule = await document.GetApplicableNamingRuleAsync(
+                new SymbolSpecification.SymbolKindOrTypeKind(SymbolKind.Field),
+                DeclarationModifiers.None,
+                Accessibility.Private,
+                options,
+                cancellationToken).ConfigureAwait(false);
+
+            var fieldName = rule.NamingStyle.MakeCompliant(parameterName).First();
+            return NameGenerator.GenerateUniqueName(fieldName, n => namedType.Name != n && !namedType.GetMembers(n).Any());
+        }
+
+        ConstructorDeclarationSyntax CreateConstructorDeclaration()
+        {
+            var attributes = List(methodTargetingAttributes);
+            var modifiers = typeDeclaration.Modifiers
+                .Where(m => SyntaxFacts.IsAccessibilityModifier(m.Kind()))
+                .Select(m => m.WithoutTrivia().WithAppendedTrailingTrivia(Space));
+
+            using var _ = ArrayBuilder<StatementSyntax>.GetInstance(out var assignmentStatements);
+            foreach (var parameter in parameters)
+            {
+                if (!parameterToSynthesizedFields.TryGetValue(parameter, out var field))
+                    continue;
+
+                var fieldName = field.Name.ToIdentifierName();
+                var left = parameter.Name == field.Name
+                    ? MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, ThisExpression(), fieldName)
+                    : (ExpressionSyntax)fieldName;
+                var assignment = AssignmentExpression(SyntaxKind.SimpleAssignmentExpression, left, parameter.Name.ToIdentifierName());
+                assignmentStatements.Add(ExpressionStatement(assignment));
+            }
+
+            return ConstructorDeclaration(
+                attributes,
+                TokenList(modifiers),
+                typeDeclaration.Identifier.WithoutTrivia().WithAppendedTrailingTrivia(Space),
+                parameterList.WithoutTrivia(),
+                baseType?.ArgumentList is null ? null : ConstructorInitializer(SyntaxKind.BaseConstructorInitializer, baseType.ArgumentList),
+                Block(assignmentStatements));
         }
     }
 }
