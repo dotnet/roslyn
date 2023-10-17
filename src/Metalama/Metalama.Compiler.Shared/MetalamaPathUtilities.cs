ae38753c9d5a2230de81e4fe25fbb674d8faebaf
Antonín Procházka <antonin@postsharp.net>
2023-10-17 18:58:32 +0200

    #33907 MetalamaPathUtilities modified.

--- src/Metalama/Metalama.Compiler.Shared/MetalamaPathUtilities.cs
+++ src/Metalama/Metalama.Compiler.Shared/MetalamaPathUtilities.cs
@@ -1,25 +1,25 @@
-﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+﻿// This is a copy of Metalama.Backstage.Utilities.MetalamaPathUtilities.
 
 using System;
 using System.IO;
 
 namespace Metalama.Compiler;
 
-public static class MetalamaPathUtilities
+internal static class MetalamaPathUtilities
 {
-    private static readonly string? _overriddenTempPath;
+    private static readonly string? s_overriddenTempPath;
 
     static MetalamaPathUtilities()
     {
-        var overriddenTempPath = Environment.GetEnvironmentVariable( "METALAMA_TEMP" );
-        _overriddenTempPath = string.IsNullOrEmpty( overriddenTempPath ) ? null : overriddenTempPath;
+        var overriddenTempPath = Environment.GetEnvironmentVariable("METALAMA_TEMP");
+        s_overriddenTempPath = string.IsNullOrEmpty(overriddenTempPath) ? null : overriddenTempPath;
     }
 
-    public static string GetTempPath() => _overriddenTempPath ?? Path.GetTempPath();
+    public static string GetTempPath() => s_overriddenTempPath ?? Path.GetTempPath();
 
     public static string GetTempFileName()
     {
-        if ( _overriddenTempPath == null )
+        if (s_overriddenTempPath == null)
         {
             return Path.GetTempFileName();
         }
@@ -27,24 +27,18 @@ public static class MetalamaPathUtilities
         // https://stackoverflow.com/a/10152460/4100001
         var attempt = 0;
 
-        while ( true )
+        while (true)
         {
-            var path = Path.Combine( _overriddenTempPath, $"{Guid.NewGuid()}.tmp" );
+            var path = Path.Combine(s_overriddenTempPath, $"{Guid.NewGuid()}.tmp");
 
             try
             {
-                using ( var newFile = new FileStream( path, FileMode.Create ) )
+                using (var newFile = new FileStream(path, FileMode.Create))
                 {
                     newFile.Close();
                 }
             }
-            catch ( Exception e )
-            {
-                if ( ++attempt == 10 )
-                {
-                    throw new IOException( "No unique temporary file name is available.", e );
-                }
-            }
+            catch (IOException) when (++attempt < 10) { continue; }
 
             return path;
         }
