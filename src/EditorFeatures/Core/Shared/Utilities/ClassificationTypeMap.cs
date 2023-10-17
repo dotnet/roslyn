d1923dd72801a2163f6d6a031e783230943b8e2b
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-10-17 14:19:03 -0700

    Fix classification layering

--- src/EditorFeatures/Core/Shared/Utilities/ClassificationTypeMap.cs
+++ src/EditorFeatures/Core/Shared/Utilities/ClassificationTypeMap.cs
@@ -7,13 +7,16 @@ using System.ComponentModel.Composition;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.VisualStudio.Text.Classification;
 
-namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities
+namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities;
+
+/// <summary>
+/// Note: we very intentionally place ourselves in the editors 'syntactic' bucket (and not 'semantic' bucket).  See
+/// comments on <see cref="AbstractClassificationTypeMap"/> for more details.
+/// </summary>
+[Export]
+[method: ImportingConstructor]
+[method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+internal sealed class ClassificationTypeMap(IClassificationTypeRegistryService registryService)
+    : AbstractClassificationTypeMap(registryService, ClassificationLayer.Syntactic)
 {
-    [Export]
-    [method: ImportingConstructor]
-    [method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-    internal class ClassificationTypeMap(
-        IClassificationTypeRegistryService registryService) : AbstractClassificationTypeMap(registryService, ClassificationLayer.Default)
-    {
-    }
 }
