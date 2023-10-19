56373ec24d1949e7912d8deb17d0dfd62d94a5dd
DoctorKrolic <70431552+DoctorKrolic@users.noreply.github.com>
2023-10-20 01:00:10 +0300

    Fix VB comment
    Co-authored-by: Fred Silberberg <fred@silberberg.xyz>

--- src/Compilers/VisualBasic/Portable/Lowering/LocalRewriter/LocalRewriter_ConditionalExpressions.vb
+++ src/Compilers/VisualBasic/Portable/Lowering/LocalRewriter/LocalRewriter_ConditionalExpressions.vb
@@ -242,7 +242,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
                     End If
                 End If
 
-                ' Optimize left ?? right to left.GetValueOrDefault(right) when left is T? and right is a side-effectless expression of type T
+                ' Optimize If(left, right) to left.GetValueOrDefault(right) when left is T? and right is a side-effectless expression of type T
                 If rewrittenRight.ConstantValueOpt IsNot Nothing OrElse
                    Not If(TryCast(rewrittenRight, BoundLocal)?.LocalSymbol.IsRef, True) OrElse
                    Not If(TryCast(rewrittenRight, BoundParameter)?.ParameterSymbol.IsByRef, True) Then
