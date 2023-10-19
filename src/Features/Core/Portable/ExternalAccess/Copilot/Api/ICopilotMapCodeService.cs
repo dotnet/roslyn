79cf4ed15506fa988d22c3cf201d756e3dad928c
gel@microsoft.com <gel@microsoft.com>
2023-10-19 15:55:51 -0700

    Add restricted IVT to Coplit Chat for code mapper service API

---
+++ src/Features/Core/Portable/ExternalAccess/Copilot/Api/ICopilotMapCodeService.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Copilot.Api
+{
+    internal interface IMapCodeService : ILanguageService
+    {
+        Task<Document> MapCodeAsync(
+            Document document,
+            ImmutableArray<string> contents,
+            ImmutableArray<DocumentSpan> focusLocations,
+            bool formatMappedCode,
+            CancellationToken cancellationToken);
+    }
+}
