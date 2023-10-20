43ea3e5e6c27362221dc5b5398cffa6d5e4b019e
Julien Couvreur <julien.couvreur@gmail.com>
2023-10-20 12:06:03 -0700

    Avoid using AttributeData in PE symbols

--- src/Compilers/VisualBasic/Portable/Symbols/Metadata/PE/PEModuleSymbol.vb
+++ src/Compilers/VisualBasic/Portable/Symbols/Metadata/PE/PEModuleSymbol.vb
@@ -513,22 +513,13 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Symbols.Metadata.PE
         Friend Overrides ReadOnly Property ObsoleteAttributeData As ObsoleteAttributeData
             Get
                 If _lazyObsoleteAttributeData Is ObsoleteAttributeData.Uninitialized Then
-                    Interlocked.CompareExchange(_lazyObsoleteAttributeData, ComputeObsoleteAttributeData(), ObsoleteAttributeData.Uninitialized)
+                    Dim experimentalData = _module.TryDecodeExperimentalAttributeData(EntityHandle.ModuleDefinition, New MetadataDecoder(Me))
+                    Interlocked.CompareExchange(_lazyObsoleteAttributeData, experimentalData, ObsoleteAttributeData.Uninitialized)
                 End If
 
                 Return _lazyObsoleteAttributeData
             End Get
         End Property
-
-        Private Function ComputeObsoleteAttributeData() As ObsoleteAttributeData
-            For Each attrData In GetAttributes()
-                If attrData.IsTargetAttribute(Me, AttributeDescription.ExperimentalAttribute) Then
-                    Return attrData.DecodeExperimentalAttribute()
-                End If
-            Next
-
-            Return Nothing
-        End Function
 
     End Class
 End Namespace
