366ed650556f4a6017eb6028de6b466a8564d4da
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-10-23 11:02:03 -0700

    in progress

--- src/EditorFeatures/Core/Classification/TotalClassificationTaggerProvider.cs
+++ src/EditorFeatures/Core/Classification/TotalClassificationTaggerProvider.cs
@@ -85,6 +85,7 @@ internal sealed class TotalClassificationTaggerProvider : IViewTaggerProvider
             // the syntactic state, and we do not want things like semantic classifications showing up over that.
 
             var totalTags = new SegmentedList<ITagSpan<IClassificationTag>>();
+            var embeddedClassificationTree = 
 
             var syntacticSpans = syntacticTagger.GetTags(spans).GetEnumerator();
             var semanticSpans = semanticTagger.GetTags(spans).GetEnumerator();
@@ -102,58 +103,94 @@ internal sealed class TotalClassificationTaggerProvider : IViewTaggerProvider
                 {
                     totalTags.Add(currentSemantic);
                     currentSemantic = NextOrNull(semanticSpans);
-                    continue;
                 }
-
-                if (currentSyntactic.Tag.ClassificationType.Classification is ClassificationTypeNames.StringLiteral or ClassificationTypeNames.VerbatimStringLiteral)
+                else
                 {
+                    // We're on a syntactic span before the next semantic onde.
+
                     // If we have a string literal of some sort, see if there are embedded classifications within it.
-                    var embeddedClassifications = embeddedTagger.GetTags(new NormalizedSnapshotSpanCollection(currentSyntactic.Span));
-                    MergeEmbeddedClassifications(currentSyntactic, embeddedClassifications, totalTags);
+                    if (TryProcessSyntacticStringLiteral())
+                        continue;
+
+                    // Otherwise, we've got a syntactic span starting before a semantic one.  If it's a comment or excluded
+                    // code, then we want to ignore every semantic classification that potentially overlaps with it so that
+                    // semantic classifications don't show up *on top of* them.  We want commenting out code to feel like'
+                    // it instantly snaps to that state.
+                    if (TryProcessCommentOrExcludedCode())
+                        continue;
+
+                    // Normal case.  Just add the syntactic span and continue.
+                    totalTags.Add(currentSyntactic);
                     currentSyntactic = NextOrNull(syntacticSpans);
-                    continue;
                 }
-
-                // Otherwise, we've got a syntactic span starting before a semantic one.  If it's a comment or excluded
-                // code, then we want to ignore every semantic classification that potentially overlaps with it so that
-                // semantic classifications don't show up *on top of* them.  We want commenting out code to feel like'
-                // it instantly snaps to that state.
-                if (currentSyntactic.Tag.ClassificationType.Classification is ClassificationTypeNames.Comment or ClassificationTypeNames.ExcludedCode)
-                {
-                    // Keep skipping semantic tags that overlaps with this syntactic tag.
-                    while (currentSemantic != null && currentSemantic.Span.OverlapsWith(currentSyntactic.Span.Span))
-                        currentSemantic = NextOrNull(semanticSpans);
-                }
-
-                // now add that syntactic span.
-                totalTags.Add(currentSyntactic);
-                currentSyntactic = NextOrNull(syntacticSpans);
             }
 
+            // Add any remaining semantic spans following the syntactic ones.
             while (currentSemantic != null)
             {
                 totalTags.Add(currentSemantic);
                 currentSemantic = NextOrNull(semanticSpans);
-                continue;
             }
 
+            // Add any remaining syntacticspans following the semantic ones.
             while (currentSyntactic != null)
             {
+                // don't have to worry about comments/excluded code since there are no semantic tags we want to override.
 
+                if (TryProcessSyntacticStringLiteral())
+                    continue;
+
+                totalTags.Add(currentSyntactic);
+                currentSyntactic = NextOrNull(syntacticSpans);
             }
 
-            //while (latest != null)
-            //{
-            //    added.Add(latest.Span);
-            //    latest = NextOrNull(latestEnumerator);
-            //}
+            return totalTags;
 
-            //while (previous != null)
-            //{
-            //    removed.Add(previous.Span);
-            //    previous = NextOrNull(previousEnumerator);
-            //}
+            bool TryProcessSyntacticStringLiteral()
+            {
+                if (currentSyntactic.Tag.ClassificationType.Classification is not ClassificationTypeNames.StringLiteral and not ClassificationTypeNames.VerbatimStringLiteral)
+                    return false;
+
+                if (!TryMergeEmbeddedClassificationsWithStringLiteral())
+                    return false;
+
+                currentSyntactic = NextOrNull(syntacticSpans);
+                return true;
+            }
+
+            bool TryProcessCommentOrExcludedCode()
+            {
+                if (currentSyntactic.Tag.ClassificationType.Classification is not ClassificationTypeNames.Comment and not ClassificationTypeNames.ExcludedCode)
+                    return false;
+
+                // Keep skipping semantic tags that overlaps with this syntactic tag.
+                while (currentSemantic != null && currentSemantic.Span.OverlapsWith(currentSyntactic.Span.Span))
+                    currentSemantic = NextOrNull(semanticSpans);
+
+                // now add that syntactic span.
+                totalTags.Add(currentSyntactic);
+                currentSyntactic = NextOrNull(syntacticSpans);
+
+                return true;
+            }
+
+            bool TryMergeEmbeddedClassificationsWithStringLiteral()
+            {
+                var embeddedClassifications = embeddedTagger.GetTags(new NormalizedSnapshotSpanCollection(currentSyntactic.Span));
+
+                // If we had no embedded classifications, there's nothing we need to do.  Just process this string literal normally.
+                if (embeddedClassifications.Count == 0)
+                    return false;
+
+
+            }
         }
+
+        private static ITagSpan<IClassificationTag>? NextOrNull(IEnumerator<ITagSpan<IClassificationTag>> enumerator)
+            => enumerator.MoveNext() ? enumerator.Current : null;
+
+        private static ITagSpan<IClassificationTag>? NextOrNull(SegmentedList<ITagSpan<IClassificationTag>>.Enumerator enumerator)
+            => enumerator.MoveNext() ? enumerator.Current : null;
 
         //private readonly struct TagSpanIntrospector : IIntervalIntrospector<ITagSpan<TTag>>
         //{
@@ -170,10 +207,4 @@ internal sealed class TotalClassificationTaggerProvider : IViewTaggerProvider
         //        => value.Span.Length;
         //}
     }
-
-    private static ITagSpan<IClassificationTag>? NextOrNull(IEnumerator<ITagSpan<IClassificationTag>> enumerator)
-        => enumerator.MoveNext() ? enumerator.Current : null;
-
-    private static ITagSpan<IClassificationTag>? NextOrNull(SegmentedList<ITagSpan<IClassificationTag>>.Enumerator enumerator)
-        => enumerator.MoveNext() ? enumerator.Current : null;
 }
