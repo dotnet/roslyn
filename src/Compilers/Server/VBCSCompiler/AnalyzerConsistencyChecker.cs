9a0356df4cc589f996fc91d9a3e50867575470c9
Jared Parsons <jared@paranoidcoding.org>
2023-10-23 08:25:45 -0700

    more

--- src/Compilers/Server/VBCSCompiler/AnalyzerConsistencyChecker.cs
+++ src/Compilers/Server/VBCSCompiler/AnalyzerConsistencyChecker.cs
@@ -15,6 +15,29 @@ using Microsoft.CodeAnalysis.VisualBasic;
 
 namespace Microsoft.CodeAnalysis.CompilerServer
 {
+    /// <summary>
+    /// The compiler server is a long lived process and loads analyzers from a series of build 
+    /// commands. This type is responsible for ensuring that analyzers loaded into the server 
+    /// match, as closely as possible, what would be loaded from a single invocation of csc / vbc.
+    ///
+    /// There are a few type of events that can lead to incorrect <see cref="Assembly"/> loads:
+    ///
+    ///  1. <see cref="AppDomain"/> pollution: On .NET Framework all analyzers are loaded into the same 
+    ///     <see cref="AppDomain"/> instance. When analyzers have dependencies at different versions 
+    ///     that can lead to them binding to different dependencies than they would through a single 
+    ///     invocation of csc.
+    ///  2. File system changes: The implementations of <see cref="IAnalyzerAssemblyLoader"/> assume 
+    ///     that the file system is unchanged during build. If the file system does change (say if 
+    ///     an analyzer is rebuilt) then the new instances need to be loaded not the previous ones.
+    ///
+    /// When these type of events happen the consistency checker should fail.
+    /// </summary>
+    /// <remarks>
+    /// The fact that <see cref="IAnalyzerAssemblyLoader"/> believe the file system is unchanging
+    /// makes sense for environments like IDEs but makes little sense for a build server. A  possible
+    /// future improvement is reworking the server implementation to be resilient to file system
+    /// changing events. At least on .NET Core that could lead to less server restarts.
+    /// </remarks>
     internal static class AnalyzerConsistencyChecker
     {
         public static bool Check(
