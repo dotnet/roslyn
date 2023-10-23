e5fdee1b577d2d5f890c17e1ebf0e532dd000989
Sam Harwell <Sam.Harwell@microsoft.com>
2023-10-23 14:29:12 -0500

    Remove conflicting key binding for integration tests
    Closes #70511

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/AbstractIntegrationTest.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/AbstractIntegrationTest.cs
@@ -28,6 +28,7 @@ namespace Roslyn.VisualStudio.IntegrationTests
                 await TestServices.SolutionExplorer.CloseSolutionAsync(HangMitigatingCancellationToken);
             }
 
+            await TestServices.Workarounds.RemoveConflictingKeyBindingsAsync(HangMitigatingCancellationToken);
             await TestServices.StateReset.ResetGlobalOptionsAsync(HangMitigatingCancellationToken);
             await TestServices.StateReset.ResetHostSettingsAsync(HangMitigatingCancellationToken);
         }
--- src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/WorkaroundsInProcess.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/WorkaroundsInProcess.cs
@@ -2,16 +2,37 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Shared.TestHooks;
 using Microsoft.VisualStudio.Extensibility.Testing;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Roslyn.VisualStudio.IntegrationTests.InProcess
 {
     [TestService]
     internal partial class WorkaroundsInProcess
     {
+        public async Task RemoveConflictingKeyBindingsAsync(CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            var dte = await GetRequiredGlobalServiceAsync<SDTE, EnvDTE80.DTE2>(cancellationToken);
+            EnvDTE.Command command;
+            try
+            {
+                command = dte.Commands.Item("Edit.IntelliCode.APIUsageExamples");
+            }
+            catch
+            {
+                // Ignore if the command doesn't exist
+                return;
+            }
+
+            command.Bindings = Array.Empty<object>();
+        }
+
         public async Task WaitForNavigationAsync(CancellationToken cancellationToken)
         {
             await TestServices.Workspace.WaitForAllAsyncOperationsAsync(new[] { FeatureAttribute.Workspace, FeatureAttribute.NavigateTo }, cancellationToken);
@@ -20,7 +41,7 @@ namespace Roslyn.VisualStudio.IntegrationTests.InProcess
             // It's not clear why this delay is necessary. Navigation operations are expected to fully complete as part
             // of one of the above waiters, but GetActiveWindowCaptionAsync appears to return the previous window
             // caption for a short delay after the above complete.
-            await Task.Delay(2000);
+            await Task.Delay(2000, cancellationToken);
         }
 
         /// <summary>
