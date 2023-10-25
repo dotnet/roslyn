e38ba8cbe74a85a31e5648280c51486765f60623
Jan Jones <janjones@microsoft.com>
2023-10-25 12:34:05 +0300

    Extend test

--- src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenAsyncEHTests.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenAsyncEHTests.cs
@@ -2268,54 +2268,72 @@ class Driver
             CompileAndVerify(source, options: TestOptions.ReleaseExe, expectedOutput: expectedOutput).VerifyDiagnostics();
         }
 
-        [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/70483")]
-        public void NestedCatch_DuplicateLocal_NoAwaitInNestedCatch()
+        [Theory, CombinatorialData, WorkItem("https://github.com/dotnet/roslyn/issues/70483")]
+        public void NestedCatch_DuplicateLocal_NoAwaitInNestedCatch(bool awaitInTry2)
         {
-            var source = """
+            var source = $$"""
                 using System;
                 using System.Threading.Tasks;
-                static class Program
+                using static System.Console;
+                
+                class C
                 {
                     static async Task Main()
                     {
+                        await new C().M1(catchFirst: false);
+                        WriteLine("--- catch first ---");
+                        await new C().M1(catchFirst: true);
+                    }
+                
+                    bool F(string caller, bool result)
+                    {
+                        WriteLine($"F: {caller}");
+                        return result;
+                    }
+                
+                    async Task M1(bool catchFirst)
+                    {
                         try
                         {
-                            await Task.Yield();
+                            throw new Exception("M1");
                         }
-                        catch (Exception ex) when (true)
+                        catch (Exception ex) when (F("M1-catch1", catchFirst))
                         {
-                            await Task.Yield();
+                            await M2("M1-catch1", ex);
                         }
-                        catch (Exception ex) when (true)
+                        catch (Exception ex) when (F("M1-catch2", true))
                         {
                             try
                             {
-                                await Task.Yield();
+                                {{(awaitInTry2 ? "await Task.Yield();" : "")}}
+                                throw new Exception("M1-catch2");
                             }
-                            catch
+                            catch 
                             {
-                                Console.Write(ex);
+                                _ = M2("M1-catch2-catch", ex);
                             }
                         }
+                    }
+                
+                    async Task M2(string caller, Exception ex)
+                    {
+                        WriteLine($"M2: {caller} {ex.Message}");
+                        await Task.Yield();
                     }
                 }
                 """;
 
-            var expectedDiagnostics = new[]
-            {
-                // (11,26): warning CS0168: The variable 'ex' is declared but never used
-                //         catch (Exception ex) when (true)
-                Diagnostic(ErrorCode.WRN_UnreferencedVar, "ex").WithArguments("ex").WithLocation(11, 26),
-                // (11,36): warning CS7095: Filter expression is a constant 'true', consider removing the filter
-                //         catch (Exception ex) when (true)
-                Diagnostic(ErrorCode.WRN_FilterIsConstantTrue, "true").WithLocation(11, 36),
-                // (15,36): warning CS7095: Filter expression is a constant 'true', consider removing the filter
-                //         catch (Exception ex) when (true)
-                Diagnostic(ErrorCode.WRN_FilterIsConstantTrue, "true").WithLocation(15, 36)
-            };
+            var expectedOutput = """
+                F: M1-catch1
+                F: M1-catch2
+                M2: M1-catch2-catch M1
+                --- catch first ---
+                F: M1-catch1
+                M2: M1-catch1 M1
+                """;
 
-            CompileAndVerify(source, options: TestOptions.DebugExe).VerifyDiagnostics(expectedDiagnostics);
-            CompileAndVerify(source, options: TestOptions.ReleaseExe).VerifyDiagnostics(expectedDiagnostics);
+            CompileAndVerify(source, options: TestOptions.DebugExe, expectedOutput: expectedOutput).VerifyDiagnostics();
+            CompileAndVerify(source, options: TestOptions.ReleaseExe, expectedOutput: expectedOutput).VerifyDiagnostics();
         }
     }
 }
