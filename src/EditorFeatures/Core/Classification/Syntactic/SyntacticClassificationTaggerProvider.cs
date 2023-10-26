e292cee1dbc403776f7c1b79dce6ee1b05387dad
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-10-26 13:57:28 -0700

    Remove usage of CWT in syntactic classifier

--- src/EditorFeatures/Core/Classification/Syntactic/SyntacticClassificationTaggerProvider.cs
+++ src/EditorFeatures/Core/Classification/Syntactic/SyntacticClassificationTaggerProvider.cs
@@ -17,52 +17,39 @@ using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Utilities;
 
-namespace Microsoft.CodeAnalysis.Classification
+namespace Microsoft.CodeAnalysis.Classification;
+
+[Export(typeof(ITaggerProvider))]
+[ContentType(ContentTypeNames.RoslynContentType)]
+[TextViewRole(PredefinedTextViewRoles.Document)]
+[TagType(typeof(IClassificationTag))]
+[method: ImportingConstructor]
+[method: SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
+internal sealed partial class SyntacticClassificationTaggerProvider(
+    IThreadingContext threadingContext,
+    SyntacticClassificationTypeMap typeMap,
+    IGlobalOptionService globalOptions,
+    IAsynchronousOperationListenerProvider listenerProvider) : ITaggerProvider
 {
-    [Export(typeof(ITaggerProvider))]
-    [ContentType(ContentTypeNames.RoslynContentType)]
-    [TextViewRole(PredefinedTextViewRoles.Document)]
-    [TagType(typeof(IClassificationTag))]
-    [method: ImportingConstructor]
-    [method: SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
-    internal partial class SyntacticClassificationTaggerProvider(
-        IThreadingContext threadingContext,
-        SyntacticClassificationTypeMap typeMap,
-        IGlobalOptionService globalOptions,
-        IAsynchronousOperationListenerProvider listenerProvider) : ITaggerProvider
+    private readonly IAsynchronousOperationListener _listener = listenerProvider.GetListener(FeatureAttribute.Classification);
+    private readonly IThreadingContext _threadingContext = threadingContext;
+    private readonly SyntacticClassificationTypeMap _typeMap = typeMap;
+    private readonly IGlobalOptionService _globalOptions = globalOptions;
+
+    public ITagger<T>? CreateTagger<T>(ITextBuffer buffer) where T : ITag
     {
-        private readonly IAsynchronousOperationListener _listener = listenerProvider.GetListener(FeatureAttribute.Classification);
-        private readonly IThreadingContext _threadingContext = threadingContext;
-        private readonly SyntacticClassificationTypeMap _typeMap = typeMap;
-        private readonly IGlobalOptionService _globalOptions = globalOptions;
-
-        private readonly ConditionalWeakTable<ITextBuffer, TagComputer> _tagComputers = new();
-
-        public ITagger<T>? CreateTagger<T>(ITextBuffer buffer) where T : ITag
-        {
-            _threadingContext.ThrowIfNotOnUIThread();
-            if (!_globalOptions.GetOption(SyntacticColorizerOptionsStorage.SyntacticColorizer))
-                return null;
-
-            if (!_tagComputers.TryGetValue(buffer, out var tagComputer))
-            {
-                tagComputer = new TagComputer(this, (ITextBuffer2)buffer, _listener, _typeMap, TaggerDelay.NearImmediate.ComputeTimeDelay());
-                _tagComputers.Add(buffer, tagComputer);
-            }
-
-            tagComputer.IncrementReferenceCount();
-
-            var tagger = new Tagger(tagComputer);
-
-            if (tagger is ITagger<T> typedTagger)
-                return typedTagger;
-
-            // Oops, we can't actually return this tagger, so just clean up
-            tagger.Dispose();
+        _threadingContext.ThrowIfNotOnUIThread();
+        if (!_globalOptions.GetOption(SyntacticColorizerOptionsStorage.SyntacticColorizer))
             return null;
-        }
 
-        private void DisconnectTagComputer(ITextBuffer buffer)
-            => _tagComputers.Remove(buffer);
+        // Note: creating the Tagger must not fail (or we will leak the TagComputer).
+        var tagger = new Tagger(TagComputer.GetOrCreate(this, (ITextBuffer2)buffer));
+
+        if (tagger is ITagger<T> typedTagger)
+            return typedTagger;
+
+        // Oops, we can't actually return this tagger, so just clean up
+        tagger.Dispose();
+        return null;
     }
 }
