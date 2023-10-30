4beeb055d808cc82841480766138e845163e0502
Sam Harwell <Sam.Harwell@microsoft.com>
2023-10-30 11:23:44 -0500

    Port organizing tests to the new framework

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/VisualBasic/BasicOrganizing.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/VisualBasic/BasicOrganizing.cs
@@ -2,30 +2,28 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Shared.TestHooks;
 using Microsoft.CodeAnalysis.Test.Utilities;
-using Microsoft.VisualStudio.IntegrationTest.Utilities;
-using Roslyn.Test.Utilities;
+using Roslyn.VisualStudio.IntegrationTests;
 using Xunit;
-using Xunit.Abstractions;
 
-namespace Roslyn.VisualStudio.IntegrationTests.VisualBasic
+namespace Roslyn.VisualStudio.NewIntegrationTests.VisualBasic
 {
-    [Collection(nameof(SharedIntegrationHostFixture))]
     public class BasicOrganizing : AbstractEditorTest
     {
         protected override string LanguageName => LanguageNames.VisualBasic;
 
-        public BasicOrganizing(VisualStudioInstanceFactory instanceFactory)
-            : base(instanceFactory, nameof(BasicOrganizing))
+        public BasicOrganizing()
+            : base(nameof(BasicOrganizing))
         {
         }
 
-        [WpfFact, Trait(Traits.Feature, Traits.Features.Organizing)]
-        public void RemoveAndSort()
+        [IdeFact, Trait(Traits.Feature, Traits.Features.Organizing)]
+        public async Task RemoveAndSort()
         {
-            SetUpEditor(@"Imports System.Linq$$
+            await SetUpEditorAsync(@"Imports System.Linq$$
 Imports System
 Imports System.Runtime.InteropServices
 Imports System.Runtime.CompilerServices
@@ -33,18 +31,18 @@ Class Test
     Sub Method(<CallerMemberName> Optional str As String = Nothing)
         Dim data As COMException
     End Sub
-End Class");
-            VisualStudio.ExecuteCommand("Edit.RemoveAndSort");
-            VisualStudio.Workspace.WaitForAsyncOperations(
-                Helper.HangMitigatingTimeout,
-                FeatureAttribute.OrganizeDocument);
-            VisualStudio.Editor.Verify.TextContains(@"Imports System.Runtime.CompilerServices
+End Class", HangMitigatingCancellationToken);
+            await TestServices.Shell.ExecuteCommandAsync(WellKnownCommands.Edit.RemoveAndSort, HangMitigatingCancellationToken);
+            await TestServices.Workspace.WaitForAsyncOperationsAsync(
+                FeatureAttribute.OrganizeDocument,
+                HangMitigatingCancellationToken);
+            await TestServices.EditorVerifier.TextContainsAsync(@"Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 Class Test
     Sub Method(<CallerMemberName> Optional str As String = Nothing)
         Dim data As COMException
     End Sub
-End Class");
+End Class", cancellationToken: HangMitigatingCancellationToken);
 
         }
     }
