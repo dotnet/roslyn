f63c1f7e0c3739190fced1729ffc6f632b0d9569
Jan Jones <janjones@microsoft.com>
2023-11-06 13:48:10 +0100

    Revert fix using `basesBeingResolved`

--- src/Compilers/CSharp/Portable/Symbols/Tuples/TupleTypeSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Tuples/TupleTypeSymbol.cs
@@ -37,8 +37,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
             bool includeNullability,
             ImmutableArray<bool> errorPositions,
             CSharpSyntaxNode? syntax = null,
-            BindingDiagnosticBag? diagnostics = null,
-            ConsList<TypeSymbol>? basesBeingResolved = null)
+            BindingDiagnosticBag? diagnostics = null)
         {
             Debug.Assert(!shouldCheckConstraints || syntax is object);
             Debug.Assert(elementNames.IsDefault || elementTypesWithAnnotations.Length == elementNames.Length);
@@ -53,8 +52,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
 
             NamedTypeSymbol underlyingType = getTupleUnderlyingType(elementTypesWithAnnotations, syntax, compilation, diagnostics);
 
-            if (numElements >= ValueTupleRestPosition && diagnostics != null && !underlyingType.IsErrorType() &&
-                basesBeingResolved?.ContainsReference(underlyingType.OriginalDefinition) != true)
+            if (numElements >= ValueTupleRestPosition && diagnostics != null && !underlyingType.IsErrorType())
             {
                 WellKnownMember wellKnownTupleRest = GetTupleTypeMember(ValueTupleRestPosition, ValueTupleRestPosition);
                 _ = GetWellKnownMemberInType(underlyingType.OriginalDefinition, wellKnownTupleRest, diagnostics, syntax);
