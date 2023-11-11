c50b3f26da61f1b45c206db32f7e07d7a8a23919
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-11-11 11:19:00 -0800

    Search whole solution at a time

--- src/Features/Core/Portable/NavigateTo/INavigateToSearchService.cs
+++ src/Features/Core/Portable/NavigateTo/INavigateToSearchService.cs
@@ -7,6 +7,7 @@ using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Shared.Utilities;
 
 namespace Microsoft.CodeAnalysis.NavigateTo
 {
@@ -15,26 +16,57 @@ namespace Microsoft.CodeAnalysis.NavigateTo
         IImmutableSet<string> KindsProvided { get; }
         bool CanFilter { get; }
 
-        Task SearchDocumentAsync(Document document, string searchPattern, IImmutableSet<string> kinds, Document? activeDocument, Func<INavigateToSearchResult, Task> onResultFound, CancellationToken cancellationToken);
+        Task SearchDocumentAsync(
+            Document document,
+            string searchPattern,
+            IImmutableSet<string> kinds,
+            // Document? activeDocument,
+            Func<INavigateToSearchResult, Task> onResultFound,
+            CancellationToken cancellationToken);
 
         /// <summary>
-        /// Searches the documents inside <paramref name="project"/> for symbols that matches <paramref name="searchPattern"/>.
-        /// <paramref name="priorityDocuments"/> is an optional subset of the documents from <paramref name="project"/> that can
-        /// be used to prioritize work.  Generates files should not be searched.  Results should be up to date with the actual
-        /// document contents for the requested project.
+        /// Searches the documents inside <paramref name="project"/> for symbols that matches <paramref
+        /// name="searchPattern"/>. <paramref name="priorityDocuments"/> is an optional subset of the documents from
+        /// <paramref name="project"/> that can be used to prioritize work.  Generates files should not be searched.
+        /// Results should be up to date with the actual document contents for the requested project.
         /// </summary>
-        Task SearchProjectAsync(Project project, ImmutableArray<Document> priorityDocuments, string searchPattern, IImmutableSet<string> kinds, Document? activeDocument, Func<INavigateToSearchResult, Task> onResultFound, CancellationToken cancellationToken);
+        Task SearchProjectsAsync(
+            Solution solution,
+            IImmutableSet<Project> projects,
+            IImmutableSet<Document> priorityDocuments,
+            string searchPattern,
+            IImmutableSet<string> kinds,
+            // Document? activeDocument,
+            Func<INavigateToSearchResult, Task> onResultFound,
+            IStreamingProgressTracker progress,
+            CancellationToken cancellationToken);
 
         /// <summary>
-        /// Searches the documents inside <paramref name="project"/> for symbols that matches <paramref name="searchPattern"/>.
-        /// Results should be reported from a previous computed cache (even if that cache is out of date) to produce results as
-        /// quickly as possible.
+        /// Searches the documents inside <paramref name="project"/> for symbols that matches <paramref
+        /// name="searchPattern"/>. Results should be reported from a previous computed cache (even if that cache is out
+        /// of date) to produce results as quickly as possible.
         /// </summary>
-        Task SearchCachedDocumentsAsync(Project project, ImmutableArray<Document> priorityDocuments, string searchPattern, IImmutableSet<string> kinds, Document? activeDocument, Func<INavigateToSearchResult, Task> onResultFound, CancellationToken cancellationToken);
+        Task SearchCachedDocumentsAsync(
+            Solution solution,
+            ImmutableArray<Document> priorityDocuments,
+            string searchPattern,
+            IImmutableSet<string> kinds,
+            Document? activeDocument,
+            Func<INavigateToSearchResult, Task> onResultFound,
+            IStreamingProgressTracker progress,
+            CancellationToken cancellationToken);
 
         /// <summary>
-        /// Searches the generated documents inside <paramref name="project"/> for symbols that matches <paramref name="searchPattern"/>.
+        /// Searches the generated documents inside <paramref name="project"/> for symbols that matches <paramref
+        /// name="searchPattern"/>.
         /// </summary>
-        Task SearchGeneratedDocumentsAsync(Project project, string searchPattern, IImmutableSet<string> kinds, Document? activeDocument, Func<INavigateToSearchResult, Task> onResultFound, CancellationToken cancellationToken);
+        Task SearchGeneratedDocumentsAsync(
+            Solution solution,
+            string searchPattern,
+            IImmutableSet<string> kinds,
+            Document? activeDocument,
+            Func<INavigateToSearchResult, Task> onResultFound,
+            IStreamingProgressTracker progress,
+            CancellationToken cancellationToken);
     }
 }
