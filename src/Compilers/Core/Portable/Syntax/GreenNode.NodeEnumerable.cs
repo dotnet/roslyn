8a572826c4580c124c00c1a2cd8ba873ed1851c0
Cyrus Najmabadi <cyrusn@microsoft.com>
2023-11-14 09:00:47 -0800

    new file

---
+++ src/Compilers/Core/Portable/Syntax/GreenNode.NodeEnumerable.cs
@@ -0,0 +1,86 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.PooledObjects;
+
+namespace Microsoft.CodeAnalysis;
+
+internal abstract partial class GreenNode
+{
+    public struct NodeEnumerable(GreenNode node) : IEnumerable<GreenNode>
+    {
+        public readonly Enumerator GetEnumerator()
+            => new(node);
+
+        IEnumerator<GreenNode> IEnumerable<GreenNode>.GetEnumerator()
+            => GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        public struct Enumerator(GreenNode node) : IEnumerator<GreenNode>
+        {
+            private readonly GreenNode _node = node;
+            private readonly ArrayBuilder<Syntax.InternalSyntax.ChildSyntaxList.Enumerator> _stack = ArrayBuilder<Syntax.InternalSyntax.ChildSyntaxList.Enumerator>.GetInstance();
+
+            private bool _started;
+            private GreenNode _current = null!;
+
+            public readonly GreenNode Current
+                => _current;
+
+            public bool MoveNext()
+            {
+                if (!_started)
+                {
+                    _started = true;
+                    _current = _node;
+                    _stack.Push(_node.ChildNodesAndTokens().GetEnumerator());
+                    return true;
+                }
+                else
+                {
+                    while (_stack.TryPop(out var currentEnumerator))
+                    {
+                        while (currentEnumerator.MoveNext())
+                        {
+                            var current = currentEnumerator.Current;
+                            if (current is null)
+                                continue;
+
+                            // push back this enumerator back onto the stack as it may still have more elements to give.
+                            _stack.Push(currentEnumerator);
+
+                            // also push the children of this current node so we'll walk into those.
+                            if (!current.IsToken)
+                                _stack.Push(current.ChildNodesAndTokens().GetEnumerator());
+
+                            // Finally, return the current node we ran into back to the caller.
+                            _current = current;
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                _started = false;
+                _stack.Clear();
+                _current = null!;
+            }
+
+            public readonly void Dispose()
+            {
+                _stack.Free();
+            }
+
+            readonly object IEnumerator.Current => Current;
+        }
+    }
+}
--- src/Compilers/Core/Portable/Syntax/GreenNode.cs
+++ src/Compilers/Core/Portable/Syntax/GreenNode.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
@@ -17,7 +16,7 @@ using Roslyn.Utilities;
 namespace Microsoft.CodeAnalysis
 {
     [DebuggerDisplay("{GetDebuggerDisplay(), nq}")]
-    internal abstract class GreenNode
+    internal abstract partial class GreenNode
     {
         private string GetDebuggerDisplay()
         {
@@ -199,80 +198,6 @@ namespace Microsoft.CodeAnalysis
         /// </summary>
         public NodeEnumerable EnumerateNodes()
             => new NodeEnumerable(this);
-
-        public struct NodeEnumerable(GreenNode node) : IEnumerable<GreenNode>
-        {
-            public readonly Enumerator GetEnumerator()
-                => new(node);
-
-            IEnumerator<GreenNode> IEnumerable<GreenNode>.GetEnumerator()
-                => GetEnumerator();
-
-            IEnumerator IEnumerable.GetEnumerator()
-                => GetEnumerator();
-
-            public struct Enumerator(GreenNode node) : IEnumerator<GreenNode>
-            {
-                private readonly GreenNode _node = node;
-                private readonly ArrayBuilder<Syntax.InternalSyntax.ChildSyntaxList.Enumerator> _stack = ArrayBuilder<Syntax.InternalSyntax.ChildSyntaxList.Enumerator>.GetInstance();
-
-                private bool _started;
-                private GreenNode _current = null!;
-
-                public readonly GreenNode Current
-                    => _current;
-
-                public bool MoveNext()
-                {
-                    if (!_started)
-                    {
-                        _started = true;
-                        _current = _node;
-                        _stack.Push(_node.ChildNodesAndTokens().GetEnumerator());
-                        return true;
-                    }
-                    else
-                    {
-                        while (_stack.TryPop(out var currentEnumerator))
-                        {
-                            while (currentEnumerator.MoveNext())
-                            {
-                                var current = currentEnumerator.Current;
-                                if (current is null)
-                                    continue;
-
-                                // push back this enumerator back onto the stack as it may still have more elements to give.
-                                _stack.Push(currentEnumerator);
-
-                                // also push the children of this current node so we'll walk into those.
-                                if (!current.IsToken)
-                                    _stack.Push(current.ChildNodesAndTokens().GetEnumerator());
-
-                                // Finally, return the current node we ran into back to the caller.
-                                _current = current;
-                                return true;
-                            }
-                        }
-                    }
-
-                    return false;
-                }
-
-                public void Reset()
-                {
-                    _started = false;
-                    _stack.Clear();
-                    _current = null!;
-                }
-
-                public readonly void Dispose()
-                {
-                    _stack.Free();
-                }
-
-                readonly object IEnumerator.Current => Current;
-            }
-        }
 
         /// <summary>
         /// Find the slot that contains the given offset.
