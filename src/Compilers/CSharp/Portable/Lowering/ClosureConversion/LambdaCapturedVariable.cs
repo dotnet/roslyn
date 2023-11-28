b1e1019229786e96622801e36d70fab150a1320c
Fredric Silberberg <frsilb@microsoft.com>
2023-11-28 17:43:22 -0500

    Small refactor to a switch statement for easier addition

--- src/Compilers/CSharp/Portable/Lowering/ClosureConversion/LambdaCapturedVariable.cs
+++ src/Compilers/CSharp/Portable/Lowering/ClosureConversion/LambdaCapturedVariable.cs
@@ -59,19 +59,14 @@ namespace Microsoft.CodeAnalysis.CSharp
 
             if (variable is LocalSymbol local)
             {
-                if (local.SynthesizedKind == SynthesizedLocalKind.LambdaDisplayClass)
+                switch (local.SynthesizedKind)
                 {
-                    return GeneratedNames.MakeLambdaDisplayLocalName(uniqueId++);
-                }
-
-                if (local.SynthesizedKind == SynthesizedLocalKind.ExceptionFilterAwaitHoistedExceptionLocal)
-                {
-                    return GeneratedNames.MakeHoistedLocalFieldName(local.SynthesizedKind, uniqueId++);
-                }
-
-                if (local.SynthesizedKind == SynthesizedLocalKind.InstrumentationPayload)
-                {
-                    return GeneratedNames.MakeSynthesizedInstrumentationPayloadLocalFieldName(uniqueId++);
+                    case SynthesizedLocalKind.LambdaDisplayClass:
+                        return GeneratedNames.MakeLambdaDisplayLocalName(uniqueId++);
+                    case SynthesizedLocalKind.ExceptionFilterAwaitHoistedExceptionLocal:
+                        return GeneratedNames.MakeHoistedLocalFieldName(local.SynthesizedKind, uniqueId++);
+                    case SynthesizedLocalKind.InstrumentationPayload:
+                        return GeneratedNames.MakeSynthesizedInstrumentationPayloadLocalFieldName(uniqueId++);
                 }
 
                 // should never be captured:
