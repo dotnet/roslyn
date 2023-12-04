cb65f3fa97a2a3d3dc85b27aa36eff2dbc2ea3d6
Julien Couvreur <julien.couvreur@gmail.com>
2023-12-04 10:02:40 -0800

    Unused using

--- src/Compilers/CSharp/Portable/BoundTree/BoundImplicitIndexerAccess.cs
+++ src/Compilers/CSharp/Portable/BoundTree/BoundImplicitIndexerAccess.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
-using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp
