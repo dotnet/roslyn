2536f05ba0b42d8ba258a40700bd6adfe8f21eef
Julien Couvreur <julien.couvreur@gmail.com>
2023-12-06 14:37:30 -0800

    Allow collection expressions with CreateCompilation

---
+++ src/Compilers/Test/Core/CollectionBuilderAttribute.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if !NET8_0_OR_GREATER
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface)]
+    public sealed class CollectionBuilderAttribute : Attribute
+    {
+        public CollectionBuilderAttribute(Type builderType, string methodName)
+        {
+            BuilderType = builderType;
+            MethodName = methodName;
+        }
+
+        public Type BuilderType { get; }
+        public string MethodName { get; }
+    }
+}
+
+#endif
