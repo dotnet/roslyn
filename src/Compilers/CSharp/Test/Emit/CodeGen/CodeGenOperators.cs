3b24552644677ae7d97615d180fc0cd4ccb6e8b2
Jared Parsons <jared@paranoidcoding.org>
2023-12-12 12:54:29 -0800

    more tests

--- src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenOperators.cs
+++ src/Compilers/CSharp/Test/Emit/CodeGen/CodeGenOperators.cs
@@ -3945,21 +3945,24 @@ Infinity");
         {
             var text = @"
 using System;
+using System.Globalization;
  
 class C
 {
     static void Main()
     {
-        Console.WriteLine(+0d == -0d);
-        Console.WriteLine(1d / 0d);
-        Console.WriteLine(1d / -0d);
-        Console.WriteLine(-1d / 0d);
-        Console.WriteLine(-1d / -0d);
-        Console.WriteLine(1d / (1d * 0d));
-        Console.WriteLine(1d / (1d * -0d));
-        Console.WriteLine(1d / (-1d * 0d));
-        Console.WriteLine(1d / (-1d * -0d));
+        WriteLine(+0d == -0d);
+        WriteLine(1d / 0d);
+        WriteLine(1d / -0d);
+        WriteLine(-1d / 0d);
+        WriteLine(-1d / -0d);
+        WriteLine(1d / (1d * 0d));
+        WriteLine(1d / (1d * -0d));
+        WriteLine(1d / (-1d * 0d));
+        WriteLine(1d / (-1d * -0d));
     }
+    static void WriteLine(bool b) => Console.WriteLine(b);
+    static void WriteLine(double d) => Console.WriteLine(d.ToString(CultureInfo.InvariantCulture));
 }";
 
             var comp = CompileAndVerify(text, expectedOutput: @"
@@ -3982,21 +3985,25 @@ Infinity");
         {
             var text = @"
 using System;
+using System.Globalization;
  
 class C
 {
     static void Main()
     {
-        Console.WriteLine(+0m == -0m);
-        Console.WriteLine(1d / (double)(0m));
-        Console.WriteLine(1d / (double)(-0m));
-        Console.WriteLine(-1d / (double)(0m));
-        Console.WriteLine(-1d / (double)(-0m));
-        Console.WriteLine(1d / (double)(1m * 0m));
-        Console.WriteLine(1d / (double)(1m * -0m));
-        Console.WriteLine(1d / (double)(-1m * 0m));
-        Console.WriteLine(1d / (double)(-1m * -0m));
+        WriteLine(+0m == -0m);
+        WriteLine(1d / (double)(0m));
+        WriteLine(1d / (double)(-0m));
+        WriteLine(-1d / (double)(0m));
+        WriteLine(-1d / (double)(-0m));
+        WriteLine(1d / (double)(1m * 0m));
+        WriteLine(1d / (double)(1m * -0m));
+        WriteLine(1d / (double)(-1m * 0m));
+        WriteLine(1d / (double)(-1m * -0m));
     }
+
+    static void WriteLine(bool b) => Console.WriteLine(b);
+    static void WriteLine(double d) => Console.WriteLine(d.ToString(CultureInfo.InvariantCulture));
 }";
 
             var comp = CompileAndVerify(text, expectedOutput: @"
