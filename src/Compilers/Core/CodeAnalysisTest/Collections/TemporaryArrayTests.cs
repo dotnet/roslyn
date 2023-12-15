5b91cd33ddaeac551e7da9ab809bc082bc4dbf82
Youssef Victor <youssefvictor00@gmail.com>
2023-12-15 22:20:06 +0200

    Cleanup

--- src/Compilers/Core/CodeAnalysisTest/Collections/TemporaryArrayTests.cs
+++ src/Compilers/Core/CodeAnalysisTest/Collections/TemporaryArrayTests.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Collections;
 using Xunit;
 
