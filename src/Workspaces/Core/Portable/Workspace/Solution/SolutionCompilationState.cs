f571ec278b43ae974a107ff6a862834444cc41a3
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-01-03 18:00:40 -0800

    comments

--- src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.cs
@@ -495,22 +495,24 @@ internal sealed partial class SolutionCompilationState
     public SolutionCompilationState WithProjectAnalyzerReferences(
         ProjectId projectId, IReadOnlyList<AnalyzerReference> analyzerReferences)
     {
-        // The .Except() methods here aren't going to terribly cheap, but the assumption is adding or removing just the generators
-        // we changed, rather than creating an entire new generator driver from scratch and rerunning all generators, is cheaper
-        // in the end. This was written without data backing up that assumption, so if a profile indicates to the contrary,
-        // this could be changed.
-        //
-        // When we're comparing AnalyzerReferences, we'll compare with reference equality; AnalyzerReferences like AnalyzerFileReference
-        // may implement their own equality, but that can result in things getting out of sync: two references that are value equal can still
-        // have their own generator instances; it's important that as we're adding and removing references that are value equal that we
-        // still update with the correct generator instances that are coming from the new reference that is actually held in the project state from above.
-        // An alternative approach would be to call oldProject.WithAnalyzerReferences keeping all the references in there that are value equal the same,
-        // but this avoids any surprises where other components calling WithAnalyzerReferences might not expect that.
-
         return ForkProject(
             this.SolutionState.WithProjectAnalyzerReferences(projectId, analyzerReferences),
             static stateChange =>
             {
+                // The .Except() methods here aren't going to terribly cheap, but the assumption is adding or removing
+                // just the generators we changed, rather than creating an entire new generator driver from scratch and
+                // rerunning all generators, is cheaper in the end. This was written without data backing up that
+                // assumption, so if a profile indicates to the contrary, this could be changed.
+                //
+                // When we're comparing AnalyzerReferences, we'll compare with reference equality; AnalyzerReferences
+                // like AnalyzerFileReference may implement their own equality, but that can result in things getting
+                // out of sync: two references that are value equal can still have their own generator instances; it's
+                // important that as we're adding and removing references that are value equal that we still update with
+                // the correct generator instances that are coming from the new reference that is actually held in the
+                // project state from above. An alternative approach would be to call oldProject.WithAnalyzerReferences
+                // keeping all the references in there that are value equal the same, but this avoids any surprises
+                // where other components calling WithAnalyzerReferences might not expect that.
+
                 var addedReferences = stateChange.NewProjectState.AnalyzerReferences.Except<AnalyzerReference>(stateChange.OldProjectState.AnalyzerReferences, ReferenceEqualityComparer.Instance).ToImmutableArray();
                 var removedReferences = stateChange.OldProjectState.AnalyzerReferences.Except<AnalyzerReference>(stateChange.NewProjectState.AnalyzerReferences, ReferenceEqualityComparer.Instance).ToImmutableArray();
 
