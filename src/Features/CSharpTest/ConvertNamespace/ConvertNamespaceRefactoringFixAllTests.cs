7ee3efd4b9ffaa9a5f809fb81716a91b9592e558
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-01-09 13:35:13 -0800

    Move tests to features layer

--- src/Features/CSharpTest/ConvertNamespace/ConvertNamespaceRefactoringFixAllTests.cs
+++ src/Features/CSharpTest/ConvertNamespace/ConvertNamespaceRefactoringFixAllTests.cs
@@ -14,9 +14,9 @@ using Xunit;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.ConvertNamespace
 {
-    public class ConvertNamespaceRefactoringFixAllTests : AbstractCSharpCodeActionTest
+    public class ConvertNamespaceRefactoringFixAllTests : AbstractCSharpCodeActionTest_NoEditor
     {
-        protected override CodeRefactoringProvider CreateCodeRefactoringProvider(EditorTestWorkspace workspace, TestParameters parameters)
+        protected override CodeRefactoringProvider CreateCodeRefactoringProvider(TestWorkspace workspace, TestParameters parameters)
             => new ConvertNamespaceCodeRefactoringProvider();
 
         private OptionsCollection PreferBlockScopedNamespace
