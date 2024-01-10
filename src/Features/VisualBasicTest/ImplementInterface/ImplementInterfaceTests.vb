c46c376256414bbeac4a74ff821a001a0f994dd1
Cyrus Najmabadi <cyrusn@microsoft.com>
2024-01-09 19:01:40 -0800

    Move test down to features layer

--- src/Features/VisualBasicTest/ImplementInterface/ImplementInterfaceTests.vb
+++ src/Features/VisualBasicTest/ImplementInterface/ImplementInterfaceTests.vb
@@ -13,7 +13,7 @@ Imports Microsoft.CodeAnalysis.VisualBasic.ImplementInterface
 Namespace Microsoft.CodeAnalysis.Editor.VisualBasic.UnitTests.ImplementInterface
     <Trait(Traits.Feature, Traits.Features.CodeActionsImplementInterface)>
     Partial Public Class ImplementInterfaceTests
-        Inherits AbstractVisualBasicDiagnosticProviderBasedUserDiagnosticTest
+        Inherits AbstractVisualBasicDiagnosticProviderBasedUserDiagnosticTest_NoEditor
 
         Friend Overrides Function CreateDiagnosticProviderAndFixer(workspace As Workspace) As (DiagnosticAnalyzer, CodeFixProvider)
             Return (Nothing, New VisualBasicImplementInterfaceCodeFixProvider)
