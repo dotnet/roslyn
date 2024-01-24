5fb90acc3bda2bc7f30f442b62014c60f4c5543e
Todd Grunke <toddgrun@microsoft.com>
2024-01-23 22:22:39 -0800

    Change WriteEvent calls to WriteEventCore
    WriteEvent is quite inefficient in that it takes in a params array of objects, thus allocating both the array and boxing the int/long parameters passed in. Instead, WriteEventCore should be called with an appropriately populated EventSource.EventData.
    
    Found while investigating OOP allocations in a speedometer test.

--- src/Compilers/Core/Portable/CodeAnalysisEventSource.cs
+++ src/Compilers/Core/Portable/CodeAnalysisEventSource.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.Diagnostics.Tracing;
-using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis
 {
@@ -38,12 +37,63 @@ namespace Microsoft.CodeAnalysis
         internal void StartSingleGeneratorRunTime(string generatorName, string assemblyPath, string id) => WriteEvent(3, generatorName, assemblyPath, id);
 
         [Event(4, Message = "Generator {0} ran for {2} ticks", Keywords = Keywords.Performance, Level = EventLevel.Informational, Opcode = EventOpcode.Stop, Task = Tasks.SingleGeneratorRunTime)]
-        internal void StopSingleGeneratorRunTime(string generatorName, string assemblyPath, long elapsedTicks, string id) => WriteEvent(4, generatorName, assemblyPath, elapsedTicks, id);
+        internal void StopSingleGeneratorRunTime(string generatorName, string assemblyPath, long elapsedTicks, string id)
+        {
+            unsafe
+            {
+                fixed (char* generatorNameBytes = generatorName)
+                fixed (char* assemblyPathBytes = assemblyPath)
+                fixed (char* idBytes = id)
+                {
+                    EventSource.EventData* data = stackalloc EventSource.EventData[4];
+                    data[0].DataPointer = (IntPtr)generatorNameBytes;
+                    data[0].Size = ((generatorName.Length + 1) * 2);
+                    data[1].DataPointer = (IntPtr)assemblyPathBytes;
+                    data[1].Size = ((assemblyPath.Length + 1) * 2);
+                    data[2].DataPointer = (IntPtr)(&elapsedTicks);
+                    data[2].Size = 8;
+                    data[3].DataPointer = (IntPtr)idBytes;
+                    data[3].Size = ((id.Length + 1) * 2);
+                    WriteEventCore(4, 4, data);
+                }
+            }
+        }
 
         [Event(5, Message = "Generator '{0}' failed with exception: {1}", Level = EventLevel.Error)]
         internal void GeneratorException(string generatorName, string exception) => WriteEvent(5, generatorName, exception);
 
         [Event(6, Message = "Node {0} transformed", Keywords = Keywords.Correctness, Level = EventLevel.Verbose, Task = Tasks.BuildStateTable)]
-        internal void NodeTransform(int nodeHashCode, string name, string tableType, int previousTable, string previousTableContent, int newTable, string newTableContent, int input1, int input2) => WriteEvent(6, nodeHashCode, name, tableType, previousTable, previousTableContent, newTable, newTableContent, input1, input2);
+        internal void NodeTransform(int nodeHashCode, string name, string tableType, int previousTable, string previousTableContent, int newTable, string newTableContent, int input1, int input2)
+        {
+            unsafe
+            {
+                fixed (char* nameBytes = name)
+                fixed (char* tableTypeBytes = tableType)
+                fixed (char* previousTableContentBytes = previousTableContent)
+                fixed (char* newTableContentBytes = newTableContent)
+                {
+                    EventSource.EventData* data = stackalloc EventSource.EventData[9];
+                    data[0].DataPointer = (IntPtr)(&nodeHashCode);
+                    data[0].Size = 4;
+                    data[1].DataPointer = (IntPtr)nameBytes;
+                    data[1].Size = ((name.Length + 1) * 2);
+                    data[2].DataPointer = (IntPtr)tableTypeBytes;
+                    data[2].Size = ((tableType.Length + 1) * 2);
+                    data[3].DataPointer = (IntPtr)(&previousTable);
+                    data[3].Size = 4;
+                    data[4].DataPointer = (IntPtr)previousTableContentBytes;
+                    data[4].Size = ((previousTableContent.Length + 1) * 2);
+                    data[5].DataPointer = (IntPtr)(&newTable);
+                    data[5].Size = 4;
+                    data[6].DataPointer = (IntPtr)newTableContentBytes;
+                    data[6].Size = ((newTableContent.Length + 1) * 2);
+                    data[7].DataPointer = (IntPtr)(&input1);
+                    data[7].Size = 4;
+                    data[8].DataPointer = (IntPtr)(&input2);
+                    data[8].Size = 4;
+                    WriteEventCore(6, 9, data);
+                }
+            }
+        }
     }
 }
