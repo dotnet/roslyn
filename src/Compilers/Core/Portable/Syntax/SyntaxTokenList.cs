16751ed83e2fb1236da52dd667064183aa7fee74
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-01-24 16:44:52 -0800

    Inline construction

--- src/Compilers/Core/Portable/Syntax/SyntaxTokenList.cs
+++ src/Compilers/Core/Portable/Syntax/SyntaxTokenList.cs
@@ -66,9 +66,6 @@ namespace Microsoft.CodeAnalysis
             if (tokens.Length == 0)
                 return default;
 
-            if (tokens.Length == 1)
-                return new SyntaxTokenList(tokens[0]);
-
             return new SyntaxTokenList(parent: null, CreateNodeFromSpan(tokens), position: 0, index: 0);
         }
 
@@ -77,16 +74,21 @@ namespace Microsoft.CodeAnalysis
             if (tokens == null)
                 return null;
 
-            // TODO: it would be nice to avoid the intermediary builder.  We could just inline what ToList does here.
-            var builder = new SyntaxTokenListBuilder(tokens.Length);
-            for (int i = 0; i < tokens.Length; i++)
+            switch (tokens.Length)
             {
-                var node = tokens[i].Node;
-                Debug.Assert(node is not null);
-                builder.Add(node);
-            }
+                case 0: return null;
+                case 1: return tokens[0].Node;
+                case 2: return Syntax.InternalSyntax.SyntaxList.List(tokens[0].Node!, tokens[1].Node!);
+                case 3: return Syntax.InternalSyntax.SyntaxList.List(tokens[0].Node!, tokens[1].Node!, tokens[2].Node!);
+                default:
+                    {
+                        var copy = new GreenNode[tokens.Length];
+                        for (int i = 0, n = tokens.Length; i < n; i++)
+                            copy[i] = tokens[i].Node!;
 
-            return builder.ToList().Node;
+                        return Syntax.InternalSyntax.SyntaxList.List(copy, tokens.Length);
+                    }
+            }
         }
 
         private static GreenNode? CreateNode(IEnumerable<SyntaxToken> tokens)
