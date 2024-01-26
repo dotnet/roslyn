0d0e5119c1581b10e42a0096ae1072ce3cde953e
Todd Grunke <toddgrun@microsoft.com>
2024-01-26 12:59:50 -0800

    Ensure related docs in the preview workspace link to same sourcetext
    Without this change, I'm seeing a massive number of solution forks occuring.

--- src/EditorFeatures/Core/Preview/AbstractPreviewFactoryService.cs
+++ src/EditorFeatures/Core/Preview/AbstractPreviewFactoryService.cs
@@ -468,10 +468,18 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Preview
 
             // Create PreviewWorkspaces around the buffers to be displayed on the left and right
             // so that all IDE services (colorizer, squiggles etc.) light up in these buffers.
-            using var leftWorkspace = new ReferenceCountedDisposable<PreviewWorkspace>(new PreviewWorkspace(oldDocument.Project.Solution));
+            var leftSolution = oldDocument.Project.Solution;
+            var allLeftIds = leftSolution.GetRelatedDocumentIds(oldDocument.Id);
+            leftSolution = leftSolution.WithDocumentText(allLeftIds, oldBuffer.AsTextContainer().CurrentText, PreservationMode.PreserveIdentity);
+
+            using var leftWorkspace = new ReferenceCountedDisposable<PreviewWorkspace>(new PreviewWorkspace(leftSolution));
             leftWorkspace.Target.OpenDocument(oldDocument.Id, oldBuffer.AsTextContainer());
 
-            using var rightWorkspace = new ReferenceCountedDisposable<PreviewWorkspace>(new PreviewWorkspace(newDocument.Project.Solution));
+            var rightSolution = newDocument.Project.Solution;
+            var allRightIds = rightSolution.GetRelatedDocumentIds(newDocument.Id);
+            rightSolution = rightSolution.WithDocumentText(allRightIds, newBuffer.AsTextContainer().CurrentText, PreservationMode.PreserveIdentity);
+
+            using var rightWorkspace = new ReferenceCountedDisposable<PreviewWorkspace>(new PreviewWorkspace(rightSolution));
             rightWorkspace.Target.OpenDocument(newDocument.Id, newBuffer.AsTextContainer());
 
 #pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task (containing method uses JTF)
