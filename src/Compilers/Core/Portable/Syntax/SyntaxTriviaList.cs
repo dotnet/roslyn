e1c36b10ba98527e0c50bca3b72c711f9a058aa4
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-01-26 09:56:12 -0800

    Avoid unnecessary temporary builders when creating syntax lists. (#71799)
    Co-authored-by: Rikki Gibson <rikkigibson@gmail.com>

--- src/Compilers/Core/Portable/Syntax/SyntaxTriviaList.cs
+++ src/Compilers/Core/Portable/Syntax/SyntaxTriviaList.cs
@@ -72,21 +72,27 @@ namespace Microsoft.CodeAnalysis
             if (trivias.Length == 0)
                 return default;
 
-            if (trivias.Length == 1)
-                return new SyntaxTriviaList(trivias[0]);
-
             return new SyntaxTriviaList(token: default, CreateNodeFromSpan(trivias), position: 0, index: 0);
         }
 
         private static GreenNode? CreateNodeFromSpan(ReadOnlySpan<SyntaxTrivia> trivias)
         {
-            if (trivias == null)
-                return null;
+            switch (trivias.Length)
+            {
+                // Also handles case where trivias is `null`.
+                case 0: return null;
+                case 1: return trivias[0].UnderlyingNode!;
+                case 2: return Syntax.InternalSyntax.SyntaxList.List(trivias[0].UnderlyingNode!, trivias[1].UnderlyingNode!);
+                case 3: return Syntax.InternalSyntax.SyntaxList.List(trivias[0].UnderlyingNode!, trivias[1].UnderlyingNode!, trivias[2].UnderlyingNode!);
+                default:
+                    {
+                        var copy = new ArrayElement<GreenNode>[trivias.Length];
+                        for (int i = 0, n = trivias.Length; i < n; i++)
+                            copy[i].Value = trivias[i].UnderlyingNode!;
 
-            // TODO: it would be nice to avoid the intermediary builder.  We could just inline what ToList does here.
-            var builder = new SyntaxTriviaListBuilder(trivias.Length);
-            builder.Add(trivias);
-            return builder.ToList().Node;
+                        return Syntax.InternalSyntax.SyntaxList.List(copy);
+                    }
+            }
         }
 
         internal SyntaxToken Token { get; }
