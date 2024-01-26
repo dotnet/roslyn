330f9f2ed4b292b3c2357218d968b1cb9e4b135e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-01-26 15:34:54 -0800

    Simplify

--- src/Workspaces/Remote/ServiceHub/Host/RemoteWorkspace.SolutionCreator.cs
+++ src/Workspaces/Remote/ServiceHub/Host/RemoteWorkspace.SolutionCreator.cs
@@ -69,10 +69,6 @@ namespace Microsoft.CodeAnalysis.Remote
                     // the host and remote side here.
                     var projectConeId = newSolutionChecksums.ProjectConeId;
 
-                    //var oldSolutionCompilationChecksums = projectConeId == null
-                    //    ? await solution.CompilationState.GetStateChecksumsAsync(cancellationToken).ConfigureAwait(false)
-                    //    : await solution.CompilationState.GetStateChecksumsAsync(projectConeId, cancellationToken).ConfigureAwait(false);
-
                     var oldSolutionChecksums = projectConeId == null
                         ? await solution.CompilationState.SolutionState.GetStateChecksumsAsync(cancellationToken).ConfigureAwait(false)
                         : await solution.CompilationState.SolutionState.GetStateChecksumsAsync(projectConeId, cancellationToken).ConfigureAwait(false);
