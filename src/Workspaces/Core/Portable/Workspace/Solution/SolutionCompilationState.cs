8d8829e1c90ec418051e7cb33dbf79ec60400e07
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-01-26 14:08:40 -0800

    update comment

--- src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.cs
@@ -1066,10 +1066,10 @@ internal sealed partial class SolutionCompilationState
                 var allDocumentIds = @this.SolutionState.GetRelatedDocumentIds(documentId);
                 using var _ = ArrayBuilder<(DocumentState, SyntaxTree)>.GetInstance(allDocumentIds.Length, out var builder);
 
-                // We grab all the contents of linked files as well to ensure that our snapshot is correct wrt to the set of
-                // linked document ids our state says are in it.  Note: all of these trees should share the same green
-                // trees, as that is setup in Soltion.WithFrozenPartialCompilationIncludingSpecificDocument.  This helps
-                // ensure that the cost here is low for files with lots of linked siblings.
+                // We grab all the contents of linked files as well to ensure that our snapshot is correct wrt to the
+                // set of linked document ids our state says are in it.  Note: all of these trees should share the same
+                // green trees, as that is setup in our outer caller.  This helps ensure that the cost here is low for
+                // files with lots of linked siblings.
                 foreach (var currentDocumentId in allDocumentIds)
                 {
                     var document = @this.SolutionState.GetRequiredDocumentState(currentDocumentId);
