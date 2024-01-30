4c3f135eb0f7a664d7dce3851845f74ffb385b8a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-01-30 12:40:11 -0800

    Remove unused functionality

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Serialization/ObjectWriter.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Serialization/ObjectWriter.cs
@@ -704,12 +704,6 @@ namespace Roslyn.Utilities
             _writer.Write((byte)kind);
         }
 
-        public void WriteType(Type type)
-        {
-            _writer.Write((byte)TypeCode.Type);
-            this.WriteString(type.AssemblyQualifiedName);
-        }
-
         public void WriteEncoding(Encoding? encoding)
         {
             if (encoding == null)
@@ -798,11 +792,6 @@ namespace Roslyn.Utilities
             /// The null value
             /// </summary>
             Null,
-
-            /// <summary>
-            /// A type
-            /// </summary>
-            Type,
 
             /// <summary>
             /// A string encoded as UTF-8 (using BinaryWriter.Write(string))
