7d613619f322c8b8d738ac4790a2df4d57702805
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-01-30 17:19:13 -0800

    fix

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Utilities/BKTree.Serialization.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Utilities/BKTree.Serialization.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.CodeAnalysis.Internal.Log;
-using Microsoft.CodeAnalysis.Serialization;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Roslyn.Utilities;
