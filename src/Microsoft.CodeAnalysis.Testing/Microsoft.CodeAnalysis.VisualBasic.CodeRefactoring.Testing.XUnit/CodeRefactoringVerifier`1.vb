0f8bb684edae87642cf20c12a73e3ce5daa0a208
Sam Harwell <Sam.Harwell@microsoft.com>
2024-01-31 07:52:01 -0600

    Remove xunit specific packages
    Closes #1099

--- src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.CodeRefactoring.Testing.XUnit/CodeRefactoringVerifier`1.vb
+++
@@ -1,8 +0,0 @@
-﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-
-Imports Microsoft.CodeAnalysis.CodeRefactorings
-Imports Microsoft.CodeAnalysis.Testing.Verifiers
-
-Public Class CodeRefactoringVerifier(Of TCodeRefactoring As {CodeRefactoringProvider, New})
-    Inherits VisualBasicCodeRefactoringVerifier(Of TCodeRefactoring, XUnitVerifier)
-End Class
