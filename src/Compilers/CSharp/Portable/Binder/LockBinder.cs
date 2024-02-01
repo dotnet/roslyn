a20823e0286f2caf6d8e9affe6f5ed8f02892115
Jan Jones <janjones@microsoft.com>
2024-02-01 10:53:48 +0100

    Simplify code

--- src/Compilers/CSharp/Portable/Binder/LockBinder.cs
+++ src/Compilers/CSharp/Portable/Binder/LockBinder.cs
@@ -92,8 +92,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             }
 
             var scopeType = enterLockScopeMethod.ReturnType;
-            if (!(scopeType is { Name: LockScopeTypeName, IsValueType: true, DeclaredAccessibility: Accessibility.Public } &&
-                scopeType.GetArity() == 0 &&
+            if (!(scopeType is NamedTypeSymbol { Name: LockScopeTypeName, Arity: 0, IsValueType: true, DeclaredAccessibility: Accessibility.Public } &&
                 TypeSymbol.Equals(scopeType.ContainingType, lockType, TypeCompareKind.ConsiderEverything)))
             {
                 Error(diagnostics, ErrorCode.ERR_MissingPredefinedMember, syntax, LockTypeFullName, EnterLockScopeMethodName);
