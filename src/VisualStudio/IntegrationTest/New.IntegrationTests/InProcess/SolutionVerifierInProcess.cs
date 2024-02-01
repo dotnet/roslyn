1303d7db5652681d3dae7d14a510c6df5894c3bd
Sam Harwell <Sam.Harwell@microsoft.com>
2024-01-31 18:04:37 -0600

    Provide more specific information when tests fail

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/SolutionVerifierInProcess.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/InProcess/SolutionVerifierInProcess.cs
@@ -2,8 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Microsoft.VisualStudio.Extensibility.Testing;
 using Xunit;
 
@@ -15,11 +17,18 @@ namespace Roslyn.VisualStudio.IntegrationTests.InProcess
         public async Task AssemblyReferencePresentAsync(string projectName, string assemblyName, string assemblyVersion, string assemblyPublicKeyToken, CancellationToken cancellationToken)
         {
             var assemblyReferences = await TestServices.SolutionExplorer.GetAssemblyReferencesAsync(projectName, cancellationToken);
-            var expectedAssemblyReference = assemblyName + "," + assemblyVersion + "," + assemblyPublicKeyToken.ToUpper();
+            var expectedAssemblyReference = (assemblyName, assemblyVersion, assemblyPublicKeyToken.ToUpper());
+            if (assemblyReferences.Contains(expectedAssemblyReference))
+                return;
+
+            var assemblyReferencesMatchingName = assemblyReferences.WhereAsArray(reference => string.Equals(reference.name, expectedAssemblyReference.assemblyName, StringComparison.OrdinalIgnoreCase));
+            if (!assemblyReferencesMatchingName.IsEmpty)
+                assemblyReferences = assemblyReferencesMatchingName;
+
             Assert.Contains(expectedAssemblyReference, assemblyReferences);
         }
 
-        public async Task ProjectReferencePresent(string projectName, string referencedProjectName, CancellationToken cancellationToken)
+        public async Task ProjectReferencePresentAsync(string projectName, string referencedProjectName, CancellationToken cancellationToken)
         {
             var projectReferences = await TestServices.SolutionExplorer.GetProjectReferencesAsync(projectName, cancellationToken);
             Assert.Contains(referencedProjectName, projectReferences);
