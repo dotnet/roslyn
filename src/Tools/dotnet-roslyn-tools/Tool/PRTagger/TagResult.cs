f1be14b7b3cd01e2275b9df089af9c614f001da7
Shen Chen <shech@microsoft.com>
2024-02-06 16:15:21 -0800

    Add tag result

---
+++ src/Tools/dotnet-roslyn-tools/Tool/PRTagger/TagResult.cs
@@ -0,0 +1,13 @@
+// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+// See the License.txt file in the project root for more information.
+
+namespace Microsoft.RoslynTools.PRTagger
+{
+    internal enum TagResult
+    {
+        Succeed,
+        Failed,
+        NoChange
+    }
+}
