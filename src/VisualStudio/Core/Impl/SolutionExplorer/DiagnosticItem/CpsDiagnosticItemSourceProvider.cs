dotnet/roslyn

commit de95a79d637744b038bf2fb767731d4f03c94bd0

Switch CpsDiagnosticItemSourceProvider to use IProjectTree/ProjectTreeFlags (#71664)
This shows up in both allocations/CPU during solution explorer search locally.

src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/CpsDiagnosticItemSourceProvider.cs
@@ -3,25 +3,26 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.Internal.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Utilities;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplorer
 {
+    using OrderAttribute = Microsoft.VisualStudio.Utilities.OrderAttribute;
     using Workspace = Microsoft.CodeAnalysis.Workspace;
 
     [Export(typeof(IAttachedCollectionSourceProvider))]
     [Name(nameof(CpsDiagnosticItemSourceProvider))]
     [Order]
-    [AppliesToProject("(CSharp | VB) & CPS")]
+    [AppliesToProject($"({ProjectCapabilities.CSharp} | {ProjectCapabilities.VB}) & {ProjectCapabilities.Cps}")]
     internal sealed class CpsDiagnosticItemSourceProvider : AttachedCollectionSourceProvider<IVsHierarchyItem>
     {
         private readonly IAnalyzersCommandHandler _commandHandler;
@@ -74,7 +75,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
 
         /// <summary>
         /// Starting at the given item, walks up the tree to find the item representing the project root.
-        /// If the item is located under a target-framwork specific node, the corresponding 
+        /// If the item is located under a target-framework specific node, the corresponding 
         /// TargetFrameworkMoniker will be found as well.
         /// </summary>
         private static IVsHierarchyItem? FindProjectRootItem(IVsHierarchyItem item, out string? targetFrameworkMoniker)
@@ -100,66 +101,61 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
         /// </summary>
         private static string? GetTargetFrameworkMoniker(IVsHierarchyItem item)
         {
-            var hierarchy = item.HierarchyIdentity.NestedHierarchy;
-            var itemId = item.HierarchyIdentity.NestedItemID;
-
-            var isTargetNode = false;
-            string? potentialTFM = null;
-
-            MatchProjectTreeCapabilities(hierarchy, itemId, isMatch, state: false);
-
-            return isTargetNode ? potentialTFM : null;
-
-            bool isMatch(ReadOnlyMemory<char> capability, bool _)
+            if (TryGetFlags(item, out var flags) &&
+                flags.Contains("TargetNode"))
             {
-                if (ReadOnlyMemoryOfCharComparer.Equals("TargetNode".AsSpan(), capability))
-                {
-                    isTargetNode = true;
-                }
-                else if (capability.Span.StartsWith("$TFM:".AsSpan()))
-                {
-                    potentialTFM = capability["$TFM:".Length..].ToString();
-                }
+                const string prefix = "$TFM:";
+                var flag = flags.FirstOrDefault(f => f.StartsWith(prefix));
 
-                return false;
+                return flag?.Substring(prefix.Length);
             }
+
+            return null;
         }
 
         private static bool NestedHierarchyHasProjectTreeCapability(IVsHierarchyItem item, string capability)
         {
-            var hierarchy = item.HierarchyIdentity.NestedHierarchy;
-            var itemId = item.HierarchyIdentity.NestedItemID;
+            if (TryGetFlags(item, out var flags))
+                return flags.Contains(capability);
 
-            return MatchProjectTreeCapabilities(hierarchy, itemId, isMatch, capability.AsMemory());
-
-            static bool isMatch(ReadOnlyMemory<char> capability, ReadOnlyMemory<char> capabilityToMatch)
-            {
-                return ReadOnlyMemoryOfCharComparer.Equals(capabilityToMatch.Span, capability);
-            }
+            return false;
         }
 
-        private static bool MatchProjectTreeCapabilities<T>(IVsHierarchy hierarchy, uint itemId, Func<ReadOnlyMemory<char>, T, bool> isMatch, T state)
+        public static bool TryGetFlags(IVsHierarchyItem item, out ProjectTreeFlags flags)
         {
-            if (hierarchy.GetProperty(itemId, (int)__VSHPROPID7.VSHPROPID_ProjectTreeCapabilities, out var capabilitiesObj) == VSConstants.S_OK)
+            if (item.HierarchyIdentity.IsRoot)
             {
-                var capabilitiesString = (string)capabilitiesObj;
-
-                // Perf: Avoid string.Split
-                var afterLastMatchIndex = 0;
-                var nextMatchIndex = capabilitiesString.IndexOf(' ', afterLastMatchIndex);
-                while (nextMatchIndex >= 0)
+                if (item.HierarchyIdentity.NestedHierarchy is IVsBrowseObjectContext { UnconfiguredProject.Services.ProjectTreeService.CurrentTree.Tree: { } tree })
                 {
-                    if (isMatch(capabilitiesString.AsMemory(afterLastMatchIndex, nextMatchIndex - afterLastMatchIndex), state))
-                        return true;
+                    flags = tree.Flags;
+                    return true;
+                }
+            }
+            else
+            {
+                var itemId = item.HierarchyIdentity.ItemID;
 
-                    afterLastMatchIndex = nextMatchIndex + 1;
-                    nextMatchIndex = capabilitiesString.IndexOf(' ', afterLastMatchIndex);
+                // Browse objects are created lazily, and we want to avoid creating them when possible.
+                // This method is typically invoked for every hierarchy item in the tree, via Solution Explorer APIs.
+                // Rather than create a browse object for every node, we find the project root node and use that.
+                // In this way, we only ever create one browse object per project.
+                var root = item;
+                while (!root.HierarchyIdentity.IsRoot)
+                {
+                    root = root.Parent;
                 }
 
-                if (isMatch(capabilitiesString.AsMemory(afterLastMatchIndex), state))
-                    return true;
+                if (root.HierarchyIdentity.NestedHierarchy is IVsBrowseObjectContext { UnconfiguredProject.Services.ProjectTreeService.CurrentTree.Tree: { } tree })
+                {
+                    if (tree?.TryFind((IntPtr)itemId, out var subtree) == true)
+                    {
+                        flags = subtree.Flags;
+                        return true;
+                    }
+                }
             }
 
+            flags = default;
             return false;
         }
 

src/Setup/DevDivInsertionFiles/DevDivInsertionFiles.csproj
@@ -111,6 +111,8 @@
       <_Dependency Remove="Microsoft.Bcl.AsyncInterfaces"/>
       <_Dependency Remove="Microsoft.Build"/>
       <_Dependency Remove="Microsoft.Build.Framework"/>
+      <_Dependency Remove="Microsoft.Build.Tasks.Core"/>
+      <_Dependency Remove="Microsoft.Build.Utilities.Core"/>
       <_Dependency Remove="Microsoft.Diagnostics.Tracing.EventSource.Redist"/>
       <_Dependency Remove="Microsoft.Extensions.DependencyInjection"/>
       <_Dependency Remove="Microsoft.Extensions.DependencyInjection.Abstractions"/>
@@ -127,6 +129,7 @@
       <_Dependency Remove="StreamJsonRpc"/>
       <_Dependency Remove="System.Buffers" />
       <_Dependency Remove="System.Collections.Immutable"/>
+      <_Dependency Remove="System.Configuration.ConfigurationManager"/>
       <_Dependency Remove="System.Diagnostics.DiagnosticSource"/>
       <_Dependency Remove="System.Drawing.Common"/>
       <_Dependency Remove="System.IO.Packaging"/>
@@ -134,10 +137,13 @@
       <_Dependency Remove="System.Memory"/>
       <_Dependency Remove="System.Numerics.Vectors"/>
       <_Dependency Remove="System.Reflection.Metadata"/>
+      <_Dependency Remove="System.Reflection.MetadataLoadContext"/>
       <_Dependency Remove="System.Reflection.TypeExtensions"/>
+      <_Dependency Remove="System.Resources.Extensions"/>
       <_Dependency Remove="System.Runtime.CompilerServices.Unsafe"/>
       <_Dependency Remove="System.ValueTuple"/>
       <_Dependency Remove="System.Security.AccessControl"/>
+      <_Dependency Remove="System.Security.Permissions"/>
       <_Dependency Remove="System.Security.Principal.Windows"/>
       <_Dependency Remove="System.Text.Encoding.CodePages" />
       <_Dependency Remove="System.Text.Encodings.Web" />

src/VisualStudio/Core/Impl/Microsoft.VisualStudio.LanguageServices.Implementation.csproj
@@ -39,6 +39,7 @@
     <Reference Include="WindowsFormsIntegration" />
     <EmbeddedResource Update="SolutionExplorerShim.resx" GenerateSource="true" />
     <PackageReference Include="Microsoft.CSharp" />
+    <PackageReference Include="Microsoft.VisualStudio.ProjectSystem" />
     <PackageReference Include="Microsoft.VisualStudio.Sdk" />
     <PackageReference Include="Microsoft.VisualStudio.CodeAnalysis.Sdk.UI" />
   </ItemGroup>

commit c05d2471f2ebe77791c246d5f5e3c6af816e5c0e

Modify CpsDiagnosticItemSourceProvider.CreateCollectionSource to be resilient to being passed a disposed hierarchy item.
Addresses https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1950695

This behavior regressed recently due to the fix I introduced in https://github.com/dotnet/roslyn/pull/71664. The code winds it's way down to IVsBrowseObjectContext.UnconfiguredProject via NestedHierarchyHasProjectTreeCapability => TryGetFlags. The UnconfiguredProject property throws if the hierarchy is disposed, whereas we previously would go through interop, get back a failed hresult and return false, and silently fail out of the callstack.

I've pinged several folks and haven't gotten back a response about whether sending in a disposed hierarchy item is a bug in the caller. I assume this isn't a recent change though as we were resilient to this before and probably should continue to be.

src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/CpsDiagnosticItemSourceProvider.cs
@@ -46,6 +46,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
         protected override IAttachedCollectionSource? CreateCollectionSource(IVsHierarchyItem item, string relationshipName)
         {
             if (item != null &&
+                !item.IsDisposed &&
                 item.HierarchyIdentity != null &&
                 item.HierarchyIdentity.NestedHierarchy != null &&
                 relationshipName == KnownRelationships.Contains)