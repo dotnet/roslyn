05a2e3c3e5ea45b064dd76f78cf5eddae6ead6ad
DoctorKrolic <mapmyp03@gmail.com>
2024-02-08 12:49:28 +0300

    Remove assert

--- src/Compilers/CSharp/Portable/Lowering/LocalRewriter/LocalRewriter_StringConcat.cs
+++ src/Compilers/CSharp/Portable/Lowering/LocalRewriter/LocalRewriter_StringConcat.cs
@@ -246,7 +246,6 @@ namespace Microsoft.CodeAnalysis.CSharp
                                        locals.Remove(constructorLocal.LocalSymbol) &&
                                        (object)objectCreationConstructor.OriginalDefinition == _compilation.GetWellKnownTypeMember(WellKnownMember.System_ReadOnlySpan_T__ctor_Reference) &&
                                        objectCreationConstructor.ContainingType.IsReadOnlySpanChar():
-                                    Debug.Assert(assignment.Right.Type?.IsCharType() == true);
                                     var wrappedExpr = ConvertConcatExprToString(assignment.Right);
                                     unwrappedArgsBuilder.Add(wrappedExpr);
                                     break;
