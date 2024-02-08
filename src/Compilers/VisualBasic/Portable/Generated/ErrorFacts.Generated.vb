2ee79a35048fdf476966b3d1af95b751d59efe1f
Jan Jones <janjones@microsoft.com>
2024-02-08 11:19:13 +0100

    Handle Lock type in VB (#71927)
    * Warn when value of Lock type is locked in VB
    
    * Warn when converting Lock value in VB
    
    * Mark a sub shared
    
    * Check arity
    
    * Avoid warnings when converting Lock to Lock
    
    * Add more tests
    
    * Add more tests
    
    * Simplify code

--- src/Compilers/VisualBasic/Portable/Generated/ErrorFacts.Generated.vb
+++ src/Compilers/VisualBasic/Portable/Generated/ErrorFacts.Generated.vb
@@ -177,7 +177,9 @@
                      ERRID.WRN_CallerArgumentExpressionAttributeSelfReferential,
                      ERRID.WRN_CallerArgumentExpressionAttributeHasInvalidParameterName,
                      ERRID.WRN_AnalyzerReferencesNewerCompiler,
-                     ERRID.WRN_DuplicateAnalyzerReference
+                     ERRID.WRN_DuplicateAnalyzerReference,
+                     ERRID.WRN_LockTypeUnsupported,
+                     ERRID.WRN_ConvertingLock
                     Return True
                 Case Else
                     Return False
