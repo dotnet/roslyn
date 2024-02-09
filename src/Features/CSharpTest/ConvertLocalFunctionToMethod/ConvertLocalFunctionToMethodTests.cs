024f7ef05fe63eec51e8605dbf244d00012d6cd7
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-02-09 12:05:37 -0800

    Fix null ref in convert-local-function

--- src/Features/CSharpTest/ConvertLocalFunctionToMethod/ConvertLocalFunctionToMethodTests.cs
+++ src/Features/CSharpTest/ConvertLocalFunctionToMethod/ConvertLocalFunctionToMethodTests.cs
@@ -1092,5 +1092,45 @@ $@"class C
                 }
                 """);
         }
+
+        [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72024")]
+        public async Task TestLocalFunctionInField()
+        {
+            await TestInRegularAndScript1Async(
+                """
+                using System;
+
+                class C
+                {
+                    private Action a = () =>
+                    {
+                        var s = default(S);
+                        SetValue(3);
+                        void [||]SetValue(int value) => s.Value = value;
+                    }
+                }
+                struct S
+                {
+                    public int Value;
+                }
+                """,
+                """
+                using System;
+
+                class C
+                {
+                    private Action a = () =>
+                    {
+                        var s = default(S);
+                        SetValue(3);
+                        void SetValue(int value) => s.Value = value;
+                    }
+                }
+                struct S
+                {
+                    public int Value;
+                }
+                """);
+        }
     }
 }
