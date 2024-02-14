15503b9f5afc5ff8b88057c3acd60ab94c98a374
Michael Render <render.michael@gmail.com>
2024-02-14 17:03:18 -0500

    PR feedback

--- src/Utilities/Compiler/Lightup/IsFileLocalWrapper.cs
+++ src/Utilities/Compiler/Lightup/IsFileLocalWrapper.cs
@@ -5,27 +5,13 @@
 namespace Analyzer.Utilities.Lightup
 {
     using System;
-    using System.Diagnostics.CodeAnalysis;
     using Microsoft.CodeAnalysis;
 
-    [SuppressMessage("Performance", "CA1815:Override equals and operator equals on value types", Justification = "Not a comparable instance.")]
-    internal readonly struct IsFileLocalWrapper
+    internal static class IsFileLocalWrapper
     {
-        private static readonly Func<INamedTypeSymbol, bool> PropertyAccessor = LightupHelpers.CreateSymbolPropertyAccessor<INamedTypeSymbol, bool>(typeof(INamedTypeSymbol), nameof(IsFileLocal), fallbackResult: false);
+        private static readonly Func<INamedTypeSymbol, bool> s_isFileLocal = LightupHelpers.CreateSymbolPropertyAccessor<INamedTypeSymbol, bool>(typeof(INamedTypeSymbol), nameof(IsFileLocal), fallbackResult: false);
 
-        public INamedTypeSymbol WrappedSymbol { get; }
-
-        private IsFileLocalWrapper(INamedTypeSymbol symbol)
-        {
-            WrappedSymbol = symbol;
-        }
-
-        public static IsFileLocalWrapper FromSymbol(INamedTypeSymbol symbol)
-        {
-            return new IsFileLocalWrapper(symbol);
-        }
-
-        public bool IsFileLocal => PropertyAccessor(WrappedSymbol);
+        public static bool IsFileLocal(this INamedTypeSymbol symbol) => s_isFileLocal(symbol);
     }
 }
 
