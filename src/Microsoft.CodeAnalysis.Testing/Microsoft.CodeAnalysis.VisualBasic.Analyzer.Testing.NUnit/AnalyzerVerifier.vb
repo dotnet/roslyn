f1dddf982de9101a1b095b57726f11c0ab72eb6e
Sam Harwell <Sam.Harwell@microsoft.com>
2024-02-15 08:39:39 -0600

    Remove all framework specific packages

--- src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.Analyzer.Testing.NUnit/AnalyzerVerifier.vb
+++
@@ -1,9 +0,0 @@
-﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-
-Imports Microsoft.CodeAnalysis.Diagnostics
-
-Module AnalyzerVerifier
-    Function Create(Of TAnalyzer As {DiagnosticAnalyzer, New})() As AnalyzerVerifier(Of TAnalyzer)
-        Return New AnalyzerVerifier(Of TAnalyzer)
-    End Function
-End Module
--- src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.SourceGenerators.Testing.NUnit/SourceGeneratorVerifier.vb
+++
@@ -1,7 +0,0 @@
-﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-
-Module SourceGeneratorVerifier
-    Public Function Create(Of TSourceGenerator As New)() As SourceGeneratorVerifier(Of TSourceGenerator)
-        Return New SourceGeneratorVerifier(Of TSourceGenerator)
-    End Function
-End Module
