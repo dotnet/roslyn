90f15989095d83f5ef9bcd40cb777fd19c4e3d98
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-02-15 13:15:01 -0800

    remove solution crawler type

--- src/Features/Core/Portable/SolutionCrawler/SolutionCrawlerLogger.cs
+++ src/Features/Core/Portable/SolutionCrawler/SolutionCrawlerLogger.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if false
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -294,3 +295,4 @@ namespace Microsoft.CodeAnalysis.SolutionCrawler
             => logAggregator.IncreaseCount(ProjectNotExist);
     }
 }
+#endif
