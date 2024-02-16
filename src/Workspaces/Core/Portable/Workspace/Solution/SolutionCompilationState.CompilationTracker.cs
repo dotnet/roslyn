59e216d020082a857381e220152f7c9a80bef1e0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-02-15 16:37:01 -0800

    siplify

--- src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.CompilationTracker.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.CompilationTracker.cs
@@ -250,58 +250,22 @@ namespace Microsoft.CodeAnalysis
                     // Otherwise, we're not finalized, or the document has changed.  We'll create an in-progress state
                     // to represent the new state of the project, with all the necessary translation steps to
                     // incorporate the new document.
-                    var pendingActions = ImmutableList<(ProjectState oldState, CompilationAndGeneratorDriverTranslationAction action)>.Empty;
-                    if (!inProgressProject.DocumentStates.TryGetState(docState.Id, out oldState) || oldState != docState)
+                    ImmutableList<(ProjectState oldState, CompilationAndGeneratorDriverTranslationAction action)> pendingActions;
+                    if (inProgressProject.DocumentStates.TryGetState(docState.Id, out oldState))
                     {
-                        // We do not have the exact document. It either means this document was recently added, or the
-                        // document was recently changed. We now need to update both the inProgressState and the
-                        // compilation. There are several possibilities we want to consider:
-                        //
-                        // 1. An earlier version of the document is present in the compilation, and we just need to update it to the current version
-                        // 2. The document wasn't present in the original snapshot at all, and we just need to add the
-                        //    document.
-                        // 3. The document wasn't present in the original snapshot, but an older file had been removed that
-                        //    had the same file path. As a heuristic, we remove the old one so we don't end up with
-                        //    duplicate documents.
-                        //
-                        // Note it's possible that we simply had never tried to produce a compilation yet for this project
-                        // at all, in that case GetPartialCompilationState would have produced an empty compilation, and it
-                        // would have updated inProgressProject to remove all the documents. Thus, that is no different than
-                        // the "add" case above.
-                        if (oldState != null)
-                        {
-                            // Scenario 1. The document had been previously parsed and it's there, so we can update it with
-                            // our current state. Note if no compilation existed GetPartialCompilationState would have
-                            // produced an empty one, and removed any documents, so inProgressProject.DocumentStates would
-                            // have been empty originally.
-                            pendingActions = [(oldState: inProgressProject, new CompilationAndGeneratorDriverTranslationAction.TouchDocumentAction(oldState, docState))];
-                            inProgressProject = inProgressProject.UpdateDocument(docState, contentChanged: true);
-                        }
-                        else
-                        {
-                            // We're in either scenario 2 or 3. Do we have an existing document to try replacing? Note: the
-                            // file path here corresponds to Document.FilePath. If a document's file path is null, we then
-                            // substitute Document.Name, so we usually expect there to be a unique string regardless.
-                            oldState = inProgressProject.DocumentStates.States.FirstOrDefault(kvp => kvp.Value.FilePath == docState.FilePath).Value;
-                            if (oldState is null)
-                            {
-                                // Scenario 2.
-                                pendingActions = [(oldState: inProgressProject, new CompilationAndGeneratorDriverTranslationAction.AddDocumentsAction([docState]))];
-                                inProgressProject = inProgressProject.AddDocuments([docState]);
-                            }
-                            else
-                            {
-                                // Scenario 3. The old doc came from some other document with a different ID then we started
-                                // with -- if the document ID still existed we would have been in the Scenario 1 case
-                                // instead. We'll find the old document ID, remove that state, and then add ours.
-                                var inProgressWithDocumentRemoved = inProgressProject.RemoveDocuments([oldState.Id]);
-                                pendingActions = [
-                                    (oldState: inProgressProject, new CompilationAndGeneratorDriverTranslationAction.RemoveDocumentsAction([oldState])),
-                                    (oldState: inProgressWithDocumentRemoved, new CompilationAndGeneratorDriverTranslationAction.AddDocumentsAction([docState]))];
-                                inProgressProject = inProgressWithDocumentRemoved
-                                    .AddDocuments([docState]);
-                            }
-                        }
+                        // The document had been previously parsed and it's there, so we can update it with our current
+                        // state. Note if no compilation existed GetPartialCompilationState would have produced an empty
+                        // one, and removed any documents, so inProgressProject.DocumentStates would have been empty
+                        // originally.
+                        pendingActions = [(oldState: inProgressProject, new CompilationAndGeneratorDriverTranslationAction.TouchDocumentAction(oldState, docState))];
+                        inProgressProject = inProgressProject.UpdateDocument(docState, contentChanged: true);
+                    }
+                    else
+                    {
+                        // The document wasn't present in the original snapshot at all, and we just need to add the
+                        // document.
+                        pendingActions = [(oldState: inProgressProject, new CompilationAndGeneratorDriverTranslationAction.AddDocumentsAction([docState]))];
+                        inProgressProject = inProgressProject.AddDocuments([docState]);
                     }
 
                     // Transition us to a frozen in progress state.  With the best compilations up to this point, and the
