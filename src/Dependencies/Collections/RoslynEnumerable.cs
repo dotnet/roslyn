40dac54c8f8ae8c53610b73e8e9963975e917053
Todd Grunke <toddgrun@microsoft.com>
2024-02-22 06:54:28 -0800

    Add TryGetCount to reduce allocations during ImmutableSegmentedDictionary.AddRange

--- src/Dependencies/Collections/RoslynEnumerable.cs
+++ src/Dependencies/Collections/RoslynEnumerable.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections;
 using System.Collections.Generic;
 using Microsoft.CodeAnalysis.Collections;
 using Microsoft.CodeAnalysis.Collections.Internal;
@@ -16,6 +17,36 @@ namespace System.Linq
                 ThrowHelper.ThrowArgumentNullException(ExceptionArgument.source);
 
             return new SegmentedList<TSource>(source);
+        }
+
+        public static bool TryGetCount<T>(this IEnumerable<T> source, out int count)
+        {
+            if (source is IReadOnlyCollection<T> readOnlyCollection)
+            {
+                count = readOnlyCollection.Count;
+                return true;
+            }
+
+            if (source is ICollection<T> genericCollection)
+            {
+                count = genericCollection.Count;
+                return true;
+            }
+
+            if (source is ICollection collection)
+            {
+                count = collection.Count;
+                return true;
+            }
+
+            if (source is string str)
+            {
+                count = str.Length;
+                return true;
+            }
+
+            count = 0;
+            return false;
         }
     }
 }
