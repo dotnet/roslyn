7d6d8b72772d87c842767d77a0fd6a270dc745c2
Todd Grunke <toddgrun@microsoft.com>
2024-02-23 11:07:41 -0800

    remove unused using

--- src/Dependencies/Collections/SegmentedList`1.cs
+++ src/Dependencies/Collections/SegmentedList`1.cs
@@ -11,7 +11,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
