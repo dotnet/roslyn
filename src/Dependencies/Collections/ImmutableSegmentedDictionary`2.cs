52edc8923b9052bddde92cafc886aae2d9a68b5b
Todd Grunke <toddgrun@microsoft.com>
2024-02-27 07:44:59 -0800

    missed removing a using stmt

--- src/Dependencies/Collections/ImmutableSegmentedDictionary`2.cs
+++ src/Dependencies/Collections/ImmutableSegmentedDictionary`2.cs
@@ -7,7 +7,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 
 namespace Microsoft.CodeAnalysis.Collections
 {
