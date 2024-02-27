8fad7cebb5dd174067e20ebcc3ef5cd785179b17
tmat <tomas.matousek@microsoft.com>
2024-02-27 11:52:51 -0800

    feedback

--- src/Workspaces/Core/Portable/Workspace/Host/Mef/ExportLanguageServiceFactoryAttribute.cs
+++ src/Workspaces/Core/Portable/Workspace/Host/Mef/ExportLanguageServiceFactoryAttribute.cs
@@ -15,8 +15,8 @@ namespace Microsoft.CodeAnalysis.Host.Mef
     /// Declares a <see cref="ILanguageServiceFactory"/> implementation for inclusion in a MEF-based workspace.
     /// </remarks>
     /// <param name="type">The type that will be used to retrieve the service from a <see cref="HostLanguageServices"/>.</param>
-    /// <param name="language">The language that the service is target for; LanguageNames.CSharp, etc.</param>
-    /// <param name="layer">The layer that the service is specified for; ServiceLayer.Default, etc.</param>
+    /// <param name="language">The language that the service is target for; <see cref="LanguageNames.CSharp"/>, etc.</param>
+    /// <param name="layer">The layer that the service is specified for; <see cref="ServiceLayer.Default"/>, etc.</param>
     [MetadataAttribute]
     [AttributeUsage(AttributeTargets.Class)]
     public class ExportLanguageServiceFactoryAttribute(Type type, string language, string layer = ServiceLayer.Default) : ExportAttribute(typeof(ILanguageServiceFactory))
@@ -27,19 +27,19 @@ namespace Microsoft.CodeAnalysis.Host.Mef
         public string ServiceType { get; } = LayeredServiceUtilities.GetAssemblyQualifiedServiceTypeName(type, nameof(type));
 
         /// <summary>
-        /// The language that the service is target for; LanguageNames.CSharp, etc.
+        /// The language that the service is target for. Specify a value from <see cref="LanguageNames"/>, or other language name.
         /// </summary>
         public string Language { get; } = language ?? throw new ArgumentNullException(nameof(language));
 
         /// <summary>
-        /// The layer that the service is specified for; ServiceLayer.Default, etc.
+        /// The layer that the service is specified for. Specify a value from <see cref="ServiceLayer"/>.
         /// </summary>
         public string Layer { get; } = layer ?? throw new ArgumentNullException(nameof(layer));
 
         /// <summary>
         /// <see cref="WorkspaceKind"/>s that the service is specified for.
         /// If non-empty the service is only exported for the listed workspace kinds and <see cref="Layer"/> is not applied,
-        /// unless <see cref="Layer"/> is is ignored if non-empty.
+        /// unless <see cref="Layer"/> is <see cref="ServiceLayer.Test"/> in which case the export overrides all other exports.
         /// </summary>
         internal IReadOnlyList<string> WorkspaceKinds { get; } = [];
 
--- src/Workspaces/Core/Portable/Workspace/Host/Mef/ExportWorkspaceServiceAttribute.cs
+++ src/Workspaces/Core/Portable/Workspace/Host/Mef/ExportWorkspaceServiceAttribute.cs
@@ -26,14 +26,14 @@ namespace Microsoft.CodeAnalysis.Host.Mef
         public string ServiceType { get; } = LayeredServiceUtilities.GetAssemblyQualifiedServiceTypeName(serviceType, nameof(serviceType));
 
         /// <summary>
-        /// The layer that the service is specified for; ServiceLayer.Default, etc.
+        /// The layer that the service is specified for. Specify a value from <see cref="ServiceLayer"/>.
         /// </summary>
         public string Layer { get; } = layer ?? throw new ArgumentNullException(nameof(layer));
 
         /// <summary>
         /// <see cref="WorkspaceKind"/>s that the service is specified for.
         /// If non-empty the service is only exported for the listed workspace kinds and <see cref="Layer"/> is not applied,
-        /// unless <see cref="Layer"/> is is ignored if non-empty.
+        /// unless <see cref="Layer"/> is <see cref="ServiceLayer.Test"/> in which case the export overrides all other exports.
         /// </summary>
         internal IReadOnlyList<string> WorkspaceKinds { get; } = [];
 
