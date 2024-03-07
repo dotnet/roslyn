02a6cb2e7d63efbe8b9a4930eafdeb10a5b0afe3
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-03-07 11:28:09 -0800

    file scoped

--- src/Workspaces/Core/Portable/SolutionCrawler/IIncrementalAnalyzer.cs
+++ src/Workspaces/Core/Portable/SolutionCrawler/IIncrementalAnalyzer.cs
@@ -8,28 +8,27 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Options;
 
-namespace Microsoft.CodeAnalysis.SolutionCrawler
+namespace Microsoft.CodeAnalysis.SolutionCrawler;
+
+internal interface IIncrementalAnalyzer
 {
-    internal interface IIncrementalAnalyzer
-    {
 #if false
-        Task AnalyzeDocumentAsync(Document document, SyntaxNode bodyOpt, InvocationReasons reasons, CancellationToken cancellationToken);
-        Task AnalyzeSyntaxAsync(Document document, InvocationReasons reasons, CancellationToken cancellationToken);
-        Task DocumentCloseAsync(Document document, CancellationToken cancellationToken);
-        Task DocumentOpenAsync(Document document, CancellationToken cancellationToken);
-        Task ActiveDocumentSwitchedAsync(TextDocument document, CancellationToken cancellationToken);
-        Task NewSolutionSnapshotAsync(Solution solution, CancellationToken cancellationToken);
-        Task AnalyzeProjectAsync(Project project, bool semanticsChanged, InvocationReasons reasons, CancellationToken cancellationToken);
+    Task AnalyzeDocumentAsync(Document document, SyntaxNode bodyOpt, InvocationReasons reasons, CancellationToken cancellationToken);
+    Task AnalyzeSyntaxAsync(Document document, InvocationReasons reasons, CancellationToken cancellationToken);
+    Task DocumentCloseAsync(Document document, CancellationToken cancellationToken);
+    Task DocumentOpenAsync(Document document, CancellationToken cancellationToken);
+    Task ActiveDocumentSwitchedAsync(TextDocument document, CancellationToken cancellationToken);
+    Task NewSolutionSnapshotAsync(Solution solution, CancellationToken cancellationToken);
+    Task AnalyzeProjectAsync(Project project, bool semanticsChanged, InvocationReasons reasons, CancellationToken cancellationToken);
 
-        Task RemoveDocumentAsync(DocumentId documentId, CancellationToken cancellationToken);
-        Task RemoveProjectAsync(ProjectId projectId, CancellationToken cancellationToken);
+    Task RemoveDocumentAsync(DocumentId documentId, CancellationToken cancellationToken);
+    Task RemoveProjectAsync(ProjectId projectId, CancellationToken cancellationToken);
 
-        Task NonSourceDocumentOpenAsync(TextDocument textDocument, CancellationToken cancellationToken);
-        Task NonSourceDocumentCloseAsync(TextDocument textDocument, CancellationToken cancellationToken);
+    Task NonSourceDocumentOpenAsync(TextDocument textDocument, CancellationToken cancellationToken);
+    Task NonSourceDocumentCloseAsync(TextDocument textDocument, CancellationToken cancellationToken);
 
-        Task AnalyzeNonSourceDocumentAsync(TextDocument textDocument, InvocationReasons reasons, CancellationToken cancellationToken);
-        void LogAnalyzerCountSummary();
-        void Shutdown();
+    Task AnalyzeNonSourceDocumentAsync(TextDocument textDocument, InvocationReasons reasons, CancellationToken cancellationToken);
+    void LogAnalyzerCountSummary();
+    void Shutdown();
 #endif
-    }
 }
