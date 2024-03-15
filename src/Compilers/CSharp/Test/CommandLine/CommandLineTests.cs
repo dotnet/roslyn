881bf0eead38d02ca871c0e5a51564d73992bd5f
Juan C. Diaz <juan.diaz@nicklaushealth.org>
2024-03-15 17:37:37 -0400

    Reuse old test

--- src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
+++ src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
@@ -9353,35 +9353,13 @@ public class C { }
                 responseFile: null,
                 srcDirectory,
                 new[] { "/reportanalyzer", "/t:library", srcFile.Path },
-                analyzers: new[] { new WarningDiagnosticAnalyzer() },
+                analyzers: [new WarningDiagnosticAnalyzer(), new DiagnosticSuppressorForId("Warning01", "Suppressor01")],
                 generators: new[] { new DoNothingGenerator().AsSourceGenerator() });
             var exitCode = csc.Run(outWriter);
             Assert.Equal(0, exitCode);
             var output = outWriter.ToString();
             Assert.Contains(CodeAnalysisResources.AnalyzerExecutionTimeColumnHeader, output, StringComparison.Ordinal);
             Assert.Contains($"{nameof(WarningDiagnosticAnalyzer)} (Warning01)", output, StringComparison.Ordinal);
-            Assert.Contains(CodeAnalysisResources.GeneratorNameColumnHeader, output, StringComparison.Ordinal);
-            Assert.Contains(typeof(DoNothingGenerator).FullName, output, StringComparison.Ordinal);
-            CleanupAllGeneratedFiles(srcFile.Path);
-        }
-
-        [Fact]
-        public void ReportAnalyzerOutput_ShowSuppressorIds()
-        {
-            var srcFile = Temp.CreateFile().WriteAllText(@"class C {}");
-            var srcDirectory = Path.GetDirectoryName(srcFile.Path);
-
-            var outWriter = new StringWriter(CultureInfo.InvariantCulture);
-            var csc = CreateCSharpCompiler(
-                responseFile: null,
-                srcDirectory,
-                ["/reportanalyzer", "/t:library", srcFile.Path],
-                analyzers: [new DiagnosticSuppressorForId("Warning01", "Suppressor01")],
-                generators: [new DoNothingGenerator().AsSourceGenerator()]);
-            var exitCode = csc.Run(outWriter);
-            Assert.Equal(0, exitCode);
-            var output = outWriter.ToString();
-            Assert.Contains(CodeAnalysisResources.AnalyzerExecutionTimeColumnHeader, output, StringComparison.Ordinal);
             Assert.Contains($"{nameof(DiagnosticSuppressorForId)} (Suppressor01)", output, StringComparison.Ordinal);
             Assert.Contains(CodeAnalysisResources.GeneratorNameColumnHeader, output, StringComparison.Ordinal);
             Assert.Contains(typeof(DoNothingGenerator).FullName, output, StringComparison.Ordinal);
