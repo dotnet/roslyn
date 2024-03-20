452661afec0024ead273db5a1541c3f28265e73f
Cyrus Najmabadi <cyrus.najmaadi@gmail.com>
2024-03-20 13:56:45 -0700

    Lint

--- src/Compilers/Core/Portable/InternalUtilities/EnumerableExtensions.cs
+++ src/Compilers/Core/Portable/InternalUtilities/EnumerableExtensions.cs
@@ -7,7 +7,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
