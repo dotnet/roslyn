fa00b99f195a4e675470a09065e6a71639305865
Julien Couvreur <julien.couvreur@gmail.com>
2024-03-21 13:17:42 -0700

    Intentionally fail CI

---
+++ src/Compilers/CSharp/Test/Emit3/Tests.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.CodeGen
+{
+    public class Tests : CSharpTestBase
+    {
+        [Fact]
+        public void Fail()
+        {
+            Assert.False(true);
+        }
+    }
+}
