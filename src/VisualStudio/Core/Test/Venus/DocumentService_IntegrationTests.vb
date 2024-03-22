a718806783140d3c1ce6c8dfa0033c968eaa881f
Cyrus Najmabadi <cyrus.najmaadi@gmail.com>
2024-03-21 22:14:02 -0700

    Remove legacy IDiagnosticService

--- src/VisualStudio/Core/Test/Venus/DocumentService_IntegrationTests.vb
+++ src/VisualStudio/Core/Test/Venus/DocumentService_IntegrationTests.vb
@@ -38,9 +38,7 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Venus
     <UseExportProvider>
     <Trait(Traits.Feature, Traits.Features.FindReferences)>
     Public Class DocumentService_IntegrationTests
-        Private Shared ReadOnly s_compositionWithMockDiagnosticUpdateSourceRegistrationService As TestComposition = EditorTestCompositions.EditorFeatures _
-            .AddExcludedPartTypes(GetType(IDiagnosticUpdateSourceRegistrationService)) _
-            .AddParts(GetType(MockDiagnosticUpdateSourceRegistrationService))
+        Private Shared ReadOnly s_compositionWithMockDiagnosticUpdateSourceRegistrationService As TestComposition = EditorTestCompositions.EditorFeatures
 
         <WpfFact>
         Public Async Function TestFindUsageIntegration() As System.Threading.Tasks.Task
