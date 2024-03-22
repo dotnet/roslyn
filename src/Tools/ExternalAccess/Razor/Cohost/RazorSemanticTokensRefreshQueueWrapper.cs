34c2a8665e8843e5f535708a58c194fbd5b55b51
David Wengier <david.wengier@microsoft.com>
2024-03-22 16:17:46 +1100

    Expose semantic tokens refresh queue so Razor can initialize it

---
+++ src/Tools/ExternalAccess/Razor/Cohost/RazorSemanticTokensRefreshQueueWrapper.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.LanguageServer;
+using Microsoft.CodeAnalysis.LanguageServer.Handler.SemanticTokens;
+using Roslyn.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
+
+[ExportRazorLspServiceFactory(typeof(RazorSemanticTokensRefreshQueueWrapper)), Shared]
+[method: ImportingConstructor]
+[method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+internal class RazorSemanticTokensRefreshQueueWrapperFactory() : ILspServiceFactory
+{
+    public ILspService CreateILspService(LspServices lspServices, WellKnownLspServerKinds serverKind)
+    {
+        var semanticTokensRefreshQueue = lspServices.GetRequiredService<SemanticTokensRefreshQueue>();
+
+        return new RazorSemanticTokensRefreshQueueWrapper(semanticTokensRefreshQueue);
+    }
+
+    internal class RazorSemanticTokensRefreshQueueWrapper(SemanticTokensRefreshQueue semanticTokensRefreshQueue) : ILspService, IDisposable
+    {
+        public void Initialize(ClientCapabilities clientCapabilities)
+            => semanticTokensRefreshQueue.Initialize(clientCapabilities);
+
+        public Task TryEnqueueRefreshComputationAsync(Project project, CancellationToken cancellationToken)
+            => semanticTokensRefreshQueue.TryEnqueueRefreshComputationAsync(project, cancellationToken);
+
+        public void Dispose()
+        {
+            semanticTokensRefreshQueue.Dispose();
+        }
+    }
+}
