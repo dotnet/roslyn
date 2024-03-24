26f3db95b105a301010b858f00f9a90558369c27
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-03-24 14:14:18 -0700

    remove unused usings

--- src/Features/Core/Portable/Diagnostics/AbstractHostDiagnosticUpdateSource.cs
+++ src/Features/Core/Portable/Diagnostics/AbstractHostDiagnosticUpdateSource.cs
@@ -5,8 +5,6 @@
 using System;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Shared.Collections;
 using Roslyn.Utilities;
 
