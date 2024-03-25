9f47ad71f16b88bc2d4cd94c787b84b3a21d7e06
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-03-25 16:33:31 -0700

    Remove unused eventing functionality

--- src/VisualStudio/Core/Def/TaskList/HostDiagnosticUpdateSource.cs
+++ src/VisualStudio/Core/Def/TaskList/HostDiagnosticUpdateSource.cs
@@ -20,71 +20,28 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TaskList;
 // use HostDiagnosticUpdateSource when abstract one is not needed for testing purpose
 [Export(typeof(AbstractHostDiagnosticUpdateSource))]
 [Export(typeof(HostDiagnosticUpdateSource))]
-internal sealed class HostDiagnosticUpdateSource : AbstractHostDiagnosticUpdateSource, IProjectSystemDiagnosticSource
+[method: ImportingConstructor]
+[method: SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
+internal sealed class HostDiagnosticUpdateSource(Lazy<VisualStudioWorkspace> workspace) : AbstractHostDiagnosticUpdateSource, IProjectSystemDiagnosticSource
 {
-    private readonly Lazy<VisualStudioWorkspace> _workspace;
+    private readonly Lazy<VisualStudioWorkspace> _workspace = workspace;
 
-    private readonly object _gate = new();
-    private readonly Dictionary<ProjectId, HashSet<object>> _diagnosticMap = [];
-
-    [ImportingConstructor]
-    [SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
-    public HostDiagnosticUpdateSource(Lazy<VisualStudioWorkspace> workspace)
-    {
-        _workspace = workspace;
-    }
-
-    public override Workspace Workspace
-    {
-        get
-        {
-            return _workspace.Value;
-        }
-    }
-
-    public void UpdateAndAddDiagnosticsArgsForProject(ProjectId projectId, object key)
-    {
-        Contract.ThrowIfNull(projectId);
-        Contract.ThrowIfNull(key);
-
-        lock (_gate)
-        {
-            _diagnosticMap.GetOrAdd(projectId, id => new HashSet<object>()).Add(key);
-        }
-    }
+    public override Workspace Workspace => _workspace.Value;
 
     void IProjectSystemDiagnosticSource.UpdateDiagnosticsForProject(ProjectId projectId, object key, IEnumerable<DiagnosticData> items)
-        => UpdateAndAddDiagnosticsArgsForProject(projectId, key);
+    {
+    }
 
     void IProjectSystemDiagnosticSource.ClearAllDiagnosticsForProject(ProjectId projectId)
     {
         Contract.ThrowIfNull(projectId);
 
-        lock (_gate)
-        {
-            if (_diagnosticMap.TryGetValue(projectId, out _))
-            {
-                _diagnosticMap.Remove(projectId);
-            }
-        }
-
         AddArgsToClearAnalyzerDiagnostics(projectId);
     }
 
-    internal void ClearAndAddDiagnosticsArgsForProject(ProjectId projectId, object key)
-    {
-        Contract.ThrowIfNull(projectId);
-        Contract.ThrowIfNull(key);
-
-        lock (_gate)
-        {
-            if (_diagnosticMap.TryGetValue(projectId, out var projectDiagnosticKeys))
-                projectDiagnosticKeys.Remove(key);
-        }
-    }
-
     void IProjectSystemDiagnosticSource.ClearDiagnosticsForProject(ProjectId projectId, object key)
-        => ClearAndAddDiagnosticsArgsForProject(projectId, key);
+    {
+    }
 
     public DiagnosticData CreateAnalyzerLoadFailureDiagnostic(AnalyzerLoadFailureEventArgs e, string fullPath, ProjectId projectId, string language)
     {
