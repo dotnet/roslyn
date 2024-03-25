bd4b38d542027f032b302aa23ee05abafb8a228e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-03-25 13:45:05 -0700

    Sort

--- src/Workspaces/Core/Portable/SourceGeneration/IRemoteSourceGenerationService.cs
+++ src/Workspaces/Core/Portable/SourceGeneration/IRemoteSourceGenerationService.cs
@@ -2,13 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Immutable;
 using System.Runtime.Serialization;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.CodeAnalysis.Text;
-using System;
 
 namespace Microsoft.CodeAnalysis.SourceGeneration;
 
