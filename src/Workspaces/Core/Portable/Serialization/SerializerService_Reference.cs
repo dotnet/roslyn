997acb7263fe48dbf4933c12ddb468234d6fba38
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-03-25 10:32:30 -0700

    Suppress

--- src/Workspaces/Core/Portable/Serialization/SerializerService_Reference.cs
+++ src/Workspaces/Core/Portable/Serialization/SerializerService_Reference.cs
@@ -478,7 +478,9 @@ internal partial class SerializerService
             var offset = reader.ReadInt64();
             var size = reader.ReadInt64();
 
+#pragma warning disable CA1416 // Validate platform compatibility
             var storage = service2.AttachTemporaryStreamStorage(name, offset, size);
+#pragma warning restore CA1416 // Validate platform compatibility
             var length = size;
 
             return (storage, length);
