e96388e6d909dee2d5db31467e0f1408ab3bf0d3
Tomáš Matoušek <tmat@users.noreply.github.com>
2024-03-26 15:46:04 -0700

    Proffer ManagedHotReloadLanguageService brokered service in VS (#72737)

---
+++ src/Workspaces/Remote/Core/EditAndContinue/ManagedHotReloadLanguageServiceDescriptor.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using Microsoft.CodeAnalysis.BrokeredServices;
+using Microsoft.ServiceHub.Framework;
+using Microsoft.CodeAnalysis.Remote;
+
+namespace Microsoft.CodeAnalysis.EditAndContinue;
+
+internal static class ManagedHotReloadLanguageServiceDescriptor
+{
+    private const string ServiceName = "ManagedHotReloadLanguageService";
+    public const string ServiceVersion = "1.0";
+    public const string MonikerName = BrokeredServiceDescriptors.LanguageServerComponentNamespace + "." + BrokeredServiceDescriptors.LanguageServerComponentName + "." + ServiceName;
+
+    public static readonly ServiceJsonRpcDescriptor DevKitDescriptor = BrokeredServiceDescriptors.CreateServerServiceDescriptor(ServiceName, new(ServiceVersion));
+    public static readonly ServiceJsonRpcDescriptor VisualStudioDescriptor = ServiceDescriptor.CreateInProcServiceDescriptor(ServiceDescriptors.ComponentName, ServiceName, suffix: "", ServiceDescriptors.GetFeatureDisplayName);
+
+    static ManagedHotReloadLanguageServiceDescriptor()
+        => Debug.Assert(DevKitDescriptor.Moniker.Name == MonikerName);
+}
