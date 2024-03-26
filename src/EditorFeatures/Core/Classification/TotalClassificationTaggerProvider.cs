d37253242762d16027adc95b99c79a531a6dbbe6
Cyrus Najmabadi <cyrus.najmaadi@gmail.com>
2024-03-26 15:25:15 -0700

    Stricter checks

--- src/EditorFeatures/Core/Classification/TotalClassificationTaggerProvider.cs
+++ src/EditorFeatures/Core/Classification/TotalClassificationTaggerProvider.cs
@@ -136,7 +136,8 @@ internal sealed class TotalClassificationAggregateTagger(
         {
             // If both the syntactic and semantic tags are for the same span, then prefer the semantic one.  The latter
             // is more accurate, but often will produce these accurate tags more slowly than the syntactic classifier.
-            if (currentSyntactic.Span == currentSemantic.Span)
+            if (currentSyntactic.Span == currentSemantic.Span &&
+                PreferSemanticOverSyntactic(currentSyntactic, currentSemantic))
             {
                 totalTags.Add(currentSemantic);
                 currentSyntactic = GetNextSyntacticSpan();
@@ -268,6 +269,23 @@ internal sealed class TotalClassificationAggregateTagger(
 
         ITagSpan<IClassificationTag>? GetNextSemanticSpan()
             => semanticEnumerator.MoveNext() ? semanticEnumerator.Current : null;
+    }
+
+    private static bool PreferSemanticOverSyntactic(
+        ITagSpan<IClassificationTag> currentSyntactic,
+        ITagSpan<IClassificationTag> currentSemantic)
+    {
+        // If they have the same classification type, then can def take one instead of both.
+        if (currentSyntactic.Tag.ClassificationType == currentSemantic.Tag.ClassificationType)
+            return true;
+
+        // Syntactic identifiers can be overridden by better semantic interpretations.
+        if (currentSyntactic.Tag.ClassificationType.Classification == ClassificationTypeNames.Identifier)
+            return true;
+
+        // Something else.  For now keep both.  This can be revised in the future if other cases where semantics should
+        // override syntax are found.
+        return false;
     }
 
     private readonly struct ClassificationTagSpanIntervalIntrospector : IIntervalIntrospector<ITagSpan<IClassificationTag>>
