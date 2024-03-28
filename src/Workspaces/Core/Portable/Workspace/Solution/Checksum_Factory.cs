bf7a8c1b836e7d83c99309bd3b90091f05e32e8a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-03-28 15:25:24 -0700

    REvert

--- src/Workspaces/Core/Portable/Workspace/Solution/Checksum_Factory.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/Checksum_Factory.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.IO.Hashing;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.CodeAnalysis.PooledObjects;
