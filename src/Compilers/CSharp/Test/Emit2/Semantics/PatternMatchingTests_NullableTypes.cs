5c8eb4744cc9ef0c37b6d977e9c2c1249f826725
DoctorKrolic <mapmyp03@gmail.com>
2024-03-30 13:04:56 +0300

    Add array binding tests

--- src/Compilers/CSharp/Test/Emit2/Semantics/PatternMatchingTests_NullableTypes.cs
+++ src/Compilers/CSharp/Test/Emit2/Semantics/PatternMatchingTests_NullableTypes.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
 using Roslyn.Test.Utilities;
 using Xunit;
 
@@ -79,6 +80,69 @@ public class PatternMatchingTests_NullableTypes : PatternMatchingTestBase
     }
 
     [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
+    public void PatternCombinations_Arrays()
+    {
+        var source = """
+            #nullable enable
+
+            class C
+            {
+                void M(object obj)
+                {
+                    if (obj is int[]? or string[]?) { }
+                    if (obj is int[]? i1 or string[]? s) { }
+                    if (obj is int[]? and { }) { }
+                    if (obj is int[]? i2 and { }) { }
+                    if (obj is int[]? i3 and (1, 2)) { }
+                    if (obj is int[]? i4 and []) { }
+                }
+            }
+            """;
+
+        var comp = CreateCompilation(source);
+        comp.VerifyDiagnostics(
+            // (7,20): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (obj is int[]? or string[]?) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(7, 20),
+            // (7,30): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (obj is int[]? or string[]?) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(7, 30),
+            // (8,20): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (obj is int[]? i1 or string[]? s) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(8, 20),
+            // (8,27): error CS8780: A variable may not be declared within a 'not' or 'or' pattern.
+            //         if (obj is int[]? i1 or string[]? s) { }
+            Diagnostic(ErrorCode.ERR_DesignatorBeneathPatternCombinator, "i1").WithLocation(8, 27),
+            // (8,33): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (obj is int[]? i1 or string[]? s) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(8, 33),
+            // (8,43): error CS8780: A variable may not be declared within a 'not' or 'or' pattern.
+            //         if (obj is int[]? i1 or string[]? s) { }
+            Diagnostic(ErrorCode.ERR_DesignatorBeneathPatternCombinator, "s").WithLocation(8, 43),
+            // (9,20): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (obj is int[]? and { }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(9, 20),
+            // (10,20): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (obj is int[]? i2 and { }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(10, 20),
+            // (11,20): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (obj is int[]? i3 and (1, 2)) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(11, 20),
+            // (11,34): error CS1061: 'int[]' does not contain a definition for 'Deconstruct' and no accessible extension method 'Deconstruct' accepting a first argument of type 'int[]' could be found (are you missing a using directive or an assembly reference?)
+            //         if (obj is int[]? i3 and (1, 2)) { }
+            Diagnostic(ErrorCode.ERR_NoSuchMemberOrExtension, "(1, 2)").WithArguments("int[]", "Deconstruct").WithLocation(11, 34),
+            // (11,34): error CS8129: No suitable 'Deconstruct' instance or extension method was found for type 'int[]', with 2 out parameters and a void return type.
+            //         if (obj is int[]? i3 and (1, 2)) { }
+            Diagnostic(ErrorCode.ERR_MissingDeconstruct, "(1, 2)").WithArguments("int[]", "2").WithLocation(11, 34),
+            // (12,20): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (obj is int[]? i4 and []) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(12, 20),
+            // (12,34): error CS0518: Predefined type 'System.Index' is not defined or imported
+            //         if (obj is int[]? i4 and []) { }
+            Diagnostic(ErrorCode.ERR_PredefinedTypeNotFound, "[]").WithArguments("System.Index").WithLocation(12, 34));
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
     public void PatternsInSwitchStatement()
     {
         var source = """
@@ -126,6 +190,53 @@ public class PatternMatchingTests_NullableTypes : PatternMatchingTestBase
     }
 
     [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
+    public void PatternsInSwitchStatement_Arrays()
+    {
+        var source = """
+            #nullable enable
+
+            class C
+            {
+                void M(object obj)
+                {
+                    var x = 0;
+                    switch (obj)
+                    {
+                        case int[]?:
+                            break;
+                        case int[]? i1:
+                            break;
+                        case int[]? when x > 0:
+                            break;
+                        case int[]? i2 when x > 0:
+                            break;
+                        case (int[]? when) when x > 0:
+                            break;
+                    }
+                }
+            }
+            """;
+
+        var comp = CreateCompilation(source);
+        comp.VerifyDiagnostics(
+            // (10,18): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             case int[]?:
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(10, 18),
+            // (12,18): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             case int[]? i1:
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(12, 18),
+            // (14,18): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             case int[]? when x > 0:
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(14, 18),
+            // (16,18): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             case int[]? i2 when x > 0:
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(16, 18),
+            // (18,19): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             case (int[]? when) when x > 0:
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(18, 19));
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
     public void PatternsInSwitchExpression()
     {
         var source = """
@@ -165,6 +276,48 @@ public class PatternMatchingTests_NullableTypes : PatternMatchingTestBase
             // (14,14): error CS8116: It is not legal to use nullable type 'int?' in a pattern; use the underlying type 'int' instead.
             //             (int? when) when x > 0 => 5
             Diagnostic(ErrorCode.ERR_PatternNullableType, "int?").WithArguments("int").WithLocation(14, 14));
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
+    public void PatternsInSwitchExpression_Arrays()
+    {
+        var source = """
+            #nullable enable
+
+            class C
+            {
+                void M(object obj)
+                {
+                    var x = 0;
+                    _ = obj switch
+                    {
+                        int[]? => 1,
+                        int[]? i1 => 2,
+                        int[]? when x > 0 => 3,
+                        int[]? i2 when x > 0 => 4,
+                        (int[]? when) when x > 0 => 5
+                    };
+                }
+            }
+            """;
+
+        var comp = CreateCompilation(source);
+        comp.VerifyDiagnostics(
+            // (10,13): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             int[]? => 1,
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(10, 13),
+            // (11,13): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             int[]? i1 => 2,
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(11, 13),
+            // (12,13): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             int[]? when x > 0 => 3,
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(12, 13),
+            // (13,13): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             int[]? i2 when x > 0 => 4,
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(13, 13),
+            // (14,14): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //             (int[]? when) when x > 0 => 5
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(14, 14));
     }
 
     [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
@@ -234,6 +387,72 @@ public class PatternMatchingTests_NullableTypes : PatternMatchingTestBase
     }
 
     [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
+    public void PropertySubpatterns_Arrays()
+    {
+        var source = """
+            #nullable enable
+
+            class E
+            {
+                public object? Prop { get; set; }
+                public object? Prop2 { get; set; }
+            }
+
+            class C
+            {
+                void M(E e)
+                {
+                    if (e is { Prop: int[]? }) { }
+                    if (e is { Prop: int[]? i1 }) { }
+                    if (e is { Prop: int[]? or string[]? }) { }
+                    if (e is { Prop: int[]? i2 or string[]? s1 }) { }
+                    if (e is { Prop: int[]?, Prop2: string[]? }) { }
+                    if (e is { Prop: int[]? i3, Prop2: string[]? s2 }) { }
+                }
+            }
+            """;
+
+        var comp = CreateCompilation(source);
+        comp.VerifyDiagnostics(
+            // (13,26): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (e is { Prop: int[]? }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(13, 26),
+            // (14,26): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (e is { Prop: int[]? i1 }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(14, 26),
+            // (15,26): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (e is { Prop: int[]? or string[]? }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(15, 26),
+            // (15,36): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (e is { Prop: int[]? or string[]? }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(15, 36),
+            // (16,26): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (e is { Prop: int[]? i2 or string[]? s1 }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(16, 26),
+            // (16,33): error CS8780: A variable may not be declared within a 'not' or 'or' pattern.
+            //         if (e is { Prop: int[]? i2 or string[]? s1 }) { }
+            Diagnostic(ErrorCode.ERR_DesignatorBeneathPatternCombinator, "i2").WithLocation(16, 33),
+            // (16,39): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (e is { Prop: int[]? i2 or string[]? s1 }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(16, 39),
+            // (16,49): error CS8780: A variable may not be declared within a 'not' or 'or' pattern.
+            //         if (e is { Prop: int[]? i2 or string[]? s1 }) { }
+            Diagnostic(ErrorCode.ERR_DesignatorBeneathPatternCombinator, "s1").WithLocation(16, 49),
+            // (17,26): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (e is { Prop: int[]?, Prop2: string[]? }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(17, 26),
+            // (17,41): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (e is { Prop: int[]?, Prop2: string[]? }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(17, 41),
+            // (18,26): error CS8116: It is not legal to use nullable type 'int[]?' in a pattern; use the underlying type 'int[]' instead.
+            //         if (e is { Prop: int[]? i3, Prop2: string[]? s2 }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "int[]?").WithArguments("int[]").WithLocation(18, 26),
+            // (18,44): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (e is { Prop: int[]? i3, Prop2: string[]? s2 }) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(18, 44));
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
     public void ListPatterns()
     {
         var source = """
@@ -261,5 +480,35 @@ public class PatternMatchingTests_NullableTypes : PatternMatchingTestBase
             // (9,22): error CS8116: It is not legal to use nullable type 'string?' in a pattern; use the underlying type 'string' instead.
             //         if (s is [.. string? slice2, ')']) { }
             Diagnostic(ErrorCode.ERR_PatternNullableType, "string?").WithArguments("string").WithLocation(9, 22));
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72720")]
+    public void ListPatterns_Arrays()
+    {
+        var source = """
+            #nullable enable
+
+            class C
+            {
+                void M(string[] s)
+                {
+                    if (s is [.. string[]?]) { }
+                    if (s is [.. string[]? slice1]) { }
+                    if (s is [.. string[]? slice2, ")"]) { }
+                }
+            }
+            """;
+
+        var comp = CreateCompilationWithIndexAndRange([source, TestSources.GetSubArray]);
+        comp.VerifyDiagnostics(
+            // (7,22): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (s is [.. string[]?]) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(7, 22),
+            // (8,22): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (s is [.. string[]? slice1]) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(8, 22),
+            // (9,22): error CS8116: It is not legal to use nullable type 'string[]?' in a pattern; use the underlying type 'string[]' instead.
+            //         if (s is [.. string[]? slice2, ')']) { }
+            Diagnostic(ErrorCode.ERR_PatternNullableType, "string[]?").WithArguments("string[]").WithLocation(9, 22));
     }
 }
