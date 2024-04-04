84c7aab431ba46a36bda99c74e7b25c198de6144
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-04 13:55:05 -0700

    REmove type

--- src/EditorFeatures/Core/Tagging/TagSourceQueueItem.cs
+++ src/EditorFeatures/Core/Tagging/TagSourceQueueItem.cs
@@ -8,16 +8,13 @@ namespace Microsoft.CodeAnalysis.Editor.Tagging;
 
 internal partial class AbstractAsynchronousTaggerProvider<TTag>
 {
-    private sealed partial class TagSource
-    {
-        /// <param name="HighPriority">Specifies if this is the initial set of tags being computed or not, and no
-        /// artificial delays should be inserted when computing the tags.</param>
-        /// <param name="FrozenPartialSemantics">Indicates if we should
-        /// compute with frozen partial semantics or not.</param>
-        /// <param name="NonFrozenComputationToken">If <paramref name="FrozenPartialSemantics"/> is false, and this
-        /// queue does support computing frozen partial semantics (see <see cref="SupportsFrozenPartialSemantics"/>)
-        /// then this is a cancellation token that can cancel the expensive work being done if new frozen-partial work
-        /// is requested.</param>
-        private record struct TagSourceQueueItem(bool HighPriority, bool FrozenPartialSemantics, CancellationToken? NonFrozenComputationToken);
-    }
+    /// <param name="HighPriority">Specifies if this is the initial set of tags being computed or not, and no
+    /// artificial delays should be inserted when computing the tags.</param>
+    /// <param name="FrozenPartialSemantics">Indicates if we should
+    /// compute with frozen partial semantics or not.</param>
+    /// <param name="NonFrozenComputationToken">If <paramref name="FrozenPartialSemantics"/> is false, and this
+    /// queue does support computing frozen partial semantics (see <see cref="SupportsFrozenPartialSemantics"/>)
+    /// then this is a cancellation token that can cancel the expensive work being done if new frozen-partial work
+    /// is requested.</param>
+    private record struct TagSourceQueueItem(bool HighPriority, bool FrozenPartialSemantics, CancellationToken? NonFrozenComputationToken);
 }
