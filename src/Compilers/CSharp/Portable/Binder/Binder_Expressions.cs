7f05bbb9d0f1522c3e1d7d30913fba42de42aeba
Fred Silberberg <frsilb@microsoft.com>
2024-04-05 15:02:13 -0700

    Disallow dynamic calls on ref struct receivers (#72674)
    Fixes https://github.com/dotnet/roslyn/issues/72606. Note that the behavior in the bug has slightly changed from the sharplab build used in the repro due to https://github.com/dotnet/roslyn/pull/71421.

--- src/Compilers/CSharp/Portable/Binder/Binder_Expressions.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Expressions.cs
@@ -6326,7 +6326,7 @@ namespace Microsoft.CodeAnalysis.CSharp
 
                 if (implicitReceiver.Type.IsDynamic())
                 {
-                    var hasErrors = ReportBadDynamicArguments(elementInitializer, boundElementInitializerExpressions, refKinds: default, diagnostics, queryClause: null);
+                    var hasErrors = ReportBadDynamicArguments(elementInitializer, implicitReceiver, boundElementInitializerExpressions, refKinds: default, diagnostics, queryClause: null);
 
                     return new BoundDynamicCollectionElementInitializer(
                         elementInitializer,
@@ -6605,7 +6605,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                         var argArray = BuildArgumentsForDynamicInvocation(analyzedArguments, diagnostics);
                         var refKindsArray = analyzedArguments.RefKinds.ToImmutableOrNull();
 
-                        hasErrors &= ReportBadDynamicArguments(node, argArray, refKindsArray, diagnostics, queryClause: null);
+                        hasErrors &= ReportBadDynamicArguments(node, receiver: null, argArray, refKindsArray, diagnostics, queryClause: null);
 
                         BoundObjectInitializerExpressionBase boundInitializerOpt;
                         boundInitializerOpt = MakeBoundInitializerOpt(typeNode, type, initializerSyntaxOpt, initializerTypeOpt, diagnostics);
@@ -9659,7 +9659,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             var argArray = BuildArgumentsForDynamicInvocation(arguments, diagnostics);
             var refKindsArray = arguments.RefKinds.ToImmutableOrNull();
 
-            hasErrors &= ReportBadDynamicArguments(syntax, argArray, refKindsArray, diagnostics, queryClause: null);
+            hasErrors &= ReportBadDynamicArguments(syntax, receiver, argArray, refKindsArray, diagnostics, queryClause: null);
 
             return new BoundDynamicIndexerAccess(
                 syntax,
