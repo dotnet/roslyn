a966b2496c788aeda822386f0dd07c9c186e6dbf
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-05 13:39:30 -0700

    lint

--- src/Features/CSharp/Portable/Snippets/CSharpIntMainSnippetProvider.cs
+++ src/Features/CSharp/Portable/Snippets/CSharpIntMainSnippetProvider.cs
@@ -16,7 +16,6 @@ using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Snippets;
 using Microsoft.CodeAnalysis.Snippets.SnippetProviders;
 using Microsoft.CodeAnalysis.Text;
-using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.CSharp.Snippets;
 
