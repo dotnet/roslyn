360d0f4e982019af3320ab6f4b1cffe77c3d7f21
Shen Chen <shech@microsoft.com>
2024-04-05 12:19:23 -0700

    Add Verify DefaultValue

--- src/VisualStudio/CSharp/Test/UnifiedSettings/CSharpUnifiedSettingsTests.cs
+++ src/VisualStudio/CSharp/Test/UnifiedSettings/CSharpUnifiedSettingsTests.cs
@@ -32,37 +32,54 @@ namespace Roslyn.VisualStudio.CSharp.UnitTests.UnifiedSettings
         public async Task CSharpIntellisensePageTest()
         {
             var registrationFileStream = typeof(CSharpUnifiedSettingsTests).GetTypeInfo().Assembly.GetManifestResourceStream("Roslyn.VisualStudio.CSharp.UnitTests.csharpSettings.registration.json");
-            using var reader = new StreamReader(registrationFileStream);
+            using var reader = new StreamReader(registrationFileStream!);
             var registrationFile = await reader.ReadToEndAsync().ConfigureAwait(false);
             var registrationJsonObject = JObject.Parse(registrationFile, new JsonLoadSettings() { CommentHandling = CommentHandling.Ignore });
 
-            var categoriesTitle = registrationJsonObject.SelectToken("$.categories.['textEditor.csharp'].title")!;
+            var categoriesTitle = registrationJsonObject.SelectToken("$.categories['textEditor.csharp'].title")!;
             Assert.Equal("C#", categoriesTitle.ToString());
-            var optionPageId = registrationJsonObject.SelectToken("$.categories.['textEditor.csharp.intellisense'].legacyOptionPageId")!;
+            var optionPageId = registrationJsonObject.SelectToken("$.categories['textEditor.csharp.intellisense'].legacyOptionPageId")!;
             Assert.Equal(Microsoft.VisualStudio.LanguageServices.Guids.CSharpOptionPageIntelliSenseIdString, optionPageId.ToString());
 
-            //foreach (var option in s_onboardedOptions)
-            //{
-            //    var optionName = option.Definition.ConfigName;
-            //    if (VisualStudioOptionStorage.UnifiedSettingsStorages.TryGetValue(optionName, out var unifiedSettingsStorage))
-            //    {
-            //        var unifiedSettingsPath = unifiedSettingsStorage.GetUnifiedSettingsPath(LanguageNames.CSharp);
-
-            //    }
-            //    else
-            //    {
-            //        // Can't find the option in the storage dictionary
-            //        throw ExceptionUtilities.UnexpectedValue(optionName);
-            //    }
-            //}
+            foreach (var option in s_onboardedOptions)
+            {
+                var optionName = option.Definition.ConfigName;
+                if (VisualStudioOptionStorage.UnifiedSettingsStorages.TryGetValue(optionName, out var unifiedSettingsStorage))
+                {
+                    var unifiedSettingsPath = unifiedSettingsStorage.GetUnifiedSettingsPath(LanguageNames.CSharp);
+                    VerifyType(registrationJsonObject, unifiedSettingsPath, option);
+                    VerifyDefaultValue(registrationJsonObject, unifiedSettingsPath, option);
+                }
+                else
+                {
+                    // Can't find the option in the storage dictionary
+                    throw ExceptionUtilities.UnexpectedValue(optionName);
+                }
+            }
         }
 
-        private static void VerifyType()
+        private static void VerifyType(JObject registrationJsonObject, string unifiedSettingPath, IOption2 option)
         {
+            var actualType = registrationJsonObject.SelectToken($"$.properties['{unifiedSettingPath}'].type")!;
+            var expectedType = ConvertTypeNameToJsonType(option.Definition.Type.Name);
+            Assert.Equal(expectedType, actualType.ToString());
 
+            static string ConvertTypeNameToJsonType(string typeName)
+                => typeName switch
+                {
+                    "Boolean" => "boolean",
+                    _ => throw ExceptionUtilities.Unreachable()
+                };
         }
 
-        private static void VerifyDefaultValue()
+        private static void VerifyDefaultValue(JObject registrationJsonObject, string unifiedSettingPath, IOption2 option, string? alternateDefaultOnNull = null)
+        {
+            var actualDefaultValue = registrationJsonObject.SelectToken($"$.properties['{unifiedSettingPath}'].default")!;
+            var expectedDefaultValue = option.Definition.DefaultValue?.ToString() ?? alternateDefaultOnNull;
+            Assert.Equal(actualDefaultValue, expectedDefaultValue);
+        }
+
+        private static void VerifyMigration()
         {
 
         }
