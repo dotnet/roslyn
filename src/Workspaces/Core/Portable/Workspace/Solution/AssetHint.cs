4b7c554b47186eba5a9912808215e4f72f3043b3
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-06 16:25:42 -0700

    Use enum

--- src/Workspaces/Core/Portable/Workspace/Solution/AssetHint.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/AssetHint.cs
@@ -17,39 +17,46 @@ internal readonly struct AssetPath
     /// <summary>
     /// Instance that will only look up solution-level data when searching for checksums.
     /// </summary>
-    public static readonly AssetPath SolutionOnly = default;
+    public static readonly AssetPath SolutionOnly = new(kind: AssetPathKind.Solution);
 
     /// <summary>
     /// Instance that will only look up solution-level, as well as the top level nodes for projects when searching for
     /// checksums.  It will not descend into projects.
     /// </summary>
-    public static readonly AssetPath SolutionAndTopLevelProjectsOnly = new(projectId: null, documentId: null, topLevelProjects: true, isFullLookup_ForTestingPurposesOnly: false);
+    public static readonly AssetPath SolutionAndTopLevelProjectsOnly = new(kind: AssetPathKind.SolutionAndTopLevelProjects);
 
     /// <summary>
     /// Special instance, allowed only in tests/debug-asserts, that can do a full lookup across the entire checksum
     /// tree.  Should not be used in normal release-mode product code.
     /// </summary>
-    public static readonly AssetPath FullLookupForTesting = new(projectId: null, documentId: null, topLevelProjects: false, isFullLookup_ForTestingPurposesOnly: true);
+    public static readonly AssetPath FullLookupForTesting = new(kind: AssetPathKind.FullLookupForTests);
 
     [DataMember(Order = 0)]
-    public readonly ProjectId? ProjectId;
+    private readonly AssetPathKind _kind;
     [DataMember(Order = 1)]
+    public readonly ProjectId? ProjectId;
+    [DataMember(Order = 2)]
     public readonly DocumentId? DocumentId;
-    [DataMember(Order = 3)]
-    public readonly bool TopLevelProjects;
-    [DataMember(Order = 4)]
-    public readonly bool IsFullLookup_ForTestingPurposesOnly;
 
-    public bool IsSolutionOnly => !IsFullLookup_ForTestingPurposesOnly && ProjectId is null;
+    public bool TopLevelProjects => _kind == AssetPathKind.SolutionAndTopLevelProjects;
+    public bool IsFullLookup_ForTestingPurposesOnly => _kind == AssetPathKind.FullLookupForTests;
+    public bool IsSolutionOnly => _kind == AssetPathKind.Solution;
 
-    private AssetPath(ProjectId? projectId, DocumentId? documentId, bool topLevelProjects, bool isFullLookup_ForTestingPurposesOnly)
+    private AssetPath(AssetPathKind kind, ProjectId? projectId = null, DocumentId? documentId = null)
     {
+        _kind = kind;
         ProjectId = projectId;
         DocumentId = documentId;
-        TopLevelProjects = topLevelProjects;
-        IsFullLookup_ForTestingPurposesOnly = isFullLookup_ForTestingPurposesOnly;
     }
 
-    public static implicit operator AssetPath(ProjectId projectId) => new(projectId, documentId: null, topLevelProjects: false, isFullLookup_ForTestingPurposesOnly: false);
-    public static implicit operator AssetPath(DocumentId documentId) => new(documentId.ProjectId, documentId, topLevelProjects: false, isFullLookup_ForTestingPurposesOnly: false);
+    public static implicit operator AssetPath(ProjectId projectId) => new(kind: AssetPathKind.ProjectOrDocument, projectId, documentId: null);
+    public static implicit operator AssetPath(DocumentId documentId) => new(kind: AssetPathKind.ProjectOrDocument, documentId.ProjectId, documentId);
+
+    private enum AssetPathKind
+    {
+        Solution = 0,
+        SolutionAndTopLevelProjects = 1,
+        FullLookupForTests = 2,
+        ProjectOrDocument = 3,
+    }
 }
