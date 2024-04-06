ef22741238962ebf5aaa1ac33ae6d8dfdaeb0b5c
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-05 17:47:59 -0700

    use collection expr

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/LanguageServices/TypeInferenceService/AbstractTypeInferenceService.AbstractTypeInferrer.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/LanguageServices/TypeInferenceService/AbstractTypeInferenceService.AbstractTypeInferrer.cs
@@ -84,9 +84,7 @@ internal partial class AbstractTypeInferenceService : ITypeInferenceService
             => CreateResult(Compilation.GetSpecialType(type).WithNullableAnnotation(nullableAnnotation));
 
         protected static IEnumerable<TypeInferenceInfo> CreateResult(ITypeSymbol type)
-            => type == null
-                ? SpecializedCollections.EmptyCollection<TypeInferenceInfo>()
-                : SpecializedCollections.SingletonEnumerable(new TypeInferenceInfo(type));
+            => type == null ? [] : [new TypeInferenceInfo(type)];
 
         protected static IEnumerable<ITypeSymbol> ExpandParamsParameter(IParameterSymbol parameterSymbol)
         {
