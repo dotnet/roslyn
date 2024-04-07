d63de5af844a2581183867c8f0610b31036cadda
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-07 10:20:17 -0700

    Add docs

--- src/Workspaces/Core/Portable/Workspace/Solution/AssetHint.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/AssetHint.cs
@@ -19,39 +19,30 @@ internal readonly struct AssetPath
     /// <summary>
     /// Instance that will only look up solution-level data when searching for checksums.
     /// </summary>
-    public static readonly AssetPath SolutionOnly = new(AssetPathKind.Solution, forTesting: false);
+    public static readonly AssetPath SolutionOnly = new(AssetPathKind.Solution);
 
     /// <summary>
     /// Instance that will only look up solution-level, as well as the top level nodes for projects when searching for
     /// checksums.  It will not descend into projects.
     /// </summary>
-    public static readonly AssetPath SolutionAndTopLevelProjectsOnly = new(AssetPathKind.Solution | AssetPathKind.TopLevelProjects, forTesting: false);
+    public static readonly AssetPath SolutionAndTopLevelProjectsOnly = new(AssetPathKind.Solution | AssetPathKind.TopLevelProjects);
 
     /// <summary>
     /// Special instance, allowed only in tests/debug-asserts, that can do a full lookup across the entire checksum
     /// tree.  Should not be used in normal release-mode product code.
     /// </summary>
-    public static readonly AssetPath FullLookupForTesting = new(AssetPathKind.Solution | AssetPathKind.TopLevelProjects | AssetPathKind.Projects | AssetPathKind.Documents, forTesting: true);
+    public static readonly AssetPath FullLookupForTesting = new(AssetPathKind.Solution | AssetPathKind.TopLevelProjects | AssetPathKind.Projects | AssetPathKind.Documents | AssetPathKind.Testing);
 
     [DataMember(Order = 0)]
     private readonly AssetPathKind _kind;
-#pragma warning disable IDE0052 // Remove unread private members
-    /// <summary>
-    /// Indicates if this was a full-search done by a test utility.  Useful when debugging tests to know what sort of
-    /// caller was doing the search.
-    /// </summary>
     [DataMember(Order = 1)]
-    private readonly bool _forTesting;
-#pragma warning restore IDE0052 // Remove unread private members
-    [DataMember(Order = 2)]
     public readonly ProjectId? ProjectId;
-    [DataMember(Order = 3)]
+    [DataMember(Order = 2)]
     public readonly DocumentId? DocumentId;
 
-    private AssetPath(AssetPathKind kind, bool forTesting, ProjectId? projectId = null, DocumentId? documentId = null)
+    private AssetPath(AssetPathKind kind, ProjectId? projectId = null, DocumentId? documentId = null)
     {
         _kind = kind;
-        _forTesting = forTesting;
         ProjectId = projectId;
         DocumentId = documentId;
     }
@@ -64,12 +55,12 @@ internal readonly struct AssetPath
     /// <summary>
     /// Searches only for information about this project.
     /// </summary>
-    public static implicit operator AssetPath(ProjectId projectId) => new(AssetPathKind.Projects, forTesting: false, projectId, documentId: null);
+    public static implicit operator AssetPath(ProjectId projectId) => new(AssetPathKind.Projects, projectId, documentId: null);
 
     /// <summary>
     /// Searches only for information about this document.
     /// </summary>
-    public static implicit operator AssetPath(DocumentId documentId) => new(AssetPathKind.Documents, forTesting: false, documentId.ProjectId, documentId);
+    public static implicit operator AssetPath(DocumentId documentId) => new(AssetPathKind.Documents, documentId.ProjectId, documentId);
 
     /// <summary>
     /// Searches the requested project, and all documents underneath it.  Used only in tests.
@@ -77,7 +68,7 @@ internal readonly struct AssetPath
     /// <param name="projectId"></param>
     /// <returns></returns>
     public static AssetPath SolutionAndProjectForTesting(ProjectId projectId)
-        => new(AssetPathKind.Solution | AssetPathKind.Projects, forTesting: true, projectId);
+        => new(AssetPathKind.Solution | AssetPathKind.Projects | AssetPathKind.Testing, projectId);
 
     /// <summary>
     /// Searches the requested project, and all documents underneath it.  used during normal sync when bulk syncing a
@@ -86,14 +77,36 @@ internal readonly struct AssetPath
     /// <param name="projectId"></param>
     /// <returns></returns>
     public static AssetPath ProjectAndDocuments(ProjectId projectId)
-        => new(AssetPathKind.Projects | AssetPathKind.Documents, forTesting: false, projectId);
+        => new(AssetPathKind.Projects | AssetPathKind.Documents, projectId);
 
     [Flags]
     private enum AssetPathKind
     {
+        /// <summary>
+        /// Search solution-level information.
+        /// </summary>
         Solution = 1 << 0,
+
+        /// <summary>
+        /// Search projects, with descending into them. In effect, only finding direct ProjectStateChecksum children of
+        /// the solution.
+        /// </summary>
         TopLevelProjects = 1 << 1,
+
+        /// <summary>
+        /// Search projects for results.
+        /// </summary>
         Projects = 1 << 2,
+
+        /// <summary>
+        /// Search documents for results.
+        /// </summary>
         Documents = 1 << 3,
+
+        /// <summary>
+        /// Indicates that this is a special search performed during testing.  These searches are allowed to search
+        /// everything for expediency purposes.
+        /// </summary>
+        Testing = 1 << 4,
     }
 }
