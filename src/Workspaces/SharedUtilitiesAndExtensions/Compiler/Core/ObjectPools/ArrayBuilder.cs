d19c082a21d8ab54398c4c21cd6c2214dbf2e34b
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-09 13:49:40 -0700

    Move to existing extension location

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/ObjectPools/ArrayBuilder.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/ObjectPools/ArrayBuilder.cs
@@ -26,33 +26,4 @@ internal partial class ArrayBuilder<T> : IPooled
         instance = GetInstance(capacity, fillWithValue);
         return new PooledDisposer<ArrayBuilder<T>>(instance);
     }
-
-    public bool HasDuplicates()
-        => HasDuplicates(static x => x);
-
-    public bool HasDuplicates<U>(Func<T, U> selector)
-    {
-        switch (this.Count)
-        {
-            case 0:
-            case 1:
-                return false;
-
-            case 2:
-                return EqualityComparer<U>.Default.Equals(selector(this[0]), selector(this[1]));
-
-            default:
-                {
-                    using var _ = PooledHashSet<U>.GetInstance(out var set);
-
-                    foreach (var element in this)
-                    {
-                        if (!set.Add(selector(element)))
-                            return true;
-                    }
-
-                    return false;
-                }
-        }
-    }
 }
