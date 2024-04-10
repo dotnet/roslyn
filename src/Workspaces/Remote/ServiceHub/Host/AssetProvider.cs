d140c85924b80146ebd8ed3f4421f93ef3215d19
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-10 15:17:08 -0700

    delete

--- src/Workspaces/Remote/ServiceHub/Host/AssetProvider.cs
+++ src/Workspaces/Remote/ServiceHub/Host/AssetProvider.cs
@@ -206,9 +206,6 @@ internal sealed partial class AssetProvider(Checksum solutionChecksum, SolutionA
             AddAll(checksums, projectChecksums.AdditionalDocuments.Checksums);
             AddAll(checksums, projectChecksums.AnalyzerConfigDocuments.Checksums);
 
-            await this.SynchronizeAssetsAsync<DocumentStateChecksums, VoidResult>(
-                AssetPath.ProjectAndDocuments(projectChecksums.ProjectId), checksums, callback: null, arg: default, cancellationToken).ConfigureAwait(false);
-
             // First, fetch all the DocumentStateChecksums for all the documents in the project.
             using var _2 = ArrayBuilder<DocumentStateChecksums>.GetInstance(out var allDocumentStateChecksums);
             await this.SynchronizeAssetsAsync<DocumentStateChecksums, ArrayBuilder<DocumentStateChecksums>>(
