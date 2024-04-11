618712a108f3e79ef964cf82f466bc4f15463183
Todd Grunke <toddgrun@microsoft.com>
2024-04-11 14:16:42 -0700

    Def allocating array and frozen dictionary until used.

--- src/Workspaces/Core/Portable/Workspace/Solution/ProjectIdMapping.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/ProjectIdMapping.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Frozen;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 using Microsoft.CodeAnalysis.PooledObjects;
@@ -16,7 +17,7 @@ namespace Microsoft.CodeAnalysis;
 /// Population of values are done through calls to GetOrAdd.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-internal struct ProjectIdMapping<T>
+internal sealed class ProjectIdMapping<T>
     where T : class
 {
     public static ProjectIdMapping<T> Empty = new([], FrozenDictionary<ProjectId, int>.Empty);
@@ -24,12 +25,12 @@ internal struct ProjectIdMapping<T>
     /// <summary>
     /// Mapping from ProjectId to index in _projects
     /// </summary>
-    private readonly FrozenDictionary<ProjectId, int> _projectIdToIndex;
+    private FrozenDictionary<ProjectId, int>? _projectIdToIndex;
 
     /// <summary>
     /// Values are created on demand by calls to <see cref="GetOrAdd{TArg}(ProjectId?, Func{ProjectId, TArg, T}, TArg)" />
     /// </summary>
-    private readonly T?[] _projects;
+    private T?[]? _projects;
 
     /// <summary>
     /// Domain of ProjectIds for which values may be associated. Set equivalent to <see cref="_projectIdToIndex"/>'s keys.
@@ -38,48 +39,26 @@ internal struct ProjectIdMapping<T>
     /// </summary>
     private readonly IReadOnlyList<ProjectId> _projectIds;
 
-    private ProjectIdMapping(IReadOnlyList<ProjectId> projectIds, FrozenDictionary<ProjectId, int> projectIdToIndex)
+    private ProjectIdMapping(IReadOnlyList<ProjectId> projectIds, FrozenDictionary<ProjectId, int>? projectIdToIndex)
     {
         _projectIds = projectIds;
         _projectIdToIndex = projectIdToIndex;
-        _projects = new T?[_projectIds.Count];
     }
 
     /// <summary>
     /// Returns a new ProjectIdMapping whose keys are limited to the specified project ids.
     /// </summary>
-    public readonly ProjectIdMapping<T> WithProjectIds(IReadOnlyList<ProjectId> projectIds)
+    public ProjectIdMapping<T> WithProjectIds(IReadOnlyList<ProjectId> projectIds)
     {
-        var projectIdToIndex = _projectIdToIndex;
-
         // If the old project ids are not equivalent to the set passed in, then we will
         // build a frozen dictionary from the new project ids.
         var canReuseIndexes = AreEquivalent(projectIds, _projectIds);
-        if (!canReuseIndexes)
-        {
-            using var _ = PooledDictionary<ProjectId, int>.GetInstance(out var dict);
-
-            for (int i = 0, n = projectIds.Count; i < n; i++)
-            {
-                var projectId = projectIds[i];
-                dict[projectId] = i;
-            }
-
-            projectIdToIndex = dict.ToFrozenDictionary();
-        }
+        var projectIdToIndex = canReuseIndexes ? _projectIdToIndex : null;
 
         return new(projectIds, projectIdToIndex);
 
         static bool AreEquivalent(IReadOnlyList<ProjectId> originalIds, IReadOnlyList<ProjectId> newIds)
-        {
-            if (object.ReferenceEquals(originalIds, newIds))
-                return true;
-
-            if (originalIds.Count != newIds.Count)
-                return false;
-
-            return originalIds.SequenceEqual(newIds);
-        }
+            => ReferenceEquals(originalIds, newIds);
     }
 
     /// <summary>
@@ -93,24 +72,39 @@ internal struct ProjectIdMapping<T>
     /// project id.</returns>
     public T? GetOrAdd<TArg>(ProjectId? projectId, Func<ProjectId, TArg, T> createValue, TArg arg)
     {
+        EnsureInitialized();
+
         if (projectId is null || !_projectIdToIndex.TryGetValue(projectId, out var projectIndex))
             return null;
 
-        var project = _projects[projectIndex];
-        if (project == null)
+        if (_projects[projectIndex] == null)
         {
             // Create the actual project. Note that it's ok if this isn't the project actually used
-            // if someone updates _projects at this index before we do.
-            project = createValue(projectId, arg);
-
-            if (null != Interlocked.CompareExchange(ref _projects[projectIndex], project, null))
-            {
-                // If someone else injected themselves into the array at this index before us,
-                //  use the project they created.
-                project = _projects[projectIndex];
-            }
+            // if someone updates _projects[index] before we do.
+            Interlocked.CompareExchange(ref _projects[projectIndex], createValue(projectId, arg), null);
         }
 
-        return project;
+        return _projects[projectIndex];
+    }
+
+    [MemberNotNull(nameof(_projects))]
+    [MemberNotNull(nameof(_projectIdToIndex))]
+    private void EnsureInitialized()
+    {
+        if (_projects is null)
+            Interlocked.CompareExchange(ref _projects, new T?[_projectIds.Count], null);
+
+        if (_projectIdToIndex is null)
+        {
+            using var _ = PooledDictionary<ProjectId, int>.GetInstance(out var projectIdToIndex);
+
+            for (int i = 0, n = _projectIds.Count; i < n; i++)
+            {
+                var projectId = _projectIds[i];
+                projectIdToIndex[projectId] = i;
+            }
+
+            Interlocked.CompareExchange(ref _projectIdToIndex, projectIdToIndex.ToFrozenDictionary(), null);
+        }
     }
 }
