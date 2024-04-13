246cbebaa5d34637a49ff3f16fc10213bb83602b
DoctorKrolic <mapmyp03@gmail.com>
2024-04-13 15:05:49 +0300

    Add 'Make anonymous function static' feature

---
+++ src/Analyzers/CSharp/Analyzers/MakeAnonymousFunctionStatic/MakeAnonymousFunctionStaticDiagnosticAnalyzer.cs
@@ -0,0 +1,71 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.CodeStyle;
+using Microsoft.CodeAnalysis.CSharp.Extensions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeAnalysis.CSharp.MakeAnonymousFunctionStatic;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+internal sealed class MakeAnonymousFunctionStaticDiagnosticAnalyzer : AbstractBuiltInCodeStyleDiagnosticAnalyzer
+{
+    public MakeAnonymousFunctionStaticDiagnosticAnalyzer()
+        : base(IDEDiagnosticIds.MakeAnonymousFunctionStaticDiagnosticId,
+               EnforceOnBuildValues.MakeAnonymousFunctionStatic,
+               CSharpCodeStyleOptions.PreferStaticAnonymousFunction,
+               new LocalizableResourceString(nameof(CSharpAnalyzersResources.Make_anonymous_function_static), CSharpAnalyzersResources.ResourceManager, typeof(CSharpAnalyzersResources)),
+               new LocalizableResourceString(nameof(CSharpAnalyzersResources.Anonymous_function_can_be_made_static), CSharpAnalyzersResources.ResourceManager, typeof(CSharpAnalyzersResources)))
+    {
+    }
+
+    public override DiagnosticAnalyzerCategory GetAnalyzerCategory()
+        => DiagnosticAnalyzerCategory.SemanticSpanAnalysis;
+
+    protected override void InitializeWorker(AnalysisContext context)
+    {
+        context.RegisterCompilationStartAction(context =>
+        {
+            if (context.Compilation.LanguageVersion() < LanguageVersion.CSharp9)
+                return;
+
+            context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.SimpleLambdaExpression, SyntaxKind.ParenthesizedLambdaExpression, SyntaxKind.AnonymousMethodExpression);
+        });
+    }
+
+    private void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+    {
+        var option = context.GetCSharpAnalyzerOptions().PreferStaticAnonymousFunction;
+        if (!option.Value || ShouldSkipAnalysis(context, option.Notification))
+            return;
+
+        var anonymousFunction = (AnonymousFunctionExpressionSyntax)context.Node;
+        if (anonymousFunction.Modifiers.Any(SyntaxKind.StaticKeyword))
+            return;
+
+        if (TryGetCaptures(anonymousFunction, context.SemanticModel, out var captures) && captures.IsEmpty)
+        {
+            context.ReportDiagnostic(
+                Diagnostic.Create(
+                    Descriptor,
+                    anonymousFunction.GetLocation()));
+        }
+    }
+
+    private static bool TryGetCaptures(AnonymousFunctionExpressionSyntax anonymousFunction, SemanticModel semanticModel, out ImmutableArray<ISymbol> captures)
+    {
+        var dataFlow = semanticModel.AnalyzeDataFlow(anonymousFunction);
+        if (dataFlow is null)
+        {
+            captures = default;
+            return false;
+        }
+
+        captures = dataFlow.Captured;
+        return dataFlow.Succeeded;
+    }
+}
--- src/VisualStudio/CSharp/Impl/Options/Formatting/StyleViewModel.cs
+++ src/VisualStudio/CSharp/Impl/Options/Formatting/StyleViewModel.cs
@@ -1548,6 +1548,23 @@ class Customer2
 }}
 ";
 
+        private static readonly string s_preferStaticAnonymousFunction = $$"""
+            using System;
+
+            //[
+            // {{ServicesVSResources.Prefer_colon}}
+            Func<int, int> f1 = static i => i * i;
+            Func<int, int> f2 = static (i, j) => i * j;
+            Func<int, int> f3 = static delegate (int i) { return i * i; };
+            //]
+            //[
+            // {{ServicesVSResources.Over_colon}}
+            Func<int, int> f1 = i => i * i;
+            Func<int, int> f2 = (i, j) => i * j;
+            Func<int, int> f3 = delegate (int i) { return i * i; };
+            //]
+            """;
+
         #endregion
 
         #region New Line Preferences
@@ -2292,6 +2309,7 @@ class C2
             CodeStyleItems.Add(new BooleanCodeStyleOptionViewModel(CodeStyleOptions2.PreferReadonly, ServicesVSResources.Prefer_readonly_fields, s_preferReadonly, s_preferReadonly, this, optionStore, modifierGroupTitle));
             CodeStyleItems.Add(new BooleanCodeStyleOptionViewModel(CSharpCodeStyleOptions.PreferReadOnlyStruct, ServicesVSResources.Prefer_read_only_struct, s_preferReadOnlyStruct, s_preferReadOnlyStruct, this, optionStore, modifierGroupTitle));
             CodeStyleItems.Add(new BooleanCodeStyleOptionViewModel(CSharpCodeStyleOptions.PreferStaticLocalFunction, ServicesVSResources.Prefer_static_local_functions, s_preferStaticLocalFunction, s_preferStaticLocalFunction, this, optionStore, modifierGroupTitle));
+            CodeStyleItems.Add(new BooleanCodeStyleOptionViewModel(CSharpCodeStyleOptions.PreferStaticAnonymousFunction, ServicesVSResources.Prefer_static_anonymous_functions, s_preferStaticAnonymousFunction, s_preferStaticAnonymousFunction, this, optionStore, modifierGroupTitle));
 
             // Parameter preferences
             AddParameterOptions(optionStore, parameterPreferencesGroupTitle);
