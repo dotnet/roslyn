5c93961c314c9f30f4647c90733c8d092eef8617
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-14 10:08:43 -0700

    remove unused methods

--- src/Workspaces/Core/Portable/Serialization/PooledList.cs
+++ src/Workspaces/Core/Portable/Serialization/PooledList.cs
@@ -27,11 +27,4 @@ internal static class Creator
 
     public static PooledObject<List<T>> CreateList<T>()
         => SharedPools.Default<List<T>>().GetPooledObject();
-
-    public static PooledObject<Dictionary<Checksum, object>> CreateResultMap(out Dictionary<Checksum, object> result)
-    {
-        var pooled = SharedPools.Default<Dictionary<Checksum, object>>().GetPooledObject();
-        result = pooled.Object;
-        return pooled;
-    }
 }
