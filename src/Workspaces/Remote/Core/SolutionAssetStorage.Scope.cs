768128265ddcc010b83c2412c17c6bfcc85ec0b6
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-14 11:37:07 -0700

    Pass along generic args to allow for non-allocating lambda lookup paths

--- src/Workspaces/Remote/Core/SolutionAssetStorage.Scope.cs
+++ src/Workspaces/Remote/Core/SolutionAssetStorage.Scope.cs
@@ -60,15 +60,16 @@ internal partial class SolutionAssetStorage
 
             await FindAssetsAsync(
                 assetPath, checksumsToFind,
-                (checksum, asset) => assetMap[checksum] = asset,
+                static (checksum, asset, assetMap) => assetMap[checksum] = asset,
+                assetMap,
                 cancellationToken).ConfigureAwait(false);
 
             Contract.ThrowIfTrue(checksumsToFind.Count > 0);
             Contract.ThrowIfTrue(assetMap.Count != numberOfChecksumsToSearch);
         }
 
-        private async Task FindAssetsAsync(
-            AssetPath assetPath, HashSet<Checksum> remainingChecksumsToFind, Action<Checksum, object> onAssetFound, CancellationToken cancellationToken)
+        private async Task FindAssetsAsync<TArg>(
+            AssetPath assetPath, HashSet<Checksum> remainingChecksumsToFind, Action<Checksum, object, TArg> onAssetFound, TArg arg, CancellationToken cancellationToken)
         {
             var solutionState = this.CompilationState;
 
@@ -77,13 +78,13 @@ internal partial class SolutionAssetStorage
                 // If we're not in a project cone, start the search at the top most state-checksum corresponding to the
                 // entire solution.
                 Contract.ThrowIfFalse(solutionState.TryGetStateChecksums(out var stateChecksums));
-                await stateChecksums.FindAsync(solutionState, this.ProjectCone, assetPath, remainingChecksumsToFind, onAssetFound, cancellationToken).ConfigureAwait(false);
+                await stateChecksums.FindAsync(solutionState, this.ProjectCone, assetPath, remainingChecksumsToFind, onAssetFound, arg, cancellationToken).ConfigureAwait(false);
             }
             else
             {
                 // Otherwise, grab the top-most state checksum for this cone and search within that.
                 Contract.ThrowIfFalse(solutionState.TryGetStateChecksums(this.ProjectCone.RootProjectId, out var stateChecksums));
-                await stateChecksums.FindAsync(solutionState, this.ProjectCone, assetPath, remainingChecksumsToFind, onAssetFound, cancellationToken).ConfigureAwait(false);
+                await stateChecksums.FindAsync(solutionState, this.ProjectCone, assetPath, remainingChecksumsToFind, onAssetFound, arg, cancellationToken).ConfigureAwait(false);
             }
         }
 
