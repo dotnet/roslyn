e71f1c6105a41e20c90f85923128cee11be73c7d
David Wengier <david.wengier@microsoft.com>
2024-04-15 11:12:41 +1000

    Remove everything unnecessary now that there isn't a Cohost specific server

--- src/Tools/ExternalAccess/Razor/Cohost/Constants.cs
+++ src/Tools/ExternalAccess/Razor/Cohost/Constants.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.LanguageServer;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
@@ -10,10 +9,4 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
 internal static class Constants
 {
     public const string RazorLanguageName = LanguageInfoProvider.RazorLanguageName;
-
-    public const string RazorLSPContentType = "Razor";
-
-    public const string RazorLanguageContract = ProtocolConstants.RazorCohostContract;
-
-    public static readonly ImmutableArray<string> RazorLanguage = ImmutableArray.Create("Razor");
 }
