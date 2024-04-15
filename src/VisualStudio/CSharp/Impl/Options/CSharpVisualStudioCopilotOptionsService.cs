c9a79cf996c7bc5ef02eb2e385e5cc795a50e03d
gel@microsoft.com <gel@microsoft.com>
2024-04-15 15:53:11 -0700

    fix

--- src/VisualStudio/CSharp/Impl/Options/CSharpVisualStudioCopilotOptionsService.cs
+++ src/VisualStudio/CSharp/Impl/Options/CSharpVisualStudioCopilotOptionsService.cs
@@ -18,12 +18,20 @@ namespace Microsoft.VisualStudio.LanguageServices.CSharp.Options;
 [ExportLanguageService(typeof(ICopilotOptionsService), LanguageNames.CSharp), Shared]
 internal sealed class CSharpVisualStudioCopilotOptionsService : ICopilotOptionsService
 {
+    /// <summary>
+    /// Guid for UI context that is set from Copilot when the package is initialized
+    /// </summary>
+    private const string CopilotHasLoadedGuid = "871c3e1c-e58c-4ce9-b6a7-26600555739a";
 
     /// <summary>
     /// Guid for UI Context that is set from Copilot when sign in related UI contexts have been properly. Used to determine when UI context status is final
     /// for a set of operations. When this UI context is not active, the signed in and entitled contexts values may not be correct.
     /// </summary>
     /// <remarks>
+    /// This UI context should always be set to false at the beginning of other UI context operations and then to true after operations finish.
+    /// This ensures that the UI contexts are only read when they are finalized and not after each UI context is set.
+    /// </remarks>
+    private const string GitHubAccountStatusDetermined = "3049be7e-71ee-4045-a510-f8ee1a967723";
 
     /// <summary>
     /// Guid for UI context that is set from Copilot when we detect a GitHub account is signed in.
@@ -31,18 +39,14 @@ internal sealed class CSharpVisualStudioCopilotOptionsService : ICopilotOptionsS
     private const string GitHubAccountStatusSignedIn = "ef3ebbb7-511d-472c-ae4b-6af1bb44f378";
 
     /// <summary>
-    /// Guid for UI context that is set from Copilot when the package is initialized
-    /// </summary>
-    private const string CopilotHasLoadedGuid = "871c3e1c-e58c-4ce9-b6a7-26600555739a";
-
-    /// <summary>
     /// Guid for UI context that is set from VS Identity Service when we detect that a signed in GitHub account is entitled to access Copilot.
     /// </summary>
     private const string GitHubAccountStatusIsCopilotEntitled = "3DE3FA6E-91B2-46C1-9E9E-DD04975BB890";
 
+    private static readonly UIContext s_copilotHasLoadedUIContext = UIContext.FromUIContextGuid(new Guid(CopilotHasLoadedGuid));
+    private static readonly UIContext s_gitHubAccountStatusDeterminedContext = UIContext.FromUIContextGuid(new Guid(GitHubAccountStatusDetermined));
     private static readonly UIContext s_gitHubAccountStatusIsCopilotEntitledUIContext = UIContext.FromUIContextGuid(new Guid(GitHubAccountStatusIsCopilotEntitled));
     private static readonly UIContext s_gitHubAccountStatusSignedInUIContext = UIContext.FromUIContextGuid(new Guid(GitHubAccountStatusSignedIn));
-    private static readonly UIContext s_copilotHasLoadedUIContext = UIContext.FromUIContextGuid(new Guid(CopilotHasLoadedGuid));
 
     private const string CopilotOptionNamePrefix = "Microsoft.VisualStudio.Conversations";
     private const string CopilotCodeAnalysisOptionName = "EnableCSharpCodeAnalysis";
@@ -55,6 +59,7 @@ internal sealed class CSharpVisualStudioCopilotOptionsService : ICopilotOptionsS
     /// </summary>
     private static bool IsGithubCopilotLoadedAndSignedIn
         => s_copilotHasLoadedUIContext.IsActive
+        && s_gitHubAccountStatusDeterminedContext.IsActive
         && s_gitHubAccountStatusSignedInUIContext.IsActive
         && s_gitHubAccountStatusIsCopilotEntitledUIContext.IsActive;
 
