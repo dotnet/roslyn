111ec282e7c86ca6cdf525672bd9569f3b8c9946
Jan Jones <janjones@microsoft.com>
2024-04-16 10:21:09 +0200

    Warn for `yield return` in `lock` (#72755)
    * Warn for `yield return` in `lock`
    
    * Encapsulate utility for filtering diagnostics
    
    * Filter warning for `yield return` in the new `lock`
    
    * Test `yield` in `lock`
    
    * Update existing test
    
    * Revert "Filter warning for `yield return` in the new `lock`"
    
    This reverts commit eafc5948c82c8b0cd28150dc68980a47d821157a.
    
    * Revert "Encapsulate utility for filtering diagnostics"
    
    This reverts commit 654d458b91692d7bab3066b8a16ed7bc31e64ad8.
    
    * Add a comment separating error and warning conditions
    
    * Update tests after removing warning filtering
    
    * Remove runtime-dependent output verification
    
    * Remove unused code from a test

--- src/Compilers/CSharp/Portable/Binder/Binder_Statements.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Statements.cs
@@ -268,6 +268,11 @@ namespace Microsoft.CodeAnalysis.CSharp
             {
                 Error(diagnostics, ErrorCode.ERR_YieldNotAllowedInScript, node.YieldKeyword);
             }
+            // NOTE: Error conditions should be checked above this point; only warning conditions below.
+            else if (this.Flags.Includes(BinderFlags.InLockBody))
+            {
+                Error(diagnostics, ErrorCode.WRN_BadYieldInLock, node.YieldKeyword);
+            }
 
             CheckRequiredLangVersionForIteratorMethods(node, diagnostics);
             return new BoundYieldReturnStatement(node, argument);
