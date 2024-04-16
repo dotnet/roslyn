7ea7c174a29eda91e3a8131ad46c88bd1239a171
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-16 16:12:17 -0700

    Use a simpler pattern when working with stacks

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Services/SyntaxFacts/ISyntaxFactsExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Services/SyntaxFacts/ISyntaxFactsExtensions.cs
@@ -49,9 +49,9 @@ internal static class ISyntaxFactsExtensions
     private static bool IsOnSingleLine(
         ISyntaxFacts syntaxFacts, Stack<(SyntaxNodeOrToken nodeOrToken, bool leading, bool trailing)> stack)
     {
-        while (stack.Count > 0)
+        while (stack.TryPop(out var tuple))
         {
-            var (currentNodeOrToken, currentLeading, currentTrailing) = stack.Pop();
+            var (currentNodeOrToken, currentLeading, currentTrailing) = tuple;
             if (currentNodeOrToken.IsToken)
             {
                 // If this token isn't on a single line, then the original node definitely
