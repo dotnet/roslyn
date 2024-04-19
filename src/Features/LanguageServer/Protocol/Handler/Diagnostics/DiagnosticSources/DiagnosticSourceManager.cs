e3015603ce1733238891eaf829014db239dc1b2e
Evgeny Tvorun <evgenyt@microsoft.com>
2024-04-19 11:49:59 -0700

    Tweaks

--- src/Features/LanguageServer/Protocol/Handler/Diagnostics/DiagnosticSources/DiagnosticSourceManager.cs
+++ src/Features/LanguageServer/Protocol/Handler/Diagnostics/DiagnosticSources/DiagnosticSourceManager.cs
@@ -15,45 +15,36 @@ using Microsoft.CodeAnalysis.LanguageServer.Handler.Diagnostics.DiagnosticSource
 namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Diagnostics
 {
     [Export(typeof(DiagnosticSourceManager)), Shared]
-    [method: ImportingConstructor]
-    [method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-    internal class DiagnosticSourceManager([ImportMany] Lazy<IEnumerable<IDiagnosticSourceProvider>> sourceProviders) : IDiagnosticSourceManager
+    internal class DiagnosticSourceManager : IDiagnosticSourceManager
     {
-        private ImmutableDictionary<string, IDiagnosticSourceProvider>? _documentProviders;
-        private ImmutableDictionary<string, IDiagnosticSourceProvider>? _workspaceProviders;
+        private readonly Lazy<ImmutableDictionary<string, IDiagnosticSourceProvider>> _documentProviders;
+        private readonly Lazy<ImmutableDictionary<string, IDiagnosticSourceProvider>> _workspaceProviders;
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public DiagnosticSourceManager([ImportMany] Lazy<IEnumerable<IDiagnosticSourceProvider>> sourceProviders)
+        {
+            _documentProviders = new(() => sourceProviders.Value
+                    .Where(p => p.IsDocument)
+                    .ToImmutableDictionary(kvp => kvp.Name, kvp => kvp));
+
+            _workspaceProviders = new(() => sourceProviders.Value
+                    .Where(p => !p.IsDocument)
+                    .ToImmutableDictionary(kvp => kvp.Name, kvp => kvp));
+        }
 
         /// <inheritdoc />
         public IEnumerable<string> GetSourceNames(bool isDocument)
-        {
-            EnsureInitialized();
-            return (isDocument ? _documentProviders : _workspaceProviders)!.Keys;
-        }
+            => (isDocument ? _documentProviders : _workspaceProviders).Value.Keys;
 
         /// <inheritdoc />
         public ValueTask<ImmutableArray<IDiagnosticSource>> CreateDiagnosticSourcesAsync(RequestContext context, string sourceName, bool isDocument, CancellationToken cancellationToken)
         {
-            EnsureInitialized();
             var providersDictionary = isDocument ? _documentProviders : _workspaceProviders;
-            if (providersDictionary!.TryGetValue(sourceName, out var provider))
+            if (providersDictionary.Value.TryGetValue(sourceName, out var provider))
                 return provider.CreateDiagnosticSourcesAsync(context, cancellationToken);
 
             return new([]);
-        }
-
-        private void EnsureInitialized()
-        {
-            if (_documentProviders == null || _workspaceProviders == null)
-            {
-                var documentProviders = sourceProviders.Value
-                    .Where(p => p.IsDocument)
-                    .ToImmutableDictionary(kvp => kvp.Name, kvp => kvp);
-                Interlocked.CompareExchange(ref _documentProviders, documentProviders, null);
-
-                var workspaceProviders = sourceProviders.Value
-                    .Where(p => !p.IsDocument)
-                    .ToImmutableDictionary(kvp => kvp.Name, kvp => kvp);
-                Interlocked.CompareExchange(ref _workspaceProviders, workspaceProviders, null);
-            }
         }
     }
 }
