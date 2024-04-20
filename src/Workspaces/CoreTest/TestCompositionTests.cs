8c564823fc5bf2b5cfad7e31326eb42334631c9f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-19 20:40:52 -0700

    Specialize for windows

--- src/Workspaces/CoreTest/TestCompositionTests.cs
+++ src/Workspaces/CoreTest/TestCompositionTests.cs
@@ -2,14 +2,29 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Immutable;
+using System;
+using System.Composition;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.Test.Utilities;
-using Microsoft.CodeAnalysis.UnitTests.Persistence;
 using Roslyn.Test.Utilities;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.UnitTests
 {
+    [ExportWorkspaceServiceFactory(typeof(ITemporaryStorageServiceInternal), ServiceLayer.Test), Shared, PartNotDiscoverable]
+    internal sealed class TestTemporaryStorageServiceFactory : IWorkspaceServiceFactory
+    {
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public TestTemporaryStorageServiceFactory()
+        {
+        }
+
+        public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
+            => TrivialTemporaryStorageService.Instance;
+    }
+
     public class TestCompositionTests
     {
         [Fact]
