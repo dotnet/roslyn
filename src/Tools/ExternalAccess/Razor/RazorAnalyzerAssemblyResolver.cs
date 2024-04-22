55af5cf1576f362e70996657895af0bf99e9f9d0
Chris Sienkiewicz <chsienki@microsoft.com>
2024-04-22 14:39:21 -0700

    Add a specific Razor assembly resolver in external accces

---
+++ src/Tools/ExternalAccess/Razor/RazorAnalyzerAssemblyResolver.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using System.Reflection;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.VisualStudio.Composition;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
+{
+    [Export(typeof(IAnalyzerAssemblyResolver)), Shared]
+    internal class RazorAnalyzerAssemblyResolver : IAnalyzerAssemblyResolver
+    {
+        internal static Func<AssemblyName, Assembly?>? AssemblyResolver;
+
+        [ImportingConstructor]
+        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public RazorAnalyzerAssemblyResolver()
+        {
+        }
+
+        public Assembly? ResolveAssembly(AssemblyName assemblyName) => AssemblyResolver?.Invoke(assemblyName);
+    }
+}
