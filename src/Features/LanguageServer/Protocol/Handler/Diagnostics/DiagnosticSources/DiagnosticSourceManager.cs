a9e72d6d1a1efb941a25c1c8375271c6f3efe46a
Evgeny Tvorun <evgenyt@microsoft.com>
2024-04-23 15:11:03 -0700

    CR feedback

--- src/Features/LanguageServer/Protocol/Handler/Diagnostics/DiagnosticSources/DiagnosticSourceManager.cs
+++ src/Features/LanguageServer/Protocol/Handler/Diagnostics/DiagnosticSources/DiagnosticSourceManager.cs
@@ -48,8 +48,12 @@ internal class DiagnosticSourceManager : IDiagnosticSourceManager
         var providersDictionary = isDocument ? _documentProviders : _workspaceProviders;
         if (sourceName != null)
         {
-            Contract.ThrowIfFalse(providersDictionary.TryGetValue(sourceName, out var provider), $"Unrecognized source {sourceName}");
-            return await provider.CreateDiagnosticSourcesAsync(context, cancellationToken).ConfigureAwait(false);
+            // VS does not distinguish between document and workspace sources. Thus it can request
+            // document diagnostics with workspace source name. We need to handle this case.
+            if (providersDictionary.TryGetValue(sourceName, out var provider))
+                return await provider.CreateDiagnosticSourcesAsync(context, cancellationToken).ConfigureAwait(false);
+
+            return [];
         }
         else
         {
@@ -57,7 +61,7 @@ internal class DiagnosticSourceManager : IDiagnosticSourceManager
             using var _ = ArrayBuilder<IDiagnosticSource>.GetInstance(out var sourcesBuilder);
             foreach (var kvp in providersDictionary)
             {
-                // Exclude task diagnostics from the aggregated sources.
+                // Exclude Task diagnostics from the aggregated sources.
                 if (kvp.Key != PullDiagnosticCategories.Task)
                 {
                     var namedSources = await kvp.Value.CreateDiagnosticSourcesAsync(context, cancellationToken).ConfigureAwait(false);
@@ -66,32 +70,57 @@ internal class DiagnosticSourceManager : IDiagnosticSourceManager
             }
 
             var sources = sourcesBuilder.ToImmutableAndClear();
-            if (!isDocument || sources.Length <= 1)
+            if (sources.Length <= 1)
             {
                 return sources;
             }
+
+            if (isDocument)
+            {
+                // Group all document sources into a single source.
+                Debug.Assert(sources.All(s => s.IsLiveSource()), "All document sources should be live");
+                sources = [new AggregatedDocumentDiagnosticSource(sources)];
+            }
             else
             {
-                // VS Code document handler (and legacy VS ?) expects a single source for document diagnostics.
-                // For more details see PublicDocumentPullDiagnosticsHandler.CreateReturn.
-                Debug.Assert(sources.All(s => s.IsLiveSource()), "All document sources should be live");
-                return [new AggregatedDocumentDiagnosticSource(sources)];
+                // For workspace we need to group sources by document and IsLiveSource
+                sources = sources.GroupBy(s => (s.GetId(), s.IsLiveSource()), s => s)
+                    .SelectMany(g => AggregatedDocumentDiagnosticSource.AggregateIfNeeded(g))
+                    .ToImmutableArray();
             }
+
+            return sources;
         }
     }
 
-    private class AggregatedDocumentDiagnosticSource(ImmutableArray<IDiagnosticSource> sources) : IDiagnosticSource
+    private class AggregatedDocumentDiagnosticSource : IDiagnosticSource
     {
+        private readonly ImmutableArray<IDiagnosticSource> _sources;
+
+        public static ImmutableArray<IDiagnosticSource> AggregateIfNeeded(IEnumerable<IDiagnosticSource> sources)
+        {
+            var result = sources.ToImmutableArray();
+            if (result.Length > 1)
+            {
+                result = [new AggregatedDocumentDiagnosticSource(result)];
+            }
+
+            return result;
+        }
+
+        public AggregatedDocumentDiagnosticSource(ImmutableArray<IDiagnosticSource> sources)
+            => this._sources = sources;
+
         public bool IsLiveSource() => true;
-        public Project GetProject() => sources[0].GetProject();
-        public ProjectOrDocumentId GetId() => sources[0].GetId();
-        public TextDocumentIdentifier? GetDocumentIdentifier() => sources[0].GetDocumentIdentifier();
-        public string ToDisplayString() => $"{this.GetType().Name}: count={sources.Length}";
+        public Project GetProject() => _sources[0].GetProject();
+        public ProjectOrDocumentId GetId() => _sources[0].GetId();
+        public TextDocumentIdentifier? GetDocumentIdentifier() => _sources[0].GetDocumentIdentifier();
+        public string ToDisplayString() => $"{this.GetType().Name}: count={_sources.Length}";
 
         public async Task<ImmutableArray<DiagnosticData>> GetDiagnosticsAsync(RequestContext context, CancellationToken cancellationToken)
         {
             using var _ = ArrayBuilder<DiagnosticData>.GetInstance(out var diagnostics);
-            foreach (var source in sources)
+            foreach (var source in _sources)
             {
                 var namedDiagnostics = await source.GetDiagnosticsAsync(context, cancellationToken).ConfigureAwait(false);
                 diagnostics.AddRange(namedDiagnostics);
