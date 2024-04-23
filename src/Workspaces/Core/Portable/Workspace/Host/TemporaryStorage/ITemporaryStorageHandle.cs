eedc456250a4681b245e96237d56cbf7fc680a73
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-22 18:01:15 -0700

    cleanup

--- src/Workspaces/Core/Portable/Workspace/Host/TemporaryStorage/ITemporaryStorageHandle.cs
+++ src/Workspaces/Core/Portable/Workspace/Host/TemporaryStorage/ITemporaryStorageHandle.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.IO;
-using System.IO.MemoryMappedFiles;
 using System.Threading;
 
 namespace Microsoft.CodeAnalysis.Host;
