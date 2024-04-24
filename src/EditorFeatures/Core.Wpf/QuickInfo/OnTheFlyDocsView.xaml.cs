808e6b71aba7a3ae29772c0209a0d75e09dd6243
akhera99 <ankitakhera@microsoft.com>
2024-04-24 11:19:24 -0700

    more cleanup

--- src/EditorFeatures/Core.Wpf/QuickInfo/OnTheFlyDocsView.xaml.cs
+++ src/EditorFeatures/Core.Wpf/QuickInfo/OnTheFlyDocsView.xaml.cs
@@ -21,179 +21,177 @@ using Microsoft.VisualStudio.PlatformUI;
 using Microsoft.VisualStudio.Text.Adornments;
 using Microsoft.VisualStudio.Text.Editor;
 
-namespace Microsoft.CodeAnalysis.Editor.QuickInfo
+namespace Microsoft.CodeAnalysis.Editor.QuickInfo;
+
+/// <summary>
+/// Interaction logic for OnTheFlyDocsView.xaml.
+/// </summary>
+internal partial class OnTheFlyDocsView : UserControl, INotifyPropertyChanged
 {
+    private readonly ITextView _textView;
+    private readonly IViewElementFactoryService _viewElementFactoryService;
+    private readonly IThreadingContext _threadingContext;
+    private readonly ISymbol _symbol;
+    private readonly ContentControl _responseControl = new();
+    private State _currentState = State.OnDemandLink;
+
+    /// <inheritdoc/>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
     /// <summary>
-    /// Interaction logic for OnTheFlyDocsView.xaml.
+    /// Event that fires when the user requests results.
     /// </summary>
-    internal partial class OnTheFlyDocsView : UserControl, INotifyPropertyChanged
-    {
-        private readonly ITextView _textView;
-        private readonly IViewElementFactoryService _viewElementFactoryService;
-        private readonly IThreadingContext _threadingContext;
-        private readonly ISymbol _symbol;
-        private readonly ContentControl _responseControl = new();
-        private State _currentState = State.OnDemandLink;
-
-        /// <inheritdoc/>
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        /// <summary>
-        /// Event that fires when the user requests results.
-        /// </summary>
-        public event EventHandler ResultsRequested;
+    public event EventHandler ResultsRequested;
 
 #pragma warning disable CA1822 // Mark members as static
-        public string OnTheFlyDocumentation => EditorFeaturesResources.On_the_fly_documentation;
+    public string OnTheFlyDocumentation => EditorFeaturesResources.On_the_fly_documentation;
 #pragma warning restore CA1822 // Mark members as static
 
-        public OnTheFlyDocsView(ITextView textView, IViewElementFactoryService viewElementFactoryService, IThreadingContext threadingContext, Document document, ISymbol symbol, string descriptionText)
-        {
-            _textView = textView;
-            _viewElementFactoryService = viewElementFactoryService;
-            _threadingContext = threadingContext;
-            _symbol = symbol;
-            var sparkle = new ImageElement(new VisualStudio.Core.Imaging.ImageId(CopilotConstants.CopilotIconMonikerGuid, CopilotConstants.CopilotIconSparkleId));
+    public OnTheFlyDocsView(ITextView textView, IViewElementFactoryService viewElementFactoryService, IThreadingContext threadingContext, Document document, ISymbol symbol, string descriptionText)
+    {
+        _textView = textView;
+        _viewElementFactoryService = viewElementFactoryService;
+        _threadingContext = threadingContext;
+        _symbol = symbol;
+        var sparkle = new ImageElement(new VisualStudio.Core.Imaging.ImageId(CopilotConstants.CopilotIconMonikerGuid, CopilotConstants.CopilotIconSparkleId));
 
-            OnDemandLinkContent = ToUIElement(
-                new ContainerElement(
-                    ContainerElementStyle.Wrapped,
-                    new object[]
-                    {
-                        sparkle,
-                        ClassifiedTextElement.CreateHyperlink(EditorFeaturesResources.Tell_me_more, EditorFeaturesResources.Show_an_AI_generated_summary_of_this_code, () =>
-                        RequestResults()),
-                    }));
-
-            LoadingContent = ToUIElement(
-                new ContainerElement(
-                    ContainerElementStyle.Stacked,
-                    new object[]
-                    {
-                        new ClassifiedTextElement(new ClassifiedTextRun(
-                            ClassificationTypeDefinitions.ReducedEmphasisText, EditorFeaturesResources.GitHub_Copilot_thinking)),
-                        new SmoothProgressBar { IsIndeterminate = true, Height = 2, Margin = new Thickness { Top = 2 } },
-                    }));
-
-            // Ensure the loading content stretches so that the progress bar
-            // takes the entire width of the quick info tooltip.
-            if (LoadingContent is FrameworkElement element)
-            {
-                element.HorizontalAlignment = HorizontalAlignment.Stretch;
-            }
-
-            ResultsContent = ToUIElement(
-                new ContainerElement(
-                    ContainerElementStyle.Stacked,
-                    new object[]
-                    {
-                        new ContainerElement(
-                            ContainerElementStyle.Wrapped,
-                            new object[]
-                            {
-                                sparkle,
-                                ClassifiedTextElement.CreatePlainText(EditorFeaturesResources.GitHub_Copilot),
-                            }),
-                        new ThematicBreakElement(),
-                        _responseControl,
-                        new ThematicBreakElement(),
-                        new ClassifiedTextElement(new ClassifiedTextRun(
-                            ClassificationTypeDefinitions.ReducedEmphasisText, EditorFeaturesResources.AI_generated_content_may_be_inaccurate)),
-                    }));
-
-            ResultsRequested += (_, _) => PopulateAIDocumentationElements(threadingContext, document, symbol, descriptionText, _threadingContext.DisposalToken);
-            InitializeComponent();
-        }
-
-        /// <summary>
-        /// Retrieves the documentation for the given symbol from the Copilot service and displays it in the view.
-        /// </summary>
-        private async void PopulateAIDocumentationElements(IThreadingContext threadingContext, Document document, ISymbol symbol, string descriptionText, CancellationToken cancellationToken)
-        {
-            var copilotRequestTime = TimeSpan.Zero;
-            var copilotService = document.GetRequiredLanguageService<ICopilotCodeAnalysisService>();
-            var symbolStrings = symbol.DeclaringSyntaxReferences.Select(reference => reference.GetSyntax(cancellationToken).ToFullString()).ToImmutableArray();
-            var stopwatch = Stopwatch.StartNew();
-            var response = await copilotService.GetOnTheFlyDocsAsync(descriptionText, symbolStrings, cancellationToken).ConfigureAwait(false);
-            stopwatch.Stop();
-            copilotRequestTime = stopwatch.Elapsed;
-
-            await threadingContext.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            if (response is null || response.Length == 0)
-            {
-                SetResultText(EditorFeaturesResources.An_error_occurred_while_generating_documentation_for_this_code);
-                CurrentState = State.Finished;
-                Logger.Log(FunctionId.Copilot_On_The_Fly_Docs_Error_Displayed, KeyValueLogMessage.Create(m =>
+        OnDemandLinkContent = ToUIElement(
+            new ContainerElement(
+                ContainerElementStyle.Wrapped,
+                new object[]
                 {
-                    m["ElapsedTime"] = copilotRequestTime;
-                }, LogLevel.Information));
-            }
-            else
-            {
-                SetResultText(response);
-                CurrentState = State.Finished;
+                    sparkle,
+                    ClassifiedTextElement.CreateHyperlink(EditorFeaturesResources.Tell_me_more, EditorFeaturesResources.Show_an_AI_generated_summary_of_this_code, () =>
+                    RequestResults()),
+                }));
 
-                Logger.Log(FunctionId.Copilot_On_The_Fly_Docs_Error_Displayed, KeyValueLogMessage.Create(m =>
+        LoadingContent = ToUIElement(
+            new ContainerElement(
+                ContainerElementStyle.Stacked,
+                new object[]
                 {
-                    m["ElapsedTime"] = copilotRequestTime;
-                    m["ResponseLength"] = response.Length;
-                }, LogLevel.Information));
-            }
-        }
+                    new ClassifiedTextElement(new ClassifiedTextRun(
+                        ClassificationTypeDefinitions.ReducedEmphasisText, EditorFeaturesResources.GitHub_Copilot_thinking)),
+                    new SmoothProgressBar { IsIndeterminate = true, Height = 2, Margin = new Thickness { Top = 2 } },
+                }));
 
-        /// <summary>
-        /// Gets or sets the current state of the view.
-        /// </summary>
-        public State CurrentState
+        // Ensure the loading content stretches so that the progress bar
+        // takes the entire width of the quick info tooltip.
+        if (LoadingContent is FrameworkElement element)
         {
-            get => _currentState;
-            set => OnPropertyChanged(ref _currentState, value);
+            element.HorizontalAlignment = HorizontalAlignment.Stretch;
         }
 
-        /// <summary>
-        /// Gets the content of the on-demand link state of the view.
-        /// </summary>
-        public UIElement OnDemandLinkContent { get; }
+        ResultsContent = ToUIElement(
+            new ContainerElement(
+                ContainerElementStyle.Stacked,
+                new object[]
+                {
+                    new ContainerElement(
+                        ContainerElementStyle.Wrapped,
+                        new object[]
+                        {
+                            sparkle,
+                            ClassifiedTextElement.CreatePlainText(EditorFeaturesResources.GitHub_Copilot),
+                        }),
+                    new ThematicBreakElement(),
+                    _responseControl,
+                    new ThematicBreakElement(),
+                    new ClassifiedTextElement(new ClassifiedTextRun(
+                        ClassificationTypeDefinitions.ReducedEmphasisText, EditorFeaturesResources.AI_generated_content_may_be_inaccurate)),
+                }));
 
-        /// <summary>
-        /// Gets the content of the loading state of the view.
-        /// </summary>
-        public UIElement LoadingContent { get; }
-
-        /// <summary>
-        /// Gets the content of the results state of the view.
-        /// </summary>
-        public UIElement ResultsContent { get; }
-
-        public void RequestResults()
-        {
-            CurrentState = State.Loading;
-            Logger.Log(FunctionId.Copilot_On_The_Fly_Docs_Loading_State_Entered, KeyValueLogMessage.Create(m =>
-            {
-                m["SymbolName"] = _symbol.Name;
-            }, LogLevel.Information));
-            ResultsRequested?.Invoke(this, EventArgs.Empty);
-        }
-
-        /// <summary>
-        /// Sets the text of the AI-generated response.
-        /// </summary>
-        /// <param name="text">Response text to display.</param>
-        public void SetResultText(string text)
-        {
-            List<ClassifiedTextRun> runs = [new ClassifiedTextRun(ClassificationTypeNames.Text, text)];
-            //new ClassifiedRunsToolTipWrapper(runs).WrapAndTruncate(text);
-            _responseControl.Content = ToUIElement(
-                new ContainerElement(ContainerElementStyle.Wrapped, new ClassifiedTextElement(runs)));
-        }
-
-        private void OnPropertyChanged<T>(ref T member, T value, [CallerMemberName] string? name = null)
-        {
-            member = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-
-        private UIElement ToUIElement(object model)
-            => _viewElementFactoryService.CreateViewElement<UIElement>(_textView, model);
+        ResultsRequested += (_, _) => PopulateAIDocumentationElements(threadingContext, document, symbol, descriptionText, _threadingContext.DisposalToken);
+        InitializeComponent();
     }
+
+    /// <summary>
+    /// Retrieves the documentation for the given symbol from the Copilot service and displays it in the view.
+    /// </summary>
+    private async void PopulateAIDocumentationElements(IThreadingContext threadingContext, Document document, ISymbol symbol, string descriptionText, CancellationToken cancellationToken)
+    {
+        var copilotRequestTime = TimeSpan.Zero;
+        var copilotService = document.GetRequiredLanguageService<ICopilotCodeAnalysisService>();
+        var symbolStrings = symbol.DeclaringSyntaxReferences.Select(reference => reference.GetSyntax(cancellationToken).ToFullString()).ToImmutableArray();
+        var stopwatch = Stopwatch.StartNew();
+        var response = await copilotService.GetOnTheFlyDocsAsync(descriptionText, symbolStrings, cancellationToken).ConfigureAwait(false);
+        stopwatch.Stop();
+        copilotRequestTime = stopwatch.Elapsed;
+
+        await threadingContext.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        if (response is null || response.Length == 0)
+        {
+            SetResultText(EditorFeaturesResources.An_error_occurred_while_generating_documentation_for_this_code);
+            CurrentState = State.Finished;
+            Logger.Log(FunctionId.Copilot_On_The_Fly_Docs_Error_Displayed, KeyValueLogMessage.Create(m =>
+            {
+                m["ElapsedTime"] = copilotRequestTime;
+            }, LogLevel.Information));
+        }
+        else
+        {
+            SetResultText(response);
+            CurrentState = State.Finished;
+
+            Logger.Log(FunctionId.Copilot_On_The_Fly_Docs_Error_Displayed, KeyValueLogMessage.Create(m =>
+            {
+                m["ElapsedTime"] = copilotRequestTime;
+                m["ResponseLength"] = response.Length;
+            }, LogLevel.Information));
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the current state of the view.
+    /// </summary>
+    public State CurrentState
+    {
+        get => _currentState;
+        set => OnPropertyChanged(ref _currentState, value);
+    }
+
+    /// <summary>
+    /// Gets the content of the on-demand link state of the view.
+    /// </summary>
+    public UIElement OnDemandLinkContent { get; }
+
+    /// <summary>
+    /// Gets the content of the loading state of the view.
+    /// </summary>
+    public UIElement LoadingContent { get; }
+
+    /// <summary>
+    /// Gets the content of the results state of the view.
+    /// </summary>
+    public UIElement ResultsContent { get; }
+
+    public void RequestResults()
+    {
+        CurrentState = State.Loading;
+        Logger.Log(FunctionId.Copilot_On_The_Fly_Docs_Loading_State_Entered, KeyValueLogMessage.Create(m =>
+        {
+            m["SymbolName"] = _symbol.Name;
+        }, LogLevel.Information));
+
+        ResultsRequested?.Invoke(this, EventArgs.Empty);
+    }
+
+    /// <summary>
+    /// Sets the text of the AI-generated response.
+    /// </summary>
+    /// <param name="text">Response text to display.</param>
+    public void SetResultText(string text)
+    {
+        _responseControl.Content = ToUIElement(
+            new ContainerElement(ContainerElementStyle.Wrapped, new ClassifiedTextElement([new ClassifiedTextRun(ClassificationTypeNames.Text, text)])));
+    }
+
+    private void OnPropertyChanged<T>(ref T member, T value, [CallerMemberName] string? name = null)
+    {
+        member = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    private UIElement ToUIElement(object model)
+        => _viewElementFactoryService.CreateViewElement<UIElement>(_textView, model);
 }
