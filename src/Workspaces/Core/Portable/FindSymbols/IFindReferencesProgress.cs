5bcb1ca94e21848102be8897b2c0982f4fde2c52
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-27 15:57:32 -0700

    revert

--- src/Workspaces/Core/Portable/FindSymbols/IFindReferencesProgress.cs
+++ src/Workspaces/Core/Portable/FindSymbols/IFindReferencesProgress.cs
@@ -13,6 +13,9 @@ public interface IFindReferencesProgress
     void OnStarted();
     void OnCompleted();
 
+    void OnFindInDocumentStarted(Document document);
+    void OnFindInDocumentCompleted(Document document);
+
     void OnDefinitionFound(ISymbol symbol);
     void OnReferenceFound(ISymbol symbol, ReferenceLocation location);
 
