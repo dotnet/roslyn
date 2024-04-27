8a43b5cd232cd3f151d1a466ea557952ffd4c253
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-27 14:34:23 -0700

    Use same helper

--- src/Features/Core/Portable/NavigateTo/AbstractNavigateToSearchService.CachedDocumentSearch.cs
+++ src/Features/Core/Portable/NavigateTo/AbstractNavigateToSearchService.CachedDocumentSearch.cs
@@ -136,10 +136,8 @@ internal abstract partial class AbstractNavigateToSearchService
             var project = group.Key;
 
             // Break the project into high-pri docs and low pri docs, and process in that order.
-            var highPriDocs = group.Where(priorityDocumentKeysSet.Contains);
-            var lowPriDocs = group.Where(d => !highPriDocs.Contains(d));
             await ParallelForEachAsync(
-                highPriDocs.Concat(lowPriDocs),
+                Prioritize(group, priorityDocumentKeysSet.Contains),
                 cancellationToken,
                 async (documentKey, cancellationToken) =>
                 {
