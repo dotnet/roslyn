d30867d6f6885931a8775cbba12a8f80b5c4ed32
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-27 20:37:17 -0700

    Equals

--- src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.RootedSymbolSet.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.RootedSymbolSet.cs
@@ -132,7 +132,7 @@ internal partial class SolutionCompilationState
                     while (index < secondarySymbols.Length && secondarySymbols[index].hashCode == symbolHash)
                     {
                         var cached = secondarySymbols[index];
-                        if (cached.symbol == symbol)
+                        if (Equals(cached.symbol, symbol))
                         {
                             referencedThrough = cached.referenceInfo;
                             compilation = this.Compilation;
