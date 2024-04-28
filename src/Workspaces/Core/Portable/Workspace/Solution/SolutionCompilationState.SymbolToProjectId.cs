48fdc8e83c310e29d8572dd0870ce52babdef1f2
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-04-27 23:18:30 -0700

    restore

--- src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.SymbolToProjectId.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/SolutionCompilationState.SymbolToProjectId.cs
@@ -173,6 +173,14 @@ internal partial class SolutionCompilationState
 
             return projectId;
         }
+        else if (symbol.IsKind(SymbolKind.TypeParameter, out ITypeParameterSymbol? typeParameter) &&
+                 typeParameter.TypeParameterKind == TypeParameterKind.Cref)
+        {
+            // Cref type parameters don't belong to any containing symbol.  But we can map them to a doc/project
+            // using the declaring syntax of the type parameter itself.
+            if (GetDocumentState(typeParameter.Locations[0].SourceTree, projectId: null) is { } document)
+                return new OriginatingProjectInfo(document.Id.ProjectId, Compilation: null, ReferencedThrough: null);
+        }
 
         return null;
 
