bab1a3d66477ad81bfcb43e71342343281c3ec7f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-05-07 12:18:11 -0700

    Have code action tests run in OOP mode by default

--- src/EditorFeatures/DiagnosticsTestUtilities/CodeActions/AbstractCodeActionOrUserDiagnosticTest.cs
+++ src/EditorFeatures/DiagnosticsTestUtilities/CodeActions/AbstractCodeActionOrUserDiagnosticTest.cs
@@ -78,7 +78,7 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions
                 bool retainNonFixableDiagnostics = false,
                 bool includeDiagnosticsOutsideSelection = false,
                 string title = null,
-                TestHost testHost = TestHost.InProcess,
+                TestHost testHost = TestHost.OutOfProcess,
                 string workspaceKind = null,
                 bool includeNonLocalDocumentDiagnostics = false,
                 bool treatPositionIndicatorsAsCode = false)
@@ -414,7 +414,7 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions
             object fixProviderData = null,
             ParseOptions parseOptions = null,
             string title = null,
-            TestHost testHost = TestHost.InProcess)
+            TestHost testHost = TestHost.OutOfProcess)
         {
             return TestInRegularAndScript1Async(
                 initialMarkup, expectedMarkup,
@@ -454,7 +454,7 @@ namespace Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions
             OptionsCollectionAlias globalOptions = null,
             object fixProviderData = null,
             CodeActionPriority? priority = null,
-            TestHost testHost = TestHost.InProcess)
+            TestHost testHost = TestHost.OutOfProcess)
         {
             return TestAsync(
                 initialMarkup,
