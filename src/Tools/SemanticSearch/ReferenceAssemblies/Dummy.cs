d2d0c3b4f7f96963dc2e1b0ad7d591919df715c2
tmat <tomas.matousek@microsoft.com>
2024-05-08 09:18:35 -0700

    Add dummy type to SemanticSearch.ReferenceAssemblies

---
+++ src/Tools/SemanticSearch/ReferenceAssemblies/Dummy.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace SemanticSearch.ReferenceAssemblies;
+
+/// <summary>
+/// Dummy type to avoid having empty DLL.
+/// An experiment ot see if it would prevent locking of the intermediate output during build.
+/// </summary>
+internal class Dummy
+{
+}
