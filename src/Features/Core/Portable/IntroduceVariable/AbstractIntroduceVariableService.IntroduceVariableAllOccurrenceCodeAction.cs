a5ee41498c02f54e16d53c5d6572f6858233eb38
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-05-08 11:05:25 -0700

    Use helper

--- src/Features/Core/Portable/IntroduceVariable/AbstractIntroduceVariableService.IntroduceVariableAllOccurrenceCodeAction.cs
+++ src/Features/Core/Portable/IntroduceVariable/AbstractIntroduceVariableService.IntroduceVariableAllOccurrenceCodeAction.cs
@@ -2,14 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CaseCorrection;
 using Microsoft.CodeAnalysis.CodeCleanup;
-using Microsoft.CodeAnalysis.Formatting;
-using Microsoft.CodeAnalysis.Simplification;
 
 namespace Microsoft.CodeAnalysis.IntroduceVariable;
 
@@ -17,7 +12,7 @@ internal partial class AbstractIntroduceVariableService<TService, TExpressionSyn
 {
     private class IntroduceVariableAllOccurrenceCodeAction : AbstractIntroduceVariableCodeAction
     {
-        internal IntroduceVariableAllOccurrenceCodeAction(
+        public IntroduceVariableAllOccurrenceCodeAction(
             TService service,
             SemanticDocument document,
             CodeCleanupOptions options,
@@ -30,12 +25,7 @@ internal partial class AbstractIntroduceVariableService<TService, TExpressionSyn
         {
         }
 
-        protected override async Task<Document> PostProcessChangesAsync(Document document, CancellationToken cancellationToken)
-        {
-            document = await Simplifier.ReduceAsync(document, Simplifier.Annotation, Options.SimplifierOptions, cancellationToken).ConfigureAwait(false);
-            document = await Formatter.FormatAsync(document, Formatter.Annotation, Options.FormattingOptions, cancellationToken).ConfigureAwait(false);
-            document = await CaseCorrector.CaseCorrectAsync(document, CaseCorrector.Annotation, cancellationToken).ConfigureAwait(false);
-            return document;
-        }
+        protected override Task<Document> PostProcessChangesAsync(Document document, CancellationToken cancellationToken)
+            => CleanupSemanticsAsync(document, this.Options, cancellationToken);
     }
 }
