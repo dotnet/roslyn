c68a521f6a0312a8ec5324149c3b65bfdc4d1193
Andrew Hall <andrha@microsoft.com>
2024-05-15 14:26:45 -0700

    Usings

--- src/Features/LanguageServer/Protocol/Protocol/TextDocumentChangeRegistrationOptions.cs
+++ src/Features/LanguageServer/Protocol/Protocol/TextDocumentChangeRegistrationOptions.cs
@@ -2,14 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using Roslyn.LanguageServer.Protocol;
 
 namespace Roslyn.LanguageServer.Protocol
 {
