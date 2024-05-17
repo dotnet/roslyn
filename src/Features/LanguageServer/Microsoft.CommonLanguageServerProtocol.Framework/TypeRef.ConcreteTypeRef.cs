beca74459d4f3a1ea08afb78479a8164fcce9b99
Dustin Campbell <dustin@teamcampbell.org>
2024-05-17 15:42:25 -0700

    Fix up file headers

--- src/Features/LanguageServer/Microsoft.CommonLanguageServerProtocol.Framework/TypeRef.ConcreteTypeRef.cs
+++ src/Features/LanguageServer/Microsoft.CommonLanguageServerProtocol.Framework/TypeRef.ConcreteTypeRef.cs
@@ -1,4 +1,5 @@
-﻿// The .NET Foundation licenses this file to you under the MIT license.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 // This is consumed as 'generated' code in a source package and therefore requires an explicit nullable enable
