c085c3f02b99aa4bb52da02b2a8a11336b805fc9
Rikki Gibson <rigibson@microsoft.com>
2024-05-20 14:00:32 -0700

    Address feedback

--- src/Compilers/CSharp/Test/Symbol/Symbols/PartialPropertiesTests.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/PartialPropertiesTests.cs
@@ -5,8 +5,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
