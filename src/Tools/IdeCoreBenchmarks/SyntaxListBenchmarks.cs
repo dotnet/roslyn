7f29425feb6d3644eb06dc24fd3854a78f8be6a9
Todd Grunke <toddgrun@microsoft.com>
2024-05-22 17:35:16 -0700

    updated benchmarks

--- src/Tools/IdeCoreBenchmarks/SyntaxListBenchmarks.cs
+++ src/Tools/IdeCoreBenchmarks/SyntaxListBenchmarks.cs
@@ -4,7 +4,6 @@
 
 #nullable disable
 
-using System;
 using System.IO;
 using BenchmarkDotNet.Attributes;
 using Microsoft.CodeAnalysis;
@@ -19,54 +18,85 @@ namespace IdeCoreBenchmarks
         private SyntaxNode _smallRoot;
         private SyntaxNode _mediumRoot;
         private SyntaxNode _largeRoot;
-        private const int IterationCount = 1000;
+        private SourceText _smallText;
+        private SourceText _mediumText;
+        private SourceText _largeText;
+
+        private const int IterationCountWalkTree = 1000;
+        private const int IterationCountWalkLines = 10;
 
         [GlobalSetup]
         public void GlobalSetup()
         {
-            _smallRoot = GetRootFrom(@"d:\src\Roslyn2\src\Compilers\CSharp\Portable\Parser\AbstractLexer.cs");
-            _mediumRoot = GetRootFrom(@"d:\src\Roslyn2\src\Compilers\CSharp\Portable\Parser\SyntaxParser.cs");
-            _largeRoot = GetRootFrom(@"d:\src\Roslyn2\src\Compilers\CSharp\Portable\Parser\LanguageParser.cs");
+            (_smallRoot, _smallText) = GetRootAndTextFrom(@"d:\src\Roslyn2\src\Compilers\CSharp\Portable\Parser\AbstractLexer.cs");
+            (_mediumRoot, _mediumText) = GetRootAndTextFrom(@"d:\src\Roslyn2\src\Compilers\CSharp\Portable\Parser\SyntaxParser.cs");
+            (_largeRoot, _largeText) = GetRootAndTextFrom(@"d:\src\Roslyn2\src\Compilers\CSharp\Portable\Parser\LanguageParser.cs");
         }
 
-        private SyntaxNode GetRootFrom(string path)
+        private (SyntaxNode, SourceText) GetRootAndTextFrom(string path)
         {
             var textContents = File.ReadAllText(path);
 
             var text = SourceText.From(textContents);
             var tree = SyntaxFactory.ParseSyntaxTree(text);
 
-            return tree.GetCompilationUnitRoot();
+            return (tree.GetCompilationUnitRoot(), text);
+        }
+
+        [Benchmark]
+        public void WalkLines_Small()
+        {
+            WalkLines(_smallText, _smallRoot);
         }
 
         [Benchmark]
         public void WalkTree_Small()
         {
-            for (int i = 0; i < IterationCount; i++)
-            {
-                foreach (var token in _smallRoot.DescendantTokens())
-                {
-                }
-            }
+            WalkTree(_smallRoot);
+        }
+
+        [Benchmark]
+        public void WalkLines_Medium()
+        {
+            WalkLines(_mediumText, _mediumRoot);
         }
 
         [Benchmark]
         public void WalkTree_Medium()
         {
-            for (int i = 0; i < IterationCount; i++)
-            {
-                foreach (var token in _mediumRoot.DescendantTokens())
-                {
-                }
-            }
+            WalkTree(_mediumRoot);
+        }
+
+        [Benchmark]
+        public void WalkLines_Large()
+        {
+            WalkLines(_largeText, _largeRoot);
         }
 
         [Benchmark]
         public void WalkTree_Large()
         {
-            for (int i = 0; i < IterationCount; i++)
+            WalkTree(_largeRoot);
+        }
+
+        private static void WalkLines(SourceText sourceText, SyntaxNode root)
+        {
+            for (var i = 0; i < IterationCountWalkLines; i++)
             {
-                foreach (var token in _largeRoot.DescendantTokens())
+                foreach (var line in sourceText.Lines)
+                {
+                    foreach (var token in root.DescendantTokens(line.Span))
+                    {
+                    }
+                }
+            }
+        }
+
+        private static void WalkTree(SyntaxNode root)
+        {
+            for (var i = 0; i < IterationCountWalkTree; i++)
+            {
+                foreach (var token in root.DescendantTokens())
                 {
                 }
             }
