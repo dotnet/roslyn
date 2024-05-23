f9513a72c0d13b4fb173ccfd072e66b2854f62f2
David Barbet <dabarbet@microsoft.com>
2024-05-22 18:08:25 -0700

    Fix race causing workspace diagnostics to be stale

--- src/Features/LanguageServer/Protocol/Handler/Diagnostics/AbstractWorkspacePullDiagnosticsHandler.cs
+++ src/Features/LanguageServer/Protocol/Handler/Diagnostics/AbstractWorkspacePullDiagnosticsHandler.cs
@@ -3,7 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -22,12 +24,15 @@ internal abstract class AbstractWorkspacePullDiagnosticsHandler<TDiagnosticsPara
     protected readonly IDiagnosticSourceManager DiagnosticSourceManager;
 
     /// <summary>
-    /// Flag that represents whether the LSP view of the world has changed.
-    /// It is totally fine for this to somewhat over-report changes
-    /// as it is an optimization used to delay closing workspace diagnostic requests
-    /// until something has changed.
+    /// Gate to guard access to <see cref="_categoryToLspChanged"/>
     /// </summary>
-    private int _lspChanged = 0;
+    private readonly object _gate = new();
+
+    /// <summary>
+    /// Stores the LSP changed state on a per category basis.  This ensures that requests for different categories
+    /// are 'walled off' from each other and only reset state for their own category.
+    /// </summary>
+    private readonly Dictionary<string, bool> _categoryToLspChanged = new();
 
     protected AbstractWorkspacePullDiagnosticsHandler(
         LspWorkspaceManager workspaceManager,
@@ -76,23 +81,50 @@ internal abstract class AbstractWorkspacePullDiagnosticsHandler<TDiagnosticsPara
 
     private void UpdateLspChanged()
     {
-        Interlocked.Exchange(ref _lspChanged, 1);
+        lock (_gate)
+        {
+            // Loop through our map of source -> has changed and mark them as all having changed.
+            foreach (var category in _categoryToLspChanged.Keys.ToImmutableArray())
+            {
+                _categoryToLspChanged[category] = true;
+            }
+        }
     }
 
-    protected override async Task WaitForChangesAsync(RequestContext context, CancellationToken cancellationToken)
+    protected override async Task WaitForChangesAsync(string? category, RequestContext context, CancellationToken cancellationToken)
     {
+        // A null category counts a separate category and should track changes independently of other categories, so we'll add an empty entry in our map for it.
+        category ??= string.Empty;
+
         // Spin waiting until our LSP change flag has been set.  When the flag is set (meaning LSP has changed),
         // we reset the flag to false and exit out of the loop allowing the request to close.
         // The client will automatically trigger a new request as soon as we close it, bringing us up to date on diagnostics.
-        while (Interlocked.CompareExchange(ref _lspChanged, value: 0, comparand: 1) == 0)
+        while (!HasChanged())
         {
             // There have been no changes between now and when the last request finished - we will hold the connection open while we poll for changes.
             await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken).ConfigureAwait(false);
         }
 
-        context.TraceInformation("Closing workspace/diagnostics request");
         // We've hit a change, so we close the current request to allow the client to open a new one.
+        context.TraceInformation("Closing workspace/diagnostics request");
         return;
+
+        bool HasChanged()
+        {
+            lock (_gate)
+            {
+                // Get the LSP changed value of this category.  If it doesn't exist we add it with a value of 'changed' since this is the first
+                // request for the category and we don't know if it has changed since the request started.
+                var changed = _categoryToLspChanged.GetOrAdd(category, true);
+                if (changed)
+                {
+                    // We've observed a change, so we reset the flag to false for this source and return true.
+                    _categoryToLspChanged[category] = false;
+                }
+
+                return changed;
+            }
+        }
     }
 
     internal abstract TestAccessor GetTestAccessor();
