7b6522464ba51319c942cbfbb9290326b66ffe7a
tmat <tomas.matousek@microsoft.com>
2024-05-23 09:54:47 -0700

    Fixes and cleanup

--- src/EditorFeatures/Test/EditAndContinue/EditAndContinueLanguageServiceTests.cs
+++ src/EditorFeatures/Test/EditAndContinue/EditAndContinueLanguageServiceTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,6 +14,7 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.EditAndContinue;
 using Microsoft.CodeAnalysis.EditAndContinue.UnitTests;
 using Microsoft.CodeAnalysis.Editor.UnitTests;
+using Microsoft.CodeAnalysis.Emit;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Shared.Extensions;
@@ -27,8 +29,10 @@ using DebuggerContracts = Microsoft.VisualStudio.Debugger.Contracts.HotReload;
 namespace Roslyn.VisualStudio.Next.UnitTests.EditAndContinue;
 
 [UseExportProvider]
-public class EditAndContinueLanguageServiceTests
+public class EditAndContinueLanguageServiceTests : TestBase
 {
+    private static readonly Guid s_solutionTelemetryId = Guid.Parse("00000000-AAAA-AAAA-AAAA-000000000000");
+
     private static string Inspect(DiagnosticData d)
         => $"{d.Severity} {d.Id}:" +
             (!string.IsNullOrWhiteSpace(d.DataLocation.UnmappedFileSpan.Path) ? $" {d.DataLocation.UnmappedFileSpan.Path}({d.DataLocation.UnmappedFileSpan.StartLinePosition.Line}, {d.DataLocation.UnmappedFileSpan.StartLinePosition.Character}, {d.DataLocation.UnmappedFileSpan.EndLinePosition.Line}, {d.DataLocation.UnmappedFileSpan.EndLinePosition.Character}):" : "") +
@@ -38,6 +42,42 @@ public class EditAndContinueLanguageServiceTests
         => $"{d.Severity} {d.Id}:" +
             (!string.IsNullOrWhiteSpace(d.FilePath) ? $" {d.FilePath}({d.Span.StartLine}, {d.Span.StartColumn}, {d.Span.EndLine}, {d.Span.EndColumn}):" : "") +
             $" {d.Message}";
+
+    private Func<Project, CompilationOutputs> _mockCompilationOutputsProvider;
+
+    private TestWorkspace CreateEditorWorkspace(out Solution solution, out EditAndContinueService service, out EditAndContinueLanguageService languageService, Type[] additionalParts = null)
+    {
+        var composition = EditorTestCompositions.EditorFeatures
+            .RemoveParts(typeof(MockWorkspaceEventListenerProvider))
+            .AddParts(
+                typeof(MockHostWorkspaceProvider),
+                typeof(MockManagedHotReloadService),
+                typeof(MockServiceBrokerProvider))
+            .AddParts(additionalParts);
+
+        var workspace = new TestWorkspace(composition: composition, solutionTelemetryId: s_solutionTelemetryId);
+
+        ((MockServiceBroker)workspace.GetService<IServiceBrokerProvider>().ServiceBroker).CreateService = t => t switch
+        {
+            _ when t == typeof(DebuggerContracts.IHotReloadLogger) => new MockHotReloadLogger(),
+            _ => throw ExceptionUtilities.UnexpectedValue(t)
+        };
+
+        ((MockHostWorkspaceProvider)workspace.GetService<IHostWorkspaceProvider>()).Workspace = workspace;
+
+        solution = workspace.CurrentSolution;
+        service = GetEditAndContinueService(workspace);
+        languageService = workspace.GetService<EditAndContinueLanguageService>();
+        return workspace;
+    }
+
+    private EditAndContinueService GetEditAndContinueService(TestWorkspace workspace)
+    {
+        var service = (EditAndContinueService)workspace.GetService<IEditAndContinueService>();
+        var accessor = service.GetTestAccessor();
+        accessor.SetOutputProvider(project => _mockCompilationOutputsProvider(project));
+        return service;
+    }
 
     [Theory, CombinatorialData]
     public async Task Test(bool commitChanges)
@@ -178,5 +218,65 @@ public class EditAndContinueLanguageServiceTests
         Assert.Equal(++observedDiagnosticVersion, diagnosticRefresher.GlobalStateVersion);
         Assert.Empty(sessionState.ApplyChangesDiagnostics);
         Assert.False(sessionState.IsSessionActive);
+    }
+
+    [Fact]
+    public async Task DefaultPdbMatchingSourceTextProvider()
+    {
+        var source1 = "class C1 { void M() { System.Console.WriteLine(\"a\"); } }";
+        var source2 = "class C1 { void M() { System.Console.WriteLine(\"b\"); } }";
+        var source3 = "class C1 { void M() { System.Console.WriteLine(\"c\"); } }";
+
+        var dir = Temp.CreateDirectory();
+        var sourceFile = dir.CreateFile("test.cs").WriteAllText(source1, Encoding.UTF8);
+
+        using var workspace = CreateEditorWorkspace(out var solution, out var service, out var languageService);
+        var sourceTextProvider = workspace.GetService<PdbMatchingSourceTextProvider>();
+
+        var projectId = ProjectId.CreateNewId();
+        var documentId = DocumentId.CreateNewId(projectId);
+
+        solution = solution.
+            AddProject(projectId, "test", "test", LanguageNames.CSharp).
+            WithProjectChecksumAlgorithm(projectId, SourceHashAlgorithms.Default).
+            AddMetadataReferences(projectId, TargetFrameworkUtil.GetReferences(TargetFramework.Mscorlib40)).
+            AddDocument(DocumentInfo.Create(
+                documentId,
+                name: "test.cs",
+                loader: new WorkspaceFileTextLoader(workspace.Services.SolutionServices, sourceFile.Path, Encoding.UTF8),
+                filePath: sourceFile.Path));
+
+        Assert.True(workspace.SetCurrentSolution(_ => solution, WorkspaceChangeKind.SolutionAdded));
+        solution = workspace.CurrentSolution;
+
+        var moduleId = EmitAndLoadLibraryToDebuggee(source1, sourceFilePath: sourceFile.Path);
+
+        // hydrate document text and overwrite file content:
+        var document1 = await solution.GetDocument(documentId).GetTextAsync();
+        File.WriteAllText(sourceFile.Path, source2, Encoding.UTF8);
+
+        await languageService.StartSessionAsync(CancellationToken.None);
+        await languageService.EnterBreakStateAsync(CancellationToken.None);
+
+        workspace.OnDocumentOpened(documentId, new TestSourceTextContainer()
+        {
+            Text = SourceText.From(source3, Encoding.UTF8, SourceHashAlgorithm.Sha1)
+        });
+
+        await workspace.GetService<AsynchronousOperationListenerProvider>().GetWaiter(FeatureAttribute.Workspace).ExpeditedWaitAsync();
+
+        var (key, (documentState, version)) = sourceTextProvider.GetTestAccessor().GetDocumentsWithChangedLoaderByPath().Single();
+        Assert.Equal(sourceFile.Path, key);
+        Assert.Equal(solution.WorkspaceVersion, version);
+        Assert.Equal(source1, (await documentState.GetTextAsync(CancellationToken.None)).ToString());
+
+        // check committed document status:
+        var debuggingSession = service.GetTestAccessor().GetActiveDebuggingSessions().Single();
+        var (document, state) = await debuggingSession.LastCommittedSolution.GetDocumentAndStateAsync(documentId, currentDocument: null, CancellationToken.None);
+        var text = await document.GetTextAsync();
+        Assert.Equal(CommittedSolution.DocumentState.MatchesBuildOutput, state);
+        Assert.Equal(source1, (await document.GetTextAsync(CancellationToken.None)).ToString());
+
+        await languageService.EndSessionAsync(CancellationToken.None);
     }
 }
