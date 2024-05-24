4d1998a23fa3b0bf71da5dc8568b766933cb0861
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-05-24 14:03:36 -0700

    Cleanup

--- src/EditorFeatures/Core/EditAndContinue/ActiveStatementTaggerProvider.cs
+++ src/EditorFeatures/Core/EditAndContinue/ActiveStatementTaggerProvider.cs
@@ -40,10 +40,6 @@ internal partial class ActiveStatementTaggerProvider(
     [Import(AllowDefault = true)] ITextBufferVisibilityTracker? visibilityTracker,
     IAsynchronousOperationListenerProvider listenerProvider) : AsynchronousTaggerProvider<ITextMarkerTag>(threadingContext, globalOptions, visibilityTracker, listenerProvider.GetListener(FeatureAttribute.Classification))
 {
-    // We want to track text changes so that we can try to only reclassify a method body if
-    // all edits were contained within one.
-    protected override TaggerTextChangeBehavior TextChangeBehavior => TaggerTextChangeBehavior.TrackTextChanges;
-
     protected override TaggerDelay EventChangeDelay => TaggerDelay.NearImmediate;
 
     protected override ITaggerEventSource CreateEventSource(ITextView? textView, ITextBuffer subjectBuffer)
