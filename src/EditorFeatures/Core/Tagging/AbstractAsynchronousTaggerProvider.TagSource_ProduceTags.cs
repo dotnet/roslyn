3991c600c1bfb694ddcb6c401642fd01f1f93478
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-05-24 19:01:21 -0700

    Comment

--- src/EditorFeatures/Core/Tagging/AbstractAsynchronousTaggerProvider.TagSource_ProduceTags.cs
+++ src/EditorFeatures/Core/Tagging/AbstractAsynchronousTaggerProvider.TagSource_ProduceTags.cs
@@ -277,10 +277,13 @@ internal partial class AbstractAsynchronousTaggerProvider<TTag>
             {
                 _dataSource.ThreadingContext.ThrowIfNotOnUIThread();
 
-                isVisible = _visibilityTracker is null || _visibilityTracker.IsVisible(_subjectBuffer);
                 // Make a copy of all the data we need while we're on the foreground.  Then switch to a threadpool
                 // thread to do the computation. Finally, once new tags have been computed, then we update our state
                 // in a threadsafe fashion in the background.
+
+                // Grab the visibility state of the view while we're already on the UI thread.  This saves an
+                // unnecessary switch below.
+                isVisible = _visibilityTracker is null || _visibilityTracker.IsVisible(_subjectBuffer);
                 spansToTag = GetSpansAndDocumentsToTag();
                 caretPosition = _dataSource.GetCaretPoint(_textView, _subjectBuffer);
             }, cancellationToken).NoThrowAwaitable(captureContext: true);
