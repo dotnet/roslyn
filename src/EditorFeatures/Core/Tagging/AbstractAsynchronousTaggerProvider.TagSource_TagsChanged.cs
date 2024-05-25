3c366456315c93e55c1773bb73b021bd3be8dbf0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-05-25 00:44:24 -0700

    less allocs

--- src/EditorFeatures/Core/Tagging/AbstractAsynchronousTaggerProvider.TagSource_TagsChanged.cs
+++ src/EditorFeatures/Core/Tagging/AbstractAsynchronousTaggerProvider.TagSource_TagsChanged.cs
@@ -21,7 +21,7 @@ internal partial class AbstractAsynchronousTaggerProvider<TTag>
         public event EventHandler<SnapshotSpanEventArgs>? TagsChanged;
 
         private void OnTagsChangedForBuffer(
-            Dictionary<ITextBuffer, DiffResult> changes, bool highPriority)
+            ICollection<KeyValuePair<ITextBuffer, DiffResult>> changes, bool highPriority)
         {
             // Can be called from any thread.  Just filters out changes that aren't for our buffer and adds to the right
             // queue to actually notify interested parties.
