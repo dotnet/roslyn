9ee0c66905e3fcb87b53d11246f54f942a78e37d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-05-28 11:22:13 -0700

    Make strongly typed

--- src/EditorFeatures/Core/Tagging/TaggerMainThreadManager.cs
+++ src/EditorFeatures/Core/Tagging/TaggerMainThreadManager.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Collections;
@@ -12,47 +13,14 @@ using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Editor.Tagging;
 
-internal sealed class TaggerMainThreadManager
+internal sealed class TaggerMainThreadManager(
+    IThreadingContext threadingContext,
+    IAsynchronousOperationListenerProvider listenerProvider)
 {
-    private readonly IThreadingContext _threadingContext;
-    private readonly AsyncBatchingWorkQueue<(Func<object?> action, CancellationToken cancellationToken, TaskCompletionSource<object?> taskCompletionSource)> _workQueue;
+    private readonly IThreadingContext _threadingContext = threadingContext;
+    private readonly IAsynchronousOperationListenerProvider _listenerProvider = listenerProvider;
 
-    public TaggerMainThreadManager(
-        IThreadingContext threadingContext,
-        IAsynchronousOperationListenerProvider listenerProvider)
-    {
-        _threadingContext = threadingContext;
-
-        _workQueue = new AsyncBatchingWorkQueue<(Func<object?> action, CancellationToken cancellationToken, TaskCompletionSource<object?> taskCompletionSource)>(
-            DelayTimeSpan.NearImmediate,
-            ProcessWorkItemsAsync,
-            listenerProvider.GetListener(FeatureAttribute.Tagger),
-            threadingContext.DisposalToken);
-    }
-
-    /// <remarks>This will not ever throw.</remarks>
-    private static void RunActionAndUpdateCompletionSource_NoThrow(
-        Func<object?> action,
-        TaskCompletionSource<object?> taskCompletionSource)
-    {
-        try
-        {
-            // Run the underlying task.
-            taskCompletionSource.SetResult(action());
-        }
-        catch (OperationCanceledException ex)
-        {
-            taskCompletionSource.TrySetCanceled(ex.CancellationToken);
-        }
-        catch (Exception ex)
-        {
-            taskCompletionSource.TrySetException(ex);
-        }
-        finally
-        {
-            taskCompletionSource.TrySetResult(null);
-        }
-    }
+    private readonly ConcurrentDictionary<Type, StronglyTypedTaggerMainThreadManager> _stronglyTypedManagers = new();
 
     /// <summary>
     /// Adds the provided action to a queue that will run on the UI thread in the near future (batched with other
@@ -60,69 +28,135 @@ internal sealed class TaggerMainThreadManager
     /// </summary>
     public Task<TResult> PerformWorkOnMainThreadAsync<TResult>(Func<TResult> action, CancellationToken cancellationToken)
     {
-        var taskSource = new TaskCompletionSource<object?>();
-        var objectWrapper = object? () => action();
+        var manager = (StronglyTypedTaggerMainThreadManager<TResult>)GetManager();
 
-        // If we're already on the main thread, just run the action directly without any delay.  This is important
-        // for cases where the tagger is performing a blocking call to get tags synchronously on the UI thread (for
-        // example, for determining collapsed outlining tags on document open).
-        if (_threadingContext.JoinableTaskContext.IsOnMainThread)
+        return manager.PerformWorkOnMainThreadAsync(action, cancellationToken);
+
+        StronglyTypedTaggerMainThreadManager GetManager()
         {
-            RunActionAndUpdateCompletionSource_NoThrow(objectWrapper, taskSource);
-        }
-        else
-        {
-            // Ensure that if the host is closing and hte queue stops running that we transition this task to the canceled state.
-            var registration = _threadingContext.DisposalToken.Register(static taskSourceObj => ((TaskCompletionSource<VoidResult>)taskSourceObj!).TrySetCanceled(), taskSource);
-
-            _workQueue.AddWork((objectWrapper, cancellationToken, taskSource));
-
-            // Ensure that when our work is done that we let go of the registered callback.
-            taskSource.Task.CompletesTrackingOperation(registration);
+            // First try the path that doesn't allocate a lambda closure. If that fails defer to helper that will allocate.
+            return _stronglyTypedManagers.TryGetValue(typeof(TResult), out var manager)
+                ? manager
+                : GetOrCreateManager();
         }
 
-        return CastTaskResultAsync(taskSource.Task);
-
-        async Task<TResult> CastTaskResultAsync(Task<object?> task)
-            => ((TResult?)await task.ConfigureAwait(false))!;
+        StronglyTypedTaggerMainThreadManager GetOrCreateManager()
+            => _stronglyTypedManagers.GetOrAdd(
+                typeof(TResult), _ => new StronglyTypedTaggerMainThreadManager<TResult>(_threadingContext, _listenerProvider));
     }
 
-    private async ValueTask ProcessWorkItemsAsync(
-        ImmutableSegmentedList<(Func<object?> action, CancellationToken cancellationToken, TaskCompletionSource<object?> taskCompletionSource)> list,
-        CancellationToken queueCancellationToken)
+    private abstract class StronglyTypedTaggerMainThreadManager
     {
-        var nonCanceledActions = ImmutableSegmentedList.CreateBuilder<(Func<object?> action, CancellationToken cancellationToken, TaskCompletionSource<object?> taskCompletionSource)>();
-        foreach (var (action, cancellationToken, taskCompletionSource) in list)
-        {
-            if (cancellationToken.IsCancellationRequested)
-            {
-                // If the work was already canceled, then just transition the task to the canceled state without
-                // running the action.
-                taskCompletionSource.TrySetCanceled(cancellationToken);
-                continue;
-            }
+    }
 
-            nonCanceledActions.Add((action, cancellationToken, taskCompletionSource));
+    private sealed class StronglyTypedTaggerMainThreadManager<TResult> : StronglyTypedTaggerMainThreadManager
+    {
+        private readonly IThreadingContext _threadingContext;
+        private readonly AsyncBatchingWorkQueue<(Func<TResult> action, CancellationToken cancellationToken, TaskCompletionSource<TResult> taskCompletionSource)> _workQueue;
+
+        public StronglyTypedTaggerMainThreadManager(
+            IThreadingContext threadingContext,
+            IAsynchronousOperationListenerProvider listenerProvider)
+        {
+            _threadingContext = threadingContext;
+
+            _workQueue = new AsyncBatchingWorkQueue<(Func<TResult> action, CancellationToken cancellationToken, TaskCompletionSource<TResult> taskCompletionSource)>(
+                DelayTimeSpan.NearImmediate,
+                ProcessWorkItemsAsync,
+                listenerProvider.GetListener(FeatureAttribute.Tagger),
+                threadingContext.DisposalToken);
         }
 
-        // No need to do anything if all the requested work was canceled.
-        if (nonCanceledActions.Count == 0)
-            return;
-
-        await _threadingContext.JoinableTaskFactory.SwitchToMainThreadAsync(queueCancellationToken);
-
-        foreach (var (action, cancellationToken, taskCompletionSource) in nonCanceledActions)
+        /// <remarks>This will not ever throw.</remarks>
+        private static void RunActionAndUpdateCompletionSource_NoThrow(
+            Func<TResult> action,
+            TaskCompletionSource<TResult> taskCompletionSource)
         {
-            if (cancellationToken.IsCancellationRequested)
+            try
             {
-                // If the work was already canceled, then just transition the task to the canceled state without
-                // running the action.
-                taskCompletionSource.TrySetCanceled(cancellationToken);
-                continue;
+                // Run the underlying task.
+                taskCompletionSource.SetResult(action());
+            }
+            catch (OperationCanceledException ex)
+            {
+                taskCompletionSource.TrySetCanceled(ex.CancellationToken);
+            }
+            catch (Exception ex)
+            {
+                taskCompletionSource.TrySetException(ex);
+            }
+            finally
+            {
+                taskCompletionSource.TrySetResult(default!);
+            }
+        }
+
+        /// <summary>
+        /// Adds the provided action to a queue that will run on the UI thread in the near future (batched with other
+        /// registered actions).  If the cancellation token is triggered before the action runs, it will not be run.
+        /// </summary>
+        public Task<TResult> PerformWorkOnMainThreadAsync(Func<TResult> action, CancellationToken cancellationToken)
+        {
+            var taskSource = new TaskCompletionSource<TResult>();
+
+            // If we're already on the main thread, just run the action directly without any delay.  This is important
+            // for cases where the tagger is performing a blocking call to get tags synchronously on the UI thread (for
+            // example, for determining collapsed outlining tags on document open).
+            if (_threadingContext.JoinableTaskContext.IsOnMainThread)
+            {
+                RunActionAndUpdateCompletionSource_NoThrow(action, taskSource);
+            }
+            else
+            {
+                // Ensure that if the host is closing and hte queue stops running that we transition this task to the canceled state.
+                var registration = _threadingContext.DisposalToken.Register(static taskSourceObj => ((TaskCompletionSource<VoidResult>)taskSourceObj!).TrySetCanceled(), taskSource);
+
+                _workQueue.AddWork((action, cancellationToken, taskSource));
+
+                // Ensure that when our work is done that we let go of the registered callback.
+                taskSource.Task.CompletesTrackingOperation(registration);
             }
 
-            // Run the user action, completing the task completion source as appropriate. This will not ever throw.
-            RunActionAndUpdateCompletionSource_NoThrow(action, taskCompletionSource);
+            return taskSource.Task;
+        }
+
+        private async ValueTask ProcessWorkItemsAsync(
+            ImmutableSegmentedList<(Func<TResult> action, CancellationToken cancellationToken, TaskCompletionSource<TResult> taskCompletionSource)> list,
+            CancellationToken queueCancellationToken)
+        {
+            var nonCanceledActions = ImmutableSegmentedList.CreateBuilder<(Func<TResult> action, CancellationToken cancellationToken, TaskCompletionSource<TResult> taskCompletionSource)>();
+            foreach (var (action, cancellationToken, taskCompletionSource) in list)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    // If the work was already canceled, then just transition the task to the canceled state without
+                    // running the action.
+                    taskCompletionSource.TrySetCanceled(cancellationToken);
+                    continue;
+                }
+
+                nonCanceledActions.Add((action, cancellationToken, taskCompletionSource));
+            }
+
+            // No need to do anything if all the requested work was canceled.
+            if (nonCanceledActions.Count == 0)
+                return;
+
+            await _threadingContext.JoinableTaskFactory.SwitchToMainThreadAsync(queueCancellationToken);
+
+            foreach (var (action, cancellationToken, taskCompletionSource) in nonCanceledActions)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    // If the work was already canceled, then just transition the task to the canceled state without
+                    // running the action.
+                    taskCompletionSource.TrySetCanceled(cancellationToken);
+                    continue;
+                }
+
+                // Run the user action, completing the task completion source as appropriate. This will not ever throw.
+                RunActionAndUpdateCompletionSource_NoThrow(action, taskCompletionSource);
+            }
         }
     }
 }
