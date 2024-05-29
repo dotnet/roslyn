6d08dff25b937b897ecc4ecbc262f4e43c64d667
tmat <tomas.matousek@microsoft.com>
2024-05-29 16:17:07 -0700

    Move LanguageServer directory out of Features

---
+++ src/LanguageServer/Protocol/IOnInitialized.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.LanguageServer.Handler;
+using Roslyn.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer;
+
+internal interface IOnInitialized
+{
+    Task OnInitializedAsync(ClientCapabilities clientCapabilities, RequestContext context, CancellationToken cancellationToken);
+}
