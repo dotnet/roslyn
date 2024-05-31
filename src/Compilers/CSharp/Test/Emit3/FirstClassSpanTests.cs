3c9311b0486d294892335127ab99d90e79ef8715
Jan Jones <janjones@microsoft.com>
2024-05-31 12:52:54 +0200

    Extend tests

--- src/Compilers/CSharp/Test/Emit3/FirstClassSpanTests.cs
+++ src/Compilers/CSharp/Test/Emit3/FirstClassSpanTests.cs
@@ -2592,34 +2592,98 @@ public class FirstClassSpanTests : CSharpTestBase
         CompileAndVerify(comp, expectedOutput: "aa").VerifyDiagnostics();
     }
 
+    [Theory, MemberData(nameof(LangVersions))]
+    public void OverloadResolution_SpanVsReadOnlySpan_01(LanguageVersion langVersion)
+    {
+        var source = """
+            using System;
+
+            C.M(new int[0]);
+            C.M(default(Span<int>));
+            C.M(default(ReadOnlySpan<int>));
+
+            static class C
+            {
+                public static void M(Span<int> arg) => Console.Write(1);
+                public static void M(ReadOnlySpan<int> arg) => Console.Write(2);
+            }
+            """;
+        var comp = CreateCompilationWithSpan(source, parseOptions: TestOptions.Regular.WithLanguageVersion(langVersion));
+        CompileAndVerify(comp, expectedOutput: "112").VerifyDiagnostics();
+    }
+
+    [Theory, MemberData(nameof(LangVersions))]
+    public void OverloadResolution_SpanVsReadOnlySpan_02(LanguageVersion langVersion)
+    {
+        var source = """
+            using System;
+
+            C.M(new object[0]);
+            C.M(default(Span<object>));
+            C.M(default(ReadOnlySpan<object>));
+
+            C.M(new string[0]);
+
+            static class C
+            {
+                public static void M(Span<object> arg) => Console.Write(1);
+                public static void M(ReadOnlySpan<object> arg) => Console.Write(2);
+            }
+            """;
+        var comp = CreateCompilationWithSpan(source, parseOptions: TestOptions.Regular.WithLanguageVersion(langVersion));
+        CompileAndVerify(comp, expectedOutput: "1121").VerifyDiagnostics();
+    }
+
+    [Theory, MemberData(nameof(LangVersions))]
+    public void OverloadResolution_SpanVsReadOnlySpan_03(LanguageVersion langVersion)
+    {
+        var source = """
+            using System;
+
+            C.M(default(Span<string>));
+            C.M(default(ReadOnlySpan<string>));
+
+            static class C
+            {
+                public static void M(Span<object> arg) { }
+                public static void M(ReadOnlySpan<object> arg) { }
+            }
+            """;
+        CreateCompilationWithSpan(source, parseOptions: TestOptions.Regular.WithLanguageVersion(langVersion)).VerifyDiagnostics(
+            // (3,5): error CS1503: Argument 1: cannot convert from 'System.Span<string>' to 'System.Span<object>'
+            // C.M(default(Span<string>));
+            Diagnostic(ErrorCode.ERR_BadArgType, "default(Span<string>)").WithArguments("1", "System.Span<string>", "System.Span<object>").WithLocation(3, 5),
+            // (4,5): error CS1503: Argument 1: cannot convert from 'System.ReadOnlySpan<string>' to 'System.Span<object>'
+            // C.M(default(ReadOnlySpan<string>));
+            Diagnostic(ErrorCode.ERR_BadArgType, "default(ReadOnlySpan<string>)").WithArguments("1", "System.ReadOnlySpan<string>", "System.Span<object>").WithLocation(4, 5));
+    }
+
     [Fact]
     public void OverloadResolution_SpanVsReadOnlySpan_ExtensionMethodReceiver_01()
     {
         var source = """
             using System;
 
+            (new int[0]).E();
+
             static class C
             {
-                static void M(int[] arg) => arg.E();
-
-                static void E(this Span<int> arg) { }
-                static void E(this ReadOnlySpan<int> arg) { }
+                public static void E(this Span<int> arg) => Console.Write(1);
+                public static void E(this ReadOnlySpan<int> arg) => Console.Write(2);
             }
             """;
         CreateCompilationWithSpan(source, parseOptions: TestOptions.Regular12).VerifyDiagnostics(
-            // (5,33): error CS1929: 'int[]' does not contain a definition for 'E' and the best extension method overload 'C.E(Span<int>)' requires a receiver of type 'System.Span<int>'
-            //     static void M(int[] arg) => arg.E();
-            Diagnostic(ErrorCode.ERR_BadInstanceArgType, "arg").WithArguments("int[]", "E", "C.E(System.Span<int>)", "System.Span<int>").WithLocation(5, 33));
+            // (3,2): error CS1929: 'int[]' does not contain a definition for 'E' and the best extension method overload 'C.E(Span<int>)' requires a receiver of type 'System.Span<int>'
+            // (new int[0]).E();
+            Diagnostic(ErrorCode.ERR_BadInstanceArgType, "new int[0]").WithArguments("int[]", "E", "C.E(System.Span<int>)", "System.Span<int>").WithLocation(3, 2));
 
-        var expectedDiagnostics = new[]
-        {
-            // (5,37): error CS0121: The call is ambiguous between the following methods or properties: 'C.E(Span<int>)' and 'C.E(ReadOnlySpan<int>)'
-            //     static void M(int[] arg) => arg.E();
-            Diagnostic(ErrorCode.ERR_AmbigCall, "E").WithArguments("C.E(System.Span<int>)", "C.E(System.ReadOnlySpan<int>)").WithLocation(5, 37)
-        };
+        var expectedOutput = "1";
 
-        CreateCompilationWithSpan(source, parseOptions: TestOptions.RegularNext).VerifyDiagnostics(expectedDiagnostics);
-        CreateCompilationWithSpan(source).VerifyDiagnostics(expectedDiagnostics);
+        var comp = CreateCompilationWithSpan(source, parseOptions: TestOptions.RegularNext);
+        CompileAndVerify(comp, expectedOutput: expectedOutput).VerifyDiagnostics();
+
+        comp = CreateCompilationWithSpan(source);
+        CompileAndVerify(comp, expectedOutput: expectedOutput).VerifyDiagnostics();
     }
 
     [Theory, MemberData(nameof(LangVersions))]
@@ -2628,21 +2692,75 @@ public class FirstClassSpanTests : CSharpTestBase
         var source = """
             using System;
 
-            C.M1(default);
-            C.M2(default);
+            default(Span<int>).E();
+            default(ReadOnlySpan<int>).E();
 
             static class C
             {
-                public static void M1(Span<int> arg) => arg.E();
-                public static void M2(ReadOnlySpan<int> arg) => arg.E();
-
-                static void E(this Span<int> arg) => Console.Write("S ");
-                static void E(this ReadOnlySpan<int> arg) => Console.Write("R ");
+                public static void E(this Span<int> arg) => Console.Write(1);
+                public static void E(this ReadOnlySpan<int> arg) => Console.Write(2);
             }
             """;
-
         var comp = CreateCompilationWithSpan(source, parseOptions: TestOptions.Regular.WithLanguageVersion(langVersion));
-        CompileAndVerify(comp, expectedOutput: "S R").VerifyDiagnostics();
+        CompileAndVerify(comp, expectedOutput: "12").VerifyDiagnostics();
+    }
+
+    [Fact]
+    public void OverloadResolution_SpanVsReadOnlySpan_ExtensionMethodReceiver_03()
+    {
+        var source = """
+            using System;
+
+            (new string[0]).E();
+            (new object[0]).E();
+
+            static class C
+            {
+                public static void E(this Span<object> arg) => Console.Write(1);
+                public static void E(this ReadOnlySpan<object> arg) => Console.Write(2);
+            }
+            """;
+        CreateCompilationWithSpan(source, parseOptions: TestOptions.Regular12).VerifyDiagnostics(
+            // (3,2): error CS1929: 'string[]' does not contain a definition for 'E' and the best extension method overload 'C.E(Span<object>)' requires a receiver of type 'System.Span<object>'
+            // (new string[0]).E();
+            Diagnostic(ErrorCode.ERR_BadInstanceArgType, "new string[0]").WithArguments("string[]", "E", "C.E(System.Span<object>)", "System.Span<object>").WithLocation(3, 2),
+            // (4,2): error CS1929: 'object[]' does not contain a definition for 'E' and the best extension method overload 'C.E(Span<object>)' requires a receiver of type 'System.Span<object>'
+            // (new string[0]).E();
+            Diagnostic(ErrorCode.ERR_BadInstanceArgType, "new object[0]").WithArguments("object[]", "E", "C.E(System.Span<object>)", "System.Span<object>").WithLocation(4, 2));
+
+        var expectedOutput = "21";
+
+        var comp = CreateCompilationWithSpan(source, parseOptions: TestOptions.RegularNext);
+        CompileAndVerify(comp, expectedOutput: expectedOutput).VerifyDiagnostics();
+
+        comp = CreateCompilationWithSpan(source);
+        CompileAndVerify(comp, expectedOutput: expectedOutput).VerifyDiagnostics();
+    }
+
+    [Theory, MemberData(nameof(LangVersions))]
+    public void OverloadResolution_SpanVsReadOnlySpan_ExtensionMethodReceiver_04(LanguageVersion langVersion)
+    {
+        var source = """
+            using System;
+
+            default(Span<string>).E();
+            default(ReadOnlySpan<string>).E();
+
+            static class C
+            {
+                public static void E(this Span<object> arg) { }
+                public static void E(this ReadOnlySpan<object> arg) { }
+            }
+            """;
+        CreateCompilationWithSpan(source, parseOptions: TestOptions.Regular.WithLanguageVersion(langVersion)).VerifyDiagnostics(
+            // (3,1): error CS1929: 'Span<string>' does not contain a definition for 'E' and the best extension method overload 'C.E(Span<object>)' requires a receiver of type 'System.Span<object>'
+            // default(Span<string>).E();
+            Diagnostic(ErrorCode.ERR_BadInstanceArgType, "default(Span<string>)").WithArguments("System.Span<string>", "E", "C.E(System.Span<object>)", "System.Span<object>").WithLocation(3, 1),
+            // (4,1): error CS1929: 'ReadOnlySpan<string>' does not contain a definition for 'E' and the best extension method overload 'C.E(Span<object>)' requires a receiver of type 'System.Span<object>'
+            // default(ReadOnlySpan<string>).E();
+            Diagnostic(ErrorCode.ERR_BadInstanceArgType, "default(ReadOnlySpan<string>)").WithArguments("System.ReadOnlySpan<string>", "E", "C.E(System.Span<object>)", "System.Span<object>").WithLocation(4, 1));
+
+        // PROTOTYPE: Should work in C# 13 when ROS->ROS conversion is implemented.
     }
 
     [Theory, MemberData(nameof(LangVersions))]
