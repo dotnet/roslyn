001d6e2705938e69c7b6c3a71e3ee07574bd897a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-05-31 16:42:12 -0700

    remove unused overload

--- src/Workspaces/Core/Portable/Shared/Collections/IntervalTree.cs
+++ src/Workspaces/Core/Portable/Shared/Collections/IntervalTree.cs
@@ -8,12 +8,6 @@ namespace Microsoft.CodeAnalysis.Shared.Collections;
 
 internal static class IntervalTree
 {
-    public static IntervalTree<T> Create<T, TIntrospector>(in TIntrospector introspector, params T[] values)
-        where TIntrospector : struct, IIntervalIntrospector<T>
-    {
-        return Create(in introspector, (IEnumerable<T>)values);
-    }
-
     public static IntervalTree<T> Create<T, TIntrospector>(in TIntrospector introspector, IEnumerable<T>? values1 = null, IEnumerable<T>? values2 = null)
         where TIntrospector : struct, IIntervalIntrospector<T>
     {
