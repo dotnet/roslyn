9ad83d46617eaeb4f95331c284cfc58e8e3076e0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-06-04 14:23:59 -0700

    in progress

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Collections/IIntervalTree`1.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Collections/IIntervalTree`1.cs
@@ -2,6 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis.Collections;
+using Roslyn.Utilities;
+
 namespace Microsoft.CodeAnalysis.Shared.Collections;
 
 /// <summary>
@@ -39,4 +44,86 @@ internal interface IIntervalTree<T>
     /// </summary>
     bool Any<TIntrospector>(int start, int length, TestInterval<T, TIntrospector> testInterval, in TIntrospector introspector)
         where TIntrospector : struct, IIntervalIntrospector<T>;
+}
+
+internal readonly struct FlatArrayIntervalTree<T> : IIntervalTree<T>
+{
+    private readonly record struct Node(T Value, int MaxEndNodeIndex);
+
+    public static readonly FlatArrayIntervalTree<T> Empty = new(new SegmentedArray<Node>(0));
+
+    private readonly SegmentedArray<Node> _array;
+
+    private FlatArrayIntervalTree(SegmentedArray<Node> array)
+    {
+        _array = array;
+    }
+
+    /// <summary>
+    /// Creates a <see cref="FlatArrayIntervalTree{T}"/> from an unsorted list of <paramref name="values"/>.  This will
+    /// incur a delegate allocation to sort the values.  If callers can avoid that allocation by pre-sorting the values,
+    /// they should do so and call <see cref="CreateFromSorted"/> instead.
+    /// </summary>
+    /// <remarks>
+    /// <paramref name="values"/> will be sorted in place.
+    /// </remarks>
+    public static FlatArrayIntervalTree<T> CreateFromUnsorted<TIntrospector>(in TIntrospector introspector, SegmentedList<T> values)
+        where TIntrospector : struct, IIntervalIntrospector<T>
+    {
+        var localIntrospector = introspector;
+        values.Sort((t1, t2) => localIntrospector.GetSpan(t1).Start - localIntrospector.GetSpan(t2).Start);
+        return CreateFromSorted(introspector, values);
+    }
+
+    /// <summary>
+    /// Creates an interval tree from a sorted list of values.  This is more efficient than creating from an unsorted
+    /// list as building doesn't need to figure out where the nodes need to go n-log(n) and doesn't have to rebalance
+    /// anything (again, another n-log(n) operation).  Rebalancing is particularly expensive as it involves tons of
+    /// pointer chasing operations, which is both slow, and which impacts the GC which has to track all those writes.
+    /// </summary>
+    /// <remarks>
+    /// The values must be sorted such that given any two elements 'a' and 'b' in the list, if 'a' comes before 'b' in
+    /// the list, then it's "start position" (as determined by the introspector) must be less than or equal to 'b's
+    /// start position.  This is a requirement for the algorithm to work correctly.
+    /// </remarks>
+    public static FlatArrayIntervalTree<T> CreateFromSorted<TIntrospector>(in TIntrospector introspector, SegmentedList<T> values)
+        where TIntrospector : struct, IIntervalIntrospector<T>
+    {
+#if DEBUG
+        var localIntrospector = introspector;
+        Debug.Assert(values.IsSorted(Comparer<T>.Create((t1, t2) => localIntrospector.GetSpan(t1).Start - localIntrospector.GetSpan(t2).Start)));
+#endif
+
+        if (values.Count == 0)
+            return Empty;
+
+        var array = new SegmentedArray<T>()
+        return new BinaryIntervalTree<T>
+        {
+            root = CreateFromSortedWorker(values, 0, values.Count, in introspector),
+        };
+    }
+
+    private static Node? CreateFromSortedWorker<TIntrospector>(
+        SegmentedList<T> values, int startInclusive, int endExclusive, in TIntrospector introspector) where TIntrospector : struct, IIntervalIntrospector<T>
+    {
+        var length = endExclusive - startInclusive;
+        if (length <= 0)
+            return null;
+
+        var mid = startInclusive + (length >> 1);
+        var node = new Node(values[mid]);
+        node.SetLeftRight(
+            CreateFromSortedWorker(values, startInclusive, mid, in introspector),
+            CreateFromSortedWorker(values, mid + 1, endExclusive, in introspector),
+            in introspector);
+
+        // Everything is sorted, and we're always building a node up from equal subtrees.  So we're never unbalanced
+        // enough to require balancing here.
+        var balanceFactor = BalanceFactor(node);
+        Debug.Assert(balanceFactor >= -1, "balanceFactor >= -1");
+        Debug.Assert(balanceFactor <= 1, "balanceFactor <= 1");
+
+        return node;
+    }
 }
