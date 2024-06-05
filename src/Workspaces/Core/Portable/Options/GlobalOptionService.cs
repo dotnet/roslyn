39c276ebee83fe3e234d7f2198b8459ccdb725f8
tmat <tomas.matousek@microsoft.com>
2024-06-05 11:37:19 -0700

    Empty line

--- src/Workspaces/Core/Portable/Options/GlobalOptionService.cs
+++ src/Workspaces/Core/Portable/Options/GlobalOptionService.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-
 #pragma warning disable LAYERING_IGlobalOptionService
 
 using System;
