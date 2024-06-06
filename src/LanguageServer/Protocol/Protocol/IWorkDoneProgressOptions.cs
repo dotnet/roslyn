15025cf21593cb4823c45461ca5c1930f0e6806e
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2024-06-06 01:20:54 -0400

    LSP Protocol: Implement WorkDoneProgress types

--- src/LanguageServer/Protocol/Protocol/IWorkDoneProgressOptions.cs
+++ src/LanguageServer/Protocol/Protocol/IWorkDoneProgressOptions.cs
@@ -2,18 +2,25 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Text.Json.Serialization;
+
 namespace Roslyn.LanguageServer.Protocol
 {
     /// <summary>
     /// Options to signal work done progress support in server capabilities.
     /// </summary>
+    /// <remarks>Since LSP 3.15</remarks>
     internal interface IWorkDoneProgressOptions
     {
         /// <summary>
         /// Gets or sets a value indicating whether work done progress is supported.
-        ///
+        /// <para>
         /// See the <see href="https://microsoft.github.io/language-server-protocol/specifications/specification-current/#workDoneProgressOptions">Language Server Protocol specification</see> for additional information.
+        /// </para>
         /// </summary>
-        bool WorkDoneProgress { get; init; }
+        /// <remarks>Since LSP 3.15</remarks>
+        [JsonPropertyName("workDoneProgress")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public bool WorkDoneProgress { get; init; }
     }
 }
---
+++ src/LanguageServer/Protocol/Protocol/IWorkDoneProgressParams.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Text.Json.Serialization;
+
+namespace Roslyn.LanguageServer.Protocol;
+
+/// <summary>
+/// Interface to describe parameters for requests that support reporting work done via the <c>$/progress</c> notification.
+/// <para>
+/// </para>
+/// See the <see href="https://microsoft.github.io/language-server-protocol/specifications/specification-current/#clientInitiatedProgress">Language Server Protocol specification</see> for additional information.
+/// </summary>
+/// <remarks>Since LSP 3.15</remarks>
+internal interface IWorkDoneProgressParams
+{
+    /// <summary>
+    /// An optional token that a server can use to report work done progress.
+    /// <para>
+    /// The derived classes <see cref="WorkDoneProgressBegin"/>, <see cref="WorkDoneProgressReport"/> and <see cref="WorkDoneProgressEnd"/>
+    /// are used to report the beginning, progression, and end of the operation.
+    /// </para>
+    /// </summary>
+    /// <remarks>Since LSP 3.15</remarks>
+    [JsonPropertyName(Methods.WorkDoneTokenName)]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public IProgress<WorkDoneProgress>? WorkDoneToken { get; set; }
+}
