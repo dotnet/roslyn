e867be9f6f8777b61b00bbd6ab46cd99e5da617e
Tomáš Matoušek <tmat@users.noreply.github.com>
2024-06-07 10:55:58 -0700

    ExternalAccess layering (#73887)
    * ExternalAccess layering
    
    * Style fixes

---
+++ src/Features/ExternalAccess/OmniSharp/CodeRefactorings/WorkspaceServices/IOmniSharpSymbolRenamedCodeActionOperationFactoryWorkspaceService.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.CodeActions;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.OmniSharp.CodeRefactorings.WorkspaceServices
+{
+    internal interface IOmniSharpSymbolRenamedCodeActionOperationFactoryWorkspaceService
+    {
+        CodeActionOperation CreateSymbolRenamedOperation(ISymbol symbol, string newName, Solution startingSolution, Solution updatedSolution);
+    }
+}
