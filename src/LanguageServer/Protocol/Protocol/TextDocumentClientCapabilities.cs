b3c3459e1387ca6a94db3ac4891f79c785e3d63f
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2024-06-08 23:22:31 -0400

    LSP Protocol: Use spec ordering in capabilities types
    This makes it easier to update them

--- src/LanguageServer/Protocol/Protocol/TextDocumentClientCapabilities.cs
+++ src/LanguageServer/Protocol/Protocol/TextDocumentClientCapabilities.cs
@@ -13,256 +13,166 @@ namespace Roslyn.LanguageServer.Protocol
     /// </summary>
     internal class TextDocumentClientCapabilities
     {
+        // NOTE: these are kept in the same order as the spec to make them easier to update
+
         /// <summary>
         /// Gets or sets the synchronization setting.
         /// </summary>
         [JsonPropertyName("synchronization")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public SynchronizationSetting? Synchronization
-        {
-            get;
-            set;
-        }
+        public SynchronizationSetting? Synchronization { get; set; }
 
         /// <summary>
         /// Gets or sets the completion setting.
         /// </summary>
         [JsonPropertyName("completion")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public CompletionSetting? Completion
-        {
-            get;
-            set;
-        }
+        public CompletionSetting? Completion { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if hover can be dynamically registered.
         /// </summary>
         [JsonPropertyName("hover")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public HoverSetting? Hover
-        {
-            get;
-            set;
-        }
+        public HoverSetting? Hover { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if signature help can be dynamically registered.
         /// </summary>
         [JsonPropertyName("signatureHelp")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public SignatureHelpSetting? SignatureHelp
-        {
-            get;
-            set;
-        }
+        public SignatureHelpSetting? SignatureHelp { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if definition can be dynamically registered.
         /// </summary>
         [JsonPropertyName("definition")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? Definition
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? Definition { get; set; }
 
         /// <summary>
         /// Gets or sets the settings which determines if type definition can be dynamically registered.
         /// </summary>
         [JsonPropertyName("typeDefinition")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? TypeDefinition
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? TypeDefinition { get; set; }
 
         /// <summary>
         /// Gets or sets the settings which determines if implementation can be dynamically registered.
         /// </summary>
         [JsonPropertyName("implementation")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? Implementation
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? Implementation { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if references can be dynamically registered.
         /// </summary>
         [JsonPropertyName("references")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? References
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? References { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if document highlight can be dynamically registered.
         /// </summary>
         [JsonPropertyName("documentHighlight")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? DocumentHighlight
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? DocumentHighlight { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if document symbol can be dynamically registered.
         /// </summary>
         [JsonPropertyName("documentSymbol")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DocumentSymbolSetting? DocumentSymbol
-        {
-            get;
-            set;
-        }
+        public DocumentSymbolSetting? DocumentSymbol { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if code action can be dynamically registered.
         /// </summary>
         [JsonPropertyName("codeAction")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public CodeActionSetting? CodeAction
-        {
-            get;
-            set;
-        }
+        public CodeActionSetting? CodeAction { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if code lens can be dynamically registered.
         /// </summary>
         [JsonPropertyName("codeLens")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? CodeLens
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? CodeLens { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if document link can be dynamically registered.
         /// </summary>
         [JsonPropertyName("documentLink")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? DocumentLink
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? DocumentLink { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if formatting can be dynamically registered.
         /// </summary>
         [JsonPropertyName("formatting")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? Formatting
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? Formatting { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if range formatting can be dynamically registered.
         /// </summary>
         [JsonPropertyName("rangeFormatting")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? RangeFormatting
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? RangeFormatting { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if on type formatting can be dynamically registered.
         /// </summary>
         [JsonPropertyName("onTypeFormatting")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? OnTypeFormatting
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? OnTypeFormatting { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if rename can be dynamically registered.
         /// </summary>
         [JsonPropertyName("rename")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public RenameClientCapabilities? Rename
-        {
-            get;
-            set;
-        }
+        public RenameClientCapabilities? Rename { get; set; }
 
         /// <summary>
         /// Gets or sets the setting publish diagnostics setting.
         /// </summary>
         [JsonPropertyName("publishDiagnostics")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public PublishDiagnosticsSetting? PublishDiagnostics
-        {
-            get;
-            set;
-        }
+        public PublishDiagnosticsSetting? PublishDiagnostics { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines how folding range is supported.
         /// </summary>
         [JsonPropertyName("foldingRange")]
-        public FoldingRangeSetting? FoldingRange
-        {
-            get;
-            set;
-        }
+        public FoldingRangeSetting? FoldingRange { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if linked editing range can be dynamically registered.
         /// </summary>
         [JsonPropertyName("linkedEditingRange")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting LinkedEditingRange
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting LinkedEditingRange { get; set; }
 
         /// <summary>
         /// Gets or sets a setting indicating whether semantic tokens is supported.
         /// </summary>
         [JsonPropertyName("semanticTokens")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public SemanticTokensSetting? SemanticTokens
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// Gets or sets the setting which determines what support the client has for pull diagnostics.
-        /// </summary>
-        [JsonPropertyName("diagnostic")]
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DiagnosticSetting? Diagnostic
-        {
-            get;
-            set;
-        }
+        public SemanticTokensSetting? SemanticTokens { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines what support the client has for pull diagnostics.
         /// </summary>
         [JsonPropertyName("inlayHint")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public InlayHintSetting? InlayHint
-        {
-            get;
-            set;
-        }
+        public InlayHintSetting? InlayHint { get; set; }
+
+        /// <summary>
+        /// Gets or sets the setting which determines what support the client has for pull diagnostics.
+        /// </summary>
+        [JsonPropertyName("diagnostic")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public DiagnosticSetting? Diagnostic { get; set; }
     }
 }
--- src/LanguageServer/Protocol/Protocol/WorkspaceClientCapabilities.cs
+++ src/LanguageServer/Protocol/Protocol/WorkspaceClientCapabilities.cs
@@ -8,130 +8,89 @@ namespace Roslyn.LanguageServer.Protocol
 
     /// <summary>
     /// Class which represents workspace capabilities.
-    ///
+    /// <para>
     /// See the <see href="https://microsoft.github.io/language-server-protocol/specifications/specification-current/#clientCapabilities">Language Server Protocol specification</see> for additional information.
+    /// </para>
     /// </summary>
     internal class WorkspaceClientCapabilities
     {
+        // NOTE: these are kept in the same order as the spec to make them easier to update
+
         /// <summary>
         /// Gets or sets a value indicating whether apply edit is supported.
         /// </summary>
         [JsonPropertyName("applyEdit")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public bool ApplyEdit
-        {
-            get;
-            set;
-        }
+        public bool ApplyEdit { get; set; }
 
         /// <summary>
         /// Gets or sets the workspace edit setting.
         /// </summary>
         [JsonPropertyName("workspaceEdit")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public WorkspaceEditSetting? WorkspaceEdit
-        {
-            get;
-            set;
-        }
+        public WorkspaceEditSetting? WorkspaceEdit { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if did change configuration can be dynamically registered.
         /// </summary>
         [JsonPropertyName("didChangeConfiguration")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? DidChangeConfiguration
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? DidChangeConfiguration { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if did change watched files can be dynamically registered.
         /// </summary>
         [JsonPropertyName("didChangeWatchedFiles")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? DidChangeWatchedFiles
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? DidChangeWatchedFiles { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if symbols can be dynamically registered.
         /// </summary>
         [JsonPropertyName("symbol")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public SymbolSetting? Symbol
-        {
-            get;
-            set;
-        }
+        public SymbolSetting? Symbol { get; set; }
 
         /// <summary>
         /// Gets or sets the setting which determines if execute command can be dynamically registered.
         /// </summary>
         [JsonPropertyName("executeCommand")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DynamicRegistrationSetting? ExecuteCommand
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// Gets or sets capabilities specific to the semantic token requests scoped to the workspace.
-        /// </summary>
-        [JsonPropertyName("semanticTokens")]
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public SemanticTokensWorkspaceSetting? SemanticTokens
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// Gets or sets capabilities indicating what support the client has for workspace pull diagnostics.
-        /// </summary>
-        [JsonPropertyName("diagnostics")]
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DiagnosticWorkspaceSetting? Diagnostics
-        {
-            get;
-            set;
-        }
+        public DynamicRegistrationSetting? ExecuteCommand { get; set; }
 
         /// <summary>
         /// Gets or sets the capabilities if client support 'workspace/configuration' requests.
         /// </summary>
         [JsonPropertyName("configuration")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public bool Configuration
-        {
-            get;
-            set;
-        }
+        public bool Configuration { get; set; }
 
         /// <summary>
-        /// Gets of sets capabilities specific to the inlay hint requests scoped to the workspace.
+        /// Gets or sets capabilities specific to the semantic token requests scoped to the workspace.
         /// </summary>
-        [JsonPropertyName("inlayHint")]
+        [JsonPropertyName("semanticTokens")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public InlayHintWorkspaceSetting? InlayHint
-        {
-            get;
-            set;
-        }
+        public SemanticTokensWorkspaceSetting? SemanticTokens { get; set; }
 
         /// <summary>
         /// Gets of sets capabilities specific to the code lens requests scoped to the workspace.
         /// </summary>
         [JsonPropertyName("codeLens")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public CodeLensWorkspaceSetting? CodeLens
-        {
-            get;
-            set;
-        }
+        public CodeLensWorkspaceSetting? CodeLens { get; set; }
+
+        /// <summary>
+        /// Gets of sets capabilities specific to the inlay hint requests scoped to the workspace.
+        /// </summary>
+        [JsonPropertyName("inlayHint")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public InlayHintWorkspaceSetting? InlayHint { get; set; }
+
+        /// <summary>
+        /// Gets or sets capabilities indicating what support the client has for workspace pull diagnostics.
+        /// </summary>
+        [JsonPropertyName("diagnostics")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public DiagnosticWorkspaceSetting? Diagnostics { get; set; }
     }
 }
