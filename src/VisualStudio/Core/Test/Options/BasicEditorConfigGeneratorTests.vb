9fc2928041329a6c8cfe8b83a3f307691a4238c9
tmat <tomas.matousek@microsoft.com>
2024-06-09 09:49:34 -0700

    Fix

--- src/VisualStudio/Core/Test/Options/BasicEditorConfigGeneratorTests.vb
+++ src/VisualStudio/Core/Test/Options/BasicEditorConfigGeneratorTests.vb
@@ -7,7 +7,6 @@ Imports Microsoft.CodeAnalysis
 Imports Microsoft.CodeAnalysis.CodeStyle
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Workspaces
 Imports Microsoft.CodeAnalysis.Options
-Imports Microsoft.CodeAnalysis.Options.EditorConfig
 Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Microsoft.VisualStudio.LanguageServices.Implementation.Options
 Imports Roslyn.Test.Utilities
