da99c656fe662507c668685ded55674402d8b46c
Tomáš Matoušek <tmat@users.noreply.github.com>
2024-06-10 14:13:32 -0700

    Editorconfig options enumerator refactoring (#73907)

---
+++ src/Workspaces/VisualBasic/Portable/Options/VisualBasicEditorConfigOptionsEnumerator.vb
@@ -0,0 +1,29 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports System.Collections.Immutable
+Imports System.Composition
+Imports Microsoft.CodeAnalysis.Host.Mef
+Imports Microsoft.CodeAnalysis.Options
+Imports Microsoft.CodeAnalysis.VisualBasic.CodeStyle
+
+Namespace Microsoft.CodeAnalysis.VisualBasic.Options
+    <EditorConfigOptionsEnumerator(LanguageNames.VisualBasic), [Shared]>
+    Friend Class VisualBasicEditorConfigOptionsEnumerator
+        Implements IEditorConfigOptionsEnumerator
+
+        <ImportingConstructor>
+        <Obsolete(MefConstruction.ImportingConstructorMessage, True)>
+        Public Sub New()
+        End Sub
+
+        Public Iterator Function GetOptions() As IEnumerable(Of (String, ImmutableArray(Of IOption2))) Implements IEditorConfigOptionsEnumerator.GetOptions
+            For Each entry In EditorConfigOptionsEnumerator.GetLanguageAgnosticEditorConfigOptions()
+                Yield entry
+            Next
+
+            Yield (VBWorkspaceResources.VB_Coding_Conventions, VisualBasicCodeStyleOptions.AllOptions.As(Of IOption2))
+        End Function
+    End Class
+End Namespace
