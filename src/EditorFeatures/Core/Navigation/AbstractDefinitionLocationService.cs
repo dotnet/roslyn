a4b610074b8da1a77d499f3b9998dae02f33768e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-06-13 10:07:16 -0700

    in progress

--- src/EditorFeatures/Core/Navigation/AbstractDefinitionLocationService.cs
+++ src/EditorFeatures/Core/Navigation/AbstractDefinitionLocationService.cs
@@ -3,6 +3,10 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Editor.Host;
@@ -12,22 +16,20 @@ using Microsoft.CodeAnalysis.GoToDefinition;
 using Microsoft.CodeAnalysis.LanguageService;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Shared.Utilities;
+using Microsoft.CodeAnalysis.Text;
 using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Navigation;
 
-internal abstract class AbstractDefinitionLocationService : IDefinitionLocationService
+internal abstract partial class AbstractDefinitionLocationService(
+    IThreadingContext threadingContext,
+    IStreamingFindUsagesPresenter streamingPresenter) : IDefinitionLocationService
 {
-    private readonly IThreadingContext _threadingContext;
-    private readonly IStreamingFindUsagesPresenter _streamingPresenter;
+    private readonly IThreadingContext _threadingContext = threadingContext;
+    private readonly IStreamingFindUsagesPresenter _streamingPresenter = streamingPresenter;
 
-    protected AbstractDefinitionLocationService(
-        IThreadingContext threadingContext,
-        IStreamingFindUsagesPresenter streamingPresenter)
-    {
-        _threadingContext = threadingContext;
-        _streamingPresenter = streamingPresenter;
-    }
+    private static readonly ConditionalWeakTable<ProjectState, AsyncLazy<Dictionary<ImmutableArray<byte>, DocumentId>>> s_projectToLazyContentHashMap = new();
 
     private static Task<INavigableLocation?> GetNavigableLocationAsync(
         Document document, int position, CancellationToken cancellationToken)
@@ -42,6 +44,8 @@ internal abstract class AbstractDefinitionLocationService : IDefinitionLocationS
 
     public async Task<DefinitionLocation?> GetDefinitionLocationAsync(Document document, int position, CancellationToken cancellationToken)
     {
+        var symbolService = document.GetRequiredLanguageService<IGoToDefinitionSymbolService>();
+
         // We want to compute this as quickly as possible so that the symbol be squiggled and navigated to.  We
         // don't want to wait on expensive operations like computing source-generators or skeletons if we can avoid
         // it.  So first try with a frozen document, then fallback to a normal document.  This mirrors how go-to-def
@@ -49,47 +53,53 @@ internal abstract class AbstractDefinitionLocationService : IDefinitionLocationS
         return await GetDefinitionLocationWorkerAsync(document.WithFrozenPartialSemantics(cancellationToken)).ConfigureAwait(false) ??
                await GetDefinitionLocationWorkerAsync(document).ConfigureAwait(false);
 
-        async Task<DefinitionLocation?> GetDefinitionLocationWorkerAsync(Document document)
+        async ValueTask<DefinitionLocation?> GetDefinitionLocationWorkerAsync(Document document)
         {
-            var symbolService = document.GetRequiredLanguageService<IGoToDefinitionSymbolService>();
+            return await GetControlFlowTargetLocationAsync(document).ConfigureAwait(false) ??
+                   await GetSymbolLocationAsync(document).ConfigureAwait(false);
+        }
+
+        async ValueTask<DefinitionLocation?> GetControlFlowTargetLocationAsync(Document document)
+        {
             var (controlFlowTarget, controlFlowSpan) = await symbolService.GetTargetIfControlFlowAsync(
                 document, position, cancellationToken).ConfigureAwait(false);
-            if (controlFlowTarget != null)
-            {
-                var location = await GetNavigableLocationAsync(
-                    document, controlFlowTarget.Value, cancellationToken).ConfigureAwait(false);
-                return location is null ? null : new DefinitionLocation(location, new DocumentSpan(document, controlFlowSpan));
-            }
-            else
-            {
-                // Try to compute the referenced symbol and attempt to go to definition for the symbol.
-                var (symbol, project, span) = await symbolService.GetSymbolProjectAndBoundSpanAsync(
-                    document, position, cancellationToken).ConfigureAwait(false);
-                if (symbol is null)
-                    return null;
+            if (controlFlowTarget == null)
+                return null;
 
-                // if the symbol only has a single source location, and we're already on it,
-                // try to see if there's a better symbol we could navigate to.
-                var remappedLocation = await GetAlternativeLocationIfAlreadyOnDefinitionAsync(
-                    project, position, symbol, originalDocument: document, cancellationToken).ConfigureAwait(false);
-                if (remappedLocation != null)
-                    return new DefinitionLocation(remappedLocation, new DocumentSpan(document, span));
+            var location = await GetNavigableLocationAsync(
+                document, controlFlowTarget.Value, cancellationToken).ConfigureAwait(false);
+            return location is null ? null : new DefinitionLocation(location, new DocumentSpan(document, controlFlowSpan));
+        }
 
-                var isThirdPartyNavigationAllowed = await IsThirdPartyNavigationAllowedAsync(
-                    symbol, position, document, cancellationToken).ConfigureAwait(false);
+        async ValueTask<DefinitionLocation?> GetSymbolLocationAsync(Document document)
+        {
+            // Try to compute the referenced symbol and attempt to go to definition for the symbol.
+            var (symbol, project, span) = await symbolService.GetSymbolProjectAndBoundSpanAsync(
+                document, position, cancellationToken).ConfigureAwait(false);
+            if (symbol is null)
+                return null;
 
-                var location = await GoToDefinitionHelpers.GetDefinitionLocationAsync(
-                    symbol,
-                    project.Solution,
-                    _threadingContext,
-                    _streamingPresenter,
-                    thirdPartyNavigationAllowed: isThirdPartyNavigationAllowed,
-                    cancellationToken: cancellationToken).ConfigureAwait(false);
-                if (location is null)
-                    return null;
+            // if the symbol only has a single source location, and we're already on it,
+            // try to see if there's a better symbol we could navigate to.
+            var remappedLocation = await GetAlternativeLocationIfAlreadyOnDefinitionAsync(
+                project, position, symbol, originalDocument: document, cancellationToken).ConfigureAwait(false);
+            if (remappedLocation != null)
+                return new DefinitionLocation(remappedLocation, new DocumentSpan(document, span));
 
-                return new DefinitionLocation(location, new DocumentSpan(document, span));
-            }
+            var isThirdPartyNavigationAllowed = await IsThirdPartyNavigationAllowedAsync(
+                symbol, position, document, cancellationToken).ConfigureAwait(false);
+
+            var location = await GoToDefinitionHelpers.GetDefinitionLocationAsync(
+                symbol,
+                project.Solution,
+                _threadingContext,
+                _streamingPresenter,
+                thirdPartyNavigationAllowed: isThirdPartyNavigationAllowed,
+                cancellationToken: cancellationToken).ConfigureAwait(false);
+            if (location is null)
+                return null;
+
+            return new DefinitionLocation(location, new DocumentSpan(document, span));
         }
     }
 
@@ -154,38 +164,60 @@ internal abstract class AbstractDefinitionLocationService : IDefinitionLocationS
             // Find attributes of the form: [InterceptsLocationAttribute(version: 1, data: "...")];
 
             var attributes = method.GetAttributes();
-            var interceptsLocationAttributes = attributes.WhereAsArray(IsInterceptsLocationAttribute);
-            if (interceptsLocationAttributes.Length == 0)
+            var interceptsLocationDatas = InterceptsLocationUtilities.GetInterceptsLocationData(attributes);
+            if (interceptsLocationDatas.Length == 0)
                 return null;
 
-            // Single location, just navigate directly to that.
-            if (interceptsLocationAttributes is [var interceptsLocationAttribute])
+            var lazyContentHashToDocumentMap = s_projectToLazyContentHashMap.GetValue(
+                project.State,
+                static projectState => AsyncLazy.Create(
+                    static (projectState, cancellationToken) => ComputeContentHashToDocumentMapAsync(projectState, cancellationToken),
+                    projectState));
+
+            var contentHashToDocumentMap = await lazyContentHashToDocumentMap.GetValueAsync(cancellationToken).ConfigureAwait(false);
+
+            using var _ = ArrayBuilder<DocumentSpan>.GetInstance(out var documentSpans);
+
+            foreach (var interceptsLocationData in interceptsLocationDatas)
             {
-                if (!TryDecodeInterceptsLocationData(interceptsLocationAttribute, out var location))
-                    return null;
+                if (contentHashToDocumentMap.TryGetValue(interceptsLocationData.ContentHash, out var documentId))
+                {
+                    var document = solution.GetDocument(documentId);
+                    if (document != null)
+                        documentSpans.Add(new DocumentSpan(document, new TextSpan(interceptsLocationData.Position, 0)));
+                }
+            }
+
+            documentSpans.RemoveDuplicates();
+
+            if (documentSpans.Count == 0)
+            {
+                return null;
+            }
+            else if (documentSpans is [var documentSpan])
+            {
+                // Just one document span this mapped to.  Navigate directly do that.
+                return await documentSpan.GetNavigableLocationAsync(cancellationToken).ConfigureAwait(false);
+            }
+            else
+            {
+                // Multiple document spans this mapped to.  Show them all.
             }
         }
     }
 
-    private static bool IsInterceptsLocationAttribute(AttributeData attribute)
+    private static async Task<Dictionary<ImmutableArray<byte>, DocumentId>> ComputeContentHashToDocumentMapAsync(ProjectState projectState, CancellationToken cancellationToken)
     {
-        return attribute.AttributeClass?.Name == "InterceptsLocationAttribute" &&
-               attribute.ConstructorArguments is [{ Value: 1 }, { Value: string }];
-    }
+        var result = new Dictionary<ImmutableArray<byte>, DocumentId>(ByteArrayComparer.Instance);
 
-    private static bool TryDecodeInterceptsLocationData(AttributeData attribute)
-    {
-        Contract.ThrowIfFalse(IsInterceptsLocationAttribute(attribute));
-        var data = (string)attribute.ConstructorArguments[1].Value!;
-
-        try
+        foreach (var (documentId, documentState) in projectState.DocumentStates.States)
         {
-
+            var text = await documentState.GetTextAsync(cancellationToken).ConfigureAwait(false);
+            var checksum = text.GetContentHash();
+            result[checksum] = documentId;
         }
-        finally
-        {
 
-        }
+        return result;
     }
 
     private static async Task<bool> IsThirdPartyNavigationAllowedAsync(
