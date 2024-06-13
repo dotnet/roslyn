05156fbac6b05b6510b200c288cc708e0597a844
Shen Chen <shech@microsoft.com>
2024-06-13 16:02:56 -0700

    Fix format

--- src/EditorFeatures/Test/InheritanceMargin/InheritanceMarginTests.cs
+++ src/EditorFeatures/Test/InheritanceMargin/InheritanceMarginTests.cs
@@ -4,7 +4,6 @@
 
 using System.Collections.Immutable;
 using System.Linq;
-using System.ServiceModel.Configuration;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.InheritanceMargin;
