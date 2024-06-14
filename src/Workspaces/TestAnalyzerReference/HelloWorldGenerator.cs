7699fdc3a20a77797fb07ae7306f246b290bebf7
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-06-14 11:01:24 -0700

    revert

--- src/Workspaces/TestAnalyzerReference/HelloWorldGenerator.cs
+++ src/Workspaces/TestAnalyzerReference/HelloWorldGenerator.cs
@@ -3,10 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Linq;
 using System.Text;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.TestSourceGenerator
@@ -23,31 +20,8 @@ namespace Microsoft.CodeAnalysis.TestSourceGenerator
         {
         }
 
-#pragma warning disable RSEXPERIMENTAL002 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-
         public void Execute(GeneratorExecutionContext context)
         {
-            var methodCalls = context.Compilation.SyntaxTrees
-                .Select(t => t.GetRoot())
-                .SelectMany(n => n.DescendantNodes().OfType<InvocationExpressionSyntax>())
-                .Where(i => i.ArgumentList.Arguments.Count == 0);
-
-            var interceptorMethods = new StringBuilder();
-
-            var index = 0;
-            foreach (var methodCall in methodCalls)
-            {
-                var semanticModel = context.Compilation.GetSemanticModel(methodCall.SyntaxTree);
-                var interceptorLocation = semanticModel.GetInterceptableLocation(methodCall);
-
-                if (interceptorLocation != null)
-                {
-                    interceptorMethods.AppendLine(interceptorLocation.GetInterceptsLocationAttributeSyntax());
-                    interceptorMethods.AppendLine($@"public static void Intercept{index++}() {{ }}");
-                    interceptorMethods.AppendLine();
-                }
-            }
-
             context.AddSource(GeneratedEnglishClassName, SourceText.From($$"""
                 /// <summary><see cref="{{GeneratedEnglishClassName}}" /> is a simple class to fetch the classic message.</summary>
                 internal class {{GeneratedEnglishClassName}}
@@ -56,8 +30,6 @@ namespace Microsoft.CodeAnalysis.TestSourceGenerator
                     {
                         return "Hello, World!";
                     }
-
-                    {{interceptorMethods}}
                 }
                 """, encoding: Encoding.UTF8));
 
