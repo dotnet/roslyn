c18fb1bb179c403527da469c44193fd4fb501a1d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-06-13 22:23:13 -0700

    usings

--- src/EditorFeatures/Core/Navigation/AbstractDefinitionLocationService.cs
+++ src/EditorFeatures/Core/Navigation/AbstractDefinitionLocationService.cs
@@ -2,10 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Classification;
@@ -17,7 +14,6 @@ using Microsoft.CodeAnalysis.LanguageService;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Shared.Utilities;
-using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Navigation;
 
