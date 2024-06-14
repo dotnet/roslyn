56392a8187c4e8b878f4a84ef56c420268a9d82d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-06-14 10:27:55 -0700

    Cleanup

--- src/EditorFeatures/Core/GoToDefinition/GoToDefinitionHelpers.cs
+++ src/EditorFeatures/Core/GoToDefinition/GoToDefinitionHelpers.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Editor.Host;
