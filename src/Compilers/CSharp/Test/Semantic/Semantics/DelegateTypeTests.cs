dfeec7fb8492accfeda1dea65f9f428bd940dd22
Jan Jones <janjones@microsoft.com>
2024-06-20 15:11:41 +0200

    Remove unnecessary using

--- src/Compilers/CSharp/Test/Semantic/Semantics/DelegateTypeTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/DelegateTypeTests.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using ILVerify;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
