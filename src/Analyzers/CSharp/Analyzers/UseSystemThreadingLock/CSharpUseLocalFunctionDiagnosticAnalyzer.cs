76a955e403e275a7c71234f2afeb5f1e9a1bd76f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-06-26 09:31:21 -0700

    Finish analyzer

--- src/Analyzers/CSharp/Analyzers/UseSystemThreadingLock/CSharpUseLocalFunctionDiagnosticAnalyzer.cs
+++ src/Analyzers/CSharp/Analyzers/UseSystemThreadingLock/CSharpUseLocalFunctionDiagnosticAnalyzer.cs
@@ -48,6 +48,9 @@ internal class CSharpUseSystemThreadingLockDiagnosticAnalyzer : AbstractBuiltInC
     {
     }
 
+    public override DiagnosticAnalyzerCategory GetAnalyzerCategory()
+        => DiagnosticAnalyzerCategory.SemanticSpanAnalysis;
+
     protected override void InitializeWorker(AnalysisContext context)
     {
         context.RegisterCompilationStartAction(compilationContext =>
@@ -152,299 +155,36 @@ internal class CSharpUseSystemThreadingLockDiagnosticAnalyzer : AbstractBuiltInC
                 return;
             }
 
+            // Fine to use `nameof(someLock)` as that's not actually using the lock.
+            if (fieldReferenceOperation.Parent is INameOfOperation)
+                return;
+
             // Add more supported patterns here as needed.
 
             // This wasn't a supported case.
             potentialLockFields.Remove(fieldReference);
         }, OperationKind.FieldReference);
 
-        context.RegisterSymbolEndAction()
-
-        if (styleOption.Notification.Severity.WithDefaultSeverity(DiagnosticSeverity.Hidden) < ReportDiagnostic.Hidden)
+        context.RegisterSymbolEndAction(context =>
         {
-            // If the diagnostic is not hidden, then just place the user visible part
-            // on the local being initialized with the lambda.
-            syntaxContext.ReportDiagnostic(DiagnosticHelper.Create(
-                Descriptor,
-                localDeclaration.Declaration.Variables[0].Identifier.GetLocation(),
-                styleOption.Notification,
-                syntaxContext.Options,
-                additionalLocations,
-                properties: null));
-        }
-        else
-        {
-            // If the diagnostic is hidden, place it on the entire construct.
-            syntaxContext.ReportDiagnostic(DiagnosticHelper.Create(
-                Descriptor,
-                localDeclaration.GetLocation(),
-                styleOption.Notification,
-                syntaxContext.Options,
-                additionalLocations,
-                properties: null));
+            var cancellationToken = context.CancellationToken;
 
-            if (shouldReportOnAnonymousFunctionStatement)
+            foreach (var lockField in potentialLockFields)
             {
-                syntaxContext.ReportDiagnostic(DiagnosticHelper.Create(
+                if (!wasLockedSet.Contains(lockField))
+                    continue;
+
+                // .Single is safe here as we confirmed there was only one DeclaringSyntaxReference in the field at the beginning of analysis.
+                var declarator = (VariableDeclaratorSyntax)lockField.DeclaringSyntaxReferences.Single().GetSyntax(cancellationToken);
+
+                context.ReportDiagnostic(DiagnosticHelper.Create(
                     Descriptor,
-                    anonymousFunctionStatement!.GetLocation(),
-                    styleOption.Notification,
-                    syntaxContext.Options,
-                    additionalLocations,
+                    declarator.Identifier.GetLocation(),
+                    option.Notification,
+                    context.Options,
+                    additionalLocations: null,
                     properties: null));
             }
-        }
-
-        static bool IsInAnalysisSpan(
-            SyntaxNodeAnalysisContext context,
-            LocalDeclarationStatementSyntax localDeclaration,
-            StatementSyntax? anonymousFunctionStatement,
-            bool shouldReportOnAnonymousFunctionStatement)
-        {
-            if (context.ShouldAnalyzeSpan(localDeclaration.Span))
-                return true;
-
-            if (shouldReportOnAnonymousFunctionStatement
-                && context.ShouldAnalyzeSpan(anonymousFunctionStatement!.Span))
-            {
-                return true;
-            }
-
-            return false;
-        }
+        });
     }
-
-    private static bool CheckForPattern(
-        AnonymousFunctionExpressionSyntax anonymousFunction,
-        [NotNullWhen(true)] out LocalDeclarationStatementSyntax? localDeclaration)
-    {
-        // Look for:
-        //
-        // Type t = <anonymous function>
-        // var t = (Type)(<anonymous function>)
-        //
-        // Type t = null;
-        // t = <anonymous function>
-        return CheckForSimpleLocalDeclarationPattern(anonymousFunction, out localDeclaration) ||
-               CheckForCastedLocalDeclarationPattern(anonymousFunction, out localDeclaration) ||
-               CheckForLocalDeclarationAndAssignment(anonymousFunction, out localDeclaration);
-    }
-
-    private static bool CheckForSimpleLocalDeclarationPattern(
-        AnonymousFunctionExpressionSyntax anonymousFunction,
-        [NotNullWhen(true)] out LocalDeclarationStatementSyntax? localDeclaration)
-    {
-        // Type t = <anonymous function>
-        if (anonymousFunction is
-            {
-                Parent: EqualsValueClauseSyntax
-                {
-                    Parent: VariableDeclaratorSyntax
-                    {
-                        Parent: VariableDeclarationSyntax
-                        {
-                            Parent: LocalDeclarationStatementSyntax declaration,
-                        }
-                    }
-                }
-            })
-        {
-            localDeclaration = declaration;
-            return true;
-        }
-
-        localDeclaration = null;
-        return false;
-    }
-
-    private static bool CanReplaceAnonymousWithLocalFunction(
-        SemanticModel semanticModel, INamedTypeSymbol? expressionTypeOpt, ISymbol local, BlockSyntax block,
-        AnonymousFunctionExpressionSyntax anonymousFunction, out ImmutableArray<Location> referenceLocations, CancellationToken cancellationToken)
-    {
-        // Check all the references to the anonymous function and disallow the conversion if
-        // they're used in certain ways.
-        using var _ = ArrayBuilder<Location>.GetInstance(out var references);
-        referenceLocations = [];
-        var anonymousFunctionStart = anonymousFunction.SpanStart;
-        foreach (var descendentNode in block.DescendantNodes())
-        {
-            var descendentStart = descendentNode.Span.Start;
-            if (descendentStart <= anonymousFunctionStart)
-            {
-                // This node is before the local declaration.  Can ignore it entirely as it could
-                // not be an access to the local.
-                continue;
-            }
-
-            if (descendentNode is IdentifierNameSyntax identifierName)
-            {
-                if (identifierName.Identifier.ValueText == local.Name &&
-                    local.Equals(semanticModel.GetSymbolInfo(identifierName, cancellationToken).GetAnySymbol()))
-                {
-                    if (identifierName.IsWrittenTo(semanticModel, cancellationToken))
-                    {
-                        // Can't change this to a local function if it is assigned to.
-                        return false;
-                    }
-
-                    var nodeToCheck = identifierName.WalkUpParentheses();
-                    if (nodeToCheck.Parent is BinaryExpressionSyntax)
-                    {
-                        // Can't change this if they're doing things like delegate addition with
-                        // the lambda.
-                        return false;
-                    }
-
-                    if (nodeToCheck.Parent is InvocationExpressionSyntax invocationExpression)
-                    {
-                        references.Add(invocationExpression.GetLocation());
-                    }
-                    else if (nodeToCheck.Parent is MemberAccessExpressionSyntax memberAccessExpression)
-                    {
-                        if (memberAccessExpression.Parent is InvocationExpressionSyntax explicitInvocationExpression &&
-                            memberAccessExpression.Name.Identifier.ValueText == WellKnownMemberNames.DelegateInvokeName)
-                        {
-                            references.Add(explicitInvocationExpression.GetLocation());
-                        }
-                        else
-                        {
-                            // They're doing something like "del.ToString()".  Can't do this with a
-                            // local function.
-                            return false;
-                        }
-                    }
-                    else
-                    {
-                        references.Add(nodeToCheck.GetLocation());
-                    }
-
-                    var convertedType = semanticModel.GetTypeInfo(nodeToCheck, cancellationToken).ConvertedType;
-                    if (!convertedType.IsDelegateType())
-                    {
-                        // We can't change this anonymous function into a local function if it is
-                        // converted to a non-delegate type (i.e. converted to 'object' or 
-                        // 'System.Delegate'). Local functions are not convertible to these types.  
-                        // They're only convertible to other delegate types.
-                        return false;
-                    }
-
-                    if (nodeToCheck.IsInExpressionTree(semanticModel, expressionTypeOpt, cancellationToken))
-                    {
-                        // Can't reference a local function inside an expression tree.
-                        return false;
-                    }
-                }
-            }
-        }
-
-        referenceLocations = references.ToImmutableAndClear();
-        return true;
-    }
-
-    private static bool CheckForCastedLocalDeclarationPattern(
-        AnonymousFunctionExpressionSyntax anonymousFunction,
-        [NotNullWhen(true)] out LocalDeclarationStatementSyntax? localDeclaration)
-    {
-        // var t = (Type)(<anonymous function>)
-        var containingStatement = anonymousFunction.GetAncestor<StatementSyntax>();
-        if (containingStatement.IsKind(SyntaxKind.LocalDeclarationStatement, out localDeclaration) &&
-            localDeclaration.Declaration.Variables.Count == 1)
-        {
-            var variableDeclarator = localDeclaration.Declaration.Variables[0];
-            if (variableDeclarator.Initializer != null)
-            {
-                var value = variableDeclarator.Initializer.Value.WalkDownParentheses();
-                if (value is CastExpressionSyntax castExpression)
-                {
-                    if (castExpression.Expression.WalkDownParentheses() == anonymousFunction)
-                    {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        localDeclaration = null;
-        return false;
-    }
-
-    private static bool CheckForLocalDeclarationAndAssignment(
-        AnonymousFunctionExpressionSyntax anonymousFunction,
-        [NotNullWhen(true)] out LocalDeclarationStatementSyntax? localDeclaration)
-    {
-        // Type t = null;
-        // t = <anonymous function>
-        if (anonymousFunction?.Parent is AssignmentExpressionSyntax(SyntaxKind.SimpleAssignmentExpression) assignment &&
-            assignment.Parent is ExpressionStatementSyntax expressionStatement &&
-            expressionStatement.Parent is BlockSyntax block)
-        {
-            if (assignment.Left.IsKind(SyntaxKind.IdentifierName))
-            {
-                var expressionStatementIndex = block.Statements.IndexOf(expressionStatement);
-                if (expressionStatementIndex >= 1)
-                {
-                    var previousStatement = block.Statements[expressionStatementIndex - 1];
-                    if (previousStatement.IsKind(SyntaxKind.LocalDeclarationStatement, out localDeclaration) &&
-                        localDeclaration.Declaration.Variables.Count == 1)
-                    {
-                        var variableDeclarator = localDeclaration.Declaration.Variables[0];
-                        if (variableDeclarator.Initializer == null ||
-                            variableDeclarator.Initializer.Value.Kind() is
-                                SyntaxKind.NullLiteralExpression or
-                                SyntaxKind.DefaultLiteralExpression or
-                                SyntaxKind.DefaultExpression)
-                        {
-                            var identifierName = (IdentifierNameSyntax)assignment.Left;
-                            if (variableDeclarator.Identifier.ValueText == identifierName.Identifier.ValueText)
-                            {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        localDeclaration = null;
-        return false;
-    }
-
-    private static bool CanReplaceDelegateWithLocalFunction(
-        INamedTypeSymbol delegateType,
-        LocalDeclarationStatementSyntax localDeclaration,
-        SemanticModel semanticModel,
-        CancellationToken cancellationToken)
-    {
-        var delegateContainingType = delegateType.ContainingType;
-        if (delegateContainingType is null || !delegateContainingType.IsGenericType)
-        {
-            return true;
-        }
-
-        var delegateTypeParamNames = delegateType.GetAllTypeParameters().Select(p => p.Name).ToImmutableHashSet();
-        var localEnclosingSymbol = semanticModel.GetEnclosingSymbol(localDeclaration.SpanStart, cancellationToken);
-        while (localEnclosingSymbol != null)
-        {
-            if (localEnclosingSymbol.Equals(delegateContainingType))
-            {
-                return true;
-            }
-
-            var typeParams = localEnclosingSymbol.GetTypeParameters();
-            if (typeParams.Any())
-            {
-                if (typeParams.Any(static (p, delegateTypeParamNames) => delegateTypeParamNames.Contains(p.Name), delegateTypeParamNames))
-                {
-                    return false;
-                }
-            }
-
-            localEnclosingSymbol = localEnclosingSymbol.ContainingType;
-        }
-
-        return true;
-    }
-
-    public override DiagnosticAnalyzerCategory GetAnalyzerCategory()
-        => DiagnosticAnalyzerCategory.SemanticSpanAnalysis;
 }
