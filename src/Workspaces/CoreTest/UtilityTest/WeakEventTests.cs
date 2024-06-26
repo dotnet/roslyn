beeba0369dbea44bd54ab98896c175485e3c23d8
tmat <tomas.matousek@microsoft.com>
2024-06-25 17:45:47 -0700

    Test

---
+++ src/Workspaces/CoreTest/UtilityTest/WeakEventTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Roslyn.Test.Utilities;
+using Roslyn.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.UnitTests.UtilityTest;
+
+public class WeakEventTests
+{
+    [Fact]
+    public void AddAndRemove()
+    {
+        var e = new WeakEvent<int>();
+
+        var target = new List<int>();
+        var handler1 = new EventHandler<int>((sender, arg) => Assert.IsType<List<int>>(sender).Add(arg * 10));
+        var handler2 = new EventHandler<int>((sender, arg) => Assert.IsType<List<int>>(sender).Add(arg * 20));
+        var handler3 = new EventHandler<int>((sender, arg) => Assert.IsType<List<int>>(sender).Add(arg * 30));
+
+        e.AddHandler(target, handler1);
+        e.AddHandler(target, handler2);
+        e.AddHandler(target, handler3);
+
+        e.RemoveHandler(target, handler2);
+
+        e.RaiseEvent(1);
+
+        AssertEx.Equal([10, 30], target);
+        target.Clear();
+
+        e.RemoveHandler(target, handler1);
+        e.RemoveHandler(target, handler3);
+
+        e.RaiseEvent(1);
+        Assert.Empty(target);
+    }
+}
