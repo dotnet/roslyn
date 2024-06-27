c5e8624c7601a2d04424cfb00b3899bb57ca0dc6
Todd Grunke <toddgrun@microsoft.com>
2024-06-27 14:42:05 -0700

    Fix scenario where lightbulbs weren't being displayed
    Addresses https://devdiv.visualstudio.com/DevDiv/_workitems/edit/2087100
    
    The recent change to remove solution crawler has apparently caused some regressions in lightbulb functionality and performance. This PR attempts to address at least one functional issue introduced as part of that change.
    
    Previously, during solution load, solution crawler would cause the analyzer state sets to update via IncrementalAnalyzerProcessor.LowPriorityProcessor.ProcessProjectAsync's call to DiagnosticIncrementalAnalyzer.AnalyzeProjectAsync. Similarly, document updates would update the analyzer states the crawler's idle processing.
    
    Instead of trying to hook back in these calls, this PR instead changes the diagnostic analyzer service to no longer offer a TryGetDiagnosticsForSpanAsync, instead forcing users to use the GetDiagnosticsForSpanAsync instead. The only caller of TryGetDiagnosticsForSpanAsync was inside the CodeFixService, and it seems like that code should just go ahead and force correct results. Thus, there is no concept of a non-upToDate call into the diagnostic service.
    
    I'm going to separately investigate an issue with the StateManager's statesets not getting updated. I'm unsure why the code doesn't update the statesets upon calculation, but the fact that we are now recreating this on every lightbulb request is at least partly responsible for the recent lightbulb perf regression.

--- src/LanguageServer/Protocol/Features/Diagnostics/DiagnosticAnalyzerService.cs
+++ src/LanguageServer/Protocol/Features/Diagnostics/DiagnosticAnalyzerService.cs
@@ -69,30 +69,6 @@ namespace Microsoft.CodeAnalysis.Diagnostics
         public void RequestDiagnosticRefresh()
             => _diagnosticsRefresher?.RequestWorkspaceRefresh();
 
-        public async Task<(ImmutableArray<DiagnosticData> diagnostics, bool upToDate)> TryGetDiagnosticsForSpanAsync(
-            TextDocument document,
-            TextSpan range,
-            Func<string, bool>? shouldIncludeDiagnostic,
-            bool includeSuppressedDiagnostics,
-            ICodeActionRequestPriorityProvider priorityProvider,
-            DiagnosticKind diagnosticKinds,
-            bool isExplicit,
-            CancellationToken cancellationToken)
-        {
-            var analyzer = CreateIncrementalAnalyzer(document.Project.Solution.Workspace);
-
-            // always make sure that analyzer is called on background thread.
-            await TaskScheduler.Default;
-            priorityProvider ??= new DefaultCodeActionRequestPriorityProvider();
-
-            using var _ = ArrayBuilder<DiagnosticData>.GetInstance(out var diagnostics);
-            var upToDate = await analyzer.TryAppendDiagnosticsForSpanAsync(
-                document, range, diagnostics, shouldIncludeDiagnostic,
-                includeSuppressedDiagnostics, true, priorityProvider, blockForData: false,
-                diagnosticKinds, isExplicit, cancellationToken).ConfigureAwait(false);
-            return (diagnostics.ToImmutable(), upToDate);
-        }
-
         public async Task<ImmutableArray<DiagnosticData>> GetDiagnosticsForSpanAsync(
             TextDocument document,
             TextSpan? range,
@@ -105,13 +81,14 @@ namespace Microsoft.CodeAnalysis.Diagnostics
             CancellationToken cancellationToken)
         {
             var analyzer = CreateIncrementalAnalyzer(document.Project.Solution.Workspace);
-            priorityProvider ??= new DefaultCodeActionRequestPriorityProvider();
 
             // always make sure that analyzer is called on background thread.
             await TaskScheduler.Default;
+            priorityProvider ??= new DefaultCodeActionRequestPriorityProvider();
+
             return await analyzer.GetDiagnosticsForSpanAsync(
                 document, range, shouldIncludeDiagnostic, includeSuppressedDiagnostics, includeCompilerDiagnostics,
-                priorityProvider, blockForData: true, diagnosticKinds, isExplicit, cancellationToken).ConfigureAwait(false);
+                priorityProvider, diagnosticKinds, isExplicit, cancellationToken).ConfigureAwait(false);
         }
 
         public Task<ImmutableArray<DiagnosticData>> GetCachedDiagnosticsAsync(Workspace workspace, ProjectId? projectId, DocumentId? documentId, bool includeSuppressedDiagnostics, bool includeLocalDocumentDiagnostics, bool includeNonLocalDocumentDiagnostics, CancellationToken cancellationToken)
