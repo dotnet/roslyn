b71f6fdd9fe3ded99ba58a6e73f66562ae2ddd22
Tomáš Matoušek <tmat@users.noreply.github.com>
2024-06-28 12:23:01 -0700

    EnumerableConditionalWeakTable and WeakEvent (#74160)

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Utilities/EventHandlerFactory.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Utilities/EventHandlerFactory.cs
@@ -6,7 +6,7 @@ using System;
 
 namespace Roslyn.Utilities;
 
-internal static class WeakEventHandler<TArgs>
+internal static class EventHandlerFactory<TArgs>
 {
     /// <summary>
     /// Creates an event handler that holds onto the target weakly.
@@ -14,7 +14,7 @@ internal static class WeakEventHandler<TArgs>
     /// <param name="target">The target that is held weakly, and passed as an argument to the invoker.</param>
     /// <param name="invoker">An action that will receive the event arguments as well as the target instance. 
     /// The invoker itself must not capture any state.</param>
-    public static EventHandler<TArgs> Create<TTarget>(TTarget target, Action<TTarget, object?, TArgs> invoker)
+    public static EventHandler<TArgs> CreateWeakHandler<TTarget>(TTarget target, Action<TTarget, object?, TArgs> invoker)
         where TTarget : class
     {
         var weakTarget = new WeakReference<TTarget>(target);
