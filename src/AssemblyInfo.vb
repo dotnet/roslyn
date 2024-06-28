39d0a95056a1af5c61ca0a57eb3ac16a6e5da058
Andrew Arnott <andrew.arnott@microsoft.com>
2024-06-28 13:56:21 -0600

    Fix copyright notice

--- src/AssemblyInfo.vb
+++ src/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-' Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
+' Copyright (c) Microsoft Corporation. All rights reserved.
 ' Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 Imports System.Runtime.InteropServices
