2ee53757a847d680b8cd51218368b63172ce931e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-07-03 12:32:37 -0700

    Pass along correct VS guid when reporting task list items

---
+++ src/LanguageServer/Protocol/Handler/Diagnostics/IDiagnosticProjectInformationService.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Roslyn.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Diagnostics;
+
+internal interface IDiagnosticProjectInformationService : IWorkspaceService
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    VSDiagnosticProjectInformation GetDiagnosticProjectInformation(Project project);
+}
+
+[ExportWorkspaceService(typeof(IDiagnosticProjectInformationService)), Shared]
+[method: ImportingConstructor]
+[method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+internal sealed class DefaultDiagnosticProjectInformationService() : IDiagnosticProjectInformationService
+{
+    public VSDiagnosticProjectInformation GetDiagnosticProjectInformation(Project project)
+        => GetDiagnosticProjectInformationHelper(project);
+
+    public static VSDiagnosticProjectInformation GetDiagnosticProjectInformationHelper(Project project)
+        => new()
+        {
+            ProjectIdentifier = project.Id.Id.ToString(),
+            ProjectName = project.Name,
+        };
+}
