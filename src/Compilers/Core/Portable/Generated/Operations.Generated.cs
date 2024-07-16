e6f52abb133c6af86655411d111006f2034c57cd
Fredric Silberberg <frsilb@microsoft.com>
2024-07-16 11:04:26 -0700

    Update the IOp writer for the new pattern.

--- src/Compilers/Core/Portable/Generated/Operations.Generated.cs
+++ src/Compilers/Core/Portable/Generated/Operations.Generated.cs
@@ -10511,8 +10511,8 @@ namespace Microsoft.CodeAnalysis.Operations
         [return: NotNullIfNotNull("node")]
         private T? Visit<T>(T? node) where T : IOperation? => (T?)Visit(node, argument: null);
         public override IOperation DefaultVisit(IOperation operation, object? argument) => throw ExceptionUtilities.Unreachable();
-        private ImmutableArray<T> VisitArray<T>(ImmutableArray<T> nodes) where T : IOperation => nodes.SelectAsArray(map: (n, @this) => @this.Visit(n), arg: this)!;
-        private ImmutableArray<(ISymbol, T)> VisitArray<T>(ImmutableArray<(ISymbol, T)> nodes) where T : IOperation => nodes.SelectAsArray(map: (n, @this) => (n.Item1, @this.Visit(n.Item2)), arg: this)!;
+        private ImmutableArray<T> VisitArray<T>(ImmutableArray<T> nodes) where T : IOperation => nodes.SelectAsArray(this, (n, @this) => @this.Visit(n))!;
+        private ImmutableArray<(ISymbol, T)> VisitArray<T>(ImmutableArray<(ISymbol, T)> nodes) where T : IOperation => nodes.SelectAsArray(this, (n, @this) => (n.Item1, @this.Visit(n.Item2)))!;
         public override IOperation VisitBlock(IBlockOperation operation, object? argument)
         {
             var internalOperation = (BlockOperation)operation;
--- src/Tools/Source/CompilerGeneratorTools/Source/IOperationGenerator/IOperationClassWriter.cs
+++ src/Tools/Source/CompilerGeneratorTools/Source/IOperationGenerator/IOperationClassWriter.cs
@@ -936,8 +936,8 @@ namespace IOperationGenerator
             WriteLine(@"[return: NotNullIfNotNull(""node"")]");
             WriteLine("private T? Visit<T>(T? node) where T : IOperation? => (T?)Visit(node, argument: null);");
             WriteLine("public override IOperation DefaultVisit(IOperation operation, object? argument) => throw ExceptionUtilities.Unreachable();");
-            WriteLine("private ImmutableArray<T> VisitArray<T>(ImmutableArray<T> nodes) where T : IOperation => nodes.SelectAsArray((n, @this) => @this.Visit(n), this)!;");
-            WriteLine("private ImmutableArray<(ISymbol, T)> VisitArray<T>(ImmutableArray<(ISymbol, T)> nodes) where T : IOperation => nodes.SelectAsArray((n, @this) => (n.Item1, @this.Visit(n.Item2)), this)!;");
+            WriteLine("private ImmutableArray<T> VisitArray<T>(ImmutableArray<T> nodes) where T : IOperation => nodes.SelectAsArray(this, (n, @this) => @this.Visit(n))!;");
+            WriteLine("private ImmutableArray<(ISymbol, T)> VisitArray<T>(ImmutableArray<(ISymbol, T)> nodes) where T : IOperation => nodes.SelectAsArray(this, (n, @this) => (n.Item1, @this.Visit(n.Item2)))!;");
 
             foreach (var node in _tree.Types.OfType<Node>())
             {
