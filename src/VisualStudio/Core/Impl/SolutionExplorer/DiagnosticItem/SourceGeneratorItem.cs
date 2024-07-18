3f0a8f9a08b72054ea339da39ab18e5c8e60c024
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-07-18 09:15:45 -0700

    In progress

--- src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/SourceGeneratorItem.cs
+++ src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/SourceGeneratorItem.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.Imaging;
 using Microsoft.VisualStudio.Imaging.Interop;
 
@@ -11,19 +11,32 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
 
 internal sealed partial class SourceGeneratorItem(
     ProjectId projectId,
-    string generatorName,
-    SourceGeneratorIdentity generatorIdentity,
-    AnalyzerReference analyzerReference) : BaseItem(generatorName)
+    string name,
+    SourceGeneratorIdentity identity,
+    string? path) : BaseItem(name), IEquatable<SourceGeneratorItem>
 {
     public ProjectId ProjectId { get; } = projectId;
-    public SourceGeneratorIdentity Identity { get; } = generatorIdentity;
-    public AnalyzerReference AnalyzerReference { get; } = analyzerReference;
+    public SourceGeneratorIdentity Identity { get; } = identity;
+    public string? Path { get; } = path;
 
     // TODO: do we need an icon for our use?
     public override ImageMoniker IconMoniker => KnownMonikers.Process;
 
     public override object GetBrowseObject()
+        => new BrowseObject(this);
+
+    public override int GetHashCode()
+        => throw new NotImplementedException();
+
+    public override bool Equals(object obj)
+        => Equals(obj as SourceGeneratorItem);
+
+    public bool Equals(SourceGeneratorItem? other)
     {
-        return new BrowseObject(this);
+        return other != null &&
+            this.Name == other.Name &&
+            this.ProjectId == other.ProjectId &&
+            this.Identity == other.Identity &&
+            this.Path == other.Path;
     }
 }
