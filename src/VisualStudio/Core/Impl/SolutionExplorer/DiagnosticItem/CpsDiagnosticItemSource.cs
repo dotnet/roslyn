c0de5f3e5357151cc6edb08307da09e58705b80a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-07-18 08:14:34 -0700

    Comment formatting

--- src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/CpsDiagnosticItemSource.cs
+++ src/VisualStudio/Core/Impl/SolutionExplorer/DiagnosticItem/CpsDiagnosticItemSource.cs
@@ -34,9 +34,11 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
             _analyzerReference = TryGetAnalyzerReference(Workspace.CurrentSolution);
             if (_analyzerReference == null)
             {
-                // The ProjectId that was given to us was found by enumerating the list of projects in the solution, thus the project must have already
-                // been added to the workspace at some point. As long as the project is still there, we're going to assume the reason we don't have the reference
-                // yet is because while we have a project, we don't have all the references added yet. We'll wait until we see the reference and then connect to it.
+                // The ProjectId that was given to us was found by enumerating the list of projects in the solution,
+                // thus the project must have already been added to the workspace at some point. As long as the project
+                // is still there, we're going to assume the reason we don't have the reference yet is because while we
+                // have a project, we don't have all the references added yet. We'll wait until we see the reference and
+                // then connect to it.
                 if (workspace.CurrentSolution.ContainsProject(projectId))
                 {
                     Workspace.WorkspaceChanged += OnWorkspaceChangedLookForAnalyzer;
