70e4931e95909c86255d4f4a2edff639aa42c21a
tmat <tomas.matousek@microsoft.com>
2024-07-22 17:57:14 -0700

    EnC: Implement support for partial properties

---
+++ src/Compilers/Core/Portable/Symbols/IPropertySymbolInternal.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.CodeAnalysis.Symbols;
+
+internal interface IPropertySymbolInternal : ISymbolInternal
+{
+    IPropertySymbolInternal? PartialImplementationPart { get; }
+    IPropertySymbolInternal? PartialDefinitionPart { get; }
+}
