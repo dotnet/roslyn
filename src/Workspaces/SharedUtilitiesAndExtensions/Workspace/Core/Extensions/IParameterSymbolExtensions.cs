9ad4bc135a7e36149b1ff20eaa30a068ce5ac722
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-07-24 10:52:20 -0700

    Move resource

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Extensions/IParameterSymbolExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Extensions/IParameterSymbolExtensions.cs
@@ -10,16 +10,25 @@ namespace Microsoft.CodeAnalysis.Shared.Extensions;
 
 internal static partial class IParameterSymbolExtensions
 {
-    public static IParameterSymbol WithAttributes(this IParameterSymbol parameter, ImmutableArray<AttributeData> attributes)
+    public static ImmutableArray<IParameterSymbol> RenameParameters(this IList<IParameterSymbol> parameters, ImmutableArray<string> parameterNames)
     {
-        return parameter.GetAttributes() == attributes
+        var result = new FixedSizeArrayBuilder<IParameterSymbol>(parameters.Count);
+        for (var i = 0; i < parameterNames.Length; i++)
+            result.Add(parameters[i].RenameParameter(parameterNames[i]));
+
+        return result.MoveToImmutable();
+    }
+
+    public static IParameterSymbol RenameParameter(this IParameterSymbol parameter, string parameterName)
+    {
+        return parameter.Name == parameterName
             ? parameter
             : CodeGenerationSymbolFactory.CreateParameterSymbol(
-                    attributes,
+                    parameter.GetAttributes(),
                     parameter.RefKind,
                     parameter.IsParams,
                     parameter.Type,
-                    parameter.Name,
+                    parameterName,
                     parameter.IsOptional,
                     parameter.HasExplicitDefaultValue,
                     parameter.HasExplicitDefaultValue ? parameter.ExplicitDefaultValue : null);
