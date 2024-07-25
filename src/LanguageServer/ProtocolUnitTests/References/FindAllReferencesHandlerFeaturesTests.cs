e76f120343eb3660c0b2231fabbdec111ff231f1
David Barbet <dabarbet@microsoft.com>
2024-07-25 16:48:53 -0700

    Add LSP far many references tests

--- src/LanguageServer/ProtocolUnitTests/References/FindAllReferencesHandlerFeaturesTests.cs
+++ src/LanguageServer/ProtocolUnitTests/References/FindAllReferencesHandlerFeaturesTests.cs
@@ -3,7 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
+using EnvDTE;
 using Microsoft.CodeAnalysis.Editor.Test;
 using Microsoft.CodeAnalysis.Test.Utilities;
 using Roslyn.Test.Utilities;
@@ -46,5 +48,37 @@ class B
 
         var results = await FindAllReferencesHandlerTests.RunFindAllReferencesNonVSAsync(testLspServer, testLspServer.GetLocations("caret").First());
         AssertLocationsEqual(testLspServer.GetLocations("reference"), results.Select(result => result));
+    }
+
+    [Theory, CombinatorialData]
+    public async Task TestFindAllReferencesAsync_LargeNumberOfReferences(bool mutatingLspWorkspace, [CombinatorialRange(0, 50)] int iteration)
+    {
+        _ = iteration;
+        var markup =
+@"using System.Threading.Tasks
+class A
+{
+    private {|caret:Task|} someTask = Task.CompletedTask;
+}";
+        await using var testLspServer = await CreateTestLspServerAsync(markup, mutatingLspWorkspace, new LSP.ClientCapabilities());
+
+        for (var i = 0; i < 100; i++)
+        {
+            var source = $$"""
+            using System.Threading.Tasks
+            class SomeClass{{i}}
+            {
+                private Task someTask;
+            }
+            """;
+
+            var testDocument = new EditorTestHostDocument(text: source, displayName: @$"C:\SomeFile{i}.cs", exportProvider: testLspServer.TestWorkspace.ExportProvider, filePath: @$"C:\SomeFile{i}.cs");
+            testLspServer.TestWorkspace.AddTestProject(new EditorTestHostProject(testLspServer.TestWorkspace, documents: new[] { testDocument }));
+        }
+
+        await WaitForWorkspaceOperationsAsync(testLspServer.TestWorkspace);
+
+        var results = await FindAllReferencesHandlerTests.RunFindAllReferencesNonVSAsync(testLspServer, testLspServer.GetLocations("caret").First());
+        Assert.Equal(103, results.Length);
     }
 }
