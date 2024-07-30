0f0ae058d600a44f754c5ce3192bd387ba3c77d2
David Wengier <david.wengier@microsoft.com>
2024-07-30 14:30:20 +1000

    Add missing Lsp types/properties/defaults

--- src/LanguageServer/Protocol/Protocol/Internal/Diagnostics/VSInternalDiagnosticKind.cs
+++ src/LanguageServer/Protocol/Protocol/Internal/Diagnostics/VSInternalDiagnosticKind.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -20,8 +20,8 @@ namespace Roslyn.LanguageServer.Protocol
         public static readonly VSInternalDiagnosticKind Task = new("task");
 
         /// <summary>
-        /// Edit and Continue diagnostic kind.
+        /// Syntax diagnostic kind.
         /// </summary>
-        public static readonly VSInternalDiagnosticKind EditAndContiue = new("enc");
+        public static readonly VSInternalDiagnosticKind Syntax = new("syntax");
     }
 }
