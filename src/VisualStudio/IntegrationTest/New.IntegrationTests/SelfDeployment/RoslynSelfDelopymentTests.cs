36065a31bb679290609d604f79b050f5f03f1250
Shen Chen <shech@microsoft.com>
2024-07-31 15:26:26 -0700

    Add test stub

---
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/SelfDeployment/RoslynSelfDelopymentTests.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Roslyn.VisualStudio.IntegrationTests;
+using Xunit;
+
+namespace Roslyn.VisualStudio.NewIntegrationTests.DevLoop;
+
+[IdeSettings(MinVersion = VisualStudioVersion.VS2022, RootSuffix = "TestHive", MaxAttempts = 1)]
+public class RoslynSelfBuildTests : AbstractIntegrationTest
+{
+
+    [IdeFact]
+    public async Task Test()
+    {
+        var solutionDir = @"D:\Sample\roslyn\roslyn.sln";
+
+    }
+}
