b56c6c5a321dec3a94f2cc0b5907e0ab23898f19
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-01 13:50:34 -0700

    Add test

--- src/Analyzers/CSharp/Tests/UseAutoProperty/UseAutoPropertyTests_Field.cs
+++ src/Analyzers/CSharp/Tests/UseAutoProperty/UseAutoPropertyTests_Field.cs
@@ -3,17 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Test.Utilities;
-using Microsoft.CodeAnalysis.CSharp.UseAutoProperty;
-using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics;
-using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.Test.Utilities;
-using Roslyn.Test.Utilities;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.UseAutoProperty;
 
@@ -124,6 +116,26 @@ public sealed partial class UseAutoPropertyTests
                         field = value;
                     }
                 }
+            }
+            """);
+    }
+
+    [Fact]
+    public async Task TestSimpleFieldInExpressionBody()
+    {
+        await TestInRegularAndScript1Async(
+            """
+            class Class
+            {
+                [|string s|];
+
+                string P => s.Trim();
+            }
+            """,
+            """
+            class Class
+            {
+                string P => field.Trim();
             }
             """);
     }
