868a1046768f8192b418970a64560fb7be153828
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-01 18:55:54 -0700

    Docs

--- src/Analyzers/Core/Analyzers/UseAutoProperty/AnalysisResult.cs
+++ src/Analyzers/Core/Analyzers/UseAutoProperty/AnalysisResult.cs
@@ -16,6 +16,15 @@ internal abstract partial class AbstractUseAutoPropertyAnalyzer<
     TExpression,
     TIdentifierName>
 {
+    /// <param name="Property">The property we will make into an auto-property.</param>
+    /// <param name="Field">The field we are removing.</param>
+    /// <param name="PropertyDeclaration"></param>
+    /// <param name="FieldDeclaration"></param>
+    /// <param name="VariableDeclarator"></param>
+    /// <param name="Notification"></param>
+    /// <param name="IsTrivialGetAccessor">If the get-accessor is of a trivial form like <c>get =>
+    /// fieldName;</c></param>
+    /// <param name="IsTrivialSetAccessor">Same as <paramref name="IsTrivialSetAccessor"/>.</param>
     internal sealed record AnalysisResult(
         IPropertySymbol Property,
         IFieldSymbol Field,
