dc71576115d11adb3ed40dd2f2572dfca223df3c
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-02 11:23:19 -0700

    Docs

--- src/Analyzers/Core/Analyzers/UseAutoProperty/AnalysisResult.cs
+++ src/Analyzers/Core/Analyzers/UseAutoProperty/AnalysisResult.cs
@@ -18,13 +18,16 @@ internal abstract partial class AbstractUseAutoPropertyAnalyzer<
 {
     /// <param name="Property">The property we will make into an auto-property.</param>
     /// <param name="Field">The field we are removing.</param>
-    /// <param name="PropertyDeclaration"></param>
-    /// <param name="FieldDeclaration"></param>
-    /// <param name="VariableDeclarator"></param>
-    /// <param name="Notification"></param>
-    /// <param name="IsTrivialGetAccessor">If the get-accessor is of a trivial form like <c>get =>
-    /// fieldName;</c></param>
-    /// <param name="IsTrivialSetAccessor">Same as <paramref name="IsTrivialSetAccessor"/>.</param>
+    /// <param name="PropertyDeclaration">The single declaration that <paramref name="Property"/> has.</param>
+    /// <param name="FieldDeclaration">The single containing declaration that <paramref name="Field"/> has.</param>
+    /// <param name="VariableDeclarator">The single containing declarator that <paramref name="Field"/> has.</param>
+    /// <param name="Notification">The option value/severity at this particular analysis location.</param>
+    /// <param name="IsTrivialGetAccessor">If the get-accessor is of a trivial form like <c>get => fieldName;</c>.  Such
+    /// an accessor is a simple 'read through to the field' accessor.  As such, reads of the field can be replaced with
+    /// calls to this accessor as it will have the same semantics.</param>
+    /// <param name="IsTrivialSetAccessor">Same as <paramref name="IsTrivialSetAccessor"/>. Such an accessor is a simple
+    /// 'write through to the field' accessor.  As such, writes of the field can be replaced with calls to this accessor
+    /// as it will have the same semantics.</param>
     internal sealed record AnalysisResult(
         IPropertySymbol Property,
         IFieldSymbol Field,
