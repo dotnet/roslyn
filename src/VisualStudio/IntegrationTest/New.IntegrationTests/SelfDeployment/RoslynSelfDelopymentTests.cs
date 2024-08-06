7a9e7ea1f4b876c876f362942345f084a5e57b25
Shen Chen <shech@microsoft.com>
2024-08-06 16:08:08 -0700

    Update yaml

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/SelfDeployment/RoslynSelfDelopymentTests.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/SelfDeployment/RoslynSelfDelopymentTests.cs
@@ -23,12 +23,11 @@ public class RoslynSelfBuildTests(ITestOutputHelper output) : AbstractIntegratio
         Environment.SetEnvironmentVariable("RoslynSelfBuildTest", "true");
         Environment.SetEnvironmentVariable("MSBUILDTERMINALLOGGER ", "auto");
         Environment.SetEnvironmentVariable("MSBuildDebugEngine", "1");
-        Environment.SetEnvironmentVariable("RoslynSelfBuildTestAsset", @"D:\Sample\roslyn\");
+        Environment.SetEnvironmentVariable("RoslynSelfBuildTestAsset", @"C:\Users\shech\Workspace\Sample\roslyn");
         var testAssetDirectory = Environment.GetEnvironmentVariable("RoslynSelfBuildTestAsset");
         Assert.NotNull(testAssetDirectory);
         var solutionDir = Path.Combine(testAssetDirectory, "roslyn.sln");
         Assert.True(File.Exists(solutionDir));
-
         await this.TestServices.SolutionExplorer.OpenSolutionAsync(solutionDir, HangMitigatingCancellationToken);
 
         await this.TestOperationAndReportIfFailedAsync(
