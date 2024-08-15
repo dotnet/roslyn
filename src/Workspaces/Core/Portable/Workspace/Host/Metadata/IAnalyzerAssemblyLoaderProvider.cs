df1fa069f746f427b05deda2ef657ffa0f369127
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-15 14:45:55 -0700

    Docs

--- src/Workspaces/Core/Portable/Workspace/Host/Metadata/IAnalyzerAssemblyLoaderProvider.cs
+++ src/Workspaces/Core/Portable/Workspace/Host/Metadata/IAnalyzerAssemblyLoaderProvider.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.CodeAnalysis.Diagnostics;
+
 #if NET
 using System.Runtime.Loader;
 #endif
@@ -11,8 +13,16 @@ namespace Microsoft.CodeAnalysis.Host;
 internal interface IAnalyzerAssemblyLoaderProvider : IWorkspaceService
 {
 #if NET
+    /// <summary>
+    /// In .Net core, gives back a shadow copying loader that will load all <see cref="AnalyzerReference"/> in the
+    /// requested <paramref name="loadContext"/> (or a default one if unspecified).  <paramref name="isolatedRoot"/> can
+    /// be used to ensure a dedicated directory for the shadow copying to happen in, to prevent any collisions
+    /// whatsoever.  For example, given two <see cref="AnalyzerReference"/>s with the same MVID, but loaded into
+    /// different <see cref="AssemblyLoadContext"/>s; different paths would be desired to prevent collisions.
+    /// </summary>
     IAnalyzerAssemblyLoader GetShadowCopyLoader(AssemblyLoadContext? loadContext = null, string isolatedRoot = "");
 #else
+
     IAnalyzerAssemblyLoader GetShadowCopyLoader();
 #endif
 }
