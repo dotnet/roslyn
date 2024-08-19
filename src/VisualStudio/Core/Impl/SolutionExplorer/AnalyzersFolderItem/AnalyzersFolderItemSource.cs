841fcbb80db5c9434ff1d434740af110156b1bb2
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-19 13:24:44 -0700

    cleanup

--- src/VisualStudio/Core/Impl/SolutionExplorer/AnalyzersFolderItem/AnalyzersFolderItemSource.cs
+++ src/VisualStudio/Core/Impl/SolutionExplorer/AnalyzersFolderItem/AnalyzersFolderItemSource.cs
@@ -4,46 +4,30 @@
 
 using System.Collections;
 using System.Collections.ObjectModel;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.VisualStudio.Shell;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplorer;
 
-internal sealed class AnalyzersFolderItemSource : IAttachedCollectionSource
+internal sealed class AnalyzersFolderItemSource(
+    IThreadingContext threadingContext,
+    Workspace workspace,
+    ProjectId projectId,
+    IVsHierarchyItem projectHierarchyItem,
+    IAnalyzersCommandHandler commandHandler)
+    : IAttachedCollectionSource
 {
-    private readonly IThreadingContext _threadingContext;
-    private readonly Workspace _workspace;
-    private readonly ProjectId _projectId;
-    private readonly IVsHierarchyItem _projectHierarchyItem;
-    private readonly IAnalyzersCommandHandler _commandHandler;
-    private readonly ObservableCollection<AnalyzersFolderItem> _folderItems;
-
-    public AnalyzersFolderItemSource(
-        IThreadingContext threadingContext,
-        Workspace workspace,
-        ProjectId projectId,
-        IVsHierarchyItem projectHierarchyItem,
-        IAnalyzersCommandHandler commandHandler)
-    {
-        _threadingContext = threadingContext;
-        _workspace = workspace;
-        _projectId = projectId;
-        _projectHierarchyItem = projectHierarchyItem;
-        _commandHandler = commandHandler;
-
-        _folderItems = [new AnalyzersFolderItem(
-            _threadingContext,
-            _workspace,
-            _projectId,
-            _projectHierarchyItem,
-            _commandHandler.AnalyzerFolderContextMenuController)];
-    }
+    private readonly ObservableCollection<AnalyzersFolderItem> _folderItems = [new AnalyzersFolderItem(
+        threadingContext,
+        workspace,
+        projectId,
+        projectHierarchyItem,
+        commandHandler.AnalyzerFolderContextMenuController)];
 
     public bool HasItems => true;
 
     public IEnumerable Items => _folderItems;
 
-    public object SourceItem => _projectHierarchyItem;
+    public object SourceItem => projectHierarchyItem;
 }
