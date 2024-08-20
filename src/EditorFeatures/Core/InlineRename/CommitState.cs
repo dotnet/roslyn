39ac7cdabdf94b9d5a924bcae8893ecfee9dc35d
Shen Chen <shech@microsoft.com>
2024-08-19 20:17:56 -0700

    Revert "Use State to cancel"
    This reverts commit cda7a87427fd5f12d8c1bdcd9b3a6a94c2a54a5d.

---
+++ src/EditorFeatures/Core/InlineRename/CommitState.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.CodeAnalysis.InlineRename;
+
+/// <summary>
+/// State represents different stages when commit starts in InlineRenameSession.
+/// </summary>
+internal enum CommitState
+{
+    NotStart,
+    WaitConflictResolution,
+    StartApplyChanges,
+    End,
+}
