3fff183ecb44cb29fcc3df4011e7862c6bea8f14
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-21 08:55:56 -0700

    revert

--- src/Compilers/Server/VBCSCompilerTests/TouchedFileLoggingTests.cs
+++ src/Compilers/Server/VBCSCompilerTests/TouchedFileLoggingTests.cs
@@ -46,11 +46,7 @@ End Class
         [ConditionalFact(typeof(DesktopOnly))]
         public void CSharpTrivialMetadataCaching()
         {
-            var loader = DefaultAnalyzerAssemblyLoader.CreateNonLockingLoader(
-#if NET
-                loadContext: null,
-#endif
-                Temp.CreateDirectory().Path);
+            var loader = DefaultAnalyzerAssemblyLoader.CreateNonLockingLoader(Temp.CreateDirectory().Path);
             var filelist = new List<string>();
 
             // Do the following compilation twice.
@@ -101,11 +97,7 @@ End Class
         [ConditionalFact(typeof(DesktopOnly))]
         public void VisualBasicTrivialMetadataCaching()
         {
-            var loader = DefaultAnalyzerAssemblyLoader.CreateNonLockingLoader(
-#if NET
-                loadContext: null,
-#endif
-                Temp.CreateDirectory().Path);
+            var loader = DefaultAnalyzerAssemblyLoader.CreateNonLockingLoader(Temp.CreateDirectory().Path);
             var filelist = new List<string>();
 
             // Do the following compilation twice.
