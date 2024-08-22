13abfd67544a1841e2bfe3b04ce0299a5052be6d
DoctorKrolic <mapmyp03@gmail.com>
2024-08-22 22:52:49 +0300

    Simplify

--- src/Features/Core/Portable/Snippets/SnippetProviders/AbstractInlineStatementSnippetProvider.cs
+++ src/Features/Core/Portable/Snippets/SnippetProviders/AbstractInlineStatementSnippetProvider.cs
@@ -83,19 +83,17 @@ internal abstract class AbstractInlineStatementSnippetProvider<TStatementSyntax>
             syntaxFacts.IsExpressionStatement(parentNode?.Parent))
         {
             var expression = syntaxFacts.GetExpressionOfMemberAccessExpression(parentNode)!;
-
-            var typeInfo = semanticModel.GetTypeInfo(expression, cancellationToken);
             var symbolInfo = semanticModel.GetSymbolInfo(expression, cancellationToken);
 
-            // If we get the same symbol from symbol info as expression's type
-            // then we are dotting from a type name itself, e.g. `string.$$`.
+            // Forbid a case when we are dotting of a type, e.g. `string.$$`.
             // Inline statement snippets are not valid in this context
-            if (SymbolEqualityComparer.Default.Equals(symbolInfo.Symbol, typeInfo.Type))
+            if (symbolInfo.Symbol is ITypeSymbol)
             {
                 expressionInfo = null;
                 return false;
             }
 
+            var typeInfo = semanticModel.GetTypeInfo(expression, cancellationToken);
             expressionInfo = new(expression, typeInfo);
             return true;
         }
