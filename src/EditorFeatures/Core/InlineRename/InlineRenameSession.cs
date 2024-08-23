6256a96dfa07e50f7bb99a66fb3738752ff673a4
Shen Chen <shech@microsoft.com>
2024-08-22 20:48:09 -0700

    Also forward commitSync to StartCommitAsync

--- src/EditorFeatures/Core/InlineRename/InlineRenameSession.cs
+++ src/EditorFeatures/Core/InlineRename/InlineRenameSession.cs
@@ -747,7 +747,7 @@ internal partial class InlineRenameSession : IInlineRenameSession, IFeatureContr
         // which at least will allow the user to cancel the rename if they want.
         //
         // In the future we should remove this entrypoint and have all callers use CommitAsync instead.
-        return _threadingContext.JoinableTaskFactory.Run(() => CommitWorkerAsync(previewChanges, canUseBackgroundWorkIndicator: false));
+        return _threadingContext.JoinableTaskFactory.Run(() => StartCommitAsync(previewChanges, canUseBackgroundWorkIndicator: false));
     }
 
     public async Task CommitAsync(bool previewChanges)
