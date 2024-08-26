67ea275a8df470d31f6c68e2563dcb4231128fab
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-26 14:33:06 -0700

    Add remote impl

---
+++ src/Workspaces/Remote/ServiceHub/Services/RelatedDocuments/RemoteRelatedDocumentsService.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.RelatedDocuments;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+
+namespace Microsoft.CodeAnalysis.Remote;
+
+internal sealed class RemoteRelatedDocumentsService(
+    in BrokeredServiceBase.ServiceConstructionArguments arguments,
+    RemoteCallback<IRemoteRelatedDocumentsService.ICallback> callback)
+    : BrokeredServiceBase(arguments), IRemoteRelatedDocumentsService
+{
+    internal sealed class Factory : FactoryBase<IRemoteRelatedDocumentsService, IRemoteRelatedDocumentsService.ICallback>
+    {
+        protected override IRemoteRelatedDocumentsService CreateService(
+            in ServiceConstructionArguments arguments, RemoteCallback<IRemoteRelatedDocumentsService.ICallback> callback)
+            => new RemoteRelatedDocumentsService(arguments, callback);
+    }
+
+    private readonly RemoteCallback<IRemoteRelatedDocumentsService.ICallback> _callback = callback;
+
+    public ValueTask GetRelatedDocumentIdsAsync(
+        Checksum solutionChecksum,
+        DocumentId documentId,
+        int position,
+        RemoteServiceCallbackId callbackId,
+        CancellationToken cancellationToken)
+    {
+        return RunServiceAsync(solutionChecksum, async solution =>
+        {
+            var document = solution.GetRequiredDocument(documentId);
+
+            var service = document.GetRequiredLanguageService<IRelatedDocumentsService>();
+            await foreach (var relatedDocument in service.GetRelatedDocumentIdsAsync(document, position, cancellationToken))
+            {
+                await _callback.InvokeAsync((callback, cancellationToken) => callback.ReportRelatedDocumentAsync(
+                    callbackId, relatedDocument, cancellationToken), cancellationToken).ConfigureAwait(false);
+            }
+        }, cancellationToken);
+    }
+}
