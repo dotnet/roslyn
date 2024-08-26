d0db45a9ea2330b01702388f1ec3679f8487ae59
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-26 14:53:38 -0700

    wrapping

---
+++ src/Features/Core/Portable/RelatedDocuments/AbstractRelatedDocumentsService.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Remote;
+
+namespace Microsoft.CodeAnalysis.RelatedDocuments;
+
+internal abstract class AbstractRelatedDocumentsService : IRelatedDocumentsService
+{
+    public async ValueTask GetRelatedDocumentIdsAsync(
+        Document document, int position, Func<DocumentId, CancellationToken, ValueTask> callbackAsync, CancellationToken cancellationToken)
+    {
+        var project = document.Project;
+        var client = await RemoteHostClient.TryGetClientAsync(project, cancellationToken).ConfigureAwait(false);
+        if (client != null)
+        {
+            var remoteCallback = new RemoteRelatedDocumentsServiceCallback(callbackAsync, cancellationToken);
+
+            var result = await client.TryInvokeAsync<IRemoteRelatedDocumentsService>(
+                // We don't need to sync the entire solution (only the project) to ask for the related files for a
+                // particular document.
+                document.Project,
+                (service, solutionChecksum, callbackId, cancellationToken) => service.GetRelatedDocumentIdsAsync(
+                    solutionChecksum, document.Id, position, callbackId, cancellationToken),
+                remoteCallback,
+                cancellationToken).ConfigureAwait(false);
+        }
+        else
+        {
+            await GetRelatedDocumentIdsInCurrentProcessAsync(
+                document, position, callbackAsync, cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    protected abstract ValueTask GetRelatedDocumentIdsInCurrentProcessAsync(
+        Document document, int position, Func<DocumentId, CancellationToken, ValueTask> callbackAsync, CancellationToken cancellationToken);
+}
--- src/Features/Core/Portable/RelatedDocuments/IRelatedDocumentsService.cs
+++ src/Features/Core/Portable/RelatedDocuments/IRelatedDocumentsService.cs
@@ -6,7 +6,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
-using Microsoft.CodeAnalysis.Remote;
 
 namespace Microsoft.CodeAnalysis.RelatedDocuments;
 
@@ -19,34 +18,4 @@ internal interface IRelatedDocumentsService : ILanguageService
     /// those symbols are defined in.
     /// </summary>
     ValueTask GetRelatedDocumentIdsAsync(Document document, int position, Func<DocumentId, CancellationToken, ValueTask> callbackAsync, CancellationToken cancellationToken);
-}
-
-internal abstract class AbstractRelatedDocumentsService : IRelatedDocumentsService
-{
-    public async ValueTask GetRelatedDocumentIdsAsync(
-        Document document, int position, Func<DocumentId, CancellationToken, ValueTask> callbackAsync, CancellationToken cancellationToken)
-    {
-        var project = document.Project;
-        var client = await RemoteHostClient.TryGetClientAsync(project, cancellationToken).ConfigureAwait(false);
-        if (client != null)
-        {
-            var remoteCallback = new RemoteRelatedDocumentsServiceCallback(callbackAsync, cancellationToken);
-
-            var result = await client.TryInvokeAsync<IRemoteRelatedDocumentsService>(
-                // We don't need to sync the entire solution (only the project) to ask for the related files for a
-                // particular document.
-                document.Project,
-                (service, solutionChecksum, callbackId, cancellationToken) => service.GetRelatedDocumentIdsAsync(solutionChecksum, document.Id, position, callbackId, cancellationToken),
-                remoteCallback,
-                cancellationToken).ConfigureAwait(false);
-        }
-        else
-        {
-            await GetRelatedDocumentIdsInCurrentProcessAsync(
-                document, position, callbackAsync, cancellationToken).ConfigureAwait(false);
-        }
-    }
-
-    protected abstract ValueTask GetRelatedDocumentIdsInCurrentProcessAsync(
-        Document document, int position, Func<DocumentId, CancellationToken, ValueTask> callbackAsync, CancellationToken cancellationToken);
 }
