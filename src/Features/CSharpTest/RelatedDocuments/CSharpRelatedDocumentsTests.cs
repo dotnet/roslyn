94c48399d90204d0165bd087767fa4be67c26767
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-27 10:32:25 -0700

    Add tests

--- src/Features/CSharpTest/RelatedDocuments/CSharpRelatedDocumentsTests.cs
+++ src/Features/CSharpTest/RelatedDocuments/CSharpRelatedDocumentsTests.cs
@@ -12,7 +12,7 @@ namespace Microsoft.CodeAnalysis.CSharp.UnitTests.RelatedDocuments;
 public sealed class CSharpRelatedDocumentsTests : AbstractRelatedDocumentsTests
 {
     [Theory, CombinatorialData]
-    public async Task TestEmptyDocument(TestHost testHost)
+    public async Task EmptyDocument(TestHost testHost)
         => await TestAsync("""
             <Workspace>
                 <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
@@ -22,7 +22,7 @@ public sealed class CSharpRelatedDocumentsTests : AbstractRelatedDocumentsTests
             """, testHost);
 
     [Theory, CombinatorialData]
-    public async Task TestReferenceToSameDocDocument(TestHost testHost)
+    public async Task ReferenceToSameDocument(TestHost testHost)
         => await TestAsync("""
             <Workspace>
                 <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
@@ -30,6 +30,179 @@ public sealed class CSharpRelatedDocumentsTests : AbstractRelatedDocumentsTests
                     class C
                     {
                         C c;
+                    }
+                    </Document>
+                </Project>
+            </Workspace>
+            """, testHost);
+
+    [Theory, CombinatorialData]
+    public async Task MultipleReferencesToSameDocument(TestHost testHost)
+        => await TestAsync("""
+            <Workspace>
+                <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
+                    <Document>[||]$$
+                    class C
+                    {
+                        C c;
+                        C c;
+                    }
+                    </Document>
+                </Project>
+            </Workspace>
+            """, testHost);
+
+    [Theory, CombinatorialData]
+    public async Task ReferenceToDifferentDocument(TestHost testHost)
+        => await TestAsync("""
+            <Workspace>
+                <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
+                    <Document>$$
+                    class C
+                    {
+                        D d;
+                    }
+                    </Document>
+                    <Document>[||]
+                    class D
+                    {
+                    }
+                    </Document>
+                </Project>
+            </Workspace>
+            """, testHost);
+
+    [Theory, CombinatorialData]
+    public async Task MultipleReferencesToDifferentDocument(TestHost testHost)
+        => await TestAsync("""
+            <Workspace>
+                <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
+                    <Document>$$
+                    class C
+                    {
+                        D d;
+                        D d;
+                    }
+                    </Document>
+                    <Document>[||]
+                    class D
+                    {
+                    }
+                    </Document>
+                </Project>
+            </Workspace>
+            """, testHost);
+
+    [Theory, CombinatorialData]
+    public async Task ReferenceWithinGeneric(TestHost testHost)
+        => await TestAsync("""
+            <Workspace>
+                <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
+                    <Document><![CDATA[$$
+                    class C
+                    {
+                        List<D> d;
+                    }
+                    ]]></Document>
+                    <Document>[||]
+                    class D
+                    {
+                    }
+                    </Document>
+                </Project>
+            </Workspace>
+            """, testHost);
+
+    [Theory, CombinatorialData]
+    public async Task QualifiedReferenceWithinGeneric(TestHost testHost)
+        => await TestAsync("""
+            <Workspace>
+                <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
+                    <Document>$$
+                    class C
+                    {
+                        N.D d;
+                    }
+                    </Document>
+                    <Document>[||]
+                    namespace N;
+
+                    class D
+                    {
+                    }
+                    </Document>
+                </Project>
+            </Workspace>
+            """, testHost);
+
+    [Theory, CombinatorialData]
+    public async Task QualifiedReferenceThroughStaticWithinGeneric(TestHost testHost)
+        => await TestAsync("""
+            <Workspace>
+                <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
+                    <Document>$$
+                    class C
+                    {
+                        void M()
+                        {
+                            Console.WriteLine(N.D.I);
+                        }
+                    }
+                    </Document>
+                    <Document>[||]
+                    namespace N;
+
+                    class D
+                    {
+                        public static int I;
+                    }
+                    </Document>
+                </Project>
+            </Workspace>
+            """, testHost);
+
+    [Theory, CombinatorialData]
+    public async Task ReferenceToPartialType(TestHost testHost)
+        => await TestAsync("""
+            <Workspace>
+                <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
+                    <Document>$$
+                    class C
+                    {
+                        D d;
+                    }
+                    </Document>
+                    <Document>[||]
+                    partial class D
+                    {
+                    }
+                    </Document>
+                    <Document>[||]
+                    partial class D
+                    {
+                    }
+                    </Document>
+                </Project>
+            </Workspace>
+            """, testHost);
+
+    [Theory, CombinatorialData]
+    public async Task NoReferenceToNamespace(TestHost testHost)
+        => await TestAsync("""
+            <Workspace>
+                <Project Language="C#" AssemblyName="Assembly1" CommonReferences="true">
+                    <Document>$$
+                    using N;
+
+                    class C
+                    {
+                    }
+                    </Document>
+                    <Document>
+                    namespace N;
+
+                    partial class D
+                    {
                     }
                     </Document>
                 </Project>
