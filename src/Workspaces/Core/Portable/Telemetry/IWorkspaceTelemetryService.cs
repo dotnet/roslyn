2cadd1c4b58e42f3e19b5af303e67eea94a86310
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-29 11:36:56 -0700

    Simplify

--- src/Workspaces/Core/Portable/Telemetry/IWorkspaceTelemetryService.cs
+++ src/Workspaces/Core/Portable/Telemetry/IWorkspaceTelemetryService.cs
@@ -3,10 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
-using Microsoft.CodeAnalysis.Remote;
 
 namespace Microsoft.CodeAnalysis.Telemetry;
 
