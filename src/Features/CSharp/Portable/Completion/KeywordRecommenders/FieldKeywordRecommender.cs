fbb9634935b17bf55e7390601fb3ba2b5807aaff
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-30 10:25:09 -0700

    Simplify

--- src/Features/CSharp/Portable/Completion/KeywordRecommenders/FieldKeywordRecommender.cs
+++ src/Features/CSharp/Portable/Completion/KeywordRecommenders/FieldKeywordRecommender.cs
@@ -2,12 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using Microsoft.CodeAnalysis.CSharp.Extensions.ContextQuery;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.CSharp.Completion.KeywordRecommenders;
 
