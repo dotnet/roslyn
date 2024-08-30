a61e789588fa5c25a494efcdc8a867a66b74348a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-08-30 10:14:17 -0700

    Add 'field' keyword recommender
    Add test
    
    Add test

--- src/Features/CSharp/Portable/Completion/KeywordRecommenders/FieldKeywordRecommender.cs
+++ src/Features/CSharp/Portable/Completion/KeywordRecommenders/FieldKeywordRecommender.cs
@@ -2,13 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using Microsoft.CodeAnalysis.CSharp.Extensions.ContextQuery;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.CSharp.Completion.KeywordRecommenders;
 
-internal class FieldKeywordRecommender : AbstractSyntacticSingleKeywordRecommender
+internal sealed class FieldKeywordRecommender()
+    : AbstractSyntacticSingleKeywordRecommender(SyntaxKind.FieldKeyword)
 {
     // interfaces don't have members that you can put a [field:] attribute on
     private static readonly ISet<SyntaxKind> s_validTypeDeclarations = new HashSet<SyntaxKind>(SyntaxFacts.EqualityComparer)
@@ -20,11 +24,34 @@ internal class FieldKeywordRecommender : AbstractSyntacticSingleKeywordRecommend
         SyntaxKind.EnumDeclaration,
     };
 
-    public FieldKeywordRecommender()
-        : base(SyntaxKind.FieldKeyword)
+    protected override bool IsValidContext(int position, CSharpSyntaxContext context, CancellationToken cancellationToken)
     {
+        if (context.IsMemberAttributeContext(s_validTypeDeclarations, cancellationToken))
+            return true;
+
+        if (context.IsAnyExpressionContext || context.IsStatementContext)
+        {
+            var token = context.TargetToken;
+            if (IsInPropertyAccessor(token.Parent))
+                return true;
+        }
+
+        return false;
     }
 
-    protected override bool IsValidContext(int position, CSharpSyntaxContext context, CancellationToken cancellationToken)
-        => context.IsMemberAttributeContext(s_validTypeDeclarations, cancellationToken);
+    private static bool IsInPropertyAccessor(SyntaxNode? node)
+    {
+        while (node != null)
+        {
+            if (node is ArrowExpressionClauseSyntax { Parent: PropertyDeclarationSyntax })
+                return true;
+
+            if (node is AccessorDeclarationSyntax { Parent: AccessorListSyntax { Parent: PropertyDeclarationSyntax } })
+                return true;
+
+            node = node.Parent;
+        }
+
+        return false;
+    }
 }
