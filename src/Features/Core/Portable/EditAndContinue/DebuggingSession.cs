61b83c852f2cf968e80671c0c3c462a0f3728aab
tmat <tomas.matousek@microsoft.com>
2024-09-09 12:43:45 -0700

    Session logging

--- src/Features/Core/Portable/EditAndContinue/DebuggingSession.cs
+++ src/Features/Core/Portable/EditAndContinue/DebuggingSession.cs
@@ -111,6 +111,8 @@ internal sealed class DebuggingSession : IDisposable
         IEnumerable<KeyValuePair<DocumentId, CommittedSolution.DocumentState>> initialDocumentStates,
         bool reportDiagnostics)
     {
+        EditAndContinueService.Log.Write($"Debugging session started: #{id}");
+
         _compilationOutputsProvider = compilationOutputsProvider;
         SourceTextProvider = sourceTextProvider;
         _reportTelemetry = ReportTelemetry;
@@ -198,6 +200,8 @@ internal sealed class DebuggingSession : IDisposable
         _reportTelemetry(telemetryData);
 
         Dispose();
+
+        EditAndContinueService.Log.Write($"Debugging session ended: #{Id}");
     }
 
     public void BreakStateOrCapabilitiesChanged(bool? inBreakState)
@@ -205,6 +209,8 @@ internal sealed class DebuggingSession : IDisposable
 
     internal void RestartEditSession(ImmutableDictionary<ManagedMethodId, ImmutableArray<NonRemappableRegion>>? nonRemappableRegions, bool? inBreakState)
     {
+        EditAndContinueService.Log.Write($"Edit session restarted (break state: {inBreakState?.ToString() ?? "null"})");
+
         ThrowIfDisposed();
 
         EndEditSession();
