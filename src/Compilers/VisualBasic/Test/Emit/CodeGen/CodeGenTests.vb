8c3f99f94843bb55406b4792e4d040a60328973b
Jared Parsons <jared@paranoidcoding.org>
2024-09-09 22:10:36 -0700

    more

--- src/Compilers/VisualBasic/Test/Emit/CodeGen/CodeGenTests.vb
+++ src/Compilers/VisualBasic/Test/Emit/CodeGen/CodeGenTests.vb
@@ -5,7 +5,6 @@
 Imports System.Collections.Immutable
 Imports System.Reflection
 Imports Microsoft.CodeAnalysis
-Imports Microsoft.CodeAnalysis.Test.Resources.Proprietary
 Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Microsoft.CodeAnalysis.VisualBasic
 Imports Microsoft.CodeAnalysis.VisualBasic.Symbols
