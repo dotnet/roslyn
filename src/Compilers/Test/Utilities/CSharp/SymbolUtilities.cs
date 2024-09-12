ffa25f9b301ad72676d44b04bd08f8cb53820c7e
tmat <tomas.matousek@microsoft.com>
2024-09-12 14:18:37 -0700

    Remove unused using

--- src/Compilers/Test/Utilities/CSharp/SymbolUtilities.cs
+++ src/Compilers/Test/Utilities/CSharp/SymbolUtilities.cs
@@ -9,7 +9,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.Test.Utilities;
-using Roslyn.Utilities;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.CSharp.UnitTests
