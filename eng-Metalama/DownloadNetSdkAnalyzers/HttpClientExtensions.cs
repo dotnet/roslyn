250ab440cb16d19f21fe373412b0e45e37827083
Petr Onderka <petro@postsharp.net>
2024-10-09 18:00:03 +0200

    Attempt to make downloading SDK ZIP more reliable

---
+++ eng-Metalama/DownloadNetSdkAnalyzers/HttpClientExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace DownloadNetSdkAnalyzers;
+
+public static class HttpClientExtensions
+{
+    public static async Task<Stream> GetSeekableStreamAsync(this HttpClient client, string requestUri)
+    {
+        var bytes = await client.GetByteArrayAsync(requestUri).ConfigureAwait(false);
+
+        return new MemoryStream(bytes, writable: false);
+    }
+}
