3e1739844dccff9e5b4d295865ae11c2050cca45
Charles Stoner <10732005+cston@users.noreply.github.com>
2024-10-10 07:06:04 -0700

    Skip {CSharp,Basic}Immediate tests
    This reverts commit 8cfc30ace021fa54dce07d973d117595fbf1bf77.

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpImmediate.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpImmediate.cs
@@ -29,7 +29,7 @@ public class CSharpImmediate : AbstractEditorTest
         await TestServices.SolutionExplorer.AddProjectAsync("TestProj", WellKnownProjectTemplates.ConsoleApplication, LanguageNames.CSharp, HangMitigatingCancellationToken);
     }
 
-    [IdeFact]
+    [IdeFact(Skip = "https://github.com/dotnet/roslyn/issues/75456")]
     public async Task DumpLocalVariableValue()
     {
         await TestServices.Editor.SetTextAsync(@"
