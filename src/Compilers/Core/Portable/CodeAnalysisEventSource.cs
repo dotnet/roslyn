08a2ec765abf311d17d5876dac045b1504a7af21
Rikki Gibson <rigibson@microsoft.com>
2024-10-17 11:40:45 -0700

    use error level for exception case

--- src/Compilers/Core/Portable/CodeAnalysisEventSource.cs
+++ src/Compilers/Core/Portable/CodeAnalysisEventSource.cs
@@ -109,7 +109,7 @@ namespace Microsoft.CodeAnalysis
         [Event(10, Message = "ALC for directory '{0}' disposed", Keywords = Keywords.Performance, Level = EventLevel.Informational, Opcode = EventOpcode.Stop, Task = Tasks.AnalyzerAssemblyLoader)]
         internal void DisposeAssemblyLoadContext(string directory) => WriteEvent(10, directory);
 
-        [Event(11, Message = "ALC for directory '{0}' disposal failed with exception '{1}'", Keywords = Keywords.Performance, Level = EventLevel.Informational, Opcode = EventOpcode.Stop, Task = Tasks.AnalyzerAssemblyLoader)]
+        [Event(11, Message = "ALC for directory '{0}' disposal failed with exception '{1}'", Keywords = Keywords.Performance, Level = EventLevel.Error, Opcode = EventOpcode.Stop, Task = Tasks.AnalyzerAssemblyLoader)]
         internal void DisposeAssemblyLoadContextException(string directory, string errorMessage) => WriteEvent(11, directory, errorMessage);
 
         private static unsafe EventData GetEventDataForString(string value, char* ptr)
