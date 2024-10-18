f662b88b4221608e5a2f198f9a66a0edfc929e39
Shen Chen <shech@microsoft.com>
2024-10-18 16:55:55 -0700

    Also don't move caret for openline above & down

--- src/EditorFeatures/Core/InlineRename/CommandHandlers/AbstractRenameCommandHandler_OpenLineAboveHandler.cs
+++ src/EditorFeatures/Core/InlineRename/CommandHandlers/AbstractRenameCommandHandler_OpenLineAboveHandler.cs
@@ -17,7 +17,7 @@ internal abstract partial class AbstractRenameCommandHandler : IChainedCommandHa
     {
         HandlePossibleTypingCommand(args, nextHandler, context.OperationContext, (activeSession, operationContext, span) =>
         {
-            CommitOrCancel(args, operationContext);
+            CommitOrCancel(args, operationContext, placeCaretAtTheEndOfIdentifier: false);
             nextHandler();
         });
     }
