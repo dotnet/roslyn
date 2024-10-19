8a8f27c4eb39269542c3a474f6877cca1ec36626
Rekkonnect <8298332+Rekkonnect@users.noreply.github.com>
2024-10-19 18:06:21 +0300

    Add explanations for the ruled out types

--- src/Features/Core/Portable/Completion/Providers/AbstractObjectInitializerCompletionProvider.cs
+++ src/Features/Core/Portable/Completion/Providers/AbstractObjectInitializerCompletionProvider.cs
@@ -131,16 +131,26 @@ internal abstract class AbstractObjectInitializerCompletionProvider : LSPComplet
 
     private static bool MemberTypeCanSupportObjectInitializer(ITypeSymbol type)
     {
+        // NOTE: While in C# it is legal to write 'Member = {}' on a member of any of
+        // the ruled out types below, it has no effects and is thus a needless recommendation
+
+        // We avoid some types that are common and easy to rule out
         switch (type.SpecialType)
         {
-            // While it is perfectly legal to assign instances of all these types to `{}`,
-            // it has no effects and is thus a needless recommendation
             case SpecialType.System_Enum:
             case SpecialType.System_String:
-            case SpecialType.System_Array:
             case SpecialType.System_Object:
             case SpecialType.System_Delegate:
             case SpecialType.System_MulticastDelegate:
+
+            // We cannot use collection initializers in Array members,
+            // but for members of an array type with a typed rank we can
+            // For example, assuming Array2D is int[,]:
+            // Array2D = { [0, 0] = value, [0, 1] = value1 },
+            case SpecialType.System_Array:
+
+            // We cannot add to an enumerable or enumerator
+            // so we cannot use a collection initializer
             case SpecialType.System_Collections_IEnumerable:
             case SpecialType.System_Collections_IEnumerator:
                 return false;
@@ -157,6 +167,9 @@ internal abstract class AbstractObjectInitializerCompletionProvider : LSPComplet
             }
         }
 
+        // - Delegate types have no settable members, which is the case for Delegate and MulticastDelegate too
+        // - Non-settable struct members cannot be used in object initializers
+        // - Pointers and function pointers do not have accessible members
         return !type.IsDelegateType()
             && !type.IsStructType()
             && !type.IsFunctionPointerType()
