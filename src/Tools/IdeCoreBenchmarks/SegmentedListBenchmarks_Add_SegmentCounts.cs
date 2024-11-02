5a98efbdc6fe5f7cdd5abe3dc5b90bc4f93f4d6c
Todd Grunke <toddgrun@microsoft.com>
2024-11-02 06:16:13 -0700

    Missed header

--- src/Tools/IdeCoreBenchmarks/SegmentedListBenchmarks_Add_SegmentCounts.cs
+++ src/Tools/IdeCoreBenchmarks/SegmentedListBenchmarks_Add_SegmentCounts.cs
@@ -1,4 +1,8 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
 using Microsoft.CodeAnalysis.Collections;
 using Microsoft.CodeAnalysis.Collections.Internal;
 
