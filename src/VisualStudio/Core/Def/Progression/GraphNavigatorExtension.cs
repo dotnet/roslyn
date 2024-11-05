c5018e827b85a6d1067cfb70dfbc0c931d165ab6
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-11-05 10:31:50 -0800

    Support navigating to metadata in progression results

--- src/VisualStudio/Core/Def/Progression/GraphNavigatorExtension.cs
+++ src/VisualStudio/Core/Def/Progression/GraphNavigatorExtension.cs
@@ -16,6 +16,7 @@ using Microsoft.CodeAnalysis.Navigation;
 using Microsoft.VisualStudio.GraphModel;
 using Microsoft.VisualStudio.GraphModel.CodeSchema;
 using Microsoft.VisualStudio.GraphModel.Schemas;
+using Microsoft.VisualStudio.Shell;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.Progression;
 
@@ -32,74 +33,67 @@ internal sealed class GraphNavigatorExtension(
 
     public void NavigateTo(GraphObject graphObject)
     {
+        if (graphObject is not GraphNode graphNode)
+            return;
 
-        if (graphObject is GraphNode graphNode)
-        {
-            var sourceLocation = graphNode.GetValue<SourceLocation>(CodeNodeProperties.SourceLocation);
-            if (sourceLocation.FileName == null)
-            {
-                return;
-            }
-
-            var projectId = graphNode.GetValue<ProjectId>(RoslynGraphProperties.ContextProjectId);
-            var symbolId = graphNode.GetValue<SymbolKey?>(RoslynGraphProperties.SymbolId);
-
-            if (projectId != null)
-            {
-                var solution = _workspace.CurrentSolution;
-                var project = solution.GetProject(projectId);
-                if (project == null)
-                    return;
-
-                var document = project.Documents.FirstOrDefault(
-                    d => string.Equals(
-                        d.FilePath,
-                        sourceLocation.FileName.LocalPath,
-                        StringComparison.OrdinalIgnoreCase));
-
-                if (document == null)
-                    return;
-
-                _threadingContext.JoinableTaskFactory.Run(() =>
-                    NavigateToAsync(sourceLocation, symbolId, project, document, CancellationToken.None));
-            }
-        }
+        _threadingContext.JoinableTaskFactory.Run(() => NavigateToAsync(graphNode, CancellationToken.None));
     }
 
-    private async Task NavigateToAsync(
-        SourceLocation sourceLocation, SymbolKey? symbolId, Project project, Document document, CancellationToken cancellationToken)
+    private async Task NavigateToAsync(GraphNode graphNode, CancellationToken cancellationToken)
     {
-        // Notify of navigation so third parties can intercept the navigation
-        if (symbolId != null)
+        var projectId = graphNode.GetValue<ProjectId>(RoslynGraphProperties.ContextProjectId);
+        var symbolId = graphNode.GetValue<SymbolKey?>(RoslynGraphProperties.SymbolId);
+
+        if (projectId is null)
+            return;
+
+        var solution = _workspace.CurrentSolution;
+        var project = solution.GetProject(projectId);
+        if (project is null)
+            return;
+
+        // Go through the mainline symbol id path if we have it.  That way we notify third parties, and we can navigate
+        // to metadata.
+        if (symbolId is not null)
         {
-            var symbol = symbolId.Value.Resolve(await project.GetCompilationAsync(cancellationToken).ConfigureAwait(false), cancellationToken: cancellationToken).Symbol;
+            var symbol = symbolId.Value.Resolve(await project.GetCompilationAsync(cancellationToken).ConfigureAwait(false), cancellationToken: cancellationToken).GetAnySymbol();
+            if (symbol is null)
+                return;
+
             await GoToDefinitionHelpers.TryNavigateToLocationAsync(
                 symbol, project.Solution, _threadingContext, _streamingPresenter.Value, cancellationToken).ConfigureAwait(false);
+        }
+
+        // If we didn't have a symbol id, attempt to navigate to the source location directly if the node includes one.
+        var sourceLocation = graphNode.GetValue<SourceLocation>(CodeNodeProperties.SourceLocation);
+        if (sourceLocation.FileName is null || !sourceLocation.IsValid)
             return;
-        }
 
-        if (sourceLocation.IsValid)
-        {
-            // We must find the right document in this project. This may not be the
-            // ContextDocumentId if you have a partial member that is shown under one
-            // document, but only exists in the other
+        var document = project.Documents.FirstOrDefault(
+            d => string.Equals(
+                d.FilePath,
+                sourceLocation.FileName.LocalPath,
+                StringComparison.OrdinalIgnoreCase));
 
-            if (document != null)
-            {
-                var editorWorkspace = document.Project.Solution.Workspace;
-                var navigationService = editorWorkspace.Services.GetService<IDocumentNavigationService>();
+        if (document == null)
+            return;
 
-                // TODO: Get the platform to use and pass us an operation context, or create one ourselves.
-                await navigationService.TryNavigateToLineAndOffsetAsync(
-                    _threadingContext,
-                    editorWorkspace,
-                    document.Id,
-                    sourceLocation.StartPosition.Line,
-                    sourceLocation.StartPosition.Character,
-                    NavigationOptions.Default,
-                    cancellationToken).ConfigureAwait(false);
-            }
-        }
+        // We must find the right document in this project. This may not be the
+        // ContextDocumentId if you have a partial member that is shown under one
+        // document, but only exists in the other
+
+        var editorWorkspace = document.Project.Solution.Workspace;
+        var navigationService = editorWorkspace.Services.GetService<IDocumentNavigationService>();
+
+        // TODO: Get the platform to use and pass us an operation context, or create one ourselves.
+        await navigationService.TryNavigateToLineAndOffsetAsync(
+            _threadingContext,
+            editorWorkspace,
+            document.Id,
+            sourceLocation.StartPosition.Line,
+            sourceLocation.StartPosition.Character,
+            NavigationOptions.Default,
+            cancellationToken).ConfigureAwait(false);
     }
 
     public int GetRank(GraphObject graphObject)
