0a18a7f831353e2ac0a6ccc764849adf815f38ee
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-11-07 13:26:05 -0800

    Fix

--- src/EditorFeatures/Test2/IntelliSense/VisualBasicCompletionCommandHandlerTests.vb
+++ src/EditorFeatures/Test2/IntelliSense/VisualBasicCompletionCommandHandlerTests.vb
@@ -1,4 +1,4 @@
-' Licensed to the .NET Foundation under one or more agreements.
+﻿' Licensed to the .NET Foundation under one or more agreements.
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
@@ -3174,7 +3174,7 @@ Class C
 
         End Function
 
-        <ConditionalWpfTheory(GetType(IsEnglishLocal)), WorkItem("https://github.com/dotnet/roslyn/issues/29938")>
+        <ConditionalWpfFact(GetType(IsEnglishLocal)), WorkItem("https://github.com/dotnet/roslyn/issues/29938")>
         Public Async Function TestMatchWithTurkishIWorkaround10() As Task
             Using New CultureContext(New Globalization.CultureInfo("tr-TR", useUserOverride:=False))
                 Using state = TestStateFactory.CreateVisualBasicTestState(
