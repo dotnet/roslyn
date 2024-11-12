8280879ab3ffba73914f20570adb3403480a6ec4
Jan Jones <janjones@microsoft.com>
2024-11-12 10:29:45 +0100

    Improve comment wording
    Co-authored-by: Jared Parsons <jared@paranoidcoding.org>

--- src/Compilers/Shared/BuildServerConnection.cs
+++ src/Compilers/Shared/BuildServerConnection.cs
@@ -564,7 +564,7 @@ namespace Microsoft.CodeAnalysis.CommandLine
             // of this method.
             clientDirectory = clientDirectory.TrimEnd(Path.DirectorySeparatorChar);
 
-            // Similarly, we don't want multiple servers if the path differs in casing.
+            // Similarly, we don't want multiple servers if the provided launch path differs in casing.
             clientDirectory = clientDirectory.ToLowerInvariant();
 
             var pipeNameInput = $"{userName}.{isAdmin}.{clientDirectory}";
