2e4bcdcc160d08a081c4d3a160dd45323b5684cc
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-11-29 23:04:33 -0800

    Update suggestion mode provider to support params collections

--- src/Features/CSharp/Portable/Completion/CompletionProviders/CSharpSuggestionModeCompletionProvider.cs
+++ src/Features/CSharp/Portable/Completion/CompletionProviders/CSharpSuggestionModeCompletionProvider.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Composition;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Completion;
@@ -19,17 +20,12 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.CSharp.Completion.Providers;
 
-[ExportCompletionProvider(nameof(CSharpSuggestionModeCompletionProvider), LanguageNames.CSharp)]
+[ExportCompletionProvider(nameof(CSharpSuggestionModeCompletionProvider), LanguageNames.CSharp), Shared]
 [ExtensionOrder(After = nameof(ObjectAndWithInitializerCompletionProvider))]
-[Shared]
-internal class CSharpSuggestionModeCompletionProvider : AbstractSuggestionModeCompletionProvider
+[method: ImportingConstructor]
+[method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+internal class CSharpSuggestionModeCompletionProvider() : AbstractSuggestionModeCompletionProvider
 {
-    [ImportingConstructor]
-    [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-    public CSharpSuggestionModeCompletionProvider()
-    {
-    }
-
     internal override string Language => LanguageNames.CSharp;
 
     protected override async Task<CompletionItem?> GetSuggestionModeItemAsync(
@@ -190,9 +186,20 @@ internal class CSharpSuggestionModeCompletionProvider : AbstractSuggestionModeCo
     private static ITypeSymbol? GetDelegateType(TypeInferenceInfo typeInferenceInfo, Compilation compilation)
     {
         var typeSymbol = typeInferenceInfo.InferredType;
-        if (typeInferenceInfo.IsParams && typeInferenceInfo.InferredType.IsArrayType())
+        if (typeInferenceInfo.IsParams)
         {
-            typeSymbol = ((IArrayTypeSymbol)typeInferenceInfo.InferredType).ElementType;
+            if (typeInferenceInfo.InferredType.IsSpanOrReadOnlySpan())
+            {
+                typeSymbol = typeInferenceInfo.InferredType.GetTypeArguments().Single();
+            }
+            else
+            {
+                var ienumerableType = typeInferenceInfo.InferredType
+                    .GetAllInterfacesIncludingThis()
+                    .FirstOrDefault(i => i.OriginalDefinition.SpecialType == SpecialType.System_Collections_Generic_IEnumerable_T);
+                if (ienumerableType != null)
+                    typeSymbol = ienumerableType.TypeArguments.Single();
+            }
         }
 
         return typeSymbol.GetDelegateType(compilation);
