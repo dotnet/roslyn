7048431a2d2fd7e295ded3df4a891092573aed4d
Omar Bonnet <obonnet@techsola.com>
2024-11-29 21:16:33 -0500

    Reformat

--- src/Workspaces/CSharpTest/Formatting/FormattingTests.cs
+++ src/Workspaces/CSharpTest/Formatting/FormattingTests.cs
@@ -5608,28 +5608,32 @@ public class Test
         [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/72196")]
         public async Task FormatSwitchExpression_ListPatternAligned()
         {
-            var code = @"class C
-{
-    void M()
-    {
-        _ = Array.Empty<string>() switch
-        {
-        [] => 0,
-            _ => 1,
-        };
-    }
-}";
-            var expectedCode = @"class C
-{
-    void M()
-    {
-        _ = Array.Empty<string>() switch
-        {
-            [] => 0,
-            _ => 1,
-        };
-    }
-}";
+            var code = """
+                class C
+                {
+                    void M()
+                    {
+                        _ = Array.Empty<string>() switch
+                        {
+                        [] => 0,
+                            _ => 1,
+                        };
+                    }
+                }
+                """;
+            var expectedCode = """
+                class C
+                {
+                    void M()
+                    {
+                        _ = Array.Empty<string>() switch
+                        {
+                            [] => 0,
+                            _ => 1,
+                        };
+                    }
+                }
+                """;
             await AssertFormatAsync(expectedCode, code);
         }
 
