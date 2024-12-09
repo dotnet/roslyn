9c6df7d5f2314d10fb6bcf75b38062493e058025
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-12-09 15:32:32 -0800

    Remove

--- src/Features/CSharp/Portable/ExtractMethod/CSharpSelectionValidator.cs
+++ src/Features/CSharp/Portable/ExtractMethod/CSharpSelectionValidator.cs
@@ -205,18 +205,8 @@ internal sealed partial class CSharpSelectionValidator(
                 LastTokenInOriginalSpan = lastTokenInSelection
             };
         }
-            //if (!adjustedSpan.Contains(firstTokenInSelection.Span) && !adjustedSpan.Contains(lastTokenInSelection.Span))
-            //{
-            //    return new SelectionInfo
-            //    {
-            //        Status = new OperationStatus(succeeded: false, FeaturesResources.Selection_does_not_contain_a_valid_token),
-            //        OriginalSpan = adjustedSpan,
-            //        FirstTokenInOriginalSpan = firstTokenInSelection,
-            //        LastTokenInOriginalSpan = lastTokenInSelection
-            //    };
-            //}
 
-            if (!UnderValidContext(firstTokenInSelection) || !UnderValidContext(lastTokenInSelection))
+        if (!UnderValidContext(firstTokenInSelection) || !UnderValidContext(lastTokenInSelection))
         {
             return new SelectionInfo
             {
