52dce403bc490fed157e9130fe2c71173e5d57d8
Shen Chen <shech@microsoft.com>
2024-12-10 22:14:12 -0800

    Add category model

---
+++ src/VisualStudio/TestUtilities2/UnifiedSettings/TestModels/UnifiedSettingsCategory.vb
@@ -0,0 +1,16 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports Newtonsoft.Json
+
+Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.UnifiedSettings.TestModels
+    Public Class UnifiedSettingsCategory
+        <JsonProperty("title")>
+        Public Property Title As String
+
+        <JsonProperty("legacyOptionPageId")>
+        Public Property LegacyOptionPageId As String
+
+    End Class
+End Namespace
