4d1f33d4f658508d4a9ef335c19e5afa6638aee9
Shen Chen <shech@microsoft.com>
2024-12-13 18:03:52 -0800

    Add converter

---
+++ src/VisualStudio/Core/Test.Next/UnifiedSettings/TestModel/ResourceConverter.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Roslyn.VisualStudio.Next.UnitTests.UnifiedSettings.TestModel
+{
+    internal class ResourceConverter : JsonConverter<string>
+    {
+        public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var locReference = reader.GetString()!;
+            return Utilities.EvalResource(locReference);
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
