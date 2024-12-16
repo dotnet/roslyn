31c348cca7e27ff2188daa27d7c8e75a0bf5b152
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-12-16 13:35:38 -0800

    Delete another usage of an extension

--- src/Workspaces/Core/Portable/Shared/TestHooks/TaskExtensions.cs
+++ src/Workspaces/Core/Portable/Shared/TestHooks/TaskExtensions.cs
@@ -36,13 +36,16 @@ internal static partial class TaskExtensions
 
         return CompletesTrackingOperationSlow(task, token);
 
-        static Task CompletesTrackingOperationSlow(Task task, IDisposable token)
+        static async Task CompletesTrackingOperationSlow(Task task, IDisposable token)
         {
-            return task.SafeContinueWith(
-                t => token.Dispose(),
-                CancellationToken.None,
-                TaskContinuationOptions.ExecuteSynchronously,
-                TaskScheduler.Default);
+            try
+            {
+                await task.NoThrowAwaitableInternal(captureContext: false);
+            }
+            finally
+            {
+                token.Dispose();
+            }
         }
     }
 }
