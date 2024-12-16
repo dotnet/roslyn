7854b9e750e416fc2a9b7e482f713d6b67049ab9
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-12-16 13:24:39 -0800

    Delete task extension

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Utilities/TaskFactoryExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Utilities/TaskFactoryExtensions.cs
@@ -15,14 +15,6 @@ namespace Roslyn.Utilities;
 [SuppressMessage("ApiDesign", "CA1068:CancellationToken parameters must come last", Justification = "Matching TPL Signatures")]
 internal static partial class TaskFactoryExtensions
 {
-    public static Task SafeStartNewFromAsync(this TaskFactory factory, Func<Task> actionAsync, CancellationToken cancellationToken, TaskScheduler scheduler)
-    {
-        // The one and only place we can call StartNew<>().
-        var task = factory.StartNew(actionAsync, cancellationToken, TaskCreationOptions.None, scheduler).Unwrap();
-        TaskExtensions.ReportNonFatalError(task, actionAsync);
-        return task;
-    }
-
     public static Task<TResult> SafeStartNewFromAsync<TResult>(this TaskFactory factory, Func<Task<TResult>> funcAsync, CancellationToken cancellationToken, TaskScheduler scheduler)
     {
         // The one and only place we can call StartNew<>().
