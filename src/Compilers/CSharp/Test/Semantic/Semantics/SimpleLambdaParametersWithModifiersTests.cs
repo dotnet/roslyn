da0699349605163ffa30ac8116016cfdb6b89c6f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2024-12-20 12:42:08 -0800

    Add tests

--- src/Compilers/CSharp/Test/Semantic/Semantics/SimpleLambdaParametersWithModifiersTests.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/SimpleLambdaParametersWithModifiersTests.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp.UnitTests;
-using Microsoft.CodeAnalysis.Operations;
 using Xunit;
 
 namespace Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests.Semantics;
@@ -131,6 +130,44 @@ public sealed class SimpleLambdaParametersWithModifiersTests : SemanticModelTest
             compilation.GetTypeByMetadataName(typeof(CLSCompliantAttribute).FullName).GetPublicSymbol());
     }
 
+    [Fact]
+    public void TestOneParameterWithAnAttributeAndDefaultValue()
+    {
+        var compilation = CreateCompilation("""
+            using System;
+
+            delegate void D(ref int x);
+
+            class C
+            {
+                void M()
+                {
+                    D d = ([CLSCompliant(false)] ref x = 0) => { };
+                }
+            }
+            """).VerifyDiagnostics(
+                // (9,38): error CS1741: A ref or out parameter cannot have a default value
+                //         D d = ([CLSCompliant(false)] ref x = 0) => { };
+                Diagnostic(ErrorCode.ERR_RefOutDefaultValue, "ref").WithLocation(9, 38),
+                // (9,42): error CS9098: Implicitly typed lambda parameter 'x' cannot have a default value.
+                //         D d = ([CLSCompliant(false)] ref x = 0) => { };
+                Diagnostic(ErrorCode.ERR_ImplicitlyTypedDefaultParameter, "x").WithArguments("x").WithLocation(9, 42));
+
+        var tree = compilation.SyntaxTrees.Single();
+        var root = tree.GetRoot();
+        var lambda = root.DescendantNodes().OfType<ParenthesizedLambdaExpressionSyntax>().Single();
+
+        var semanticModel = compilation.GetSemanticModel(tree);
+        var symbol = (IMethodSymbol)semanticModel.GetSymbolInfo(lambda).Symbol!;
+
+        Assert.Equal(MethodKind.LambdaMethod, symbol.MethodKind);
+        Assert.Equal(
+            symbol.Parameters.Single().GetAttributes().Single().AttributeClass,
+            compilation.GetTypeByMetadataName(typeof(CLSCompliantAttribute).FullName).GetPublicSymbol());
+        Assert.True(symbol.Parameters is [
+            { Type.SpecialType: SpecialType.System_Int32, RefKind: RefKind.Ref, IsOptional: false }]);
+    }
+
     [Theory]
     [InlineData("[CLSCompliant(false), My]")]
     [InlineData("[CLSCompliant(false)][My]")]
@@ -166,5 +203,68 @@ public sealed class SimpleLambdaParametersWithModifiersTests : SemanticModelTest
         Assert.True(
             symbol.Parameters.Single().GetAttributes().Any(a => a.AttributeClass!.Equals(
                 compilation.GetTypeByMetadataName("MyAttribute").GetPublicSymbol())));
+    }
+
+    [Fact]
+    public void TestOneParameterWithScoped()
+    {
+        var compilation = CreateCompilationWithSpan("""
+            using System;
+            delegate void D(scoped ReadOnlySpan<int> x);
+
+            class C
+            {
+                void M()
+                {
+                    D d = (scoped x) => { };
+                }
+            }
+            """).VerifyDiagnostics();
+
+        var tree = compilation.SyntaxTrees.Single();
+        var root = tree.GetRoot();
+        var lambda = root.DescendantNodes().OfType<ParenthesizedLambdaExpressionSyntax>().Single();
+
+        var semanticModel = compilation.GetSemanticModel(tree);
+        var symbol = (IMethodSymbol)semanticModel.GetSymbolInfo(lambda).Symbol!;
+
+        Assert.Equal(MethodKind.LambdaMethod, symbol.MethodKind);
+        Assert.Equal(ScopedKind.ScopedValue, symbol.Parameters.Single().ScopedKind);
+        Assert.Equal(compilation.GetTypeByMetadataName(typeof(ReadOnlySpan<>).FullName).GetPublicSymbol(), symbol.Parameters.Single().Type.OriginalDefinition);
+    }
+
+    [Fact]
+    public void TestOneParameterWithScopedAndOptionalValue()
+    {
+        var compilation = CreateCompilationWithSpan("""
+            using System;
+            delegate void D(scoped ReadOnlySpan<int> x);
+
+            class C
+            {
+                void M()
+                {
+                    D d = (scoped x = default) => { };
+                }
+            }
+            """).VerifyDiagnostics(
+                // (8,23): error CS9098: Implicitly typed lambda parameter 'x' cannot have a default value.
+                //         D d = (scoped x = default) => { };
+                Diagnostic(ErrorCode.ERR_ImplicitlyTypedDefaultParameter, "x").WithArguments("x").WithLocation(8, 23),
+                // (8,23): warning CS9099: Parameter 1 has default value 'null' in lambda but '<missing>' in the target delegate type.
+                //         D d = (scoped x = default) => { };
+                Diagnostic(ErrorCode.WRN_OptionalParamValueMismatch, "x").WithArguments("1", "null", "<missing>").WithLocation(8, 23));
+
+        var tree = compilation.SyntaxTrees.Single();
+        var root = tree.GetRoot();
+        var lambda = root.DescendantNodes().OfType<ParenthesizedLambdaExpressionSyntax>().Single();
+
+        var semanticModel = compilation.GetSemanticModel(tree);
+        var symbol = (IMethodSymbol)semanticModel.GetSymbolInfo(lambda).Symbol!;
+
+        Assert.Equal(MethodKind.LambdaMethod, symbol.MethodKind);
+        Assert.Equal(ScopedKind.ScopedValue, symbol.Parameters.Single().ScopedKind);
+        Assert.True(symbol.Parameters.Single().IsOptional);
+        Assert.Equal(compilation.GetTypeByMetadataName(typeof(ReadOnlySpan<>).FullName).GetPublicSymbol(), symbol.Parameters.Single().Type.OriginalDefinition);
     }
 }
