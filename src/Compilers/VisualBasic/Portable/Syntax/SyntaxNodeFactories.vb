c2d113700c498367fe0f02b61abfe8fd2b8a65e9
Todd Grunke <toddgrun@microsoft.com>
2025-01-06 08:09:06 -0800

    Ensure VB parser is disposed
    Noticed then when I was trying out adding a pooled object to the parser and that the dispose wasn't always being called to release the object back to the pool. I'm still investigating whether I want to add the pooled data to the parser, but minimially, the existing dispose method should be called.

--- src/Compilers/VisualBasic/Portable/Syntax/SyntaxNodeFactories.vb
+++ src/Compilers/VisualBasic/Portable/Syntax/SyntaxNodeFactories.vb
@@ -292,17 +292,18 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
             Using scanner As New InternalSyntax.Scanner(MakeSourceText(text, 0), VisualBasicParseOptions.Default) ' NOTE: Default options should be enough
                 scanner.ForceScanningXmlDocMode()
 
-                Dim parser = New InternalSyntax.Parser(scanner)
-                parser.GetNextToken(InternalSyntax.ScannerState.Element)
+                Using parser = New InternalSyntax.Parser(scanner)
+                    parser.GetNextToken(InternalSyntax.ScannerState.Element)
 
-                Dim xmlName = InternalSyntax.SyntaxFactory.XmlName(
+                    Dim xmlName = InternalSyntax.SyntaxFactory.XmlName(
                     Nothing, InternalSyntax.SyntaxFactory.XmlNameToken(parentElementName, SyntaxKind.XmlNameToken, Nothing, Nothing))
 
-                Return DirectCast(
+                    Return DirectCast(
                     parser.ParseXmlAttribute(
                         requireLeadingWhitespace:=False,
                         AllowNameAsExpression:=False,
                         xmlElementName:=xmlName).CreateRed(Nothing, 0), BaseXmlAttributeSyntax)
+                End Using
             End Using
         End Function
 
