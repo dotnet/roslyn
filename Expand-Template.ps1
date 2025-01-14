b128bc9a7276220b001dd87ef7a572e0f413fd50
Matteo Prosperi <41970398+matteo-prosperi@users.noreply.github.com>
2025-01-14 11:06:13 -0800

    Create a separate unofficial.yml pipeline (#334)

--- Expand-Template.ps1
+++ Expand-Template.ps1
@@ -156,7 +156,7 @@ try {
     $YmlReplacements = @{
         "(?m)^\s+- microbuild`r?`n"=""
     }
-    Replace-Placeholders -Path "azure-pipelines\official.yml" -Replacements $YmlReplacements
+    Replace-Placeholders -Path "azure-pipelines\unofficial.yml" -Replacements $YmlReplacements
     Replace-Placeholders -Path "azure-pipelines.yml" -Replacements $YmlReplacements
 
     $YmlReplacements = @{}
