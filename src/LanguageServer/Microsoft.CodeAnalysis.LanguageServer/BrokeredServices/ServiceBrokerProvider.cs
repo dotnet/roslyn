c90a3d03326f1c613b7d15377d69673c2932a815
tmat <tomas.matousek@microsoft.com>
2025-01-15 18:57:32 -0800

    FIx

--- src/LanguageServer/Microsoft.CodeAnalysis.LanguageServer/BrokeredServices/ServiceBrokerProvider.cs
+++ src/LanguageServer/Microsoft.CodeAnalysis.LanguageServer/BrokeredServices/ServiceBrokerProvider.cs
@@ -2,23 +2,20 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#pragma warning disable RS0030 // Do not use banned APIs (MEFv1)
-
 using System;
-using System.ComponentModel.Composition;
+using System.Composition;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.ServiceHub.Framework;
-using Microsoft.VisualStudio.Shell.ServiceBroker;
 
 namespace Microsoft.CodeAnalysis.BrokeredServices;
 
 /// <summary>
 /// MEF service that can be used to fetch an <see cref="IServiceBroker"/> instance without having to use legacy MEF imports.
 /// </summary>
-[Export(typeof(IServiceBrokerProvider))]
+[Export(typeof(IServiceBrokerProvider)), Shared]
 [method: ImportingConstructor]
 [method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-internal sealed class ServiceBrokerProvider([Import(typeof(SVsFullAccessServiceBroker))] IServiceBroker serviceBroker) : IServiceBrokerProvider
+internal sealed class ServiceBrokerProvider([Import("Microsoft.VisualStudio.Shell.ServiceBroker.SVsFullAccessServiceBroker")] IServiceBroker serviceBroker) : IServiceBrokerProvider
 {
     public IServiceBroker ServiceBroker { get; } = serviceBroker;
 }
