6c0d300d19cee1219996c102325bda1531493980
Tomáš Matoušek <tmat@users.noreply.github.com>
2025-01-16 13:43:45 -0800

    Move impl of ILspWorkpace to EditorTestWorkspace/LspTestWorkspace (#76753)

--- src/Workspaces/CoreTestUtilities/Workspaces/TestWorkspace`1.cs
+++ src/Workspaces/CoreTestUtilities/Workspaces/TestWorkspace`1.cs
@@ -52,7 +52,6 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
         internal override bool IgnoreUnchangeableDocumentsWhenApplyingChanges { get; }
 
         private readonly string _workspaceKind;
-        private readonly bool _supportsLspMutation;
 
         internal TestWorkspace(
             TestComposition? composition = null,
@@ -60,8 +59,7 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
             Guid solutionTelemetryId = default,
             bool disablePartialSolutions = true,
             bool ignoreUnchangeableDocumentsWhenApplyingChanges = true,
-            WorkspaceConfigurationOptions? configurationOptions = null,
-            bool supportsLspMutation = false)
+            WorkspaceConfigurationOptions? configurationOptions = null)
             : base(GetHostServices(ref composition, configurationOptions != null), workspaceKind ?? WorkspaceKind.Host)
         {
             this.Composition = composition;
@@ -88,7 +86,6 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
 
             this.CanApplyChangeDocument = true;
             this.IgnoreUnchangeableDocumentsWhenApplyingChanges = ignoreUnchangeableDocumentsWhenApplyingChanges;
-            _supportsLspMutation = supportsLspMutation;
             this.GlobalOptions = GetService<IGlobalOptionService>();
 
             if (Services.GetService<INotificationService>() is INotificationServiceCallback callback)
@@ -469,8 +466,6 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
         /// </summary>
         internal override ValueTask TryOnDocumentClosedAsync(DocumentId documentId, CancellationToken cancellationToken)
         {
-            Contract.ThrowIfFalse(this._supportsLspMutation);
-
             var testDocument = this.GetTestDocument(documentId);
             Contract.ThrowIfNull(testDocument);
             Contract.ThrowIfTrue(testDocument.IsSourceGenerated);
