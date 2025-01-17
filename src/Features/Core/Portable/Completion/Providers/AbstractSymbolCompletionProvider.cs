efa4d2cca49055cdea0d9734fee5115acae302f5
Todd Grunke <toddgrun@microsoft.com>
2025-01-17 14:26:16 -0800

    Support target type completion tags in object creation contexts (#76786)
    * Support target type completion tags in object creation contexts
    
    This allows "new |" completion to populate the target type filter with types that derive from the specified target type.
    
    Fixes https://github.com/dotnet/roslyn/issues/45109

--- src/Features/Core/Portable/Completion/Providers/AbstractSymbolCompletionProvider.cs
+++ src/Features/Core/Portable/Completion/Providers/AbstractSymbolCompletionProvider.cs
@@ -47,15 +47,13 @@ internal abstract partial class AbstractSymbolCompletionProvider<TSyntaxContext>
     /// because we ignore nullability.</param>
     private static bool ShouldIncludeInTargetTypedCompletionList(
         ISymbol symbol,
-        ImmutableArray<ITypeSymbol> inferredTypes,
-        SemanticModel semanticModel,
-        int position,
+        TSyntaxContext syntaxContext,
         Dictionary<ITypeSymbol, bool> typeConvertibilityCache)
     {
-        // When searching for identifiers of type C, exclude the symbol for the `C` type itself.
+        // When searching for identifiers of type C, exclude the symbol for the `C` type itself except in an object creation context.
         if (symbol.Kind == SymbolKind.NamedType)
         {
-            return false;
+            return ShouldIncludeInTargetTypedCompletionListForNamedType((INamedTypeSymbol)symbol, syntaxContext, typeConvertibilityCache);
         }
 
         // Avoid offering members of object since they too commonly show up and are infrequently desired.
@@ -69,7 +67,7 @@ internal abstract partial class AbstractSymbolCompletionProvider<TSyntaxContext>
         {
             var local = (ILocalSymbol)symbol;
             var declarationSyntax = symbol.DeclaringSyntaxReferences.Select(r => r.GetSyntax()).SingleOrDefault();
-            if (declarationSyntax != null && position < declarationSyntax.FullSpan.End)
+            if (declarationSyntax != null && syntaxContext.Position < declarationSyntax.FullSpan.End)
             {
                 return false;
             }
@@ -86,8 +84,60 @@ internal abstract partial class AbstractSymbolCompletionProvider<TSyntaxContext>
             return isConvertible;
         }
 
-        typeConvertibilityCache[type] = CompletionUtilities.IsTypeImplicitlyConvertible(semanticModel.Compilation, type, inferredTypes);
+        typeConvertibilityCache[type] = CompletionUtilities.IsTypeImplicitlyConvertible(syntaxContext.SemanticModel.Compilation, type, syntaxContext.InferredTypes);
         return typeConvertibilityCache[type];
+    }
+
+    private static bool ShouldIncludeInTargetTypedCompletionListForNamedType(INamedTypeSymbol symbol, TSyntaxContext syntaxContext, Dictionary<ITypeSymbol, bool> typeConvertibilityCache)
+    {
+        // Only create target typed completion entries in the object creation context
+        if (!syntaxContext.IsObjectCreationTypeContext)
+            return false;
+
+        if (!typeConvertibilityCache.TryGetValue(symbol, out var isConvertible))
+        {
+            isConvertible = IsConvertible(symbol, syntaxContext);
+
+            typeConvertibilityCache[symbol] = isConvertible;
+        }
+
+        return isConvertible;
+
+        static bool IsConvertible(INamedTypeSymbol symbol, TSyntaxContext syntaxContext)
+        {
+            for (var i = 0; i < syntaxContext.InferredTypes.Length; i++)
+            {
+                var inferredType = syntaxContext.InferredTypes[i];
+                if (inferredType.IsArrayType())
+                {
+                    while (inferredType is IArrayTypeSymbol arrayType)
+                        inferredType = arrayType.ElementType;
+                }
+                else
+                {
+                    // Abstract types should not be offered in target typed completion except in array contexts
+                    if (symbol.IsAbstract)
+                        continue;
+                }
+
+                if (inferredType.IsInterfaceType())
+                {
+                    if (Equals(symbol, inferredType.OriginalDefinition) || symbol.AllInterfaces.Any(static (typeInterface, inferredType) => Equals(typeInterface.OriginalDefinition, inferredType.OriginalDefinition), inferredType))
+                        return true;
+                }
+                else
+                {
+                    var typeToCheck = symbol;
+                    while (typeToCheck != null && !Equals(typeToCheck.OriginalDefinition, inferredType.OriginalDefinition))
+                        typeToCheck = typeToCheck.BaseType;
+
+                    if (typeToCheck != null)
+                        return true;
+                }
+            }
+
+            return false;
+        }
     }
 
     /// <summary>
@@ -187,7 +237,7 @@ internal abstract partial class AbstractSymbolCompletionProvider<TSyntaxContext>
         {
             var symbol = symbolList[index];
             var syntaxContext = contextLookup(symbol);
-            if (ShouldIncludeInTargetTypedCompletionList(symbol.Symbol, syntaxContext.InferredTypes, syntaxContext.SemanticModel, syntaxContext.Position, typeConvertibilityCache))
+            if (ShouldIncludeInTargetTypedCompletionList(symbol.Symbol, syntaxContext, typeConvertibilityCache))
                 break;
         }
 
