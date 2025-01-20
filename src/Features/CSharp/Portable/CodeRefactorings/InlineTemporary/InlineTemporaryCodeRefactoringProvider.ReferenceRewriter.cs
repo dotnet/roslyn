5677d8a60e3661f4ca610508fe5ecde981303557
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-01-20 13:38:42 -0800

    Special case inlining a collection expr into a spreaded element

--- src/Features/CSharp/Portable/CodeRefactorings/InlineTemporary/InlineTemporaryCodeRefactoringProvider.ReferenceRewriter.cs
+++ src/Features/CSharp/Portable/CodeRefactorings/InlineTemporary/InlineTemporaryCodeRefactoringProvider.ReferenceRewriter.cs
@@ -1,116 +1,132 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+﻿//// Licensed to the .NET Foundation under one or more agreements.
+//// The .NET Foundation licenses this file to you under the MIT license.
+//// See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+//using System.Collections.Generic;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Linq;
+//using System.Threading;
+//using Microsoft.CodeAnalysis.CSharp.Syntax;
+//using Microsoft.CodeAnalysis.Shared.Extensions;
+//using Microsoft.CodeAnalysis.Shared;
+//using Microsoft.CodeAnalysis.CSharp.Extensions;
 
-namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.InlineTemporary;
+//namespace Microsoft.CodeAnalysis.CSharp.CodeRefactorings.InlineTemporary;
 
-internal sealed partial class CSharpInlineTemporaryCodeRefactoringProvider
-{
-    private sealed class ReferenceRewriter : CSharpSyntaxRewriter
-    {
-        private readonly ISet<IdentifierNameSyntax> _conflictReferences;
-        private readonly ISet<IdentifierNameSyntax> _nonConflictReferences;
-        private readonly ExpressionSyntax _expressionToInline;
-        private readonly CancellationToken _cancellationToken;
+//internal sealed partial class CSharpInlineTemporaryCodeRefactoringProvider
+//{
+//    private sealed class ReferenceRewriter(
+//        ISet<IdentifierNameSyntax> conflictReferences,
+//        ISet<IdentifierNameSyntax> nonConflictReferences,
+//        ExpressionSyntax expressionToInline,
+//        ExpressionSyntax originalDeclaratorExpression,
+//        CancellationToken cancellationToken) : CSharpSyntaxRewriter
+//    {
+//        private readonly ISet<IdentifierNameSyntax> _conflictReferences = conflictReferences;
+//        private readonly ISet<IdentifierNameSyntax> _nonConflictReferences = nonConflictReferences;
+//        private readonly ExpressionSyntax _expressionToInline = expressionToInline;
+//        private readonly ExpressionSyntax _originalDeclaratorExpression = originalDeclaratorExpression;
+//        private readonly CancellationToken _cancellationToken = cancellationToken;
 
-        private ReferenceRewriter(
-            ISet<IdentifierNameSyntax> conflictReferences,
-            ISet<IdentifierNameSyntax> nonConflictReferences,
-            ExpressionSyntax expressionToInline,
-            CancellationToken cancellationToken)
-        {
-            _conflictReferences = conflictReferences;
-            _nonConflictReferences = nonConflictReferences;
-            _expressionToInline = expressionToInline;
-            _cancellationToken = cancellationToken;
-        }
+//        private ExpressionSyntax UpdateIdentifier(IdentifierNameSyntax node)
+//        {
+//            _cancellationToken.ThrowIfCancellationRequested();
 
-        private ExpressionSyntax UpdateIdentifier(IdentifierNameSyntax node)
-        {
-            _cancellationToken.ThrowIfCancellationRequested();
+//            if (_conflictReferences.Contains(node))
+//                return node.Update(node.Identifier.WithAdditionalAnnotations(CreateConflictAnnotation()));
 
-            if (_conflictReferences.Contains(node))
-                return node.Update(node.Identifier.WithAdditionalAnnotations(CreateConflictAnnotation()));
+//            if (_nonConflictReferences.Contains(node))
+//                return _expressionToInline;
 
-            if (_nonConflictReferences.Contains(node))
-                return _expressionToInline;
+//            return node;
+//        }
 
-            return node;
-        }
+//        private bool ShouldSpreadCollectionIntoCollection(
+//            ExpressionSyntax expressions,
+//            [NotNullWhen(true)] out CollectionExpressionSyntax? collectionToInline)
+//        {
+//            if (expressions is not IdentifierNameSyntax identifier ||
+//                !_nonConflictReferences.Contains(identifier) ||
+//                expressions.Parent is not SpreadElementSyntax)
+//            {
+//                collectionToInline = null;
+//                return false;
+//            }
 
-        public override SyntaxNode? VisitIdentifierName(IdentifierNameSyntax node)
-        {
-            var result = UpdateIdentifier(node);
-            return result == _expressionToInline
-                ? result.WithTriviaFrom(node)
-                : result;
-        }
+//            collectionToInline = _originalDeclaratorExpression as CollectionExpressionSyntax;
+//            return collectionToInline != null;
+//        }
 
-        public override SyntaxNode? VisitAnonymousObjectMemberDeclarator(AnonymousObjectMemberDeclaratorSyntax node)
-        {
-            if (node.NameEquals == null &&
-                node.Expression is IdentifierNameSyntax identifier &&
-                _nonConflictReferences.Contains(identifier))
-            {
+//        public override SyntaxNode? VisitIdentifierName(IdentifierNameSyntax node)
+//        {
+//            // Special case inlining a collection into a spread element.  We can just move the original elements into
+//            // the spreaded location.
+//            if (ShouldSpreadCollectionIntoCollection(node, out _))
+//                return node;
 
-                // Special case inlining into anonymous types to ensure that we keep property names:
-                //
-                // E.g.
-                //     int x = 42;
-                //     var a = new { x; };
-                //
-                // Should become:
-                //     var a = new { x = 42; };
-                return node.Update(
-                    SyntaxFactory.NameEquals(identifier), UpdateIdentifier(identifier)).WithTriviaFrom(node);
-            }
+//            var result = UpdateIdentifier(node);
+//            return result == _expressionToInline
+//                ? result.WithTriviaFrom(node)
+//                : result;
+//        }
 
-            return base.VisitAnonymousObjectMemberDeclarator(node);
-        }
+//        public override SyntaxNode? VisitAnonymousObjectMemberDeclarator(AnonymousObjectMemberDeclaratorSyntax node)
+//        {
+//            if (node.NameEquals == null &&
+//                node.Expression is IdentifierNameSyntax identifier &&
+//                _nonConflictReferences.Contains(identifier))
+//            {
 
-        public override SyntaxNode? VisitArgument(ArgumentSyntax node)
-        {
-            if (node.Parent is TupleExpressionSyntax tupleExpression &&
-                ShouldAddTupleMemberName(node, out var identifier) &&
-                tupleExpression.Arguments.Count(a => ShouldAddTupleMemberName(a, out _)) == 1)
-            {
-                return node.Update(
-                    SyntaxFactory.NameColon(identifier), node.RefKindKeyword, UpdateIdentifier(identifier)).WithTriviaFrom(node);
-            }
+//            }
 
-            return base.VisitArgument(node);
-        }
+//            return base.VisitAnonymousObjectMemberDeclarator(node);
+//        }
 
-        private bool ShouldAddTupleMemberName(ArgumentSyntax node, [NotNullWhen(true)] out IdentifierNameSyntax? identifier)
-        {
-            if (node.NameColon == null &&
-                node.Expression is IdentifierNameSyntax id &&
-                _nonConflictReferences.Contains(id) &&
-                !SyntaxFacts.IsReservedTupleElementName(id.Identifier.ValueText))
-            {
-                identifier = id;
-                return true;
-            }
+//        public override SyntaxNode? VisitArgument(ArgumentSyntax node)
+//        {
+//            if (node.Parent is TupleExpressionSyntax tupleExpression &&
+//                ShouldAddTupleMemberName(node, out var identifier) &&
+//                tupleExpression.Arguments.Count(a => ShouldAddTupleMemberName(a, out _)) == 1)
+//            {
+//                return node.Update(
+//                    SyntaxFactory.NameColon(identifier), node.RefKindKeyword, UpdateIdentifier(identifier)).WithTriviaFrom(node);
+//            }
 
-            identifier = null;
-            return false;
-        }
+//            return base.VisitArgument(node);
+//        }
 
-        public static SyntaxNode Visit(
-            SyntaxNode scope,
-            ISet<IdentifierNameSyntax> conflictReferences,
-            ISet<IdentifierNameSyntax> nonConflictReferences,
-            ExpressionSyntax expressionToInline,
-            CancellationToken cancellationToken)
-        {
-            var rewriter = new ReferenceRewriter(conflictReferences, nonConflictReferences, expressionToInline, cancellationToken);
-            return rewriter.Visit(scope);
-        }
-    }
-}
+//        private bool ShouldAddTupleMemberName(ArgumentSyntax node, [NotNullWhen(true)] out IdentifierNameSyntax? identifier)
+//        {
+//            if (node.NameColon == null &&
+//                node.Expression is IdentifierNameSyntax id &&
+//                _nonConflictReferences.Contains(id) &&
+//                !SyntaxFacts.IsReservedTupleElementName(id.Identifier.ValueText))
+//            {
+//                identifier = id;
+//                return true;
+//            }
+
+//            identifier = null;
+//            return false;
+//        }
+
+//        public override SyntaxNode? VisitCollectionExpression(CollectionExpressionSyntax node)
+//        {
+
+
+//            node.Update(VisitToken(node.OpenBracketToken), VisitList(node.Elements), VisitToken(node.CloseBracketToken))
+//        }
+
+//        public override SyntaxNode? VisitSpreadElement(SpreadElementSyntax node)
+//        {
+//            if (!ShouldSpreadCollectionIntoCollection(node.Expression, out var collectionToInline))
+//                return node;
+
+//            // inlining an existing `[...]` collection into a `..` spread element.  We can just move the original
+//            // elements into the final location.
+//            var leadingTrivia = node.GetLeadingTrivia() is [.., (kind: SyntaxKind.WhitespaceTrivia) lastWhitespace]
+//                ? lastWhitespace
+//                : default;
+//        }
+//    }
+//}
