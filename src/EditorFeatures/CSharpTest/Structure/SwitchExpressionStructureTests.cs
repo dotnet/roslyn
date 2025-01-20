8fd131ce0131726bcbc5d0302fb1985836b0a04f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-01-20 15:21:44 -0800

    Add support for outlining switch expressions

---
+++ src/EditorFeatures/CSharpTest/Structure/SwitchExpressionStructureTests.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Structure;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Structure;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Structure;
+
+[Trait(Traits.Feature, Traits.Features.Outlining)]
+public sealed class SwitchExpressionStructureTests : AbstractCSharpSyntaxNodeStructureTests<SwitchExpressionSyntax>
+{
+    internal override AbstractSyntaxStructureProvider CreateProvider() => new SwitchExpressionStructureProvider();
+
+    [Fact]
+    public async Task TestSwitchExpression1()
+    {
+        var code = """
+            class C
+            {
+                void M(int i)
+                {
+                    var v = {|hint:$$i switch{|textspan:
+                    {
+                    }|}|}
+                }
+            }
+            """;
+
+        await VerifyBlockSpansAsync(code,
+            Region("textspan", "hint", CSharpStructureHelpers.Ellipsis, autoCollapse: false));
+    }
+}
