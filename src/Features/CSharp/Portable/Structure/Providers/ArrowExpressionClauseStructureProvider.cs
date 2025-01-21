49bd25e7a05123089b2d04814c184d1cef3d1cd2
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-01-21 11:25:03 -0800

    Improve collapsing of members followed by pp directives

--- src/Features/CSharp/Portable/Structure/Providers/ArrowExpressionClauseStructureProvider.cs
+++ src/Features/CSharp/Portable/Structure/Providers/ArrowExpressionClauseStructureProvider.cs
@@ -4,10 +4,12 @@
 
 #nullable disable
 
+using System.Linq;
 using System.Threading;
 using Microsoft.CodeAnalysis.CSharp.Extensions;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Structure;
 using Microsoft.CodeAnalysis.Text;
 
@@ -22,11 +24,42 @@ internal class ArrowExpressionClauseStructureProvider : AbstractSyntaxNodeStruct
         BlockStructureOptions options,
         CancellationToken cancellationToken)
     {
+        var parent = node.Parent;
+        var end = GetEndPointIfFollowedByDirectives() ?? parent.Span.End;
         spans.Add(new BlockSpan(
             isCollapsible: true,
-            textSpan: TextSpan.FromBounds(previousToken.Span.End, node.Parent.Span.End),
-            hintSpan: node.Parent.Span,
+            textSpan: TextSpan.FromBounds(previousToken.Span.End, end),
+            hintSpan: TextSpan.FromBounds(parent.Span.Start, end),
             type: BlockTypes.Nonstructural,
-            autoCollapse: !node.IsParentKind(SyntaxKind.LocalFunctionStatement)));
+            autoCollapse: parent.Kind() != SyntaxKind.LocalFunctionStatement));
+
+        int? GetEndPointIfFollowedByDirectives()
+        {
+            var endToken = parent.GetLastToken();
+            var nextToken = endToken.GetNextToken();
+            if (nextToken != default)
+            {
+                foreach (var trivia in nextToken.LeadingTrivia)
+                {
+                    if (trivia.IsDirective)
+                    {
+                        var directive = (DirectiveTriviaSyntax)trivia.GetStructure()!;
+                        var matchingDirectives = directive.GetMatchingConditionalDirectives(cancellationToken);
+                        if (matchingDirectives.Length > 0 &&
+                            matchingDirectives[0].Span.Start >= node.Span.Start &&
+                            matchingDirectives.All(d => d.Span.End <= nextToken.Span.Start))
+                        {
+                            // The directives start within the node we're collapsing, but end outside of it (since we
+                            // have at least one on the leading trivia of the next token).  Collapse all the directives
+                            // along with the node itself since we're collapsing the node.
+                            var lastDirective = matchingDirectives.Last();
+                            return lastDirective.Span.End;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
