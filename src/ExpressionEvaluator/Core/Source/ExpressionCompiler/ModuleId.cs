00977b1d3d78c8eb766f2cd4560778fdf2855a22
Tomáš Matoušek <tmat@users.noreply.github.com>
2025-01-23 12:41:16 -0800

    Improve handling bad metadata in EE (#76878)

---
+++ src/ExpressionEvaluator/Core/Source/ExpressionCompiler/ModuleId.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.VisualStudio.Debugger.Clr;
+
+namespace Microsoft.CodeAnalysis.ExpressionEvaluator;
+
+internal readonly struct ModuleId(Guid id, string displayName)
+{
+    public Guid Id { get; } = id;
+    public string DisplayName { get; } = displayName;
+}
+
+internal static class Extensions
+{
+    public static ModuleId GetModuleId(this DkmClrModuleInstance module)
+        => new(module.Mvid, module.FullName);
+}
