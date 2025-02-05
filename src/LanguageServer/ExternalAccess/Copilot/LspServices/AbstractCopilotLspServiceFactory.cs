55cdd6fd7cf01df3734c1481f0bf0b7c7bd37474
gel@microsoft.com <gel@microsoft.com>
2025-02-04 18:56:46 -0800

    Add Copilot EA for langauge server

---
+++ src/LanguageServer/ExternalAccess/Copilot/LspServices/AbstractCopilotLspServiceFactory.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.CodeAnalysis.LanguageServer.ExternalAccess.Copilot;
+
+internal abstract class AbstractCopilotLspServiceFactory : ILspServiceFactory
+{
+    public abstract AbstractCopilotLspService CreateILspService(CopilotLspServices lspServices);
+
+    ILspService ILspServiceFactory.CreateILspService(LspServices lspServices, WellKnownLspServerKinds serverKind)
+    {
+        return CreateILspService(new CopilotLspServices(lspServices));
+    }
+}
