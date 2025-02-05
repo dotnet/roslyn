7f68865c877c129ffee645facc2878a8ccd7e7e2
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-02-05 09:35:02 -0800

    Remove unused methods

--- src/Workspaces/Core/Portable/Diagnostics/DiagnosticAnalysisResult.cs
+++ src/Workspaces/Core/Portable/Diagnostics/DiagnosticAnalysisResult.cs
@@ -221,9 +221,6 @@ internal readonly struct DiagnosticAnalysisResult
     public DiagnosticAnalysisResult ToAggregatedForm()
         => new(ProjectId, Version, DocumentIds, IsEmpty);
 
-    public DiagnosticAnalysisResult UpdateAggregatedResult(VersionStamp version, DocumentId documentId)
-        => new(ProjectId, version, DocumentIdsOrEmpty.Add(documentId), isEmpty: false);
-
     public DiagnosticAnalysisResult DropExceptSyntax()
     {
         // quick bail out
