865191ba7158139885f9983cdf737fdfd81e3a02
Jared Parsons <jared@paranoidcoding.org>
2025-02-04 17:29:45 -0800

    more

--- src/Compilers/Core/Portable/DiagnosticAnalyzer/AnalyzerAssemblyLoader.Core.cs
+++ src/Compilers/Core/Portable/DiagnosticAnalyzer/AnalyzerAssemblyLoader.Core.cs
@@ -18,24 +18,6 @@ using Microsoft.CodeAnalysis.PooledObjects;
 
 namespace Microsoft.CodeAnalysis
 {
-    /// <summary>
-    /// This interface allows hosts to control exactly how a given <see cref="AssemblyName"/> is resolved to an
-    /// <see cref="Assembly"/> instance. This is useful for hosts that need to load assemblies in a custom way like
-    /// Razor or stream based loading.
-    /// </summary>
-    internal interface IAnalyzerAssemblyResolver
-    {
-        /// <summary>
-        /// Resolve a <see cref="Assembly"/> for the given parameters.
-        /// </summary>
-        /// <param name="loader">The <see cref="AnalyzerAssemblyLoader"/> instance that is performing the load</param>
-        /// <param name="directoryContext">The <see cref="AssemblyLoadContext"/> for the current directory</param>
-        /// <param name="assemblyName">The name of the assembly to be loaded</param>
-        /// <param name="directory">The directory where the assembly is being loaded from</param>
-        /// <returns></returns>
-        Assembly? Resolve(AnalyzerAssemblyLoader loader, AssemblyName assemblyName, AssemblyLoadContext directoryContext, string directory);
-    }
-
     internal sealed partial class AnalyzerAssemblyLoader
     {
         internal static IAnalyzerAssemblyResolver DiskAnalyzerAssemblyResolver => DiskResolver.Instance;
