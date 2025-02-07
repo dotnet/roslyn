5f58f9a4ada57ae63d70b251dc47860317a2a04a
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-02-07 12:31:45 -0800

    Doc

--- src/LanguageServer/Protocol/Features/Diagnostics/EngineV2/DiagnosticIncrementalAnalyzer_IncrementalAnalyzer.cs
+++ src/LanguageServer/Protocol/Features/Diagnostics/EngineV2/DiagnosticIncrementalAnalyzer_IncrementalAnalyzer.cs
@@ -19,6 +19,12 @@ internal partial class DiagnosticAnalyzerService
 {
     private partial class DiagnosticIncrementalAnalyzer
     {
+        /// <summary>
+        /// Cached data from a real <see cref="Project"/> instance to the cached diagnostic data produced by
+        /// <em>all</em> the analyzers for the project.  This data can then be used by <see
+        /// cref="DiagnosticGetter.ProduceDiagnosticsAsync"/> to speed up subsequent calls through the normal <see
+        /// cref="IDiagnosticAnalyzerService"/> entry points as long as the project hasn't changed at all.
+        /// </summary>
         private readonly ConditionalWeakTable<Project, StrongBox<(ImmutableArray<StateSet> stateSets, ImmutableDictionary<DiagnosticAnalyzer, DiagnosticAnalysisResult> diagnosticAnalysisResults)>> _projectToForceAnalysisData = new();
 
         public async Task<ImmutableArray<DiagnosticData>> ForceAnalyzeProjectAsync(Project project, CancellationToken cancellationToken)
