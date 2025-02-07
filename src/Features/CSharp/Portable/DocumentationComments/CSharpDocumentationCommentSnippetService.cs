dde66abbe651a72e8adcccd5bd3a89f5c53ce810
akhera99 <ankitakhera@microsoft.com>
2025-02-07 13:05:59 -0800

    feedback

--- src/Features/CSharp/Portable/DocumentationComments/CSharpDocumentationCommentSnippetService.cs
+++ src/Features/CSharp/Portable/DocumentationComments/CSharpDocumentationCommentSnippetService.cs
@@ -15,7 +15,6 @@ using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Roslyn.Utilities;
-using Microsoft.CodeAnalysis.Text;
 using System.Collections.Immutable;
 
 namespace Microsoft.CodeAnalysis.CSharp.DocumentationComments;
@@ -27,15 +26,6 @@ internal sealed class CSharpDocumentationCommentSnippetService : AbstractDocumen
 
     protected override bool AddIndent => true;
     protected override string ExteriorTriviaText => "///";
-
-    private static readonly SymbolDisplayFormat s_format =
-        new(
-            globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Omitted,
-            typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypes,
-            genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters,
-            miscellaneousOptions:
-                SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers |
-                SymbolDisplayMiscellaneousOptions.UseSpecialTypes);
 
     [ImportingConstructor]
     [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
