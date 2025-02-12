b2654c8eb8ed5b924b0cf6c865a1c794e9f3bf42
gel@microsoft.com <gel@microsoft.com>
2025-02-12 11:52:23 -0800

    Address review comments

--- src/LanguageServer/ExternalAccess/Copilot/Handler/CopilotRequestContext.cs
+++ src/LanguageServer/ExternalAccess/Copilot/Handler/CopilotRequestContext.cs
@@ -2,17 +2,20 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Microsoft.CodeAnalysis.LanguageServer.Handler;
 
 namespace Microsoft.CodeAnalysis.LanguageServer.ExternalAccess.Copilot;
 
 /// <summary>
-/// Context for requests handled by handlers like <see cref="AbstractCopilotLspServiceDocumentRequestHandler{TRequest, TResponse}"/>
+/// Context for requests handled by <see cref="AbstractCopilotLspServiceDocumentRequestHandler{TRequest, TResponse}"/>
 /// </summary>
 internal readonly struct CopilotRequestContext(RequestContext context)
 {
-    /// <inheritdoc cref="RequestContext.Solution"/>
-    public Solution? Solution => context.Solution;
+    /// <summary>
+    /// The solution state that the request should operate on.
+    /// </summary>
+    public Solution Solution => context.Solution ?? throw new InvalidOperationException();
 
     /// <inheritdoc cref="RequestContext.Document"/>
     public Document? Document => context.Document;
