eb904d98a018856319797ca71a3d953f3e9bfe9a
David Wengier <david.wengier@microsoft.com>
2025-02-20 17:33:04 +1100

    Forgot to change the file header when copying the file contents

--- src/LanguageServer/Protocol/Protocol/Internal/Diagnostics/VSInternalDiagnosticRegistrationOptions.cs
+++ src/LanguageServer/Protocol/Protocol/Internal/Diagnostics/VSInternalDiagnosticRegistrationOptions.cs
@@ -1,6 +1,6 @@
-﻿// <copyright file="VSInternalDiagnosticRegistrationOptions.cs" company="Microsoft">
-// Copyright (c) Microsoft. All rights reserved.
-// </copyright>
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 namespace Roslyn.LanguageServer.Protocol
 {
