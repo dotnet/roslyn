9396c12acd73d77eaa23ad3a5f727b3cac2d546c
Jared Parsons <jared@paranoidcoding.org>
2025-02-21 10:31:28 -0800

    more

--- src/Compilers/CSharp/Portable/Lowering/DiagnosticsPass_ExpressionTrees.cs
+++ src/Compilers/CSharp/Portable/Lowering/DiagnosticsPass_ExpressionTrees.cs
@@ -328,6 +328,14 @@ namespace Microsoft.CodeAnalysis.CSharp
                 {
                     Error(ErrorCode.ERR_ExpressionTreeContainsIndexedProperty, node);
                 }
+                else if (hasDefaultArgument(arguments, defaultArguments))
+                {
+                    Error(ErrorCode.ERR_ExpressionTreeContainsOptionalArgument, node);
+                }
+                else if (!argumentNamesOpt.IsDefaultOrEmpty)
+                {
+                    Error(ErrorCode.ERR_ExpressionTreeContainsNamedArgument, node);
+                }
                 else if (IsComCallWithRefOmitted(method, arguments, argumentRefKindsOpt))
                 {
                     Error(ErrorCode.ERR_ComRefCallInExpressionTree, node);
