a1e69b675a36229f8e30aae0ccf3f4b621696245
Youssef1313 <youssefvictor00@gmail.com>
2025-02-23 21:30:40 +0100

    Delete seemingly deadcode

--- src/Microsoft.VisualStudio.Extensibility.Testing.Xunit.Shared/InProcess/TestInvoker_InProc.cs
+++ src/Microsoft.VisualStudio.Extensibility.Testing.Xunit.Shared/InProcess/TestInvoker_InProc.cs
@@ -22,62 +22,13 @@ namespace Xunit.InProcess
             AppDomain.CurrentDomain.AssemblyResolve += VisualStudioInstanceFactory.AssemblyResolveHandler;
         }
 
+        // NOTE: This is called by OutOfProcComponent.CreateInProcComponent using Activator.
         public static TestInvoker_InProc Create()
             => new TestInvoker_InProc();
-
-        public void LoadAssembly(string codeBase)
-        {
-            var assembly = Assembly.LoadFrom(codeBase);
-        }
 
         public InProcessIdeTestAssemblyRunner CreateTestAssemblyRunner(ITestAssembly testAssembly, IXunitTestCase[] testCases, IMessageSink diagnosticMessageSink, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
         {
             return new InProcessIdeTestAssemblyRunner(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions);
-        }
-
-        public Tuple<decimal, Exception> InvokeTest(
-            ITest test,
-            IMessageBus messageBus,
-            Type testClass,
-            object?[]? constructorArguments,
-            MethodInfo testMethod,
-            object?[]? testMethodArguments)
-        {
-            var aggregator = new ExceptionAggregator();
-            var beforeAfterAttributes = new BeforeAfterTestAttribute[0];
-            var cancellationTokenSource = new CancellationTokenSource();
-
-            var synchronizationContext = new DispatcherSynchronizationContext(Application.Current.Dispatcher, DispatcherPriority.Background);
-            var result = Task.Factory.StartNew(
-                async () =>
-                {
-                    try
-                    {
-                        var invoker = new XunitTestInvoker(
-                            test,
-                            messageBus,
-                            testClass,
-                            constructorArguments,
-                            testMethod,
-                            testMethodArguments,
-                            beforeAfterAttributes,
-                            aggregator,
-                            cancellationTokenSource);
-                        return await invoker.RunAsync();
-                    }
-                    catch (Exception)
-                    {
-                        Debugger.Launch();
-                        throw;
-                    }
-                },
-                CancellationToken.None,
-                TaskCreationOptions.None,
-#pragma warning disable VSTHRD002 // Avoid problematic synchronous waits
-                new SynchronizationContextTaskScheduler(synchronizationContext)).Unwrap().GetAwaiter().GetResult();
-#pragma warning restore VSTHRD002 // Avoid problematic synchronous waits
-
-            return Tuple.Create(result, aggregator.ToException());
         }
     }
 }
