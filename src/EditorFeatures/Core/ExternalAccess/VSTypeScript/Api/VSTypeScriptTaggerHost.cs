728b2a3f9267b2160f4afd857c3213f24cf856b8
Tomas Matousek <tomat@microsoft.com>
2025-02-23 09:15:09 -0800

    Implement TypeScript ExternalAccess wrappers for AsynchronousOperationListener

---
+++ src/EditorFeatures/Core/ExternalAccess/VSTypeScript/Api/VSTypeScriptTaggerHost.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using Microsoft.CodeAnalysis.Editor.Tagging;
+using Microsoft.CodeAnalysis.Host.Mef;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.VSTypeScript.Api;
+
+[Export(typeof(VSTypeScriptTaggerHost)), Shared]
+[method: ImportingConstructor]
+[method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+internal sealed class VSTypeScriptTaggerHost(TaggerHost underlyingObject)
+{
+    internal TaggerHost UnderlyingObject { get; } = underlyingObject;
+}
