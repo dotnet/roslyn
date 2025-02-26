e56be3a8d00ad1acbfeb26a4dd3ffb98096b06cc
Joey Robichaud <jorobich@microsoft.com>
2025-02-25 22:44:58 -0800

    Fix file headers

--- src/RoslynAnalyzers/Utilities/Compiler/RoslynHashCode.cs
+++ src/RoslynAnalyzers/Utilities/Compiler/RoslynHashCode.cs
@@ -1,4 +1,6 @@
-﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 // NOTE: This code is derived from an implementation originally in dotnet/runtime:
 // https://github.com/dotnet/runtime/blob/v5.0.3/src/libraries/System.Private.CoreLib/src/System/HashCode.cs
