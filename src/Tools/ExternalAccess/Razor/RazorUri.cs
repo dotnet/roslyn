ddad76d6edbf5bb657798574f402ab982dd70cc3
David Wengier <david.wengier@microsoft.com>
2025-02-26 14:23:44 +1100

    Expose some functionality from SourceGeneratedDocumentUri to Razor

--- src/Tools/ExternalAccess/Razor/RazorUri.cs
+++ src/Tools/ExternalAccess/Razor/RazorUri.cs
@@ -14,4 +14,20 @@ internal static class RazorUri
 
     public static string GetDocumentFilePathFromUri(Uri uri)
         => ProtocolConversions.GetDocumentFilePathFromUri(uri);
+
+    public static bool IsGeneratedDocumentUri(Uri generatedDocumentUri)
+        => generatedDocumentUri.Scheme == SourceGeneratedDocumentUri.Scheme;
+
+    // TODO: This method is unnecessary if https://github.com/dotnet/roslyn/issues/50546 is done
+    public static string GetHintNameFromGeneratedDocumentUri(Solution solution, Uri generatedDocumentUri)
+    {
+        Contract.ThrowIfFalse(IsGeneratedDocumentUri(generatedDocumentUri));
+
+        if (SourceGeneratedDocumentUri.DeserializeIdentity(solution, generatedDocumentUri) is not { } identity)
+        {
+            throw new InvalidOperationException($"Could not deserialize Uri into a source generated Uri: {generatedDocumentUri}");
+        }
+
+        return identity.HintName;
+    }
 }
