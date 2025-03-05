10af07509517e092d5dcdcef74365b6f092ddb93
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-03-05 10:12:02 -0800

    Add test

--- src/EditorFeatures/CSharpTest/SignatureHelp/WithElementSignatureHelpProviderTests.cs
+++ src/EditorFeatures/CSharpTest/SignatureHelp/WithElementSignatureHelpProviderTests.cs
@@ -3,10 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.SignatureHelp;
-using Microsoft.CodeAnalysis.Editor.UnitTests.SignatureHelp;
 using Microsoft.CodeAnalysis.Test.Utilities;
 using Xunit;
 
@@ -59,5 +57,77 @@ public sealed class WithElementSignatureHelpProviderTests : AbstractCSharpSignat
             """;
 
         await TestAsync(markup, []);
+    }
+
+    [Fact]
+    public async Task TestConstructibleType()
+    {
+        var markup = $$"""
+            using System;
+            using System.Collections.Generic;
+
+            class C
+            {
+                void Goo()
+                {
+                    HashSet<int> set = [with($$)];
+                }
+            }
+            """;
+
+        await TestAsync(markup, [
+            new("HashSet<int>()", string.Empty, null, currentParameterIndex: 0),
+            new("HashSet<int>(IEnumerable<int> collection)", string.Empty, null, currentParameterIndex: 0),
+            new("HashSet<int>(IEqualityComparer<int> comparer)", string.Empty, null, currentParameterIndex: 0),
+            new("HashSet<int>(IEnumerable<int> collection, IEqualityComparer<int> comparer)", string.Empty, null, currentParameterIndex: 0)]);
+    }
+
+    [Fact]
+    public async Task TestBuilder1()
+    {
+        var markup = $$"""
+            using System;
+            using System.Collections;
+            using System.Collections.Generic;
+            using System.Runtime.CompilerServices;
+
+            namespace System
+            {
+                public readonly ref struct ReadOnlySpan<T> { }
+            }
+
+            namespace System.Runtime.CompilerServices
+            {
+                [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface)]
+                internal sealed class CollectionBuilderAttribute : Attribute
+                {
+                    public CollectionBuilderAttribute(Type builderType, string methodName) { }
+                }
+            }
+
+            [CollectionBuilder(typeof(MyCollectionBuilder), "Create")]
+            class MyCollection<T> : IEnumerable<T>
+            {
+                public IEnumerator<T> GetEnumerator() => new System.NotImplementedException();
+                IEnumerator IEnumerable.GetEnumerator() => new System.NotImplementedException();
+            }
+
+            static class MyCollectionBuilder
+            {
+                public static MyCollection<T> Create<T>(ReadOnlySpan<T> values, int capacity, int extra) => new System.NotImplementedException();
+                public static MyCollection<T> Create<T>(string capacity, string extra, ReadOnlySpan<T> values) => new System.NotImplementedException();
+            }
+
+            class C
+            {
+                public void Test()
+                {
+                    MyCollection<int> z = [with($$)];
+                }
+            }
+            """;
+        await TestAsync(markup, [
+            new("MyCollection<int> MyCollectionBuilder.Create<T>(int capacity, int extra)", string.Empty, null, currentParameterIndex: 0),
+            new("MyCollection<int> MyCollectionBuilder.Create<T>(string capacity, string extra)", string.Empty, null, currentParameterIndex: 0)]);
     }
 }
