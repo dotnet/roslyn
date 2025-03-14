09e4a9f37eb67419dfbff6263523f74eb49aadff
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-03-14 12:12:52 -0700

    Fix find refs

--- src/Compilers/Core/Portable/Symbols/ITypeSymbol.cs
+++ src/Compilers/Core/Portable/Symbols/ITypeSymbol.cs
@@ -87,6 +87,11 @@ namespace Microsoft.CodeAnalysis
         bool IsExtension { get; }
 
         /// <summary>
+        /// The extension parameter if this is an extension declaration (<see cref="IsExtension"/> is true).
+        /// </summary>
+        IParameterSymbol? ExtensionParameter { get; }
+
+        /// <summary>
         /// The original definition of this symbol. If this symbol is constructed from another
         /// symbol by type substitution then <see cref="OriginalDefinition"/> gets the original symbol as it was defined in
         /// source or metadata.
