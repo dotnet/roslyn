d620f6394a3e014dad33b1c1e2adaac0cbf2b3c0
tmat <tomas.matousek@microsoft.com>
2025-03-17 15:11:38 -0700

    Fix

---
+++ src/VisualStudio/Core/Def/SemanticSearch/SemanticSearchFeatureFlag.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.Options;
+
+namespace Microsoft.VisualStudio.LanguageServices;
+
+internal static class SemanticSearchFeatureFlag
+{
+    public static readonly Option2<bool> PromptEnabled = new("visual_studio_enable_semantic_search_prompt", defaultValue: false);
+}
