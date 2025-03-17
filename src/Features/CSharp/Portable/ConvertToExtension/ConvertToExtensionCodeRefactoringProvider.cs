4014124d3a6f0f2945374c6a87bacaea5ed07be1
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-03-17 14:17:12 -0700

    In progress

--- src/Features/CSharp/Portable/ConvertToExtension/ConvertToExtensionCodeRefactoringProvider.cs
+++ src/Features/CSharp/Portable/ConvertToExtension/ConvertToExtensionCodeRefactoringProvider.cs
@@ -6,12 +6,15 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Text;
@@ -31,19 +34,75 @@ internal sealed class ConvertToExtensionCodeRefactoringProvider() : CodeRefactor
 
     public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
     {
-        var cancellationToken = context.CancellationToken;
+        // If the user is on an extension method, offer to convert it to a modern extension.
         var methodDeclaration = await context.TryGetRelevantNodeAsync<MethodDeclarationSyntax>().ConfigureAwait(false);
+        if (methodDeclaration != null)
+        {
+            if (IsExtensionMethod(methodDeclaration, out var classDeclaration))
+            {
+                await ComputeRefactoringsAsync(
+                    context, classDeclaration, [methodDeclaration],
+                    CSharpFeaturesResources.Convert_extension_method_to_extension,
+                    nameof(CSharpFeaturesResources.Convert_extension_method_to_extension)).ConfigureAwait(false);
+            }
 
-        if (methodDeclaration is not { ParameterList.Parameters: [var firstParameter, ..] })
             return;
+        }
+        else
+        {
+            // Otherwise, if they're on a static class, which contains extension methods, offer to convert all of them.
+            var classDeclaration = await context.TryGetRelevantNodeAsync<ClassDeclarationSyntax>().ConfigureAwait(false);
+            if (classDeclaration != null)
+            {
+                await ComputeRefactoringsAsync(
+                    context, classDeclaration, GetExtensionMethods(classDeclaration),
+                    CSharpFeaturesResources.Convert_all_extension_methods_to_extension,
+                    nameof(CSharpFeaturesResources.Convert_all_extension_methods_to_extension)).ConfigureAwait(false);
+            }
+        }
+    }
+
+    private static bool IsExtensionMethod(
+        MethodDeclarationSyntax methodDeclaration,
+        [NotNullWhen(true)] out ClassDeclarationSyntax? classDeclaration)
+    {
+        classDeclaration = null;
+        if (methodDeclaration.ParameterList.Parameters is not [var firstParameter, ..])
+            return false;
 
         if (!firstParameter.Modifiers.Any(SyntaxKind.ThisKeyword))
+            return false;
+
+        classDeclaration = methodDeclaration.Parent as ClassDeclarationSyntax;
+        return classDeclaration != null;
+    }
+
+    private static ImmutableArray<MethodDeclarationSyntax> GetExtensionMethods(ClassDeclarationSyntax classDeclaration)
+        => [.. classDeclaration.Members.OfType<MethodDeclarationSyntax>().Where(m => IsExtensionMethod(m, out _))];
+
+    private async Task ComputeRefactoringsAsync(
+        CodeRefactoringContext context,
+        ClassDeclarationSyntax classDeclaration,
+        ImmutableArray<MethodDeclarationSyntax> extensionMethods,
+        string title,
+        string equivalenceKey)
+    {
+        if (extensionMethods.IsEmpty)
             return;
 
-        if (methodDeclaration.Parent is not ClassDeclarationSyntax classDeclaration)
-            return;
+        context.RegisterRefactoring(CodeAction.Create(
+            title,
+            cancellationToken => ConvertToExtensionAsync(context.Document, classDeclaration, extensionMethods, cancellationToken),
+            equivalenceKey));
+    }
 
-
+    private async Task<Document> ConvertToExtensionAsync(
+        Document document,
+        ClassDeclarationSyntax classDeclaration,
+        ImmutableArray<MethodDeclarationSyntax> extensionMethods,
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
     }
 
     private sealed class ConvertToExtensionFixAllProvider()
@@ -57,8 +116,19 @@ internal sealed class ConvertToExtensionCodeRefactoringProvider() : CodeRefactor
         {
             var cancellationToken = fixAllContext.CancellationToken;
 
+            var semanticModel = await document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
             var root = await document.GetRequiredSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-            var staticClassDeclarations = GetTopLevelClassDeclarations(root, fixAllSpans);
+            var staticClassDeclarations = GetTopLevelClassDeclarations(root, fixAllSpans)
+                .Where(c => c.Modifiers.Any(SyntaxKind.StaticKeyword));
+
+            var editor = new SyntaxEditor(root, document.Project.Solution.Services);
+            foreach (var declaration in staticClassDeclarations)
+            {
+                var newDeclaration = await ConvertToExtensionAsync(
+                    semanticModel, declaration, cancellationToken).ConfigureAwait(false);
+                if (newDeclaration != null)
+                    editor.ReplaceNode(declaration, newDeclaration);
+            }
         }
 
         private static IEnumerable<ClassDeclarationSyntax> GetTopLevelClassDeclarations(
