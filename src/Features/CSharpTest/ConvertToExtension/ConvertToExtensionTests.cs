4811f2bab11db21fd9bd985bb24d01d584e533e6
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-03-18 10:25:45 -0700

    Loc strings

---
+++ src/Features/CSharpTest/ConvertToExtension/ConvertToExtensionTests.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.ConvertToExtension;
+using Microsoft.CodeAnalysis.CSharp.Shared.Extensions;
+using Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests.ConvertToExtension;
+
+using VerifyCS = CSharpCodeRefactoringVerifier<ConvertToExtensionCodeRefactoringProvider>;
+
+public sealed class ConvertToExtensionTests
+{
+    [Fact]
+    public async Task TestBaseCase()
+    {
+        await new VerifyCS.Test
+        {
+            TestCode = """
+            static class C
+            {
+                [||]public static void M(this int i) { }
+            }
+            """,
+            FixedCode = """
+            static class C
+            {
+                extension(int i)
+                {
+                    public void M() { }
+                }
+            }
+            """,
+            LanguageVersion = LanguageVersionExtensions.CSharpNext,
+        }.RunAsync();
+    }
+}
