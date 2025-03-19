e3af84311e53947ebe31fd833b67d7c38e1b6459
Julien Couvreur <jcouv@microsoft.com>
2025-03-19 10:42:35 -0700

    Don't check constraint in explicit type argument scenario for now

--- src/Compilers/CSharp/Portable/Compilation/CSharpSemanticModel.cs
+++ src/Compilers/CSharp/Portable/Compilation/CSharpSemanticModel.cs
@@ -4690,11 +4690,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 {
                     constructedMethod = method.ConstructIncludingExtension(typeArguments);
                     Debug.Assert((object)constructedMethod != null);
-
-                    if (!checkConstraintsIncludingExtension(method, typeArguments, compilation, method.ContainingAssembly.CorLibrary.TypeConversions))
-                    {
-                        return false;
-                    }
+                    // PROTOTYPE we should check constraints
                 }
                 else
                 {
@@ -4746,46 +4742,6 @@ namespace Microsoft.CodeAnalysis.CSharp
             }
 
             throw ExceptionUtilities.UnexpectedValue(member.Kind);
-
-            static bool checkConstraintsIncludingExtension(MethodSymbol symbol, ImmutableArray<TypeWithAnnotations> typeArgs, CSharpCompilation compilation, TypeConversions conversions)
-            {
-                var diagnosticsBuilder = ArrayBuilder<TypeParameterDiagnosticInfo>.GetInstance();
-                ArrayBuilder<TypeParameterDiagnosticInfo>? useSiteDiagnosticsBuilder = null;
-
-                var constraintArgs = new ConstraintsHelper.CheckConstraintsArgs(compilation, conversions, includeNullability: false, NoLocation.Singleton, diagnostics: null, template: CompoundUseSiteInfo<AssemblySymbol>.Discarded);
-
-                bool success = true;
-
-                if (symbol.GetIsNewExtensionMember())
-                {
-                    NamedTypeSymbol extensionDeclaration = symbol.ContainingType;
-                    if (extensionDeclaration.Arity > 0)
-                    {
-                        var extensionTypeParameters = extensionDeclaration.TypeParameters;
-                        var extensionTypeArguments = typeArgs[..extensionDeclaration.Arity];
-                        var extensionSubstitution = new TypeMap(extensionTypeParameters, extensionTypeArguments);
-
-                        success = extensionDeclaration.CheckConstraints(
-                           constraintArgs, extensionSubstitution, extensionTypeParameters, extensionTypeArguments, diagnosticsBuilder,
-                           nullabilityDiagnosticsBuilderOpt: null, ref useSiteDiagnosticsBuilder, ignoreTypeConstraintsDependentOnTypeParametersOpt: null);
-                    }
-                }
-
-                if (success && symbol.Arity > 0)
-                {
-                    var memberTypeParameters = symbol.TypeParameters;
-                    var memberTypeArguments = typeArgs[^symbol.Arity..];
-                    var memberSubstitution = new TypeMap(memberTypeParameters, memberTypeArguments);
-
-                    success = symbol.CheckConstraints(
-                        constraintArgs, memberSubstitution, memberTypeParameters, memberTypeArguments, diagnosticsBuilder,
-                        nullabilityDiagnosticsBuilderOpt: null, ref useSiteDiagnosticsBuilder, ignoreTypeConstraintsDependentOnTypeParametersOpt: null);
-                }
-
-                diagnosticsBuilder.Free();
-
-                return success;
-            }
 #nullable disable
         }
 
