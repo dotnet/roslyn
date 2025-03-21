41cd8931cbe0eb5dfee3656d444749402cb52ad2
Jason Malinowski <jason.malinowski@microsoft.com>
2025-03-21 16:44:09 -0700

    Remove all the existing workaround checks
    All of these have been fixed long ago (or we suspect they've long been
    fixed.)
    
    Closes https://github.com/dotnet/roslyn/issues/35123

--- src/VisualStudio/IntegrationTest/TestSetup/TestExtensionErrorHandler.cs
+++ src/VisualStudio/IntegrationTest/TestSetup/TestExtensionErrorHandler.cs
@@ -23,28 +23,6 @@ namespace Microsoft.VisualStudio.IntegrationTest.Setup
 
         public void HandleError(object sender, Exception exception)
         {
-            if (exception is ArgumentException argumentException
-                && argumentException.Message.Contains("SnapshotPoint")
-                && argumentException.StackTrace.Contains("Microsoft.VisualStudio.Text.Editor.Implementation.WpfTextView.ValidateBufferPosition"))
-            {
-                // Known issue https://github.com/dotnet/roslyn/issues/35123
-                return;
-            }
-
-            if (exception is TaskCanceledException taskCanceledException
-                && taskCanceledException.StackTrace.Contains("Microsoft.CodeAnalysis.Editor.Implementation.Suggestions.SuggestedActionsSourceProvider.SuggestedActionsSource.GetSuggestedActions"))
-            {
-                // Workaround for https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1070469
-                return;
-            }
-
-            if (exception is ObjectDisposedException objectDisposedException
-                && objectDisposedException.StackTrace.Contains("Microsoft.VisualStudio.Text.IntraTextTaggerAggregator.Implementation.IntraTextAdornmentTagger"))
-            {
-                // Workaround for https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1935805
-                return;
-            }
-
             FatalError.ReportAndPropagate(exception);
             TestTraceListener.Instance.AddException(exception);
         }
