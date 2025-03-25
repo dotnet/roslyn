1392423d47604820aeefb3c38eac457a9fd70ce5
Todd Grunke <toddgrun@microsoft.com>
2025-03-24 21:25:09 -0700

    lint

--- src/VisualStudio/Core/Impl/SolutionExplorer/AnalyzerNodeSetup.cs
+++ src/VisualStudio/Core/Impl/SolutionExplorer/AnalyzerNodeSetup.cs
@@ -7,7 +7,6 @@ using System.ComponentModel.Composition;
 using System.ComponentModel.Design;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.VisualStudio.Shell;
 
@@ -16,18 +15,15 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
     [Export(typeof(IAnalyzerNodeSetup))]
     internal sealed class AnalyzerNodeSetup : IAnalyzerNodeSetup
     {
-        private readonly IThreadingContext _threadingContext;
         private readonly AnalyzerItemsTracker _analyzerTracker;
         private readonly AnalyzersCommandHandler _analyzerCommandHandler;
 
         [ImportingConstructor]
         [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
         public AnalyzerNodeSetup(
-            IThreadingContext threadingContext,
             AnalyzerItemsTracker analyzerTracker,
             AnalyzersCommandHandler analyzerCommandHandler)
         {
-            _threadingContext = threadingContext;
             _analyzerTracker = analyzerTracker;
             _analyzerCommandHandler = analyzerCommandHandler;
         }
