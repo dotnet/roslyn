fc958587880700c82e0b97fdbcff0ec8d0cbb2c2
Andrew Hall <andrha@microsoft.com>
2025-04-07 17:45:29 -0700

    Moniker comments

--- src/LanguageServer/Protocol/Protocol/Moniker/MonikerRegistrationOptions.cs
+++ src/LanguageServer/Protocol/Protocol/Moniker/MonikerRegistrationOptions.cs
@@ -12,6 +12,9 @@ namespace Roslyn.LanguageServer.Protocol;
 /// See the <see href="https://microsoft.github.io/language-server-protocol/specifications/specification-current/#monikerRegistrationOptions">Language Server Protocol specification</see> for additional information.
 /// </para>
 /// </summary>
+/// <remarks>
+/// Do not seal this class. It is intended to be an extensible LSP type through IVT.
+/// </remarks>
 internal class MonikerRegistrationOptions : CallHierarchyOptions, ITextDocumentRegistrationOptions, IStaticRegistrationOptions
 {
     /// <summary>
