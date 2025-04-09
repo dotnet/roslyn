7b29ab7110a7fcb24c842bef9c82ea1d1fe539fb
tmat <tomas.matousek@microsoft.com>
2025-04-08 17:47:15 -0700

    Fixes

--- src/Compilers/CSharp/Test/Emit2/Emit/EditAndContinue/EditAndContinueTests.cs
+++ src/Compilers/CSharp/Test/Emit2/Emit/EditAndContinue/EditAndContinueTests.cs
@@ -19872,7 +19872,7 @@ file class C
             // The max number of bytes that can fit into #US the heap is 2^29 - 1,
             // but each string also needs to have an offset < 0x1000000 (2^24) to be addressable by a token.
             // If the string is larger than that the next string can't be emitted.
-            var baseString = new string('x', 1 << 24);
+            var baseString = new string('x', 1 << 23);
 
             using var _ = new EditAndContinueTest(targetFramework: TargetFramework.Net90, verification: Verification.Skipped)
                 .AddBaseline(
@@ -19972,7 +19972,7 @@ file class C
             // The max number of bytes that can fit into #US the heap is 2^29 - 1,
             // but each string also needs to have an offset < 0x1000000 (2^24) to be addressable by a token.
             // If the string is larger than that the next string can't be emitted.
-            var baseString = new string('x', 1 << 24);
+            var baseString = new string('x', 1 << 23);
 
             using var _ = new EditAndContinueTest(targetFramework: TargetFramework.Net90, verification: Verification.Skipped)
                 .AddBaseline(
