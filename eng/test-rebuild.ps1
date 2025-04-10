aa094318d5e24f170d7d90a3616b76305d1a8896
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-04-09 23:12:29 -0700

    remove

--- eng/test-rebuild.ps1
+++ eng/test-rebuild.ps1
@@ -45,7 +45,7 @@ try {
   " --assembliesPath `"$ArtifactsDir/obj/`"" +
 
 # Rebuilds with output differences
-  " --exclude net472\Microsoft.CodeAnalysis.EditorFeatures.Wpf.dll" +
+  " --exclude net472\Microsoft.CodeAnalysis.EditorFeatures.dll" +
   " --exclude net472\Microsoft.VisualStudio.LanguageServices.CSharp.dll" +
   " --exclude net472\Microsoft.VisualStudio.LanguageServices.dll" +
   " --exclude net472\Microsoft.VisualStudio.LanguageServices.Implementation.dll" +
