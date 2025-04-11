26c834a32262e4c4383e51f2ab45185d2a60a291
Jared Parsons <jared@paranoidcoding.org>
2025-04-11 10:21:15 -0700

    more

--- src/Compilers/Test/Core/CompilationVerifier.EmitOutput.cs
+++ src/Compilers/Test/Core/CompilationVerifier.EmitOutput.cs
@@ -3,27 +3,12 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Reflection.Metadata;
-using System.Reflection.Metadata.Ecma335;
 using System.Reflection.PortableExecutable;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using ICSharpCode.Decompiler.Metadata;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeGen;
-using Microsoft.CodeAnalysis.Emit;
-using Microsoft.CodeAnalysis.PooledObjects;
-using Microsoft.DiaSymReader.Tools;
-using Roslyn.Test.Utilities;
-using Xunit;
 
 namespace Microsoft.CodeAnalysis.Test.Utilities
 {
