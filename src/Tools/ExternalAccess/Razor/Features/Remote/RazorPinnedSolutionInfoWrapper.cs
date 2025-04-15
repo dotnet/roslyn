f0276d867f607be95818c39b649e6533b2ff9bbd
David Wengier <david.wengier@microsoft.com>
2025-04-15 11:54:15 +1000

    Allow the solution info wrapper to carry a real solution for in-proc purposes

--- src/Tools/ExternalAccess/Razor/Features/Remote/RazorPinnedSolutionInfoWrapper.cs
+++ src/Tools/ExternalAccess/Razor/Features/Remote/RazorPinnedSolutionInfoWrapper.cs
@@ -2,12 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Runtime.Serialization;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
 {
     /// <summary>
-    /// A wrapper for a solution that can be used by Razor for OOP services that communicate via MessagePack
+    /// A wrapper for a solution that can be used by Razor for OOP services that communicate via MessagePack, or in proc services that don't communicate.
     /// </summary>
     [DataContract]
     internal readonly struct RazorPinnedSolutionInfoWrapper
@@ -15,10 +16,22 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
         [DataMember(Order = 0)]
         internal readonly Checksum UnderlyingObject;
 
-        public RazorPinnedSolutionInfoWrapper(Checksum underlyingObject)
-            => UnderlyingObject = underlyingObject;
+        // Not serialized because it should only be used in in-proc scenarios.
+        internal readonly Solution? Solution;
 
-        public static implicit operator RazorPinnedSolutionInfoWrapper(Checksum info)
-            => new(info);
+        internal RazorPinnedSolutionInfoWrapper(Checksum checksum, Solution? solution)
+        {
+            Contract.ThrowIfTrue(checksum == Checksum.Null && solution is null, "Either a Checksum or a Solution must be provided.");
+            Contract.ThrowIfTrue(checksum != Checksum.Null && solution is not null, "Only one of Checksum or Solution can be provided.");
+
+            UnderlyingObject = checksum;
+            Solution = solution;
+        }
+
+        public static implicit operator RazorPinnedSolutionInfoWrapper(Checksum checksum)
+            => new(checksum, null);
+
+        public static implicit operator RazorPinnedSolutionInfoWrapper(Solution solution)
+            => new(Checksum.Null, solution);
     }
 }
