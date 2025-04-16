b134cfc58e55e21387cbe1e89243b90de349b45e
David Wengier <david.wengier@microsoft.com>
2025-04-16 13:39:34 +1000

    Fix deserialization when calling OOP

--- src/Tools/ExternalAccess/Razor/Features/Remote/RazorPinnedSolutionInfoWrapper.cs
+++ src/Tools/ExternalAccess/Razor/Features/Remote/RazorPinnedSolutionInfoWrapper.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Runtime.Serialization;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
@@ -18,6 +17,12 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.Razor
 
         // Not serialized because it should only be used in in-proc scenarios.
         internal readonly Solution? Solution;
+
+        // Needed for the message pack formatter to work
+        internal RazorPinnedSolutionInfoWrapper(Checksum checksum)
+            : this(checksum, null)
+        {
+        }
 
         internal RazorPinnedSolutionInfoWrapper(Checksum checksum, Solution? solution)
         {
