9d8382e11ed9d239056c49b18907ca84bd801964
michael <michael.baumanns@gmail.com>
2025-04-21 14:09:59 +0200

    handle extension in interface

--- src/Compilers/CSharp/Portable/Symbols/VarianceSafety.cs
+++ src/Compilers/CSharp/Portable/Symbols/VarianceSafety.cs
@@ -76,6 +76,9 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 case TypeKind.Struct:
                 case TypeKind.Enum:
                     break;
+                case TypeKind.Extension:
+                    //this is always an error case - we do not want to emit further variance diagnostics for this it
+                    return;
                 case TypeKind.Interface:
                 case TypeKind.Delegate:
                     return;
