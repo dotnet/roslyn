1a6136ee33930bc26abf0c5420619adc7a3d6049
tmat <tomas.matousek@microsoft.com>
2025-04-30 10:07:18 -0700

    Fix

--- src/VisualStudio/CSharp/Test/DesignerAttribute/DesignerAttributeServiceTests.cs
+++ src/VisualStudio/CSharp/Test/DesignerAttribute/DesignerAttributeServiceTests.cs
@@ -124,7 +124,7 @@ public sealed class DesignerAttributeServiceTests
         var compilation = await document.Project.GetRequiredCompilationAsync(CancellationToken.None);
         var actual = await DesignerAttributeDiscoveryService.ComputeDesignerAttributeCategoryAsync(
             compilation.DesignerCategoryAttributeType() != null, document.Project, document.Id, existingCategory, CancellationToken.None);
-        
+
         Assert.Equal(category, actual);
     }
 }
