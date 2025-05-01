6ac68c6f43109d00a5e1f690e7a21f1b18d8aa5f
Jason Malinowski <jason.malinowski@microsoft.com>
2025-05-01 15:51:39 -0700

    Call the same test accessor helper used everywhere else

--- src/LanguageServer/ProtocolUnitTests/Miscellaneous/LspMiscellaneousFilesWorkspaceTests.cs
+++ src/LanguageServer/ProtocolUnitTests/Miscellaneous/LspMiscellaneousFilesWorkspaceTests.cs
@@ -56,7 +56,7 @@ public sealed class LspMiscellaneousFilesWorkspaceTests : AbstractLanguageServer
         // Create a server that supports LSP misc files and verify no misc files present.
         await using var testLspServer = await CreateTestLspServerAsync(string.Empty, mutatingLspWorkspace, new InitializationOptions { ServerKind = WellKnownLspServerKinds.CSharpVisualBasicLspServer });
 
-        var miscWorkspace = (Workspace)testLspServer.GetRequiredLspService<ILspMiscellaneousFilesWorkspace>();
+        var miscWorkspace = testLspServer.GetManager().GetTestAccessor().GetLspMiscellaneousFilesWorkspace();
         testLspServer.TestWorkspace.GetService<LspWorkspaceRegistrationService>().Register(miscWorkspace);
 
         Assert.Null(GetMiscellaneousDocument(testLspServer));
