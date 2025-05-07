b12bd0bdb9fa763eca9f13984c35425e96b04cde
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-05-07 15:05:09 -0700

    fsn

--- src/VisualStudio/ExternalAccess/FSharp/Internal/Diagnostics/FSharpDiagnosticAnalyzerService.cs
+++ src/VisualStudio/ExternalAccess/FSharp/Internal/Diagnostics/FSharpDiagnosticAnalyzerService.cs
@@ -10,22 +10,21 @@ using System.Composition;
 using Microsoft.CodeAnalysis.ExternalAccess.FSharp.Diagnostics;
 using Microsoft.CodeAnalysis.Host.Mef;
 
-namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Internal.Diagnostics
+namespace Microsoft.CodeAnalysis.ExternalAccess.FSharp.Internal.Diagnostics;
+
+[Shared]
+[Export(typeof(IFSharpDiagnosticAnalyzerService))]
+internal class FSharpDiagnosticAnalyzerService : IFSharpDiagnosticAnalyzerService
 {
-    [Shared]
-    [Export(typeof(IFSharpDiagnosticAnalyzerService))]
-    internal class FSharpDiagnosticAnalyzerService : IFSharpDiagnosticAnalyzerService
+    private readonly Microsoft.CodeAnalysis.Diagnostics.IDiagnosticAnalyzerService _delegatee;
+
+    [ImportingConstructor]
+    [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+    public FSharpDiagnosticAnalyzerService(Microsoft.CodeAnalysis.Diagnostics.IDiagnosticAnalyzerService delegatee)
     {
-        private readonly Microsoft.CodeAnalysis.Diagnostics.IDiagnosticAnalyzerService _delegatee;
-
-        [ImportingConstructor]
-        [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-        public FSharpDiagnosticAnalyzerService(Microsoft.CodeAnalysis.Diagnostics.IDiagnosticAnalyzerService delegatee)
-        {
-            _delegatee = delegatee;
-        }
-
-        public void Reanalyze(Workspace workspace, IEnumerable<ProjectId> projectIds = null, IEnumerable<DocumentId> documentIds = null, bool highPriority = false)
-            => _delegatee.RequestDiagnosticRefresh();
+        _delegatee = delegatee;
     }
+
+    public void Reanalyze(Workspace workspace, IEnumerable<ProjectId> projectIds = null, IEnumerable<DocumentId> documentIds = null, bool highPriority = false)
+        => _delegatee.RequestDiagnosticRefresh();
 }
