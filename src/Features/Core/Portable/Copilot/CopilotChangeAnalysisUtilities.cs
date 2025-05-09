b1ed09490089a74cdcf92ef487502538e5edf437
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-05-09 13:52:16 -0700

    lint

--- src/Features/Core/Portable/Copilot/CopilotChangeAnalysisUtilities.cs
+++ src/Features/Core/Portable/Copilot/CopilotChangeAnalysisUtilities.cs
@@ -5,7 +5,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.ComponentModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
