6210f8484fdb2a8e9bb966a257824c268c2fb7c9
Tomas Matousek <tomat@microsoft.com>
2025-05-14 14:21:52 -0700

    Fix header

--- src/Dependencies/Collections/Internal/SR.cs
+++ src/Dependencies/Collections/Internal/SR.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.CodeAnalysis.Collections.Internal;
+﻿﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.CodeAnalysis.Collections.Internal;
 
 internal static class SR
 {
