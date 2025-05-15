6ca368ca074b7ce501c5c7d29622fff42e65cf5f
Matteo Prosperi <41970398+matteo-prosperi@users.noreply.github.com>
2025-05-15 15:12:31 -0700

    Rename LSP messages for VisualStudio.Extensibility integration (#78598)
    Co-authored-by: Matteo Prosperi <maprospe@microsoft.com>

--- src/LanguageServer/Protocol/Handler/Extensions/DispatchExtensionMessageResponse.cs
+++ src/LanguageServer/Protocol/Handler/Extensions/DispatchExtensionMessageResponse.cs
@@ -8,12 +8,12 @@ using System.Text.Json.Serialization;
 namespace Microsoft.CodeAnalysis.LanguageServer.Handler.Extensions;
 
 /// <summary>
-/// Return type for the roslyn/extensionWorkspaceMessage and roslyn/extensionDocumentMessage request.
+/// Return type for the workspace/_vs_dispatchExtensionMessage and textDocument/_vs_dipatchExtensionMessage request.
 /// </summary>
 /// <param name="Response">Json response returned by the extension message handler. Can be <see langword="null"/> if the
 /// extension was unloaded concurrently with the response being issued, or if the extension threw an exception while
 /// processing.</param>
-internal readonly record struct ExtensionMessageResponse(
+internal readonly record struct DispatchExtensionMessageResponse(
     [property: JsonPropertyName("response"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] string? Response,
     [property: JsonPropertyName("extensionWasUnloaded"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)] bool ExtensionWasUnloaded,
     [property: JsonPropertyName("extensionException"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] Exception? ExtensionException);
