4ccadc422017ca66fddd554115dad6937acd6794
Gael Fraiteur <gael@postsharp.net>
2025-05-17 22:02:09 +0200

    Publish fewer private artefacts.

--- eng-Metalama/src/Program.cs
+++ eng-Metalama/src/Program.cs
@@ -16,21 +16,6 @@ var product = new Product(MetalamaDependencies.MetalamaCompiler)
     AdditionalDirectoriesToClean = ["artifacts"],
     Solutions = [new RoslynSolution()],
     PublicArtifacts = Pattern.Create("Metalama.Compiler.$(PackageVersion).nupkg", "Metalama.Compiler.Sdk.$(PackageVersion).nupkg"),
-    PrivateArtifacts = Pattern.Create(
-    "Metalama.Roslyn.CodeAnalysis.Common.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.CSharp.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.CSharp.Features.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.CSharp.Scripting.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.CSharp.Workspaces.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.Features.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.Scripting.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.Scripting.Common.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.Workspaces.Common.$(PackageVersion).nupkg",
-
-    // Visual Basic is needed by Metalama.Try
-    "Metalama.Roslyn.CodeAnalysis.VisualBasic.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.VisualBasic.Features.$(PackageVersion).nupkg",
-    "Metalama.Roslyn.CodeAnalysis.VisualBasic.Workspaces.$(PackageVersion).nupkg"),
     Dependencies = [DevelopmentDependencies.PostSharpEngineering],
     SupportedProperties = new() { ["TestAll"] = "Supported by the 'test' command. Run all tests instead of just Metalama's unit tests." },
     ExportedProperties = { { @"eng\Versions.props", new[] { "RoslynVersion" } } },
@@ -39,7 +24,6 @@ var product = new Product(MetalamaDependencies.MetalamaCompiler)
     DefaultTestsFilter = "Category!=OuterLoop"
 };
 
-product.BuildCompleted += OnBuildCompleted;
 
 var app = new EngineeringApp(product);
 
@@ -51,24 +35,3 @@ app.Configure(delegate (IConfigurator root)
 });
 
 return app.Run( args );
-
-static void OnBuildCompleted( BuildCompletedEventArgs args )
-{
-    // Rename the packages as a post-build step.
-    args.Context.Console.WriteHeading( "Renaming packages" );
-
-    var success = RenamePackagesCommand.Execute( args.Context.Console, new RenamePackageCommandSettings { Directory = args.PrivateArtifactsDirectory } );
-
-    if ( success )
-    {
-        // Delete original packages (those non-renamed) so they don't get uploaded.
-        foreach ( var file in Directory.GetFiles( args.PrivateArtifactsDirectory, "Microsoft.*.nupkg" ) )
-        {
-            File.Delete( file );
-        }
-
-        args.Context.Console.WriteSuccess( "Renaming packages was successful." );
-    }
-
-    args.IsFailed = !success;
-}
