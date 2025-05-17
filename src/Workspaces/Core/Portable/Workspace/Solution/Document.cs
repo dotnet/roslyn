a3c94561b37e62d71275ab25110860e636791201
Gael Fraiteur <gael@postsharp.net>
2025-05-17 13:24:32 +0200

    Fixing more build issues.

--- src/Workspaces/Core/Portable/Workspace/Solution/Document.cs
+++ src/Workspaces/Core/Portable/Workspace/Solution/Document.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Diagnostics;
