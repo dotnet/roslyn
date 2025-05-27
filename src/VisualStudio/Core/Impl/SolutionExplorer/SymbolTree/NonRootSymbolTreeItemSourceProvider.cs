a81eb060b5f5042bc2c8762fcaddcb5e00d9bed7
Cyrus Najmabadi <cyrusn@microsoft.com>
2025-05-27 12:44:09 +0200

    Simplify

--- src/VisualStudio/Core/Impl/SolutionExplorer/SymbolTree/NonRootSymbolTreeItemSourceProvider.cs
+++ src/VisualStudio/Core/Impl/SolutionExplorer/SymbolTree/NonRootSymbolTreeItemSourceProvider.cs
@@ -2,27 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Diagnostics.CodeAnalysis;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.CodeAnalysis.Shared.TestHooks;
-using Microsoft.CodeAnalysis.Threading;
 using Microsoft.Internal.VisualStudio.PlatformUI;
-using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Utilities;
-using Roslyn.Utilities;
-using Microsoft.VisualStudio.LanguageServices.Extensions;
-using Microsoft.CodeAnalysis.Host;
-using Microsoft.CodeAnalysis.Shared.Extensions;
-using Microsoft.CodeAnalysis.ErrorReporting;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplorer;
 
@@ -30,21 +16,14 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplore
 [Name(nameof(NonRootSymbolTreeItemSourceProvider))]
 [Order(Before = HierarchyItemsProviderNames.Contains)]
 [AppliesToProject("CSharp | VB")]
-internal sealed class NonRootSymbolTreeItemSourceProvider : AbstractSymbolTreeItemSourceProvider<SymbolTreeItem>
+[method: SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
+[method: ImportingConstructor]
+internal sealed class NonRootSymbolTreeItemSourceProvider(
+    IThreadingContext threadingContext,
+    VisualStudioWorkspace workspace,
+    IAsynchronousOperationListenerProvider listenerProvider)
+    : AbstractSymbolTreeItemSourceProvider<SymbolTreeItem>(threadingContext, workspace, listenerProvider)
 {
-    [SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
-    [ImportingConstructor]
-    public NonRootSymbolTreeItemSourceProvider(
-        IThreadingContext threadingContext,
-        VisualStudioWorkspace workspace,
-        IAsynchronousOperationListenerProvider listenerProvider
-    /*,
-[Import(typeof(AnalyzersCommandHandler))] IAnalyzersCommandHandler commandHandler*/)
-        : base(threadingContext, workspace, listenerProvider)
-    {
- 
-    }
-
     protected override IAttachedCollectionSource? CreateCollectionSource(SymbolTreeItem item, string relationshipName)
     {
         if (relationshipName != KnownRelationships.Contains)
@@ -54,19 +33,4 @@ internal sealed class NonRootSymbolTreeItemSourceProvider : AbstractSymbolTreeIt
         // and can be queried directly for them.
         return item;
     }
-
-    //private sealed class NonRootSymbolTreeItemCollectionSource(
-    //    RootSymbolTreeItemSourceProvider rootProvider,
-    //    SymbolTreeItem parentItem)
-    //    : AbstractSymbolTreeItemCollectionSource<SymbolTreeItem>(
-    //        rootProvider, parentItem), ISupportExpansionEvents
-    //{
-    //    private int _expanded = 0;
-
-
-    //    public void AfterCollapse()
-    //    {
-    //        // No op
-    //    }
-    //}
 }
