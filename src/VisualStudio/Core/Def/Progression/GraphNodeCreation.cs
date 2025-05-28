0d8c514083b438320fefefe5a6bd5a64d864dbc6
Cyrus Najmabadi <cyrusn@microsoft.com>
2025-05-28 21:02:54 +0200

    REstore api

--- src/VisualStudio/Core/Def/Progression/GraphNodeCreation.cs
+++ src/VisualStudio/Core/Def/Progression/GraphNodeCreation.cs
@@ -2,11 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.VisualStudio.GraphModel;
+
+#pragma warning disable IDE0060 // Remove unused parameter
 
 namespace Microsoft.VisualStudio.LanguageServices.Progression;
 
@@ -16,12 +17,8 @@ namespace Microsoft.VisualStudio.LanguageServices.Progression;
 public static class GraphNodeCreation
 {
     public static Task<GraphNodeId> CreateNodeIdAsync(ISymbol symbol, Solution solution, CancellationToken cancellationToken)
-    {
-        throw new NotSupportedException();
-    }
+        => Task.FromResult(GraphNodeId.Empty);
 
     public static Task<GraphNode> CreateNodeAsync(this Graph graph, ISymbol symbol, Solution solution, CancellationToken cancellationToken)
-    {
-        throw new NotSupportedException();
-    }
+        => Task.FromResult(graph.Nodes.GetOrCreate(GraphNodeId.Empty));
 }
