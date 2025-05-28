7e62b62697a59171ef0fce7c55b3567edba61392
Cyrus Najmabadi <cyrusn@microsoft.com>
2025-05-28 18:12:10 +0200

    Revert

--- src/VisualStudio/Core/Impl/SolutionExplorer/SourceGeneratedFileItems/NoSourceGeneratedFilesPlaceholderItem.cs
+++ src/VisualStudio/Core/Impl/SolutionExplorer/SourceGeneratedFileItems/NoSourceGeneratedFilesPlaceholderItem.cs
@@ -7,7 +7,8 @@ using Microsoft.VisualStudio.Imaging.Interop;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.SolutionExplorer;
 
-internal sealed class NoSourceGeneratedFilesPlaceholderItem() : BaseItem(SolutionExplorerShim.This_generator_is_not_generating_files)
+internal sealed class NoSourceGeneratedFilesPlaceholderItem()
+    : BaseItem(SolutionExplorerShim.This_generator_is_not_generating_files)
 {
     public override ImageMoniker IconMoniker
         => KnownMonikers.StatusInformationNoColor;
