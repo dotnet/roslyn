20036bb8e82cc89735019d6966c58eccc6d90a63
Cyrus Najmabadi <cyrusn@microsoft.com>
2025-05-28 15:18:13 +0200

    Avoid boxing

--- src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Extensions/StringBuilderExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Workspace/Core/Extensions/StringBuilderExtensions.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Text;
 
@@ -18,15 +17,32 @@ internal static class StringBuilderExtensions
         return result;
     }
 
-    public static StringBuilder AppendJoinedValues<T>(this StringBuilder builder, string separator, IEnumerable<T> values, Action<T, StringBuilder> append)
+    public static StringBuilder AppendJoinedValues<TNode>(
+        this StringBuilder builder, string separator, SyntaxList<TNode> values, Action<TNode, StringBuilder> append)
+        where TNode : SyntaxNode
     {
         var first = true;
         foreach (var value in values)
         {
             if (!first)
-            {
                 builder.Append(separator);
-            }
+
+            first = false;
+            append(value, builder);
+        }
+
+        return builder;
+    }
+
+    public static StringBuilder AppendJoinedValues<TNode>(
+        this StringBuilder builder, string separator, SeparatedSyntaxList<TNode> values, Action<TNode, StringBuilder> append)
+        where TNode : SyntaxNode
+    {
+        var first = true;
+        foreach (var value in values)
+        {
+            if (!first)
+                builder.Append(separator);
 
             first = false;
             append(value, builder);
@@ -41,9 +57,7 @@ internal static class StringBuilderExtensions
         foreach (var value in values)
         {
             if (!first)
-            {
                 builder.Append(separator);
-            }
 
             first = false;
             append(value, builder);
