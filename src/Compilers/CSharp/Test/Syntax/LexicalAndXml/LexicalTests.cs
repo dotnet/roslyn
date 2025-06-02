6b92eb88ca6b17be3abdf74d5e5ef5e76a0c8b25
Cyrus Najmabadi <cyrusn@microsoft.com>
2025-06-02 09:06:26 -0700

    test docs

--- src/Compilers/CSharp/Test/Syntax/LexicalAndXml/LexicalTests.cs
+++ src/Compilers/CSharp/Test/Syntax/LexicalAndXml/LexicalTests.cs
@@ -4576,19 +4576,21 @@ class C
         [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/78593")]
         public void TestDotPrefixedNumberStartingAtStartOfSlidingTextWindow()
         {
-            // This is the entire test.  We want to ensure this tree passes invariants.
             // It specifically validates what happens when we see `.0` at the start of the
             // sliding text window, where the lexer tries to peek back one char to see if this
             // is actually `..0` (a range expr) or `.0` (a floating point number).
-            CSharpSyntaxTree.ParseText(Resources.DotPrefixedNumberStartingAtStartOfSlidingTextWindow);
 
-            var lexer = new Lexer(
-                SourceText.From(Resources.DotPrefixedNumberStartingAtStartOfSlidingTextWindow),
-                CSharpParseOptions.Default);
+            // Do a full parse of the file that previous had broken parsing.  Running ParseText
+            // will ensure this tree passes invariants.  
+            CSharpSyntaxTree.ParseText(Resources.DotPrefixedNumberStartingAtStartOfSlidingTextWindow);
 
             // Now, replicate the same conditions that hte parser runs through by driving the a new lexer here
             // directly.  That ensures that we are actually validating exactly the conditions that led to the bug
             // (a dot token starting a number, right at the start of the character window).
+            var lexer = new Lexer(
+                SourceText.From(Resources.DotPrefixedNumberStartingAtStartOfSlidingTextWindow),
+                CSharpParseOptions.Default);
+
             var mode = LexerMode.Syntax;
             for (var i = 0; i < 1327; i++)
             {
