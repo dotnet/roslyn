4e84a1e46fc360772b5af3c1dc0ae30b201c3767
Cyrus Najmabadi <cyrusn@microsoft.com>
2025-06-05 11:08:47 -0700

    Restore some parts of the progression api some legacy components (like codemap) use.

---
+++ src/VisualStudio/Core/Def/Progression/IProgressionLanguageService.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host;
+
+namespace Microsoft.VisualStudio.LanguageServices.Implementation.Progression;
+
+internal interface IProgressionLanguageService : ILanguageService
+{
+    IEnumerable<SyntaxNode> GetTopLevelNodesFromDocument(SyntaxNode root, CancellationToken cancellationToken);
+    string GetDescriptionForSymbol(ISymbol symbol, bool includeContainingSymbol);
+    string GetLabelForSymbol(ISymbol symbol, bool includeContainingSymbol);
+}
