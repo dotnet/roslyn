949909fab24d81a930865701cea5a6d6bb4310fd
Julien Couvreur <julien.couvreur@gmail.com>
2025-06-04 19:35:32 -0700

    Remove TODO2

--- src/Compilers/CSharp/Portable/Binder/Binder_Query.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Query.cs
@@ -676,7 +676,7 @@ namespace Microsoft.CodeAnalysis.CSharp
 
         private static BoundExpression? ExtractCastInvocation(BoundCall invocation)
         {
-            int index = invocation.InvokedAsExtensionMethod ? 1 : 0; // Tracked by https://github.com/dotnet/roslyn/issues/76130: Add test coverage for his code path TODO2
+            int index = invocation.InvokedAsExtensionMethod ? 1 : 0; // Tracked by https://github.com/dotnet/roslyn/issues/76130: Add test coverage for his code path
             var c1 = invocation.Arguments[index] as BoundConversion;
             var l1 = c1 != null ? c1.Operand as BoundLambda : null;
             var r1 = l1 != null ? l1.Body.Statements[0] as BoundReturnStatement : null;
