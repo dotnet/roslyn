c4062db51f91747c6a3885a61ef9b48b37604344
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-06-09 14:22:15 -0700

    Simplify

--- src/Features/Core/Portable/ExternalAccess/VSTypeScript/VSTypeScriptClassificationService.cs
+++ src/Features/Core/Portable/ExternalAccess/VSTypeScript/VSTypeScriptClassificationService.cs
@@ -18,18 +18,11 @@ using Microsoft.CodeAnalysis.Text;
 namespace Microsoft.CodeAnalysis.ExternalAccess.VSTypeScript;
 
 [ExportLanguageService(typeof(IClassificationService), InternalLanguageNames.TypeScript), Shared]
-internal sealed class VSTypeScriptClassificationService : IClassificationService
+[method: ImportingConstructor]
+[method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+internal sealed class VSTypeScriptClassificationService(
+    [Import(AllowDefault = true)] IVSTypeScriptClassificationService? classificationService) : IClassificationService
 {
-    private readonly IVSTypeScriptClassificationService? _classificationService;
-
-    [ImportingConstructor]
-    [Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
-    public VSTypeScriptClassificationService(
-        [Import(AllowDefault = true)] IVSTypeScriptClassificationService? classificationService)
-    {
-        _classificationService = classificationService;
-    }
-
     public void AddLexicalClassifications(SourceText text, TextSpan textSpan, SegmentedList<ClassifiedSpan> result, CancellationToken cancellationToken)
     {
     }
@@ -39,38 +32,28 @@ internal sealed class VSTypeScriptClassificationService : IClassificationService
     }
 
     public Task AddSyntacticClassificationsAsync(Document document, ImmutableArray<TextSpan> textSpans, SegmentedList<ClassifiedSpan> result, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
+        => Task.CompletedTask;
 
     public Task AddEmbeddedLanguageClassificationsAsync(Document document, ImmutableArray<TextSpan> textSpans, ClassificationOptions options, SegmentedList<ClassifiedSpan> result, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
+        => Task.CompletedTask;
 
     public ClassifiedSpan AdjustStaleClassification(SourceText text, ClassifiedSpan classifiedSpan)
-    {
-        return classifiedSpan;
-    }
+        => classifiedSpan;
 
     public ValueTask<TextChangeRange?> ComputeSyntacticChangeRangeAsync(Document oldDocument, Document newDocument, TimeSpan timeout, CancellationToken cancellationToken)
-    {
-        return default;
-    }
+        => default;
 
     public TextChangeRange? ComputeSyntacticChangeRange(SolutionServices workspace, SyntaxNode oldRoot, SyntaxNode newRoot, TimeSpan timeout, CancellationToken cancellationToken)
-    {
-        return null;
-    }
+        => null;
 
     public async Task AddSemanticClassificationsAsync(Document document, ImmutableArray<TextSpan> textSpans, ClassificationOptions options, SegmentedList<ClassifiedSpan> result, CancellationToken cancellationToken)
     {
-        if (_classificationService is null)
+        if (classificationService is null)
             return;
 
         using var _ = SharedPools.BigDefault<List<ClassifiedSpan>>().GetPooledObject(out var list);
 
-        await _classificationService.AddSemanticClassificationsAsync(document, textSpans, list, cancellationToken).ConfigureAwait(false);
+        await classificationService.AddSemanticClassificationsAsync(document, textSpans, list, cancellationToken).ConfigureAwait(false);
 
         foreach (var classifiedSpan in list)
             result.Add(classifiedSpan);
