abe6596bf956c8397f19ae66b84090d319d2ee87
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-06-17 09:20:48 -0700

    Move roslyn-analyzers code over to using shared projects

--- src/RoslynAnalyzers/Utilities/Compiler/IsExternalInit.cs
+++ src/RoslynAnalyzers/Utilities/Compiler/IsExternalInit.cs
@@ -1,20 +1,20 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+﻿//// Licensed to the .NET Foundation under one or more agreements.
+//// The .NET Foundation licenses this file to you under the MIT license.
+//// See the LICENSE file in the project root for more information.
 
-// Copied from:
-// https://github.com/dotnet/runtime/blob/218ef0f7776c2c20f6c594e3475b80f1fe2d7d08/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/IsExternalInit.cs
+//// Copied from:
+//// https://github.com/dotnet/runtime/blob/218ef0f7776c2c20f6c594e3475b80f1fe2d7d08/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/IsExternalInit.cs
 
-using System.ComponentModel;
+//using System.ComponentModel;
 
-namespace System.Runtime.CompilerServices
-{
-    /// <summary>
-    /// Reserved to be used by the compiler for tracking metadata.
-    /// This class should not be used by developers in source code.
-    /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    internal static class IsExternalInit
-    {
-    }
-}
+//namespace System.Runtime.CompilerServices
+//{
+//    /// <summary>
+//    /// Reserved to be used by the compiler for tracking metadata.
+//    /// This class should not be used by developers in source code.
+//    /// </summary>
+//    [EditorBrowsable(EditorBrowsableState.Never)]
+//    internal static class IsExternalInit
+//    {
+//    }
+//}
--- src/RoslynAnalyzers/Utilities/Compiler/Range.cs
+++ src/RoslynAnalyzers/Utilities/Compiler/Range.cs
@@ -1,107 +1,107 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+﻿//// Licensed to the .NET Foundation under one or more agreements.
+//// The .NET Foundation licenses this file to you under the MIT license.
+//// See the LICENSE file in the project root for more information.
 
-#if !NETCOREAPP
+//#if !NETCOREAPP
 
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using Analyzer.Utilities;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Runtime.CompilerServices;
+//using Analyzer.Utilities;
 
-namespace System
-{
-    /// <summary>Represent a range has start and end indexes.</summary>
-    /// <remarks>
-    /// Range is used by the C# compiler to support the range syntax.
-    /// <code>
-    /// int[] someArray = new int[5] { 1, 2, 3, 4, 5 };
-    /// int[] subArray1 = someArray[0..2]; // { 1, 2 }
-    /// int[] subArray2 = someArray[1..^0]; // { 2, 3, 4, 5 }
-    /// </code>
-    /// </remarks>
-    [SuppressMessage("Performance", "CA1815:Override equals and operator equals on value types", Justification = "Matches implementation from dotnet/runtime")]
-    [SuppressMessage("Usage", "CA2231:Overload operator equals on overriding value type Equals", Justification = "Matches implementation from dotnet/runtime")]
-    internal readonly struct Range : IEquatable<Range>
-    {
-        /// <summary>Represent the inclusive start index of the Range.</summary>
-        public Index Start { get; }
+//namespace System
+//{
+//    /// <summary>Represent a range has start and end indexes.</summary>
+//    /// <remarks>
+//    /// Range is used by the C# compiler to support the range syntax.
+//    /// <code>
+//    /// int[] someArray = new int[5] { 1, 2, 3, 4, 5 };
+//    /// int[] subArray1 = someArray[0..2]; // { 1, 2 }
+//    /// int[] subArray2 = someArray[1..^0]; // { 2, 3, 4, 5 }
+//    /// </code>
+//    /// </remarks>
+//    [SuppressMessage("Performance", "CA1815:Override equals and operator equals on value types", Justification = "Matches implementation from dotnet/runtime")]
+//    [SuppressMessage("Usage", "CA2231:Overload operator equals on overriding value type Equals", Justification = "Matches implementation from dotnet/runtime")]
+//    internal readonly struct Range : IEquatable<Range>
+//    {
+//        /// <summary>Represent the inclusive start index of the Range.</summary>
+//        public Index Start { get; }
 
-        /// <summary>Represent the exclusive end index of the Range.</summary>
-        public Index End { get; }
+//        /// <summary>Represent the exclusive end index of the Range.</summary>
+//        public Index End { get; }
 
-        /// <summary>Construct a Range object using the start and end indexes.</summary>
-        /// <param name="start">Represent the inclusive start index of the range.</param>
-        /// <param name="end">Represent the exclusive end index of the range.</param>
-        public Range(Index start, Index end)
-        {
-            Start = start;
-            End = end;
-        }
+//        /// <summary>Construct a Range object using the start and end indexes.</summary>
+//        /// <param name="start">Represent the inclusive start index of the range.</param>
+//        /// <param name="end">Represent the exclusive end index of the range.</param>
+//        public Range(Index start, Index end)
+//        {
+//            Start = start;
+//            End = end;
+//        }
 
-        /// <summary>Indicates whether the current Range object is equal to another object of the same type.</summary>
-        /// <param name="obj">An object to compare with this object</param>
-        public override bool Equals(object? obj) =>
-            obj is Range r &&
-            r.Start.Equals(Start) &&
-            r.End.Equals(End);
+//        /// <summary>Indicates whether the current Range object is equal to another object of the same type.</summary>
+//        /// <param name="obj">An object to compare with this object</param>
+//        public override bool Equals(object? obj) =>
+//            obj is Range r &&
+//            r.Start.Equals(Start) &&
+//            r.End.Equals(End);
 
-        /// <summary>Indicates whether the current Range object is equal to another Range object.</summary>
-        /// <param name="other">An object to compare with this object</param>
-        public bool Equals(Range other) => other.Start.Equals(Start) && other.End.Equals(End);
+//        /// <summary>Indicates whether the current Range object is equal to another Range object.</summary>
+//        /// <param name="other">An object to compare with this object</param>
+//        public bool Equals(Range other) => other.Start.Equals(Start) && other.End.Equals(End);
 
-        /// <summary>Returns the hash code for this instance.</summary>
-        public override int GetHashCode()
-        {
-            return RoslynHashCode.Combine(Start.GetHashCode(), End.GetHashCode());
-        }
+//        /// <summary>Returns the hash code for this instance.</summary>
+//        public override int GetHashCode()
+//        {
+//            return RoslynHashCode.Combine(Start.GetHashCode(), End.GetHashCode());
+//        }
 
-        /// <summary>Converts the value of the current Range object to its equivalent string representation.</summary>
-        public override string ToString()
-        {
-            return Start.ToString() + ".." + End.ToString();
-        }
+//        /// <summary>Converts the value of the current Range object to its equivalent string representation.</summary>
+//        public override string ToString()
+//        {
+//            return Start.ToString() + ".." + End.ToString();
+//        }
 
-        /// <summary>Create a Range object starting from start index to the end of the collection.</summary>
-        public static Range StartAt(Index start) => new(start, Index.End);
+//        /// <summary>Create a Range object starting from start index to the end of the collection.</summary>
+//        public static Range StartAt(Index start) => new(start, Index.End);
 
-        /// <summary>Create a Range object starting from first element in the collection to the end Index.</summary>
-        public static Range EndAt(Index end) => new(Index.Start, end);
+//        /// <summary>Create a Range object starting from first element in the collection to the end Index.</summary>
+//        public static Range EndAt(Index end) => new(Index.Start, end);
 
-        /// <summary>Create a Range object starting from first element to the end.</summary>
-        public static Range All => new(Index.Start, Index.End);
+//        /// <summary>Create a Range object starting from first element to the end.</summary>
+//        public static Range All => new(Index.Start, Index.End);
 
-        /// <summary>Calculate the start offset and length of range object using a collection length.</summary>
-        /// <param name="length">The length of the collection that the range will be used with. length has to be a positive value.</param>
-        /// <remarks>
-        /// For performance reason, we don't validate the input length parameter against negative values.
-        /// It is expected Range will be used with collections which always have non negative length/count.
-        /// We validate the range is inside the length scope though.
-        /// </remarks>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public (int Offset, int Length) GetOffsetAndLength(int length)
-        {
-            int start;
-            Index startIndex = Start;
-            if (startIndex.IsFromEnd)
-                start = length - startIndex.Value;
-            else
-                start = startIndex.Value;
+//        /// <summary>Calculate the start offset and length of range object using a collection length.</summary>
+//        /// <param name="length">The length of the collection that the range will be used with. length has to be a positive value.</param>
+//        /// <remarks>
+//        /// For performance reason, we don't validate the input length parameter against negative values.
+//        /// It is expected Range will be used with collections which always have non negative length/count.
+//        /// We validate the range is inside the length scope though.
+//        /// </remarks>
+//        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+//        public (int Offset, int Length) GetOffsetAndLength(int length)
+//        {
+//            int start;
+//            Index startIndex = Start;
+//            if (startIndex.IsFromEnd)
+//                start = length - startIndex.Value;
+//            else
+//                start = startIndex.Value;
 
-            int end;
-            Index endIndex = End;
-            if (endIndex.IsFromEnd)
-                end = length - endIndex.Value;
-            else
-                end = endIndex.Value;
+//            int end;
+//            Index endIndex = End;
+//            if (endIndex.IsFromEnd)
+//                end = length - endIndex.Value;
+//            else
+//                end = endIndex.Value;
 
-            if ((uint)end > (uint)length || (uint)start > (uint)end)
-            {
-                throw new ArgumentOutOfRangeException(nameof(length));
-            }
+//            if ((uint)end > (uint)length || (uint)start > (uint)end)
+//            {
+//                throw new ArgumentOutOfRangeException(nameof(length));
+//            }
 
-            return (start, end - start);
-        }
-    }
-}
+//            return (start, end - start);
+//        }
+//    }
+//}
 
-#endif
+//#endif
