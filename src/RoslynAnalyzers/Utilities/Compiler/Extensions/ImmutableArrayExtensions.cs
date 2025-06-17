400b21f15c222240232613ba63f6ac7f93d2a17d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-06-17 16:18:42 -0700

    Fix

--- src/RoslynAnalyzers/Utilities/Compiler/Extensions/ImmutableArrayExtensions.cs
+++ src/RoslynAnalyzers/Utilities/Compiler/Extensions/ImmutableArrayExtensions.cs
@@ -41,26 +41,26 @@ namespace System.Collections.Immutable
         /// <returns><see langword="true" /> the source sequence contains less then <paramref name="count"/> elements; otherwise, <see langword="false" />.</returns>
         public static bool HasFewerThan<TSource>(this ImmutableArray<TSource> source, int count) => source.Length < count;
 
-        /// <summary>
-        /// Determines whether a sequence contains any elements.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of array.</typeparam>
-        /// <typeparam name="TArg">The type of arg.</typeparam>
-        /// <param name="array">The <see cref="Immutable.ImmutableArray{T}"/> whose elements to apply the predicate to.</param>
-        /// <param name="predicate">A function to test each element for a condition.</param>
-        /// <param name="arg">The argument to pass into the predicate.</param>
-        /// <returns> true if any elements in the source sequence pass the test in the specified predicate otherwise, false.</returns>
-        public static bool Any<T, TArg>(this ImmutableArray<T> array, Func<T, TArg, bool> predicate, TArg arg)
-        {
-            foreach (var a in array)
-            {
-                if (predicate(a, arg))
-                {
-                    return true;
-                }
-            }
+        ///// <summary>
+        ///// Determines whether a sequence contains any elements.
+        ///// </summary>
+        ///// <typeparam name="T">The type of the elements of array.</typeparam>
+        ///// <typeparam name="TArg">The type of arg.</typeparam>
+        ///// <param name="array">The <see cref="Immutable.ImmutableArray{T}"/> whose elements to apply the predicate to.</param>
+        ///// <param name="predicate">A function to test each element for a condition.</param>
+        ///// <param name="arg">The argument to pass into the predicate.</param>
+        ///// <returns> true if any elements in the source sequence pass the test in the specified predicate otherwise, false.</returns>
+        //public static bool Any<T, TArg>(this ImmutableArray<T> array, Func<T, TArg, bool> predicate, TArg arg)
+        //{
+        //    foreach (var a in array)
+        //    {
+        //        if (predicate(a, arg))
+        //        {
+        //            return true;
+        //        }
+        //    }
 
-            return false;
-        }
+        //    return false;
+        //}
     }
 }
