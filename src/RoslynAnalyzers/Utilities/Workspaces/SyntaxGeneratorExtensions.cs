8f3ca2e7963cff67eff4df5e1280776a8fa48ff5
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-06-17 10:09:25 -0700

    Fix

--- src/RoslynAnalyzers/Utilities/Workspaces/SyntaxGeneratorExtensions.cs
+++ src/RoslynAnalyzers/Utilities/Workspaces/SyntaxGeneratorExtensions.cs
@@ -10,6 +10,7 @@ using Microsoft.CodeAnalysis.Editing;
 
 namespace Analyzer.Utilities
 {
+    using DeclarationKind = Microsoft.CodeAnalysis.Internal.Editing.DeclarationKind;
     using DeclarationModifiers = Microsoft.CodeAnalysis.Internal.Editing.DeclarationModifiers;
 
     internal static class SyntaxGeneratorExtensions
