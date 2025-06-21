897d88ac7525b9c663be107d1c3c20ac288d6636
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-06-20 18:57:46 -0700

    Delete unused code

--- src/RoslynAnalyzers/Utilities/Compiler/Extensions/KeyValuePairExtensions.cs
+++ src/RoslynAnalyzers/Utilities/Compiler/Extensions/KeyValuePairExtensions.cs
@@ -8,12 +8,6 @@ namespace Analyzer.Utilities.Extensions
 {
     internal static class KeyValuePairExtensions
     {
-        //        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
-        //        {
-        //            key = pair.Key;
-        //            value = pair.Value;
-        //        }
-
         public static KeyValuePair<TKey?, TValue?> AsNullable<TKey, TValue>(this KeyValuePair<TKey, TValue> pair)
         {
             // This conversion is safe
