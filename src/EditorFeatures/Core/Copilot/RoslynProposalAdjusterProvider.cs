0a9c9df912d3774e2c6b92c7321c35f07c7d5d49
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-06-23 10:45:21 -0700

    Report telemetyr

--- src/EditorFeatures/Core/Copilot/RoslynProposalAdjusterProvider.cs
+++ src/EditorFeatures/Core/Copilot/RoslynProposalAdjusterProvider.cs
@@ -3,12 +3,14 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Editor;
+using Microsoft.CodeAnalysis.ErrorReporting;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.Internal.Log;
 using Microsoft.CodeAnalysis.Remote;
@@ -16,6 +18,7 @@ using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis.Text.Shared.Extensions;
 using Microsoft.VisualStudio.Language.Proposals;
 using Microsoft.VisualStudio.Utilities;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Copilot;
 
@@ -32,15 +35,105 @@ internal sealed class RoslynProposalAdjusterProvider() : ProposalAdjusterProvide
     public override Task<ProposalBase> AdjustProposalAfterAcceptAsync(ProposalBase proposal, string providerName, CancellationToken cancellationToken)
         => AdjustProposalAsync(proposal, providerName, before: false, cancellationToken);
 
+    private static void SetDefaultTelemetryProperties(Dictionary<string, object?> map, string providerName, bool before, TimeSpan elapsedTime)
+    {
+        // Common properties that all adjustments will log.
+        map["ProviderName"] = providerName;
+        map["AdjustProposalBeforeDisplay"] = before;
+        map["ComputationTime"] = elapsedTime.TotalMilliseconds.ToString("G17");
+    }
+
     private async Task<ProposalBase> AdjustProposalAsync(
         ProposalBase proposal, string providerName, bool before, CancellationToken cancellationToken)
     {
+        var stopwatch = SharedStopwatch.StartNew();
+
         // Ensure we're only operating on one solution.  It makes the logic much simpler, as we don't have to
         // worry about edits that touch multiple solutions.
         var solution = CopilotEditorUtilities.TryGetAffectedSolution(proposal);
         if (solution is null)
-            return proposal;
+        {
+            Logger.LogBlock(FunctionId.Copilot_AdjustProposal, KeyValueLogMessage.Create(static (d, args) =>
+            {
+                var (providerName, before, elapsedTime) = args;
+                SetDefaultTelemetryProperties(d, providerName, before, elapsedTime);
+                d["SolutionAcquisitionFailure"] = true;
+            },
+            args: (providerName, before, stopwatch.Elapsed)),
+            cancellationToken).Dispose();
 
+            return proposal;
+        }
+
+        return await AdjustProposalAsync(
+            solution, proposal, providerName, before, stopwatch, cancellationToken).ConfigureAwait(false);
+    }
+
+    private async Task<ProposalBase> AdjustProposalAsync(
+        Solution solution,
+        ProposalBase proposal,
+        string providerName,
+        bool before,
+        SharedStopwatch stopwatch,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var (newProposal, adjustmentsProposed) = await AdjustProposalAsync(
+                solution, proposal, cancellationToken).ConfigureAwait(false);
+            var adjustmentsAccepted = newProposal != proposal;
+
+            Logger.LogBlock(FunctionId.Copilot_AdjustProposal, KeyValueLogMessage.Create(static (d, args) =>
+            {
+                var (providerName, before, adjustmentsProposed, adjustmentsAccepted, elapsedTime) = args;
+                SetDefaultTelemetryProperties(d, providerName, before, elapsedTime);
+                d["AdjustmentsProposed"] = adjustmentsProposed;
+                d["AdjustmentsAccepted"] = adjustmentsAccepted;
+            },
+            args: (providerName, before, adjustmentsProposed, adjustmentsAccepted, stopwatch.Elapsed)),
+            cancellationToken).Dispose();
+
+            return newProposal;
+        }
+        catch (Exception ex) when (ReportFailureTelemetry(ex))
+        {
+            // Don't leak out any exceptions.  We report them as telemetry (and/or NFW), but we don't want to block the
+            // user getting the original proposal.
+            return proposal;
+        }
+
+        bool ReportFailureTelemetry(Exception ex)
+        {
+            // If it's not cancellation, report an NFW to track down our bug.
+            if (ex is not OperationCanceledException)
+                FatalError.ReportAndCatch(ex);
+
+            Logger.LogBlock(FunctionId.Copilot_AdjustProposal, KeyValueLogMessage.Create(static (d, args) =>
+            {
+                var (providerName, before, ex, elapsedTime) = args;
+                SetDefaultTelemetryProperties(d, providerName, before, elapsedTime);
+                d["AdjustmentsProposed"] = false;
+
+                if (ex is OperationCanceledException)
+                {
+                    d["Canceled"] = true;
+                }
+                else
+                {
+                    // Will be able to get a count of how often this happens.  NFWs can be used to track down the issue.
+                    d["Failed"] = true;
+                }
+            },
+            args: (providerName, before, ex, stopwatch.Elapsed)),
+            cancellationToken).Dispose();
+
+            return true;
+        }
+    }
+
+    private async Task<(ProposalBase newProposal, bool adjustmentsProposed)> AdjustProposalAsync(
+        Solution solution, ProposalBase proposal, CancellationToken cancellationToken)
+    {
         // We're potentially making multiple calls to oop here.  So keep a session alive to avoid
         // resyncing the solution and recomputing compilations.
         using var _1 = await RemoteKeepAliveSession.CreateAsync(solution, cancellationToken).ConfigureAwait(false);
@@ -80,34 +173,12 @@ internal sealed class RoslynProposalAdjusterProvider() : ProposalAdjusterProvide
 
         // No adjustments were made.  Don't touch anything.
         if (!adjustmentsProposed)
-        {
-            using var _3 = Logger.LogBlock(FunctionId.Copilot_AdjustProposal, KeyValueLogMessage.Create(static (d, args) =>
-            {
-                var (providerName, before) = args;
-                d["ProviderName"] = providerName;
-                d["Before"] = before;
-                d["AdjustmentsProposed"] = false;
-            },
-            args: (providerName, before)),
-            cancellationToken);
+            return (proposal, adjustmentsProposed: false);
 
-            return proposal;
-        }
-
+        // We have some changes we want to to make to the proposal.  See if the proposal system allows us merging
+        // those changes in.
         var newProposal = Proposal.TryCreateProposal(proposal, finalEdits);
-
-        using var _4 = Logger.LogBlock(FunctionId.Copilot_AdjustProposal, KeyValueLogMessage.Create(static (d, args) =>
-        {
-            var (providerName, before, newProposal) = args;
-            d["ProviderName"] = providerName;
-            d["Before"] = before;
-            d["AdjustmentsProposed"] = true;
-            d["AdjustmentsAccepted"] = newProposal != null;
-        },
-        args: (providerName, before, newProposal)),
-        cancellationToken);
-
-        return newProposal ?? proposal;
+        return (newProposal ?? proposal, adjustmentsProposed: true);
     }
 
     private async Task<ImmutableArray<TextChange>> TryAdjustProposalDisplayAsync(
