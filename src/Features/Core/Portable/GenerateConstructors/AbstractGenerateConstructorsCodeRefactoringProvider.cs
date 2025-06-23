0e8719c93acb5212429f6a64bda7c0f99ab06050
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-06-23 13:28:56 -0700

    revert

--- src/Features/Core/Portable/GenerateConstructors/AbstractGenerateConstructorsCodeRefactoringProvider.cs
+++ src/Features/Core/Portable/GenerateConstructors/AbstractGenerateConstructorsCodeRefactoringProvider.cs
@@ -137,8 +137,7 @@ internal abstract partial class AbstractGenerateConstructorsCodeRefactoringProvi
                     dialogAction.ViableMembers,
                     dialogAction.PickMembersOptions,
                     selectedAll: true);
-                var operations = await dialogAction.GetOperationsAsync(
-                    originalSolution, options, progressTracker, CodeActionCleanup.SyntaxAndSemantics, cancellationToken).ConfigureAwait(false);
+                var operations = await dialogAction.GetOperationsAsync(originalSolution, options, progressTracker, cancellationToken).ConfigureAwait(false);
                 return operations == null ? [] : [.. operations];
             }
             else
