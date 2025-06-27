e5f086e80844bbf932fea612cfa718363d1f0bab
Cyrus Najmabadi <cyrusn@microsoft.com>
2025-06-27 11:29:47 -0700

    Fix

--- src/EditorFeatures/Test2/GoToHelpers/GoToHelpers.vb
+++ src/EditorFeatures/Test2/GoToHelpers/GoToHelpers.vb
@@ -2,6 +2,7 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
+Imports System.Threading
 Imports Microsoft.CodeAnalysis
 Imports Microsoft.CodeAnalysis.Editor.UnitTests
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Utilities.GoToHelpers
@@ -22,7 +23,7 @@ Friend Class GoToHelpers
             Dim position = documentWithCursor.CursorPosition.Value
 
             Dim solution = workspace.CurrentSolution
-            Dim document = Await solution.GetRequiredDocumentAsync(documentWithCursor.Id, includeSourceGenerated:=True)
+            Dim document = Await solution.GetRequiredDocumentAsync(documentWithCursor.Id, includeSourceGenerated:=True, CancellationToken.None)
 
             Dim context = New SimpleFindUsagesContext()
             Await testingMethod(document, position, context)
