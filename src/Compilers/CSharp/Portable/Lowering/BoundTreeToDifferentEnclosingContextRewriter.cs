98d13a45d9bee278ff1d3c088b230eb304b4a015
Rikki Gibson <rigibson@microsoft.com>
2025-07-01 11:36:52 -0700

    Remove sealed

--- src/Compilers/CSharp/Portable/Lowering/BoundTreeToDifferentEnclosingContextRewriter.cs
+++ src/Compilers/CSharp/Portable/Lowering/BoundTreeToDifferentEnclosingContextRewriter.cs
@@ -139,7 +139,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 VisitType(node.Type));
         }
 
-        public sealed override BoundNode? VisitMethodDefIndex(BoundMethodDefIndex node)
+        public override BoundNode? VisitMethodDefIndex(BoundMethodDefIndex node)
         {
             // Cannot replace a MethodDefIndex's Method/Type with a substituted symbol.
             Debug.Assert(node.Type.Equals(VisitType(node.Type), TypeCompareKind.ConsiderEverything));
