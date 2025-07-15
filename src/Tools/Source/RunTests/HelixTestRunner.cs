f5124630a97cbd73ec4c8d008e2a1287adcab234
David Barbet <dabarbet@microsoft.com>
2025-07-14 20:27:04 -0700

    Add console logger to ensure helix console log has detailed info

--- src/Tools/Source/RunTests/HelixTestRunner.cs
+++ src/Tools/Source/RunTests/HelixTestRunner.cs
@@ -421,6 +421,9 @@ internal sealed class HelixTestRunner
         // The xml file must end in test-results.xml for the Azure Pipelines reporter to pick it up.
         builder.AppendLine($@"/Logger:xunit;LogFilePath=work-item-test-results.xml");
 
+        // Also add a console logger so that the helix log reports results as we go.
+        builder.AppendLine($@"/Logger:console;verbosity=detailed");
+
         // Specifies the results directory - this is where dumps from the blame options will get published. 
         builder.AppendLine($"/ResultsDirectory:.");
 
