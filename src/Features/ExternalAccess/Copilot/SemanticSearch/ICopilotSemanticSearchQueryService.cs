01b5df0c41c36cff6fdda7d8c08fba2596301e3c
Tomas Matousek <tomat@microsoft.com>
2025-07-17 17:24:40 -0700

    SemanticSearchService refactoring

---
+++ src/Features/ExternalAccess/Copilot/SemanticSearch/ICopilotSemanticSearchQueryService.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.ExternalAccess.Copilot.SemanticSearch;
+
+internal interface ICopilotSemanticSearchQueryService
+{
+    CompileQueryResult CompileQuery(
+        SolutionServices services,
+        string query,
+        TraceSource traceSource,
+        CancellationToken cancellationToken);
+
+    Task<ExecuteQueryResult> ExecuteQueryAsync(
+        Solution solution,
+        CompiledQueryId queryId,
+        ICopilotSemanticSearchResultsObserver observer,
+        TraceSource traceSource,
+        CancellationToken cancellationToken);
+
+    void DiscardQuery(CompiledQueryId queryId);
+
+    internal readonly record struct ExecuteQueryResult(
+        string? ErrorMessage,
+        string[]? ErrorMessageArgs = null,
+        TimeSpan ExecutionTime = default);
+
+    internal readonly record struct CompileQueryResult(
+        CompiledQueryId QueryId,
+        ImmutableArray<QueryCompilationError> CompilationErrors,
+        TimeSpan EmitTime = default);
+
+    internal readonly record struct QueryCompilationError(
+        string Id,
+        string Message,
+        TextSpan Span);
+
+    internal readonly record struct CompiledQueryId(
+        int Id);
+}
