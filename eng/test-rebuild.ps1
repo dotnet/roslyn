ce38637d29560ecc6488fc854b32e0a954b49c6d
Joey Robichaud <joseph.robichaud@microsoft.com>
2025-07-21 07:21:48 -0700

    Exclude Microsoft.CodeAnalysis.Threading.Package from Rebuild check

--- eng/test-rebuild.ps1
+++ eng/test-rebuild.ps1
@@ -64,6 +64,7 @@ try {
   " --exclude netstandard2.0\Microsoft.CodeAnalysis.Collections.Package.dll" +
   " --exclude netstandard2.0\Microsoft.CodeAnalysis.Debugging.Package.dll" +
   " --exclude netstandard2.0\Microsoft.CodeAnalysis.PooledObjects.Package.dll" +
+  " --exclude net9.0\Microsoft.CodeAnalysis.Threading.Package.dll" +
   " --exclude netstandard2.0\Microsoft.CodeAnalysis.Threading.Package.dll" +
   " --exclude netstandard2.0\Microsoft.CodeAnalysis.Extensions.Package.dll" +
   " --exclude netcoreapp3.1\Microsoft.CodeAnalysis.Workspaces.UnitTests.dll" +
