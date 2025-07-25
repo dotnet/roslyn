a40a54039e25aefb648da64dc607935cb425fc90
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-07-25 12:51:41 +0200

    Add work item

--- src/Analyzers/CSharp/Tests/ImplementInterface/ImplementInterfaceCodeFixTests.cs
+++ src/Analyzers/CSharp/Tests/ImplementInterface/ImplementInterfaceCodeFixTests.cs
@@ -11902,7 +11902,7 @@ class Goo : [|IComparable|]
             LanguageVersion = LanguageVersionExtensions.CSharpNext,
         }.RunAsync();
 
-    [Fact]
+    [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/79584")]
     public Task TestImplementIDisposable_DisposePattern_LF_EndOfLine()
          => new VerifyCS.Test
          {
