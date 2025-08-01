6f3fa21ac628fbc391efc997f48cc20814aaa79b
David Wengier <david.wengier@microsoft.com>
2025-08-01 16:55:42 +1000

    Allow more lifecycle events in Razor cohosting

--- src/Tools/ExternalAccess/Razor/Features/Cohost/ICohostStartupService.cs
+++ src/Tools/ExternalAccess/Razor/Features/Cohost/ICohostStartupService.cs
@@ -2,11 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
 
+[Obsolete("Please move to AbstractRazorCohostLifecycleService. This will be removed in a future release.")]
 internal interface ICohostStartupService
 {
     Task StartupAsync(string serializedClientCapabilities, RazorCohostRequestContext requestContext, CancellationToken cancellationToken);
