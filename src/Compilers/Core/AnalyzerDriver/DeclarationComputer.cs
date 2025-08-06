3eee5010191a3c0f82b553a9091fa9b3177fb45b
Todd Grunke <toddgrun@microsoft.com>
2025-08-05 21:53:43 -0700

    Fix a couple more allocations that show up under this method that were more obvious after looking at a test run with changes from the first commit

--- src/Compilers/Core/AnalyzerDriver/DeclarationComputer.cs
+++ src/Compilers/Core/AnalyzerDriver/DeclarationComputer.cs
@@ -75,7 +75,7 @@ namespace Microsoft.CodeAnalysis
             if (declaredSymbol is INamespaceSymbol namespaceSymbol && namespaceSymbol.ConstituentNamespaces.Length > 1)
             {
                 var assemblyToScope = model.Compilation.Assembly;
-                var assemblyScopedNamespaceSymbol = namespaceSymbol.ConstituentNamespaces.FirstOrDefault(ns => ns.ContainingAssembly == assemblyToScope);
+                var assemblyScopedNamespaceSymbol = namespaceSymbol.ConstituentNamespaces.FirstOrDefault(static (ns, assemblyToScope) => ns.ContainingAssembly == assemblyToScope, assemblyToScope);
                 if (assemblyScopedNamespaceSymbol != null)
                 {
                     Debug.Assert(assemblyScopedNamespaceSymbol.ConstituentNamespaces.Length == 1);
