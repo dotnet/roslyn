6019566380f21d4ecec15c619274b37e04b87017
Todd Grunke <toddgrun@microsoft.com>
2025-08-08 13:28:01 -0700

    Allow large InlineHint ArrayBuilder pooling
    The razor cohosting speedometer test shows 0.4% of total allocations during it's completion scenarios allocating InlineHint arrays. Manual debugging of the test scenario showed these arrays were about 260 items long, and thus weren't being placed back into the standard ArrayBuilder pool. Instead, we can use the non-bounded pool by passing in false  as discardLargeInstances to the ArrayBuilder.GetInstance call.

--- src/Features/Core/Portable/InlineHints/AbstractInlineParameterNameHintsService.cs
+++ src/Features/Core/Portable/InlineHints/AbstractInlineParameterNameHintsService.cs
@@ -59,7 +59,8 @@ internal abstract class AbstractInlineParameterNameHintsService : IInlineParamet
         var semanticModel = await document.GetRequiredSemanticModelAsync(cancellationToken).ConfigureAwait(false);
         var syntaxFacts = document.GetRequiredLanguageService<ISyntaxFactsService>();
 
-        using var _1 = ArrayBuilder<InlineHint>.GetInstance(out var result);
+        // Allow large array instances in the pool, as these arrays often exceed the ArrayBuilder reuse size threshold
+        using var _1 = ArrayBuilder<InlineHint>.GetInstance(discardLargeInstances: false, out var result);
         using var _2 = ArrayBuilder<(int position, SyntaxNode argument, IParameterSymbol? parameter, HintKind kind)>.GetInstance(out var buffer);
 
         foreach (var node in root.DescendantNodes(textSpan, n => n.Span.IntersectsWith(textSpan)))
