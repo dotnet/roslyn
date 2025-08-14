ee730c86c2593124dfecc2b2b1b5ba7e0188c2d6
Jan Jones <janjones@microsoft.com>
2025-08-14 18:54:46 +0200

    Simplify infra

--- src/Compilers/Core/SdkTaskTests/IntegrationTests.cs
+++ src/Compilers/Core/SdkTaskTests/IntegrationTests.cs
@@ -2,16 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.IO;
 using Microsoft.CodeAnalysis.BuildTasks.UnitTests;
 using Xunit.Abstractions;
 
 namespace Microsoft.CodeAnalysis.BuildTasks.Sdk.UnitTests;
 
-public sealed class IntegrationTests : IntegrationTestBase
-{
-    public IntegrationTests(ITestOutputHelper output) : base(output)
-    {
-        _buildTaskDll = Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "../tasks/netcore/binfx/Microsoft.Build.Tasks.CodeAnalysis.Sdk.dll");
-    }
-}
+public sealed class IntegrationTests(ITestOutputHelper output) : IntegrationTestBase(output);
