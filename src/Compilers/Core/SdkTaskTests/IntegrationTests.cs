68d20d8d28d7e5dffaf6c70cf784814cefe8eeef
Jan Jones <janjones@microsoft.com>
2025-08-15 13:30:04 +0200

    Fix bincore hydratation

--- src/Compilers/Core/SdkTaskTests/IntegrationTests.cs
+++ src/Compilers/Core/SdkTaskTests/IntegrationTests.cs
@@ -2,9 +2,23 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.IO;
 using Microsoft.CodeAnalysis.BuildTasks.UnitTests;
 using Xunit.Abstractions;
 
 namespace Microsoft.CodeAnalysis.BuildTasks.Sdk.UnitTests;
 
-public sealed class IntegrationTests(ITestOutputHelper output) : IntegrationTestBase(output);
+public sealed class IntegrationTests : IntegrationTestBase
+{
+    public IntegrationTests(ITestOutputHelper output) : base(output)
+    {
+        // Our task will search for `../bincore` directory.
+        // We cannot populate it during build because our test infrastructure
+        // would not rehydrate that (as it's outside the output directory).
+        var realBincoreDirectory = "../bincore";
+        if (!Directory.Exists(realBincoreDirectory))
+        {
+            Directory.Move("bincore", "../bincore");
+        }
+    }
+}
