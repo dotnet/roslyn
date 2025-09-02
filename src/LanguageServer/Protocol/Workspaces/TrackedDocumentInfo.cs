d9f2ed2d179e433a9551b7daf36d91591459e079
David Barbet <dabarbet@microsoft.com>
2025-09-02 12:43:48 -0700

    Doc TrackedDocumentInfo

--- src/LanguageServer/Protocol/Workspaces/TrackedDocumentInfo.cs
+++ src/LanguageServer/Protocol/Workspaces/TrackedDocumentInfo.cs
@@ -3,7 +3,14 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.CodeAnalysis.Text;
+using Roslyn.LanguageServer.Protocol;
 
 namespace Microsoft.CodeAnalysis.LanguageServer;
 
+/// <summary>
+/// Holds state information from the client about a tracked LSP document.  See <see cref="LspWorkspaceManager"/> 
+/// </summary>
+/// <param name="SourceText">A snapshot of the text as seen by LSP.</param>
+/// <param name="LanguageId">The language id for the text, from <see cref="TextDocumentItem.LanguageId"/> </param>
+/// <param name="LspVersion">The client version associated with the text, from <see cref="VersionedTextDocumentIdentifier.Version"/></param>
 internal record struct TrackedDocumentInfo(SourceText SourceText, string LanguageId, int LspVersion);
