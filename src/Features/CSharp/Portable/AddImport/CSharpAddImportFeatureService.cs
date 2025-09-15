b69d264fb117e378d7e5feeb265fc861ff898afa
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-09-15 14:24:05 +0200

    Simplify

--- src/Features/CSharp/Portable/AddImport/CSharpAddImportFeatureService.cs
+++ src/Features/CSharp/Portable/AddImport/CSharpAddImportFeatureService.cs
@@ -616,29 +616,6 @@ internal sealed class CSharpAddImportFeatureService() : AbstractAddImportFeature
         return (CompilationUnitSyntax)contextNode.SyntaxTree.GetRoot(cancellationToken);
     }
 
-    //protected override bool IsViableExtensionMethod(IMethodSymbol method, SyntaxNode expression, SemanticModel semanticModel, ISyntaxFacts syntaxFacts, CancellationToken cancellationToken)
-    //{
-    //    var leftExpression = syntaxFacts.IsMemberAccessExpression(expression)
-    //        ? syntaxFacts.GetExpressionOfMemberAccessExpression(expression)
-    //        : syntaxFacts.GetTargetOfMemberBinding(expression);
-    //    if (leftExpression == null)
-    //    {
-    //        if (expression.IsKind(SyntaxKind.CollectionInitializerExpression))
-    //        {
-    //            leftExpression = expression.GetAncestor<ObjectCreationExpressionSyntax>();
-    //        }
-    //        else
-    //        {
-    //            return false;
-    //        }
-    //    }
-
-    //    var semanticInfo = semanticModel.GetTypeInfo(leftExpression, cancellationToken);
-    //    var leftExpressionType = semanticInfo.Type;
-
-    //    return IsViableExtensionMethod(method, leftExpressionType);
-    //}
-
     protected override bool IsAddMethodContext(
         SyntaxNode node, SemanticModel semanticModel, out SyntaxNode objectCreationExpression)
     {
