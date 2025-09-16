fdef1ddb1f7b599f155b53a008d1ddb458bcc1a4
Fredric Silberberg <frsilb@microsoft.com>
2025-09-16 13:48:19 -0700

    Refactor for readability

--- eng/generate-compiler-code.cs
+++ eng/generate-compiler-code.cs
@@ -3,7 +3,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-// Need this fix to delete the static graph disable: https://github.com/dotnet/sdk/pull/50532
+// Need this fix to delete the static graph disable: https://github.com/dotnet/sdk/pull/50532, 10.0.100-rc.2
 #:property RestoreUseStaticGraphEvaluation=false
 #:project ../src/Tools/Source/CompilerGeneratorTools/Source/BoundTreeGenerator/
 #:project ../src/Tools/Source/CompilerGeneratorTools/Source/IOperationGenerator/
@@ -14,130 +14,143 @@
 
 using System.Runtime.CompilerServices;
 
-var repoRoot = GetRoslynDirectory();
-
-var test = false;
-var configuration = "Debug";
-
-for (var i = 0; i < args.Length; i++)
-{
-    var a = args[i];
-    switch (a)
-    {
-        case "-test":
-            test = true;
-            break;
-        case "-configuration" when i + 1 < args.Length:
-            configuration = args[i++];
-            break;
-        default:
-            Console.WriteLine("Unknown argument: " + a);
-            return 1;
-    }
-}
-
-var csharpDir = Path.Join(repoRoot, "src", "Compilers", "CSharp", "Portable");
-var vbDir = Path.Join(repoRoot, "src", "Compilers", "VisualBasic", "Portable");
+var (test, configuration, repoRoot) = ParseArgs(args);
 
 // Folders for test artifacts when running in -test mode
 var generationTempDirRoot = Path.Join(repoRoot, "artifacts", "log", configuration, "Generated");
-
-var generatedCSharpDir = Path.Join(csharpDir, "Generated");
-var generatedCSharpTestDir = Path.Join(repoRoot, "src", "Compilers", "CSharp", "Test", "Syntax", "Generated");
-var generatedTempCSharpDir = Path.Join(generationTempDirRoot, "CSharp", "Src");
-var generatedTempCSharpTestDir = Path.Join(generationTempDirRoot, "CSharp", "Test");
-
 var retVal = 0;
 
+// Generate C# syntax source
+var csharpLocation = GetLanguageDirStructure(repoRoot, "CSharp", generationTempDirRoot, test);
 retVal = GenerateLanguage(
     BoundTreeGenerator.TargetLanguage.CSharp,
-    csharpDir,
-    test ? generatedTempCSharpDir : generatedCSharpDir,
-    test ? generatedTempCSharpTestDir : generatedCSharpTestDir,
+    csharpLocation.LanguageDir,
+    test ? csharpLocation.GeneratedSourceTempDir : csharpLocation.GeneratedSourceDir,
+    (test ? csharpLocation.GeneratedTestTempDir : csharpLocation.GeneratedTestDir)!,
     (inputFile, outputFile, writeSource, writeTests, writeGrammar)
         => CSharpSyntaxGenerator.Program.Generate(inputFile, outputFile, writeSource, writeTests, writeGrammar, writeSignatures: false),
     Microsoft.CodeAnalysis.CSharp.Internal.CSharpErrorFactsGenerator.Program.Generate);
 
-var generatedVisualBasicDir = Path.Join(vbDir, "Generated");
-var generatedVisualBasicTestDir = Path.Join(repoRoot, "src", "Compilers", "VisualBasic", "Test", "Syntax", "Generated");
-var generatedTempVisualBasicDir = Path.Join(generationTempDirRoot, "VisualBasic", "Src");
-var generatedTempVisualBasicTestDir = Path.Join(generationTempDirRoot, "VisualBasic", "Test");
-
+// Generate VB syntax source
+var vbLocation = GetLanguageDirStructure(repoRoot, "VisualBasic", generationTempDirRoot, test);
 retVal |= GenerateLanguage(
     BoundTreeGenerator.TargetLanguage.VB,
-    vbDir,
-    test ? generatedTempVisualBasicDir : generatedVisualBasicDir,
-    test ? generatedTempVisualBasicTestDir : generatedVisualBasicTestDir,
+    vbLocation.LanguageDir,
+    test ? vbLocation.GeneratedSourceTempDir : vbLocation.GeneratedSourceDir,
+    (test ? vbLocation.GeneratedTestTempDir : vbLocation.GeneratedTestDir)!,
     Microsoft.CodeAnalysis.VisualBasic.Internal.VBSyntaxGenerator.Program.Generate,
     Microsoft.CodeAnalysis.VisualBasic.Internal.VBErrorFactsGenerator.Program.Generate);
 
-var basicXmlFile = Path.Join(vbDir, "Syntax", "Syntax.xml");
-var generatedGetTextDirectory = Path.Join(repoRoot, "src", "ExpressionEvaluator", "VisualBasic", "Source", "ResultProvider", "Generated");
-var generatedGetTextFile = Path.Join(generatedGetTextDirectory, "Syntax.xml.GetText.Generated.vb");
-var generatedTempGetTextDirectory = Path.Join(generationTempDirRoot, "VisualBasic", "GetText");
-var generatedTempGetTextFile = Path.Join(generatedTempGetTextDirectory, "Syntax.xml.GetText.Generated.vb");
-if (test)
-{
-    Directory.CreateDirectory(generatedTempGetTextDirectory);
-}
-Microsoft.CodeAnalysis.VisualBasic.Internal.VBSyntaxGenerator.Program.Generate(basicXmlFile, test ? generatedTempGetTextFile : generatedGetTextFile, writeSource: false, writeTests: false, writeGrammar: false);
+// Generate VB GetText source
+var getTextLocation = GetGetTextLocation(vbLocation, repoRoot, generationTempDirRoot, test);
+Microsoft.CodeAnalysis.VisualBasic.Internal.VBSyntaxGenerator.Program.Generate(
+    Path.Join(vbLocation.LanguageDir, "Syntax", "Syntax.xml"),
+    Path.Join(test ? getTextLocation.GeneratedSourceTempDir : getTextLocation.GeneratedSourceDir, "Syntax.xml.GetText.Generated.vb"),
+    writeSource: false, writeTests: false, writeGrammar: false);
 
-var coreDir = Path.Join(repoRoot, "src", "Compilers", "Core", "Portable");
-var operationsXml = Path.Join(coreDir, "Operations", "OperationInterfaces.xml");
-var generatedOperationsDir = Path.Join(coreDir, "Generated");
-var generatedTempOperationsDir = Path.Join(generationTempDirRoot, "Core", "Src");
-if (test)
-{
-    Directory.CreateDirectory(generatedTempOperationsDir);
-}
-retVal |= IOperationGenerator.Program.Generate(operationsXml, test ? generatedTempOperationsDir : generatedOperationsDir);
+// Generate IOperation source
+var ioperationLocation = GetIOperationLocation(repoRoot, generationTempDirRoot, test);
+retVal |= IOperationGenerator.Program.Generate(Path.Join(ioperationLocation.LanguageDir, "Operations", "OperationInterfaces.xml"), test ? ioperationLocation.GeneratedSourceTempDir : ioperationLocation.GeneratedSourceDir);
 
 if (test)
 {
-    string[] actualDirs = [generatedCSharpDir, generatedCSharpTestDir, generatedVisualBasicDir, generatedVisualBasicTestDir, generatedOperationsDir, generatedGetTextDirectory];
-    string[] tempDirs = [generatedTempCSharpDir, generatedTempCSharpTestDir, generatedTempVisualBasicDir, generatedTempVisualBasicTestDir, generatedTempOperationsDir, generatedTempGetTextDirectory];
-
-    foreach (var (actualDir, tempDir) in actualDirs.Zip(tempDirs))
-    {
-        var tempFiles = Directory.GetFiles(tempDir).Select(Path.GetFileName);
-        foreach (var fileName in tempFiles)
-        {
-            Console.WriteLine($"Verifying {fileName}");
-            var actualFile = Path.Join(actualDir, fileName);
-            var tempFile = Path.Join(tempDir, fileName);
-
-            if (!File.Exists(actualFile))
-            {
-                Console.WriteLine($"Generated file '{actualFile}' does not exist.");
-                Console.WriteLine($"Run {Path.Join(repoRoot, "eng", "generate-compiler-code.cmd")} to update it.");
-                retVal = 1;
-            }
-            else if (!File.ReadAllText(actualFile).Equals(File.ReadAllText(tempFile), StringComparison.Ordinal))
-            {
-                Console.WriteLine($"Generated file '{actualFile}' is out of date.");
-                Console.WriteLine($"Run {Path.Join(repoRoot, "eng", "generate-compiler-code.cmd")} to update it.");
-                retVal = 1;
-            }
-        }
-    }
+    retVal |= TestGenerationResults(repoRoot, csharpLocation, vbLocation, getTextLocation, ioperationLocation);
 }
 
 return retVal;
 
-static string GetRoslynDirectory([CallerFilePath] string sourceFilePath = "")
+static (bool test, string configuration, string roslynDirectory) ParseArgs(string[] args, [CallerFilePath] string sourceFilePath = "")
 {
-    if (Path.GetDirectoryName(sourceFilePath) is not string engDir || Path.GetDirectoryName(engDir) is not string roslynRoot || !File.Exists(Path.Join(roslynRoot, "eng", Path.GetFileName(sourceFilePath))))
+    var test = false;
+    var configuration = "Debug";
+
+    for (var i = 0; i < args.Length; i++)
     {
-        throw new InvalidOperationException("Could not determine source file path. This script must be located in the 'eng' directory of the Roslyn repo.");
+        var a = args[i];
+        switch (a)
+        {
+            case "-test":
+                test = true;
+                break;
+            case "-configuration" when i + 1 < args.Length:
+                configuration = args[i++];
+                break;
+            default:
+                Console.WriteLine("Unknown argument: " + a);
+                Environment.Exit(1);
+                break;
+        }
     }
 
-    return roslynRoot;
+    if (Path.GetDirectoryName(sourceFilePath) is not string engDir || Path.GetDirectoryName(engDir) is not string roslynRoot || !File.Exists(Path.Join(roslynRoot, "eng", Path.GetFileName(sourceFilePath))))
+    {
+        Console.WriteLine("Could not determine source file path. This script must be located in the 'eng' directory of the Roslyn repo.");
+        Environment.Exit(1);
+        throw null!;
+    }
+
+    return (test, configuration, roslynRoot);
+}
+
+static GenerationLocation GetLanguageDirStructure(string repoRoot, string languageName, string generationTempDirRoot, bool isTest)
+{
+    var languageDir = Path.Join(repoRoot, "src", "Compilers", languageName, "Portable");
+    var loc = new GenerationLocation(
+        languageDir,
+        Path.Join(languageDir, "Generated"),
+        Path.Join(repoRoot, "src", "Compilers", languageName, "Test", "Syntax", "Generated"),
+        Path.Join(generationTempDirRoot, languageName, "Src"),
+        Path.Join(generationTempDirRoot, languageName, "Test")
+    );
+
+    if (isTest)
+    {
+        Directory.CreateDirectory(loc.GeneratedSourceTempDir);
+        Directory.CreateDirectory(loc.GeneratedTestTempDir!);
+    }
+
+    return loc;
+}
+
+static GenerationLocation GetGetTextLocation(GenerationLocation vbLocation, string repoRoot, string generationTempDirRoot, bool test)
+{
+    var getTextDir = new GenerationLocation(
+        vbLocation.LanguageDir,
+        Path.Join(repoRoot, "src", "ExpressionEvaluator", "VisualBasic", "Source", "ResultProvider", "Generated"),
+        GeneratedTestDir: null,
+        Path.Join(generationTempDirRoot, "VisualBasic", "GetText"),
+        GeneratedTestTempDir: null
+    );
+
+    if (test)
+    {
+        Directory.CreateDirectory(getTextDir.GeneratedSourceTempDir);
+    }
+
+    return getTextDir;
+}
+
+static GenerationLocation GetIOperationLocation(string repoRoot, string generationTempDirRoot, bool test)
+{
+    var languageDir = Path.Join(repoRoot, "src", "Compilers", "Core", "Portable");
+    var loc = new GenerationLocation(
+        languageDir,
+        Path.Join(languageDir, "Generated"),
+        GeneratedTestDir: null,
+        Path.Join(generationTempDirRoot, "Core", "Src"),
+        GeneratedTestTempDir: null
+    );
+
+    if (test)
+    {
+        Directory.CreateDirectory(loc.GeneratedSourceTempDir);
+    }
+
+    return loc;
 }
 
 static int GenerateLanguage(BoundTreeGenerator.TargetLanguage language, string languageDir, string generatedDir, string generatedTestDir, SyntaxGeneratorAction syntaxGenerator, ErrorFactsGeneratorAction errorFactsGenerator)
 {
-    Directory.CreateDirectory(generatedDir);
-    Directory.CreateDirectory(generatedTestDir);
     var extension = language == BoundTreeGenerator.TargetLanguage.CSharp ? "cs" : "vb";
     var generatedTestFile = Path.Join(generatedTestDir, $"Syntax.Test.xml.Generated.{extension}");
     var errorsPath = Path.Join(languageDir, "Errors", language == BoundTreeGenerator.TargetLanguage.CSharp ? "ErrorCode.cs" : "Errors.vb");
@@ -165,5 +178,55 @@ static int GenerateLanguage(BoundTreeGenerator.TargetLanguage language, string l
     return retVal;
 }
 
+static int TestGenerationResults(string repoRoot, GenerationLocation csharpLocation, GenerationLocation vbLocation, GenerationLocation getTextLocation, GenerationLocation ioperationLocation)
+{
+    string[] actualDirs = [.. csharpLocation.GetActualDirs(), .. vbLocation.GetActualDirs(), getTextLocation.GeneratedSourceDir, ioperationLocation.GeneratedSourceDir];
+    string[] tempDirs = [.. csharpLocation.GetTempDirs(), .. vbLocation.GetTempDirs(), getTextLocation.GeneratedSourceTempDir, ioperationLocation.GeneratedSourceTempDir];
+
+    var retVal = 0;
+    foreach (var (actualDir, tempDir) in actualDirs.Zip(tempDirs))
+    {
+        var tempFiles = Directory.GetFiles(tempDir).Select(Path.GetFileName);
+        foreach (var fileName in tempFiles)
+        {
+            Console.WriteLine($"Verifying {fileName}");
+            var actualFile = Path.Join(actualDir, fileName);
+            var tempFile = Path.Join(tempDir, fileName);
+
+            if (!File.Exists(actualFile))
+            {
+                Console.WriteLine($"Generated file '{actualFile}' does not exist.");
+                Console.WriteLine($"Run {Path.Join(repoRoot, "eng", "generate-compiler-code.cmd")} to update it.");
+                retVal = 1;
+            }
+            else if (!File.ReadAllText(actualFile).Equals(File.ReadAllText(tempFile), StringComparison.Ordinal))
+            {
+                Console.WriteLine($"Generated file '{actualFile}' is out of date.");
+                Console.WriteLine($"Run {Path.Join(repoRoot, "eng", "generate-compiler-code.cmd")} to update it.");
+                retVal = 1;
+            }
+        }
+    }
+
+    return retVal;
+}
+
 delegate int SyntaxGeneratorAction(string inputFile, string outputFile, bool writeSource, bool writeTests, bool writeGrammar);
 delegate int ErrorFactsGeneratorAction(string inputPath, string outputPath);
+
+readonly record struct GenerationLocation(string LanguageDir, string GeneratedSourceDir, string? GeneratedTestDir, string GeneratedSourceTempDir, string? GeneratedTestTempDir)
+{
+    public readonly IEnumerable<string> GetActualDirs()
+    {
+        yield return GeneratedSourceDir;
+        if (GeneratedTestDir is not null)
+            yield return GeneratedTestDir;
+    }
+
+    public readonly IEnumerable<string> GetTempDirs()
+    {
+        yield return GeneratedSourceTempDir;
+        if (GeneratedTestTempDir is not null)
+            yield return GeneratedTestTempDir;
+    }
+}
