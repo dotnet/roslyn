56b71a0c587361447ffa97346efa556551d40f34
Dustin Campbell <dustin@teamcampbell.org>
2025-09-15 17:39:20 -0700

    Add external access API for dotnet-watch to MS.CA.Workspaces.MSBuild
    Add interfaces and wrappers for dotnet-watch.

---
+++ src/Workspaces/MSBuild/Core/ExternalAccess/Watch/Api/WatchDocumentFileInfo.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Immutable;
+
+namespace Microsoft.CodeAnalysis.MSBuild.ExternalAccess.Watch.Api;
+
+/// <summary>
+/// Represents a source file that is part of a project file.
+/// </summary>
+internal readonly struct WatchDocumentFileInfo
+{
+    internal DocumentFileInfo UnderlyingObject { get; }
+
+    internal WatchDocumentFileInfo(DocumentFileInfo underlyingObject)
+    {
+        UnderlyingObject = underlyingObject;
+    }
+
+    public WatchDocumentFileInfo(string filePath, string logicalPath, bool isLinked, bool isGenerated, ImmutableArray<string> folders)
+        : this(new(filePath, logicalPath, isLinked, isGenerated, folders))
+    {
+    }
+
+    /// <summary>
+    /// The absolute path to the document file on disk.
+    /// </summary>
+    public string FilePath => UnderlyingObject.FilePath;
+
+    /// <summary>
+    /// A fictional path to the document, relative to the project.
+    /// The document may not actually exist at this location, and is used
+    /// to represent linked documents. This includes the file name.
+    /// </summary>
+    public string LogicalPath => UnderlyingObject.LogicalPath;
+
+    /// <summary>
+    /// True if the document has a logical path that differs from its 
+    /// absolute file path.
+    /// </summary>
+    public bool IsLinked => UnderlyingObject.IsLinked;
+
+    /// <summary>
+    /// True if the file was generated during build.
+    /// </summary>
+    public bool IsGenerated => UnderlyingObject.IsGenerated;
+
+    /// <summary>
+    /// Containing folders of the document relative to the containing project root path.
+    /// </summary>
+    public ImmutableArray<string> Folders => UnderlyingObject.Folders;
+}
---
+++ src/Workspaces/MSBuild/Core/ExternalAccess/Watch/Api/WatchMSBuildProjectLoader.cs
@@ -0,0 +1,71 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.CodeAnalysis.MSBuild.ExternalAccess.Watch.Api;
+
+internal static class WatchMSBuildProjectLoader
+{
+    public static MSBuildProjectLoader Create(
+        Workspace workspace,
+        IWatchProjectFileInfoLoaderFactory loaderFactory)
+        => Create(workspace, properties: null, loaderFactory);
+
+    public static MSBuildProjectLoader Create(
+        Workspace workspace,
+        ImmutableDictionary<string, string>? properties,
+        IWatchProjectFileInfoLoaderFactory loaderFactory)
+        => new(workspace, properties, new WatchProjectFileInfoLoaderFactory(loaderFactory));
+
+    private sealed class WatchProjectFileInfoLoaderFactory(IWatchProjectFileInfoLoaderFactory loaderFactory) : IProjectFileInfoLoaderFactory
+    {
+        public ProjectFileInfoLoader Create(
+            ImmutableDictionary<string, string> properties,
+            ProjectFileExtensionRegistry projectFileExtensionRegistry,
+            ProjectLoadOperationRunner operationRunner,
+            DiagnosticReporter diagnosticReporter,
+            ILoggerFactory loggerFactory)
+            => new WatchProjectFileInfoLoader(diagnosticReporter, projectFileExtensionRegistry, operationRunner, loaderFactory.Create(properties));
+    }
+
+    private sealed class WatchProjectFileInfoLoader(
+        DiagnosticReporter diagnosticReporter,
+        ProjectFileExtensionRegistry projectFileExtensionRegistry,
+        ProjectLoadOperationRunner operationRunner,
+        IWatchProjectFileInfoLoader loader)
+        : ProjectFileInfoLoader(diagnosticReporter, projectFileExtensionRegistry, operationRunner)
+    {
+        private readonly IWatchProjectFileInfoLoader _loader = loader;
+
+        public override ValueTask DisposeAsync()
+            => (_loader as IAsyncDisposable)?.DisposeAsync() ?? default;
+
+        public override async Task<ImmutableArray<ProjectFileInfo>> LoadProjectFileInfosAsync(
+            string projectFilePath,
+            DiagnosticReportingOptions reportingOptions,
+            CancellationToken cancellationToken)
+        {
+            if (!TryGetLanguageNameFromProjectPath(projectFilePath, reportingOptions, out var languageName))
+            {
+                return [];
+            }
+
+            var result = await _loader
+                .LoadProjectFileInfosAsync(projectFilePath, languageName, cancellationToken)
+                .ConfigureAwait(false);
+
+            ReportDiagnostics(result.DiagnosticItems.SelectAsArray(static x => x.UnderlyingObject));
+
+            return result.ProjectFileInfos.SelectAsArray(static x => x.UnderlyingObject);
+        }
+
+        public override Task<string?> TryGetProjectOutputPathAsync(string projectFilePath, CancellationToken cancellationToken)
+            => _loader.TryGetProjectOutputPathAsync(projectFilePath, cancellationToken);
+    }
+}
