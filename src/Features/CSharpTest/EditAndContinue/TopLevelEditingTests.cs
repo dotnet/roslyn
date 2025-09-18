5a28157410a2cbef12360b1425c0d4021de38252
tmat <tomas.matousek@microsoft.com>
2025-09-18 12:57:02 -0700

    Fix handling edits in types nested in reloadable types

--- src/Features/CSharpTest/EditAndContinue/TopLevelEditingTests.cs
+++ src/Features/CSharpTest/EditAndContinue/TopLevelEditingTests.cs
@@ -2707,8 +2707,12 @@ public sealed class TopLevelEditingTests : EditingTestBase
     {
         var srcA1 = ReloadableAttributeSrc + "[CreateNewOnMetadataUpdate]class C { void F() {} }";
         var srcA2 = ReloadableAttributeSrc;
+
+        var srcB1 = "";
+        var srcB2 = "using System.Runtime.CompilerServices; [CreateNewOnMetadataUpdate]class C { void F() {} }";
+
         EditAndContinueValidation.VerifySemantics(
-            [GetTopEdits(srcA1, srcA2), GetTopEdits("", "[CreateNewOnMetadataUpdate]class C { void F() {} }")],
+            [GetTopEdits(srcA1, srcA2), GetTopEdits(srcB1, srcB2)],
             [
                 DocumentResults(),
                 DocumentResults(
@@ -5408,7 +5412,7 @@ public sealed class TopLevelEditingTests : EditingTestBase
                 ]),
                 DocumentResults(semanticEdits:
                 [
-                    SemanticEdit(SemanticEditKind.Update, c => c.GetMember("C.D.M"))
+                    SemanticEdit(SemanticEditKind.Replace, c => c.GetMember("C"), partialType: "C")
                 ])
             ],
             capabilities: EditAndContinueCapabilities.NewTypeDefinition);
@@ -5427,7 +5431,6 @@ public sealed class TopLevelEditingTests : EditingTestBase
             semanticEdits:
             [
                 SemanticEdit(SemanticEditKind.Replace, c => c.GetMember("C")),
-                SemanticEdit(SemanticEditKind.Update, c => c.GetMember("C.D.M"))
             ],
             capabilities: EditAndContinueCapabilities.NewTypeDefinition);
     }
@@ -5444,8 +5447,7 @@ public sealed class TopLevelEditingTests : EditingTestBase
             edits,
             semanticEdits:
             [
-                SemanticEdit(SemanticEditKind.Replace, c => c.GetMember("C")),
-                SemanticEdit(SemanticEditKind.Update, c => c.GetMember("C.D.M"))
+                SemanticEdit(SemanticEditKind.Replace, c => c.GetMember("C"))
             ],
             capabilities: EditAndContinueCapabilities.NewTypeDefinition);
     }
@@ -5873,6 +5875,36 @@ public sealed class TopLevelEditingTests : EditingTestBase
 
         edits.VerifySemanticDiagnostics(
              Diagnostic(RudeEditKind.Move, "public class X", FeaturesResources.class_));
+    }
+
+    /// <summary>
+    /// Scenario: Razor page types are marked with CreateNewOnMetadataUpdateAttribute.
+    /// It is possible to define nested types via <c>@functions</c> block and any changes to this block should be allowed.
+    /// </summary>
+    [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/76989")]
+    public void NestedType_Enum_Update()
+    {
+        var src1 = ReloadableAttributeSrc + "[CreateNewOnMetadataUpdate]class C { enum E { A } }";
+        var src2 = ReloadableAttributeSrc + "[CreateNewOnMetadataUpdate]class C { enum E { A, B } }";
+
+        var edits = GetTopEdits(src1, src2);
+
+        edits.VerifySemantics(
+            semanticEdits: [SemanticEdit(SemanticEditKind.Replace, c => c.GetMember("C"))],
+            capabilities: EditAndContinueCapabilities.NewTypeDefinition);
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/roslyn/issues/76989")]
+    public void NestedType_Rename_Reloadable()
+    {
+        var src1 = ReloadableAttributeSrc + "[CreateNewOnMetadataUpdate]class C { class D1 { public void F() { Console.WriteLine(1); }}; }";
+        var src2 = ReloadableAttributeSrc + "[CreateNewOnMetadataUpdate]class C { class D2 { public void F() { Console.WriteLine(2); }}; }";
+
+        var edits = GetTopEdits(src1, src2);
+
+        edits.VerifySemantics(
+            semanticEdits: [SemanticEdit(SemanticEditKind.Replace, c => c.GetMember("C"))],
+            capabilities: EditAndContinueCapabilities.NewTypeDefinition);
     }
 
     /// <summary>
