1493970761ab1d1fc2dbe3eca0b54732df1bc725
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-09-22 16:44:56 +0200

    performance

--- src/Analyzers/CSharp/CodeFixes/RemoveUnnecessarySuppressions/CSharpRemoveUnnecessaryNullableWarningSuppressionsCodeFixProvider.cs
+++ src/Analyzers/CSharp/CodeFixes/RemoveUnnecessarySuppressions/CSharpRemoveUnnecessaryNullableWarningSuppressionsCodeFixProvider.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Immutable;
 using System.Composition;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CodeFixes;
@@ -12,6 +13,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.CSharp.RemoveUnnecessarySuppressions;
@@ -36,15 +38,25 @@ internal sealed class CSharpRemoveUnnecessaryNullableWarningSuppressionsCodeFixP
         SyntaxEditor editor,
         CancellationToken cancellationToken)
     {
-        foreach (var diagnostic in diagnostics)
+        using var _ = PooledHashSet<PostfixUnaryExpressionSyntax>.GetInstance(out var processedOuterNodes);
+
+        // Process the nodes from outside in.  If we remove the outer suppression, then don't process any inner nodes
+        // inside of that.  Those suppressions may now be necessary due to the outer going away.  this way we push
+        // suppressions to the narrowest possible scope when there are multiple.
+        foreach (var diagnostic in diagnostics.OrderBy(d => d.AdditionalLocations[0].SourceSpan.Start))
         {
             var node = diagnostic.AdditionalLocations[0].FindNode(getInnermostNodeForTie: true, cancellationToken);
-            if (node is PostfixUnaryExpressionSyntax postfixUnary)
-            {
-                editor.ReplaceNode(
-                    postfixUnary,
-                    postfixUnary.Operand.WithTriviaFrom(postfixUnary));
-            }
+            if (node is not PostfixUnaryExpressionSyntax postfixUnary)
+                continue;
+
+            // Skip inner nodes if we already processed an outer node.
+            if (node.Ancestors().OfType<PostfixUnaryExpressionSyntax>().Any(processedOuterNodes.Contains))
+                continue;
+
+            editor.ReplaceNode(
+                postfixUnary,
+                postfixUnary.Operand.WithTriviaFrom(postfixUnary));
+            processedOuterNodes.Add(postfixUnary);
         }
 
         return Task.CompletedTask;
