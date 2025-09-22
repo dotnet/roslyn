734344de56da96012584eb78dab5fd02f741ebbf
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-09-22 11:23:56 +0200

    Simplify

--- src/Features/VisualBasic/Portable/Copilot/VisualBasicCopilotProposalAdjusterService.vb
+++ src/Features/VisualBasic/Portable/Copilot/VisualBasicCopilotProposalAdjusterService.vb
@@ -2,13 +2,11 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
-Imports System.Collections.Immutable
 Imports System.Composition
 Imports System.Threading
 Imports Microsoft.CodeAnalysis.Copilot
 Imports Microsoft.CodeAnalysis.Host.Mef
 Imports Microsoft.CodeAnalysis.Options
-Imports Microsoft.CodeAnalysis.Text
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.Copilot
     <ExportLanguageService(GetType(ICopilotProposalAdjusterService), LanguageNames.VisualBasic), [Shared]>
@@ -21,8 +19,8 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Copilot
             MyBase.New(globalOptions)
         End Sub
 
-        Protected Overrides Function AddMissingTokensIfAppropriateAsync(originalDocument As Document, forkedDocument As Document, cancellationToken As CancellationToken) As Task(Of (changed As Boolean, document As Document))
-            Return Task.FromResult((False, forkedDocument))
+        Protected Overrides Function AddMissingTokensIfAppropriateAsync(originalDocument As Document, forkedDocument As Document, cancellationToken As CancellationToken) As Task(Of Document)
+            Return Task.FromResult(forkedDocument)
         End Function
     End Class
 End Namespace
