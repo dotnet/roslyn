05034aad750fa887868f006589a69eb4ac9bd722
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-10-01 13:38:05 +0200

    COmpiling

--- src/EditorFeatures/CSharpTest2/EmbeddedLanguages/RegularExpressions/CSharpRegexParserTests.cs
+++ src/EditorFeatures/CSharpTest2/EmbeddedLanguages/RegularExpressions/CSharpRegexParserTests.cs
@@ -376,7 +376,7 @@ public sealed partial class CSharpRegexParserTests
         foreach (var (charClass, _) in RegexCharClass.EscapeCategories)
         {
             foreach (var ch in charClass)
-                Assert.True(RegexLexer.IsEscapeCategoryChar(VirtualChar.Create(new Rune(ch), new TextSpan(0, 1))));
+                Assert.True(RegexLexer.IsEscapeCategoryChar(new(VirtualCharGreen.Create(new Rune(ch), offset: 0, width: 1), tokenStart: 0)));
         }
     }
 }
