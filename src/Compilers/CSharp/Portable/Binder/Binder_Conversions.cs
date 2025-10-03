83ed4d19060956248c5819abd0966170ae15380e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-10-03 21:47:34 +0200

    fix release mode

--- src/Compilers/CSharp/Portable/Binder/Binder_Conversions.cs
+++ src/Compilers/CSharp/Portable/Binder/Binder_Conversions.cs
@@ -1164,8 +1164,8 @@ namespace Microsoft.CodeAnalysis.CSharp
             }
             else
             {
-                Debug.Assert(targetType.IsMutableArrayInterface(out var typeArgument));
-                Debug.Assert(typeArgument != null);
+                var isMutableArray = targetType.IsMutableArrayInterface(out var typeArgument);
+                Debug.Assert(isMutableArray);
 
                 // For the mutable array interfaces (ICollection<E>, IList<E>), we allow only the no-arg `with()` and
                 // single int arg for the `List(int capacity)` case.
