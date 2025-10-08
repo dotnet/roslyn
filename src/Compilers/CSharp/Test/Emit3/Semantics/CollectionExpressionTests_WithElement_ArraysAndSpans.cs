ac38bbb41ed9be2859fa1d8143073941ae93e738
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-10-08 15:52:59 +0200

    Update test

--- src/Compilers/CSharp/Test/Emit3/Semantics/CollectionExpressionTests_WithElement_ArraysAndSpans.cs
+++ src/Compilers/CSharp/Test/Emit3/Semantics/CollectionExpressionTests_WithElement_ArraysAndSpans.cs
@@ -411,9 +411,9 @@ public sealed class CollectionExpressionTests_WithElement_ArraysAndSpans : CShar
             """;
 
         CreateCompilation(source, targetFramework: TargetFramework.Net90).VerifyDiagnostics(
-            // (8,31): error CS9404: Element type of an interface collection may not be a ref struct or a type parameter allowing ref structs
+            // (7,31): error CS9404: Element type of an interface collection may not be a ref struct or a type parameter allowing ref structs
             //         IEnumerable<T> list = [t1];
-            Diagnostic(ErrorCode.ERR_CollectionRefLikeElementType, "[t1]").WithLocation(8, 31));
+            Diagnostic(ErrorCode.ERR_CollectionRefLikeElementType, "[t1]").WithLocation(7, 31));
     }
 
     [Fact]
