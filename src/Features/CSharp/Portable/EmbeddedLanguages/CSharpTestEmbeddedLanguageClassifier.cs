0aa9fe25f171cf345080396bea74df805b600349
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-10-12 19:47:04 +0200

    Extract out helpers

--- src/Features/CSharp/Portable/EmbeddedLanguages/CSharpTestEmbeddedLanguageClassifier.cs
+++ src/Features/CSharp/Portable/EmbeddedLanguages/CSharpTestEmbeddedLanguageClassifier.cs
@@ -5,17 +5,17 @@
 using System;
 using System.Collections.Generic;
 using System.Composition;
-using System.Text;
 using System.Threading;
 using Microsoft.CodeAnalysis.Classification;
 using Microsoft.CodeAnalysis.Collections;
 using Microsoft.CodeAnalysis.CSharp.EmbeddedLanguages.VirtualChars;
 using Microsoft.CodeAnalysis.EmbeddedLanguages.VirtualChars;
 using Microsoft.CodeAnalysis.Host.Mef;
-using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.CSharp.Features.EmbeddedLanguages;
+
+using static VirtualCharUtilities;
 
 [ExportEmbeddedLanguageClassifier(
     PredefinedEmbeddedLanguageNames.CSharpTest, [LanguageNames.CSharp], supportsUnannotatedAPIs: false,
@@ -24,9 +24,6 @@ namespace Microsoft.CodeAnalysis.CSharp.Features.EmbeddedLanguages;
 [method: Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
 internal sealed class CSharpTestEmbeddedLanguageClassifier() : IEmbeddedLanguageClassifier
 {
-    private static TextSpan FromBounds(VirtualChar vc1, VirtualChar vc2)
-        => TextSpan.FromBounds(vc1.Span.Start, vc2.Span.End);
-
     public void RegisterClassifications(EmbeddedLanguageClassificationContext context)
     {
         var cancellationToken = context.CancellationToken;
@@ -46,10 +43,8 @@ internal sealed class CSharpTestEmbeddedLanguageClassifier() : IEmbeddedLanguage
 
         cancellationToken.ThrowIfCancellationRequested();
 
-        using var _ = ArrayBuilder<TextSpan>.GetInstance(out var markdownSpans);
-
         // First, add all the markdown components (`$$`, `[|`, etc.) into the result.
-        var virtualCharsWithoutMarkup = StripMarkupCharacters(virtualCharsWithMarkup, markdownSpans, cancellationToken);
+        var (virtualCharsWithoutMarkup, markdownSpans) = StripMarkupCharacters(virtualCharsWithMarkup, cancellationToken);
         foreach (var span in markdownSpans)
             context.AddClassification(ClassificationTypeNames.TestCodeMarkdown, span);
 
@@ -118,109 +113,6 @@ internal sealed class CSharpTestEmbeddedLanguageClassifier() : IEmbeddedLanguage
             ClassificationOptions.Default,
             cancellationToken);
         return testFileClassifiedSpans;
-    }
-
-    /// <summary>
-    /// Takes a <see cref="VirtualCharSequence"/> and returns the same characters from it, without any characters
-    /// corresponding to test markup (e.g. <c>$$</c> and the like).  Because the virtual chars contain their
-    /// original text span, these final virtual chars can be used both as the underlying source of a <see
-    /// cref="SourceText"/> (which only cares about their <see cref="char"/> value), as well as the way to then map
-    /// positions/spans within that <see cref="SourceText"/> to actual full virtual char spans in the original
-    /// document for classification.
-    /// </summary>
-    private static ImmutableSegmentedList<VirtualChar> StripMarkupCharacters(
-        VirtualCharSequence virtualChars, ArrayBuilder<TextSpan> markdownSpans, CancellationToken cancellationToken)
-    {
-        var builder = ImmutableSegmentedList.CreateBuilder<VirtualChar>();
-
-        var nestedAnonymousSpanCount = 0;
-        var nestedNamedSpanCount = 0;
-
-        for (int i = 0, n = virtualChars.Length; i < n;)
-        {
-            var vc1 = virtualChars[i];
-            var vc2 = i + 1 < n ? virtualChars[i + 1] : default;
-
-            // These casts are safe because we disallowed virtual chars whose Value doesn't fit in a char in
-            // RegisterClassifications.
-            //
-            // TODO: this algorithm is not actually the one used in roslyn or the roslyn-sdk for parsing a
-            // markup file.  for example it will get `[|]` wrong (as that depends on knowing if we're starting
-            // or ending an existing span).  Fix this up to follow the actual algorithm we use.
-            switch ((vc1.Value, vc2.Value))
-            {
-                case ('$', '$'):
-                    markdownSpans.Add(FromBounds(vc1, vc2));
-                    i += 2;
-                    continue;
-                case ('|', ']'):
-                    nestedAnonymousSpanCount = Math.Max(0, nestedAnonymousSpanCount - 1);
-                    markdownSpans.Add(FromBounds(vc1, vc2));
-                    i += 2;
-                    continue;
-                case ('|', '}'):
-                    markdownSpans.Add(FromBounds(vc1, vc2));
-                    nestedNamedSpanCount = Math.Max(0, nestedNamedSpanCount - 1);
-                    i += 2;
-                    continue;
-
-                // We have a slight ambiguity with cases like these:
-                //
-                // [|]    [|}
-                //
-                // Is it starting a new match, or ending an existing match.  As a workaround, we special case
-                // these and consider it ending a match if we have something on the stack already.
-
-                case ('[', '|'):
-                    var vc3 = i + 2 < n ? virtualChars[i + 2] : default;
-                    if ((vc3 == ']' && nestedAnonymousSpanCount > 0) ||
-                        (vc3 == '}' && nestedNamedSpanCount > 0))
-                    {
-                        // not the start of a span, don't classify this '[' specially.
-                        break;
-                    }
-
-                    nestedAnonymousSpanCount++;
-                    markdownSpans.Add(FromBounds(vc1, vc2));
-                    i += 2;
-                    continue;
-
-                case ('{', '|'):
-                    if (TryConsumeNamedSpanStart(ref i, n))
-                        continue;
-
-                    // didn't find the colon.  don't classify these specially.
-                    break;
-            }
-
-            // Nothing special, add character as is.
-            builder.Add(vc1);
-            i++;
-        }
-
-        cancellationToken.ThrowIfCancellationRequested();
-        return builder.ToImmutable();
-
-        bool TryConsumeNamedSpanStart(ref int i, int n)
-        {
-            var start = i;
-            var seekPoint = i;
-            while (seekPoint < n)
-            {
-                var colonChar = virtualChars[seekPoint];
-                if (colonChar == ':')
-                {
-                    markdownSpans.Add(FromBounds(virtualChars[start], colonChar));
-                    nestedNamedSpanCount++;
-                    i = seekPoint + 1;
-                    return true;
-                }
-
-                seekPoint++;
-            }
-
-            return false;
-        }
     }
 
     private static void AddClassifications(
