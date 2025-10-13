0b35b121561c523fbb635a123cfdd979720b25b4
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-10-13 11:26:46 +0200

    In progress

--- src/Features/Core/Portable/AddConstructorParametersFromMembers/AddConstructorParametersFromMembersCodeRefactoringProvider.AddConstructorParametersCodeAction.cs
+++ src/Features/Core/Portable/AddConstructorParametersFromMembers/AddConstructorParametersFromMembersCodeRefactoringProvider.AddConstructorParametersCodeAction.cs
@@ -14,6 +14,7 @@ using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.GenerateFromMembers;
 using Microsoft.CodeAnalysis.LanguageService;
+using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.AddConstructorParametersFromMembers;
@@ -46,50 +47,54 @@ internal sealed partial class AddConstructorParametersFromMembersCodeRefactoring
         protected override async Task<Solution?> GetChangedSolutionAsync(
             IProgress<CodeAnalysisProgress> progress, CancellationToken cancellationToken)
         {
-            var services = _document.Project.Solution.Services;
             var declarationService = _document.GetRequiredLanguageService<ISymbolDeclarationService>();
             var constructor = declarationService.GetDeclarations(
                 _constructor).Select(r => r.GetSyntax(cancellationToken)).First();
 
-            // Check if this is a primary constructor
-            var isPrimaryConstructor = _constructor.IsPrimaryConstructor();
+            return !_constructor.IsPrimaryConstructor()
+                ? AddParametersToRegularConstructor(constructor, cancellationToken)
+                : await AddParametersAndInitializersToPrimaryConstructorAsync(constructor, cancellationToken).ConfigureAwait(false);
+        }
 
+        private SyntaxNode GetNewConstructor(SyntaxNode oldConstructor, CancellationToken cancellationToken)
+        {
+            var codeGenerator = _document.GetRequiredLanguageService<ICodeGenerationService>();
+            var newConstructor = codeGenerator.AddParameters(
+                oldConstructor, _missingParameters.SelectAsArray(t => t.parameter), _info, cancellationToken);
+
+            return newConstructor;
+        }
+
+        private Solution AddParametersToRegularConstructor(SyntaxNode constructor, CancellationToken cancellationToken)
+        {
             var codeGenerator = _document.GetRequiredLanguageService<ICodeGenerationService>();
 
-            var newConstructor = constructor;
-            newConstructor = codeGenerator.AddParameters(
-                newConstructor, _missingParameters.SelectAsArray(t => t.parameter), _info, cancellationToken);
+            // For regular constructors, add assignment statements
+            var newConstructor = GetNewConstructor(constructor, cancellationToken);
+            newConstructor = codeGenerator
+                .AddStatements(newConstructor, CreateAssignStatements(), _info, cancellationToken)
+                .WithAdditionalAnnotations(Formatter.Annotation);
 
-            if (!isPrimaryConstructor)
-            {
-                // For regular constructors, add assignment statements
-                newConstructor = codeGenerator
-                    .AddStatements(newConstructor, CreateAssignStatements(), _info, cancellationToken)
-                    .WithAdditionalAnnotations(Formatter.Annotation);
+            var syntaxTree = constructor.SyntaxTree;
+            var newRoot = syntaxTree.GetRoot(cancellationToken).ReplaceNode(constructor, newConstructor);
 
-                var syntaxTree = constructor.SyntaxTree;
-                var newRoot = syntaxTree.GetRoot(cancellationToken).ReplaceNode(constructor, newConstructor);
+            // Make sure we get the document that contains the constructor we just updated
+            var constructorDocument = _document.Project.GetDocument(syntaxTree);
+            Contract.ThrowIfNull(constructorDocument);
 
-                // Make sure we get the document that contains the constructor we just updated
-                var constructorDocument = _document.Project.GetDocument(syntaxTree);
-                Contract.ThrowIfNull(constructorDocument);
-
-                return constructorDocument.WithSyntaxRoot(newRoot).Project.Solution;
-            }
-            else
-            {
-                // For primary constructors, we need to:
-                // 1. Update the primary constructor with new parameters
-                // 2. Add initializers to the properties/fields
-                return await AddParametersAndInitializersToPrimaryConstructorAsync(
-                    newConstructor, cancellationToken).ConfigureAwait(false);
-            }
+            return constructorDocument.WithSyntaxRoot(newRoot).Project.Solution;
         }
 
         private async Task<Solution> AddParametersAndInitializersToPrimaryConstructorAsync(
-            SyntaxNode newConstructor,
+            SyntaxNode constructor,
             CancellationToken cancellationToken)
         {
+            // For primary constructors, we need to:
+            // 1. Update the primary constructor with new parameters
+            // 2. Add initializers to the properties/fields
+
+            var newConstructor = GetNewConstructor(constructor, cancellationToken);
+
             var solution = _document.Project.Solution;
             var solutionEditor = new SolutionEditor(solution);
 
@@ -99,9 +104,36 @@ internal sealed partial class AddConstructorParametersFromMembersCodeRefactoring
                 .Select(r => r.GetSyntax(cancellationToken))
                 .First();
 
+            var oldConstructorSyntaxTree = oldConstructor.SyntaxTree;
+
+            // First, update the members in different files.  This is trivial as we're not editing that file as well to
+            // adjust the primary constructor, and thus do not have to worry about order of syntax editing operations.
+            foreach (var (parameter, member) in _missingParameters)
+            {
+                if (member.DeclaringSyntaxReferences is [var syntaxRef, ..] &&
+                    syntaxRef.SyntaxTree != oldConstructorSyntaxTree)
+                {
+                    await AddInitializerToMemberAsync(
+                        solutionEditor, member, parameter, syntaxRef.GetSyntax(cancellationToken), cancellationToken).ConfigureAwait(false);
+                }
+            }
+
             var syntaxTree = oldConstructor.SyntaxTree;
             var documentToUpdate = solution.GetRequiredDocument(syntaxTree);
             var editor = await solutionEditor.GetDocumentEditorAsync(documentToUpdate.Id, cancellationToken).ConfigureAwait(false);
+
+            // Now: 
+            foreach (var (parameter, member) in _missingParameters)
+            {
+                if (member.DeclaringSyntaxReferences is [var syntaxRef, ..] &&
+                    syntaxRef.SyntaxTree == oldConstructorSyntaxTree)
+                {
+                    nodesToTrack.Add(syntaxRef.GetSyntax(cancellationToken));
+                }
+            }
+
+            var trackedRoot = root.TrackNodes(nodesToTrack);
+
             editor.ReplaceNode(oldConstructor, newConstructor.WithAdditionalAnnotations(Formatter.Annotation));
 
             // Now add initializers to each member
@@ -118,31 +150,24 @@ internal sealed partial class AddConstructorParametersFromMembersCodeRefactoring
             SolutionEditor solutionEditor,
             ISymbol member,
             IParameterSymbol parameter,
+            SyntaxNode memberSyntax,
             CancellationToken cancellationToken)
         {
             var solution = solutionEditor.OriginalSolution;
             var generator = _document.GetRequiredLanguageService<SyntaxGenerator>();
 
-            // Create the initializer expression using the parameter name
-            var initializerExpression = generator.IdentifierName(parameter.Name);
+            var memberDocument = solution.GetRequiredDocument(memberSyntax.SyntaxTree);
+            var memberEditor = await solutionEditor.GetDocumentEditorAsync(memberDocument.Id, cancellationToken).ConfigureAwait(false);
 
-            // Wrap the initializer expression in an EqualsValueClause
-            var equalsValueClause = generator.SyntaxGeneratorInternal.EqualsValueClause(initializerExpression);
+            var equalsValueClause = generator.SyntaxGeneratorInternal.EqualsValueClause(
+                generator.IdentifierName(parameter.Name));
 
-            foreach (var syntaxRef in member.DeclaringSyntaxReferences)
-            {
-                var memberSyntax = syntaxRef.GetSyntax(cancellationToken);
-                var memberDocument = solution.GetRequiredDocument(memberSyntax.SyntaxTree);
-                var memberEditor = await solutionEditor.GetDocumentEditorAsync(memberDocument.Id, cancellationToken).ConfigureAwait(false);
+            // Add the initializer to the member - use different methods for properties vs fields
+            var newMemberSyntax = member is IPropertySymbol
+                ? generator.SyntaxGeneratorInternal.WithPropertyInitializer(memberSyntax, equalsValueClause)
+                : generator.WithInitializer(memberSyntax, equalsValueClause);
 
-                // Add the initializer to the member - use different methods for properties vs fields
-                var newMemberSyntax = member is IPropertySymbol
-                    ? generator.SyntaxGeneratorInternal.WithPropertyInitializer(memberSyntax, equalsValueClause)
-                    : generator.WithInitializer(memberSyntax, equalsValueClause);
-
-                memberEditor.ReplaceNode(memberSyntax, newMemberSyntax);
-                break;
-            }
+            memberEditor.ReplaceNode(memberSyntax, newMemberSyntax);
         }
 
         private IEnumerable<SyntaxNode> CreateAssignStatements()
@@ -150,12 +175,12 @@ internal sealed partial class AddConstructorParametersFromMembersCodeRefactoring
             var factory = _document.GetRequiredLanguageService<SyntaxGenerator>();
             foreach (var (parameter, fieldOrProperty) in _missingParameters)
             {
-                var memberName = fieldOrProperty.Name;
-                var parameterName = parameter.Name;
                 yield return factory.ExpressionStatement(
                     factory.AssignmentStatement(
-                        factory.MemberAccessExpression(factory.ThisExpression(), factory.IdentifierName(memberName)),
-                        factory.IdentifierName(parameterName)));
+                        factory.MemberAccessExpression(
+                            factory.ThisExpression(),
+                            factory.IdentifierName(fieldOrProperty.Name)),
+                        factory.IdentifierName(parameter.Name)));
             }
         }
 
@@ -168,15 +193,11 @@ internal sealed partial class AddConstructorParametersFromMembersCodeRefactoring
                 var signature = $"{_containingType.Name}({parameterString})";
 
                 if (_useSubMenuName)
-                {
                     return string.Format(CodeFixesResources.Add_to_0, signature);
-                }
-                else
-                {
-                    return _missingParameters[0].parameter.IsOptional
-                        ? string.Format(FeaturesResources.Add_optional_parameters_to_0, signature)
-                        : string.Format(FeaturesResources.Add_parameters_to_0, signature);
-                }
+
+                return _missingParameters[0].parameter.IsOptional
+                    ? string.Format(FeaturesResources.Add_optional_parameters_to_0, signature)
+                    : string.Format(FeaturesResources.Add_parameters_to_0, signature);
             }
         }
 
