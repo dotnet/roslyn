bb7238969567e169080e8eddf0a855dede84b4ee
Peter Waldschmidt <peter@waldschmidt.com>
2025-10-14 14:10:45 -0400

    Revert "Add using"
    This reverts commit 62b83beb4ac4fe975f51d9021834401a6f80d173.

--- src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpSourceGenerators.cs
+++ src/VisualStudio/IntegrationTest/New.IntegrationTests/CSharp/CSharpSourceGenerators.cs
@@ -11,7 +11,6 @@ using Microsoft.CodeAnalysis.Test.Utilities;
 using Microsoft.CodeAnalysis.TestSourceGenerator;
 using Microsoft.VisualStudio.IntegrationTest.Utilities;
 using Microsoft.VisualStudio.LanguageServices;
-using Roslyn.Test.Utilities;
 using Roslyn.VisualStudio.IntegrationTests;
 using Roslyn.VisualStudio.IntegrationTests.InProcess;
 using Roslyn.VisualStudio.NewIntegrationTests.InProcess;
@@ -181,7 +180,8 @@ public sealed class CSharpSourceGenerators()
         Assert.Equal(HelloWorldGenerator.GeneratedFolderClassName, await TestServices.Editor.GetSelectedTextAsync(HangMitigatingCancellationToken));
     }
 
-    [IdeFact, WorkItem("https://github.com/dotnet/roslyn/issues/77895")]
+    [IdeFact]
+    [WorkItem("https://github.com/dotnet/roslyn/issues/77895")]
     public async Task GoToDefinitionWithPartialUserAndGeneratedCode()
     {
         // This test verifies that when a type has both user code and generated code definitions,
