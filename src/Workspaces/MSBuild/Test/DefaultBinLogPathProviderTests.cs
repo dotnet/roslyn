95400ebd22b28fd64892d235a0075c6943102236
Joey Robichaud <jorobich@microsoft.com>
2025-10-13 23:29:27 -0700

    Add limited support for binarylogger to MSBuildWorkspace (#80120)
    * Add limited support for binarylogger to MSBuildWorkspace
    
    With the move to using a separate BuildHost we lost binlog support in MSBuildWorkspace. The issue being serializing arbitrary MSBuild ILogger implementations to send OOP. This change check the logger's type and if it is BinaryLogger we send it across using a BinLogPathProvider.
    
    * Extract helper method
    
    * Remove unnecessary pragma
    
    * Update DefaultBinLogPathProvider to return unique paths
    
    * Add comment
    
    * Fix unit tests
    
    * Fix binarylogger test
    
    * Apply suggestions from code review
    
    Co-authored-by: Rikki Gibson <rigibson@microsoft.com>
    
    * Duplicate consts in tests
    
    * Update src/Workspaces/MSBuild/Core/MSBuild/MSBuildProjectLoader.cs
    
    * Fix up
    
    * PR Feedback
    
    * Update src/Workspaces/MSBuild/Core/MSBuild/MSBuildProjectLoader.cs
    
    Co-authored-by: Jason Malinowski <jason.malinowski@microsoft.com>
    
    ---------
    
    Co-authored-by: Rikki Gibson <rigibson@microsoft.com>
    Co-authored-by: Jason Malinowski <jason.malinowski@microsoft.com>

---
+++ src/Workspaces/MSBuild/Test/DefaultBinLogPathProviderTests.cs
@@ -0,0 +1,64 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using Xunit;
+using static Microsoft.CodeAnalysis.MSBuild.MSBuildProjectLoader;
+
+namespace Microsoft.CodeAnalysis.MSBuild.UnitTests;
+
+public sealed class DefaultBinLogPathProviderTests
+{
+    private const string DefaultFileName = "msbuild";
+    private const string DefaultExtension = ".binlog";
+
+    private static string RelativeLogDirectory => $".{Path.DirectorySeparatorChar}logs";
+    private static string LogDirectory => Path.GetFullPath(RelativeLogDirectory);
+    private static string LogFileName => "mylog";
+    private static string LogExtension => ".mylog";
+
+    [Fact]
+    public void DefaultBinLogPathProvider_ExpandsRelativePath()
+    {
+        var logPath = Path.Combine(RelativeLogDirectory, LogFileName + LogExtension);
+        var provider = new BinLogPathProvider(logPath);
+        AssertUniquePaths(provider, LogDirectory, LogFileName, LogExtension);
+    }
+
+    [Fact]
+    public void DefaultBinLogPathProvider_UsesDefaultExtension()
+    {
+        var logPath = Path.Combine(LogDirectory, LogFileName);
+        var provider = new BinLogPathProvider(logPath);
+        AssertUniquePaths(provider, LogDirectory, LogFileName, DefaultExtension);
+    }
+
+    [Fact]
+    public void DefaultBinLogPathProvider_UsesDefaultFileName()
+    {
+        var provider = new BinLogPathProvider(LogDirectory + Path.DirectorySeparatorChar);
+        AssertUniquePaths(provider, LogDirectory, DefaultFileName, DefaultExtension);
+    }
+
+    private static void AssertUniquePaths(BinLogPathProvider provider, string expectedDirectory, string expectedFilePrefix, string expectedExtension)
+    {
+        var newLogPaths = Enumerable.Range(0, 10)
+            .Select(_ => provider.GetNewLogPath())
+            .ToImmutableHashSet();
+        Assert.Equal(10, newLogPaths.Count);
+
+        foreach (var newLogPath in newLogPaths)
+        {
+            var newLogDirectory = Path.GetDirectoryName(newLogPath);
+            var newLogFileName = Path.GetFileNameWithoutExtension(newLogPath);
+            var newLogExtension = Path.GetExtension(newLogPath);
+
+            Assert.Equal(expectedDirectory, newLogDirectory);
+            Assert.StartsWith(expectedFilePrefix, newLogFileName);
+            Assert.Equal(expectedExtension, newLogExtension);
+        }
+    }
+}
