20995b5d57709cddd47a5bdd9e90a187e0c1b193
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-10-17 13:14:29 +0200

    Downstream fallout

--- src/Analyzers/CSharp/Tests/RemoveUnnecessaryUnsafeModifier/RemoveUnnecessaryUnsafeModifierTests.cs
+++ src/Analyzers/CSharp/Tests/RemoveUnnecessaryUnsafeModifier/RemoveUnnecessaryUnsafeModifierTests.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CSharp.RemoveUnnecessarySuppressions;
 using Microsoft.CodeAnalysis.CSharp.RemoveUnnecessaryUnsafeModifier;
 using Microsoft.CodeAnalysis.Editor.UnitTests.CodeActions;
 using Microsoft.CodeAnalysis.Test.Utilities;
