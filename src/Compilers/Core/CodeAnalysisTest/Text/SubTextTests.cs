4c78fb50a07bab8a2e8220960f0074b26ef28e8d
Todd Grunke <toddgrun@microsoft.com>
2025-10-26 18:33:37 -0700

    Fix issue with when a CR/NL split occurs at a SubText's underlying span start
    I hit an exception in the SourceText system when inserting a copilot edit for C# content in a razor file when the file uses newlines only. This ended up causing a CR/NL split in the subtext class, which had some bogus logic for when that happened.
    
    Added a very simple unit test that demonstrated previous failure.

---
+++ src/Compilers/Core/CodeAnalysisTest/Text/SubTextTests.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis.Text;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.UnitTests.Text
+{
+    public class SubTextTests
+    {
+        [Fact]
+        public void SubTextCarriageReturnNewLineLSplit()
+        {
+            var fullStringText = SourceText.From("\r\n");
+            var subText = new SubText(fullStringText, TextSpan.FromBounds(1, 2));
+
+            Assert.Equal(1, subText.Lines.IndexOf(1));
+        }
+    }
+}
