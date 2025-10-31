06cbfeaabd3cdbe1a0613e6121ec54645d4f01f5
Rekkonnect <kalfas.alex@gmail.com>
2025-10-31 22:50:35 +0100

    use block scoped namespace

--- src/Compilers/CSharp/Portable/Binder/ConstantFieldsInProgress.cs
+++ src/Compilers/CSharp/Portable/Binder/ConstantFieldsInProgress.cs
@@ -7,52 +7,53 @@
 using System.Collections.Generic;
 using Microsoft.CodeAnalysis.CSharp.Symbols;
 
-namespace Microsoft.CodeAnalysis.CSharp;
-
-/// <summary>
-/// This is used while computing the values of constant fields.  Since they can depend on each
-/// other, we need to keep track of which ones we are currently computing in order to avoid (and
-/// report) cycles.
-/// </summary>
-internal sealed class ConstantFieldsInProgress
+namespace Microsoft.CodeAnalysis.CSharp
 {
-    private readonly SourceFieldSymbol _fieldOpt;
-    private readonly HashSet<SourceFieldSymbolWithSyntaxReference> _dependencies;
-
     /// <summary>
-    /// Stores the last dependency that was added to the set. This is used to check if
-    /// the dependency that was added is not after all a dependency after successful rebinding
-    /// in Color Color resolution, i.e. `public const Color Color = Color.Red;`
+    /// This is used while computing the values of constant fields.  Since they can depend on each
+    /// other, we need to keep track of which ones we are currently computing in order to avoid (and
+    /// report) cycles.
     /// </summary>
-    private SourceFieldSymbolWithSyntaxReference _lastDependency;
-
-    internal static readonly ConstantFieldsInProgress Empty = new ConstantFieldsInProgress(null, null);
-
-    internal ConstantFieldsInProgress(
-        SourceFieldSymbol fieldOpt,
-        HashSet<SourceFieldSymbolWithSyntaxReference> dependencies)
+    internal sealed class ConstantFieldsInProgress
     {
-        _fieldOpt = fieldOpt;
-        _dependencies = dependencies;
-    }
+        private readonly SourceFieldSymbol _fieldOpt;
+        private readonly HashSet<SourceFieldSymbolWithSyntaxReference> _dependencies;
 
-    public bool IsEmpty
-    {
-        get { return (object)_fieldOpt == null; }
-    }
+        /// <summary>
+        /// Stores the last dependency that was added to the set. This is used to check if
+        /// the dependency that was added is not after all a dependency after successful rebinding
+        /// in Color Color resolution, i.e. `public const Color Color = Color.Red;`
+        /// </summary>
+        private SourceFieldSymbolWithSyntaxReference _lastDependency;
 
-    internal void AddDependency(SourceFieldSymbolWithSyntaxReference field)
-    {
-        _dependencies.Add(field);
-        _lastDependency = field;
-    }
+        internal static readonly ConstantFieldsInProgress Empty = new ConstantFieldsInProgress(null, null);
 
-    internal void RemoveIfLastDependency(SourceFieldSymbolWithSyntaxReference field)
-    {
-        if (_lastDependency is not null && _lastDependency == (object)field)
+        internal ConstantFieldsInProgress(
+            SourceFieldSymbol fieldOpt,
+            HashSet<SourceFieldSymbolWithSyntaxReference> dependencies)
         {
-            _dependencies.Remove(_lastDependency);
-            _lastDependency = null;
+            _fieldOpt = fieldOpt;
+            _dependencies = dependencies;
+        }
+
+        public bool IsEmpty
+        {
+            get { return (object)_fieldOpt == null; }
+        }
+
+        internal void AddDependency(SourceFieldSymbolWithSyntaxReference field)
+        {
+            _dependencies.Add(field);
+            _lastDependency = field;
+        }
+
+        internal void RemoveIfLastDependency(SourceFieldSymbolWithSyntaxReference field)
+        {
+            if (_lastDependency is not null && _lastDependency == (object)field)
+            {
+                _dependencies.Remove(_lastDependency);
+                _lastDependency = null;
+            }
         }
     }
 }
