11cdca215fd15151b94ef5bb0ce99526d16923f6
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-11-05 14:20:40 -0800

    Simplify

--- src/Features/Core/Portable/DocumentHighlighting/AbstractDocumentHighlightsService.cs
+++ src/Features/Core/Portable/DocumentHighlighting/AbstractDocumentHighlightsService.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
-using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
