5547fbb92a12383ad2f9209561efd0b87101ca2d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-11-10 23:36:16 +0100

    Make FAR and Rename work on cref-type parameters

--- src/Workspaces/Core/Portable/FindSymbols/FindReferences/Finders/ReferenceFinders.cs
+++ src/Workspaces/Core/Portable/FindSymbols/FindReferences/Finders/ReferenceFinders.cs
@@ -12,6 +12,7 @@ internal static class ReferenceFinders
     public static readonly ImmutableArray<IReferenceFinder> DefaultRenameReferenceFinders = [
             AliasSymbolReferenceFinder.Instance,
             ConstructorSymbolReferenceFinder.Instance,
+            CrefTypeParameterSymbolReferenceFinder.Instance,
             PropertySymbolReferenceFinder.Instance,
             new DestructorSymbolReferenceFinder(),
             DynamicTypeSymbolReferenceFinder.Instance,
@@ -21,7 +22,7 @@ internal static class ReferenceFinders
             new FieldSymbolReferenceFinder(),
             new LabelSymbolReferenceFinder(),
             new LocalSymbolReferenceFinder(),
-            new MethodTypeParameterSymbolReferenceFinder(),
+            MethodTypeParameterSymbolReferenceFinder.Instance,
             new NamedTypeSymbolReferenceFinder(),
             new NamespaceSymbolReferenceFinder(),
             new OperatorSymbolReferenceFinder(),
@@ -30,7 +31,7 @@ internal static class ReferenceFinders
             new PreprocessingSymbolReferenceFinder(),
             new PropertyAccessorSymbolReferenceFinder(),
             new RangeVariableSymbolReferenceFinder(),
-            new TypeParameterSymbolReferenceFinder(),
+            TypeParameterSymbolReferenceFinder.Instance,
         ];
 
     /// <summary>
