32a1361d12ed6f06bacaf480e84ad099845fd9e0
Todd Grunke <toddgrun@microsoft.com>
2025-11-13 14:09:13 -0800

    For now, don't publish packages with VS dependencies

--- src/Tools/dotnet-roslyn-tools/Tool/NuGet/NuGetPrepare.cs
+++ src/Tools/dotnet-roslyn-tools/Tool/NuGet/NuGetPrepare.cs
@@ -21,7 +21,6 @@ internal class NuGetPrepare
         "Microsoft.CodeAnalysis.CSharp.Features",
         "Microsoft.CodeAnalysis.CSharp.Scripting",
         "Microsoft.CodeAnalysis.CSharp.Workspaces",
-        "Microsoft.CodeAnalysis.EditorFeatures.Text",
         "Microsoft.CodeAnalysis.Features",
         "Microsoft.CodeAnalysis.Scripting",
         "Microsoft.CodeAnalysis.Scripting.Common",
@@ -33,7 +32,10 @@ internal class NuGetPrepare
         "Microsoft.CodeAnalysis.Workspaces.MSBuild",
         "Microsoft.Net.Compilers.Toolset",
         "Microsoft.Net.Compilers.Toolset.Framework",
-        "Microsoft.VisualStudio.LanguageServices",
+
+        // These rely on Visual Studio packages and should not be published at this time.
+        //"Microsoft.CodeAnalysis.EditorFeatures.Text",
+        //"Microsoft.VisualStudio.LanguageServices",
 
         // These are the RoslynAnalyzer packages. We should not publish them at this time.
         //"Microsoft.CodeAnalysis.Analyzers",
