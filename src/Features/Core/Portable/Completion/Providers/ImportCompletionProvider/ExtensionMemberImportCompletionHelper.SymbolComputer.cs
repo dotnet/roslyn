79802d48695c3234c715b4b8a0f028032fb7dcbe
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-11-14 15:30:28 +0100

    Cleanup

--- src/Features/Core/Portable/Completion/Providers/ImportCompletionProvider/ExtensionMemberImportCompletionHelper.SymbolComputer.cs
+++ src/Features/Core/Portable/Completion/Providers/ImportCompletionProvider/ExtensionMemberImportCompletionHelper.SymbolComputer.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Collections;
