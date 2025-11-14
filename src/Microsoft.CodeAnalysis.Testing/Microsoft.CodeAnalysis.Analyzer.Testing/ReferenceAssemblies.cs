0d3ea7ad817b2d24f76e64f3dd9673fd60afb266
Phil Allen <phillipa@microsoft.com>
2025-11-14 08:49:37 -0800

    Update package versions (part 2) (#1234)
    Update package versions (part 2)
    * SDK
    * System.Format.Asn1
    * Nuget.*
    * Now that nuget packages are updated, can converge some #if related logic

--- src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/ReferenceAssemblies.cs
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/ReferenceAssemblies.cs
@@ -15,13 +15,10 @@ using NuGet.Configuration;
 using NuGet.Frameworks;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
+using NuGet.Packaging.Signing;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
 using NuGet.Resolver;
-
-#if NUGET_SIGNING
-using NuGet.Packaging.Signing;
-#endif
 
 namespace Microsoft.CodeAnalysis.Testing
 {
@@ -91,14 +88,10 @@ namespace Microsoft.CodeAnalysis.Testing
         {
             get
             {
-#if NETSTANDARD1_6
-                return NetStandard.NetStandard16;
-#elif NETSTANDARD2_0
+#if NETSTANDARD2_0
                 return NetStandard.NetStandard20;
-#elif NET452
-                return NetFramework.Net452.Default;
-#elif NET46
-                return NetFramework.Net46.Default;
+#elif NET461
+                return NetFramework.Net461.Default;
 #elif NET472
                 return NetFramework.Net472.Default;
 #elif NETCOREAPP3_1
@@ -345,21 +338,11 @@ namespace Microsoft.CodeAnalysis.Testing
 
                 var globalPathResolver = new PackagePathResolver(SettingsUtility.GetGlobalPackagesFolder(settings));
                 var localPathResolver = new PackagePathResolver(temporaryPackagesFolder);
-#if NET452 || NET46 || NETSTANDARD1_6
-                var packageExtractionContext = new PackageExtractionContext(logger)
-                {
-                    PackageSaveMode = PackageSaveMode.Defaultv3,
-                    XmlDocFileSaveMode = XmlDocFileSaveMode.None,
-                };
-#elif NUGET_SIGNING
                 var packageExtractionContext = new PackageExtractionContext(
                     PackageSaveMode.Defaultv3,
                     XmlDocFileSaveMode.None,
                     ClientPolicyContext.GetClientPolicy(settings, logger),
                     logger);
-#else
-#error The current target framework is not supported.
-#endif
 
                 var frameworkReducer = new FrameworkReducer();
 
@@ -421,11 +404,7 @@ namespace Microsoft.CodeAnalysis.Testing
                         if (downloadResult.Status == DownloadResourceResultStatus.AvailableWithoutStream)
                         {
                             await PackageExtractor.ExtractPackageAsync(
-#if NUGET_SIGNING
-#pragma warning disable SA1114 // Parameter list should follow declaration
                                 downloadResult.PackageSource,
-#pragma warning restore SA1114 // Parameter list should follow declaration
-#endif
                                 downloadResult.PackageReader,
                                 localPathResolver,
                                 packageExtractionContext,
@@ -435,11 +414,7 @@ namespace Microsoft.CodeAnalysis.Testing
                         {
                             Debug.Assert(downloadResult.PackageStream != null, "PackageStream should not be null if download result status != DownloadResourceResultStatus.AvailableWithoutStream");
                             await PackageExtractor.ExtractPackageAsync(
-#if NUGET_SIGNING
-#pragma warning disable SA1114 // Parameter list should follow declaration
                                 downloadResult.PackageSource,
-#pragma warning restore SA1114 // Parameter list should follow declaration
-#endif
                                 downloadResult.PackageStream,
                                 localPathResolver,
                                 packageExtractionContext,
@@ -677,9 +652,7 @@ namespace Microsoft.CodeAnalysis.Testing
                 var dependencyInfo = await dependencyInfoResource.ResolvePackage(
                     packageIdentity,
                     targetFramework,
-#if NUGET_SIGNING
                     cacheContext,
-#endif
                     logger,
                     cancellationToken);
                 if (dependencyInfo is null)
