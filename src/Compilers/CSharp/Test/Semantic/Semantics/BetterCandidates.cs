30ef4d62203eb272eab779abf84c34c4c55a8922
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-11-17 19:33:20 +0100

    Update tests

--- src/Compilers/CSharp/Test/Semantic/Semantics/BetterCandidates.cs
+++ src/Compilers/CSharp/Test/Semantic/Semantics/BetterCandidates.cs
@@ -302,13 +302,7 @@ class MyDeconstructable
             CreateCompilationWithoutBetterCandidates(source, options: TestOptions.ReleaseExe).VerifyDiagnostics(
                 // (6,26): error CS0121: The call is ambiguous between the following methods or properties: 'MyDeconstructable.Deconstruct(out int, out int)' and 'MyDeconstructable.Deconstruct(out long, out long)'
                 //         (var a, var b) = o;
-                Diagnostic(ErrorCode.ERR_AmbigCall, "o").WithArguments("MyDeconstructable.Deconstruct(out int, out int)", "MyDeconstructable.Deconstruct(out long, out long)").WithLocation(6, 26),
-                // (6,14): error CS8130: Cannot infer the type of implicitly-typed deconstruction variable 'a'.
-                //         (var a, var b) = o;
-                Diagnostic(ErrorCode.ERR_TypeInferenceFailedForImplicitlyTypedDeconstructionVariable, "a").WithArguments("a").WithLocation(6, 14),
-                // (6,21): error CS8130: Cannot infer the type of implicitly-typed deconstruction variable 'b'.
-                //         (var a, var b) = o;
-                Diagnostic(ErrorCode.ERR_TypeInferenceFailedForImplicitlyTypedDeconstructionVariable, "b").WithArguments("b").WithLocation(6, 21)
+                Diagnostic(ErrorCode.ERR_AmbigCall, "o").WithArguments("MyDeconstructable.Deconstruct(out int, out int)", "MyDeconstructable.Deconstruct(out long, out long)").WithLocation(6, 26)
                 );
             var compilation = CreateCompilationWithBetterCandidates(source, options: TestOptions.ReleaseExe).VerifyDiagnostics(
                 );
