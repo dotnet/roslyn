58e2ed342ba5bf9dcf3a36e177a29cb64ad1e5c0
David Barbet <dabarbet@microsoft.com>
2025-11-18 11:39:52 -0800

    feedback

--- src/LanguageServer/Protocol/Handler/WorkDoneProgress/WorkDoneProgressManager.cs
+++ src/LanguageServer/Protocol/Handler/WorkDoneProgress/WorkDoneProgressManager.cs
@@ -20,6 +20,69 @@ class WorkDoneProgressManager(IClientLanguageServerManager clientLanguageServerM
 {
     private readonly IClientLanguageServerManager _clientLanguageServerManager = clientLanguageServerManager;
 
+    /// <summary>
+    /// Guards access to <see cref="_progressReporters"/>.
+    /// While generally a single thread acts on a single <see cref="WorkDoneProgressReporter"/>,
+    /// a single reporter may get concurrent requests to cancel while the server is disposing of it.
+    /// Additionally multiple threads may create separate reporters concurrently.
+    /// </summary>
+    private readonly object _progressLock = new();
+
+    /// <summary>
+    /// Tracks active work done progress reporters by their token.
+    /// Required so we can cancel them when the client requests us to.
+    /// Guarded by <see cref="_progressLock"/> 
+    /// </summary>
+    /// <remarks>
+    /// A singe entry is added once to the dictionary when a new work done progress session is initiated.
+    /// Multiple threads may create new sessions concurrently.  Additionally, a single entry may have
+    /// have a concurrent request from the client to cancel while the server is disposing of it.
+    /// </remarks>
+    private readonly Dictionary<string, WorkDoneProgressReporter> _progressReporters = [];
+
+    /// <summary>
+    /// Initiates a new work done progress reporting session with the client.
+    /// This sends the initial `window/workDoneProgress/create` request, but callers are responsible for sending the
+    /// begin and end reports.
+    /// In the case of server side cancellation, an end report will be sent automatically with a "Cancelled" message.
+    /// </summary>
+    /// <param name="serverCancellationToken">a cancellation token that signals when the server wants to cancel the operation</param>
+    public async Task<IWorkDoneProgressReporter> CreateWorkDoneProgressAsync(bool reportProgressToClient, CancellationToken serverCancellationToken)
+    {
+        var token = Guid.NewGuid().ToString();
+        IWorkDoneProgressReporter reporter;
+        if (reportProgressToClient)
+        {
+            var clientReporter = new WorkDoneProgressReporter(token, this, serverCancellationToken);
+            await clientReporter.SendCreateRequestAsync().ConfigureAwait(false);
+            lock (_progressLock)
+            {
+                _progressReporters[token] = clientReporter;
+            }
+
+            return clientReporter;
+        }
+        else
+        {
+            reporter = new NoOpProgressReporter(serverCancellationToken);
+        }
+
+        return reporter;
+    }
+
+    public void CancelWorkDoneProgress(string token)
+    {
+        lock (_progressLock)
+        {
+            // We may be handling a client cancellation request after the server already completed and disposed of the progress.
+            // Check that we still have a non-disposed reporter for this token.
+            if (_progressReporters.TryGetValue(token, out var reporter))
+            {
+                reporter.CancelSource_NoLock();
+            }
+        }
+    }
+
     private class WorkDoneProgressReporter : IWorkDoneProgressReporter
     {
         private readonly WorkDoneProgressManager _manager;
@@ -96,68 +159,6 @@ class WorkDoneProgressManager(IClientLanguageServerManager clientLanguageServerM
         private record struct ProgressReportType(
             [property: JsonPropertyName("token")] string Token,
             [property: JsonPropertyName("value")] WorkDoneProgress Value);
-    }
-
-    /// <summary>
-    /// Guards access to <see cref="_progressReporters"/>.
-    /// While generally a single thread acts on a single <see cref="WorkDoneProgressReporter"/>,
-    /// a single reporter may get concurrent requests to cancel while the server is disposing of it.
-    /// Additionally multiple threads may create separate reporters concurrently.
-    /// </summary>
-    private readonly object _progressLock = new();
-
-    /// <summary>
-    /// Tracks active work done progress reporters by their token.
-    /// Required so we can cancel them when the client requests us to.
-    /// </summary>
-    /// <remarks>
-    /// A singe entry is added once to the dictionary when a new work done progress session is initiated.
-    /// Multiple threads may create new sessions concurrently.  Additionally, a single entry may have
-    /// have a concurrent request from the client to cancel while the server is disposing of it.
-    /// </remarks>
-    private readonly Dictionary<string, WorkDoneProgressReporter> _progressReporters = [];
-
-    /// <summary>
-    /// Initiates a new work done progress reporting session with the client.
-    /// This sends the initial `window/workDoneProgress/create` request, but callers are responsible for sending the
-    /// begin and end reports.
-    /// In the case of server side cancellation, an end report will be sent automatically with a "Cancelled" message.
-    /// </summary>
-    /// <param name="serverCancellationToken">a cancellation token that signals when the server wants to cancel the operation</param>
-    public async Task<IWorkDoneProgressReporter> CreateWorkDoneProgressAsync(bool reportProgressToClient, CancellationToken serverCancellationToken)
-    {
-        var token = Guid.NewGuid().ToString();
-        IWorkDoneProgressReporter reporter;
-        if (reportProgressToClient)
-        {
-            var clientReporter = new WorkDoneProgressReporter(token, this, serverCancellationToken);
-            await clientReporter.SendCreateRequestAsync().ConfigureAwait(false);
-            lock (_progressLock)
-            {
-                _progressReporters[token] = clientReporter;
-            }
-
-            return clientReporter;
-        }
-        else
-        {
-            reporter = new NoOpProgressReporter(serverCancellationToken);
-        }
-
-        return reporter;
-    }
-
-    public void CancelWorkDoneProgress(string token)
-    {
-        lock (_progressLock)
-        {
-            // We may be handling a client cancellation request after the server already completed and disposed of the progress.
-            // Check that we still have a non-disposed reporter for this token.
-            if (_progressReporters.TryGetValue(token, out var reporter))
-            {
-                reporter.CancelSource_NoLock();
-            }
-        }
     }
 
     private record struct NoOpProgressReporter(CancellationToken cancellationToken) : IWorkDoneProgressReporter
