ad5aea7e5299560537871f318997b1d643bbf833
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-11-29 13:34:41 +0100

    Update c#

--- src/Features/CSharp/Portable/AddImport/CSharpAddImportCodeRefactoringProvider.cs
+++ src/Features/CSharp/Portable/AddImport/CSharpAddImportCodeRefactoringProvider.cs
@@ -6,10 +6,20 @@ using System.Composition;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis.AddImport;
 using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp.LanguageService;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Microsoft.CodeAnalysis.CSharp.AddImport;
 
 [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = PredefinedCodeRefactoringProviderNames.AddImport), Shared]
 [method: ImportingConstructor]
 [method: SuppressMessage("RoslynDiagnosticsReliability", "RS0033:Importing constructor should be [Obsolete]", Justification = "Used in test code: https://github.com/dotnet/roslyn/issues/42814")]
-internal sealed class CSharpAddImportCodeRefactoringProvider() : AbstractAddImportCodeRefactoringProvider;
+internal sealed class CSharpAddImportCodeRefactoringProvider()
+    : AbstractAddImportCodeRefactoringProvider<
+        ExpressionSyntax,
+        MemberAccessExpressionSyntax,
+        NameSyntax,
+        SimpleNameSyntax,
+        QualifiedNameSyntax,
+        AliasQualifiedNameSyntax,
+        UsingDirectiveSyntax>(CSharpSyntaxFacts.Instance);
