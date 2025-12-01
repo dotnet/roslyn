70f7eb0bf339e2df9395c5d98d1cde49c3fd239b
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-12-01 20:13:21 +0100

    Updaate comment

--- src/Compilers/Core/Portable/InternalUtilities/ConsList`1.cs
+++ src/Compilers/Core/Portable/InternalUtilities/ConsList`1.cs
@@ -49,8 +49,6 @@ namespace Roslyn.Utilities
 
                 if (newTail != null)
                 {
-                    // Suppress false positive CS8717 reported for MaybeNull assignment to AllowNull
-                    // https://github.com/dotnet/roslyn/issues/38926
                     _current = currentTail._head;
                     _tail = newTail;
                     return true;
