e70f9c0d8fe252268b41d616a6f5620bb7f5d529
Julien Couvreur <julien.couvreur@gmail.com>
2025-12-02 08:55:49 -0800

    Temporarily add dummy/failing test

---
+++ src/Compilers/CSharp/Test/Test15/DummyTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Microsoft.CodeAnalysis.CSharp.UnitTests;
+
+public class DummyTest
+{
+    [Fact]
+    public void TestMethod()
+    {
+        Assert.True(false);
+    }
+}
