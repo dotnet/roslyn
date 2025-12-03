ebcca9313fa7ae0b2e8d5ec2573f82ab649dfbb8
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-12-03 23:59:41 +0100

    Remove unnecessary using

--- src/Compilers/CSharp/Portable/BoundTree/BoundCollectionExpression.cs
+++ src/Compilers/CSharp/Portable/BoundTree/BoundCollectionExpression.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Microsoft.CodeAnalysis.CSharp
 {
