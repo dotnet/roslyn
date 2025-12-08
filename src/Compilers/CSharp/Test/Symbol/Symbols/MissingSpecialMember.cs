d65388b2d5cad764148226bab09cc83f4db8eb29
Jan Jones <janjones@microsoft.com>
2025-12-08 12:34:34 +0100

    Update well-known member tests

--- src/Compilers/CSharp/Test/Symbol/Symbols/MissingSpecialMember.cs
+++ src/Compilers/CSharp/Test/Symbol/Symbols/MissingSpecialMember.cs
@@ -645,6 +645,7 @@ namespace System
                     case WellKnownType.System_Runtime_CompilerServices_ScopedRefAttribute:
                     case WellKnownType.System_Runtime_CompilerServices_RefSafetyRulesAttribute:
                     case WellKnownType.System_Runtime_CompilerServices_MemorySafetyRulesAttribute:
+                    case WellKnownType.System_Runtime_CompilerServices_RequiresUnsafeAttribute:
                     case WellKnownType.System_MemoryExtensions:
                     case WellKnownType.System_Runtime_CompilerServices_CompilerFeatureRequiredAttribute:
                     case WellKnownType.System_Diagnostics_CodeAnalysis_UnscopedRefAttribute:
@@ -1032,6 +1033,7 @@ namespace System
                     case WellKnownMember.System_Runtime_CompilerServices_ScopedRefAttribute__ctor:
                     case WellKnownMember.System_Runtime_CompilerServices_RefSafetyRulesAttribute__ctor:
                     case WellKnownMember.System_Runtime_CompilerServices_MemorySafetyRulesAttribute__ctor:
+                    case WellKnownMember.System_Runtime_CompilerServices_RequiresUnsafeAttribute__ctor:
                     case WellKnownMember.System_MemoryExtensions__SequenceEqual_Span_T:
                     case WellKnownMember.System_MemoryExtensions__SequenceEqual_ReadOnlySpan_T:
                     case WellKnownMember.System_MemoryExtensions__AsSpan_String:
