099f42de7c6e998be485e6c73e140894a2edaecb
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2025-12-08 12:47:04 +0100

    Simplify

--- src/EditorFeatures/Test2/GoToDefinition/GoToDefinitionCommandHandlerTests.vb
+++ src/EditorFeatures/Test2/GoToDefinition/GoToDefinitionCommandHandlerTests.vb
@@ -2,7 +2,6 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
-Imports Microsoft.Build
 Imports Microsoft.CodeAnalysis.Editor.[Shared].Extensions
 Imports Microsoft.CodeAnalysis.Editor.Shared.Utilities
 Imports Microsoft.CodeAnalysis.Editor.UnitTests.Utilities
