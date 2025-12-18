32e329fbbb3d5e00f95bc37c2c46dd2a7141f919
Cyrus Najmabadi <cyrusn@microsoft.com>
2025-12-17 22:11:23 -0500

    Cleanup

--- src/Features/Core/Portable/CodeFixes/CodeFixCollection.cs
+++ src/Features/Core/Portable/CodeFixes/CodeFixCollection.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Immutable;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.CodeFixes;
