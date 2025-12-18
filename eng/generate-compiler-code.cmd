4b094aa282b70e631a46927b62471b9260813e44
Jan Jones <janjones@microsoft.com>
2025-12-18 10:51:08 +0100

    Workaround up-to-date bug in generate-compiler-code.cs

--- eng/generate-compiler-code.cmd
+++ eng/generate-compiler-code.cmd
@@ -1,3 +1,2 @@
 @echo off
-call "%~dp0\common\dotnet.cmd" run --file "%~dp0\generate-compiler-code.cs" %* 
-
+call "%~dp0\common\dotnet.cmd" run --file "%~dp0\generate-compiler-code.cs" --no-cache %* 
