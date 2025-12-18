15c32c5b839c115aeafdd81a15b035cd76cbdebb
Jason Malinowski <jason.malinowski@microsoft.com>
2025-12-18 13:26:45 -0800

    Load the core RoslynPackage with a service rather than LoadPackage
    This trick means we don't have to switch to the UI thread to call
    LoadPackage, but can stay on the background thread the whole time.

---
+++ src/VisualStudio/Core/Def/RoslynPackageLoadService.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.LanguageServices.Setup;
+
+namespace Microsoft.VisualStudio.LanguageServices
+{
+    /// <summary>
+    /// A placeholder service that exists to trigger a package load of <see cref="RoslynPackage"/>. The only way to trigger an explicit load
+    /// is via <see cref="Microsoft.VisualStudio.Shell.Interop.IVsShell.LoadPackage"/>, but calling that API requires a transition of the UI thread first.
+    /// Requesting a service that is free-threaded instead can trigger the package load from the background thread in the first place.
+    /// </summary>
+    [Guid("89e9dfce-d0f3-48c4-be76-140d5cce69ef")]
+    internal sealed class RoslynPackageLoadService
+    {
+    }
+}
