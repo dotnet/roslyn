2b15a56aadefb6a1ddbd633fcfeee846cbee2fa4
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2026-01-05 22:59:20 -0500

    use patterns

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Extensions/Symbols/ISymbolExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Extensions/Symbols/ISymbolExtensions.cs
@@ -752,7 +752,7 @@ internal static partial class ISymbolExtensions
             .ToList();
 
         // T Current { get }
-        if (!members.OfType<IPropertySymbol>().Any(p => p.Name == WellKnownMemberNames.CurrentPropertyName && p.GetMethod != null))
+        if (!members.OfType<IPropertySymbol>().Any(p => p is { Name: WellKnownMemberNames.CurrentPropertyName, GetMethod: not null }))
         {
             return false;
         }
