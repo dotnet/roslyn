490b0d4d910761bd18d39cd634c362b37a407e61
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2026-01-06 21:18:10 -0500

    Docs

--- src/Analyzers/CSharp/Analyzers/RemoveUnnecessarySuppressions/UnnecessaryNullableWarningSuppressionsUtilities.cs
+++ src/Analyzers/CSharp/Analyzers/RemoveUnnecessarySuppressions/UnnecessaryNullableWarningSuppressionsUtilities.cs
@@ -71,33 +71,6 @@ internal static class UnnecessaryNullableWarningSuppressionsUtilities
         ForkSemanticModelAndCheckNodes(semanticModel, nodesToCheck, nodeToAnnotation, result, cancellationToken);
     }
 
-    private static SyntaxNode? GetNodeToCheck(SyntaxNode node)
-    {
-        var globalStatement = node.Ancestors().OfType<GlobalStatementSyntax>().FirstOrDefault();
-
-        if (globalStatement is not null)
-            return globalStatement;
-
-        // Otherwise, find our containing code-containing member (attributes, accessors, fields, methods, properties,
-        // anonymous methods), and check that entire member.  This means we only offer to remove the suppression if all
-        // the suppressions in the member are unnecessary.  We need this granularity as doing things on a
-        // per-suppression is just far too slow.
-        //
-        // We also check at this level because suppressions can have effects far outside of the containing statement (or
-        // even things like the containing block.  For example: a suppression inside a block like `a = b!` can affect
-        // the nullability of a variable which may be referenced far outside of the block.  So we really need to check
-        // the entire code region that the suppression is in to make an accurate determination.
-        var ancestor = node.Ancestors().FirstOrDefault(
-            n => n is AttributeSyntax
-                   or AccessorDeclarationSyntax
-                   or AnonymousMethodExpressionSyntax
-                   or BaseFieldDeclarationSyntax
-                   or BaseMethodDeclarationSyntax
-                   or BasePropertyDeclarationSyntax);
-
-        return ancestor;
-    }
-
     private static void ForkSemanticModelAndCheckNodes(
         SemanticModel semanticModel,
         ArrayBuilder<PostfixUnaryExpressionSyntax> nodesToCheck,
@@ -208,5 +181,32 @@ internal static class UnnecessaryNullableWarningSuppressionsUtilities
             if (!ContainsErrorOrWarning(intersectingDiagnostics))
                 result.AddRange(nodes.Select(t => t.suppression));
         }
+    }
+
+    private static SyntaxNode? GetNodeToCheck(SyntaxNode node)
+    {
+        var globalStatement = node.Ancestors().OfType<GlobalStatementSyntax>().FirstOrDefault();
+
+        if (globalStatement is not null)
+            return globalStatement;
+
+        // Otherwise, find our containing code-containing member (attributes, accessors, fields, methods, properties,
+        // anonymous methods), and check that entire member.  This means we only offer to remove the suppression if all
+        // the suppressions in the member are unnecessary.  We need this granularity as doing things on a
+        // per-suppression is just far too slow.
+        //
+        // We also check at this level because suppressions can have effects far outside of the containing statement (or
+        // even things like the containing block.  For example: a suppression inside a block like `a = b!` can affect
+        // the nullability of a variable which may be referenced far outside of the block.  So we really need to check
+        // the entire code region that the suppression is in to make an accurate determination.
+        var ancestor = node.Ancestors().FirstOrDefault(
+            n => n is AttributeSyntax
+                   or AccessorDeclarationSyntax
+                   or AnonymousMethodExpressionSyntax
+                   or BaseFieldDeclarationSyntax
+                   or BaseMethodDeclarationSyntax
+                   or BasePropertyDeclarationSyntax);
+
+        return ancestor;
     }
 }
