30fd86e6e74041fbeac4299a81790c70f1d186d0
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2026-01-12 14:00:26 +0100

    Add prototype

--- src/Compilers/CSharp/Test/Emit3/Semantics/CollectionExpressionTests_WithElement_Nullable.cs
+++ src/Compilers/CSharp/Test/Emit3/Semantics/CollectionExpressionTests_WithElement_Nullable.cs
@@ -143,6 +143,8 @@ public sealed class CollectionExpressionTests_WithElement_Nullable : CSharpTestB
     [Fact]
     public void ConstructorNonNullParameterPassedNull_Inference1()
     {
+        // PROTOTYPE: Currently, inference on Goo is producing `Goo<string>` even though it should be `Goo<string!>`.
+        // This is problematic as it then means T is oblivious and we don't properly report a warning on 'null'.
         var source = """
             #nullable enable
             using System;
