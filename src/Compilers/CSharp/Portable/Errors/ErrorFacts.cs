384ccc39a5733c54fc6dd2542575d57dc32e7586
DoctorKrolic <70431552+DoctorKrolic@users.noreply.github.com>
2026-01-18 00:36:14 +0300

    Add dedicated diagnostic for awaiting an `IAsyncEnumerable` (#82057)

--- src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
+++ src/Compilers/CSharp/Portable/Errors/ErrorFacts.cs
@@ -2559,6 +2559,7 @@ namespace Microsoft.CodeAnalysis.CSharp
                 or ErrorCode.ERR_InvalidModifierAfterScoped
                 or ErrorCode.ERR_StructLayoutAndExtendedLayout
                 or ErrorCode.ERR_RuntimeDoesNotSupportExtendedLayoutTypes
+                or ErrorCode.ERR_NoAwaitOnAsyncEnumerable
                     => false,
             };
 #pragma warning restore CS8524 // The switch expression does not handle some values of its input type (it is not exhaustive) involving an unnamed enum value.
