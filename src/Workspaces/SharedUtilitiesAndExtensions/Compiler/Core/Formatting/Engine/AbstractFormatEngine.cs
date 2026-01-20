dotnet/roslyn

commit e97fcbe5131508400209a56ba8a76c05f3713319

switch to tuple

src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Formatting/Engine/AbstractFormatEngine.cs
@@ -51,8 +51,11 @@ internal abstract partial class AbstractFormatEngine
     /// options. To help with that, cache the last set of ChainedFormattingRules that was produced, as it is not a cheap
     /// type to create.
     /// </summary>
-    private static readonly object s_gate = new();
-    private static (ImmutableArray<AbstractFormattingRule> formattingRules, SyntaxFormattingOptions options, ChainedFormattingRules chainedRules) s_lastRulesAndOptions;
+    /// <remarks>
+    /// Stored as a <see cref="Tuple{T1, T2, T3}"/> instead of a <see cref="ValueTuple{T1, T2, T3}"/> so we don't have
+    /// to worry about torn write concerns.
+    /// </remarks>
+    private static Tuple<ImmutableArray<AbstractFormattingRule>, SyntaxFormattingOptions, ChainedFormattingRules>? s_lastRulesAndOptions;
 
     public AbstractFormatEngine(
         TreeData treeData,
@@ -71,13 +74,14 @@ internal abstract partial class AbstractFormatEngine
 
     private static ChainedFormattingRules GetChainedFormattingRules(ImmutableArray<AbstractFormattingRule> formattingRules, SyntaxFormattingOptions options)
     {
-        lock (s_gate)
+        var lastRulesAndOptions = s_lastRulesAndOptions;
+        if (formattingRules != lastRulesAndOptions?.Item1 || options != s_lastRulesAndOptions?.Item2)
         {
-            if (formattingRules != s_lastRulesAndOptions.formattingRules || options != s_lastRulesAndOptions.options)
-                s_lastRulesAndOptions = (formattingRules, options, new ChainedFormattingRules(formattingRules, options));
-
-            return s_lastRulesAndOptions.chainedRules;
+            lastRulesAndOptions = Tuple.Create(formattingRules, options, new ChainedFormattingRules(formattingRules, options));
+            s_lastRulesAndOptions = lastRulesAndOptions;
         }
+
+        return lastRulesAndOptions.Item3;
     }
 
     internal AbstractFormatEngine(

src/Tools/ExternalAccess/OmniSharp/Formatting/OmniSharpFormatter.cs
@@ -15,7 +15,7 @@ namespace Microsoft.CodeAnalysis.ExternalAccess.OmniSharp.Formatting
     internal static class OmniSharpFormatter
     {
         public static Task<Document> FormatAsync(Document document, IEnumerable<TextSpan>? spans, OmniSharpSyntaxFormattingOptionsWrapper options, CancellationToken cancellationToken)
-            => Formatter.FormatAsync(document, spans, options.CleanupOptions.FormattingOptions, rules: null, cancellationToken);
+            => Formatter.FormatAsync(document, spans, options.CleanupOptions.FormattingOptions, rules: default, cancellationToken);
 
         public static async Task<Document> OrganizeImportsAsync(Document document, OmniSharpOrganizeImportsOptionsWrapper options, CancellationToken cancellationToken)
         {

src/Workspaces/CoreTestUtilities/Formatting/FormattingTestBase.cs
@@ -72,7 +72,7 @@ namespace Microsoft.CodeAnalysis.UnitTests.Formatting
         internal void AssertFormatWithTransformation(
             SolutionServices services, string expected, SyntaxNode root, IEnumerable<TextSpan> spans, SyntaxFormattingOptions options, bool treeCompare = true, ParseOptions? parseOptions = null)
         {
-            var newRootNode = Formatter.Format(root, spans, services, options, rules: null, CancellationToken.None);
+            var newRootNode = Formatter.Format(root, spans, services, options, rules: default, CancellationToken.None);
 
             Assert.Equal(expected, newRootNode.ToFullString());
 

commit 8dfb05c638f630618d306809357188c58d9bd082

Fix race in AbstractFormatEngine.GetChainedFormattingRules
On a project with mixed whitespace settings, occasionally a document
could be formatted (using dotnet-format) using the wrong settings.  This
was caused by a race in GetChainedFormattingRules, when
s_lastRulesAndOptions was written between the two reads.

I believe the second read was just a typo, so this change removes it.

Fixes: e97fcbe5131508400209a56ba8a76c05f3713319

src/Workspaces/SharedUtilitiesAndExtensions/Compiler/Core/Formatting/Engine/AbstractFormatEngine.cs
@@ -75,7 +75,7 @@ internal abstract partial class AbstractFormatEngine
     private static ChainedFormattingRules GetChainedFormattingRules(ImmutableArray<AbstractFormattingRule> formattingRules, SyntaxFormattingOptions options)
     {
         var lastRulesAndOptions = s_lastRulesAndOptions;
-        if (formattingRules != lastRulesAndOptions?.Item1 || options != s_lastRulesAndOptions?.Item2)
+        if (formattingRules != lastRulesAndOptions?.Item1 || options != lastRulesAndOptions?.Item2)
         {
             lastRulesAndOptions = Tuple.Create(formattingRules, options, new ChainedFormattingRules(formattingRules, options));
             s_lastRulesAndOptions = lastRulesAndOptions;