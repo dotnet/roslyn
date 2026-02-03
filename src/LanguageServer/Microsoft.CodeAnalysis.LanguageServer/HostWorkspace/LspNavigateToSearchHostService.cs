7d853935dbd73f5619c0abdd050267266f3efb75
David Barbet <dabarbet@microsoft.com>
2026-02-03 15:48:29 -0800

    remove usings

--- src/LanguageServer/Microsoft.CodeAnalysis.LanguageServer/HostWorkspace/LspNavigateToSearchHostService.cs
+++ src/LanguageServer/Microsoft.CodeAnalysis.LanguageServer/HostWorkspace/LspNavigateToSearchHostService.cs
@@ -2,10 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Composition;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.NavigateTo;
 
