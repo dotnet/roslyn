e3db663372d3ae1eb3a2ae81e9688c360eafe4c7
Fred Silberberg <frsilb@microsoft.com>
2026-02-25 10:49:05 -0800

    Add additional bullet point

--- src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
+++ src/Compilers/CSharp/Test/CommandLine/CommandLineTests.cs
@@ -1723,7 +1723,8 @@ class C
             // - [ ] update _MaxAvailableLangVersion cap (a relevant test should break when new version is introduced)
             // - [ ] update the "UpgradeProject" codefixer
             // - [ ] Remove the `ExperimentalUrl` section from any entries for language features being shipped in Syntax.xml and OperationInterfaces.xml, and rerun the generator
-            // - [ ] Search the codebase for references tied to issues linked from Syntax.xml and OperationInterfaces.xml, and remove suppressions or attributes added for those issues.
+            // - [ ] Search the codebase for references tied to issues linked from Syntax.xml and OperationInterfaces.xml, and remove suppressions or attributes added for those issues
+            // - [ ] Search for any remaining uses of `Experimental` on APIs and remove if the feature is being shipped in stable form
             // - [ ] test VS insertion and deal with breaking changes. (note: the runtime repo uses "preview" so breaks are resolved sooner)
             //
             // Other repos also need updates:
