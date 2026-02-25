be664393f4d30ffb22b34683cbf08409dea64005
Julien Couvreur <julien.couvreur@gmail.com>
2026-02-24 16:35:34 -0800

    Formatting

--- src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Extensions/LanguageVersionExtensions.cs
+++ src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Extensions/LanguageVersionExtensions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Shared.Extensions;
 internal static class LanguageVersionExtensions
 {
     public static bool IsCSharp15OrAbove(this LanguageVersion languageVersion)
-        => languageVersion >= LanguageVersion.Preview;
+        => languageVersion >= CSharpNext;
 
 #if ROSLYN_4_12_OR_LOWER
     public static bool IsCSharp14OrAbove(this LanguageVersion languageVersion)
