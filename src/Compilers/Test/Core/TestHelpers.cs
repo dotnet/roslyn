347221d194fab5310854cce2699eae7ac3b80b40
Rikki Gibson <rikkigibson@gmail.com>
2026-02-27 13:22:27 -0800

    add using

--- src/Compilers/Test/Core/TestHelpers.cs
+++ src/Compilers/Test/Core/TestHelpers.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
