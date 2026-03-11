fa3542d8c9bdec2e27ec2e0ab0e81efff0a236a4
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2026-03-11 19:06:13 +0200

    Update WellKnownTypeValidationTests.vb

--- src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/WellKnownTypeValidationTests.vb
+++ src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/WellKnownTypeValidationTests.vb
@@ -1,9 +1,8 @@
-﻿' Licensed to the .NET Foundation under one or more agreements.
+' Licensed to the .NET Foundation under one or more agreements.
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 
 Imports Basic.Reference.Assemblies
-Imports Internal.TypeSystem
 Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Roslyn.Test.Utilities
 
