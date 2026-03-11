30260eff34e6d375ac112f1642af2b090642a83f
Joey Robichaud <jorobich@microsoft.com>
2026-03-11 11:43:03 -0700

    Fix up tests

--- src/VisualStudio/Core/Test/Diagnostics/VisualStudioDiagnosticIdCacheTests.vb
+++ src/VisualStudio/Core/Test/Diagnostics/VisualStudioDiagnosticIdCacheTests.vb
@@ -9,6 +9,7 @@ Imports Microsoft.CodeAnalysis.Serialization
 Imports Microsoft.CodeAnalysis.Shared.TestHooks
 Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Microsoft.VisualStudio.LanguageServices.TaskList
+Imports Microsoft.VisualStudio.LanguageServices.UnitTests.ProjectSystemShim.Framework
 Imports Roslyn.Test.Utilities
 
 Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Diagnostics
@@ -17,31 +18,58 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Diagnostics
     Public Class VisualStudioDiagnosticIdCacheTests
         Private Shared ReadOnly s_composition As TestComposition = VisualStudioTestCompositions.LanguageServices
 
+        <WpfFact>
+        Public Async Function LegacyProject_RegistersProjectWithCache() As Task
+            Using environment = New TestEnvironment()
+                Dim workspace = environment.Workspace
+                Dim diagnosticIdCache = workspace.Services.GetRequiredService(Of VisualStudioDiagnosticIdCache)()
+                Dim listenerProvider = environment.ExportProvider.GetExportedValue(Of AsynchronousOperationListenerProvider)()
+
+                ' There should be no registered projects in an empty workspace.
+                Assert.Equal(0, diagnosticIdCache.GetTestAccessor().RegisteredProjectCount)
+
+                ' Create a legacy project.
+                Dim csharpProject = CSharpHelpers.CreateCSharpProject(environment, "Test")
+                Dim project = workspace.CurrentSolution.Projects.Single()
+
+                ' The project should have been registered with the cache but it should be empty until diagnostics are fetched.
+                Assert.Equal(1, diagnosticIdCache.GetTestAccessor().RegisteredProjectCount)
+                Assert.False(diagnosticIdCache.TryGetDiagnosticIds(project.Id, Nothing))
+
+                ' Waiting for DiagnosticService to process should populate the cache.
+                Await listenerProvider.WaitAllAsync(workspace, {FeatureAttribute.DiagnosticService})
+
+                ' Cache should now be populated and contain the legacy project id.
+                Assert.True(diagnosticIdCache.TryGetDiagnosticIds(project.Id, Nothing))
+            End Using
+        End Function
+
         <Fact>
         Public Async Function TestDiagnosticDescriptorCache_Populates() As Task
             Using workspace = EditorTestWorkspace.CreateCSharp("class A { }", composition:=s_composition)
+                Dim diagnosticIdCache = workspace.Services.GetRequiredService(Of VisualStudioDiagnosticIdCache)()
                 Dim listenerProvider = workspace.GetService(Of AsynchronousOperationListenerProvider)()
 
+                ' Create an in memory analyzer.
                 Dim analyzer = New DescriptorOnlyAnalyzer("CACHE001")
                 Dim analyzerReference = New AnalyzerImageReference(ImmutableArray.Create(Of DiagnosticAnalyzer)(analyzer))
                 SerializerService.TestAccessor.AddAnalyzerImageReference(analyzerReference)
 
+                ' Add the analyzer reference to our project.
                 Dim project = workspace.CurrentSolution.Projects.Single().AddAnalyzerReference(analyzerReference)
                 Assert.True(workspace.TryApplyChanges(project.Solution))
-
                 project = workspace.CurrentSolution.Projects.Single()
-                Dim service = workspace.Services.GetRequiredService(Of VisualStudioDiagnosticIdCache)()
 
-                Assert.False(service.TryGetDiagnosticIds(project.Id, Nothing))
+                ' The cache should not contain the project until it is registered.
+                Assert.False(diagnosticIdCache.TryGetDiagnosticIds(project.Id, Nothing))
 
-                service.RegisterProject(project.Id)
-                ' Waiting for DiagnosticService to process should populate the cache.
+                ' Register the project with cache (this mimics what LegacyProjects do automatically) and for it to populate.
+                diagnosticIdCache.RegisterProject(project.Id)
                 Await listenerProvider.WaitAllAsync(workspace, {FeatureAttribute.DiagnosticService})
 
+                ' Cache should be populated and contain the diagnostic id from our analyzer.
                 Dim diagnosticIds As ImmutableHashSet(Of String) = Nothing
-                Assert.True(service.TryGetDiagnosticIds(project.Id, diagnosticIds))
-
-                AssertEx.NotNull(diagnosticIds)
+                Assert.True(diagnosticIdCache.TryGetDiagnosticIds(project.Id, diagnosticIds))
                 Assert.Contains("CACHE001", diagnosticIds)
             End Using
         End Function
@@ -49,38 +77,44 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Diagnostics
         <Fact>
         Public Async Function TestDiagnosticDescriptorCache_RefreshesOnAnalyzerReferenceChange() As Task
             Using workspace = EditorTestWorkspace.CreateCSharp("class A { }", composition:=s_composition)
+                Dim diagnosticIdCache = workspace.Services.GetRequiredService(Of VisualStudioDiagnosticIdCache)()
                 Dim listenerProvider = workspace.GetService(Of AsynchronousOperationListenerProvider)()
 
+                ' Create an in memory analyzer.
                 Dim analyzer1 = New DescriptorOnlyAnalyzer("CACHE001")
                 Dim analyzerReference1 = New AnalyzerImageReference(ImmutableArray.Create(Of DiagnosticAnalyzer)(analyzer1))
                 SerializerService.TestAccessor.AddAnalyzerImageReference(analyzerReference1)
 
+                ' Add the analyze reference to our project.
                 Dim project = workspace.CurrentSolution.Projects.Single().AddAnalyzerReference(analyzerReference1)
                 Assert.True(workspace.TryApplyChanges(project.Solution))
-
                 project = workspace.CurrentSolution.Projects.Single()
-                Dim service = workspace.Services.GetRequiredService(Of VisualStudioDiagnosticIdCache)()
-                service.RegisterProject(project.Id)
 
+                ' Register the project with cache (this mimics what LegacyProjects do automatically) and for it to populate.
+                diagnosticIdCache.RegisterProject(project.Id)
                 Await listenerProvider.WaitAllAsync(workspace, {FeatureAttribute.DiagnosticService})
 
+                ' Cache should be populated and contain the diagnostic id from our analyzer.
                 Dim initialDiagnosticIds As ImmutableHashSet(Of String) = Nothing
-                Assert.True(service.TryGetDiagnosticIds(project.Id, initialDiagnosticIds))
+                Assert.True(diagnosticIdCache.TryGetDiagnosticIds(project.Id, initialDiagnosticIds))
                 Assert.Contains("CACHE001", initialDiagnosticIds)
 
+                ' Create an in memory analyzer with a new diagnostic id.
                 Dim analyzer2 = New DescriptorOnlyAnalyzer("CACHE002")
                 Dim analyzerReference2 = New AnalyzerImageReference(ImmutableArray.Create(Of DiagnosticAnalyzer)(analyzer2))
                 SerializerService.TestAccessor.AddAnalyzerImageReference(analyzerReference2)
 
+                ' Replace the analyzer references with only the new one.
                 project = workspace.CurrentSolution.Projects.Single().WithAnalyzerReferences({analyzerReference2})
                 Assert.True(workspace.TryApplyChanges(project.Solution))
+                project = workspace.CurrentSolution.Projects.Single()
 
+                ' Wait for the workspace change event to propagate to the cache and for the cache to refresh.
                 Await listenerProvider.WaitAllAsync(workspace, {FeatureAttribute.Workspace, FeatureAttribute.DiagnosticService})
 
-                project = workspace.CurrentSolution.Projects.Single()
+                ' The cache should now contain the new diagnostic id but not the old one.
                 Dim refreshedDiagnosticIds As ImmutableHashSet(Of String) = Nothing
-                Assert.True(service.TryGetDiagnosticIds(project.Id, refreshedDiagnosticIds))
-
+                Assert.True(diagnosticIdCache.TryGetDiagnosticIds(project.Id, refreshedDiagnosticIds))
                 Assert.Contains("CACHE002", refreshedDiagnosticIds)
                 Assert.DoesNotContain("CACHE001", refreshedDiagnosticIds)
             End Using
@@ -89,31 +123,37 @@ Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.Diagnostics
         <Fact>
         Public Async Function TestDiagnosticDescriptorCache_RemovesDeletedProjects() As Task
             Using workspace = EditorTestWorkspace.CreateCSharp("class A { }", composition:=s_composition)
+                Dim diagnosticIdCache = workspace.Services.GetRequiredService(Of VisualStudioDiagnosticIdCache)()
                 Dim listenerProvider = workspace.GetService(Of AsynchronousOperationListenerProvider)()
 
+                ' Create an in memory analyzer.
                 Dim analyzer = New DescriptorOnlyAnalyzer("CACHE001")
                 Dim analyzerReference = New AnalyzerImageReference(ImmutableArray.Create(Of DiagnosticAnalyzer)(analyzer))
                 SerializerService.TestAccessor.AddAnalyzerImageReference(analyzerReference)
 
+                ' Add the analyze reference to our project.
                 Dim project = workspace.CurrentSolution.Projects.Single().AddAnalyzerReference(analyzerReference)
                 Assert.True(workspace.TryApplyChanges(project.Solution))
-
                 project = workspace.CurrentSolution.Projects.Single()
-                Dim projectId = project.Id
-                Dim service = workspace.Services.GetRequiredService(Of VisualStudioDiagnosticIdCache)()
-                service.RegisterProject(projectId)
 
+                ' Register the project with cache (this mimics what LegacyProjects do automatically) and for it to populate.
+                diagnosticIdCache.RegisterProject(project.Id)
                 Await listenerProvider.WaitAllAsync(workspace, {FeatureAttribute.DiagnosticService})
 
-                Assert.True(service.TryGetDiagnosticIds(projectId, Nothing))
+                ' The cache should now contain the project and have one registered project.
+                Assert.True(diagnosticIdCache.TryGetDiagnosticIds(project.Id, Nothing))
+                Assert.Equal(1, diagnosticIdCache.GetTestAccessor().RegisteredProjectCount)
 
-                Dim newSolution = workspace.CurrentSolution.RemoveProject(projectId)
+                ' Remove the project from the workspace.
+                Dim newSolution = workspace.CurrentSolution.RemoveProject(project.Id)
                 Assert.True(workspace.TryApplyChanges(newSolution))
 
+                ' Wait for the workspace change event to propagate to the cache and for the cache to refresh.
                 Await listenerProvider.WaitAllAsync(workspace, {FeatureAttribute.Workspace, FeatureAttribute.DiagnosticService})
 
-                Assert.False(service.TryGetDiagnosticIds(projectId, Nothing))
-                Assert.Equal(0, service.GetTestAccessor().RegisteredProjectCount)
+                ' Ensure the cache no longer contains the project and it has been unregistered.
+                Assert.False(diagnosticIdCache.TryGetDiagnosticIds(project.Id, Nothing))
+                Assert.Equal(0, diagnosticIdCache.GetTestAccessor().RegisteredProjectCount)
             End Using
         End Function
 
---
+++ src/VisualStudio/TestUtilities2/ProjectSystemShim/CSharpHelpers/CSharpHelpers.vb
@@ -0,0 +1,32 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports System.IO
+Imports Microsoft.VisualStudio.LanguageServices.CSharp.ProjectSystemShim
+Imports Microsoft.VisualStudio.LanguageServices.UnitTests.ProjectSystemShim.Framework
+Imports Microsoft.VisualStudio.Shell.Interop
+
+Namespace Microsoft.VisualStudio.LanguageServices.UnitTests.CSharpHelpers
+
+    Friend Module CSharpHelpers
+
+        Public Function CreateCSharpProject(environment As TestEnvironment, projectName As String) As CSharpProjectShim
+            Dim projectBinPath = Path.GetTempPath()
+            Dim hierarchy = environment.CreateHierarchy(projectName, projectBinPath, projectRefPath:=Nothing, projectCapabilities:="CSharp")
+
+            Return CreateCSharpProject(environment, projectName, hierarchy)
+        End Function
+
+        Public Function CreateCSharpProject(environment As TestEnvironment, projectName As String, hierarchy As IVsHierarchy) As CSharpProjectShim
+            Return New CSharpProjectShim(
+                New MockCSharpProjectRoot(hierarchy),
+                projectSystemName:=projectName,
+                hierarchy:=hierarchy,
+                serviceProvider:=environment.ServiceProvider,
+                threadingContext:=environment.ThreadingContext)
+        End Function
+
+    End Module
+
+End Namespace
