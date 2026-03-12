0670e394b810f433c565c5d0fc42039dbe0b3fa6
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2026-03-12 03:54:13 +0200

    Update PatternMatchingTests_ListPatterns.cs

--- src/Compilers/CSharp/Test/Emit3/Semantics/PatternMatchingTests_ListPatterns.cs
+++ src/Compilers/CSharp/Test/Emit3/Semantics/PatternMatchingTests_ListPatterns.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -3500,9 +3500,8 @@ public class C
             "_ = new C()[..];"
         };
 
-        for (int i = 0; i < sources.Length; i++)
+        foreach (string source in sources)
         {
-            string source = sources[i];
             var comp = CreateCompilation(source, references: new[] { libComp.EmitToImageReference(), rangeRef });
             comp.VerifyDiagnostics();
             var used = comp.GetUsedAssemblyReferences();
