dotnet/razor

Add a new analyzer + baseline for existing: - Adds a new analyzer that looks for creations of IntermediateToken with a type of CSharp - Adds a baseline file that covers all existing usages - Warns if any new usages are introduced - Adds a startup profile for the local analyzers that runs them on the compiler project

src/Analyzers/Razor.Diagnostics.Analyzers/DiagnosticIds.cs
@@ -8,4 +8,6 @@ internal static class DiagnosticIds
     public const string PooledArrayBuilderAsRef = "RZD001";
 
     public const string IRemoteJsonServiceParameter = "RZD002";
+
+    public const string RawIntermediateTokenCreation = "RZD003";
 }

dotnet/roslyn

src/Razor/src/Analyzers/Razor.Diagnostics.Analyzers/DiagnosticIds.cs
@@ -10,4 +10,6 @@ internal static class DiagnosticIds
     public const string IRemoteJsonServiceParameter = "RZD002";
 
     public const string ImmutableArrayBoxing = "RZD003";
+
+    public const string RawIntermediateTokenCreation = "RZD004";
 }