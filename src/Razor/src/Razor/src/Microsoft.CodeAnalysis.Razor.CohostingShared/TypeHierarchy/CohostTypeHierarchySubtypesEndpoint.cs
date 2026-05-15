60512eee424bad86bda441db84559d083a1b62ec
David Wengier <david.wengier@microsoft.com>
2026-05-16 01:21:03 +1000

    Fix main build (#83719)
    #83637 added code that didn't compile after #83669 and #83668 were
    merged to main.
    
    They're like a bad Captain Planet.
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/dotnet/roslyn/pull/83719)

--- src/Razor/src/Razor/src/Microsoft.CodeAnalysis.Razor.CohostingShared/TypeHierarchy/CohostTypeHierarchySubtypesEndpoint.cs
+++ src/Razor/src/Razor/src/Microsoft.CodeAnalysis.Razor.CohostingShared/TypeHierarchy/CohostTypeHierarchySubtypesEndpoint.cs
@@ -6,7 +6,6 @@ using System.Composition;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor.Features;
 using Microsoft.CodeAnalysis.Razor;
 using Microsoft.CodeAnalysis.Razor.Cohost;
@@ -32,8 +31,8 @@ internal sealed class CohostTypeHierarchySubtypesEndpoint(
 
     protected override bool RequiresLSPSolution => true;
 
-    protected override RazorTextDocumentIdentifier? GetRazorTextDocumentIdentifier(TypeHierarchySubtypesParams request)
-        => RazorTypeHierarchyResolveData.Unwrap(request.Item)?.TextDocument.ToRazorTextDocumentIdentifier();
+    protected override TextDocumentIdentifier? GetRazorTextDocumentIdentifier(TypeHierarchySubtypesParams request)
+        => RazorTypeHierarchyResolveData.Unwrap(request.Item)?.TextDocument;
 
     protected override async Task<TypeHierarchyItem[]?> HandleRequestAsync(TypeHierarchySubtypesParams request, TextDocument razorDocument, CancellationToken cancellationToken)
     {
@@ -61,7 +60,7 @@ internal sealed class CohostTypeHierarchySubtypesEndpoint(
 
     private static TypeHierarchyItem WrapRazorItem(TypeHierarchyItem item)
     {
-        var uri = item.Uri.GetRequiredParsedUri();
+        var uri = item.Uri.GetRequiredSystemUri();
         return uri.GetDocumentFilePathFromUri().IsRazorFilePath()
             ? RazorTypeHierarchyResolveData.Wrap(item, new TextDocumentIdentifier { DocumentUri = item.Uri })
             : item;
