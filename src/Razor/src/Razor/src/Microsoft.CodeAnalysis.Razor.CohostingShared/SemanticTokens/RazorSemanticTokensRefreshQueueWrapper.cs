c1f5341972468d550afe086a617a1413cdb58c25
David Wengier <david.wengier@microsoft.com>
2026-05-25 08:00:34 +1000

    Remove unused using

--- src/Razor/src/Razor/src/Microsoft.CodeAnalysis.Razor.CohostingShared/SemanticTokens/RazorSemanticTokensRefreshQueueWrapper.cs
+++ src/Razor/src/Razor/src/Microsoft.CodeAnalysis.Razor.CohostingShared/SemanticTokens/RazorSemanticTokensRefreshQueueWrapper.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Composition;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor.Features;
