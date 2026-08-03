6af07ff409903f9a7b1a0d9587629f8a83a646bd
Hamish Arblaster <hamarb123@gmail.com>
2026-08-03 23:23:47 +1000

    thanks for that vs

--- src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/Source/TypeTests.vb
+++ src/Compilers/VisualBasic/Test/Symbol/SymbolsTests/Source/TypeTests.vb
@@ -5,7 +5,6 @@
 Imports System.Collections.Immutable
 Imports System.Runtime.InteropServices
 Imports Basic.Reference.Assemblies
-Imports ICSharpCode.Decompiler.IL
 Imports Microsoft.CodeAnalysis.Test.Utilities
 Imports Microsoft.CodeAnalysis.Text
 Imports Microsoft.CodeAnalysis.VisualBasic.Symbols
