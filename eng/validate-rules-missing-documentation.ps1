b72af6b7e7e4e3b18b9ff04c8a2106692679a002
Joey Robichaud <jorobich@microsoft.com>
2026-08-05 14:13:52 -0700

    Revert net11 tool change and add roll to prerelease

--- eng/validate-rules-missing-documentation.ps1
+++ eng/validate-rules-missing-documentation.ps1
@@ -13,7 +13,8 @@ try {
   $prepareMachine = $ci
 
   $projectFilePath = Join-Path $RepoRoot "src\Features\Core\Portable\Microsoft.CodeAnalysis.Features.csproj"
-  Exec-DotNet "build $projectFilePath -t:GenerateRulesMissingDocumentation -p:RoslynEnforceCodeStyle=false -p:RunAnalyzersDuringBuild=false -p:ContinuousIntegrationBuild=$ci -c Release"
+  $env:DOTNET_ROLL_FORWARD_TO_PRERELEASE = "1"
+  Exec-DotNet "--roll-forward major build $projectFilePath -t:GenerateRulesMissingDocumentation -p:RoslynEnforceCodeStyle=false -p:RunAnalyzersDuringBuild=false -p:ContinuousIntegrationBuild=$ci -c Release"
 }
 catch {
   Write-Host $_
