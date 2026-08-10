8d042e3fc8ed3394963e549ebb81eba4635f22ec
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2026-08-10 17:32:13 +0200

    Apply suggestion from @CyrusNajmabadi

--- src/Compilers/CSharp/Portable/Symbols/Source/ModifierUtils.cs
+++ src/Compilers/CSharp/Portable/Symbols/Source/ModifierUtils.cs
@@ -487,7 +487,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols
                 var messageId = isForTypeDeclaration ? MessageID.IDS_FeaturePartialTypes : MessageID.IDS_FeaturePartialMethod;
                 messageId.CheckFeatureAvailability(diagnostics, modifier);
 
-                // `partial` must always be the last modifier according to the language. However, there was a bug
+                // `partial` must always be the last modifier according to the language.  However, there was a bug
                 // where we allowed `partial async` at the end of modifiers on methods. We keep this behavior for
                 // backcompat.
                 var isLast = i == modifiers.Count - 1;
