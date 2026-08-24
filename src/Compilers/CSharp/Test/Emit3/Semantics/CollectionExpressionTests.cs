430db72edbe3ff3c68572ea2447701142813a425
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2026-08-24 15:34:43 +0800

    Update test codegen

--- src/Compilers/CSharp/Test/Emit3/Semantics/CollectionExpressionTests.cs
+++ src/Compilers/CSharp/Test/Emit3/Semantics/CollectionExpressionTests.cs
@@ -17138,35 +17138,37 @@ partial class Program
             verifier.VerifyIL("Program.F2",
                 """
                 {
-                  // Code size       79 (0x4f)
-                  .maxstack  2
+                  // Code size       97 (0x61)
+                  .maxstack  3
                   .locals init (System.Collections.Generic.List<object> V_0,
-                                System.Collections.Generic.IEnumerator<object> V_1,
-                                object V_2)
+                                System.Collections.Generic.List<object> V_1,
+                                System.Collections.Generic.IEnumerator<object> V_2,
+                                object V_3,
+                                System.ReadOnlySpan<object> V_4)
                   IL_0000:  newobj     "System.Collections.Generic.List<object>..ctor()"
                   IL_0005:  stloc.0
                   IL_0006:  ldarga.s   V_0
                   IL_0008:  call       "System.Collections.Generic.IEnumerator<object> MyCollection<object>.GetEnumerator()"
-                  IL_000d:  stloc.1
+                  IL_000d:  stloc.2
                   .try
                   {
                     IL_000e:  br.s       IL_001e
-                    IL_0010:  ldloc.1
+                    IL_0010:  ldloc.2
                     IL_0011:  callvirt   "object System.Collections.Generic.IEnumerator<object>.Current.get"
-                    IL_0016:  stloc.2
+                    IL_0016:  stloc.3
                     IL_0017:  ldloc.0
-                    IL_0018:  ldloc.2
+                    IL_0018:  ldloc.3
                     IL_0019:  callvirt   "void System.Collections.Generic.List<object>.Add(object)"
-                    IL_001e:  ldloc.1
+                    IL_001e:  ldloc.2
                     IL_001f:  callvirt   "bool System.Collections.IEnumerator.MoveNext()"
                     IL_0024:  brtrue.s   IL_0010
                     IL_0026:  leave.s    IL_0032
                   }
                   finally
                   {
-                    IL_0028:  ldloc.1
+                    IL_0028:  ldloc.2
                     IL_0029:  brfalse.s  IL_0031
-                    IL_002b:  ldloc.1
+                    IL_002b:  ldloc.2
                     IL_002c:  callvirt   "void System.IDisposable.Dispose()"
                     IL_0031:  endfinally
                   }
@@ -17175,10 +17177,18 @@ partial class Program
                   IL_0034:  box        "int"
                   IL_0039:  callvirt   "void System.Collections.Generic.List<object>.Add(object)"
                   IL_003e:  ldloc.0
-                  IL_003f:  callvirt   "object[] System.Collections.Generic.List<object>.ToArray()"
-                  IL_0044:  newobj     "System.ReadOnlySpan<object>..ctor(object[])"
-                  IL_0049:  call       "MyCollection<object> MyCollectionBuilder.Create<object>(System.ReadOnlySpan<object>)"
-                  IL_004e:  ret
+                  IL_003f:  stloc.1
+                  IL_0040:  ldloc.1
+                  IL_0041:  call       "System.Span<object> System.Runtime.InteropServices.CollectionsMarshal.AsSpan<object>(System.Collections.Generic.List<object>)"
+                  IL_0046:  call       "System.ReadOnlySpan<object> System.Span<object>.op_Implicit(System.Span<object>)"
+                  IL_004b:  stloc.s    V_4
+                  IL_004d:  ldloca.s   V_4
+                  IL_004f:  ldc.i4.0
+                  IL_0050:  ldloc.1
+                  IL_0051:  callvirt   "int System.Collections.Generic.List<object>.Count.get"
+                  IL_0056:  call       "System.ReadOnlySpan<object> System.ReadOnlySpan<object>.Slice(int, int)"
+                  IL_005b:  call       "MyCollection<object> MyCollectionBuilder.Create<object>(System.ReadOnlySpan<object>)"
+                  IL_0060:  ret
                 }
                 """);
         }
