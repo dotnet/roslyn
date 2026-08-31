3a42c066ef47d7b4d8dc001bdfe68a0791462da7
Joey Robichaud <jorobich@microsoft.com>
2026-08-31 10:24:12 -0700

    Apply suggestion from @JoeRobich

--- src/CodeStyle/Tools/Program.cs
+++ src/CodeStyle/Tools/Program.cs
@@ -246,8 +246,8 @@ public static class Program
                                                           '$(AnalysisLevelStyle)' != ''">$(AnalysisLevelStyle)</EffectiveAnalysisLevelStyle>
 
                       <!-- Check whether the analysis level is high enough that we should include the analyzer configuration.
-                           From .NET 11, the global config is systematically added if the file exists. Please check https://github.com/dotnet/roslyn/pull/71173 for more info. -->
-                      <_IncludeStyleConfiguration Condition="'$(EffectiveAnalysisLevelStyle)' != '' and $([MSBuild]::VersionGreaterThanOrEquals('$(EffectiveAnalysisLevelStyle)', '11.0'))">true<_IncludeStyleConfiguration>
+                           From .NET 12, the global config is systematically added if the file exists. Please check https://github.com/dotnet/roslyn/pull/71173 for more info. -->
+                      <_IncludeStyleConfiguration Condition="'$(EffectiveAnalysisLevelStyle)' != '' and $([MSBuild]::VersionGreaterThanOrEquals('$(EffectiveAnalysisLevelStyle)', '12.0'))">true<_IncludeStyleConfiguration>
 
                       <!-- Set the default analysis mode, if not set by the user -->
                       <_GlobalAnalyzerConfigAnalysisMode_MicrosoftCodeAnalysis{language}CodeStyle>$(AnalysisModeStyle)</_GlobalAnalyzerConfigAnalysisMode_MicrosoftCodeAnalysis{language}CodeStyle>
