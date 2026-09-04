5bf8338bfbd2a89b4d6290e21b4902ff21a84c9b
David Barbet <dabarbet@microsoft.com>
2026-09-04 11:21:04 -0700

    remove unnecessary devkit dependency path check

--- src/Workspaces/CoreTest/Log/RoslynTelemetryTests.cs
+++ src/Workspaces/CoreTest/Log/RoslynTelemetryTests.cs
@@ -162,7 +162,8 @@ public sealed class RoslynTelemetryTests
     }
 
     [Fact]
-    public void RequestScopeRestoresServerInstance()    {
+    public void RequestScopeRestoresServerInstance()
+    {
         var previousTelemetry = RoslynTelemetry.Current;
         var serverTelemetry = new RoslynTelemetry();
 
