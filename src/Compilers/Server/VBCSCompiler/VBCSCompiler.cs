cca6ce1013c4a7800a6bf3b2970d2dfa6f5cd007
Jan Jones <janjones@microsoft.com>
2026-09-07 17:53:12 +0200

    Fix compiler server logging

--- src/Compilers/Server/VBCSCompiler/VBCSCompiler.cs
+++ src/Compilers/Server/VBCSCompiler/VBCSCompiler.cs
@@ -19,7 +19,9 @@ namespace Microsoft.CodeAnalysis.CompilerServer
                 return CommonCompiler.Failed;
             }
 
-            using var logger = new CompilerServerLogger($"VBCSCompiler {Process.GetCurrentProcess().Id}", options.LogFilePath);
+            using var logger = options.LogFilePath is null
+                ? new CompilerServerLogger($"VBCSCompiler {Process.GetCurrentProcess().Id}", StandardBuildEnvironment.Instance)
+                : new CompilerServerLogger($"VBCSCompiler {Process.GetCurrentProcess().Id}", options.LogFilePath);
 
 #if BOOTSTRAP
             ExitingTraceListener.Install(logger);
