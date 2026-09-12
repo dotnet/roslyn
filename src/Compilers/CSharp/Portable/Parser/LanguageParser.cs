1edcf224f12484633d55284a7fe432b0d6ab769c
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2026-09-12 15:00:16 -0400

    Simplify partial modifier lookahead

--- src/Compilers/CSharp/Portable/Parser/LanguageParser.cs
+++ src/Compilers/CSharp/Portable/Parser/LanguageParser.cs
@@ -1639,25 +1639,19 @@ namespace Microsoft.CodeAnalysis.CSharp.Syntax.InternalSyntax
             if (this.IsUnambiguousAnonymousFunctionModifierListFollowedByOpenParen())
                 return false;
 
-            using var _ = this.GetDisposableResetPoint(resetOnDispose: true);
-
-            this.EatToken();
+            var nextToken = this.PeekToken(1);
 
             // '(' may start either the parameter list for a member named 'partial' or a tuple
             // return type. A tuple type followed by a member name proves that 'partial' is a modifier.
-            if (this.CurrentToken.Kind == SyntaxKind.OpenParenToken)
+            if (nextToken.Kind == SyntaxKind.OpenParenToken)
             {
+                using var _ = this.GetDisposableResetPoint(resetOnDispose: true);
+                this.EatToken();
                 return this.IsTypeFollowedByMemberName();
             }
 
-            // These tokens prove that 'partial' is the member name.
-            if (IsDefinitePartialMemberNameContinuation(this.CurrentToken.Kind))
-            {
-                return false;
-            }
-
-            // Nothing proved that 'partial' was the member name, so it is a modifier.
-            return true;
+            // If the next token does not prove that 'partial' is the member name, it is a modifier.
+            return !IsDefinitePartialMemberNameContinuation(nextToken.Kind);
         }
 
         private static bool IsDefinitePartialMemberNameContinuation(SyntaxKind kind)
