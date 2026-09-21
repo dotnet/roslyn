5c8370aaeba88468840502cda5c903545b1e4e2a
David Wengier <git@wengier.com>
2026-09-21 19:35:06 +1000

    Fold documentation directives
    Documentation blocks need their own folding behaviour without changing code-block folding. A dedicated provider follows the existing syntax-node provider pattern.

---
+++ src/Razor/src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/FoldingRanges/RazorDocumentationFoldingProvider.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Language.Syntax;
+
+namespace Microsoft.CodeAnalysis.Remote.Razor.FoldingRanges;
+
+[Shared]
+[Export(typeof(IRazorFoldingRangeProvider))]
+internal sealed class RazorDocumentationFoldingProvider : AbstractSyntaxNodeFoldingProvider<RazorDocumentationDirectiveSyntax>
+{
+    protected override string GetCollapsedText(RazorDocumentationDirectiveSyntax node)
+        => "@documentation";
+
+    protected override ImmutableArray<RazorDocumentationDirectiveSyntax> GetFoldableNodes(RazorSyntaxTree syntaxTree)
+        => syntaxTree.GetDirectives<RazorDocumentationDirectiveSyntax>();
+}
