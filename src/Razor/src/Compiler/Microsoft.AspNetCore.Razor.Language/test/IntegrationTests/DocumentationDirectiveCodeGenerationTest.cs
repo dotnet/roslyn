7cc90f6ee1cb61ede0d5d79acbc2835385d53d99
David Wengier <git@wengier.com>
2026-09-21 18:28:02 +1000

    Demonstrate documentation compiler output
    These two examples show the entire compiler feature from Razor syntax to generated C#. Keeping their source and baselines together gives reviewers a small starting point before the rest of the test matrix.

---
+++ src/Razor/src/Compiler/Microsoft.AspNetCore.Razor.Language/test/IntegrationTests/DocumentationDirectiveCodeGenerationTest.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+using System.Runtime.CompilerServices;
+using Xunit;
+using WorkItemAttribute = Roslyn.Test.Utilities.WorkItemAttribute;
+
+namespace Microsoft.AspNetCore.Razor.Language.IntegrationTests;
+
+[WorkItem("https://github.com/dotnet/roslyn/issues/85414")]
+public class DocumentationDirectiveCodeGenerationTest()
+    : RazorBaselineIntegrationTestBase(TestProject.Layer.Compiler)
+{
+    internal override RazorFileKind? FileKind => RazorFileKind.Component;
+    internal override string DefaultFileName => "TestComponent.razor";
+
+    protected override string GetDirectoryPath(string testName)
+        => Path.Combine("TestFiles", "IntegrationTests", nameof(DocumentationDirectiveCodeGenerationTest), testName);
+
+    [Fact]
+    public void Documentation()
+    {
+        VerifyBaseline("""
+            @attribute [System.Serializable]
+            @documentation {
+                <summary>
+                Hello from <see cref="System.String"/>.
+                </summary>
+                <remarks>More documentation.</remarks>
+            }
+            <p>Rendered content</p>
+            """);
+    }
+
+    private CompileToCSharpResult VerifyBaseline(
+        string source,
+        RazorConfiguration? configuration = null,
+        [CallerMemberName] string testName = "")
+    {
+        var result = CompileToCSharp(source, configuration: configuration);
+        AssertSyntaxTreeMatchesBaseline(result.CodeDocument, testName);
+        AssertDocumentNodeMatchesBaseline(result.CodeDocument, testName);
+        AssertCSharpDocumentMatchesBaseline(result.CodeDocument, testName: testName);
+        CompileToAssembly(result);
+        return result;
+    }
+}
---
+++ src/Razor/src/Compiler/Microsoft.AspNetCore.Razor.Language/test/IntegrationTests/DocumentationDirectiveCodeGenerationTest_Legacy.cs
@@ -0,0 +1,60 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc.Razor.Extensions;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+using WorkItemAttribute = Roslyn.Test.Utilities.WorkItemAttribute;
+
+namespace Microsoft.AspNetCore.Razor.Language.IntegrationTests;
+
+[WorkItem("https://github.com/dotnet/roslyn/issues/85414")]
+public class DocumentationDirectiveCodeGenerationTest_Legacy()
+    : IntegrationTestBase(TestProject.Layer.Compiler)
+{
+    private RazorConfiguration _configuration =
+        RazorConfiguration.Default with { LanguageVersion = RazorLanguageVersion.Preview };
+
+    protected override RazorConfiguration Configuration => _configuration;
+
+    protected override CSharpParseOptions CSharpParseOptions { get; } =
+        new(LanguageVersion.Preview, documentationMode: DocumentationMode.Diagnose);
+
+    protected override void ConfigureProjectEngine(RazorProjectEngineBuilder builder)
+        => RazorExtensions.Register(builder);
+
+    [Fact]
+    public void Documentation()
+    {
+        var result = VerifyBaselineWithSourceMappings();
+
+        var compiled = CompileToAssembly(result);
+        var @namespace = result.CodeDocument.GetRequiredDocumentNode().FindPrimaryNamespace();
+        var @class = result.CodeDocument.GetRequiredDocumentNode().FindPrimaryClass();
+        Assert.NotNull(@namespace);
+        Assert.NotNull(@class);
+        var type = compiled.Compilation.GetTypeByMetadataName($"{@namespace.Name}.{@class.Name}");
+        Assert.NotNull(type);
+        Assert.Contains("<remarks>More documentation.</remarks>", type.GetDocumentationCommentXml());
+    }
+
+    private CompiledCSharpCode VerifyBaseline([CallerMemberName] string testName = "")
+    {
+        var result = CompileToCSharp(CreateProjectItemFromFile(testName: testName));
+        AssertSyntaxTreeMatchesBaseline(result.CodeDocument, testName);
+        AssertDocumentNodeMatchesBaseline(result.CodeDocument.GetRequiredDocumentNode(), testName);
+        AssertCSharpDocumentMatchesBaseline(result.CodeDocument.GetRequiredImplCSharpDocument(), testName);
+
+        return result;
+    }
+
+    private CompiledCSharpCode VerifyBaselineWithSourceMappings([CallerMemberName] string testName = "")
+    {
+        var result = VerifyBaseline(testName);
+        AssertSourceMappingsMatchBaseline(result.CodeDocument, testName);
+        return result;
+    }
+}
